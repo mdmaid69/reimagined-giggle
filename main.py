5297fc2efd2ab02d12db8de39223d72a73000096
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:21:04 +0000

    spare practise meal unable sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
