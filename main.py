09ee46845bb8c99b90831fda31124ab3cd69f739
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:33:53 +0000

    trouble feeling impossible correct near

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_current_directory():
-        return os.getcwd()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
