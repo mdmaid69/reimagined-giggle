bc0b3f8db80d29270dc43e1418b7c102518acb6e
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:50:58 +0000

    gather fence learn design trap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
