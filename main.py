203deed04ed22de0716d90e007f8e3ecb1b02e1d
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:57:06 +0000

    experiment border gooff earth replace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
