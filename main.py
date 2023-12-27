ebd4f3c6f87e0c850c87fbf448ab2df8cfcdd5c0
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:01:06 +0000

    among stay hold relationship strong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
