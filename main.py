8d303cb43dd47c0c01d426147a64a426ab55fb73
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:19:53 +0000

    trust special swimming responsibility though

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_typecode(array):
+        return array.typecode
