58ed7705d66ffe9317fa05b249ba1cba7858fc2d
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:43:45 +0000

    constantly quality coffee anybody list

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+for i in range(10): print(i)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
