ad80bc223f4b84bd331d2c707e4fb744891ee864
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:00:53 +0000

    speech advice cost start lose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
