19a160abcf00e957b50f67b9cc50f95c86b9f02c
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:22:36 +0000

    door land fairly sticky comeup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_density(mass, volume):
+        return mass / volume
