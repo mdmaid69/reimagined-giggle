e452fa14df84fdb1e25d09bedec9e7b126ce2b31
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:27:37 +0000

    closed usually aware tomorrow pin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import tensorflow as tf
-print(tf.__version__)
+import collections
+def create_counter():
+        return collections.Counter()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
