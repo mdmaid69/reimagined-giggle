535ce856f3421503131cb2e4178b2cdc27f2e0ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:47:06 +0000

    emergency load certain serious copy

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-print(math.pi)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
