9d9ceed253ff80d470ad8ad879c09c706f094e31
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:39:02 +0000

    agree takeover minute bath owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
