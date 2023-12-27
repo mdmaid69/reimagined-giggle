e36743d0e4e5662d7b014e8f7439c4f073592792
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:38:17 +0000

    me justice acceptable another season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_acceleration(speed, time):
+        return speed / time
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_floor(x):
+        return math.floor(x)
