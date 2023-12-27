822bfd508b8c7f2728b79bc6eb7325b75ce8f436
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:42:35 +0000

    unpleasant happen coat bill intend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  def multiply_numbers(x, y):
+        return x * y
