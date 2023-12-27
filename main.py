464130657346b6b47492d79d5c0cbb659e05394e
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:15:46 +0000

    comefrom little float expensive question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import platform
+def get_python_version():
+        return platform.python_version()
+import sys
+def print_python_version():
+        print(sys.version)
