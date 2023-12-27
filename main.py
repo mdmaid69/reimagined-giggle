d8bbef097a30f0367ae6f32f0af696369a7b79e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:13:36 +0000

    die thin offence sink noise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import platform
+def get_os_info():
+        return platform.uname()
+for i in range(5):
+        print(i)
