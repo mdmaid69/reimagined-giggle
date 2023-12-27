17245dc2422694b383a1fd95c28ad55f315c9f48
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:30:27 +0000

    quickly force get swimming name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def subtract_numbers(x, y):
+        return x - y
