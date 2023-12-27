9ebf61b2ff35614e38f33db0e96092afb952cc11
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:56:47 +0000

    cause rush stamp type below

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-name = "Python"
-print("Hello,", name)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+print("Hello, world!")
