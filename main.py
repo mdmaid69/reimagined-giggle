3c5533fe3f7ed51b9657366a3670b138dd20bc0c
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:47:17 +0000

    substance any extraordinary lend variety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import os
-print(os.getcwd())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
