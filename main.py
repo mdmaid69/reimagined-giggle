9bbf376cda1b27001a5ec4d433729206a46f1c8b
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:51:29 +0000

    congratulations react away sink pin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
