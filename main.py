de70b36b71f8361a284c27f454700a89f2667ea1
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:17:24 +0000

    capital area mark rescue show

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
