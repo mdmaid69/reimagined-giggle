7b78ea15d6efe1f491ea027dca53a14dc1765b6a
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:11:34 +0000

    increase her forget smell goaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
