72c2bba7b8f32bb0f94202848d8171bfbc073f74
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:07:20 +0000

    throughout worried chase burn sort

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def cube_number(x):
-        return x**3
+import collections
+def create_user_dict():
+        return collections.UserDict()
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
