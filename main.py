abd45d98c08f876311547f935cfec3edf86c200b
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:29:13 +0000

    distance shock run wish branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import platform
-def get_python_version():
-        return platform.python_version()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
