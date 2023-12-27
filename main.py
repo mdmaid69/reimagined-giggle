d964c6160bae3a31ff8d7f240b4dca86c80ec278
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:14:24 +0000

    anywhere foot wide terrible responsibility

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
