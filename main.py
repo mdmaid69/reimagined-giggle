a7578268b9ef6a21981d1adbab3ebad103b0084f
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:34:21 +0000

    workout among unable family up

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def cube_number(x):
-        return x**3
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def multiply_numbers(x, y):
+        return x * y
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
