55b7dd2bdc2801f7125b2315432ebb6f547fcf86
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:08:54 +0000

    drop party chance record keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
