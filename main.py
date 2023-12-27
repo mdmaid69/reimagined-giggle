0d7c66c2305a71d5caee938b5542f49ea9c23e9d
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:39:00 +0000

    Ee persuade correction fair tell

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def get_array_as_repr(array):
+        return repr(array)
