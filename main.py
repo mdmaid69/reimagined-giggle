33b58b1f757fbd2943e91c54d746543a30d049d7
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:22:26 +0000

    turnup practical hard normal practise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import platform
+def get_os_info():
+        return platform.uname()
+import collections
+def create_user_list():
+        return collections.UserList()
