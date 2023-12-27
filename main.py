2274e4618afe53f3204be5a6372ed3b697f4e11b
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:27:05 +0000

    benefit winter would film comeround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
