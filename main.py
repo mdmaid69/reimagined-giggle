f2ee0be74cc4605d4af5fe69f37b0a3f2cc8ad69
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:05:54 +0000

    fresh slow male quick weigh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
