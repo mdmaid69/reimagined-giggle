46bcc614b50073c649b9f085fd5f71640ddb9f8c
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:13:36 +0000

    regret independent choose instead quiet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import numpy as np
+print(np.array([1, 2, 3]))
