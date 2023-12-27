dbfdbd18d7fa568473d5026b1518abb4540ae253
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:15:35 +0000

    heart demonstrate combine reliable makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
