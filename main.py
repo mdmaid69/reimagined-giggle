26ba0c560acfce99527c85b77aeeafc956b236eb
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:42:31 +0000

    contrast sharp future want concentration

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def reverse_list(lst):
+        return lst[::-1]
+def find_max(numbers):
+        return max(numbers)
