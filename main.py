d31988eb993e2b262a8f26652f51743b249d11fb
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:55:18 +0000

    interrupt space rent curious again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
