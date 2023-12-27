8e2d30bd9658cf0fa7689f0b85eea22c9f9c180f
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:39:40 +0000

    try worry room address feel

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
