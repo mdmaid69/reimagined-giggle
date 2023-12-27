f61e735f205d7ce79e05797d9a0d72b279b29aca
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:15:48 +0000

    snow rent different thankyou grand

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def multiply_numbers(x, y):
+        return x * y
