9dcc4ba5f33d2254e680c74f2a7d22899b07e882
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:02:26 +0000

    choice idea equipment compare hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
