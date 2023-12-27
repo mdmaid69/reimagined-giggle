7add1f2f9544ed58765d4218bc5769c2535486be
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:29:24 +0000

    breakup possibly obvious win stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
