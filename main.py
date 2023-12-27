0c2f9818c3380264976f237915d832657ad13c60
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:24:26 +0000

    between below law top whatever

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
