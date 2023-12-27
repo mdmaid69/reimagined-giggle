f2c2ef8f77c845554c64f5bdf3ce1bf78d60c43d
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:59:51 +0000

    later ourselves save fall so

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import re
+print(re.match("h.*o", "hello world"))
