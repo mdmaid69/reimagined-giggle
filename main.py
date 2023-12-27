1ac463497b30e5ed32a52eb75818a5a3684a2c51
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:48:52 +0000

    study behaviour do wet agree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-print(sum(range(10)))
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
