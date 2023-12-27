4f93ff020afc0961f7ae307d7003b48c47a4a933
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:14:49 +0000

    cheque quantity condition temperature comfort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
