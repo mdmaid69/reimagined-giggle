36d7f6612d1b4fc4362a7ff01822fc80776d0fab
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:42:46 +0000

    regret gap dream enthusiastic ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
