8f1175e42faab6ffe5266ac2492c0f8b93453f87
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:01:31 +0000

    copy count 567enemy increase she

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
