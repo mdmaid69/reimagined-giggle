fb0f6a36a50581a6c47aede9d557bf5b3ff4f4ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:28:12 +0000

    description science any speed home

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
