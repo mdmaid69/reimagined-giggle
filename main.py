6ec1bcd08359618a7288b4296fae785e1e042b6a
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:41:26 +0000

    delight pleasant rich unless cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
