0df8c197470315a8163e64932fdb002cb725ef07
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:39:58 +0000

    bringback slow cross persuade across

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_list_from_array(array):
+        return array.tolist()
