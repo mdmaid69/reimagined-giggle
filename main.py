80e434c3d3aa163363acc8e98bcf5d25c48c493e
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:01:30 +0000

    shape middle second sore pain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
