52eefe20e378b70ed7f38354fba0165b50eb251c
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:19:14 +0000

    table kind unit replace dick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
