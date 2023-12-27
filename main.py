dac30bede7423d0812fbe2a80174501589384c2d
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:43:07 +0000

    care by cause water trouble

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import sys
+print(sys.version)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
