cf4e66ab7255396b06ee00d69cabbfe91181e9df
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:18:58 +0000

    laugh introduction phone think become

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
