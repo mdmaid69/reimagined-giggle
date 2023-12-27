fd8c0b02ec20ee453ab273fe045dc4e976b69e73
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:54:09 +0000

    only talkback bright drop obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_sine(x):
+        return math.sin(x)
