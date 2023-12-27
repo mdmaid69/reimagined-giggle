21f01d167c4e8d62c7c1b820d32205c5cab41f5a
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:43:15 +0000

    press the holdout hang create

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
