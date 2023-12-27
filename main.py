451716fcc6f1cdb405302c807947e41faff59599
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:35:44 +0000

    keepaway Oo fly cover expect

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_power(work, time):
+        return work / time
