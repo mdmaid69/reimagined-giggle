29c412e922cc4ef5d4476b3feb44eb0657aa5322
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:18:09 +0000

    thorough excuse object number design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
