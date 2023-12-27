9c1c744946b3f7f657792802d9a93b6f7e4bf2b8
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:55:57 +0000

    development prepare effect grateful Itself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def split_path(path):
-        return os.path.split(path)
-def cube_number(x):
-        return x**3
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
