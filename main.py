25d0d1c26e5ad67df21adbdc7022ccc843f4bc9a
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:33:29 +0000

    guess flavour silent cause agree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
