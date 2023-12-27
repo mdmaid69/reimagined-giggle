8f0c786f94015431134e426f6f0c83d2d4cc275e
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:39:04 +0000

    rush excuse cut calloff comfort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
