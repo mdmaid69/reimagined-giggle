1491705a0f8face637cbcf3a471ceb84f126bcab
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:31:30 +0000

    grateful level fear Oo stand

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
