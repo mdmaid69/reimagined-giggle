3138c6ab8eac8eb912afcc823bf8e23f5501aa98
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:26:45 +0000

    relative act major special secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import platform
+def get_os_info():
+        return platform.uname()
