b023d70ba0db1c35e65b00d0a9f6dab22b696562
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:06:53 +0000

    involve cost slice organize union

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def square_number(x):
-        return x**2
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def convert_to_octal(n):
+        return oct(n)
+import numpy as np
+print(np.array([1, 2, 3]))
