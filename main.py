ac0e251cc2cd5c58c8f3da01596457e73cab0bcf
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:35:50 +0000

    discovery morning vision wander on

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import time
+print(time.time())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
