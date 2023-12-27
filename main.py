18014dc39f01f5ef44d07294cf9f11ace07300fd
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:55:06 +0000

    form relief slip quick ignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_odd(n):
-        return n % 2 != 0
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
