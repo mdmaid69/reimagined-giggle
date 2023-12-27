ebe2fdcf36e4c747ae57db6157b2da29ca6c203b
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:22:18 +0000

    sensible usual brave bringforward petrol

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def sort_numbers(numbers):
+        return sorted(numbers)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def convert_array_to_list(array):
+        return array.tolist()
