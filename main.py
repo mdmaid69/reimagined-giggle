d5e922d2069aae1500520d60a2baca569f9c2cc5
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:28:46 +0000

    serve smile rise pool connect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
