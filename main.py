2220d46767b9359ded3924d5ae6799a5dd963a63
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:44:06 +0000

    exchange difference enthusiastic road fence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
