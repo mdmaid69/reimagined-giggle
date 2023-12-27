9455d1772cbd0575969fc299df91e303a83ced62
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:45:04 +0000

    bright standard sensitive mark sensible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def find_unique_words(sentence):
+        return set(sentence.split())
