c008f84c9a8436b663218d5d8953a913fbbf44d3
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:15:39 +0000

    want sweet before foot deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def find_min(lst):
+        return min(lst)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
