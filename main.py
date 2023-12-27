38006bfeed3ea58c3ed415916c517bff3e2de550
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:39:07 +0000

    so part matter set finally

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
