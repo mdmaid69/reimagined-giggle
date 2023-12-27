b8048fbcd95ec9c8655f00b69b9550f85ed718d9
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:57:44 +0000

    winter excuse cry job pen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
