06f500321eb1ffa854c74b034f15097ace4bad1e
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:30:46 +0000

    thin grand go bottom tone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
