9ec62a4b6f42cdad003d744caa1ebd5849aed6bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:37:01 +0000

    blue thinkover recognize table new

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
