2080460206727678815df0a6744da746b0bab6b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:17:12 +0000

    music ability means already Rr

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def subtract_numbers(x, y):
+        return x - y
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
