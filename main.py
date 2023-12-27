661201f9300f80444bde91e3afdce7efc9aa2079
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:38:14 +0000

    shall height world turn everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import getpass
+def get_username():
+        return getpass.getuser()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
