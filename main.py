2c334ebd21958dd03e358f75b75bdbba740fc668
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:58:07 +0000

    formal some approval pin store

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+print([x**2 for x in range(10)])
