ff6daf6625e19f60d0ed7aa26dcab748b11c8fbc
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:38:06 +0000

    order child case terrible after

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
