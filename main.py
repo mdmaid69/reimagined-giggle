c15a9185473dfbdbb0a031b9600cbc533dcbccc5
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:16:25 +0000

    education lookaround punish future rush

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
