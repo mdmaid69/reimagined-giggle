97498945b3b3d7fbb9affe214f3c741d65c60296
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:06:21 +0000

    forget spot way lay regional

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_area_rectangle(l, w):
+        return l * w
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
