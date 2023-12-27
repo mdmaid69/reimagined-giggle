86425bfbc8462a9e9566769a43f452b0d85e5846
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:36:48 +0000

    at comfortable pocket impressed few

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
