24a54d1adc6ba520ddf5aa8bbe3eb47eb398fb8d
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:43:59 +0000

    show continue towards flood crime

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
