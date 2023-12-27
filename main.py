40ebfcac00e62ac4b6d04a7a79f6ca7f4de257e7
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:31:31 +0000

    normal wander bringup round female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
