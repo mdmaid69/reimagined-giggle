c5277e1ec2fc6e34748c9deb534fe43f84aed0f9
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:33:39 +0000

    regret introduction influence rush object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  def square_number(x):
+        return x**2
