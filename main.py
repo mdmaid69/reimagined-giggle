8b9c08e44a10d3c3354d7623ff08e8a63b7b6a16
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:37:02 +0000

    wide violence ready replace giveup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
