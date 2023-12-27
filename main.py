11932b00b6ec88500d76b2b860337c53255b0cdf
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:28:26 +0000

    payment swimming minute left strange

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
