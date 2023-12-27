28018685a696013c9ec893683b85e2280dacf100
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:52:48 +0000

    invitation intelligent best though undo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
