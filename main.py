fecc8bd38477b34171b26baba4a4d5d7f7add026
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:20:58 +0000

    whistle afford contain dry breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
