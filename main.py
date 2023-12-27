a3b05849c5873715448b3d38c12cbee6f6386a3b
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:35:06 +0000

    lend greeting preparation point experiment

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
