1b842902cdf26bf72be26c5fd45e08bb2de9f8dc
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:01:00 +0000

    scale sink floor protection object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
