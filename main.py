4aaa355c64418fba47ffb4a4bfead6ee94257e09
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:23:25 +0000

    another important yet not sometimes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
