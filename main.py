4df5a06f78cbfc1a603cc83e38b4a894e36b261d
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:16:51 +0000

    behind again dance fight pipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import json
+print(json.dumps({"name": "John", "age": 30}))
