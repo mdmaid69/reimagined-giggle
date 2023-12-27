afb143da9ba42eea807e41c773357d6775dae080
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:21:42 +0000

    bringdown benefit classroom negative ready

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def convert_to_octal(n):
-        return oct(n)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import logging
+def log_message(message):
+        logging.info(message)
