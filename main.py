2410082a76c90893e7a07e9fed20d9f7ff0bc79a
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:25:29 +0000

    unable question generally autumn rely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
