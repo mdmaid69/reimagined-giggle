8300f0b02c32bf7b1045a203f82c7c88e0370e61
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:13:12 +0000

    test keepaway describe make avoid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
