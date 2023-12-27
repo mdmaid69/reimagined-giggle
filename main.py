4dd74f7427fcd74c21802ff45ad08d4bd89c32cd
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:49:40 +0000

    discuss spite where turnout minimum

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-print("Hello, world!")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_int(array):
+        return int(array[0])
