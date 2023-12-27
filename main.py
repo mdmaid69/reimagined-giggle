6d0e622485d788a356c61b06efcdfecb9bfe8e5d
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:38:21 +0000

    tired neck fine professional beautiful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_speed(distance, time):
+        return distance / time
