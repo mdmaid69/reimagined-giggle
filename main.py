54ecb17bc89c5fb30af0fde03bba1d6605b030ec
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:07:36 +0000

    dance opposite division nerve believe

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
