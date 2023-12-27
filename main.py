ca29a1bc635d699ca4438561c7850f6af569a673
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:59:32 +0000

    influence dry bus free substance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
