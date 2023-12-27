f4ce712a2b125e1625e2de57a1f7802a2469df54
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:01:09 +0000

    might example really thorough mistake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_as_str(array):
+        return str(array)
