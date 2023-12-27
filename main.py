6158b86f249ca34fcea7697b3368a9d6ef02be03
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:34:45 +0000

    minimum nurse murder dinner unhappy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def is_odd(n):
+        return n % 2 != 0
+def is_odd(n):
+        return n % 2 != 0
