577f9636a9ee76c7fd0fc15f67a23381d1fb5fd2
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:01:36 +0000

    find load steam muscle bet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def convert_to_hex(n):
+        return hex(n)
