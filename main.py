d98483d3c60791835ce7c5e7855195ceed07a30e
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:55:57 +0000

    mood impressed unfortunate so passenger

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
