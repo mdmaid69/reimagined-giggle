395d941739aadd7aed7c12f961e1151ccc346b45
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:39:14 +0000

    travel against press she rid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def get_string_from_array(array):
+        return array.tobytes()
