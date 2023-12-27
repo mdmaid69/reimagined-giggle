a95c3164fe2472191376b4683f432f3d3f7216f6
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:16:03 +0000

    afterwards book offer than calloff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import tensorflow as tf
-print(tf.__version__)
+import array
+def reverse_array(array):
+        array.reverse()
+def count_characters(sentence):
+        return len(sentence)
