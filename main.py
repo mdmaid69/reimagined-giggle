4f72c285586b24aee8a5ba35b12b1e15b2ab14a7
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:05:24 +0000

    offer score afford chief gentle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
