4115e09067e0e3be7a48eadb1d132f7a08f486e8
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:35:46 +0000

    alive question unkind match mend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_set(array):
+        return set(array)
