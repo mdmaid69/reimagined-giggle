3f12793e532fa9428f91d736593d2c50a4b28ce3
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:38:31 +0000

    passenger phone wire rush married

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
