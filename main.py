29bcb4028dcf8c7a9246c9aa1823b81b6641ca4b
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:27:26 +0000

    debt needle sweep around people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
