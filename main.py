110768a9fa45004803025d75e7979c09d692ba2a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:38:21 +0000

    general learn runinto minimum suck

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
         plt.show()
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
