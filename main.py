272feb55cd699b1fd88a55ec9975b34fb0ec70e6
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:23:15 +0000

    edge block transparent hold tight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
