45fc560f0570b543f68630be8378ed1df5bdfffc
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:32:47 +0000

    arrange habit knock circle squeeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
