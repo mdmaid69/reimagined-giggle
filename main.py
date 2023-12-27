2ae300e1a1bdf461d3d2ddacd33a86e21c63077f
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:23:13 +0000

    admire failure noisy slice town

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
