643d749d4958f537b75a478780fb6158d3c4b369
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:27:12 +0000

    straight finally love ever fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
