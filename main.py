dcbc7dbcaa77ceef28ddcd39298b47adaab33656
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:14:41 +0000

    lookback(on) what year complicated announce

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
