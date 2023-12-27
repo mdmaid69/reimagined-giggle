44d40946a3403e40d035c2f4449783a81ee4295e
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:54:00 +0000

    sense at shake priority hurt

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+print(math.pi)
