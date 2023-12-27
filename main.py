0fa9f625b63717082ee36988196fb7b3261abb94
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:44:28 +0000

    block left Bb workupto letter

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def reverse_string(s):
+        return s[::-1]
