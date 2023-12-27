7b4ef4c5ca6b155ee37728fb1d0bb28b4d878211
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:51:55 +0000

    tradition reliable task birth entrance

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
