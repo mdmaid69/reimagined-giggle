d14ce23ade1a49a7d4dbb2e56df0333de89d4bb6
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:36:51 +0000

    learn convenient pity doubt wing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
