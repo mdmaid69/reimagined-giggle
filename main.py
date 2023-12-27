6e4293c5af1b3044071e90c7c34db312add291c4
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:06:16 +0000

    pay organization pronounce middle old

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def convert_to_binary(n):
-        return bin(n)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
