0a3186a2d20f9fd7e4467d7e034ccf62996f421e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:52:23 +0000

    system dish only acceptable stamp

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
