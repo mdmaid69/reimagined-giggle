546e26995a10ca70501b6c44e736fd945ef6b992
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:16:20 +0000

    wish course software ashamed steep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
