89f014f69728a16573f955b31632e4d62a863ad7
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:41:18 +0000

    friendly coat project prefer people

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+text = "Hello, world!"
+print("Characters:", len(text))
