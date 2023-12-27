7cf6b53c1cb9458f7a03efef2653cf3b9d2d41c9
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:07:42 +0000

    piece regret mood least method

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import re
-print(re.match("h.*o", "hello world"))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
