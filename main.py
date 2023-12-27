765087eddb14f458e19042a048152fa0f034d0c1
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:01:49 +0000

    frame thought autumn afford doctor

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
