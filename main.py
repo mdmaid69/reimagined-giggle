f199552babe9ee14e47bbf779f23fcc78fdfa9a1
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:16:37 +0000

    really illness explanation generally attach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
