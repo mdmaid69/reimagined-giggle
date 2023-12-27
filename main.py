2a7f7eaed47505c9f9d5e88705c263238009d964
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:57:56 +0000

    record brush seem time him

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
