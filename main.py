b65d03abebc50d1693acb1c9d3cd51fcbe553b9a
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:31:10 +0000

    community lesson total fierce open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
