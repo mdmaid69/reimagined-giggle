db58b0cf9a4f42cf96bd6d771a75cdc154bee35a
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:29:04 +0000

    pleasure mass that typical design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
