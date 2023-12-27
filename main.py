dbe7c88bc707c27d41cdd80ae537fa9179633fc1
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:10:19 +0000

    goout level connected 566end recommend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
