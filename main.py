c78b149e7c6ba2835ceba30c648936d955bb5c4e
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:07:59 +0000

    pump cross section rare about

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_list(array):
+        return array.tolist()
