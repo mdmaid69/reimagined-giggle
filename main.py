383b7f56ca36f2610306a80cb74d7eb6ae5b03c5
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:54:48 +0000

    whisper get hot any information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
