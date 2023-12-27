3e51b4633979a754bd188ba8a3dc97a39da4ebb3
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:33:01 +0000

    record getoff getout 565end sensible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
