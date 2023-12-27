2023dead9f0fa98f65a0f13e38d574f6f5f614b3
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:22:06 +0000

    breakdown cause swimming charge be

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
