1c4f3b263c84bfe713e91e112dd20bf6fa97d81b
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:55:33 +0000

    put afterwards feature to hello

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
