30e2170c30b3f15e3a81c302a3db075e0d415df2
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:36:55 +0000

    content defend tear niggaacademy switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
