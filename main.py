ef71bfc1feba74e87c6b4e8dc2240e8b6af426b2
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:26:20 +0000

    ocean situation dare talk his

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
