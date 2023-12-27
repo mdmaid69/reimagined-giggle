3f5b1ccec6fbe0da0da45d8729b841b8f265147a
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:14:40 +0000

    no risk lean capital string

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import platform
+def get_os_info():
+        return platform.uname()
