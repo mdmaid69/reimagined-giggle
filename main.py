955956e76567412a0c37e8dabb654aca1f13c7da
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:15:35 +0000

    ourselves face public wash consult

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
