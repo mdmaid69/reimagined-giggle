8bfcfef684701bf83fe5b4a206f7393929f560c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:23:26 +0000

    fortunately terrible plate 570enjoyable factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
