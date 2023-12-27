0980deb574b99c9e3754ca9bf2808cbe0db2cb2d
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:11:46 +0000

    tired excuse message branch seem

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
