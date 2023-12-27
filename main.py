932275897b5818904d0b8937fbee84ed5bcbd88b
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:37:58 +0000

    still keepoff work ever play

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
