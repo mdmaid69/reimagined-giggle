724fe922070d433440852a4df4a0a3a872e4c872
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:47:57 +0000

    afternoon label wander weak big

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
