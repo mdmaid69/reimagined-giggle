a9d5d073ef98746396d59fcfcb7c3ccdcccd93f3
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:55:16 +0000

    comeup rarely each side along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
