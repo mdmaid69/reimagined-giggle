1ff96a1b68cf13fd1d58214eeccb8f9a096cd4e2
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:47:28 +0000

    extraordinary over everywhere call sea

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-print(os.getcwd())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
