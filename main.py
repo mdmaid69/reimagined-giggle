fc5a21b14330c912d532b22f8b62018ee1b4c815
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:58:50 +0000

    fashion pencil young continue lost

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
