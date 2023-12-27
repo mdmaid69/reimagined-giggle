22886a36dfc03656da02d2a469eb224a0e427593
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:29:28 +0000

    snow runaway strike separate chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import datetime
+def get_today_date():
+        return datetime.date.today()
