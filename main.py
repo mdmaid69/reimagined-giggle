0ac774a8cf062b4c9386a39a87659f3544df8ccd
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:09:33 +0000

    win today confident stage limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
