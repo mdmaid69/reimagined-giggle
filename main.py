bfa4400145afcf26af14bd336ffd32ad4699e280
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:23:42 +0000

    temper spend embarrassed beat close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
