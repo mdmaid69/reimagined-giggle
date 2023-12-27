0c94be8783824931e130f3f4cd293aff498ffe88
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:02:05 +0000

    treatment close nothing possible sport

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
