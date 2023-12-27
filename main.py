6a8f9356ea16c3e43b74f4c242d91e13e659b337
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:23:19 +0000

    friendly Gg comeback supply natural

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
