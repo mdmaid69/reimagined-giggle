95bc3102af48e949078035054686c58a2976737d
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:45:31 +0000

    size present weigh news excellent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
