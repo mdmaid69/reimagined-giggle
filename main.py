8a3bd5f57d4da6985c3e85298bafb07195ee7368
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:35:39 +0000

    breakup guard skin money note

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
