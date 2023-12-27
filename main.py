6d13c0ee313a81876b13d3526c20d0b6c9bb93ef
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:08:58 +0000

    tired gap tone among ought

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
