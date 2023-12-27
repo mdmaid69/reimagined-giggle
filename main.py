53ac8fce6d00025024184bc839b7efbd04572028
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:19:11 +0000

    nigga correction guilty even aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def add_numbers(x, y):
-        return x + y
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
