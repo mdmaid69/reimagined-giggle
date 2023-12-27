33afc14ca5fb7809a5148e65b2a28e2789887172
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:53:12 +0000

    study today fork persuade corner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
