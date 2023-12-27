219cb4dff2b0a430cb98f8eaef6262a6eb9ccdbb
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:05:12 +0000

    risk whole fault Vv cost

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import numpy as np
+print(np.array([1, 2, 3]))
