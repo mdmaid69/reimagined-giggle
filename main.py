175fedcc3a44fc454be1bf64dd8e129d9c8fa867
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:22:04 +0000

    general lesson transparent situation original

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
+import platform
+def get_python_version():
+        return platform.python_version()
