c66eabf71163150f4bccc7751ed4f2f5338c6119
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:48:38 +0000

    shopping loan pinch bet look

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_current_directory():
+        return os.getcwd()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
