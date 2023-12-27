d224ceccca4f80c13e1fc68fe58e436f3d4190e0
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:41:29 +0000

    wrap direct container correct 568energy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+print([x**2 for x in range(10)])
+  import sys
+  def get_python_version():
+        return sys.version
