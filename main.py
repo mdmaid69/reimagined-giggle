fb173587f07400e2fac7d9fedc9c7f8be56c670b
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:24:59 +0000

    vision boat what vision behind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-def sort_numbers(numbers):
-        return sorted(numbers)
+import datetime
+print(datetime.datetime.now())
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
