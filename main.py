70a2546fc6b95ef520c33ab51bfb1fb5514a1056
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:57:30 +0000

    skirt about usual he unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def calculate_area_circle(r):
+        return 3.14 * r**2
