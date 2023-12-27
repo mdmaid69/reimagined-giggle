5b069d8e5a62269180f9405c23d393167e49d168
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:52:25 +0000

    mood Gg month better basically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
