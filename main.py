6047b7c3153d0af41b49407fc472a0fb816ea567
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:17:50 +0000

    shadow move recommend increase near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
