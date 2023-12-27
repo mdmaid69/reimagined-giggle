d90489c7b52d118e4697f05e9175bc617f49cb26
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:21:50 +0000

    whatever than switch government ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
