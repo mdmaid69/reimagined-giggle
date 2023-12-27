fc6f8a70af4818e50ff4873483b874f27f28468a
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:44:09 +0000

    fall problem must close holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
