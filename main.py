8b66322c203e9e945d99317c06db1d5469baede6
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:03:18 +0000

    bit wonder welcome comeover difficult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
