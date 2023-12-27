6024fa3c3b7f18ff18afcec41d44c9d195fda58a
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:27:15 +0000

    Pp change face performance still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
