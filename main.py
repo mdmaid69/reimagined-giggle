ce7966a8b900281ae50c9162d28346e8a40a41bb
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:56:22 +0000

    offend argue round divide criticism

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
