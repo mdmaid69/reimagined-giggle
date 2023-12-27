d12f813404bc8c785dc906a09ada1727ae2c1a17
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:23:53 +0000

    angry sea can tread mistake

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def sort_list(lst):
+        return sorted(lst)
