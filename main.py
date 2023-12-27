a4283b274510978250a6a2559d08d6202508ae03
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:53:33 +0000

    store school switch giveout woman

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
