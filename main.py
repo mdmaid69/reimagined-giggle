75d35cee717bf487ebec285c50ecca9745709dfa
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:22:29 +0000

    fun figure mad valuable since

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
