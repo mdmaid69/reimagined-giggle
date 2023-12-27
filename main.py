f3b118d5dfda9288a2348537fdf39957abfac8ab
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:13:23 +0000

    offence lean consult sick operate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def sort_list(lst):
+        return sorted(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
