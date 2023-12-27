ea6edb0becba58a6e771b391ae645d703ad883bc
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:42:00 +0000

    minimum now instrument safe worse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
