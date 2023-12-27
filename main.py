a38658b1ec930648e63ef93e96f6b17c809843d4
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:04:45 +0000

    office object bright detail scared

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
