163db0279502786c016451fd8e7170550a544c26
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:46:15 +0000

    escape best treat contribute question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
