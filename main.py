33ac63473490e745366a9400396dac41b0e6c7b5
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:20:31 +0000

    fault constantly ourselves left swallow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
