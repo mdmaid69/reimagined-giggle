db4bd4c30e390cda9080210c123a8ff1bf370686
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:10:08 +0000

    mistake something accidental hide ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
