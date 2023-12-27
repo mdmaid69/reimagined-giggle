9a6100466afa106b71b20d3689f690b93ac702b6
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:17:22 +0000

    callup giveout worse workon touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
