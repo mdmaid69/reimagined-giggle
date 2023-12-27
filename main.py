4eae70ba84d047e396678c337bfbef7f7f35cd47
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:58:43 +0000

    song deep trick same anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
