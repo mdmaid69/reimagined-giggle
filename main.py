0f0deb485a5333956b65afb0aa1f2ac75e00e1b1
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:14:55 +0000

    colour often left thinkover course

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def cube_number(x):
+        return x**3
+def count_characters(sentence):
+        return len(sentence)
