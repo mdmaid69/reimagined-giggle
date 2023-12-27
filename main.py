30574f0cd6c74425c6f9a1c03864f04ee59649ce
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:02:28 +0000

    joke Jj success fear union

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def remove_duplicates(lst):
+        return list(set(lst))
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
