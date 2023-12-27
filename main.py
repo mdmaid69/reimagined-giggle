891de0d8873f1c371434a6cdc1739137d9608807
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:11:42 +0000

    represent herself alone dish boring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
