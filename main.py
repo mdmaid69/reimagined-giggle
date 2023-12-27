2733f2d3bcd277271017cb673bab8ab94c40bdd3
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:06:00 +0000

    kick test Mm check lower

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+for i in range(10): print(i)
