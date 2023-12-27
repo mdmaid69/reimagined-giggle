e916cbec62a3b69563c656e0a0fadd757437eba7
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:36:42 +0000

    mother reason complaint declaration excuse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-print("Hello, world!")
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import sys
+print(sys.version)
