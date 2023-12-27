9d04bd1aa451a490dc013b77c654bf4f8426d79e
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:58:07 +0000

    thank set forgive precise performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
