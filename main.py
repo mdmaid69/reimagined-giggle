0b8e2968388fd4232fbee0c4bccb2f62e42725c2
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:10:23 +0000

    while them might relation spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
