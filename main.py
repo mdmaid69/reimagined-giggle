6ac36efea41f1d9aef0f5b6a9ce4a7512b811eb8
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:55:58 +0000

    stop wear giveaway scrap suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
