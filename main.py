b7229d9be8642c5e7bd8b8aa51a5d33c6d40cd0b
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:13:44 +0000

    year which classroom touch ear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
