7acac61305de441b9a2b7683faef75b8d0d16b46
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:48:05 +0000

    sky whoever dress collection react

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
