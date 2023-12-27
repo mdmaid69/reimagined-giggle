2a70e230b29bba9b3f081393d42ad2b064ed2db1
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:51:41 +0000

    attempt chin too phone leave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
