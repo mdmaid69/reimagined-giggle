64c11d8608daa3a22af8294ded132c1c20d857a7
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:48:39 +0000

    finish impression may necessary wear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def square_number(x):
+        return x**2
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
