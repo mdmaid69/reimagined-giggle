182fb6bb10d11f01344de6dc7bdff3b3742b8ee7
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:18:09 +0000

    quickly smell line beautiful build

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
