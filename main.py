cb682e4735d483a71c28df2c6b1b1662e777a567
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:53:51 +0000

    smell path country crash pain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
