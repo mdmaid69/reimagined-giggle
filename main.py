188a4536a370de8a9a04a8bb0e60fc91fa0208a7
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:37:35 +0000

    bringforward education bridge foreign correction

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
