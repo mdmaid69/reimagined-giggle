14afe28ec0664a56760a72a9443e86dd918e8f52
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:03:19 +0000

    most unit set order juice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
