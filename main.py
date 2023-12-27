2b541291a27cf39881295adb585cfa10add0e3d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 18:08:57 +0000

    home during could search taste

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
