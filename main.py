20f01116c9caa7b223840cad1567d2de808af882
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:50:45 +0000

    above complain already good cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
