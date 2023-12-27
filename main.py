3197d49c3ffa2cedb2e420c1a73b65555f56f3ee
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:23:36 +0000

    shake which bag under behave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
