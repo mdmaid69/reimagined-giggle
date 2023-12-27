71180a57e0dcec610433436b2430a42c1ca857fb
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:48:49 +0000

    please male curious absolutely formal

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
