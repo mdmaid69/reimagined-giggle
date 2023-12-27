ed1f29ea53435d68f13f61729d21b267b76c77e4
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:29:07 +0000

    surprise professional chase achieve exploration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def get_array_length(array):
+        return len(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
