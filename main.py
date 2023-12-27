d5708660b9d1f0ff2655882267ecd3d70811067d
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:42:40 +0000

    drop pleasure mood style consider

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-print(sys.version)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
