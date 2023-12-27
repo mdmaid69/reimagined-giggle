39d145948d48fd1cb9054e6ac03e401d72be9147
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:28:52 +0000

    wrap oil smell yesterday exhibition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
