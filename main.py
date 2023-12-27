ec02275546d53ea4ac67e12d740f3e76a6245fea
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:06:54 +0000

    raise chat formal offence coffee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
