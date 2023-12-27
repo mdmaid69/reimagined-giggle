7b4826ed9eaaea9c9184ee51e0b81f9b5e036736
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:26:36 +0000

    sort trust visit approval underneath

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
