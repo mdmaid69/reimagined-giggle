7086d672312d8cb5b1b30e02949d965c0bb588df
mdmaid69 <robot@alexsyw.me>
2023-12-27 12:11:14 +0000

    platform leg tired close fortunate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
