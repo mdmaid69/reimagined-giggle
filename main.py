4aa3df4553081f9e40eb191caae90d1a313e8a22
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:32:30 +0000

    slightly definite last table fat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
