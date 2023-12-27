536cebb9fae73de42af6f706482ae05fb6d724f1
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:17:22 +0000

    ill look unite many gift

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
