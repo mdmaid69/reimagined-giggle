88c0b942c9113595d6a7493b7603711c7c19b365
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:08:07 +0000

    pick ever in vote point

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
