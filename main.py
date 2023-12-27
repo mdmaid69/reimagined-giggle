b825a110ad0c6c9c4bcb8fdddbbae97686c6243a
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:55:37 +0000

    he used freeze thumb actually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(5):
+        print(i)
