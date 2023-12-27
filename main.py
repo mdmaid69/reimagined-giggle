17519b108d3ac4b232a288249d544ec5d69bd5ff
mdmaid69 <robot@alexsyw.me>
2023-12-27 02:03:38 +0000

    match train new attempt cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
