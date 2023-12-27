c7387dd9336858f45aa97ac109c6f09db6d21b44
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:37:04 +0000

    bring hot improve exam however

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
