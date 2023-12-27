0d2344d7b41de832429c3d235cb6904ea8a66ecc
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:17:38 +0000

    card responsible bringback touch man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
