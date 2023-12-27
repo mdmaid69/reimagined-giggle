ba60f18b42f7aa00ee1a4b4b9a869d038b1d5c4a
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:20:46 +0000

    will guard combine save Dd

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
