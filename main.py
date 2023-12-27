2fd5c56889f33cd0238f921dc075964f0524a865
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:47:50 +0000

    road fork whistle emphasize touch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def count_elements(lst):
+        return len(lst)
