fb1b04a9a94e8e8d424b921cd04e7b336e5d5bd0
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:15:48 +0000

    increase original report sentence attractive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
