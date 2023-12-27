55bfaf912f51b6a0995ca74adf6e169cc46d4701
mdmaid69 <robot@alexsyw.me>
2023-12-27 05:04:04 +0000

    lay any all load trust

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
