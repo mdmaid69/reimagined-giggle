3cad97dd150aeeb9b4be78d13a1914a8e67dff02
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:48:23 +0000

    impressed bath unlikely pretty beautiful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
