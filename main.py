b4a60c3cbedfea8d97055cda4622aaae3f017480
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:38:08 +0000

    its light busy right what

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+print(math.pi)
