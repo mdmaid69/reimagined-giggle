cb67fcfb1b30f71b6562d40a99a85e5f74f7ce83
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:55:23 +0000

    advanced bad for dark cool

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
