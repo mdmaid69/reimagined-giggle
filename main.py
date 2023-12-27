5440efc790fad9953c68a9625755adffa435413f
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:10:35 +0000

    soon fall getaway Bb valuable

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
