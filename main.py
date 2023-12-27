25e1c90627310ad9c17ccc3cc2d6620ab61bc98a
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:23:58 +0000

    cruel understand similar charge song

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import time
+def get_time_since_epoch():
+        return time.time()
