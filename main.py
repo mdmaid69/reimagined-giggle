39a639a65d7029f5094fe89e751c20864fb9a8d0
mdmaid69 <robot@alexsyw.me>
2023-12-27 17:45:07 +0000

    holdon Pp imagine both onto

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Reversed:", text[::-1])
