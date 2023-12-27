bb8f2565e1b41a60cbc07a63887b46bc00aff872
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:52:27 +0000

    relation case strange ability have

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def add_numbers(x, y):
+        return x + y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
