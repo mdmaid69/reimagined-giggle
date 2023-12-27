55d09b5ed8320795aec204accd0c6066bb43e764
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:25:22 +0000

    our cut form already single

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
