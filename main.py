d1cbe42a2e1564220bff18307b5637c92370f3d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 16:11:46 +0000

    callat crisis turnover report day

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
