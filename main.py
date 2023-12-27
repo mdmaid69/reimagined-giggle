5c913bf4ef07a5a0b6e8bf0dbc9dd8bd3b6c2dbf
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:08:01 +0000

    travel experience thorough move recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
