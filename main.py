8eecebef52a191cc3a1f6b439d09fd8eaa734a0d
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:01:36 +0000

    simple plant happen although permission

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def convert_to_octal(n):
+        return oct(n)
