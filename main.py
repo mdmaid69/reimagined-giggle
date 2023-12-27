96e4602c346373527ff18ecc7b6dd4b3610d645f
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:15:55 +0000

    yourself brush battery regret separate

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import sys
+def print_python_version():
+        print(sys.version)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
