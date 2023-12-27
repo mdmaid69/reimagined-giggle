605cb5f0daf19c3ece5adc8294751718e2261165
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:56:16 +0000

    minimum wave keepaway backwards deaf

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
