ac7d86c11f514c47297de8d9a69684168942a3d8
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:22:00 +0000

    comeback lookafter likely here worried

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
