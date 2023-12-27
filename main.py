f246879cb7ef78cfdc83b7fd565701102b18ecdc
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:00:41 +0000

    test equal area rate rise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
