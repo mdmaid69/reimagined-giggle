a7b4ddeed75ed36ae50f8aa89d55e5e89be48109
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:50:16 +0000

    free trap many see smile

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
