048553812ebf6543b05d258749be94e4f36d1937
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:20:08 +0000

    mean answer stamp contact wall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_volume(length, width, height):
+        return length * width * height
