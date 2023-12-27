2b39e914c62b6a05a99237d97f3fe2f1ab9bd180
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:06:48 +0000

    hour soft eager complicated both

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
