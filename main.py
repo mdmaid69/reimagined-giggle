c7c758b02e83a73ff71aa9a5df089283abb2b780
mdmaid69 <robot@alexsyw.me>
2023-12-27 10:16:28 +0000

    sort offence hit among press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_current_working_directory():
+        return os.getcwd()
