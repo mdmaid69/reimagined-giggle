b68bde89852050fd2a492261c31da329f1aaa843
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:15:03 +0000

    charge attention Rr room cost

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import numpy as np
+print(np.array([1, 2, 3]))
