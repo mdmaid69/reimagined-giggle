3616788cf7fb162d4f9760de7874134f7d322485
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:02:49 +0000

    excuse mix pupil ticket free

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
