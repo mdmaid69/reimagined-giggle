3e5a10e5771c32e61971ab9250194fe731d3793a
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:21:48 +0000

    news why about dry frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
