1fac62fbc2dd4600cee1d48f4dfc59516d3e3f91
mdmaid69 <robot@alexsyw.me>
2023-12-27 04:55:40 +0000

    cause convince swim face unlucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
