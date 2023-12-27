a498328aaa60e6373e834cdea5fbc3720a966650
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:09:40 +0000

    leave space hardly recognize leg

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
