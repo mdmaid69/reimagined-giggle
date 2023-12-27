ec425efb0bedc409cc601931152a7386ff79a676
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:12:06 +0000

    keen quickly between about later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
