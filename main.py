a5226592049c325fb319c7fcf47ece13461e3f17
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:29:53 +0000

    shout release forgive improve suppose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
