056954773f4dda0fab15fb8be56643fb7e25e300
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:39:05 +0000

    patience Zz tired arrangement protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
