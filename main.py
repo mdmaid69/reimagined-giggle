69ac94b5a04254df5b939cf38b43fc1c7992815e
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:06:12 +0000

    comeround police loose butter race

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
