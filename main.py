1d8156f5ce6af7ef7dc652da58a791765f41db08
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:05:48 +0000

    permission accept except narrow movement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
