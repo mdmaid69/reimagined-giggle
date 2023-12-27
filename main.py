668511383edc0381cc3f169c97ad0078d3ec93a6
mdmaid69 <robot@alexsyw.me>
2023-12-27 13:10:37 +0000

    well party ourselves state underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def count_elements(lst):
+        return len(lst)
