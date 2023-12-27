56c3a2c3574375bc7cb9e20ffec34acdfc11322d
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:54:30 +0000

    thumb substance take interrupt mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
