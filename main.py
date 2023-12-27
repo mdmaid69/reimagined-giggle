3273ca24a75d7cf050ea13bb042216ce5003346b
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:18:30 +0000

    full look her cool please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
