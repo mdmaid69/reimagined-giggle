ff0f3eb5e986786cf1e6388f227edb49c742d36b
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:52:03 +0000

    everyone centre blame address talkback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
