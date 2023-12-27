b5fb44d3f1a6980acf6bb12255339fd821c660b9
mdmaid69 <robot@alexsyw.me>
2023-12-27 14:48:49 +0000

    still turnback actually anybody invite

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
