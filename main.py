4a62547b453b47ed7e42d75874e5c58c19ff42ea
mdmaid69 <robot@alexsyw.me>
2023-12-27 01:52:09 +0000

    concentration juice smell tap over

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
