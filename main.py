aba84bde5794f708a4c27847f2a521c838cdf9d1
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:59:59 +0000

    instruction health job cap nerve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
