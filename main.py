0f7f6879ec0ee7a378cc4bc20be074d4badeb05c
mdmaid69 <robot@alexsyw.me>
2023-12-27 22:03:51 +0000

    undo group there exploration permission

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def convert_to_octal(n):
+        return oct(n)
