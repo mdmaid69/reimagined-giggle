2273b715d47e7ca2b3e2f91ac66c88f6fc8ea065
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:13:09 +0000

    recover thumb proud port left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
