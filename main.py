ef40c9f2004e8ad472211526c4084737078d51e1
mdmaid69 <robot@alexsyw.me>
2023-12-27 07:54:28 +0000

    practice child convincing money wrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
