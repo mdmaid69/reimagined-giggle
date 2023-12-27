6148ff9c0f20e6bbf7df4d3a4430926ef1680d81
mdmaid69 <robot@alexsyw.me>
2023-12-27 08:24:39 +0000

    nowhere than funny health quickly

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
