89ec1d5d7dfb388fe3de97c72dba3c49b6d739f8
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:13:51 +0000

    owe ring any street common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def find_max(lst):
-        return max(lst)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
