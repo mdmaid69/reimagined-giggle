b7206430746a2906c637acf3a52c82d627e829d4
mdmaid69 <robot@alexsyw.me>
2023-12-27 11:00:04 +0000

    collection return conduct agree breakin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
