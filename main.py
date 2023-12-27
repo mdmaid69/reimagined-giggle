d9591be407f1ad541377f2e93a9f99486a61b920
mdmaid69 <robot@alexsyw.me>
2023-12-27 15:44:03 +0000

    squeeze slowly contact wind improvement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
