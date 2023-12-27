757f3ad670988593e3aeb952cdf234511be70449
mdmaid69 <robot@alexsyw.me>
2023-12-27 23:26:00 +0000

    Nn by like skin loud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
