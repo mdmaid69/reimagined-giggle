08a9bcd1d90941332b8af098be0961c2eef7d33c
mdmaid69 <robot@alexsyw.me>
2023-12-27 21:04:46 +0000

    bend spring trick trade fierce

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def reverse_string(s):
+        return s[::-1]
+text = "Hello, world!"
+print("Words:", len(text.split()))
