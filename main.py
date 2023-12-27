48ba31e56750ad02661dbee9af52389e74ae6ac0
mdmaid69 <robot@alexsyw.me>
2023-12-27 00:38:50 +0000

    pinch accommodation exit programme long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
