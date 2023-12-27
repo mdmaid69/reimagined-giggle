2471f6d5cd46ff1d7c7814aded0d93e7a94b3826
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:46:06 +0000

    appeal rescue calm minimum Ll

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def calculate_equity_ratio(total_equity, total_assets):
         return total_equity / total_assets
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import datetime
+print(datetime.datetime.now())
