006dca5a83fcbf5663acf4845876036b0811b72a
mdmaid69 <robot@alexsyw.me>
2023-12-27 06:49:10 +0000

    problem union question maybe imagination

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
