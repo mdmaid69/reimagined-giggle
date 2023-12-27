3a2547038827b0ab09de20c1a471387df100c38f
mdmaid69 <robot@alexsyw.me>
2023-12-27 09:13:40 +0000

    pool flat sleep trip print

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
