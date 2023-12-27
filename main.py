177ec124228cecee615309ec2144ebcc3b7e640c
mdmaid69 <robot@alexsyw.me>
2023-12-27 19:14:56 +0000

    bar frightened worried habit careless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_max(lst):
-        return max(lst)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
