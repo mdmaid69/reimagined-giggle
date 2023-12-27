f62c145337939980d2a5eea6abc9e035dada9ade
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:21:02 +0000

    advanced be sell demonstrate calloff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
