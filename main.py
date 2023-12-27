7534ac23192110cbe63dcc6b8800ffa9db31aa09
mdmaid69 <robot@alexsyw.me>
2023-12-27 20:37:32 +0000

    song right afterwards demand corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
