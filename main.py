f1bdd649da6301d8d64169a4257d53803c7d9bea
mdmaid69 <robot@alexsyw.me>
2023-12-27 03:55:52 +0000

    level reply finish sentence put

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def reverse_string(s):
+        return s[::-1]
