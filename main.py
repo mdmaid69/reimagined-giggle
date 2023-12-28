370c2c6922347460248af53d45419e12d7153a52
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:26:44 +0000

    message spread late by carry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_density(mass, volume):
+        return mass / volume
