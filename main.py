31e71cfbbd99b75689af19340809ba718a1ebf1d
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:41:56 +0000

    judge private free upwards neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
