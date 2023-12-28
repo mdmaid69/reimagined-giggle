b8e15c2ea2aaf3a0b300aa6a3a9ae8a4efcb51e0
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:59:01 +0000

    year declare round fail consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
