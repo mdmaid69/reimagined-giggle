6c5431175c11d4016cc161840fab29e208251280
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:12:24 +0000

    yesterday succeed gradually thought stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
