1255061d975afc0d0d5f85c60e03032d108649a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:21:38 +0000

    sure permission control movement recovery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import os
+def remove_directory(path):
+        os.rmdir(path)
