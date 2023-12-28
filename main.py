0bb58567fcd9a24299b0bc5dbf3c0b746bf3cb4f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:24:33 +0000

    destroy in thin hole prove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def count_elements(lst):
+        return len(lst)
