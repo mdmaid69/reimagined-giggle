3174f4f65251c744881f9c479b176db4714aa168
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:17:50 +0000

    block project mixture visit drive

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
