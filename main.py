9a92fd084a3325ba7f52888f19d70becc5c7ad35
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:04:38 +0000

    cut tune nurse cook moment

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def find_min(lst):
-        return min(lst)
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
