8340d907dc291e815bd8dfd66b92df1639ccf93c
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:12:54 +0000

    unless drive unusual tap car

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
