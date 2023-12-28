d6c76f8fc5fdea735307de88385c8b2ac02a5edf
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:07:48 +0000

    experiment clear justice custom edge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
