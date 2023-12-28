364d19a2c9108ae34341fc3f67a28d7b8ae02260
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:31:32 +0000

    goout exciting smile anybody performance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
