9f02b6084569b36d04c360a2ff43e64abfb60538
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:24:56 +0000

    chance talk bottom pull walk

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
