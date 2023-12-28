86a8f9349cccfa14c72a008abbe2f631fd6232c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:50:14 +0000

    compete so same maybe opponent

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_pressure(force, area):
+        return force / area
