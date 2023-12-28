2a3b68662c426d97270b1d1d6428ce67774048dc
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:19:30 +0000

    frighten central dry keepout wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import os
+def change_working_directory(path):
+        os.chdir(path)
