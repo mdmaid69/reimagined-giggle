0a47cc9bc88d0e94c6bce0e902f38ea78b1b80d4
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:36:31 +0000

    rush innocent fail seat breath

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
