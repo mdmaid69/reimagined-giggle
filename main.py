0ad4ad09606fd4299afda49b005d4fef12f8cd29
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:12:23 +0000

    block ought pile example through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import numpy as np
+print(np.array([1, 2, 3]))
