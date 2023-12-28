7e68cee09221dab8b5c483c3b656b0c120368a26
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:34:08 +0000

    free shake unless shallow frighten

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
