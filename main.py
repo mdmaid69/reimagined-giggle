41c0139aeed7fb9c4c6c7bf4cbeb49764caeb3c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:56:44 +0000

    hardly war cool before pain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
