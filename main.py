8c170758768408b86fd85617807b2a0c7580f779
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:50:05 +0000

    who loose exhibition dream bit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def count_characters(sentence):
+        return len(sentence)
