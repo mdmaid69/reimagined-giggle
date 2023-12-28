ff6c84ea51646d71b78da0bf4855f6503ca13d51
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:30:55 +0000

    answer Hh dress exciting pride

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
