dad3d4b727ec6eeb99b3967364d64cf893697732
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:08:04 +0000

    size flood uncomfortable lend store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import platform
-def get_python_version():
-        return platform.python_version()
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
