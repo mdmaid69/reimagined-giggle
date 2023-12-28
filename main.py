6a7e15bd5ccae747f0e1bbd3d5aa9ce3ee39fca8
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:28:25 +0000

    swallow message must share hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
