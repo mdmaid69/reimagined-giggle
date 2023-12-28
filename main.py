5dd8064d826bf0b55064c9f59d5dd99bf4eda183
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:08:18 +0000

    seriously mistake waste gift why

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
