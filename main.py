7deca8f588c59079a67185b9da3074a3d023627c
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:02:39 +0000

    information rarely makeup pretend straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def square_number(x):
-        return x**2
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
