6e1217e2a9f0a0187b4a358800bf67c8fee4b814
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:34:41 +0000

    sail and child me surprise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_power(work, time):
+        return work / time
