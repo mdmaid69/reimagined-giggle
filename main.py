0dc58ad03bbe9be32a30679a708cd0db500f2f22
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:27:34 +0000

    winter interest brother nerve dare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
