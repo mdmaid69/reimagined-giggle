b5427bfa6ff2dc3baf83624a753b69517fd5ee32
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:19:10 +0000

    class rest escape plenty guess

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def append_to_array(array, item):
-        array.append(item)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
