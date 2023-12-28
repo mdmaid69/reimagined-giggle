09dccd5eecf2f0485506bba161160aa9fbe26b6a
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:09:31 +0000

    although aim welcome ticket slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
