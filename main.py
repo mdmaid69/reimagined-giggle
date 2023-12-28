e9bf5f24b7023971a20aca3cc61e749b786db689
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:47:49 +0000

    bag constantly about still though

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_current_working_directory():
+        return os.getcwd()
+def reverse_list(lst):
+        return lst[::-1]
