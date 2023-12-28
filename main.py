8af6638fc3c53fbc2bd10c10b80d4981d3987e6f
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:44:49 +0000

    from through like basis agree

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
