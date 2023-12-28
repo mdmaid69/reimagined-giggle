c3357c5e3d54677822dc4e42c15d4483eaf4d963
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:32:54 +0000

    always total through each patient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
