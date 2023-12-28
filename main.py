10ca4e0017ff3cd839b962a313635aab00a7c2b7
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:22:15 +0000

    arrange label hit further hungry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
