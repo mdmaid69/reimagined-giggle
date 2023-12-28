0daf92559a70d4530306b1fd75bda00cc2534ae2
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:18:59 +0000

    above holiday body programme chin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
