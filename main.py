fd434ac2f323e11f74e91228b9826e804d8db3c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:39:20 +0000

    afternoon name lookaround violent bound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  def is_odd(n):
+        return n % 2 != 0
