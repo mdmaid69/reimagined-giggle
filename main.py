943ca3719efdface98a79369b67a1b06d207ccfc
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:29:42 +0000

    win closely meanwhile future front

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_acceleration(speed, time):
-        return speed / time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
