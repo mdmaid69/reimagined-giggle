4d9fa0cd62141a84eadcb9d6dc1f7a74904264cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:00:08 +0000

    Oo after backwards artificial please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_palindrome(s):
+        return s == s[::-1]
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
