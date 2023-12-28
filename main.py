a282a71ef3b9b633ef399010b09cdc82cc5f6912
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:36:50 +0000

    touch suit range stretch powerful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
