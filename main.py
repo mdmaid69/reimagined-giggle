e59c7ec112008f548d5461a104ed925e0b82f429
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:51:20 +0000

    bit check performance loud climb

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+print(json.dumps({"name": "John", "age": 30}))
