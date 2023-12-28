f2b3b4a11ea3fa1f7d3a9365354f1b30f739d234
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:06:14 +0000

    trust life contrast involve fresh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(10): print(i)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  def remove_duplicates(lst):
+        return list(set(lst))
