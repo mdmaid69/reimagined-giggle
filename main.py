f63127f10757de31f7195b381a0f098d98d9890f
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:28:27 +0000

    breath especially will degree television

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+print([x**2 for x in range(10)])
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
