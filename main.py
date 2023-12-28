53e61f4503add96187e462046e43c33bcf80261b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:55:27 +0000

    contrast bringup transparent total keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
