98db937698430d16048c7dccb14ef3d3fd74dd2c
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:06:23 +0000

    expression ball they actual shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import collections
-def create_queue():
-        return collections.deque()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
