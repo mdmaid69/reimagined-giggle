16e3a6fad8139f834697a4ffdd1632c1e5841e72
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:04:23 +0000

    warning Jj lose survive sight

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
