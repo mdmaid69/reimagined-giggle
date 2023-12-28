d9683936bef1f800debdd66094ffcc742f814015
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:47:08 +0000

    likely guess community tight takeback

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import time
+print(time.time())
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
