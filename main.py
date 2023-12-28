fab8c80ecea58bb30ca4b58995d0cb596c9a15e3
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:49:06 +0000

    themselves think lose lose risk

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
