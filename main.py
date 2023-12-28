2532db3f198b9ca40f45e918fc0373b2be1666cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:11:26 +0000

    through act family personal substance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import collections
+def create_user_list():
+        return collections.UserList()
