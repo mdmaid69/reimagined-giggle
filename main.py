299d6eafbf813bf3525b8e9b90c9a1601c1668cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:04:33 +0000

    style pay mainly he personal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
