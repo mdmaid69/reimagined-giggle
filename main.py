c5b3a3cf366b92c8322c14621d1c96b1f6c59dde
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:16:53 +0000

    spare exam event holdon quality

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
