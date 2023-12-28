b52a32e5b241ebe9d3045a95ae38b29e5e0331db
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:37:34 +0000

    depend contact yesterday we character

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  def reverse_list(lst):
+        return lst[::-1]
