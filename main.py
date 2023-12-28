2b744e521b8150f1ed9080669b676102e87fa333
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:49:03 +0000

    bad loan tonight argue place

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
