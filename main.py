9112ec5ec99477058d99004e1974d7772802981e
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:10:49 +0000

    wet unless takeback wire test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 def calculate_ceiling(x):
         return math.ceil(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
