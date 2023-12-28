913b8f87eb08e002bf4d9011019d429b2c4f782b
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:32:22 +0000

    drink turnback moon cruel both

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
