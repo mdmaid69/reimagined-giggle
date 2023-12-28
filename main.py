9224aaa604319e053bb036395c1bd3eacf8e9f34
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:41:22 +0000

    cover pay off back Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_exponential(x):
+        return math.exp(x)
