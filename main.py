3fd2b82c52ffeac13cbbf52e2c71c53c9bb554b5
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:43:33 +0000

    similar fence kind slowly contact

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_item(array, i):
+        return array[i]
