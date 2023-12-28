5f866c1b9651eb9f5de0e6904254060434ff99dc
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:20:13 +0000

    slide rare loud quiet achievement

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_current_working_directory():
+        return os.getcwd()
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
