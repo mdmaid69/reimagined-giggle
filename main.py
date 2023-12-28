a9762d1fad3a6ed85a8f599ac37e8ffc81156d5b
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:59:27 +0000

    satisfactory television any dry month

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
