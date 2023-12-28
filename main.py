22011d9929e76d119697342e1ba5324628f00fcb
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:09:30 +0000

    shock straight shut wash butter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
