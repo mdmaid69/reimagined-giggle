0f2e42b3c15429bb69ac98c832039cf6e46f8870
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:06:18 +0000

    brief occur rank contrast rush

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
