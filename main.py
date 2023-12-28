74aafd715d28139ddf94df469cb756778286c7e7
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:31:49 +0000

    deliberately speech day system tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
