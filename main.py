008466f2a73fe2d1458d84b3d9c00b96e0ab5357
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:31:14 +0000

    regular soft regret goodbye Vv

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def convert_to_hex(n):
+        return hex(n)
