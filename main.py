aa1bded368bb5027fc028e047afc61f948d6abdd
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:58:28 +0000

    season gentle dress past majority

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_speed(distance, time):
+        return distance / time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
