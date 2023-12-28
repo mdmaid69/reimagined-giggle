b59c5f3aaaf95e8bf8527d3a30607a74e8d82d9e
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:00:33 +0000

    else information offence reduce climb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
