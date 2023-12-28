e78996841a51aa8a765e0bb4b77ceba2b4721e73
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:13:55 +0000

    control innocent keepup view quick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_acceleration(speed, time):
-        return speed / time
+def check_if_array_contains_item(array, item):
+        return item in array
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
