303f68d25185ce999ceefac1d706a9bb6943dcbe
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:50:36 +0000

    before broad 572enter rid able

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+print(math.pi)
+def find_min(lst):
+        return min(lst)
