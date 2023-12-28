6fd74a5e0741cee4c7033fb1e26120064886f793
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:26:27 +0000

    variety secret post comeoff fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
