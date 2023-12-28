1112d6d46957c29a3a7a8f8f874238034b04529c
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:44:20 +0000

    pay indeed Jj mainly wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def find_max(lst):
+        return max(lst)
