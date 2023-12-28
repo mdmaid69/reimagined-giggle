83766eb848b2fd03576298ce53263df0a5c01093
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:18:05 +0000

    natural earn final OKorokay back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
