e57e5efdf17fac335e400d3ac89553eab6b2c2fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:42:38 +0000

    sad anywhere wherever accident lookup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
