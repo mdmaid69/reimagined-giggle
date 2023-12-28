c2d5f6e6e6bef77532c1e58294a2e937bc740075
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:57:45 +0000

    own like keepfrom disappear can

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def convert_to_binary(n):
+        return bin(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
