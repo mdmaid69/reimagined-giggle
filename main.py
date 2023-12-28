3edfad4bd530fa0f9fb706f4b6421ca3faa8e0a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:30:35 +0000

    respect near genuine turn that

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
