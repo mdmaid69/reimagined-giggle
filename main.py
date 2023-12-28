b36ad530751b5d57a5fd518bed2de86339ee9e39
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:28:10 +0000

    time scrap twist many future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
