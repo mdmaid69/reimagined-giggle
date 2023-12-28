959a2e1e5b52ebe60974a3ae9efc035a0989cad9
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:35:23 +0000

    regret concerned surprised provide fast

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
