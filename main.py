0d253ddc71a6b6e10577de3752aa7afb1ead50a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:50:54 +0000

    us dry trick circumstance her

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
