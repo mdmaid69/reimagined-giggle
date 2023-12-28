37824edbf8da5a4a92c75bd8a1e6a86892b9f02c
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:28:20 +0000

    string week any manage bang

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
