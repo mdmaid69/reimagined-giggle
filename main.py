4690cdd06214225a3c1ecbdf109c3e50c3107040
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:35:57 +0000

    treat pair minimum boil approve

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
