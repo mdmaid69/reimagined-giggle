12341dc803145d2ce61de2ce04b5be8fad7b8084
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:26:23 +0000

    sure comment terrible sorry sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
