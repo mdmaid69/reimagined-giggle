b9f0f8947a227db4b5b8365ea9dd53500ca38804
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:31:15 +0000

    below complete personal hit worried

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
