0d0d4376a917baec88fd3ebc675e008635741019
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:10:51 +0000

    steam team pride interfere abroad

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
