ea4e9a53575785ca61b43fc11bc94d6967b70715
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:17:19 +0000

    moon of escape eat average

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
