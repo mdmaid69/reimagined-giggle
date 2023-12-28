83704fcc56d67964d27aa57bf9a8e677412cbd5c
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:30:09 +0000

    lift flat steam uncomfortable evidence

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
