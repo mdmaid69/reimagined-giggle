282db907cf4133c1e7aab891fa00f0822f31e5e3
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:18:13 +0000

    break peace exit dick grow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def square_number(x):
+        return x**2
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
