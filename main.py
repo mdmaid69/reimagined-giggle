ff3f8e466ce37701768a20dd377e654a908cc12f
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:23:48 +0000

    feeling course dangerous measure welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_current_directory():
+        return os.getcwd()
