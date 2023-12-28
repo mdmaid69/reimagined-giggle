44306c04a83d2d50fa1d98f847330167fd06f85a
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:10:47 +0000

    on fill settle hold rather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def convert_to_octal(n):
+        return oct(n)
