4f444986937ae3d611751a8cbbed079c3887f573
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:57:25 +0000

    considering keepout workout cheek later

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
