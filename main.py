620d8283571130fb14a77ec4f5f6985355b7da0b
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:27:05 +0000

    below match wash throughout open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import sys
-print(sys.version)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
