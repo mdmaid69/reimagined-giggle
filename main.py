42c1b2ab1dcaa7b6677fb12cbdf73c8355389797
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:56:05 +0000

    sort recognize chase free thing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+print(math.pi)
