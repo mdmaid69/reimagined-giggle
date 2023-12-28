a4cfcf590fb0eb0c45fe806dec77b8e50a991b3f
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:01:11 +0000

    bear deserve abroad whenever child

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def square_number(x):
+        return x**2
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
