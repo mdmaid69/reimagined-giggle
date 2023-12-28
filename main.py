88ea3edb7d7247e0c28e0358aeb6456deb2e07fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:06:29 +0000

    available top suit whatever earth

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
