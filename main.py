0a4c5b6c8d1d501115fe8423bc2e1573b0a5e51c
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:37:33 +0000

    circle cross vanish good road

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
