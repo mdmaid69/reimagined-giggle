9cef5f518f348426da01d25d974e98fcb892e230
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:05:02 +0000

    what exit form beside call

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import datetime
+print(datetime.datetime.now())
