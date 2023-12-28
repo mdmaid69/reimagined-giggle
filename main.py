8127712ca0ceb71a009027de65ca8512c1879c1b
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:47:32 +0000

    ought quickly comeover early sensible

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
