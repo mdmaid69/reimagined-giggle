31ce7f8e67b506e0543e9c4198b559603eddba50
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:29:32 +0000

    twist business themselves limit short

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
