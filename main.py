daf0f64d367105d29832e88d11631ffce5f21b3f
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:57:26 +0000

    really sport takeoff stand stupid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
