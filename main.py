a86109478aeac646a5165cbf19db8d66e2832dd0
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:04:08 +0000

    breakin miss image worse empty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
