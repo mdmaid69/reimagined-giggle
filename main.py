b2c05157724481f29987180105f3479a57a73415
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:33:47 +0000

    wall opinion mix condition spoil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import tensorflow as tf
-print(tf.__version__)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
