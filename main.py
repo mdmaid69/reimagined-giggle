500cf350d345eba8b108f04bf1c0d95743d92df0
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:30:12 +0000

    even pupil accident visit pollution

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
