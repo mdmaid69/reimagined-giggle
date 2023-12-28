2f77d2298fd4b376ff8e3d060044023e11dff9db
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:02:07 +0000

    wish today burst wing mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def find_max(numbers):
+        return max(numbers)
