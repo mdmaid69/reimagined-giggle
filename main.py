bb5b9bd914cc176e403d14790082f1ed85ebadfd
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:55:23 +0000

    place music tread violence mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
