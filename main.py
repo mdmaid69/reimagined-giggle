5618c1fe1075b40ffce5c8725dbabda2d5c45a6e
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:40:05 +0000

    offend track achieve pin concern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
