9eb2c49eb17e3039ec65cf82f571fd268d7b0946
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:46:47 +0000

    tone pollution wear heat forever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_current_working_directory():
+        return os.getcwd()
