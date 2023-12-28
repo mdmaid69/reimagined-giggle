29faf82af6ffbf48381282278781b9086765419a
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:47:09 +0000

    roof seem dare nowhere bear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
