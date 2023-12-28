49b0a48db1c7e516c1c8d216d078e1fd62dd28d6
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:51:58 +0000

    old thought table offer sad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
