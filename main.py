71d24780851a9164909dd5aea99f34d3434cb3f2
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:44:42 +0000

    giveback Uu believe rent check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+name = "Python"
+print("Hello,", name)
