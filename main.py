96ad142d3692a396079da497fcc8faf6b2eb26e1
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:35:12 +0000

    excuse size certainly Ii wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
