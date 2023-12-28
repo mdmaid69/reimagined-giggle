883fac41bc8a04e85b8bb6826e5052175e81bc88
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:19:43 +0000

    recently between beautiful bad begin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-for i in range(5):
-        print(i)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
