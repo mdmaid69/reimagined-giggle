714b187006f4dfc2e3962e76841ffe86083a5488
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:20:42 +0000

    dry aware passenger keepoff noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
