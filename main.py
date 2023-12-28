4fb8124952edec285394a5a2f9a65b18aa43e611
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:16:31 +0000

    getaway base cruelty offer state

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
