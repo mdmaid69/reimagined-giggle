f1e89e963876de8466bbb5070091498ec05f06c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:00:06 +0000

    interruption replace now cost test

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def is_odd(n):
+        return n % 2 != 0
+def calculate_work(force, distance):
+        return force * distance
