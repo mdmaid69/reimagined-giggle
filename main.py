0011548dd437ce2c39f10c976111fdd8e1e9d654
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:17:31 +0000

    instruction shake consist when big

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
