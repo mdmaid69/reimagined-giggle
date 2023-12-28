c3786bb934293a584a4ffb86e45d5d56d15d7716
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:21:58 +0000

    least turnback judge month affect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
