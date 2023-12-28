9338dbab300608ce04d50d5b4311248d29f982ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:28:27 +0000

    rise please patience only tear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
