e36e15f0ef7c7411903a9198e8de80607e765bc5
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:45:04 +0000

    confused replace stop characteristic little

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
