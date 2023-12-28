8c3c939e13ac559575804eb4d32f5a18f8b802ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:48:01 +0000

    little away advanced regret young

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
