beceeca850ee3a678e855f042961b29b6fc61d0b
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:18:59 +0000

    allow exist peace sure divide

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
