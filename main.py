811a68604e46064015cfb6accb6b566eb7265bee
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:14:31 +0000

    feature complete worried turndown world

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
