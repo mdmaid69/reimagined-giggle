1a431255f2cbd825baa95c73650f8a4ea249cf15
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:46:07 +0000

    deaf takeback whatever onto attitude

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
