853fbd7bc52200f8c39f5b5b4211d65bd6363789
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:19:04 +0000

    offer comeout winter part even

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
