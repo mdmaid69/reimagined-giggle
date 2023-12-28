02fe25a2300ea98743129ff49d109dcd5a5ab427
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:34:01 +0000

    copy challenge sky most row

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import sys
-def exit_program():
-        sys.exit()
+import time
+def get_current_time():
+        return time.time()
+import array
+def get_array_as_list(array):
+        return list(array)
