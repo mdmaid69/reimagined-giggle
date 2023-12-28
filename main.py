e75c51eeae5ece5d7da41e38dcfbd97bcd78f886
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:50:15 +0000

    beginning action put lunch extremely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_work(force, distance):
+        return force * distance
