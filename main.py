f8cfe73f9eb0fa5a82dd5b58aacc59a1800c3b25
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:52:20 +0000

    sink development personal please sink

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_acceleration(speed, time):
+        return speed / time
