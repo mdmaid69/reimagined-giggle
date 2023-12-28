aab264df3a4d3b72ede3eac9c38f74ee7cde041d
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:19:05 +0000

    finish hurt cut crime herself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
