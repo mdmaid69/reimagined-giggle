c24c951a42895b99c94421ce3c94e674b462007d
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:59:39 +0000

    mother wait raw so ability

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_length(array):
+        return len(array)
