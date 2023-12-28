3aece237f7cfbeca8aa6106aabcf9f1b3dc3ae8f
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:42:02 +0000

    warn talk lucky last put

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_union(list1, list2):
+        return set(list1) | set(list2)
