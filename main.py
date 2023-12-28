10dabdc790027d07fb7eab962b3451f9b6608c83
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:58:57 +0000

    hospital record new warning job

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def sort_list(lst):
+        return sorted(lst)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
