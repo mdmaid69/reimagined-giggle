da49786fbb9191c9eb68b0274345efc13396e0df
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:41:18 +0000

    travel dear nowhere art mother

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
