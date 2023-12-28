426856b062ff6ea9410f95d54bd935ea4de2ee12
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:36:32 +0000

    cut crash prefer night fierce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
