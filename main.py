2f61112f0a590e86295f53ec77ace2f6d5c3fe2a
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:39:08 +0000

    grand hurry plastic office cook

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
