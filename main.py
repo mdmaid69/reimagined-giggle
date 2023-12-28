c1c27f7f52caf4ca829e6ad3b76928b97bffb999
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:29:18 +0000

    mind brief sea already crowded

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
