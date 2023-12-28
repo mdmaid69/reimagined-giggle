0fddc8ebeeeb14a125f41fd1fa188fa52db7f645
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:08:54 +0000

    constantly suggest whom breathe shall

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
