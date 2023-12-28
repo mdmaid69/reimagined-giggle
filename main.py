16d37b83dece4fa4fc14653513b0e1583489a80e
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:30:11 +0000

    swing myself mind accident 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
