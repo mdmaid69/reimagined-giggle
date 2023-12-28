c860c1c3e0bb36f22cf1d848003d995f80f44e60
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:04:30 +0000

    final store rely prevent OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import sys
-def print_python_version():
-        return sys.version
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
