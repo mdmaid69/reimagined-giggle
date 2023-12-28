78c6b53b5695614484d506687c056ef6ae89bd77
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:25:51 +0000

    announce disappear impossible ilovedicks amount

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
