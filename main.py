858890f404381bdc3604f0db80d4334ebeda47ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:35:43 +0000

    class rubbish pity aim platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
