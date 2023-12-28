dc5daed1e74173d19f2997e11ba6b9d2fe9808da
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:52:49 +0000

    balance field dry male forgive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def find_min(lst):
+        return min(lst)
