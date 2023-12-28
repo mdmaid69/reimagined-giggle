7f06cbc741166b14c45e46c94c83c74c2e9f1721
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:17:42 +0000

    might cruelty contact reduction deliberately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+print(math.pi)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
