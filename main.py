39e3d6ca2b450dbdb58f27e034ba8c9135c6d813
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:49:32 +0000

    peaceful film best impossible admire

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
