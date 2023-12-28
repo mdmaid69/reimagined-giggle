cec9a8e05b536a173d7421b9e56f4ad4f8f1b631
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:40:00 +0000

    question intend dick great condition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
