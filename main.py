483ae5ebe0ad0829d7dc172f6e9e3d375734b5f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:39:12 +0000

    miss shall welcome branch serious

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
