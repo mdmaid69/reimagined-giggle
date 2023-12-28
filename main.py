4a889eff2cb8fe2bb9143339f8dff7b5166e4555
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:13:37 +0000

    condition skirt moon giveout who

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
