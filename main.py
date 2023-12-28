8d9a930a2fd88a6caed80d063262c1f344beba14
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:56:46 +0000

    back damage destroy account properly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
