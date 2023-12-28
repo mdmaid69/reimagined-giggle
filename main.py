dc706aae623b4b3e1b2e843e0fbc31164cec2dc2
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:45:01 +0000

    natural pay taste edge money

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_area(radius):
+        return 3.14 * radius * radius
