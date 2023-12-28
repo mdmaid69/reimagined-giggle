c5bad84fbc7d1e05f7247b30c07ecffe3aacb886
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:02:33 +0000

    usually himself surprise whistle criticism

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  def is_odd(n):
+        return n % 2 != 0
