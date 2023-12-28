afe54ef5e84e6d134a1f75cc37c5e46fd5ea4f71
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:54:17 +0000

    Ss wrong lesson difficult example

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
