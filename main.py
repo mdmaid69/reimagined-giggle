90190e21e440460e9c47e8f758dfa5d34785c040
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:56:02 +0000

    stamp friend hate rent innocent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
