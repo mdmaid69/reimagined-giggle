f923ca6183fb7ea7b9dd676c5864d85abdb3cb01
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:40:34 +0000

    swing order separate often pretty

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_item_count(array, item):
+        return array.count(item)
