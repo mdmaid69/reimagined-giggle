52dd535897431c6f4ceb4bb1f9de8d198bd5e9b3
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:00:10 +0000

    couple seriously think smile fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
