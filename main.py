3f91d2d8e0e4a99d4e49fe89f4a99122956f102d
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:17:40 +0000

    exist more lower order bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def divide_numbers(x, y):
-        return x / y
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
