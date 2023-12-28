0646d14939064993cfcf6dc69b01176120038520
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:59:27 +0000

    old embarrassed prove accordingto allow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import array
+def append_to_array(array, item):
+        array.append(item)
