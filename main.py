0c884ba990e7545d4a55438ee3331b9d3482dc92
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:54:51 +0000

    woman old bear probably workon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import collections
+def create_user_dict():
+        return collections.UserDict()
