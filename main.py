df8f92acdc4000807219fee48d4b38909a1e6dbb
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:28:59 +0000

    share legal conduct bag guilty

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-name = "Python"
-print("Hello,", name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+x = 10
+y = 20
+print("Sum:", x + y)
