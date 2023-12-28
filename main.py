5e39eff9803c0b4baade57bd03333e547f300671
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:04:02 +0000

    butter confidence sail that than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
