0c77aec576708504c5ffda49406f74a4806bebc1
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:58:42 +0000

    long event wild rank smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
