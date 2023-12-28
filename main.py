652920b87cdd4ac911946ce6ada8686edc25ccec
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:06:56 +0000

    thank thankyou giveback store note

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def clear_array(array):
+        array *= 0
+import array
+def convert_array_to_string(array):
+        return array.tostring()
