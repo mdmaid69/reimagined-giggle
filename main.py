0e7f3ffd3d1ab6f9857c8bc52a9bfaa393fc548e
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:09:14 +0000

    relief life increase cut discovery

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
