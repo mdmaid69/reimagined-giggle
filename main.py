e9dd4d68cc2574aeb20462267bfc8a6daf8e4121
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:09:28 +0000

    from responsibility anybody fresh call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
