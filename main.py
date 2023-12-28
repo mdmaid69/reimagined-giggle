2910d47468b4c125d4388d3bf67e8879a63f4e10
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:20:16 +0000

    fairly autumn bringback read principle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
