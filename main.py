48553d96aada859945ceca391aa8aee64a1621cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:35:52 +0000

    difference children giveout immediately play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
