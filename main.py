e2030a4487a2720f5cc0e072492cd2f6fd554c11
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:57:46 +0000

    fine look surprised talk persuade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
