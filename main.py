b8a1fdea72ea665392927ac23fc65c52437a97df
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:29:15 +0000

    sorry pass another realize chase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
