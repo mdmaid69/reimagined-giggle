47da9c1de20ca9149109531a3444df267d6190fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:35:17 +0000

    alive repair quit fence general

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
