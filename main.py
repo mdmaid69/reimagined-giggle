bfd263e55e5c7f5109a6f71f80a464eae716f183
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:21:14 +0000

    scared finally whenever fact sing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
