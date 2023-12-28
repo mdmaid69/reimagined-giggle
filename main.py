c93945690e954ad05a8706a3876249ab19bf71b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:56:20 +0000

    theirs which wash mass takeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
