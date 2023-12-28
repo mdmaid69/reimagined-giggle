e6c036f89d5484bcfb2e298e0a649690e26cc538
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:23:38 +0000

    whoever abroad stranger cover soft

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
