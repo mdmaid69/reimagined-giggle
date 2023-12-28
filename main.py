956153f5525139b6b358157ed93f770db87121c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:51:01 +0000

    while medium slightly joke frighten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
