a50b12894ad377d47d8f9163b69d84e7604b1281
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:37:05 +0000

    able wear exactly your escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
