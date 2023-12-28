7578d90babd4cba97fcf6b06e45ec95f5faab96d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:01:11 +0000

    noise please secret wander soon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def subtract_numbers(x, y):
+        return x - y
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
