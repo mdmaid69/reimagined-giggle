99203c3f85316300ff1fd922f265649ee04de88f
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:03:10 +0000

    proud dull save young on

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def square_number(x):
+        return x**2
