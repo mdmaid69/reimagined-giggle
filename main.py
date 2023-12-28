5018bc5886e204d4f5557a2455d8b0cb015b5867
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:45:31 +0000

    sleep effort long area ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
