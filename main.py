c9b64fffeabf667b4b566e13e25a1070bb607385
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:58:20 +0000

    furniture help train surprise I

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def square_number(x):
-        return x**2
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_speed(distance, time):
+        return distance / time
