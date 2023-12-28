aba3a1ea32c26b1c18f5af3eb132dbccb04e0fc6
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:50:46 +0000

    almost you degree fortunate understand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
