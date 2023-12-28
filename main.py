cc7e37700314c77d4fe3494d4cb2e96dfeb14ce1
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:07:53 +0000

    frighten apologizeapologise snow dress confusing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
