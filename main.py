1ab47f96880985bcf7b746b85f248852cc6b67fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:43:18 +0000

    weigh over field race different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+print(sys.version)
