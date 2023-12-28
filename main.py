e94152f7db7c9d855976b2bda7cd3e9eaa54ae5b
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:09:54 +0000

    zero break difficulty system goaway

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
