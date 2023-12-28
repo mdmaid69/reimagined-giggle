cec3d1122a6bf00928ca269756e300b5082e408d
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:58:54 +0000

    life keepout certain crowded problem

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
