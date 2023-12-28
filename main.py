93de682e5cc58baeec48e01645fb418bfdbfabe2
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:43:31 +0000

    fly when quickly centimetre answer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def convert_to_hex(n):
+def convert_to_hex(n):
         return hex(n)
+  def remove_duplicates(lst):
+        return list(set(lst))
