1bc32a316480c588c0afbe3223a3e8dee4ee6bfb
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:37:33 +0000

    unable promise entry concerned couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
