46d07c1e0bce162da134e48f5c2fbc0f8acbe03d
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:06:23 +0000

    war or survive increase defend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
