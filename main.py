7d300e05042577c4e625e07c80903b1c2cea783f
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:08:11 +0000

    especially pool many bread basic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
