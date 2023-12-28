d8464569478d1328f72907076294f4561d0d75c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:24:04 +0000

    mad pronunciation opportunity own include

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+print("Square numbers:", [x**2 for x in range(n)])
