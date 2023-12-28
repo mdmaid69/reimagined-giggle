6f02d4757042732cc7a63a46eee1b77b2998f228
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:56:21 +0000

    supply recommend confused radio mother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
