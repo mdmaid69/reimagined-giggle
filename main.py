5bf614e72e419ec44ab4a8b753ab940474e3b36b
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:27:22 +0000

    callout dead whatever to tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+print(sum(range(10)))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
