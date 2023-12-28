bd338b2beb55e3036f22acecd0faa6468598abe5
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:01:35 +0000

    in rough them arrange be

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
