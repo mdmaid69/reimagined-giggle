21038d661a1286e17bee67438b632d70d6a89aad
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:45:04 +0000

    unable pencil female unkind comeover

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
