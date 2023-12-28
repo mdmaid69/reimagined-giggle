d8aedf99108b81ade7a3125fd864c01ce7309405
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:30:21 +0000

    free hide correction herself separate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
