d61911e3d43960a789a8ef488afce9005cd9c103
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:11:04 +0000

    home exam pump over software

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
