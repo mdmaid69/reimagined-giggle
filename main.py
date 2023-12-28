794a9a9e963ccbac7f9f7f70d7cc97f427d6a44d
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:09:57 +0000

    content broad something aim prove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_min(numbers):
+        return min(numbers)
