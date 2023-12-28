c76d3e853cdff59374ff687fd00d0d4c88746141
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:18:38 +0000

    black movement float major value

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-name = "Python"
-print("Hello,", name)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_repr(array):
+        return repr(array)
