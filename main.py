679a29723a0d4729bdd95507ee946c7bab26c14e
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:33:53 +0000

    quantity course fire extraordinary above

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_typecode(array):
+        return array.typecode
+def reverse_string(s):
+        return s[::-1]
