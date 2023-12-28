770d97473eb5e3782a04be8dac0a4ffeee0338a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:14:07 +0000

    giveaway autumn bath murder ought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import os
-print(os.getcwd())
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
