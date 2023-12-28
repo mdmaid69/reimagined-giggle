9cdd774173f71aff04d39d3c069e315caf275796
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:31:38 +0000

    border picture goout continue arrangement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
