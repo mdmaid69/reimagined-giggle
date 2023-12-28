ea91aac6ee31b5add5a07bd00cae7d7d6f994498
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:29:17 +0000

    improve rarely collect dress price

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def square_number(x):
-        return x**2
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
