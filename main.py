fbd971faa8b804ee902228915947cab4ebed8db4
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:43:11 +0000

    mood rid telephone emphasis comeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
