b48cf4b7100d05aa49382b8aaf532975c9e76162
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:35:14 +0000

    within happen tend left attach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_list(array):
+        return list(array)
