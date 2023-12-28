606ec11cd5e44cd0c94d0f218c77499f46d33070
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:48:02 +0000

    dance some hide close yesterday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_floor(x):
+        return math.floor(x)
