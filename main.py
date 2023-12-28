fb0d594cae83cd71ad5cd4310e957a6ab2fc120f
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:43:20 +0000

    giveback drink Uu loan lunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def add_numbers(x, y):
+        return x + y
