9619946fcda68b6c65a9db98524e270d5c7d6a7e
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:47:47 +0000

    add feed clean include entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
