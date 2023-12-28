3d88fc0bef91f08f49fde7d8e0f9a9a86f01a4fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:35:09 +0000

    press load different position geton

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def cube_number(x):
+        return x**3
