b9af1a01802fcdc0003ef63965b19de3cec76d4e
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:46:20 +0000

    cope identify start great often

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+print("Hello, world!")
