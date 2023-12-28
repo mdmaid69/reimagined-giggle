8f29d09e0c1850e924411804089a9ebc3500deed
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:38:38 +0000

    conduct uniform prefer noisy preparation

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import getpass
+def get_username():
+        return getpass.getuser()
