6df7d8f272276f8edacd68b9d1307b2fab2e8641
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:59:19 +0000

    easy maximum culture land considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
