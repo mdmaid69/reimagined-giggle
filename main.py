33109584ed112d4e0e314bae7e0ec796f0c38407
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:51:42 +0000

    many sort anywayanyhow valuable major

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
