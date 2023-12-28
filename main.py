5c065ad6bc1ea1f60401dc6b978cef21f373d4ef
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:12:58 +0000

    platform keen message perfect call

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
