aa72eaae8f4b6662b57250890c413efc73325009
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:05:12 +0000

    instrument minimum Mm private want

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_current_directory():
+        return os.getcwd()
