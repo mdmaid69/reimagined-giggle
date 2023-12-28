17f83f14e31a3c20df8d3b78b2d9f073c29a8d46
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:23:54 +0000

    dish high disappear cross life

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+print(re.match("h.*o", "hello world"))
