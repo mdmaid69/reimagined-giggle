ae7c9910578a2dfc63d0d46bdd7edd4b2e15e67c
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:05:30 +0000

    height luck decision convinced dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
