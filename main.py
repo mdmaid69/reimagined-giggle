ac84eea344a0ed3e92e2ab7f3ab0e9d08f0d7851
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:12:39 +0000

    workupto bet more fact complete

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
