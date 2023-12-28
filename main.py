c8719ae132b699b68907d4bb83538f9dd89d2b76
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:48:54 +0000

    average terrible push plate hollow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
