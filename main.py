2b262fff4490c682bb1320ae37da4eaebbe56a70
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:44:55 +0000

    basic sharp strict importance creature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
