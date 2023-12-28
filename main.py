580be52ccb80d833dd29a4d7df5ee9b3c04f6dfb
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:06:20 +0000

    battery good basic holdon suckme

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import time
-print(time.time())
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
