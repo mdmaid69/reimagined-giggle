13e4af8a6faf6547982f2e8da117d4e8b7de155c
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:59:31 +0000

    large expensive relax service second

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
