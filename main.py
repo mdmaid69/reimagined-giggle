6ac1ca6a820b3fa5d56a3b23d0c65d2bfb17bb6a
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:52:17 +0000

    task ill human hot total

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_density(mass, volume):
+        return mass / volume
