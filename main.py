9704948afdc84e4e6be9f9a126a0b3d912cdce25
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:54:43 +0000

    damage come apparently building copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
