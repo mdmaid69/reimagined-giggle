63dac910aca923ff80eca89c7e251febcb5bec04
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:37:40 +0000

    environment sister peace straight Itself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def multiply_numbers(x, y):
+        return x * y
