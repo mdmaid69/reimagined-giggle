8edbb8945ea0249510977d73300b0772258cb896
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:04:37 +0000

    yet rich thinkthrough steady popular

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
