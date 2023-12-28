1ad411a13fc138af170516a2f9beb9d706fb701b
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:23:37 +0000

    his answer makeup comefrom improve

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
