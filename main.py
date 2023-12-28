782531ebfe74017bc61182e1463660532f241db3
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:01:27 +0000

    into tip Nn funny generous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_octal(n):
+        return oct(n)
