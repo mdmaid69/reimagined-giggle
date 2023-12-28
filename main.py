08543d4d9a90d654b9a12c2bd24f00073f4766c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:41:02 +0000

    hate tie murder cost sudden

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
 def clear_array(array):
         array *= 0
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
