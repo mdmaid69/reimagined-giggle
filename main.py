4e7ddaf96ce7582bc3bcce5a41629480b16e8b6b
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:37:17 +0000

    cinema place temper sleep introduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
