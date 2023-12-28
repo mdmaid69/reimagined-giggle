baf7ba1c12b3c2510c55d549a04f9606c198952a
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:26:54 +0000

    mention turnoff fall ours system

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
