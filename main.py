2ed1c41b67c495abc891c4aec0e2868a0684fd33
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:51:04 +0000

    declare back immediately uponon rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import datetime
+print(datetime.datetime.now())
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
