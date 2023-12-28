e6727d4a1b6664059766ae2d33618254b365deb6
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:34:24 +0000

    handle tone laugh off sudden

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def divide_numbers(x, y):
+        return x / y
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
