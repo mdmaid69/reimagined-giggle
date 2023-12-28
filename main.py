32515f82af5d21ac1a583f1de19d34ed7f37f408
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:14:08 +0000

    cook successful ordinary first blame

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import array
+def append_to_array(array, item):
+        array.append(item)
