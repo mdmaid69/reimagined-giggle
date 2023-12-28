324b47f33dd516a4555785b1c9c9afeaab6d793a
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:56:01 +0000

    production Oo complain guess hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
