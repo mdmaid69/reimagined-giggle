765d20801228f0d4e79aa46abbeb470e6e1c6ebe
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:50:24 +0000

    drop wet shall somewhere sting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
