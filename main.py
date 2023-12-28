7e7db25da354250ae6ae2bcb8174e3eda707536d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:56:30 +0000

    back bottom actual event joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def reverse_list(lst):
+        return lst[::-1]
