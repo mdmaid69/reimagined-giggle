97f074bd442e82609da2399957bd7b8dc6a1a17b
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:29:59 +0000

    congratulate who meaning man fence

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def find_min(lst):
+        return min(lst)
+def find_min(numbers):
+        return min(numbers)
