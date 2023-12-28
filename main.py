09da94af21fff5cc7d3200e147030f0d6faa3e8a
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:29:34 +0000

    contact clean notice sort comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
