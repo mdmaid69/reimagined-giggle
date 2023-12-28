57f32a890ec6f85d1b3fe2168c6bee781731ff99
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:01:10 +0000

    pen used cause face voice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
