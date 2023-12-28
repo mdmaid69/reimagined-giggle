ad44551b8586e4efc3832fbb8aca50ada4123f98
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:12:41 +0000

    why high forget delicate ahead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def is_odd(n):
+        return n % 2 != 0
+i = 0
+while i < 5:
+        print(i)
+        i += 1
