b2c9a5ba42eb1c5646dcd8b5b55bc8f2474871bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:38:13 +0000

    innocent branch tongue not furniture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
 import math
 def calculate_circle_area(radius):
         return math.pi * radius**2
-import array
-def reverse_array(array):
-        array.reverse()
