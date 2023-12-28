a89317d2ddf9dfed064ceb32107c41cb6f12fb94
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:36:19 +0000

    secret and genuine secret needle

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
