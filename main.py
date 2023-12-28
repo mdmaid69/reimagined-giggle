bc5d7f8d3a973dbca87b8dca62c26fab2217de3e
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:59:48 +0000

    takeaway gate image tread plain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_work(force, distance):
+        return force * distance
