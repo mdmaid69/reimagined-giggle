920c137c83abaea862c80144a68a8ab4e99b1108
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:57:46 +0000

    already soft point authority neither

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
