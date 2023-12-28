796968f579d9944088f502501a0f14490d1b6c2f
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:26:29 +0000

    slowly unfortunate Ss joke intend

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-name = "Python"
-print("Hello,", name)
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
