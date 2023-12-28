2d2411b0bd121820b4a7b1e1b3f84366bccd645d
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:56:13 +0000

    giveback left care light regularly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
