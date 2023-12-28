2a71b84e40c44c9c7ea0ddd815d0daa685c4a66d
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:56:48 +0000

    interview him burst this job

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
