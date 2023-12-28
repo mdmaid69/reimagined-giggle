9681bfe93551d9d11db073b9c39819a332aa0361
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:47:29 +0000

    strike sure comfortable many skill

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
