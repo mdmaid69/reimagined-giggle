444ff5a6a957540770137654ccdbf83ffcd8c579
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:01:37 +0000

    always she major dear appear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print([x**2 for x in range(10)])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import getpass
+def get_username():
+        return getpass.getuser()
