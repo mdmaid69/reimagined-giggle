47ae6626109846c4048b890dd3325e94774efe6a
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:22:39 +0000

    march comeback truth sure comfortable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
