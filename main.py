11165264699c2aefba011b0ba8dd12116464032e
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:02:39 +0000

    valuable recognize out disturb skill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
