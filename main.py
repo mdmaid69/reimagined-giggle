7cd0883a8edb89075c3d42e63ca1229e5b47156a
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:11:08 +0000

    this block convince fun share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
