d1208de3115813c7e0f636090a773954a020e0a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:15:06 +0000

    empty rob either Ii set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
