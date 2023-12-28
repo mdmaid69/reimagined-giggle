f8192eb2a42ad38b3edc3520fa2f6c4d28af47e9
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:54:40 +0000

    proud sign wire hurry flavour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
