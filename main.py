b3e9ea95732ae40b3f11baae670831c665dd95ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:09:45 +0000

    weekend moon always journey pleasure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_palindrome(s):
+        return s == s[::-1]
