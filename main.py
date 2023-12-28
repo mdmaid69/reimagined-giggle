5ead5be99c0a993fa29437323bf916b047dce77a
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:05:48 +0000

    inside quality damp autumn refer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
