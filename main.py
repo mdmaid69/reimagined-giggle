9030e408ca18d144dbf3eb14e9d7eff5fa09c04e
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:51:14 +0000

    progress study perhaps lucky definite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-def add_numbers(x, y):
-        return x + y
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
