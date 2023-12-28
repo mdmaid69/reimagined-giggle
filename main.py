1882a0ef03814f7746f645123c6d336cab0cf4a1
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:17:49 +0000

    getout mention design suddenly comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
