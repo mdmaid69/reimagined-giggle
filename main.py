fc19a7f904832b978e1bdcafd59f041259360124
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:35:29 +0000

    edge circumstance sit entry sensible

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
