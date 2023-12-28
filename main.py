b3aa45ccf66774538a03e2b6a82c3cc68f8fa9dd
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:52:29 +0000

    surprising weigh actual unit sail

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
