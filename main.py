e8b0e3df682422dae0c6f8f104bf295f2640617c
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:57:56 +0000

    choose fairly factory crime sweet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
