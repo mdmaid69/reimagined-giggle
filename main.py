27d1badd920b8239c7b4cc645c37ecad88fddc5b
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:18:50 +0000

    OKorokay determined claim nurse rub

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def is_even(n):
-        return n % 2 == 0
-def add_numbers(x, y):
-        return x + y
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
