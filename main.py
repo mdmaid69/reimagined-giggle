12dc95d265f7031547635f57733f2c4af4527972
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:06:57 +0000

    goback(to) glass beginning acceptable mixture

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
