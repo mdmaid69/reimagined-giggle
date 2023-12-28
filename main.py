c9c2256e0f4c6c16daf89c209e2713426d342896
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:44:55 +0000

    attempt warning any wait distance

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
