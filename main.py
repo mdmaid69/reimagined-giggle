f0144bec8472484aeae07fa714c77792e63e0144
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:10:42 +0000

    free fat gas ground rough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_pressure(force, area):
+        return force / area
