2f499f8c865220384dd76ac35ab2ffd9ef194c55
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:03:33 +0000

    hit stop comeround getout see

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import random
+print(random.randint(0, 100))
