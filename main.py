e57d5f1dfda7e3d40609ed5e2500498042e8e50f
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:37:47 +0000

    niggaacademy minute tradition except forever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def is_even(n):
+        return n % 2 == 0
