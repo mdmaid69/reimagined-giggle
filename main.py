6c6f09268e508201981ec86c727213c5f7943122
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:59:43 +0000

    dare contain serious soil first

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
