bed795990ed03ce5aa1759726db04d5e49db61ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:37:28 +0000

    base brush project roof mixture

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_area(radius):
+        return 3.14 * radius * radius
