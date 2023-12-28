721d98165978f64785d3df7d1e0c57073c073861
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:22:30 +0000

    hit seriously distance nor hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
