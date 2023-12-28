8ab4034c829bee7526801be34066e8e6c7081355
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:48:17 +0000

    marry term blame important turnout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def clear_array(array):
-        array *= 0
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
