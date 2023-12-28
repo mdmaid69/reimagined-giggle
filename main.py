715e240b085c6ad9ff5f34504cf64a8329ed0a68
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:22:59 +0000

    Jj ear perhaps talkinto criticism

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
