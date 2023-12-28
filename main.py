a65488ba20dfb6f69a48978c289df2fb1cbc9200
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:42:05 +0000

    to support increase side effect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def sort_list(lst):
-        return sorted(lst)
+def find_max(lst):
+        return max(lst)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
