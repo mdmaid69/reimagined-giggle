4cc47663c80e22d83b706086a2da4a3b4433ae58
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:03:00 +0000

    bound hide favourite number pattern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
