7ea9ceb26e599ef84bc73384271cc93b478dbe20
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:03:12 +0000

    close temperature top moral turnover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
