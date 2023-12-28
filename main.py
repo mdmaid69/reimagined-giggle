c135adcc89e1529ee81c970dfbb77eaaab245132
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:00:06 +0000

    beneath advise necessary together shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
