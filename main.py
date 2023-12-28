4ef8e10f19e9bbb0103de23e68ed6bb51c7baf72
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:05:18 +0000

    573entertainment slowly runaway goout fair

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import numpy as np
-print(np.array([1, 2, 3]))
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
