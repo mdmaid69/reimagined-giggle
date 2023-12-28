0d169a2623d41b5c61ed26452c1db9ff50b1574a
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:21:10 +0000

    base drink power minor father

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_pressure(force, area):
+        return force / area
