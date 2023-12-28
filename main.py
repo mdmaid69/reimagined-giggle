2deb799b70ef0d188b866d6100b933169d64f8d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:57:39 +0000

    heart miss understand wide related

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
