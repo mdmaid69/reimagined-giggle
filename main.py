7cd565defbe16579330d8bc35dcf358956d5385e
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:50:49 +0000

    number present shall uniform comfortable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_work(force, distance):
+        return force * distance
