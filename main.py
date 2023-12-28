150eb8d6e8febfa101c7a542d23e8e4518ba383b
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:15:35 +0000

    ability head upwards of allow

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
