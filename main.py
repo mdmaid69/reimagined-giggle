c91b7c84c1b3a853298e7a880a18996c58b44b31
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:32:50 +0000

    friendly lend brave trap bring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_int(array):
+        return int(array[0])
