cadff5e2add795740dbb6bffc6ff3067a1d4783c
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:24:51 +0000

    trip plain hers so open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
