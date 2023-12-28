74ceffe2d72fa4276f5110094dfa69a1a5b44fad
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:34:30 +0000

    protest future profit meet free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_work(force, distance):
-        return force * distance
+def square_number(x):
+        return x**2
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
