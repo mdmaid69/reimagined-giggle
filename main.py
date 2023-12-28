8632b1c5b75c0131d80e31ab416cc33375b03076
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:50:48 +0000

    measure join try advise meet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-x = 10
-y = 20
-print("Sum:", x + y)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
