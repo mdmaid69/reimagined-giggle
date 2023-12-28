6b5b2bd6b41496a0b8f000ebd7684583f3f91e06
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:43:31 +0000

    memory 179bite quickly direction round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
