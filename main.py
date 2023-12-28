02f462ab95b01649defb5fddebb673ce9638b674
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:26:00 +0000

    familiar switch position sell peace

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-name = "Python"
-print("Hello,", name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
