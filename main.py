e2035a688897f50f993d5cdb0c4da061a044b7d4
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:14:32 +0000

    final store trap terrible illness

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def cube_number(x):
+        return x**3
