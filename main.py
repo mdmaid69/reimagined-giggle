334d08ec6310b678be41cf94696a9701822f0602
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:26:46 +0000

    sentence basic busy wander honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def split_path(path):
+        return os.path.split(path)
