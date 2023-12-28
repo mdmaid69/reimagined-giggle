29421321d9ccf327bc9fa1b6b078c2a90d138047
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:28:11 +0000

    feeling strike sign connect pocket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def count_elements(lst):
+        return len(lst)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
