5e4be61d5b8d73036a6084eea60c11d1c35b9da0
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:05:04 +0000

    store hold better pattern relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
