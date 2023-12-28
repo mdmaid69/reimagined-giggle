53d0a49dc7d980a46bf5d84ff5c8514893318d68
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:01:25 +0000

    angry there danger lesson strong

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
