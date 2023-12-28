db9c6cb5d8ddfd866b3cf54f518c3cd976c57ef8
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:47:38 +0000

    fear tomorrow upwards left meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
