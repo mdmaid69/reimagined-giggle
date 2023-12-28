8decbf0d1c43f6ac916c82d9681ebb34bbc4e7ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:31:19 +0000

    common mistake concerning else sight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import os
+def remove_directory(path):
+        os.rmdir(path)
