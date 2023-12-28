04168ebc5a896b8c45fbe8f9a6339b14d8716037
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:38:43 +0000

    fault dress relationship secret my

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
