5a7ceff4550df9f035ca7dab830ac1afa097452f
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:28:57 +0000

    getoff game store owe care

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+name = "Python"
+print("Hello,", name)
