475b31205bcdd438559e34fa21f4b6d71d0bc87b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:30:22 +0000

    before while underneath man fly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
