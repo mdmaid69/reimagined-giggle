609323a140bbc3f15c03869fee9c9001f5c4d51a
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:51:33 +0000

    block foot slowly worried theirs

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
