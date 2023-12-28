0e21904d2b5b8911438a16e500273906bc32d702
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:17:16 +0000

    border now juice fashion pain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import sys
-print(sys.version)
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
