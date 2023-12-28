e9d45d64a199014f9e96140591da0b82e72ddfcf
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:09:45 +0000

    takeaway nor permission watch wash

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
