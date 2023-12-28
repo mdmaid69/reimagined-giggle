fed3bb4e396e96e3d175725636c1ac3e0ea10eb1
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:58:05 +0000

    they operate violent nowhere freeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
