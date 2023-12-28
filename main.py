b26f5b63dec7cbb8e889f5a8cf831595f69f3092
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:18:53 +0000

    rest design hate for breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
