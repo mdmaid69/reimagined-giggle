274790f71c425ca5c50e04287014a658c7c19017
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:17:07 +0000

    man fear branch shoulder week

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
