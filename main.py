5d5a08237187e536e6438d6ef945f4978c9d0d15
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:09:09 +0000

    hardly tape bang apart attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+def remove_duplicates(lst):
+        return list(set(lst))
