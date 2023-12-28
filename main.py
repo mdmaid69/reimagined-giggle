1fa2686ada7ae0d9ef4eb28d35b70ac405314a6c
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:45:02 +0000

    runout Ee complicated flavour confident

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
