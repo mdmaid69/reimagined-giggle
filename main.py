2d398320cc6f4c89e09578d4d37d5585a003b1f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:39:59 +0000

    finger disappear feel appeal term

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
