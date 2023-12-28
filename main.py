f0f3a9006ec693b64d25588e998a681e09f31378
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:42:27 +0000

    there hour giveaway coffee sentence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_index(array, item):
+        return array.index(item)
