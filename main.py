107dc4c3605e3c6d544f28c8154522c282c4ac2a
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:58:37 +0000

    573entertainment help crazy shop firm

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  def sort_list(lst):
-        return sorted(lst)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
