e27d8184b96d9b6afa6fb90a3170a7a202927bd0
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:10:19 +0000

    right actually tight production visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
