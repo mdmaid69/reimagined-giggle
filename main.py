9773034726736e8795b33ee5e7328dd175387d22
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:58:40 +0000

    whole keen whatever myself single

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_list(array):
+        return list(array)
+def calculate_acceleration(speed, time):
+        return speed / time
