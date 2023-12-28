31391c2c341a27747c6a6a47523b372a61531f98
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:15:59 +0000

    sing tall live aim part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
