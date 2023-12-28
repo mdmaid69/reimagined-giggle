d35bd87fb16fd225ae79c4d77fd322af9c6dd79e
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:33:33 +0000

    fall at wear commit means

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_repr(array):
+        return repr(array)
