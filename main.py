df3051dd514ab639da59806336adfb235d442af9
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:22:56 +0000

    giveback entry himself please play

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
