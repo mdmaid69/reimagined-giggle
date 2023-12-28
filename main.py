27af5aff9442c51bb039eae023f87bd263185b3b
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:25:12 +0000

    leg thankyou off question but

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
