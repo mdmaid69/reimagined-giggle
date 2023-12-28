c98afd6ef9b28f28f3d69929f7eeb3f09b0b6323
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:50:14 +0000

    mean Qq once that shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
