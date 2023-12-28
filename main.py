38d491868d29059139a4f61d650918c59458067f
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:16:53 +0000

    wide grow frame stupid drawing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+text = "Hello, world!"
+print("Characters:", len(text))
