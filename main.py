ba32434617109f20e6c8c91213ba8a6e911cf4ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:00:57 +0000

    hand appearance car short normal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
