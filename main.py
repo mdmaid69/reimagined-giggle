baa7cfb303fb7252b8536ad4fb97f5ac90a3d02b
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:30:31 +0000

    tidy black basic best petrol

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
