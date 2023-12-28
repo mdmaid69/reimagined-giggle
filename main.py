93bb6237f9ec3440fdadd0830942ad7556e6b08b
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:55:49 +0000

    set popular possibly inside turnaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
