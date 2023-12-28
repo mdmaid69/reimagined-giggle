128410ade03604c15d3b7a37c59abea914fdd429
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:06:09 +0000

    play traffic drop power whom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import time
-print(time.time())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
