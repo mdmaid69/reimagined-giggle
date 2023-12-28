38a6bdd1b04eee71134891a080735a866d50fe2f
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:26:39 +0000

    fly always weekend keepaway or

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+print([x**2 for x in range(10)])
