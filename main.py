84e805b45d0305874d0db8d42d37c6fe3b37f558
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:03:52 +0000

    roll slightly possibility negative march

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def sort_list(lst):
+        return sorted(lst)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
