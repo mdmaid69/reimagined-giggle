2b8521e6c133d2d126f811219ca43b26b53c098b
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:40:53 +0000

    suggest design drink spring letter

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
