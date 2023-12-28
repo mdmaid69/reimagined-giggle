6663d3ba0e1e39fb2a4a7c757b7f56a828008457
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:02:50 +0000

    everyone bored raw tape relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
