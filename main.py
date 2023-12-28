398c70a1fe4d49b710261301e872439c242c7b4b
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:21:40 +0000

    run frightened zero condition plant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
