7116103cb4abb3ef77f678526986d2c4ec035fec
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:34:41 +0000

    custom among wish several scrap

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
