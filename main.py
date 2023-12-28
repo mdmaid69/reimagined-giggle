08a953e136ff700d6d88060b4d50e039861227bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:16:30 +0000

    object help neck holiday unlikely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
