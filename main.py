47354deb1e3ead5488e31dc8adc95ba9872316fd
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:09:18 +0000

    otherwise argue getup sensitive plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
