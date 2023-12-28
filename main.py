0e4e391577d4a386d19ed1ea166d25b1c465ebed
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:37:54 +0000

    patience centre offence tax wall

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
