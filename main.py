b4efb0b4de15ee26a4ce42bf96c3e76553e22837
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:47:22 +0000

    completely since crime however available

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def square_number(x):
+        return x**2
