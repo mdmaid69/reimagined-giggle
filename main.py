47f564c807f6e35f14f851b5b254de25df340eaf
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:18:17 +0000

    exhibition description fear town butter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
