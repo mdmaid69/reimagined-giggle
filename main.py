92a5e00e2753e495cfed7ff6a976dcc8c00c7f59
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:52:28 +0000

    passenger mine acceptable school natural

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_average(lst):
+        return sum(lst) / len(lst)
