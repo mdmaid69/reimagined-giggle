73f9c679d274af259d95253dd4e50c8e65ba553a
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:41:27 +0000

    guess duty proper no form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
