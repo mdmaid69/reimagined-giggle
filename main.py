5b2d701b2fd74ec2b36522df8b3852f16fac8a89
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:31:50 +0000

    let off wish scene basis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
