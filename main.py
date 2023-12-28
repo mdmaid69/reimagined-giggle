8fa05c1c569948da0c65ddd25f2063914994f243
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:06:16 +0000

    shame test pleasant personal my

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def remove_duplicates(lst):
+        return list(set(lst))
