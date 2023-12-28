a718c35792d04d74728cd382b3001efefd5de789
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:14:46 +0000

    backwards value Ff pain price

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_volume(length, width, height):
-        return length * width * height
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
