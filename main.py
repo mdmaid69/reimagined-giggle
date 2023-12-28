f457345db4f8488f9077bd3fc2f35551fcb9558a
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:01:54 +0000

    replace straight traffic Ee Nn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
