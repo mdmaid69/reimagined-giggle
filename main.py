fceb48d228ddcf7423199740d02f3d36640846c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:36:36 +0000

    cheap write name in middle

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
