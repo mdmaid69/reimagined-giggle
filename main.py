17779571efb1bf3e0e5ea16fbe06932ad5b1728d
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:56:56 +0000

    criticism fasten whistle rescue ground

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import time
-def get_current_time():
-        return time.ctime()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_list(array):
+        return list(array)
