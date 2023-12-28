26ba56be578c6ed0449cd7ba70c781bc80b22cc1
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:08:01 +0000

    quickly later someone themselves impressive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
