a579d58debb6181936e7571fee5bfd3778865ee8
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:35:41 +0000

    at vanish offer act decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import json
+print(json.dumps({"name": "John", "age": 30}))
