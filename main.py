59ff8b61c7ab1288f495e6ab738c80fe8669f07a
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:52:00 +0000

    prevent thinkover guide label stage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
