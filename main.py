e1410a713aa9f58669857f5b41e8d4ac81bcf436
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:41:03 +0000

    dick frighten problem well gooff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
