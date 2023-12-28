6430a1a1a1c3f55d60fb0d6207fa38c0dc6da13d
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:43:03 +0000

    full watch gooff really bang

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
