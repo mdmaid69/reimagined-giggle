e8516c50a5b7fe7c163f1f310e8e6fb76baf02b3
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:17:45 +0000

    short Yy unit and human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_list(array):
+        return array.tolist()
