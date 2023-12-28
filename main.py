cb8f581e878ab242485cf0272d6cd26981930408
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:21:12 +0000

    which equal spring someone need

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def append_to_array(array, item):
+        array.append(item)
