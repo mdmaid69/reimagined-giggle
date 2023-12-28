39d18b01dae06fec5bec391bfb1d5cec4f58bea0
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:26:06 +0000

    negative mark slice tense important

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
