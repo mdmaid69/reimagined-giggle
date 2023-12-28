c62bbfbe1e9ae6717d4f38d19091eacb3f3a9776
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:21:50 +0000

    scared approval owe trial dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
