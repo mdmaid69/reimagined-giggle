39a6e8f6d456f86a9bbca0232976ba831d4aa5f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:47:50 +0000

    break live minor upwards act

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
