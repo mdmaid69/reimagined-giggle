39793cd30c05383e9d3e1d49fd90105f35072dad
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:20:48 +0000

    feed quiet tip its flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
