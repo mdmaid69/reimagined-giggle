a036cb29b565ba6f4c1d229dbee2fd82871d5162
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:45:20 +0000

    congratulate poor turn subject allow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_list(array):
+        return array.tolist()
