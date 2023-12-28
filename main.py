e538a04acb5e8bfad53133ceddde3397bd4cf659
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:55:06 +0000

    walk sister practise keep sea

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import os
-print(os.getcwd())
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
