d5866383aadfaf90fa487555d78b1b1afeb9ffb1
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:03:54 +0000

    satisfactory takeout mix smooth chance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
