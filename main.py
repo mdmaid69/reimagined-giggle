fabef34756904795584baffe077d698f614472c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:48:33 +0000

    trouble answer already fine disappointed

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
