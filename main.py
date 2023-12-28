1a32fb14c3291b0cd47616ad968eb4ccac4fad11
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:25:16 +0000

    joke forgive already last summer

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def divide_numbers(x, y):
-        return x / y
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_list(array):
+        return list(array)
