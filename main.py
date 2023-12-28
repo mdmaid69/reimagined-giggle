fd7d873335d4b119d34945efd3d5323cccd91467
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:41:22 +0000

    trousers suit advantage storm woman

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def convert_to_hex(n):
+        return hex(n)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
