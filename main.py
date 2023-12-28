e24cd686ca95810a3ad33bb33dadfed9e16c2d3f
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:29:00 +0000

    talk interested copy fight dinner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
