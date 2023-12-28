65804dd36c9dbc0fc559385aeb52b5f9675dee6d
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:49:30 +0000

    fact conclusion clearly instrument design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
