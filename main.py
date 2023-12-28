ed6bbe0aac32ec94aae3d02c9dfa7fcf135a16fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:05:05 +0000

    right lay pain die shame

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import tensorflow as tf
+print(tf.__version__)
