483ba0962d954a6a407fa27e90d08d9d00ca1db4
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:40:26 +0000

    reject judge admit bet like

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def cube_number(x):
-        return x**3
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
