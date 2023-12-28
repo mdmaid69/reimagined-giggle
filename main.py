4c827a2efc8095e0809536ce5389a1212c2f65ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:21:58 +0000

    week pretty 573entertainment wish lead

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
