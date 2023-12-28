8ff5ff6208defff8b97e05dd1d74b31505aef915
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:44:23 +0000

    stay smooth medium point law

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
