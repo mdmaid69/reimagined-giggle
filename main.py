bff1df55af63498aad50559624ffa9a18681641f
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:27:01 +0000

    determined keep rent desperate also

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
 import array
 def get_list_from_array(array):
         return array.tolist()
-def calculate_area(radius):
-        return 3.14 * radius * radius
