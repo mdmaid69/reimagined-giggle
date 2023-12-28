a5725f2976ea7aa14b809061ab33bb33bb11bee2
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:28:55 +0000

    rain takeout young relax reduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import os
+def remove_directory(path):
+        os.rmdir(path)
