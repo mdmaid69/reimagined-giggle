0353f46a181ee1ee8e162d57bbc7e1cedf5d9f80
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:13:01 +0000

    cut get scrap immediately report

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
