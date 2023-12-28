b3c03244ef2d7d904a14084c834e086b90ff3b5a
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:46:14 +0000

    keepout entrance check odd that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_base_name(path):
+        return os.path.basename(path)
+def sort_list(lst):
+        return sorted(lst)
