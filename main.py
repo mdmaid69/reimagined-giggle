9e0fa43c37c9f643140820fefacf2c679076e1a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:09:47 +0000

    explanation match appeal sight swing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
