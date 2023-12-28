c3351f043c81ba788a144b913448f558b1e9cad0
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:18:31 +0000

    smile children myself pot generous

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sys
+print(sys.version)
+import array
+def get_array_item(array, i):
+        return array[i]
