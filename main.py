41fc8ea092bc6d49b77a3452fe00dba966876954
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:18:59 +0000

    belief thank wing concentrate undo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def exit_program():
+        sys.exit()
