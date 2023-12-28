8690a6ce45bbfdbcd81c63573ad56eff865620a1
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:04:15 +0000

    who fear herself reject change

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
