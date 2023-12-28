1c016321b49c6c59fd032f05905161c4b834cb39
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:15:45 +0000

    afraid operate well arrival limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
