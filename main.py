26ac913f98279a95625fb5516d99473e9ee26237
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:56:05 +0000

    luck wipe arrival reason consist

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
