273c9645ca46f330403d4e20df3125195522634c
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:36:31 +0000

    turninto mind light load care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
