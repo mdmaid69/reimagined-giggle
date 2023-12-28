018363ceeaa772d05d9a6ac15de4369915746dc1
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:20:56 +0000

    post many state case sleep

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def square_number(x):
+        return x**2
