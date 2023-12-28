781eac2cef204794a9d09520647bd041d3c56b80
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:02:50 +0000

    stay fold reply allow mistake

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
