81ddff271db6cd331e62d6b04d37ebd32062735a
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:26:56 +0000

    whether compare score lift happen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
