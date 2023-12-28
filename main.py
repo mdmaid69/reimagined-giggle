6134663d34087abc37f2781a6c22bcf72c4733ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:28:23 +0000

    concern important storm most emphasis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def greet(name):
-        print(f"Hello, {name}!")
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
