57346d411cb841c23c74440d2fcca45029e35861
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:00:42 +0000

    rid fast underneath alone good

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
