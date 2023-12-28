1cab670c3e88cb42345e043a088763c5d573cb0f
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:50:53 +0000

    quality gather pay drink tip

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
