cd289e571f8430f9d81518cecb62db047c98c45d
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:50:42 +0000

    better precise quick middle stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
