b2ff505094780883a9f0f787ce9bc93be8af978c
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:01:20 +0000

    tomorrow city entry happen typical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_current_working_directory():
+        return os.getcwd()
