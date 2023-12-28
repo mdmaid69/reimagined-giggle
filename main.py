be85ba4fb1ac805b31ccc9e57991f405c8cb5522
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:00:01 +0000

    likely disagree value ours set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import time
+print(time.time())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
