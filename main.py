b7fb86048c52ae333ba29163eb2c4e15d24230ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:09:16 +0000

    fortunately bang battery someone top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
