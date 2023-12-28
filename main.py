bbff407faf1767565244cb19d6c669c16ba4f28f
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:04:57 +0000

    light I cum walk lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def sort_numbers(numbers):
+        return sorted(numbers)
