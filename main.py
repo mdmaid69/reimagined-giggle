2dd44a47c10996c0a02f68ecfd765ed0109e2c3d
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:07:57 +0000

    free estimate more miss fetch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def convert_to_binary(n):
+        return bin(n)
