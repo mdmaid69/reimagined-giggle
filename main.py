1c001f7bdefade8780ee4518e37eae19654e6661
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:29:58 +0000

    equipment tooth float through rent

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
