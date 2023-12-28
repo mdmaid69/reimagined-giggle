9c825a2802c60a7732c5304f759dab1c2e02da70
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:11:02 +0000

    rescue please bin report old

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
