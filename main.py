60d5d2c909c1cfeed0ea4a1ed5884a798a6013a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:35:39 +0000

    follow accidentally satisfied capital noone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
