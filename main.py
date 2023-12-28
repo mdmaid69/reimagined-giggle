9f3ea04206eaf2c4326bdaae712e297ae2894b8a
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:34:14 +0000

    central turnup afterwards swim old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
