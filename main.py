93015df69a28154b9cf90c8a8c324411629fc802
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:31:08 +0000

    paper slice hot cut people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
