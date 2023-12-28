e7c73724e275b46a72642b1d780ab9131612f20e
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:41:11 +0000

    injury during many one machine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def reverse_array(array):
-        array.reverse()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
