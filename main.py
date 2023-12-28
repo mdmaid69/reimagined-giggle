d4ec76d9b2b93e8432d7ed0a859f35f188475e23
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:46:00 +0000

    all breath position cough understand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
