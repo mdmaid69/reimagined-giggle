877bd0a1f90b968528a51bbab370fe1468dc29fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:25:00 +0000

    Qq trouble fill upwards difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def reverse_array(array):
+        array.reverse()
