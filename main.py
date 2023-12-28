7b278bccaec77671b25688e15d6c8cdc9bd57a3c
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:14:04 +0000

    hand comment choice cinema instrument

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def divide_numbers(x, y):
-        return x / y
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_as_set(array):
+        return set(array)
