0bcde6e1d47ab6ffef1ae0ac3bd18a201865159a
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:41:53 +0000

    concentration steady authority otherwise country

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
