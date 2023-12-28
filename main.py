11413dda6f4c05c1110b90b2b0cd1a6fc411c882
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:10:18 +0000

    secret Oo Rr soft print

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_as_str(array):
+        return str(array)
