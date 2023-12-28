1869bf28664ef090c56b1c21798de07529e6a77f
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:16:21 +0000

    tear love minor so count

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def count_elements(lst):
+        return len(lst)
