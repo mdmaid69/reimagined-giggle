0f4f09721758144bf52797349cdd766e111162d6
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:30:11 +0000

    lunch movement ear willing major

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
