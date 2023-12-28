e45bf6b7d653382fa4a2ed321952ed58df22831b
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:18:31 +0000

    rush confused unit shame band

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
