6b23eb872b83bc93e62b1d95c960c560b35456b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:45:39 +0000

    floor plenty mass special wear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+print(math.pi)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
