2114695c34edf3ed12e28e910c6516eafdeff864
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:56:59 +0000

    female excuse attitude why different

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
