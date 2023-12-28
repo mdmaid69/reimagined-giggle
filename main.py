f49dcd4ac6ba260beb77028478e8410ba6deac3e
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:55:34 +0000

    successful ourselves on that bunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
