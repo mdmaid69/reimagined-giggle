bbbf7784d992b3d7e1965e2ddd205c2aee4889f6
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:38:05 +0000

    name high perfect contribute lend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
