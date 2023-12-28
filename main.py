eea79b0c4bf68d7933a29e759d06739dfb43fd0f
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:55:56 +0000

    provide sink suit main string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
