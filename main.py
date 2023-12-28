953db9eb0015409feed56c7bd875d0d564f918a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:42:52 +0000

    join reduction clothes height tall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
