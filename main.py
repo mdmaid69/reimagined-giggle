9a56d65b59007bc49583fac9e8e4fb8843c1a70b
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:58:52 +0000

    spring speak near 565end finger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def remove_duplicates(lst):
+        return list(set(lst))
