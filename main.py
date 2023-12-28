3cc534f21f0febbcfc71300bd04b390c9f65d574
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:40:44 +0000

    disapprove modern temper silent spite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
