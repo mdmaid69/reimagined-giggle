7806a8e4e1c1773bbdb949fcae94cefd3f9af8bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:18:32 +0000

    phone sing official tread pick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area(radius):
+        return 3.14 * radius * radius
