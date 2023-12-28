b347db1c56db053b227fbeeb4f0abcde84fa11a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:09:46 +0000

    occasional admit left case achievement

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
