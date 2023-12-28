7b39047e5b2e214ebfebe80d518dcfb560e41cb2
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:20:55 +0000

    mix trousers look top main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def count_elements(lst):
+        return len(lst)
