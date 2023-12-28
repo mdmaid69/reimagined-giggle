75129e008e60f074d31d14e279bf6bde58776f97
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:21:59 +0000

    type begin nearly crazy either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
