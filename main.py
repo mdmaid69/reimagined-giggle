3301c2e0190a701cc04fda245dee181325c75500
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:46:56 +0000

    gap damage apart content smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
