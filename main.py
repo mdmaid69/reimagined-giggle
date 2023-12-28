f09b4ab977b0b3e22368bbea8ceb6f5286674e9c
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:25:25 +0000

    after neat judge description demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
