a0dc1363d1bd31cf85036b0cf412b372c94dd015
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:25:31 +0000

    guide he pin hide concerning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def count_elements(lst):
+        return len(lst)
+import os
+def change_working_directory(path):
+        os.chdir(path)
