078d01b135d2e59604e5dd65b47ea08c9d614ec9
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:20:41 +0000

    share beyond like close Uu

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
