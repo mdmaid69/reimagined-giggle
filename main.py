27bef34dd0d335d12683033b2aba4fc8f5a636df
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:47:36 +0000

    how safe corner shut mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
