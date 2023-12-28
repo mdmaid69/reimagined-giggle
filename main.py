f34c0d691397301f387407f9b2139c1a73d6f12a
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:09:52 +0000

    gradually special mess defeat bright

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
