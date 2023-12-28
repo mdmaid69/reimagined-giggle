f2fbbec48122e74ec93aae5e1d80f6cca9af7c4b
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:43:37 +0000

    fierce shall gentle discuss try

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import getpass
+def get_username():
+        return getpass.getuser()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
