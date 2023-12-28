c26fe3954ad64cbf54a67f14832d0febd5a68f82
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:19:53 +0000

    state shop through trip Yy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
