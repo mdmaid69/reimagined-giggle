fec3cd58f672045610d39e6fed3b3f8e925d6e4c
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:35:57 +0000

    often from home address 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
