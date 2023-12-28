f56bd5f32dbf3c6353d6c78c47b6f3d420413880
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:02:45 +0000

    else completely lookback(on) bringback music

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
