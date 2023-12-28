5bd1453daae4a2f21941e80f4327cd10116df26d
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:33:43 +0000

    cook memory tooth properly excited

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import collections
+def create_user_string():
+        return collections.UserString()
