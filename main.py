db7969f74d14ca28e6506e6abd00cfbd06d0487b
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:25:40 +0000

    criticism trick cut rich push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
