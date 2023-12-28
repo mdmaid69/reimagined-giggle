a1cc6366a18d85db8bf987c602d03fef879cb063
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:29:09 +0000

    look trial shame shop uniform

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_speed(distance, time):
-        return distance / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
