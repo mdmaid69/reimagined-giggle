c0b7a814391ad5f5e6721a5ef3f68870ce177e98
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:34:34 +0000

    exaggerate away fire splash float

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
