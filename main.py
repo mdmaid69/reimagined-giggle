486aae51989360711712c8e95546e18b8a4ebca9
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:30:33 +0000

    holdout finally show sweep rather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def is_odd(n):
+        return n % 2 != 0
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
