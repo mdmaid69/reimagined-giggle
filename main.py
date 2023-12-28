707a31ebd4e58eb8c560191621e1add42e5e3a12
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:45:02 +0000

    careful afternoon company its call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import array
-def reverse_array(array):
-        array.reverse()
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def get_array_as_list(array):
+        return list(array)
