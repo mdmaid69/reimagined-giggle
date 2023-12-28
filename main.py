ff4ca79418659e31333f34a9646de59efd0a0fcb
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:10:43 +0000

    holdup long expect section curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
