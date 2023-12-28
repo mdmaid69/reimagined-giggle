34d0db911633a6ed166aa8312ad800d5c390f719
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:13:17 +0000

    something second opportunity government violent

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
