fa00bd48ed317f9241c305446c33e6809ac5e4a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:36:44 +0000

    impression ignore ring Bb choice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
