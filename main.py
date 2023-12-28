546f7bd3876cad4ea0e47a97a6c385cf2c18f21e
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:44:40 +0000

    play maximum education event distance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
