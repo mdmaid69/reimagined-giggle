ed4de73169cf29ea1404177dec2ae13546f4770c
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:12:35 +0000

    hope sort correction drink human

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
