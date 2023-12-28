c1e27b9bb9978fdf5649910973d8b5d75a4fe4b7
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:15:39 +0000

    runover watch disturb congratulations thinkover

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
