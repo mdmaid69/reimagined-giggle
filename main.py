df75b2bf86a969a27b14df49de11c928a768f2e7
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:04:26 +0000

    original shock hang morning me

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
