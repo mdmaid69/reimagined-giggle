66892b2cb59b5e128a44afc2cf58252e834d4991
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:08:31 +0000

    cruelty step track reach name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
