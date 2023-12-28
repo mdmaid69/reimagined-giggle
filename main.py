6c0a280353eaea4b3a0c38b6bab97d26c9a80fc4
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:38:16 +0000

    fight if agree desperate bottom

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
