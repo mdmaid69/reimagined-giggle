963f494a3cb8a4fce43e65b98e26bd73e9a7a8d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:04:48 +0000

    rub conversation accordingto drive nice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
