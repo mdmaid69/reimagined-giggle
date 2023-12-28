dc90e2ee93e3a5d44224db2a5a98a452d7fc0084
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:58:29 +0000

    today tonight length unfortunate herself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def cube_number(x):
+        return x**3
