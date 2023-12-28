5ad34d138c14433a18b5f9ad219fced9b5f1c438
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:50:42 +0000

    borrow no stupid room our

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-print(sum(range(10)))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
