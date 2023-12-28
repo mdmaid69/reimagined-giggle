87580fa4b4473158fac50313b97716b429f7bffa
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:23:54 +0000

    list process doubt intend angry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-print(math.pi)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
