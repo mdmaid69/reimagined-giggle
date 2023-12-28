9fd05d895a4276f2ad053a9be67d1f23e2b82a37
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:51:22 +0000

    pronunciation worth except climb wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
