c800d03e65353182128cf439d6c7b93beef70852
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:40:17 +0000

    emphasis explain convincing test port

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def count_elements(lst):
-        return len(lst)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
