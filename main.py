4c836c4a23f1619974840a333b405f2af24c1108
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:42:07 +0000

    wake convincing secret not begin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
