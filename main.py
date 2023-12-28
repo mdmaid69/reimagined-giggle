14e5b3a596dab88509fa900b81139860ed1f8374
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:20:59 +0000

    environment general stranger 566end number

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
