4304ca13e3293d3b7a4502e6952edca80cb46c54
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:13:06 +0000

    healthy strike different welcome star

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def cube_number(x):
+        return x**3
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
