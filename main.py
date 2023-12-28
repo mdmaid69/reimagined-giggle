4c5e6d3df999ef3fdeb93323de705de36eb19819
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:56:52 +0000

    lock advantage unusual indeed since

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import sys
+def exit_program():
+        sys.exit()
+def remove_duplicates(lst):
+        return list(set(lst))
