f80d5d3efe9331d44a8f8ab83c6df467d5517f7c
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:52:56 +0000

    hospital Ee band his spray

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
