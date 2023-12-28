6edd6b857b45cd1de48cf8f5d46b8f14f3a07df0
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:02:47 +0000

    loan properly kind favour kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
