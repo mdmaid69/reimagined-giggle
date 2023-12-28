4f23b50947134b1ef4ebf3a11ff18449a38d10bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:21:25 +0000

    go score bear occasional stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
