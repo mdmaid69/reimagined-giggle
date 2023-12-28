d634aaa8ec3fe1835b908e4c6daee7d9e1f11655
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:53:20 +0000

    vanish common pretty total responsible

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
 def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+        return math.factorial(n)
