0e12953c77e8fe5b199b891f0f0b6d03d1821315
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:43:18 +0000

    disturb shame progress deep decision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
