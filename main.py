f29f0dbfd8e2d3f1fd82badde63c97838500bff4
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:48:44 +0000

    same wire success load science

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import collections
+def create_priority_queue():
+        return collections.deque()
