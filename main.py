1ef4a79452db7c1c6eb5bbe641fc2f0982bf24c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:10:10 +0000

    get try wait cost crowd

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Hello, world!")
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_volume(length, width, height):
+        return length * width * height
