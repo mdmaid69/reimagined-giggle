f843f43e4b9ed9378dd420d9a8ab6d26262047ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:49:42 +0000

    switch busy age whistle cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import platform
+def get_python_version():
+        return platform.python_version()
