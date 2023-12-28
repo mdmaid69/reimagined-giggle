a1b9e46853f894a53968d5da30201e17242c046a
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:15:25 +0000

    anything knowledge meeting change threaten

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def is_odd(n):
+        return n % 2 != 0
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_slice(array, i, j):
+        return array[i:j]
