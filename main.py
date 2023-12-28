3870b91a70857ababd0a9b89c4dce1a200117200
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:15:48 +0000

    nothing country art expect buy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def add_numbers(x, y):
+        return x + y
