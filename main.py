5c11f62964da806516db02371268d0ecb252277e
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:00:19 +0000

    refuse attitude government sharp snow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_typecode(array):
-        return array.typecode
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def convert_array_to_list(array):
+        return array.tolist()
