935894117aecf051a5f5cf6af59985a9a540b607
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:07:15 +0000

    ear science reason load once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
