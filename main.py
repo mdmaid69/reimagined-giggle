972630e32e6e37bff6447944d58ce414787e506d
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:39:22 +0000

    behave ground ring pencil attention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_octal(n):
+        return oct(n)
