6cfa9fccacdd75636b8451ad13c3a5e2ecfe8f87
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:21:08 +0000

    emotion appeal realize murder pleased

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import collections
+def create_queue():
+        return collections.deque()
