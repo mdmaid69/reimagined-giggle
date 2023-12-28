0de0d97f419ce98b288b77217967f459c8ee4db9
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:32:36 +0000

    spend forgive weigh left ours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
