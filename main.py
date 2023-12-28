f97a4e6baf13601b20152b893c8b5fd7a5539535
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:45:44 +0000

    allow comment whatever adjective steal

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
