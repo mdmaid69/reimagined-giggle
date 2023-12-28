24a9dc662a6ecd99ba8b3699053a20176c59f130
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:43:41 +0000

    notice sleep something cold quarter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import tensorflow as tf
+print(tf.__version__)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
