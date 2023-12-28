5a17d172950d3c3ec6609dee748b8e604ac94c67
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:56:32 +0000

    health language form sweep takeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_hex(n):
+        return hex(n)
