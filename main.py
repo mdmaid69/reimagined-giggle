d2a2993ac226d40d0745b2128fe219e808756f3c
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:08:58 +0000

    appeal praise cry survive wear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
+def find_union(list1, list2):
+        return set(list1) | set(list2)
