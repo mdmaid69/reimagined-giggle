b17933645dae8ff0313b6a719ddc2c3376555f49
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:33:35 +0000

    feature spray exploration strike scrap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
