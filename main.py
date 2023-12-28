a0becf70e8de5db94d0f95dca2beb86454ba9b10
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:57:35 +0000

    slight evidence free juice centre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
