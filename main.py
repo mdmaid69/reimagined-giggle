546dc72d5d89e87a386124db698a9771221b6f7f
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:54:14 +0000

    until couple guess performance single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+text = "Hello, world!"
+print("Reversed:", text[::-1])
