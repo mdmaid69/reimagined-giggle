fce527822106c1e43aee6628f5f39c2bc1600e43
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:55:18 +0000

    learn introduction deliberately trust flight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
