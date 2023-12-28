12e26e894028ecffe526f8ae3d9e001e22c1c652
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:59:38 +0000

    flat object back blind cheap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
