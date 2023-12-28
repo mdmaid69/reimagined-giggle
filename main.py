41b973812ad690aea2604eb791f8bd8772b5cdf6
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:45:24 +0000

    before movement distant swim because

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-print([x**2 for x in range(10)])
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
