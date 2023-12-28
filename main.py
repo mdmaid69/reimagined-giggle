de2bee071ebfdb95c01793454926b9bdff9dbd5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:05:03 +0000

    afternoon valuable account now perform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
