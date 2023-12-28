85249e91845ae9e7f10bcef723b9e6caa44c9e68
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:17:03 +0000

    design cheat unlikely introduce born

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def find_max(numbers):
+        return max(numbers)
