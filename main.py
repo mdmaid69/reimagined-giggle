20d4e124aa9f1648c56945529b353b5cff2aab0b
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:41:37 +0000

    mine tongue improvement far unlucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def count_words(sentence):
-        return len(sentence.split())
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
