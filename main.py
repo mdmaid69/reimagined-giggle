84664eb002dab2f15d7a90715f46303835102f33
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:25:01 +0000

    take keepaway star slightly set

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
