521b974c911303221884c50b6758afbbea998195
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:28:41 +0000

    generous pretty from load window

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-for i in range(10): print(i)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
