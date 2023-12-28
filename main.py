9813257c7b43e1e0c81e35c9f110aa6cd2be12a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:26:06 +0000

    related petrol rather tooth strike

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
