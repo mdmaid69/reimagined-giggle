1d640092ff7ee752e70b9572a3d04186d439bb41
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:11:56 +0000

    dinner too maybe instruction extraordinary

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def square_number(x):
+        return x**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
