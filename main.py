6d6ad2cf44345c26a5aa803cf1cb7281552bcd9e
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:38:45 +0000

    friend one measure type relax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
