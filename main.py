74adc482ff0bce4edc24f483b36250d156606fa0
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:48:55 +0000

    conclusion tear lay divide tense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
