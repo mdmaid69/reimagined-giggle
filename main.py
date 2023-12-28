1a0f1f3ad857155cd6a57357b80656ab953c8bcd
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:53:25 +0000

    object no talkoutof inquire drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def split_string(pattern, string):
+        return re.split(pattern, string)
