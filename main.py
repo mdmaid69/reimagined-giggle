e6758a555d30db7f0b8f9789de34683d994314fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:48:20 +0000

    unit repeat breath century order

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import sys
+def exit_program():
+        sys.exit()
