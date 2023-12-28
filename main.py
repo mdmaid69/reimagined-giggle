555b778235f6b1769aaf3b955962b0a3544d1eb1
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:45:53 +0000

    rub soft send quick mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_current_directory():
+        return os.getcwd()
