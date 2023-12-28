b321c49e889804baeaa2a67140ca41ea31a4fec5
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:01:21 +0000

    practical responsible trade bang noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
