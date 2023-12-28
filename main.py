d94106fd04c6b13f5cf63b93b639be0b23b02d16
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:25:58 +0000

    tidy frightened married nonsense imagine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def find_max(numbers):
+        return max(numbers)
+def find_unique_words(sentence):
+        return set(sentence.split())
