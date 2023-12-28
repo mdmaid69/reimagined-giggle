2755d69e09334124cc49ca02e1657f6a7a9bcbe7
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:37:13 +0000

    old rise approval gas sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
