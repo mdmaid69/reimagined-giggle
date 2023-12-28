414fc6bdd9754de7f749988a979e4b5e90dd2abe
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:34:12 +0000

    innocent borrow uncomfortable familiar machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
