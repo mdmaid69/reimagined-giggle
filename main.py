61018a6aedcc40eb4c80a0efae60efcdde42d567
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:36:31 +0000

    joke build discovery century expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
