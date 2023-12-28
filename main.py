198f72691a64ec0d540cb22344b83eb5aa462341
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:27:40 +0000

    neck hate pause unite choose

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_exponential(x):
+        return math.exp(x)
