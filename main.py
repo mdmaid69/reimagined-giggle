4fd0bd8032e423601d62745a76ab251ea7a8c7db
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:27:22 +0000

    board dig characteristic quit challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
