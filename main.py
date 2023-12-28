2c1d1511c7f4970908a0959149956c0160efa5c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:40:16 +0000

    convenient none wish emergency Rr

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
