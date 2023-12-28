64cc5a97f78e49f37e40580a5ad07a5654c8a22f
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:54:44 +0000

    567enemy reach bin shake Zz

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
