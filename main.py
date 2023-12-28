10eeb9e7598162afdff0c4a6013aaa62eb99508a
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:27:32 +0000

    Ww terrible base mystery charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_base_name(path):
+        return os.path.basename(path)
