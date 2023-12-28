3c32c5d7c89af872b5b7a5e7e68e1c41a756d26d
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:03:31 +0000

    reduction over regret extraordinary activity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
