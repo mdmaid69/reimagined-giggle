2da0972ef1ba3d087222e3e487bf43cf37f15277
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:12:51 +0000

    disapprove disagree design bother discuss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
