87e588f1497ad730bf4e46dd83cf3f142eda18f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:53:32 +0000

    slow night clean destroy heavy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
