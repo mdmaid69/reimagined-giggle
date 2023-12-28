8526117d83de1b30637c138113b1f130ff1dd58b
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:54:58 +0000

    flat lot plain once direction

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
