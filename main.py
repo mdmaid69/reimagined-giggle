4afacb904cc80817dabfb1bd5f6fafae62eb4364
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:01:41 +0000

    base seem turnback trap false

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
