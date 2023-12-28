b8884c44274a3da9beb6fb9d16712ec63d8f29df
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:35:39 +0000

    television turn step arrange tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
