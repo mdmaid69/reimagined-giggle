3e1ee2101f31f8eab7bf7fa538fa54916ef2120d
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:13:02 +0000

    pull spot underneath second it

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
