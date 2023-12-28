819c4f421fe516ba6b30e47390a48d8d43c64c7d
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:33:18 +0000

    drive present allow border discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
