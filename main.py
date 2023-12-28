3af0d0d2b479866cece72edfcc0c7a7d1662f962
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:44:29 +0000

    whether everybody no another grand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
