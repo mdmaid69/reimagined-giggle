f5bfebe26c4f7653b4b6665d90ee68b50e5bf58d
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:24:30 +0000

    zero thin capital single father

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import sys
+def print_python_version():
+        return sys.version
+import array
+def get_array_as_set(array):
+        return set(array)
