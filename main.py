2442f0e811756c87b1f25f486a95f5a06227dec3
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:44:49 +0000

    walk sleep today keepfrom short

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_index(array, item):
+        return array.index(item)
