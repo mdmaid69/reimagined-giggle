55e29d5b6fb6355c6b83ac166f8346b2300b5e98
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:41:04 +0000

    shout able direct Ss land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
