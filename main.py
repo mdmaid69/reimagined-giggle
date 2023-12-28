d2ab19bd80e260fc26e2f1916b536a4d5fa6abb0
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:22:15 +0000

    club less permission bringforward another

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
