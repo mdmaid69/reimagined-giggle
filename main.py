2191d89676a905b1999ec2a7f664db9fa7fa78fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:51:39 +0000

    breakin too into basis 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def get_time_since_epoch():
+        return time.time()
