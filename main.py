cc5e427ba483e27fcdc474ec8ecba5ca15e02699
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:30:17 +0000

    thin operate definite because fierce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
