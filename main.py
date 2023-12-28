479ff63173ba1d630fb08ecf639a5bc8bd3a7c41
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:18:41 +0000

    loss criticism of advanced model

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  def square_number(x):
-        return x**2
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+print([x**2 for x in range(10)])
