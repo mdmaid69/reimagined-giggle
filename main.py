6f1237b73c2433d95a843566be04eb7687c544fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:12:07 +0000

    same join rob wish conclusion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+print(sum(range(10)))
