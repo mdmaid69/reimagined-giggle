c9d7c168041d8bf00c118a3475db24a207699e07
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:07:45 +0000

    wrong mystery turnout contents reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
