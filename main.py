92d7db4bf693ea5142f15eb6424b25090c03d900
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:01:29 +0000

    operate borrow everywhere language present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
