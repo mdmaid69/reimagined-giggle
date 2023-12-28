fcf949862ca8a1bf7cc981de84cd824244445a8b
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:31:52 +0000

    lie upwards boring hollow condition

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
