0735d0ffaa6e4bd747e5dea6269d6b550f4083fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:20:38 +0000

    office peace treatment in area

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def is_even(n):
+        return n % 2 == 0
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
