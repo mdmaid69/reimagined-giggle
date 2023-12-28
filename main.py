6f3a51f800eda61b573d8ce67970300dd1cead62
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:15:44 +0000

    share event unlikely cost steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_float(array):
+        return float(array[0])
