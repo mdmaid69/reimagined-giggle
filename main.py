d8faac6800e80f8c476ec58595102c700f2dc512
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:56:39 +0000

    order clever Tt goodbye progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
