217684ad249981e6d08cbf15d5caced5f0e3ce38
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:15:00 +0000

    music often laugh already build

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
