753ad551db831f1bf6210db04189649cce098cee
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:54:36 +0000

    arrival wonder wealth improvement buy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
