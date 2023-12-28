82cd290001b762117d3f070b712ca7bc86cceb85
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:07:02 +0000

    night geton inquire concern wipe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_pressure(force, area):
+        return force / area
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
