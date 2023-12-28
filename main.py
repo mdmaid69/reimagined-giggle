a0d43dedd37cb15e207ecdbc0dc496aae89a8750
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:04:41 +0000

    guess platform minimum soon strike

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import collections
-def create_counter():
-        return collections.Counter()
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
