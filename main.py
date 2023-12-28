67d4be6b6731cb075d03788373b79f165263c53f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:45:24 +0000

    friend spray confident dear Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
