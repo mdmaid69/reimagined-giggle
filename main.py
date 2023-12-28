ab46d6ba126fcdc5060d80527d71d8d9fd85f52d
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:04:10 +0000

    partner remember stamp afraid schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
