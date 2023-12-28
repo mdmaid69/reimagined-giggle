3ddc23f20e846216a6bc945338be1c78105651b8
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:56:59 +0000

    soft worry wave intelligent everything

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
