9e7710b3531f6cfa7ebd4185c4fa27eacc78dee1
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:43:53 +0000

    polite issue any since water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import os
+print(os.getcwd())
