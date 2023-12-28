d00b7aab6281ce18491a8cc199643c3f5856b28e
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:13:59 +0000

    marry if major personal few

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import numpy as np
-print(np.array([1, 2, 3]))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import collections
+def create_priority_queue():
+        return collections.deque()
