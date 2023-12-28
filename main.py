7abae724fa5455fa0ec76b8131fcaf7e85683575
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:44:47 +0000

    practice 565end scream claim imagination

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+print([x**2 for x in range(10)])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
