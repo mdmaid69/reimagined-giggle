d3d34d43fddf3a3654693a15ea3db5b0aeb3cf1a
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:54:09 +0000

    unlucky cross clean story keepout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import numpy as np
-print(np.array([1, 2, 3]))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
