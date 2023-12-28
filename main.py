d6530fcac25f043b6ce9af84eb88150122f69f22
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:02:18 +0000

    stone spring poor combination unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
 import collections
-def create_user_dict():
-        return collections.UserDict()
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
