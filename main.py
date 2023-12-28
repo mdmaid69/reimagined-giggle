28745969aaf65061ee1636089b36055cde9eba95
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:50:00 +0000

    sight detail nice direct destroy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_acceleration(speed, time):
+        return speed / time
