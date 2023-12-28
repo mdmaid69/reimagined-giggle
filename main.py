9f8627a5b80078a923790f3beafa029759b90945
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:19:28 +0000

    slice explain container variety project

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-print(math.pi)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
