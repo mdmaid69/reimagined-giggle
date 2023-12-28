c902aadde7d70d37bb0b2a96bd868898c4cd1a9f
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:09:36 +0000

    simple water about education prevent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
