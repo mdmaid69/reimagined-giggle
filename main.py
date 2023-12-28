48fccd163d7b01a5315f94f535e250c3cdc5ced3
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:46:43 +0000

    report dull shall wise theirs

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def convert_to_octal(n):
+        return oct(n)
