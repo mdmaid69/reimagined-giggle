11f5ac0705a9a15f619b2e132322abca7fd6c16b
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:08:55 +0000

    noise range cheap sister persuade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
