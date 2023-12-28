db543ad39e6b3467ff6c442ca7a53f19fd24bbf1
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:55:42 +0000

    bringup develop suppose often also

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_error_function(x):
+        return math.erf(x)
