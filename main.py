7b12028c5a685d9e50af590227713fe6f3362557
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:17:36 +0000

    rubbish operate improve his turnup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
