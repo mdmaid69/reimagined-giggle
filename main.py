883a0faacb48149c25911ad406e58b3139a00f02
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:05:12 +0000

    keepout finally plant geton about

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
