0f639871f6df263a3893b9bebf64b8a29947bc7f
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:52:03 +0000

    value stick bang unlucky disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
