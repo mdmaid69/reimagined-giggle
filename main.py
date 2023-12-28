d9a05bcca427ad39657a056b8226007b1feebb15
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:41:17 +0000

    quantity correct imagination most attract

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
