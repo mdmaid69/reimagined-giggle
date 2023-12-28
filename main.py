3619f960081338749cadb48a5406aac45a6c7f7e
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:49:59 +0000

    apart mean room common understand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
