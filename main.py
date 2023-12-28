f507ada06448b7fd6289cbba067fdc46f9eb0b99
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:03:46 +0000

    large job noise war apart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_power(work, time):
+        return work / time
