27a159f0eab8f5575fcabf838a29d2e64366fb3a
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:47:39 +0000

    round objection pronunciation natural track

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
