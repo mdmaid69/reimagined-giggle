23e169181b73cfb453a619ee89eb3642bf4e6202
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:25:40 +0000

    pair letter variety talk classroom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
