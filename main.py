70f5c3ded895d821a887b79c35d5d038adee945a
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:02:23 +0000

    noisy perhaps sell thick much

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
