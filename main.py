5afc4aacd54236222cf5e3cd51cd0d8e450c2959
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:13:03 +0000

    mad shock shelter pure sign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
   def set_environment_variable(var_name, value):
         os.environ[var_name] = value
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
