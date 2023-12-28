cbee50ed225d9be01385061dd3588fc991e4f8a1
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:37:52 +0000

    become letter complete ability lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
