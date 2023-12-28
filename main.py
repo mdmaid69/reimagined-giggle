27919871cea5136928ad9c5705344cca6aee16f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:29:13 +0000

    convince charge smooth last region

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
