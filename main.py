e67b55a06922bc3ea6a541d3e7ce3ee80459d433
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:47:39 +0000

    worse taste address apply fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
