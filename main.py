929bff86b33deb8958f2b7524faf4270fcb508c1
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:56:45 +0000

    sticky awake respect swallow nature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_power(work, time):
+        return work / time
