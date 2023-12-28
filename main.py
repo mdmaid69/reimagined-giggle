2b4611d6aed127ae2703f7fc4b5d5d4d86556fab
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:08:42 +0000

    muscle interruption bear make roof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
