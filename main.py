ecba1a05b935559030403a1649f8525882bd2c2c
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:48:43 +0000

    main praise scream unfortunate pretty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
