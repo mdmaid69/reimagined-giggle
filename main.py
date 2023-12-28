4f68f530fe2ee399d55fd1f4b36bd06f7cc81883
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:18:20 +0000

    day test tall together spare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
