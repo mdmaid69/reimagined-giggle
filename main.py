386a23a9c3db4d4ef42c7222a38adb0b26c743d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:45:41 +0000

    production nonsense hide arrangement past

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sys
+print(sys.version)
+def find_max(lst):
+        return max(lst)
