cd1190762404dda5432203fb74c13ec29ecbe25f
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:56:00 +0000

    down poison object by debt

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
