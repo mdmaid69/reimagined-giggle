73844b172400636547a436bc054c4f4d6f4ca465
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:21:04 +0000

    intend spite passage dick likely

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
