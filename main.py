ad2ee48daa4136fcae1d64b1159f439122d95a69
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:57:11 +0000

    study know terrible expression bottom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
