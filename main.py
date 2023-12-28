300f13162f1f02775043217f1fa15f3fe2678a0b
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:53:07 +0000

    benefit please list fall happy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
