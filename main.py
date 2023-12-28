298317afe173e060f20f0fa48dca570d11d5a25b
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:23:00 +0000

    affect not whoever shall lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
