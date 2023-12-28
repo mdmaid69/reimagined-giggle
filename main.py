a9ec5761b74ca59fcf31050745cd6a5081b76090
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:34:03 +0000

    fit interesting Jj central soon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_item(array, i):
-        return array[i]
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
