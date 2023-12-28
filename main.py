d19501a78760989fec327a380ba520b6743afb8d
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:03:33 +0000

    swallow powerful behind settle their

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def reverse_string(s):
+        return s[::-1]
