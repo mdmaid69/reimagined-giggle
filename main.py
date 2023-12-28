cc70af6c35ebea29c0cbc04c51186b0d276c852e
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:01:37 +0000

    hide season coffee want after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
