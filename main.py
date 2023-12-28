b97435678ef12b50e188996c21e41fe079a3770a
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:33:22 +0000

    for allow contact repeat warm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
