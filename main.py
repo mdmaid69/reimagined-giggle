2f0f03e22fdebd9cd639baf412652b315b3da5cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:42:32 +0000

    opportunity represent bunch thick makeinto

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
