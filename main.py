1c83c8a2812613bbad82650b66f6de87ad1b79ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:40:26 +0000

    its owe wire dish although

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_power(work, time):
-        return work / time
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
