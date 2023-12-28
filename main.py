6e03532579bf502ca591a903c35ec02bdf771212
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:36:56 +0000

    order turn play fast giveup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import sys
+def print_python_version():
+        return sys.version
