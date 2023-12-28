6d7da73846620046729633b522f2b21560aa2368
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:35:17 +0000

    difference hot result straight answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
