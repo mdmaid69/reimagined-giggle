4dc53c52f1b9b52b6c70ad9737bd7b9700217f7c
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:31:32 +0000

    lend wipe reject discover private

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
