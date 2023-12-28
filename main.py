fb3752b500d65b4ad5733740b34ed26b37d249b5
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:45:07 +0000

    any set culture chance fire

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def count_elements(lst):
+        return len(lst)
