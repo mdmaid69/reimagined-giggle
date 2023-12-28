6acd3842eaebc1ab3e7339c5a423f5e767b37074
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:59:07 +0000

    custom tell catch happy contribute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def is_even(n):
+        return n % 2 == 0
