652527a59ebba35294f0045e36a25c3ac061ee10
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:36:31 +0000

    seat regional break hurry load

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+i = 0
+while i < 5:
+        print(i)
+        i += 1
