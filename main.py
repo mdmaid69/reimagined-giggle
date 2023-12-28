1e9f6c0f16aad683d8a8240f7ad7771c28057b62
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:22:17 +0000

    exchange patience speech well goout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
