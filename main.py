0c089b7e8885fc54a8198bcf0d3c6333cc247304
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:41:52 +0000

    lower neither look pin church

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
