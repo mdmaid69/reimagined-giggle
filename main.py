edffb896523d1d6b29ed16a91172da3d11d036a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:26:57 +0000

    size recommend collect defend brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_density(mass, volume):
+        return mass / volume
