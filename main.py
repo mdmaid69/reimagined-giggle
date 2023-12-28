53b169e6ea65dd35be3c4bb171230fc4142aa528
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:36:35 +0000

    home level love birth splash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def find_max(numbers):
+        return max(numbers)
