ded547fc1080dd3ed1196ce5796173afad2e837f
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:37:45 +0000

    as leave bite awake gate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
