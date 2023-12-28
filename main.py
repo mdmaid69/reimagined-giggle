0f9db024e223672f9ed66a9abd14789f9f021f85
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:10:11 +0000

    572enter precise fond thing advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
