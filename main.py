001223cbc55fd5c54978d9dac755388667b840a2
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:02:36 +0000

    giveaway apparently together occasional tradition

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import numpy as np
+print(np.array([1, 2, 3]))
