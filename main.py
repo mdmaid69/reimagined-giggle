41be53fd442f682ebc8894bb2a47450a50e576e4
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:27:21 +0000

    trade Qq attempt that prove

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import array
+def clear_array(array):
+        array *= 0
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def remove_duplicates(lst):
-        return list(set(lst))
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
