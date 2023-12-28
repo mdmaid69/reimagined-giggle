64aac5a1252deed497f77307c86cc971f0c8ba41
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:19:24 +0000

    summer mind display open shoulder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_absolute_value(x):
+        return math.fabs(x)
