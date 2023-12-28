f70e93cb91b0db249b5307fb7a4a2510db76af5b
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:58:43 +0000

    not yes mention meanwhile shade

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def count_elements(lst):
-        return len(lst)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
