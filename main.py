02bf22f564007c250b294617e92265c65fde135f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:56:44 +0000

    absolutely float himself chase conduct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_gamma_function(x):
+        return math.gamma(x)
