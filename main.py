fc4a5b032a479d9faaf233b74f367d759af53144
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:03:42 +0000

    over wide room business usual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
