d1c19e723fcd3f0c1ea126c2759a309cc5d55dd8
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:34:22 +0000

    march correct break Tt pay

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def set_array_item(array, i, item):
+        array[i] = item
