07cb0923adfda63db7a97b3fd037ddb914a95dad
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:10:45 +0000

    beat clothes holdup concentrate variety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
