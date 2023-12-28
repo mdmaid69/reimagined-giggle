f5f68f5ece6fe66e948ecafac389e3ca754d9571
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:25:53 +0000

    talkoutof yourself hair usually debt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
