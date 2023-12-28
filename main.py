7f8c8b611f7d66e9d9c7495992327c6a3b633d85
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:37:38 +0000

    wealth exchange valuable separate it

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_repr(array):
+        return repr(array)
