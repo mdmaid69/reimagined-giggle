ad8cc6f0c17237c73155a16b1588c2d79e48a351
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:01:35 +0000

    force land bite complete geton

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
