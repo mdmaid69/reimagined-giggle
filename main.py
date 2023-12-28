873fc48896f9dba0b31f1f21b69c81da5911ea80
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:29:23 +0000

    design home getup interesting yes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
