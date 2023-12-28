11a6408f5e87216c5fb5cf9e7e58781789ba974d
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:55:19 +0000

    appeal in whenever introduce shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
