1f1f47a516596e75dc977b8c8ba36bd91592c589
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:13:28 +0000

    wave natural holdout avoid Cc

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
