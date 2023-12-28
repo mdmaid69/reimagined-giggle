b424566d226a1dee2c36e60e1cd72901467cc6b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:46:45 +0000

    pipe many pupil fold our

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
+def calculate_power(work, time):
+        return work / time
