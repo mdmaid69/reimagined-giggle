14f1d5bc76fff37a71c29416a60f72d2b2537bd8
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:47:12 +0000

    secret sit hurry plenty lend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def extend_array(array, iterable):
+        array.extend(iterable)
+def convert_to_hex(n):
+        return hex(n)
