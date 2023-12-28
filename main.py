95ecd08dd4a472b4761a2174432eca3f174fa909
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:33:10 +0000

    length heart bet favourite chance

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
