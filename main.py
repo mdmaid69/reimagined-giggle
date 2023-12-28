a5914b7eee1e154ff76170faa09a3f4fb43c1e8b
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:32:45 +0000

    cry 569enjoy fine passenger force

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_pressure(force, area):
+        return force / area
