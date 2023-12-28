cdd87cc65b82c1171dc00817a08d6e8adb9264d4
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:38:16 +0000

    course buy feature television later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
