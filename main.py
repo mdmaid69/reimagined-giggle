ea922fd77bc7b01658d435607ee56940648d780e
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:34:10 +0000

    club instrument rubbish waste too

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
