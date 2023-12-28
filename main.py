dcbf59e45b247930d13811e6f36a8bd798cccd36
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:07:45 +0000

    work concerning still who clearly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+print(sum(range(10)))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
