7fb2bf6de40be0b4644d87aecc083405dd9a97fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:22:05 +0000

    be scream fine quality straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
