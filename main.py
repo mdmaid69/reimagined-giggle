4d5f84f59ecd5ef618c3f486e4609025ab26d583
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:07:20 +0000

    bye willing however stop unkind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
