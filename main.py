db44b2ebcc7e679c6d49d2f1bca66a672838da84
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:37:24 +0000

    squash wait beyond wait stick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
