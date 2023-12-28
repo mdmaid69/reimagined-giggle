b725faa52b427e94f268c36f864de4239cb6489b
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:31:48 +0000

    niggaacademy consult round post lookaround

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
