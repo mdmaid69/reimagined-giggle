ab1dacdec77b668fb19ef5e21bd516aee410581e
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:31:13 +0000

    low practise style bad exit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_item(array, i):
+        return array[i]
