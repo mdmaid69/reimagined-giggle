d8a2e4de5b58c2c75c124774fe39cff5526fc7e8
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:19:10 +0000

    sense please myself actually instruction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
