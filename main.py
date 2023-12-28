82903b0866a8de81d929b7e55aa048bd9279b4ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:57:18 +0000

    influence deal empty bringforward human

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
   import numpy as np
   def create_numpy_array(lst):
         return np.array(lst)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
