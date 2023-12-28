78847eabd2d0f2e14b7a124f16b10e3d66a18cd0
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:08:48 +0000

    cold slice danger road exhibition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
