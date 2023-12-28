5d5f405e49b2505e8c03dac6a81103d3d582be97
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:17:13 +0000

    fit cost makeinto broad bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
