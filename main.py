cbe6500debcf9e542c8ac3de226420874d049b2d
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:13:26 +0000

    confusing fat lookback(on) foreign argument

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_ceiling(x):
+        return math.ceil(x)
