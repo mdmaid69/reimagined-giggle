316030b9b392101842b78f89157553ae5a4bbcaf
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:38:11 +0000

    shoot answer anywayanyhow memory gap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
