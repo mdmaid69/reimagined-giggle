d76c5ae764c1a87e562750a77bbc434694018656
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:34:10 +0000

    ocean matter compare crowded lunch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def is_odd(n):
+        return n % 2 != 0
+def find_union(list1, list2):
+        return set(list1) | set(list2)
