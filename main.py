6c0a10218a145f78a319e544ab8eb0a7ec01853f
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:48:19 +0000

    curious congratulations somebody who promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+from collections import Counter
+print(Counter("hello world"))
