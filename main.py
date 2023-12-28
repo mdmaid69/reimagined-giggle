cc461ef99870c5e87e946d7c53cf632a509d49c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:05:40 +0000

    badly flood search only not

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_perimeter_rectangle(l, w):
+  def calculate_perimeter_rectangle(l, w):
         return 2 * (l + w)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
