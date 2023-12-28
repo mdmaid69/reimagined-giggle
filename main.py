02e859f26108fdd228f5a6750ce66d619242b983
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:38:05 +0000

    means weekend start offer record

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def cube_number(x):
+        return x**3
