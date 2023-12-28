eb249679b37903cc2451a684ad81ba205567b1b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:13:55 +0000

    her left length thinkof luck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+print(math.pi)
