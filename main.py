3eb3d600b33f484c24ff37cafc2a5ce0d434d270
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:47:04 +0000

    sign refuse expensive hit person

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def add_numbers(x, y):
+        return x + y
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
