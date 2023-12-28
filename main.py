c7b3f8ac21fa41b7001e7634460c804990ad88f2
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:21:30 +0000

    no interesting record age pride

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
