c755c4cbb717dbf1a04dadfc8e6907a21280e61d
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:34:37 +0000

    classroom cope accidentally normal state

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def subtract_numbers(x, y):
+        return x - y
