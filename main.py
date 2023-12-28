0ee237f4da313ce6c821880dc48dd1a21d25dc3e
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:46:09 +0000

    suppose Vv refer flood match

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-print(sys.version)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
