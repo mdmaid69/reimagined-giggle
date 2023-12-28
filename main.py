6e12ba9f9d2f9db549ecbf908b6051682b265c3b
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:11:19 +0000

    moon your rather break hair

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
