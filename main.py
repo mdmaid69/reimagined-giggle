a82b4b2eab87de0f3ac3dc7a7d77018382fa453e
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:37:32 +0000

    year birthday block Ee what

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
