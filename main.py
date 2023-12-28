58d58b5c9e19d280dcc230eeaf861b4df8300d86
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:54:00 +0000

    bin butter male settle disappointed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
