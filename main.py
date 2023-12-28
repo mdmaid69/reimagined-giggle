40997d94d1c96a4adfe7009c37df9270bfe64a40
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:07:08 +0000

    play like opponent use desperate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+name = "Python"
+print("Hello,", name)
