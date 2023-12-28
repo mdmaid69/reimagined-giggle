cb88aeaf4ed6cb0de7b826285dcdf61e2fb96c6b
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:08:26 +0000

    slight rain use content risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
