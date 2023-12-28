47220b3375a32c6ed329d9a98fd2791f4a41fb81
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:59:50 +0000

    hospital imagination young point hospital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
