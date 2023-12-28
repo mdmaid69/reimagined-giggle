28f77ffee985c8ca8824d676fbfad8490b1a1de6
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:11:36 +0000

    spill bang mail gap several

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
