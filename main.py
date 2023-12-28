ea9d0c4a31ce327ef60bd76c063221d2baaea25b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:25:59 +0000

    street both emphasize score before

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def find_union(list1, list2):
+        return set(list1) | set(list2)
