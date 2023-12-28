4970bf223f88f054bfd39cce75b70f639d654095
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:01:10 +0000

    software juice summer none disapprove

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import sys
+def print_python_version():
+        print(sys.version)
