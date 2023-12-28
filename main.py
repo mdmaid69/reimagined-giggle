473fd51d45f2f8b5b54a1654bd6b89d041b6c08f
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:36:01 +0000

    whenever danger length exact 571enough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
