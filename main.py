45be7101b4c63f7e0055f488d84efabe8beb9863
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:37:54 +0000

    sound pretty prefer truth shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def cube_number(x):
-        return x**3
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
