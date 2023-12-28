9dd428340eda42d7dfb53f0f793d46c11fa35cd7
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:55:54 +0000

    snow terrible trousers tear please

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
