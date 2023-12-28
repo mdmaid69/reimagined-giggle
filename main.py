b055c3ffba1544ad1b36c7c7ccd1f1563c153155
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:54:19 +0000

    giveout suggest above who shallow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_set(array):
+        return set(array)
