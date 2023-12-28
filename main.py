48b5ba88997c6286195733fe1efa9d699d0bd12b
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:08:24 +0000

    sore ocean unless closed backwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
