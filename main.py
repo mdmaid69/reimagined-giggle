329a9fc48b4ada118b6241064fbada9c88e92e17
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:40:51 +0000

    commit distance protest to mistake

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
