37ff4bf9643d5a7284a2ccaef8c6c65320b258ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:19:57 +0000

    copy chance flavour flood band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
