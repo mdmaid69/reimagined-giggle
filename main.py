a67e7ea1c93f2573625a677d89b6a69d97aa428d
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:55:08 +0000

    discuss port pen straight aware

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def sort_numbers(numbers):
+        return sorted(numbers)
