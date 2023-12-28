309f3d7b512eb968e15d386114b6ab79cc2f9987
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:21:25 +0000

    special gate block stupid spend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import time
+print(time.time())
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
