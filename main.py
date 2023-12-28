25c983471fb28b6169e8ad6ad1d55e4d6c1d8433
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:16:01 +0000

    tax boil somebody quantity book

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_directory_name(path):
+        return os.path.dirname(path)
