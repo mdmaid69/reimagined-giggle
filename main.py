c2e864233a5066eac0e28adaf6917ef4e6ebd81c
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:52:52 +0000

    protect shame reply various month

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def add_numbers(a, b):
+        return a + b
