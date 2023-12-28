92b9aebcf8fab27df4d977c53a39a26aeaf5baa7
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:08:43 +0000

    work every sensitive yours experience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
