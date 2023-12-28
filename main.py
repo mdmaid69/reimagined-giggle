0f4cf0625315181d8d66af1023a55c1aeec05ab6
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:39:45 +0000

    squash seriously by splash hollow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import sys
-print(sys.version)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
