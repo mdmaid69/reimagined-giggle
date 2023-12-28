d4367d7e932faed65807c9be39c1631e09100dac
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:58:24 +0000

    group children maximum most vote

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
