316934cd389e7fc73bf197f85f5b4ea9ed374297
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:59:01 +0000

    lift strong survive over inside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
