539688ad93f8c3e2f431f19a1fbabb9be5cb673b
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:48:30 +0000

    distance wide write personal cough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
