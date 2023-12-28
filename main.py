ee3547698a80329b937038d25778311830a07313
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:02:25 +0000

    Zz turndown that tight minor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
