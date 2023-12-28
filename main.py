aafc580f13e2f50989321d842eaa1cc19a8c3c1b
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:37:42 +0000

    race pull home certain container

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
+for i in range(10): print(i)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
