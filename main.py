921f94d4e4126d47244632877b1c0d850d063757
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:14:49 +0000

    above moral though start either

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
