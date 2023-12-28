30bc976b54bd151359cf9935f17f618a52c71c18
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:00:29 +0000

    sweet television her walk size

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
