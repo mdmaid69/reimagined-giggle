e936a31a9c2f0812d651208241c3d4ebb16bd245
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:04:02 +0000

    up warn distant influence murder

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+def count_words(sentence):
+        return len(sentence.split())
