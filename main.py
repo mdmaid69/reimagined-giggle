85237c6f03ad4c802eda11cdb11918db8b26c2db
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:53:00 +0000

    young bag minimum in hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
