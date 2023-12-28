44de1fc9adcc74e9cb8859f05dbb1b38f856d198
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:23:08 +0000

    politics excellent getoff comeback circumstance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import sys
+def print_python_version():
+        return sys.version
