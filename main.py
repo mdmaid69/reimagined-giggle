6f52c03f67be5b98b476ea8ac56b7d5da3cf9b88
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:46:03 +0000

    alone feed water conversation visit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
