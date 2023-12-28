c3492097ebe42a4e5189dc417612298b1f87efd2
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:01:15 +0000

    sort wheel limit turnout wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def count_words(sentence):
+        return len(sentence.split())
