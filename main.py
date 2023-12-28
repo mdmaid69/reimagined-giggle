d9955b81fbcfb87fe582dc7edf5cfed541ac7faf
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:18:52 +0000

    exist Vv flat split store

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
