ffa777ff2821290226af29ccc5f958114c26c221
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:16:36 +0000

    within yesterday original unfair equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
