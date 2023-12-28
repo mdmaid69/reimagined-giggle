7a76dc66e76b109a61a9f99674944e5dd183eff3
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:16:10 +0000

    water team voice also tour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def find_max(numbers):
-        return max(numbers)
+def is_even(n):
+        return n % 2 == 0
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
