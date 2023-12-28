aee2fb5d4a924a965b9ca46e90a10840222dc512
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:41:07 +0000

    plenty sleep shop fond excuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import os
-print(os.getcwd())
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
