83d92e2493990ff15f0e2dfa3dc170da55e34074
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:46:49 +0000

    service fashion space beautiful shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(5):
+        print(i)
