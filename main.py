cae4076e608ea2e5afc62aed7049ae396aa432eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:54:46 +0000

    stop full approval dull trouble

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
