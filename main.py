e9b727d5d04253ab617fea309d91a00eb5d1bca5
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:37:27 +0000

    look room comeup speech divide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def calculate_mean(arr):
+        return np.mean(arr)
+import random
+print(random.randint(0, 100))
