c7a8db94d1aecfc7787ee1fbcdc3663c16db4f98
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:36:00 +0000

    mention branch it settle speed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
