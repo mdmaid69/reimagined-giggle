b17e3dbc498bcdbad975cb3b601f86e6f88a4050
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:00:46 +0000

    official class underneath travel hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_slice(array, i, j):
+        return array[i:j]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
