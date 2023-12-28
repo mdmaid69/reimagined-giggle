6760b1cef13309c4e5ddb2fdeabf2703a7efa896
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:35:11 +0000

    star quickly feature walk low

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_octal(n):
+        return oct(n)
