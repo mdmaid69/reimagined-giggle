a85f22e0421651d5977a0892431128cfb7608a7d
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:08:00 +0000

    cut be invitation stir tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
