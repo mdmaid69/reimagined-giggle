63bf882d93a1541742b446375fcc754349afa647
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:25:47 +0000

    bit stone bath minute beautiful

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
