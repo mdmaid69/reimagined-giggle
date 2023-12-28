0479895f8c6bc874674f219c90ceb8ae99af21f2
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:07:20 +0000

    tall pump why truth talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
