a20c7217385b65fbf1428396995ad4ca3fe24043
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:33:14 +0000

    suit somebody right slightly man

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
