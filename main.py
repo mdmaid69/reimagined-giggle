ee8f10c55efd45287504e4d3b459c17957724be1
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:42:56 +0000

    thick advanced damp opposite obtain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
