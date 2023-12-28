94403e00e3e9065f79935a7b56c288e0ff469a48
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:26:17 +0000

    spin today welcome quick necessary

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def convert_to_octal(n):
+        return oct(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
