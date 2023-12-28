10b6d3111af2e93d3ed7ee6d8d68dc79e0b34e53
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:35:15 +0000

    morning usually exact talkinto treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def append_to_array(array, item):
+        array.append(item)
+import random
+print(random.randint(0, 100))
