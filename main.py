e2fed9f4ca3efcede30f5904557411fae05d8c17
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:18:05 +0000

    pattern from ill characteristic uponon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
