9e5d9a36eb9634ad19707e8ab340b6d0658e96b7
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:28:09 +0000

    want tradition forward that straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
