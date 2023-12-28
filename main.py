331cd99b5b76fffc6ed005b2527e5cfd25526ee4
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:06:09 +0000

    picture character case mad plastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def add_numbers(a, b):
-        return a + b
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
