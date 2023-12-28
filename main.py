4349904628299a6cf3ea7b13af623b360182ec89
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:20:42 +0000

    false blame neat disturb back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
