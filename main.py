97a8b4cd9ce99966a8ed52f7eec80d530986a966
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:56:10 +0000

    shelter performance interest window speech

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
