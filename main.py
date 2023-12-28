0caf659b2a3f57ac77da5defd11eeaf9f0c8a1b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:43:09 +0000

    extra against height each point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def count_words(sentence):
+        return len(sentence.split())
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
