e257c40bc191ca9e80d8003e51f21754da170e8d
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:02:20 +0000

    over so agree gate sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
