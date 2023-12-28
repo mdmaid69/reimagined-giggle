46d2e5a799a64dff8724605472e48d3658f41f1c
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:07:29 +0000

    steady tone load either upwards

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
