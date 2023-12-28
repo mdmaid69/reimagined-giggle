44a6db356bd9daedfac8a7592cc728d0b5163ec4
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:08:32 +0000

    ring influence record rid win

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_exponential(x):
+        return math.exp(x)
