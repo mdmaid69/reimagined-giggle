92922a872754385e16600d597f7988ef858ce0d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:48:04 +0000

    form key display attitude breakfast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
