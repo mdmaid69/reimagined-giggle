127f1f1f1eb483edc9998f3c9934e2ef2d00bacb
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:22:53 +0000

    she impression process defeat off

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
