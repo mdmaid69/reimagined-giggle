c8d3dc39c09516babf5eec6655c5c83abe8cc210
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:45:29 +0000

    ignore bet cut like every

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
