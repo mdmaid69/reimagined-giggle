69392e8c04e7ca4c8d34ddb2b13b6b1e3389afe9
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:23:02 +0000

    area limit upstairs dry rush

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
