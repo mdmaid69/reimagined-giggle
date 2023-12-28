145446a865958ced82f61876e9d06b7f1e697051
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:32:25 +0000

    time joke personal determined stay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
