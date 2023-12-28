996dc1bfcd5284006486e9dc6f8f6a992e824bb6
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:25:27 +0000

    our getaway free slip confusing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
