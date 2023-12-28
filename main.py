eb138baa43e8635608906b1da801796c4156182f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:11:29 +0000

    pain actually live 564encourage reasonable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import time
+print(time.time())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
