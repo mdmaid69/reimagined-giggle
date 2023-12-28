525a80370cb0f96ad05ea7241eb6a0b6b6b1e7cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:04:01 +0000

    alone improvement defeat pump safe

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def count_elements(lst):
-        return len(lst)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
