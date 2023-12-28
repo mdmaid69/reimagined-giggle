8f65609dc6e71b6bca6b4fc59a4071a50a3c2a77
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:25:12 +0000

    wait remain coat contribute opposite

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def cube_number(x):
-        return x**3
-def is_palindrome(s):
-        return s == s[::-1]
+def find_min(lst):
+        return min(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
