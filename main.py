98412772cc70b26168589de532e0a5e916c72d03
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:36:10 +0000

    demonstrate suggestion vote shoulder children

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
