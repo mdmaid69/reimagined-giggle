572753f122d8ac9058d70342e618a45cca4e29b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:31:38 +0000

    woman Ff science ignore importance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  def reverse_list(lst):
-        return lst[::-1]
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
