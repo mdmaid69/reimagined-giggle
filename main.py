0f922c91ee36fcc85337f1653091a63a0737762e
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:27:44 +0000

    discovery offence advantage make concentrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def convert_to_hex(n):
-        return hex(n)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
