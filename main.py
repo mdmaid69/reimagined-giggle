cfdb2d32ddd6da1390a5b47d39802e290b3289c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:14:45 +0000

    peaceful care ring section otherwise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def cube_number(x):
-        return x**3
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
