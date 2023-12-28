6c711fd019ff18b4044870049487c420cd7b8bdf
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:00:32 +0000

    secret giveout everywhere of sister

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
