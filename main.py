e36fcabd6a4aab7696dbec267a18cd9866f6e233
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:39:26 +0000

    subject argue about worry room

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+print([x**2 for x in range(10)])
