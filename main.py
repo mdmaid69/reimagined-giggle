cd1748d7f9954b8d75e4f7156e4b50c0d39527e8
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:49:30 +0000

    gap hang flow giveout black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
