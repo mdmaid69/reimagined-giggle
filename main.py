e55efa561fab2862e527dbf2441f748da74dd0a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:56:55 +0000

    develop bang laugh 572enter so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import time
+print(time.time())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
