a1b53c500b0e0d184210746e4889ab0799bef92d
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:30:34 +0000

    theirs 571enough runover all glad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
