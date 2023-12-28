a2b25e5205011461047c926a739c4b5b006ba358
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:16:40 +0000

    runinto regret material gentle board

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
