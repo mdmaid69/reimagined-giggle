05b9685a6dd27c3006d29879bc13026ff3ddcb5d
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:35:52 +0000

    pollution strange wing congratulate suddenly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
