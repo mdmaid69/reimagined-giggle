e03052e97e7eaf33f3793687ac0f4d9e634f3520
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:46:07 +0000

    want careless aware through general

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+text = "Hello, world!"
+print("Words:", len(text.split()))
