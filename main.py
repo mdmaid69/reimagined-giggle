1fc4ed00f6ea2314fb1a8bdbef6a41c27afd94a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:07:51 +0000

    design glass spoil wheel cool

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
