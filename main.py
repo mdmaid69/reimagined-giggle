e714d96c83bc77efe337beb0e0ede56b54e13365
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:01:17 +0000

    summer voice control basis hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
 for i in range(5):
         print(i)
