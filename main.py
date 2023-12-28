51326cbfcf25d58dbb84a50eb970161b130413cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:30:40 +0000

    mother serve knowledge really total

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
