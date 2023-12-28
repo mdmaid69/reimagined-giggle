8a6e754455ef77829997724c4ee9318afdd8581d
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:27:23 +0000

    plate return listenfor settle report

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+print(math.pi)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
