82febed919aebdcc309ba1e51336b02ef3da64df
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:47:13 +0000

    about address punish everywhere holdout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
