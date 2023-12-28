c260f60020df3544910bc82d29481a3703a63496
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:24:31 +0000

    runinto meaning mind method whether

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
