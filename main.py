c78b969bd7c775d67b16390b5e9c3fb584f31546
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:53:31 +0000

    holiday match vision adjective stiff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-import time
-def get_current_time():
-        return time.ctime()
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def change_working_directory(path):
+        os.chdir(path)
