e2fbf3bdbc0a09d161d5f85aa32eedda7f59bb54
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:25:08 +0000

    tax correct explanation everything once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def square_number(x):
+        return x**2
+import array
+def get_array_as_float(array):
+        return float(array[0])
