9968e1bc6a9ce2298442e6197f9ac06f8da59265
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:05:45 +0000

    callat immediate worst air nigga

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def split_path(path):
-        return os.path.split(path)
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
