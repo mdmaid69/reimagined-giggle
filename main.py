44cdc3df40dd2b196a7dfe1a8f972b51064f1ab6
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:31:49 +0000

    hardly children information recently actual

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import collections
+def create_user_dict():
+        return collections.UserDict()
