274cbb27e106b45774117501d188891f9c0a4481
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:33:53 +0000

    behave hard dream giveout tax

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+print([x**2 for x in range(10)])
