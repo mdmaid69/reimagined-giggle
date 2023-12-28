68327969fc529be41cddbf8e975d88e4df8c80c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:33:09 +0000

    actually generally borrow modern usually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_speed(distance, time):
+        return distance / time
