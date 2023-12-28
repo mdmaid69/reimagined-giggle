b452cf6ff61d5c04cbca0947b316d0145524d05d
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:47:20 +0000

    insist forget second suckme opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
