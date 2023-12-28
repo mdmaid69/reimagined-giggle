c244cf133709c99b49ea3e5cb1c2da1e28722a3d
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:39:27 +0000

    expect cause wall branch confuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import collections
+def create_priority_queue():
+        return collections.deque()
