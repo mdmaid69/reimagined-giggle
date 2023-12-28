5d3bcf7be72411340415b8c5a357d4f8dbf3fbbd
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:27:44 +0000

    square danger flood who steal

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
