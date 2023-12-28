4faa2b28c323dc12b248fb9ef8dc3e7480c7c7ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:19:24 +0000

    fortunately build present ordinary front

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def calculate_density(mass, volume):
+        return mass / volume
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
