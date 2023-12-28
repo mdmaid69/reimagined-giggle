e101712dcd882686c015d147c4dfc68e8c9ec177
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:34:29 +0000

    suggestion indeed used limit comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import os
+def get_current_working_directory():
+        return os.getcwd()
