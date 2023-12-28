106280baf95fb86ef0e35c70624e16ec6b20f00a
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:55:13 +0000

    drawing battery survive watch break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
