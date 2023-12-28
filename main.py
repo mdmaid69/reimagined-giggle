2670a49ad6dcea69aad2be6b170475c33bbebb2e
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:38:56 +0000

    finger doubt honest claim thinkof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
