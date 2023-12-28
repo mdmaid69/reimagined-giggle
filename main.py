473f13f7cfc3f695c7c4b783be391448c32a90fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:38:20 +0000

    capital tap question slip path

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import logging
-def log_message(message):
-        logging.info(message)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
