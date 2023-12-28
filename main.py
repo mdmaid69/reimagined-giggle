95a89e02b421b60e4025d136c01bc28769594c89
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:39:14 +0000

    prefer honest runinto second comeround

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
