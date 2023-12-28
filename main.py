5663d95db938fa7e27df3f6c5f7e9ecb26630732
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:49:33 +0000

    write shop picture through single

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def add_numbers(x, y):
-        return x + y
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
