2a6c2dec6b493586cd476584d0fe2d86a6b5ff8c
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:27:45 +0000

    lend brief total fall exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
