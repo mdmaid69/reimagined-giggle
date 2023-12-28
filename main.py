7c2bfa2636141635c359972e12ca0322340315d2
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:39:52 +0000

    ready excellent whoever past important

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def append_to_array(array, item):
+        array.append(item)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
