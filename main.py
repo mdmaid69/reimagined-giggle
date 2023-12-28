927ba0a78246db7f53eb963a4eff285f9878c8b5
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:50:12 +0000

    creature touch Dd only beautiful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def split_path(path):
+        return os.path.split(path)
