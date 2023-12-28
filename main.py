14517f549865d74025e90592ed19bde84a7070e8
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:03:48 +0000

    next effort control load noisy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import platform
+def get_python_version():
+        return platform.python_version()
