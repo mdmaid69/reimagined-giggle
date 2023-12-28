a5ff85be13a10aafc9af51abf292425b6a7f8e3c
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:27:30 +0000

    chin to far permission card

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_directory_name(path):
+        return os.path.dirname(path)
