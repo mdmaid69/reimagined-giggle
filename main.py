650dc51a87dd80ece5d093ce993b92de5035487b
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:40:11 +0000

    sign thumb lookafter study properly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
