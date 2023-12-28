fb5311684395c5bb347cdf1d5c107463f2a372bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:55:16 +0000

    refuse sometimes active until performance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
