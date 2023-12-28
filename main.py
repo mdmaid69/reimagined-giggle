62b3304fde2cb46cec2e94430432095c6024c0a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:12:44 +0000

    turninto bridge pour silent guide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
