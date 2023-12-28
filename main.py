f523eae0a2824bd1ffc7b9320734ec83ae54e64d
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:18:01 +0000

    mistake knock sting behave week

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_power(work, time):
-        return work / time
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
