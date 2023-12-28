e5280e91c9ad222b8845e36d1a3d5ccd11bb3a7a
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:07:52 +0000

    always coffee impressive part behind

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
