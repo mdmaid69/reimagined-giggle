ce769b4718174d4d9d02a184132375b198b50942
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:14:02 +0000

    estimate admire single awake announce

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
