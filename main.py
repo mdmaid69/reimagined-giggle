708c7988bcdb7e08e1343f3cdc38580df1fb609f
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:33:28 +0000

    bill object keen advantage repeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
