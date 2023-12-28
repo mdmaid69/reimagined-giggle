15fda3319fd5972e15e1086eaab56c9da0836212
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:25:55 +0000

    copy do teach throughout hope

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
