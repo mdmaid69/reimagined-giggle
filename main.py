528c5316740ad328577575fd057c77d2c72bd4e0
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:25:16 +0000

    straight clearly side plenty operation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_array_index(array, item):
+        return array.index(item)
