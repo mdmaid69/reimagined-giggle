f17eda670a13bb6bae6264e195584f67854891c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:58:27 +0000

    ring absolutely advanced successful chief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import datetime
+def get_today_date():
+        return datetime.date.today()
