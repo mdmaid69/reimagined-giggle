b937ecdcb9b5a565787e76016ac832309b0289f2
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:32:08 +0000

    fasten butter job too already

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
