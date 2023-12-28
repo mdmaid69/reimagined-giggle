1a749ecea2dbe0f3bb7bcdcd486550b1b85cfb73
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:58:51 +0000

    straight experience team smile type

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
