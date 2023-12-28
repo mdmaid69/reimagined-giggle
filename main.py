66882b02738674a50850baab3a2b08a715bc03d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:11:50 +0000

    stir freeze waste impression shopping

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
