2412312f222881779d81eb4b2431c15e556dc670
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:42:29 +0000

    callup law meal order pencil

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-name = "Python"
-print("Hello,", name)
+def find_max(lst):
+        return max(lst)
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
