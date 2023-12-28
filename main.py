55455b3997384ab1729fde90149dbf51f5ddc662
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:34:10 +0000

    goback(to) return big call safe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import sys
+  def get_python_version():
+        return sys.version
