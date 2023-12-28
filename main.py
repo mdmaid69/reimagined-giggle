837d2c0626cbada2855a2c6beada659bdfc26c48
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:23:16 +0000

    such share him unfortunate trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
