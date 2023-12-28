48b4367475c7e23da3896df48879e5739d61f850
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:29:36 +0000

    getback generally listen wealth contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+print(random.randint(0, 100))
