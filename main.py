ee9b56495e54d70a7920f67ddeb0c0f26d2218ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:31:05 +0000

    afternoon everybody way now forget

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
