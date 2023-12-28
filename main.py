27e5d9902de61eff34efda4d74138069376452ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:49:40 +0000

    off need mistake talkinto recover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import numpy as np
-print(np.array([1, 2, 3]))
+def is_odd(n):
+        return n % 2 != 0
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
