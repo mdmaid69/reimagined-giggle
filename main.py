19529f133d15f818878e9b3233b11b8dcb5eaeec
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:53:57 +0000

    record attract sweep office warm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
