54afdd6f54324e5a1a0b37f6185771827b56fec5
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:31:01 +0000

    cry busy flavour fall phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
