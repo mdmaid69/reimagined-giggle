81474b46a347514447e461f4b526baade80ccc46
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:15:52 +0000

    nose sensible it limit town

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
