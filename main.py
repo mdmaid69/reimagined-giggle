5e1405a87d3f7c2491c33dc92f57bc77fb9bb50c
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:22:18 +0000

    in wrong progress pain water

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
