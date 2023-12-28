136aa7422f076c2a9a25b0db1fb8334146941ae3
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:17:42 +0000

    have improve loss stiff pocket

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
