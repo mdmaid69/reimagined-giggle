b45378b962fd9c2330335a421f8f36f1f57f4dd4
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:17:42 +0000

    represent exercise safe store teach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
