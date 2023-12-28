7fdbc36f42ba7065f346a3b5a27f6eddc82c9135
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:42:31 +0000

    Zz shape race bite nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import getpass
+def get_username():
+        return getpass.getuser()
