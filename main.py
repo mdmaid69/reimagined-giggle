fc3b65d8de63fdb0b863a64a6ab78f4033aa0cc0
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:58:10 +0000

    confusing involve conduct name instruction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
