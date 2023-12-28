33fb587efb618ad89dd66d2675a427363d693483
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:20:32 +0000

    temperature steady spite visit entry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
