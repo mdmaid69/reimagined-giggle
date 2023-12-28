f8aedb1e2eb74780446adb9c00eafa4f64656365
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:43:49 +0000

    substance sport scrap maintain stir

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
