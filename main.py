39e99d566992cbb9827bf00397d3da3c0be348e5
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:51:27 +0000

    whisper high whenever square far

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def find_max(numbers):
+        return max(numbers)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
