439b74c25abfa1c094b355eb9b73ebe042d4274d
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:44:25 +0000

    neat block although completely disappointed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
