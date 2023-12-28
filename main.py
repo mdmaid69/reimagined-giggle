2b0b3c4eefc9aca491e3d36192c300aeb08f71f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:57:43 +0000

    meanwhile rarely all increase create

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_base_name(path):
+        return os.path.basename(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
