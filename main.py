7e3d1d3376e1a494182a9c8d2806445da8d8fc97
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:10:59 +0000

    successful Oo mark tidy scare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def get_array_as_str(array):
+        return str(array)
+def find_min(numbers):
+        return min(numbers)
