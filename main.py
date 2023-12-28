989c5def50c5e539ef0ba82ba0af141b5d5737b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:53:04 +0000

    attempt custom especially failure arrive

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
