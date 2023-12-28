fa997b1778f87f45641b20d807ac291c9b82d47b
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:08:01 +0000

    intend connection substance minimum keen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
