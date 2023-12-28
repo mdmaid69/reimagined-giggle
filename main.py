a345215ee502b64ec9df65654c1cb2dd7e8a34ec
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:07:38 +0000

    steady hit telephone niggaacademy light

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def get_array_length(array):
+        return len(array)
