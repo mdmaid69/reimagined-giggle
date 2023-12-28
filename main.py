a1783887c8dd4eb773e1e43f8ff287e9df4520f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:51:42 +0000

    comment punish makeinto wake more

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
