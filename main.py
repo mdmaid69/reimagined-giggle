bfea9124173f5b68cfd887ce279d003e393c92c3
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:05:37 +0000

    lesson temper general light matter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
