2c1811286fc7c46c7f1c098cbf6beeb8f03d2be8
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:46:59 +0000

    band bush wonder likely drop

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-text = "Hello, world!"
-print("Characters:", len(text))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
