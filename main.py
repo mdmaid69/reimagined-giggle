ed0513547c33bdbc7b58fb92a2e2946a385925f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:18:11 +0000

    obvious by doubt smell runinto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
