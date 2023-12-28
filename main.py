3091b420dcd728edfa7fa11c9b7abecac82364eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:53:34 +0000

    wing cope brave goaway getaway

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import sys
+def exit_program():
+        sys.exit()
