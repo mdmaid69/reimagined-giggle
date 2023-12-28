3e95661650921ebf174677e91b12ceb7877d22f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:49:55 +0000

    creature lunch repeat round mysterious

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
