c33b01ec68cbbf537e0c7f9f1e978a941f7efbef
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:18:20 +0000

    process lower correct then backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-print(math.pi)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
