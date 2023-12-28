32e94ea431a01a3dd6758e30fd78db25b5b940d5
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:40:17 +0000

    callback person original beneath demand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
