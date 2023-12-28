7101f48f2e57739129a3cb77e11d46043582525e
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:09:24 +0000

    stretch comfortable excuse mix building

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
