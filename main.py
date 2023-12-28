35980a30cdae25a428bdfe3aca2a9b721da0f359
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:31:07 +0000

    genuine goodbye popular choice survive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
