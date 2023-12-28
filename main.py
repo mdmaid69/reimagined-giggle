f0fb1d7ba67e0b286f8355187a4a7b317a648b24
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:42:47 +0000

    board tool firm some seem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+import array
+def reverse_array(array):
+        array.reverse()
