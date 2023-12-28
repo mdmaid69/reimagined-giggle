2c96dde0226e237252c0b35f6667e28f4177bfa0
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:11:00 +0000

    bright rain direct grow effort

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
