3faa81709dd0c49309fe384952ab82b58088654e
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:14:17 +0000

    love exactly reduce naturally chin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def find_min(lst):
+        return min(lst)
