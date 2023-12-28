c286dd97c87c06cab68ef20e920b1173b46908f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:07:30 +0000

    size unable plain breakfast worry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
