a0da0ed57f06dd50eb24ae5dce8c3f6ae04fd9c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:29:19 +0000

    explain confusing of both fetch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def is_even(n):
+        return n % 2 == 0
