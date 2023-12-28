d91c36c652a0d9fb1ca26d6cac5a88eb987df53a
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:14:41 +0000

    surprise top smell jump Oo

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
