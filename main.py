1c5ebb37d7c442f96b1eaf0acd8c22deda1c2182
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:35:46 +0000

    come sudden break loud behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_even(n):
+        return n % 2 == 0
