122ca0ecc9d41cd3a5229e05328a6f839aba354b
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:24:35 +0000

    say crazy lock lose interesting

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+print(sum(range(10)))
