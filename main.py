0e151a3444617ea8a01aa2c143f2e71090ac27eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:18:30 +0000

    back run dinner future compete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
