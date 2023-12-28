e769904b437c4db5e9fd5631b8edf5b8ec60f76d
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:44:38 +0000

    free reaction over pin accept

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_priority_queue():
+        return collections.deque()
