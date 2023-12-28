3a2a735ab8dd15b64eeebdfeee455bb96bd8ff97
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:24:27 +0000

    culture point switch church pain

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
