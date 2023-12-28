6f11cb0cba8d9e8726bb8d3e6ad1e2cda5eab233
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:18:38 +0000

    pump platform come best afford

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import os
-def get_current_working_directory():
-        return os.getcwd()
+def list_files_in_directory(path):
+        return os.listdir(path)
