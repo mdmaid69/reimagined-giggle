e40727768fb9ace0fed5fd600f4567de946512d6
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:37:08 +0000

    left relative wash alone work

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
