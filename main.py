7abe64021d9355b6c23e0f444dc8551c7fbf0e9a
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:42:10 +0000

    remark familiar inquire even soon

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import platform
-def get_os_info():
-        return platform.uname()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
