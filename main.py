0843626af040bbe5f6a09f4a6723013f7b3ea652
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:02:06 +0000

    border almost thick yes imagination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import time
+def get_current_time():
+        return time.ctime()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
