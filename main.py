458214769014b7b25e18dc968471ce0f40dad95b
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:17:02 +0000

    switch usual side keepup neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def divide_numbers(x, y):
+        return x / y
