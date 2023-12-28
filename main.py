f847371ce6f5f3e042d05246a2c98c5186c07997
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:39:08 +0000

    unless godown pen tomorrow transport

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 def factorial(n):
         if n == 0:
         return 1
         else:
         return n * factorial(n-1)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
