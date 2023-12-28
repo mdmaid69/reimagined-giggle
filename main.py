2222917a960ac30b34e859968d96f200a4ba0ec6
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:52:45 +0000

    tell true successful pronounce measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_power(work, time):
+        return work / time
