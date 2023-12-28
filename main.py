9d54078df18981a42b674952494b04e135a2eb85
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:42:55 +0000

    stage importance manner give swallow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def find_max(lst):
+        return max(lst)
