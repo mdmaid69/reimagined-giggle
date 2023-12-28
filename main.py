c765924d8757db9d5acd5a2120bebe1c3f63e395
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:02:24 +0000

    book doubt shame include precise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_power(work, time):
+        return work / time
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
