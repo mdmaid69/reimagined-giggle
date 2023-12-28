3230d71e2d88b5b69da33d6c8e49e7e6eff44cf2
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:05:42 +0000

    share gift off 570enjoyable across

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_octal(n):
+        return oct(n)
