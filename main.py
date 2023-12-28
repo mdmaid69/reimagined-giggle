be7c286dae6d660d267ea9747ba1a96251ec79bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:04:40 +0000

    welcome unkind bush unpleasant dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
