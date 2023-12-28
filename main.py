dadc816334601f3553fcff715571214801b4fbd2
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:41:36 +0000

    extraordinary glass deliberately easy butter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
