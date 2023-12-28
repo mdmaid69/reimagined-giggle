93406ca3bcb19f8b71a95204f79fec081b15baed
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:21:13 +0000

    community shake as reasonable score

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def append_to_array(array, item):
+        array.append(item)
