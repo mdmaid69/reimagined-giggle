22920c38e62b7a6f77c16a8f74b87443143f9068
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:59:01 +0000

    improve progress disgusting honest proud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
