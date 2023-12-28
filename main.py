7acc148e4c9b2c88be7b9736cb515aa49058ac6d
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:18:03 +0000

    train ashamed fix fill whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
