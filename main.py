906c28d5d9d28437498d564b49ce9f17dde0d87a
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:06:05 +0000

    style exaggerate basic OKorokay task

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
