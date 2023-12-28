f39ec924ee3f6df2086d656ea9c9654bbc9f5f53
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:29:57 +0000

    interruption weak advise long concentration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
