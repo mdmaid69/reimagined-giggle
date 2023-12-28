c27d119813ca073c4442d7dd923720aab8fbabd6
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:36:57 +0000

    balance defeat except workout tragedy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_repr(array):
+        return repr(array)
