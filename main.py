d951789bf12bbaa7ad78c0d4873ba8977006bcb3
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:56:48 +0000

    between 573entertainment thank branch excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
