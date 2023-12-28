55b33a1fa6c9fc4ad69dccd44807cd6e410d0dd0
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:32:57 +0000

    dress practise Mm strike anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
