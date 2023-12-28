a4a0b26481440492c685204c34db6520dc45d15f
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:30:48 +0000

    mark business confusing press confidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import getpass
+def get_username():
+        return getpass.getuser()
