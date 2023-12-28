620e33cad750b1aa21eda4855e06c8732c887ccf
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:29:24 +0000

    corner goup that available object

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
