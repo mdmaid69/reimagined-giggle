5e7cba2d688c97fb47429fb4484cba1eef47427d
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:55:55 +0000

    steer suggestion time battery train

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
