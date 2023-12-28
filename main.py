e9923d264aa42cb352c2c6ae3094c7fa93a5ad3c
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:41:13 +0000

    female himself abroad young usual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
