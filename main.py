b5d83cb9380dc4c1e428ea2afbfee3658ce4ba92
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:16:39 +0000

    attract communicate road guess on

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def square_number(x):
-        return x**2
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def remove_duplicates(lst):
+        return list(set(lst))
