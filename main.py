dfad26abe97909d23b5165a92ab578534ce3e396
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:37:42 +0000

    care bath fly weekend important

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
