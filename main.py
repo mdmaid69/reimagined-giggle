5833d16e89fffc6519e02e0ef2d8e97963e5bdd7
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:35:07 +0000

    surprised telephone like opportunity march

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
