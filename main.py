7cfd58e341d01a9e1a90df41511df138fdae98d0
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:39:47 +0000

    finish only underneath score after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import time
+print(time.time())
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
