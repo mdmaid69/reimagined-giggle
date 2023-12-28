9f65411b60ea709270c2f841e7b35a8a7973f9a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:46:30 +0000

    mistake save valuable test mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
