4f3a24af741e2b7aa2b5e314cb4d019ded18efd4
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:24:39 +0000

    bound match charge forward grand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
