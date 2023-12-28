91058810170d88b9bc1b79448085e4a70ded06f2
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:36:55 +0000

    attempt whom also runover lend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
