eb0fcf2429ca14538df453a1d3f52c7edc3b9eb4
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:33:24 +0000

    getback always circumstance get honest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def add_numbers(a, b):
+        return a + b
