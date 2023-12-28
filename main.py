59f0005f251355ae1645681e3234c4ef42c68c7b
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:04:03 +0000

    centimetre pretty push firm account

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
