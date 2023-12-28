678e352597c8f5562e52431f054a3b08e9ce5900
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:00:09 +0000

    place suffer raise in nurse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
