3bef2dc7c45be92cc265b78b17654894fdb39295
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:25:18 +0000

    various refer mother chat turn

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
