b815610468f1fb91886a9119fc6fd1a88c6442f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:12:19 +0000

    transparent television say could hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def square_number(x):
+        return x**2
