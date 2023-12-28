f4ccf0b366a4e95b2bc396477ef10f391932ef9a
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:12:44 +0000

    wrong day false hard include

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
