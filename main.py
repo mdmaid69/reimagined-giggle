f45c2dfeadf8b381fab3dbcfab5d9c8a63042274
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:45:36 +0000

    grow fair principle coat dance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+print([x**2 for x in range(10)])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
