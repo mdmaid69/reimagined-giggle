20253f1cb067aedc513d02424456560f65700734
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:17:10 +0000

    pronounce immediate pick everybody history

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
