94dff62afdae560734386a5ca10bea4dac4d674c
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:46:24 +0000

    any arm near fun once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
