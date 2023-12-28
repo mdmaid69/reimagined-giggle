22f0a2c96ba20a04f8ae3f1d00d489a914599a28
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:10:41 +0000

    back only callup shoot comment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
