4c41a2936d68955ee91d93385e9f772b89ac2480
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:16:28 +0000

    and tap calm after hope

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
