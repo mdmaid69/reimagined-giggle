77dcb78cc09b34edefe5cae00dbf10f8c2d9deab
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:16:18 +0000

    first special slice callat defend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
