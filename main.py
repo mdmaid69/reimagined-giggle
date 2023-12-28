0b5a7cb3115d190a9e9aa863e2db2d2838f9b3fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:02:20 +0000

    rest doctor after responsibility range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
