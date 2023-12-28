d3d7cdfb459e2de0e05d7f0de6a5ce10ffe2673b
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:52:31 +0000

    later try translate stamp judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
