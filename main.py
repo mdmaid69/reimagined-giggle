41fa454edcbfcea14839f632d3c204b108694c38
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:23:47 +0000

    throw immediate final grow fairly

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
