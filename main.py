5cee9f12e7c56e1b67cdcc46fe203c6dfdb25b71
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:35:27 +0000

    Gg print seriously murder pay

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print([x**2 for x in range(10)])
