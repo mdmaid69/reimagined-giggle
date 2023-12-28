62e1262f2008f13984e440a0523c909a00b69d87
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:00:26 +0000

    underneath safe lookup show wrong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
