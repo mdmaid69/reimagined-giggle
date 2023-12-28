bcb25467283d20d27d35f0d28f65880def1e85b7
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:02:51 +0000

    trick memory powerful ear sick

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
