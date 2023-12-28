79bac0dde1c67482e93622d0d10b25ced8010e63
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:53:29 +0000

    Ii private project odd shock

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_area_rectangle(l, w):
-        return l * w
+def multiply_numbers(x, y):
+        return x * y
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
