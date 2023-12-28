642d08f990741655c3fa13f63a0851be07c0fd88
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:25:54 +0000

    peace his danger count return

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-print("Hello, world!")
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
