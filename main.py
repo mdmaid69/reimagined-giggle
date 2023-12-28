9d0178eaea89664e57871fa48f0c0753b49feeb1
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:48:04 +0000

    makeup over cool cause outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
