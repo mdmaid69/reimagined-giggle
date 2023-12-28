06bab8f9d559f5e8c42338614a70b422e088a954
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:58:58 +0000

    lift keen floor close age

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-print("Hello, world!")
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
