59a4f9f5c25a919366c45f6ff7c4345c07d2ec8f
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:12:59 +0000

    complete abroad chance stage bear

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def convert_to_octal(n):
-        return oct(n)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def count_elements(lst):
+        return len(lst)
