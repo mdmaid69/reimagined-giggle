2d5702f1b15a8cdacc1b2d405eecd6a5aaf956cb
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:37:22 +0000

    odd main unkind luck himself

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
