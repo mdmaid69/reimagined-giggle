5a4bb0d22462602b24aaa6886b5d90f6769077fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:23:24 +0000

    wonder answer pretend telephone make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
