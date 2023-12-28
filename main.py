89ef156516564eb19d5cc0341171f377fb01da7a
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:15:23 +0000

    probably side turninto talk similar

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+text = "Hello, world!"
+print("Words:", len(text.split()))
