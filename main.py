9aadfff41b5973cd94c58f0cd606db044fa6ab08
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:47:52 +0000

    pay accidentally manner tape lookafter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def get_array_typecode(array):
+        return array.typecode
