972e357377f3f59356c71c55b5cd4e329d19f662
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:45:35 +0000

    deliberate middle trick traffic modern

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
