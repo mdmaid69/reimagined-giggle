80029dba5ae31110ef355e8f98127dc326e1835d
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:00:44 +0000

    pause actually complete outside club

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import tensorflow as tf
+print(tf.__version__)
