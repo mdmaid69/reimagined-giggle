eda3c2ae2f8b08a42f298658ea1cf90b22cbca92
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:13:31 +0000

    service spin stamp discuss correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
