3abac9ba04ef964b8a7383b9cda0839d314e0f78
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:54:41 +0000

    black money my concern scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import datetime
-print(datetime.datetime.now())
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
