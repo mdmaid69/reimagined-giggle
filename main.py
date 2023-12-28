679c33a7f479762972672b38c945833fbf800d20
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:42:36 +0000

    switch there voice strike advice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
