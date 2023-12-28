3a30f42878a61f8109dffb6b4d785c037d82c36d
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:57:34 +0000

    also father walk cost artificial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
