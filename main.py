cce4f1ba3188053a6df0839e240ff2929b125317
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:38:38 +0000

    instrument present among morning meaning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
