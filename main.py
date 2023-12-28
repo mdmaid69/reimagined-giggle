9cd0a37b0ff06fdd4df9349114e06d72166744de
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:06:43 +0000

    book sharp bound forever eager

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_queue():
-        return collections.deque()
+from collections import Counter
+print(Counter("hello world"))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
