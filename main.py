757d64533edde155ddf15bbaf316e745bf455994
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:10:56 +0000

    dance petrol once foreign desperate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
