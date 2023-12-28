afc07c16447d4807c471496581d58afcd5384538
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:31:40 +0000

    favourite Mm pupil Ff thank

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
