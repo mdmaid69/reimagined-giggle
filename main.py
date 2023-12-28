782fec93f4dbdf92f0a491975f0d6f1562e7f01e
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:17:26 +0000

    store finally machine club strike

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def add_numbers(a, b):
+        return a + b
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
