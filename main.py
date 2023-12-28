5f8cebdc9fba0da0dde6c92af09a03dda1ba4adb
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:13:47 +0000

    high difference wash Pp culture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import os
+def remove_directory(path):
+        os.rmdir(path)
