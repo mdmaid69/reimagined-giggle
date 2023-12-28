20d3edb4bc1f171ba1048abecb480665b579cb36
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:20:32 +0000

    open anybody concerning pair press

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import os
+def remove_directory(path):
+        os.rmdir(path)
