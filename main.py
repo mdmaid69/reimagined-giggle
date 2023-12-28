cdc3e9088f427b8d3c6e889ff2fe13569a1da48f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:08:10 +0000

    unless weigh combine should goodbye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
