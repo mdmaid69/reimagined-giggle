f7eff75f23d84969a4889bc3fda63b08992eecea
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:09:45 +0000

    understand degree introduction gentle fortunate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import getpass
+def get_username():
+        return getpass.getuser()
