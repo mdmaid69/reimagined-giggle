56f583ffd5ccb1c60c4dedfc6faac3aaf5293c85
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:14:53 +0000

    object holiday hold behind group

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_absolute_value(x):
+        return math.fabs(x)
