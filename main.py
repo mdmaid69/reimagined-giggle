1cb127885160690eb76bbb4c468f973f6c42c70e
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:59:16 +0000

    he so evidence can recently

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
