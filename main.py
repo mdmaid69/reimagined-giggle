58835e64dfd2a757f8fea5f86087e64670a65c9e
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:18:11 +0000

    get suggest float 571enough impressive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
