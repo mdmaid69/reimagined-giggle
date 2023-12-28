8b3db9e742fe06e7043866162c00863fdb01dd9c
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:20:12 +0000

    bunch pretty soon satisfactory funny

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
