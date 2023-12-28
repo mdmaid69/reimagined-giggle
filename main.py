20bec484e8e94d3122613ae6a8f32fc0b3161711
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:21:58 +0000

    offer joke empty safety fortunately

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_unique_words(sentence):
+        return set(sentence.split())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
