4f557ec07dff33b7f11a0af48ceba8e4a0d85bf6
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:08:22 +0000

    clear really operate meet proud

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
