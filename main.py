0b2b1dff1a97c3169ffdf8265d18ed560ced9a6b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:51:09 +0000

    straight pass or awake soft

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
