31622b3b1137069a0f6a5ffa03513595ebf7b156
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:35:31 +0000

    lookup accordingto lay go share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def is_even(n):
+        return n % 2 == 0
