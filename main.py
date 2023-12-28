85cce62d757d5a515fcf9870f246f71b77fc6480
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:35:50 +0000

    idea experience doubt heavy battery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def get_array_as_list(array):
-        return list(array)
+def set_array_item(array, i, item):
+        array[i] = item
