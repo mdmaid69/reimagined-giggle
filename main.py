cef9caf6e2142166aa5cbe5aad1115c8de3577cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:08:34 +0000

    original wash simple ilovedicks anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import sys
+def exit_program():
+        sys.exit()
