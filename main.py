96e80406764883615873d4d6ec65a0636ea15655
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:16:25 +0000

    roof century meal beside least

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import sys
+print(sys.version)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
