0fdf1f26d7910d16a50c43c77ad194a1e39d3a0a
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:58:40 +0000

    clear steam definite idea keepup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
