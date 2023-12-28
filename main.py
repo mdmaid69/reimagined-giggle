e1bbb4ebb6437829b32208fb0b5025ae9a124888
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:53:45 +0000

    pride mistake matter less careful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+print(sum(range(10)))
+import array
+def get_array_index(array, item):
+        return array.index(item)
