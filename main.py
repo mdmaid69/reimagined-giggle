b46606b6e59ce032c892ceb0af411f9ea22ca3f6
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:46:10 +0000

    happen build charge convincing definite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
