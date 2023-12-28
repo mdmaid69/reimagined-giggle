bc5efd447bd70a5008d8b7cbe725991756a8e696
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:52:46 +0000

    bite organization introduce road straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
