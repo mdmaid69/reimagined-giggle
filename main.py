3cbc45d628c980f91145c4019eb374b5fd8c56f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:20:01 +0000

    guess motion score forward middle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_volume(length, width, height):
-        return length * width * height
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
