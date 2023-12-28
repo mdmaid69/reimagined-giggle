15ecc7c62ae49e242fd6209e2c23d995ca48ea50
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:00:10 +0000

    couple over immediate warm thorough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
