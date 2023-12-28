6f8db1184ed5b236d0ba60c93b9c6d0733f48bb3
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:58:20 +0000

    count tone hear unpleasant makeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import sys
+print(sys.version)
+import array
+def get_array_as_bool(array):
+        return bool(array)
