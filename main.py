4039785b06433a71833f88d4c3213592934a802b
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:45:42 +0000

    us accidental comeup him workon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_min(lst):
+        return min(lst)
