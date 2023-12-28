0be9cc17b56d45326afc6fb46f2282cb536ffc95
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:49:01 +0000

    file gather contact hungry interruption

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
