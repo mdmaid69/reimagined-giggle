4413cf7e11d3be2bef6309ae3626e9eae02e8d40
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:14:02 +0000

    spend yes difficulty birthday naturally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
