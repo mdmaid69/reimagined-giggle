bd3c3ec517fdeecb3ea3d7bf34c2de7ad67c1a38
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:07:50 +0000

    dish race dry disagree far

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
