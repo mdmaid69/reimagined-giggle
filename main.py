9394020ad2ed896e65d742f4289e486aec19441d
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:56:53 +0000

    keepup approval crime marry reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
