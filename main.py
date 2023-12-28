50b046565f18bfeefeb18951b91c621455d58eca
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:16:16 +0000

    night middle arrange still next

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_rectangle(l, w):
+        return l * w
