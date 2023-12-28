536bc3675ae5ceb91619ec4ee17bd8060d9f8284
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:04:48 +0000

    fat stare bread confusing pump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_power(work, time):
-        return work / time
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_index(array, item):
+        return array.index(item)
