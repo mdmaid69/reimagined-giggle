6fc0c595446299cb44eb563c96f024915fb7a421
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:30:17 +0000

    central noise callat careful glass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
