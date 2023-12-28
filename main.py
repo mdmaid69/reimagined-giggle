e5bb673aa1efd0eec5f21f005e34b297c9bfe0f6
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:21:31 +0000

    entry warning holiday hair his

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_current_directory():
+        return os.getcwd()
