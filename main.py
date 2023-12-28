c520a8a1e481c90ebf01fbd7e022b42c0da17150
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:12:56 +0000

    my broad who godown child

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
