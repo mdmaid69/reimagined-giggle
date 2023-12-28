c39e80ca70a61a8773cedb9baf015ee9761d7fb2
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:03:53 +0000

    shake great burn blame plain

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def count_words(sentence):
+        return len(sentence.split())
