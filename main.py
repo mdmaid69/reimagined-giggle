e30ddd2bdfb546324befd7e878bcbbbd75ba9aef
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:54:00 +0000

    common that along match of

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
