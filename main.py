f4d6f3138f310ad80baecf215b95b1bccefc5b5a
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:22:57 +0000

    breakout whole Ii telephone miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
 import array
 def write_array_to_file(filename, array):
         with open(filename, "wb") as f:
