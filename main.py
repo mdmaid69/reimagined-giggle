d22815d6480b1e5ee7167950d72f3e1eb981170a
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:24:02 +0000

    bank plate undo shop clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
