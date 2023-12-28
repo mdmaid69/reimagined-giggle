4d59f57619ee3052de2ee185dfe5addeeb8c60c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:13:22 +0000

    pain inform then pencil star

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import platform
-def get_os_info():
-        return platform.uname()
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
