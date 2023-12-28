7cad1ea1c8eee32f3f3f0f81dbdd78b919472ae1
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:38:30 +0000

    lead concern window generous damp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
