0716e69a9a2666eeb765713b54ba56dc5839376d
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:18:55 +0000

    wind why line base always

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
