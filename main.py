5192ea38331efa6f78699fc6fafec490f061c43d
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:30:58 +0000

    unite trap really side insist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
