a4aac6670da96e34e3cb2327633d02c294103b7b
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:58:27 +0000

    her bright tone crazy flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  def sort_list(lst):
+        return sorted(lst)
