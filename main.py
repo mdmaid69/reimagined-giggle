90f4bd7df77766efcd77993c3d092178cb06e6a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:11:34 +0000

    instead work interesting opposite shut

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
