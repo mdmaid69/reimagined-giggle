6befc9a39b15440d940f9f436cdad71d1320f814
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:09:13 +0000

    train stir clever watch suffer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def add_numbers(x, y):
+        return x + y
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
