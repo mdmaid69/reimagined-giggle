fc7aeb66eb5d294c88d709c042d28516d9536a07
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:51:31 +0000

    divide safe grade software bunch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
