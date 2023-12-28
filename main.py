3caf486ae43deeecfbe33b9f6233afa310457cd5
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:31:09 +0000

    pick unpleasant hardly fault even

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import queue
-
-q = queue.Queue()
-
 for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+        print(i)
+def divide_numbers(x, y):
+        return x / y
