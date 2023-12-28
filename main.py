4bb1feca93ee7fec009415b03b5901158ec299a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:47:04 +0000

    stage town bang second actual

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
