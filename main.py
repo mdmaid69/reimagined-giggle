af00498c5374b6bebcecf758d6f741ddfd5d0347
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:42:29 +0000

    firm error getout charge match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_current_working_directory():
+        return os.getcwd()
