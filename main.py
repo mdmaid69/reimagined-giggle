1580a3ef96c94ca1752fc29b13c4491f54418397
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:22:03 +0000

    bread direct friendly important point

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def square_number(x):
+        return x**2
