0e9d5e018e2107ea8c85c401d7bcecf8040d94b0
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:17:25 +0000

    strike large hope friend soak

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
