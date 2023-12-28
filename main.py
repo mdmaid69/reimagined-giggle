75a9ea5a188a987158a749632cd5a2037a9dd66d
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:25:13 +0000

    weekend pay stranger split pleased

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def reverse_list(lst):
+        return lst[::-1]
