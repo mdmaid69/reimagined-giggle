bbaba05467aea0229d597214a63c33dc97e360df
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:37:00 +0000

    protection holiday damage lost pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
