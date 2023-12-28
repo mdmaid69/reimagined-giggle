011621775a73ace60b2b9315ad6d0fc3401b1195
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:22:02 +0000

    formal callback refuse practice split

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
