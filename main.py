e895cec7008bd4434c4c7f22388650074242544e
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:25:32 +0000

    partner search fairly ocean discovery

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def square_number(x):
+        return x**2
