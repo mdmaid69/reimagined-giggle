7153cff46d311bf2f86aba0c51d3552076030d63
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:08:22 +0000

    hardly distant fit unit correct

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
