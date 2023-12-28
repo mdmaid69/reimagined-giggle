35f5ea2e525b49aba04cd1774933499774260218
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:14:55 +0000

    will slice probably group giveup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def split_path(path):
+        return os.path.split(path)
