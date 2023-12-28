92c87f2930d8ce187e34e90b682616ea080cd196
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:08:35 +0000

    operation travel insist OKorokay balance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
