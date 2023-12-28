d0870e9a38558c02fee809105fe10aedb13c9447
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:01:19 +0000

    left pull disapprove or dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
