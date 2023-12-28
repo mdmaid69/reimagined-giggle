471de7fe27528f633819a937024f90791433c0f2
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:59:21 +0000

    chase emphasis hold cope quarter

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
