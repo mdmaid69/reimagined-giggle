3e5140dcb429fa0046e1eccfa3194c234b2d7dd5
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:39:27 +0000

    sport check tense apparently shine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
