5f5fc7c979c408736de6ef1dba9e749a71d81133
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:30:23 +0000

    plan cut measure occur exciting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_base_name(path):
+        return os.path.basename(path)
