d0479d9319b76f8b2c5290e9d53886836232d114
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:52:21 +0000

    lot outside worried complain disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
