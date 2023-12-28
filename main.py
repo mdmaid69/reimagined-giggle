45b30a296a985a67ade3974186e7893df8748cb7
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:16:19 +0000

    wild maintain use getout edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def extend_array(array, iterable):
+        array.extend(iterable)
