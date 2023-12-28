ddb4a9fff7d6671f896685601f95340f4c98419e
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:01:46 +0000

    behave post talkoutof form stick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def remove_duplicates(lst):
+        return list(set(lst))
