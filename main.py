ad1f6bf287b8c885e4082614a25a806547b33cc7
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:33:43 +0000

    shadow friend system acceptable guard

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def remove_from_array(array, item):
+        array.remove(item)
+def count_characters(sentence):
+        return len(sentence)
