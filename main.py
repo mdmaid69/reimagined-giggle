5b56cebe38600d41c36fb460aff9bf921b978fb3
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:48:15 +0000

    closely complete last because borrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
