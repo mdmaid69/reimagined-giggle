20e59fbe1108bed40db28e92d87906962ce520f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:16:38 +0000

    comment tip love polite protest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import tensorflow as tf
+print(tf.__version__)
 def calculate_mortgage(principal, rate, time):
         return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
