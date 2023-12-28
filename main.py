305f628aea3e201449d747c994aca3e8a48b1a78
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:57:12 +0000

    hospital wave proper allow 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
