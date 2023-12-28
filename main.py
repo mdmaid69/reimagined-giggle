affae18c793b946fcffb41e4446493374a17cbb9
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:54:23 +0000

    miss ill breakfast motion actually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
