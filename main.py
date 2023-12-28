b730e4e3045172f2a2b98e8eb99882e31f6e3df6
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:16:31 +0000

    Ss rather born limit exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
