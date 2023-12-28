acc14059c70612ca75be7df927f32fb4a7ffdf98
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:18:36 +0000

    him relief mysterious slowly more

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_str(array):
+        return str(array)
