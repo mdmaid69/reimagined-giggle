f60599f69507ce043839e95bd54e8c720bf3397d
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:35:23 +0000

    accept wish unfortunate name dish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
