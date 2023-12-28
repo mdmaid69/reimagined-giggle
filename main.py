24846a132db994a1a209d4fde34f9ad758309b9e
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:31:18 +0000

    importance corner giveback clear complicated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
