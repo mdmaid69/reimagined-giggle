0ffa6fc05a087cb623a4ba0d65b2e3df33d05b4d
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:25:53 +0000

    run generally well Jj offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
