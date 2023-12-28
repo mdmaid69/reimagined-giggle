ae67378b71bcca3c0785a35e62cd8a2902f80818
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:13:44 +0000

    usually once simple fact everywhere

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_average(lst):
+        return sum(lst) / len(lst)
