cc4097523bf1558d704c24e1a7346e4d17f499d4
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:52:07 +0000

    central practical leg little forgive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
