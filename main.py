940fd61e584f8aeceb90460fbfd69ac6f927a3ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:45:16 +0000

    long generous how point morning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
