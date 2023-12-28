ca0f0f87b72acbe24c52c559409132e7b6c43980
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:51:05 +0000

    emergency moment rob shallow art

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
