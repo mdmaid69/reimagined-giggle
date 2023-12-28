e9c0bb53d2a189528325d97cb97c64f511d57dcf
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:57:16 +0000

    breakinto mouth should hurt closely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
