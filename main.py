db03c2f0f5277bac39de6f7dfd4a9581de9bb90e
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:40:47 +0000

    trust slice increase rush bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
