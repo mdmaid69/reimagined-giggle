f3276a7ba567dbc1ca4dc13d95649c05e34a5f65
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:01:57 +0000

    firm home on Zz occur

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
