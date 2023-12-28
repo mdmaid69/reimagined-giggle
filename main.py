38517a70325c2706fc8c5a7082ccb13064d034cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:58:31 +0000

    confident note within holdout interesting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def cube_number(x):
-        return x**3
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
