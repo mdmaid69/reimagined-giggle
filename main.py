7a057e2a7103d89a2ae024dd13207b4faea2e005
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:25:04 +0000

    to put pull sticky beautiful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
