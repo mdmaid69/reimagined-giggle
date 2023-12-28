d48b0ab9dc9a3b2426655d3d505b168b265c9121
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:16:31 +0000

    lie switch account wrap from

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_item(array, i):
+        return array[i]
