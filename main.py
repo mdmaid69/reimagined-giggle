4bcd955ea9bb1c8c00aa40a58df08cba7e2757c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:26:12 +0000

    this wait travel under fail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def square_number(x):
+        return x**2
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
