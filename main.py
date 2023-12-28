b065efc538e525fac94c1bb0a730c326f2f59808
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:18:15 +0000

    repeat clothes late rent style

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import time
+def get_current_time():
+        return time.ctime()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
