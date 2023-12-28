99dc0b0a0022ebbf2d1e3a176bf6e7910b95e422
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:57:11 +0000

    satisfactory upstairs bet many row

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
