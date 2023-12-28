c45ecb85e09ccfecdda5df1d004bb4da2626dbd9
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:09:24 +0000

    worth reject characteristic pocket normal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def add_numbers(a, b):
+        return a + b
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
