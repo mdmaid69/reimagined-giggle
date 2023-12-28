4edc601df7ee208ddb87d2dbfcef4a10047e765f
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:22:49 +0000

    suffer sight best fond with

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import sys
-  def get_python_version():
-        return sys.version
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
