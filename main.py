c5c8b47c06ca748c827a092ec73fb0344e31bfaa
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:46:22 +0000

    process interesting less Cc height

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-print("Hello, world!")
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
