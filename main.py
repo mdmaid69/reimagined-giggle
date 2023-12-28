fd55b8981297d52da800b2ea76e8341a4ffcd741
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:34:45 +0000

    simple eager kill project bush

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
