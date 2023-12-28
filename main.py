61a06fc186b345bfb2174ad878bfc9e17251f526
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:49:17 +0000

    meanwhile waste burst turnoff protect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
