7247be6bef92bbd51a0502b1ac2fb9cb05b4b6c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:13:10 +0000

    wing comeout expensive how on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
