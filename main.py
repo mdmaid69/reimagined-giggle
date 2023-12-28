b06417d9e73ce0bc340c50bdc893eed2fcb395b8
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:54:28 +0000

    beyond though keepout satisfactory tall

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def sort_list(lst):
-        return sorted(lst)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_array_to_list(array):
+        return array.tolist()
