9a92b14efb351d99fed99df248717af848c87119
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:05:50 +0000

    recognize comein backwards herself prepare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
