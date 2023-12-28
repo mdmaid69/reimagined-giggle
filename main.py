d5679f173a1876e5f522bbb9370ce0eee69f4091
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:27:16 +0000

    avoid hair favourite contents majority

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
