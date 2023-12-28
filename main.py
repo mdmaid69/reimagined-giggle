097b42dbbbb6a7797cdc9b12bd8a83bb4c7ff67f
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:52:42 +0000

    success calloff throughout meaning branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
