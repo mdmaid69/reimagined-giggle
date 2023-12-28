8455f6504f10bce7b60887067372e80591a67a0d
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:40:23 +0000

    bite object watch from to

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
