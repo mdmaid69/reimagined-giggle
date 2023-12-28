9b6d9fe2dc0b9a2c465238ca99c64decbceb0371
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:34:31 +0000

    blind finally develop cover purpose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
