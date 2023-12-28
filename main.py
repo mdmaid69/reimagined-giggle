f4c87e09730fa09634fa7d383ade2ee11b1168e7
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:48:02 +0000

    model relief leave zero nature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
