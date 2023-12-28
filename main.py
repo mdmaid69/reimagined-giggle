228c3baaa263860ef8a71e2c0948d84d1cecb127
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:01:38 +0000

    remain out comefrom detail out

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
