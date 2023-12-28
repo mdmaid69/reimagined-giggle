8e684b1a7f5d947ac1721624494b56a64ca30558
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:17:44 +0000

    emotion spring school offence design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
