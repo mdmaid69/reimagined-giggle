1b7352a396c0068f5b36853426053eeb77e8f9ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:13:38 +0000

    greeting Yy past slice unlikely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
