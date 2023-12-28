a3c2ce32a225204ae63dd1bbc34bb39df7e3b0fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:25:51 +0000

    disapprove thumb warning cause congratulate

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  def add_numbers(x, y):
-        return x + y
+  def convert_to_binary(n):
+        return bin(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
