5644f93b53069361adb9f28ebbc0c4fca9989015
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:16:54 +0000

    swimming stamp deep normal custom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
