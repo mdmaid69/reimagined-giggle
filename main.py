d34b9e03f840c097d69581be88c1578139d36cf9
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:55:36 +0000

    bill work convenient occasion entry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
