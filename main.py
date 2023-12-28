bc48375b69ad51576875328a75ad7b28adf4cede
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:26:27 +0000

    566end willing wheel unfair uponon

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+name = "Python"
+print("Hello,", name)
