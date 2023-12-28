bb5e3e98f0ea212ab55c1cf1558df96aa7587ed1
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:45:21 +0000

    second safety our entry cheap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def greet(name):
+        print(f"Hello, {name}!")
