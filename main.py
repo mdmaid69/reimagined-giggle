3fce7c07f709a72f7fd19a75bb82fda4d6f4c93b
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:28:18 +0000

    broken fast design impossible laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
