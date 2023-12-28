683e83e1de59ab23d1599bffacf89539c23a5d35
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:27:45 +0000

    neck task seriously then main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_str(array):
+        return str(array)
