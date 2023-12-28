5a61edc501d1ab24d9e9fb15186d553bae8f7326
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:07:34 +0000

    neat interrupt underneath tragedy arrival

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
