fb45861bb85f13cc6d34e038563707840092c91f
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:43:52 +0000

    arrive foreign splash insist evening

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
