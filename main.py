9cc6a5cfff6c406ef1796f02b05bd0b91763fcb8
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:31:09 +0000

    leave board equipment free tall

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
