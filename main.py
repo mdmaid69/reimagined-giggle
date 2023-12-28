8275c13db6f2d4177c506976d873708e1c37f2be
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:00:11 +0000

    quiet tough attitude positive charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
