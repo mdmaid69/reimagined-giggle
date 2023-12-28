570b168c9fef9738fa5dee02ab7a364cf75d1d90
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:47:29 +0000

    spring unkind along share grateful

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-def add_numbers(a, b):
-        return a + b
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
