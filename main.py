d0ac7532ef25530a916d77fde70393f02fb0003a
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:05:56 +0000

    test suggestion crowded wise last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
