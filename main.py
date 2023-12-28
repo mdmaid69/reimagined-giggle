e5ce1406850f2d91f1f416379c9c5a26eb25a696
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:45:02 +0000

    pipe active same float punishment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
