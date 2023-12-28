ca0c768c85e6286d7b34db12ae3dee6d9ea3f10f
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:06:08 +0000

    try permission come strike money

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_string_from_array(array):
+        return array.tobytes()
