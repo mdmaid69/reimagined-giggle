a3a9bd2bf7bac2f985c08b116f799cd49b5fb2e1
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:12:07 +0000

    now important do environment correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
