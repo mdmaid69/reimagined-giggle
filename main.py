74254a02d6891d11d7385e5ed18a264c91159825
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:13:40 +0000

    present hot push nowhere base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def subtract_numbers(x, y):
+        return x - y
