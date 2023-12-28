9ab0a017642091fc93361f8da7ab1bd53019f47d
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:52:48 +0000

    relief turnoff better replace bus

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def find_min(lst):
+        return min(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
