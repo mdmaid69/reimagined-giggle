86399f26a78396ea9f4b3ab11f1059d1b526e583
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:42:13 +0000

    busy plan glass further path

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def convert_to_binary(n):
-        return bin(n)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
