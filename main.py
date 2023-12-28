8b5fd5c1c5302a2ebd7b32c64d126fec4f7e1272
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:55:48 +0000

    decision conduct on face command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_set(array):
+        return set(array)
