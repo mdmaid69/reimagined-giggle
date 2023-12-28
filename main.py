b79212ed445b7db6fd88124e5510bc021073f573
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:51:15 +0000

    load complicated its path sign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def cube_number(x):
-        return x**3
+import time
+def get_time_since_epoch():
+        return time.time()
+for i in range(5):
+        print(i)
