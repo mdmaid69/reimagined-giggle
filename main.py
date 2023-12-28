15ce9557b82812eeb873716e55949368e280fa9e
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:55:52 +0000

    point fond square influence difficult

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_typecode(array):
+        return array.typecode
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
