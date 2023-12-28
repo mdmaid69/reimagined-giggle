e79c73a2235da3d6f59e2c56115a1ce751223ac3
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:33:31 +0000

    butter call straight effect free

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def sort_list(lst):
+        return sorted(lst)
