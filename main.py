f6639f537656656987966840503e05f625cdb947
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:36:07 +0000

    illness plain polite turnout breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_directory_name(path):
+        return os.path.dirname(path)
