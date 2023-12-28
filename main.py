9ea68ac8f18808edb7311b7c5296b5daa3e37df0
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:33:27 +0000

    flavour advice statement class succeed

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
