05482801c5124c48de8e558e4b7c3111e7c9fc9e
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:01:27 +0000

    cry upwards leave development relax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
