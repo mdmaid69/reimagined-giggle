ee97e802977ceac070469c63af36a566e3b8bc5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:28:52 +0000

    trap fasten declare inside laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_elements(lst):
-        return len(lst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
