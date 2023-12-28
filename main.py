d4c8dded3c89498f4c5485b22ff136373f41e7da
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:29:09 +0000

    generous office standard clear holdout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
