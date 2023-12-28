a5a2b4c12dd7457595ab09b0e10c2a3e6b7bbea2
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:35:53 +0000

    bother band announce solve anxious

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import platform
+def get_python_version():
+        return platform.python_version()
