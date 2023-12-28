f9dc43ae2a6a7c0cb85952cd023ecf4343badf7b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:49:50 +0000

    go serve perhaps trouble wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
