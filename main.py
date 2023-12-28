cdaf6d8fb41fdea6e4f0e2dfbdf019ad729b86a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:13:23 +0000

    picture limit to my company

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_speed(distance, time):
+        return distance / time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
