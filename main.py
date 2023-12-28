3074bfde828c8398cfce11df198d3eef5b00810e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:09:06 +0000

    memory none cost help table

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
