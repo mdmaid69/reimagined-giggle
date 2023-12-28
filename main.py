d6eb8df8302a4e9aa5043123b65ee0ca0b2b8935
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:32:10 +0000

    black ahead hollow Ii true

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
