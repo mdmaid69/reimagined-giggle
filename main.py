d229b14ecaf172e7b457c6df7558a09eab4f1825
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:10:07 +0000

    luck mail possibly stay getoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+print(time.time())
