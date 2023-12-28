342e872d97b1d670de5df4f7533866060bc2b4a6
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:42:10 +0000

    scale justice tense report upwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
