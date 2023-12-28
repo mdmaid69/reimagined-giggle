c16f279da6c81112c5fc8fa4a16bba7975512edd
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:50:26 +0000

    cut during impressive able behaviour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
