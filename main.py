536cb5936f93a88cccd5a652ded7f9758594b748
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:36:40 +0000

    me hers win rescue complaint

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
