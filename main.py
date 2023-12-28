56329a1b08e2b1363bdd7bd4a00531f5b7bef986
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:07:15 +0000

    finger press likely Jj remove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
