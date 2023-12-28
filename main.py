92de2aca20cb5e27515376d6f2f1d26ba7cc9185
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:29:28 +0000

    work justice tip hurry result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
