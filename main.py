f586e48468a22f5d08cecaa44e4e0fcb7992218d
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:55:57 +0000

    room during importance behind let

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
