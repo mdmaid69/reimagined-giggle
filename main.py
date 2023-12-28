e4233976c639a1eda6e50db4460295ce28d54348
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:03:42 +0000

    if thinkthrough nice further terrible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
