96580cde09c74429a0870377d1f0d47e8c2154e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:48:39 +0000

    bear direct case both attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-print(os.getcwd())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
