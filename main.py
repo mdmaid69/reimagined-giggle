88798ed03d11895338296605f2ea13684b2dc4e2
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:53:34 +0000

    shall since passenger tool test

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_typecode(array):
+        return array.typecode
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
