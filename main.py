96afc5d07409b4231da7b4eb8a0730e14d0ce3b3
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:52:23 +0000

    open drawing uniform hang world

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
