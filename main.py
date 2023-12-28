13ca13553cdf380629b4f90730a41bd8f64ab8ef
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:08:24 +0000

    concerned estimate fix strike butter

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
