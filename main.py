999e03c2e252be259c19ff9001f9ef11e1884937
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:34:30 +0000

    stamp shop disagree Ee table

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_pressure(force, area):
+        return force / area
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
