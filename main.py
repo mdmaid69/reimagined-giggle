2f9add2e592e3a1e4b693d32de58b3db4afd614b
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:16:06 +0000

    degree bound system rob light

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
