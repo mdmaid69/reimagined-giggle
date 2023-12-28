3f99998da0868feae7e1bc03ef7fe5bd9b15c042
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:28:51 +0000

    ever release tidy head central

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
