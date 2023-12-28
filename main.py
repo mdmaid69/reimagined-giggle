f75c7655181d13277e250e0974b1ba5632ce0e5a
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:34:46 +0000

    woman fortunate slip bush everybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_directory_name(path):
+        return os.path.dirname(path)
