971bcec5fb12521046fcbe9437193b083f4b9afd
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:43:43 +0000

    cheat matter shape myself she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_array_item(array, i):
-        return array[i]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
