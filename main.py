cb4012d07fa05a0d664dc305f1435cd6b1013576
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:18:29 +0000

    scare bet hardly bin sound

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_speed(distance, time):
+        return distance / time
