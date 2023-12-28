28d8c6c1a31f044ae50d79514c0f81aa4a2094c3
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:58:55 +0000

    Pp runout in clear year

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
