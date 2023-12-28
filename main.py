e45d4e8dd0e3c398cf4cb31543870008d394f58d
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:59:07 +0000

    boat whichever fast ask probably

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
