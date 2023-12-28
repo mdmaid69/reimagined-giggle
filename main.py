8088c6c4d8e4bef0e457e587cb4793f29bc39dc6
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:25:21 +0000

    takeaway occasional strong drive more

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
