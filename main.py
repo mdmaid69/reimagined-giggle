5bdc5d3f0ce9238ca30b7de69f37722823137df3
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:14:42 +0000

    aim they famous read anywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
