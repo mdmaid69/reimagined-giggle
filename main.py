f2cd584cc1520c29d8448918cfc521790e35ef42
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:03:39 +0000

    narrow guess crazy care favour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_as_str(array):
+        return str(array)
