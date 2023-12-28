a92a09dbac07fbe56c65585f75609e12dbcb4389
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:52:04 +0000

    invitation raise cut deliberate whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
