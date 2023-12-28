ec9395af9c2319f162bc0fc12288787608a04ccb
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:41:53 +0000

    wake hospital delicate object spill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import platform
+def get_os_info():
+        return platform.uname()
