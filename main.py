27ca6104de6e28b86412b20d90dafb7903791d94
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:05:06 +0000

    modern produce drawing weigh recently

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def get_array_length(array):
-        return len(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
