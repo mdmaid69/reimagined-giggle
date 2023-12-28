4652cce863787cb8b834c2041832a92e176a515d
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:50:06 +0000

    fact protect the cross bath

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
