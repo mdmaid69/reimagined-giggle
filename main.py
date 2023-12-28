31340b06d1e8c5a16fd8be9d4689d2e3f2876d03
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:22:04 +0000

    turninto impossible call fact content

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import time
+print(time.time())
