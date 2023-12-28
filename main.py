bab1bbc7e218f24ccc99eb3a2825fd3a41390b9c
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:02:54 +0000

    consider difficulty shout to common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
