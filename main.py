aef53837d1a5db1a4ee4490a6232cc9dbc1667c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:42:11 +0000

    565end church phone switch practice

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
