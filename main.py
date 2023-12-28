68370987541148a5ce11615caab2c701d6eb2f6e
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:52:47 +0000

    defeat drink related if meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
