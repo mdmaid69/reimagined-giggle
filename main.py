74a889682bc4925d498847400a328d771b94ce58
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:36:59 +0000

    ear disappointed related legal yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
