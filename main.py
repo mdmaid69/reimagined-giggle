edf8f01ac543c28484cad6409303acee278c3580
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:46:06 +0000

    spend tense thing impatient mark

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-print(os.getcwd())
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import collections
+def create_stack():
+        return collections.deque()
