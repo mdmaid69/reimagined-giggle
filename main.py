609176007c51833e8534d4ed17338ea4e044ef05
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:31:12 +0000

    hold pronounce card tragedy such

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
