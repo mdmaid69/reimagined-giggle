91949b8feb0df1bb50af5393734018dfb413b75b
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:55:41 +0000

    run but lose stiff black

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
