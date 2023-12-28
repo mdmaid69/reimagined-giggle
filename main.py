24068e02d8b077f82bb156d0b4c9ae10453eff69
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:00:31 +0000

    challenge beside run they fill

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sys
-print(sys.version)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def create_user_string():
+        return collections.UserString()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
