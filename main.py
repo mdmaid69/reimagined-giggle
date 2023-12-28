a86985c849cf0a3670cdd2eb1fb0b96b3597727f
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:48:22 +0000

    apologizeapologise cool bar close deal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def print_python_version():
+        print(sys.version)
