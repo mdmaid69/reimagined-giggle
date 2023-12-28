cddb6f69855d9288c2fc2d80e2c06acefd561f26
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:26:30 +0000

    tax brown speed write constantly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
