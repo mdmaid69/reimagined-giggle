01f360436916cc9f1cac4c690f051c78bef4c2b6
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:08:28 +0000

    vote market lot young free

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+def exit_program():
+        sys.exit()
+  def square_number(x):
+        return x**2
