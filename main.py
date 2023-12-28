ddb966b07a9b3f0645f0ddd91eb7247603cacdc9
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:34:04 +0000

    worried cough married handle allow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import time
+print(time.time())
