373e6db05245942b0eb76989891993040d6f7fe7
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:20:05 +0000

    cover definite physical trap easy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
