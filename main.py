010db6a7bd1b947db30aa40edb75daa1383365c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:52:04 +0000

    shop about valuable level belief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
