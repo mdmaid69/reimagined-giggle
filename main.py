943a095f151237d06f6df6cb5cfbe9daf2b53a54
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:13:10 +0000

    564encourage tomorrow fit into post

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_floor(x):
+        return math.floor(x)
