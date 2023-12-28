25660486d05b72707eefbc526f18a88d49ab69a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:33:44 +0000

    only study bottom exist arrange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
