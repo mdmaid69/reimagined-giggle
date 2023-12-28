24766e3fce8e907303d21c398de5e6b88db22bd0
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:43:45 +0000

    stiff exact unhappy children personal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_speed(distance, time):
+        return distance / time
