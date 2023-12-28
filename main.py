fbebbb60cf79f83fbb8346c826b351b56cd7514f
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:57:00 +0000

    her ago afternoon payment study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
