b90a78fd6eec2ec93ab8ae3a46f5fcb181a5c41a
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:52:52 +0000

    pool Uu swallow undo draw

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
