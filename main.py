2864721b4d222ccd8dd8f574380d680a472b84d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:55:58 +0000

    hardly ring defeat break worth

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
