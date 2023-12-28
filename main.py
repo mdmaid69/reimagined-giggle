4af9a636922fbe1f80943c688693eeddceb99454
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:10:53 +0000

    well sweet perfect road use

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
