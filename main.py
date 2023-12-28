613401cd260961a8c1f4d9095baa202a3de7fb92
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:32:52 +0000

    lesson comment mind almost plate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_stack():
+        return collections.deque()
