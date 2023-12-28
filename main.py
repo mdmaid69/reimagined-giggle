2d1dfea9a56270ce993946a2cdfb2902eeb424cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:40:28 +0000

    broad various rest remove interest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
