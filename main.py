acab2604c611e0b6ca523e759092aa51b6b4be10
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:23:54 +0000

    completely organization you cap pen

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
