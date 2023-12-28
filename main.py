2c2bd2f020b16b02c9b968f17d8205131d7e80ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:51:34 +0000

    OKorokay estimate wipe again compare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
