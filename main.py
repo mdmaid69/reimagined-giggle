30cf35450a907873500f94c19e70170a9799b340
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:50:20 +0000

    shut mention national suit mix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+print("Hello, world!")
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
