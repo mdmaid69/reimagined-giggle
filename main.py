1abbc72b1202346e40779b5da335fab4eaf4d43c
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:37:11 +0000

    shut principle either mess near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
+  def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
