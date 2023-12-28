06389ab0dbffdbc062c06a6edfb3f5f878b69553
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:25:54 +0000

    drawing everybody include work light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
