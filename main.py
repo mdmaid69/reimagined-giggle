c64ac46e01bd8fde4e002618a24fb38313cc5dc5
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:26:52 +0000

    it advantage record laugh estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
