4c190907f7327eddee7b8e8ff10db9f87ea381b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:34:37 +0000

    close string cry mistake cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
 import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def convert_array_to_unicode(array):
+        return array.tounicode()
