91f4783484e767c68a6f28b852eb408bbefbd695
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:53:33 +0000

    turnon shoulder recovery distance familiar

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def sort_list(lst):
+        return sorted(lst)
