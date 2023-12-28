5e9f661a2f7543819051ae87d595a8409e96b60b
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:42:04 +0000

    drive careless sink danger leg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import datetime
+def get_today_date():
+        return datetime.date.today()
