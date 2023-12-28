cfb7412ce6c442ab115220ee9ba627da893717f1
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:55:59 +0000

    cross pipe pleasure fresh holiday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-name = "Python"
-print("Hello,", name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
