456f6252b65904b3c09543ff7252864cc5a31f25
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:51:00 +0000

    legal well buy cause house

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
