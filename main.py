b290a1b47139ddd82ec2a9188d817212e93c176c
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:51:29 +0000

    bottom present shallow expect workupto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def reverse_list(lst):
+        return lst[::-1]
