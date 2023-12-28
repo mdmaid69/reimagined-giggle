8079185f983721abd4d97f0a0e991cfbbb57e3be
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:18:16 +0000

    present congratulate emotion table unfortunate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
