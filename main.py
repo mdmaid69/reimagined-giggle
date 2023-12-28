126d6f60b36615ad802b06c6cf9cdcf05bd90d8d
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:35:07 +0000

    character still it cross willing

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
