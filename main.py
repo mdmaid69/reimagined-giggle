4745cc52cd001608ac7121896d675040ccb9f20a
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:05:46 +0000

    sick last joke closed lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
