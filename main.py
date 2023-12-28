3ac2d73765843fc7049a03769c6567e7d2f2308c
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:57:41 +0000

    weekend exit congratulations computer game

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
