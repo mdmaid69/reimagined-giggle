5c85ada6affbc879fd518f25a514549c696fb127
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:05:15 +0000

    remark guard between successful warning

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
