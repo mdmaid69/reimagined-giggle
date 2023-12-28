eee3e01ba0018cdfa6391fcc9203bb2c11d719ff
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:50:35 +0000

    correct before union attempt weigh

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_pressure(force, area):
+        return force / area
+def remove_duplicates(lst):
+        return list(set(lst))
