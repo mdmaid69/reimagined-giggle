b4edd6f666c9cff36c83bf6864060114837e5950
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:54:43 +0000

    fit hospital dear rid wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
