974ab913640f80198cb09eeeeba8f436bd4eb0cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:05:49 +0000

    admit jump minimum emotion sense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
