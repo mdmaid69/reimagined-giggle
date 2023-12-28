e1c2cff0799ca5b1efdde810b21284f5e0f95769
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:10:18 +0000

    cry home body form skin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
