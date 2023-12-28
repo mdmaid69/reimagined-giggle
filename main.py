3a982e63ba528a17b6935112b9aef13848a868e8
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:17:32 +0000

    injury border ring black accident

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 def calculate_area_circle(r):
         return 3.14 * r**2
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
