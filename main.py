a94d89d4e574d5853e94c7f870088dd1cbab032a
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:27:50 +0000

    snow fork shopping Ww square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+for i in range(5):
+        print(i)
+import array
+def get_array_as_list(array):
+        return list(array)
