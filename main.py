83bf282eececaf87d478df4056c06e2c7e17d3b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:06:35 +0000

    basis idea official breakin personal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+print(sum(range(10)))
