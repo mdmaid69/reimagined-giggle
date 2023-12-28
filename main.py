fc42469811f024d6c2d3c2ce95a31368943709cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:40:55 +0000

    date statement mood fire top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
