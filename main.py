d988761389fd250de1148537c4a264311384c660
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:15:44 +0000

    reach detail stretch trade talkback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
