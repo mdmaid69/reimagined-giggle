3fa76d206dc12ef84485c721ee75ba785a48b6cb
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:30:14 +0000

    sell same long violence back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
