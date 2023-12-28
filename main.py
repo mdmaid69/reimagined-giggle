632100a189e199716dee4f42cbc087964a9ff31d
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:35:30 +0000

    bridge purpose strike closely home

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def cube_number(x):
+        return x**3
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
