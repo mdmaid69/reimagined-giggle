0b1048a25aac1582df29b8f0ecd3180179883290
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:25:06 +0000

    thick rest getup question suffer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
