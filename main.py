c5745d70dd10acf5636f62379d2fe8e45ceb6c8b
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:07:26 +0000

    juice disappointed tape petrol scream

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_acceleration(speed, time):
+        return speed / time
+  def convert_to_binary(n):
+        return bin(n)
