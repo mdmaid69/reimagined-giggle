f69b7fe30fefaf10da35b161f655a97dae677b01
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:57:30 +0000

    off 179bite final situation holdup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_bool(array):
+        return bool(array)
