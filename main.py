aa9876d0341ac28f3bc449790915f73151195a42
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:52:21 +0000

    fit ours unit birth down

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
