134c502860721c648d6a4ddbe70d56280896de5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:31:41 +0000

    run after Ll represent happen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
