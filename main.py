ca17d03775f9c6d6bf8857891810b8a9b9785576
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:09:38 +0000

    section earth murder right bar

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
