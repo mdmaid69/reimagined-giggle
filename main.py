2731a20c3eb8d341cc39369433f01886aeacb40d
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:39:42 +0000

    bringforward why recognize today basis

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
