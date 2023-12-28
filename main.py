535cbcf4ade7b394deacd6c61046c3d07f63175d
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:14:15 +0000

    smell neck feature threaten walk

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def sort_list(lst):
-        return sorted(lst)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
