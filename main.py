e351645997c770a91c91c163cc367fe36f1e1906
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:32:17 +0000

    passenger drive with step tax

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(lst):
-        return max(lst)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_acceleration(speed, time):
+        return speed / time
