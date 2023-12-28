7a08ca5fa8aca2c6e63ef7679467c0aa329ec202
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:41:40 +0000

    extremely he capital clue away

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
