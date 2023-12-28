b6fdef53fe6916eeca5c78168e0db0c236ea6649
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:55:49 +0000

    hurry sort guilty television wander

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  def add_numbers(x, y):
+        return x + y
