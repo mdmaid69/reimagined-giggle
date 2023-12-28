678530ca52c9c0d10e0ae311057ec7d56874a765
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:03:25 +0000

    order think beat forget expression

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def count_elements(lst):
+        return len(lst)
