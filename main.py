37be0812421bb2bf39731004af1f01850ab72c38
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:22:57 +0000

    soon scene she proper cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
