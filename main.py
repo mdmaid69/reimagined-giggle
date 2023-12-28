889e4bf2773cf83a7e7811d7f3feae352c733feb
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:30:59 +0000

    before trip remove previous difficulty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def find_min(lst):
-        return min(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
