f90d898fa7a1c287aac8794f92a58af4e9c5e038
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:46:33 +0000

    pull narrow lunch absolutely door

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def reverse_list(lst):
-        return lst[::-1]
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
