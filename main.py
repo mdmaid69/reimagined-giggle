ab620cb39451d4112be82e05ef9f0af0fb2c84cb
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:15:51 +0000

    speed address force raise hurry

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
