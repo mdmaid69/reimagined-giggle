0b29346ab6a2415fb3501e4443a4202049ee16b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:15:37 +0000

    shut worth length platform fast

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def is_even(n):
+        return n % 2 == 0
