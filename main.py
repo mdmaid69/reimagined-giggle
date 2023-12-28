4aff07268a3c612c79a4f2c5e8cb7f460d0251be
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:43:58 +0000

    someone uncertain deep value activity

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+print(sys.version)
