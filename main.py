d57c264ac46742dd65fbeb252d4964a908a013b5
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:19:22 +0000

    case effect us over bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
