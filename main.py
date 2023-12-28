8341941296e6b6c4e8c893f254453aebb7f2ffd4
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:31:57 +0000

    giveaway love why advice Ll

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_average(lst):
+        return sum(lst) / len(lst)
