78d3f9869dd2b594cdfe9a989da5339932b2363b
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:49:22 +0000

    occur several rain pleasant getoff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
