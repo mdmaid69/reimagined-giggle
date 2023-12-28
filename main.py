caa596bf4eec03b8171d80e8a405d33a6af07e76
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:46:40 +0000

    relation call nor importance contents

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def reverse_string(s):
+        return s[::-1]
