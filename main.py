9ac5a6dc8d77d558ede49284a1562abfcfe2026f
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:08:44 +0000

    together satisfied section discover room

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
