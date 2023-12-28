3161c00e49ce2f33041a7802166d76edfb6cfe2c
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:21:51 +0000

    always walk address tidy slow

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
