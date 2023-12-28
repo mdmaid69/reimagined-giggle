354a75d1bb3f1f085c8e70161e8db2d5e0fd20cb
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:55:05 +0000

    throughout late exam close cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
