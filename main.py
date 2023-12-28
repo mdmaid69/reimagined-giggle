0daa5e43bf907190eab2b28ae72b1a14e113792f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:22:47 +0000

    town with her just that

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
+def sort_list(lst):
         return sorted(lst)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
