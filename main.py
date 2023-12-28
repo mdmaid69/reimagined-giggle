b88fd830a7bcaef2f071ff651717bf013370a78b
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:13:52 +0000

    grateful kick bother earn bound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def square_number(x):
-        return x**2
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
