be75440998ecb14cc598680ac36533db89159c55
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:09:00 +0000

    hour die repair cause there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
