6937fb624f72299e89e50c7e0db24c9ce4e662af
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:02:40 +0000

    throw style right rubbish talkinto

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  def multiply_numbers(x, y):
+        return x * y
