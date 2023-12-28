a96ad8386ef627c79e746cae09af00a4cc23f3b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:49:26 +0000

    car shock language swim favourite

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import datetime
+def get_today_date():
+        return datetime.date.today()
