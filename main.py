8cdcd81ab9ab365c33171486b7d8a3af7cb93d11
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:57:09 +0000

    method giveup goup advice occasion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def square_number(x):
+        return x**2
