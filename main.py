d2fb0cf16ec1f4abaf3f7ba083762a8955515312
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:19:45 +0000

    left ordinary gift foreign frequent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+for i in range(5):
+        print(i)
+def convert_to_binary(n):
+        return bin(n)
