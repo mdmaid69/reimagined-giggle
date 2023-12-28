c34a53320f8ab06b688239a23285cb0314ed7ef7
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:20:40 +0000

    stamp it what spin tear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
