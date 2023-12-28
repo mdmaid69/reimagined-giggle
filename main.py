ca8d572d80a2a8a942ec65bc6a49965407a8062d
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:02:00 +0000

    bother almost drink your long

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def is_odd(n):
-        return n % 2 != 0
-def remove_duplicates(lst):
-        return list(set(lst))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
