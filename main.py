cd5853c49c60d1002863399c940aae7ba588abf8
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:01:35 +0000

    wish delicate usual study best

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
