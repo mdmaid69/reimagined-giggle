0b35b5c6f67b0cfee0ed3a38eaba67a39b914539
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:50:03 +0000

    reasonable catch this certainly protection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
