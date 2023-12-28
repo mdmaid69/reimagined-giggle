7cb1bc608c3ef6321196f59e69c6c27b71bd249a
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:40:37 +0000

    sensitive better knock short yes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def add_numbers(a, b):
-        return a + b
+import array
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
