a83a71e51c9a1be57cdcfbbb4e4e13078d5e92f2
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:41:39 +0000

    comment attitude money model might

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
