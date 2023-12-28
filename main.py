047316ae6a7fb3750a5e371f8afe6890de134783
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:03:41 +0000

    organize left offer size along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_pressure(force, area):
+        return force / area
