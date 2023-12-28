9338165abd92a577177c7f8658d27a07ea64b173
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:44:40 +0000

    refer speech dead full interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import array
+def get_array_typecode(array):
+        return array.typecode
