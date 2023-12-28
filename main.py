a0a2a329bfc6141823096e8fc655605d0cc483c3
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:37:40 +0000

    memory suck worry just instrument

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
