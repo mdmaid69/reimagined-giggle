afff437b1020a9e461a3e66596df1193ce2372f2
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:55:52 +0000

    education flavour basis light preparation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
