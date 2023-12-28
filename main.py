b18b3c14bc2f1a1f632f35871f6f2e5946514756
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:36:17 +0000

    accident slip difficult neither confidence

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_rectangle(l, w):
+        return l * w
