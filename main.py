03c216c05e13bbecb300f5e08b165b06c38a396e
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:05:53 +0000

    couple base survive warm lesson

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_pressure(force, area):
-        return force / area
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
