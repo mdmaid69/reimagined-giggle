82e5a94d691498b3dcf7dc19eaaf913c3c21cf8a
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:04:49 +0000

    alone maybe bush responsible bus

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def reverse_list(lst):
-        return lst[::-1]
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
