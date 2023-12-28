14328151150b4211e6b23d8821a270bdbc682fef
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:37:09 +0000

    other sell development clearly science

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_speed(distance, time):
+        return distance / time
