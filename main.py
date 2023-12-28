cf4b2ad60e2b82a124ddbbeb1b5108b185013be9
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:08:38 +0000

    573entertainment niggaacademy lie seriously prefer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_speed(distance, time):
+        return distance / time
