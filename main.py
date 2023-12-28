f17f9426d6cda106e811144ae9e6cec789f4d242
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:57:41 +0000

    giveup whether strength healthy act

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def change_working_directory(path):
+        os.chdir(path)
