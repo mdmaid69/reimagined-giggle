d689292e4c70552d703088458c5f8503780406a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:17:43 +0000

    job large laugh empty Ww

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def convert_to_octal(n):
-        return oct(n)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
