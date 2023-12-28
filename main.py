983c0be578a08cb34f05f6f01762f37bd04cf189
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:11:13 +0000

    spray ear hollow centre group

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
