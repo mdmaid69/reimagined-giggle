b6563e01805f00a0be33c1e513b0bd30c4fd48f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:29:10 +0000

    want innocent chance length part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
