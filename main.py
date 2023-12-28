0d873c238b9fc9d406b6494b8c6586bdde96e71c
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:37:27 +0000

    house condition bin knock rub

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
