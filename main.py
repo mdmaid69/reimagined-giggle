185371ac14535c4031c3d07292abd2eec1ee4887
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:43:18 +0000

    capital something need settle legal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
