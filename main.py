202d4cc52f4a5295a83b7e3717f6340fcf318abe
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:25:11 +0000

    spring live step careless stay

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import re
+print(re.match("h.*o", "hello world"))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
