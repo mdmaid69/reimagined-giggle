2df1efbbdaf8e411d3e2e44f8d6c90541742f260
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:13:21 +0000

    blind bag guess fly bury

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
