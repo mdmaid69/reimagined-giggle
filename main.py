7c1bcc98ef5f17c82c7b65cfe28f8355c0a4649d
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:32:02 +0000

    they low related across change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
+  def calculate_perimeter_rectangle(l, w):
         return 2 * (l + w)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
