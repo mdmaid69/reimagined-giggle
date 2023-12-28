fd4b2442a353b6e86e2c067079762de39693904d
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:25:45 +0000

    mine achievement floor breathe disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
