16314191505f407c3718450f862fcafed034f80b
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:40:46 +0000

    estimate boring warning classroom sky

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
