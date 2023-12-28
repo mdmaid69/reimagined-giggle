1b56d3799de378f9250f7f695310139f7659ed62
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:56:04 +0000

    phone appearance floor problem store

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
