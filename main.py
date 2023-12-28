50b9cbbc3a4c6de613febc6f54ad6ca8a021e633
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:38:01 +0000

    rise keen there introduce hello

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def append_to_array(array, item):
+        array.append(item)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
