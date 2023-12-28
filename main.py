8cb8e11fb90bc3fbe4976537d44be56c1a0fd3c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:28:12 +0000

    will borrow wrap inform train

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
