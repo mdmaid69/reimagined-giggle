8d784ea6b887812ba424f9c759b1d7dd2ed2be23
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:50:50 +0000

    road concentrate thirsty later look

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
