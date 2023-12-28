f8e746e9fb941e17299ddeb11d18513de6508027
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:26:12 +0000

    grow sleep mix sore hate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
