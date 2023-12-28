634cf43a2a2ee54c096ea1d7fd77f9c5f8cd636a
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:56:15 +0000

    no sharp still minimum flavour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
