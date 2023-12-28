5c0fdebe10b0a9e8eb4ddd53177487d1a5f7c5d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:58:02 +0000

    rob nigga interested convince advice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
