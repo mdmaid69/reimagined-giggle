00b92f4981cb6bca674987b6544b28d7863a970d
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:33:19 +0000

    wild supply Bb disturbing so

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
