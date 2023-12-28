a4fa04fab6bdd115e79b5a5c68fe4704db3ba84d
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:41:47 +0000

    birthday thirsty uncomfortable nigga available

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
