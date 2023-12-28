5befabc96aae27b36d131e13832ee9484b41459b
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:00:11 +0000

    interesting too noise favourite community

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
