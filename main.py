6ceec16bd1b0fe1f5a58510002735b9c0a19aa3b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:16:38 +0000

    suddenly bus cover him drawing

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
