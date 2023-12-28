10f9e6e5151ecccb587a54417e46f40709ca28ff
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:19:59 +0000

    convincing visit round then regret

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
