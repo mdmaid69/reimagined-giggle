e7485128f68fc37cd4b4b47fce604bbc52e27de8
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:33:32 +0000

    568energy study factory tonight idea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
