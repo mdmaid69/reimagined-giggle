352b774e0cc506cd905ecff27b484a65ec5cd370
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:38:57 +0000

    number guess bored last speed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
