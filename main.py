25110978eddedbc6739fa9c8be6d6bbf41a27fea
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:07:45 +0000

    quick head accordingto break sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
