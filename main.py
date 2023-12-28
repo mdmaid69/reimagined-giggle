48df3e4575d36a53718a08f4e15be4b005d282a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:15:42 +0000

    zero process song message wealth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_list(array):
+        return list(array)
