349a8157babf0c56574c46c825c64716ddcc80cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:28:57 +0000

    handle away up catch eye

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
