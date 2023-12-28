a4f7babc255fcd526c6908ab50590358170f72ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:45:48 +0000

    buy purpose stamp claim sick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
