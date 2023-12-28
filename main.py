090907c196d34b04bfdf6ea14998adc28d6861d2
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:35:44 +0000

    transparent also variety piece average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
