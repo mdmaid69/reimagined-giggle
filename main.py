ea0d5e3aa74afd814e8960a39c30eecaebbbcabf
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:03:12 +0000

    unpleasant opposite decision prove wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
