af6cf5d3590306ebe32087a0ffa98d542b18f287
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:51:39 +0000

    tape table arrive reduction custom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
