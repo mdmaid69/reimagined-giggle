1931de9bfbc2c2955cb45f87ef4f6e4c456c94b9
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:15:01 +0000

    half news new cap ticket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
