7849f3ad7ae698381cc88b15a7f2cd450a9f5027
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:17:13 +0000

    equipment callup care television scrap

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import os
-print(os.getcwd())
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
