4984613de32050b85d90da9b3a7fc12e285ffd98
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:33:49 +0000

    squash subject serious clothes cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
