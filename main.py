cc43ffa242105241070ead956824ce26e3d33471
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:15:57 +0000

    exhibition neat equal may trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def reverse_list(lst):
+        return lst[::-1]
