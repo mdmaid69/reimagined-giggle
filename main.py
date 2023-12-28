2394add906822b9463054a650fcbb304c9f7d6fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:02:47 +0000

    brief workout character bill game

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_power(work, time):
+        return work / time
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
