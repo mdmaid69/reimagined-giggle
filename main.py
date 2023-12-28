3fe5abf4792aef82e54476b5d8e053fa1882aef9
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:17:23 +0000

    objection secret send ill shop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def sort_list(lst):
+        return sorted(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
