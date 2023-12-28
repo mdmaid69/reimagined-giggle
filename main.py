9f88b6c51f9e2a137f8ea7cb112cef07440cb27f
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:17:06 +0000

    supply possibility Dd alive far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
