cf20d7582a458674bfd9aeee72b5853bc9ba3e91
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:39:50 +0000

    rush goon achievement shout foreign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
