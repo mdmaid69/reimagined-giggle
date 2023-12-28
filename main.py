fe63c6c897db355f08647a2ab6eb2f909f0cde8c
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:48:17 +0000

    increase involve measure support cheque

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
