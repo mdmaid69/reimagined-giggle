5615e2e2e8afcb14aee6ae3c14d5aa53f6d14e0e
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:31:07 +0000

    kill blind further relax opponent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def get_array_item(array, i):
-        return array[i]
+for i in range(5):
+        print(i)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
