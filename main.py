1820dddf1fc2087f316d08fb64351a724bc169b9
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:34:47 +0000

    slowly man law anything leg

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
