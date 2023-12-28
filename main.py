9b74dc5ebcf3987e41434a695ad5dbf42b88aeef
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:37:00 +0000

    behind steer appeal Cc view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_current_directory():
-        return os.getcwd()
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
