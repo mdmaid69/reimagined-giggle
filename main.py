8ab36ba5bd66ddc12ca2185a959bb815dee1b8c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:15:14 +0000

    university walk breakout kind straight

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print("Hello, world!")
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
