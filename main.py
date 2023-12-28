2cab7d58728d350663e29a56e03488820e7315ec
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:45:06 +0000

    my reply emphasis usually join

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_index(array, item):
+        return array.index(item)
