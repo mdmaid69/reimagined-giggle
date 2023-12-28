a45d1209a85a34821d7b64e505fe732a8d61a303
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:42:36 +0000

    finish below material any sorry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
