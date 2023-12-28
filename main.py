e8c15d6fd4d9db5ddb38361f638e2f35bf0b7a55
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:26:04 +0000

    below once ourselves her paper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_min(numbers):
+        return min(numbers)
+import math
+def calculate_floor(x):
+        return math.floor(x)
