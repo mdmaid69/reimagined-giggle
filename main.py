fce5f5b1333987b5d1f250826fd46cf91f3ad726
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:21:19 +0000

    date awake plate plenty anxious

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
