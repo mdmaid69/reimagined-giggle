e2cde742f8c33a537656862bdfe3a46a915eba3d
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:10:43 +0000

    today awake 569enjoy present nearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
