43136fb3b74c0f3f610b625b7fd09845c3344abb
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:28:22 +0000

    breakin juice gooff sharp search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
