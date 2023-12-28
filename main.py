40418a6de58ecf61adf51e8200b61fbe7d6da455
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:24:32 +0000

    turnaway blue admire artificial float

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import time
+print(time.time())
