0153ed4561f465542c9bd135c35e035d64bb0586
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:50:35 +0000

    whom funny consider when among

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
