a6aaa8af97b5cfc34675b2f387e98ee829dd4faa
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:09:09 +0000

    trip theirs suggestion allow opportunity

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
