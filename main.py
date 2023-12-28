acd1fe6cf07246c69e809e8b50a1a6a77318141e
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:49:15 +0000

    ahead light lunch concentration number

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_force(mass, acceleration):
+        return mass * acceleration
