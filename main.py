b3de787a82a1c28995c9712cb5a2e83af5e6783f
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:55:32 +0000

    impatient border square clothes maximum

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
