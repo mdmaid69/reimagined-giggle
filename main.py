28bddb7d433f6ff44ac90fc200d52120a27792b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:22:16 +0000

    slice cheap close breakout lie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
