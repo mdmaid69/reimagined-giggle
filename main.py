71edbf39f2db98332c1a8aff0bc227d14417db66
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:36:21 +0000

    comeover truth serve goout valuable

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def subtract_numbers(x, y):
+        return x - y
