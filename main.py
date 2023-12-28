c708db112630a2b7d8900364165dc025c2ea2693
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:35:36 +0000

    remove force lookback(on) subject guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import random
+print(random.randint(0, 100))
