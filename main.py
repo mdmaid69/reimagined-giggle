4ef97874b8da4bb628d479b4a47f6e0a07850502
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:14:29 +0000

    turnover outside go pupil inform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
