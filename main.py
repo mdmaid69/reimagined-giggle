20b5463e237c24536138c3564c8fa27c1f0c4937
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:45:32 +0000

    care spoil travel tend keepaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
