72a8550c2cc8c38724df0d3936cf891c871d9488
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:55:05 +0000

    chat best worth giveback claim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
