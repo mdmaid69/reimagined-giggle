a7194f86f0fe439d7a874ddb716c521941f61aa1
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:51:54 +0000

    content possession blame connected drink

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
