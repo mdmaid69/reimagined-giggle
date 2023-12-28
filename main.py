07965cfecd6e665f91ad476bbe896393dfb09825
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:34:40 +0000

    throw stone claim drink responsibility

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
