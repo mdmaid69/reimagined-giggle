70d41220d615316fa1829a253e4c187fa6fdf58b
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:59:02 +0000

    load instead bad miss Gg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
