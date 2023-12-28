cc70f39909443c87e6864599eccf351f35fa64b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:45:23 +0000

    concerning human tomorrow bush cool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
