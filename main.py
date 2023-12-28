718acbf894eefb228b44dd34667c6465a1ed95cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:42:52 +0000

    mysterious change plan welcome fortunate

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
