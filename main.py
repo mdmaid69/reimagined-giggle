936336dfdc39f38d73f1acf76efcb77e5f46bbf9
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:45:29 +0000

    prove rob concerning possibility actual

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-from collections import Counter
-print(Counter("hello world"))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
