f13ba0e8ef371d233ad869fc538dd877ad3f19f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:07:25 +0000

    tour exact spray during scrape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
