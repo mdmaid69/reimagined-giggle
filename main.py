160bc9580cf1286a92b433f110d4ee3a7112037b
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:40:03 +0000

    load ours shape effect however

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
