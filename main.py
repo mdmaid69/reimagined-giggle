6fafac65e2fa69b20445dc013bed120a09b0a27d
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:13:18 +0000

    especially difficult wet means destroy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
