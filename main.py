c10d45aaa669d8f41c78fc3c8a9013a98c39c96c
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:28:11 +0000

    shoulder welcome independent direction freedom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
