d492a95423615518632825a091953c47a04cf777
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:14:00 +0000

    we consider runinto pool official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
