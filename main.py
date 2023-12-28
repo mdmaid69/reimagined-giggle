34f29a7e9d76bdf0a5b8ce78c2690d181993bd85
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:28:24 +0000

    inquire suffer supply trip innocent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+print(math.pi)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
