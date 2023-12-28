37d36425ed63e8cc5d96cb61df2f6b00f3190090
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:18:39 +0000

    patient influence that sore section

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_item(array, i):
-        return array[i]
+def is_palindrome(s):
+        return s == s[::-1]
+  def square_number(x):
+        return x**2
