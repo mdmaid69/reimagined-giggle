4a781742ea885681d00f81597dc2e291ee949fd0
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:21:50 +0000

    better big sale pour number

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_area_rectangle(l, w):
+        return l * w
