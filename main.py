acc141cffa8a8ebcd12afb9e0f637f9027b73c2c
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:24:06 +0000

    finish wrap date gift real

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
