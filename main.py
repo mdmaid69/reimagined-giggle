defa5b822e77099a969e08dde2e54e7dd6479a97
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:52:44 +0000

    wet present tired little visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
