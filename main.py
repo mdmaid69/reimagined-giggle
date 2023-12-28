39ba79a93dd06e6e0eb4db52ad2134aa0b199b0b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:48:20 +0000

    female pleased process bang side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Uppercase:", text.upper())
