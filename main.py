ca829d3f5bad53ff10ca08904a61f5f7de7b8733
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:39:42 +0000

    combine consult price second principle

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
