fa4f10b6628e217376bb7e7432c07b58973e1ef4
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:52:27 +0000

    seriously professional possible important physical

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  def is_odd(n):
+        return n % 2 != 0
