e8813a1453fcee624304eabb6f06186705dc60d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:15:19 +0000

    practical around vote intend material

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
