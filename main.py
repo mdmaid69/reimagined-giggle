85288671d546c618b1eeb201037508293762a7ff
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:12:21 +0000

    service after outside joke risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
