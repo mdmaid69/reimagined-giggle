9ee7ab3ea77191e098bad245003222f4dd77296f
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:57:16 +0000

    spare lot leave meanwhile search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
