4b4cefb1d4d1dcda7f98325764f8fda749c3a298
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:23:54 +0000

    watch worst blow recommend minor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
