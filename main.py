da8a1e734daf2e0ab400fd3428ca13813db5bd0f
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:49:28 +0000

    through sense cheat habit muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
 import random
 def generate_random_number(start, end):
         return random.randint(start, end)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
