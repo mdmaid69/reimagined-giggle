efd238765d0aa451f21c09a59699b0b40d56c665
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:42:24 +0000

    medium software aware serve among

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_int(array):
-        return int(array[0])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def create_queue():
+        return collections.deque()
