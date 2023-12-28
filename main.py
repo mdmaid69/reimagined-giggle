ca4022b53707ca5ee13d38c5859869bd5d18af10
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:16:08 +0000

    such practical surface uncomfortable beside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import time
+print(time.time())
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
