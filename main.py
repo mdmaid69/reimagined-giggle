d0e9cc1837bf307a92974572ccf6e19d6683554a
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:23:26 +0000

    home wheel big married habit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def reverse_array(array):
+        array.reverse()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
