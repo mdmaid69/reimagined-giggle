3029703df829399579f61afeb25d50de68b2718d
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:55:28 +0000

    far rate brush post partner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def add_numbers(x, y):
+        return x + y
