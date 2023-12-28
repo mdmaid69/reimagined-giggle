a9f57c7bd8345c3a45626a120cbaaa77e5cdd26a
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:12:30 +0000

    rise connected really hope once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import sys
+def exit_program():
+        sys.exit()
