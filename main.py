227652bac0fa558a87d10c5d654b4684973cecab
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:49:21 +0000

    kind get sell bored train

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
