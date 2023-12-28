b1b62c336bf72b8bd64f80c5be0245855e27dc3e
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:14:53 +0000

    flight cope birth method separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def split_path(path):
+        return os.path.split(path)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
