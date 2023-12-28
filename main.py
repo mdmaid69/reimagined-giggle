311347074108f46ac8960cebd378244c213aef2e
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:00:02 +0000

    good judge nurse Kk achievement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
