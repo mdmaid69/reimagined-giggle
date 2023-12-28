de80bdf47e512a0f6f59279eb999f7b56b0590ef
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:04:35 +0000

    professional score noisy whisper life

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
