c68e41850fcc5cfbf202b4c2c29cb0b924824625
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:47:36 +0000

    explanation declare provide watch pair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import os
+print(os.getcwd())
