34aa1569b16b5e4305ec2660df0d28d00a255b83
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:42:39 +0000

    pot busy aware inside except

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
