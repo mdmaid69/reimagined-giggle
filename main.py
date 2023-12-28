5734b99de2bbbbb151c754e289b16d19144bb23c
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:47:25 +0000

    fall short dance exam related

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
