112c7cb975a52dbd96e6b2d99d1603dc8245069a
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:40:12 +0000

    couple knowledge comment activity Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_item(array, i):
+        return array[i]
