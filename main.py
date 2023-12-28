c5956791c6c2aff77ded89cdfe7305d23f2bfa76
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:06:41 +0000

    home sleep risk its knowledge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_work(force, distance):
+        return force * distance
+def convert_to_binary(n):
+        return bin(n)
