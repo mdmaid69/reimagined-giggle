3f4f03bb71f451cadc54c5c165365f9f598fee4e
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:13:53 +0000

    onto accidental tell lookup confusing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
