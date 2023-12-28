37160a435690a63e46b99885094181b0bc704bf2
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:33:19 +0000

    close definite Mm moral hurt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
