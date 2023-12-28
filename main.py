6c6eeaca9226cf31ed1663e30ac20fc7c229a56c
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:26:39 +0000

    pull bridge travel lock tongue

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  def cube_number(x):
+        return x**3
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
