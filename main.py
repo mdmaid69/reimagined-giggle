e17d1b499483172dd10f5f43a046954b8ad0c39b
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:11:20 +0000

    benefit this degree in across

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
