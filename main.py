b713511afccaa7e2b71b338d5ef2bef5efd89fc8
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:38:53 +0000

    notice compete Yy murder but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def get_array_item(array, i):
+        return array[i]
