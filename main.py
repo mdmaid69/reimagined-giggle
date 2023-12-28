58a4623428d23792f5376103e228331d986949c1
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:10:58 +0000

    community bag tall visit apply

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
