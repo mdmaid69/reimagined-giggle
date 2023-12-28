fb57a4d206395e9666affb0c1977cbcf9ca12893
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:57:41 +0000

    film divide rarely doctor invitation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  def convert_to_octal(n):
+        return oct(n)
