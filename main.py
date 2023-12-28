a419dd185bdd5429e9f9702ca535b8fa6a3df23f
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:42:25 +0000

    home race Uu perhaps check

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
