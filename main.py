02020925266cec0a0535dfeef77c9431acae6ffc
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:20:31 +0000

    tape feel precise conclusion write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_hex(n):
+        return hex(n)
