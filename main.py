2ea45f8f3ff09e1714017da7b58f3cee434e18c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:54:58 +0000

    different herself wander usual underneath

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import json
+print(json.dumps({"name": "John", "age": 30}))
