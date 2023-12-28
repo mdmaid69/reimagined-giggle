51cc67321302af578c8847305bf64e8ff462b53e
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:10:35 +0000

    squeeze grand fortunately cough pleasure

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
