faecbf13060d1c691243148f9fbbeb64c227ad45
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:13:18 +0000

    less lunch priority organization general

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import time
+def get_time_since_epoch():
+        return time.time()
