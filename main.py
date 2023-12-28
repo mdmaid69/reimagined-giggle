9395d335cd3c76f34a54d20e59d416c7e9b09229
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:37:40 +0000

    before concern impressive passage measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  def reverse_list(lst):
-        return lst[::-1]
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
