88216d4517269ce4b80f4d45a8fe421a9a20214f
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:56:39 +0000

    reduce might talkinto while rare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
