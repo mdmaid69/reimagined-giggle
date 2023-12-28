ca6ff28c75ec78694114055ce3b362ca45b6fea4
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:25:12 +0000

    film Hh imagination big it

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def clear_array(array):
+        array *= 0
