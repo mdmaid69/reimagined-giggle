9c0872d8f8ca16fdf52ea971d962f88429e2dc70
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:18:40 +0000

    runaway past support sign holdout

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
