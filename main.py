fd1480dddf275d85937adaa1aac4753fd7b35cfe
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:40:22 +0000

    laugh knock spread harm difficult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
