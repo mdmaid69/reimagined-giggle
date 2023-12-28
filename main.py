357894636340def7f9dc3eba878313c1c9a6a6d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:49:09 +0000

    by getout imagination read recent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
