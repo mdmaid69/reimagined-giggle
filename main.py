fe5a358b6419749556811ec1b9aa9a050722081a
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:59:03 +0000

    instead wish short own keepup

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+x = 10
+y = 20
+print("Sum:", x + y)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
