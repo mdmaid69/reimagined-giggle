9dca5b6876b06d008ee238917c6262cfa692e768
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:30:25 +0000

    warn near welcome independent welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
