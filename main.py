2520453b2a179a415f30f654066d6fa210002425
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:08:59 +0000

    belong case trip split class

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import getpass
+def get_username():
+        return getpass.getuser()
