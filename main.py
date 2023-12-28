3692617adebe16941f2c8635e05266f72e306044
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:12:59 +0000

    such concentration necessary authority supply

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def subtract_numbers(x, y):
+        return x - y
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
