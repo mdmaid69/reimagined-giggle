a932d001d0f3692552678279ae2a6bf91a9dba97
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:30:21 +0000

    skin vote terrible bet sign

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
