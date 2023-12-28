8bc33cc157512d3b527d470a88bb33b2a701d84c
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:15:23 +0000

    seat plan ahead concerned tidy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
