a8b505b3dfea3c4e6b3a6aa4f046ce1686a437ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:56:31 +0000

    deep congratulations modern form cause

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
