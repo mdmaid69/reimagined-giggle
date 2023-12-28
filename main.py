ddc680777229c1c8da698ceb77a2d52503788057
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:41:54 +0000

    impressed 564encourage child bang it

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+for i in range(5):
+        print(i)
