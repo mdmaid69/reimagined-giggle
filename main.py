bfe1aef57b8ab3fb492ec085a418c61c1645c79a
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:28:42 +0000

    owe ours but song smoke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
