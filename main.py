57493a4a18fe5531e9a0d4e6bc9af8a2d057676e
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:03:55 +0000

    count praise unable intend shoot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
