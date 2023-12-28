98f72b3e3ef9931a585e5767a491eb8fd94a532f
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:17:23 +0000

    accept confuse frightened announce disappointed

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import platform
+def get_python_version():
+        return platform.python_version()
