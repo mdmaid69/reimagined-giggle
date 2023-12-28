099c8aafc030f967fbb1928ffa671d99b710c418
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:16:59 +0000

    problem innocent consult nor possibly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
