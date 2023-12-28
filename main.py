7374d456092ebe7b19d212b1de9938b730711181
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:31:53 +0000

    responsible guard wave nose wrong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def get_array_as_str(array):
+        return str(array)
+def subtract_numbers(x, y):
+        return x - y
