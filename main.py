1f28660f39835a4702674ffd7f84efbe40a01a83
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:04:33 +0000

    file promise left project excited

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
