c7030ba13fa1bdb3fbf75093a64858041819762a
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:53:45 +0000

    pull pen sink alive hungry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
