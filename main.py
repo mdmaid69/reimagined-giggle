becf0b654b03ff95cd1201da5681b87e5a22d194
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:06:39 +0000

    content place thank return return

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
