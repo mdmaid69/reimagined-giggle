acdca268bb5adb52af918130ba46a9516d7c5938
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:21:47 +0000

    sink frightened which attract famous

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
