d93f4145b2e583f7e8c00cee5642d7cab20cc6f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:56:53 +0000

    from little respect tune during

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
