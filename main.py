4875aa25e0b0d91328658434e067db8b0c88f160
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:56:10 +0000

    everyone cut heat brush beautiful

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-print("Hello, world!")
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
