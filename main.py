4f41e7210b8ac1a9203d2acb6bf1439bb260978a
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:37:57 +0000

    cover late sight Itself star

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
