6ab1d84aa1f4815d17a19e94cfd4a07b883feab5
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:27:38 +0000

    thinkof wing feeling defeat lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
