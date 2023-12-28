4804c879c2f03295a3696b84ba9d8ecdef11d048
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:36:33 +0000

    mine food example crash content

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import re
+print(re.match("h.*o", "hello world"))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
