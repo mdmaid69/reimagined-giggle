5cb9c13649a6f3084d327313c61d4e4154edf0a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:40:11 +0000

    deliberate trade breathe tread across

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-from collections import Counter
-print(Counter("hello world"))
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
