034e0252b173d1391a8ead7a2a973788448db55a
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:40:24 +0000

    close same pin weather big

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
