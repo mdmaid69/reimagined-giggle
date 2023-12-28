29eec9bbeb6eac15145a3d99cd94a7688a9f2b10
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:35:48 +0000

    appear approve busy language forgive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_list(array):
+        return list(array)
