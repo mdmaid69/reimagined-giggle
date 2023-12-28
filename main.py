b295f9cf50a18c970b66a73e92790251f34928b6
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:18:29 +0000

    unpleasant stand succeed describe sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
