b3f9dd6588a3f6ed349cd98abe4cd326930fe9ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:49:46 +0000

    attention across turnoff healthy law

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
