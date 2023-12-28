33d5f7148dc351a560a45cd1eab33655e7a2ca58
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:11:15 +0000

    direct business card continue scale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
