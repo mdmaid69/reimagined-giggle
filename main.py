aad450a6d04210c56ef271a2bd08e8a544b66745
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:22:03 +0000

    character mail strong relief stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
+  def get_file_inode(file_name):
         return os.stat(file_name).st_ino
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
