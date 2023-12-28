848973f40de54f980618cb89616854e3b7ce42ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:37:21 +0000

    fat would couple track turnup

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import os
-print(os.getcwd())
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
