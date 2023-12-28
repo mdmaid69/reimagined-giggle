3f3585ba7da32c7733304fb30d68f4b9f5374ad4
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:21:06 +0000

    lock rely onto promise backwards

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
