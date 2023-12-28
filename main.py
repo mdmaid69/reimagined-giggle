7b9f3243b5c6d0e5747daa357f1699b24f07f302
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:03:48 +0000

    regular branch Ii against answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Uppercase:", text.upper())
