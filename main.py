77f9a1474ab6bf21873235d231578a4cad5be5c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:43:07 +0000

    bang defend occur similar attempt

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
