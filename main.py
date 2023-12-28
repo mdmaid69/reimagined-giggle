da6461badce3262587530bd38924e0a445fa2fc6
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:01:00 +0000

    inside suffer pick expensive pair

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
