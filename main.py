a70c2d3ca497b833573c777e77f3821a7aecc382
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:44:57 +0000

    pump relation brain closed plain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
