e093142b8f2fd1993466460c1f719866c15b6b7d
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:41:24 +0000

    jump miss thin distance human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
