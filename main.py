d20aede4a1069645b1ac7126adada642432cbdc7
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:29:14 +0000

    below mail goon copy leave

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
