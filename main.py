f103b0b9a7d1ffa027605086c37935579761af6a
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:44:23 +0000

    explain worst protest centimetre loan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_base_name(path):
+        return os.path.basename(path)
