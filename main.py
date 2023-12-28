f2d41260d516df46aedfd06a5e904c95962a3590
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:56:28 +0000

    prove unless pay uponon secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
