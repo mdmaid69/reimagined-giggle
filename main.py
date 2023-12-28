73a0487e531d6791e610366cdf30db9234534657
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:31:43 +0000

    afraid although same long means

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
