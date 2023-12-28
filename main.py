7df22f9f2a4ee04215a6e6c2b275c11580380d63
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:54:44 +0000

    university surprise defend mystery borrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
