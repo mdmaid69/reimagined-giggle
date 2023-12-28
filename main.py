7b36916b77fe240353e61b8a256cf4505302153c
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:48:42 +0000

    bored anybody control ball proper

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def is_odd(n):
-        return n % 2 != 0
+import collections
+def create_queue():
+        return collections.deque()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
