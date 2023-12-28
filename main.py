ffe3a6381d426c10a3c22a9fb4e4ba056389b966
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:34:19 +0000

    pleasant interesting heat pin improve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
