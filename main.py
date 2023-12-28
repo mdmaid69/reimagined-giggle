89a9add9cb4df0a0e1e9ec94d6fed34e27f4bba1
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:01:37 +0000

    extremely fly form close broken

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
