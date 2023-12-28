551a10de84e1f875eabaec0018689a30c79d7189
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:49:43 +0000

    injury offence all never child

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
