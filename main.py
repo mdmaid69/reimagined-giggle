f96b2b1871087a6d4711004540dca516e4cef1cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:57:05 +0000

    basically unlikely ourselves acceptable name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
