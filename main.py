3d206e7841bd1d9d8592f084aca3e36a48be45b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:25:47 +0000

    rob getaway sorry this empty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
