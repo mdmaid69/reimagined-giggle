4587e5aab35759e393a9aedc310171eefa6a5172
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:08:16 +0000

    meaning excuse obtain bend tone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
