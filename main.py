2da0cf206b5b59133d3e8b98c90fba29be53132c
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:34:52 +0000

    ticket window care his comeover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
