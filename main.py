5b8c892237015440bf2be107b1a1ced97cba342f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:44:38 +0000

    affect grow bar wash uponon

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
