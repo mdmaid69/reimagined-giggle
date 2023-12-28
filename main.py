cb958a98f8ac9f14b83c47cabd38c9331f036754
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:25:30 +0000

    search job rank smile operation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def find_max(lst):
+        return max(lst)
