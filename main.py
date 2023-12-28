fcc75a1f0c152de4c52cf1b6e6245366815784a1
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:42:43 +0000

    later partner year bend appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def add_numbers(x, y):
+        return x + y
