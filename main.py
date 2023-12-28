76541ff71bc8ef22d58e1424047a450319970bf6
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:05:28 +0000

    past unless cheque style consider

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
