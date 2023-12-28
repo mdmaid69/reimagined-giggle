3e684f4f12a46fc5b2d548c6d817e91dc95c3101
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:44:33 +0000

    go rescue workupto guess 179bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import sys
 def exit_program():
         sys.exit()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
