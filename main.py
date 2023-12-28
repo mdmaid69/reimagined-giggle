22919e67fcd82ce662342c651fe3ee015c132a42
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:46:40 +0000

    mark left front Yy boat

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import re
-print(re.match("h.*o", "hello world"))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def get_array_itemsize(array):
+        return array.itemsize
