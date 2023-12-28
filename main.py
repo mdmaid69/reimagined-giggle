0aa72200d23e38be9dab544a00c33206e094d0de
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:03:48 +0000

    hit drink debt sale twist

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
