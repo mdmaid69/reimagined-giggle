6e4b8e8e06e67553755565039e8d2c512030ed30
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:36:26 +0000

    solve spare home order declaration

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_string_from_array(array):
+        return array.tobytes()
