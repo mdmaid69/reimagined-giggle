909cd65b83db2ed585ecbdfaa369bdb1b3e3a92f
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:52:18 +0000

    ago spin minimum value taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
