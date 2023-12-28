f65e1984c621103d9c8aa3b1427e5ec4fbd7630b
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:02:35 +0000

    tip angry offence takeoff future

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
