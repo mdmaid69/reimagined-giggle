ee7484505ceb072b4e92b5c3eef5b38a84197a1b
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:28:44 +0000

    rain top neck cut once

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
