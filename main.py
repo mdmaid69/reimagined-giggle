5ac9b483aa2dbd1c2d04408d6f76e879549976d5
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:03:13 +0000

    rescue sort little service really

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
