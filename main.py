619d1c7960237a9a2023996b86c9e1171c593f8d
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:38:58 +0000

    Mm compare team there square

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def square_number(x):
-        return x**2
+import platform
+def get_python_version():
+        return platform.python_version()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
