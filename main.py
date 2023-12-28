d7532ea9cfd9bd9cf18210f3324536468c260662
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:48:58 +0000

    share fairly protest front loose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_acceleration(speed, time):
+        return speed / time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
