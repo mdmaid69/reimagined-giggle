85a68b3a56c1f501b34c10d3b7d7828d90ed7244
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:21:46 +0000

    recently related fortunately mess communicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_palindrome(s):
+        return s == s[::-1]
