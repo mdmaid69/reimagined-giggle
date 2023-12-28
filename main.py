f40b1d272cbce7466d14fdbaec94c2ac2546b890
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:47:30 +0000

    snow bringback hour also file

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+x = 10
+y = 20
+print("Sum:", x + y)
