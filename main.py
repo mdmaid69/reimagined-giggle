d869aeb77228d675e5938a42771893bfa572b51f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:27:59 +0000

    hers pretend bringback steep Hh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import sys
-def print_python_version():
-        print(sys.version)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def convert_to_hex(n):
+        return hex(n)
