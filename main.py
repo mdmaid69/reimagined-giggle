e82a3d367e747f95d44a1ccca751436014292c0d
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:59:24 +0000

    university expect name fetch ignore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
