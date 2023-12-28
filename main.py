a0aac86ece8ff79a1f3e05b4602b0a420bddaaa6
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:18:03 +0000

    Yy safe separate organize fit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def is_palindrome(s):
-        return s == s[::-1]
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
