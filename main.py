d85b04cf520c5e65da610ae5ca5609eab1134ac3
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:58:58 +0000

    various nor fly sound custom

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_array_as_list(array):
-        return list(array)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
