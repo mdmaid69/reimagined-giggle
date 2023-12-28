959ad78db105b24449dbc3e8f0a2d5f9330bee64
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:06:44 +0000

    attractive scrap statement bunch experiment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def find_min(numbers):
+        return min(numbers)
