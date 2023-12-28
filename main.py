bfbd174f59eb39362bb1be1bea06c4f9790eebc6
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:00:50 +0000

    safe scale recommend actually food

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_ino(file_name):
+  def get_file_inode(file_name):
         return os.stat(file_name).st_ino
