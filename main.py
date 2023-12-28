07cef7b5a70a2322fb8d8717d807f21adc890036
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:23:24 +0000

    apart join popular forgive bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-name = "Python"
-print("Hello,", name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
