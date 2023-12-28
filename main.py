e85090c7d7a64a14c0b3d9c57c578485b1b787c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:35:53 +0000

    knowledge accidentally respect crazy different

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
