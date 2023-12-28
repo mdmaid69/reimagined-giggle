43fc50994e045ee54fa0db358db98ba8d7284738
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:20:42 +0000

    sort term thumb ring remove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
