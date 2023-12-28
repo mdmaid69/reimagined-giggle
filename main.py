94cd75a77401f1546248ae0c4179b40f729cb7c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:07:44 +0000

    sing curious rain flat tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_length(array):
-        return len(array)
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
