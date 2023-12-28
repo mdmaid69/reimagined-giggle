0582c3754c1bc35f3217aa580c3f728152635f7b
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:49:38 +0000

    sort mark report straight border

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
