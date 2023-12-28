95495312c2a1df0150b27a2e22bee8e4dcd5a367
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:31:39 +0000

    clean sore vote ring suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def convert_to_octal(n):
+        return oct(n)
