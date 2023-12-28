f6aa0382a5923911ff465ba0ddaea369d0828908
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:30:17 +0000

    inquire outside once throw bottom

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+text = "Hello, world!"
+print("Characters:", len(text))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
