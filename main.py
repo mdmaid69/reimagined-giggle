fd5414852b2ca2f5bed3dbb14da6fa96ce1b7ca5
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:17:18 +0000

    build bunch impossible hollow blue

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_floor(x):
+        return math.floor(x)
