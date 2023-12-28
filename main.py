6758ad4a3f0490084e57f33201e06cc4e138a3a1
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:55:23 +0000

    rob note sentence foot urgent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
