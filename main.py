d80cd10192db6c7a072ac558fc3e3aae7840aa07
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:39:55 +0000

    meeting turndown cool moral for

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def multiply_numbers(x, y):
+        return x * y
