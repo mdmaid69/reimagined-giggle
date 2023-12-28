d840c88281856fcc9048da55c687f72f94a64bd6
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:30:12 +0000

    mysterious ill Rr needle there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def reverse_list(lst):
+        return lst[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
