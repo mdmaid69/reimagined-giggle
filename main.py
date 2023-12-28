be3a77a16d2187c2558a2cc10b296762c2d5b223
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:25:11 +0000

    pick out safety accept prevent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import sys
+def print_python_version():
+        return sys.version
