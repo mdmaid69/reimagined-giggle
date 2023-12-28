19d0a0dab5bebf06f966e4428875bc643b29bc2a
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:54:08 +0000

    but even spray legal fit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import datetime
+def get_today_date():
+        return datetime.date.today()
