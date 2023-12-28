c90d99832fe5f4f147457c25ec6a9d170f7c264d
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:14:20 +0000

    equal separate change never much

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
