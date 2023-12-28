1933770e2a0a00bc8900dee840643a854474c68e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:44:06 +0000

    gate stop surface decision arrangement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
