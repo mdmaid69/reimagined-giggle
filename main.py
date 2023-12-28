27550783759af652718b7f271fa73570c9830b38
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:13:11 +0000

    middle nurse quick soon rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
