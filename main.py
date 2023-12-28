8cbbd420fb5bf1d7a5adf262e40599289f7b6cde
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:54:54 +0000

    something shout whether admire thanks

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+print([x**2 for x in range(10)])
