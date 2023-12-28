d2060aac51674fe7344f0f6062ead0e0f25dae1e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:11:59 +0000

    speak tidy skirt hear geton

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
