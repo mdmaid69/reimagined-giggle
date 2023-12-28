d2b9ccd17fcd0e94334c34343543667987dd5dfc
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:18:26 +0000

    blame play pass start soak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def remove_duplicates(lst):
+        return list(set(lst))
