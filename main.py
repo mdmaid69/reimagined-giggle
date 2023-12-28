6d52e771fa000b573fd473a231c53524982fe3b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:12:11 +0000

    importance consider opposite late to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
