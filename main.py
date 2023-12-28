5297b22252e20173108f2be2fd970184dcc126af
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:40:07 +0000

    but point recover face bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
