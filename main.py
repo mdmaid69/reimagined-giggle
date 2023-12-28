b4e560fc6479034e35d34d0b5dc59f1a76b3a674
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:12:55 +0000

    weekend issue if suck model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
