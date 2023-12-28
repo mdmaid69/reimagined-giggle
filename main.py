acb743ba5577801c8ce6d7ab64cd55f037bfdb06
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:09:44 +0000

    immediate remove breath practise like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import time
+print(time.time())
