3658ee0c910cfcf973be7889300aca41df655462
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:43:25 +0000

    announce town lunch zero herself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
