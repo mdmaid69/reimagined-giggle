43fd1b5def690574443cd2553b37f9d2c9c78239
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:33:27 +0000

    567enemy pleased aim shock basic

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_octal(n):
+        return oct(n)
