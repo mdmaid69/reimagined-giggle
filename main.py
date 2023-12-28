6f1e813e3faa484f916bf3534fec71e26ff60dc2
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:14:56 +0000

    comeup treat Ii recovery difficult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
