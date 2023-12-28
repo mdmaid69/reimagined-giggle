044014d5842d257ae19822385968d80316b2580a
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:43:53 +0000

    aware normal convenient violence birth

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def convert_to_octal(n):
-        return oct(n)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
