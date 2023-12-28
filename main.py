f165cdd06b86e7201b5da2fe4f80a35f18b6129e
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:29:58 +0000

    cry chase tone heat 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
