0d37c8634bb142e950eb55830dc2f2e2576e4ed7
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:18:37 +0000

    translate club university schedule search

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
