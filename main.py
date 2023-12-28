d749315ee473c94fd2dbde5d842b8fef0a783901
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:40:24 +0000

    country could maintain question spring

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
