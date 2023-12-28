237b624d97b0d154da397ac4484b27d7f95b0b0c
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:36:33 +0000

    peaceful any out become runaway

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import re
-print(re.match("h.*o", "hello world"))
+  def calculate_area_rectangle(l, w):
+        return l * w
+import sys
+def exit_program():
+        sys.exit()
