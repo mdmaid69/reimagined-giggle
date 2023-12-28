5ee5f396d8359aea95433ece72be82c1a0fd4605
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:46:31 +0000

    pull experiment tragedy care talk

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import sys
-print(sys.version)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
