a1bd5a912e226df2ee74f7530036872db8f7eb82
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:25:55 +0000

    wheel autumn pressure cum meanwhile

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
 import datetime
 def get_days_until_next_year():
         next_year = datetime.date.today().year + 1
         next_new_year = datetime.date(next_year, 1, 1)
         return (next_new_year - datetime.date.today()).days
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
