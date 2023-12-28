43ae5b7eb5e57af32983874eb7752513db477c22
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:28:38 +0000

    demand remark turnover pot press

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
+        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
