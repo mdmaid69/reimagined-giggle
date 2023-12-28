ab34dee118641d87fd424f6df1478c1adab004d0
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:58:06 +0000

    may bend practical action drink

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def is_odd(n):
-        return n % 2 != 0
+def cube_number(x):
+        return x**3
+def calculate_average(lst):
+        return sum(lst) / len(lst)
