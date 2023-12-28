a8f24ff534dd35bea98f420a3bbb15d508cf8695
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:58:37 +0000

    realize can unpleasant black paper

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+for i in range(5):
+        print(i)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
