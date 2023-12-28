ecce5cc3f63332d216850ac9d47504d9b8e13efa
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:53:26 +0000

    condition year wave onto whole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
