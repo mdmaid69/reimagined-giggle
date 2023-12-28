e6996b84da04a74139fcc4ad8b854d0a9d387b72
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:59:08 +0000

    payment tell tired object equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def remove_duplicates(lst):
+        return list(set(lst))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
