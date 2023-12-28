c4eb8fbab2b5b4136d2c7a83d1109ebd314148ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:52:39 +0000

    thick together off brother law

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
