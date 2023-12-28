081fe0d73f8d6fe62cf9f06bea468d3a94b2ac5d
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:24:56 +0000

    light shallow waste spread seat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def extend_array(array, iterable):
+        array.extend(iterable)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
