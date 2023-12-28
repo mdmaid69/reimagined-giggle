f9edd3abdfe54c140ea386ff09e2c9b903eda520
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:13:11 +0000

    edge first call judge earth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
