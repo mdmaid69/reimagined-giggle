7b181c4c1c664b88675b65e6b21dadb95fe092e2
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:49:35 +0000

    wave calm use slice spot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print([x**2 for x in range(10)])
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def count_words(sentence):
+        return len(sentence.split())
