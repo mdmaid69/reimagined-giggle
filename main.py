1bf241b2b63b13ad2b079b3f2959d617d1126b72
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:29:41 +0000

    thankyou something cheek wash exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
