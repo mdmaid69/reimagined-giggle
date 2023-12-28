dd4def75c43326669aeff951726d669d5107ba6d
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:01:34 +0000

    sea fall label task project

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def subtract_numbers(x, y):
+        return x - y
