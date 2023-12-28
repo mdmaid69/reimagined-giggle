2fed3e069ad08a092156b80f785dd69b3df97113
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:14:35 +0000

    continue attitude makeup approve train

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def sort_list(lst):
+        return sorted(lst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
