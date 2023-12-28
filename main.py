fbec4658a732d69b5bf32b379717571a42b8cb9b
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:21:01 +0000

    issue dick payment hear exist

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
