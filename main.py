423a74dc0ac81078c4db5f38d1503df43b121ea9
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:34:42 +0000

    sad cool joke giveout string

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
