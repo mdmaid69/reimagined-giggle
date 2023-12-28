af71222ec713caf26e1b31519d3dc04a4ba5cc40
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:37:55 +0000

    relief hold among average consist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
