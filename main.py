abcc40b63295b696d47183407ac678cf1595f859
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:32:57 +0000

    sort finger or moon match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_acceleration(speed, time):
-        return speed / time
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
