55bb9e221da6af11fbb01ca658606cfd31b5f6da
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:08:02 +0000

    funny pronounce sorry crowded table

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
