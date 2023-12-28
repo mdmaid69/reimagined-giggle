0c3c87b539b99bca9370fb43d9224a28a575a711
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:41:10 +0000

    later keepaway clue support rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import collections
+def create_user_list():
+        return collections.UserList()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
