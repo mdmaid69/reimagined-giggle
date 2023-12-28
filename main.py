e0beae1e52a8f683803dec47d5f0441b75679eb6
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:32:01 +0000

    expression pin dress stranger responsible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def cube_number(x):
+        return x**3
