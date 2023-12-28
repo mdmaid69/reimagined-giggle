2b96df3f1c621d4c9e376e4a57d9921816af3e8b
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:51:25 +0000

    connect block pronounce when among

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
