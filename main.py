29ffa0a7973e90908ee4bfab2bec1b31a1e2d353
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:07:09 +0000

    nature door front cost yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
