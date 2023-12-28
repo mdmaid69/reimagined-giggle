c7f23f96c351083bceeaf875c846b245159281e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:55:22 +0000

    press bread region prepare entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
