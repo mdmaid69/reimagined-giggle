3bb55dc7898a13eab2c81969eeb32852a8f462dd
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:45:19 +0000

    everybody appeal port backwards murder

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
