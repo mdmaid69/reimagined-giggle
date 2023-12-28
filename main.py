aab406543877226b56cd1591619586fd37112d6d
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:50:13 +0000

    correct advise base save worse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  def remove_duplicates(lst):
+        return list(set(lst))
