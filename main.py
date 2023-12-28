8927a92188521689858a5b8a7939786bb5f8230d
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:49:26 +0000

    who study tidy price silent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
