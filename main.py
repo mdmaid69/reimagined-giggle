910b04f7988de8c5fe4eb9676f10f9f8f5de6635
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:11:13 +0000

    exact prepare shoulder wish content

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def clear_array(array):
-        array *= 0
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
