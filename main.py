de039721637aa7572535186912f77260e453bd51
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:12:28 +0000

    slowly young she unable fault

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
