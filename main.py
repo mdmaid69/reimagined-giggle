e7dd3eee1c5e8f71c84875cefe47463accc0658b
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:44:21 +0000

    interrupt tread poor generous seat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
