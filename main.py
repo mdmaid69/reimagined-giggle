f702d9463cb1efd9fa33684882f4b15b08689c11
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:27:11 +0000

    add dance scrap steam problem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_stack():
+        return collections.deque()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
