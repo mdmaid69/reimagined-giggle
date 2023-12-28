22ba3f5f00e63b4fd37fcfdd89319769403102b5
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:16:20 +0000

    so there excellent Ee period

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
