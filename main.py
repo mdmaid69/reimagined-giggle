fb5dc57528573fb29dd4327d9cfd4ef33678c292
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:54:56 +0000

    condition check disturb conversation notice

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
