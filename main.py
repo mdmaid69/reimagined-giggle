65b40da634f0cbb79992795ff255a879763c9276
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:19:38 +0000

    convenient instruction fashion cost downstairs

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
