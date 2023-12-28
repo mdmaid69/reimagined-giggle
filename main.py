8d9aa5bb6dedbfe338e82f1affb89d005104cdfd
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:10:35 +0000

    contrast floor produce bye row

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_unique_words(sentence):
+        return set(sentence.split())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
