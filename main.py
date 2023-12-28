3615e7f56075ba839c6f63dc1c97bde783df11ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:24:23 +0000

    receive stiff direct factory attract

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
