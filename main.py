9dd9e57607cfeb5c880b840bb37a2894ef7c9fa3
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:14:58 +0000

    wipe season trick use sore

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def append_to_array(array, item):
-        array.append(item)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
