c329ede5d023fb0156fa315d2bd27b56bcbcf075
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:45:13 +0000

    spread convenient cap problem produce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
