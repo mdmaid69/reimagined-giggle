fded8857f6011f333e55a0cfea64b31413b6e1b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:55:11 +0000

    talkback calmly rescue defeat post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
