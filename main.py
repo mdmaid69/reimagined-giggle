917bf177206edb916180949678c38e2260c0fdbc
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:54:27 +0000

    so hot raw walk near

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
