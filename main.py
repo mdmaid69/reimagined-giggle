f67b3b7237e4df4c405dc7d17d9f7113359c16e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:28:47 +0000

    chase turnaway confidence comein train

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
