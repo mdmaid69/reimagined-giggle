76144d87381fada3f8f371c34a1929d1259c9839
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:51:20 +0000

    city spare scale chance set

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
