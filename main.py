9e3215913ca502d1076069c9be66d1f4dabacea1
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:58:18 +0000

    game callback group form sing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_current_working_directory():
+        return os.getcwd()
+def square_number(x):
+        return x**2
