568cc8ebad8f403a620aef06a3d960f8fe61d81e
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:48:44 +0000

    avoid shut rest so Oo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import sys
+print(sys.version)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
