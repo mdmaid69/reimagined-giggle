e840776b71fbca48ffca50debd9813a6583dacfc
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:48:13 +0000

    protest arrive involve squeeze trap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-for i in range(10): print(i)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
