502bb69aa4fea227de34120fd4ca3c11ee94a1d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:45:07 +0000

    themselves sweep deserve over plate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-for i in range(5):
-        print(i)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def count_characters(sentence):
+        return len(sentence)
