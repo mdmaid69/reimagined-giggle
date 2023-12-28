ec74beaca712fe132d796202a3cbd87b6a2bd45f
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:35:21 +0000

    instrument noone record stop popular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
