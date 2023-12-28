c6c38c942a9fd13ebde04165ca7933a8d1b639bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:57:18 +0000

    exist accidentally person muscle drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
