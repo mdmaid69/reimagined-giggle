577a179079faa7ba1e9dda4d24cfd4eed93c2385
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:39:56 +0000

    great work swim lookaround impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
