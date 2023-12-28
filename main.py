989fcd0f3a3882614ac89dc2b0b0e64ba17ace23
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:47:38 +0000

    else event hurry sign escape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def change_working_directory(path):
+        os.chdir(path)
 import os
-print(os.getcwd())
+def get_current_working_directory():
+        return os.getcwd()
