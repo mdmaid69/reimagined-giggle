06795a7b911d584d684c748d4ad0f886b654cc97
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:36:25 +0000

    rubbish before check stage run

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
