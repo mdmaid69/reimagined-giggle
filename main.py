af1fb56141107ebf42da52a4b6919a808493f5ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:10:59 +0000

    clue thinkthrough contrast other complicated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
