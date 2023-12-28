274607b7d2852b72a236be0dc31fc3ec42567a7d
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:41:28 +0000

    round guide it broad address

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import sys
+def exit_program():
+        sys.exit()
