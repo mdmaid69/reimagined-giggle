6dded8004b0d5a8e86158bb3fe2634a6ee7a026f
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:43:48 +0000

    expensive convenient avoid invitation search

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def divide_numbers(x, y):
+        return x / y
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
