8694905852270adedcb918e6008e204aaf8cb15e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:24:13 +0000

    to sea opposite major swing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import datetime
+print(datetime.datetime.now())
