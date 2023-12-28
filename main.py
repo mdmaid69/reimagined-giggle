7f2b4840f98b7bbe281a7d7a668906db3762ebc1
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:29:36 +0000

    late sentence firm talkback young

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def convert_array_to_list(array):
+        return array.tolist()
