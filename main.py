0ced9dd6dc9c05a8850a54c90b963f96ab10c3fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:06:43 +0000

    welcome generally personal violent somewhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import platform
+def get_os_info():
+        return platform.uname()
