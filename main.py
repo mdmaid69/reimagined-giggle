1f67a3d9cabc0b88abff440c1cdf996f89f30f65
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:05:51 +0000

    drive wear company for to

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def divide_numbers(x, y):
-        return x / y
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
