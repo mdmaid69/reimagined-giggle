626fff3f84cf52762c14d92b8d492ce0dec36ff2
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:48:39 +0000

    thing tie promise gate slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
