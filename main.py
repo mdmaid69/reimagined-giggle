e0063d23d2bcd5b5b2355b5385f2a52560ff0b7b
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:15:21 +0000

    fix fit turnover regret attempt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
