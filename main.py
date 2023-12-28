c0778ec4bfa487b194e1f14a5edba9be04362184
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:51:59 +0000

    better OKorokay tradition total sit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
