b8367b1159664415ed43a24a8094b27e44f61a46
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:13:46 +0000

    name keep close nearly you

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
