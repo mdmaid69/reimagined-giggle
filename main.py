0ff12c5e6fb4f31183b6eb964c6970db8f27b880
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:10:52 +0000

    fold thin tread sound remark

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def count_elements(lst):
+        return len(lst)
