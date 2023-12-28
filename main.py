52131bbafcccc8772a606f287a563c969f677832
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:39:16 +0000

    think odd introduce kill song

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
