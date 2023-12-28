7bb12259ec624b5e2f82712ab8c9276a7c8ff53e
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:48:18 +0000

    example thanks from disappear like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
