1e29c081a978abe2df1110bea544827a088ddc9a
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:49:13 +0000

    break upset strength man consult

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
