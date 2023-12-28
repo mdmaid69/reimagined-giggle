a018d04efe3424dc5f7663aa43c49c8b67614d98
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:17:51 +0000

    funny strong comeup sense safety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
