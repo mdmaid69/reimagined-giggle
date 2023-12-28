c4a0ab20c93970f2a6b89295cd8ba6fa04bf1255
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:11:36 +0000

    any stop well emotion start

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def load_json(filename):
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import csv
+def read_csv_file(filename):
         with open(filename, "r") as f:
-        return json.load(f)
+        reader = csv.reader(f)
+        return list(reader)
