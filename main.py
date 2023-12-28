64b9db66dcb6c0860cdb04573e0552a398347316
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:17:44 +0000

    fair outside nowhere post plenty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_max(numbers):
+        return max(numbers)
