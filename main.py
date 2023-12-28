8f24579a6b1dc17b37b3376b78dec58ea66262a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:24:22 +0000

    wide interruption limit disgusting insist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
