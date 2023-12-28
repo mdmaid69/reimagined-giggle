0cceb92262a6b986cfcd74ac3953d8e3464c34f6
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:44:45 +0000

    getoff store please slow feel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
