6155e7fdfabcffa13c211a5af614d3ff7ae430cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:40:05 +0000

    public recently ground big trust

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
