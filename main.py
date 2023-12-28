03e563188d544cc661552d9f814ff32035da9d39
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:13:44 +0000

    bringback language hand runover present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
