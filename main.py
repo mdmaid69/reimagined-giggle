2798ad43f8b7c56f4317e45e26f37c350cc9563f
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:46:38 +0000

    ticket well comedown somehow invite

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
