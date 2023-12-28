060c97af1ecd9cc4b38a104cb9c21aa94cbf1d6d
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:32:26 +0000

    holdout design follow now basis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
