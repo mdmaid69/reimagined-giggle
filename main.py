4aa9afabe5b4d7b9ddfc3e4f111a613ec283ebb1
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:08:16 +0000

    season dull explain doubt attract

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
