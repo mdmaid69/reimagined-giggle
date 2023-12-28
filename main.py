6d41a21964021aac23f61c52e9a8b3216807bcec
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:31:12 +0000

    active us guess block effort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
