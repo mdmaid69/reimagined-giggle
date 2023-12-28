73bdf63b49aa1055ef7eedb308f908a753060651
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:48:41 +0000

    last pile table new friend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import sys
-def exit_program():
-        sys.exit()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import platform
+def get_os_info():
+        return platform.uname()
