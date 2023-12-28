22bb757cbc8c956bdbe9a5bd57c3ea8a2df02e59
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:57:47 +0000

    deep long swimming wind bother

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
