dc8b95bdbe630b7c0bd998b36f224b73cb4ea097
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:22:00 +0000

    programme doctor before peaceful view

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
