392efd358c984e3de67d4ad19aa0cd7a4feee19b
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:52:59 +0000

    challenge explanation company bush repeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
