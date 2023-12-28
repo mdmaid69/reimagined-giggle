480a2127863c5ae8b3b3d960d46589f1dfc73213
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:31:11 +0000

    bush meanwhile guide its possibly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def reverse_string(s):
+        return s[::-1]
