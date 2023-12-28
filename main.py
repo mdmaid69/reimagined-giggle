ae7d330115c1f7982cba4a4840e922691383a9af
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:31:19 +0000

    interfere ahead class anything hair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def sort_list(lst):
+        return sorted(lst)
