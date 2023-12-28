bde0cc1c9a3ce8bebebe49ce69c0be808f105b88
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:30:28 +0000

    when whenever failure phone thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
