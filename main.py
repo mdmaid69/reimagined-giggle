fff0c766710bbce0049051080086d45a10f58fbd
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:11:43 +0000

    order law scream getaway private

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
