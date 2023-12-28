2d35df577d54d0b1e8fe46f595588543eab6e1b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:31:59 +0000

    speech decide goout shall land

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
