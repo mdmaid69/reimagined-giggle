f3fe0f0c30c37bf515583e9e7484472dd32558b9
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:10:43 +0000

    match congratulate sea typical expensive

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def greet(name):
+        print(f"Hello, {name}!")
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
