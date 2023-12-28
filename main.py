0ee81b250e33ba379c631838b6cd9c29aa741a35
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:31:35 +0000

    favour aim careful furniture regard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
