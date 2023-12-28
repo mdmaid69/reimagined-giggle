c6051712a57a60f2859402b9e47eb5ce0ce9798c
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:40:21 +0000

    base forgive walk rent alone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
