6b333dbdd513ce26f6ed1350e4879ccf3e36da6f
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:10:01 +0000

    contact care central classroom many

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
