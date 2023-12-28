e84ff53ceb7711b43c34e6ba8e45258d752934a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:49:25 +0000

    talkoutof charge figure emergency promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
