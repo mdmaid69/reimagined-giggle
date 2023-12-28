c148afac155571f1cb2df53dbda55da68f74e7b0
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:51:09 +0000

    kill remember charge jump Yy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def convert_to_binary(n):
+        return bin(n)
