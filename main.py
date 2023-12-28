c25d72d45421849dded08286171ef70d69605106
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:02:56 +0000

    keepup earth ours custom final

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_typecode(array):
+        return array.typecode
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
