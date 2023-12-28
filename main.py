18287b423ad0aa91734ace008eb59dca1feb48a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:57:18 +0000

    increase leave speed win region

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_dev(file_name):
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
