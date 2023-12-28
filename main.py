9ce14f8c341ba10b12681dd488f3bd0b39e8aa3c
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:45:35 +0000

    ball dare extraordinary unless favour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
