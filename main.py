23d1aaf357cf689610bc357a705f3ca50be7aa00
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:05:47 +0000

    while creature pen tragedy stretch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_int(array):
+        return int(array[0])
