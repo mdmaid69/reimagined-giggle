864b5da30f1f00e3ad4ff251ce1b8d4cbe4cb829
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:59:02 +0000

    phone wish shop need entry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+print(re.match("h.*o", "hello world"))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
