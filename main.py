55689e81cacd5d37ed7b643bf28a022497e2fa89
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:36:01 +0000

    will character bet travel far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
