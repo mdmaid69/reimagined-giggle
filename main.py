5713deff8655b581e6bbeec1adbf8ba7accafec6
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:38:02 +0000

    practice shake since change government

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_work(force, distance):
+        return force * distance
