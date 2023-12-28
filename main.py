190108da084351ee9019f8b72d6d167aa164b001
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:36:12 +0000

    honest typical this slowly impressive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
