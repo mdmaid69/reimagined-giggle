f5467435a689b7811de012bc80f72b95bbc85b80
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:27:52 +0000

    charge crazy tell duty invite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def convert_to_binary(n):
+        return bin(n)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
