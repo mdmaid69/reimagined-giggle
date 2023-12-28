eac4eef5f15b3e2a6a81743a049262423f81f67a
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:20:52 +0000

    place apply middle suit proof

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
