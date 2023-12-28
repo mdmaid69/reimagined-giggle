ad49f9d82b495a5c9136bc93b41754c5a51e7318
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:57:26 +0000

    beginning shadow consult herself nigga

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
