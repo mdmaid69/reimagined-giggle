7ce5b5302f6bed5a4cf478333bb67ff125c132f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:29:03 +0000

    own them deep deliberate loss

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_max(lst):
-        return max(lst)
+def find_min(numbers):
+        return min(numbers)
+def calculate_work(force, distance):
+        return force * distance
