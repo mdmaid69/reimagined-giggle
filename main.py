2f60271cd3bb78e0363f1e5b5e4480d626a9fc9d
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:49:11 +0000

    survive environment protection tone face

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
