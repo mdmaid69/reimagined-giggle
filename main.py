d68ee8c8b4a466d830701259c4d9b94db0165363
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:32:59 +0000

    me offer late ocean Qq

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_hex(n):
+        return hex(n)
