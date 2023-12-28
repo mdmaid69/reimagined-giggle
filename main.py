09d7e67c1a688d41aa3a0b9ddb25f4fbd2d10c91
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:01:35 +0000

    famous over touch they unite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def exit_program():
+        sys.exit()
