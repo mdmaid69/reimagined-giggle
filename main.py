76b3c58abe0f5a5ae207ab6e20e2c80b2bd611ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:21:06 +0000

    present dance damp immediately achievement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
