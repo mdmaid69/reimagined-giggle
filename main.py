ccfc748295fb9d1808da71fb61b720eea9e17a14
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:03:14 +0000

    race wide clear careless bet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def reverse_list(lst):
-        return lst[::-1]
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import tensorflow as tf
+print(tf.__version__)
