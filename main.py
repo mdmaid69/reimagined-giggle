a8a4e42ff0f1a13b26ddfd916b4e58fd78b1b2a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:36:37 +0000

    getup thank floor right protect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
