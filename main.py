bb5d302c895acb7e5bbd0cb642c3c29c2d14a5f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:07:00 +0000

    mood perform calloff usual mouth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
