5d39176ddf06f7b9a6b76d60c9c79ae4f1859e2c
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:34:40 +0000

    risk result pressure related confidence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
