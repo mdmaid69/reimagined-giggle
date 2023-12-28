76a4b4dd0d3614b254a8a65051bc3c9f12534423
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:05:24 +0000

    period complaint breakdown draw hello

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
