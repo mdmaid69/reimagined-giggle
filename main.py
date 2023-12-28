ba5e8677fe4473bf597b8fe497c93de680051c47
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:48:40 +0000

    round schedule object serious fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
