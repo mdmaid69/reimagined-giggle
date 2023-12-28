e9a88dd2913f204f3ed132143d6de7d8d739250e
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:31:13 +0000

    threaten enthusiastic emphasis snow last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
