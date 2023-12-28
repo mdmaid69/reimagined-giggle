35964f90202f8bf6439c570caa905acec956b96f
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:35:22 +0000

    authority turnover defeat damage season

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def sort_numbers(numbers):
+        return sorted(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
