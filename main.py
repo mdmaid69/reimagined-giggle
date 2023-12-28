620f7d2ed9f8aa2f6fce5e57824ac807dc115b28
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:08:19 +0000

    then except train win crime

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
