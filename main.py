6afebfefdbe6b5cac3e3bc70c45edb9b1b1453e8
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:39:11 +0000

    for delight permission record sell

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
