096dbcfff406dd8fdd07331722a0ccdbefa56bfc
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:48:23 +0000

    let sorry that pollution order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
