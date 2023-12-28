b6bfe90d100e3d5f16124cc13cf970dafa298518
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:43:57 +0000

    us test solve spare set

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_acceleration(speed, time):
+        return speed / time
