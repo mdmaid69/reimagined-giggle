76ab015c8a73286c12dd18d48ae1a1f4c797a553
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:57:55 +0000

    region argument price quit accommodation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
