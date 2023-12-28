72c54bee8fb839231a8d7414ca586f9dfd858c3a
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:49:01 +0000

    split reserve mess school major

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
