b4368bc88e68e5d78dd47b0590477580bb64a399
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:44:15 +0000

    reject regional instant future speech

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
