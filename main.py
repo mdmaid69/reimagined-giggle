46d413304189676c9908a3085f3d5f1ebd034936
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:58:39 +0000

    emphasis shoulder majority boat result

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
