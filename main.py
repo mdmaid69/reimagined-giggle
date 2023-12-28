d5d9a364f0265ad0bec9a6429ea3a515beef2d34
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:49:25 +0000

    dull nonsense art anywhere odd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+print(re.match("h.*o", "hello world"))
