fbd9c1f14b1d1a4abcbc54d68ef068be8b25744d
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:25:37 +0000

    bread hole represent sight whenever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
