fe54c8d5083a8930756589842e1f9df6342ab553
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:23:33 +0000

    kick now estimate interview ask

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
