100b2adc85a9b0217d21471832608364e7d21f96
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:41:03 +0000

    tidy because side how please

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
