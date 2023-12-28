f8863fdff7d6422ddc4700927520ca90a444f97e
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:33:07 +0000

    increase knock away hungry decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
