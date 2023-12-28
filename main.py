cba71bc666382f777b8448d56a7a33bcce5299ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:32:48 +0000

    lose undo load bet keep

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+print("Hello, world!")
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
