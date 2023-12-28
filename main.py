00f4be72804aaff90fe9d307749fe53838470f9b
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:10:04 +0000

    promise lock fit complaint suckme

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import os
+print(os.getcwd())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
