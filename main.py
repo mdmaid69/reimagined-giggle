24ccf2b55fe238d4b690a91b44ee6f5c2f2af36b
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:58:30 +0000

    you solve unusual they dress

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
