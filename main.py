bdac743b84601df68c464278a5882b0c166e56ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:21:34 +0000

    wish father anywhere custom niggaacademy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
