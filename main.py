3f093baf31718c03cbe111df689d4300839598d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:42:25 +0000

    take during conduct pretend eye

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
