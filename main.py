71726b1d012fb6979558c936c1250a97737919e4
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:21:54 +0000

    music quit us everybody neither

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
