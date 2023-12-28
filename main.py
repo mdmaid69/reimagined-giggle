12adbb299f4dc4b60a8568fd5173e4cf44de0cab
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:48:34 +0000

    cheek step plenty male disgusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
