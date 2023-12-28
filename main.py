9f727d5aabe327355f7071f5aa4fa92d7c7a0b17
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:07:10 +0000

    advanced smell wall pattern raw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
