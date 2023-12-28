0bcef56f11bb7978c6d4c2ae79142b90fe192b22
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:01:51 +0000

    object calloff sport university form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
