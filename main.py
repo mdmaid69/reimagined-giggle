07b4c94cff8ce268b93d22874e039ebc6d879b25
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:25:53 +0000

    progress over honest pool impatient

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
