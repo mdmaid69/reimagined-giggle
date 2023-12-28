8237127e135d50f7ea9ae7748b1a24e124740786
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:17:01 +0000

    consist perfect frequent communicate mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import os
+print(os.getcwd())
