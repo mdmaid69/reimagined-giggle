8f87809175cab7194db9c23b4756d48a94090fc6
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:12:11 +0000

    rule floor guide throughout calmly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_acceleration(speed, time):
+        return speed / time
