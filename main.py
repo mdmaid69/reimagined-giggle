8ac1ec6e7119bd9802fafb9946d66172c6779167
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:24:29 +0000

    dull confusing interrupt again skirt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
