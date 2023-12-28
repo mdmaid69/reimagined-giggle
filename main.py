fcf2037653b90b51dd5856f66310bad6ae53b403
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:17:52 +0000

    fly goon survive regret position

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import datetime
-print(datetime.datetime.now())
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
