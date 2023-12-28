dc0c45b6dd30c95a605912fab49b9fe9ee63591f
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:29:21 +0000

    top turndown firm tone special

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-print("Hello, world!")
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
