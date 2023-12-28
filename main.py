b5641ae05988b49e0b9c3a0cf6c5dbbff2bac146
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:25:28 +0000

    tour long figure insist set

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import sys
+def print_python_version():
+        print(sys.version)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
