b23ae4929bd258fccd5691cb666b2aaec2a33524
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:10:30 +0000

    play until suddenly help takeaway

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+print("Hello, world!")
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
