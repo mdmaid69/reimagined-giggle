920c6e87e7a09a55c45de4c533e7fde2dfec003f
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:06:15 +0000

    precise deaf pretend cruelty Rr

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
