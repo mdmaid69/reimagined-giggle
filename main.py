3fc024baa1c446bb15fc4789e5d6d61b79489e56
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:29:57 +0000

    breath claim goaway love should

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
