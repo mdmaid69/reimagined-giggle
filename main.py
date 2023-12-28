0a8cba82b1b247ef61d3d2f697818422b81f4594
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:39:24 +0000

    moon low accidental dress wherever

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def convert_to_octal(n):
-        return oct(n)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
