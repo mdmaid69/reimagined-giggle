ca99fd9c67367e0bef643c78486cc56e8d6a94a5
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:12:09 +0000

    disagree problem demand develop trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
