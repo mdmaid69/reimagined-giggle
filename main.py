19d484e1b0237f68306210127ca72b6304f2e7ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:54:53 +0000

    by watch treat wheel sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
