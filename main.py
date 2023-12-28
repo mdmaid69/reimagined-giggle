59cba6f1600291fbb3141bcfa2c02d5172a4cbb4
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:51:40 +0000

    goout last summer connect plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_list_from_array(array):
+        return array.tolist()
