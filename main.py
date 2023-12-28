a4d98c5155849130999d45330e26574127ca5ff2
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:54:19 +0000

    connect present intend position rank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
