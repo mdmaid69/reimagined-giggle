9c17153a113fc6c54b7a048b71843a2929a95d9f
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:23:13 +0000

    mess respect university more traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
