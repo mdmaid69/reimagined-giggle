3b7a460854697581c3351820a0e0923450f2a0b9
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:38:03 +0000

    trap bath flood ticket height

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
