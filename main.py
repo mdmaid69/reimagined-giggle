1eec2e2deae8b0cf697602186dc60f998ee92dbc
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:44:28 +0000

    definite risk trouble genuine react

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def count_words(sentence):
-        return len(sentence.split())
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
