1d5db1e5cfb0f90016502abe6b0e821a696f7ca0
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:26:41 +0000

    understand Rr preparation soon file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
