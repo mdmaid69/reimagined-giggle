647881a2b34450dc0f3b3874c4e7fcbaa84f357b
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:49:47 +0000

    might I hate tone leave

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+name = "Python"
+print("Hello,", name)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
