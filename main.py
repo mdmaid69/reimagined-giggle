6f78f13d572f9c2e1d0e6970aaf882a049fafaca
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:54:53 +0000

    together public roll example determined

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
