09100c56f36884eb50dde800a2d8ac08c1e3d1f6
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:09:04 +0000

    really turnup manage ear when

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
