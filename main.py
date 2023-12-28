50c621e952cf453d7c1b8e91f1b9e445200a7556
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:17:07 +0000

    gooff necessary journey free friendly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
