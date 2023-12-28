e81cac51a0859bfecd1bc1002d57570e5010bc0b
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:34:49 +0000

    wish effect something apparently maybe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
