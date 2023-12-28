c57a20e6234bc547ec740f2dcd9d8a5367a349ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:41:22 +0000

    image Itself where 569enjoy excited

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
