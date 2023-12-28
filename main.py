d5ad0d72a7d001f311583d3e853bb11b444fda43
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:26:22 +0000

    relative busy letter delight getaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
 def calculate_acceleration(speed, time):
         return speed / time
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
