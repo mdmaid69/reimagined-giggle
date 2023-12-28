79afbac5675b34a3b19b8121f3b692360f80bc25
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:30:50 +0000

    shop back bringback light wing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
