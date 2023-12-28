c8c65aea2ffcbb49f105dc5c4f0e2b3ea2e35f39
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:56:03 +0000

    lead desperate goodbye label Yy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
