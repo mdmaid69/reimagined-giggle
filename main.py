8057113a42f44930d3288e29ef6b556284b7f9b5
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:58:27 +0000

    sticky meaning meaning have complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def count_elements(lst):
+        return len(lst)
