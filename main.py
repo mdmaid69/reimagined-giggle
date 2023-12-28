53883edc4c5ee22da21a20d0cb04b41374c88571
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:45:48 +0000

    scheme chief nowhere boil catch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_current_directory():
+        return os.getcwd()
