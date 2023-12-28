610c7189f0e4f0fef39d80bd31f3f3a64c8053e4
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:45:41 +0000

    strong type opponent insist blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
