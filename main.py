39dd4b71f46ade9bd52d58c84e0685e5d8362c86
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:13:59 +0000

    trade later exit interruption turnon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
