e6b64636a544f25ab79f589d6ac6ab7ee64493a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:42:40 +0000

    issue history side emphasis talkback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
