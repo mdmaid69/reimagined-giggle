b5e01461648cd5d813792d1ce3a5f24e7396a257
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:21:27 +0000

    sink gift correction near culture

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import time
-print(time.time())
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
