20fcc98091b4362c282772ef140005e0e907a709
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:38:30 +0000

    everywhere 570enjoyable conversation miss approve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+for i in range(5):
+        print(i)
