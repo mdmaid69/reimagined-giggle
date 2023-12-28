78e5079bf54c6d940e315d1d79bfa01b19292052
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:26:20 +0000

    crime shallow cross pattern we

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
