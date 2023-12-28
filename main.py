9b0da094eba345f93ca356d465398c1e66757293
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:51:52 +0000

    turnover fun thin nor experience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def add_numbers(a, b):
+        return a + b
