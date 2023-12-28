a2ebc637be27684105ba61326ca8cf784244d2e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:19:09 +0000

    born makeup slice rubbish simple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def square_number(x):
+        return x**2
