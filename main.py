863280f4cf493812a2e342fff56a3c806fb8637d
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:48:27 +0000

    proof hand workon powerful region

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
