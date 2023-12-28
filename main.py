19151855d9e8ca2cbd818646119c6ef0d017bbb0
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:41:25 +0000

    image see compare stay late

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def is_palindrome(s):
+        return s == s[::-1]
