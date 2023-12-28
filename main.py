f0af5644c5dea0bb6f90b5e54b600fe62e901b1c
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:09:20 +0000

    ours ignore difficult form quarter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def reverse_list(lst):
+        return lst[::-1]
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
