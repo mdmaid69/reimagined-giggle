c7d039976c038d8cab0dd93116c6fcb595d94c5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:34:06 +0000

    fit doctor reduce together rubbish

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import time
-print(time.time())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
