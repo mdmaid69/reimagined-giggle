d77b561e43e289e10f548a3e9c68e6b06161de80
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:25:25 +0000

    story difference block coat emotion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
