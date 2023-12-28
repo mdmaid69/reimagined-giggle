cafa0f8b1938d9cdb62f7ac1f8ac58e3394c4ddf
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:53:22 +0000

    walk error available contribute cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def square_number(x):
+        return x**2
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
