e826b0519ffba1ee32e32230684cf8724b9803a2
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:09:31 +0000

    finally flat car better nice

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
