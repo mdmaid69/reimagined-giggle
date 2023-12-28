d81cd6aec7866bff71ccbf7fb533afa3601dc16b
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:44:46 +0000

    become ear urgent wake after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
