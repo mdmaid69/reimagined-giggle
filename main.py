a4c0e649cec85ce99785e62835f9e5c2e9a5e881
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:17:19 +0000

    beat wind catch miss whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
