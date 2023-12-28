7650b843f1de0a8e98f37c8e356ca8e5a5daa259
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:51:13 +0000

    spin owe alone satisfactory half

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def cube_number(x):
+        return x**3
