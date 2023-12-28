f018d5d6a1b5af0085a7f63a3550b733dad67ef4
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:57:20 +0000

    year exactly under town onto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def get_array_length(array):
+        return len(array)
