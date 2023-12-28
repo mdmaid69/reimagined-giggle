ae5ea66d10245608d652577b2c8129415534af47
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:09:54 +0000

    grade cum better target determined

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
