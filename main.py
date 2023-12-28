9c97f84351a5c4cf477eeb91aa53c3c9e627cb1e
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:44:59 +0000

    stick mouth fold sting no

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
