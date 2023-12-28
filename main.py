fbfa7751f2ae4be17149d3f50a532340ba0a4df3
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:22:25 +0000

    pipe rush minimum listen improvement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
