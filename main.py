f0ee151787c14ef1478e3ca4abc2c5a831c70b7b
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:51:40 +0000

    neither pile reply fence fortunately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_max(lst):
+        return max(lst)
