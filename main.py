a848bed9a5a15a50c86e1972b46a85cccd947dac
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:59:27 +0000

    climb repeat treatment quick conversation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_string_from_array(array):
+        return array.tobytes()
