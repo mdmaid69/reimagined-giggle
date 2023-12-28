b8c8a2ae981d577a785685f4a17d3f3a60db1ae8
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:50:03 +0000

    admit nor godown scheme protection

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
