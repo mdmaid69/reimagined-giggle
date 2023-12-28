8882ed7a2dd71e112349fd512b08535c1c4452df
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:17:57 +0000

    firm rank cost float giveback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_power(work, time):
-        return work / time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
