a4dbbac90b930c327382932fa6ac1302d2d1b96d
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:53:40 +0000

    Tt idea roll plastic appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
