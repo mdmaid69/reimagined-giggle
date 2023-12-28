860c81d28fd04b8cfbf47ffb6b9b7b45733dab51
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:38:23 +0000

    branch habit spring operation certainly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import time
-print(time.time())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
