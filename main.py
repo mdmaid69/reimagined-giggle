9ec555ebae506456e7588b93821bb9b429ead344
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:47:07 +0000

    after matter safety achieve move

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def sort_numbers(numbers):
-        return sorted(numbers)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  def sort_list(lst):
+        return sorted(lst)
