f388186fdeb928fd519d28c5cb2df91cfb269578
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:21:33 +0000

    Kk 569enjoy often spill together

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def set_array_item(array, i, item):
+        array[i] = item
