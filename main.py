fb85feeae90fb010304a19ad911b2cc4711cab5c
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:09:36 +0000

    power contrast wet effort lookback(on)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
