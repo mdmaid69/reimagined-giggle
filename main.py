69e7d172f8b7f4592cd0d421d69a18767906f69f
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:56:05 +0000

    plenty bury foot would kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
