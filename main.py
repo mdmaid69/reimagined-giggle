5c1de4720b385f6a880b3d691e6f770c7da94fcf
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:37:50 +0000

    record our close mind sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
