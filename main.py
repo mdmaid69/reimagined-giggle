9a185f65166c9747f62bb041a9390274647740b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:34:01 +0000

    emotion 566end bit press Vv

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
