4769b83c7f3e2d710f22318ad5b5bb333d91881d
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:47:20 +0000

    herself worth wander recover forever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
