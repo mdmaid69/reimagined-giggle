1edd413ea27be68388ac8cd29570d6f07f009ca4
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:07:14 +0000

    reject talkoutof freeze tradition answer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
