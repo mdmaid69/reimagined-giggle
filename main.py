795682b3233b602d6f9e1ee5cdb98107df07b7ba
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:46:05 +0000

    delicate fond pen must passenger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def remove_duplicates(lst):
+        return list(set(lst))
