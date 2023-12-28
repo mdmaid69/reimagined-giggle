cd367b0cf4c2f32dfe53fcdc25c445bb2e7017e5
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:30:43 +0000

    year pleasure direction the join

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
