ec4f7ac03cf711447700e00582e332faf7fa9474
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:37:16 +0000

    like sorry direct approve Pp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
