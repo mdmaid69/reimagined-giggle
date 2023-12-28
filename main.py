29e3b3a3d3582cf4edc6a10bc4e0e55e70e786ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:53:04 +0000

    few dark sleep sleep put

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def sort_list(lst):
-        return sorted(lst)
+def find_min(numbers):
+        return min(numbers)
+def convert_to_hex(n):
+        return hex(n)
