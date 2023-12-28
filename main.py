fc2c15e849a098a3fd89872bc226d64db7fe8fe8
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:56:26 +0000

    grateful suit invite length imagination

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
