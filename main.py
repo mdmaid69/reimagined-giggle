81403dd812df2fc20acbf16ed0edf8f0a7d30757
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:30:00 +0000

    nor holiday bush disturb independent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
