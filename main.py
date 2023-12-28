ac6650ed56c25eccf33a71947cff01222a7b14a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:27:38 +0000

    instant big wire birth question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def convert_to_hex(n):
+        return hex(n)
