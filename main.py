5fa34f1a60ca9cc819c5b1c69dd65d22c4c989e7
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:05:45 +0000

    see room breakinto commit silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
