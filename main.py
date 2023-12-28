047eee6d75ba467f5796b43894e8b6859570614a
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:52:54 +0000

    day track opponent shake disturb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def add_numbers(a, b):
-        return a + b
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
