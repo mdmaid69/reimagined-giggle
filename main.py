4610b8b67d2c4ed7b13f7b050000cb56a69ab205
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:25:08 +0000

    school peaceful insist surprised ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
