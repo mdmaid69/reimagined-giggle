15de11f84db6dd85c02904c264e9fa5bc7ac48e5
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:52:30 +0000

    match accident anything people Uu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
