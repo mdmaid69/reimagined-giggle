8774d228cd11021553d291affe2a5030b30891da
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:56:17 +0000

    suggestion unlucky tread strike preparation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
