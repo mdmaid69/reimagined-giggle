1d79ba46f7368ff9756bc309e59fba11456d9348
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:24:49 +0000

    attractive reasonable provide like near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
