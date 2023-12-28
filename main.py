881449a153bffb2216ef90e22a58284482d5cc74
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:46:08 +0000

    form tap order past eat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
