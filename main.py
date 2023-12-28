8c2b6b89a6c69972dbca7c32550fe33bad4682a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:19:10 +0000

    either respect forgive cost delicate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
