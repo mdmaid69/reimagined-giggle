2f130b09bf617a36a753bbef99350d0f34154b9d
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:44:28 +0000

    journey level aware thinkof food

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+for i in range(10): print(i)
+import time
+print(time.time())
