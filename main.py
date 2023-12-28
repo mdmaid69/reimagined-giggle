65e9b3115a5815d63977ed822fd08ba7a33b73be
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:26:10 +0000

    compete principle next mix slight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def reverse_string(s):
-        return s[::-1]
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
