13de7cfb286188b73c6fa8cf2847db55ebb9ebdc
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:30:36 +0000

    turnout separate deal find bit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
