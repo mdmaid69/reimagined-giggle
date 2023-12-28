b49dd1e6ffc8495833058f4de9daa282125ef545
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:51:57 +0000

    basis drop promise charge bored

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import re
+print(re.match("h.*o", "hello world"))
