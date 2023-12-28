422cd4b2a665f399b5df63bfd9b0e6606bf90c4c
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:00:17 +0000

    fail broken last read blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_pressure(force, area):
+        return force / area
