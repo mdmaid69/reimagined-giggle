30ff63403504a60ebb84d1fb4bed08a0f672f5d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:42:59 +0000

    better situation except improvement correct

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
