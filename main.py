7d310e83229f84a4af514c0a19da58db85b72ca4
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:57:57 +0000

    Mm cover report male give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
