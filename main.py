0228f055914349d7d9e8ad395bcf015e14b141a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:27:32 +0000

    before anywhere extra of comeround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_hex(n):
+        return hex(n)
