3a43cd1cb80f852adf21ceb281de15af9be7877d
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:04:01 +0000

    aim obvious favourite criticize split

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def reverse_string(s):
+        return s[::-1]
