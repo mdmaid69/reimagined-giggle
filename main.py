2b25f23e42027e0c5ecdc46c58b3a7f96bc929d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:31:18 +0000

    steam exam swim exchange size

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
