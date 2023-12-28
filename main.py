73820351c2483b8d02c6b9994131ed8cc574d4cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:41:24 +0000

    tough cinema street argument smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
