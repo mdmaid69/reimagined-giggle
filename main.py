036b3c495efe4fcd7e90523246ed2d3d0c1a872f
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:48:50 +0000

    Pp sad dinner pollution market

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
