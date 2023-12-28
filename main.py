6132a07c7d1060e0ce9ab94d9d652d5afc6f75e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:18:30 +0000

    upset face although influence fix

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
