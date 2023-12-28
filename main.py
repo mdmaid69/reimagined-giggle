17ab6b1f981a2b3dcbe618661360e639eee135a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:09:47 +0000

    backwards above giveaway discuss road

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
