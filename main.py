1bc7e648a5d772bc35d6ddd89fa2c253f4cde39a
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:49:45 +0000

    force country bang their lead

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-print([x**2 for x in range(10)])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_work(force, distance):
+        return force * distance
