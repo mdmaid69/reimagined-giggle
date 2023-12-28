3d787a448851ddf41edfde1bec7a23c655fb8b8d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:34:12 +0000

    unpleasant runover expression flight deliver

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
