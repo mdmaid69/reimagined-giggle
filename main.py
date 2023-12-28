ae24f8a8f755bc4ed38736012ae059d3eeb3bdc3
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:12:38 +0000

    consider worse hollow centre far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
