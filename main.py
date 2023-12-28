466980dcbb3c0bb1077bdc3c3282c0502ec9ca5a
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:07:37 +0000

    fly office watch exhibition development

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_int(array):
+        return int(array[0])
