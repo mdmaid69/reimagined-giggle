c08b79d6f5a5c88a88cb3643541563ece56f98cb
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:58:00 +0000

    later interested underneath speech high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
