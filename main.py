d9bdfb6d943e8fcb33017310d3a8298297cf2e61
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:51:38 +0000

    passenger trade gate mark stiff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_int(array):
+        return int(array[0])
