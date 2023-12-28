4430794b0ee3a2164b1b9c847a0daf238eb253b0
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:22:19 +0000

    fasten female callup later valuable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
