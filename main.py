efab8394ace8bc27447aadafc3979fabeb9c9f35
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:42:13 +0000

    thing fail out young oil

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
