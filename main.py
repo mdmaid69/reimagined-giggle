9e01b23486678f12f2b8b9e2e8642a30f921465e
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:07:29 +0000

    shopping because custom manage content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
   import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_density(mass, volume):
-        return mass / volume
+  def calculate_mean(arr):
+        return np.mean(arr)
