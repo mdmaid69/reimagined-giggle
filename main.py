31168e3826a5b449f4a692acdae6c4b8f32ad65f
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:25:45 +0000

    wide gooff simple talk tragedy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import random
+print(random.randint(0, 100))
+import time
+def get_current_time():
+        return time.time()
