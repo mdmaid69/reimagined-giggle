2938dadf0d356c13c904410bc19c6eedc642f832
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:01:19 +0000

    number cost breakdown road balance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
