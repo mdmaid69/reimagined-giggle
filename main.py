d7bff4731e261880e877f08690987e528cb7d649
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:11:40 +0000

    eat neat clear guilty only

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
