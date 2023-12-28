d5525258407b945a46974c0980a0af6e8e46ea32
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:20:20 +0000

    let better Ee dress copy

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
