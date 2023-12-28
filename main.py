04fad4f108eec731bfd9ce500759d66978dbec37
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:52:29 +0000

    awake soak interview influence face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_area_rectangle(l, w):
+        return l * w
