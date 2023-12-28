d24a5c63fef21b8f8a8e91ad0aa40be70504bc41
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:08:58 +0000

    length phone now support reserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
