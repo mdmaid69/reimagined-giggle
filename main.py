13cb948a632a0b5759febc7cea5efe9dceb89962
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:41:51 +0000

    software position pot flavour match

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
