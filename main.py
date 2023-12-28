d6a05d530fec1b8035484ee339be59663d317c8f
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:35:00 +0000

    repair drawing up speak trust

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  def square_number(x):
+        return x**2
