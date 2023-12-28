9516be5a7554fea3f0b976caac6feee2248427c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:48:34 +0000

    entrance leave estimate understand land

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-print("Hello, world!")
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
