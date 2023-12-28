47c59c8e71b464b8d6b1e1c32814e76f7d2d06d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:11:31 +0000

    black offer too lend crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
