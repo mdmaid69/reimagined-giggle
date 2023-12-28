c7938648473aeb11657b188bf6fb811aa8c29ab2
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:32:42 +0000

    567enemy steer visit tell busy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
