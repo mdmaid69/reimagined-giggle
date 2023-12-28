afca20fb0130a733357abfbc2f74f036a9c7bfa3
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:31:20 +0000

    situation doubt plant floor open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
