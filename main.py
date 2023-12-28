7f809ec7f79754348044220597b9a889dc4cd4e4
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:55:42 +0000

    nearly wear situation shoot bringback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
