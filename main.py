8f95bd42a7709c53acc71bab93edd4dcd09c1003
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:17:20 +0000

    fit unlikely report complaint drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
