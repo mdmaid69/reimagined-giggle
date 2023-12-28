5d4c0d8dd52bd61fa3deb2ffed2ca0e50bddcb88
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:54:55 +0000

    second convinced favourite programme claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
