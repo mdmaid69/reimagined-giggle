0821b757b702b0b50567ef0a84f5a3975aaf8281
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:10:37 +0000

    nervous suggest beyond geton knock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
