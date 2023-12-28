4bbc4cd535e43af3be1eb8540cbd52baaa2df498
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:17:34 +0000

    foot idea stiff the surface

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def find_max(numbers):
+        return max(numbers)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
