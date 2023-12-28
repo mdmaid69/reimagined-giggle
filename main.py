6389e40d93299e6c0394fbd11fd92912cf94e919
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:06:04 +0000

    profit flow hard young glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
