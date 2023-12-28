4dc9338838fe781665f6e9c12127d3dd72b5e1de
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:25:46 +0000

    generous office air drink free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
