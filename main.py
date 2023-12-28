b833b9209c2339929cb945b700caa169efd04b9b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:53:55 +0000

    store its movement formal wonder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
