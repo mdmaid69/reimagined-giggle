2b5545c90230df0c9d686bb75bebb3e9acd747d6
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:55:48 +0000

    square mine smile immediately profit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_dev(file_name):
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+print([x**2 for x in range(10)])
