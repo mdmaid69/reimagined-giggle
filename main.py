5168ae6c8bf3ee67176eaceda70f8a2a150e5e83
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:16:28 +0000

    rub spin record goaway scene

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import random
+print(random.randint(0, 100))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
