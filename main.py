e5239109451577f5d0c45b99aebc3f934be5ccf8
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:11:37 +0000

    large air familiar lie tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import sys
-  def get_python_version():
-        return sys.version
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
