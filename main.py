966dfd380eda05f55b302f47e8e07c500742d3fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:10:10 +0000

    sell sometimes crazy sign describe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_power(work, time):
-        return work / time
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_pressure(force, area):
+        return force / area
