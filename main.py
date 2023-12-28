59440e1cdbec98fa22db89ca953bb331d275e66c
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:27:21 +0000

    objection success container somebody need

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
