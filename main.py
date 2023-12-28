b514e270c46f6f2968a2cdc06aac864bd7cd549a
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:19:47 +0000

    plenty Zz class offend increase

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
