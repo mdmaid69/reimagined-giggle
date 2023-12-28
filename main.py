d044f10d1513fde0a72367eafffa22d185246c17
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:36:33 +0000

    bang card sudden announce lookafter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
