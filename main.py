87114eb7064351fdb86e50eb86653b56079f66ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:00:55 +0000

    influence sometimes crowd account comfort

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def sort_list(lst):
+        return sorted(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
