062cbdaaa60e090402af01d0f58a01e5b8dd1d7c
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:27:45 +0000

    close appear avoid treat strange

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import sys
+def exit_program():
+        sys.exit()
