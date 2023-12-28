73fdec7ad313bcee8ef14f3e178b0c9558296d10
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:55:55 +0000

    state makeinto look breakin opportunity

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def convert_to_hex(n):
+        return hex(n)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
