8d24fc35746927f5549425896993cc3bba34a0bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:48:29 +0000

    before special flat attention achievement

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
