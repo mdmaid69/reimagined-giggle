8874230eb6d8d1b8f2eba5e3b5bc24c9e03cd4d0
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:36:09 +0000

    switch breakfast date see sleep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import tensorflow as tf
+print(tf.__version__)
