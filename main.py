2830b17ac76eecc9a02ad766827f701ee77936be
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:48:14 +0000

    kind perhaps afterwards whatever open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
