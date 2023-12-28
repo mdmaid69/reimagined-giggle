8527eafae08426d946fa2a496c77100c1944d277
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:14:52 +0000

    anywayanyhow silent right draw bin

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
