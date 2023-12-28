038268d3df68edcbae2639614b0e704f95f584f6
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:41:12 +0000

    fail load ocean grade call

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def greet(name):
-        print(f"Hello, {name}!")
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
