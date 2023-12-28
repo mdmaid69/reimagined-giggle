dae4690a965bfe7363be7f943ba485f2301d57ec
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:37:45 +0000

    borrow your injury target wall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_as_set(array):
+        return set(array)
