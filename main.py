7ba649f080c07f2171a5e69eaf4fb818cdf8e2d5
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:57:14 +0000

    sign mainly female any appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
