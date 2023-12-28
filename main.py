9cb7016a7f65d18ff752a51763baba40317a9baa
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:30:24 +0000

    payment scene nothing wind cough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
