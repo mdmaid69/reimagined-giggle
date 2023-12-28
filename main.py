fcb3204a66089117b6c080d90410939f452a106e
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:42:14 +0000

    together extremely view shake watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
