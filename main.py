22accddbac71d8533fe3f0784888af7924044ef6
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:24:04 +0000

    bag block future hand fasten

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
