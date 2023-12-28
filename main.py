76590458a70187d0fe778e13fb946a5a6986c8a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:07:48 +0000

    morning makeinto think style sad

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+print(sum(range(10)))
