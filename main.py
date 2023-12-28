dca87d2388501f9c5e1bc9437ae861e852b13d6a
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:36:56 +0000

    change form imagination accident string

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
