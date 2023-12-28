3a06b45697d42b153838e1f7f0b845220d9b3548
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:28:41 +0000

    whisper tidy getout concerned blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def find_min(lst):
+        return min(lst)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
