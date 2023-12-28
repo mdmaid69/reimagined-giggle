8dca70bc34625246f800b8faf36d745248953eef
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:37:54 +0000

    Kk contact copy question across

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
