198bbe8502e3725e8500b69d451c33cd56be0094
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:02:10 +0000

    on store hard lonely peace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def remove_duplicates(lst):
+        return list(set(lst))
