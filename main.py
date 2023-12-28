7d53d9e26030733ec51c1be36ab14d5ff05391ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:50:29 +0000

    sorry special whom swimming still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def is_palindrome(s):
+        return s == s[::-1]
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
