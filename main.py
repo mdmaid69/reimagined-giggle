6ac8c26eba01a7898abdb3f56d930915eb598dcf
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:51:32 +0000

    work afraid more punish guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
