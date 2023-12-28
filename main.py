ff657eefd50e6e96781e8190390ea59690866f52
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:46:29 +0000

    broad stamp believe stranger stiff

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def cube_number(x):
+        return x**3
