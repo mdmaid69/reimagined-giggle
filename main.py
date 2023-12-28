43c07afa15fdddda61cee0348e6ac720a3858752
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:10:58 +0000

    goodbye thinkthrough 565end apply climb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
