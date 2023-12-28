a4556fc9c1845eff77fc0199d77546efb0ff36b0
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:53:08 +0000

    kind negative contribute accommodation thanks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
