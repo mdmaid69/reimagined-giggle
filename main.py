cc9e206f3344f17725923251d3e70fa51ea02601
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:41:04 +0000

    pile forever complete powerful sad

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-  def is_odd(n):
-        return n % 2 != 0
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
