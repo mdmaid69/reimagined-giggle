234444fe31d15ac7d12c364d8fdbd22b9dbf5295
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:40:05 +0000

    wave cruel runaway pretty rank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_absolute_value(x):
+        return math.fabs(x)
