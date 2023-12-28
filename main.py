1102fa16dca830ad55d3ca921c5aec201edfffac
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:32:40 +0000

    possession shoulder deep stone grand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_list(array):
+        return list(array)
