917f733548ec6b6383e0cd9390b5b06186623ec4
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:00:12 +0000

    cheek pollution praise suddenly week

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def is_odd(n):
+        return n % 2 != 0
