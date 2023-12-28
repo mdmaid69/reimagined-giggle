f6c6bc61942dae0a08fea65a4418aeeb2245400b
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:35:20 +0000

    belong stir future juice rely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
