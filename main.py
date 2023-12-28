1f7aa6b03a79d137c3b592267638f9b3a6aa9c14
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:23:52 +0000

    thumb prefer talkback remark speak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
