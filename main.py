dd86b2e183f2a510672ec8e64b39af137fb3f161
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:41:34 +0000

    neither shut long test acceptable

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_arc_cosine(x):
+        return math.acos(x)
