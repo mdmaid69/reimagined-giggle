3d2c1f8c7253df1c3b6d1ff64cea9b3cd68f749e
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:43:38 +0000

    front settle file tight deliver

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def is_palindrome(s):
+        return s == s[::-1]
