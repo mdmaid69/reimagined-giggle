3e2687032113f34a9510e4c138e1045a10d2cfb0
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:53:02 +0000

    obvious sink rarely thinkof error

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_current_directory():
+        return os.getcwd()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
