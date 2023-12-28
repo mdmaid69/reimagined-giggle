8d929504883c6f1227d18ef87d5f5a784d1e05bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:41:39 +0000

    upset underneath rise true game

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
