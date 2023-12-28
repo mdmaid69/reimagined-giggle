1d43da24300f5be37b910e2aa72622252335a7a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:42:32 +0000

    tap and floor mail themselves

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def find_max(lst):
-        return max(lst)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
