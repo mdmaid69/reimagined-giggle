098586e4e6db140acbc37455e82c79675a792539
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:04:05 +0000

    fasten comeback discover I fault

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_index(array, item):
+        return array.index(item)
