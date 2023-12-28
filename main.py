82205a519d47816c3e3f87a5af879b841513ad1b
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:57:19 +0000

    discovery praise concerning become occasional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
