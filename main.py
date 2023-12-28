4cd77aeb5a5cb62451e64139dd0e230ef3e94411
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:57:58 +0000

    report shout design seem giveback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
