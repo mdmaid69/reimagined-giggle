0a693ee24f5409f2bea0aedba3923e210ab3ef25
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:39:40 +0000

    path increase OKorokay board steal

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(5):
-        print(i)
-def calculate_pressure(force, area):
-        return force / area
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
