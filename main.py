824af7c76d0b87737bcd8bfba7fbb27aa85553da
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:30:46 +0000

    low large comeoff opinion matter

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print([x**2 for x in range(10)])
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
