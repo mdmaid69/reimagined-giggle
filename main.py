8d7342f6cd8f2f4db5a4fa6ceee93ab8ca400449
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:52:50 +0000

    tragedy interfere tip sore meaning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
