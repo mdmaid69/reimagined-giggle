fae8401e1d40345cd5ee97a2cfc118276a7bb02b
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:44:44 +0000

    content film design attitude apply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def add_numbers(x, y):
+        return x + y
