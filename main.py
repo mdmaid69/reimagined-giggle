33de713a360cd5977287cfd297c44f7e1d4151bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:23:54 +0000

    occur from state OKorokay land

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def find_min(lst):
-        return min(lst)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
