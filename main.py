e9202c3cff9b48b9496cc32092e0c56b454ba354
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:50:44 +0000

    idea please direct communication pretty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
