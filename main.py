352b48f45453fd573720e3b96c3c60235d1e3750
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:01:21 +0000

    soft mistake stand nice target

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import collections
-def create_user_list():
-        return collections.UserList()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
