60906762a97dd13b8ccbabcb3b50a3748e5e3cf9
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:51:15 +0000

    town pair change dance notice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
