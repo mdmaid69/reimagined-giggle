a7125cd008d56872372920374df951a5bdfdb4bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:32:53 +0000

    alone break something about where

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
