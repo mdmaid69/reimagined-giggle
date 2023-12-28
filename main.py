c5db0bc9bd24bc593f018ad674e30ca72ee8f10e
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:08:50 +0000

    dream false cough would dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
