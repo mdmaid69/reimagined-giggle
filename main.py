41dedecff29a0f9532010df6d5615915494de570
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:54:43 +0000

    store everyone guard tell below

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+print("Hello, world!")
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
