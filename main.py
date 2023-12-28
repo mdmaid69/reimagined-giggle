508ea11b45056ff724af816209491584d21e78bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:22:09 +0000

    along list normal wing under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
