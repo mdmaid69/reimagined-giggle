ee65e1e4bfebb44eb6ff15d380a1544c06f45da9
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:01:15 +0000

    complicated search minute limit inquire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_elements(lst):
+        return len(lst)
