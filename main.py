ea361ff129a0928380417112874786a21cc90160
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:33:30 +0000

    since stir only runout I

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_gamma_function(x):
+        return math.gamma(x)
