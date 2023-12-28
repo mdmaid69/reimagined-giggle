ab45855751917f4a31b241a5f32924695648d614
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:38:53 +0000

    tidy expression suit protect fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def square_number(x):
-        return x**2
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
