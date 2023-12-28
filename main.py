7f312d75da015542c05afd6ac28bedf7f19357a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:18:02 +0000

    push characteristic that far any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
