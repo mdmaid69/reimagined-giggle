0c88399460d92b47178ffdaa138bbdf82df7289c
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:18:29 +0000

    where keen become attitude tradition

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def reverse_string(s):
+        return s[::-1]
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
