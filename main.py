ee519c02922dd530713bf6e0b62f3c651946be85
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:30:34 +0000

    difficulty breath emphasis various best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
