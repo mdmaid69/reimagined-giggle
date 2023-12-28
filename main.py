c361b8791c2e241af42539b01f04bd68c6db00d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:11:52 +0000

    minute play under surprise pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
