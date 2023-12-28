816a31c41447925ad924920a15aa346e3e9fdd70
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:20:08 +0000

    relative study attach holdup instead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
