50304c26d487a4ec3174732f8eb9621eb0b49af2
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:28:36 +0000

    giveback sound human broad wealth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
