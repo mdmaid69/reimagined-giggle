25dcd2314ef13e444fe0dbea6d55cf73b4eee440
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:48:46 +0000

    pair single swimming repeat print

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+for i in range(10): print(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
