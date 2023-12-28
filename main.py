398e5d14f7c848679715ef85ae74e9480191159b
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:20:12 +0000

    let borrow ahead care blow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_user_dict():
+        return collections.UserDict()
