b206ccdb0eb7da0bf160d187e2b62795d1534153
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:13:09 +0000

    bottom scream top might production

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
