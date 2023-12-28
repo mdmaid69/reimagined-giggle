0fe5a0ab1280065a9695c94c1f32e47d470f8f6a
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:56:21 +0000

    flight man dig whenever profit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import random
+print(random.randint(0, 100))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
