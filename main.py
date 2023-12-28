674e9e8213c2228568bb41a345ae99d8a8173ed7
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:58:34 +0000

    tap allow team include comeback

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
