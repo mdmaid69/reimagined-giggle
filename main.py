2db90aa19da380e661cd9e37e27afd08cf38b172
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:47:13 +0000

    bother now acceptable passenger content

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def is_odd(n):
-        return n % 2 != 0
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
