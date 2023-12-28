878cf3602cb4cc175ff2c9b051a692905dacaa73
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:42:58 +0000

    study may cough hot make

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
