2741837e687becc5f4ccb22c367ef7a5ea7d7053
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:53:59 +0000

    argue dear tooth boat cool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
