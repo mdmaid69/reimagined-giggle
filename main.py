5ba579cce356a75ab283a379dec59dea1b2677b5
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:44:23 +0000

    lose battery joke evidence often

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import getpass
+def get_username():
+        return getpass.getuser()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
