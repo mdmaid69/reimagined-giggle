a0076eee26531a8e8e8ccd809ee840f2dd08d32c
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:29:02 +0000

    classroom key pride play scream

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
