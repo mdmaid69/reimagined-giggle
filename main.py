d15d411386f4ab7f2e4806b045dd81e16f959d46
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:59:37 +0000

    floor sweet 570enjoyable future choice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def add_numbers(x, y):
+        return x + y
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
