78eb798c0540d4c66f4e237ee7e6d00c9f9c2982
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:05:08 +0000

    search lower enthusiastic scream declare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
