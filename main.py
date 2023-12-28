7522d82396ad216a5797c311984e90461a486ad2
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:28:47 +0000

    luck university trouble tough turnout

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
