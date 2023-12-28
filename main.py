937b79fa10c22fa348c8dfd860e9d3dc624858a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:03:12 +0000

    shape quickly nearly takeout surface

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
