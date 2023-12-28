25d51cb0f26b36225bd1e8197656b36c89c9239a
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:07:06 +0000

    turnover hardly undo kill nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
