0213ced8986438f8fae1407dedd4cdb352dbecb2
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:34:02 +0000

    although laugh shop message down

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
