7ef08e346ddf1ead44909b4f0f75e05e7b9c3d95
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:13:03 +0000

    hollow convenient discuss love address

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_work(force, distance):
-        return force * distance
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
