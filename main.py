cc56394d7ab673bf2d331a15fa4075f6c4c70bde
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:46:08 +0000

    hand extraordinary true build about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_length(array):
+        return len(array)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
