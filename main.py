3b0bc9158c8d2dd588055532a3369f56f2989523
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:14:36 +0000

    rather mess square typical almost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
