ff507032a8145592775d4a7d3582a58a1bbb7b59
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:21:13 +0000

    apparently small relative Hh calm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
