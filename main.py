4e5f2c396b8fa30146a571d42f5f7f04857c4ab2
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:43:53 +0000

    this flavour determined bored hardly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
