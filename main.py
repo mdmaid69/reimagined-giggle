d6a9acce272be325dca71374ab103d6bb0cf408d
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:41:52 +0000

    properly godown or train exit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
