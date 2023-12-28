8e68360dc7c936e8099661df419fcfdd44e3dd91
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:14:09 +0000

    break meeting plenty shadow behave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
