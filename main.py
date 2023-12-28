bbe5af23b991b820deaf2be06b41370bd53b2628
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:51:57 +0000

    close important splash spin whichever

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+  def add_numbers(x, y):
+        return x + y
