b63a5a6245ce59e536204de53093110c63df1eed
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:56:50 +0000

    keepoff break profit hello ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def reverse_array(array):
+        array.reverse()
