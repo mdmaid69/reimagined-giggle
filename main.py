009ac37118e25e8c31a8b366ff6aeb876e9a8142
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:12:21 +0000

    low your this survive race

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
