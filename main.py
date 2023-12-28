07d709e4859cd3c7426c7989a7f10640907a7223
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:46:41 +0000

    gate praise getoff poor reject

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_acceleration(speed, time):
+        return speed / time
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
