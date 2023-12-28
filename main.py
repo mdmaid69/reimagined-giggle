567b58584f8b3e148984bdc7dcb15cefefe6fcb7
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:49:48 +0000

    result law practice skin impressive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_base_name(path):
+        return os.path.basename(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
