223bdec234de2bdd5a0099166ac8c7f45fc1458b
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:19:00 +0000

    shoot change never interview lot

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_int(array):
+        return int(array[0])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
