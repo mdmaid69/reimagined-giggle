e8286e545e628b814e1fade8024eabdf927cf1a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:22:00 +0000

    also suit big against shine

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
