e5deb28cbce05a003501fad2f9064ed337bdae4c
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:32:36 +0000

    cause bottom workon guard trousers

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-for i in range(5):
-        print(i)
+def calculate_density(mass, volume):
+        return mass / volume
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
