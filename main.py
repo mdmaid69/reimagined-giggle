d0f64162aa6f0e4f22bddd0a01b2104832b98df3
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:25:49 +0000

    score climb hungry pretty sing

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
