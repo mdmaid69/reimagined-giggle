079c0d4bb77c377359a765915b7167a675776827
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:39:45 +0000

    married class weather tragedy more

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
