e30457414f4a395776d29ab48194acaeeef4042f
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:51:27 +0000

    cruelty home street punish turnover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import collections
+def create_user_list():
+        return collections.UserList()
