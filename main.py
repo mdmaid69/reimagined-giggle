6765509d2dbd71370d1e79dbe65b43ffb1d130a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:04:07 +0000

    length recover show long takeover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def add_numbers(x, y):
-        return x + y
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
