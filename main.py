deed73931d82b86ca09e562ea18350ef3e7682ff
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:29:02 +0000

    hand listen takeoff lunch free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
