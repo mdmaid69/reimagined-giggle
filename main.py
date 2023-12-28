f4fbae4503d93edb8d564c2c242de22d0b2b899b
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:58:14 +0000

    sort rain school turn nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
