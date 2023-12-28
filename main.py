4f45a31d1895d19ab60aeacd2c2b8abb04ef030f
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:58:41 +0000

    heart reply precise underneath left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
