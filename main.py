83a4219edac4ee0bec3153a5bd33cf8ba6fe21bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:48:28 +0000

    so waste print practise relax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
