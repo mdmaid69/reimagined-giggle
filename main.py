fdf75621309a05ec9e6b3b2416f42779422980d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:50:24 +0000

    raise replace shadow generally nor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
