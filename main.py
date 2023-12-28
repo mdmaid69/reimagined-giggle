1bf0cc8ac49447a1b2ced822233bb955c6be95e2
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:07:48 +0000

    bet probably wave area preparation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def get_array_as_set(array):
-        return set(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
