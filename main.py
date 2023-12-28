9cb2308e254340503932bf4e5b3cb99fd1cbc36e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:54:40 +0000

    excuse our occur dig anything

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
