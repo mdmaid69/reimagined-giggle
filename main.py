0643c03b300e06b372bf55bcf5235b37154e3087
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:29:55 +0000

    piece wild cover extraordinary action

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print(sum(range(10)))
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def get_array_index(array, item):
+        return array.index(item)
