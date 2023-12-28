4ca8778a622cf1272a10cb650e0b1fd2e56537f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:48:47 +0000

    increase contents speak year brush

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import os
+print(os.getcwd())
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
