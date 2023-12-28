c2ff945336b3e360c28836e1255de62edc6869ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:58:33 +0000

    back sometimes hand attract test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
