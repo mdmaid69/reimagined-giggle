230c59868fb660371d89469d7ab4fb3614c3b20e
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:08:36 +0000

    roof circumstance aim if reason

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
