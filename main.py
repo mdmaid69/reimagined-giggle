5838ebcb9231796442dd25fa4c67b653b3260f53
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:16:14 +0000

    anywayanyhow suffer cheat report telephone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
