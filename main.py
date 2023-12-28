3f53f33558f06275849bbb43a0dbd0593dd37e3d
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:00:16 +0000

    they health follow anywayanyhow mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
