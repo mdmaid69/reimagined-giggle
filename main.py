dbc0975195008e87b1c1024f19a98e058877ff3c
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:54:46 +0000

    deep full brush science contrast

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def square_number(x):
-        return x**2
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
