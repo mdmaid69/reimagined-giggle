7ea33823e75aa7d0a3662f17b2d25be8943893f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:10:29 +0000

    silent shall after tip draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def delete_file(file_name):
+        os.remove(file_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
