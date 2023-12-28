f14735c2f4db4097002f61ba2443e9b72ab88526
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:38:13 +0000

    female suggest live unless little

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-def sort_numbers(numbers):
-        return sorted(numbers)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
