fd386c22edaab179882347c6f3c6d0fdaef477f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:19:25 +0000

    drawing plenty radio interested dinner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
