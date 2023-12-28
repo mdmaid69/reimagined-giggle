247405ba41683a6bb39ae6366c55b8b4b6c87b36
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:24:50 +0000

    fork mistake conclusion concerning shade

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
