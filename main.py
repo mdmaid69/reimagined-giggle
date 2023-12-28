2cf8e6abfc169b0ec510ef2dfb33815fa0cbc1a6
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:37:59 +0000

    place gate exaggerate upstairs unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_string_from_array(array):
+        return array.tobytes()
