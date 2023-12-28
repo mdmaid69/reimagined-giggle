0d5f33b1d378b8308293b308e856f345e1627d4e
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:01:30 +0000

    watch confident muscle train satisfied

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
