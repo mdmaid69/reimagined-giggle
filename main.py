47f562274c1f85b71d0e6c91a05e3fd4cb826f0f
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:53:31 +0000

    since way thank oil crisis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
