5d558549266fb78ce1c7fd6f5ac375299f025e8d
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:32:27 +0000

    means upstairs convenient declaration short

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
