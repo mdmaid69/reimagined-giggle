212d27afe1f64bda12a4ca6e2b7136d25ebcee81
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:51:59 +0000

    record Ss track several themselves

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
