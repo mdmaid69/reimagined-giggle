e42f7663ebd9d522315f12587f78af6b8c6621ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:58:37 +0000

    can question shop can attempt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def greet(name):
-        print(f"Hello, {name}!")
+text = "Hello, world!"
+print("Characters:", len(text))
+def remove_duplicates(lst):
+        return list(set(lst))
