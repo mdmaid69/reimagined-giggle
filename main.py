ef6551cf015994e02df25d7ed0633117ecbd18a5
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:53:13 +0000

    split song meal hurt Kk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
