085fae44f8b78a5f48ed068cba5a19093bcd474a
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:49:55 +0000

    force store description no bend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
