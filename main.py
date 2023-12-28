54be2aa30e2dc8c7f7bb09a33f1ea633a0d52466
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:05:02 +0000

    speak rent chat match arm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
