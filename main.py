4154af0045f8be305b54ef7578643d49c4762cdf
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:51:48 +0000

    fear possibility good flood tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
