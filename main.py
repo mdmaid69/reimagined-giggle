3ffc8cb9b59c7167179201eae6bad9f0a3485604
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:10:42 +0000

    busy ring result poor wander

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
