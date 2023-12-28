965ea2905ea6fa6e3275a9145ae3da4c37bf48e4
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:14:10 +0000

    correction walk hide demand sail

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def sort_list(lst):
-        return sorted(lst)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
