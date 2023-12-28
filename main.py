ec11ebf0f6510c97704dea84f1ea03f71f9fa488
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:56:24 +0000

    man breathe nothing allow container

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
