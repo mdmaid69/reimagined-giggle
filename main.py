4dfe7b23a06e1cc956984c2dae0f0297e99c1824
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:34:33 +0000

    discovery trade laugh meanwhile increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
