75ea534c8ba46e2e571a95fa373fc6ac8fddff3c
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:39:30 +0000

    herself ring wide door word

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
