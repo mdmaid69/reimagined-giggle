319c9b337b7f58e9ea5cc63b365983508db69c98
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:18:55 +0000

    swimming bear skin disagree concentrate

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
