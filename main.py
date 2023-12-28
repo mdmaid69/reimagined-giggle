15f4b3392812666600a81d76ce5f00bd1d71c40c
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:57:42 +0000

    sore visit journey question then

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
