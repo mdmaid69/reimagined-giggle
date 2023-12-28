d94ff6afa91225d209a447928bedfd71ba94b97e
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:06:57 +0000

    level object understand crowded she

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
