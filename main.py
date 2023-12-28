051879c8c2264daebcfc399b07364ee7daeb8989
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:54:33 +0000

    clue contents cross invite centimetre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
