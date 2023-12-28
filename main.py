4000f7e679cf7eb63730cc23649efd2b87481a31
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:05:19 +0000

    kick inform into evidence yes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
