43fd1367da5c16c953d777cf3c4f52024a75d556
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:54:07 +0000

    official remove hot uniform workout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
