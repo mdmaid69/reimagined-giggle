2f734c4f1796e5515c14f49559a682d961fbb84a
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:26:54 +0000

    flavour belong square sign basis

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
