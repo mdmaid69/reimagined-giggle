8f005c2c48baeed4325aeb14928d75a91f4fb78f
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:32:32 +0000

    accommodation runover dance laugh slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def remove_from_array(array, item):
+        array.remove(item)
