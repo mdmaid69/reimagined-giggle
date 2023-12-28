b567d8bf635a4862d0e3a63c42922c8bcf456400
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:01:16 +0000

    juice dance collect up attract

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
