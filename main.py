ae8d5df775306b312f93a1e3c82835ccdaf1f3dd
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:17:52 +0000

    close point high anywhere introduce

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def sort_list(lst):
+        return sorted(lst)
