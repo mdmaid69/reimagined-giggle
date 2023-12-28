25cdc6e7aad82ed65b4bb535d8e785376d99af70
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:07:41 +0000

    usual platform sound quit count

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
