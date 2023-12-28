f5295d4747106f1572ff7f916322cdef6f315bce
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:38:34 +0000

    cough as explanation handle office

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
