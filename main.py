a682e2474733f0f2a83e37ad8df6b0a0dfea7a66
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:51:07 +0000

    mine small lift agreement contribute

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
