19c0a7b3728125fcfb4c1ab4eb25f605f0b4e299
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:35:20 +0000

    moon this convincing turnaway minimum

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import math
+print(math.pi)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
