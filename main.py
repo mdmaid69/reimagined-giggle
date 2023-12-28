f322165bec1fc9ed46eba67099bc04221b6f99f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:39:23 +0000

    software charge load talkback afternoon

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
