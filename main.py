70854b40ebd86ec6c459d62889ec8be03945c51c
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:30:03 +0000

    anybody seriously light recovery block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import collections
-def create_counter():
-        return collections.Counter()
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def count_elements(iterable):
+        return collections.Counter(iterable)
