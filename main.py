34f62cfb61380a9ef78bf4afee4fbaeb52d2a2d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:36:11 +0000

    disappointed shop between wrong from

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
