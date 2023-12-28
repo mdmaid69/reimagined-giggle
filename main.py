2f28a98b664c96310861a6391319f23d3ab8c99f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:58:31 +0000

    criticize pot activity port relative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import time
-def get_current_time():
-        return time.ctime()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
