abb91bfabd64c05cc33f0a599ab3b8feaed85800
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:51:48 +0000

    eye to badly race news

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
