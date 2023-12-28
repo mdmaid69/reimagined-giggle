d8bdfde62be60061686c2a5c30a887a81dd13ab2
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:21:50 +0000

    convinced Nn capital young stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
