5814aae7e1995e90e14ea0d9ea96c947f883606a
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:05:49 +0000

    week result suit discover touch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
