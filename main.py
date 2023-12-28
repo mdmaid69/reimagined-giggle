a953eac28d8580aa1db336aeea27b5a603b3c4cb
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:11:03 +0000

    recent pot distant what short

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
