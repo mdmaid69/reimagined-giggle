fdc5eca14cfed48e1dce63b50138612982be9041
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:25:06 +0000

    transport present have suffer thought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_current_directory():
+        return os.getcwd()
