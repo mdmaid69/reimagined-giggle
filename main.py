33dc7e5ee7e2b2372e4621ff9f9be9e8877b77ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:37:01 +0000

    switch react smile Bb place

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print(sum(range(10)))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
