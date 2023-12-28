ac974a5189a27d3bb868f64c9dd3e0f235210a4b
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:58:13 +0000

    practical shrink stranger low obtain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_repr(array):
+        return repr(array)
