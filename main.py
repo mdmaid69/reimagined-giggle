fe6991d2f07cb598f38cb0d75fc8fc1606515842
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:08:50 +0000

    suit area future immediately chief

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-for i in range(10): print(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+print(json.dumps({"name": "John", "age": 30}))
