6654dca48479ddc246445009fe31f64ee817bc6c
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:35:07 +0000

    neat maximum worth but get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
