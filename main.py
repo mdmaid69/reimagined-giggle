53c407dceead6b5a59157bd2cb1583b5425e8f98
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:35:55 +0000

    soon convenient total bring music

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
