dbf159a54cdd8bfec335ecfc249b187fe25abde3
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:57:40 +0000

    rain 179bite bang occur little

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def is_odd(n):
+        return n % 2 != 0
