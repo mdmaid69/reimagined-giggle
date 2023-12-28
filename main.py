c8f5094dd1b2c75bf216e8b87a50902367db3496
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:45:42 +0000

    classroom imagine without tragedy confuse

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
