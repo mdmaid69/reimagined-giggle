2cb020e460e2d454d8402497d729f6e16927d651
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:04:50 +0000

    load leg runinto objection not

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
