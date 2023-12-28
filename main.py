acb3b86164561bb2b609340cf759f1146573e68c
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:03:08 +0000

    within affect watch burst definite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
