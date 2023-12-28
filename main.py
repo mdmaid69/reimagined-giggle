92ea8dc5b701d173646f4e992a49d61368936af6
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:42:39 +0000

    late abroad fix shout persuade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
