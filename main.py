e4681c9e448fa8fbbca476febb2872cd540eaccc
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:04:29 +0000

    catch hold sign bit direct

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
