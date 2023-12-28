d72475c27d002b30feb8ff8aeb67b8ff66ae6a42
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:04:15 +0000

    speak chief one few bang

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-for i in range(5):
-        print(i)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_area_rectangle(l, w):
+        return l * w
