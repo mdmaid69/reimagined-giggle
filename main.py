e31e4d2c11b11e71f34399d83fde4ae5166c800d
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:20:09 +0000

    choose lower hit along single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_perpetuity(payment, rate):
+        return payment / rate
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_memoryview(array):
+        return memoryview(array)
