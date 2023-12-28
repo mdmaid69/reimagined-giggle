2b271e302252f026ce995e7338747177dfb3e1c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:12:03 +0000

    guess stiff sleep rest and

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
