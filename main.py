f719d442f591aa9b0bb636b8a313497cce3469a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:45:05 +0000

    cold blue deliver power interrupt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
