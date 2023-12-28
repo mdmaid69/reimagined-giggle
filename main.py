63757e424f1acdad0dd0500530dd516c3900cede
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:17:05 +0000

    change fairly best hit place

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
