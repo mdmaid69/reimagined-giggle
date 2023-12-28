a59cdb6f89fe28d29e62ec9d1c5cb24b6eb5aa40
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:43:49 +0000

    once visit speed practise correction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
