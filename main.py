a49e80cdf3a5794f06f024dd52ae2ab2d02e48af
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:50:49 +0000

    takeover port unpleasant remove appeal

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
