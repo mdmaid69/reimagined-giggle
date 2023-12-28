3a82379e6db9dfed7f36c870a1d0fbd43d6674f6
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:06:40 +0000

    smart children phone sound collection

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_work(force, distance):
+        return force * distance
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
