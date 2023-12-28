3800032bf951f4470677f121e44402fae964ae33
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:35:43 +0000

    deal organize pass on precise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
