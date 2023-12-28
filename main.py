ccd41b54191ef975150140e82ec5593f9655b716
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:43:47 +0000

    quit copy do repair turnup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
