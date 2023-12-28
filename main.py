f22cd6a5007509a5efc06a8ca364229019b1683d
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:25:56 +0000

    stop fair stranger front indeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
