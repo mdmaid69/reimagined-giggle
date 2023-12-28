61ce590cd4ca4ebc0aa0ef69599f906a80e6238a
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:09:20 +0000

    development tooth defend seem risk

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_speed(distance, time):
+        return distance / time
