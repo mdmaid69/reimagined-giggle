0bc7a4a9d21da354a67039a0bb608a9bdee69678
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:47:21 +0000

    weekend label empty tax wear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
