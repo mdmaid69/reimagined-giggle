172955588c96f9764e87483c44c602f2e57df09f
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:01:38 +0000

    cheek react getout exploration air

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def subtract_numbers(x, y):
+        return x - y
