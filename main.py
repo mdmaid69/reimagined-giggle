f26b9960e1dcd5f19bb7b37b094448ea958ecc61
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:49:49 +0000

    exercise in second female any

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
