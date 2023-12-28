3c4262cb22041110b8065ec5fe1dfd0aed639b27
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:54:14 +0000

    sweet speak fit shallow die

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_length(array):
+        return len(array)
