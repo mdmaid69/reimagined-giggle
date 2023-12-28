f5cfe7136785b3330b140b15b1cc6419ed7baec3
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:02:17 +0000

    beneath point occur he murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-print(math.pi)
+  def get_current_directory():
+        return os.getcwd()
