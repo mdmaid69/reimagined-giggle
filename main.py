d99979c747a485fcc60978f8468cc6ff8c112b58
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:52:03 +0000

    bye forget famous praise pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
