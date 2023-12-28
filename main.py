78ded0b8c3cd846ae362e5eda987e9764f0fd461
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:28:35 +0000

    open pause kill giveup taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_even(n):
-        return n % 2 == 0
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
