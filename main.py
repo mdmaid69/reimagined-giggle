060489e5796cc3c26fc5c2763daddabb455f9e90
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:32:55 +0000

    comeup repair defend obtain near

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
