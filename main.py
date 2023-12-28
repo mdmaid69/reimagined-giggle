b69763f42b378a35136c4353dacf2277c76adde1
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:10:41 +0000

    near 569enjoy store tragedy one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
