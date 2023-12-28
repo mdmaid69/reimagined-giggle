770b8be1ec376f7b24edbe04194a23aa72080cba
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:01:32 +0000

    true picture which comfort possible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
