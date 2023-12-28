ed616ef37646718065b40a3d51a8b271ac44e74e
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:53:10 +0000

    wind mention scare swallow actual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_itemsize(array):
+        return array.itemsize
+  def is_odd(n):
+        return n % 2 != 0
