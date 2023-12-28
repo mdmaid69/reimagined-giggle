575c65d1b628e6b55df1696256c9196ead1ca146
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:08:50 +0000

    male sink reply loud language

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_min(lst):
-        return min(lst)
+def sort_list(lst):
+        return sorted(lst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
