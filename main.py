a6fe966992a9b8feab4888eda7d3c0d42ef4bb1c
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:17:03 +0000

    combination lookaround compete movement introduction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_min(lst):
-        return min(lst)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import numpy as np
+print(np.array([1, 2, 3]))
