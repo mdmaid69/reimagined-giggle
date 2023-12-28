116f5c3743cd0702f1c8681f4733f09a05f40caf
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:02:07 +0000

    pencil risk limit centre warning

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_speed(distance, time):
-        return distance / time
+def square_number(x):
+        return x**2
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
