13a26002e63637461e3995ef9fd6deb3dc6e57d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:48:58 +0000

    date nervous cold tradition guard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
