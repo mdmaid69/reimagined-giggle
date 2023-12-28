ebf3457e93c217e6d683bf2b8620b2bea5956e50
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:22:20 +0000

    check actual exchange calm dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
