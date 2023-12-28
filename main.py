637430e181c7facadeb0dbe75f2957578fb26761
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:45:24 +0000

    advanced around group after pair

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
