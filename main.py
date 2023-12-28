47509a90a4cbe8b8bcd5093fc6a23010e9238d09
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:49:35 +0000

    go murder condition over range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
