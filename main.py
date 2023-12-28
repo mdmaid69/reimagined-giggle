e836732f89e6d20d63d5020ef59c7a1f29923e10
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:02:57 +0000

    across sure pot bag Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+print(sum(range(10)))
