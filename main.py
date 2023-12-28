7cbde6df21412a8ed5a1ff6cac4bbb09cda136cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:38:55 +0000

    the pride brown discovery winter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_list(array):
+        return list(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
