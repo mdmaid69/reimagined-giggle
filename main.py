2520abd0728f35e2e82fc98bb40d0a24eb522de4
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:43:39 +0000

    cross collection there walk fortunate

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
