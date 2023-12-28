21b50679a03855d0e9477b513ef9c977f38693c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:53:54 +0000

    address seat her steady spot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
