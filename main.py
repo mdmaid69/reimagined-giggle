f5ae645168a884ce7d427000d7015bce719ef083
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:57:48 +0000

    previous offer appear persuade tear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
