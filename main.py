30518688741e76fb18ddfd8a0297d9ff4beaa062
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:49:22 +0000

    praise here convinced during card

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_length(array):
+        return len(array)
