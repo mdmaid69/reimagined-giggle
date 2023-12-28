5ace98d2e758ce249b839ba29cde5e5cb7d24425
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:38:40 +0000

    negative exact arm sweet separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
