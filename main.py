dc5a2df1dd80ed4eef9c2f162c878d8611baf1c3
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:14:32 +0000

    disappear transport police talk set

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
