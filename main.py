623bfa3f451e51c425721bb05eff701aa964f2dc
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:53:26 +0000

    this along bus face emphasize

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
