6fc7e1a5868c378e55f7b028562cba5d4381918d
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:51:21 +0000

    respect concerning ability exam callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
