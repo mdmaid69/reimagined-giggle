0f6e93e08739a1171ead2d172b24109645f0cc1d
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:00:05 +0000

    hurry but only failure radio

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
