9f2fe63bac0d49eb37385b161a8bb405a1122cc0
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:16:29 +0000

    whistle wild weak custom left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_work(force, distance):
-        return force * distance
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
