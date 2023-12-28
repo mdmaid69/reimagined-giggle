a96f29df714e94b716aad3bf485b550d252d7b6b
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:09:41 +0000

    during return visit brother big

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+print(sum(range(10)))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
