336eb6305a275af0de0ea52c84ccbfd564df4458
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:35:24 +0000

    period temperature mysterious miss sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
