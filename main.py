3fd1a1a351c98459ca71628a134987a5ec3c7885
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:58:33 +0000

    crash condition emphasize blue slide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
