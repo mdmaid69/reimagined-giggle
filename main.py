a3f78a1812c5c751ca80a1ab2b135017f4791612
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:13:34 +0000

    everywhere modern wish tell separate

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
