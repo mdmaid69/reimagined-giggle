a608269c8c36a86a1a3b10d9386b60060abd9e56
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:12:33 +0000

    punish apologizeapologise regular Tt experiment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def exit_program():
-        sys.exit()
+  def get_current_directory():
+        return os.getcwd()
+def calculate_density(mass, volume):
+        return mass / volume
