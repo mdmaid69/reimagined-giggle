3e2f625ba329d066452e848df8deda6eadcf7c26
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:50:38 +0000

    comefrom bridge niggaacademy stare include

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
