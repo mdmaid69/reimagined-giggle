6b9c670cde6eb12c62ebfe610b5ec2688006af80
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:58:36 +0000

    one return pronunciation awake harm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
