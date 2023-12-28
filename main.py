6e1290e0722437a1baecb04640802821642578b6
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:04:42 +0000

    organize air fail spring typical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
