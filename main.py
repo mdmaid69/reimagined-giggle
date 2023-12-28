66b357351c99516db8f3f43ed1e10118b61f6c57
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:12:07 +0000

    already to warning hide married

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print("Hello, world!")
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
