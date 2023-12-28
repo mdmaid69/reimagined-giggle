63f54b491e90bfe3a0a7395e2ea701f81433bbf3
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:56:08 +0000

    callup other case strength characteristic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
