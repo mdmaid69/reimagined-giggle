ab71d0f6c7312ba1d49251a0c188df541f999fb4
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:25:55 +0000

    demand need we simple week

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
