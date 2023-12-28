2757700c8f41da9950cfcfabe65277e1b6927fe8
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:46:29 +0000

    watch join limit market meaning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
