27c777aab03571b24e3d0bbfe9e7b820b414bfa5
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:29:36 +0000

    seat contents collect quick acceptable

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import datetime
+def get_current_date():
+        return datetime.date.today()
