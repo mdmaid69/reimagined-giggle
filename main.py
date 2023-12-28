b33fad3e2e6864c815faecc84ea66b293144913d
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:46:52 +0000

    once extraordinary find opinion feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
