fdc7a8c373f08351b830eca8748b633b386ac38b
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:58:34 +0000

    disapprove telephone disapprove wall exchange

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
