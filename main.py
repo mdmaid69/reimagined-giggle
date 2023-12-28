84e9d0026c6bce397912e5cfa661014795416315
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:03:53 +0000

    trust that guide top now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
