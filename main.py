6d5727000562a94b7be5328751785478527c9adc
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:49:12 +0000

    divide definite respect possibility independent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
