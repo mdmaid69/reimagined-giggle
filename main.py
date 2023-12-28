0f89308aebb98b460772416c1210bf8a374b3043
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:49:27 +0000

    past shock ride same distant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
