5f01f307bf590077eb4b791d3a0e1aa92422eb68
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:21:58 +0000

    kill bread fail cinema snow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
