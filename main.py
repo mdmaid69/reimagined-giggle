47ff620e59bbf112bcc0777b18b0ff4a7076a1bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:22:55 +0000

    frequent emotion little apart prove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import getpass
+def get_username():
+        return getpass.getuser()
+import tensorflow as tf
+print(tf.__version__)
