0b12a3b2494e7f72982aa8afb29627018df4841a
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:59:25 +0000

    warm thing machine progress rest

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def sort_list(lst):
+        return sorted(lst)
