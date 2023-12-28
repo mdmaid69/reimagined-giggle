df782e744ba702855c40b7db0e2ebb372f686d43
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:52:49 +0000

    cut used regret yours scheme

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
