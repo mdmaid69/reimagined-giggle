2770521afab84e0361bf6626413200e2d0a78d59
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:36:07 +0000

    plain she pinch turn aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
