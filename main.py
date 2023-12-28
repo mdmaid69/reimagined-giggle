ef35920876cf94d8584fcb733ab3ebcb0f8368ec
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:56:03 +0000

    tough comment refer habit agreement

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def convert_to_binary(n):
+        return bin(n)
+import os
+print(os.getcwd())
