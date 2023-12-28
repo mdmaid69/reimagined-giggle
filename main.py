b106bd81e1995cd03f1cd58a7ddcaf763887cf90
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:01:50 +0000

    interest relation lunch tall nurse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
