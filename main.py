211c89a5443d664ed7ea4b2f1595f3c5edc39e3f
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:58:31 +0000

    obtain ever or over future

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
