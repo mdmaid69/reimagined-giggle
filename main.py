3794ea6335cb59356caac5b831be7a82a9d2f5fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:28:54 +0000

    wish comedown heavy reasonable set

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def count_words(sentence):
-        return len(sentence.split())
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
