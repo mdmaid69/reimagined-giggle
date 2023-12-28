4422c976a5df71bc788fcef913b0942b7e0e3aed
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:40:36 +0000

    public whoever disturbing thinkthrough war

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def convert_to_hex(n):
+        return hex(n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
