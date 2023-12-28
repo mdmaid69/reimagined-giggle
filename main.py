c030ed6672f077c38582193328d01ef1bc704428
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:04:04 +0000

    sleep disturb zero suggest Ii

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
