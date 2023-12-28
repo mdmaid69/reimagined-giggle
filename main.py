21df3e6a88438eb36a13c43113da9849b8f7b456
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:45:26 +0000

    reach neck empty sensible meanwhile

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def subtract_numbers(x, y):
-        return x - y
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
