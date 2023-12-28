ab7c5d156f2893eca0a69c010ce082ddeac87d75
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:14:38 +0000

    rain I environment bill opposite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def find_min(numbers):
+        return min(numbers)
