8d2c0e49b030d5a82cb70c004850972634247abc
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:50:35 +0000

    avoid stay game breakin holdup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def convert_to_octal(n):
-        return oct(n)
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
