6c5ebfd769529c6eac85d30d33eb2c945b5c5458
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:59:45 +0000

    pot geton branch feel no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
