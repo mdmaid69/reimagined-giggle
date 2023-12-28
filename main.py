49e90a14bf90ad70559516de66020be39bfff601
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:58:35 +0000

    shine hardly drink stranger satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
