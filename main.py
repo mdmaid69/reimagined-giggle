070287a8f8ba816d583ec7e161618bb46a33a87f
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:18:45 +0000

    hit instruction plenty absolutely far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
