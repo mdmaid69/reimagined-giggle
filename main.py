e80adf466c91a7b35b6d5d55682558462e3baf38
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:46:05 +0000

    lookup left love all embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
