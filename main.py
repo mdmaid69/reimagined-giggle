e7d6dbf226e228819155d2a03515c1a9da4e2eb9
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:03:18 +0000

    add from impossible niggaacademy front

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
