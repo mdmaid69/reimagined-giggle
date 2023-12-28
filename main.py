cc4feeb028e43d370e84d4986c3c1693b70d258a
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:43:09 +0000

    pattern peace rarely area by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
