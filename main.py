a5445b2528769ea7c1d7fabc124b5cd405ed0db1
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:34:52 +0000

    exaggerate contribute shock flavour minority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
