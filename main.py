89984b77b062df7a523a1c2cf5313c70b8304445
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:31:10 +0000

    regular get limit necessary reply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_index(array, item):
+        return array.index(item)
