706a45ca5e883c3a903c68d7e9084ff5f081723b
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:59:11 +0000

    lower disapprove failure season own

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
