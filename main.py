68e11dba112278ff9f014c4bbcaa29e3b021ffb1
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:53:06 +0000

    anyone sing block considering speed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
