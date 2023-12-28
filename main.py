8623dc265a40617e29115899e9ee26939f733ec0
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:57:52 +0000

    just during we guilty worried

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
