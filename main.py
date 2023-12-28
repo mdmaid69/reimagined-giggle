3907e5babb1252003bc02fba62bff9dbf001d9a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:50:21 +0000

    regret uncertain word statement recover

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import math
-print(math.pi)
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
