a1206ee40ce551b1922d4505958e31882cc2d2e2
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:10:51 +0000

    permission whatever tired house drop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
