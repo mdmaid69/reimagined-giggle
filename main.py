ffd1cf5926f68426d943060f7651fc77bc51f79a
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:14:11 +0000

    flavour understand true feed sweep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_work(force, distance):
-        return force * distance
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
