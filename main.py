63ddc2cefa41328914532daf357d500a7b261028
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:52:52 +0000

    complaint breakdown beautiful temper against

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def add_numbers(x, y):
+        return x + y
