35db12787aaac123af4ec237dc76a3c6a8da5480
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:52:49 +0000

    Ll object wind lucky total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
