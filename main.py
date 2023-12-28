e3bacefec02852f91b7649f3ff5e34dd32d3de39
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:03:08 +0000

    indeed beautiful punishment recover bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
