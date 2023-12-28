1d3e49780235d0fb6f64f07e1eaa557cbe69e4fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:48:36 +0000

    study 570enjoyable talk congratulate on

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
