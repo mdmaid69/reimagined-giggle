cbe87ccfb0c5b311e6c2114a528716d3ff500e0e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:13:16 +0000

    anywhere choose plate sea dangerous

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def is_even(n):
-        return n % 2 == 0
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
