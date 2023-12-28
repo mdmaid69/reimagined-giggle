8a8f56885d213006f014a82c80b3b1c1de19646f
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:12:36 +0000

    proper fit serious square tall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def convert_to_binary(n):
+        return bin(n)
