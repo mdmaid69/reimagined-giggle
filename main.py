b1d9a5fc7aad61f021f2991249469e84532c2a0d
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:48:30 +0000

    run performance keep top child

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
