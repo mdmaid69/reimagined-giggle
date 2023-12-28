5fadafe59a1dc0ce23a1c7e473cbf192761d8fb8
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:31:09 +0000

    bush Yy unless move none

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_as_list(array):
+        return list(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
