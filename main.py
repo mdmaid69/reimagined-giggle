ae16809501c761adf16e54bdccc73aad669a674d
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:17:46 +0000

    turnup relax love show adjective

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
