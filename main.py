f6375c93afee0ed2660ccf925d27cd5736a10031
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:47:16 +0000

    bread Pp case actual instruction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
