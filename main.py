d7724b3ea932d56f6817e720ef001dfc74d7ca25
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:51:06 +0000

    order wing getup through burn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
