22c9518f2cc5cbaefbd7634320bbfb29c03a2902
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:23:54 +0000

    surprised kill difficult give fast

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
