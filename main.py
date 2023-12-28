991b30cf5b395ad5649363efb4b5fa7f014ebf51
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:05:51 +0000

    trade entry war harm force

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+for i in range(5):
+        print(i)
