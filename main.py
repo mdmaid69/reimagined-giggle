5fb952bbcfaaba2ade3550a1cf56d0efc0b22be0
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:16:03 +0000

    mean little 567enemy first beside

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_average(lst):
+        return sum(lst) / len(lst)
