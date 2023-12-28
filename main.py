51afa83e147ca8e236884bdcbda1776b6ba6b75a
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:45:34 +0000

    Kk finally opinion turninto exciting

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+print(sum(range(10)))
