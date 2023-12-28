952a7d9c5bc798bdb8cb6cdf952f1a7068d77016
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:48:30 +0000

    573entertainment drive fold story workout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
