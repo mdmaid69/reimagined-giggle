79de067279032ab34c2c2566f6ae49ba955ef06d
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:24:39 +0000

    opposite again do chat obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import getpass
+def get_username():
+        return getpass.getuser()
+import sys
+def print_python_version():
+        return sys.version
