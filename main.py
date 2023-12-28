e9f4acc2147fc7ebb059dc83e186e3f7f80952c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:01:39 +0000

    contrast comein over share cheek

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import getpass
-def get_username():
-        return getpass.getuser()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def is_odd(n):
+        return n % 2 != 0
