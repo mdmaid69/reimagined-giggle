dfff78d044d2e33d906dac218bdb05aa7c8cf488
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:01:22 +0000

    contrast goaway move accordingto surprised

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
