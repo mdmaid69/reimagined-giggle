01c82b23071b342f14469dc8ec3ca4e9b11f29e8
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:43:19 +0000

    able high all squash constantly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
