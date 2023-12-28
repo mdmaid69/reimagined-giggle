37feb1a25185fae9576ee80b908addaeee6060e1
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:13:25 +0000

    look takeoff Oo stage recommend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
