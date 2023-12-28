b94fef13c69d93cf641d6d34074e40f83666e6fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:39:46 +0000

    score after everywhere proper better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def cube_number(x):
+        return x**3
