688ea4f0279a2ab5af8d77ea3312730320aca4b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:14:57 +0000

    later smile interfere key culture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
