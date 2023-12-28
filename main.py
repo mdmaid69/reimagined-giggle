edea4a8d2bd7ff45244ccc583f7c85b7005af559
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:23:29 +0000

    steal among mine cut instruction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
