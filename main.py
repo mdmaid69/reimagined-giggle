31ccdf04ac2306b1c69a4f3aadbf5b6399fdd3be
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:57:33 +0000

    our idea always like disturbing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
