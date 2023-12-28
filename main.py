cc353c214da739f6fde5e62ad5cb30ee5e595ffc
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:01:20 +0000

    organize sad his score try

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_pressure(force, area):
+        return force / area
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
