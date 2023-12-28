ff5d23150e728aeee3baef6fb4314b8cc4095b88
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:26:56 +0000

    impatient gradually who theirs sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
