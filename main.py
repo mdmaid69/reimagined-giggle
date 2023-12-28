dfbd0f3ecdeae8ef930e06cc34c4643bfb2ce250
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:22:48 +0000

    oil evidence acceptable instrument boil

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
