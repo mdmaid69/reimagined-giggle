c8c46c7a8b306af25f2d4b793983ebc534b2df45
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:29:46 +0000

    male expression ticket storm keep

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
