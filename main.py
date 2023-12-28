0e7d6fc64fde37405ce70ee9f70efeac93da7cd8
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:46:29 +0000

    autumn unhappy rely passenger total

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import collections
+def create_counter():
+        return collections.Counter()
