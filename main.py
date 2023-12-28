4adabbe1c88b02edd59419da45cd47087fd0db3f
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:44:33 +0000

    invite rarely typical silent question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
