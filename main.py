51246824171ff4aad504f95ca7368ca0f92afcd1
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:18:11 +0000

    secret hurt church ticket slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
