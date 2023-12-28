599fdb953cf52e6ff2a2ba16ef25b786208ed3cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:46:27 +0000

    exercise further us dull unusual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
