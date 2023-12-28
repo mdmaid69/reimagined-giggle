aadaec3f2272e4f2a978962837c16b3444af0b93
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:42:22 +0000

    government before willing pretty boil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
