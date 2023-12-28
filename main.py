8b8bfebe5aa8b7440877a1d7ed4f7e1bd5ee27ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:11:45 +0000

    music some can head successful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
