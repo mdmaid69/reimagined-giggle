aa4ab34e8d00743454ea48a5a5d0348f3c667418
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:07:21 +0000

    me concentrate basic later exit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
