e45f49b0d7f3e276b106828961ac4164431c73b6
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:30:13 +0000

    whenever several expensive basic fortunate

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import datetime
-print(datetime.datetime.now())
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
