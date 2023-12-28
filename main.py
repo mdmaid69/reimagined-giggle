cb0ae2e1710a5dbd1e5e181438ff28c8542d2bd7
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:00:31 +0000

    stage influence list me communication

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def get_array_as_bool(array):
+        return bool(array)
