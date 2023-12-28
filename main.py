db9134c9fe4d736091e6597313ee119722dcb4a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:56:28 +0000

    inside may priority blue city

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
