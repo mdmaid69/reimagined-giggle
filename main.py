900d4c9bb77880d84a21b51195ae744810c03588
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:36:54 +0000

    number method spring smart broken

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
