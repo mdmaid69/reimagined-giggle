eeeaae52e2fe70abdb1b3957894428c5ee24b4e8
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:55:17 +0000

    concerned fold as sometimes because

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import tensorflow as tf
+print(tf.__version__)
