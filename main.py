40cd3c368e7676633e2b7d1722248a24ac012346
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:52:32 +0000

    shut stick luck along question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
