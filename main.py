24bafeb0691b6bda00a9d82e1be308b33de0f013
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:15:52 +0000

    clearly somewhere able move float

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_to_hex(n):
+        return hex(n)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
