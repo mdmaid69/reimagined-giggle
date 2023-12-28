057be9a799de5827701d56063c74fed337f82c61
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:00:57 +0000

    rid twist stop hole fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_priority_queue():
+        return collections.deque()
