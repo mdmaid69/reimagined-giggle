33eca13c5ec5b69bbd74586e6ac047c55d8edf14
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:01:05 +0000

    relief sentence protest some careless

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
