544f56b5d0b6b714f88a38708333c9355f7e42a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:53:50 +0000

    unable stone wrap bit 565end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
