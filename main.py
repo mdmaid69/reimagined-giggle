640c2368ef60e3960678499d964803e3302586ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:35:01 +0000

    but mood polite nerve task

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
