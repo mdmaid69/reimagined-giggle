474aed7d4e26c6daf4ebba2dce17419217ccd9e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:21:33 +0000

    partner turnon possibly wear powerful

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def square_number(x):
-        return x**2
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
