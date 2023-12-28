1c109e71fecc8d10d3c19f1020d5e3f001c41283
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:11:47 +0000

    range switch its copy generous

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_power(work, time):
-        return work / time
+def calculate_speed(distance, time):
+        return distance / time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
