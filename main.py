7d8e28804e5b414658a20c23b557a7f2c892481f
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:58:14 +0000

    hand smile knock wing string

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_density(mass, volume):
+        return mass / volume
