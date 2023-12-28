4f51a97ded2cbae75eebc92e513eebb40ceceb62
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:35:43 +0000

    plan refer she have lend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def find_min(lst):
+        return min(lst)
