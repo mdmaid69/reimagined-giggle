43d754cd0e690f3fb5d9dcb53510a2976bf0b12a
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:51:21 +0000

    with butter oil feel moon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
