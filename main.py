5dfc337faf504659c4234d01e2d30262c50564e0
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:59:30 +0000

    change hold Ss deal boat

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
