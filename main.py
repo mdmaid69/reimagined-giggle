3668d2d541eeb53fc0573926eb9232b72ce8675b
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:01:52 +0000

    house once surprising complete common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
