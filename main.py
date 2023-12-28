f0f7fdabcaa70e15b4bcbd66ac8af850efc1f096
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:53:11 +0000

    that second week relax punishment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
