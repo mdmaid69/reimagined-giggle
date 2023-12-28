c9532a9582269c4da53831a454fe2e4a48e1dae8
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:43:13 +0000

    spot uncertain delight official relative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
