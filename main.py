1efe33708e2c1ec3173c958056bb54e0d4a73365
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:05:08 +0000

    like spite year warm suggest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def convert_to_octal(n):
-        return oct(n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
