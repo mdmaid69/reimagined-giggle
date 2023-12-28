8e1abe6289c45385cc08cf882434916ad390f8d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:42:36 +0000

    ashamed greeting either usually rarely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_work(force, distance):
-        return force * distance
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
