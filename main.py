11a0b1c4d37084471816736eb192c9abe45033f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:09:15 +0000

    reply cheap pull enthusiastic case

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
