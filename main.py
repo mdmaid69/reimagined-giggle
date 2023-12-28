4aadef9bf6e30f9fd867e88c3df021ba3c69edf4
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:33:21 +0000

    on smile crowded under clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
