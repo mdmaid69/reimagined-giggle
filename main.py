24a20c5c0ae4e935d69bb889d1e7e38b46abf564
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:40:08 +0000

    snow march invite claim tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def split_path(path):
+        return os.path.split(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
