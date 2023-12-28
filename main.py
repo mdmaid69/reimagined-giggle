bc791e8ddbedd4bbc15418b863365f4beebbf156
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:25:55 +0000

    disgusting worth break friend authority

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import platform
+def get_os_info():
+        return platform.uname()
