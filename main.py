ae1e6a389ce070b964cf4b3e86d04196beffccf3
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:18:50 +0000

    trouble offer regularly use recently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_speed(distance, time):
-        return distance / time
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
