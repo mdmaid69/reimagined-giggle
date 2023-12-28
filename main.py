5cd1cbe00616fd8e8a939140963137588b283856
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:50:40 +0000

    ever talkinto cause period custom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import sys
-def exit_program():
-        sys.exit()
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
