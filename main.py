03767c01b69ecc6d6634736fef12c79dddf23107
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:45:46 +0000

    letter sell target content ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import platform
-def get_os_info():
-        return platform.uname()
+def convert_to_hex(n):
+        return hex(n)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
