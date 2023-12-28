34f4e6dbee586f943f7735cb1819c43629dcf54c
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:10:10 +0000

    television pay relationship insist concentrate

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-print(math.pi)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
