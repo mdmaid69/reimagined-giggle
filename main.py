e8c7d5619215b3ad99b4c0ee52c16cf2440d9cb6
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:48:29 +0000

    book himself closely ground truth

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
