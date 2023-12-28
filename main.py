8d8db2c586d3a32a61ea94711284fbae27c2c129
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:24:11 +0000

    worried passenger majority some boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def create_priority_queue():
+        return collections.deque()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
