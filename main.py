291c360d6b7bf555039eb824e7b4d40d0d646969
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:08:17 +0000

    pity murder sorry able with

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
