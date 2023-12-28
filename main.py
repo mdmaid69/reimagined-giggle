1f7bd5d9c3f4a92f63894102f913dd4c36eba745
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:05:17 +0000

    ever appear of child provide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+print(os.getcwd())
