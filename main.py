163658826ef8db4b38a0ee4bf640f9cde4e50e78
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:58:30 +0000

    mess reduce gooff visit common

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import sys
+print(sys.version)
