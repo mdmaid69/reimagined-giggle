59391130e2ebfb3b24cb098f5080ea03dc3525ec
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:12:17 +0000

    basis upstairs bear strike imagination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
