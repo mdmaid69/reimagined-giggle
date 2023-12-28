be4b603640fda6075958f8ad3b1c58628d302842
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:21:35 +0000

    different money difference rough within

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def convert_to_octal(n):
+        return oct(n)
+  def is_odd(n):
+        return n % 2 != 0
