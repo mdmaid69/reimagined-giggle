a0d0817c87984c57d4d2eded208edda43ca537c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:00:32 +0000

    stranger answer persuade display sing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def add_numbers(a, b):
+        return a + b
+import os
+def remove_directory(path):
+        os.rmdir(path)
