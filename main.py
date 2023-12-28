7ef4d7a2d53cb4ab37ced187e2817eab1bb67715
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:18:40 +0000

    herself now progress cross plenty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-print(sum(range(10)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
