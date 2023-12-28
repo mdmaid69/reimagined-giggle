95384fce5f1e9edf3d23046e39d596691e941906
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:16:53 +0000

    pause form middle run people

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
