b244f14dc9579b92d700c9ec384ce3cf96acad96
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:31:42 +0000

    finish base turnback chin allow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def add_numbers(x, y):
+        return x + y
