7dddf843f9d86bdafefdd0cc88ccc79cde38f3ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:25:39 +0000

    neither minimum giveback without big

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_palindrome(s):
+        return s == s[::-1]
