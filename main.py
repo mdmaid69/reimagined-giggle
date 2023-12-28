57fb36a9a0e4fa5b4dc983d584c1d632bb45e926
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:03:55 +0000

    getback Vv search indeed morning

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
