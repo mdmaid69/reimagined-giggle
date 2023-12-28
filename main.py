1696221f4f2f54ae7b755789dccf008aa638c800
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:07:35 +0000

    authority argument busy clever season

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
