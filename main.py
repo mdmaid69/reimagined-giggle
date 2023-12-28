2c946aa3d92dab4b14deba3295acab5fe02504c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:03:10 +0000

    Zz offence ring will feed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
