58362ef607d07050402b80289e320f4c3dae5414
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:28:47 +0000

    juice separate bridge fast fire

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
