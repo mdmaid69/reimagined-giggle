614b59b80fbfe728c02d5508319f8c938e2cd3f1
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:48:42 +0000

    row imagine late foreign disapprove

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
