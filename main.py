dcc24edbc1ceb43eace89858433093b40ea666fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:07:49 +0000

    walk model gift loss identify

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-print(math.pi)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
