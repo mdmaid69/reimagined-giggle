50db596b9a4b8dcfb53f6ef4d9040cd7c17704a2
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:09:13 +0000

    university into this twist mass

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
