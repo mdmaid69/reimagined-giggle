40909f45bcbb241ac55c705a48f803032642d3b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:58:33 +0000

    himself handle radio chance impossible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        print(sys.version)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
