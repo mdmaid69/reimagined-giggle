be996b3f39d725f0a3bc2821dfe06c45f23a3d1a
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:26:43 +0000

    very at thanks show him

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_power(work, time):
+        return work / time
