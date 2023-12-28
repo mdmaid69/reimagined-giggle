8abf60c9ffcc560a5418dbcc3df13182182b8c9e
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:48:45 +0000

    564encourage tongue whisper used perhaps

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
