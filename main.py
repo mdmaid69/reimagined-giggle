84ba6283c1c34e26d2a93251c9f89280261b692f
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:56:42 +0000

    should clear smooth think science

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def find_max(numbers):
-        return max(numbers)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
