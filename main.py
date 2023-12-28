c867d1fa0e898c5616cac3119d61466403eba0f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:59:06 +0000

    scream art vanish rain scrap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print(sum(range(10)))
