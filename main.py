85e58611455fab0946c4d7f3849fa01ebf8d1c5c
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:17:48 +0000

    laugh wind complicated ring never

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def subtract_numbers(x, y):
+        return x - y
