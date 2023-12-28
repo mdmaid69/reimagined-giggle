1afc524596f1131255e20e030a1e94eb65bd9100
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:07:06 +0000

    office profit keepoff manner pick

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
