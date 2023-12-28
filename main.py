72ddc1d855f4c9fd98603511108df3b8e8d236e0
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:51:03 +0000

    answer emotion capital serious brush

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
