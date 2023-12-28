030df7acdb3dd3ca0dd4f4022228499ef4c6a9af
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:28:20 +0000

    pay quickly through hide convince

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
