c37c97859c78ccbff2ece87d48c752bb218faaf3
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:41:41 +0000

    owe back convinced breakfast noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
