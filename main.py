d5342c235c329f92a452f4572a63f1a9326e7d56
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:49:19 +0000

    bread roll visit late lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
