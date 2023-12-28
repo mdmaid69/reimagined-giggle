dffcae37b1213c2322667e75835d6d952d58344b
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:46:13 +0000

    environment arrival holdout priority excited

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-print(time.time())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import json
+def convert_to_json(data):
+        return json.dumps(data)
