e0c17ad20786b06062471987ba3e4a121784c7f2
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:31:09 +0000

    place perhaps patient translate light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_base_name(path):
+        return os.path.basename(path)
