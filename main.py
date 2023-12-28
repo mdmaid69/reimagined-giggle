36ddd9cf0d1f1494274b457427811935a5032205
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:10:41 +0000

    bringup pot apparently break Itself

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def is_odd(n):
+        return n % 2 != 0
+def find_union(list1, list2):
+        return set(list1) | set(list2)
