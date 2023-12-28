52a299f2017434f562852d3a43e5126bf06a8052
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:21:31 +0000

    sometimes fail lookafter number pencil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_min(lst):
+        return min(lst)
