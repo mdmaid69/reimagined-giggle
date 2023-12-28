6144537d67a44f69d51041c3feed6d2bb3271703
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:20:31 +0000

    else short share at eager

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
