74234955438d1656fdb269e85836d0503d3eb43e
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:55:03 +0000

    slowly official getup statement sweet

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
-import time
-def get_current_time():
-        return time.ctime()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
