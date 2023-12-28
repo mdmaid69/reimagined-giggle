2c8825e2f32304e4c126c215218abd6c6d4c7c58
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:51:50 +0000

    kind wrap point life ring

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
