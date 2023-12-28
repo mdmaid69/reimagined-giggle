f4d56136f5039febb5aa683012fc1467f1f58355
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:28:41 +0000

    comment theirs rain wrap admire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
