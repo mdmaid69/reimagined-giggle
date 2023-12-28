47a25b64bd3eb5b634570674ee217cda5f919011
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:20:09 +0000

    turnback far pocket drawing scared

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_work(force, distance):
+        return force * distance
