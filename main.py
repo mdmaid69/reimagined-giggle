4a2f501c289ebd309d974940993a3b68bed256a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:57:26 +0000

    quit throughout hand answer image

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def square_number(x):
+        return x**2
