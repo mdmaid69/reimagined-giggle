d55e5b9518e0fac2a78dc2fa6c40b66070d7af62
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:50:56 +0000

    fasten block near present question

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import platform
+def get_python_version():
+        return platform.python_version()
