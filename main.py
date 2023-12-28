525d87179fd967e65a241fad632042985cbffaec
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:40:38 +0000

    one speak pleasant stir interfere

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
