d233a48c1b278e22e5937f7a5fdd528650d709e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:52:55 +0000

    deep smell distant skin whom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
