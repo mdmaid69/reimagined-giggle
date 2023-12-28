fb68d2694b51fecf78c8e00febc1207089782215
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:55:09 +0000

    fond wash myself minimum paper

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def cube_number(x):
+        return x**3
