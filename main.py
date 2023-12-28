5029b13ea5e638b831cd23022103af04ef3a2c91
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:01:49 +0000

    passage rent ring shop life

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
