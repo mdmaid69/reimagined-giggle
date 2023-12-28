f7cbedc7a40e7bb8e5c37bf05d27a3d8e40ea65c
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:35:53 +0000

    entrance ring now stone name

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def subtract_numbers(x, y):
+        return x - y
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
