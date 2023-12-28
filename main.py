7a612d4f8f7f919b44199b5e72078a14f40c948b
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:40:09 +0000

    worse pay ring score nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
