5119836771fc8937635a787066c9eb098c344c4a
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:44:41 +0000

    include safe crazy sure holdon

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
