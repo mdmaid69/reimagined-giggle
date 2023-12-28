0bf00b31547b02c5a61f443262ab290bc1f642c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:51:36 +0000

    proper action easy separate match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def clear_array(array):
+        array *= 0
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
