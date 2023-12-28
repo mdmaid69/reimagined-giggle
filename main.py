199e4a8c7cbb278c0086562d4d8b3110e82bbb3e
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:15:12 +0000

    thank her suffer attract before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
