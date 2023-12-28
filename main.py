850e2e3718a7f519ae5fcfed984c4b8e6fd397e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:53:51 +0000

    punishment bunch experience possible maintain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_sine(x):
+        return math.sin(x)
