8542cec37ccf081b050921449d1da7d23c83801e
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:42:52 +0000

    mend will punish unkind smoke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
