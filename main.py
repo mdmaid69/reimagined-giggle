8cb8bfa6784e7b497fc283e9252787c3e2671b30
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:10:44 +0000

    can fight deaf letter left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
