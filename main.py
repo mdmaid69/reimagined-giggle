454c490d08e384eabf3cc304d4038aa1b088e91c
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:47:27 +0000

    impossible weak skill list sad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def count_elements(lst):
+        return len(lst)
