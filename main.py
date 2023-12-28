a661f7cf600783ecdde9f29c8223c87beb8d7c0c
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:44:49 +0000

    earth space bite view regional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+for i in range(5):
+        print(i)
