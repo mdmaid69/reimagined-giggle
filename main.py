1f1bf98020f268983be96932edf544e5a0e4fbda
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:37:32 +0000

    feel increase luck cut there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
