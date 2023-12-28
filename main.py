3b1f7fef83c96e77db6e579e53eafbf886c0f73a
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:23:58 +0000

    prevent exactly Zz angry wind

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def cube_number(x):
+        return x**3
