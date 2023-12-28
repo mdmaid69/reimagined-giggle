5e6072da65515aee19f3cae26f56833cc764c047
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:24:38 +0000

    blame bound enthusiastic ago bear

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
