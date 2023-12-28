78e3a52cc05ca5e38d4b691b5ad17c8f5650a0c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:38:49 +0000

    young design proof about transport

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_acceleration(speed, time):
+        return speed / time
+  def reverse_list(lst):
+        return lst[::-1]
