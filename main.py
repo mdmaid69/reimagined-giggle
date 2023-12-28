32d5105c139f94f34c7f9ebd9c79478f018abe3f
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:36:29 +0000

    hang path skirt escape exam

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-print("Hello, world!")
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
