75f7c014d2440d1902f33ddb278d0cceeaea8d2b
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:31:11 +0000

    point project 572enter lucky score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
