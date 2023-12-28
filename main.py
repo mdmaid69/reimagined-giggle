50432220362b55aef3fc03f3deaad2fb360cb579
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:43:46 +0000

    thing people usual this you

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def convert_to_binary(n):
+        return bin(n)
