70eaafb73eee797674b9a1629407bcfd49d3428d
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:20:06 +0000

    learn compare so holdup bath

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import time
-print(time.time())
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
