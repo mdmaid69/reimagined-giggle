f89ec53090f11aa6d17a680736fd236d6a5d76a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:49:11 +0000

    with short conversation protect pretend

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
