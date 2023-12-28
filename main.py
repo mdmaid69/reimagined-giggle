826540427eef8001e4f61f2a379496310f23ee23
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:42:32 +0000

    between environment Nn pencil stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
