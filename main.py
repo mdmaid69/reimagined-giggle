16aab2bf9bd440c0103cf58654c346f8fce3b64f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:36:04 +0000

    only pronounce start include boring

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
