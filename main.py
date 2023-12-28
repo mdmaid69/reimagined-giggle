75925ca975f53ca89516cb1e0e340f5f2b020869
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:41:08 +0000

    smart burst contact born high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
