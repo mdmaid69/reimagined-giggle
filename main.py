0b77c25d4ca5b63feb10cd47473e53e2914f28f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:20:30 +0000

    fortunate boat creature right under

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-name = "Python"
-print("Hello,", name)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
