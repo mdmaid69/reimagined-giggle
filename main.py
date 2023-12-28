651ca67bb765b3b685d00cfc7a81bbb5ddfea1d5
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:39:46 +0000

    street wipe sign reach tip

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
