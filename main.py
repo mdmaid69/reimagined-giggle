322471ead2ebb898a3366b99c24ec65d37ec251f
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:59:23 +0000

    better cope condition mood sight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_floor(x):
+        return math.floor(x)
