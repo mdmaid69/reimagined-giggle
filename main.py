66fbfdfe494d02943a843b98e64a2681ea8dfe1f
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:15:13 +0000

    task well leave loud communication

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
