6b7409f1eaac64d166624baa9472bda790ab5a3e
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:22:13 +0000

    event undo about principle after

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  def remove_duplicates(lst):
+        return list(set(lst))
