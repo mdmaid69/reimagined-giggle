d5d7d12c3557112b17cc4d5a3596ff5c9a40fa75
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:55:42 +0000

    emotion suggestion before personal keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
