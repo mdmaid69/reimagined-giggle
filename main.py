fd98a46c7f123233cf18b109b99a0f69e376dea2
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:23:41 +0000

    father treat sight brave little

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
