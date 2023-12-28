a0fc9b82e0ec7a042475d2921c000d7e8c401aaa
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:45:32 +0000

    substance disturb behave pump save

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import sys
-print(sys.version)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
