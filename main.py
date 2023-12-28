e02fd99ba8b8882428a568dd34260cea4456d3c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:51:38 +0000

    unpleasant only familiar happen lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
