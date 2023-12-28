23c44076be4a3fa335bdb1a611920df53e6e9e99
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:40:45 +0000

    mass job unlucky male attract

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
