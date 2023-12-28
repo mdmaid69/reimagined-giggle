d7a43e3f21d920250a0c102a11e981d241c96dcc
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:36:03 +0000

    polite thumb wave workout connection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_max(lst):
+        return max(lst)
