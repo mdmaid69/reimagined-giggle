192335528df434b04e505edda7ced99a311ffde5
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:16:45 +0000

    arrive quarter harm favourite very

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import datetime
-print(datetime.datetime.now())
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
