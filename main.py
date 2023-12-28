37957916854093750a4b7353883b775f7392d538
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:36:27 +0000

    sort present game surprised dress

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
