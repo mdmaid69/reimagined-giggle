00ac648f148011e28c41069f8431809e7d5cef99
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:54:42 +0000

    benefit wet suddenly creature lonely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
