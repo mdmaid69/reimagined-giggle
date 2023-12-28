73e125b015334a7ba97b467c8d62d4dc38b45418
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:51:35 +0000

    keep brown frightened operate sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
