7f702f80189ba9d26535e6478ae9b4071d510652
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:22:10 +0000

    cost clear content minute turnback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
