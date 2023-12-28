fc3b29f8c939f45acb0b9ce52af316204ba94360
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:31:36 +0000

    avoid birth sting why generous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
