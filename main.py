c2f37141e032900c8a1f53e4fc27545fd02f296a
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:42:46 +0000

    father goon poison contact many

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def is_palindrome(s):
+        return s == s[::-1]
+def sort_list(lst):
+        return sorted(lst)
