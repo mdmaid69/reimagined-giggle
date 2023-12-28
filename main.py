4c34af05c112ce9f718a945d8c83741fe38ed0e4
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:54:44 +0000

    scared near crash base minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
