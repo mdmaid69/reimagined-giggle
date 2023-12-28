4761402a256b24859bd977510bde6508531d260e
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:44:50 +0000

    famous production final evening respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
