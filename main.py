c1ce67eb7b850c9ac93b1de210d848e1af63cb7f
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:06:55 +0000

    soil flat band himself release

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
