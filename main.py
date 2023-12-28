461a3906c2bcfc9e9d8550c973f31b6900dc63ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:55:15 +0000

    wipe duty memory base bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
