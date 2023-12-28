0d4d0d161bb232aed9792e78a7f1954ba73859eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:52:11 +0000

    godown fear formal thorough run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
