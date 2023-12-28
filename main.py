45326affe7d9db2d1c5e20b72500a912e8f7c68d
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:00:39 +0000

    independent that protection treat and

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
