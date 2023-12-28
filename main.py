ad971e0025e48373ca19e7b56ac418687a01ada5
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:52:38 +0000

    simple afterwards form reduce only

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_stack():
+        return collections.deque()
