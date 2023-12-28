130b341f31d53c6f471115f1e77f4b8856dbffbe
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:22:52 +0000

    wise down exact would field

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
