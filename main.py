a619b5cd4d28c47f05a2979484e8384b152bc034
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:49:44 +0000

    recently aware quality heavy similar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
