c9835e4ed603ef31ea498a734308ff5878336f70
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:17:11 +0000

    goodbye still disappointed compare stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_area_rectangle(l, w):
+        return l * w
