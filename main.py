fb0634897aa57837c079a2dd67e41cc429c175c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:58:55 +0000

    furniture nose embarrassed such of

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
