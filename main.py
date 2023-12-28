ebe1d732ada12691d6e51eff5ce088f61889568b
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:28:06 +0000

    fairly willing getup prevent holiday

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def add_numbers(x, y):
+        return x + y
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
