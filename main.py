f42d07cac75fbfefe8e61e5ecb174ad8f49b7d82
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:26:02 +0000

    consider top proper suggestion slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
