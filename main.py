938c9e86a49ee90fb9a7870f58c30ff27e9ef3a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:28:43 +0000

    surprise like pin careless uponon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_item(array, i):
+        return array[i]
