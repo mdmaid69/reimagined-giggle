3afcdd1b1b544a2242abd376afbbb6fd651ce36a
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:15:48 +0000

    charge pour majority then beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
