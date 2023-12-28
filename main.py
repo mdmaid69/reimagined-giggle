a9da04637d162cf333f88f93c515d243db64028e
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:15:02 +0000

    mysterious round bad sweet light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
