812456a8b465d264b9b80f4e684c10be120a8a2e
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:31:39 +0000

    hurt doubt mail do convincing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
