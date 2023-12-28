7a6d2a091bdc3f48ddebd43a57e70acf5f131f06
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:50:02 +0000

    live taste whichever winter distant

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
 import array
-def reverse_array(array):
-        array.reverse()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
