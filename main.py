ab7e4e9aa214f1abafa1fae4485d067659cb40ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:55:48 +0000

    aim negative luck used neither

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_as_str(array):
+        return str(array)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
