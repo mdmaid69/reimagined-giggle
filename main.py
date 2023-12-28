0b6db423db1014ca0aa23b5bac3380fb2032e462
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:38:01 +0000

    brief slightly cope exchange need

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
