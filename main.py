6c6f5f3a324bb92f62d3b2c3ca7e7f6fb4245f05
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:27:44 +0000

    cry any right natural can

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
