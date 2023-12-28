5b4a6093369d86e35211b181fd7738478f36130a
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:36:22 +0000

    slowly over disgusting certainly condition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
