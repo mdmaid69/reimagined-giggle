ec7be2c228512ecb708161db0aabaee5ec934f99
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:56:15 +0000

    regional him partner without store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
