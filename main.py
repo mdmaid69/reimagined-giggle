d0f411d5a78eb26ba634230b4ebe4ac25f4e753e
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:48:42 +0000

    there impatient loss refuse Dd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
