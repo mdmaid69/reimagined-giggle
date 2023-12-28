cbb922dd7cfb2924ebb2ec70d718cde9b6c57ada
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:05:01 +0000

    trade prefer involve discuss muscle

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def count_characters(sentence):
-        return len(sentence)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import collections
+def create_user_dict():
+        return collections.UserDict()
