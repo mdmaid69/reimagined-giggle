3a9f0db956fce4b147ab68cb107a9170808d443d
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:53:50 +0000

    relation roll vanish party welcome

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
