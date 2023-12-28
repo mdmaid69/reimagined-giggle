1160f1e762ed350fbf1ecafcab70ad56e8d4d3e9
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:19:09 +0000

    morning partner recently recommend laugh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
