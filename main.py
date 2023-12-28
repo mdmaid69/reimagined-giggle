3e90c6b6ed943e47f0b3d346cd072992e46ae830
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:37:51 +0000

    everyone high autumn duty shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
