95b9735b63c10fe654f0d096e29f97dec560f367
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:33:44 +0000

    Jj dream sensible sound better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
