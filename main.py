3cd755cc5a312a66a14fe066afd60f1da2591907
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:14:18 +0000

    building 570enjoyable almost Gg spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
