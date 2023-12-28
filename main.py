3ac42f3600b0549b021e36f91205c82aa93a2743
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:56:27 +0000

    answer dick runinto chat Tt

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def cube_number(x):
+        return x**3
