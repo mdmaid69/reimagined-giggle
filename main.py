0b28aec6630479cbe437d31394393ba3ef67e8cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:11:09 +0000

    experience intend home cause feed

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def cube_number(x):
-        return x**3
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
