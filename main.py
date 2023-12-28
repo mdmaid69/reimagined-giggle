daa803c0f26e36c7dfbf1200913f11e4e754f228
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:24:08 +0000

    themselves heavy home material release

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
