3266f1bb228785ea8e7c11ce82015c0f5e1a8a94
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:13:05 +0000

    decision during detail kill expensive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-print("Hello, world!")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
