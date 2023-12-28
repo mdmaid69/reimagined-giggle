7ac6e6148670ae036788b3dc59062eb2c680962e
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:06:12 +0000

    what unfair stretch bend mixture

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-for i in range(10): print(i)
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
