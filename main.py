78dad8ba33bbaf8db01af67c3e86f0e4ec1eab80
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:52:23 +0000

    cap seem record proud doubt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
