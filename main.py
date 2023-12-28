b68e279d9f8855793dfe35f89b4941ac79fdb73b
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:22:57 +0000

    except print opinion hollow flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
