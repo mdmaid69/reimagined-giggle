8ebd8d736bab84e94838054feae93b6e9a50d56e
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:13:25 +0000

    understand guilty dry behaviour rest

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
