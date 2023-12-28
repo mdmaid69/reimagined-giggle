6431f3891cc5b389ac54a51e3c8d436b90bea227
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:17:59 +0000

    warn bottom think grateful sharp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
