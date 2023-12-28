770c8509e094081419f7d48148dad0134a3ec076
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:58:23 +0000

    change persuade number play see

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+print(sum(range(10)))
