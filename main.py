10840f48c7fba95ada6bf9824e8834c3ad8fb55e
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:42:42 +0000

    off connection slightly flood through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
