a744c2ca32ea7892a978036fa7172890d2efc5e9
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:07:20 +0000

    otherwise throw ahead fear somewhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def count_elements(lst):
-        return len(lst)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
