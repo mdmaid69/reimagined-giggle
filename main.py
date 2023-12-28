0d1444210cb88bd7570c02e6d01e3d41bf6f131e
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:50:07 +0000

    callup secret alone neat takeaway

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
