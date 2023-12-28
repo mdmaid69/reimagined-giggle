ff99850683c02d3f15172dd1a43f70ce24a96c6c
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:44:31 +0000

    present lesson acceptable hand test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
