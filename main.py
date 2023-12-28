6ae387cb347d81c0c418c1f2f3df66ee2659a3dd
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:59:25 +0000

    any judge instrument choose first

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_perpetuity(payment, rate):
+        return payment / rate
