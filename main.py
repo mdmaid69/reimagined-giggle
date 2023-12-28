017e7a5158229f388b854add1dee599d4dd45e4d
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:20:01 +0000

    reliable power unhappy chat destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
