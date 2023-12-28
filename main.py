bd70d7b876b690e4e921b2d6839ab72f6f98e539
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:29:11 +0000

    short willing refer label account

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
