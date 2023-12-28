b13635c2eefbbba2579f43db9a8cbf2e94f67b89
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:08:42 +0000

    trouble copy mix accident schedule

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
