b7e1dc7b4fb3e8ff8b916c9ec769ad5c7edb5681
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:24:19 +0000

    brother with need hurt cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_as_set(array):
-        return set(array)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
