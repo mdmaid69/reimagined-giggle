fd02f0e6d7bceb7ed2e896171352a1723de2350c
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:55:50 +0000

    flat comment guess loose cap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
