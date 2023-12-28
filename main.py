f6beaea536ce302ba540ee1da42107e9d4ee794e
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:48:45 +0000

    scared from makeinto plain noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
