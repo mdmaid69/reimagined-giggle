f6bd42fdadc4ff9fd0f61f712cff1afcac149e64
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:37:03 +0000

    accommodation lose Pp wish bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
