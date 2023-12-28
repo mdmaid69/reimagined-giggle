4dd6ff469c7faf93b3f70072c47985212a8324d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:30:25 +0000

    already unfair responsible everything combine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
