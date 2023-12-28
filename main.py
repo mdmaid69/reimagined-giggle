7dad4280d1b5afc804b4eefacfd4c32af56244b9
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:38:07 +0000

    hungry hungry brief 571enough there

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
