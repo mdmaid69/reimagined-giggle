5e6ce0d514d5396f6335a9883764a6c0cb06ba38
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:29:25 +0000

    as somebody square lean form

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
