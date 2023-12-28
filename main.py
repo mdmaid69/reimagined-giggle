5b1bfb08c8b75b8c063bb4847ba4f37d31883f9c
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:25:50 +0000

    hungry idea fortunate use uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
