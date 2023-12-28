4818dc441eaf48c54bec785872b085d38cec527e
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:54:54 +0000

    nowhere really position it turnaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_max(numbers):
+        return max(numbers)
