fec71d3355d5111285ad1272bf5c1265589e97fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:04:46 +0000

    question either joke hurry criticize

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
