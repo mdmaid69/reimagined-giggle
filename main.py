edf4b6a965b8a93860051657c8aadd9f0e537721
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:40:28 +0000

    edge main cruelty sweep mistake

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_speed(distance, time):
+        return distance / time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
