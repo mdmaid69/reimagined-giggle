0cdf9e59e4483c5a30444ca23722096f1393fde6
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:25:43 +0000

    area deliver suppose turnaway autumn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
