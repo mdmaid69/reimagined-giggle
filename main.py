4a4f07cbb24f85f6b50e0cc4ea566a2a39e7607c
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:50:35 +0000

    deserve zero trip punish pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
