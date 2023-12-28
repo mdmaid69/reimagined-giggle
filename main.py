9175c3de99140fba3f1a547018aa1a76944827d5
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:53:49 +0000

    survive service tend centre Oo

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
