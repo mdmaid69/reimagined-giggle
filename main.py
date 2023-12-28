a174f95ba5f1fccbf2ff8ca0c9b8c474f4ae2f68
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:37:50 +0000

    it especially escape possibly join

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
