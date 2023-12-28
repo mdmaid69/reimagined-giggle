d83123b911a85b73608cdd4ecd90f328596cf5b0
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:25:14 +0000

    wear middle brown normal takeout

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_str(array):
+        return str(array)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
