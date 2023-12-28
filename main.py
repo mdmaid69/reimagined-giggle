57da8f044b19cfb79c9b530e09df7f7c8a44d579
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:07:50 +0000

    circumstance deliberate shoot slice sister

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-print(sum(range(10)))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
