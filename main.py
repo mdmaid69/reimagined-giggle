a87a211c475bb120f8dbdb6bc41e98c3dc242438
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:18:58 +0000

    hang honest naturally forget demand

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def greet(name):
+        print(f"Hello, {name}!")
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
