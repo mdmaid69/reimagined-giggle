648eed904ba92d174017ae7bdfcfbe0fecc81f73
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:33:20 +0000

    dry land straight bag soil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_acceleration(speed, time):
+        return speed / time
