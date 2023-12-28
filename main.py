acdcd7f11d420c55dcbfce21bc0ed0925c38741d
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:27:07 +0000

    disappear mess imagination total where

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
