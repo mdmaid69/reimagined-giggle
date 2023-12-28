dc801fcc3a874232349d6f26294da4160aa9b7e2
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:40:43 +0000

    pride little climb second please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_rectangle(l, w):
+        return l * w
