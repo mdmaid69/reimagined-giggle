9cf8b613b42d36e94eed8a88b30da9d0727b6aed
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:49:30 +0000

    arrival close scale example practice

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
