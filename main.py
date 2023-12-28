cb921a744fc24345d3eb5e76c9a0797bce5049e2
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:21:54 +0000

    instruction order level stick convinced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
