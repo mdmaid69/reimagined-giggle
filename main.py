fcae4070783a3741f721234a146583dd821492bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:42:33 +0000

    wash ignore ignore excuse mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import getpass
 def get_username():
         return getpass.getuser()
