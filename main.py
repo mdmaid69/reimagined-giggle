0439d1bff6301d924c1434ba9ab0ba87140e9aea
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:02:24 +0000

    geton legal fortunate true downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_list(array):
+        return list(array)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
