6b276959852c2b55d83aa43afe7488e9623b2dbc
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:30:25 +0000

    concerning history as mouth Jj

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-print(math.pi)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def remove_duplicates(lst):
+        return list(set(lst))
