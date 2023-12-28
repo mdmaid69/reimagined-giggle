5e1281b8b6fe0331f7f47fe7cb3ff83924cdc5ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:02:03 +0000

    great keepaway scream fear rob

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def is_odd(n):
+        return n % 2 != 0
