06d649e2736c1d955b871491ac462e4f726f0cc9
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:58:51 +0000

    dance run lookup could bye

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
