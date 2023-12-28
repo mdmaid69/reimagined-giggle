5d7522c373050275eb7b7814d5fe4d5ba2129372
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:05:14 +0000

    equal quickly mind class scene

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
