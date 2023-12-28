6153b32eaaa75bc7cfdf09439da53f5060a8472f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:53:55 +0000

    answer violence miss takeoff loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
