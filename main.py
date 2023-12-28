51048765c21ecc538a2a4b604d4561d0fcc3e15b
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:28:03 +0000

    dark begin brown who sore

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
