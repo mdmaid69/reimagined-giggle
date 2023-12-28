c24bbbec41bdc813aed9f966db2298b5af5301d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:37:45 +0000

    middle flow position doctor grade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def cube_number(x):
-        return x**3
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
