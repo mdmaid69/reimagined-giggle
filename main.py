56bfef9b9059786b826cd6956018c31098961971
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:11:35 +0000

    regard commit distance high damp

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
