e0eb879ce612b55675847dc38386e7c2ae95b0b0
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:13:15 +0000

    wheel thorough some probably demand

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
