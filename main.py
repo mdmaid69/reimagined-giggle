abeefa68f8d76362fab887c23967471dd509fb60
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:33:51 +0000

    child complete along raw that

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def sort_list(lst):
-        return sorted(lst)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
