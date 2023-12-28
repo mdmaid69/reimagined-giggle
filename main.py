9a82cbe36ea49c451e91cf97796367125d3f3df1
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:00:26 +0000

    hand peaceful arrange ring guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
