3cf7f7df7e31f444a0bfa6d5708bcc26a4900dae
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:27:14 +0000

    base couple succeed now sense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        print(sys.version)
