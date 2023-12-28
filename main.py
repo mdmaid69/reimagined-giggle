330c459cb3c804973841b1a23c98dc4f1b159513
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:14:58 +0000

    major flat holiday finger rain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
