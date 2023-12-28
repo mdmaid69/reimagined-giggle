53b7da3a9c273e4566706fceec12bd2f28c88402
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:10:58 +0000

    watch yours slip short in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def add_numbers(x, y):
-        return x + y
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
