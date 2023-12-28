614bcaa1939dff37aed8453942413808257b6695
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:54:48 +0000

    effort away drive except exciting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
