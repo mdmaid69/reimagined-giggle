e5c3616e6ddcec78745fa2d8f719580143273b61
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:15:15 +0000

    their dig best purpose object

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_power(work, time):
-        return work / time
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def count_words(sentence):
+        return len(sentence.split())
