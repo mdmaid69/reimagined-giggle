e40cfbde5527f42238c5d81a60356eff47e97d96
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:53:57 +0000

    runout temperature abroad deliver shall

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
