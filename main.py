d2bf5342cc06e25be2e6db772d5246ead7a2c438
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:39:02 +0000

    coat fit shine tidy mysterious

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
