620584d5e6ad5acad70f9ba4c3bb0373c2dd3d28
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:04:13 +0000

    danger gas along scene earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
