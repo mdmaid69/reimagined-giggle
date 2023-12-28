261ce688d9b652e5753272c01f4f452915878289
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:46:37 +0000

    fit field maintain welcome something

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
