1ec7034fd95fb0c1a89e215d99d2a4ac10bc7ecf
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:22:42 +0000

    to party fire afternoon trick

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def cube_number(x):
-        return x**3
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_min(numbers):
+        return min(numbers)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
