bf95ad084a173f01b46e176f20c99c838998fded
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:53:24 +0000

    Vv issue not provide age

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
