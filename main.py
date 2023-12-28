6a2042fbe8b54c5a1da91f38f251680b3de7b049
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:12:58 +0000

    ahead goon comein hope line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
