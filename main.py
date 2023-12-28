806014753ad8d2188b45838997971697b19caf32
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:12:40 +0000

    room visit corner judge part

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-print("Hello, world!")
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
