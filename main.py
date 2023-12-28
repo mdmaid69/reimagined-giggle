ac990fc8160a7b8391a04421a58ccec2fa120f1f
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:32:10 +0000

    really no rubbish brush breakout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
