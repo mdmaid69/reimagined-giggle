adefe497007f03cfda496315fc46deaaceb12ca4
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:17:19 +0000

    process figure country push uniform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def get_current_time():
+        return time.ctime()
