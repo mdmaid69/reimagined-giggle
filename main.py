c67df0f2faff832072e0e79c01271ea6bd7956ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:51:57 +0000

    refer bright memory advise seem

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
