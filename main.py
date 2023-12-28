0412b33cc676f7b0593c14087ff1955ef36c7fbf
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:29:02 +0000

    creature test through yes ability

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  def sort_list(lst):
+        return sorted(lst)
