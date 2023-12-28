45b3f5dda53a068af7e8d24f4487f9f740cc86f1
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:59:27 +0000

    call type lean goback(to) corner

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def sort_list(lst):
-        return sorted(lst)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def reverse_list(lst):
+        return lst[::-1]
