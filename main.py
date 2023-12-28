f1c35a161b3edd2bfd9a8beab871e50835d065c1
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:33:19 +0000

    minimum turnover later slightly lookup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
