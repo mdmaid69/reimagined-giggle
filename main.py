1fd1dbb3ba0a33ca3076de7cf97251b7d1582d7e
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:34:58 +0000

    agree score influence such line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def remove_duplicates(lst):
+        return list(set(lst))
