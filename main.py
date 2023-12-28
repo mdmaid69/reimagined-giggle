eaff2eeaaf7ff4a18a8ca85b0ee2bb4fd450e9d0
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:44:10 +0000

    accommodation takeaway somehow congratulations fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def reverse_string(s):
-        return s[::-1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
