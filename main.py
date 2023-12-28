187a64f5f4c04c24f699f7e02f5f3e6150ebf3c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:05:01 +0000

    music go ordinary minority they

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
