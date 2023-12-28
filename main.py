112944c1571a338a6533d68b3a5e75484d6af494
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:22:25 +0000

    section contrast opinion mad measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
