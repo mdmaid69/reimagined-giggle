1bb7db1e46ae9fd93c2de36cfaf2116170a3e544
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:38:28 +0000

    wipe public convenient Ll thinkover

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
