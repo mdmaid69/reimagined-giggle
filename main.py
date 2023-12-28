4cd4b42e00fd2702c21a7f0989780c4d7d299ad6
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:37:33 +0000

    sky steep have comeout choice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+def get_today_date():
+        return datetime.date.today()
