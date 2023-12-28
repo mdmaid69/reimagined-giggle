d441d872667658b02c018335bcf21f23ef3c3472
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:44:05 +0000

    union sensible reserve speak pattern

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-for i in range(5):
-        print(i)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def append_to_array(array, item):
+        array.append(item)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
