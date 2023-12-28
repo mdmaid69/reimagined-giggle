7b737bf37f37bd9e371bf4ab0f0db9a31968cc23
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:00:23 +0000

    bring walk special ability earth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import getpass
+def get_username():
+        return getpass.getuser()
