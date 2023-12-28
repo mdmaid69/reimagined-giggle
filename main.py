3221577c497081ce0781a2deeb77259955c912fd
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:46:51 +0000

    duty increase emphasis goup makeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_as_int(array):
+        return int(array[0])
