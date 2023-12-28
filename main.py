6b4097cf766e1e89375c6e5562a13493a1b25476
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:10:20 +0000

    he his pick slice help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
