789215dc5f369ee8a7140426de196bf5055dbd85
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:14:29 +0000

    rare spring fold yourself us

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
