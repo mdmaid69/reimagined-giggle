91088f2bbccca9f938e6d68f0901ddbc6a5a11e5
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:24:19 +0000

    Ss last minority rain watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_current_directory():
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import os
+def get_current_working_directory():
         return os.getcwd()
