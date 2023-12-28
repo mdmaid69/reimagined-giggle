78219506db198768c4aa8bbf0b291f4c1e10787f
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:21:30 +0000

    pile set lie temperature peaceful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_speed(distance, time):
+        return distance / time
