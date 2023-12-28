d54609a26f91df0f0ccec585b756b96e4f08dfe0
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:53:45 +0000

    warning ring trip trick goout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_current_working_directory():
+        return os.getcwd()
