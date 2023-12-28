a1da89cb45fca7e5ad027021107320085ccccb34
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:08:36 +0000

    situation sound introduction play Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def greet(name):
+        print(f"Hello, {name}!")
