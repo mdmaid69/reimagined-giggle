2b279104883b2b854c23bee4b43dc791805a481c
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:55:51 +0000

    criticism unusual set level Oo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_acceleration(speed, time):
+        return speed / time
