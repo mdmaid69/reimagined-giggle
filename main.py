d198a04486c48cf2392dd0310d1e83aa0795ac4f
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:47:48 +0000

    dig interfere morning ignore relief

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def multiply_numbers(x, y):
+        return x * y
