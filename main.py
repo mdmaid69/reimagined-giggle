0d9a626d94a6d6a4d4cb7ea97b809b7bd601bcb1
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:13:53 +0000

    frightened persuade terrible negative feel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
