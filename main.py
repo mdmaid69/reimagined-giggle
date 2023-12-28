ada4466f5c9917c3e523edd9f581b86537128045
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:01:11 +0000

    ago left spoil object habit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def greet(name):
+        print(f"Hello, {name}!")
