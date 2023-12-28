c538723acb09cccafa1f312050e591425c284abe
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:55:50 +0000

    country work design description time

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
