5461ecec4e57c6f05b1eccee1e72b6cece28e236
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:57:27 +0000

    shut bringup instrument experience more

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
