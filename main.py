5e1e138b251117448cafe2721b36561e7e2b37f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:24:18 +0000

    mark children year rough play

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def reverse_string(s):
+        return s[::-1]
