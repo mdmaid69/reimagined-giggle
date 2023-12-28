853b72f1cb66ea8a258d8a743a76103339f91f52
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:07:08 +0000

    picture among original whistle foreign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_array_itemsize(array):
+        return array.itemsize
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
