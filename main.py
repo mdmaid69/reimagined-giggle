ec0e1855530f7ad5bc2178afc3aa2b4b31947e9d
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:42:35 +0000

    of development form lead already

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
