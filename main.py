8d1501527fba34fe61156931b37c4a8e702131f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:14:07 +0000

    bus shout positive reject lead

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
