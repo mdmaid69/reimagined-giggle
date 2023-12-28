e942197cb1fcba97eff05f9cbfae65daebe89062
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:14:21 +0000

    seem wipe opponent arrangement detail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import os
-print(os.getcwd())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
