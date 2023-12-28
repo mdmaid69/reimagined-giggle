31c340f0f3a2d853045cf3972443fa945664ae1d
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:51:41 +0000

    arm dark keepup under path

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
