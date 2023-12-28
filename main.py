352d4993d134774c78da9b76f9e356db9df235eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:51:30 +0000

    create stretch notice mother turnback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def reverse_list(lst):
+        return lst[::-1]
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
