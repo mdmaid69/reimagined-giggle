6100bf516e3f735e91f7be215b89214d79741f56
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:21:43 +0000

    unable butter past Vv car

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
