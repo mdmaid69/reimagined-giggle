150660917bc68f1d4247878bc197f9deb8c6fd99
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:20:53 +0000

    break bill declaration autumn whether

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def sort_list(lst):
+        return sorted(lst)
