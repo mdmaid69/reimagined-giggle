8edca69cf8f8df19dc8e4aedd30a34bbddacd291
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:55:13 +0000

    of exhibition test pity indeed

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import sys
+  def get_python_version():
+        return sys.version
