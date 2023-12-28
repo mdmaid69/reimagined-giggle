a5ba41f3bb230096cfb226522fea745e3651a0d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:43:10 +0000

    party breakout disturbing copy turn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
