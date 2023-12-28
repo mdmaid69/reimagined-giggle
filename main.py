353355e9c3cace4021cd21d9efbd70ce8bed5403
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:44:49 +0000

    support instruction approval gap today

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
