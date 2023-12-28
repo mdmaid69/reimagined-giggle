100979ab178d8e671a45dcc5ddd28aef1e0fe912
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:50:41 +0000

    emotion method unfair impressive stone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
