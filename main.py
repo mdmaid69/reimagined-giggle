d166cb7463a0b508f9073cc29890b703f315a4b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:08:05 +0000

    mistake usually recommend price flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
