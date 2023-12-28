d8ae310161ca1a33861e3666b0b67ff48d663534
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:24:53 +0000

    previous movement similar rest general

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
