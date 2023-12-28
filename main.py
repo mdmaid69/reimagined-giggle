cf830e89517b8032689d361ea51a5a415ba87f14
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:40:32 +0000

    this above original square no

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
