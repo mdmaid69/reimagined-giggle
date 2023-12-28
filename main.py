132ddeff37241a37dfab60ae6c8cf58f96468373
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:13:16 +0000

    start business what intelligent afternoon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import sys
+def exit_program():
+        sys.exit()
