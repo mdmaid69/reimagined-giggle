ebece87d84da5ea2df3e3b98df988a8f04b32dfb
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:22:00 +0000

    Itself programme rise main union

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import random
+print(random.randint(0, 100))
