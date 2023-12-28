34140c5c346eccbe55f6f3bd15faa26521a25439
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:09:43 +0000

    hungry sentence thick please pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
