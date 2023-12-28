fca9a98a0aa32f3daef705359e1c9355c52d1049
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:56:22 +0000

    factory past taste reduction sport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
