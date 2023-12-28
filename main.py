894a20f341a9c33f942a663a592bea8cfd64d438
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:42:56 +0000

    introduction by suggestion lean well

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def reverse_string(s):
-        return s[::-1]
+  def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
