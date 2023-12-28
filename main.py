761c652a1ebbce11155a0ab593e3f7d9db956037
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:21:14 +0000

    along row familiar food protect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-for i in range(5):
-        print(i)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
