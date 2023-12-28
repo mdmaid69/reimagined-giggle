470e5f9d796bd28e0afb44b0fb74ba4983e25538
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:53:07 +0000

    entrance course show callback favourite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_max(lst):
+        return max(lst)
