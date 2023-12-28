d582f5b2725d49f8fdea97dea94f7ad0bfb20417
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:49:13 +0000

    possibly slight ignore suddenly perhaps

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
