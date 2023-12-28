fc1835b56985a6f7f527bc20f4401d27162e0d22
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:47:33 +0000

    reach wave strange practice live

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
