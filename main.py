ad1fad1d240205832b32503ed0662ba48ac04078
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:49:51 +0000

    direct national error definite stir

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
