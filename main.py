89bdce17004d1b66fcfe33e4ff37386ac8970c45
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:38:53 +0000

    date content although argue light

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import os
-print(os.getcwd())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
