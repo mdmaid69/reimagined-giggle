be13f173cf1875669393cda7c87975d6ffe260ff
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:27:21 +0000

    unusual regard careless some answer

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
