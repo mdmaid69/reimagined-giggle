5667034cdf2fadff1059e1a8c94b84ca20e630eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:28:56 +0000

    bite declare person silent yet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import getpass
+def get_username():
+        return getpass.getuser()
