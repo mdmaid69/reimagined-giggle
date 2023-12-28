8174c9ba97e4525e691938218bf08987a9c880f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:24:21 +0000

    importance beat foreign habit reaction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
