6ae0f28a0d301a764ad2c484c0160cff05c1c063
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:27:21 +0000

    brown about discover Tt meaning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def is_palindrome(s):
+        return s == s[::-1]
