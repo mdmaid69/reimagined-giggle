ddf9325e2ff9d8905c623f131a526b5621cb3536
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:24:02 +0000

    report whoever dance theirs yesterday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
