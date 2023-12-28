f38681b760431ed89c3025da3faf5a791c3d4b85
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:16:11 +0000

    clue talkoutof bag safety recognize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import sys
-def exit_program():
-        sys.exit()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
