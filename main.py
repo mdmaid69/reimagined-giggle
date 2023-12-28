955ab452b6dd9ee14eaa307ad9bc50e1c5bbe7ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:27:52 +0000

    aware nonsense terrible after weather

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def add_numbers(x, y):
-        return x + y
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
