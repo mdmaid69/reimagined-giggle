86ec134aa8ad54532aaa80355fe3a7a9e55b8c24
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:48:13 +0000

    quiet frightened pressure treatment convince

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def reverse_list(lst):
+        return lst[::-1]
