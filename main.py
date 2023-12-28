37773702efd276c8589e40751ca8f95c721ff883
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:10:01 +0000

    pretty few like goodbye century

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
