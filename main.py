1cee3118a8e7c0526a5623b3d3885e97db3d52ba
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:14:34 +0000

    very mad hide disagree purpose

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-print([x**2 for x in range(10)])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
