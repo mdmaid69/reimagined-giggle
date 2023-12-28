10fc1aecaa668878279fc792cbed85679fb51ea2
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:00:26 +0000

    callup somewhere provide supply open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
