e2969325314af6523c6b2bf6f680197952f95a6e
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:38:17 +0000

    lookaround foreign collection birthday geton

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
