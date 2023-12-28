1d90f6c7ae6d99b56dc32386a3e62a440e077d90
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:58:49 +0000

    copy certain child top unkind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import sys
+print(sys.version)
