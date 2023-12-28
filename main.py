261212bd8ab08192b3f0f8a34523faecff33db1d
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:19:34 +0000

    post present expensive getback fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
