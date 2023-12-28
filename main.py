a21acec6347a12aa04b1a28db02eb86111f181e9
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:11:47 +0000

    thinkof past sight fix next

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def append_to_array(array, item):
+        array.append(item)
