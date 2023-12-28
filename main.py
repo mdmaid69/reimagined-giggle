56ef82b6eb73d5ec1cf3b5334e412151a1049b1a
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:41:38 +0000

    win children Vv string organize

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
