fdbe3574365dc6e7cecb5b30c3401c252454ef39
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:47:39 +0000

    forgive disappointed plate half upstairs

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_work(force, distance):
+        return force * distance
