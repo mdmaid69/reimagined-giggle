6613111f8b8b6796744691e8f6bfbf8ed22f46b0
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:36:45 +0000

    pretty centre 573entertainment journey sight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def cube_number(x):
+        return x**3
