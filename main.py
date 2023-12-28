1761bac156795883382517247c7d4d7154638642
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:06:34 +0000

    increase fierce favourite close miss

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
