ebb026c5181f18b5446466c9ce3e0b9651b6acff
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:37:37 +0000

    567enemy ask contrast beneath throw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
