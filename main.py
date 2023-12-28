bb543c650262241ea41a6917973af3a69a24ce1c
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:43:45 +0000

    whistle question back consider chin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
