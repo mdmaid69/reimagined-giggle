bc2e5764a2de49d97cd43c7a9b1affc4c5b26f09
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:48:51 +0000

    wander know none sad process

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def remove_from_array(array, item):
+        array.remove(item)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
