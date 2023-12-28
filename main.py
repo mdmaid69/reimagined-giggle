a698abb427681d742b1c5c5313de6c2c1d28769c
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:27:20 +0000

    ring reduce next turnback there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_as_bytes(array):
+        return bytes(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
