bdfa4c42d0ab1b765538f82b7e8f2472360e2ce7
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:07:00 +0000

    neck lock custom alive body

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
