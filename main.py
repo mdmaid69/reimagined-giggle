c22054aa3e5bbc145d80a9ea2ecf7fabb10664cb
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:06:31 +0000

    protest eye relationship dry active

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
