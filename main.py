c1184f880368bbfdf96aa12ececddda27b3c067f
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:39:47 +0000

    describe forever relative estimate wave

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
