21c52146316d4d987590bfb9d881b63357effb2c
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:00:58 +0000

    label copy rubbish getoff reserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def find_max(numbers):
+        return max(numbers)
