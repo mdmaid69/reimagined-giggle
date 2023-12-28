0d938cc5543bbee94811ba09ee69c5517dcedd8a
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:42:09 +0000

    hour freeze news smile collect

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
