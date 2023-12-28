95b257ad575308a6f23473b4d6f3f96c9745e71d
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:16:21 +0000

    guess soon fun refuse matter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  def multiply_numbers(x, y):
-        return x * y
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
