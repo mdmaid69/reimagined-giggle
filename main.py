3edbfebd5221926c5d2636a58979392fc1a8e4e0
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:49:46 +0000

    instruction number funny appeal nice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
