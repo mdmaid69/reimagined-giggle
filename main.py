364c226fc9f58162fa850c70d0726f0960d56f38
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:08:07 +0000

    runover chance tap wave mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
