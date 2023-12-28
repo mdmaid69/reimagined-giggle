db5e79cec6b15e5447bfcefee7cf2f22728ab2d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:25:03 +0000

    behaviour sink disturb minimum advantage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
