89c506d2b17d9600a33cc874e4100d743d89c3c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:00:07 +0000

    big guilty uponon snow Oo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
