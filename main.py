47befbd530608801c635ea689778b04a2dcc8018
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:59:49 +0000

    ticket experience begin peaceful Yy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def find_min(lst):
+        return min(lst)
