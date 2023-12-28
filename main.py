f2280025dc5b8d80478317d3e991a7e9effa547f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:32:04 +0000

    way standard seriously string street

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-for i in range(10): print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
