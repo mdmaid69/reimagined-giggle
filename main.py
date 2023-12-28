c611a79d53171209850af46d2eb54d2c1fcd3417
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:07:07 +0000

    difference home wherever roof shape

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def convert_to_binary(n):
+        return bin(n)
+import sys
+print(sys.version)
