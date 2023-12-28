a827a9985439c9e1a31a5de39050220d9690e521
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:35:43 +0000

    flavour idea perhaps moral pleasure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Characters:", len(text))
