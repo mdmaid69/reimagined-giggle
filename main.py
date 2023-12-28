59c67da7406df137879d39ecde15f0f715abc6a2
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:17:43 +0000

    knowledge transport deep soil practice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def add_numbers(x, y):
+        return x + y
