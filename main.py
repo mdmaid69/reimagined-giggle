7bd3d775df1fe5340e6ca9f934e3ee2960bf4564
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:08:34 +0000

    understand fast guess right remove

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def convert_to_binary(n):
-        return bin(n)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def greet(name):
+        print(f"Hello, {name}!")
