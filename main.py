ec7fbeea92a60be37e5a38cbc328adbbb20e7274
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:12:29 +0000

    bet subject there rest circumstance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_gamma_function(x):
+        return math.gamma(x)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
