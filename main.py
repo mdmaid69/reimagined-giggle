b4060a7ad1ed3ebe4f1c69e1501694e16fe56e7c
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:20:26 +0000

    objection somebody this shape question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_max(numbers):
-        return max(numbers)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
