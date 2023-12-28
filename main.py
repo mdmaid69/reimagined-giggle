20563b38e8c72c18112d295e46bc78f6320e4339
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:45:41 +0000

    break let pencil coat yes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_list_from_array(array):
+        return array.tolist()
