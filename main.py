5e12f86197dffa993c883c88808f725ddbc3660b
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:29:20 +0000

    cope house ago their country

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def set_array_item(array, i, item):
+        array[i] = item
