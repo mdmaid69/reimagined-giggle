0ec3a4c7d155d25ddf7dc0877eb22468eec4996f
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:35:09 +0000

    light moon dig estimate order

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print([x**2 for x in range(10)])
