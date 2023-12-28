d7d5e6745595916bbd11c2f053da4f650cdcd87f
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:02:50 +0000

    takeover risk anywayanyhow runover telephone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_list(array):
+        return array.tolist()
