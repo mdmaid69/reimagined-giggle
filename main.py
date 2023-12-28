6736f94f1f73363d79c7593718aaadcd8ec1b53f
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:15:23 +0000

    capital thumb central morning move

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
