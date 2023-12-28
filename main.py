234d33d391d49bbb8a552041e83a75c79a94df9d
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:51:32 +0000

    fold wake call swing town

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
