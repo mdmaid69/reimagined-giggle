2f914faa2c67354e32883a4e8453cf8ee8ad2d1d
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:43:51 +0000

    interesting broken sick brown various

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def get_array_as_bool(array):
+        return bool(array)
