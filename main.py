f2ec94fd68f6f5473bc65f3a50e5f5b8518fa30f
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:07:03 +0000

    draw open second repair keepup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
