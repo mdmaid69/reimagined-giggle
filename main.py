badbf56e56edabcc47ede5c4a9d3d03a781800ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:54:42 +0000

    worth tidy head important roof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-name = "Python"
-print("Hello,", name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
