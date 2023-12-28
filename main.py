727aa81f845b69472e127b250742e2640c7e1e1c
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:47:04 +0000

    cap 566end congratulations giveup generous

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import collections
+def create_user_list():
+        return collections.UserList()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
