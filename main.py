bf93485b637ff737f20c14b10de6445e729ca09b
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:38:26 +0000

    short to event broad wish

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
 import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+print(time.time())
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
