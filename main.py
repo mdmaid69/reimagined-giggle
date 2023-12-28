ba0a28415e794f661a8f2a85ef5070f254a1fb15
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:34:30 +0000

    some concerned for group at

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
