8b8815898df3b337b89f6a3be1c8a1f1840c3ae8
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:29:42 +0000

    suggestion right lesson drawing clothes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def convert_to_octal(n):
-        return oct(n)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
