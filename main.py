f1e7c281b3a758d8bb8851bbb48113aa5de7b80e
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:24:51 +0000

    each taste carry butter regularly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
