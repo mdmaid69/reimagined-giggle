b3c44c81afbdcd5a4281368a9f7ada81ef33ad77
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:06:30 +0000

    567enemy list recovery storm funny

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+print(sys.version)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
