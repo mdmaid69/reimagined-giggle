9baf9de95d54ae1fb10b20c6947fdc54fe72c96e
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:47:18 +0000

    guess mix false add live

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.time()
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
