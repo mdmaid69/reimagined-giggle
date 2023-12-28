8f3e2347c6c7379eadcc97d42f0139bd577b4091
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:05:55 +0000

    noone add drawing make inquire

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
