f3477a73d85a348b76d936d4ba25aae74d94b7ba
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:20:41 +0000

    piece Bb set crowd control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
