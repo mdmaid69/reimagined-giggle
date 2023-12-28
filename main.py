03d0ba08e2b1c230ba925ac97a6c92440b63238e
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:41:46 +0000

    story spoil step process obtain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
