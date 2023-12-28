52748c87feaa16b330acea056c0f74b389d473ef
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:32:57 +0000

    own used perform muscle Ii

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def sort_numbers(numbers):
+        return sorted(numbers)
