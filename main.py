c66b57bcb194adc0d5d9bad401b459ab46485cf2
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:39:49 +0000

    quiet tap should machine satisfied

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_as_repr(array):
+        return repr(array)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
