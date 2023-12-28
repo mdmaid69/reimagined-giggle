5aabb0492353493c684f3db161be8cdce8cdc293
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:45:54 +0000

    alone fetch wait connection issue

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def square_number(x):
+        return x**2
