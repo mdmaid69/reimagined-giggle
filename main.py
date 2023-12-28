1f2a18ac87c5803aef39fff970a7f5b59c46026f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:40:14 +0000

    show charge wrong telephone down

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
