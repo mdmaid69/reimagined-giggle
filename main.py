c6551d3c12bf96d86e5c796255ebd27ee202ac3a
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:30:35 +0000

    save stir stay deliberate union

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
