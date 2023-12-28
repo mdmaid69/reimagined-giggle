7fe55e7d4032583da60f7b0e0a415c4da2722bed
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:07:47 +0000

    scrap fresh part crime takeback

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+for i in range(5):
+        print(i)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
