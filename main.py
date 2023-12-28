9f5a4aeadba880f2fbe9487efb5c00f208110544
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:47:32 +0000

    meaning lunch tool song hurry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_sine(x):
+        return math.sin(x)
