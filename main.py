d690afe7dba50de9685c6760ffe3d97021bc8026
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:01:46 +0000

    load joke coat cruel polite

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
