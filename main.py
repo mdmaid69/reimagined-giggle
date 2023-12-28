adb545ccb96a64c993461f8a5cc42d8daaa99a7b
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:11:17 +0000

    dream decision lucky complaint heat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
