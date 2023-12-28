c10db83d643877db955ed4c59a9b5083b7686995
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:31:02 +0000

    right hot outside male Pp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
