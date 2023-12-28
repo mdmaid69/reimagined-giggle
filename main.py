8c81889050b8d640fcea9d5b1afe83f8df728315
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:22:28 +0000

    boil bringup free next flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
