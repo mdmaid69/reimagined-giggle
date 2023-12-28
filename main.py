9e4f1485d53b69cfccf8a4e0f2dce348ffd0c705
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:22:00 +0000

    comfort life busy birth news

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
