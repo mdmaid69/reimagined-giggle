9fcc2daaf3e02e0421133a9c74c7c7287c8b433f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:21:40 +0000

    up comeoff typical proof noisy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
