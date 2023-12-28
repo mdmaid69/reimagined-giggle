574b4fb0774d7aa93efb182469fe8c94e83cc4c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:09:52 +0000

    long means relax corner pupil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
