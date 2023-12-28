6aa19f72d7dd52337cbfe0bb01935d8bbc08d438
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:38:03 +0000

    control practice dig rank short

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import sys
-def print_python_version():
-        return sys.version
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
