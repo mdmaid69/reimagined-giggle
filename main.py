179356344cd767a60a652b3ccd2c0d54d081b7b6
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:45:54 +0000

    before television crash equipment hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
