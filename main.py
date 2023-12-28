cef9b89050826bd151ecbd905679ab52f817ee89
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:55:45 +0000

    word borrow lie steal other

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_memoryview(array):
+        return memoryview(array)
