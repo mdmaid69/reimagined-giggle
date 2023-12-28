6e19dd9dc7dc1dd147f33053b9e60b72da1c2cf4
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:03:19 +0000

    comfort town smoke confusing but

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import os
+print(os.getcwd())
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
