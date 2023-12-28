b0eec42a26a093c82544fe5c596604d2d8d56fee
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:54:23 +0000

    mine home too soil defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
