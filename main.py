00c177a49cc3585065a06fa1ab7207f8810b4f33
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:55:33 +0000

    drop ilovedicks during children pen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
