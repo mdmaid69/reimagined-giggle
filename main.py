2f94ca57de75c50d2655a6cf9e9c185733803748
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:06:26 +0000

    instruction some skirt murder cope

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
