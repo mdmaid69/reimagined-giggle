b823956f27e8b8af61f924725cb847b4b1968fec
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:51:17 +0000

    story money ordinary brain keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_length(array):
-        return len(array)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
