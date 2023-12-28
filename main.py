da51256b5740a0e0ad8e0a9a2e4dede9fb65f065
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:15:14 +0000

    exchange report event age common

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_typecode(array):
+        return array.typecode
