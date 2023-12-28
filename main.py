f4cba771873e83231d7d52f85efe4eae1717a6c3
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:56:22 +0000

    frightened children matter similar distance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def convert_to_octal(n):
+        return oct(n)
