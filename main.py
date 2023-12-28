4370da50a52eb0fbb4e3b86f8a4791f55e06c6e2
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:48:15 +0000

    purpose please careless press pretty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_as_str(array):
-        return str(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+print("Hello, world!")
