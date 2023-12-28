0adc05e5128b29f8320d82773ac993e6dd24ed42
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:38:51 +0000

    must unfair note recommend tense

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def is_even(n):
+        return n % 2 == 0
+def reverse_list(lst):
+        return lst[::-1]
