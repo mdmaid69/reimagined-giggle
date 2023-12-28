47779ab763e32efacf198cb758617090d35c4b50
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:48:00 +0000

    partner top among fold regularly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def square_number(x):
-        return x**2
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
