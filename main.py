f633ed6eada69abbcf512748e141f8f974b26ef5
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:17:21 +0000

    traffic damage send of serious

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
