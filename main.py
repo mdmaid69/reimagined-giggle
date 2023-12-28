4581a4cc8ce98df10742093ab0d92680430f25d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:12:06 +0000

    its greeting whatever who face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def reverse_string(s):
+        return s[::-1]
