b94b665025a7cd507df17dce41c6daf42b4b5a82
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:17:01 +0000

    lower performance tooth study search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
