c86c2ab23dc3119fed352eff8a4edf3963d651ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:55:11 +0000

    spray recognize cheap impression react

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
