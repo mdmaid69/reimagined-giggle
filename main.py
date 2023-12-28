65d8067584312c9a9bb8cd693613cc54110d066a
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:29:38 +0000

    occasional comment regular proper whisper

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def convert_to_octal(n):
+        return oct(n)
