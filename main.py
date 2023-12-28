95451ef19d8dfd2dff9df30b2423e19aa5bda271
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:20:51 +0000

    nonsense 572enter we flavour adjective

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
