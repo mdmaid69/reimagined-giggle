3c631844833798c4c570f7ba440130ca802eee9b
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:10:34 +0000

    motion difficulty bus force advantage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
