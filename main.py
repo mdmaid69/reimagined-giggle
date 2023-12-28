25f7dd0d4d56664e243ecdd94ed3d43e6b09cbf8
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:31:04 +0000

    lay less pin shop memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+name = "Python"
+print("Hello,", name)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
