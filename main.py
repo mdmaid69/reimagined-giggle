80996e420b5f72029e11ee945205b1d7f11193c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:58:48 +0000

    characteristic afterwards address sport hurt

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
