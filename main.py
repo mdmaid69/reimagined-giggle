5fbe4b96eb85e38ea930c14d158259e1f070b905
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:25:07 +0000

    workupto judge future spring hello

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-print([x**2 for x in range(10)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
