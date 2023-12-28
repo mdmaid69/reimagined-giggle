5a5dc5b3b83b34d7d46a0a4c2f801fab1421e196
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:50:54 +0000

    block share swimming grade chat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def multiply_numbers(x, y):
+        return x * y
