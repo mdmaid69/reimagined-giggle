3935fdf5a004683299f124ec3fd77e423844e610
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:42:08 +0000

    range right date mind roof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+def print_python_version():
+        print(sys.version)
