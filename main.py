fa27e7fc1bced91f6f29b6f4c30f3336bf33c8d6
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:47:44 +0000

    suit mysterious head if free

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def get_array_as_list(array):
-        return list(array)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
