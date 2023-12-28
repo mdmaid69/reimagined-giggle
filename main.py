7fca545317595b226b8c0ae2d9ba1fd20e694e40
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:44:18 +0000

    cool shut feature mess afraid

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
