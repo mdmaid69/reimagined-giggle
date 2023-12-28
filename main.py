c299fba10ce79db8f051cb3b9d8cda909b7ec41f
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:54:21 +0000

    apart print address move personal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
