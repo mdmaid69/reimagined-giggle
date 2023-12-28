7ecefd2a6444be35df38939ce2dec1fda9f18bd0
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:47:13 +0000

    paper hurry recent flow process

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_modification_time(file_name):
         return os.path.getmtime(file_name)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
