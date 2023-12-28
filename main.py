7c923692d3bcc314078d6e0a1598fcb991aa2dcf
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:52:45 +0000

    moment damp rather sticky store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_acceleration(speed, time):
+        return speed / time
