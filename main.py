f05e4a7ba6373fe087a6f13107e1bb32bd805df7
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:46:51 +0000

    value other deliberately custom extra

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
