0b63b4cd12703f0b5f0bb46d38226985023fb9dd
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:32:18 +0000

    deliberate office completely announce official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
