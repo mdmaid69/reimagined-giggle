f92131cadd0eb0dadd4e1c758120698fc258f6b8
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:05:41 +0000

    business eye importance regard use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_acceleration(speed, time):
+        return speed / time
