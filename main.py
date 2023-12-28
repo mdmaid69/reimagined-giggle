99ba6ea7d764148750e316e46cff1a975ddc16ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:12:12 +0000

    comeout opposite switch mess left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
