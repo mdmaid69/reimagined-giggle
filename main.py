024d2e05c062b93d26c25d54ceca21ea9eab723d
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:03:37 +0000

    set proper thin depend spring

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
 import math
-def calculate_pythagorean_theorem(a, b):
+def calculate_hypotenuse(a, b):
         return math.sqrt(a**2 + b**2)
