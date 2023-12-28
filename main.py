cffaddea81393f64db1aeb3e61aa666335946985
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:20:21 +0000

    waste finger content beside turn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
