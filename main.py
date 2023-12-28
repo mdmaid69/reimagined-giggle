981134c5c77e2584490be81a0ea2c09a8f7ad3f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:13:03 +0000

    takeback produce together bend wish

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  def convert_to_octal(n):
+        return oct(n)
