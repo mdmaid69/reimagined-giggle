cb941a08bfa0ded8118e97f5258a021cd0c153f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:11:46 +0000

    life after whistle slice broad

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def cube_number(x):
+        return x**3
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
