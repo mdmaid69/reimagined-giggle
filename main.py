d5450264bf1d77e951a22715d54c1b5ecdac31a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:08:17 +0000

    man answer extraordinary otherwise confidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
