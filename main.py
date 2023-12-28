398688f453ac138291127412d23a022d5f059139
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:06:43 +0000

    lookaround vision sight cover off

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-def convert_to_binary(n):
-        return bin(n)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import numpy as np
+print(np.array([1, 2, 3]))
