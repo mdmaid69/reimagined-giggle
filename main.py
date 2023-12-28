5f41eaa8d9aad7185a948281e328529599251176
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:33:16 +0000

    transport dance one sell protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
