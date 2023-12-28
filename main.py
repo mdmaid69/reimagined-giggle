113b793b2e313586d5473a5d94c64373761a63ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:34:39 +0000

    rate mind comment discuss positive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
