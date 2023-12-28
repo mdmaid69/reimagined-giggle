1f0fdb727e7902f9ef0bf22c6240820acd7d1f21
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:45:34 +0000

    bottom intelligent stage afternoon only

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
