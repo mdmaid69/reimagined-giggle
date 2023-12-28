955adc1060fc55225792409820dc90e461351afc
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:43:30 +0000

    giveaway address represent raw draw

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
