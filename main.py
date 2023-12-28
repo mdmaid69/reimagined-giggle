029ad3e140983cae28ead9c810240659739d95c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:40:25 +0000

    bottom unlucky child confuse think

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        print(sys.version)
+import platform
+def get_python_version():
+        return platform.python_version()
