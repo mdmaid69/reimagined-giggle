36e758d55c6d389137f1008ff946c79642f44f12
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:43:48 +0000

    bag stupid left introduce instruction

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import tensorflow as tf
+print(tf.__version__)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
