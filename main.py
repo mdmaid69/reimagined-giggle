cb1e32c3a6f70fddada977f9da3fd55fc7bac666
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:35:04 +0000

    physical complaint murder distance punishment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def multiply_numbers(x, y):
-        return x * y
+import json
+print(json.dumps({"name": "John", "age": 30}))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
