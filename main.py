2e22bcb825a7520c8c24e111a8a18634fc71dc36
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:30:51 +0000

    wipe thick wonder same turn

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
