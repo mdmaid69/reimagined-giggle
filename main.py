fe4c876244320ae2486187d7c61bf0a635ef868a
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:49:00 +0000

    scene chin hate change twist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import collections
+def create_stack():
+        return collections.deque()
