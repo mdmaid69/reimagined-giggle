6527e9dac5fe2b55b82070cad3732c6121e9624d
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:49:33 +0000

    poor pick considering steep event

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print([x**2 for x in range(10)])
