fd76951c9cf58a6ebfa5ad01a597eaf0b33f7d2c
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:53:12 +0000

    bend busy rest definite commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def count_elements(lst):
+        return len(lst)
