5d5e03aa4c79d98bdc0ee0b44ce3891e996f454c
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:27:59 +0000

    healthy successful trap recovery breakdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
