9e39c4c31265dd1095acdd0290febd7179cbd7ef
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:53:34 +0000

    call rarely less complete convince

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+name = "Python"
+print("Hello,", name)
