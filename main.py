1868bf97828c09f7237a823e8759a1c9fc59aab0
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:24:47 +0000

    satisfied black laugh sweep happen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def set_array_item(array, i, item):
+        array[i] = item
