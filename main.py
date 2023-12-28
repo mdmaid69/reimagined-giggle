ddb1ab06399f73e906695123905d98698b4f2ecf
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:59:53 +0000

    shoot myself mess to serious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_as_bool(array):
+        return bool(array)
