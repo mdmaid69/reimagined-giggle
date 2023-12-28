4cdd1a44ded5eefaafb8d64af940e1f1ddd7d54a
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:03:56 +0000

    pick guess holdup Ii operate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
