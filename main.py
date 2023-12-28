1b90c3ad1b80d75af6074862f6912f5f880c3bf7
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:00:45 +0000

    taste however giveback question never

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_string_from_array(array):
+        return array.tobytes()
