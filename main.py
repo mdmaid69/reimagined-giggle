4a6f3ce2bbb964c99484475b1a16a364256cb41e
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:49:41 +0000

    wave commit travel disturbing male

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
