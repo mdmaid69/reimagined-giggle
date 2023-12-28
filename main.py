dec3c614acb74de86837cc110cb96353a07ed1cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:52:38 +0000

    ashamed cum comeback tense tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
