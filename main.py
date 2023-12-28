f6d84a0858b779de4d4443f7f887bb5ed479f680
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:44:11 +0000

    stop keen light manner car

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
