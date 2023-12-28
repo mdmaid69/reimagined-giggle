0066daea4381a4367937856933ba79bf0c98b061
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:47:14 +0000

    recently boat sound pin criticize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def print_python_version():
+        print(sys.version)
