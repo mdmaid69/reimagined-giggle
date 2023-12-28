d018a88ec190ae803066837c8b67783ea7e994e2
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:32:39 +0000

    clear uncertain dream left together

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-name = "Python"
-print("Hello,", name)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
