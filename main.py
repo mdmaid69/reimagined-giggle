c616840deb8c403a9a46c2d41cf10d19cd0037c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:47:55 +0000

    lay leave beginning trust hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
