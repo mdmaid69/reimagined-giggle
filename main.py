30d5921da3b929d9fd37bcdc1b67aef252b80005
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:07:12 +0000

    near goback(to) appear connected actual

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
