bf50178f78ee689b26160f3ee04a0823ee28f216
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:25:56 +0000

    rare unfortunate Yy lesson rain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
