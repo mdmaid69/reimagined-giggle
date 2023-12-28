f2702074d3e23d0b082a5a81964e40ffdbdcaa51
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:08:09 +0000

    depend owe badly meeting personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
