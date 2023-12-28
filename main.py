5fc5f087aeebe984ef6b38bd9fd1b47eae80d00e
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:31:55 +0000

    instruction justice invitation below backwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
