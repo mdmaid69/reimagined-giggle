84899166017a5e97a0cd8d2d666e41c6bda9b3a2
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:27:05 +0000

    must bin rid possibility joke

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_work(force, distance):
+        return force * distance
