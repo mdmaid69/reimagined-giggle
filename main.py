8c188da4bd72314ec64824c95fae93e05e0fc0ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:54:25 +0000

    my thin naturally warn rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+print(json.dumps({"name": "John", "age": 30}))
