20fb115c71c41a010c73b165739cd6625b4f1850
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:11:20 +0000

    prevent last 564encourage behaviour uncertain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
