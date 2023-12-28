32f800b36bb57855645e16529eafa4abde008a7f
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:39:46 +0000

    drawing discussion follow boil left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
