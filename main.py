6942024112ad57cfd6f936d28bdfd14f39edaedf
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:44:44 +0000

    let believe weather begin flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
