3900c126598a12a9969368dda23da2978f303613
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:09:06 +0000

    bin position through sound all

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
