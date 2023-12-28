d8d8655206a0e60dcebbd9289c22eb94c77b15c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:46:53 +0000

    drive pull trip thanks public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
