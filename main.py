b23be3b7fa757d22cdc8dcf50d91d26c8bd5a756
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:58:49 +0000

    squeeze reliable arrange wrong turndown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def clear_array(array):
+        array *= 0
