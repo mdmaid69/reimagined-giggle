b5c5103ebdbdece01afbd65a5db360ae141eb69a
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:36:26 +0000

    raise suck many firm its

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_volume(length, width, height):
+        return length * width * height
