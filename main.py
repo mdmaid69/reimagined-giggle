8529720995c31527c19c80bcf27de6bc09ea37a5
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:51:30 +0000

    cheat beneath blow back catch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def convert_to_binary(n):
+        return bin(n)
