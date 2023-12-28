fa664e2c88a5fba8fead51227d16fc7372c74b02
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:19:14 +0000

    over wear state getoff treatment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def reverse_array(array):
+        array.reverse()
