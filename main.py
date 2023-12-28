6ec3ea2c4fec3c7588f651b2ceeaddcd5641d166
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:02:56 +0000

    character declare finish commit basic

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
