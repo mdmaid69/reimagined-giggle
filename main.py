c8c251e65839890efc8cf8c7cf527a718530694f
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:44:16 +0000

    die measure takeaway what here

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  def sort_list(lst):
+        return sorted(lst)
