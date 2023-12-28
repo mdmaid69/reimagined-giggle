a708af796f4eb34c600f40ef8dcc56d44fddbe9f
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:38:44 +0000

    family doctor bin too but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_item(array, i):
+        return array[i]
