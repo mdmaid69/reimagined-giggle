3d8632cc27747dbb34d5749eb5ffdbc7f392e944
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:50:37 +0000

    area then pretty start store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
