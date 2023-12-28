ce3b0921e7459ea56729dcd2e5b0c4a1fec284d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:29:51 +0000

    reply noise similar properly calloff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
