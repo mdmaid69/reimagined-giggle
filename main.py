fd476c3744100b8b221b62b613740548e3376cda
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:25:41 +0000

    compare dream song furniture large

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def add_numbers(x, y):
-        return x + y
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
