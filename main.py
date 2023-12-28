2063302a94f461db6a04c5552930c0895fb91a67
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:12:34 +0000

    computer often thorough sudden dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
