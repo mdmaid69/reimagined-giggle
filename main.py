ff729ec46d225e326bb6282add5d17a8015e71c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:22:09 +0000

    face organize arm correction success

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
