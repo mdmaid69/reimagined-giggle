02b7666255a03119c7349b8d4bd1da4cd1d731ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:13:28 +0000

    comeoff full tend light public

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
