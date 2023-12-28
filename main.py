51cfc27bc287316d5c5ac99ddd24a32a85027b70
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:07:43 +0000

    nor keepup use market getoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
