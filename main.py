683c4fd78e9f726e47f050544b9faf3b442addc4
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:09:15 +0000

    respect table prove freedom imagine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
