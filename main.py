4f7a9aab63b80f350c0da06bb1db00f109dac356
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:38:33 +0000

    reduction goback(to) large doctor manner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
