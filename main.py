75c99b1302f68fd0da6412109f529ae1c3a55a25
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:35:30 +0000

    basic extraordinary listenfor rob relationship

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
