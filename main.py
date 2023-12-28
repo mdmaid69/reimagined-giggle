b01b337400ff93b8793a2beb70027498d761b6e1
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:05:44 +0000

    565end scare everything open university

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def find_min(lst):
+        return min(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
