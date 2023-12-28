e0f7dfc20f77c95c5d40dca66b71b147977da3e9
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:44:20 +0000

    below since every flat thinkover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
