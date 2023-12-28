d219ae7f18e7454a5370400eb975a53fb0e07b3b
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:44:58 +0000

    worry slowly quality what surprised

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
