e37e7a60308016e5b5037a8ec4ab47ed668b4238
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:09:22 +0000

    invite towards rescue backwards both

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
