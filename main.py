9cffb329e360add255324b5944ddf9a95131df48
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:04:44 +0000

    right recent watch consult row

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_counter():
+        return collections.Counter()
