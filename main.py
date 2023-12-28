aabc6afb51f2d5527e28ec8761f4063a5a4c3f37
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:31:41 +0000

    except hurry future mistake disagree

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-from collections import Counter
-print(Counter("hello world"))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
