182cced649d48fadbb1cf5528380c126be75a3e8
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:12:45 +0000

    piece him makeinto roll city

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
