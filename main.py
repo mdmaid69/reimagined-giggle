2f30961f82fa92328addfb723e59d6a4c779cc9c
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:07:29 +0000

    noise continue date careless lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_pressure(force, area):
+        return force / area
