780e73aec045b93ee2a3fbe63ff9c2202623c0b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:33:06 +0000

    organize makeup drink value about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
