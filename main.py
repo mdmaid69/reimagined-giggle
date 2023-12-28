00508b0f2774682ecbdfa5fced065c3621981d4d
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:12:17 +0000

    replace report bringdown feel exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def change_working_directory(path):
+        os.chdir(path)
