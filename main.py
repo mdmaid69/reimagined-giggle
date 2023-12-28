84ddd65961d6dce905147a1fd10d5e63b067a61b
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:56:31 +0000

    confusing cost measure lot subject

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def add_numbers(x, y):
-        return x + y
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
