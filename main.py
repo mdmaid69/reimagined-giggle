5db5f240cab39a0280caf78392101eb634e59baf
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:19:59 +0000

    bite spill rate alive never

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+print(sum(range(10)))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
