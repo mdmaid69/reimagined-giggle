992361b181a329636d07ebdf2a1758dd9a77a442
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:50:29 +0000

    take occur key reasonable use

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import collections
+def create_user_string():
+        return collections.UserString()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
