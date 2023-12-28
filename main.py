0af2bd6e0f8eacd591b972b223937944bc4a6a04
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:16:14 +0000

    refer tradition usually pass minor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_base_name(path):
+        return os.path.basename(path)
