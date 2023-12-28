0ec9df6b3b678f141043dac950fcfb79b1449a05
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:17:14 +0000

    scream complain bar godown national

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
