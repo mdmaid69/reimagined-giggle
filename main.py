788b88ec8782e0ece0bb3668310dc1efc56524ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:37:22 +0000

    protest share straight of watch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_work(force, distance):
+        return force * distance
+import random
+def roll_die():
+        return random.randint(1, 6)
