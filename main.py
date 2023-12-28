f22994836715071f85c122ed40375393a1860044
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:57:10 +0000

    shape refuse scrape except will

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_min(lst):
+        return min(lst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
