cbc0c25c704679c131656419dfb48029c2927f0d
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:53:32 +0000

    tap middle deep congratulations sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
