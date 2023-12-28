ac81477e48a26ff3e573a22414c66bef878e3688
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:53:35 +0000

    noone stand performance good conversation

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
