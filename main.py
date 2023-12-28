63ae8b37d5eb28b8184b788161dbce394cd86bd2
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:03:59 +0000

    lucky either dream must school

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def count_elements(lst):
+        return len(lst)
