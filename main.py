23938b737d0f3fcd86e1cd98f443e5776431475a
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:59:03 +0000

    getoff talk quantity heart punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
