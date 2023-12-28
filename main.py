9e03618b81d3d7c99b91612c49f3f72b739faca2
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:26:51 +0000

    yes increase special visit lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
