0403dd1a5c8cc447238dafed41d71ec95952a203
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:25:57 +0000

    mark move meaning confusing laugh

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def convert_to_hex(n):
+        return hex(n)
