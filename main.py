77152587d67010cffaae5ab9f89ba573beab4c85
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:30:10 +0000

    only eye close average post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import random
+def roll_die():
+        return random.randint(1, 6)
