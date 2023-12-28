18eabba9e7afc85ba159f650c0101aa97fda7b45
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:56:18 +0000

    child tone disturbing minimum satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
