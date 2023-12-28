4941a6a929104594764f9f2f98a5048b2b5501f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:28:07 +0000

    spend throw cost apply calmly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
