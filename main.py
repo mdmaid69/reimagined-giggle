06a504f8085618bf8d9d8d051e0b9a90687ec250
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:33:01 +0000

    love runout material sore slow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
