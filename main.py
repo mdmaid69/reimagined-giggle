4eee9aa7dc71654793ab61f11e5086702b300111
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:16:11 +0000

    feed interesting size takeover impressive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_binary(n):
+        return bin(n)
