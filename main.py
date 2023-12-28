73666bc4310909c13bd4c93fb1c48c0efa325ac1
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:45:45 +0000

    even switch century mass bank

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def cube_number(x):
-        return x**3
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
