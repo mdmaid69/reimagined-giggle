119b8dcede93cec660c13078ece6786eef61a7b9
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:06:07 +0000

    state travel teach vision recognize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def square_number(x):
+        return x**2
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
