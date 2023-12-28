b1852faecbde9407515bee907ce2a58afb181140
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:33:41 +0000

    party money turnover where control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-print(math.pi)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_repr(array):
+        return repr(array)
