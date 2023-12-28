692e19de6b43700df5c3baaae7d9b2bc31311116
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:39:48 +0000

    wrong system gift once scared

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def cube_number(x):
-        return x**3
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
