7a3096c3f84e4365dcddce9d692aaf74ffe6c2d5
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:54:30 +0000

    love possible organization mess profit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
