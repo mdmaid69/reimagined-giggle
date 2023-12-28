e0c315e6ff3b25619a8a3403b417f6d179dde1a2
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:18:49 +0000

    spot seriously murder if empty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
