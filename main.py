efd29411bb0cb84b646392dfb704cdb2351c3c77
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:56:54 +0000

    consult final keepaway question thank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
