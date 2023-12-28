410ed00d4912fd4e0f85b6570235dfc53e753c99
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:36:44 +0000

    stranger 569enjoy Nn correct wipe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
