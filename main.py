10afe0ba26e0a07a1626e6f1f3671de69127ab7c
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:44:03 +0000

    only 568energy secret mistake peace

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
