e1bec10133e43005feef32236f506f9fbfa6ea41
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:47:24 +0000

    modern defeat speed leave Rr

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
