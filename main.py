59afd314be15f75021a3ba063df4cbd6177b5895
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:17:34 +0000

    relation kind now copy refer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
