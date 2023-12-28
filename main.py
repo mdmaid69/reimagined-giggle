013d9bafad6c4760ebf3e5a20728ce9b13d994b7
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:03:39 +0000

    example drive yesterday stay mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
