8255462d4a7d9ac49ddc6b6da218bfe7385c28e2
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:14:46 +0000

    impatient jump separate control adjective

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
