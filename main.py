d212e5213eab5d73ddcd17c62d6da1607e859b8d
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:24:46 +0000

    fall television world position where

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_array_as_set(array):
-        return set(array)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
