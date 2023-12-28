93b3b0a8aea7e60024cac0e6d55e3e4edba816cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:55:33 +0000

    frighten possession deep shoulder introduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
