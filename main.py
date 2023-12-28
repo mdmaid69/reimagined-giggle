6e0b0cb078b0f6cd434a2c02ea6ffcf6414ce870
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:33:25 +0000

    poison note mix pencil form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
