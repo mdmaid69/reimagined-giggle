c863812cbb4ca173959c0922b309025254ccb77b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:56:34 +0000

    treat balance coat appear surprised

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
