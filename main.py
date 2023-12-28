aaad3524a86cf55873dae72a964b2ba69023aa77
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:35:26 +0000

    country disappear clean exaggerate runover

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
