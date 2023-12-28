80e1efad41d77b7ee9fe14f9db9b80f19974991f
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:14:38 +0000

    section benefit fire when shadow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
