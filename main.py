12aaa610c8bd121857eb7955b36178c9d74b47cb
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:49:42 +0000

    goback(to) ordinary board accident together

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_base_name(path):
+        return os.path.basename(path)
