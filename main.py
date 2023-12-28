87a6166d666cb8f695869ac53d7b9a574ef2a7e1
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:45:29 +0000

    ability list afterwards this piece

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def multiply_numbers(x, y):
-        return x * y
+  def get_current_directory():
+        return os.getcwd()
