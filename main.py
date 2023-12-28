3d2c40d85dd33099bb4c38e830ddfd9291719827
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:33:45 +0000

    admit prove uniform gap contribute

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+print(random.randint(0, 100))
