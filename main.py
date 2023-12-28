5b167194568452fac945d1cef6c25156b4085db9
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:03:25 +0000

    pin confused sudden get uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def find_min(lst):
+        return min(lst)
