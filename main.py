03ecb4dc65af59158b8ab03b8de66095462f55db
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:12:16 +0000

    day people from reach square

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
