b00f40decafcf1a5729a0c2e63ac63bf7cdd00c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:08:51 +0000

    explanation hair brain own bend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-print(math.pi)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
