46dfedae9a9f861525a075567cb06a3d56a41330
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:04:59 +0000

    deep near programme trade experiment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def count_characters(sentence):
+        return len(sentence)
