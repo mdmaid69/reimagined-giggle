60d0b086b479eab5852be57f415c7400fe34ca04
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:20:25 +0000

    top stamp dry better takeout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
