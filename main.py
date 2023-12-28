fe7faa04600a2dd6fe37eb104923ba1050f0dcf0
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:32:38 +0000

    either manage anywayanyhow trial apart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
