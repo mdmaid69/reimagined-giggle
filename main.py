5e9a6d578d7fc5d970ab258e82593c79d91bdde2
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:24:31 +0000

    aware chance cruel copy bank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
