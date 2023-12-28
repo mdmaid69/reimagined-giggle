8602ac2d38d415a3c0e718418417be7c048dbfbc
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:11:23 +0000

    priority tread easy settle doctor

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+print(sys.version)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
