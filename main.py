9e0ec019cd47f72ed23463b3cbdc51105ed68078
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:05:58 +0000

    bringforward factory hospital concerned car

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
