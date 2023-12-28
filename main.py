6ff5c38f607d4e604565e0a82fea3ea92f75a689
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:19:41 +0000

    spot buy nearly method now

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
