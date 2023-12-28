a09e9a484cb568b5ceedc8a9a847be0178a96769
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:23:36 +0000

    otherwise flight wise here right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import platform
-def get_os_info():
-        return platform.uname()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
