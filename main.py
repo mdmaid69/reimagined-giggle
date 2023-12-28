17df053765ae9ea2d9309fd0ea13d285093ecd36
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:10:14 +0000

    meanwhile principle inquire mind also

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def change_working_directory(path):
+        os.chdir(path)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
