270ff87aef46a33d601edafd535bbbc57c82e127
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:20:32 +0000

    bridge female comfort regret avoid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_sine(x):
+        return math.sin(x)
