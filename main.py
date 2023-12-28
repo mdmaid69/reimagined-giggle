a3b20fb4428529c5a225d41e53d8bc10f87af236
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:14:08 +0000

    takeout come favour take cruel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
