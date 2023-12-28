42c8ceec5f05b2197f0991bf4b69d11d9535eb3d
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:10:42 +0000

    nerve Cc maybe low like

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
