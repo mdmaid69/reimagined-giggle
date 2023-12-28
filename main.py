f1adb96a41fd48346c5f7a1ceb8d0efa624cdac1
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:44:08 +0000

    estimate unhappy vision rid brave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
