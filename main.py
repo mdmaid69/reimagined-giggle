902184cc56ca1d80359b5155f72b499752ee07ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:22:49 +0000

    use act activity joke busy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def reverse_list(lst):
+        return lst[::-1]
