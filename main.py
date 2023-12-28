e0bd68654932d081b4d99b32bfe8a18ca4f5e2f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:56:34 +0000

    earth waste present phone pronunciation

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def append_to_array(array, item):
+        array.append(item)
