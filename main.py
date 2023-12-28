42206fa2ddc7eb4d2686907dc61032786cbdf2f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:43:32 +0000

    meaning police powerful convince announce

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
