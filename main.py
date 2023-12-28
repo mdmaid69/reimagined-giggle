03ebd1bbf6438d6e6e0047172b890da87858de11
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:37:25 +0000

    follow classroom press love proper

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import time
+print(time.time())
