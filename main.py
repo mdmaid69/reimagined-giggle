2a59788bb8b7899a0eb65c00d6ce027044d3623a
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:06:19 +0000

    comment honest kind speak wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
