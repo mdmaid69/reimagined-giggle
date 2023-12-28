4cfdb677bfda82e441549d07205d635845751cb9
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:53:58 +0000

    cough professional interview live mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def is_odd(n):
+        return n % 2 != 0
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
