809142e27e455e20e61bcb54dc7aaf84f90de84a
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:45:22 +0000

    foot dead become plan air

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
