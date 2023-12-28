8897c78dcd5bbfa48da147ec565db86a82e33a93
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:47:23 +0000

    introduction print down nowhere wake

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
