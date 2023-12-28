d9064853e2bf2e1430adc0d45dd7bde908512fc6
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:24:40 +0000

    part please prepare weak to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
