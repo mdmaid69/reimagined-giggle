f69074ddd35035fc549bedb9f8f7dc3d27e2bb37
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:48:28 +0000

    stranger getout question gather occasion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-print(sum(range(10)))
+import collections
+def create_stack():
+        return collections.deque()
+def subtract_numbers(x, y):
+        return x - y
