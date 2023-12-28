fc8a9c8de98e1b5c7ac396d1705a5676f5042360
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:34:42 +0000

    subject rush gate cause backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
