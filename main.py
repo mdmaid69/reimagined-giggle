5033de92efd51f41d3cb4cabae578057c20f7235
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:06:44 +0000

    cause pipe affect support until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
