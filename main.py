e11bff919b666bb52b710210706cf5e230432467
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:59:39 +0000

    many as quarter offend anyone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
