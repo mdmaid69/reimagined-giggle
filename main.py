a14a7666051df9319e9255ae036de42794e899a6
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:13:55 +0000

    replace comeover gradually fond concentration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def convert_to_binary(n):
+        return bin(n)
