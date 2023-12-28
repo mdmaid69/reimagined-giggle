6338a28b5e443c40c0dc01d49071c16aa2fba0cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:19:58 +0000

    trust tap apply effect cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
