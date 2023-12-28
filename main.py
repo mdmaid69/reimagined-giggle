47578029a16e026406713550f5c70ca8d213d116
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:33:49 +0000

    actual interested gap spin keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
