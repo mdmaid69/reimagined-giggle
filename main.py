8468a1036096fbd2ddb65ec7f6bab96585cec0de
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:31:13 +0000

    announce course period 569enjoy commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_length(array):
+        return len(array)
