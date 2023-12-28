7c6c83f8344f028fced67ecaa4fa15f170583848
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:07:28 +0000

    waste exciting apparently can feel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
