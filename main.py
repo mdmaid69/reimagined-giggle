51d8544a6f3b6399aabdb173899e3e7cd0693f2b
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:28:18 +0000

    education more hot post grow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import datetime
+print(datetime.datetime.now())
