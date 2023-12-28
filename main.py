81bc383da001baa7dce888ad7de411c259978187
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:51:06 +0000

    handle door clue punishment rather

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
