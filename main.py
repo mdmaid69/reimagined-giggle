f49b7cc5a32b04b15c1b0f1d5abb078bef555989
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:57:10 +0000

    Uu arm arm talkback taste

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
