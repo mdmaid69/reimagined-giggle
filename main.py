bc5f0c65bb46045f41509551e8f66d1dc5b7a036
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:55:14 +0000

    see protest finally yet brave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
