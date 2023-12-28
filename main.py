7aee50b5ec1e4f78bf2639114cd024535a8515a5
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:01:45 +0000

    total callup as arm form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
