0c46312b2641c60d8e82ff4a359fd59d83fcf747
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:12:59 +0000

    buy convenient lead summer thankyou

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
