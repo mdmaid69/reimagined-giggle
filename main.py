0f80eedf843be2fe577249f1c9b76456c3708750
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:49:07 +0000

    remove uncomfortable feed pocket introduction

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
