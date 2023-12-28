8c3a8105f413b85952a5201fe718185532c2947e
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:29:28 +0000

    mouth back standard music dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_binary(n):
+        return bin(n)
