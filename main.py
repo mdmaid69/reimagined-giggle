b78371a17b87c80962897a4d81d7f9446453649b
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:27:28 +0000

    now rain person develop together

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def reverse_array(array):
-        array.reverse()
+import collections
+def create_queue():
+        return collections.deque()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
