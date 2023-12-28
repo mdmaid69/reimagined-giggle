6d6b13ac9371293f8ec98d14e946a8552ac03f20
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:15:59 +0000

    workout repeat wave brush this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
