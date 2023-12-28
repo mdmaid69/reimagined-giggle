54d2e2d5feae0c603ee5d847429ad45cb7aef3a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:45:33 +0000

    cause 566end cry near onto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-print(math.pi)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
