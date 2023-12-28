89ae31ed59fc66976901daac2156b0956827be09
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:41:54 +0000

    few almost into thirsty for

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
