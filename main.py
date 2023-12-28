013c30cb18762273ff755aa42a60a92bc3bac4c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:34:39 +0000

    objection shut figure ear completely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
