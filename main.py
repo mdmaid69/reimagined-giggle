4d5f241597e1ece38fd777a559bb2cae71bd2ffa
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:17:37 +0000

    present lose introduce drink without

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
