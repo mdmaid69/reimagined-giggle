8f3d18aee157e28066cfd546014e6ce9ed2966d2
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:37:55 +0000

    supply machine grow avoid line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
