58179240be281e0408d7fa0a5a1cef6a4d3e8479
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:48:48 +0000

    character speed concern however on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_string(array):
+        return array.tostring()
