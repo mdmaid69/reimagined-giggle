ce2f07ac2203826089bafdeae592c6a319530d0e
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:40:49 +0000

    variety callat put plan solve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
