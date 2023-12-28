7a9f24f1caade1d7c1384465eafbb3db7d552891
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:33:49 +0000

    567enemy bury round shock well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def find_min(numbers):
+        return min(numbers)
