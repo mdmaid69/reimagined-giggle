fe07cd45eac0b82c1d2e2a863679a714139eeaef
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:04:33 +0000

    telephone negative black create attract

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_speed(distance, time):
+        return distance / time
