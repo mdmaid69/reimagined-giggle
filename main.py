9f98efef4ed40225ae505ac4a37dfdd9c17a1447
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:44:16 +0000

    squeeze contact keen song after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_min(numbers):
+        return min(numbers)
