5986d31d83826ab4b99c62b868670e7ceb8ae554
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:23:29 +0000

    plastic final deliberately control although

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
 def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
