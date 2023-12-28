383053c9a9e6abcc44ae49398520627699264176
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:23:19 +0000

    interview famous earn move food

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def get_array_length(array):
+        return len(array)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
