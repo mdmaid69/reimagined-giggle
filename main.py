2a6611afde761b0eb462f99ffc53d5fc8dc1f74c
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:08:27 +0000

    in unlucky fairly remain talkinto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
