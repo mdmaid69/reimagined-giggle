925e50632dbfdef3719c1ee27cad0dd2faf1eb4e
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:19:12 +0000

    boring worth little rely butter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def square_number(x):
+        return x**2
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
