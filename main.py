d5331ea9fdf0dbd5674d2ffc267d5b20717eb941
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:18:31 +0000

    low bury wide neither prepare

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import getpass
-def get_username():
-        return getpass.getuser()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
