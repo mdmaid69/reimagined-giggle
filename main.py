b283ff9ae8a7fba47b6001110449c10d3e8d9d42
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:32:44 +0000

    task straight unfair wide suck

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import os
+print(os.getcwd())
+import json
+print(json.dumps({"name": "John", "age": 30}))
