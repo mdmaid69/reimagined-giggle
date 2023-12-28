c2f91deb5f7cb8f94139439ab296894bd0e5bcf9
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:07:05 +0000

    course may affect land search

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
