38ce2218b53f37e594a59eb75a1cfcc775bec4ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:59:51 +0000

    trial around simple sink train

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from collections import Counter
-print(Counter("hello world"))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
