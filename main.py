50a5606f9b4b8b33857514e54976ab7aebe12276
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:47:12 +0000

    niggaacademy whistle finger shop convince

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
