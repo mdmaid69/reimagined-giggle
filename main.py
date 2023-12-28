efb11285574584353817d74baa5be3b3d1837e5f
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:05:41 +0000

    squash spread suffer clear line

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def find_max(lst):
+        return max(lst)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
