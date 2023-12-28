2318b7465e1edc5efa894e5b779f3a6089d70d38
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:54:10 +0000

    previous poor smile find operate

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
