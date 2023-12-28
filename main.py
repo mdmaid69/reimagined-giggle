8e1fc4fca88330645962460ad2ffe41f9b553201
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:45:19 +0000

    inform task generally solve sometimes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
