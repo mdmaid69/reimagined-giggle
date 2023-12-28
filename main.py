1ec98968258ab919083ff2a50563c0936f548d18
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:49:30 +0000

    getout inquire satisfied meal breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
