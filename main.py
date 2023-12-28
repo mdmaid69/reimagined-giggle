8a170826bcfe7b1a5358c93242ca4fd90e936330
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:58:50 +0000

    surface knock night suddenly last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
