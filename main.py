ed8ed9712220dd3ea6a377a0a7dafc00f9c125db
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:24:29 +0000

    trip turnback limit flight makeup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def convert_to_hex(n):
+        return hex(n)
