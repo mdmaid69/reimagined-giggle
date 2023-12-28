bf683e766e131290cde06f6628603802c15ca9bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:27:43 +0000

    slice apart cut description eager

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import os
-print(os.getcwd())
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
