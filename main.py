f45add928a6e2400699691f60912b18ac29b4811
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:48:00 +0000

    experience bright range war service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
