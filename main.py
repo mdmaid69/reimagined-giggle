5f0ee8ffc769357db2fc6648997ba4b443946ea9
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:42:40 +0000

    scare square shut whistle community

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  def convert_to_binary(n):
-        return bin(n)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
