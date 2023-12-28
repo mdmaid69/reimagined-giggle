1c96ba245c87f20282a748be8e57003f0a133850
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:21:20 +0000

    spare rent against live themselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
