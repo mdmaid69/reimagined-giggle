ef880ba0dd2bf043b6089e7f5b53170c868a73a5
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:13:47 +0000

    hardly we above computer art

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def sort_list(lst):
+        return sorted(lst)
