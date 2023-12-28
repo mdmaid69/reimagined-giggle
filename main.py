81428fdd43cfd1561de6dcaa128e4a97a66fa6ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:28:02 +0000

    with mass consist emotion usual

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def count_characters(sentence):
-        return len(sentence)
+def find_min(lst):
+        return min(lst)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
