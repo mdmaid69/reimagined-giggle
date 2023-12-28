d25b77154208d517caaef75fb9cd9c4d8b1214a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:36:38 +0000

    embarrassing behaviour container fact war

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
