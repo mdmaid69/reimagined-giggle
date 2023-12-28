44fa54ef27ed252b9f89ed1cb7d27f6d83de173b
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:33:07 +0000

    cold earn cover upset house

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
 import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_item(array, i):
+        return array[i]
