28fe353f476d7f3e816cd1690f620978439ef40a
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:36:26 +0000

    block step combine afford touch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
