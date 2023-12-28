1954e8e2fea1ec62683716d72fd5e93cd9dfbf70
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:40:43 +0000

    hate basically loan concentrate clever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
