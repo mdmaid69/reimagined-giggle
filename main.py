8da1b3ea0bdac634918507558d240e632855eb64
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:51:01 +0000

    suit settle smooth know peace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
