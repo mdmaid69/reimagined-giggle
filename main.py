c8a63f0a0229116f0fc25168af3b393fec5c3371
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:54:59 +0000

    culture spray delicate respect kick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
