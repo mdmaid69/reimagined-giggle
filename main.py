4527be98396d332446a70ce4c75c470dcdb6daf5
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:39:07 +0000

    divide plan dull earth point

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_buffer_info(array):
+        return array.buffer_info()
