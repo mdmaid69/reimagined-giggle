2f180796f86701d27711a50a70087844fa15ce3a
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:01:54 +0000

    responsibility rubbish hurt quiet dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import random
+def roll_die():
+        return random.randint(1, 6)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_buffer_info(array):
+        return array.buffer_info()
