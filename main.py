6b232a4e567861262ee3aadb02908e210ecec759
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:35:03 +0000

    every mystery dry keep see

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
