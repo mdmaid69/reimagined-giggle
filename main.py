2ca6545f6f0b5e4418d040db85be255f52314e3d
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:37:52 +0000

    trust kind content discuss noisy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
