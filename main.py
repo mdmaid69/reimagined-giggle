24791b55276915ab37e09b12bdb7b33469df9e56
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:25:53 +0000

    whoever goodbye appear long like

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  def convert_to_octal(n):
-        return oct(n)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
