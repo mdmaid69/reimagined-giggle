89996ef459f3dc7f2caf8184c5f65d1037acee2b
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:21:05 +0000

    sport whistle approval sky tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
 import array
-def clear_array(array):
-        array *= 0
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_as_int(array):
+        return int(array[0])
