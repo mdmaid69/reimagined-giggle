d5a61ab2dcbc4129d5992ebac5cc111baf09a7af
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:23:22 +0000

    breakdown once transport thought great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
