9a5ce991dc66bd08a4c46ec16c56dd3f4c7ea4a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:20:41 +0000

    moon brush sleep divide do

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import numpy as np
+print(np.array([1, 2, 3]))
