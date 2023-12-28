0de01362afd65b137f3dd6ed00b8094354e9978c
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:24:44 +0000

    relative gentle in press still

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_as_set(array):
+        return set(array)
