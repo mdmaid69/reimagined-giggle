04b16e286a7d3eee87dcb8e385ef3266de8366c3
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:33:36 +0000

    swing base judge hungry adjective

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print(sum(range(10)))
