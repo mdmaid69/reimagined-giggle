b1e20bb2451b9e0da22baf44c32a556d4ceadbb5
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:12:13 +0000

    stamp nerve like poor but

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sys
+  def get_python_version():
+        return sys.version
