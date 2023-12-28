2e7cf6822a193c1b94459bd6b61a1388f77845e1
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:26:18 +0000

    practice return scare dare education

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
   def fibonacci(n):
         return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
