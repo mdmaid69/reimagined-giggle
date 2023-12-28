918f07262abb79e49aa3ebb33387f3320943678e
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:59:06 +0000

    breakinto mix anything song consider

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import json
+def convert_to_json(data):
+        return json.dumps(data)
