e45a1a202660b6b9f1030713a943f16fcb7afba2
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:54:53 +0000

    realize whole tongue decide takeback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_array_to_list(array):
+        return array.tolist()
