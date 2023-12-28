88db9055461ffdc7e5f25369519f7ae0c4f3da22
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:58:00 +0000

    imagination hot exchange wash thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
