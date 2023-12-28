438e619dd2e34d211d63ec7417215a4bcdb95ff0
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:45:46 +0000

    warn trick Uu cheque keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
