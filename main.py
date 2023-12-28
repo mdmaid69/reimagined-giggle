23212e1245ff2edc4447744a5a3323b9a1008c35
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:20:10 +0000

    warn forgive score lend dark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
