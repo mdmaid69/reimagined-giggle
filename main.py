02ebb2ec3107df1c4ae3f83d9ad179e63568b24b
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:48:23 +0000

    stir record seat charge effect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def find_max(lst):
+        return max(lst)
