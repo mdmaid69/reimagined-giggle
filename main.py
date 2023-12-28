99f2383651e3e742e004da5ff10e2fe69830fc09
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:57:54 +0000

    future reason blame people hold

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
