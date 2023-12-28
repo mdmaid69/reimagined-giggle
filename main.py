3120a01982c96d56647b3c61c35926fb2d760ee8
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:29:46 +0000

    muscle thankyou wrap convince immediately

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
