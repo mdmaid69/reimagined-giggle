ca4fd93dbf388c36507197722f51a2e4ae1f50f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:01:29 +0000

    out basis defend development measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
