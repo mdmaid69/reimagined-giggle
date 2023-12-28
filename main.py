f9c2cc1b295c991a43f653c6823c39fc01a33ca7
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:23:04 +0000

    violence buy hollow wish order

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
