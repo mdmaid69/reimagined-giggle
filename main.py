b6a59a4318213a530c785401c094a8c85277900e
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:08:25 +0000

    lift holdon container mixture bridge

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Hello, world!")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
