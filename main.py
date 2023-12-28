b1d5acdb5dcac4fe7cde487e148eae00e684e5d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:42:59 +0000

    night every bringback cope gas

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def count_elements(lst):
+        return len(lst)
+import collections
+def create_user_dict():
+        return collections.UserDict()
