8520f6cd4e780d8ab13d9525a30e14911b8893d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:05:07 +0000

    wish generally duty ago drop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def multiply_numbers(x, y):
+        return x * y
