223e6e21be6b8e4dc6c56518243e9cba1e678d90
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:14:31 +0000

    forward health hole alone gap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def exit_program():
+        sys.exit()
