191fed79fce0062f78f781d8ad71577821933b9d
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:22:06 +0000

    century tongue promise apart principle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def cube_number(x):
+        return x**3
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
