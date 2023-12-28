72380f4a8030268f26ef6276627db8fee46d8bd2
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:55:39 +0000

    hole cruelty sign charge principle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
