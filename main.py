8984b13cc6843b2401b7c72931e4a9301638bf9d
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:04:01 +0000

    concerned around pretty tough empty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def greet(name):
+        print(f"Hello, {name}!")
