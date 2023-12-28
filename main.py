343eca4d35d05fad5419f26bfce36a86a42842c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:23:09 +0000

    hard clearly level natural pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def find_min(lst):
+        return min(lst)
