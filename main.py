e939aedf80111fac7b912884ac3c4892c9f3d847
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:58:16 +0000

    building advantage advanced club wish

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+print([x**2 for x in range(10)])
