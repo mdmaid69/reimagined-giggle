36ffd862761b587fc2f7e0ff7db4fa9168d85071
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:27:15 +0000

    practical choose add flat Ll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def count_characters(sentence):
+        return len(sentence)
