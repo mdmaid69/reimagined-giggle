3d1096725e45f2261ac701176a2bcdc06b4c7d77
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:30:47 +0000

    action bang interview because table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
