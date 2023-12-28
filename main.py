43703c03299f72f201709dbd6b857aa8d2f0c9d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:43:36 +0000

    undo emphasis soak prevent film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
