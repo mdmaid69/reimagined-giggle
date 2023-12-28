0188637a7ff5cf48b39951c061981b63b0d7efe2
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:53:41 +0000

    should declaration payment wall mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
