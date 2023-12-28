4a1d04f03e0a5f8424ab8d23f94a783dd529eab9
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:57:45 +0000

    fall contrast sleep large obtain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
