55be7d0a5762e3490be552de00425534a7ad6470
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:51:07 +0000

    work freedom pool clearly character

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-print("Hello, world!")
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
