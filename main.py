a330a08ebc766e84482681473c11587b87916d37
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:54:17 +0000

    unusual wide nonsense justice trust

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_current_working_directory():
+        return os.getcwd()
