cb0c5c1330c9a6722cb05b839479877da2b76c8b
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:20:22 +0000

    throughout clean sweet joke mother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
