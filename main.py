eb999be75635a3f02e026547cef8dce3b3bb2a3c
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:30:56 +0000

    boring related window build lie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def append_to_array(array, item):
+        array.append(item)
