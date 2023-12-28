820d7c122470ba717e8dcb9d766579a17e1cc9d0
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:11:34 +0000

    Dd arrange raw low chin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def find_max(lst):
+        return max(lst)
