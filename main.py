13d13703d17a93bde747962ba707e0bd95c366ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:33:20 +0000

    feature card almost rescue mention

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_array_item(array, i):
+        return array[i]
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
