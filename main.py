bbe235cd324d645b17c8a2de53748b81fe199666
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:15:53 +0000

    chase up occasional mark angry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_as_set(array):
-        return set(array)
+import datetime
+print(datetime.datetime.now())
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
