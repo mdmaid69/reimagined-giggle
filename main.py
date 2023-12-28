f365af0a90549e830316e74029ebaeeefcfce211
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:01:47 +0000

    detail basic prize permission love

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_power(work, time):
+        return work / time
