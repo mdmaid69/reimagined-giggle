68fd9b920867ff9f5f8ef5bac768c625f16534b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:56:14 +0000

    house free wish across connected

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def find_min(numbers):
+        return min(numbers)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
