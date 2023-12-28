7257d68dc40cd7006dff0fde8d792672dd38b95d
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:40:49 +0000

    port take information note delicate

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
