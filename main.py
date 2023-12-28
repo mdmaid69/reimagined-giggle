3775b115a59b260cafcaaa6339772db5531b0402
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:07:14 +0000

    deliberately mass kill visit age

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
