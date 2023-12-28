197fef54d3d2ae75e9ee32c9efc7c79e959e5d1b
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:18:39 +0000

    bury demand under congratulations suggestion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
