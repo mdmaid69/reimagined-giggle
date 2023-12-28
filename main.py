030675c3d08574717df24e8ce80f83e1188bafdb
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:27:42 +0000

    Kk front muscle stage century

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_work(force, distance):
+        return force * distance
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
