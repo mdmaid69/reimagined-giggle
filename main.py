b1149e73f59f19f337ef3db3a1e0a3215bdca1a6
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:53:18 +0000

    passage female either unpleasant impression

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_acceleration(speed, time):
+        return speed / time
