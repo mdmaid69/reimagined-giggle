35d3f78ef2d363644fb2abf72562d241214c2831
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:39:31 +0000

    again exist married service able

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
