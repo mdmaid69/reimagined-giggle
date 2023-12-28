550ab85055515c6c9a6f6583749a1f7912b22ff2
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:39:59 +0000

    comeout replace belief statement well

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+i = 0
+while i < 5:
+        print(i)
+        i += 1
