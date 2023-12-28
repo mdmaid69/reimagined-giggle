946621c0ea951e0a59a9380e6250230c640fa2b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:04:35 +0000

    peaceful error thanks ticket left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
