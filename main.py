bcdc1cec70e0820e67bea64ba823579dd5bea542
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:15:02 +0000

    die escape opportunity attempt small

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
