26d63f45f87459d34cd7e1c224ef2b67e3d374fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:55:11 +0000

    worst accommodation temperature getaway worth

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
