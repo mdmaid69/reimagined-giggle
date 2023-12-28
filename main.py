c0ce7a249956044d1e65ee32a335823eaaf87c2c
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:49:30 +0000

    strike strike thick rate convinced

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
