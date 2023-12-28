2c66f8a7dc510e64da42b4c7a07f078d2d822ef2
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:27:17 +0000

    prefer talkoutof invitation place label

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
