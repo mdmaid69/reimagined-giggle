d779f18feab392ae564bae32d1cb966287b81a54
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:51:45 +0000

    face private who reasonable spend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def count_characters(sentence):
+        return len(sentence)
