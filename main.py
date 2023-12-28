efcbd711d2f7a4fc32962a016d2dbac7d05511f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:51:34 +0000

    reliable consider familiar injury month

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
