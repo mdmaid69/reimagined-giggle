d0b2e0feed76fe5740fba1846caa839c5b996442
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:32:56 +0000

    butter hate neither hurt except

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
