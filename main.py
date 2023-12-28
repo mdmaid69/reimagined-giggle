a30a8250d2d3a1efcdb02ab01b430937c6a36501
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:35:06 +0000

    practical gentle morning war brother

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
