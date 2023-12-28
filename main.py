fea1da29bae60acad58698b3b9a69ab468c17814
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:49:16 +0000

    clean crazy play force goodbye

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  def subtract_numbers(x, y):
-        return x - y
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+for i in range(10): print(i)
