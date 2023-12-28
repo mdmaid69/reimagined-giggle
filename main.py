dcec355d56cf718904a3694174b5eb74d799ec92
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:11:02 +0000

    centimetre earth hope behaviour once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
