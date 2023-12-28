c5b9d5eaae6142231003c5d3162992962d62ad17
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:31:55 +0000

    file 565end dick train model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
