49ae9af6f5160683e5f1d5e7dc66c637f93c666d
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:24:26 +0000

    breakinto Vv deliberate central street

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_max(lst):
-        return max(lst)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import json
+print(json.dumps({"name": "John", "age": 30}))
