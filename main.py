560cfd53ed9730252a7c867e7846e70ee6179a3b
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:47:36 +0000

    564encourage off collect wrap fetch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
