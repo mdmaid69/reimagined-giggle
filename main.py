84bd4fb6a037787bdbc5f3f11fcbcd586d9b9404
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:25:22 +0000

    satisfactory excuse abroad feeling Kk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
