de8edadaa35e87889fa21b727aa69c8a65fcbee9
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:18:20 +0000

    no name detail born dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
