42c9405fb28a1f93578cfd89559575dfef3ecadb
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:26:58 +0000

    hate memory fence raw before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
