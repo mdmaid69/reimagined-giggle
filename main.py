e27f35039428ead60ec13575e34ebe6b8126c354
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:57:06 +0000

    occasion trouble price nowhere his

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
