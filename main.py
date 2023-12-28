2c6a3cf9cad7ece314c3b7b9a756c770e4165d4c
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:37:27 +0000

    read determined region stage escape

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def clear_array(array):
+        array *= 0
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
