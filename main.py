32b373328cbfc8f0189d721f7d989de383c1ccc5
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:47:11 +0000

    forever battery wash shake replace

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_perpetuity(payment, rate):
+        return payment / rate
