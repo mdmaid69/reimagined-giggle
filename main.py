fbf8ab4c62583a113703bda9cb18eff7a607fb55
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:17:09 +0000

    tongue show spare relation own

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
