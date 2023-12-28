015113bac11ffb458df1718cf186fb99324784b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:00:07 +0000

    realize store freedom someone goout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def get_array_item(array, i):
-        return array[i]
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
