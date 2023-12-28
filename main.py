45a76dea9b5e74009e8e77538bba9fc581af7744
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:01:03 +0000

    shock future unite table book

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
