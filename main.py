5bbc618289040b351f9c2840d51b19c4e04ee60e
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:36:46 +0000

    peace now improve the side

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
