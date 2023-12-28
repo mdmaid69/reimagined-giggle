4c3ae53839383d7c972a5dc74ef82f1283c36509
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:44:59 +0000

    organization shut getback connected family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
