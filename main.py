4ae1f68c11da84652e5f7bba866657f8509e92e3
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:19:10 +0000

    wish your juice advantage Zz

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
