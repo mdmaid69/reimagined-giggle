0d98c65a50a34860d168f604db7b59adaaaa8bc7
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:26:17 +0000

    minority thin task slice protection

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-print(sum(range(10)))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
