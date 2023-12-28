5185f702b932f238fa8338357b6527af53bb16a1
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:17:54 +0000

    mind mess disappear destroy slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import collections
-def create_user_string():
-        return collections.UserString()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
