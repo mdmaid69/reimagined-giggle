75ca94c9f360497eeb89cbe0317c82ec81f2a17e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:22:52 +0000

    describe better smile tell delight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def append_to_array(array, item):
+        array.append(item)
