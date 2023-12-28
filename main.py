9cde0f1a531d56e9835df5bfdd3451d62885a309
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:38:56 +0000

    both dream point opponent Cc

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def square_number(x):
+        return x**2
