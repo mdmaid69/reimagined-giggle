7c92d5e5e645f1638467502be57ec5a7de3e5612
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:00:47 +0000

    otherwise fetch crisis little belong

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def find_max(lst):
-        return max(lst)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
