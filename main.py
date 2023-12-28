92500befdfaf8c54095764a56b33fb6f5d6dd8df
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:34:18 +0000

    wall content song near cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
