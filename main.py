0ce218c3c406d0942c75ce63639c06b38cd38806
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:07:51 +0000

    score soft trust thin rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
