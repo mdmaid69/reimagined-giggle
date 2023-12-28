84b64a8918071ed3c264faf138432b8d7537182c
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:08:00 +0000

    container difficult recover talk know

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_user_list():
+        return collections.UserList()
