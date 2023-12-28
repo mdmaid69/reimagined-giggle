5794a6f8cca45030c4334f32de7a43bb70f66b54
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:03:24 +0000

    then protect promise describe thick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
