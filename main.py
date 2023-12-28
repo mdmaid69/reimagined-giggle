6e7099f9a54d39089ae2093c5779bdaaa0fe2665
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:13:01 +0000

    window warning decision modern Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def clear_array(array):
+        array *= 0
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
