e04cec529735eb4196dd7b3768da7522d240ab33
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:29:45 +0000

    difficulty everywhere appeal rent without

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
