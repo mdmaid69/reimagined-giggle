dc96039ea123f00d9791b8536b899445e9854583
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:12:52 +0000

    connected on rush heat important

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def is_odd(n):
+        return n % 2 != 0
