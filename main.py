6492da12e1485acd5e5d3c1c2a16980efd54f5d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:57:52 +0000

    workout slow up through birth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
