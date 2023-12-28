950f6531488372f2b060e1d80269932124f792bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:01:54 +0000

    kind as likely handle behaviour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
