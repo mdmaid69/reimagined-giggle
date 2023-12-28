8898667502b7ad9da8ef08f71ef1df7c21c3c029
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:43:35 +0000

    thankyou invite centimetre custom question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
