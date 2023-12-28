fb80c3df250738fcdc9f56bac9122b2aa0623164
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:40:52 +0000

    picture successful operate table say

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+for i in range(10): print(i)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
