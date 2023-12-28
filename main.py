9296d0ba68b2e0e2ccfeaa69134a9681507237b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:42:42 +0000

    equipment twist available stranger main

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+n = 10
+a, b = 0, 1
+while a < n:
         print(a, end=" ")
         a, b = b, a+b
