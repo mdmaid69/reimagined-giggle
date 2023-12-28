6506513366ea123e09c590eaaac1527d0e6b40fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:40:28 +0000

    action lend tax sign exciting

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_power(work, time):
+        return work / time
