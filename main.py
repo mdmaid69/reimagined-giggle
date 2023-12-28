ff4ac074a91ad6f0ad8027916e897d8dd61039a5
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:01:04 +0000

    print Itself miss thorough interest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
