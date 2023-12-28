ac7a116ae22837a698dbc325b8b174d2f4222bf7
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:25:17 +0000

    of test programme good lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_floor(x):
+        return math.floor(x)
