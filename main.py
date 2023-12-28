1e767e4bdd18e8f503238d8f023e66ac5449b9e8
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:22:20 +0000

    smile dull stare turnaway force

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
