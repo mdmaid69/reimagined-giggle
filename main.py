e797ed5282dc8748d2086505ec8c969662791aad
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:25:11 +0000

    part wish keepup send join

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_speed(distance, time):
+        return distance / time
