705d9f5dd0b0e259bc6ba113fe93ac0b071e3494
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:09:55 +0000

    disapprove 571enough share confused week

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
