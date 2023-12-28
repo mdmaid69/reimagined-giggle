94fd1e5f2d12a8a514a8188e74d5c2d7a93f355f
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:33:39 +0000

    general find speak slow morning

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
