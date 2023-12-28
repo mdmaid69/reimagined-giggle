bc1c34f0c887fe55cbfe848436ef8cc789dacaee
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:16:04 +0000

    trap feel receive avoid travel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_current_directory():
+        return os.getcwd()
