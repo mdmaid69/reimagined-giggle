c91775e8a71db54824b7ee0b5628fb834c192a38
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:38:21 +0000

    better shoot rely lock maybe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
