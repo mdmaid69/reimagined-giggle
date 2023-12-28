739704a883742df1add5bdd59bbad411ddb04cce
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:43:43 +0000

    Tt later bringdown sister however

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def remove_duplicates(lst):
+        return list(set(lst))
