f1bc80da4fc0f17a6459d1b2e6ec7ece215225a5
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:08:28 +0000

    excuse like child admire rank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+def change_working_directory(path):
+        os.chdir(path)
