964e592a9110e50aaa67873d91454d7493247dfd
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:47:24 +0000

    one table snow respect explain

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def multiply_numbers(x, y):
+        return x * y
+for i in range(10): print(i)
