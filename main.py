32c0d7b4e5287a6b5819fa1118b7a9abf4ded764
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:56:03 +0000

    life nor save considering dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
