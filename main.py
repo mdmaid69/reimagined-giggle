0b6f53e4fad74d78edc790cd9f1c9bb07212abf5
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:16:43 +0000

    case makeinto discussion however possession

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def remove_duplicates(lst):
-        return list(set(lst))
+for i in range(10): print(i)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
