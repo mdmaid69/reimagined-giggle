460649702620bcd6f5a70797300b3da5246265b5
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:48:33 +0000

    violence action Ll favour course

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
