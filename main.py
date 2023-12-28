14524d9f57f410259b871eff1cd044bd913cbece
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:41:35 +0000

    lead nature cough slightly object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def count_words(sentence):
+        return len(sentence.split())
