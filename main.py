dc46377dc503f20d63ea7336351a6df6afad3f34
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:38:28 +0000

    glad brown obtain circle children

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
