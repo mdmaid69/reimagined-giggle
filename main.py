84eb027f71f2aa0a70fe2c45201e25fe621dc072
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:12:04 +0000

    street discovery born clean show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
