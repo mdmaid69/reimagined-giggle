a470e42a6179573d2dfe270cc76b4fde3cf20d83
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:53:34 +0000

    bringup speak remain dance any

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
