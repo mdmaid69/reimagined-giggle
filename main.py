c2e37d3a7672b6e4f5d1a5dd6e53cd2135a92908
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:45:18 +0000

    shade future contact wind height

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
