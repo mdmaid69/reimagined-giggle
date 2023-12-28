fbd15e9e3e8454e5c620f1280244bd9b24929eda
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:41:39 +0000

    would team giveback degree concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
