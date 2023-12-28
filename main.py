c57770abc0fd9dbe46eb77b1976778dd5a7b5e3e
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:19:30 +0000

    education oil fine quickly description

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_min(lst):
-        return min(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
