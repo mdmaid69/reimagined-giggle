d5028c7979f4ed3dabf63809f599f46750694722
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:20:51 +0000

    frame reply either broad listen

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 def calculate_work(force, distance):
         return force * distance
-import sys
-print(sys.version)
