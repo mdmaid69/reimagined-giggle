71e31b2dceae73fa3ca7b9c0d78ef905bac9bfb0
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:54:06 +0000

    carry away boring morning path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
