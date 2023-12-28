0d7fa045193adbe39340e7662d46b07a426cf78f
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:19:45 +0000

    knowledge people collect result punishment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
