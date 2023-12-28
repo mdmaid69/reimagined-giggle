003a280a5a590cec0c8a577eb228c449f1b70a34
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:39:23 +0000

    stir especially to relief letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def change_working_directory(path):
+        os.chdir(path)
