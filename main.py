bd22cf9a885b480f13fa7e36cfd217d797eacd39
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:42:47 +0000

    reject top they day adjective

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def find_max(numbers):
-        return max(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
