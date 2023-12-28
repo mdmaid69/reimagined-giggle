c00a38a8ef773e376832b5bf612af31369acc4a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:35:24 +0000

    window anywayanyhow prepare yours clean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
