639986d1bba37087662bdd00233f6d6f68d549d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:19:27 +0000

    general tie unable pause file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
