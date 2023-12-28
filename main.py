a888f531671c0598cd60ea25bb6263d41dfacd52
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:58:53 +0000

    shopping shrink ring thinkthrough chief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_density(mass, volume):
+        return mass / volume
