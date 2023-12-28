2e09483dde06e1a3bfe6cc8e27b4f685073ff42a
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:03:35 +0000

    pile ticket regular offend trouble

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
