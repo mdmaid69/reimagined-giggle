546ba21727521764526b2b7fe78481d863af7132
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:15:52 +0000

    by regular squash miss call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import random
+def roll_die():
+        return random.randint(1, 6)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
