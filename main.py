b63066f9d4b35114764f671f536a5eafa5c7db21
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:13:49 +0000

    create house where tax could

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def count_elements(lst):
+        return len(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
