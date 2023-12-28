df340cc482813abab9b43fb00b0b9d695a18796b
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:59:04 +0000

    expression remove decision prize attractive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
