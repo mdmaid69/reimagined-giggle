a642f957c00e03f81a4b65da5b714f66db7f356f
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:04:56 +0000

    intend determined answer quality point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  def multiply_numbers(x, y):
+        return x * y
