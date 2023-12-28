c725803c4f8cd03f32871f8849c235cd56ec484b
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:14:56 +0000

    club about speak already schedule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def find_max(lst):
-        return max(lst)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import os
+def get_current_working_directory():
+        return os.getcwd()
