ce97d038b378a0343e13609be2aa577334301b10
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:58:14 +0000

    belief warning mark swim excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
