4c106693d9050465fc1afdd7125f05cf54c75934
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:41:59 +0000

    in match allow separate hold

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
