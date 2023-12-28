cba3e77a4ad2a634eec63b97f584321d5fdf323f
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:50:55 +0000

    disappear protect extremely work surprise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
