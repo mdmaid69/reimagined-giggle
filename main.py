93dfbc66433734d5eb792c10ebfa3143ac4c5e72
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:35:42 +0000

    sure judge instrument explanation child

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
