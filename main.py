e5c3f9df36fec99363510fefa3423064629d37b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:33:54 +0000

    murder plenty call glass pattern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def subtract_numbers(x, y):
+        return x - y
