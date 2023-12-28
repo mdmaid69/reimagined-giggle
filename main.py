f6cdd49b32e077915d88e36ecdf6649a401aa148
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:25:37 +0000

    after sorry gather original thirsty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
