e8822e2343e92e020140db43a0ef2d490be503e5
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:45:08 +0000

    remark birth too minor far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
