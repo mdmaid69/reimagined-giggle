9e5df10f99cfce8950d551a0ea81cd8f673ae4ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:36:12 +0000

    high board hair bored body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
