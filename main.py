301a84c07415aaf49d705cf817020cd660b21508
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:23:08 +0000

    sing always success apparently best

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def find_min(lst):
+        return min(lst)
