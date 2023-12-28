3986df0b902dbb992687ced2d92e0916659a1848
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:12:49 +0000

    turnback whenever wonder pretend machine

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
