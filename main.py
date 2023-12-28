8f0471f931a1088e34469f51598a280217625916
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:53:10 +0000

    draw familiar company suffer church

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
