55f207edf06b986207a322dd09e5f052ebec8c5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:42:00 +0000

    stretch direction each Jj hand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def convert_to_binary(n):
+        return bin(n)
