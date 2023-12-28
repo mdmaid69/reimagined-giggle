8148195632409b2885c19c59e3c1596833ef16d2
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:17:35 +0000

    gift stage bus collect wide

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_work(force, distance):
+        return force * distance
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
