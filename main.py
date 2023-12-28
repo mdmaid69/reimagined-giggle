7f8b90491399c9d504d3cbb113857e916dce1535
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:45:26 +0000

    shock week maximum scene connected

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
