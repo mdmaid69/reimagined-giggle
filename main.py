5f9acd19d62b04ba91537f07472ee03a22b647bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:33:33 +0000

    question division collection road well

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
