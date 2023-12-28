f89fed9e5e3f2c590e025a855f33801d39d8e300
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:57:40 +0000

    definite spread stupid smile appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
