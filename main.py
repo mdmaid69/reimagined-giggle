b6a0c87f718cb37c543c7c47e80ae6ee45b00c8d
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:41:11 +0000

    typical guard be top come

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
