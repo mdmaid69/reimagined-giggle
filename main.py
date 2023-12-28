fb8ff7a13199516a88e81105eec8324d0063dce8
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:44:02 +0000

    opinion damage pause feature choose

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_array_length(array):
-        return len(array)
-def sort_list(lst):
-        return sorted(lst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+from collections import Counter
+print(Counter("hello world"))
