076ed48f6413b1ebeab36833e7851f8bc38e4d49
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:05:14 +0000

    soon Yy information limit picture

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
