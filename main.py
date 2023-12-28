20a63ab1b737a16c36f41fb25273e17573568e81
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:37:26 +0000

    lucky develop crazy quick would

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
