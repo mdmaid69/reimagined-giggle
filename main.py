d18a1c3956d1973f31e625ce9defb665e491bbe3
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:54:52 +0000

    debt flat behaviour have stand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
