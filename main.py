ea816c817e59ddc43eca94b8656b1f20201abc5b
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:26:00 +0000

    sort dry strength suffer good

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
