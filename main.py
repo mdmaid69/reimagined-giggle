34c79f1b181beac4d8988a43ff9cc8f022973871
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:56:52 +0000

    congratulate do lookaround plate pity

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
