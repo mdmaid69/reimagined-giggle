b9eda9d3742d6df37959cb397bb78a725bd8c2db
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:19:50 +0000

    replace who grateful catch phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
