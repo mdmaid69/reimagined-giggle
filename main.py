0d5b78c23b8b6934d8c41b4d740adab8e626ca23
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:52:18 +0000

    rule recover constantly method further

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
