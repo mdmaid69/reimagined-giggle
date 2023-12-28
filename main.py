d5d2e1a0b332c072ab206e63c85782a42614c5a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:55:57 +0000

    famous sort cheap force air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import platform
+def get_os_info():
+        return platform.uname()
