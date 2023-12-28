ecdd043658696e722ef8f4567905d3198f15c521
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:37:06 +0000

    out stage mean add female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
