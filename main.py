ad81ad1f5ad3136127c47bde2df7f4d2dc29da6c
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:10:50 +0000

    wrap uncertain modern plate deliberate

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
