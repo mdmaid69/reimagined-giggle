001821549091c15c45a6a7295ded821ec1dbecbf
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:45:22 +0000

    introduction illness connect dress comeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def subtract_numbers(x, y):
+        return x - y
