ff6b2e7cb596867b20f9132af7e63fca999608f6
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:19:26 +0000

    mix frighten bill some mouth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
