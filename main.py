51c56ff1011e8ca1dbc413aced61488189c1dc1e
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:27:49 +0000

    correct comeback apparently wipe every

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
