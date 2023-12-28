7e65da2fc237696bcbda8e4760b5b4136f6b75d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:36:24 +0000

    piece correction description advise can

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
