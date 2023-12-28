338dee555fbd79f460a96a3ef5139a43f657a076
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:05:06 +0000

    claim play act improvement side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
