db752a389fc83a21955c4fed88399b5cf927919c
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:07:14 +0000

    protest mean allow sharp sleep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
 numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+print("Average:", sum(numbers) / len(numbers))
