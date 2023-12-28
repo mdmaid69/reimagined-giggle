791664670b6145d64c2194d78a2b295f18d6661f
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:31:45 +0000

    tape memory general little keen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
