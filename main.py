b5635d6c39f3e9687abc7b65b52ba60adbeb4df5
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:06:48 +0000

    unkind gas alive only team

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
