ef8ef85164ac6fcfb15043bb130b36ef10223672
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:36:29 +0000

    true motion however work I

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
