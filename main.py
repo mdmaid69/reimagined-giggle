cf86db40090ca4314f36d66f45191eb4a2b760de
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:37:12 +0000

    love burn cry target Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
