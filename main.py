434c177165cd8b89597d2982d60658348f47446b
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:40:30 +0000

    evening pass machine moon weather

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_power(work, time):
-        return work / time
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_typecode(array):
+        return array.typecode
