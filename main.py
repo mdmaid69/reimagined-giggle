5a7bb6cda50e285aec108aced3e1dd1c43bc66ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:49:52 +0000

    word same deaf shame smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
