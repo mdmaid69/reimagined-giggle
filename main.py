376f3e04b4c58d65c24ff7620cae0717bac64a89
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:05:20 +0000

    pity earth owe OKorokay shadow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def sort_list(lst):
+        return sorted(lst)
