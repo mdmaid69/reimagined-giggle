fd588f7bb1682e5e0609c0eb2b0a2f29d0158d8a
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:02:47 +0000

    keepfrom female to runout greeting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_density(mass, volume):
+        return mass / volume
