c333e1974650958dd0c8dedaa5dc3372390cfe45
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:50:06 +0000

    low film breakinto vanish frightened

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_typecode(array):
+        return array.typecode
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
