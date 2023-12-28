7efad1d3df4e4f89d36e6690e61284355f433f71
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:23:27 +0000

    reply convenient open runaway live

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
