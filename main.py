32deb86e5bbeaf8cf2e1a1f25de89029d6cfa61d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:07:10 +0000

    talk design improve tour already

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  def convert_to_binary(n):
+        return bin(n)
