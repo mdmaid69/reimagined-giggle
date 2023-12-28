109d8958b3f4b8869b059bc3c9ef1d86defef244
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:02:03 +0000

    stamp close roll Uu trust

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
