758d814ec349fa98b8e428a91c2d94a3489bf767
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:42:04 +0000

    fall soil exit realize during

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_odd(n):
+        return n % 2 != 0
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
