77f64dcd28520b6ebc1280470378b0560f157774
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:30:56 +0000

    act suit battery bar conduct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bool(array):
+        return bool(array)
