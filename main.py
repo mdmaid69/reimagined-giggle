4bda7243e2965b3cad1be622a7308bd0d3f06416
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:29:31 +0000

    police partner them error before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def get_array_index(array, item):
+        return array.index(item)
