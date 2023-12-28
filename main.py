8d80bf76441b14aee4dbee9bfbc7cd74b135ff4d
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:32:21 +0000

    earn score say accident range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-def calculate_power(work, time):
-        return work / time
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
