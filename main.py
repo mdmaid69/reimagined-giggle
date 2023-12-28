e25ee8a475ca94599e54674a9b6de8a5765a8d9c
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:27:46 +0000

    round at become protection thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
