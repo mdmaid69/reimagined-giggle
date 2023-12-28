d66362b4a9946c2ffb0bb71ef21b8f3647b2058e
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:24:15 +0000

    command sentence year runinto bear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
