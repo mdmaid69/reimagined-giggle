135f9309a480c92d576d68bd51883d21e8fc2253
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:18:32 +0000

    arrival suckme back finger pass

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+text = "Hello, world!"
+print("Words:", len(text.split()))
