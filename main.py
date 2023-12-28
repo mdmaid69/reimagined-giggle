e9b83b92e565bf549a87ce9d949ea439193b88c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:16:27 +0000

    Dd excellent by behind ago

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
