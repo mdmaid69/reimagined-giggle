3fa69d848d711e70a76938bea3d094c44d85acff
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:19:08 +0000

    song yesterday honest fear whistle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def print_python_version():
-        return sys.version
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def find_min(numbers):
+        return min(numbers)
