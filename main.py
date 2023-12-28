f4a80aa531c49b0cdb15920f0e2850a25690e36c
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:49:17 +0000

    interview attract difference slow connection

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import datetime
-print(datetime.datetime.now())
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
