e75403236e12a01c50db94fa1d71297b0957f238
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:11:30 +0000

    fair body warning few reasonable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
