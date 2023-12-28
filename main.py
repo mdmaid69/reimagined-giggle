01d4124a6687dcbef13209595409882df0569c8f
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:48:47 +0000

    spring comein any Oo period

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_volume(length, width, height):
-        return length * width * height
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
