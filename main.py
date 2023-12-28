225ebab723df5075a4cf6134e991c08fe5353b9e
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:53:53 +0000

    would regional mark flat uncertain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
