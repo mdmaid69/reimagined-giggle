9282b96153aceb7690398d0d6fa7dfaba642b31e
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:32:45 +0000

    urgent order clean squash godown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
