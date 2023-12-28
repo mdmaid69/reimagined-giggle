038904ebfb6cd438d6901a4ccd09dd4d65f3225e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:16:45 +0000

    unkind wear theirs wire petrol

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
