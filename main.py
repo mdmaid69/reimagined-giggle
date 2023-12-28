7e39894d3b0ac517a85ee7b8ceff20841030f719
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:09:18 +0000

    extraordinary spoil band turn tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
