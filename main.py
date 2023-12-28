8d60d8d8f41f47747d541330b53ef0b296a74632
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:23:09 +0000

    aim quantity wise female holdout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
