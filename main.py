bcb041d557c96f9275fedbe52b9716b28c310088
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:23:23 +0000

    maybe win conversation importance never

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
