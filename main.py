66d9b0ef283a503984157b4fb29f50ce48ab16d4
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:32:14 +0000

    pipe related both plain attitude

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  def reverse_list(lst):
+        return lst[::-1]
