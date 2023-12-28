a518c5e2a846a6c680e577d46665285ec0ef0f56
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:33:12 +0000

    closely 566end friendly land battery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  def count_elements(lst):
+        return len(lst)
