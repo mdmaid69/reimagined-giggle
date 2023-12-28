8b6b71ac0174a618314fe098dc80313d5eefd339
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:22:52 +0000

    excuse floor style nowhere discovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
