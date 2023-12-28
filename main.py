430efa43d767d88fe0ecfaab2468a16e79b2ff8f
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:58:38 +0000

    phone favourite environment post accordingto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
