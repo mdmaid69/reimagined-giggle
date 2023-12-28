f5bef52ba48e16003b9302e69e71631f38046910
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:36:09 +0000

    forget forget try spread tip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
