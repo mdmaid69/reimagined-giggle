9a0ab2313bfd60de4a736ea169e3cd264ec98ea4
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:56:23 +0000

    thick warm rule regional production

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
