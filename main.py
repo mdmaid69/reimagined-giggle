64a33a28669ebc0eecc1728b6593c9799741b168
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:15:05 +0000

    unkind realize doubt I join

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_work(force, distance):
+        return force * distance
