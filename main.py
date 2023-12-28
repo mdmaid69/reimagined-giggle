5479d9e02cdd486d2e41e0cadb15b5c0e34713cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:16:26 +0000

    unfair pair callout arrangement dick

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
