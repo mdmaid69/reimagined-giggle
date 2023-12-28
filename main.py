e9c89682f5d184d8fc5cb5b7838fcf6a04c43392
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:53:08 +0000

    question depend once single introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
