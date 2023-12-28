b592d12e5bead3c2cb8eabee7746d28df3cc7271
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:11:19 +0000

    bet breathe treat compete upwards

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
