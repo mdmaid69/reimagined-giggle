80de8764a5294b459e1520dbbb8faa2004cd3d9a
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:09:25 +0000

    kill in art whole sit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
