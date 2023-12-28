8715f2431dbb59d060b6269ef6b73489d296cad5
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:48:09 +0000

    on getback pause bend someone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
