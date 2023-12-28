57355d2c51c4fcd612d31889df413cd5c69054d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:54:41 +0000

    prefer evidence police argue behave

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
