dbc3258c791e76891a4273cda1143b0d5d053b72
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:03:49 +0000

    basically main dull along sweep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_length(array):
-        return len(array)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def count_elements(lst):
+        return len(lst)
