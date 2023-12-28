53845e957627bbc0d8538b4eeedb5e17f789fe63
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:34:33 +0000

    touch concerned anybody violence report

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def is_even(n):
+        return n % 2 == 0
