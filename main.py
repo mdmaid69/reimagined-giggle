201e36f2f81ebc49a5072fc22f432aaf99373dc7
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:01:23 +0000

    act model hope project possibly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
