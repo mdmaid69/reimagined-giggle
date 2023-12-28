a7bf6852b73de10e354c4f965f1972ce8b4f1f74
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:39:44 +0000

    rate since strict claim chin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
