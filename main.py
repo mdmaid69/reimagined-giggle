2aeccbd533571885bae7a0d997b0b79c80e319cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:33:47 +0000

    equal conclusion hurry Ii turnon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def add_numbers(a, b):
+        return a + b
