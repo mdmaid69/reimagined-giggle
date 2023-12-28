cae16cb707044c16f4fa9442c3ec014e7075e3ec
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:30:42 +0000

    mystery purpose drink defend moral

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
