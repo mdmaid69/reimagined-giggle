a8828c30a69e4cb3708a3220716a3a02e88322eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:01:45 +0000

    exhibition medium relation extraordinary pin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
