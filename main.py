6bb634372824b3e9b6575fe00ad355d8f855e627
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:37:15 +0000

    remain wave acceptable spend coffee

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def convert_to_octal(n):
+        return oct(n)
