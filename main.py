110f5593270a8983ea704400228122cc8637e79e
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:18:14 +0000

    culture approve unkind 179bite average

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
