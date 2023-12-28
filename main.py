14706bc3b1b735acde337dd42c376e54f527611c
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:51:55 +0000

    meal outside takeaway pay fashion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
