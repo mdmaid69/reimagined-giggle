1f6651f326bd8f91259398ecc5251657a6712e3c
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:29:53 +0000

    accidental high best even scrap

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def remove_duplicates(lst):
+        return list(set(lst))
+  def reverse_list(lst):
+        return lst[::-1]
