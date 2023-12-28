1c9d5dac3f3ea0a0bc1057e4cd2165002497fd8b
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:47:06 +0000

    trick shall cinema exist separate

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
