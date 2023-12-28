18a9dd335f81fac9242a7583f25455b1db51d63b
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:40:17 +0000

    negative carry violence remark Hh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
