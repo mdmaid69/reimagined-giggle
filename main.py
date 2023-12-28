1aabeda01f138db6a1ce864b4b8a567f999a631e
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:07:55 +0000

    seriously either love 567enemy attempt

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def add_numbers(x, y):
+        return x + y
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
