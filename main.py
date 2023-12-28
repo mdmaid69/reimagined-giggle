a6b3a1e8fd64ce8b7e33f7d5c38236bbca5e5f68
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:09:10 +0000

    guard hit whoever high tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
