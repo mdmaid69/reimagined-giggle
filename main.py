82c76c4e80d5aac3c48e1aee1db7b626f01674c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:31:30 +0000

    lock quality today boat contain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+name = "Python"
+print("Hello,", name)
