82791a5514fc08e9da7972914ab7522caf9d7cc2
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:43:18 +0000

    dry window level impressed moment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import datetime
+print(datetime.datetime.now())
+import os
+def remove_directory(path):
+        os.rmdir(path)
