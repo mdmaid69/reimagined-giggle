df0222a3b31e446569400520fc8cab9890879cde
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:22:19 +0000

    sink reply public activity custom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
