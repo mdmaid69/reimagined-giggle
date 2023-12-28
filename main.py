8b0c700c7638cf679c5d0e9726aa846603a6a7f6
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:21:20 +0000

    hand century well dear pipe

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
