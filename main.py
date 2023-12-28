50335e2ed0de261a28bc9d187691f7236f239985
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:00:03 +0000

    congratulations abroad goback(to) basic discover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-print(time.time())
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
