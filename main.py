75a8dacdf3d79ddfda829cd6b320019e4ed801f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:13:07 +0000

    cap mine quickly damp level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
