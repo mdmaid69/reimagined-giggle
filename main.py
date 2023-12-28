5f8a191a745edb029d76e95eec5fef21f07f3b5c
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:33:14 +0000

    pretty dance relationship convinced smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
