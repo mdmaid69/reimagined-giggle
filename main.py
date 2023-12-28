beba9e56b6bf0d263ded20d35e4b5a4f6b1dd6e9
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:59:30 +0000

    accident doubt clever height passenger

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
