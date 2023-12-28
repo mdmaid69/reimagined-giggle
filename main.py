9f3b4fc67134c4ac3f06fdc3c6d9b21c92c2f6ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:05:57 +0000

    send less prize row error

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def convert_to_binary(n):
+        return bin(n)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
