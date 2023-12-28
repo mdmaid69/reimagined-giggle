98188938ab9b0e3ac358ed562a23fb7698fe42ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:45:07 +0000

    warning shine skin wheel maximum

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_density(mass, volume):
+        return mass / volume
