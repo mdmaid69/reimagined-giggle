887467fe63758ac6a76431c5e5d501f47ad19f39
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:09:22 +0000

    Mm hard remember advice report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
