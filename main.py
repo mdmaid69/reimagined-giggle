a026c43f5a51afe5a7c1e04125ccc2e384ca43b9
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:42:09 +0000

    hide apply wake wave tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
