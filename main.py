669f6bf6b15ed6216406d91cd5bbef4898ac91c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:56:11 +0000

    persuade tomorrow afternoon limit image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
   import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def create_numpy_array(lst):
+        return np.array(lst)
