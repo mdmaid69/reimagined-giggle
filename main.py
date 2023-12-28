33ca5ea2bcd9b37f0a5afea627f3a5703b0be0c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:05:35 +0000

    passenger thumb side break Ss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
