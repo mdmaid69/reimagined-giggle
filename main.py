8876883e802fd5948903897115f781ac695248e4
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:52:34 +0000

    Tt protect share value recognize

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
