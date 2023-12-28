b15f4a3a1e44b9320d5d1159cc67b9edb201eaa5
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:02:12 +0000

    estimate bear pure secret wherever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
