fa4e46949b095774989ffee21bb09b113e9aabee
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:48:32 +0000

    rid motion save steep telephone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+print("Hello, world!")
