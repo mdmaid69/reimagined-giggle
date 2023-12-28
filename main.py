310c334b8e68bd8b2fa54642c84d1bfbf261fc67
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:25:21 +0000

    imagine tie front confusing narrow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
