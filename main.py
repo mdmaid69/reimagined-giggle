890292220e493bff862f4b0939da6e6c15e0cf64
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:04:46 +0000

    sure choice frighten shoot failure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
