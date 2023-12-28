7034150dc56a0ae402f4974275ebe50c7f0cc584
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:01:07 +0000

    connection question cope clear produce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
