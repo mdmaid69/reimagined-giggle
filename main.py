f76ce508a5ec1f2620cbfa83ab5a9a1add8a3c78
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:45:16 +0000

    fine offence majority church past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
