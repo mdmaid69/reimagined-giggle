cf6b738478b8907d8011df14c18ad4faf636a022
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:53:57 +0000

    failure figure shoulder party discuss

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_as_memoryview(array):
+        return memoryview(array)
