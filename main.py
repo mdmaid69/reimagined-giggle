44cd7d998b0a2d822e2bbdcea047113099c31c23
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:38:52 +0000

    absolutely save case crisis clue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import getpass
+def get_username():
+        return getpass.getuser()
