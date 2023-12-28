55f9b242cadc0b7146a77923e008becb94bc67c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:54:17 +0000

    fortunately search design area smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+for i in range(5):
+        print(i)
