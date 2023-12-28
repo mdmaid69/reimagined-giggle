28f6d9cdb3b3d14eea138a36533c41e17c406fd2
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:12:09 +0000

    active forget murder turnback our

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
