29d94bc6ea00dccab38d2cb38a53f85719145b26
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:18:51 +0000

    any comeround aware advanced turnaway

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
