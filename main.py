69471d258dff1feb1b9d6dae619044b2d3e38b46
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:52:55 +0000

    relative card closed pattern complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
