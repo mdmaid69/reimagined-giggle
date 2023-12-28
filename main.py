38ae6e4dccd5d616e593960d127602317a0ecc2c
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:53:05 +0000

    makeup here trust cruelty male

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
