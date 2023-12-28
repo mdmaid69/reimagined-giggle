9d36cdedd361244a1de9c7a383bda159a1d5cd86
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:00:17 +0000

    bridge problem tool about manage

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_power(work, time):
+        return work / time
+for i in range(5):
+        print(i)
