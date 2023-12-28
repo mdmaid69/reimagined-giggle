ba538f1541edcc61d546a810f2a92df25eeebc4d
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:38:54 +0000

    recent partner giveback pattern put

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
 import math
 def calculate_arc_sine(x):
         return math.asin(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
