4593edd00f22e532ebcea452fed1c775df5ca1c1
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:34:46 +0000

    kind unlikely inquire sound shoulder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
