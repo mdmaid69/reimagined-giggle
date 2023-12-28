22b76f748bfb132049c2a922ec040dadb4d529a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:17:15 +0000

    embarrassing catch Itself punishment accidental

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_array_item(array, i):
+        return array[i]
