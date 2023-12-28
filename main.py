1d6eab7ca731a44725ea6e0cecaab25a02d55135
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:10:34 +0000

    represent skirt tonight beautiful thinkthrough

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_typecode(array):
+        return array.typecode
