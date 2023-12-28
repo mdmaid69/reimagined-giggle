0a3614b175be2ed2c91c9f42712cfb651b5c7df7
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:24:57 +0000

    invitation earth wipe between mention

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
