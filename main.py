b2c1bbe7e9f1e6f1af1c6ed86eca2c21064e089e
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:58:38 +0000

    hardly complaint yours drink violent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
