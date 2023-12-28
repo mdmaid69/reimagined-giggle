03659ff6e7c264887826984adc60d331242ff0bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:11:19 +0000

    stir balance plant full fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_typecode(array):
+        return array.typecode
 import heapq
 def merge_sorted_iterables(*iterables):
         return heapq.merge(*iterables)
