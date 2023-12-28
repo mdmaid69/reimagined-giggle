2a281993999ca4eba8c401301d0466b75a031a67
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:03:55 +0000

    agreement crazy shake burst behind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def find_union(list1, list2):
+        return set(list1) | set(list2)
