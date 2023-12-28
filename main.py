7f5ee2ecca17712129d5ab6292db6c4f1a95440f
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:02:48 +0000

    prove height clever heart bin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
