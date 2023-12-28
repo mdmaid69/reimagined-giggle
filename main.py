f854276f0f113eb0222ca83318fb9cba7f81ab51
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:42:44 +0000

    community night base dull advantage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
