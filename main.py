c40bba40b23eb0d57dc2356a1d9faf5cc08408af
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:11:10 +0000

    roll dry design bound call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
