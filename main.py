7e184658647dae6e36b48045299669aa49bc5af7
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:45:35 +0000

    comefrom development conduct loose public

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
