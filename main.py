29fb83d67ee439947c8e8dd8ddaa3d2c30e23bfe
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:31:48 +0000

    same beautiful scared away seriously

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
