33567692a99422877e6587399dc9270293975f5c
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:14:36 +0000

    protect follow laugh tense fine

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def reverse_string(s):
+        return s[::-1]
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
