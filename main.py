bdeedd4d3b81367b7f8d3bd6b1aaa35ad75efc7b
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:48:10 +0000

    get evidence walk absolutely across

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import numpy as np
+print(np.array([1, 2, 3]))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
