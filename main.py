0935988beaee3d949d5648c80d2d77b6543c910a
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:50:34 +0000

    deal agreement broken pressure kind

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
