8dce826b50f2a7094ec6a9d97812590e47148ecf
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:41:32 +0000

    whoever stupid Ww comein impatient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
