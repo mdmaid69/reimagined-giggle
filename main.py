a744375708be54f480eadf4f398d67ccff8b2d5c
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:49:48 +0000

    debt Ff before address distant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def is_palindrome(s):
+        return s == s[::-1]
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
