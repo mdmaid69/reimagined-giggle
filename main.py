09a834e4bbdcfb32db37386f421db73c740c62b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:26:35 +0000

    disturbing back contact set relative

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
