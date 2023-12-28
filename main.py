3affea2c434945ce609df36abce3e3bfa9e1cfd5
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:15:27 +0000

    see peace increase exaggerate material

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
