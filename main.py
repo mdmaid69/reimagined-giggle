78d02f6d588212bf3f24f10ec86580388d2881cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:48:57 +0000

    stone protest scrap news influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
