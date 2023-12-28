647748799c882d8143f57e2fb31da5e707e1b958
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:11:44 +0000

    far lesson relationship interfere pool

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def insert_into_array(array, i, item):
+        array.insert(i, item)
