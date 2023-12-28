a3395189e46f62250bbb69cec5b155686c26495e
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:06:09 +0000

    split eat brother trouble for

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
