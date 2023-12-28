91341a0360eea7df566173cc75e580d7217f6a27
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:53:37 +0000

    cry congratulate cinema probably only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
