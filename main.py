959a8c622e873537fa8124d7e0ddf47fecf8a455
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:22:16 +0000

    score duty subject nature neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_circle_area(radius):
+        return math.pi * radius**2
