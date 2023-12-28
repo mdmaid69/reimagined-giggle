ac3163438db86571f3e34a246ad4cf7765636f51
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:25:01 +0000

    deserve play careless shame thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
