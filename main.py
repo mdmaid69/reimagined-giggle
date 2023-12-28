7f08e3360e9eecce71d6eb00c12dea449b7c5cb4
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:56:39 +0000

    say anywhere everything stamp agree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+for i in range(5):
+        print(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
