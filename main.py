a66fcfd03b6189601575c3e0c001de819b2dc09c
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:41:05 +0000

    remark trick advanced crisis hair

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def remove_duplicates(lst):
+        return list(set(lst))
