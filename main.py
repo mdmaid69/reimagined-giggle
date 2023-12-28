dd469d79ab58e43cbb497c40b4bf975c90dc104f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:24:09 +0000

    cope rescue greeting frequent period

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
