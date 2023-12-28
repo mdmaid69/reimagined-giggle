d5e944eb8c600472b95838859fcbfb6b7c57b6aa
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:47:02 +0000

    handle delight match both deliver

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
