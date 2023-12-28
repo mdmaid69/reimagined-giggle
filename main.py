8cb817684818ac62a404ecbe53cc46fd8ae87f07
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:03:47 +0000

    large refer touch between motion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
 def get_array_length(array):
         return len(array)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
