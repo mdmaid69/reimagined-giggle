da6cc12c296c4212bb98620ee6da889bc34333c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:33:04 +0000

    ring apparently ill comeout steal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
