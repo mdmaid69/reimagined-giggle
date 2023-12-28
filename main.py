c63e91575ed30e24315e7a2b5310e9b2b2e0d562
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:26:47 +0000

    instruction fork wash underneath window

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def sort_numbers(numbers):
+        return sorted(numbers)
