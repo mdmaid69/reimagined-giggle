2defd8361c4ce4ed5a97faae62cda610b407fb9b
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:43:22 +0000

    face evening wheel successful twist

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
