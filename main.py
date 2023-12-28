5b956470b52faa88bbd61307752d5a92290b2147
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:28:13 +0000

    570enjoyable forgive climb flow tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
