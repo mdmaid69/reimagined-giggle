7f904b8a93abf975dd9981f3e2204244c8153165
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:48:18 +0000

    reaction spring year bang uncertain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-for i in range(10): print(i)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
