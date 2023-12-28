7174e775d8d24131e74da87dc9e6f21a55f4b7c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:26:42 +0000

    crowded walk petrol compete wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
