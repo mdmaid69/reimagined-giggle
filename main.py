4b3015ea2fb4a3cbfeab80b12d0048c0be393797
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:16:31 +0000

    whether without note instrument I

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
