a936c60bbb2e27150e24e8d151732b2f6f522663
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:03:17 +0000

    loose problem succeed pronunciation tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
