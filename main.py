1985bba913c3e671207cd88e825dc49b61610476
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:56:22 +0000

    eye plastic month pause corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
