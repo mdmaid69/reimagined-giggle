ddbfb62f69c935f60f47f6a51e89a082674c7bfd
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:48:04 +0000

    turndown reaction string operate listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
