4536d11b4da2641c2533a1e510414d433a2afe53
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:44:39 +0000

    real tip match play congratulations

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
