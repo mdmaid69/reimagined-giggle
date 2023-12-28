398240a694a6e85c60cb328e3c097bad5ed003b7
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:13:50 +0000

    few shake force only like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
