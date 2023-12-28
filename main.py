bf7b7290003ac5e51a2c4ca3176966eccd55ac8b
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:24:57 +0000

    obvious steady mix attractive coffee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
 import collections
 def create_ordered_dict():
         return collections.OrderedDict()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
