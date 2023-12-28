845c88c9324946d380a0f69732616415f803546a
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:00:57 +0000

    needle next refuse me in

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
