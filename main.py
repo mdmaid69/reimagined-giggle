30500f003f6ed4ab9ebec7ed53297f3312f3692d
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:32:49 +0000

    valuable drink furniture damp plant

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def is_even(n):
+        return n % 2 == 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
