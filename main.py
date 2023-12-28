d3d6fb458bdad00dc8d93dc453063a8558125213
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:07:35 +0000

    level partner narrow allow usual

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_exponential(x):
+        return math.exp(x)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
