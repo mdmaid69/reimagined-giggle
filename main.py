c708bc3b4dcc72856f830442429fdbad64685062
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:01:26 +0000

    exist strong fail respect wind

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_speed(distance, time):
+        return distance / time
