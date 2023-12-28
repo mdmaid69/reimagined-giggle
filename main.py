f6dc36735441d96282fa4d56cd3db7c76630e2fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:12:41 +0000

    interruption deserve trick stay I

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
