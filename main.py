c7f71ea640d6fc70c4b2d1344eb3a841b285d2c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:09:21 +0000

    ability difference drink hit within

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_set(array):
+        return set(array)
+for i in range(10): print(i)
