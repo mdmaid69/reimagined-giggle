9f939c434f0b5311364ad794c65ce4f2711c985d
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:11:46 +0000

    conclusion exhibition team possibly action

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
