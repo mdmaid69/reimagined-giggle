84f7c679c3dcbf41f8e683e76d91515617350a8e
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:42:04 +0000

    think love decide unkind yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
