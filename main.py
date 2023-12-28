45c589ba8d482604c6b1e52ff58cde6873156cce
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:15:16 +0000

    afford pause like flow which

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
