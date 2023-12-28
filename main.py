e5351f80532427add001cc82f7fd7f42a59fd63e
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:29:23 +0000

    business conclusion change stamp reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
