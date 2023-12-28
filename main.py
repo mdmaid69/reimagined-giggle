02c16754dc3e89b068e25d809132768b5d2bd671
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:39:40 +0000

    shape bunch walk wish event

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
