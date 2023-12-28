297d5b0dee9fba1d145c645e400919e0bc52875f
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:50:58 +0000

    promise arrival allow admire benefit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
