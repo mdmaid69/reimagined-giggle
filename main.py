d46a20f641dbc99a29e01bcecaf30c6b7ab88339
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:14:49 +0000

    possible workon reject nearly real

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
