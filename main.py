7ef903a1f85766487843ae84d252ec5d20b944ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:22:46 +0000

    shop clear Kk surface drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
