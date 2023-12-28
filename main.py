811bb71c6a9a9fc13231f47669f63e2a53cca8eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:39:24 +0000

    mark suck behind unable rate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def find_unique_words(sentence):
+        return set(sentence.split())
