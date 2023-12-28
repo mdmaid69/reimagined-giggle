a6d3568e6cb52ff4516c8586028a88c2ff5c0b31
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:23:43 +0000

    dull support meal drink shopping

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
