68280e18c548153cdf3736e64dc6ee858dd371ef
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:59:00 +0000

    recommend 569enjoy computer month somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_typecode(array):
+        return array.typecode
