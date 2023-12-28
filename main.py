097ea5b05ef0b314a9722948d806ab840419b2fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:06:45 +0000

    sight arrangement sleep convenient everybody

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def is_even(n):
-        return n % 2 == 0
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
