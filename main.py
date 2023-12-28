c7d943e0e429f0e2f0d7a2dbd30c17acc3801763
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:57:02 +0000

    expect check limit central opposite

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def is_odd(n):
+        return n % 2 != 0
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
