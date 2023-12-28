73fd86e304f093decf9dcfb995658e22eef8c7e5
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:35:53 +0000

    towards nerve sweep ask wing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
