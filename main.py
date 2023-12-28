bbaf83ce26d5bf6f15237a7061681fe80dea4305
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:03:59 +0000

    borrow last might breakup convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
