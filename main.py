191f703eb6f270eb1ecf0026aa7798e67339e1e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:29:48 +0000

    ticket more gap willing but

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
