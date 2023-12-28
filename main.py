e21778b05925680934361e8e002ec56c68cee242
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:56:26 +0000

    serious light balance since whoever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import time
-print(time.time())
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
