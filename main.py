6001d85fabc5e865fccd166f74b745f8b714c0f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:22:05 +0000

    confuse throw suddenly loan education

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_odd(n):
+        return n % 2 != 0
+  def remove_duplicates(lst):
+        return list(set(lst))
