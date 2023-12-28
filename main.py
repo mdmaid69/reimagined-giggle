e56ab46ba0528e48c22ef2c716a7134fdf02056a
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:56:30 +0000

    memory getout neither firm why

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
