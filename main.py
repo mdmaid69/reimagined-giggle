bdef3348cf7a0ee4c738d9fa59f171fdad74dfb2
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:07:08 +0000

    serve report crash trip bus

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
