6c987e3c0721dec3dd25f530600e61ec386bcfc2
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:12:26 +0000

    passenger pocket special object breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
