172a47d79e84f09f9e0d989108272b4818f10282
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:59:21 +0000

    prefer next although translate deal

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+def sort_list(lst):
+        return sorted(lst)
