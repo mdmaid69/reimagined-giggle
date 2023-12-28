920e650959b6ed5aa30150572dac41c65248ef1a
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:47:01 +0000

    our range order common only

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
