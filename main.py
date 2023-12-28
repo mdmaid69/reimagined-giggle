6275e1067af1e84eca2c29537861a8ac03dee29c
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:22:20 +0000

    bury middle reject holdout polite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import time
+def get_current_time():
+        return time.time()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
