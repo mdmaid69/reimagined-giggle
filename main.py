92ea58041b6535ed1db7b48982adf229cecfcabf
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:07:52 +0000

    wonder unpleasant plenty important sport

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
