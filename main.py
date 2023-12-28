4a00faa2af2d357fe1e93aff0968b536ba70ea38
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:37:09 +0000

    wipe shoulder heart back continue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
