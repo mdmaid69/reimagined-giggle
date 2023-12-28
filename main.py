f75e2f4d777c61b3d7c398c4c6ffd802632ecc32
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:20:06 +0000

    complicated desperate plastic flat exchange

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
