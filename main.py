a7cb101a9d2ef5d10cd79b38203a24c5e9decdaa
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:36:25 +0000

    juice when definite any shake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
