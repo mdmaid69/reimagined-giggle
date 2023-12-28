7438cba8dee1333b75155af6a6ff552016cb5ba7
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:10:31 +0000

    within through lookaround have complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
