525fb98b5647fe48e373ad44892beeb12105b6c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:41:25 +0000

    lie march rob welcome makeup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_square_root(x):
+        return math.sqrt(x)
