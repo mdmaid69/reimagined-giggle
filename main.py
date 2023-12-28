f0ebaa742560f25f40a5c538dada32b7e3cac8bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:49:55 +0000

    bush thing transport congratulations call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
