a6e5bec21bace472845230a475b116e6054e403b
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:50:44 +0000

    region word unfair afford correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
