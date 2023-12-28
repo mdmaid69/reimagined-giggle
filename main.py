15cc543ba20dd1148ed68f0a64dcc8044bbef340
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:53:21 +0000

    present emphasize later rate wish

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def convert_to_binary(n):
+        return bin(n)
+  def multiply_numbers(x, y):
+        return x * y
