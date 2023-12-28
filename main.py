c36128c5963f2977be748693c9412766ce50d40e
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:36:14 +0000

    admit shoulder worth shape discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
