2d9fd67df296a8a88bed0dd59986565af5aa4b14
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:27:50 +0000

    worry sort quit trust surprise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
