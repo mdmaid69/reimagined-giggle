06ab610da7a750b8b01fc3c2dbede1abc4a2726d
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:01:35 +0000

    store stone giveback 568energy hungry

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print([x**2 for x in range(10)])
