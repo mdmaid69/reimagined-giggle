4218c25f6cb4f86651bf8530ae517724d0720307
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:56:14 +0000

    message something accept result voice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import sys
+def print_python_version():
+        print(sys.version)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
