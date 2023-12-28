04520d0011265b11b2fa360862746cfde5e31dd4
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:03:41 +0000

    complain couple again bunch public

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def is_odd(n):
-        return n % 2 != 0
+print([x**2 for x in range(10)])
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
