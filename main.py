29ce8c5d94f7946ac5a0dd3234fdb9a9a3c22c2e
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:07:51 +0000

    speak especially within hot respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
