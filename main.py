4cc432db7d20bd699934e0d7d03728b0b8fc9d0a
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:06:05 +0000

    bin method fresh out slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
