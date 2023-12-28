12b567a3671c9d36f5b8835ac27ae4aaf4842295
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:39:30 +0000

    pay indeed politics immediately centre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
