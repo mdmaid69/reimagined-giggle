3faa7199a5c817ba53a44c795586174ef6456530
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:35:15 +0000

    teach command runaway experience learn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+for i in range(5):
+        print(i)
