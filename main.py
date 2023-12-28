11f9ab381d235a33c8b5d844a83f47e4f3e20ed3
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:41:40 +0000

    proper least complain murder hold

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
