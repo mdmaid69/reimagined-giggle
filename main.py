9d2b5e22f116d44be8b47f26f58411508393ceae
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:45:19 +0000

    point bury loud bus punish

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_array_item(array, i):
+        return array[i]
+def find_max(lst):
+        return max(lst)
