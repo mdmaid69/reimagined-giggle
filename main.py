093f54add022d69290903e1a90632942ce81e2f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:32:12 +0000

    announce mean post wet soak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
