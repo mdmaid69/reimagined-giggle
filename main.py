5c4826d5ade53098c45e3919cc81c7eb3e703b6d
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:56:13 +0000

    tune total different in steep

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
