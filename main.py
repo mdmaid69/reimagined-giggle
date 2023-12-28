5c8bef150bcb0d0253be486439a889befbef64b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:30:00 +0000

    top almost honest murder invitation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
