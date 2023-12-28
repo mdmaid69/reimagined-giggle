41bf9f4910c4be27b5e0b28190aa1d2b8ed41999
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:18:38 +0000

    turnback breath broken belong tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
