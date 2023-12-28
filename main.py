c747dc03c91921cb0fd31fda6d1b7e4ff1f02be8
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:02:33 +0000

    load trade before cause describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
