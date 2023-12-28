d1daee19b19feff14ebb0818b0fa14984158ca73
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:19:07 +0000

    reject surprising road defend teach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import collections
+def create_queue():
+        return collections.deque()
