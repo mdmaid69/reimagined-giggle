f2aa1895b500098d79d4fd4d5e68c87fb7b4be78
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:36:02 +0000

    rub include so large combine

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_power(work, time):
+        return work / time
