e3201ac1b22288159d0bce0844937390017f2ec1
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:46:55 +0000

    cook perform rarely during bound

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
