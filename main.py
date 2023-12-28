22bb8ac913ede2debf339c42fcbe79a5fd41cc4f
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:52:19 +0000

    worse stupid complete interview Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-name = "Python"
-print("Hello,", name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
