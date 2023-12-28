7093f60ea7f3e54f9adb643d7d13e9829108695c
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:43:05 +0000

    cut relationship before muscle very

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
