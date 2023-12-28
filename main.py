99b789c684c152981bf1d490588d9355a5229473
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:27:21 +0000

    introduction name thick giveout account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
