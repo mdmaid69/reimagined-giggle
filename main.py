577b923f4307989524bd503d8e46fa1e00821633
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:43:09 +0000

    plain by goback(to) anxious stick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+name = "Python"
+print("Hello,", name)
