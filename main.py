6f6501b1b39bdfea2e26102dfd4495d0d46fd0da
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:39:21 +0000

    level dead detail through whenever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_perpetuity(payment, rate):
+        return payment / rate
