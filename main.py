6766c9b3fce1a83cc5ab3a6550c8bf3a15fcb7e2
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:00:24 +0000

    pinch supply order comment judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
