d3d674a1a9b71638ea560dcd818cf3de4e54aa89
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:33:22 +0000

    hold many learn comein upset

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
