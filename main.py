8c2af93baa5d22ca5cdb65438a2bdf4608358bf8
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:01:39 +0000

    sound moment effort rough across

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def reverse_array(array):
+        array.reverse()
