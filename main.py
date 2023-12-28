c77c71e9c18d7af53a60514da582403de8eb12bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:01:29 +0000

    look murder exaggerate turnover replace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+def get_current_date():
+        return datetime.date.today()
