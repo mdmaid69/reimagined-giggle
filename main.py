9cb307290929d12a59e2b66e827266c1e6b1df4c
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:13:20 +0000

    underneath object surface search mail

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print(sum(range(10)))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import platform
+def get_os_info():
+        return platform.uname()
