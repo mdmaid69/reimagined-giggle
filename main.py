817818e09edcf99809fcc3858c1fa0e24ea5f445
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:18:27 +0000

    kill this inquire who Kk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def remove_duplicates(lst):
+        return list(set(lst))
