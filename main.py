2cf2e349cede56c72ef65669c62cd907360ed52c
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:09:45 +0000

    nervous bunch little body church

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
