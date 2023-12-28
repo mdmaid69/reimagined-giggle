416e28bf2a92ac614bb3310e39dce054ed6faa6c
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:35:40 +0000

    bridge choose brush herself control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
