3a31eeab2f972ffaba7a50604c62d342cb74a9b3
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:09:35 +0000

    cross thing deserve speak through

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
