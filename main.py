5fe6c371941f846d662a689a9c0aea0b394b1b5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:50:13 +0000

    manner should lonely hope lead

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_user_string():
+        return collections.UserString()
