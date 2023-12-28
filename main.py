9f354461f64d61f6b35ddc33ad03ca26f23b15a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:36:42 +0000

    wet abroad violent relief upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
