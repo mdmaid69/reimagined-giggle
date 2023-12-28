230d420c5d96f56c241ce13e769cdd2965af786e
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:23:39 +0000

    tell bringforward comment average right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def get_current_working_directory():
+        return os.getcwd()
