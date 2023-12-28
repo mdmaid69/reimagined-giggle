6fcdfb8d679aecef74dfba85ade5fd7ef0befe87
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:55:58 +0000

    break proper warm support know

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
