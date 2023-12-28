827d75ef36152158dee66bb4571599a10ae94099
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:13:09 +0000

    arm trap pencil minute marry

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
