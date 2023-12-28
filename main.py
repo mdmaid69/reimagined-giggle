d666e854f3c2f3b6a14ded47c744c180a1296bc7
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:50:49 +0000

    shout murder thinkover characteristic long

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
