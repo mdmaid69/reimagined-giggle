f8891951951d6feee7921d6c04fc924ca1bf4dc2
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:31:37 +0000

    pay young race himself spread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def clear_array(array):
+        array *= 0
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
