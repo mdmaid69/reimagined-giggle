9bd5fba056a7fd21e8076e1e2612651d67781a82
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:48:37 +0000

    custom condition callback moral week

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
