aad30cca3240019f63f50d7b33d70a4facb10b7c
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:41:51 +0000

    tough consist where weak cook

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
