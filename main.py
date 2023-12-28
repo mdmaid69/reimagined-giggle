a7e30a510b6abc904709b89595b216040c23ba1f
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:34:25 +0000

    medium affect clear extraordinary sail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
