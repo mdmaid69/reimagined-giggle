d8f153a5467776cdf5f32bf4e02c1cfab99f34f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:01:03 +0000

    upstairs measure embarrassed habit them

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def sort_list(lst):
+        return sorted(lst)
