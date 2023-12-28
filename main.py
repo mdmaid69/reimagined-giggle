332ead61a02d5b50c66ff63e4e42ffb8dc6f6486
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:07:05 +0000

    themselves general sleep purpose cross

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_area_rectangle(l, w):
+        return l * w
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
