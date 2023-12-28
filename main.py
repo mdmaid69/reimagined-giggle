69dc842aee89360dbac32d13f8737a7fb33951d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:38:13 +0000

    authority union valuable steer Bb

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
