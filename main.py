f0a69a74c8afd0bca9f20126b091bc7853283221
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:49:29 +0000

    children question certainly bother dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
