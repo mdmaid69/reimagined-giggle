29e5a73c7ebe86c238484857457af87977a84d28
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:01:16 +0000

    press theirs our reliable right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
