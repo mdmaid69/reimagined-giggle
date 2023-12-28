be37a7118ff2e31f13f6df4618df89a894cd3eb1
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:14:41 +0000

    inform lose sister slice swimming

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def square_number(x):
+        return x**2
+import array
+def get_string_from_array(array):
+        return array.tobytes()
