979333eaf225fe93aad730390df1e733e39fad5a
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:58:20 +0000

    heart movement instead callback emotion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
+import os
+def get_current_working_directory():
         return os.getcwd()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
