eee7da22394bee24caa481d8a05a3e687db9a95f
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:21:07 +0000

    keepoff fat coat direct talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
