1db53c9400af369ae5184ec6084fe7e62f18367d
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:44:18 +0000

    reply speech ticket human extra

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
