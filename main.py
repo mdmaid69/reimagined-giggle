ee5ba0198b6f7159da9768dddcae4e4c24134373
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:18:43 +0000

    spring region lock reply break

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+print(sum(range(10)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
