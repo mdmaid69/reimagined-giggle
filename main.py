5f21511515eb58b82498bccd65ce4e054648301a
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:06:10 +0000

    recommend recognize judge length rub

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
