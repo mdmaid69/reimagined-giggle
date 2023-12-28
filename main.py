a02740ef33042c341d15de656c117f211f1a8320
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:36:58 +0000

    show such means size face

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import sys
-def print_python_version():
-        return sys.version
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
