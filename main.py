1d5e06f806a99117f029365cf82a1078aa7ab83d
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:20:18 +0000

    offer reply squash rain stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
