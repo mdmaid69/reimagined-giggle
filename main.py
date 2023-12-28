b2923c8e52e620e0c1ffead5e1ed5d45e2668130
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:44:56 +0000

    ground black act describe creature

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def add_numbers(x, y):
-        return x + y
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
