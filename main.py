37f76bb4f72841b6a593b9c07f77896bc508c6e9
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:34:42 +0000

    deliberately couple lost go belief

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
