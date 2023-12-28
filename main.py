437e799c4df6f32587e4ca1707412201fa644fde
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:04:16 +0000

    muscle guide surprise sentence very

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
