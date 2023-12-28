cb892901dde6f606cb067d822464023e67929b68
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:19:12 +0000

    section course bush of workupto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
