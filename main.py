28c22cf94b02a6e7a22279db727d1cc16b20ab11
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:31:08 +0000

    disapprove achievement sweet brush healthy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
