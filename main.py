cc33688f13efdc9530c7075658000299017d8ae3
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:28:16 +0000

    bottom past talk year judge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
