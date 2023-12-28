d8fff81a20640c0e001d0f0a774cd99c0d3ac7f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:36:19 +0000

    congratulate mess discuss everybody last

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
