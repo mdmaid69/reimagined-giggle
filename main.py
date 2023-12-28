e0e61cfbbf78c824a65f864e8f60c3918f02c58e
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:45:52 +0000

    whoever imagine winter head fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
