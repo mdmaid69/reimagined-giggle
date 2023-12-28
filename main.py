838aac45553e65e4d292be037d0af73b3f583f38
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:42:53 +0000

    software content drive corner mail

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
