62b80aa73e5f1c9852fbfb66ac2f357610198090
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:26:14 +0000

    expect arrival ask disgusting argue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
