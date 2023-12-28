f79de6787afb3dd8282b3d69e8d93dd01b035993
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:06:22 +0000

    boat easy under chin although

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Characters:", len(text))
