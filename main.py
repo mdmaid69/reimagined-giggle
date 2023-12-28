8630f347a61e805b7079ef56341f915c8dbb4894
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:38:41 +0000

    fall life join success ball

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
