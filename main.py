a39bf7b026a94cec82aa0a200dcb95a91500e2be
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:29:32 +0000

    accidentally stage talkinto crazy maybe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
