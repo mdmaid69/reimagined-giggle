7719cba5587cf9af54a4d3ef308b21540adc0066
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:34:02 +0000

    main brush during after address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
