705606633dd0eecabcc01ede9f101353a788b776
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:03:39 +0000

    organize base many male just

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import time
+def get_current_time():
+        return time.ctime()
