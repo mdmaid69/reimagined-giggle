a926195361cf0d604345eecce2a586deb8ba3cd2
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:46:47 +0000

    noisy close hurt language persuade

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print(sum(range(10)))
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_speed(distance, time):
+        return distance / time
+  def sort_list(lst):
+        return sorted(lst)
