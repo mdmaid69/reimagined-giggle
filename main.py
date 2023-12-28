4ee91a515fb77fc97279f3223de9ddea1915ade3
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:54:04 +0000

    majority hate major intend obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
