ea21b9e6aeea03508447950fa4e82d9d7c5928eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:35:50 +0000

    system no display his region

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def add_numbers(x, y):
-        return x + y
+def multiply_numbers(x, y):
+        return x * y
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
