2146e02d6d061bddacfec0801c9cc51e5b0e2cdd
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:44:18 +0000

    apart ourselves floor fetch takeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
