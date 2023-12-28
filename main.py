d4d009d451766158bfd9c08549b4bd039aae5051
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:58:30 +0000

    goback(to) arm ticket will judge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def find_max(numbers):
-        return max(numbers)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
