81c65287f5040bc06c7df3901137e0ab079d3143
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:18:55 +0000

    him set reason throw tip

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
