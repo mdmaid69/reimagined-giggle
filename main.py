e198a72390522163b2fe9659193eb6fed0d15aeb
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:10:48 +0000

    aware honest wall result stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
