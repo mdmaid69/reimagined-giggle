5f02e42939009d1bb4eeded8f05d17974b4aa81f
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:41:44 +0000

    increase so picture several afterwards

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
