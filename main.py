c0928dfa69ebca7d5ff2b1028fd621f3fd5e5c32
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:55:56 +0000

    confused day upstairs imagine organization

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_slice(array, i, j):
+        return array[i:j]
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
