56014d6942b42b556440ab0487974ef6acebcea3
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:16:26 +0000

    many sweep lock cause burst

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def subtract_numbers(x, y):
+        return x - y
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
