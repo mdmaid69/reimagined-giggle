7b8e5e7f05182c2af565d60fa4dbe0388653088f
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:44:22 +0000

    above choice field then active

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+text = "Hello, world!"
+print("Words:", len(text.split()))
