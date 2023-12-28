6f57070ca4468ed3baa360770b4aaca79ca616e0
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:13:47 +0000

    sensitive no quickly tape understand

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
