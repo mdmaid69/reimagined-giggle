5576559a245ac14dc9f9d72f0f21341a7cb91ebf
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:39:58 +0000

    introduction must degree radio extra

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def convert_to_octal(n):
+        return oct(n)
