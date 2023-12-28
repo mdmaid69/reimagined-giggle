31e4d7a7da02e2cf376fe4f1b1ef7ec1d4eace72
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:49:21 +0000

    find sweet clever heart press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def remove_duplicates(lst):
+        return list(set(lst))
