6984fcd30d37b49e91bbd23a5871fa4d2616c0bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:52:27 +0000

    each beside naturally term shelter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
