70afa79f8d419289bb9707bd3fb193d88438ee18
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:52:19 +0000

    include nice occasional authority uponon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_power(work, time):
+        return work / time
