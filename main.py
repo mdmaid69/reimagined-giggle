35495c91ed282e121aebe8e1162d19f927af8b49
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:49:27 +0000

    exhibition wing hers artificial corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
