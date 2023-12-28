06d148befe08be2509fb0230ed8841db1791f911
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:57:48 +0000

    care pot communication operate up

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
