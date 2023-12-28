54b2aeda9ba5f9830464cd151c1e9e1ee6c9df45
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:31:39 +0000

    here drop announce circumstance complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def square_number(x):
+        return x**2
