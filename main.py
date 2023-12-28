794a151379f474f024a07a747ca37976acdb13f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:08:26 +0000

    callout fortunate crowd important order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
