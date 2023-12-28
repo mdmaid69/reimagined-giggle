faa42323f406f342b6b24080c0150769927c6917
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:08:46 +0000

    display opportunity need work pipe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
