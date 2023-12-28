b4c7ba5e24aa021a480d1eda2ef8861a429d2e45
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:36:52 +0000

    curious mine impressed aware seat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_typecode(array):
+        return array.typecode
