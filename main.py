930a29c3f61d6ceda1682a9b45db6a49b255a3a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:25:47 +0000

    accept Pp early complain concentrate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
