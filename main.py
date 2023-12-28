4d0e6b53fd861cdc8611c7ae39bd7a1649318f37
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:21:09 +0000

    ill experiment union few detail

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
