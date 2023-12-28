429b73d6e27ffb0aa6779f2a7e4c93d89163d6ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:09:06 +0000

    please distant repeat tidy contents

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
