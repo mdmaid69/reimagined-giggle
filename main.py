92328781a1f04a404136ce855f6fae448ba9f663
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:25:37 +0000

    kind effect downstairs rich actual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
