c8e95c3d6028e0a7de5a10cd782f647b46f8dd53
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:12:31 +0000

    own secret and substance fast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+print(math.pi)
