3b1a204de9bb2b9e801544bc53db0627af25f07c
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:39:36 +0000

    contain manage upstairs eye grow

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  def convert_to_hex(n):
-        return hex(n)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
