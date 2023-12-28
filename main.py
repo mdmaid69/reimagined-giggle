acf673f5fa226f882ba15e8208ac7ba1f523d9c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:12:00 +0000

    pass near impression price education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def find_max(lst):
+        return max(lst)
