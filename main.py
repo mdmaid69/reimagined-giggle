6f859f87c7d23871ee4dc3d56b43f5f3c6067c22
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:24:01 +0000

    poison range breakup reliable reserve

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+def calculate_speed(distance, time):
+        return distance / time
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-for i in range(10): print(i)
+print("Powers of 2:", [2**x for x in range(n)])
