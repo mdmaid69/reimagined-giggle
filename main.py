7139c79dfd9864f8a1e24955f2e959c3988fe485
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:26:48 +0000

    free completely accommodation hear little

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+for i in range(10): print(i)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
