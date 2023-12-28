c91fcee3b851c4f7a5e4992206b02a9907b0cd8c
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:04:40 +0000

    habit 567enemy drive successful great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
