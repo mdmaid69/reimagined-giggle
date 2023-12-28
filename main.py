a701f20a72096b08ddb2be62072b6275aacc9b78
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:46:12 +0000

    keep pause and attempt term

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import sys
+  def get_python_version():
+        return sys.version
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
