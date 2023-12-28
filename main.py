d99c6644e1b9cad559b832a09a7ebbd7a20f5a26
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:20:37 +0000

    refer ring become contact trousers

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_length(array):
-        return len(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
