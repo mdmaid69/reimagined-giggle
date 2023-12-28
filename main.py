2dcfc3e1226dba49172698e8e5055e91e9ad3c9b
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:11:29 +0000

    arm every lonely glad flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
