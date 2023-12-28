ab3f9a6bf5aca7ab0259293f5a03e93c1f55b694
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:07:14 +0000

    apparently fall admire compare drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
