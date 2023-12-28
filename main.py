dbd132ac5fc8df321770c51eb9379c43e19a8444
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:00:34 +0000

    communicate bye warm foot factory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
