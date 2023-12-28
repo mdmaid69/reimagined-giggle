37633aa7ae4f989557f143b978101a07dee52145
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:38:32 +0000

    familiar wind successful keepoff down

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
