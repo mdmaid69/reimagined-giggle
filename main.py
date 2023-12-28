91494fe0b842ffecef142cf3dcfc42183216a1ff
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:24:21 +0000

    praise occasion Ii fashion play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
