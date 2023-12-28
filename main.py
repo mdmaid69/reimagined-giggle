88efd8488a5bcfa0073edb8c926732ab184360b8
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:57:17 +0000

    imagine account ready Ee right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
