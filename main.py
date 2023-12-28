f50325936f8c332d1e8872928c0acd33855f1938
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:01:53 +0000

    thin protection yet more waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
