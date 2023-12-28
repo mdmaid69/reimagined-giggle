3b8f1d33bce9b615d46499fa5522c7756a202f93
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:51:19 +0000

    under live weather confuse pity

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
