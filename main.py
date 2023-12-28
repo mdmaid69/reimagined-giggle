30c0bcb93e1a0ad63c47a1299492d6e324f8c2c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:40:30 +0000

    plant slide conversation wish born

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def find_max(lst):
+        return max(lst)
