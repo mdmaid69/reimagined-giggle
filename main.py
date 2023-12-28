f37eb6805a49a697d872d183fc7747cbecfcaa1f
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:13:03 +0000

    over connection suffer company hear

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_pressure(force, area):
-        return force / area
-def square_number(x):
-        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
