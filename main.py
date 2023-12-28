0d6e951c8bb508d9d9688188416b6a40451b9cbb
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:17:48 +0000

    week not cover dress maybe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import platform
+def get_python_version():
+        return platform.python_version()
