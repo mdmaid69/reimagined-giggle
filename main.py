84eb874c674766831f0bd334f216c3c227d346b6
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:28:39 +0000

    allow trip occasional confidence separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
