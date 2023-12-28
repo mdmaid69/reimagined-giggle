3e14e6c05fe83380a6921d163bb8c0e262a7f25b
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:57:13 +0000

    thank fault pleased lean fasten

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
