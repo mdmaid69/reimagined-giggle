fa4fdbc677168832d361165515b895b0e179452e
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:25:37 +0000

    slow one congratulations harm round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
