3d07d31eb44420ba61936ee9fe2034b74e53c3e8
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:32:49 +0000

    address shop apart rain corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
