bdc69af9c0436d0ad838eb2d705cf29df8cc9fc5
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:29:40 +0000

    bang near zero trade silent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
