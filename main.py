65ba2c9ec82142c817f94f5adb1b605924303cca
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:13:10 +0000

    feeling bang makeup turnon afternoon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_current_working_directory():
+        return os.getcwd()
