62eebedc4d92f21f59a6b6508dc6766fec2a2c8d
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:21:02 +0000

    collect mind share free trick

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
