aca2f100099b52654f03b9d9421f6fae03bbb705
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:19:54 +0000

    beautiful personal custom owe equipment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
