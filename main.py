d07764e0ae0700e7cda8142edfb4b90b6b025fbf
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:35:24 +0000

    drink zero club whistle medium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_user_list():
+        return collections.UserList()
