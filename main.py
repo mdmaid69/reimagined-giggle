018ce5c9f2b9f0ee70c578876f693d91ecf2eb88
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:14:13 +0000

    escape system whenever accept protest

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import multiprocessing
 def get_cpu_count():
         return multiprocessing.cpu_count()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
