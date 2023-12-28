0d675b228a0c5d6c19945afe8802e33ece45a7de
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:47:10 +0000

    short but runaway either opposite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
