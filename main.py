6a9cf0f21c4ce64416539ca955f430f8c5de37b3
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:50:48 +0000

    score act hand drink small

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def is_odd(n):
+        return n % 2 != 0
