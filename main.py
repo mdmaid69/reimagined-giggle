76fae209271013ad8ec37231d2624b3d208e98a2
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:43:22 +0000

    offend later trick although nerve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
