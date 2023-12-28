a3205d4ec10103bebb4fc4e949cfc0ca4de04b35
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:23:55 +0000

    classroom fill commit season flight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import time
-def get_current_time():
-        return time.time()
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def convert_to_octal(n):
+        return oct(n)
