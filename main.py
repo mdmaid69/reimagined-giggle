47a4e133954d71ffb565e610bd3dc9d890227955
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:45:44 +0000

    think this possibility behind low

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+print(math.pi)
+def calculate_area_circle(r):
+        return 3.14 * r**2
