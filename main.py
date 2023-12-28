94311d58987418f8ba25b460d1e4a7d6562074fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:08:49 +0000

    scream stiff grateful past minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import sys
+def print_python_version():
+        return sys.version
+import sys
+def exit_program():
+        sys.exit()
