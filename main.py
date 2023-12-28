383cca1d021f7265d37b138830a93f4bcf6b55db
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:43:06 +0000

    knock turnaway fight remain cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def convert_to_binary(n):
+        return bin(n)
