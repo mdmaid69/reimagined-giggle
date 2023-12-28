a133941e7f3f747301882d317ddb905ac7d33f94
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:26:13 +0000

    challenge private least protest bag

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_working_directory():
+        return os.getcwd()
+  def convert_to_octal(n):
+        return oct(n)
