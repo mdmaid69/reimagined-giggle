fbde0e399354226f62cdbe6f3bcf18c853f97de6
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:37:58 +0000

    permission unable worth rest reaction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def add_numbers(a, b):
-        return a + b
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
