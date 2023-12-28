5ebcc847b44db6ee07544e54df81b350ecfe8c9c
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:46:24 +0000

    squeeze almost home wave complaint

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
