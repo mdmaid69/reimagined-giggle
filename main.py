4cde87fcd14962c7b8b131cbf9a6d2597b54f16c
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:43:45 +0000

    its feature motion many bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tempfile
 def create_temp_directory():
         return tempfile.TemporaryDirectory()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
