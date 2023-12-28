940b6d2f3ba0aceeb3e4a1443c71e8da9fc47a4d
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:32:04 +0000

    today separate support far thin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
