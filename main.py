ae55fbe8c3c1b6166e2913a07945c91c789fbd1b
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:06:42 +0000

    shop free keepaway breakout division

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import datetime
-print(datetime.datetime.now())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
