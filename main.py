21c14d2189510634ab187039ad49af7032e7c145
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:38:20 +0000

    dangerous start shelter split genuine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
