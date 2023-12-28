512ffa59d3cd6f61a3be57cdf19770343baad887
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:20:33 +0000

    music want soak holdup practice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_hex(n):
-        return hex(n)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_array_as_bool(array):
+        return bool(array)
