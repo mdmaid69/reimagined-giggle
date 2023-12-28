d4e779733a4e742545187f8abc868befa1919c22
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:46:07 +0000

    mention shade behind lower meaning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
 print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
