cca6440e0a3d1bcf765933ae783d79e74bd2cf92
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:43:58 +0000

    drawing rough contain couple honest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
