e39d7b479e643968a5b3d136ea98a8f4a6ff269e
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:25:19 +0000

    sound flat person protection impression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_set(array):
+        return set(array)
