5396a653def5d97af6e966d8588cb81c21649f31
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:40:53 +0000

    affect or confidence underneath quiet

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
