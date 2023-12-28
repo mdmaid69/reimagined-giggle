d29143188532a2be5258acfdc68a33521483df18
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:10:58 +0000

    start politics concentration whistle Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
