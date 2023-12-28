92b8576298985de635d8fe3256a1df2bc9236b76
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:46:05 +0000

    colour year shrink unfair cross

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def is_even(n):
+        return n % 2 == 0
