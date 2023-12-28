879316461819a344bebc8c8bddc24a047276d652
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:03:58 +0000

    face protest strength pull little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
