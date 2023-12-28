4521f8952097d260b28ca93b602471ce4e639f82
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:03:27 +0000

    church know wall intelligent sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_work(force, distance):
-        return force * distance
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
