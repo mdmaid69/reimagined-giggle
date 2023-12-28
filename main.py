07063b30eed9364dbb22ddb633aba6bdde801817
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:25:39 +0000

    business perform stay patience pupil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
