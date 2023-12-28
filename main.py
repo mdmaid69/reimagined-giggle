e29928d053e82958190ffa7521584a2f247dc110
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:33:27 +0000

    tooth description dress keepaway underneath

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
