b6cb770c1e3f1b460beb6b7ef6db6bed4d5fbacf
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:41:49 +0000

    repeat far workout criticize aware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
