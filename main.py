c08543f2bdd392d6b408356cebd82723303629c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:28:50 +0000

    whether wealth chase consider trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
