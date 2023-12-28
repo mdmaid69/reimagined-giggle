d82a6164539e8b1649e2c4f6d9213f5f607da5ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:30:56 +0000

    address tooth period nonsense 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+print(sum(range(10)))
