8acda61a0f656946cf198961855f6139d3209c28
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:45:52 +0000

    crime same stare concentration official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
