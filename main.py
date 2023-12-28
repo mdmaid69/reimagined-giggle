bd1a9fd492bbe226db84a1838476b82736ff0740
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:07:35 +0000

    Uu offend body responsible failure

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
