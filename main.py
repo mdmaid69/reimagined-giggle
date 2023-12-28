6a946f321a61e09d0a4051703649814f472e3350
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:47:32 +0000

    address habit loss turn expression

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
