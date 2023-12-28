9decdd9414f726bef845e86b45a60db9c4b0cb0a
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:28:09 +0000

    occasion extremely form survive react

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def subtract_numbers(x, y):
-        return x - y
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
