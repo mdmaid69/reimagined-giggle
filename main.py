eaa847bca6bae3741c729b3674d263b0cb625549
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:42:07 +0000

    foot half year shadow go

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
