acd73cb20f8994b6baa31f2495abc28421bd7194
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:31:40 +0000

    nature last consult settle border

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
