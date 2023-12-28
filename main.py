fd029a0b42a97a10f2ec5be4f83355c003042a8e
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:23:05 +0000

    properly benefit punish door suddenly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
