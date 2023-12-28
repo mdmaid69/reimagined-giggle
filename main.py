3074d0fe8686b96d194cc3c1c49a397de46c0fd1
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:45:35 +0000

    lay central rise acceptable wear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import random
+print(random.randint(0, 100))
