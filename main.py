5b836b0cb95401b5862fac20db2064dd05952bf3
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:14:49 +0000

    sorry around reply favourite difficult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
