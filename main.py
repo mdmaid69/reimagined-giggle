a0557dfb394abe39ba85b7eb9a5025dfd20f4476
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:00:14 +0000

    570enjoyable muscle gather even juice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
