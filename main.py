a5a7045b052b605150eae61eee3763da71dd342b
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:27:56 +0000

    time warning fill branch bin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def count_elements(lst):
+        return len(lst)
