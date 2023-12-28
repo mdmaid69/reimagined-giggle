5155c312072c3a62dd716d94ca15b21e413375d4
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:48:54 +0000

    uponon feel mystery zero exactly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def sort_numbers(numbers):
+        return sorted(numbers)
