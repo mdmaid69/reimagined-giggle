f3671d0796079b0c9b786adde8e3eb3ed73866c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:19:49 +0000

    comment 564encourage grand wind fire

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_pressure(force, area):
-        return force / area
+import sys
+print(sys.version)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
