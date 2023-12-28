bb33f01678dc823d7a4b55ba35e3c334d22387ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:21:20 +0000

    development clothes cross risk whistle

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def convert_to_hex(n):
-        return hex(n)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
