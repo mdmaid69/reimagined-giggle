d4a744112e5d59d1a7d892865aa45806189070ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:35:28 +0000

    meeting become valuable murder reduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def reverse_list(lst):
+        return lst[::-1]
