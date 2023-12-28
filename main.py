1595daec329cc285676e659cd0ef33a405fb52e3
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:18:34 +0000

    minor design awake classroom healthy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-print(sys.version)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
