660eb96357aca1f9407ccbe253fc6d739095f417
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:17:50 +0000

    ours plant concerning breakup that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
