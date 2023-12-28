b1f6e870dd81328bccd8c0bc997261cffdd03261
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:00:06 +0000

    lookafter until thick weather drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
