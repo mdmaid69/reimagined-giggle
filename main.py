510f6645e5440eda5bb4170bfd8706514653792e
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:36:38 +0000

    accommodation respect treatment lock aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
