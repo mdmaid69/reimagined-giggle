b9692faac9e90308d294385e562ae7a3e6c1df2c
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:48:28 +0000

    conclusion display minority patient motion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def reverse_list(lst):
+        return lst[::-1]
