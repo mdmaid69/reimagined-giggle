99e47558f54954eb1ec4a761456478266d91d613
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:01:11 +0000

    plan before praise voice 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
