f5b20009c4ee33fff332db94b6f7b8596a0d4e4a
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:24:07 +0000

    deal by squeeze in limit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def is_even(n):
+        return n % 2 == 0
