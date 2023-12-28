8fcb083120907d38d5a6dda781ec3422330c429d
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:36:11 +0000

    boil secret appeal Zz opposite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
