a8e30fa75fe5c1cbf2874360644b79204daba0e5
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:16:03 +0000

    new door round left weekend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
