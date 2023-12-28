dcfb5b588c4ee580b36e8be16642c833fad82f16
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:51:58 +0000

    571enough do surprised press bringforward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_speed(distance, time):
+        return distance / time
