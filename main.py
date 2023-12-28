0fdfb57a4a72354c2f5366b5b690b010ab280183
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:32:31 +0000

    command deliberate attract serious bank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
