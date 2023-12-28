9130f79142cccf3ea7bdf18a4246c4db53de7d83
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:55:52 +0000

    pass kind project reject variety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
