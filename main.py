dd3ddbb59c14fbd21516f6184b35e8d22faba46c
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:26:06 +0000

    go negative interrupt lot scale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def find_max(numbers):
-        return max(numbers)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
