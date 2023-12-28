b8f6c13d55e7667591f143e3775a3f6a5fd739a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:27:29 +0000

    sorry write lie also except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import tensorflow as tf
+print(tf.__version__)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
