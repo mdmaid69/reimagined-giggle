c2b666e18910bdb460d81c02c2db4353dd05334f
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:18:37 +0000

    drop Cc price land tense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
