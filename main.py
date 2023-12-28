51f49f24a786eb17940a1e286ee95ce84388b24e
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:31:22 +0000

    petrol pick neither point instruction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def create_queue():
+        return collections.deque()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
