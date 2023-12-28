9f2888d3a16f56657ab331c3d2a701a6609ac4c1
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:32:48 +0000

    fail artificial bite grand hand

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
