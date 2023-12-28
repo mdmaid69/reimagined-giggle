c760ed6c9ca10adb0ee5b0c3f18ea027fd08b530
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:25:44 +0000

    but recently trick platform part

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
