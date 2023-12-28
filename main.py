fde9202f6814b18d4ea7383451f8a88da1ea2254
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:33:18 +0000

    church related manage 573entertainment positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
