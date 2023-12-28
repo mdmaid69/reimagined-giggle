0d3664653c0cfedd15cfface68fe0fff441abc32
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:21:31 +0000

    hit nowhere exist lesson contact

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
