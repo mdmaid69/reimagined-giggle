a657d9893de7b0339728492015e710033130e549
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:56:42 +0000

    comfortable past pencil fact bother

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def subtract_numbers(x, y):
+        return x - y
