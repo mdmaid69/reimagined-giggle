d0f57d4ddce137e46688b8da838225e655131561
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:48:24 +0000

    sea otherwise exact straight thing

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def reverse_array(array):
+        array.reverse()
