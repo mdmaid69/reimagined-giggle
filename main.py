2ca61625293e0962425620bd3d0f1724656d58e9
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:29:55 +0000

    organize dear fork manner term

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_cosine(x):
+        return math.cos(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
