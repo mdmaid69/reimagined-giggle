f08cc6673dab57bf40d6bc3b98a21710309e9dbc
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:02:06 +0000

    prize private swallow adjective speech

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import time
+print(time.time())
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
