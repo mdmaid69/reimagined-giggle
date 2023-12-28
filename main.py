4a02247cddee82fd02ce7c22526103ed7a9aa028
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:56:06 +0000

    accident provide appeal difference understand

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
