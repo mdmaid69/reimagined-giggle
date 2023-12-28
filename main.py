d174943a2983384aad9d13de7ad1eb389b141544
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:17:06 +0000

    clever open passage suit sudden

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
