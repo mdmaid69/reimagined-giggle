395edd8e16be8aa51c73f17a9f6eee33e8fe9923
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:23:20 +0000

    invite soft basic basis punishment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
