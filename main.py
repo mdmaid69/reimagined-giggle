7f42908c6383f3b97fb22fe6047430efe65ceba8
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:08:55 +0000

    successful search tradition dare actually

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
