7f455bcdbc3195d4d1b4cab188246d7925e2c70a
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:09:51 +0000

    broken kind everyone project apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
