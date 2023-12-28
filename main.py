b6f066be7726ceb09fce0946bc3f019e8ef0567e
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:47:47 +0000

    physical problem try strict beautiful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
