e73b5529029c24bcaadf0281ab857554cd43c8ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:26:54 +0000

    follow system pick geton surprising

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from collections import Counter
-print(Counter("hello world"))
-def subtract_numbers(x, y):
-        return x - y
+def sort_list(lst):
+        return sorted(lst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
