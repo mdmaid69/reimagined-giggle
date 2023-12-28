dd06a2480b369173dd8bfcfb1b9f985edfbaa656
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:14:25 +0000

    twist wall coffee year fast

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
