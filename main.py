00b61df6af8048696bc52730a682999c1cfc5325
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:43:58 +0000

    sorry holiday what line attitude

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_work(force, distance):
+        return force * distance
