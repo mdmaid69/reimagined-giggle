1f5b7e8761abba1461bd48ec2496fedd907dea86
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:44:25 +0000

    letter never takeaway heart remain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
