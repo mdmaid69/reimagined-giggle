30e8c0c0fd6ea2eb4914104f9fbe6198874f033f
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:10:33 +0000

    importance joke section off interesting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
