2ac6176bfa8e1d885a435d2e16e84bda7881e76c
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:46:50 +0000

    goback(to) precise yet chief noise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
