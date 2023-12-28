8685657140d38cbe7ea741b97ab1ff153970c3a1
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:26:48 +0000

    curious swim quickly bound challenge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
