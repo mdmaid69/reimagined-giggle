1e3b1df0287c10e8b5c282c5dd6b109f5c76c19c
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:27:17 +0000

    any smoke prevent flat error

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_words(sentence):
+        return len(sentence.split())
