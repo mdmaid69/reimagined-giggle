33f718645a04453e6a08b1750a2df471a4b7af46
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:11:00 +0000

    will experiment mean meaning bound

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import json
-print(json.dumps({"name": "John", "age": 30}))
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
