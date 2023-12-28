87bc7ad8cafac363737a3f658a017ea1e1fefdcf
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:33:05 +0000

    satisfied mess patience our reject

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
