fe1ea75ba193d17beb5e91841485f018e96556a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:09:37 +0000

    afford deserve emphasize depend send

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def square_number(x):
+        return x**2
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
