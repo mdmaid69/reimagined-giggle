13deb802a4c93d14181cd603f549d0f5ffb4855e
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:08:37 +0000

    expect wall completely abroad wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
