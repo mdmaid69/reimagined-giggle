572fbd54733d640775702f00611ffddc3a57f98e
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:37:11 +0000

    tongue string answer mysterious recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_int(array):
+        return int(array[0])
