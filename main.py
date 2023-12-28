091a81cd229a8ed9d6eb04c5015bbbad6f31a5eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:17:46 +0000

    scream careless badly carry decide

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_bytes_from_array(array):
+        return array.tobytes()
