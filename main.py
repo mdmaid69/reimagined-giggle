71f2439822c43b12c1435fdbde6175339e726d66
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:36:55 +0000

    finger pile peaceful pass job

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import os
+print(os.getcwd())
+import numpy as np
+print(np.array([1, 2, 3]))
