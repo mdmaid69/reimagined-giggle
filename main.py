5aaf0a98e24393c85d34b0172c64cfba5db508b0
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:59:45 +0000

    side Ee 568energy last mend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def print_python_version():
+        return sys.version
