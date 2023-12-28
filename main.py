beb4313e1506231daf3d5e07e61599a90f7f67d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:51:06 +0000

    pretty dry regional further view

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def remove_duplicates(lst):
+        return list(set(lst))
+import os
+print(os.getcwd())
