80592279a4fc0237fc852382e9dbca0caeb44960
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:22:02 +0000

    century thin equal luck present

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import random
-print(random.randint(0, 100))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
