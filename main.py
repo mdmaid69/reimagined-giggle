fcb86d3cc2af0ad6490c4f091b7be48a903bb910
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:02:03 +0000

    news real sing temper ill

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
