ff9c92dd9609fff07276ce604267930c9fc22b87
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:55:10 +0000

    smooth basis please some figure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def clear_array(array):
-        array *= 0
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
