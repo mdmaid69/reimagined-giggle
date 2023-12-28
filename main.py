fb1d4adf6a21018f6f7f5fd8e85aa2f4c8c2fb90
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:53:05 +0000

    polite near lower no account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
