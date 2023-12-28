ca96557091ec8b03d8354ff38b41389b393ea57a
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:10:06 +0000

    mess genuine distance around him

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
