6e53a9af2055ee56a7e52bdc641840007c5f2b06
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:39:12 +0000

    explain strange except breakthrough turnoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
