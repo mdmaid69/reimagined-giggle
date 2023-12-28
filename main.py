2ecd8045b5de7f3af6e071b43361a9bef4f7dec7
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:07:05 +0000

    replace closed right creature careless

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
