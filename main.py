5f94f803f1adfabba514701c66ba61ae5fd4169d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:33:16 +0000

    proud space scared juice must

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import collections
 def count_elements(iterable):
         return collections.Counter(iterable)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
