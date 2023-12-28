c907d05f6c816b5ee909d329aeb2cf4c5cc46116
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:19:47 +0000

    union human city design store

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import collections
+def create_priority_queue():
+        return collections.deque()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
