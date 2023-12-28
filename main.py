513f05aff010ef43f96355925dd7ed79a8b09562
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:58:11 +0000

    hang undo thanks look new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def get_array_as_str(array):
+        return str(array)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
