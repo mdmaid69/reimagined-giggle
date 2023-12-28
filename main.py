3a5918d819aec18dcb15decdc7bff4bcae809644
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:17:25 +0000

    convince half community achieve fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
