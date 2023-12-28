4d698891b14bc5a93ca85bff3bb41f304d5f2c18
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:18:08 +0000

    somebody possibility smooth sell wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
