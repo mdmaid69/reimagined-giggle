e0ed507db6856ea65ea00285a60206d88389f487
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:25:33 +0000

    bread boil pay play position

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def clear_array(array):
-        array *= 0
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
