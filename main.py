1a33f859c730edf26eb6d023cd4a3c4a26b05649
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:07:56 +0000

    you kind family challenge tradition

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
