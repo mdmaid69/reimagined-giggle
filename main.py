ceed8b54087aa8e144334541dd61e9474f26fc16
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:57:06 +0000

    tradition file knowledge remove obvious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_octal(n):
+        return oct(n)
