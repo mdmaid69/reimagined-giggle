3652dd095acb45189d1b54cfad906f2bf6ee1ee6
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:25:37 +0000

    complain mean hold age week

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+print(sum(range(10)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
