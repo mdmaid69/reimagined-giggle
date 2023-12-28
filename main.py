353bae3861da351b619074276e93217b562f8a84
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:08:03 +0000

    mail care small pretty bus

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
