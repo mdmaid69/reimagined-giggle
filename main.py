8a0b803b1167cca1ad028811eb73b23268b5d457
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:49:59 +0000

    accommodation bank telephone land breakin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
