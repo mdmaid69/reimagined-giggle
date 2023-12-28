8d1746a975fa70f53d1fa0c43f3259ddb580bf84
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:22:54 +0000

    set track she blame wander

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-print(sum(range(10)))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
