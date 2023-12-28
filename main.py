c93b4880fced56b699b6c43668d28806d5cce801
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:58:44 +0000

    scare look mistake design pile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
