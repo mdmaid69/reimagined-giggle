049b072a1ba13ac34e5e661250fd85e26643ae9f
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:38:46 +0000

    about cap score anxious burn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def multiply_numbers(x, y):
+        return x * y
