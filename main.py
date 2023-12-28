b9f8551011a743c6cfe5ae1bb7f74013a381780e
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:15:47 +0000

    no less store last central

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
