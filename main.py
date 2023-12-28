d77745fb03f22ff92917db4b3211dbca2d528d45
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:40:59 +0000

    love believe kind design prove

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from collections import Counter
-print(Counter("hello world"))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import sys
+print(sys.version)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
