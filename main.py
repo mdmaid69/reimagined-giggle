430c5dd9c78ec038e551d57ea7ab4de1a2b969aa
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:44:25 +0000

    interrupt she comein equal authority

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-print("Hello, world!")
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
