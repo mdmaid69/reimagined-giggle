434206d5ee6d412b1e01ccbc208f52c82bb56551
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:57:48 +0000

    yours front original workout less

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_str(array):
+        return str(array)
