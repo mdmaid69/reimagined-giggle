45235a25826de500b0bd7ad921d249e2bda5aa09
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:45:05 +0000

    flight pull smell deliver minor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_power(work, time):
+        return work / time
 import array
-def get_array_as_list(array):
-        return list(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
