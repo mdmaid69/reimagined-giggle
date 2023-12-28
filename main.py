fe512f2418f11a83ff64db8b9ad7cde8693c70fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:44:13 +0000

    up off holdout hang unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
