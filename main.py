61fa0880a859955d367bf321d50a603b9f2db2db
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:55:57 +0000

    power on great either however

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
