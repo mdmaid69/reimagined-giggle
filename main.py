41af60ec43d5cca0ec34a315c0305b9c7489235d
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:49:37 +0000

    thumb confusing left case depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_absolute_value(x):
+        return math.fabs(x)
