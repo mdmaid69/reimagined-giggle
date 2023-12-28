65b34c77d4eb8de6558ca4a08438b519ae2400aa
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:17:07 +0000

    early fat squash waste view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
