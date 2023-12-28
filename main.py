a485d5b2648d54fc0135970a85ba66b7ad523c30
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:38:34 +0000

    deliver minimum all sit depend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
