839defb7499cc37f8d57faf1f6817d99cb4db099
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:51:28 +0000

    centimetre tomorrow score year by

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
