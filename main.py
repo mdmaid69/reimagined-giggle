ae0098bbb2066e1a81251f1cf32561081e286703
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:15:46 +0000

    degree demonstrate appear whistle hello

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
