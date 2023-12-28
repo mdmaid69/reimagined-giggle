f6d52a9ffb9c973f4b30024a58f2634b2527b710
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:34:54 +0000

    entrance correct over transport prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
