ccff97149796b2657506b190b42ad926ea8ab486
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:13:17 +0000

    raw dream pity machine slide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+print(sum(range(10)))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
