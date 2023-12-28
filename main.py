d11997a79b5e0c9740dd304894b41c750ece1c93
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:22:56 +0000

    work design familiar combination provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def count_elements(lst):
+        return len(lst)
