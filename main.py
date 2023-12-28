6b246d85aec532eb55e830c5e6adbc2511d97f88
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:41:16 +0000

    spring themselves stare fair constantly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
