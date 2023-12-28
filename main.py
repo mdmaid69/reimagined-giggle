75bfdee20a0397828bad40455d357a3edfc418e2
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:09:44 +0000

    shock measure mainly block compete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
