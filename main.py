f093b07fe814bc7a1602fa176d5eadde27cfd54a
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:12:18 +0000

    likely miss return aware cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
