04dbc732b4dbe6df9a13d1091d541f6de91c1031
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:32:10 +0000

    relax present strange young if

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import json
+print(json.dumps({"name": "John", "age": 30}))
