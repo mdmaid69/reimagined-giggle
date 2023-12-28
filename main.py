533b342a38f365f8c6e31c271c37364a717dff0c
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:09:47 +0000

    platform bringdown then spring during

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print("Hello, world!")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_list(array):
+        return list(array)
