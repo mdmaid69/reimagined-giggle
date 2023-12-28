1a990abe9bd0e9ace8c214b0ad5545dc662e20c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:25:35 +0000

    write return everywhere ocean pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
