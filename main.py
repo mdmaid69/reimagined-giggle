b93c80e5d3faf3dcf7d9484d1f9a37ef8280b8be
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:56:51 +0000

    confuse listen private fast work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import time
   def wait_for_seconds(seconds):
         time.sleep(seconds)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
