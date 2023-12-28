14b7d70560df2417289257008adedd818045117a
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:13:28 +0000

    yes intend gift giveaway tonight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-print(random.randint(0, 100))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
