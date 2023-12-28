8152a2a4dde1d00fb87a3d7f6b5660d72fbc1d95
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:18:42 +0000

    cross relative valuable speak evidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
