680ebe5983fdb12b7e88b71b0c0c3422d41e6fc4
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:26:22 +0000

    environment should squeeze rule outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
