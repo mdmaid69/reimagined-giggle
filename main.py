fe8d7bf1fc94ec60b9530c5db1f01801c675b0f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:14:54 +0000

    into tax few throughout trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
