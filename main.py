f521cd8dab002ca06f4a9aff45637ad5e35f9bac
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:57:03 +0000

    separate mail speed myself wonder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
