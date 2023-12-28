483f365f591f16ef809addeec47d6ebf185a9ab5
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:26:37 +0000

    late have talk minute pot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
