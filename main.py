f64e5f4b555eb13099bc41eb3abbc4ae27a0345e
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:26:14 +0000

    fence how matter much what

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import platform
+def get_python_version():
+        return platform.python_version()
