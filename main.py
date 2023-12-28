dd6d7a0ffdbdcba092d396f137311512ad1b7050
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:55:53 +0000

    machine crisis soil dark hope

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
