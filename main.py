19f5832ca88ce86c8fd0e22ec2075e7a018efa32
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:41:23 +0000

    equipment sleep hollow help snow

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
