882edb3a2f4cf6920e142b78c154a6d5398faf17
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:28:58 +0000

    crazy pay read everywhere come

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def cube_number(x):
-        return x**3
-def find_max(lst):
-        return max(lst)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
