880ae668c2fa5483a0e6f5610d34f532df510809
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:58:00 +0000

    future environment hurry store wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
