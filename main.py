bcbc4ffb9a7a67888633b70ef99b10a822edb1eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:46:05 +0000

    us wrong cool grade generous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
