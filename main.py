a1820fe344cfd53cc1b32cb7211884a65c8a5696
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:54:02 +0000

    or waste estimate each division

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
