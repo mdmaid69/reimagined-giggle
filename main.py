c376f823df4ac11984e1f7a61e5bc46efd6da780
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:54:05 +0000

    certainly Ll plain actual object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
