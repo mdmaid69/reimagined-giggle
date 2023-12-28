dc2e72caad87e9a67ae4a8affdf2848ad38254f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:18:09 +0000

    proof personal appearance important usual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def remove_directory(path):
+        os.rmdir(path)
