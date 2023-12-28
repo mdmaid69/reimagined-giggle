09dd375b1e67cb0f68d5bf66f9036c22d0cbba1b
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:06:29 +0000

    callup file general left copy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def get_current_working_directory():
+        return os.getcwd()
