0d7721f1d83f7c8e3bb466786a980ad18c55b7a5
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:48:19 +0000

    keepoff everybody slightly degree empty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def remove_duplicates(lst):
+        return list(set(lst))
