4ee7ed4708172ea1c78eba36909c442eaa160db1
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:11:00 +0000

    secret concerning pronounce total along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def cube_number(x):
-        return x**3
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
