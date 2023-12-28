932927fac092cb41c19e101f84a906318c8a32ef
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:15:21 +0000

    example ring control takeover equipment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_average(lst):
+        return sum(lst) / len(lst)
