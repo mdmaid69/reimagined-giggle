5a8bb9d79634cfdbe8a9a19c732098270e3a48c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:01:55 +0000

    prove meaning tight black substance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
