1c193ab362ee287a8117270f1a16f8585dada883
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:27:12 +0000

    mix copy tonight dull shopping

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
