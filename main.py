0d01a8bf6cb7b504b1f7d29dd829dfed373ea73d
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:01:12 +0000

    opinion call evening agreement answer

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def convert_to_binary(n):
-        return bin(n)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
