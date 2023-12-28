1bc8ec5e3aaa6e2e3da464d393c2828f7c60edfd
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:19:45 +0000

    only pause physical tear prevent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
