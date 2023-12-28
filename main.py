0c4a3a6efd96ca55e24fb8b6c68dd1d45b4f419c
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:28:13 +0000

    equal vanish expression you lookafter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
