be4b69fb55a1d50e947d27fb3511090fcf72824c
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:20:32 +0000

    commit rub old very considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
