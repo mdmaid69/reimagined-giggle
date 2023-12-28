59643a5861234d38d0c5c2474d9727af0bf907e4
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:48:45 +0000

    in belief to capital other

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
