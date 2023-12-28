0cac96e487b0400d70a9b83b54fafe163b2b6f38
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:18:18 +0000

    unable nigga as object next

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
