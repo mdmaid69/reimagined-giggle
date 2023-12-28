c61b4b3f90ceecc2ba880d7801addf832afe58a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:50:19 +0000

    favour or strict rare frighten

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
