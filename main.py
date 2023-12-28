91d020d3e5d4e0c8598c702061e4b39686131a3d
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:48:05 +0000

    wash mention protect peace regularly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def reverse_string(s):
+        return s[::-1]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
