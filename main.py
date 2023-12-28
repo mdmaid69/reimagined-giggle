6a9f1a8df9ae6bb87a4112e7bbdd1efacddda36f
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:31:34 +0000

    formal swim grateful debt trade

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
