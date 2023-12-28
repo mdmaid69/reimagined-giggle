230238a5c81bb62732c18901b4db23dc4e3b6f8c
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:30:44 +0000

    strange field report freedom fashion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import sys
+def exit_program():
+        sys.exit()
+def find_max(numbers):
+        return max(numbers)
