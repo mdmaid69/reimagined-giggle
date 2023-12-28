014cd5f03ce39ae96f7b9cc9005579aa17664da0
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:51:42 +0000

    allow judge mark club wash

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
