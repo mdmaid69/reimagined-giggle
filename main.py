be8f64fe1931c90596b24bf3485a5d1efc2ed2a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:31:43 +0000

    Itself loan swallow attractive hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
