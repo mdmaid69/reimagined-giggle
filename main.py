43e33fe050219a763730b465aef5c6f118bc2a74
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:57:58 +0000

    treat beautiful centimetre badly comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
