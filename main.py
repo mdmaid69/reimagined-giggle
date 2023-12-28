96dcf9268129bc25ca7dc36d8dbd2f6e9357e09d
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:49:20 +0000

    wherever bar this shut stand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
