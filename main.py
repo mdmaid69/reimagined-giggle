286a741fc86b3cead885ebffc6edd8b0c166e0a5
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:01:24 +0000

    pass call book let arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
