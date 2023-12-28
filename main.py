89836d93c6b0999b3d35e9325a0f62e4af819880
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:24:49 +0000

    steam able sore section invitation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def find_min(lst):
-        return min(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
