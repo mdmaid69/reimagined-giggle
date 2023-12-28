09829402d059962f6c02583f9930ab4462ab2fa4
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:47:49 +0000

    permission criticize goon before this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def count_elements(lst):
+        return len(lst)
