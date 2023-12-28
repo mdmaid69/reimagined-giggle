c3118faab518f5b470818bc3027f5f1f726a30fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:08:02 +0000

    whether giveout pay steep anxious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
