87a9f89ae3fbd9918811b2cacf06a90d2f164961
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:34:44 +0000

    part interesting Jj could moon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
