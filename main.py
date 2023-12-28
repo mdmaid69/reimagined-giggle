4f93be99aad0592f0a659c6c65e376d60c1e0f99
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:41:12 +0000

    rate pool shine software joke

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-def find_max(lst):
-        return max(lst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
