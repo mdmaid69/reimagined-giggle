a6b73df006fa5a88459052c785cdb54cc7a07e47
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:05:01 +0000

    concern favourite suddenly swimming objection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import sys
+print(sys.version)
