3a980eac9159c2efb040fa3d90de921aa8d4f947
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:16:29 +0000

    mess promise aware show runaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
