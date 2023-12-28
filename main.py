4f550a7ae13dcf9701736b51169c737870a9e52e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:46:33 +0000

    deliberate tomorrow carry dress takeaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_odd(n):
+        return n % 2 != 0
