35124a19dc48f6e14fd6363bcc72210eef9428da
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:56:16 +0000

    his boil scream house into

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import sys
-def print_python_version():
-        print(sys.version)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+print([x**2 for x in range(10)])
