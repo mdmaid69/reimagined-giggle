78924d26397a0c657af372383be539eb27119d6d
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:22:24 +0000

    punishment rush please real mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import collections
-def create_user_list():
-        return collections.UserList()
+import random
+def roll_die():
+        return random.randint(1, 6)
+import array
+def get_array_as_set(array):
+        return set(array)
