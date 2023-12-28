0497a90024d660f8100163dc35dd0d4cb6e52fae
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:32:18 +0000

    ourselves tight thing rise relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
