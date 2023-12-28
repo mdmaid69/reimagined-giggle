281432df07953758c6c4cb4f5e9fe07d6cec7233
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:28:52 +0000

    takeover cost pay little circle

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
