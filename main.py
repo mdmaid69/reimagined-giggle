86a16a6bc5bc832b2f3118716ee85028349a863a
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:30:59 +0000

    slip spend medium mess gift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
