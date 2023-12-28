fdee541f9129a3cacec718990296841f849a53c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:25:25 +0000

    production connection authority fall circumstance

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_speed(distance, time):
-        return distance / time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def convert_to_binary(n):
+        return bin(n)
