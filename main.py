f85a6f89af878d8f7dc9d53309250abc058edfa9
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:35:59 +0000

    consist else crash every bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+text = "Hello, world!"
+print("Characters:", len(text))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
