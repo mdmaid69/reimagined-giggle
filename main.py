71db47de528e70db5acd77fc68aa6260c60ac50f
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:33:15 +0000

    unite keepaway ahead professional female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
