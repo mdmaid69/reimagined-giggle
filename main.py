2f548fbdabe7a6e9363b510e8d93e56b3f6ad9f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:22:47 +0000

    disagree whole carry rough tap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import sys
+  def get_python_version():
+        return sys.version
