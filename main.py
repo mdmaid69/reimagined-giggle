9e8cb654bd1980dfdfaba084570d0a00c02e9b8d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:53:36 +0000

    stare explanation shoulder without forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-name = "Python"
-print("Hello,", name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
