a55ff199f68221a7c3db385ca9e82fea3175fd9d
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:25:14 +0000

    dream regular tear ticket line

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+name = "Python"
+print("Hello,", name)
