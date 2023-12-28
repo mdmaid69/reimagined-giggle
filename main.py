188d4320607b1352d56c388a2f910d496a05997a
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:18:07 +0000

    stranger scream back exact tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_work(force, distance):
+        return force * distance
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
