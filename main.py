d5d8e9e8f3f647e51ce20022557e543a3507ba25
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:06:40 +0000

    whoever our exaggerate excuse flood

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
