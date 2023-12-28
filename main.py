96509ac9b2b860c7d57bb69b44544315ad55ba1c
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:46:31 +0000

    Dd pour national absolutely pattern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def cube_number(x):
-        return x**3
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
