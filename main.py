8c1c32ef8c02ba48e1f2ac34af54a78a7d0ac696
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:13:19 +0000

    nice include recover chin base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
