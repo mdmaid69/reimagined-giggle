ede331ede6f323c19665eb668caa376ba506d12a
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:04:25 +0000

    proof pay design thinkthrough shake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
