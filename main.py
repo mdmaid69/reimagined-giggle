59da0853c74e5009b128e3bd942ec7d3716b2fde
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:40:15 +0000

    deserve contrast put Rr behind

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
