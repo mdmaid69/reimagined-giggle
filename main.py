1140696de4cc2ee03eaf5b57545f872a976f2f35
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:51:45 +0000

    value imagination after scream remark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
