67e8d80a74602bdbda105bcbdd4803c1221a1233
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:34:42 +0000

    runinto eager nice pinch unlikely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
