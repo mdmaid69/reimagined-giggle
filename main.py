9e9a7f8e9a7618109aa4925ac887bc54418e0570
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:33:47 +0000

    across here press my solve

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
