d91c480f02bb0da7697bd12de989cdc1644f83d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:27:56 +0000

    appeal offer nerve disgusting whatever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import sys
+def print_python_version():
+        print(sys.version)
