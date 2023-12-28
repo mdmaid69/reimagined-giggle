b65e1ea45805edd4967f567286bb066da9f6253f
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:20:29 +0000

    post simple substance clean wear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
