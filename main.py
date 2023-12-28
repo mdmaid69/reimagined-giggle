d210192d0ea438d3805c1f84a0c4886c650a5e9d
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:30:42 +0000

    target class water maximum realize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
