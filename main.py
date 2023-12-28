087a2aff538aac4e325ef0eb61a63b092100a91b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:15:29 +0000

    decide base giveout so luck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
