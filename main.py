8fc22deb834161a7fdc411b7fc2cfe668be264e7
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:04:15 +0000

    twist major half subject real

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_power(work, time):
-        return work / time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
