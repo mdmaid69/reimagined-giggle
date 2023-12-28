48a033c7cf8007d0152163c3d8a67ff2ac40ebe2
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:14:29 +0000

    few expression give position unfair

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_cosine(x):
+        return math.cos(x)
