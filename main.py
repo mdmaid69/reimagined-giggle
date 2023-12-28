7cd847710b85c92ad267016d20b64e4ae9f464a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:58:42 +0000

    food fond hard side position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def cube_number(x):
+        return x**3
