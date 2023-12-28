c0f2abd80212adf3dee2a9476aee5d0f0c2ba4a5
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:23:31 +0000

    mother second waste car world

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+from collections import Counter
+print(Counter("hello world"))
