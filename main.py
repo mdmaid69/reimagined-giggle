1912c3f04de41c5283fab69edaa5ce0229219068
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:55:11 +0000

    cook together behaviour switch risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
