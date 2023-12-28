686b408e9170f4fad05a18c85c03ca1113709dd5
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:23:26 +0000

    damp street guard real cruelty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
