6c1a5326e7fb43b65a5fab22afa8a318a78237c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:15:18 +0000

    press on news though fill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
