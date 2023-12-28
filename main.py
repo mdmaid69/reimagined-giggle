878e817b5804b8f526c8177d1afb43b5c0b35962
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:30:10 +0000

    operation beyond freeze else prize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
