2de58349e47fbb33f9bb8f8a961b94f1c6300bbe
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:45:04 +0000

    gentle suit turn flat successful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
