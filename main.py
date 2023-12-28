527e6d0fb2c9caa4617074632bf7477fedab4478
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:14:58 +0000

    probably blame control personal custom

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
 def calculate_work(force, distance):
         return force * distance
