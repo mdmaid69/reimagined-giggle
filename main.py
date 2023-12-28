5d0f7908a23513dba6e462820feb81cb816e2e3b
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:18:30 +0000

    hot once wealth calm language

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
