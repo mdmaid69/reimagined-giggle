78367877130ae6539e48ab1681503bcd21bf0d73
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:34:46 +0000

    glass only subject buy coffee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
