da6e7faea852a92e07486ea636281dee6eb59340
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:11:27 +0000

    borrow rain raise special attempt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def reverse_string(s):
-        return s[::-1]
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
