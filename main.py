5e5493d2e59165fe6bc4688ef56dc4bb65f24d45
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:52:41 +0000

    future ourselves train building plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_floor(x):
+        return math.floor(x)
