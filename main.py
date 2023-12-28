d64f06cf8fdcb4b05bc9ab9c70df32a1769c612e
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:32:36 +0000

    turnoff well goaway condition somewhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
