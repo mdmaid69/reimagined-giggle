9306cafad00f9bcaeb0691efafa98f06d908ae33
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:42:55 +0000

    official mix bit trade forget

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import sys
+def print_python_version():
+        return sys.version
