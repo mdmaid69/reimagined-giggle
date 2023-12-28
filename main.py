31c677196d8291f339a328bbcfae0e3e2eee77fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:07:39 +0000

    rush force ill release unpleasant

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
