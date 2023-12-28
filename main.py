c7a8e35bd55b953ee056c38336644ba0b750e566
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:30:48 +0000

    smoke large Rr Zz to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import array
-def get_array_as_repr(array):
-        return repr(array)
+text = "Hello, world!"
+print("Characters:", len(text))
