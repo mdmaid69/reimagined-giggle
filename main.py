2bd46e2da7704fe28f0c37d3385811f3babaf536
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:45:12 +0000

    exam eat accidentally uncertain copy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def pop_from_array(array, i=-1):
+        return array.pop(i)
