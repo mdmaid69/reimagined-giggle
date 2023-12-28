460251fbe61773ed99691f943091c857713ed685
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:48:57 +0000

    run reserve miss back cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_power(work, time):
-        return work / time
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
