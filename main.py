d7686274ccb0dac600dcf9229d9375e9e4e2804f
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:23:43 +0000

    division share matter distant chance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def reverse_list(lst):
+        return lst[::-1]
