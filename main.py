d68f984861a2c592fe0077a2c113affe69c87522
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:28:26 +0000

    nonsense address violence handle dig

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_current_working_directory():
+        return os.getcwd()
