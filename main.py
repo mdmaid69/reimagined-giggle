2737d65e628cd1e15518046fae2f4e3517a069c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:28:30 +0000

    better correct tax holiday loose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
