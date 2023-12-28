c350b5a393798195a8739d193e8cbdc72b2f9595
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:59:28 +0000

    reply bread bend sensitive scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import sys
-def exit_program():
-        sys.exit()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
