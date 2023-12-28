140d48b8e2469cb1933175dc0053334efe9ea074
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:18:01 +0000

    instruction nice friend tool escape

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def reverse_list(lst):
+        return lst[::-1]
