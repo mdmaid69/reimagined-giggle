f20898fdda6e66051e81fe4afcdce4edee0fc009
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:06:33 +0000

    importance open taste with real

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
