32e73b566c4b45cdec4ec1f2033d4b8332b18388
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:47:47 +0000

    careless together minimum material case

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
