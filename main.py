606254269362eee46c966495b3717aeaa9f406b6
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:23:41 +0000

    sky on sleep collect Pp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
