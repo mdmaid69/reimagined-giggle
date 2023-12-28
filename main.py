ca61438d3142ca4a79e08b3611ce453e75ddc041
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:46:36 +0000

    stretch crime account Bb breathe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_circle(r):
+        return 3.14 * r**2
