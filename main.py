e38962559dd6159a23234dcd05f91dcea4fba2fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:32:02 +0000

    earth judge pleased height dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
