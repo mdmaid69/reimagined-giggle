fefb95433f02fe91d55b19383e55cb9aad2d4614
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:38:13 +0000

    order fat far wander bit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import datetime
-print(datetime.datetime.now())
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
