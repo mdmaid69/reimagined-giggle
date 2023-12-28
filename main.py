81b6f63a2b25361284c6d25ce5c6b3394138205e
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:04:43 +0000

    reduce her back swing breakfast

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import numpy as np
-print(np.array([1, 2, 3]))
+import time
+print(time.time())
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
