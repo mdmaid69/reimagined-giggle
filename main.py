44a1504a8b9186ae7f47bc64d37177bdcf9cdab0
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:54:10 +0000

    call aware something concentration impatient

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import sys
+def exit_program():
+        sys.exit()
