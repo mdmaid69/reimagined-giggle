e736f3984ca6f6da096a81d77b12995b6524fb21
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:11:05 +0000

    mean soft practical relax treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
