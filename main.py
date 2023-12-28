e09bea6b1824d194d1fb907fe07e5ab63bc2e18f
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:50:34 +0000

    bound suggestion tooth entrance spin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
