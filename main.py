70e0d0f7089048960590313c5aa5715e6035d698
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:42:25 +0000

    temperature simple dance touch pity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_density(mass, volume):
+        return mass / volume
