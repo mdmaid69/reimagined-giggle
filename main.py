4627deab18769577b30497cca0a9520452665b90
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:26:49 +0000

    frequent lookup her realize tough

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def divide_numbers(x, y):
+        return x / y
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
