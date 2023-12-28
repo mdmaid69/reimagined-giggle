e8d859e0bd59bb41db62ae99a8dab4ee938e4110
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:32:24 +0000

    market one order mine Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
