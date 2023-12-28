3d0078bf2b7e361cccb410e5f53ee84666913bb3
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:18:56 +0000

    shape swimming unable production image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
