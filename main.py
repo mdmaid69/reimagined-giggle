101225a40034d3ceced6d3db59c791dc0c115c13
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:09:14 +0000

    set takeback cry tour will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
