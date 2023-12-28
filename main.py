38fe8eb44d9ce065f02b52658eff246c3451bf66
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:26:02 +0000

    absolutely holdout traffic good telephone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
