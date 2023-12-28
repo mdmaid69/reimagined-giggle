003acf89788f994b172f2ea69fe349b3d801e3bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:22:37 +0000

    scream scream bound pay bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+print(sum(range(10)))
