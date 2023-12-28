46055091f061db0883b71b99d63f5bda3f6069d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:00:40 +0000

    your hardly contrast imagine ticket

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import numpy as np
+print(np.array([1, 2, 3]))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
