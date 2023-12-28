83f6f54f7428ff1991728e7fe662218a44cdb102
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:31:57 +0000

    stranger mean along personal post

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_float(array):
+        return float(array[0])
