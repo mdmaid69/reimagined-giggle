373fae41d451a4bea32db076e744db5dfb6ca9ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:16:13 +0000

    wealth rush zero priority shelter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def sort_list(lst):
+        return sorted(lst)
