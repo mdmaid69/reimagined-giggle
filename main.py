40a724ac64bea4626a4b2c2a97b55be5cb6a2323
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:18:26 +0000

    zero start clothes principle may

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_typecode(array):
+        return array.typecode
