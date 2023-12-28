2548d30ae14534cc3074e606c6a383c0b2c31608
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:27:09 +0000

    blame may game high breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
