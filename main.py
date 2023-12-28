67f327cc4dcf066977a0019d39e57d8a7741f6e7
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:08:35 +0000

    wherever next it next of

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
