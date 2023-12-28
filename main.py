ede66e8a12d35334cc566e9636e772cefcbffb2e
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:05:28 +0000

    nice gather zero murder rubbish

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
