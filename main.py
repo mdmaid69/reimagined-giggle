725a31209fd8ea39299cbd7bce9acd35d45df110
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:32:43 +0000

    Ll year near gift turnon

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
