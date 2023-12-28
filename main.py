d7a6acecb6e6c4f05a1be7758c1272310e62ff2c
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:10:50 +0000

    little trap now light accommodation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
