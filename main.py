427ee9b1d13ba67fd3de66e99976071867d36eed
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:45:20 +0000

    convince rich bunch plenty shout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+print([x**2 for x in range(10)])
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
