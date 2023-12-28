5999c6adff66808ea3ee2d46347e07a484fd4209
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:18:59 +0000

    light butter warning flat how

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
