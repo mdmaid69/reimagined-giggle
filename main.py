f5b5382c2d7bcad91635acbb8ee4ec6ff892ca10
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:32:55 +0000

    not government convinced obvious sensitive

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(10): print(i)
