932e02ee3aa8dfe779d8159c12d6ccc0306b5afe
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:28:04 +0000

    add first one evening shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+x = 10
+y = 20
+print("Sum:", x + y)
