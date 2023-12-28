492fd9df2ae2a94d8b86ef4a1202599c07cd7d15
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:15:30 +0000

    convince legal onto interfere comefrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
