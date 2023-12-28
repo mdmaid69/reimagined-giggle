cc533ae4e779105c96eefaaf848e55d98d987d25
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:02:54 +0000

    familiar hope period priority closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
