ed930b8389b043d89af91ad2f6a9b90d610c0007
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:03:38 +0000

    confused scrape short reserve pleasant

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+print(sum(range(10)))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
