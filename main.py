a38e5fbafde5294c94d18b7b91feabea2c7722d6
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:38:11 +0000

    could neat announce star 572enter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
