0b155e072c966a636826d9daccd2a502ef70b59f
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:57:32 +0000

    message danger air today save

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def reverse_list(lst):
+        return lst[::-1]
