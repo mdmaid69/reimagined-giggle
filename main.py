6946dca62a179dc8480be21474ce1682eb82f2c1
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:07:20 +0000

    still hole ill until question

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
