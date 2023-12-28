b38a88d12f1d936a90d717da3a8a1e1af835b815
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:53:41 +0000

    yes male keepup stupid vote

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
