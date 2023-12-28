3b213c101e26a5d7f8580f36359439b9254b0a6e
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:04:40 +0000

    deep during future Zz be

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-def calculate_volume(length, width, height):
-        return length * width * height
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
