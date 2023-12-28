107c60cc06081eae49fd0ec8dac62cca18ce3a9c
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:26:50 +0000

    charge spill possibility traffic after

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
