34acba64e75cb8ee5474ecc83354065dff40aca4
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:22:59 +0000

    argue close beside behind typical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
