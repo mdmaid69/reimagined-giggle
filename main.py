6e9becf90613ddab010a98cab1d6ef36a8313a78
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:56:24 +0000

    size season drop divide change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
