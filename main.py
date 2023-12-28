196f89f9aee35e0e68884669900b815ddf9366f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:11:27 +0000

    debt pleasant walk sort opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
