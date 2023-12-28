9ebea3c53fe737c49ba43f18562136dd30a57d01
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:28:55 +0000

    only trade think few worth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def multiply_numbers(x, y):
-        return x * y
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
