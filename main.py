6850cfbccf108fe630b2ad5dd9dac4565e121086
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:29:09 +0000

    shade relationship advanced pronunciation choice

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
