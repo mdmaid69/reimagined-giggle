e6954780361d099fb69b342130ce8f71b92e0544
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:41:16 +0000

    round organization bottom whistle empty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
