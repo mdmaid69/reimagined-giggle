766bb4cb944bf0eb246705452c1b6546bcd51b3a
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:38:14 +0000

    pleasant warm freedom intelligent likely

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def convert_to_octal(n):
+        return oct(n)
