2b0c9abb08e432d87b1f881a18379bea65839edf
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:46:54 +0000

    authority old wall right no

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
