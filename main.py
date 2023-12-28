1b786cd571c63778bed4092af50ec2b2489fd721
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:06:00 +0000

    anywayanyhow better kind heavy record

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
