e1dad248685bfbeda74d4602eeb11b03e6397545
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:06:25 +0000

    wish nurse ring scared more

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-print("Hello, world!")
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import os
+def remove_directory(path):
+        os.rmdir(path)
