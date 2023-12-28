4c49c31aa0b8a2c74505fcae991e0e5a13bb1d3a
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:28:29 +0000

    drawing bill condition hers grand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
