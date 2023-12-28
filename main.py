7ad9915c4952d68f4b29d881052cca75d02e80c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:05:34 +0000

    once male shake cost lend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
