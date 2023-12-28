9baba798df7769303a4a96de7ff8c08103c3d107
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:09:53 +0000

    worried country tonight care complaint

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-print(sys.version)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
