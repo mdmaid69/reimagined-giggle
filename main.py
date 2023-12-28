d244149a23acc5053a90b34a3e72f227e77a9a1a
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:00:10 +0000

    fair suffer stage suppose minority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
