f163eb6eeda40b0230c70d011ff50f3af460ea74
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:30:04 +0000

    our direction false earth heart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_as_str(array):
+        return str(array)
