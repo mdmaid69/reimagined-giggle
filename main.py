0c8372d8763b47fb04061d3bc90c11ded2ec48ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:56:28 +0000

    almost arrange sleep shine anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
