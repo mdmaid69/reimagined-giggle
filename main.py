9b6ab8e1537ef64294273c68e14d5fa314e85a14
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:40:11 +0000

    fail below congratulations post try

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
