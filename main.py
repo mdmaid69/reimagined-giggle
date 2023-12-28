14bf971d4f16b2a0abe00ffc7af2a68bcf86dbab
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:23:57 +0000

    fly Nn Ee point way

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
