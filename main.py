796189abf8e06fbb50a343604696d6a02db33632
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:00:34 +0000

    better start name feed tune

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
