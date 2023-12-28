189b91fb8d108481e151abb7e6fffd049e3dd5ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:48:45 +0000

    distance wrap official minor arrive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_typecode(array):
+        return array.typecode
