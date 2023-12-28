57d268f08382993b7b280effb7a6e02a210305ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:47:03 +0000

    life instrument forever want curious

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import platform
-def get_python_version():
-        return platform.python_version()
+  def count_elements(lst):
+        return len(lst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
