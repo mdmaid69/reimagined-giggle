61fd77290e2f996dbd03044a4fb0ae505228353d
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:39:39 +0000

    late vote university reply real

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def is_palindrome(s):
-        return s == s[::-1]
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_item(array, i):
+        return array[i]
