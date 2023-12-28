961b657a239a4a974c4e99392ead0ba2475b61ba
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:52:51 +0000

    goon comment post Kk transport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
