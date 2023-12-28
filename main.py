a6a3bf82e805db82635e1703864f0fa3c75edde7
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:42:00 +0000

    reasonable principle guess back accident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
