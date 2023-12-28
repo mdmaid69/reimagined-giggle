83ff744bb3eae3a8f61839616e52255137eb4fd0
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:22:24 +0000

    frame borrow sensitive above wherever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_current_working_directory():
+        return os.getcwd()
