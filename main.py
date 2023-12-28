604b6a889cbd7e0feacb9099d6a6ce6752317b75
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:27:15 +0000

    hold we middle swallow minor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_union(list1, list2):
+        return set(list1) | set(list2)
