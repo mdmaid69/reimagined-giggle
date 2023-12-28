59582701bb14d2b014f4ce7a795506a5e48cd0ba
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:43:41 +0000

    birthday trust without ought accept

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
