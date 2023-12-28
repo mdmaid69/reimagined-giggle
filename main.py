ad0a7c677918e1465bc5292ba1d9bb60ae2e5f6f
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:08:25 +0000

    move tired someone someone holiday

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def clear_array(array):
-        array *= 0
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
