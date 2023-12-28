cb3f5343373940f2915ecff16b9940de8d624e49
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:39:21 +0000

    cheat rough reasonable affect breathe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import platform
+def get_python_version():
+        return platform.python_version()
