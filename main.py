f93e1fb9c990fccb53b956b8ceaeaaf94919f110
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:39:40 +0000

    whole mistake equal satisfied edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
