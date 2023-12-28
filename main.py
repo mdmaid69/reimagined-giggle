ff6b8df77dae5c731431ac2a5e81fb1f2cb92fbd
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:51:39 +0000

    pin culture Jj survive Pp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+from collections import Counter
+print(Counter("hello world"))
