71960487f1b72ebd29741651410f779ae4ca0023
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:49:59 +0000

    step doubt stir thick regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
