c77eac592843991e72d461ad35861a27ac3ea50f
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:55:38 +0000

    draw advanced noisy type seem

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_pressure(force, area):
-        return force / area
+print([x**2 for x in range(10)])
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
