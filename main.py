7b510e66ba2e3a3d3cd6de91c335bea2efb9c21c
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:41:02 +0000

    drawing exist today release correction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
