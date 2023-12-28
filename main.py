9d9dff796ddfc66cf7ae42a5e034327fa5e7feea
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:35:30 +0000

    themselves food repair influence secret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def reverse_list(lst):
-        return lst[::-1]
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_index(array, item):
+        return array.index(item)
