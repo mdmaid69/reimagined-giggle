f4350b9054abb9d293d30b56e45a28b7c07557a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:50:31 +0000

    zero nice 565end unite comedown

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
   def get_current_directory():
         return os.getcwd()
+import math
+print(math.pi)
