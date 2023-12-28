120f0cbf639cd4774a9fb1e145e7a125d16626d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:49:08 +0000

    than extra demand steam closed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
