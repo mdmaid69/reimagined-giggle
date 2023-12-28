785ba1a58447a46ad722aac2e23cfa03d161911a
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:49:09 +0000

    side afraid settle turnup I

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
