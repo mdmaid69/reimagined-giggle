3d42f9f036334962cfa5e0a28b0bb83bed2456d6
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:50:18 +0000

    beat plan one very drink

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_item(array, i):
+        return array[i]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
