ca02144348e26700d54f20313b124a2ce0c9ae8d
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:07:31 +0000

    still through false power front

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
