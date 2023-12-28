7b1d103d4ce8a88913359b4205c7e98296ad13c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:40:45 +0000

    become answer weigh definite friendly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
