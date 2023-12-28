2e5cf086248dce2987b2b6975ce90297150d8a73
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:34:20 +0000

    drawing memory memory goon skin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
