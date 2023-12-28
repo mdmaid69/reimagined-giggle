14e3f2969448f6f97a5955f14e1959a1732eed2f
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:32:03 +0000

    visit colour late mixture imagination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
