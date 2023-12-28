4ab8cec4cb3976c5e90433f1f1016d7c1265d01f
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:48:47 +0000

    chase basic contact scare though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def get_list_from_array(array):
+        return array.tolist()
