1b5beeaedd98d34c4f55f3fc955df24bbab57ff9
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:56:29 +0000

    power speed dull hot intelligent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
