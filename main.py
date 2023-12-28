3b6cf2f098fa76d18d0adc20cb1fbcedd93c2d10
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:13:58 +0000

    attract seem heavy skirt acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
