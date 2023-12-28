be0f3568050eb1a016b4a9df9332ddc68ac31924
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:18:59 +0000

    interesting the type what hair

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def square_number(x):
-        return x**2
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
