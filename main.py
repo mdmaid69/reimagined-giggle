8f76b7d415f7c675b872ee9a84d67a19a4132f37
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:34:45 +0000

    course firm separate last whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_work(force, distance):
+        return force * distance
