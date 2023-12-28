cb4ae6ec611ae8e22e42c9243698a3ca014f78f2
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:53:53 +0000

    basic concentration intend complete crowd

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
