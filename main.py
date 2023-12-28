2f8a51c09dc365117be6d3501498534fb270baf2
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:29:21 +0000

    trap store themselves night clearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
