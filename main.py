4183598f5c27c94eaa6e3fdef84ef09969ebe5c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:42:02 +0000

    unfair threaten workon ball road

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
