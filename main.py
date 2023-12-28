bb0c21adf12c303901e9ad30d6786768e66eae03
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:30:32 +0000

    thank confuse rescue front save

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
