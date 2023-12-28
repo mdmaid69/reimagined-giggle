a449ec1a3764c9823a4cd6370bce54de1f50e0f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:12:36 +0000

    matter such cap next develop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import os
+def change_working_directory(path):
+        os.chdir(path)
