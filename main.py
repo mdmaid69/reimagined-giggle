9a89181877b00d9fb0980554407d5ee0b364a5ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:56:07 +0000

    splash complain near operate involve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
