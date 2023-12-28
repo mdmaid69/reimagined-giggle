b0eb10651f5211ccdfdcd5547ef93df4417bc827
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:32:16 +0000

    cheat front uncertain actual label

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def is_odd(n):
+        return n % 2 != 0
+import time
+print(time.time())
