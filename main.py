75f8a510fdb40efa17215bf3877b7a5c48728e10
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:37:58 +0000

    heat demand sleep dull react

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_power(work, time):
+        return work / time
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
