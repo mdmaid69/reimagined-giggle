c4e753185a3ed3a8544decd1faa309690d379125
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:52:10 +0000

    roof brain either mine godown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def is_odd(n):
+        return n % 2 != 0
