04d4a7986f2b91cc0a3aea38648d93e58aa04c14
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:37:19 +0000

    new against makeup tight rather

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
