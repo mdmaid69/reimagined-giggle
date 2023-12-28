bff175f93ff52e3058b41f36648c5a7c0de0af66
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:23:12 +0000

    prevent half insist steer choose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
