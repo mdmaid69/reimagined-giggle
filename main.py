4dbe9d5612ae364250673d89d07797da1ba1b880
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:23:43 +0000

    catch desperate taste promise burst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
