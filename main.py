32c69f8c769f184c83bc5d6768affcef41bea7fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:18:02 +0000

    judge recommend quiet uponon will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
