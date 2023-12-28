241f66d084576b0466966185d4704d6dac129df9
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:41:08 +0000

    lead offer target clean kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
