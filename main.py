bcfbd57e1052a0b6697b011799e6c30f7843b249
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:00:32 +0000

    welcome anywayanyhow reduce whom suit

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def convert_to_binary(n):
+        return bin(n)
