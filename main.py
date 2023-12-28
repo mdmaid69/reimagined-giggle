40ff953f43badb94a1c03d18997d3d2e4e77872d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:24:44 +0000

    contribute example deep pipe adjective

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def sort_list(lst):
-        return sorted(lst)
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
