83d53c8ae35f91bf73275bf52241a1a0790c5280
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:02:35 +0000

    paper divide and neck continue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
