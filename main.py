bf8953fa7347671949d1444d6966434bfbeabecb
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:14:30 +0000

    miss arrange recognize fail sometimes

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import sys
-print(sys.version)
+def find_min(lst):
+        return min(lst)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
