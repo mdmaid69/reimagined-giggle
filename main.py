7ea8a632827909d72bd529e040ba7dab782e8ae5
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:21:58 +0000

    natural tread worry book minor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
