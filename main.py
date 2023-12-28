5259f5034b0c53571e0e756e7be4aad81a282fe5
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:59:48 +0000

    any muscle gap mood send

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
