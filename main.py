b4425a030ec7ccc9a8fc3663e5f4d5300227c019
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:13:50 +0000

    young agree in straight yourself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def count_characters(sentence):
-        return len(sentence)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
