dd037c5a213d23dca692b9e4b008ae426bde4368
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:13:29 +0000

    month afterwards extraordinary suit worth

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
