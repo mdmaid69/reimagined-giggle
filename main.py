b1ecf021b133970a6db7966e7782dd0af45191c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:51:34 +0000

    argument his conclusion seriously shoulder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def sort_list(lst):
-        return sorted(lst)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
