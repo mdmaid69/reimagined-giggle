349d8139a1dce7c485ce4a4deed59ead96e6d970
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:06:35 +0000

    underneath which anyone board takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
