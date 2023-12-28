cd20f7f5803a170a8828109de738f9e3b3a223f1
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:46:14 +0000

    tape market makeup maybe commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import platform
+def get_os_info():
+        return platform.uname()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
