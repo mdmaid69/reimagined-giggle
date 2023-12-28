e0d2f6249764fe3dc630919f2b02a316727d1afa
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:35:49 +0000

    damage drink run depend murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
