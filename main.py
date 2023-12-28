774e1a3e886359684789e61adedf892cc3252963
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:26:42 +0000

    allow scare authority pure yours

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def remove_duplicates(lst):
+        return list(set(lst))
