a5b03f78a0b8bbc1a4a440e0e1af75d1f8963e1a
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:43:17 +0000

    own nearly than popular rare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  def convert_to_binary(n):
+        return bin(n)
