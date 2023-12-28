99a072c1b287a4c7975518d9265ff0afac673bc1
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:52:29 +0000

    well doctor slip attractive snow

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-def find_max(numbers):
-        return max(numbers)
+  def convert_to_binary(n):
+        return bin(n)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
