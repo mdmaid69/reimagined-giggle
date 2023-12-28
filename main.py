de5e570e34f7b4632004078515ba7f855efbeea5
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:06:52 +0000

    before pen legal bill make

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
