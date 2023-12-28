625b18cd3c904d5ec0f49ec63ad06e70f5c9569c
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:12:03 +0000

    black impatient complaint scare takeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
