dbdf82b80215ab7ae2a28c40ed2aaf03d303fbec
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:38:32 +0000

    near journey lookafter shame plenty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
