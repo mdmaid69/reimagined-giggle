07df3e0e18434de688bb81b5826eca5b1346342f
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:10:13 +0000

    skin scheme right marry instruction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
