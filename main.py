b44a624aedd5b7fc7f3c4a5922ede118b31fb4e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:05:05 +0000

    company pollution provide 570enjoyable inform

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import datetime
+print(datetime.datetime.now())
+import numpy as np
+print(np.array([1, 2, 3]))
