757db071bf9275c3776a56d87db9a785962874be
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:42:23 +0000

    normal proper stiff poison clue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
