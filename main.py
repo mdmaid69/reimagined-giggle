bb86bcdd50ce527225ffe5845c88660c2d5748be
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:29:46 +0000

    dry suit prepare recover seat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
