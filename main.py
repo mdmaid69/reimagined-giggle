a18ea84ae6fcfc9be1215e8dc1ca7c6d97485510
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:21:31 +0000

    home meet education report waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(10): print(i)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+print(os.getcwd())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
