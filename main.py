c1fd93eb38ee9e492158caf710278ac517b5d2cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:05:50 +0000

    steam below beyond this further

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def add_numbers(a, b):
-        return a + b
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
