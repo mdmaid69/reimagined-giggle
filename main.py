67520d51cc03eabab6dc660d8923d02ae4350cb9
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:28:40 +0000

    control style coffee drink crowded

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
