cc2aa270c147adc11ae92c29231d3a6fc0ae5de5
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:04:36 +0000

    tape since runinto emphasize getoff

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def add_numbers(x, y):
-        return x + y
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 def find_min(lst):
         return min(lst)
