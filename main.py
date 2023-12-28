e28fdea8cb52529dbc3d3f81c5318ad574e7e64b
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:09:16 +0000

    case talk furniture organize shout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
