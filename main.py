3c30763915660082d2cdd41f1044711f70479996
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:45:38 +0000

    part wander race crazy example

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
