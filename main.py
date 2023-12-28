13ecef94036b8c9c81d3812177a4fb03d0b5a7fd
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:40:39 +0000

    service left crowd return relationship

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
