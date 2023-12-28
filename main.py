1d9ce0f8d4409a39917e8da06ee6562053104cd5
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:14:34 +0000

    set waste official calm comfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def cube_number(x):
-        return x**3
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_repr(array):
+        return repr(array)
