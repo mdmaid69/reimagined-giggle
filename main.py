4c09f5830d154970d5e9965fd9ccd4d1f16a5b1a
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:15:51 +0000

    Tt I recently mind address

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+for i in range(5):
+        print(i)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
