be3658ca33832fad2ce502cd07b5b4a9fb4e67fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:40:18 +0000

    later frame someone by stand

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def convert_to_hex(n):
+        return hex(n)
