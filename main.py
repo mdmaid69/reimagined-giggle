042e548b3a866723b4ee81d5f3b4919fd69936f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:05:39 +0000

    wise increase throw spread correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
