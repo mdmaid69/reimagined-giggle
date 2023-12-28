2573efbb06e81a64338057baee4e728b91fde9d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:30:28 +0000

    dig 571enough again afford what

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def sort_list(lst):
+        return sorted(lst)
