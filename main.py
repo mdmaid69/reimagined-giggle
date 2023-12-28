aee56947517cfb726552d172ef11061c5ab20026
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:03:01 +0000

    city let protest theirs lose

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
