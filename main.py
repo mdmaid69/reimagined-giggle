436f42a9bcb92a50bd3f47c776a0b90e5b192544
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:07:14 +0000

    theirs recognize light the hello

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def calculate_area_circle(r):
+        return 3.14 * r**2
