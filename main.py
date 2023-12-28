0c5eb7b0d75c333e8ffb3fe492c52201c6c4150c
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:30:42 +0000

    die leave disturb full possession

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
