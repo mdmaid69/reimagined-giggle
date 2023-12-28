8e13ac1e650a20183bd092ece3ba32b6f5f679fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:27:42 +0000

    why feel obtain hand object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def convert_to_hex(n):
+        return hex(n)
