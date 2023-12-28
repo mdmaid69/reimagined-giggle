6c3d99b74d6f1bb644b4f4467407e5ca4d6e4fa8
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:42:03 +0000

    respect fine wander black way

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
