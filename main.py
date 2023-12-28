c45979fc4217117298e0c805fa01611ca8aa911a
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:51:49 +0000

    confusing influence shame cruel day

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def square_number(x):
+        return x**2
