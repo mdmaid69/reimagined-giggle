d346bda0861ff1bd11789f8c389538ed900ee421
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:47:45 +0000

    combine spoil about mine attempt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def find_max(lst):
+        return max(lst)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
