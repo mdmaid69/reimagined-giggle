e9a8b8b94d4b5c3655ecd110dfa71c44cd9cae84
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:01:37 +0000

    formal unable bet conclusion run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_index(array, item):
+        return array.index(item)
