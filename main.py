22dc7f00fdb1dbb9286b9e12297db56a8bab2785
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:52:07 +0000

    exist deliver normal embarrassed line

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
