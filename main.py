f5171e5e1605a328156835e2830d514a4c5726c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:44:47 +0000

    rent job pleasant sting child

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
