5d040d147ffc4cc230c18bc5b1209e0f65b09d7f
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:24:05 +0000

    occasion difficulty project nature grade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
