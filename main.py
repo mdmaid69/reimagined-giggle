242c434c4ae606ffd47cffb333d3d5f6b44f1378
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:43:01 +0000

    duty afternoon modern ready already

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
