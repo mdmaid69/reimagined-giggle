74971cbfdcd40c91b0317d9f9325c50d7d53e874
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:55:15 +0000

    proper realize soil hour care

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_item(array, i):
+        return array[i]
