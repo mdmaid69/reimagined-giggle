a5b5acfb1bb7e486c809db126f0f75f4ba3dff4a
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:43:16 +0000

    risk cost bother ring across

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
