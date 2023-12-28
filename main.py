8dbd5b9ed522404c21a251fb6991b9c971b46048
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:49:02 +0000

    politics yesterday back cut unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
