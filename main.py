29fbf31dda8823d6607eb797f9d53b285f1aca97
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:37:39 +0000

    tend weak roll undo by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+print(math.pi)
