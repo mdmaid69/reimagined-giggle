c50dc9b1a1ccc21fd0ebe46ebf92fdcdcc78fc6d
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:59:08 +0000

    Ww tradition carry activity aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
