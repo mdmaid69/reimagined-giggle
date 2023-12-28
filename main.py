4dc1746a44d0593db315f66929b897471319a04a
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:20:50 +0000

    neither considering regular support coat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def find_max(numbers):
+        return max(numbers)
