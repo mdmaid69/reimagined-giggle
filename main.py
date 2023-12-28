42fa7b338c28a22776912161d832fab4ab6cb84d
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:59:32 +0000

    complete situation deliberately determined offence

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_pressure(force, area):
+        return force / area
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
