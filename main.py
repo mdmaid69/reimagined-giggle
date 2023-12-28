311687d4688cc6f5dd94f812e1f00fc6517e6615
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:23:35 +0000

    approve mess inside comfortable refer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
