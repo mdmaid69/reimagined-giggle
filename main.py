58732e532442fb4bcdafd61a112c52e0fcba6d40
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:10:14 +0000

    geton invite practical feel dead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
