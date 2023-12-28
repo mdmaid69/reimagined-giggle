1ffb28ec1c160f1b15dd3f9396916a71971307ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:56:05 +0000

    talkoutof on unpleasant plate fun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
