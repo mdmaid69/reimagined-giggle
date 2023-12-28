02469bd2f999fd046474670d71bac42aee903913
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:45:32 +0000

    geton pretend ever leg limit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
