1ec535e025e781edf997b57cc20e207def4c9e4d
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:48:55 +0000

    brain careless public build this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_uid(file_name):
+  def get_file_owner(file_name):
         return os.stat(file_name).st_uid
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
