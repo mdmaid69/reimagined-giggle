65412b5140674eb81a1da080030dd5cce0d454eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:36:44 +0000

    run pull depend what definite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
