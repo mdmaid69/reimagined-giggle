9e941f72bcc6fab0f8e2ee6f448ffb5a60c914d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:37:45 +0000

    shallow fear keepoff advice get

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def square_number(x):
+        return x**2
