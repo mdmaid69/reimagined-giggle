567b2e2a993cd28d960467c53d277d2be086aff7
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:28:03 +0000

    congratulations accidentally something war flow

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
