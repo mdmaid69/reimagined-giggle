e25141dec17e4ae260fe48d539bde5ef7d2cebf4
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:54:54 +0000

    Rr forgive goaway turnback concerning

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
