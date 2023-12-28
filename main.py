20ca9a1adc7952cef0340d35ed1106294c79c0e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:19:01 +0000

    dig trap drink hello situation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def is_even(n):
+        return n % 2 == 0
