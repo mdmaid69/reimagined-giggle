c8e0476a49cace8acc1bd01765d0a451c5ff7b47
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:29:01 +0000

    child progress sometimes claim quick

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-x = 10
-y = 20
-print("Sum:", x + y)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
