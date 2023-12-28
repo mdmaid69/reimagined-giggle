270287ccb0eaced9e41a1a2ac4ccfd729eabf9a6
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:30:38 +0000

    sky innocent poor spring silent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
