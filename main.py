955b7767c6cca2bbc187f7d73a84de1823f15970
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:54:44 +0000

    peaceful myself slice please violent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
