3d4a6bd8b542b7f30919d6d28a7452b2e4476406
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:16:36 +0000

    coat press understand genuine opinion

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
