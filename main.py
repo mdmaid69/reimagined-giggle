e9c037416095e315aeacf2f6028ea47355838f0b
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:56:33 +0000

    thanks but squeeze begin boring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
