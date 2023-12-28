ef90067a24b6c3eceb61ecbec776177ef7a644ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:06:08 +0000

    late estimate still each address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
