bfe05b777c81ce970c633856ba0d0e033e289045
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:34:59 +0000

    left exploration giveback somewhere pretty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-import sys
-def print_python_version():
-        return sys.version
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_acceleration(speed, time):
+        return speed / time
