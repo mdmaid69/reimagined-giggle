69117424d2d6c02107d51980588b661ae5b2174c
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:20:39 +0000

    goback(to) eager mess skirt rent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
