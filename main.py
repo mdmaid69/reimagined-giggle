c64a54f7370172189b59c1d91a70ed39aff480e8
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:04:39 +0000

    goodbye seriously speak difficult brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import json
+print(json.dumps({"name": "John", "age": 30}))
