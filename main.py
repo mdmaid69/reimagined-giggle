14ac64f0e45bc6f92bea2d74fa69fa3448b8b4a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:08:18 +0000

    birth write instant ride squash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_repr(array):
+        return repr(array)
