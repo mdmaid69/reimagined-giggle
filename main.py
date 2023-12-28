5c6a49ab309c9e3f478fee9d9834c8192a2f5995
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:57:18 +0000

    shop pride far dance violent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
