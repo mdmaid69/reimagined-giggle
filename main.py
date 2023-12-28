6423b41ad77706d9c37c267965fdfff4e1ecfd28
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:40:27 +0000

    Qq party fly feed right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
