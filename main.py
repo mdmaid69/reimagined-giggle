f5ec6e38a3eb9cb967ff3e23f64fa7ab8055198c
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:16:56 +0000

    even influence sea summer perfect

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def convert_to_binary(n):
+        return bin(n)
+from collections import Counter
+print(Counter("hello world"))
