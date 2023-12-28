7ab789bc4feb5134a39580eefbc3d67349d5fec5
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:34:52 +0000

    string let inquire closed turndown

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
