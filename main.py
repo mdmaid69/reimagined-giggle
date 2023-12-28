c94cf6ed5d07f435a1d986457bec031f59e75061
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:23:52 +0000

    lead design attach fairly quality

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
