9d184f7a444cf5e56000b730487c2035b5b1822e
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:55:37 +0000

    capital promise picture come honest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
