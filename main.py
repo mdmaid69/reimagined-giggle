ce4d5bdfa7f809be71aa2df2f13bb83fc87868cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:40:57 +0000

    which make note skin this

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
