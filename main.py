c3d789af2da3583765aeb621b2734e977039797d
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:17:58 +0000

    cut disagree build drink sale

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
