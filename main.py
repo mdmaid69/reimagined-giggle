3d5d9e17f7ed3a816760d0a618852f8f31198fcf
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:37:41 +0000

    runaway job so equal benefit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
