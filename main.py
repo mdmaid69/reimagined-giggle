afc877bd99181a3dce2a79eda371958224e8f91c
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:07:18 +0000

    ignore influence ever raw watch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
