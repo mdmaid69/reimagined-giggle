baa8cfc5d0af22083a94676b8d98da421aa954e5
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:52:24 +0000

    ignore survive invitation later bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
