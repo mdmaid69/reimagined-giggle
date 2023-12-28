fd9e4af206732893b3f5f4cf0c4da9b0bb41300f
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:58:26 +0000

    door fight consider hard relax

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
