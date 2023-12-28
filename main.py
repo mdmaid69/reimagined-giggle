c5383c8a89bf8d58452c3045ac4262e406bdc19b
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:01:52 +0000

    cruel study measure precise sore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
