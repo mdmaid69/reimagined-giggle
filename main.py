59fb0588b419e54890c1af7079686fc471ce8695
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:53:43 +0000

    male anxious smile successful steer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_current_date():
+        return datetime.date.today()
