c70d1b6670f4e1e3d403351c5ce4516ad6e75407
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:21:12 +0000

    public ago lend lock contain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
