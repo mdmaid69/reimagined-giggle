afebb81d4b24d7ad56e6add62a0acf54513631d6
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:54:16 +0000

    string order contents tight apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
