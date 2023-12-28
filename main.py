888ad4c4805254f4f81d5fb8902bacab3b906db0
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:11:02 +0000

    Bb Oo interview square getoff

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import platform
+def get_python_version():
+        return platform.python_version()
