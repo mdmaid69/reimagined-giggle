10e5c4b1dc55e594b080391d6b20f5b31c1048a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:45:40 +0000

    convinced react convinced hair best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
