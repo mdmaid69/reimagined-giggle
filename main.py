12d7e1dda8b8fe815131be648a4a2be08d61d874
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:10:03 +0000

    undo breakin shout blue delicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def square_number(x):
+        return x**2
