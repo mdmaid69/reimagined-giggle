c7ebb025ebbb944bef84805df873646294c2fd66
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:21:35 +0000

    avoid dark pass club tense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def reverse_string(s):
+        return s[::-1]
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_array_to_list(array):
+        return array.tolist()
