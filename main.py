05a4968aa82b44a640dc3c903eb6a99e50cae146
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:04:06 +0000

    train exchange who half floor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
