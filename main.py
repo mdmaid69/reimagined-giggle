587468d09bfa0b7890d5b7f4b6f9f22411919b08
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:50:25 +0000

    promise violent deep last strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
