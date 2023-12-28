4d229b09110cfc4e37e8fff7abdcbdc49fbb7be4
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:57:42 +0000

    Rr Bb plastic care less

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
