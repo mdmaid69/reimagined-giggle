619af020ec1db31ba797b8bc5ec40a2809f71307
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:35:45 +0000

    everyone add pattern ask absolutely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
