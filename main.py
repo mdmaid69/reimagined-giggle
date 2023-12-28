973c4c120971c8d69c966ef5df9a3b16a8db140c
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:01:58 +0000

    challenge dish day bank pass

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def square_number(x):
+        return x**2
