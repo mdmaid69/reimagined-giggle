dc9a47f69d269fa1bc8b1cf169556579db7dfc09
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:12:33 +0000

    hang school class cope keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
