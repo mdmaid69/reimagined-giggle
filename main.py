5537838c5e199a17209ae26e07200eb2cd005701
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:56:04 +0000

    motion influence absolutely tip furniture

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
