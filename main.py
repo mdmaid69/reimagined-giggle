8b8f45b5e41ff7ffde7c0b189121086dd5de21c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:14:50 +0000

    want lost tend thinkof even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
