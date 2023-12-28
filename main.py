49adf00161128dbf1e73dd3e1518771b0dd55781
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:16:43 +0000

    flow wave his moon pump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def sort_numbers(numbers):
+        return sorted(numbers)
