6344908001685ffec165576dcd71489193538fbf
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:48:45 +0000

    comeout interrupt passage degree complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
+  def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
