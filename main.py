117a372012c821886c98fb724381519f92126b01
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:02:10 +0000

    tune notice negative money game

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def find_max(lst):
-        return max(lst)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
