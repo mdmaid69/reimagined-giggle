11257590cf77bbf59cfcf86fc5b988b322f096b8
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:03:00 +0000

    appearance concerning behind certainly there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
