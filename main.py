bd9cdd33f089a319cdae0ab7a920dfec0cdfaf7f
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:39:47 +0000

    most cruel runaway put relation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import sys
+def exit_program():
+        sys.exit()
