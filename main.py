73a2502f4c0735798fba0e72ec684c82ab130592
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:05:14 +0000

    their OKorokay point out stretch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
