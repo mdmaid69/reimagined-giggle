8128bbc88ff1ae9985f86fa0a87212a4efd755c3
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:35:47 +0000

    yours dinner pipe pick light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import time
+def get_current_time():
+        return time.ctime()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
