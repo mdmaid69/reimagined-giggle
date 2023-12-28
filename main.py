f08ceb6bf796a0f8715645c13edb63819482a7f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:50:14 +0000

    slide sick pipe minimum explain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def count_elements(lst):
+        return len(lst)
