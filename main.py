22673d41201bf5925316808b17fff900ff129582
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:44:54 +0000

    speech luck rush open tonight

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
