8dd7c37ccca071dad4fd0e402ff3ca63cdc3a118
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:10:06 +0000

    niggaacademy cry dress trick sense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
