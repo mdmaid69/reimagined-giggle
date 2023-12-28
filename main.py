8cd632843d72d66c812f9c8ab797f1f84bc40c67
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:17:46 +0000

    track be new together lonely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
