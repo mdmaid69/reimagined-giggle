d15a38bce1d353f215b89a1a1aceafa355662217
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:14:48 +0000

    from Ff compete manage opponent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import collections
+def create_user_dict():
+        return collections.UserDict()
