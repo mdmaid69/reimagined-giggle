51f9b75bd1ca1ace07d70c72e39aa2c6a00c3713
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:22:24 +0000

    culture breakup again reply shall

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
