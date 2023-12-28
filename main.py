3f346ef302845835d313f90b2688888ecb36c6e1
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:14:29 +0000

    murder sleep mind piece see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import sys
+def exit_program():
+        sys.exit()
