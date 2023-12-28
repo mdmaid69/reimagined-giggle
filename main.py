fba0cf603bb04131be0987edf511c2b041c81ae9
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:30:36 +0000

    dance cough sad divide compete

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
