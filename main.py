d544be51d3bfa1454850ac47fdbbdd640c315a2d
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:47:04 +0000

    content kill shall beside race

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
