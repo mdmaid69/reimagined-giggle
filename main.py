d1a406edf96f97a249f4508d334f313942f208be
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:33:57 +0000

    song offence plant takeoff suggest

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
 import queue
 
 q = queue.Queue()
@@ -9,3 +7,8 @@ for i in range(5):
 
 while not q.empty():
         print(q.get())
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
