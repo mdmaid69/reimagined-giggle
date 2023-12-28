d70e2faec565477bb80e41f42415cb3eabb470f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:41:49 +0000

    twist mistake quit imagine improvement

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
