c76aa18fe77bf05c71467aa43fc1ca75a1a977d0
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:16:03 +0000

    excuse tonight drive wet interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_max(numbers):
-        return max(numbers)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
