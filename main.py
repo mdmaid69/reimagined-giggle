95e792f89077c635bb75370c4281424d99651686
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:35:07 +0000

    reasonable hope generally wish basis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
 import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+text = "Hello, world!"
+print("Characters:", len(text))
