675f8453c3d22b3551c0478fc4945069e7cfc263
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:08:45 +0000

    use grateful wheel easy ought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
