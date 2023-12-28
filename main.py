54d0862fbec8d18a80613b3707aa9695154e6777
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:49:40 +0000

    rely memory entry Bb treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
