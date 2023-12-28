38c75327c64a284f5962393f961c98ed7db25b27
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:21:27 +0000

    admit would bet lead geton

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_volume(length, width, height):
+        return length * width * height
