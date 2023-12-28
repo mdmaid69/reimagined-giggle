23ed44d6df461de288a5a35faaa22f2f1c138f99
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:20:36 +0000

    pile later challenge example complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
