9608bee58e44a4d8a2e713a5fad3886169f97835
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:38:01 +0000

    centimetre secret for suck clear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
