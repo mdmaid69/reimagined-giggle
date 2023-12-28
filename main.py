ee2c93eb03753779fca1a87b466c76aa08d2da09
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:32:17 +0000

    stare lesson spare reserve Rr

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import time
+print(time.time())
