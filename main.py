529db7dfa30939f6ed6cbce14bc55d70e6603ca1
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:24:30 +0000

    cut control task extra computer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
