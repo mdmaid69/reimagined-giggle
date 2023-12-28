522de74fcef2eb93901318ac3a41690f6315dde8
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:38:34 +0000

    sleep fortunate anyone form flavour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def remove_directory(path):
+        os.rmdir(path)
