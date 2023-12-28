b8bb84e208eb2d88513e5292e10922740dfc4662
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:41:11 +0000

    bread such care direction catch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
