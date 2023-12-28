a09e11c7d92f99ce64d4715fdcb5574fcab88f0c
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:29:37 +0000

    present rubbish snow earth drink

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import collections
-def create_queue():
-        return collections.deque()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+for i in range(5):
+        print(i)
+  def convert_to_octal(n):
+        return oct(n)
