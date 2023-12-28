5be383bf86f3441d48f8e6dce5d0a9ca0ec13b87
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:23:18 +0000

    open else bother arrange under

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
