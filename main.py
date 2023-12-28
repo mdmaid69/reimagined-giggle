efa5bdef3b776ba374ec64a0f07ed141ca717600
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:09:30 +0000

    teach discussion swimming contribute goup

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+for i in range(10): print(i)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
