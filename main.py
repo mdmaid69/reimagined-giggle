8d8b6bac284ea080a74fea4041d82d682637789b
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:05:27 +0000

    size translate fun argument ordinary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_unique_words(sentence):
+        return set(sentence.split())
