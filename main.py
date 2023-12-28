652639f29ce3d1cd2b18d8bed4266852250f6349
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:02:26 +0000

    if type match smell slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-print(math.pi)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
