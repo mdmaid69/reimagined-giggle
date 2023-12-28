476de99a808ced8a4a172f169970a42d341f3e32
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:06:25 +0000

    nervous brush thumb skill division

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_max(numbers):
-        return max(numbers)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
