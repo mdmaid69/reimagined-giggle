bedfed54b4a6ffad0b585320197de38c96275f0b
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:21:44 +0000

    only write talk comedown into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_floor(x):
+        return math.floor(x)
