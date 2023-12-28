769893a00238ec9049f78209ee29c52acda0aab7
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:31:34 +0000

    workout estimate certainly satisfied cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
