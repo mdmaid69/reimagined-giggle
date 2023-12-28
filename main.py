7f9bb1c2f4a23e84d44ec9e9aee96c15d5fab217
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:57:27 +0000

    disturb wire begin defend whom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
