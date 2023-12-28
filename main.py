fd599c5e41584a2af7cce4ea7b8c94657c969533
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:17:41 +0000

    breakinto workon object notice body

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
