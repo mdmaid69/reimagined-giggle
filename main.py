c141c8fa3f717581107dea16a5e11bf7ef3812ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:09:27 +0000

    message relation secret help temper

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
