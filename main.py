c5e0bbb5cb8ee3b93406973760307c0e4a624dea
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:36:33 +0000

    poor onto meaning earn raise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
