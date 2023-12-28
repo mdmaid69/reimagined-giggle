5e1533431e0d3a5ec4affd601c89abf151c0950b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:45:14 +0000

    react accordingto custom equal cheque

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_work(force, distance):
+        return force * distance
+def calculate_power(work, time):
+        return work / time
