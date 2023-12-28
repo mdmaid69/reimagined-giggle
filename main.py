8fa6ff2940c97694bf83043aad639d33fb1e6170
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:51:55 +0000

    smooth operation opposite will shadow

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import numpy as np
-print(np.array([1, 2, 3]))
+import collections
+def create_queue():
+        return collections.deque()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
