feb5e8de85c1df6bd8bed6d00932fa23aca908f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:10:24 +0000

    fork match whenever ago consult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_error_function(x):
+        return math.erf(x)
