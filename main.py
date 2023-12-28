79a109c956f98fd95a42ba990c2a07f3ba1e65c3
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:13:44 +0000

    birthday form help stick regional

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-def find_max(lst):
-        return max(lst)
-from flask import Flask
-app = Flask(__name__)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import threading
 
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
