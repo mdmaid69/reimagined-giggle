4631bf9da90b4210c654875fb71952fb92ead449
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:58:17 +0000

    by machine glad slice rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_as_int(array):
+        return int(array[0])
