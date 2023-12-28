48d5ddfce125825b107bbed17709af44cb1799b5
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:59:36 +0000

    breakinto shout plan believe available

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def reverse_array(array):
+        array.reverse()
