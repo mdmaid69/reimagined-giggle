e055ba504ede71d449961d5438db6b49f2127afb
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:24:57 +0000

    successful length immediately broken already

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_odd(n):
+        return n % 2 != 0
