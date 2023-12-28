ef0934353a7d2450e1c1ebada8f1f642bbe19f45
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:30:27 +0000

    afterwards start gather turnup muscle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_pressure(force, area):
-        return force / area
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
