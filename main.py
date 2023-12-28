0f62f213398b70998c70ac58b7691db48878529e
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:03:13 +0000

    check invitation workon taste guard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
