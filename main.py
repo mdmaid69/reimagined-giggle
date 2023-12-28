7c098d70468af26baf0e5f81ea175ec5498c373f
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:23:40 +0000

    mess who contents only valuable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
