52bbd4fddca034833823271c6ef910d21e3c9918
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:21:09 +0000

    food material contain afraid law

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+print("Average:", sum(numbers) / len(numbers))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
