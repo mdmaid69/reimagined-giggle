68288cb1cbd07a85607a7930bc3683286541af93
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:45:57 +0000

    bringback punish practise work apply

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
