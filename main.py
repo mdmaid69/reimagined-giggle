2ed5999d721f0b1487c040d6b2306e3e9a138470
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:05:42 +0000

    rush offer debt true declare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
