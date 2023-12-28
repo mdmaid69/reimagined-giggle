a49594ddaf7a7fb73f097c1a377748d20b21c4f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:18:57 +0000

    567enemy power fortunately or positive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+print("Hello, world!")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
