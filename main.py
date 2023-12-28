61ceac05d534339540138f595e9090e405ba3688
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:37:59 +0000

    report computer many godown wish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_union(list1, list2):
+        return set(list1) | set(list2)
