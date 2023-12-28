3d9292df180e23a9ac007834894807bf0db61695
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:07:40 +0000

    act pain work rise connect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
