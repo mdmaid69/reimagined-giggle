fe5f6ac83913125e80738b6a043979ca2a770204
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:40:48 +0000

    relative our danger private vision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
