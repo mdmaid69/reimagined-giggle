547ce82f9e4ccf42da5d0378c20dc4f07723bb33
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:05:22 +0000

    work strange address account provide

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
