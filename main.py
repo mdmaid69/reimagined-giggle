f250898c44ebd6a06a33d76843e1d63397dd5c0b
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:36:44 +0000

    breakdown getaway satisfactory seat part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
