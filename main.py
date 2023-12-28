7bd8f369a23285394025e33e7fb852ed1dcc00ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:45:20 +0000

    allow shape argument calmly loan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-def is_odd(n):
-        return n % 2 != 0
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
