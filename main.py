e29d3032f0d01466acaa61dfd00af8dc5a83710e
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:07:35 +0000

    start warning aim fun couple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  def is_even(n):
-        return n % 2 == 0
+import datetime
+print(datetime.datetime.now())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
