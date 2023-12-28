fb81d202884edfe9a8c4de80e4e313de208610b8
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:27:08 +0000

    previous pull range print train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
