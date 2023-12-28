a35f8ee8eb8a7b9b93578e0d95e02a72894963dd
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:49:07 +0000

    cool order introduce now bridge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_density(mass, volume):
+        return mass / volume
