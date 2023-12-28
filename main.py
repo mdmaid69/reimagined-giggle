9bb016d97b1c0656864df8450fd0ec2bb345249b
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:40:29 +0000

    guess boat about afraid prove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_frozenset(array):
+        return frozenset(array)
