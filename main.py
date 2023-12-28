e0d58ea947a230a7fcb3366f5a01b7488937fbea
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:13:18 +0000

    positive club damage wide voice

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(lst):
-        return max(lst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
