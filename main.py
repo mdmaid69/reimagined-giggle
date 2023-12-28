4be48c266522627fe9329f7355bca2a328de2fe9
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:39:54 +0000

    Ee declare 571enough against soft

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
