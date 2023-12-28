4ba95a021124554a30641b470edf87779f9f6117
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:18:32 +0000

    sense Qq look sorry workon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_area_circle(r):
+        return 3.14 * r**2
