766535e7525b14a4a85f14ccf61b6e75b8c9a11d
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:38:15 +0000

    busy attractive lock conversation gradually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_density(mass, volume):
+        return mass / volume
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
