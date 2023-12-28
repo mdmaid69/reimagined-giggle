ea216e46363d2bf3288056b3e6a67f5b95ceb0d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:18:19 +0000

    criticism mass thinkof treat butter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
