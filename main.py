8ebbb2e0ca3fbfaf41569672e5d10436a046a3f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:22:37 +0000

    healthy persuade strong arrangement confuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def remove_duplicates(lst):
+        return list(set(lst))
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_int(array):
+        return int(array[0])
