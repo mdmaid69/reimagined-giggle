408118cc187b43ed2a9c80e0f737b6368f458217
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:02:33 +0000

    teach path means possibly border

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  def convert_to_binary(n):
+        return bin(n)
