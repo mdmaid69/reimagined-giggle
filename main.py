0be317a6fc4dac57bd83d9cc3b767f61eef50618
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:40:12 +0000

    sore keepfrom tired hear noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
