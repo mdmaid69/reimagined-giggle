96f43a3a4b4d56855440a2143e4e6747a8d0eebe
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:44:32 +0000

    downstairs admire possible difference go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_counter():
+        return collections.Counter()
