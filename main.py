2a4fee1b9565b0c71543c8c8d2abd897b54edfd9
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:31:23 +0000

    wire smile after ask spare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
