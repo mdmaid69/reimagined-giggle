7a88343f1d200a02a2df2db8918c4ead0d64c64b
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:59:49 +0000

    fortunate practise recently science opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def get_array_as_str(array):
-        return str(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
