79f422d824dc40f339747179ac1c03604a7aab4e
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:10:40 +0000

    blind gas person detail lose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
