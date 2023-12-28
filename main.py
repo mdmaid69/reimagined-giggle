c1d998bb3aabb2c2c1f5e3fb53e4a21cc6f88b99
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:34:20 +0000

    tomorrow uncertain boring content bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
