6860326a69a8a756d0ee5ba1042bfc5f1a0dfe3d
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:50:52 +0000

    grade date range hate runinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
