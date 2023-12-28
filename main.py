9d14acd7fa98b6c706a2d62ef95298bedf7362e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:53:00 +0000

    dull guess accidentally consider instant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
