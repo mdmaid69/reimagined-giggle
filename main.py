0f0ded4a50806b1b0e8b3fe7d03475c3948183bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:13:30 +0000

    taste turn order subject grand

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
