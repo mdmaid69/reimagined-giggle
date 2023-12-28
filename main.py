749bee701d703ea17834b4ea0f046659faf498f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:11:39 +0000

    equal lock properly yes trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
