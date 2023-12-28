ba76f289ddc64352c961a99ae0eee45fec737ec8
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:41:52 +0000

    never OKorokay back could notice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
