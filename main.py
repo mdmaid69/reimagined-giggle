abacbdb80091b77f0dd3056f039f89bee8163b71
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:40:38 +0000

    table somebody share limit many

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
