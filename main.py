d4349fb06648467d928df831b2d3b32f1ca8026a
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:44:24 +0000

    lean method dry favour evening

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
