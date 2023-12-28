320828d889279714d4a7d551f335b796ca28f36b
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:57:31 +0000

    chief speak keepup cover hardly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
