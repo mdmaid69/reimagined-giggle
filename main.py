7794fd69b39be262ba07a0f2de3b8ddf8be185e9
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:40:43 +0000

    dry drink patient need stage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import os
-print(os.getcwd())
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
