92b3e47a6913428424c88786d63ff8465ee89396
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:39:27 +0000

    one wire yesterday comment fork

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
