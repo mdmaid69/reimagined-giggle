85bc6785dac8d9e966550701df97e0aebf7b44ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:55:00 +0000

    nigga touch best sad class

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
