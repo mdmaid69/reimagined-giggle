e94efc1c0370d5ebfd800f92e548e22880ff2b9b
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:45:09 +0000

    street apart invitation circumstance gift

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def count_elements(lst):
-        return len(lst)
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
