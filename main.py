4e5776f88cf834e97a6b173209bfbd2a8402fc18
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:17:16 +0000

    warning strike create introduce last

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def square_number(x):
+        return x**2
