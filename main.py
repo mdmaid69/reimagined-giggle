e446f9540fd2ea2939c3d849c0b3aeb5eca1bb4a
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:47:49 +0000

    direct smoke music question pride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
