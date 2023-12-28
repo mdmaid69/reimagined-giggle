2da4a5edef27554a1f9eeeec16147b7ceedb4219
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:10:27 +0000

    everyone avoid bringforward include club

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def greet(name):
+        print(f"Hello, {name}!")
