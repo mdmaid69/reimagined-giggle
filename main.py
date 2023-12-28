9dd34a8cc0447692509f7ee72fa43b09f3bf0878
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:28:26 +0000

    unhappy pattern check agree gate

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-print(math.pi)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
