47cc5c40b300d7ea40e22ae2d2769bbf1bfcbc24
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:15:43 +0000

    corner centre embarrassing also second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_even(n):
+        return n % 2 == 0
