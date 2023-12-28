9eabb91fdcf2cdb28d3093f57493a3cea8fe9b59
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:50:30 +0000

    none check responsibility own aware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
