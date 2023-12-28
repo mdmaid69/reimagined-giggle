6842d3a393fdc3ee1a4cd691dc131877258923e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:10:34 +0000

    ignore test takeout even likely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
