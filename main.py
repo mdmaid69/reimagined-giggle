93c6fe1cf1ad1f2e41b0655b8b93bb14c7595a7f
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:45:53 +0000

    course quickly weigh proof something

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
