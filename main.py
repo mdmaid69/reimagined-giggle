170590d2a10da12b8feb73ce7505bbfe45bb4ac0
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:31:25 +0000

    doubt spray regional shock positive

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_elements(lst):
-        return len(lst)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
