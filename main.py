6d902a39a630d6e5c70de25efec89bd68cc7b92c
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:02:05 +0000

    interview bad part turndown harm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_speed(distance, time):
+        return distance / time
