b5e9f7ff71e9c970785076ebd48375e8be17b2d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:44:28 +0000

    closely violence copy make slowly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  def remove_duplicates(lst):
+        return list(set(lst))
