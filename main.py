b7a7621da50e6ab5bcfefd9e0a1db66f0c3f6c1f
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:38:33 +0000

    top left cook answer switch

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
