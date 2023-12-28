5f0766cf4c2778c6b1b6f64d8e1a7e6ff6eb79fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:28:35 +0000

    behave message star workout fetch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import random
-def roll_die():
-        return random.randint(1, 6)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
