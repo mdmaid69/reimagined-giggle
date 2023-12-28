d78efb3fd4517545cc6e0327d3992cd18f53d587
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:08:39 +0000

    find scared crisis me real

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def sort_list(lst):
-        return sorted(lst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
