6724abd95a4e5a374251f1b6756c14fd12059136
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:54:26 +0000

    march runover always hide sale

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def sort_list(lst):
+        return sorted(lst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
