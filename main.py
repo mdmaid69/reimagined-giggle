b2bb89d80ae1a36010b283749f0708b7e03b55a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:48:31 +0000

    intelligent priority classroom dangerous stamp

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
