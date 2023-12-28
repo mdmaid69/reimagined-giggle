b787da01e828c37ad0f1de37d8e7b8645c3683fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:37:59 +0000

    description unable shopping sister plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-name = "Python"
-print("Hello,", name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
