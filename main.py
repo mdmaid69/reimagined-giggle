f639ecfa89c0ee2026e62f5b4bdeaf3826c0fea2
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:45:33 +0000

    awake embarrassing make grand load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
