b5a2cb7d9ad5a7f3040abc3576ee29cd697420d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:04:24 +0000

    evidence wheel rob car contents

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
