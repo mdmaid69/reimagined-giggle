ae8a892d4187d97d549555abce0ebac006182cb6
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:24:21 +0000

    blind sting past comefrom bright

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import os
+print(os.getcwd())
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
