3047a5a11adbc7d0ef4ea85f31fa6f7182bf1900
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:00:50 +0000

    570enjoyable language worried treat moment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
