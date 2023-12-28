78735489e875ec9335c62a0ff5203d0cd32b9d6c
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:03:37 +0000

    thinkthrough aware Mm evidence pick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def subtract_numbers(x, y):
-        return x - y
+def remove_duplicates(lst):
+        return list(set(lst))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
