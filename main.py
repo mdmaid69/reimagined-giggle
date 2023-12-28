28d88a14a1a9039002b5f7d8048419994c92e459
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:37:43 +0000

    arrange fetch immediately true surprised

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def cube_number(x):
+        return x**3
