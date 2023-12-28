2dfce00cd6d5ac830b7a157423d3f65aeb39b548
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:12:51 +0000

    forgive far politics chase serve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
