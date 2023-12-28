ea39945e8f9065e17b9313218a08a43c4f5343a5
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:42:38 +0000

    shut hot bang rain hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
