0d4c10dcf8f9f747342a1f98b04c6c7e607cf949
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:29:56 +0000

    hard free complete kind weak

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
