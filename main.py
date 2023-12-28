08d187442c91ef0ee6ad13d313986ac5218059e9
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:53:13 +0000

    job goodbye home light takeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
