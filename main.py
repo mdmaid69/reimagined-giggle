a317ee4fa4d7882548261bc15471c3398b9b3f16
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:59:57 +0000

    pin branch join pour waste

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-name = "Python"
-print("Hello,", name)
 import array
-def get_array_from_string(string, typecode):
+def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        a.frombytes(bytes)
         return a
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
