9935d80dca74af8d0497a4d42a968fb0d3bcf85c
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:06:18 +0000

    thinkover cinema might move tired

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
