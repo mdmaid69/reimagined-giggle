03475ce3ef6f30912264e999d672517818aca00f
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:12:30 +0000

    another persuade save clean debt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
