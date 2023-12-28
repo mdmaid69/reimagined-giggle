ba74cfa724df87ec67e8b2f379714b459800cb2d
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:08:39 +0000

    section around regularly doctor runaway

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def is_even(n):
-        return n % 2 == 0
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
