74e12dd195cffa8f8a5875ecbbe5eda6da29f430
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:44:22 +0000

    petrol luck month confusing mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
