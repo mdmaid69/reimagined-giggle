3d4362a336ca7b3cf4b97c74ac8c9d507539fa6c
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:02:04 +0000

    somewhere mood ever seem closed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  def sort_list(lst):
+        return sorted(lst)
