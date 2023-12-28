3e279933c1841df4c47b8d6da13cc1706cc994e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:53:27 +0000

    bright independent line summer plain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def is_odd(n):
+        return n % 2 != 0
+def reverse_string(s):
+        return s[::-1]
