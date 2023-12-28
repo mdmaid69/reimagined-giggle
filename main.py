ba4908587d1a53887efa7707fccbd2bf55e8164d
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:26:48 +0000

    within belief sorry level occasion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
