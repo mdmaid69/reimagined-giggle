8c8bb43a097e09621e0000ec51eb549a5632d72a
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:33:08 +0000

    giveback tone onto thinkover war

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
