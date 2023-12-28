f5fa68e7adf77f1e54f212b0712f24d8769e8bff
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:51:33 +0000

    important detail wish I small

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
