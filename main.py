31c4c83f54114c76f41d61d56a7354d45d3ab8d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:59:35 +0000

    fire prevent before wash bush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
