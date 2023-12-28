b4244d6628b5c2d78b8383e63c717111f909b4d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:49:25 +0000

    scrape them pocket sight number

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
