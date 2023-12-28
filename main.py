538fd883f12dd4f2a8c5890cdfeba47e4d2169bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:30:48 +0000

    university goon difficulty reliable quantity

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def sort_list(lst):
+        return sorted(lst)
+  def convert_to_binary(n):
+        return bin(n)
