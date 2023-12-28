9be411224e03e0a0f43b4a4985325f30b3710690
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:24:58 +0000

    emergency present twist object square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sys
+print(sys.version)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
