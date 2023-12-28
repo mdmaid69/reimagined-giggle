8283003b5506010461cf16e5cc3a1013b514b295
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:26:00 +0000

    pocket forget body unit season

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def clear_array(array):
+        array *= 0
