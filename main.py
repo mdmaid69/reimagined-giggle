b0cfb5cf7b0ffce672d661f3f150e18bddf9ea53
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:49:22 +0000

    gather fix vanish shout hurry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import collections
+def create_stack():
+        return collections.deque()
+def calculate_density(mass, volume):
+        return mass / volume
