a6ac4587b466c0143c4fb889980bf50c6fad72f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:12:40 +0000

    ever date course teach debt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-def is_even(n):
-        return n % 2 == 0
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import os
+def get_current_working_directory():
+        return os.getcwd()
