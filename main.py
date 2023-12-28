1fc50d71dbf68fd8e28a3e020eeafca47125457a
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:28:44 +0000

    mad dangerous switch remark free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def reverse_array(array):
+        array.reverse()
