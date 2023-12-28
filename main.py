949244c013b6c309826c5dbb65592c0cd4cbc695
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:31:13 +0000

    tend increase responsibility common evening

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def reverse_string(s):
-        return s[::-1]
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
