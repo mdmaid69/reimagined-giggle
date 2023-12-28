18eaa5ae8735d57f6a25fc17aa27a25561383c35
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:19:16 +0000

    main bottom beneath ought leave

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
