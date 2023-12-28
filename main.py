dcb60c920e1926ddc3ac09585c0749194b565a5d
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:00:51 +0000

    equipment proof contribute feed rid

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def convert_to_binary(n):
-        return bin(n)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
