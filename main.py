52b0c538041a5a9c7cce4ec98a6e6647eafb2667
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:11:34 +0000

    relax suit getoff fold total

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
