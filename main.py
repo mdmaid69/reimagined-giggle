48b5b5f1f5389fd091ecd1345f3ac80a5960b874
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:40:56 +0000

    part fit violence entrance many

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
