f32b705246a38eddb1ab6e0f834f13a8feeb6629
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:11:51 +0000

    difficult tired side whenever area

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
