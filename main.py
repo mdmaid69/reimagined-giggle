430172c6712b62f84d2fef4cb8ec94edb73da0c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:43:39 +0000

    condition offence worried comeup nearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def remove_directory(path):
+        os.rmdir(path)
