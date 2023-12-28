bc7ee59b481ce56cfbc4de46aab4a83afdabe6ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:04:53 +0000

    present proof head near die

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def clear_array(array):
+        array *= 0
