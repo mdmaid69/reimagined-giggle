a58353b75a578d6eebe3a24d42657fa75ac23d3a
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:13:59 +0000

    soon lookup someone material Uu

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def add_numbers(x, y):
+        return x + y
