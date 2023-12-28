c32200cd4a5a3c8294c8d130d83cf8680ac67068
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:41:54 +0000

    strict remove answer maybe win

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-print(sum(range(10)))
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
