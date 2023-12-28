a4511a4bae4c6371496b1378dd359b209d167dab
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:04:49 +0000

    hand cruelty rate fly relation

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
