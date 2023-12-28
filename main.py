af3f6662de320618b371501bfd2de8a629e6251f
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:03:48 +0000

    store over funny maintain plenty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def get_current_time():
+        return time.time()
