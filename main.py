5784c1f43d411d90acd6e29f5409fc39c3c0583f
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:33:49 +0000

    frighten yes fortunately pupil might

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
