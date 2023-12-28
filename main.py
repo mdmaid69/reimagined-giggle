3cf2349c067ad1a586b11beb1b75204b4ca7dec2
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:06:31 +0000

    unlikely drive shop boil switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
