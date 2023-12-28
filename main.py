02c679e70aeb44b4d32d83951f9ad0577e85e382
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:01:05 +0000

    Mm question subject some move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_cosine(x):
+        return math.cos(x)
