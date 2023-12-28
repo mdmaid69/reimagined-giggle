d7a8b21ea48786d9b3e3223173e83f3501a98359
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:17:24 +0000

    mine thirsty spread cold acceptable

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
