d081e5caf712daf5ac8c46cbcf1fcef455729652
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:47:49 +0000

    match anywayanyhow want their every

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
