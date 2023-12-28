a5a8830cef5de3675f9872b85d18c985792f000b
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:18:30 +0000

    raise know trick but pot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
