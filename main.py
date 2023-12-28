fc5a9dd6757ef604418f72075e5bd74f14215f3f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:49:02 +0000

    feeling dig record week cook

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import platform
-def get_os_info():
-        return platform.uname()
+import time
+def get_time_since_epoch():
+        return time.time()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
