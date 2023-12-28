c0c53562389a63b550dfe895011b398c6c9a5bb2
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:56:32 +0000

    could change hit evidence lookafter

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
