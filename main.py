0d85c8d41ba29d3228d22504aa90a2293fb528bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:08:15 +0000

    direct treat related wheel operation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
