743848503502777aa17f4bd549c576f042eae858
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:44:14 +0000

    act truth laugh key fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
