867f59d06eb80738fe03031523934d19e48ea791
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:29:02 +0000

    replace long wherever improvement real

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
