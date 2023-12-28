ef7b6c204ca38bc79feae2011bca5f04289c49c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:38:21 +0000

    remove delicate arm agree eat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
