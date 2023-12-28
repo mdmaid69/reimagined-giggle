39e70d6a88ca187e47d2aa7db99898ff530b4e3f
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:30:35 +0000

    education nature original thin effect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_sine(x):
+        return math.sin(x)
