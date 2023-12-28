8bb39fa013a51369da475848bf8954aced7e7fc6
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:56:22 +0000

    tear colour pinch communicate breakdown

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print("Hello, world!")
