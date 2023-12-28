c9505365b954288f02197ae9c426463ae86bf567
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:47:29 +0000

    smell process national callat hand

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
