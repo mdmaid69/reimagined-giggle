b1ae87ffa7d981e9629b1eca315c066a39bc4d06
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:14:05 +0000

    learn continue target characteristic exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import os
+print(os.getcwd())
