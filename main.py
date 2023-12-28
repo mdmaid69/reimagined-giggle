9a657bedc097b761ddb3cfeeaaf4ac6753082023
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:58:08 +0000

    time bottom trust square direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
