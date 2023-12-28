57bbed0f37128ae7f67038cfe6c83e0dd2cf3805
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:27:47 +0000

    weigh final noone pencil equal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
