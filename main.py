8b7325b671408f7b05311b20fe146126bf876bc7
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:31:23 +0000

    bang stretch comedown dance reach

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  def sort_list(lst):
-        return sorted(lst)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
