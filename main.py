de33bb85c5371f498f8b21360868ded6d2da6b2d
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:37:32 +0000

    finish unlikely area taste share

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
