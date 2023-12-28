7e5137571a15ed70460f866e44831dd11db3ec32
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:47:27 +0000

    cheap autumn reliable autumn drive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
