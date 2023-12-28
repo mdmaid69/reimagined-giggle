ec83c4f45f9fbf2732097b1790da4a1327a98371
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:59:15 +0000

    main whistle tall refuse harm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
