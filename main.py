52b233097af94e9a46217ba17b8029c9121c8506
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:21:42 +0000

    shade interrupt piece funny so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
