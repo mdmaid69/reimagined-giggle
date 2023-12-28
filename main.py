65d16d8223b9b9854f44c421213ff25098a22788
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:50:01 +0000

    whether getoff stick hour offence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+for i in range(5):
+        print(i)
