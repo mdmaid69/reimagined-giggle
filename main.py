7cd2a82dacf63ac3c16c2991e194031c039706fd
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:57:07 +0000

    duty top answer support age

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
