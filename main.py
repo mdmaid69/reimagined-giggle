1cbb596a6929ffb8b926e47841caf042388b221a
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:24:03 +0000

    deliver loan partner address flat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
