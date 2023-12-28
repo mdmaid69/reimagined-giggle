923b01b9beb977c9d57aa655e84a688c3324c158
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:27:38 +0000

    start deserve obvious sight blind

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  def reverse_list(lst):
-        return lst[::-1]
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
