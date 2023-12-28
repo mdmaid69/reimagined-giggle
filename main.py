486a5064585dcdf63a6c10085509cdf50fcae789
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:34:24 +0000

    reject guide contain frequent file

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
