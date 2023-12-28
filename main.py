a5281359b4e476a7e52d84e7486c2f24ba4f2813
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:32:38 +0000

    their love free around worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def square_number(x):
+        return x**2
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
