6f39f1497db5a5a6161e83c32b5ffec38b0c4614
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:32:20 +0000

    opinion relationship great walk fault

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_acceleration(speed, time):
+        return speed / time
