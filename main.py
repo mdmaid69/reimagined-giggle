5945418bf277686f9298d7564df99bbd16f48d2b
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:59:25 +0000

    feed spring admit press cry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def count_characters(sentence):
+        return len(sentence)
+def square_number(x):
+        return x**2
