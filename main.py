8191a1cfcd786570b8df43fe54a4789744049ff9
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:58:37 +0000

    confused throughout tell fun band

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_as_set(array):
-        return set(array)
+def count_words(sentence):
+        return len(sentence.split())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
