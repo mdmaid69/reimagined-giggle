8da0ffdd4cae8f3595b3c4ce461c9d9022f35891
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:15:48 +0000

    react concentrate seem opposite accidentally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
