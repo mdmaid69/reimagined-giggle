aea53b35077d3e85080679aadb6f9c87f633a0c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:19:04 +0000

    lift another read slide sticky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import sys
+  def get_python_version():
+        return sys.version
