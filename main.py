cffd42a84e51b6dc4ea63696c7f1a2c07c016651
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:59:39 +0000

    worth wide rid keepfrom protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
