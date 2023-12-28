4d57f81dc25c22b199608ac787bc58d16ea3463a
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:33:05 +0000

    case mind tight transparent needle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
