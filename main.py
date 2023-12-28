8656670bee64ad4880e5024b22423ff7d93e1fa9
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:23:32 +0000

    row turnup distance care check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_counter():
+        return collections.Counter()
