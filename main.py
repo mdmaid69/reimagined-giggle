f0787c8b4faa1fc8231e77cb04fe8a2babc458e0
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:14:57 +0000

    judge believe satisfactory pump active

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_density(mass, volume):
+        return mass / volume
