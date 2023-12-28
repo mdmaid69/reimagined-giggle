f82fd70d1eee3bcb704d3086e49b396a6801b3c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:27:28 +0000

    closed exchange standard ahead hair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
