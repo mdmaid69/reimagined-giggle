db3c363dc843982d785b23b278dab975cafa2722
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:57:03 +0000

    difference mouth simple understand passenger

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
