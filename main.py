fb2fd135221e109c009ae5f7191a948f3e7a243a
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:06:51 +0000

    release takeback bye ordinary grade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
