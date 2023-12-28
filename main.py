ee961fc9c6f6f0eeeb1573ba82463941476dcd88
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:59:09 +0000

    callat mix afford hope steam

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import random
-def roll_die():
-        return random.randint(1, 6)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
