f5afc20929f5a9c9081bb580f34da55380cf9ace
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:00:13 +0000

    union Uu little sore pin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
