57971dcb9f86f6a8eee045123f3e8d6ea60becdd
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:31:51 +0000

    which grade profit content treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
