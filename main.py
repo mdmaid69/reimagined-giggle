c0163c3d2a50451fe8e27afebd51a6d79e8e2d72
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:32:24 +0000

    apply chase mind stay bang

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
