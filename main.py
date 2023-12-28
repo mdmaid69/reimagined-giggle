4a3d533ad646dd6a3154f33807c8aed2e5ee0837
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:20:13 +0000

    rich quality forever pronounce slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
