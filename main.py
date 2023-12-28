58e1d37d25e9b8a53ad2ed2b891200065b45bfc6
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:15:15 +0000

    burst suck concentrate trust brave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
