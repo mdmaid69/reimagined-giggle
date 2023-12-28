f425377f36220821d03fa8d6c66763630ac1f79e
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:10:17 +0000

    call congratulate plenty general fire

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
