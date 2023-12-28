69ec770c450ba52bf68ebf39d411499d1de3d6c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:41:05 +0000

    ability compete arrange front buy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
