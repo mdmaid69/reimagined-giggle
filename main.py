575ab34754aca86059458a25aafa98d5b572e6cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:34:58 +0000

    if underneath even black getoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def find_unique_words(sentence):
+        return set(sentence.split())
