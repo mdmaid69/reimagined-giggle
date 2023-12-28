3f2d47460794e63746ea9d45a7236518490618ec
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:11:31 +0000

    round stone either ocean usually

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_index(array, item):
+        return array.index(item)
