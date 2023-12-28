c697d102a7c7f036ce1901be66f59052dade0ed6
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:23:58 +0000

    shame godown interesting programme slow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_max(numbers):
+        return max(numbers)
+  def cube_number(x):
+        return x**3
