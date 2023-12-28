e756d0497471074b9fd2ea01fd91297f2defa8c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:18:03 +0000

    Qq cry necessary effort than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
