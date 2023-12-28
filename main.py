fa6568cea439f742a35f92f73d727b7a4a8614d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:19:09 +0000

    container ticket fix comefrom secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
