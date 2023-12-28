504c3131e7c25c2cff23235c3dc5fc0a3f29b0d5
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:00:19 +0000

    formal taste mine bottom march

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
