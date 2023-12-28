fcc3a855f325c93196f8590b5fdab39b0497e538
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:28:25 +0000

    mother display separate careless wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
