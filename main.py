b28c5632fd84d22d3326a0aebd2df5f44b10c1d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:28:38 +0000

    bringup achievement relative test opponent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
