6f5fce94037d1811935d973dee2aa837489b174f
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:24:15 +0000

    understand brown pretty firm coffee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import os
+print(os.getcwd())
