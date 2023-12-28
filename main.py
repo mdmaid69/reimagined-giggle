7db98fd8a28105500ccde49fac7b514fe3c085f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:52:05 +0000

    rub settle turn grow drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_perpetuity(payment, rate):
+        return payment / rate
