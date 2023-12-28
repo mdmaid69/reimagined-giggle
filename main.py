3179b60499ed5eee9a88b45418be78572afd9d57
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:04:42 +0000

    die cheque woman thankyou relation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
