baf9e31cb45d1c9996c249183d2682ba78539d06
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:23:04 +0000

    shine black yesterday shoulder friendly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
