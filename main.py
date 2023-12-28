0bdf63ee21728746b0950ed773f1e59062cac135
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:35:19 +0000

    character Ss trick sort frighten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_acceleration(speed, time):
+        return speed / time
