c37a27ed71a8b2ccd3b4822d552648a0361677ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:58:48 +0000

    hurt sister crazy soft mend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def find_max(numbers):
+        return max(numbers)
+def square_number(x):
+        return x**2
