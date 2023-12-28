a425866f9e9f99d19f30e3b36869bf87d0250ad4
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:12:22 +0000

    treat regularly reason read thankyou

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def square_number(x):
-        return x**2
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
