3e258b20c56c8f70b21e599c32a41820cfbd5e99
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:00:38 +0000

    pause note round friendly very

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def get_array_as_set(array):
+        return set(array)
