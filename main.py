122ad55e3eb44588445f1fc36b765e599e96526f
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:12:36 +0000

    trouble 573entertainment turnover prefer ground

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
