e0063279638de69f4e077344df4908ea31316179
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:05:07 +0000

    tight generous beginning this unlucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
