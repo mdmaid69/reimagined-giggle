e407b91c1adc43b55a4367ff5b3661f51270ff3a
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:53:36 +0000

    notice discussion dinner cost mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_min(numbers):
+        return min(numbers)
