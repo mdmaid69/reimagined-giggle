a44101dfe39edd934379abfd5fe6d3199bae390d
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:47:16 +0000

    keep shop could switch all

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def convert_to_binary(n):
+        return bin(n)
