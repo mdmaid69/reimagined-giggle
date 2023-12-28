c2ee2fd9122d5109b44748a13e074dd2dd56499f
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:20:33 +0000

    ball another Ww separate proud

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+from collections import Counter
+print(Counter("hello world"))
