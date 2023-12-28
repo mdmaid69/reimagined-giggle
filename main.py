06ab4dda987ea071798317a00c501da16faf8c5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:25:23 +0000

    without takeout hold oil complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def change_file_permissions(file_name, mode):
         os.chmod(file_name, mode)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_set(array):
+        return set(array)
