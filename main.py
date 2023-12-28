70afff56c674ad6aff7e549c239ba94d7d1fb264
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:54:10 +0000

    concerned eat and fill similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
