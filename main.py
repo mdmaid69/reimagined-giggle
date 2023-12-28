d1d9a7eeb4a59128eb5be2f3515483096648c7d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:34:02 +0000

    bored powerful name wander goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import time
+def get_current_time():
+        return time.time()
