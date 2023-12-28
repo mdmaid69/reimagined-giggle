717624b5694db1c4df34dc140d7f5531dacac154
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:03:30 +0000

    oil also disgusting indeed thin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
