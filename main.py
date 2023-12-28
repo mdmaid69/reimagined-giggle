faa2a8cf50792e7adb7ae377f2db3634b5587140
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:39:17 +0000

    apart keepaway programme pay arrangement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
