f3710dfdbf04b08c3c69c181d33d0d364d58b7df
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:11:23 +0000

    worst further you mother tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
