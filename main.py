4f9d7e14b78b468035a438f1181e9fd439248bbd
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:53:40 +0000

    pour guilty decide noise blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
