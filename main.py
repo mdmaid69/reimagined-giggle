f6c542369613b717bb42d026e1964443fc352fbf
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:20:39 +0000

    address thick narrow congratulations far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def find_min(numbers):
+        return min(numbers)
+x = 10
+y = 20
+print("Sum:", x + y)
