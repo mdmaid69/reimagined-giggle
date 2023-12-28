0385fa2de204627205bc86747a83e14808e12f64
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:52:47 +0000

    any comeout mean call giveback

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
