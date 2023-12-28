607bd987654c5b521e7a2c806f934a2e29badb41
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:25:39 +0000

    instead compare that air wash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_speed(distance, time):
+        return distance / time
