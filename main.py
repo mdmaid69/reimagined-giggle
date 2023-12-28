6a43bcb418a4c8b1537acdb0e1216c4e5364d467
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:26:13 +0000

    plant unusual long lend ready

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_min(numbers):
-        return min(numbers)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
