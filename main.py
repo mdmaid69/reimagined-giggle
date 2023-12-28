a9820d4c87b68d8ece24ec6d9fe3540cb04ee20b
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:07:39 +0000

    wide large smell hope brown

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+text = "Hello, world!"
+print("Uppercase:", text.upper())
