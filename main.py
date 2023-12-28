6f16abe7bcf061df682e7e171deb9af6def31125
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:05:31 +0000

    whoever contain pour direction anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
