3be3e44d60b8edc598babd05703a16a2eb3aef50
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:20:24 +0000

    transparent interview cheek bit comeround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
