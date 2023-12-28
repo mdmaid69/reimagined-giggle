44451a077574e4810fe7387c9ce3f24657d1b442
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:19:39 +0000

    autumn afternoon independent rain anxious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
