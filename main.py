26c91cf9fb5d5b38f731111e22be8bdf0ba58f90
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:39:41 +0000

    goback(to) time present swim shrink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
