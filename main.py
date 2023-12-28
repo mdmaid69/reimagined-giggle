6555b9b2599bfac9dfccd25424f352edd871d53b
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:45:49 +0000

    callup back famous fond terrible

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
