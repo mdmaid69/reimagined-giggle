217ea4c1ce1680075a57e5dc4d0ea4848d02cfd7
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:17:09 +0000

    round risk avoid uponon task

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
