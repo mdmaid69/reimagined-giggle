e9b6b32c618d7e46e53ed7ebc5f4cce323be5f81
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:11:38 +0000

    health man press high anxious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area_circle(r):
-        return 3.14 * r**2
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
