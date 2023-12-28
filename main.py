32b599bb4bd33be77e6525d7bc56b1b1e82418ec
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:43:13 +0000

    share used always gas behave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
