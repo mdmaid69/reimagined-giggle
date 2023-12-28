0a25cba88f72dda0ab2c271e2057d3f3bab25589
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:24:53 +0000

    tidy shade talk formal middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def exit_program():
+        sys.exit()
