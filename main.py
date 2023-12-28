537b92c753b06f07ecc28dc09425820e95af4a37
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:42:27 +0000

    bread break bringback consist whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+name = "Python"
+print("Hello,", name)
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_set(array):
+        return set(array)
