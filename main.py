cac2be5b1a3f282309e58b0483985c6e73bbb485
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:26:57 +0000

    summer custom sing culture wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
