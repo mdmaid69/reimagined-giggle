27afb51c3d288dcf8ac09edc1db136fdd17f51a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:25:57 +0000

    reserve neither figure sound brief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
