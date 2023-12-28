eb45afcc53f01fb65b46de9bdec2ecdc493f4712
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:07:20 +0000

    throughout fear keen must apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_list_from_array(array):
+        return array.tolist()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
