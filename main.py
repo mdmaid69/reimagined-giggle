702251e9fc1a2ef97924e05600c8624eafc68e8b
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:42:56 +0000

    recommend mad grade recent sink

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
