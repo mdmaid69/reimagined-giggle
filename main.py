1e43a688b7bbd06cd6e100042691201fdfc7edef
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:19:52 +0000

    distant danger mind pump sting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def reverse_array(array):
+        array.reverse()
