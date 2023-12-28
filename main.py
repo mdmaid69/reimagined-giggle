340164764f644dc33eb3d1f52bf0fa20d3c12017
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:03:19 +0000

    owe breakdown strike male praise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def exit_program():
+        sys.exit()
