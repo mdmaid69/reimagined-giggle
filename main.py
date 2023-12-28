5a832d59cda95781def8abb332362c90975354f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:29:49 +0000

    pin lock result afford shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 def calculate_power(work, time):
         return work / time
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
