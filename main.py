aac29178284c5073647f51eb4774a633a738a311
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:03:12 +0000

    able sky cap change issue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
