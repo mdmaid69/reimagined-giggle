e722f5f984321242b78142a3961fa47349389b18
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:44:14 +0000

    exploration centimetre positive crowd chat

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_even(n):
-        return n % 2 == 0
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
