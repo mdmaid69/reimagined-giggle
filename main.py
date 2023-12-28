f965daa097205e72c9504485cd4ba8d2958e2830
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:24:40 +0000

    error down hard neat autumn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
