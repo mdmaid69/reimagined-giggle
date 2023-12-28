0617e766f61947d4234933f3d9105101ad053806
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:40:50 +0000

    better shine bread order weekend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
