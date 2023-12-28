16777cf570dd5e8c7e8f29280b46f977b4ff22ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:26:40 +0000

    justice appeal peaceful public recognize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
