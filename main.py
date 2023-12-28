82271783331c186099cb60a0020bdf972fe9f7e1
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:27:34 +0000

    personal reply scrap purpose fresh

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import platform
+def get_python_version():
+        return platform.python_version()
