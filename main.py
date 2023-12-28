e4d7e4ef7501723c4b41bb4277e490d907f66f5f
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:18:28 +0000

    apply obvious kind right emphasize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
