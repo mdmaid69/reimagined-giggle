e743798860e40b43cfd4b5e26671711b4aedced9
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:00:15 +0000

    lead flat crowded doubt up

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def cube_number(x):
-        return x**3
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
