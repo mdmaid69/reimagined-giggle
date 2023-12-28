1eb5a5fc017b9a9744f711a75fb4a442cc061939
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:46:28 +0000

    on desperate lookback(on) careless peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
