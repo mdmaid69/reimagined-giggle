16fc667539c0561f1ad1ff762c444c20524733d2
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:06:46 +0000

    identify agree name react squash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  def square_number(x):
-        return x**2
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
