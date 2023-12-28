6ea0a4f0581d4f44adf3403e6802b051bf7f0a21
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:35:44 +0000

    matter delicate wire basically feature

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
