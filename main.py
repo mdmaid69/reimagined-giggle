557b11873b96c89225f8c59ee646b9b622758c14
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:00:44 +0000

    pinch sort afford circle flood

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+print([x**2 for x in range(10)])
