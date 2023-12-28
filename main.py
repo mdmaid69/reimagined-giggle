c61dea1ac4050fcdbe4ce03cb85a8f1617f6a21f
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:28:41 +0000

    oil show declare of perfect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
