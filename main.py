39ecc2853e63bce04728501f1c29be313fb79f4f
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:20:20 +0000

    hospital finally between sport related

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_current_directory():
+        return os.getcwd()
