42f0dca350f5addcd11219e87f53eb9a595b26d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:49:13 +0000

    sail feeling public wise tap

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
