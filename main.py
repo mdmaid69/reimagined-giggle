36d2dc170fbcd209a63a2e107eb050b5b4c9ac85
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:48:32 +0000

    he forward vanish breakdown vote

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
