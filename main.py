0959d43f9f13bbb47574eacad64c5997e2302399
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:14:20 +0000

    figure stop for tradition get

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
