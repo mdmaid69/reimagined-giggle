844b713037b93477e5b2ef73f9464430a9030afe
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:19:33 +0000

    strong tour blame shout beautiful

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def divide_numbers(x, y):
+        return x / y
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
