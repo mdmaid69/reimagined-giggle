f4b09bebfd177703037f69edba7cbd86f849193e
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:11:02 +0000

    unpleasant band song quickly exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
