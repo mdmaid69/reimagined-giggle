b4b03e9abafa3d8ca747c67806db6fe84a4da5cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:25:40 +0000

    lend underneath demand contents choice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
