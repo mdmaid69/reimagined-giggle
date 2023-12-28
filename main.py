2b5714ab4e669d0f5a18eeeaf0fed85d293b951f
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:46:46 +0000

    hope development shoulder prevent afterwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_hex(n):
+        return hex(n)
