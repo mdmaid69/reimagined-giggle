214577f48a936ad01a872af1dfd57218235dce3d
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:55:48 +0000

    police do callup turnback cope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
