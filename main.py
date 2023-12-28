d2301dde74b075aeb287d999821a16ab8038196e
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:45:08 +0000

    firm listen word land hot

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
