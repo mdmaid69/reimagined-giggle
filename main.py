ba4e806803461af8ace440f45e4f836e9a38f292
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:03:03 +0000

    argument female gentle pollution freeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
