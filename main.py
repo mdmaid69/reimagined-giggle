5b93d269ce3276901ca57ea436f896a9bf47f6b6
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:40:23 +0000

    callat in lead lesson empty

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
