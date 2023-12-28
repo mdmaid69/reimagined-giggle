866067d60215a4da9921d042ef715dfb84fd089e
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:29:30 +0000

    university translate unlucky satisfactory cheat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
