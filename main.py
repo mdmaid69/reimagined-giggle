e54393b58909c77c84f57935e265ae411e8b4a9f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:56:09 +0000

    identify lend shallow stamp fashion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-for i in range(5):
-        print(i)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def find_max(lst):
+        return max(lst)
