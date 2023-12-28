617b0f5dc19caa3de394426ae655cbb17fabc7a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:43:40 +0000

    must nice sticky believe control

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_work(force, distance):
+        return force * distance
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
