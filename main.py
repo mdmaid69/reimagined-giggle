17c504e46e29b729a53a9fa1091ca21c3e7b6376
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:41:45 +0000

    mix pay past keepup test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def multiply_numbers(x, y):
-        return x * y
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
