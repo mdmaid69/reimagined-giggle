bc660f3ee7c0aeb97409ae57ea1fd72876dcfaa6
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:07:56 +0000

    thinkthrough embarrassing knock available right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
