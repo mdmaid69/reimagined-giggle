d85f0634031a85db295f89ff4896c3c1587a24fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:10:02 +0000

    contribute runinto clearly discovery callback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
