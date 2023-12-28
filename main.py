1509af9da1ddbc203d9d2fcbb51dfe8ffd21fd8b
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:47:42 +0000

    chat tap left demand practise

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def multiply_numbers(x, y):
-        return x * y
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
