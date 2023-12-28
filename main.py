77f7be10d890ccd20c9ebf6ef71dad343ba94d00
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:19:10 +0000

    across finally motion towards afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
