bfe89603306a043f50edc8414d7dcee90219627d
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:27:54 +0000

    tired plenty task play label

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_user_string():
+        return collections.UserString()
