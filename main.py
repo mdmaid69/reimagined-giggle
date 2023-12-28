c890066b22cca6b4e423d040ce1e7cc9cf24041c
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:12:52 +0000

    fast movement collect vanish less

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_array_as_str(array):
+        return str(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
