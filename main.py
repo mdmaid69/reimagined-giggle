fad9594cf3883bcbcdc1ecb5d6893f3ee6d331d2
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:28:10 +0000

    attractive business computer blame turnover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
