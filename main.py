cfa83b8a743e59acfb87a4527ad857d754fd356b
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:21:35 +0000

    curious tidy sink pain rough

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def clear_array(array):
+        array *= 0
