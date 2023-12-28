b691e25a23aa3b47a12259c6dd34a6e0cd50eded
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:49:19 +0000

    pride sleep myself middle bin

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import random
-print(random.randint(0, 100))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
