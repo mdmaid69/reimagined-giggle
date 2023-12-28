4e31ad5c9f266ebccfe763956c4156c8b274a880
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:13:41 +0000

    stop frame fairly habit evening

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
