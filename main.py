d27f1e9b08c6e9ec3ce9910f9835bfb422f2b674
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:17:15 +0000

    performance team tend plenty include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
