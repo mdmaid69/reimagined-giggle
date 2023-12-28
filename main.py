967f2064e4b187f951af914af566c835dd898c0d
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:59:25 +0000

    remember storm exam start everybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
