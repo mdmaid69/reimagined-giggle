2ee639eaca251e1619b500d10640102b99602f6b
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:49:57 +0000

    owe ear geton tense difficult

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
