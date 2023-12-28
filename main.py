c3bb2ba11a41a21d55478c3730fbe8880ef3cb52
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:01:26 +0000

    inside simple contact goout there

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
