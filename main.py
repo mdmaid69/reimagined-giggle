6a87a1ec48235a07fcc43f30b571ea6e579d0a2f
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:02:32 +0000

    likely maintain whatever nonsense schedule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
