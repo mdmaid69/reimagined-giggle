3b0f3e2caa11769bb6173ee0cdf2689fdc25d177
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:59:24 +0000

    act fair now crash check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
