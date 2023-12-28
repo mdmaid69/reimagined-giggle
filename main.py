68a57e0ce5ceca2e92ca453df12829a6ffff0e78
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:57:35 +0000

    commit us check recognize control

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_user_dict():
+        return collections.UserDict()
