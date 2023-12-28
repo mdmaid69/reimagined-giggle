6b11e0736c38c2ae460058310c1956a28f8f2eda
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:06:21 +0000

    steam boat equipment shopping gather

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_typecode(array):
+        return array.typecode
