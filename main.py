0bfb781973fe35488b5d765e5d616140f3fe2580
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:22:39 +0000

    Ee bright sensible relation defeat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
