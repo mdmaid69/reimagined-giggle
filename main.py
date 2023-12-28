efb444fd255aa4faf72adde18f22571fdb52c754
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:20:28 +0000

    hope risk stupid lookup sweet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
