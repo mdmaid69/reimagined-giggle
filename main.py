1cc00022387c89733b1bb8027eba3d8116f0efd6
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:05:11 +0000

    sign foreign escape dream rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_tangent(x):
+        return math.tan(x)
