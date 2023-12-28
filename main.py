edf9709c2bbb30428e791fb996d0cda789c4c2d5
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:06:23 +0000

    spite score actually must crowded

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
