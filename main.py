de02c03ee182c818cd9f0548e6c847effbfe67fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:32:44 +0000

    float upset moon factory stiff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
