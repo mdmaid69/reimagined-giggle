eb3bf169d4d234a02b1b26f63ac2bfd394b58b4c
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:08:29 +0000

    somebody pretty reliable pour habit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def reverse_list(lst):
+        return lst[::-1]
+def convert_to_hex(n):
+        return hex(n)
