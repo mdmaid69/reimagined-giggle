02f5f448d5e683e461aecc8b4888c1b5d637a64e
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:14:45 +0000

    also correct talkoutof bag bottom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_volume(length, width, height):
+        return length * width * height
