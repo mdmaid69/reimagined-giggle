f89314d0067197e9d13a3f6f5acc6f31e225036d
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:48:56 +0000

    hit slowly talk accommodation through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
