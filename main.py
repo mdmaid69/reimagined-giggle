4eeaef3bb7c28bb3da24f35693368b98720c50b6
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:11:15 +0000

    intelligent under principle regret rise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
