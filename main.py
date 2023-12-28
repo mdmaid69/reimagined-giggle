5d2e11d8c52f359056c1e8f2cefd41bec7c4ecc4
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:46:00 +0000

    stretch shake takeoff work 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def add_numbers(a, b):
-        return a + b
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
