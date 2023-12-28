2d8173e52e7109dafb5b3d80c624037c91630ae7
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:44:46 +0000

    point back part praise variety

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  def remove_duplicates(lst):
+        return list(set(lst))
