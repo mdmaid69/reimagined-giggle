3b08cf7959c1ddab804aaaca4836cfdd28467271
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:40:53 +0000

    long right skill tidy available

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
