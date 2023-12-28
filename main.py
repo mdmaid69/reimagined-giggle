7ba3517b8a39ace834df00706dda872e5bff0c32
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:28:38 +0000

    temper left respect ago even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import sys
-  def get_python_version():
-        return sys.version
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
