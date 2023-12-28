f7fedba8e39c59561d17bc574cfb44b20729a265
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:10:19 +0000

    pretty on half direction apart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
 import os
 def remove_directory(path):
         os.rmdir(path)
