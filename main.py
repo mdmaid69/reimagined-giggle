ae871c3031ea24210ec9cb0360177154fe78a1d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:44:33 +0000

    fight thorough through few Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
