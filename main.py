0f7febd13d6ba61aa17e3373a7a4d60d00a79875
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:26:04 +0000

    from bring once crisis want

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
