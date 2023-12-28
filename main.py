100cc888ea7c36dc52744fd1b1f0757e2ef60aa7
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:27:07 +0000

    aim content pause behaviour breakout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
