217b6e2fae81ff0daa626feb8908223aebdd08df
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:24:26 +0000

    raw thinkover top tend book

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
