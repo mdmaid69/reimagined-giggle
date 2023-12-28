241f9d207777b45302e222cb16a5a2ed535edfae
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:54:33 +0000

    concentration full mixture tape no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
