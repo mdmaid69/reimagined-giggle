de20791645a8cc74ba5a42cc20a5a534f48e3511
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:12:02 +0000

    thing see direct Qq whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
