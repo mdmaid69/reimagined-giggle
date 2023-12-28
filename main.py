47b149eeef8907b9eb6ed74cb5f25b9fbd981c8e
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:28:37 +0000

    argument attractive regional instant eye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
