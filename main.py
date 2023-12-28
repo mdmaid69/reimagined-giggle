5a54fce22a55b7031b0b32cace4c6004bccfc702
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:22:13 +0000

    loud realize reject convenient workout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
