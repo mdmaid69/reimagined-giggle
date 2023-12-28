0813c3ddbe2360354e34ac5107bf74603efa6980
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:55:16 +0000

    edge break gather clearly forever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
