dcb39f35c25df5ad53f63007f3216defbbda6dc7
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:32:46 +0000

    shadow peace inside exam charge

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+name = "Python"
+print("Hello,", name)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
