7ad0f959fbe201d939bb3ade2477067fd78bd8ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:42:41 +0000

    arrange Jj life straight reaction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
