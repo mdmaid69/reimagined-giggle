6f4cd55878183d4c20b2c36908f0364e14ec1160
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:03:58 +0000

    see heat treat sight wall

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
