abbdd8fb892f9543f341e29e85f4a90e4901ab5a
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:29:12 +0000

    soft in no Ww possibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import datetime
+def get_today_date():
+        return datetime.date.today()
