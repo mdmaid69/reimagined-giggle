e68e449f96957406a51cd0dd48569da66a74fce6
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:34:04 +0000

    influence runaway house backwards home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def convert_to_hex(n):
-        return hex(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import datetime
+def get_current_date():
+        return datetime.date.today()
