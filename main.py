2e918e66d4084fe41fe238e4c90e62dac14ddb18
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:49:08 +0000

    gift prevent goback(to) right opponent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def is_odd(n):
-        return n % 2 != 0
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
