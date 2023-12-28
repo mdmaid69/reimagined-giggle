78d01693ab48d3087181841602cbdbfa1c5a9c3d
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:20:34 +0000

    deep fall moment high yourself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
 import heapq
 def pop_push_heap(heap, item):
         return heapq.heapreplace(heap, item)
-import datetime
-def get_today_date():
-        return datetime.date.today()
