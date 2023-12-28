a3333fcf4db227455865e07a424c22d96542271a
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:26:02 +0000

    actually guilty upwards experiment famous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
