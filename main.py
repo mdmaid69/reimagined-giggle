4473381c8624a0d491b6b3c2ef64c533e1cc6cfe
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:50:44 +0000

    unfair limit nervous organize store

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
