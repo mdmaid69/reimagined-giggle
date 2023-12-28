aab2a7264ba3d246957592e084d3b73747fc29b0
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:30:43 +0000

    neat pause support length Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
