1e2b59f6402b92f195e6b5632fc35cd3a32be9af
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:50:00 +0000

    pull comeout drawing software trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_int(array):
+        return int(array[0])
