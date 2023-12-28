a25acf66d46438f040e392394237c727c30bf7c3
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:07:36 +0000

    important home community unit record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
