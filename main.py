b5956184d971f6a6f547eb72d2db7d9633808dee
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:44:28 +0000

    fair arrival offence hold people

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def get_array_as_set(array):
+        return set(array)
