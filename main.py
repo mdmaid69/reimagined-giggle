d65aba5f2161ec40540c2941f409fdefe29f6882
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:33:40 +0000

    control lie holdout your bottom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import random
+def roll_die():
+        return random.randint(1, 6)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
