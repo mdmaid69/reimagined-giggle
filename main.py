b10329fa10d22113a8f695611f8b484146b1c7c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:43:12 +0000

    suggestion recent fall mean if

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
