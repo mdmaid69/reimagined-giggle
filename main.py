f00e25abbf5c5c5af2d63213e5c29ba7ee59eaf4
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:25:14 +0000

    middle skin invitation education success

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
