dde108101d2c577a19f4fb13b0261265158d21b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:57:26 +0000

    everybody day argument too of

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import sys
+print(sys.version)
+import os
+def change_working_directory(path):
+        os.chdir(path)
