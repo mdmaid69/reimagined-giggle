cebf50ebc7cbb3e479524ed777299d9e68a2aa05
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:14:22 +0000

    off thankyou harm activity standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_array_length(array):
+        return len(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
