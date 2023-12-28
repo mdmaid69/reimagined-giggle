592664102818f0e62f1ac1166ff449e11b901acf
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:28:01 +0000

    571enough discussion later Ff brief

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
