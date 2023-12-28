3334f3779c6e557a9613dbd6494dff284452f4d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:42:46 +0000

    key really knowledge hard frightened

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
