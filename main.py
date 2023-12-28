59307c0949bbf2be3c03d41be0b07c7fc3f39e23
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:45:18 +0000

    Jj reduce capital experience fence

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  def square_number(x):
+        return x**2
