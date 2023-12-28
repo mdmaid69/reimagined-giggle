b34205a518530984c95191d5241dc1afc5379814
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:39:06 +0000

    either seat in minimum goout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def convert_to_octal(n):
+        return oct(n)
