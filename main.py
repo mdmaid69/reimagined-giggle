8c347053089ab243deb0c65f211694512f11a801
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:49:38 +0000

    immediately steer reply cook worried

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
