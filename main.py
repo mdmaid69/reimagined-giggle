5c198256d89e36e41150e476ac2a710d6df2bb57
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:12:58 +0000

    quiet comeback happen other whichever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
