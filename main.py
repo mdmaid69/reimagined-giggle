cd6f9aa54ea54c707c11d77364d2834c2e7ef741
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:34:22 +0000

    minute upstairs nigga sharp praise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
