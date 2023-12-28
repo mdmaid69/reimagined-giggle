7dd110341d1d8e0c7ac7f01c3c7f4262d07e06ba
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:31:05 +0000

    too tidy exact float speech

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
