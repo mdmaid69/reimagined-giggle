7fb8de1a7798d555d0d97bc1473ccbf17ec4336b
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:48:41 +0000

    escape stage piece spread theirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
