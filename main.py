0218089ec7e244e6618d7467cba1985674d9c183
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:41:55 +0000

    wrong tread whom behave point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_current_directory():
+        return os.getcwd()
