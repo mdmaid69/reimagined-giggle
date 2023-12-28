d0904ca9b86ef7b286bc59c3a17ebbe5aabb17b7
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:14:27 +0000

    accept so short polite card

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
