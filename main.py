6c86758745d5b6c7edb69e29b229f627ea36dab1
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:13:42 +0000

    detail contrast considering when repeat

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
