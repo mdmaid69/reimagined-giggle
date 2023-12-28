51fcf45aff51c1f78657278acc2942fc63383af5
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:58:15 +0000

    father dark even so glass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import datetime
+def get_today_date():
+        return datetime.date.today()
