5e523c6a60e00bff9ec1ad0d949888e2547ffc03
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:01:27 +0000

    no all odd calloff love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
