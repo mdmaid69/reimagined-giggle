28d029dd46e46179f682a8454851b5a822514a1f
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:27:13 +0000

    brief communicate forget marry show

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def subtract_numbers(x, y):
+        return x - y
