0e8298d2fdc46604a3053715b40c66ef8c9924a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:55:32 +0000

    interesting inquire fold turnup board

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_error_function(x):
+        return math.erf(x)
