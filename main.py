b1f91c5566b699053d44036a3b91fd1fd91ab2e1
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:25:59 +0000

    consist where break always cold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
