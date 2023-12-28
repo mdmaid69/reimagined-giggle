686bd54cac844699e9c11805694966696a50040e
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:53:35 +0000

    key congratulate learn luck band

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def square_number(x):
+        return x**2
