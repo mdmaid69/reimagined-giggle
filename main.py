526bb92c46e5d3c08908a036822f76711515ab64
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:36:59 +0000

    572enter rest interview recognize rush

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
