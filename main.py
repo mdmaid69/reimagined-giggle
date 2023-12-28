98021abb99f36c7996e6fa40a2e1f29428fb5768
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:25:10 +0000

    expensive nerve there their be

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def find_union(list1, list2):
+        return set(list1) | set(list2)
