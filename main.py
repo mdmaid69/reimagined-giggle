a3f4338c8b2695690fee2da056c630da7b6ed068
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:13:47 +0000

    measure breath comein process in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
