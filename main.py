ec44e0c0c021c75fe1baac985d647804df3e8b05
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:20:14 +0000

    fall bringdown deep mood during

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
