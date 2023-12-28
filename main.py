f7d486fd41dfb70337f9b6b28e3dc66ae6acb494
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:24:32 +0000

    me favour rely loud deep

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
