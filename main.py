3a7f24df9577d585ed2856a347f7638ea47d0213
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:26:07 +0000

    broad just roof bound getup

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
