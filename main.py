bbc01814492aeb796da79be77407312c5d337a2d
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:23:49 +0000

    cinema allow matter hold exercise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_error_function(x):
+        return math.erf(x)
