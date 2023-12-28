ab8beaa355ae2df689f87113c4ecc6dd23a74955
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:10:35 +0000

    interesting prefer bottom control help

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def count_characters(sentence):
+        return len(sentence)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
