13d2a22dab9e1fc85ead62e368bf049fdd351d9d
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:30:08 +0000

    wash tired favour blue laugh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def subtract_numbers(x, y):
-        return x - y
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+for i in range(5):
+        print(i)
