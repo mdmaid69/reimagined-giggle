05af152d8c42fa4c7d3557c9372c6d827a36dcd1
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:57:48 +0000

    level string sister able dish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
