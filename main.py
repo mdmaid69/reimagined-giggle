2217c0ec24eb1fdbe5cc962d3b0ae7cd9f6e2367
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:22:37 +0000

    holdon scale comedown branch brother

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def find_max(numbers):
+        return max(numbers)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
