8fbf49ff88a14aedf7260fbc2e2165155a6ad532
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:46:24 +0000

    extremely ticket under fit rather

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
