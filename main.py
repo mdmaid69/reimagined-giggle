bfd68133d3ae151283f6c48713049d105d2855f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:37:40 +0000

    forward difficult seat cum tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
