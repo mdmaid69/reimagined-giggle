59e8b39760873667761daf0759d08350dc3e5a6f
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:01:13 +0000

    visit twist mixture comeover row

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
