0dfe7bd4f949674bc945c11dd6250f14bdb41f48
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:40:15 +0000

    prefer please touch successful name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
