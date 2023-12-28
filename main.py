167a1fa8300ee5a70a32ca02759b816b45b19193
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:44:18 +0000

    blame seem quality deliver top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
