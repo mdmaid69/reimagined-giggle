89009d6efc2e6f7b7eba0224db6eef74b67ae542
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:56:33 +0000

    march forward quit accept callat

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
