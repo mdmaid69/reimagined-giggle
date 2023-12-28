7e61c4a174725a676f57e7f75d4337f15cc88618
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:47:58 +0000

    note touch complete easy later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
