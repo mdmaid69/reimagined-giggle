07ccd2630e2628846fbb2dee4f83d871c148dd1c
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:20:02 +0000

    comeout ever lucky treatment occur

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def square_number(x):
+        return x**2
