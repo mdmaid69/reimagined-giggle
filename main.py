c5708c119bb1f954361dadc887b47b3596f4dde8
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:05:26 +0000

    circle rarely 569enjoy record near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
