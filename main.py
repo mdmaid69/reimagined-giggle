ac2a92f3b4f32932ad0054bc34d93e4c9cc7e594
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:38:26 +0000

    uncomfortable hospital performance suffer front

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
