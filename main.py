8b5b48b75ca3b053ae049f6298724aaaf3ae9992
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:35:11 +0000

    gate flight kind disapprove forget

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def cube_number(x):
-        return x**3
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_stack():
+        return collections.deque()
