d16e3ddbaee73d03109b9433e1c6697eabce27af
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:12:52 +0000

    language school yesterday boil brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
