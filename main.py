d5c169af45a4defecdaa7a8b72b0f40bd0377f5a
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:53:43 +0000

    front lay consist empty swing

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-def square_number(x):
-        return x**2
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def sort_list(lst):
+        return sorted(lst)
