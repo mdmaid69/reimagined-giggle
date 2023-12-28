463af666d87a0c1f977561acee837eb42aa862e8
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:35:59 +0000

    lock satisfactory block 568energy separate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
