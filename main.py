13b8030ad7e6397bae138e1fa668c0c87f3a9703
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:13:23 +0000

    cheque minute stop escape Jj

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
