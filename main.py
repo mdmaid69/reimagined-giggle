69de9f1f048757dce77b344ab23ef6c30100b9c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:19:24 +0000

    heart lead information reliable generous

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-import getpass
-def get_username():
-        return getpass.getuser()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
