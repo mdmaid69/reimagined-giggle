33891efcccc37541fa71f39aa351033d3d723fe6
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:26:25 +0000

    sorry listenfor practise please difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import collections
 def create_default_dict(default_type):
         return collections.defaultdict(default_type)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
