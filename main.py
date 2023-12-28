c714c55aee9fb1db9561d705b9638bc403392673
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:38:11 +0000

    worth hurt excuse organize I

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-print(time.time())
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def remove_duplicates(lst):
+        return list(set(lst))
+import datetime
+print(datetime.datetime.now())
