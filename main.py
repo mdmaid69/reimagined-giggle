8b1cd07654832cdc2bc452fef7af764aadabdd4e
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:21:48 +0000

    threaten yet string silent Bb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
