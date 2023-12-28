0f13fc1ee2e085b4ce5aa2f92bcf9cc6ef9fcebc
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:42:05 +0000

    war unlikely skin sick excuse

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
