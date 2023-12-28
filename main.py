a1bd11f8f191efc2ed5853c77d85b346934a1bba
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:36:42 +0000

    please know obvious television recently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
