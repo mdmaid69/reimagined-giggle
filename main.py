c81a112f994a66c6686ae5fc6c829769a81c64e0
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:16:51 +0000

    test bringforward meanwhile means suggestion

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
