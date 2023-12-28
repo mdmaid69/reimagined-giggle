0cc548132a12fd9b011953017fe8ff80236c4c87
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:31:44 +0000

    punishment dead kind early opposite

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
