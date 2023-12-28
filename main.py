bc91c8e72125335269dd770fa6b21fcf230b8765
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:57:27 +0000

    cry someone hair praise force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
