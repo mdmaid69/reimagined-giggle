cb47aa915ce3d410e49da1694bcf9aa88852b2c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:15:50 +0000

    mention rid label brush eager

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def add_numbers(x, y):
-        return x + y
+import re
+print(re.match("h.*o", "hello world"))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
