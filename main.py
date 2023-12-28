672f806a4d1d8e366a41a7f475022488c3c790f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:06:45 +0000

    present skill ball party Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
