420c5876ff83d6768982e1e23ea58e71f3bcad76
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:05:44 +0000

    getoff matter nor control bringforward

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_octal(n):
-        return oct(n)
+def count_elements(lst):
+        return len(lst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
