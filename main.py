bb3a5038f81cf812e504f6b860e3b22c9e0d1722
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:36:08 +0000

    law last bit under question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
