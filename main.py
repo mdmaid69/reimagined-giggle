4335d9a3cc62e8e0bf7f8aea5d6e3578fa7d506e
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:36:27 +0000

    consider become face switch ball

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
