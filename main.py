c22c62dd99152952916c9395b4fb6926ae1087be
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:47:46 +0000

    medium cold police petrol worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
