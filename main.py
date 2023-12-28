3c5b15412e5be232d9479f111506aa06dd16d469
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:05:01 +0000

    best workout team part exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def set_array_item(array, i, item):
+        array[i] = item
