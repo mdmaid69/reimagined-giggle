fa6245678027c65cd69e709064c85a76b4c1caf3
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:31:40 +0000

    workupto reaction strike surprising path

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
