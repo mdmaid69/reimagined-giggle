28768abc3bb6c1387f58bf1f20aa2f4f253b561a
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:02:08 +0000

    turnaway that lift hungry will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_min(lst):
+        return min(lst)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
