3c99bdd6c582e1028a139f170876490f15c3e039
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:12:07 +0000

    now worth niggaacademy constantly average

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
