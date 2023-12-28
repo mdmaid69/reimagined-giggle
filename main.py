26a7f8d40b5291b952b7fd47ff312e7b496dfbc7
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:12:48 +0000

    innocent care life yes hide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
