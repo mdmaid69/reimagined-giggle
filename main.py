cfc997494baaba6962335e67fdf008774ba15581
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:19:02 +0000

    matter software view throughout from

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  def is_prime(n):
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
+                if n % i == 0:
+                return False
         return True
