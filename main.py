a73afb4b465ebe89f3838aadbe54238ef30d4a40
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:29:22 +0000

    union treat tap fight in

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
