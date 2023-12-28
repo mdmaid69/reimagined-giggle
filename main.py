28e42337c76913eff8bb4446fe84d31fbc0ebf31
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:04:29 +0000

    Ee nurse risk ground 569enjoy

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def remove_duplicates(lst):
+        return list(set(lst))
