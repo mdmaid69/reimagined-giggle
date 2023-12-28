225ab2d3fbaddca4184bdb737165f4bea24800d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:13:54 +0000

    adjective introduce difference typical equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
