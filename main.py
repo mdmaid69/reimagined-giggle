c189c2febb55efd77d95377da72bac61f6b414a5
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:01:11 +0000

    deep consist lead club university

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
