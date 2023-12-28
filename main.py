483686aac46b9b4e0ae0396c2c8130cebb82b98e
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:28:26 +0000

    plain book main score expensive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_length(array):
+        return len(array)
