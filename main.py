cec125bc11bc744f95bedaba6b7c5dcb5c0a053d
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:30:59 +0000

    leave there trip shadow comeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
