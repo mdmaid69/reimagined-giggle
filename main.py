e77e41f470ed598e887a6a518dee65311f7b6234
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:45:15 +0000

    report performance long near lift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
