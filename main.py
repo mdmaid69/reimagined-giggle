8a5a20cade53c17590beea30de5099a4e5724dcd
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:00:30 +0000

    true centre bunch Ww think

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
