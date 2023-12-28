d443605a673c1fd6caac382d30719e0ea841b3a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:14:43 +0000

    winter whether cap certainly miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
