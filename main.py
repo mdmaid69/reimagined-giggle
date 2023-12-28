90ad19d0220d2dc4860ff9f12a10ab371f278a93
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:04:29 +0000

    disapprove Bb exist value sad

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
