a106fcdc857d55cdc3b95370ec32295bf8d29434
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:24:23 +0000

    heat fold tradition defend announce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
