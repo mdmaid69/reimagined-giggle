17cd26ec9fd7d0340cb42895be1eec3b36808183
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:07:26 +0000

    avoid splash month tradition flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def convert_to_octal(n):
+        return oct(n)
