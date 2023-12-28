aead4d1b77e7ce86d4481d107f2ee76b1dd9904f
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:49:10 +0000

    ordinary seat exhibition later attitude

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
