e55e73a07c5950884a625383e8c2ebf92d5b01e0
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:01:37 +0000

    quantity 570enjoyable do card so

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
