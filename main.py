741bd2f6209bfe8f714680630c78aab352d0df48
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:04:38 +0000

    important in amount question Itself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_volume(length, width, height):
+        return length * width * height
