03d6a3feffba67fde187d583d47b14c61e35240f
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:15:57 +0000

    rich exhibition square fortunate chase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
