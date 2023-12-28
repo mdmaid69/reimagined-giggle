ebc43b5e8e645982b738e535fb0b048deaf14d37
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:31:17 +0000

    promise plan half runinto defeat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_density(mass, volume):
+        return mass / volume
+import json
+print(json.dumps({"name": "John", "age": 30}))
