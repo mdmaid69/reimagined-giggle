705c3dcb2e30b8c7b5788e0a103991705f06e3a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:33:20 +0000

    she rather order responsible connect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
