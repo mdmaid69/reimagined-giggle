1d3b89715953cdca83eb88d9d5eddf8adbeea0bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:26:16 +0000

    muscle otherwise father risk swing

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
