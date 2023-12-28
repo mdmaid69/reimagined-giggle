b9b5038813a6e02089992d5f2e7f148c01cd2fdb
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:56:19 +0000

    blind statement regret spring freeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
