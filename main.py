9ec8b31f4848985fbf052af2eb65968f2c18afbe
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:51:31 +0000

    lookup hurt most split valuable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
