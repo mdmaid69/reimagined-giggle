8e56dbb2d52b0d27ca2cc205c9773a02b18d1023
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:16:43 +0000

    understand long amount platform break

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_speed(distance, time):
-        return distance / time
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
