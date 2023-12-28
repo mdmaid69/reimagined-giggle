1cc94da7a5d6f0862f586809cf543b49b196c1b6
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:19:42 +0000

    care beyond bye whistle excuse

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
