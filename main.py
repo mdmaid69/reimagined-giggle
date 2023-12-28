63eab4bd68ca159d5e6a51862cdff7384c938b22
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:56:00 +0000

    attempt begin control bet involve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
