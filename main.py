8bd7c3c3184032a95ff0074b86395c4076467747
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:21:43 +0000

    modern form service equipment question

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
