229a16e9f3afe7a50ba22c0ae87a5b8f60b9e151
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:39:46 +0000

    adjective drawing willing female announce

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
