d966d6851775ea26450a280d788bb443eccaae48
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:19:56 +0000

    different public loose bang consist

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def convert_array_to_list(array):
+        return array.tolist()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
