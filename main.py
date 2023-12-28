d30dc7db0f4c8f76eae5fe30401c2adb3f9837e2
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:31:44 +0000

    anything put earth square person

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
