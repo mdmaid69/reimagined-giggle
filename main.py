4855f6ab0a09ebda444781fe8b0f52f53761e00f
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:32:17 +0000

    early holdon nowhere undo note

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
