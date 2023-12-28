be6391f1858bc4507a2e393fd466487900d83e4b
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:44:56 +0000

    relation crash extraordinary greeting main

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def multiply_numbers(x, y):
+        return x * y
