bebeec09f92077d07fd8c9c6ccc2dedca4f95630
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:02:10 +0000

    insist rich culture reduce voice

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(lst):
-        return min(lst)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
