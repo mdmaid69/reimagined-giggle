f0df30edf7c995f59e32a29f9e6ef85d43e08a95
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:15:05 +0000

    escape breakfast operate estimate classroom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
