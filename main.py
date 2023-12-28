8c80de2a6ea6a24f248e9eab3f6faf3c2ff53d6e
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:13:26 +0000

    make pause urgent suit across

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
