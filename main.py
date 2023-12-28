fe0962da8585b3049223fdde8e1b22b81c8cc100
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:44:19 +0000

    dead cheek room trouble his

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
