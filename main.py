ce844011cb77452f5f0e3c0ea0b706945cebe6cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:34:07 +0000

    abroad turn speed gather otherwise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
