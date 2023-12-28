09b5a94cec2fb63c56ab316e5f2109e4a7e04ee5
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:30:31 +0000

    back print 571enough reliable clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
