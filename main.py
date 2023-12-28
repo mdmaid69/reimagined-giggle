c5bcc2a346c3f74439fa3962c8f8c7e38e339303
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:28:27 +0000

    total feeling before half bend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
