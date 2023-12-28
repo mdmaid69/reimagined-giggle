9f6c1e210e615f038809f81b9fef6ae42c6685b0
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:58:59 +0000

    arrangement recovery agreement through ear

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
