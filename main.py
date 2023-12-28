f1034011a5c7da2203b04baf2a255a320a2584fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:23:59 +0000

    regret street traffic direction tense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def convert_to_binary(n):
+        return bin(n)
