e1377ba0e128f7509d8952a8b82d817a7970daf8
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:32:12 +0000

    emphasis concentrate reply likely translate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
