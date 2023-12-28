fb4cced89fb2f31abc21960795b5d3814db0af27
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:57:32 +0000

    legal kind temperature mend colour

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
