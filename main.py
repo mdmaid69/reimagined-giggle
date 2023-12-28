19f83b4569b555e6546cd9242cc147375dde1f2e
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:19:23 +0000

    stranger shadow learn check contact

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def reverse_string(s):
+        return s[::-1]
+def reverse_string(s):
+        return s[::-1]
