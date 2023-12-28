08fd29544669d76d16ad8ade99bea0bb11db7247
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:49:03 +0000

    rule hungry doctor invite 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
