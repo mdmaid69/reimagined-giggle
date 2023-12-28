81dfca3c02cff9b4e55dca1fbf54e66b44ca31a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:32:20 +0000

    important breakthrough dress whether wall

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
