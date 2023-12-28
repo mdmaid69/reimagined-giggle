55d4bca94c2fb923161ad5dee5cb5146520cd01a
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:17:44 +0000

    closed smile tone surprise group

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
