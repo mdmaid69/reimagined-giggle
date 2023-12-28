2957b65433f3de794f7897086139e613fb8e6d9a
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:57:10 +0000

    point left command town respect

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def count_characters(sentence):
-        return len(sentence)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
