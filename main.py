c370143e53a849ee21c387ae70e44c6c6cf07e2b
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:56:24 +0000

    although care sound recognize disagree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
