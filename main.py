0fd00eb3fff72d4411b2b89245ffa0a5b8ab843e
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:18:19 +0000

    range exactly pride world group

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
