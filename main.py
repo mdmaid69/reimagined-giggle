64972bd0180633ebb855c3689597f5f385b78ed6
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:07:33 +0000

    eye best go hurt recovery

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def find_max(lst):
+        return max(lst)
