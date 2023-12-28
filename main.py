3dcef17a99060ae4edb50b672d155200cc98b429
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:48:12 +0000

    challenge turninto must turn several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
