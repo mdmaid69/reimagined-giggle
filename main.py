a64f14fc05fcc470a70f9227cdc7d489d1ae6d8c
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:57:14 +0000

    suppose cheat exist splash border

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
