dc3a3896a21445c3f71f8ff5fc1c424f7549e6bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:59:12 +0000

    immediately top slide present slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
+import array
+def clear_array(array):
+        array *= 0
