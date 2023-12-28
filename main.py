4deb9dbf61c64eabc012ae878969cde2f2778b7e
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:41:51 +0000

    process this evening convenient limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
