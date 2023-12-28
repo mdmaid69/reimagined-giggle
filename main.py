b3bb8750a283bdbb6e298dc5d66378bbd60e746c
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:43:12 +0000

    such poor produce insist film

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
