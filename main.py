85af09bc8c565fce01f06b703c5eeb30cc0ec6b9
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:27:50 +0000

    active niggaacademy sell me injury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def find_max(lst):
+        return max(lst)
