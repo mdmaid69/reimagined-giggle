54c714ddbb849361aaf1c60f442d2805fd02db06
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:54:50 +0000

    card involve flat want reliable

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def find_max(lst):
+        return max(lst)
+def cube_number(x):
+        return x**3
