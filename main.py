e83567d400d20f1eb727568964af16590d422fb7
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:42:37 +0000

    consult rich price talk height

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def reverse_array(array):
+        array.reverse()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
