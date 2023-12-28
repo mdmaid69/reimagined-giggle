cb99a19dfc1de6d96dddd4bb4efe0589851b3630
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:54:48 +0000

    slide suppose moment pin prevent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
