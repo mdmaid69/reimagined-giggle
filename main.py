9c3d74e3a9b58be6ec89801077b3721915cf6f07
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:25:47 +0000

    neither copy since sleep television

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
