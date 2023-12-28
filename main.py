3a83ae64330283d33bfda957276e02383c1987b3
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:47:54 +0000

    creature definite black friend bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
