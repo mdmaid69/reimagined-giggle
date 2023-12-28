6649e94cd0dcf85444ffd422d56e5f5556622e11
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:28:28 +0000

    reduce week rid she big

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
