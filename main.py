641fcec8bb6c4fc8e10f8d1537bf37b2baa42308
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:52:21 +0000

    collect suggestion contain copy explain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def find_unique_words(sentence):
+        return set(sentence.split())
