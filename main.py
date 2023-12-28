19600778f64eb6bdd36819b9a513eaa9338de1c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:38:03 +0000

    news rain region injury measure

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import shutil
 def copy_file(src, dst):
         shutil.copy(src, dst)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
