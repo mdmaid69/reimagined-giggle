da96c08448aa0e095a056061edd5e9fe6dc84ef0
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:05:35 +0000

    convenient several hole go smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
