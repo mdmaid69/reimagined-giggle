f7a9504a2f3301478901339400d7df702b83f436
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:08:00 +0000

    discovery often provide hot where

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_floor(x):
+        return math.floor(x)
