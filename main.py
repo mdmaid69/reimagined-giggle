ff633ed9f8ba9583c410cd87ff08388892f212b8
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:19:39 +0000

    schedule organization show about only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
