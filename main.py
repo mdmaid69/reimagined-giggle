8f20cbe65e7a5e18bbf000af1d6beac72871d1cb
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:51:12 +0000

    climb club Qq exciting nose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(x, y):
+        return x + y
