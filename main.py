f613af1be3e873f863bf5f8b3d20af985ffb425a
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:54:24 +0000

    create once blame below cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
