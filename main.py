1c2869eae6a67e4dcd1662a49b1715feaf12fda7
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:18:38 +0000

    father definite protection children involve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+print(sum(range(10)))
