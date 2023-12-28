a8e457e7388838f38bce6ec05a0c5b04aa4f81ef
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:39:08 +0000

    out live programme cover receive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
