4370c3cafc65d0a9e69e79ec40b942004733657b
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:13:49 +0000

    basis normal young proud near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
