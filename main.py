65eef5c0241690324decd493f6a85e835b18a681
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:28:14 +0000

    walk flow birth tense welcome

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
