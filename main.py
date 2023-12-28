b761ccec0b2082dc65069a10c2a68f9fcd074b44
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:47:56 +0000

    leg spin again want equal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def reverse_string(s):
-        return s[::-1]
+def create_user_dict():
+        return collections.UserDict()
