596ad33d47eba247fb3b33a60d5532e15cbf3d70
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:52:41 +0000

    fall 569enjoy contrast repeat choose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_counter():
-        return collections.Counter()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def append_to_array(array, item):
+        array.append(item)
