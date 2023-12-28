9ed28a9efe43210c16106b914fd849f6e12b858c
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:35:41 +0000

    squash hurt comedown swing weak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
