e541eb55a5696e0119abcb95b708b7e5f77858d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:39:23 +0000

    work flat objection rescue aware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import sys
-  def get_python_version():
-        return sys.version
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
