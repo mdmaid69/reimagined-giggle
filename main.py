35e849a02147ad338b6157e7f360dd95463b0715
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:39:00 +0000

    fine wash commit accordingto quit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
