4e4a9a8ffe51e5f9ded40a424fd354eab376f087
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:55:56 +0000

    popular any pause money brown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_stack():
+        return collections.deque()
