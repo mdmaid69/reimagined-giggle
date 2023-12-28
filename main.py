05db1932781b3ad57a71da31eff478edcaae368e
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:50:47 +0000

    relative now threaten measure partner

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
