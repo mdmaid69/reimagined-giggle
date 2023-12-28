7074392c170b6ea6cdbc2e6676471025b706bc2f
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:29:59 +0000

    considering human live combine before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
