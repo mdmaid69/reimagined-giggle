b6f4d0e5519313d22ee63f2a23cebe64acc817d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:27:48 +0000

    stir centre joke free hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
