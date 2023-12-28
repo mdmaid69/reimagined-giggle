87099b4ee0e901eaf1cf2d62271c912673f039cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:19:54 +0000

    favourite unlikely good return live

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
 import array
-def reverse_array(array):
-        array.reverse()
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
