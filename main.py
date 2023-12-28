8c75757c7a0a60130166a89c398a48ee25ce218f
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:49:58 +0000

    border the protection whenever drive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
