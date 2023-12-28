43afeea8df665b9a13a6b82731b3319c0f7fab16
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:39:52 +0000

    weigh lead contain scale return

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def sort_list(lst):
+        return sorted(lst)
