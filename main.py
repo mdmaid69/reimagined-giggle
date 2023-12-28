e08cac552097b2810e4a3642f268cc76b521c051
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:10:39 +0000

    size Vv basically themselves crisis

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def split_path(path):
+        return os.path.split(path)
