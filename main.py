3e4f555cfb35275046e545ee70fe51533dc09dfd
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:47:03 +0000

    sport he journey set underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
