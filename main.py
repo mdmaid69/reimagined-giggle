0d28b4f19355f2152476638ed60da5ff03bf8ffc
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:05:50 +0000

    consider damage building major though

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
