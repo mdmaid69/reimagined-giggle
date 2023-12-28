326660538fc975e9c415a479be12f1643c0418ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:19:21 +0000

    occasion branch set noisy hungry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_str(array):
+        return str(array)
+def find_max(numbers):
+        return max(numbers)
