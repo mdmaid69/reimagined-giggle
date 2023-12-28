6e4a4c7ffb1cd8c52bda4a7cc0cab5547c2a8cb7
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:07:50 +0000

    side once knock sleep shut

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
