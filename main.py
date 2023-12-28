63140defd7a02d29e8ec33ca2a4d56a845f07e95
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:09:32 +0000

    crisis skill support gradually supply

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def set_array_item(array, i, item):
+        array[i] = item
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
