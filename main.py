8097e22f01925b9c076604023747ceeea85cd6cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:31:32 +0000

    comeround future life unkind burn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
