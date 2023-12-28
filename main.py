4dad04b56ef6332beae1ec69b8212fc306b06a70
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:15:40 +0000

    exact callup dare fly trust

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
