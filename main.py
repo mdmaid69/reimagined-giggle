217c6b11558e0cd1f379c0d532a44616697558b3
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:29:59 +0000

    everywhere case squeeze little with

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
