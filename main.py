0003bedf38b50b8c02702b9287bd0ad385fd4a22
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:36:59 +0000

    brown describe settle unite modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
