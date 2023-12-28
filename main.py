64aefe3fe13676e844bebf4d10b10ef9f9908551
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:49:26 +0000

    hope yes friendly minute earth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
