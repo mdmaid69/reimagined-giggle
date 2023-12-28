986fddf6d44db8220e1a07d054041020ff3d2913
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:57:16 +0000

    drawing teach object total left

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
