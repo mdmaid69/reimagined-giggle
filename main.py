23a3c3d58676a73aaf33d3d84641c093e2c3d98a
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:14:16 +0000

    bath innocent declare active quickly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
