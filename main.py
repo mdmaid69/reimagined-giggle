549b1c4b39e6c1e76412ad71bc28cc95dcd6acee
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:04:35 +0000

    declaration most comment born wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import random
-print(random.randint(0, 100))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
