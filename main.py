471f2f797d03d4c284f633cb6ad5dfd864833834
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:27:19 +0000

    generally university fond delight fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
