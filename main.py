48f9e3261a9b3752418170c50adc2a0371500071
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:11:11 +0000

    other call avoid give life

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def clear_array(array):
+        array *= 0
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
