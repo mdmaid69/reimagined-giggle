8c6623ee0fe2d74ab25037bdc7b1cb920e24fb2d
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:54:33 +0000

    dig stamp why precise understand

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
