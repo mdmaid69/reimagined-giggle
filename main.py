4c1e8e345bacde82c36f42ef044d9081cef1e795
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:22:06 +0000

    study dream wake tight suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
