5a889fbb19b5f2e98fd92da33c9d3d1b81c609fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:50:33 +0000

    tragedy show increase politics breakin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
