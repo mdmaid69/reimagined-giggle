51e92556a71e4477e15d949e573303068fde991f
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:14:39 +0000

    use absolutely importance juice consist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
