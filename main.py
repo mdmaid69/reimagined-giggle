072824c8ed1858117b2969c57cff68827f712c4a
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:29:57 +0000

    neither track wide official closed

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-print("Hello, world!")
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import re
+print(re.match("h.*o", "hello world"))
