7d62ba880906079e2cc204765b35b1a15a96fb3c
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:35:09 +0000

    run port no pin any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
