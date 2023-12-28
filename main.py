a07bc49f39936303bb914944efc5a10795fa68e1
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:14:56 +0000

    whatever young door dick season

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
