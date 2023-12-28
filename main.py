ca731ca6b727697ae61e8513b3740f03ae6b1571
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:43:03 +0000

    go combination tip excuse Rr

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
