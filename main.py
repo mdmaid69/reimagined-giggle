fa3a355ad825a238396fb95f04cb6708e3dd00aa
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:13:42 +0000

    operation good difficult classroom design

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
