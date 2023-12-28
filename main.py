42163f9805b942acfcb94d98c16eaae855ce6931
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:57:50 +0000

    567enemy yesterday meeting success pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
