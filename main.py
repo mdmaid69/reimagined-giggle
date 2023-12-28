8fe3286d77f9418259c0e653e9ec6cdfa0b5de10
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:39:09 +0000

    education zero insist reason improvement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
