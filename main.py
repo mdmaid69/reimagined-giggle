ff04c6648bd8a8c2a569f619a56860d1d4de3f6c
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:44:02 +0000

    patient breath music study taste

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
