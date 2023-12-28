3e4d8b9d01b657d47ec3fda3a3e733e4e95860c1
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:10:57 +0000

    usually Oo blame when move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
