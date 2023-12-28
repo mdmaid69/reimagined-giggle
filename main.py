a2418035a4811ced480c410edb4c928d653d8a6f
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:41:58 +0000

    night blame breakinto by change

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
