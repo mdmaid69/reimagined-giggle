57ba96aa5f05a3726e2bdc3cdb0c24dfcd225acb
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:27:47 +0000

    about shake Oo comedown brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
