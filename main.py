23d0c602eb3764a395ba4fa94eacbda47a79b633
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:35:39 +0000

    at company task protest ought

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
