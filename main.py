5567e87dd708c9032a6c897f09eabf1dd9051e9e
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:32:59 +0000

    blue crisis minor slice ground

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def sort_numbers(numbers):
-        return sorted(numbers)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
