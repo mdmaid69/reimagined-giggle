7a6a45b7f9b9959082da3d31762bd7cafef6c65b
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:34:23 +0000

    tread disagree spoil single split

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
