724eb57b98edbc60dbd9a1716bff4fcbcd5388f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:44:15 +0000

    566end top dress blow middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
