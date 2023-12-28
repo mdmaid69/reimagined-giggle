abda0b84fd2921c69f977088e3c21b220a944273
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:42:17 +0000

    meanwhile make Bb lead recognize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_as_repr(array):
+        return repr(array)
