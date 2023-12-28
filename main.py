16866b6990e8ef7376fd9d1d56293587dfac74a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:06:58 +0000

    where anyone 566end provide last

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
