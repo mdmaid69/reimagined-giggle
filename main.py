5354daaa2170c9e6eab48667389c4a64104850cb
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:47:24 +0000

    interfere phone dangerous leave entry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def square_number(x):
-        return x**2
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
