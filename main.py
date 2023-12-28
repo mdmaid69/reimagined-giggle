b1624bdb600caeb72ca9a45cc56f7228aca59cf0
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:52:18 +0000

    risk leg appeal heat loan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import getpass
+def get_username():
+        return getpass.getuser()
