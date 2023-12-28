ee137acd200eea19451e4163d89afd4e41af073e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:44:02 +0000

    go stare custom build takeout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import time
-print(time.time())
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
