fd513d8e6ba5f3feb52c315e3462b06b1fa2e499
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:50:48 +0000

    many minimum stretch yourself perfect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
