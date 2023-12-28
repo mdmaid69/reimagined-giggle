1dcc9e60f7c009660ea6a3335a207848eec6eb53
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:39:47 +0000

    bread object geton count urgent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_item_count(array, item):
+        return array.count(item)
