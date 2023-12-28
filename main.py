ed4e55f9a996fd41e8bcb103392a2e063569e80a
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:17:03 +0000

    be bright start pool air

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+print(sum(range(10)))
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
