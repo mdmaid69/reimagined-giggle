42de8f199019d26b7e7f62db83d2b22a9473fcf5
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:00:52 +0000

    murder splash scene whatever discover

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
