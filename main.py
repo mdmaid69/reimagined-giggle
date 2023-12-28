feb225df85c70e58892a095c7b56aa15d26749d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:03:45 +0000

    bang split beneath once artificial

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_power(work, time):
+        return work / time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
