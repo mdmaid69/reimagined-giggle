f812e055e94b0c0ad6e55462b557dfc16d499aea
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:15:55 +0000

    Cc determined shelter abroad confident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
