95b3d8eeb84d7857f14e152fc792d81f2c87bee9
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:06:57 +0000

    story me interested vanish help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
   def delete_file(file_name):
         os.remove(file_name)
