8f999b6811019882f5449052cfd0041db93ab79f
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:36:33 +0000

    callback high stay think trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
