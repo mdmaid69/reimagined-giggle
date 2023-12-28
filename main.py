5095ed5a7fc5712fc73aede41af016694fb0eb75
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:17:28 +0000

    reaction now sure ignore touch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
