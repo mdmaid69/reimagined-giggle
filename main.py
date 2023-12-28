953f95f8d02e7ddc81d9826082b09c918c6d2308
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:13:58 +0000

    Kk rise spring neither form

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def convert_to_hex(n):
+        return hex(n)
