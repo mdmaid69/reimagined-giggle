36f26ec5585ec93a6718514313aa4c264099c0c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:52:56 +0000

    measure sudden cool ground passage

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
