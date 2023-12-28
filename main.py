642f5d4325d38069791146e7743cfba1be6a95f6
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:58:22 +0000

    design succeed row while unfair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
