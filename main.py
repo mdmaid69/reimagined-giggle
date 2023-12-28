f86812b87367976045665a408100bb2e7b20be2b
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:12:54 +0000

    interruption authority disapprove term first

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def subtract_numbers(x, y):
+        return x - y
