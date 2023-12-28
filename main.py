626123e693112bea555d0a12934d81a59d25ac38
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:47:30 +0000

    figure luck finally bread occasional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
