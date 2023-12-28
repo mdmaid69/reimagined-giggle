0b4c60255fb2fee7148d2246849173c144d4fd67
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:37:59 +0000

    oil raw light hardly smoke

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def convert_to_octal(n):
+        return oct(n)
