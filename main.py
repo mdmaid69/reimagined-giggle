5196a8f0b537d3ca22582d935a101602c5ef9f5a
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:32:17 +0000

    majority many chase book win

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import platform
-def get_os_info():
-        return platform.uname()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
