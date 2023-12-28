0df9c49e3f0ebe4c8757ea10109ef652d05cd721
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:30:14 +0000

    final plastic thanks ocean that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def convert_to_octal(n):
-        return oct(n)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
