a2ce5f4f2d6ec04f0ed3f565a0b0f6fea66a7975
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:49:51 +0000

    black behind finish they company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
