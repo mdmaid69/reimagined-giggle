0be1e205ed95291d51cae047f7eb332c56392635
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:43:39 +0000

    frighten fortunately ask back used

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_area(radius):
+        return 3.14 * radius * radius
