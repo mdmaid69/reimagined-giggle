6773c5a7f3a6d0b33c6bb608e6fb54fd38fd2958
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:53:53 +0000

    light take towards contrast chin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
