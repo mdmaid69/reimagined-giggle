5da374a3ccc9255cbfacdca05252c42ef822fad0
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:36:58 +0000

    any hard callback 569enjoy length

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
