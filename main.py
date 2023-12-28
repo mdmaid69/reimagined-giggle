c2a2ba1235d37f97c889a09e1efefa2b74f0dd08
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:08:01 +0000

    doubt pressure design different anywayanyhow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
