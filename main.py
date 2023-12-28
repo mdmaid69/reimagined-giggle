beea8cb61282bfc0ed38fc6fb0efa28a32716379
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:32:13 +0000

    forgive different reply switch 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
