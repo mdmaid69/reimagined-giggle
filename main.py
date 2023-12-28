66e9478c5ef9d9532a8882e3e58953453f105a44
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:45:36 +0000

    star transparent communication whoever government

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
