701dc548ae13daaca6b96c402eeb347eb1dc855d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:47:16 +0000

    standard but beginning communicate suddenly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
