5e4109c62e33f4a8540da51ad7d14e8747bc775b
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:00:19 +0000

    generally love turninto minimum reserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
