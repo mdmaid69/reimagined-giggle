e64c25cd91cf07b26c791921f6b9cb7dd8103f79
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:52:14 +0000

    full target progress pleasant medium

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def reverse_string(s):
+        return s[::-1]
+  def is_odd(n):
+        return n % 2 != 0
