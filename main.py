4c043c893f19597db0bc666240083eb9353623a6
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:05:00 +0000

    accidental condition close accidental ordinary

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
