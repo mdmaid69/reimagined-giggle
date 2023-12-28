9128ceecf61c66a0e80ec7f43ab270009f4472d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:34:06 +0000

    strike hurt name deserve can

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
