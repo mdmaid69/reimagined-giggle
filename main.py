834618a57084ed45e5d57303b59010a0610b1678
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:01:06 +0000

    about once money drop again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+print(math.pi)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
