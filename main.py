33062cb5e673cca4f89981748802fc4d285d3d3e
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:26:54 +0000

    related plenty crisis pipe quickly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def reverse_string(s):
-        return s[::-1]
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import re
+print(re.match("h.*o", "hello world"))
