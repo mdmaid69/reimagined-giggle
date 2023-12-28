191ff52da17cabd1f6d9431f6fffd619cc81ab3a
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:35:16 +0000

    stone suit here young but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import sys
+def exit_program():
+        sys.exit()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
