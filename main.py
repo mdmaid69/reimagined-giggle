40ac33befada0578a5244d329e57ebe23490c471
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:52:04 +0000

    I gradually float note naturally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def clear_array(array):
+        array *= 0
