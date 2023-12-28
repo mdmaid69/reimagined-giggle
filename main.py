fb834821becc1c5605119ea71f70e03c5bc4c8bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:06:09 +0000

    eye new plastic lend such

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+print("Sum:", sum(numbers))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
