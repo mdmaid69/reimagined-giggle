ce2f699de4941228e0577a851ed554b5d8787dd6
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:02:24 +0000

    prove far cough computer related

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def is_even(n):
+        return n % 2 == 0
