327d79e0aeb0a793330b641d0dfa91598d80e008
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:36:31 +0000

    space 572enter light long matter

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
