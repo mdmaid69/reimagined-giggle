025cd9c0a9b4fcf3c05089f15162e8a991480f4c
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:51:37 +0000

    charge wide glass school fence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
