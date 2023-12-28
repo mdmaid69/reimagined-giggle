f7ba87f3d489b7edc55b98efe389e32bf9f6ce54
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:09:12 +0000

    somewhere beat nice level learn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_hex(n):
+        return hex(n)
