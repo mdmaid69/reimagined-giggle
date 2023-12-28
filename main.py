a153039d7dee7337f09cdd195c0fdb23057b08f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:28:09 +0000

    permission branch home order tour

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
