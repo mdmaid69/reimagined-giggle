fc7af15595ae23d138f8b1b9444dc23eed3a8bca
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:04:48 +0000

    whichever cum innocent hospital anxious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def find_min(lst):
+        return min(lst)
