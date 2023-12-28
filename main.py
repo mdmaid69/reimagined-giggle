1a23fa07081e786a6f96f4d7e4ad1fa688987612
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:59:17 +0000

    permission once tonight kind talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
