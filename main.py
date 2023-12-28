9d5a3a82c01341ae31bd1717ebbe67e0fc531520
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:26:10 +0000

    rush union so skirt relax

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def find_max(numbers):
+        return max(numbers)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
