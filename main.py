e08f5007cc3b2ed9a39c910b5ba89e8f16a01002
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:54:38 +0000

    letter proper charge we ready

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
