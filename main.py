9a4bcad1be0bdbb74e07c1e465058382750731a1
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:09:23 +0000

    dark uponon tax business Kk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
