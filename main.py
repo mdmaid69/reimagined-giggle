22e203d166bc77b8324aa3c83fe0d39154f996e4
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:54:49 +0000

    urgent tool long base among

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def delete_file(file_name):
+        os.remove(file_name)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
