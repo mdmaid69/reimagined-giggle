1dbcdf04d7fca043635b024f93e6e8d8598fc0f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:40:11 +0000

    apply 566end answer trust because

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
