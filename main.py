32897e94b61ab003724ba4f049f84b397fb57283
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:56:00 +0000

    splash somehow number the hungry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
