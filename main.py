806a8dde3bbcae3ddc9781fb3951f1a3c09796a6
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:38:51 +0000

    post copy direction weigh turnout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def square_number(x):
+        return x**2
