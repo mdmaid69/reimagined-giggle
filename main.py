05ed887d299e3c5efe464e00b0fbf9e9c44fc21b
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:49:29 +0000

    fall gap office matter wet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_current_directory():
+        return os.getcwd()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
