139968c16d03e607bc69d40caae0d19ef4206c6b
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:34:56 +0000

    afterwards forget course agree able

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  def is_odd(n):
+        return n % 2 != 0
