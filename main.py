f2a1587fc21f048ecf957501e5f73918689cb9e2
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:15:21 +0000

    afford duty or man dead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
