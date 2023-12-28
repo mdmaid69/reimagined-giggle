4f67613a8fb9a5b4f6e57f6019b5b21a8ce58321
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:30:35 +0000

    children share catch action hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def multiply_numbers(x, y):
+        return x * y
