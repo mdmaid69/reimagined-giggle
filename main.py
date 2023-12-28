6132b49af6bd897e3d728149f4d22e2d52d0f97f
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:32:23 +0000

    practice impression group before die

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_as_str(array):
-        return str(array)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_current_directory():
+        return os.getcwd()
