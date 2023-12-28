0d89cadc72b43a90290bea9556fc8ac0d7dfb981
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:40:34 +0000

    ordinary workupto who door twist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
