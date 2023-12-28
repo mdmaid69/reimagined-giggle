0edcc49a7467ec586ac966976eda7ec1ec4d948e
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:19:03 +0000

    dig stare crisis practice seem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def is_even(n):
-        return n % 2 == 0
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
