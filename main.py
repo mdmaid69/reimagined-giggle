770e130ce3aac236414abbaa4c3b042b708904a5
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:05:20 +0000

    freeze miss phone future practise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_length(array):
+        return len(array)
