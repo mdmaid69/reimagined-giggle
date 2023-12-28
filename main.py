9aa35d96bccf20dbe7dbfb74725ac2a49318b1a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:27:28 +0000

    against attempt year type independent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
