14b3c3df153a50a91bbbf01dce73cc96efbd9086
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:51:41 +0000

    cool destroy mend reduction makeinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
