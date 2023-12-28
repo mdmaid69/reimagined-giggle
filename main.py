14f038012a339e4f18c5f8d03cecc7d894ce1885
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:08:21 +0000

    honest across move instrument calmly

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import json
+print(json.dumps({"name": "John", "age": 30}))
