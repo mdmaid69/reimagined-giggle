1707d6b042945684299591cdf0660cb3d21e5103
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:53:25 +0000

    affect alive makeinto careful smile

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+print(list(itertools.permutations([1, 2, 3])))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
