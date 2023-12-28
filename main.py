288c59b651640f6c4305f33ca3d48140b80b3af6
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:28:46 +0000

    second imagine claim play arrive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
