9abbaeb91608c090e801500940fd6b4a00252261
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:57:24 +0000

    base night just they firm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def cube_number(x):
-        return x**3
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
