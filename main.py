f1708e40d28c522d71fb4f1c934143306ed45292
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:06:35 +0000

    bet tap convenient friendly explain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import sys
+  def get_python_version():
+        return sys.version
