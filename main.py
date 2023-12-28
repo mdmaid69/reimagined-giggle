2da03f16c3eea6269e8a19ddfc722b1555606ed5
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:12:53 +0000

    stamp card small various hate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
