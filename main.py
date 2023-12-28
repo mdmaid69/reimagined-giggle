d78c808774115d79a3774a0efb4ba6d1b18d23ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:03:36 +0000

    talkback embarrassed new minute emphasize

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
