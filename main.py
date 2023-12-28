cd93dc769900f59048889ef7c352ec6a796a9a90
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:02:10 +0000

    thought storm character quiet hang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+print(re.match("h.*o", "hello world"))
