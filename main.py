02e72283e4719419c47cd1469d4fa7434a770ac7
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:53:23 +0000

    runinto interview turndown minute glad

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
