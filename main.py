476364c757c9b00fdfd4f200955e1120b8cffb6f
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:03:33 +0000

    shelter identify blame taste fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import array
+def get_array_as_list(array):
+        return list(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
