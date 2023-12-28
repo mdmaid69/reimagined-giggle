80abe268e3a68a259d4f622795af8b2e8e7ecacf
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:07:55 +0000

    new light day however switch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
