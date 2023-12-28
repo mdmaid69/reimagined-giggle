b24fdfd90e4f68bbf6b78b54f960d340b7a7c668
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:21:38 +0000

    know smell proof bang kind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def remove_duplicates(lst):
+        return list(set(lst))
