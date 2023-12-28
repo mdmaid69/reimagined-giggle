b5e2debd0ea8bf78112c28df77cd7cac296bc11d
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:07:33 +0000

    apologizeapologise minimum Qq sea convincing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
