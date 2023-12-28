9e54db83255086b45810269ba1f800cac8a2fb84
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:13:41 +0000

    this crime sensible backwards surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
