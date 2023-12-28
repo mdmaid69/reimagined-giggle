a131e96df057201c993b0ae472bcc6cc51375bda
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:14:28 +0000

    Mm aim impressed Uu hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def is_odd(n):
+        return n % 2 != 0
