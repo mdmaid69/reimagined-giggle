6b0cf7626157363d3e7ab9f758fe0f2bf26783f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:36:28 +0000

    no print and port vision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
