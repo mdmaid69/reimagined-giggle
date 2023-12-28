3f53288a06f1db3fb27e519960711f1f9a04ac34
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:14:02 +0000

    behind dark paper clear time

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def get_array_as_int(array):
-        return int(array[0])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
