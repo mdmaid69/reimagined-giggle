8b9c58375cca4d69cf2be3a58eb846468603a0e7
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:48:27 +0000

    punish reach round plenty yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
