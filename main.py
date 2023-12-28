d567e51ed6f38e6a4fbcb5e639e8270e4533482c
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:55:02 +0000

    cheque reach ticket Oo deaf

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
