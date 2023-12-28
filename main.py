346d3e4131d0ba9a76cdae688a6d771c95dfe731
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:20:26 +0000

    passage frighten live in recent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def square_number(x):
+        return x**2
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
