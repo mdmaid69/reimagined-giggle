a1efcb234d656035c45c3c8d95c12396ea4508e1
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:04:21 +0000

    glass no guide lead Ff

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def reverse_list(lst):
-        return lst[::-1]
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
