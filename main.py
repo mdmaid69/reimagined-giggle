8157cabcc0848aae39407c7d8b155e9db7ce9ac3
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:14:17 +0000

    memory tap gooff equal many

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
