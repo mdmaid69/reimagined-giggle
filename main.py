c7133628339b74a173c2309eb6009e3f5b440de9
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:54:57 +0000

    see eye side ilovedicks emphasize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import os
+def get_current_working_directory():
+        return os.getcwd()
