602493cf8b30ac0cfb68a5d82b13ee869cc7b9e0
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:16:24 +0000

    cross strength accident acceptable inquire

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_acceleration(speed, time):
+        return speed / time
