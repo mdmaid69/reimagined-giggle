119009aa63abcffcb36db439085fd6033bea24a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:03:20 +0000

    taste makeinto pile communicate large

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
