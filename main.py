1decd2e1b5c39a4e590f1a49e77afa305d4ced41
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:41:47 +0000

    anywhere excuse set herself ask

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
