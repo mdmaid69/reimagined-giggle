05ba8b969294bca2c4f561482e083fa853518308
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:22:07 +0000

    consider to active whatever stick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
