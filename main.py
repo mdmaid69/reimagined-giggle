0fd74718b77a34e2bca9e57f58df13105bc97e89
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:51:20 +0000

    from yourself exactly cruel opinion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
