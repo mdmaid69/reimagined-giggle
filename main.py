5ee75e9e8d7aef2d641653359b3c82d99d869636
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:07:56 +0000

    thing ball soon pipe challenge

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import array
 def get_array_as_int(array):
         return int(array[0])
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_as_str(array):
+        return str(array)
