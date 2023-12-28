1c75cbb84c738a5b9504558a782de369cccd0ef4
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:54:44 +0000

    rough sort compete instrument opportunity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
