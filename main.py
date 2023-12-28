c464cd43ce9822ebc7f273b8cbd31e76d25edc53
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:00:58 +0000

    train rob slide will left

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
