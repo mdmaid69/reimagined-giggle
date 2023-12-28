d6f33eaf47f5594ec8b0328726ca68e6c8a180fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:35:34 +0000

    crowded same game friendly count

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def sort_list(lst):
+        return sorted(lst)
