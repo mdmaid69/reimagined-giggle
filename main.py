6824a6fb8352b2789181719b5edddf5fef548dd2
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:04:40 +0000

    spin hurt plant state message

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
