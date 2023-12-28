77cb3e2e8fa20b84b9d4ddec260e64f28df2e424
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:37:31 +0000

    quick win convince grateful cut

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def find_min(numbers):
-        return min(numbers)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
