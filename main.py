17aa84a65c7f1a374bb14aad57553b5468830875
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:49:29 +0000

    instruction soak bin trust increase

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def convert_to_hex(n):
+        return hex(n)
