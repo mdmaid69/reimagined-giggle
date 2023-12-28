a2414dd5a572dd7d087a2e2b74408fe841a9e992
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:15:27 +0000

    practise his greeting group previous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
