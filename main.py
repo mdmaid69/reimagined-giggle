070c3dcea357b49c47d3060a959066a70f9d7ea3
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:11:12 +0000

    confident average rate pause separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
