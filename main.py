d78ff950dedd5c524141df88f8cc6fc7c2738330
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:15:48 +0000

    taste rank prepare shallow mood

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
