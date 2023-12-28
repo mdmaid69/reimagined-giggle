73584bfec796b9cec5845fa29abcad52d4f180ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:50:27 +0000

    reason middle wonder push sentence

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+print([x**2 for x in range(10)])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
