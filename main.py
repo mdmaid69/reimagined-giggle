dc7adc4076503e21986db6ef02993e8a62472202
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:41:43 +0000

    round face order church any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
