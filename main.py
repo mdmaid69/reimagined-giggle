9b7838f53aa5f9bf703460d4aef5363a061b707d
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:55:43 +0000

    embarrassed ever fear plan blow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
