d4adb7807b96b6bbeb90633b1d6389fe2996556f
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:40:26 +0000

    freeze very how expression add

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
