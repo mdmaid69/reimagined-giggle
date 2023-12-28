41e4f77b23f6c639919151ef3202c2c148ecff89
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:19:46 +0000

    thumb because off advise dull

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
