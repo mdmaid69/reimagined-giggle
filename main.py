d5b129350e772530ddf14beff5a76b175cebb077
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:48:32 +0000

    strange absolutely repair thing advise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
