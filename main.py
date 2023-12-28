9548ec31f547a943336c32d380d33400e43fae7b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:43:01 +0000

    no Yy medium suffer opportunity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
