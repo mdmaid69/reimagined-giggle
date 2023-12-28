d2014bce9e5c635b308c986c81e33caf7bd791c1
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:35:44 +0000

    decision regret pity owe stupid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
