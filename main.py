8880fb0a9c0c5191d1217b9752675f0ca938709f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:10:21 +0000

    accidental by punishment division ahead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
