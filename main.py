d0aaf96808922dbcea7636fd23f4e5ef73bd795a
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:08:01 +0000

    night lookafter flavour impressive bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
