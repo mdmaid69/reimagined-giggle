d40512a479478a025b5dd32697b586d74f4dfa67
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:36:31 +0000

    pinch swing across communication container

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_int(array):
+        return int(array[0])
