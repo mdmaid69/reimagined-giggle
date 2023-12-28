01020041daf6cee44d4ff1ae4cd506e9e9c1a285
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:49:18 +0000

    turnoff thick discovery middle before

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_area_rectangle(l, w):
-        return l * w
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
