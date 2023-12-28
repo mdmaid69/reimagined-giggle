6dddc23c002fd94cac544f1592fb58ebded94535
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:16:20 +0000

    collection silent taste open appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+print(re.match("h.*o", "hello world"))
