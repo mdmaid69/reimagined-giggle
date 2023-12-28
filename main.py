355df05ec3ee14c896011aa98bcbe68cacc52eda
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:24:59 +0000

    tour common grade heat room

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
