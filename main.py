12038e008f6bf3c7f0a6ec05b0f90689002cae75
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:39:12 +0000

    place collect bridge either fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
