c5dc048cdca1c107c72569da70e67453b6c280b7
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:04:16 +0000

    Pp bank calmly enthusiastic hardly

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def square_number(x):
-        return x**2
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
