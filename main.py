a59c4a0e7ba519bb031b2da48c4da39fe1d4ce99
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:41:15 +0000

    present sweet light after story

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def greet(name):
+        print(f"Hello, {name}!")
