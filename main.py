db1e5ef57f4e8be38a6e46a15b59881e9fb1c0df
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:40:41 +0000

    goback(to) complain limit clearly always

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
