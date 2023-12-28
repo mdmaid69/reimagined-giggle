8494993f2ef7658b4f36893786835612b3d97f92
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:36:07 +0000

    whoever involve entrance slip run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
 import array
 def get_array_as_memoryview(array):
         return memoryview(array)
