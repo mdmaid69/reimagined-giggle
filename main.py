9debd57382aa12a04ae6496569bb1505c572894d
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:10:30 +0000

    noise out tonight emphasize image

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+x = 10
+y = 20
+print("Sum:", x + y)
