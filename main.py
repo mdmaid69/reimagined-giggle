2dffa0c8fd91aeffd7217405f78f0b59cff4ae4c
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:29:17 +0000

    left doubt coffee bear characteristic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+def get_time_since_epoch():
+        return time.time()
