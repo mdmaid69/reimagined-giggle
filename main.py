9ebf90110bf4a74b33de6cbd61ce6f86ef190328
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:14:22 +0000

    great decide dick so pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
