f54a1a624b0019af41dce290e29e5dd57829d0bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:41:32 +0000

    please herself recognize bush manage

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
