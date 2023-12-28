2e1304e3ed5b60086630d37fcaea49d2879bdcd5
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:08:21 +0000

    watch of declaration remember promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def convert_to_hex(n):
+        return hex(n)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
