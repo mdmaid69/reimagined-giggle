9a7ca04dac44ff303db734c45c5a15081253bf93
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:03:42 +0000

    education win lie process friendly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
