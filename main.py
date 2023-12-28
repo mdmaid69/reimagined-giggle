35477449cd39137bce1a10e57f451d5d8431a5b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:22:22 +0000

    produce country free unless regular

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def is_odd(n):
+        return n % 2 != 0
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
