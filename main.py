737cfe65bcf60471abbd4ffe60e504e131a0417a
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:30:58 +0000

    wake finally better blame feature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
