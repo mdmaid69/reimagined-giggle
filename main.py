8fb5abe92e4c1280629fafbcba5eaa55c253bda6
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:48:36 +0000

    bringup contrast rich convenient debt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+print(os.getcwd())
