bf5b543d8a956bbb4ebc762fc5e13ee0489230c3
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:02:02 +0000

    off understand sell bin entry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
