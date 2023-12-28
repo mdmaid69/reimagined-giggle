b8172435eda02c5f5b74176521952bdf8ee3c545
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:47:42 +0000

    strength performance cook secret earn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
