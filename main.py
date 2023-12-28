b8affe48a2a82ae80478a26f6f25e5c0c997c2fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:19:14 +0000

    help before hate cool rely

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def square_number(x):
-        return x**2
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
