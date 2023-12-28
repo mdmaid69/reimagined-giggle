4232a16313021a732918f2d819f7c01fd8b80cf3
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:32:05 +0000

    otherwise race turnback worry smart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
