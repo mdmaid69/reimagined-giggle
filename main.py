5a682f38d21df201e55b53d6bf8d006f1f161d80
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:13:08 +0000

    full difficult fall neither into

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_queue():
-        return collections.deque()
-def sort_numbers(numbers):
-        return sorted(numbers)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
