82f9f6281f1e264049986edeef653f4837b19456
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:05:02 +0000

    copy suppose match phone shame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_acceleration(speed, time):
+        return speed / time
