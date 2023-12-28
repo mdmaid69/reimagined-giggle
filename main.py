abdb0b81ba244188c6eb645fefa435adc27c1d42
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:31:22 +0000

    majority can better design smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
