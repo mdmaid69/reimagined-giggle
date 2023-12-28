810955b0d41ea3565c8b0cfee906cc012cd4872e
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:00:39 +0000

    punishment unit man giveaway left

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import collections
+def create_user_list():
+        return collections.UserList()
