16b0465f8b1a4c140b2658e6ed169601b9523696
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:59:26 +0000

    lost press description until runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
