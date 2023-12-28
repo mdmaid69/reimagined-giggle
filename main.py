1c6cd66ea1330b9a5ca09f588593d9bcd2e73299
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:38:07 +0000

    constantly question clearly opposite head

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
