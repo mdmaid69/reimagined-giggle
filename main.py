991f08be69ec2359fb57eb1a90aa0dca3d71df68
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:29:25 +0000

    total drawing snow push whistle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
