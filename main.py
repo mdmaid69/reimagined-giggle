7a069c514b4a91e59cab35d80ab256129d3356de
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:13:55 +0000

    increase can include unpleasant fold

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
