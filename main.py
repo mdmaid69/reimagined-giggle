38a9b9d1433b3ed4015d93b69c2e1db0e29b4744
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:08:37 +0000

    lunch fire schedule wide sorry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+for i in range(5):
+        print(i)
