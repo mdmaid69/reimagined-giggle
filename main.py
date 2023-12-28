c89c67635c7d62436b3ffb4fb88641b6f062d06f
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:53:00 +0000

    band tap fortunate of crisis

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def add_numbers(x, y):
+        return x + y
