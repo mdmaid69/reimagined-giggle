b162a414a4bb8296c8885432fb97fee732982b5f
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:47:23 +0000

    far persuade pen confused habit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def change_working_directory(path):
+        os.chdir(path)
