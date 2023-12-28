98a338bf62fa7fa74f4cddcbc407b9126b6af5d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:23:46 +0000

    plant gentle tradition base bad

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def count_characters(sentence):
+        return len(sentence)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
