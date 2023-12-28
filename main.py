2bca5da2967aa7475e334d0c5c3d25c2ab1202cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:37:19 +0000

    middle danger backwards suckme crowded

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def count_words(sentence):
+        return len(sentence.split())
