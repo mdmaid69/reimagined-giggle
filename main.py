b0cd19b3c30c962cfbf8eeea53bafb1bb8310b4d
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:42:23 +0000

    protest Itself major help so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
