d9062471c5ee430a5a4a32c6f7e2c6276d8d3fd9
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:33:08 +0000

    runinto runover piece careless sticky

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
