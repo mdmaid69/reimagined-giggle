d1b0767c744548a7c2e99c7ecd930dbd829335fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:12:29 +0000

    question winter up fat influence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
