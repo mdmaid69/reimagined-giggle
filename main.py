6bc1da5547627d88300c0ea86ac5833fb7644cf1
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:05:07 +0000

    poison meal difficulty stage wire

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def square_number(x):
+        return x**2
+import array
+def convert_array_to_string(array):
+        return array.tostring()
