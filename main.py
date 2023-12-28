3e44e839b6ec9871bbd1f12fb357111dc590413f
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:41:52 +0000

    react opposite wind comment young

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
