0aea05dfff34fa0d3fc904ca1f7480744b6a33da
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:22:51 +0000

    belief perhaps add fight necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  def convert_to_octal(n):
+        return oct(n)
