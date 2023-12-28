86bd7182ed5a2426402a82b614c2836b1d14bd1a
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:40:15 +0000

    operate comeback usually physical down

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
