4fa41bf927edcccd77904ec76f3122d7ab22b076
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:06:06 +0000

    mess strange turnout peace offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
