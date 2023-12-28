4f7b45318deaea6a0dcae3c955a9de5b99c6a3c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:48:12 +0000

    increase dry squeeze funny check

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
