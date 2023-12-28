72a82c678df10ae281ccdc8b77e3fcaf9c99412e
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:56:14 +0000

    city slide keepaway account row

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import logging
+def log_message(message):
+        logging.info(message)
