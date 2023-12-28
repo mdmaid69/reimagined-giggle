354c8275ea785b7257f2ea0b2b582f148e374036
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:47:45 +0000

    nerve damp callup at clothes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
