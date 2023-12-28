5263dde4d1013f629371d7beaa3f068ba698b6db
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:41:53 +0000

    preparation Nn Jj protection quick

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
