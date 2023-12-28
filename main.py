665bd8347d5b2d5d741d292905da5e638d3f2f7e
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:51:21 +0000

    low bring wait order limit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
