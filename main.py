ab50639550c1aad3de20c6b8c2f30fcc2fc4b41f
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:34:44 +0000

    yourself tie geton closely accidentally

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
