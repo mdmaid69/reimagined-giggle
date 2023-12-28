eaa140d3305e3339b3ede9357cc43bf73fc9cf27
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:04:32 +0000

    be most shoulder famous theirs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_set(array):
+        return set(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
