92b7673665725b0d9437e26a78cbce995eab1bbc
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:33:47 +0000

    trip protest hot short why

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
