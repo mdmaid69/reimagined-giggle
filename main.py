084ab41ae3a336d38af23533adbc530df25941a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:36:52 +0000

    mess direct tend turn married

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
