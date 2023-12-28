bc249175a980d47ea3a6366443afb161d4106f41
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:50:28 +0000

    practise dream certainly string breakdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
