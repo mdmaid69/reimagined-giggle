fa20edc256b0d6c1e1e0e3ef89257bbd63582a46
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:29:28 +0000

    certain obvious skill survive such

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def reverse_list(lst):
+        return lst[::-1]
