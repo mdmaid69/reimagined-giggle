8319856cd13f4996cc896178452c4e9716d90285
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:33:01 +0000

    reaction agree occasional pair which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
