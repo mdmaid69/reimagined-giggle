4db27ddd0bfeae6255d6b1fd2dd0dfd7cc36db7d
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:14:09 +0000

    rate tip roll discovery whoever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
