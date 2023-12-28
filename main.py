35eb7d68ef69b73eaf2f143141482b7a295dc218
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:15:25 +0000

    impossible common blue school no

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_pressure(force, area):
+        return force / area
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
