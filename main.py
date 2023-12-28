fbae325237e2596361e3478e34888fbefc2ee127
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:12:31 +0000

    morning try throw wash related

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
