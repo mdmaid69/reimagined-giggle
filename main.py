c3c8b41d8264d8fdaf6c58fcf431e98b2cf241c1
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:48:21 +0000

    rain violent black contents sport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import array
-def get_array_as_str(array):
-        return str(array)
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
