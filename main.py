01a13f3a736c74be5ef7775374a1f5da6b9f038d
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:54:25 +0000

    scrap high 572enter conclusion meaning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
