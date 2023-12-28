ed2aa2f8dafd5eec2bda8216722616ffd1cbe0bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:29:11 +0000

    exact wash loan rank freedom

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
