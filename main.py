ca7ee0742a77ff83e93cbee61c536c9fa861d878
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:26:20 +0000

    loan fun band train nose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
