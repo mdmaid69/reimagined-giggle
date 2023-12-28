0efa7b1c43dce75b3c14d451aab490878bf253ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:21:49 +0000

    hard light yet attitude mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_base_name(path):
+        return os.path.basename(path)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
