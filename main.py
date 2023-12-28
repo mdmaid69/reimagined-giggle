0e4c3be5f824b0239aaa323a6b84f1125a789eef
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:13:51 +0000

    post ours understand debt ourselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import collections
+def create_user_list():
+        return collections.UserList()
+def find_max(lst):
+        return max(lst)
