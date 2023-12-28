ae9112364dc3b38f4e1dcc622c259c4aa0b41069
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:43:54 +0000

    callat exist music instrument mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
