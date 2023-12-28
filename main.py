71c5b719c27a11cf620c9d7303c023eaa8fe0d5c
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:08:32 +0000

    description clear exhibition conversation thankyou

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import datetime
+def get_today_date():
+        return datetime.date.today()
