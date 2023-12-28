78d34d7ebf97f09790d9c20c3551f41c8dfd44ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:50:22 +0000

    breakup feeling early squash all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
