76ff4f5296b7514a1171c2484ab70b14f5ff7120
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:45:35 +0000

    concentrate measure sea remain first

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import random
+print(random.randint(0, 100))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
