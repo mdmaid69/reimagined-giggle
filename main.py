b4aa7056e19ffacdcd1b2c99346a40b7ba87988a
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:45:41 +0000

    fun creature different spray excited

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def find_union(list1, list2):
+        return set(list1) | set(list2)
