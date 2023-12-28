ba557b75b09c2d7b28f5bcccb1d5b4ec8abae4fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:12:43 +0000

    plain achievement actual some treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
