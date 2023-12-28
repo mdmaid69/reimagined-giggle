21eb1c5eb8c3e05794646f14110b757204b1ff80
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:39:53 +0000

    upset creature bringdown straight nerve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
