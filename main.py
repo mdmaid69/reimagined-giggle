896def67ed7f9a85aecbe8954669ae402422efc1
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:16:36 +0000

    attention uponon hand wake blue

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
