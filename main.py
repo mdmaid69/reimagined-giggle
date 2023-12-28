db005db347444496d2b4b4bb959deca804e581b8
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:32:59 +0000

    tell achieve soak creature turnoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
