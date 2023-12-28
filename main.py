839d5677a317d1ed843781360dfac5a590048b6d
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:50:27 +0000

    deaf broken check press out

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
