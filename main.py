9c412e58c3b9050ed8adabb6f404821c56a691c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:25:41 +0000

    unable medium gooff real sharp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def reverse_list(lst):
+        return lst[::-1]
