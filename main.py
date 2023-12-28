45a102a8f79d3001c9081f3949ce7655b5e96c4e
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:44:35 +0000

    notice central tragedy plan tired

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-print(os.getcwd())
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
