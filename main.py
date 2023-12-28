7f4bfa5f70e37f203a13b79e0c0693412a6a1899
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:56:02 +0000

    laugh free age proud move

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def remove_duplicates(lst):
+        return list(set(lst))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
