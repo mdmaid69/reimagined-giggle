f977ebfa20695d7c71bf985b137a9ff90c65bf00
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:05:43 +0000

    cough like behave Ll school

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
