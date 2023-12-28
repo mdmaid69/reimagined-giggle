30be73613daa0628aaa6e236d402b2270560a73e
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:14:03 +0000

    hit climb hurry sensible dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
