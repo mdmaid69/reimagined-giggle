6631371eefb7999a5c39614950f66d0cb019ef54
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:04:14 +0000

    bank Hh for meal development

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_power(work, time):
-        return work / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def square_number(x):
+        return x**2
