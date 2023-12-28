daf8ceb462e759ae65da1683173f9fc858edbece
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:23:21 +0000

    lie hurt goback(to) uponon evening

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_logarithm(base, x):
+        return math.log(x, base)
