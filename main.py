d06baccb5593b5f57d93d76a10fc9cde9453fbc7
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:24:44 +0000

    shall swing slip takeback far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
