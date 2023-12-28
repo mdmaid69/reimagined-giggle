1853cc5cee3801feea6c6426ee2d0eaf1babbe68
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:47:21 +0000

    hit worry job anywayanyhow tax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def is_even(n):
+        return n % 2 == 0
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
