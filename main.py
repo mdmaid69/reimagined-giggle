871a398cc26844a4d81261223c82b3eb9e3ee29e
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:47:41 +0000

    tense bored worth might sweep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  def remove_duplicates(lst):
+        return list(set(lst))
