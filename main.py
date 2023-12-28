bf27b7f4c199e6eb2e9d6ad80a5a8fbf0f8924a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:51:09 +0000

    they backwards them twist with

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import json
+print(json.dumps({"name": "John", "age": 30}))
