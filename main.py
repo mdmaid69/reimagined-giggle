866c42ce82ec4a737f99edf7846d409d35f0e2a6
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:15:42 +0000

    thinkof achieve how unless replace

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
