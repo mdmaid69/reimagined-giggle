5756e251c0bf5d55e85260985aff008f0ca6ff38
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:02:28 +0000

    high precise business work underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
+def set_array_item(array, i, item):
+        array[i] = item
   import os
   def get_directory_name(path):
         return os.path.dirname(path)
