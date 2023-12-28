22b7f355541ded4ffad200e58c21b3c9f61cb304
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:04:33 +0000

    union mood Zz would bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
