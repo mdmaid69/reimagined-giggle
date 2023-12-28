14e4185673ef75bf587611d7f98d514cbfef8dde
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:40:19 +0000

    nice pride goout week search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
