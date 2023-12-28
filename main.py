f793552f2cf2e87ffa480596d3cd53689659666a
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:10:21 +0000

    so major frighten breakfast but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def count_elements(lst):
+        return len(lst)
