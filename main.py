deb0bbbb5c2e6bb1ee76fdb8b8ceb7d442378900
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:26:04 +0000

    joke business future needle worst

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def convert_to_binary(n):
+        return bin(n)
