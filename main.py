c620779a6a60d09e48b0b26bf76acb0f9d294fd2
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:28:05 +0000

    life material fasten where become

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def divide_numbers(x, y):
-        return x / y
+  def multiply_numbers(x, y):
+        return x * y
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
