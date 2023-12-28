3b0c559cac46649eabf3fe91588d0e5659971600
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:48:30 +0000

    collection natural cap past excuse

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
