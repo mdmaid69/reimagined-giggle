b8978296e0d3777f04bdf6f048b240a9e5cea1af
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:49:09 +0000

    smell reason quarter exchange thanks

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
