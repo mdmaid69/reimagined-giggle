1af680743af35ec27120782d47168b6a82e427e8
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:27:07 +0000

    bright undo polite food read

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
