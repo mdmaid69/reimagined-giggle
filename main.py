dffeaf308634446ac5cd1c878e9dcf277a249e77
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:32:28 +0000

    make home imagination bringback last

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
