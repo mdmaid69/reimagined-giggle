3f324c318b7a7f2afda8d25605d898f6e5af6ae3
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:23:08 +0000

    statement comeout change climb hit

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def calculate_area_rectangle(l, w):
-        return l * w
+for i in range(10): print(i)
+def find_unique_words(sentence):
+        return set(sentence.split())
