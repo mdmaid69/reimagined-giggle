de4f1cc713f3112fb39d177d257007949f3d28bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:13:12 +0000

    care port telephone bite myself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Reversed:", text[::-1])
