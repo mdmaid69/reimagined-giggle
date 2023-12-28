5a5f4e4fe618e02ff69f47e154dfada5422e1e0b
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:51:32 +0000

    broken tie drive week lucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
+  def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_area_circle(r):
+        return 3.14 * r**2
