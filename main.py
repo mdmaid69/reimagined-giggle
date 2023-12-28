a3088e17c9aa3df549a1ab694619fb51e5b3c237
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:11:28 +0000

    agree sing silent evidence century

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
