10d5cc837e68673edd40c837ef9539929f8f627c
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:48:10 +0000

    style face picture morning support

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def convert_to_binary(n):
+        return bin(n)
