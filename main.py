f519ecec5810599a2210e3d7e6cdea6548fcdd13
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:09:53 +0000

    journey thinkover degree task bang

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def remove_duplicates(lst):
+        return list(set(lst))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
