dacf510f6389f0fa09228f8dd89cbf3b5aa50bf7
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:57:40 +0000

    crime blue commit concentration pen

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def convert_to_hex(n):
+        return hex(n)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
