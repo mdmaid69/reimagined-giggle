cecc1f432e67b678481c39ea841b88c9c6f1c97f
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:58:50 +0000

    separate keepup out shade sentence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+name = "Python"
+print("Hello,", name)
