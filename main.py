00f65f945bcf52dc135f48899118d74a610bd226
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:06:18 +0000

    ask tool score excuse walk

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import time
+print(time.time())
