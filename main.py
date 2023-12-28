96e963159d75a97d8d3d01ae5c6907faceb2d3b8
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:08:29 +0000

    comment anything nice star advise

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def square_number(x):
-        return x**2
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import time
+def get_current_time():
+        return time.ctime()
