a74d5c56cfe859d98cb989fda8e83d0d9a54d127
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:41:09 +0000

    welcome land adjective keepfrom mouth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
