86a167861f2efb4c544b44b6cfb002a067725e1d
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:53:00 +0000

    interrupt receive chat pretty degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
