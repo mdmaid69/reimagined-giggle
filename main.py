ee388e5d868cadb6567a6fef87f499cee811d0ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:36:29 +0000

    consider own pure careful add

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
