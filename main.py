9be0bb16ad19848f1c9d8b4c8f2375ac4950b044
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:30:53 +0000

    disturbing who reasonable occur reduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
