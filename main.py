34f0032cad4919968e2e6302af21b4a40cbc2912
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:49:35 +0000

    responsibility manage compare keepout criticism

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def square_number(x):
+        return x**2
