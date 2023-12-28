33753517f90a9d5bf32a8d852ef88310ea97e114
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:16:06 +0000

    tear club care test blow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
