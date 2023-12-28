bbe533bd468f9a2d1aa5883f0f315661785c60af
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:50:51 +0000

    account satisfied autumn cold style

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
