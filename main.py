1029db9bfd697d2f974c3739f0036c6ad1678763
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:12:46 +0000

    suit walk want truth drive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
