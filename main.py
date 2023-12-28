becb48492581596a4a43fd7a57934a44ad393d16
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:10:30 +0000

    refer unhappy demonstrate grow better

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
