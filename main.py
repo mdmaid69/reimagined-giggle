9517da6f109eceb06de7be217b6c4a0e352f89cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:35:25 +0000

    please ever target damp worth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
