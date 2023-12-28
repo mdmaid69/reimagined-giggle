890feed8efd29d94d9747a33302f0695417d56cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:32:52 +0000

    confuse doubt prevent warn mysterious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
