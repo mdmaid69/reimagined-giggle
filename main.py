da957a1ca3c2723c9149169251cc053f3648c704
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:29:20 +0000

    572enter suck during check put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
