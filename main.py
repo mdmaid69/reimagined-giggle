4035d39a67e78cecd46a7f198d5f8699d2bf1aa8
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:20:35 +0000

    tip Vv cum object lookaround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Uppercase:", text.upper())
