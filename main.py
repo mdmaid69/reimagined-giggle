7b9aac9ab9c71e81b1d349a7efe842d158fe7139
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:09:22 +0000

    country fashion call matter up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
