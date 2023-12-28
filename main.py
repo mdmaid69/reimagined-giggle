61e56079edf4e489f537f55c956ed3cf38eb82de
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:58:33 +0000

    recognize hold influence everywhere build

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_typecode(array):
-        return array.typecode
+def is_palindrome(s):
+        return s == s[::-1]
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
