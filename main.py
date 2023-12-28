41d8a62a00f7ff9c7c96dbbe48ea5c11b7a5afba
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:35:01 +0000

    small score by swimming project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import math
 def calculate_logarithm_of_gamma_function(x):
         return math.lgamma(x)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
