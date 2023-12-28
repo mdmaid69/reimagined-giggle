73e967bc0f2857dc90a1af22de5cf27b470a0a33
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:33:56 +0000

    start violent ago feeling one

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
