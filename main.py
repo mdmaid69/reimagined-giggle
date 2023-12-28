74114fb1fd584fade0ad7c51bd232645c6afd0a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:59:09 +0000

    eye acceptable leg value under

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_area_rectangle(l, w):
+        return l * w
