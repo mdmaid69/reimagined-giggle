01c0dc86502217b162e8f53bebfbed4f48f56138
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:16:36 +0000

    contrast card possibly blue Ss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
 import array
-def get_array_as_str(array):
-        return str(array)
+def reverse_array(array):
+        array.reverse()
