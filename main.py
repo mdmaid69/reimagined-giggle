bba7de6522d89b3cb638ee9aa0599077514201a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:04:12 +0000

    stamp little effect oil determined

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_hex(n):
+        return hex(n)
