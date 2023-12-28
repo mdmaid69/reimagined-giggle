fcdc03ce9986ed40048f3e9d29ac738fd424a1b3
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:29:56 +0000

    achievement rare neither shopping comeback

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+print([x**2 for x in range(10)])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
