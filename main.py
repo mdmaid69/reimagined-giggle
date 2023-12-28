d23cf65fc41e8f2bbe337eb492fcb8ed4ee31381
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:49:39 +0000

    bill lost previous rough stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_area(radius):
+        return 3.14 * radius * radius
