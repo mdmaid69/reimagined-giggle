a9716021a4c9e679d3e4dfe2ca669fd9fb2f858e
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:40:13 +0000

    cinema impressive measure fashion offend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
