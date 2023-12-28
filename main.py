f20a194e2e32deea290b09c8f0412f2e549c9011
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:42:30 +0000

    by although lead hate next

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
