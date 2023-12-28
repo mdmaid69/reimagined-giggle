ad152101e590a386be0a5a3d4b61bc8007cc8a65
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:22:33 +0000

    order acceptable future separate sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import sys
+print(sys.version)
