3e97436a3f7d092d92f06e15f012f9fb4d097edb
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:41:47 +0000

    foot enthusiastic language emphasis bound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_max(numbers):
-        return max(numbers)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_item(array, i):
+        return array[i]
