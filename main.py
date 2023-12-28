5eef1bb480dc359d8e386b818bd9e42e66503cb4
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:31:28 +0000

    suggestion pair boring used guard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
