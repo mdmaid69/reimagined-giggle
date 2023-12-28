dc4d0206beefc17a1029df84f2917e4b360bd5d2
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:31:46 +0000

    stop ask pleasant autumn various

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def find_min(lst):
+        return min(lst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
