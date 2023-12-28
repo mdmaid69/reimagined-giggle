91c353487767d2e7be7aefa9274f6259188819ec
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:37:51 +0000

    shine possible pause clear butter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def subtract_numbers(x, y):
+        return x - y
+def find_max(numbers):
+        return max(numbers)
