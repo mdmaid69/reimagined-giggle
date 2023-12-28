363da55855a089beeb5c1bf127486f7772bd7ee3
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:01:48 +0000

    male fix ground goout weather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
