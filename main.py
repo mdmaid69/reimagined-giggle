818d8835cd7ebab7a5809228041c591c9539c0d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:49:00 +0000

    report common takeover choose up

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def multiply_numbers(x, y):
-        return x * y
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import platform
+def get_os_info():
+        return platform.uname()
