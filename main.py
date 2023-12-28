b0cd8b419bcfcceee94d67c6f14a68acda0936fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:37:17 +0000

    back cum find string Nn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
