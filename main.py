e590b64258e0df8c1ee49c5286a020e432eeeb01
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:22:20 +0000

    material use means sure black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def reverse_list(lst):
+        return lst[::-1]
