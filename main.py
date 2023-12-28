45622d498615eebfa729a9b70f50f91d6049c5f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:38:05 +0000

    birthday colour fierce path splash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_length(array):
+        return len(array)
