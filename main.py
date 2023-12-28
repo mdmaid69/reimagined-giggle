a4d19c28015937240a66fd7d959edfd1bd065327
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:27:36 +0000

    grade instruction breakdown mean pen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
