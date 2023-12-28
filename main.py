5f7d58e3e395a9bc23dac588340e649d052f140f
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:53:20 +0000

    official pay variety mind excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import os
+def change_working_directory(path):
+        os.chdir(path)
