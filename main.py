06b714b74bdf9964e3764eeae6c65cd23f0538b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:35:29 +0000

    dead matter impressive consider 573entertainment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-print([x**2 for x in range(10)])
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_string_from_array(array):
+        return array.tobytes()
