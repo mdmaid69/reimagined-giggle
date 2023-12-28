4d63ef83ea49074c5fa96173399ea248161bf9b7
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:00:11 +0000

    off consult spread report constantly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import time
+def get_current_time():
+        return time.ctime()
