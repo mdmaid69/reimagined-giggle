18bb5e20364d5e895189884c6ffbed369132d6b8
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:32:08 +0000

    evening present good style mix

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
