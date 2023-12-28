30c62a5a779df9320d913d0c46b0cc8a1afea8f6
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:35:28 +0000

    no fear achievement fine stamp

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
