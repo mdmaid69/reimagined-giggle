c9bfefa9e88ed8c5d425f24336c4c5f57ce6bf96
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:19:39 +0000

    sorry crowded difficulty dangerous card

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
