7a479ade7e7ffb648c0e8d7e1f76479c0ec8a91a
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:46:46 +0000

    damage mean room brown skirt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
