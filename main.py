05a94398045b3cec90fbf5a26d16c997a7ba0c02
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:28:19 +0000

    every stay blow owe chance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
