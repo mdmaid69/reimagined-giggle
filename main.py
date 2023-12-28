7d6b3a56c6d2fc0ef6ea73e9e749be2634948f86
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:50:57 +0000

    watch luck peaceful over soil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def is_odd(n):
-        return n % 2 != 0
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_density(mass, volume):
+        return mass / volume
