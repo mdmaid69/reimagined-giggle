a493fc0c16706c19464b6f0d3121aaf2fe162987
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:27:46 +0000

    think love thin prepare design

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
