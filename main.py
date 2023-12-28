35935e123846b44365f3b86c1fac56ec0e180d3b
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:14:18 +0000

    protest clothes soak free direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import os
+print(os.getcwd())
