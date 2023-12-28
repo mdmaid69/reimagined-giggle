aea6d67dfc70d842912e3ee2185ed3becc0365cb
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:01:06 +0000

    usual bet sport centre criticize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+from collections import Counter
+print(Counter("hello world"))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
