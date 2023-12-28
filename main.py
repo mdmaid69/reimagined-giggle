3e7da397300122cd18c5f05e42d4733a564272d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:52:55 +0000

    size godown open turn pattern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
