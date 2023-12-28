f7cb258eaaa27815ad5786e78e597ebe49af42b5
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:20:08 +0000

    scream central coffee quarter description

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_odd(n):
+        return n % 2 != 0
