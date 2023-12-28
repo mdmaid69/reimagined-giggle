0b6d5c3e6b8b56c3743efa467660940e64445ffb
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:46:50 +0000

    breakfast sleep turnoff limit that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
