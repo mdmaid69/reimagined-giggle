cceb69e25280f89c164bd71f6a653f00e732bd5a
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:52:33 +0000

    letter sleep search previous true

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
