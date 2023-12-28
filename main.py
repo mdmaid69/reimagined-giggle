a96f0cf04192697c4be4bbc6d4972f883a3fbd83
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:19:45 +0000

    test total runover angry sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
