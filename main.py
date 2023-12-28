c190b2e5b19738947bafdc0f3e439a659534a87d
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:32:41 +0000

    strong study flow partner become

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def find_unique_words(sentence):
-        return set(sentence.split())
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
