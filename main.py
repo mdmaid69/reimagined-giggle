73af6ae32f3384c13a33cad2848d7f972ffe92bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:23:11 +0000

    special closed wild interested over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
