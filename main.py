5ad8707a9cb40c8656fe0bb2ba1f6dcabf620ec5
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:05:27 +0000

    character peaceful sweet touch tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_repr(array):
+        return repr(array)
