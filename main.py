1a637ed81caa6256ab4b4da6037cb2ee66bd04c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:11:38 +0000

    attach above violence wing welcome

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
