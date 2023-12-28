d4a2ce82a94c19bf413a78184d5b6139b28976a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:49:59 +0000

    arrangement key model yet far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
