9609f127eca071af8d3a4d5f54fc90bb1d20e81e
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:17:06 +0000

    which book goup hollow ashamed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
