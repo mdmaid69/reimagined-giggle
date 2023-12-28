286c1ca14157646312e49f2d61a91f57770c41f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:49:44 +0000

    harm behaviour present flat tight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
