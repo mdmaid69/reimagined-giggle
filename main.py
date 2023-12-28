5783035943848c38505dec96f30c7cbd0e4671d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:01:55 +0000

    forever platform little than better

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
