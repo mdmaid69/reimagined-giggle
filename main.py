387188b36108ddaae6a60f91f1c40db50d985803
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:17:09 +0000

    course once factory age search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def sort_list(lst):
+        return sorted(lst)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
