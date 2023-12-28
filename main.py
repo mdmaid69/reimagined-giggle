737ab9e6b10c34ff4015fed877c7015963ab0d31
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:44:13 +0000

    detail wash regret likely tired

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def print_python_version():
-        print(sys.version)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
