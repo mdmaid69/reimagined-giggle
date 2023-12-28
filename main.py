a434480c5905c16574860f0814bced2d2633b3b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:40:31 +0000

    well somehow again debt hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import os
+def change_working_directory(path):
+        os.chdir(path)
