2eec86fd966e69863f1787b1eb29a921373e109e
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:56:54 +0000

    conclusion disagree spread hope contents

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def split_path(path):
+        return os.path.split(path)
