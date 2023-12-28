1afee97f1cc62619e98b209863914647a0447e46
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:10:30 +0000

    may impossible organize excuse pipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
