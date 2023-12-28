3a2b666fd776f62178a547399458c1d84f66f4f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:46:36 +0000

    attempt workout wave information delicate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
