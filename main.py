a498de0f8a07e30c6b2266a4930e3bf32bc1a0da
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:11:32 +0000

    spring star it yet tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
