9e657a51dbefe9b0e488c36dfb4b47de31981bd8
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:38:39 +0000

    gather prefer accept responsibility naturally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
