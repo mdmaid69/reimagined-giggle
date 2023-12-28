6793b87b36a122f03846252ac18d75e257024dc9
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:31:15 +0000

    need gather hide split hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
