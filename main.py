e9c64dac4b7fb51f8225ec2ae16742917b47c0cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:27:09 +0000

    in by trust respect common

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def subtract_numbers(x, y):
+        return x - y
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
