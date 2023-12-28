0d13082efc85cb6ca4c57fbee45f4eb6d700ca26
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:54:36 +0000

    thirsty add base flood Oo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
