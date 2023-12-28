f448291a26b1d4d953b92c49f05883de03468fe2
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:07:52 +0000

    put silent related present chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
