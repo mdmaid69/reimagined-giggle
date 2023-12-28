419672ef97fe79d2eb18c1097b94395f50b3d97b
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:45:18 +0000

    trade sensitive combination share country

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
