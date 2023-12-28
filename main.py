f5fca6a36aafc1a97307ab763307929afdbd28f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:02:20 +0000

    disgusting best cold thought raw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def sort_list(lst):
+        return sorted(lst)
