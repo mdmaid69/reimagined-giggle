31c3227f2b84b9804622a8e79747a64268afa706
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:30:44 +0000

    model anywhere yours young anybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
