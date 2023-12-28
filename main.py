5537013d8a63c4c5a755ed0b42d8ad478cd9fc45
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:37:30 +0000

    sore later vision touch poor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
