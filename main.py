3565bdf24bffc501ad9e40ed63ada3969f62b564
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:35:30 +0000

    towards matter room content mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def sort_list(lst):
-        return sorted(lst)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
