8ed8772a8d78c461682d3392cb3502527dff7d4f
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:52:43 +0000

    slip birthday wander means finish

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_pressure(force, area):
-        return force / area
+def is_odd(n):
+        return n % 2 != 0
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
