b28dad41fdbcfae117809b6b5b73c3b9d3ca9242
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:48:58 +0000

    unpleasant temperature name sore please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import getpass
+def get_username():
+        return getpass.getuser()
