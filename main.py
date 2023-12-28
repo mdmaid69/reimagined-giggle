c937eb92fff7903764cbefd634fdadace5f1b89c
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:58:32 +0000

    goon cinema partner hair wild

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
