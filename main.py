58b99450e51f2f89d1e33498ec7ff8d23025b071
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:11:29 +0000

    probably keep pin stranger fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
