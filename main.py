5b74b899122ad9c445fc25454be2e9498a4ddae4
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:22:18 +0000

    area prefer find valuable pain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
