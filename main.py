a78bbc463b27a8ec91beff2d1e13292eb275036b
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:31:55 +0000

    goup area hurry ashamed sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import logging
-def log_message(message):
-        logging.info(message)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
