59a9ba834ae2fad982cbda3aaf6fad69fe8b062f
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:00:16 +0000

    them activity nowhere sometimes work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def get_array_as_set(array):
+        return set(array)
