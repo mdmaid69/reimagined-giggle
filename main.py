5e3049a573ac5a3c6eab0578158f1f8881f34dea
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:04:13 +0000

    eat sky along emphasis frightened

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
