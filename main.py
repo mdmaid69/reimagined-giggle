71287bef01d2cac071eb4d7fd843ba1d0914a842
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:18:36 +0000

    doctor generally number lock proper

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import platform
+def get_python_version():
+        return platform.python_version()
+def count_elements(lst):
+        return len(lst)
