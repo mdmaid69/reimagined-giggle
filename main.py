9d5bddd711cd4a54abcbc7a6e1a029438972eb24
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:54:28 +0000

    speech when play tragedy bag

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def cube_number(x):
+        return x**3
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
