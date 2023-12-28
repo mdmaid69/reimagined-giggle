48171e6979b59dac52ca636df418fa910ce9a742
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:58:47 +0000

    contrast shoulder unless comfort future

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
