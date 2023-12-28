513d10da5ab2de798ca91e0b95adf1f3adf93f3e
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:48:14 +0000

    surface great talk job cool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
