550b4559dc4aad884e40d92fc6a87bdc3142f0c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:56:57 +0000

    past attractive getback plan track

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
