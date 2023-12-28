e97dc7da0410919161614c92810bd355020f3ea8
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:26:45 +0000

    certainly uniform basically estimate talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
