f729ab960936f94c2ff613e86ead1f384a196493
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:14:44 +0000

    apparently well male emotion wave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def get_array_item(array, i):
-        return array[i]
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
