7ea2f7129ea29872caf006ba73341056723295e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:20:49 +0000

    prove order recover personal fetch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
