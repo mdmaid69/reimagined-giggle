d26016eff8700787ee770e91829d330555cc48bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:11:53 +0000

    load getoff object study couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
