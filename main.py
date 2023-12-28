c62791b65c57420d974a9163f20ede50fb43fc6c
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:06:39 +0000

    movement while for comeover comfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_length(array):
+        return len(array)
