6291beebe15ce721bd4143a4798c2cebc2ac7d8c
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:06:28 +0000

    first reason again lookback(on) badly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
