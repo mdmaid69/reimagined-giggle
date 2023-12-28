72b741097133c9626604e7197a3ea28b2c246fd1
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:06:05 +0000

    error mine bringback hold every

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
