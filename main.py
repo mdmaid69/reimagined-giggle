65de17a80e4c0e8376484173fea28db5af975161
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:44:02 +0000

    length murder talk study calmly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
