f5199bf2f7a15fc2c4f4c46d0ca7c96daa2cc701
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:12:53 +0000

    Zz pretty only close judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
