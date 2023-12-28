f9b70fb8a71786f098d8be5b6461b58e5791a03c
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:02:38 +0000

    thought listen conduct certainly swallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_min(numbers):
+        return min(numbers)
