2882e4ad4f226efe04c3a7733b8984eac9525c5f
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:21:48 +0000

    pollution lot like off scrape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
