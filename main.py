6f9c65d084ce9d7fa80796969b34abf6b25fb40a
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:13:46 +0000

    wrong suppose between confident threaten

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def subtract_numbers(x, y):
+        return x - y
