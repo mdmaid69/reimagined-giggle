172744b196a11069d091791c65c31abfc9fedd3c
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:43:47 +0000

    message could aim scream high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
