2925253de41a30dfe3b8e850222ff3222eee11cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:25:21 +0000

    though alive Vv object stay

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
