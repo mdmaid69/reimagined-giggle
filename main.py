d3160091ec97cef43283ba2548053d122d741110
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:09:37 +0000

    there party holdup fast yes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
