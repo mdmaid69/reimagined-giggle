a43647f5138af4c6ccc956e928bfc6843ace643f
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:48:23 +0000

    contribute patience pronunciation scream sink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
