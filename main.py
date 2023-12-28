3a358f8d3cbe3f39cad74bcbc38403b23ca2c24f
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:59:18 +0000

    city fail takeout urgent emphasize

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def find_min(numbers):
+        return min(numbers)
