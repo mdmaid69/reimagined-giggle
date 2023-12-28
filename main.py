cfca4def1c64dcfb403259042a11175b64b38904
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:55:19 +0000

    quantity test accidentally that little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
