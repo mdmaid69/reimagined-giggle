592d3ebfd297dfd0171b733d14e6e016244451ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:12:11 +0000

    shelter cum battery goaway collection

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
