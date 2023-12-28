415c3b41c9ff984ef490f189f9997da7a42175a2
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:47:42 +0000

    tonight formal aim line base

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
