2af179097ba97f4d6469f2011e00d959c036575d
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:14:26 +0000

    now needle judge Vv uncertain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_list(array):
+        return list(array)
