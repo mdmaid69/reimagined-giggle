1266a06014e3dd10f8ff43d5c1f649763ead1cd3
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:44:42 +0000

    board point contrast sea unkind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
