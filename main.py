ed5de697ce33010d486cba13294bc3aacb29c463
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:06:37 +0000

    effect begin call return Ff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
