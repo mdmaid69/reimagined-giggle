97dba8914ba8f4de04db160a73ca694c186adfc3
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:13:53 +0000

    tense large branch distant pay

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
