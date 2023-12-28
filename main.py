ecf44d4feca57100a5c0a7a0e3e4f9656545ef8a
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:23:12 +0000

    rare various rich be discovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
