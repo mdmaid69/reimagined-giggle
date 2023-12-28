44fa5540cb921833b054e65ea9768ed044c617ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:23:50 +0000

    book connected own would real

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
