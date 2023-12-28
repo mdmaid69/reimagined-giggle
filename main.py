b3405f301b3a5ef67a6180389d57df9e4ae2cc3e
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:55:53 +0000

    breakfast frequent backwards operation lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import time
+print(time.time())
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
