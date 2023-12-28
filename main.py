04b1486e5f38f0b73dec11bd2e16f9f4fd78618c
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:54:56 +0000

    read succeed this phone provide

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
