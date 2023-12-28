f141ab56a4d5a766479d50f1724e96d6e5fabd30
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:16:21 +0000

    569enjoy worse Ww strike smell

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import tensorflow as tf
+print(tf.__version__)
