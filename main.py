165dbbbe1d8ba0e0e06cd7079682102e67d47f5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:05:56 +0000

    cough us brown past march

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
