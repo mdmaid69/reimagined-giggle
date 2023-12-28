9136d59a905b974a5687d13fe8f094afa6518165
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:03:07 +0000

    awake curious regular blame repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_bytes(array):
+        return bytes(array)
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
