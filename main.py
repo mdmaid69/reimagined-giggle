9f7ed54f3de8cf4b9bb85db9949d9e3fde7bea8e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:15:30 +0000

    answer already otherwise show police

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-print("Hello, world!")
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
