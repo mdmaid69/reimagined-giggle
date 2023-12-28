6a6ce5c9942c5872b56f36707f05847939935af8
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:03:25 +0000

    arrival into rain soil small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+print([x**2 for x in range(10)])
