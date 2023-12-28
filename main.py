b9591f8755a9029719b7dba0b35f8a8b1ae734b8
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:12:09 +0000

    hair pollution closed valuable act

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-print(sum(range(10)))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
