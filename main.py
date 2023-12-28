4a611e46e9393e40bcde2431e20ae1471be9a208
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:59:49 +0000

    forgive shade exchange will might

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import collections
-def create_counter():
-        return collections.Counter()
+from collections import Counter
+print(Counter("hello world"))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
