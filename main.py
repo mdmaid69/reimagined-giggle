67feffec4b3d6235251aa1248e7cc7096bc6eb9e
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:20:53 +0000

    cap naturally minor unfair record

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
