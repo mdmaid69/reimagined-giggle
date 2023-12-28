90afc7902a015c856af12dca2e2b9282f3e5f56c
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:21:50 +0000

    sight exchange conduct pencil usually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
