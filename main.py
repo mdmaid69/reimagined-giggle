17ece59f1314082d427b50d5e7475be5318fef76
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:37:11 +0000

    switch down least die occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_current_directory():
-        return os.getcwd()
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
