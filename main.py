bba18bc9faffdc9e5cfb12df311060b7dade5e51
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:17:42 +0000

    common true maybe lead brief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
