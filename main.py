290a54cae995e982f3b6d286773f96bc96432929
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:30:17 +0000

    belong find union good bus

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
