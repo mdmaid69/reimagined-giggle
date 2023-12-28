5fc2b16cf75d2ef3175486d6fc92345c88bdc76f
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:19:15 +0000

    rare measure not container reserve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
