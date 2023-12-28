e6ba8864560835b6fd628b231598b59e50095312
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:33:07 +0000

    front declare fresh reason help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+def get_time_since_epoch():
+        return time.time()
