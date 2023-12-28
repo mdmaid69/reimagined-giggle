556042be0f94e0cf091017d501e4a16a10044cef
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:02:16 +0000

    day influence distance weekend bill

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
