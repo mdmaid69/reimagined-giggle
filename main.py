2b43a32fde36c68860d74f3cde251c8b9c52e554
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:32:27 +0000

    runaway anyone clever belong eye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
