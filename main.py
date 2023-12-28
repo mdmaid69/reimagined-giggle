ae093ade017ebb9f7d12c451496385a9e4e0100d
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:20:35 +0000

    choice persuade test narrow strict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def reverse_array(array):
+        array.reverse()
