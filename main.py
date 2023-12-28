fa3a425d199049f3c5091a14044be3432e466aa9
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:35:07 +0000

    belief declaration serious test identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
