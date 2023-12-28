b09bf05f124eb9c2f61e15ebd3c3a151cbf499ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:04:33 +0000

    family event laugh add wing

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
