bf59ceff6f0a2227e27e3ceafe9d79d99fe988ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:18:36 +0000

    surprised serious practise real believe

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
