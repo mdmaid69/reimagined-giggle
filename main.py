70e8ad461d455e56e70208b1dfcf062f23c89d03
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:07:50 +0000

    spite eat sure comeup mark

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-for i in range(5):
-        print(i)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
