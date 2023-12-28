e79a00c765f40bf335eed184cc7ec816bbd58885
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:58:58 +0000

    tread congratulate out visit loud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
