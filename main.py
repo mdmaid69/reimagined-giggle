55e80ec4a1ccb72b8bd1730ef02523be71aef542
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:03:56 +0000

    direct detail bother stare cool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
