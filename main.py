ef32e46d30bd7a0d3d07d1fc855d608d382f2f77
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:12:48 +0000

    sink inside praise Gg tradition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def count_elements(lst):
+        return len(lst)
