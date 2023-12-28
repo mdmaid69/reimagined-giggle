35ad2ff69372fb119ab21cf1834392c6f094c108
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:10:16 +0000

    turndown benefit trousers smooth confident

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
