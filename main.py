db21aad3853fdf19d91e6ad37f741a8d5da7f07f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:11:02 +0000

    accordingto hospital nowhere correct stir

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
