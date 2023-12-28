d29ad7ffbc4f1cdc667c830c097c04a7d26fa50d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:22:53 +0000

    town introduce study offence hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import os
+def remove_directory(path):
+        os.rmdir(path)
