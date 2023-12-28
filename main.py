ea6542a53e3f884b8131beca1482530d4d4cb72d
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:44:16 +0000

    reply travel door slip address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
