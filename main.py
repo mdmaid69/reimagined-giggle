b27cd9d5afa7e262c74049b166fde43c361df514
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:20:31 +0000

    awake cut government necessary modern

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
