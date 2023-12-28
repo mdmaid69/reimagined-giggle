08a2c14e79dd4a230b47831e0bb122c505e6b254
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:22:32 +0000

    hurry way clear trust loan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
