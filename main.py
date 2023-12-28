7540bf48cf20348156f08876f7d84dbff2fbca96
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:06:18 +0000

    image direct would tonight male

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
