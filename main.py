dc7bf3ccbd9e4aed88cd790f11f49fbf5186bcbe
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:49:52 +0000

    bush spill look challenge bus

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
