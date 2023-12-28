29d64f9b949936ad2c0721c1548bf348af295b78
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:54:44 +0000

    important risk accordingto mood wet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def square_number(x):
+        return x**2
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
