ad9811b5719418312b4b9555ba4a953e1974222d
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:35:31 +0000

    table worth owe explanation pressure

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 def calculate_pressure(force, area):
         return force / area
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
