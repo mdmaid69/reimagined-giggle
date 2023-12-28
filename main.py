d965cd94aa246c7109df3a7fcf2e7c494f3815fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:33:47 +0000

    dead create everybody necessary shade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
