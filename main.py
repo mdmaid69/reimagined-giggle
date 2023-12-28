29ed183d037ebdadb0212e3b302de53627af162a
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:13:20 +0000

    lunch today level shape firm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
