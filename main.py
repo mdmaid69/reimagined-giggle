bd04b9d475ad10fec511cc43056c3bef470572b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:36:58 +0000

    contain steady impressed path comeround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
