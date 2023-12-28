9147a2c6c14555b5297aa28c301addf5cd1158e3
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:44:29 +0000

    sometimes use bye everyone tomorrow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import time
+def get_current_time():
+        return time.ctime()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
