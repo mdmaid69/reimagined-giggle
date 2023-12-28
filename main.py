279930b4c8c042d04ed1f5789361194c7bad894b
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:31:30 +0000

    present talk terrible too expression

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
