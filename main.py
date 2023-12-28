af52a437ebfdbddf30632eee58a1ac2569d11d32
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:52:33 +0000

    him exist under see careless

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
