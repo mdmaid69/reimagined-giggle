bfc7d05a51e4d80803cb9fd94c4d205fe2033524
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:32:21 +0000

    silent comeout cook chin change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
