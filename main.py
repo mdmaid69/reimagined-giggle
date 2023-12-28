e87bdb7be80aeac51ecba24cc389c4210b1da4ba
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:02:19 +0000

    comfort wrap admit trust hate

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
