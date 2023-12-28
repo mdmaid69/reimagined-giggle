5a80b5a87c7103b337b35ec9b422d67252117ccb
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:21:32 +0000

    that know price whom mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_binary(n):
+        return bin(n)
