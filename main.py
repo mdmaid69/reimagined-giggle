37c18b2f446ed89bcd4089d63e76dcff83e443a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:30:38 +0000

    father rescue scale frightened speech

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import os
-def remove_directory(path):
-        os.rmdir(path)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
