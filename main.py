017830b419aeb150d34bd5751279f4ae1609c700
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:07:18 +0000

    sign switch start scream protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_volume(length, width, height):
-        return length * width * height
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
