45ea216820011f5833517839457abdc6f78818b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:44:24 +0000

    cruel steam advice something sweep

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
