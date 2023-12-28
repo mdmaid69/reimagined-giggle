24ff485da66177e2bf46584e2707ecd7fe16b3e3
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:06:07 +0000

    ordinary seem confusing progress word

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
