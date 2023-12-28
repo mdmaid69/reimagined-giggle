2efc0120b54684b8263d6bcc2e460d433258376c
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:01:57 +0000

    excuse flavour story embarrassed hour

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def is_odd(n):
+        return n % 2 != 0
+def remove_duplicates(lst):
+        return list(set(lst))
