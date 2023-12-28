af0a1adbf5208489b84dfeb5ad050848f969cb2b
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:42:55 +0000

    ahead trousers wet safety care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
