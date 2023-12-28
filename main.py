7499f27616fd92a5b1d11dec4e271520b3d214fd
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:02:07 +0000

    crisis handle unlucky adjective standard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
