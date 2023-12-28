a5d0df11a31cc8f3d214d78c2d8cd6a2cb6a8d87
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:05:44 +0000

    summer bridge importance come name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
