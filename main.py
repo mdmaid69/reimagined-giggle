6f2a4b8bdb1d619cd914d95d4c452815e9bd742d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:18:22 +0000

    plate callup bringup he interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
