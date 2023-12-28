2f819f0131b26304bd206f2350e31504369a5f07
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:55:35 +0000

    insist only picture base study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
