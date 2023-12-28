ca5195a1f329087c07d854ea54492beaa9a32b32
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:06:59 +0000

    connection sweep wherever through stamp

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-print(sys.version)
+import collections
+def create_user_string():
+        return collections.UserString()
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def convert_array_to_bytes(array):
+        return array.tobytes()
