03b987b237750cfec2067e4444f8a7910a5bc061
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:08:00 +0000

    float interview within possibly line

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
