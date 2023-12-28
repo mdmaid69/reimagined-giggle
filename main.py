c79682c52a0ad0c74ef4f0e89fb1c3a35abe41eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:58:06 +0000

    shall repair damp means crazy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def reverse_list(lst):
+        return lst[::-1]
