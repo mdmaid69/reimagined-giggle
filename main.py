a41a2eab6e2d309b20ef31a7e0e1720c4f8b385e
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:33:02 +0000

    medium proper survive Itself please

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
