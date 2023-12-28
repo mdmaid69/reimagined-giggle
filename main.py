885638551c03404ed106cc555bf06286a8e78a2c
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:16:01 +0000

    date branch month whom niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
