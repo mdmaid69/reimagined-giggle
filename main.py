58792e6955bac33e16cb41935cfd7e1cf87faa3e
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:56:21 +0000

    confused clothes shame beat join

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
