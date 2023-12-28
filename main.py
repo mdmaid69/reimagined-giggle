95497c7975b8bfcc3b2e0f324ad190aeb4b37ab5
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:59:25 +0000

    somebody injury team her raise

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
