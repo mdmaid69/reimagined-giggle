0f09953a77470b7cd67a9579c6e8fa4998eb6e80
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:20:30 +0000

    shape loose them very across

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def find_min(lst):
+        return min(lst)
