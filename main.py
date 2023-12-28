c314a29f9e26114b081535c7c91af45847c30fea
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:08:38 +0000

    exactly advantage talkoutof help laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
