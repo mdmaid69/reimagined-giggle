7f7e726043183afd0b0d1d1d326c7b76a20b025d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:42:20 +0000

    contact apply neither disappointed doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
