fec4fdf0c036021a819c966d3674322423bed457
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:45:59 +0000

    language commit transparent fashion sentence

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
