57d14c6caeb0ef7c567c040bc82e37946c0bc23c
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:14:44 +0000

    hurt with answer basis stage

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_volume(length, width, height):
+        return length * width * height
