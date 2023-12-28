6601b1f6c425e678e0f07088c5eecf3cd7dd91e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:26:16 +0000

    gas permission ask holiday contribute

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-print("Hello, world!")
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
