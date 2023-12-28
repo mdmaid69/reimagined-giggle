58e166622f833d9ac8a052dea541cee9f4fa5138
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:03:40 +0000

    warning traffic dish wing crisis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
