a721dc5da3a97d54d7cf555f12b87aff1ad22dd5
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:32:48 +0000

    bad about figure turnaway bush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
