0db718dd19e235cfd388538be777aa1231986cc1
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:11:36 +0000

    success steer judge track raw

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_circle(r):
+        return 3.14 * r**2
