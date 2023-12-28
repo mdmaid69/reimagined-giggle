0f542c14e350cd1cf536003371010ee123024c4d
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:40:48 +0000

    classroom high prove branch thinkover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_acceleration(speed, time):
-        return speed / time
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
