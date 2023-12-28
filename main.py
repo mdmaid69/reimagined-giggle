ed423abfa391093da754cf0a5ed4b1d153b7f2ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:59:13 +0000

    surprising table understand feature satisfied

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
