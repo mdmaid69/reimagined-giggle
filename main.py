d64c4b5fe7ae305997fd8b926beecb1589457b20
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:25:24 +0000

    across every attention why sister

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
