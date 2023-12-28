4ebc3d35149849693342ec93e15e319f2395f165
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:28:30 +0000

    fast generous exercise holdon thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
