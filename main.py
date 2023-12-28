b5ddb7f6859d581ddf55dde4efc6bee7df35c5f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:36:24 +0000

    ilovedicks ought callat back close

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def divide_numbers(x, y):
-        return x / y
+def reverse_array(array):
+        array.reverse()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
