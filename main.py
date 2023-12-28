981cf2e3a2733635f6c6724bc21875169fcddbb3
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:18:49 +0000

    lead related goback(to) respect quarter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
