9644ffc96515e8a6327adaba8c570dfc7c17f739
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:40:28 +0000

    excited but habit professional where

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
