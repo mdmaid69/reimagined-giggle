baed12ce59734219f51b7ac099075c51c845c5aa
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:18:40 +0000

    divide different exactly otherwise cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+for i in range(5):
+        print(i)
