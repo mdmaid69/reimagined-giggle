4d9c9a5c69175df184ab59b5f6c87d864a7b39e4
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:20:13 +0000

    concern discussion before apart object

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
