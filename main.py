e8415e996afac3b687317a95095fa2b923ee36e4
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:36:27 +0000

    equal room spray sick hard

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def count_characters(sentence):
+        return len(sentence)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
