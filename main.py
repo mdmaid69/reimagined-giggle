83ae152f4c160fd0da6bb1f14943ff64df203205
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:53:39 +0000

    deep confused concerned new promise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
