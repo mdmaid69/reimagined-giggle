0cda0eae840ee215c364984cc65349cdd83b6ad5
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:48:12 +0000

    claim something process skill disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def remove_duplicates(lst):
+        return list(set(lst))
