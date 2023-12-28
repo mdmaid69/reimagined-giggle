c05d2403fa847f2f1269af54ef25eee949ffa4e9
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:39:33 +0000

    accident tax fork getup gather

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_length(array):
-        return len(array)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def convert_to_hex(n):
+        return hex(n)
