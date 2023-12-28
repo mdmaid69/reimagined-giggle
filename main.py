a0c8e123c0fd226dc49c64ef97ce5a11961ba664
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:03:36 +0000

    mad pile break justice bill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
