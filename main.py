5ae80612c5990686d7e5dfdfc0597a8098c0b8ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:07:55 +0000

    second shock Ff average improvement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def is_even(n):
+        return n % 2 == 0
