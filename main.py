949f4f00bd8d6baab5af0d4c90ed6658b7b58bd5
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:05:41 +0000

    face mainly position exit near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
