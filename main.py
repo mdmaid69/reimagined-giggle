8b4db8afd1485a2af6ce8c053aae60c00f105619
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:47:28 +0000

    upwards nerve closed achievement send

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def clear_array(array):
+        array *= 0
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
