f0dde47f1eb5a230da0b45c8b4bc12b1ef65221e
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:59:25 +0000

    still approval should convenient taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_itemsize(array):
+        return array.itemsize
