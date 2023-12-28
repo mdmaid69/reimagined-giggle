4dd34bcc26f58eacc7d54fa1a4d67a4b47fd0c4a
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:54:06 +0000

    warning outside arrive Gg any

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
