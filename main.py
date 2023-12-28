188f55133ec091b084c2daafb5a5931f5573297a
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:49:50 +0000

    hurt show hospital hit manage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
