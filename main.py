b4d4aa43f275d1184a54b06580f5295cbaf2029f
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:43:27 +0000

    any purpose matter plain off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
