8362100b8785e2c7e986505f740687518d30448f
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:11:15 +0000

    at charge by exit deep

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
