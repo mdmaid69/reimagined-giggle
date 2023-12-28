f813493592fc2957810c292d8efda84226e73679
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:33:11 +0000

    every even reliable wake basic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
