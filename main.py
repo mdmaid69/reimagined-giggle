53008d2c6456ea910eaca0d4270c111367fcac29
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:35:30 +0000

    comeup sign side breath range

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+def roll_die():
+        return random.randint(1, 6)
