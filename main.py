e4443d85ca6aadfd080aa4425f7e9c73b821d5d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:08:10 +0000

    above common interest determined single

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
