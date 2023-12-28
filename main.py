60228b17160a93c202f32a8986ce302eb67eb725
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:18:43 +0000

    chief square stranger recently suggestion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
