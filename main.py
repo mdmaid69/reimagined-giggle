16e799de0cc813708def50cb18e0e8248d2b65d2
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:27:17 +0000

    insist middle like brush throughout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_speed(distance, time):
+        return distance / time
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
