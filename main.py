245bb5000d447766233f790657280ee3941ca8ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:14:58 +0000

    from duty call from course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
