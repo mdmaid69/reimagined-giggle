74be861d2f162dc68405e8036e95da13c5890c5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:33:40 +0000

    spill wonder whoever gift secret

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_speed(distance, time):
+        return distance / time
