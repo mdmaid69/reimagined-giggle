ca8ead6f82e5b809114f4cce281e3abc4888ebf5
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:27:25 +0000

    justice flat slowly feeling century

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import math
-print(math.pi)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
