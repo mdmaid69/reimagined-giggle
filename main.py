053d70617e1e929eb216ac627320db75a45dd8b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:19:27 +0000

    what lie soft organization accident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def is_palindrome(s):
+        return s == s[::-1]
