964c0a05e10f9786303d28b6c224e4706a23482e
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:53:21 +0000

    dead only perfect appear bring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
