254f5e80183f5ffe98e5d64654482491f2c69f20
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:15:02 +0000

    defend determined power manner compare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def get_array_as_list(array):
+        return list(array)
