386cc9aaf881c12cca254a011e79815399316d36
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:47:24 +0000

    gooff apart day study call

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_power(work, time):
+        return work / time
