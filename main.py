2ef4c20908f8ff3c98ae9bc89f3d048daeab7228
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:47:55 +0000

    rough win beginning lose arrangement

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
