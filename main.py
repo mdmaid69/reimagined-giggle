8760d995b1f65b2b6f91c5e703e87b366807c99c
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:29:13 +0000

    home wall impression reason over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
