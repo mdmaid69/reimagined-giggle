9ae93a32cd122d2e3aed5765e35b2f35e73f270d
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:59:13 +0000

    can say wait difficult cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
