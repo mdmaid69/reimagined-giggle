cca5de45663a7b0d08002a54b9a926cba6b503a1
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:00:50 +0000

    old anywhere turninto matter settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_length(array):
-        return len(array)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
