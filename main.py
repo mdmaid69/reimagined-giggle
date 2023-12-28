329d70a08c631ae88e9a4c918de56118d48d2657
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:42:15 +0000

    passenger language scene stone limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
