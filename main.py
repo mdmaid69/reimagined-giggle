55e5ea23668a901863482f69071b42fefed6a64d
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:16:45 +0000

    embarrassing sign fit bang right

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
