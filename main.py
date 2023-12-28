83c1a96a5f85a5226a899716a662278d3e0fb717
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:45:58 +0000

    reserve turnup forgive start announce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import collections
-def create_priority_queue():
-        return collections.deque()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
