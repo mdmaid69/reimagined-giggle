59755bc1899085121d6f515d3d37f09cbc94130e
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:23:50 +0000

    stretch traffic division brain rush

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
