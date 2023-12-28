4af3a0a608a62df573771ff4d6308d27b776c409
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:48:51 +0000

    another change place cinema emotion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
