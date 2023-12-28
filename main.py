1c9b1baf73352b5a051395c54a3fce317ff04263
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:46:36 +0000

    difficulty keepup comeover proper avoid

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_counter():
+        return collections.Counter()
