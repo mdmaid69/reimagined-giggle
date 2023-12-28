e70213a9ba4fc9dcebbc7e94f08654daa14ddaf4
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:33:24 +0000

    matter season nice personal mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
