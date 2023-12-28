e905744df388308a0e42d191e79fe212ffb5492c
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:05:53 +0000

    model that quickly closely sweet

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def add_numbers(x, y):
+        return x + y
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
