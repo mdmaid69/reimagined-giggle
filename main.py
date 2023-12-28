97771a6d031b7a5286690f1e00c1f9b0cf930c2f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:00:08 +0000

    method exchange answer formal mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
