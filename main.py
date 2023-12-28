1a0d125dd1d077007d1f5eb1df7b0d6a7e268998
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:43:38 +0000

    match if season Tt wake

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
