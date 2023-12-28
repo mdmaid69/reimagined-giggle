9fc0186811b713620a94c3694cea09e51c218b49
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:39:57 +0000

    scrape famous necessary become rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
