5c707c5b95cfafcdefa5aaee27c0dbd69b5b6f11
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:18:24 +0000

    quality alive conduct into please

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
