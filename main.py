79556a6be29b50c99b35d0bcc1018dfeef256517
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:51:16 +0000

    down steal charge list mend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
