e4df2f25992b1388edd965ce6833cb5442638d71
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:49:36 +0000

    expensive advantage solve disapprove mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def remove_duplicates(lst):
-        return list(set(lst))
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import array
+def get_array_as_int(array):
+        return int(array[0])
