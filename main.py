631f30dc5ec8ae03567d3bb87c8aa3abc0bc3b8f
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:41:30 +0000

    swing across size deserve straight

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-print(sys.version)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
