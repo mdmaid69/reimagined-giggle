84c3be792bf5a8829df8991b03858afb5f9af29f
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:24:31 +0000

    you leave cruelty reduce plant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
