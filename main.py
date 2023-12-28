ea9fcf554ca53baeeca09381e8891ffa93b16733
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:25:01 +0000

    light cost fear memory content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
