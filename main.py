400a14e413c12f1481fad73c3e762fcd3560b9e2
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:31:40 +0000

    inquire unless welcome spray morning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
