2c7ecb8d341f73e20b59a04875ddd333abee9aab
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:00:13 +0000

    comment payment to talkoutof life

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
