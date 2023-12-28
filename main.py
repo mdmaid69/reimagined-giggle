0f5803921e85cdc5a5fc57c821d48fb30544d718
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:50:40 +0000

    reason snow least separate something

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def is_even(n):
+        return n % 2 == 0
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
