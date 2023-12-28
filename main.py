fd43ca2953cd8380de5daf53d21bfb9e7faa3188
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:26:52 +0000

    where vision black evening damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
