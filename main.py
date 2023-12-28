a8e667340b5b49b2931e69fbe6faf33f89cd0eae
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:09:31 +0000

    communication who 572enter connect heavy

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  def sort_list(lst):
+import math
+print(math.pi)
+def sort_list(lst):
         return sorted(lst)
