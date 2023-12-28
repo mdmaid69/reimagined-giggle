1b3e3fb897415f732c74e1acce0379f4d9381779
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:21:42 +0000

    news still spring mind gas

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
