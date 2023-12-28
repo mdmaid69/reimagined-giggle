96a5231669639639ed82367c2edcd0e2ff450241
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:37:54 +0000

    tomorrow nose health tax cinema

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
