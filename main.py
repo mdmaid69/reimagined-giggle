942c31d0c8c097c35e18343649043e6bfc8d161d
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:02:31 +0000

    rub anxious goback(to) taste necessary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
