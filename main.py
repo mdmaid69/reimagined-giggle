5de616d53e4bb7fca13954f47d3118833dcf3961
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:38:39 +0000

    range equipment mine convince crowd

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
