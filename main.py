60d47b21a026da08d00542e660c29340ceccabe3
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:48:18 +0000

    fence clean cut clever wet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def count_elements(lst):
-        return len(lst)
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
