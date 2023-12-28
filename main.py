e2f5fbc8ab1717ad165b607b949ce3122e50bfef
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:26:26 +0000

    clear while discovery bill war

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
