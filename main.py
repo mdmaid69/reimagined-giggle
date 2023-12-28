7b2e624404c6260a39f8c9dd5acc995b41d4e16b
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:49:03 +0000

    present frightened lookafter pause lost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
