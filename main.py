634392a2ad23b93a85a203538d1a9aa3252a429f
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:33:01 +0000

    out ring flavour defend under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
