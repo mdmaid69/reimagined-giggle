4fd88d3a9ab122f5dfaa5b01fc2bfb79b4c8a312
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:01:59 +0000

    damage lookafter fun same used

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
