eed9f6d3d7f21e3ab14779786f12bc1811f25818
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:07:13 +0000

    aim nearly smell no cruelty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_pressure(force, area):
+        return force / area
