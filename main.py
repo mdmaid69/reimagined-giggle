d0fd839c4a59d4cb5abc2b4b96b23477f820745b
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:03:06 +0000

    port sting part cope sail

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-print(sum(range(10)))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
