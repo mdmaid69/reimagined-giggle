46adf96e71b40433a5d5aa3438d3366ad1c65fca
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:44:57 +0000

    leg total rain scream lunch

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
