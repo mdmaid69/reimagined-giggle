0b7d58bbdd5ff733512ad244e0957fc2780affb8
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:27:30 +0000

    contact comein his offend fair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
