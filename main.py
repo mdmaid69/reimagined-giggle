ac7becad885c4893e5d0a9b90cb844824fa77ed4
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:59:20 +0000

    declaration few height journey notice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
