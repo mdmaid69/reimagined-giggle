b18b6e55bae60fb37d3913eefa760f41ea21a0b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:50:50 +0000

    team table situation throw limit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
