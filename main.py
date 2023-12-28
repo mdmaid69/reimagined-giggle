a8c9d1715c3b80920a46863c990629c9a84a703f
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:35:14 +0000

    organize keepoff takeout consult note

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
