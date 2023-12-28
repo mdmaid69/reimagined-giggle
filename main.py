e7a65652c2f683c3daa002fcb6c84496e5bb7d94
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:53:11 +0000

    ourselves upwards bank left foreign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import time
+print(time.time())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
