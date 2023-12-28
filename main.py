edb1392edab6d35efa1a81d85ebbbfe50d67a9dc
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:31:10 +0000

    feature shop basis move programme

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
