e73596b2e620a469cb6abd0fe92b836f5acb4b32
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:43:05 +0000

    immediately ever almost hand hole

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
