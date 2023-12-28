b68ec4d9efaf8f0d11e5ecb245735da658d8b65f
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:48:23 +0000

    coat clean unite getoff enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
