e8244ff49b670da640a4f25e3a1736cb187f89ba
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:46:21 +0000

    even expensive desperate deliberately just

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
