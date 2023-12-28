0aacf150877bf30baa8670c645e59461cb6b7a96
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:37:42 +0000

    179bite embarrassing moral considering where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import sys
-def exit_program():
-        sys.exit()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
