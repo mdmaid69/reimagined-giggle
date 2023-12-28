fcd61741478bcb8ab865f1bba878fb6ce1e6a81e
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:13:46 +0000

    tall greeting definite hard callback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
