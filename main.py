1ae831a738672a9abb611eb93203d481962826b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:16:47 +0000

    Ss responsibility content design allow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
