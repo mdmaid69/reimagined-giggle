99d4686b1402478bba05a16c456b81a6de3fba2c
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:46:37 +0000

    little talkback letter organization unlucky

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def greet(name):
-        print(f"Hello, {name}!")
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def subtract_numbers(x, y):
+        return x - y
