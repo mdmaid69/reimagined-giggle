56081a2aee707b7be9f69db885ff207a364021db
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:31:59 +0000

    lean interested not pure discovery

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import collections
-def create_stack():
-        return collections.deque()
+def remove_directory(path):
+        os.rmdir(path)
