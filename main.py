768e56e25c099deb3b0f8549d984dcad9907da31
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:53:51 +0000

    across dead contrast wave soak

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
