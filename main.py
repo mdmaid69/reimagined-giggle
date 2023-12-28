0c45e1465d85574714d3c74a2419a568f20504ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:34:24 +0000

    callup environment season before welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
