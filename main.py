e2c3957416d39f2a075f5ec39737c27d19f92d81
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:49:54 +0000

    but together Dd pain sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
