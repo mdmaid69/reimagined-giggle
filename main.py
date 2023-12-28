c18229119559203226a9a18ace6d8fbf481f12d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:01:15 +0000

    project slow throughout satisfied whether

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
