497b79d70628248bdd2a6e1eb6f4b0ce1ddf55f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:02:06 +0000

    later use shine polite leg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
