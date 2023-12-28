b42c82d1a9a93bb6ab8ba8a885ac35ee3a93b692
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:14:43 +0000

    consider under building protest kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
