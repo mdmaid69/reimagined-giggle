d842f3b2e7bf5e0881e4ecd5d13de1f6a7c904ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:01:23 +0000

    kind secret their listen familiar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
