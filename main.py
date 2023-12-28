ea256ec08ab3c679ec4ac2304216c251be7b9e78
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:25:52 +0000

    discussion present unfair lookaround unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def split_path(path):
+        return os.path.split(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
