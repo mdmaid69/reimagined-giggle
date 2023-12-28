b7697894d8b3ba9fd154c811c8abbc1c1ed21e35
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:02:46 +0000

    pity inside rescue television peaceful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_pressure(force, area):
+        return force / area
+def reverse_list(lst):
+        return lst[::-1]
