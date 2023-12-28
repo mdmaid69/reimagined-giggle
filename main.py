0114d2847a284ed9ff64c80b0a7bdfe9a1a7bbe1
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:53:41 +0000

    sensible criticize share cheap appeal

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+print(sum(range(10)))
+def square_number(x):
+        return x**2
