4e08955ecd1f89094e8846b1385edfe833388a61
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:40:25 +0000

    present out welcome opinion rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
