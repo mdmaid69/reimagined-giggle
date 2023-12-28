aee0ec87057afd2ddbef82102676ef53b88e1a1a
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:22:47 +0000

    smart load stamp delicate by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def square_number(x):
+        return x**2
