93bec84843560d86d5db8da40489e87007979288
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:45:49 +0000

    pleasant lookup acceptable ring top

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
