b3bdbbb6400817af91172c2948982e79cba349e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:01:17 +0000

    skin if definite onto turnout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def count_elements(lst):
-        return len(lst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
