acf56f76026672c865d7832ef0b3d0456b9dc1b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:53:46 +0000

    cut it track proof open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
