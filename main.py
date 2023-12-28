b7c6adf073fa47221806596a1823aab5879f19b5
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:33:05 +0000

    protest hardly decision set deserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+x = 10
+y = 20
+print("Sum:", x + y)
