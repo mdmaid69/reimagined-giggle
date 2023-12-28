01c1b49ce73d202d4946271b0b698b53cae070d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:20:55 +0000

    beneath comeover distant part consider

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+def calculate_power(work, time):
+        return work / time
