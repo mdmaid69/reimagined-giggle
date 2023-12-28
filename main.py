0d9baf61ec2772d11b27d0fb4d4faaff306efd48
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:27:51 +0000

    start by Uu deliberately personal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
