b6f9a6920013d19c9258ca482c0221c706602526
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:51:27 +0000

    public block breakup worst lend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
