45840d4b5f214604a929b75cf72d1a5cacfdf229
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:54:08 +0000

    stay work defeat familiar study

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
