ffb975a173ca3945cb2e5c5362c11307d13a5353
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:34:27 +0000

    pronounce flavour unlucky away clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
