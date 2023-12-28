dff08ba69ff32402dc5716606833f4d82189d43b
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:48:36 +0000

    perhaps family problem could label

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
