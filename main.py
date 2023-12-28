39d4d1197e96cd36c2401b4fa8e8367011dcca0f
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:10:45 +0000

    concentration square high light heavy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import tensorflow as tf
-print(tf.__version__)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
