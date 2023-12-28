e6b8b4f21c3b1f19af32bfa0276cda92fd519cd8
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:06:06 +0000

    bank pain mistake clear several

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def sort_list(lst):
+        return sorted(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
