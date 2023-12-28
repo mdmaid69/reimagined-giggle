34e772abb27da6430e1c3261b0e92f040a6e551a
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:33:17 +0000

    common promise factory relation surface

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import os
+print(os.getcwd())
