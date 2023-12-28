9f5a90176f587513debb04e75a745c84f45b7e7d
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:57:25 +0000

    crash talk hard wave related

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
