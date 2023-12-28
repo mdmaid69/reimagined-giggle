f4481de6ec6cb7239a0533ec9efbbf730c129397
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:47:16 +0000

    order someone hole error holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
