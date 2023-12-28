aef6723fb6dd1619798e77662fe7992b1ed592f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:10:12 +0000

    worry cut no fasten able

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
