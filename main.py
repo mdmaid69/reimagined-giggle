ec2f1fd43ee8805783f5d1ff8a46e83131555632
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:26:17 +0000

    brown surprise I powerful basic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def square_number(x):
+        return x**2
