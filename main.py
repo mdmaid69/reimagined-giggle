dd80fdb4de79d7f49aca6a561658e88b79dfdde8
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:05:04 +0000

    measure reduce contrast round question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
