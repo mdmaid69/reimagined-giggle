c70443bc414d9be52f69ca2ad4f0a118660943ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:59:23 +0000

    keepout watch situation soil clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
