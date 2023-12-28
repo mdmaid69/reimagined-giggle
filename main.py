e52664ed472c60b3a4174a9632338ca0b0a618dc
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:11:24 +0000

    error team pour live which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
