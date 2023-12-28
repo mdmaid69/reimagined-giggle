9b32f921d49515ac4e59763549a8a33775953363
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:49:34 +0000

    566end force block pupil black

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
