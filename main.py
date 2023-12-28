60b5b2c3b9192b24c295a2faa82f5d2bb1e66a42
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:30:18 +0000

    sea sure cough clearly worse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
