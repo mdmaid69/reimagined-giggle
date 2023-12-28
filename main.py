d2314878f9d9edf354069e98802c48af29d18aaa
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:14:16 +0000

    prepare ago react correct shock

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
