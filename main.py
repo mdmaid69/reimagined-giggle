07cd1cead340bd2684d3bbfce3c532dba56ebc40
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:19:17 +0000

    concentration slice bottom trousers especially

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def count_elements(lst):
+        return len(lst)
