502cd3f1baad1e011a1d6b19a22fd0929ae494a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:50:16 +0000

    across strange book project office

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
