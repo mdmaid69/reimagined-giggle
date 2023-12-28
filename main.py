5ca478fe692fab83980f80ea68ae6d3cbdd4f159
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:52:29 +0000

    store knowledge exchange kick hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
