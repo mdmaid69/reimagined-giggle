4398b4ee107d8533650dabf243b8bd6eb5414ada
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:18:08 +0000

    harm surprised use extra hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def sort_list(lst):
+        return sorted(lst)
