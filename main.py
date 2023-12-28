8fcc00afd6fb4f29a32024278d1f0095e4ec7da4
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:56:18 +0000

    entry plenty workon flood 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
