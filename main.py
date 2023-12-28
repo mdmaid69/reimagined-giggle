7edda692cecf496d61f97c3618b3e63a2853e11b
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:43:29 +0000

    tear excuse dress record glass

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-import tensorflow as tf
-print(tf.__version__)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
