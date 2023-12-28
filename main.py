f405ac45c24533cd5ee952aabc1a5dc4a9635756
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:58:28 +0000

    disappear at intelligent answer sure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+print(math.pi)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
