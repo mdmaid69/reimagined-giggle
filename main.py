ce897d71b3611dc73740f4e7610e6c3587333e4c
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:58:08 +0000

    collect uncomfortable female Yy no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_as_repr(array):
+        return repr(array)
