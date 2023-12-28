a17c3d15345fd44cac7c57309177b051a5ef7e02
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:01:27 +0000

    bother record area majority payment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
