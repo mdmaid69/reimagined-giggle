2350424b8c8a7c7309f3b0d96de0f37e1e9091b9
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:10:16 +0000

    closed move fond minute lucky

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
