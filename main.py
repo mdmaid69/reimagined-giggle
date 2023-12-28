e76a41ceb53d708babeecbb2ab81df622121aef1
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:16:46 +0000

    usual coat return character feel

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_int(array):
+        return int(array[0])
