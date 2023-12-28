889490b5a2f884d7e1296c06f4de56b0369f7dbb
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:23:34 +0000

    tidy neck getoff accident talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
