14cf063ebebf3b60fe7962fc3249c57398fcc862
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:43:08 +0000

    middle only score part strong

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
