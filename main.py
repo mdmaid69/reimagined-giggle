c8f2d8f67275923910509690ec08c67acd23ce6b
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:50:35 +0000

    exchange reject something class sky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_hex(n):
-        return hex(n)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
