0a745e1b38720bccd1c7c6c5ae162e869d339ade
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:21:19 +0000

    memory surprised concentrate collect Mm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
