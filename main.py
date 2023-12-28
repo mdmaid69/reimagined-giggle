d565d0c9c1b4208be68d88cabe1db6db972ed148
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:33:06 +0000

    related consist turnoff necessary on

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def add_numbers(x, y):
-        return x + y
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
