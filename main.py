ea4af8da058cbc5ba6a657ad1ea9b3bae27f97a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:30:12 +0000

    emphasize be rest climb behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
