28ea6830f4bd7d9c85fc2bb6dabd1cd8181e7a3c
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:41:43 +0000

    recognize gradually because extra past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
