baf94915668767f6f6da913ed65542b6be5763ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:26:30 +0000

    his sharp middle classroom obtain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
