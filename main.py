2718185aa5851d9e23667f57ca9243dfa1289c0d
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:37:58 +0000

    impatient expect report treat interested

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
