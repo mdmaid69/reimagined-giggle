ef08cd5360541c5eb30a05c113f1892595a2b98b
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:38:24 +0000

    sick remain keepoff eye pool

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import json
+print(json.dumps({"name": "John", "age": 30}))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
