3677f50d8ba7925b154e94a6ecfd26e31a0098fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:42:37 +0000

    off sit tall cruelty material

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
