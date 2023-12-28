5d4bd15841390063c1470105bdba10aff5dfbda0
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:19:42 +0000

    smart television Ss like weigh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
