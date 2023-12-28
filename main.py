f0515a738537b78f5dac6c2fa2c1cd14142ce2bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:08:32 +0000

    glass mean explain manage close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
