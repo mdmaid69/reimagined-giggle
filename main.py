5936000efe6d67a3cd075ad7786932b70da0723c
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:39:29 +0000

    getaway breakinto only before pocket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def reverse_string(s):
+        return s[::-1]
