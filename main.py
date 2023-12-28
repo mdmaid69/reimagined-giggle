3c3efc4ba65f63bc5e84d6a3765e38fc8c06de22
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:33:19 +0000

    remove takeout union wear control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
