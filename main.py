5c214955b6955a0c71f33640106e1020c834a53f
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:44:56 +0000

    mess majority case unusual downstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_perpetuity(payment, rate):
+        return payment / rate
