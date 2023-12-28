fcfabc6f84ef4d5fe8b6846f299e56066cf8ddb8
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:42:25 +0000

    black follow above dress win

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
