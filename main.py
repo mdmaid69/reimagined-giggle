52a7eeb9152340e3689d1c3c35d19515ab49406f
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:21:56 +0000

    container unless congratulations load cry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
