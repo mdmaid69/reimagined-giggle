b741534d8f37161aa6d3e6b4fa56239fe42606cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:14:47 +0000

    skill lost as move yourself

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_volume(length, width, height):
-        return length * width * height
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
