ff3dfd7aa04ca0ef84e1a96ee35593224c2b2da8
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:51:44 +0000

    flow deliberately minor late takeaway

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
