4100aff268f352dc68fb23b87b30c17ab78d38f6
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:31:51 +0000

    regard late quickly tragedy tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
