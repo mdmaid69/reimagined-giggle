074136d420318ae92af9539bb3d115e562f13533
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:41:44 +0000

    store result attractive recover impossible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
