1b176ae1e3f53ff0845df155dcf9274e528181c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:48:51 +0000

    future draw preparation OKorokay provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+def change_working_directory(path):
+        os.chdir(path)
