72589d1e2661576ec157b0b455110d78ce75df39
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:30:40 +0000

    female cost wear before speech

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+for i in range(5):
+        print(i)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
