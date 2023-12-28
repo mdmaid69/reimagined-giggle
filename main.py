8e44834f1b3e5867c9bfd8ddca2f4974b7267a94
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:01:58 +0000

    scream direct me spill takeaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
