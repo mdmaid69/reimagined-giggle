df2606e1bcbafb76ff5f457fb56df1ddf384bafc
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:52:08 +0000

    finish concerned apply married total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
