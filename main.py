11ea757fed243cd58e4b17b3a3c4fb45b990d029
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:46:01 +0000

    compete aware happy emphasize shallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
