0dd63629a78326f7db30252957e9419f289fdd21
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:31:34 +0000

    in next mouth satisfactory admit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import datetime
-print(datetime.datetime.now())
+def find_max(numbers):
+        return max(numbers)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
