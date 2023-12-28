ba842c97bfb25c1e0d959a8debb73c93f2640790
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:27:33 +0000

    little step cinema previous broad

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
