f22a58a2fbe5bead26180ab809ec2af8746ec90f
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:33:49 +0000

    at loose frighten blame curious

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
