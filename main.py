323ed01592b25cdad334af51239b906b4e2f7b75
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:52:49 +0000

    bit see necessary anywayanyhow know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import numpy as np
+print(np.array([1, 2, 3]))
