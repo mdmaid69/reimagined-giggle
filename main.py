ad359533fcbdb825f63d2dc6aa3e9a558367e2c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:24:42 +0000

    gas pretty light unfair substance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_current_working_directory():
+        return os.getcwd()
+def find_max(lst):
+        return max(lst)
