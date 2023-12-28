18c9a905c80c0e22d77d07c51d4f853481e12b58
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:19:04 +0000

    suddenly concentration turndown clear disturb

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
