56b0700434c1316ff730b4b7cdee95f571940f0b
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:28:17 +0000

    comeover angry neither nearly runover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def set_array_item(array, i, item):
+        array[i] = item
