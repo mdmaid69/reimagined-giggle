8c4a6142a893455fb9ca9df1847e00dd21fd4ce6
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:16:21 +0000

    your recognize extraordinary agreement ring

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def reverse_list(lst):
+        return lst[::-1]
