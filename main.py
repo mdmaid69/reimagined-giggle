ab32942066306f4fe8d2faebe069076c15c85f4d
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:51:24 +0000

    marry far steam freedom holdout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
