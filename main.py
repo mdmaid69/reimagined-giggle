6cce9c339975d9ef206107cbb04529c7da3248d4
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:19:59 +0000

    sting justice sense possibly shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
