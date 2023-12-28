6170c72c7336e341b522ce048e92a59f97230d74
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:53:36 +0000

    whichever disappear load vote smile

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def get_array_as_repr(array):
+        return repr(array)
+def subtract_numbers(x, y):
+        return x - y
