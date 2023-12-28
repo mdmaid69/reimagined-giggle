32c02b11e7315a7186904dfeed7172311f1ee65e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:45:16 +0000

    odd emphasis container bored name

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import time
-def get_current_time():
-        return time.ctime()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def convert_to_hex(n):
+        return hex(n)
