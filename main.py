7142121afa4a42d6fbfad10545492e75811448cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:32:29 +0000

    scheme star through natural now

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def find_unique_words(sentence):
+        return set(sentence.split())
