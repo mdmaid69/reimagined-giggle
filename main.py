76083f12fe4388a9693f4a3f6c53b37651e74ce1
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:25:24 +0000

    among total command too smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def convert_to_hex(n):
+        return hex(n)
