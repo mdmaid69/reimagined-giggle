fb596c8e055012699d5d41a5128927df48fb3062
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:32:06 +0000

    finger divide clear question pick

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
