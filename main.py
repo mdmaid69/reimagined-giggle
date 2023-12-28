e232fcf9199be5a84f41e6ad34d62301f337ff8d
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:36:18 +0000

    OKorokay point pause makeup build

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def add_numbers(x, y):
+        return x + y
