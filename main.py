a0a924778241f3df936d3455a499ff804fe3da3f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:30:38 +0000

    funny neither count fast tape

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
