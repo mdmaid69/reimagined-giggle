a81f6f25a011fb6088734b180e86f4203c2b3edb
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:14:43 +0000

    tidy brother supply drop expensive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def count_characters(sentence):
+        return len(sentence)
