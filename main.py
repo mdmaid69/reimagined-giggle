3cfa3e8ffc5f5505c7176aff2ab967f74b48ed8c
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:58:53 +0000

    hurry plenty fall object pay

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
