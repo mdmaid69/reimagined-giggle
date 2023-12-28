37503ef39e168fc705ed6670bbb85e9b8fa998c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:17:50 +0000

    slightly present upwards many slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
