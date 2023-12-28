7e0ae440addba77ee7c7568ee3a49c04dcd2ad5a
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:35:24 +0000

    collection frightened best war introduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import datetime
+def get_today_date():
+        return datetime.date.today()
