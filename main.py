c2bc98592975edc9362b83a0e15c9fac69ef9ec7
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:35:39 +0000

    increase able common put noise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_speed(distance, time):
-        return distance / time
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import os
+def remove_directory(path):
+        os.rmdir(path)
