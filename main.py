058afbd29d0bca4827f122e240625d318819b8ef
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:57:54 +0000

    occur damage tap press priority

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
