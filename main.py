94fec2cdaa970b9ef3718bcf16d0cd084723065e
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:09:14 +0000

    ball pipe better government move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
