d0625290391d62311949d769ad025be9be0467e3
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:41:42 +0000

    swing OKorokay decision congratulate throw

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
