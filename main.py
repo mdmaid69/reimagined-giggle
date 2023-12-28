2a5c5e2bfbd5acd1f1fa2fcc12b353dba07a6df8
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:40:09 +0000

    comeback experiment party most during

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def create_stack():
+        return collections.deque()
