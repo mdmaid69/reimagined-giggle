28c43380f7f6c3ab7b68a2ee358d5ba8d1e8c3b6
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:42:55 +0000

    achieve grade move turnon busy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import collections
+def create_stack():
+        return collections.deque()
