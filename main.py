56ff4f13540ef3ba4e7d3ba79f9453c578544f99
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:35:54 +0000

    another low result scrape straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
