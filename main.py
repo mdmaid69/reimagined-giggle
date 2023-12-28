06785b8e6e8ebb5e7ef7a8066c6f90ea3bd971b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:08:14 +0000

    issue answer wealth fat trouble

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
 def calculate_degrees_to_radians(degrees):
         return math.radians(degrees)
-import random
-def roll_die():
-        return random.randint(1, 6)
