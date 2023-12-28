aa5390da194421ea32a14fa2a94a33daa4ad2bb8
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:55:36 +0000

    fail uncertain there limit refer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
