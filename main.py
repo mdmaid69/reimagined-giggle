929ce41611e672b96379e44e65fd8587674bfc60
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:12:28 +0000

    emotion Itself callup geton all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
