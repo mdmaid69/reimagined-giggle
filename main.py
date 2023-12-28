3cf8642336f3a2f8707c35548617e482e54d0493
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:30:07 +0000

    sign bin watch squash charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
