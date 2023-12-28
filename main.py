36dc786aa020c514f08d1a8982f540d1491bb521
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:56:20 +0000

    middle steer believe somewhere busy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import platform
+def get_python_version():
+        return platform.python_version()
