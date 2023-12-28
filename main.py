ebbe66f93315b1d509791ebf0e45753542ec53b0
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:08:57 +0000

    pass identify wake description reply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def cube_number(x):
+        return x**3
