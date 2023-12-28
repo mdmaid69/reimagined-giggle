6556f4f2a430f4989e56f7420481ad47ad5fe3bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:01:32 +0000

    reason embarrassing look disturb none

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
