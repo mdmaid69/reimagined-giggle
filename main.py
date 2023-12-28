cc9366b1ab35ced98d521b7676d5be56da1abc9a
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:21:24 +0000

    swing lucky public free offence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
