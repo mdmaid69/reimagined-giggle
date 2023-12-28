c2188a4c33e02a913fc94d2cdf5abfeea56c979e
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:03:40 +0000

    generally post advantage scrap opportunity

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-print(sum(range(10)))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
