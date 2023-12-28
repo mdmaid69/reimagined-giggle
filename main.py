71d65eaeacde9a312fb6c9e0ce4a9b72d95ff7d4
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:02:22 +0000

    be near sport firm ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_birthtime(file_name):
         return os.stat(file_name).st_birthtime
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
