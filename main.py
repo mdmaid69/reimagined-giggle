3b0aec0c922d57273b06455be37256a4445cd9b5
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:29:47 +0000

    prepare unlikely body skin mine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
