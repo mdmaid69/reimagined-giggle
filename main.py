8557a57a25b776a1bbfb152ef7a70cd468c34b91
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:08:49 +0000

    eye turnback that invite unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def get_array_as_list(array):
+        return list(array)
