7be815b54f05720f40f7de6d4396364ec499d329
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:25:01 +0000

    say share powerful state where

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
