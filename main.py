58d37ac9190edf9f146b2f5b75b7fe8c45505916
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:16:04 +0000

    hour important evidence and rid

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
