c8219e610490f7ad096ea3b6c25cd9c917670042
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:43:28 +0000

    afterwards wonder injury smoke example

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def count_characters(sentence):
+        return len(sentence)
