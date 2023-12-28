2120b75a2b420f69a1988e5987a9fe1c70bb4f73
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:32:42 +0000

    positive middle drink mixture getup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
