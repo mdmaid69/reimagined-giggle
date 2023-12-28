9a5cce312165e807dfc54dd8a7ef8ed4859bb257
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:54:12 +0000

    brother mad fire ask clever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
