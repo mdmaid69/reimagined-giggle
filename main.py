86e771f8519677454b866fabd3c1b48448d00d06
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:25:35 +0000

    separate happy offend whole fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+for i in range(5):
+        print(i)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
