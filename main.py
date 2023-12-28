4795971f83af4665c8b3738138eb193739800b0f
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:40:50 +0000

    heat acceptable slowly let breath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
