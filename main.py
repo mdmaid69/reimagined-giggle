7a2379b3cc363e4ade040140b9d4901130703f95
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:23:16 +0000

    report interesting official improvement train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
