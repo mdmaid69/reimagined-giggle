6b08bfbc8b097e2bb72aeb690fe534fcc33247ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:12:05 +0000

    behaviour with plan excited soak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
