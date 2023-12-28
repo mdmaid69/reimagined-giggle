6560137e6ae6d8288f7fa4462fe09874f0c8133e
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:21:23 +0000

    right nor nervous minority declare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_hex(n):
+        return hex(n)
