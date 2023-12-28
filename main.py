330ff954c1f0477fd6e13543e76aa021059b0667
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:01:28 +0000

    strong announce past ever nature

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+for i in range(10): print(i)
