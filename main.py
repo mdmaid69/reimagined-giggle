95d4a42d5cb991be907064ea1a719396cd614bbf
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:35:51 +0000

    ring round talk offence sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def split_path(path):
+        return os.path.split(path)
