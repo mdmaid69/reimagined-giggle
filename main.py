58efda137e79a40b2b5a1df2f47ecf559a693446
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:51:57 +0000

    dress square injury cheque thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_typecode(array):
+        return array.typecode
