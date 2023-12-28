a3c8b5771fe0b9e52c5bb5fbab381694b75671df
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:04:50 +0000

    stiff dream sale swing claim

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def count_elements(lst):
-        return len(lst)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def reverse_list(lst):
+        return lst[::-1]
