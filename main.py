92afaae86f4a02888f4d9a4afb2f5938fc6bc9fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:33:03 +0000

    damp dinner lower development passage

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
