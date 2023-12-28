0b80d16f16638b0edc4b8bfa9057a21128f717e7
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:14:17 +0000

    fierce experiment false story market

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
