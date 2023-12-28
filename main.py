1f4bacd171b939a27439f650355789ec0fd11be6
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:15:19 +0000

    wipe warning catch change direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_power(work, time):
-        return work / time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
