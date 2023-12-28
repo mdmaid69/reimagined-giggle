1baaf705ff80099f73fd51c1a4b4baae52b8fcc5
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:18:48 +0000

    though cheque reason deserve prove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
