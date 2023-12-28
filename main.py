f054a210daa9f3be1635842a900b848849193a57
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:53:49 +0000

    during because shadow reduction careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import sys
-def exit_program():
-        sys.exit()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
