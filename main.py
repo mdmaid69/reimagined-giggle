7acc793b10a8bff65ce90a97b408535d2a2fae59
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:34:47 +0000

    top regional fire degree tooth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
