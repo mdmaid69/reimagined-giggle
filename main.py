bc6622288beaeae93c04eff94f7dfc3be54aad60
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:55:24 +0000

    move Gg after left prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  def cube_number(x):
-        return x**3
