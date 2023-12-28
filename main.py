640b3d97bad6021dbd6b9346727b1698efe1ecd2
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:51:15 +0000

    protest except best arrange smart

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
