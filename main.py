48dba84d75d1292f5aa48ac5fa933d6289257779
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:34:42 +0000

    noise jump experience punish bad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_as_bool(array):
+        return bool(array)
