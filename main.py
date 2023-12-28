4f04a10dfdd78206c90a31cbb15a47431a763764
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:28:57 +0000

    influence secret aware convinced either

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
