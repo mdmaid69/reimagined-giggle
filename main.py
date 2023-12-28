86bac7f97af5b94a8e46b72b05d5f1cebeaaa505
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:23:54 +0000

    likely taste able see loan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
