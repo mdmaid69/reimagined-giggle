fc3c7b5deede5371ef653630be3c5b6c097a3847
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:14:14 +0000

    join rub laugh Ff exact

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
