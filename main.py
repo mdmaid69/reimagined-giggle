5d725f8cac6a5d78c3e221b2f2361e37449e9512
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:15:50 +0000

    degree float marry laugh mysterious

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_size(file_name):
         return os.path.getsize(file_name)
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_factorial(n):
+        return math.factorial(n)
