f9b2724d58baa82f52d3128bfbae280a49a31cdc
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:24:03 +0000

    protest uniform invite deliberate impression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
 import array
 def get_array_as_int(array):
         return int(array[0])
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
