3ca002f84d377152bd8eb606ed1de023eee9698a
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:53:32 +0000

    creature blue sign door difficult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
