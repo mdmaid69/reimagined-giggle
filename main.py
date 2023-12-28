29a81e7e3c177c0b9513622f63cd3003c0ecf42f
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:06:59 +0000

    bin step loan bread wrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
