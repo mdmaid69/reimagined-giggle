e7a2a60a46e05550c213d2f7a3572607079a05bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:35:05 +0000

    discovery result important stupid rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
