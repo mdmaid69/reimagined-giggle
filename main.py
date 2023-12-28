ee1a2e188f2e5a9e98ff47081347fb744621542f
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:34:31 +0000

    interview achievement almost and under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_mtime_ns(file_name):
         return os.stat(file_name).st_mtime_ns
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import time
+print(time.time())
