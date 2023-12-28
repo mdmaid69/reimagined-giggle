3394d66dbe8a99a36341ca1100f03783684d0843
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:13:28 +0000

    sea control previous charge ocean

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def convert_to_hex(n):
+        return hex(n)
+  def reverse_list(lst):
+        return lst[::-1]
