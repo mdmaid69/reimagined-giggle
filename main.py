0de0aaa0dcd95f04a2fdc206c1e87eba0115d705
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:15:33 +0000

    approval lead trust check interesting

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
