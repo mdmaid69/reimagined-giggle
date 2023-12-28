5e86baf70c3ed1e638431d6afda22155a6837a90
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:30:46 +0000

    deliver experience tip grow pipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def divide_numbers(x, y):
+        return x / y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
