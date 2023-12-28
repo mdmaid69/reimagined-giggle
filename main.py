8ca95b8f99ab0cc1c9f4815ead8cc35a0274d520
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:23:23 +0000

    fill wind now entrance common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
