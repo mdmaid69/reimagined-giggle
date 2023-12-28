7500e9269bd07aebbbe9cb513f1d9b7fbd5a28c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:10:48 +0000

    whichever anywhere near promise gate

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def find_min(numbers):
+        return min(numbers)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
