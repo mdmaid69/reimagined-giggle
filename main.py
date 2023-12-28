47611f4b4964795602f927f3a58af3b6e292762f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:28:24 +0000

    know my left bring build

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_volume(length, width, height):
+        return length * width * height
