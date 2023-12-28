2a4e1ee7ecddd723da2f48bceda53a720e220cc4
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:48:24 +0000

    national stranger rate anybody pleasure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
