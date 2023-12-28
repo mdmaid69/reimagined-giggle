b725171e15f03d92d104189a8eb4a872c9335f48
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:11:33 +0000

    excuse spend frightened active tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import collections
+def create_queue():
+        return collections.deque()
