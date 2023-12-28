9f7bad96867da86ac917580ad92d1be60025749d
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:58:37 +0000

    race turn create trap hurt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
