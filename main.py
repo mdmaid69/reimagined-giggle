e4b5a84d7b0fec607cf1e4278d41764046fc5e62
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:00:24 +0000

    write thinkof side war all

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
