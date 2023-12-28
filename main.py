9366c53e625bf438834eedfc8bbcee3e6d0af0c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:55:51 +0000

    public comfort difference shame takeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
