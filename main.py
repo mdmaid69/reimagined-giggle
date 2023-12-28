acb6116103edaa859554c90377b49e3f2435b369
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:38:32 +0000

    twist lock whom takeover cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
