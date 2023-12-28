a57a5234581e728e0f0a13a78180e23286adf456
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:03:41 +0000

    fast share especially defeat whisper

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
