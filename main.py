e5c680fab04d1e584da0aecb14cd94719907dbef
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:56:19 +0000

    another straight Pp scheme noisy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_area_rectangle(l, w):
+        return l * w
