7ba59c8a9ca142a9163fab8703c34664fb2b6d8b
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:31:30 +0000

    compete relation worry intelligent listen

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import datetime
-print(datetime.datetime.now())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def is_even(n):
+        return n % 2 == 0
