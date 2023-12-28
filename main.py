9f3b5da5429b3fc98527db6085ecef7a1628e962
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:42:45 +0000

    lend feature nigga effect complain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
