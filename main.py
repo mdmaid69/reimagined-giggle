e0089a22ba0742153ad6ee0e182b738c7b599b25
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:35:13 +0000

    luck save suit minimum education

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def remove_duplicates(lst):
+        return list(set(lst))
