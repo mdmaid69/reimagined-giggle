5eb7b4593d3d7b9ee02a95d7d86d16fc754888d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:43:30 +0000

    also test open moon wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
