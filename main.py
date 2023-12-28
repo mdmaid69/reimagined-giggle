4632e4561c0c15f892d32b417d5f2fe47969e632
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:05:24 +0000

    tap hang weak happen long

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
