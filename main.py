e04cf607df7a4fb503b70029e61b8f1e7f463806
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:14:17 +0000

    winter apparently mystery base control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
