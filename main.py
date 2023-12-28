2ab451a9af9ae96db26163e437661d8fe3e86bfb
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:52:49 +0000

    large underneath heat 568energy on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
