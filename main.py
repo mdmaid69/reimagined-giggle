4fd50bd85ce312b04bb177dc2f8c630cc1a9b54b
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:16:57 +0000

    nurse object grand grade juice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def find_min(numbers):
-        return min(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
