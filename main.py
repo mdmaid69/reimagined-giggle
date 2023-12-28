fb6bbb5b37498abeb8efee5ab46ae7b538ed545b
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:52:39 +0000

    important public silent tool appeal

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import time
+def get_current_time():
+        return time.time()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
