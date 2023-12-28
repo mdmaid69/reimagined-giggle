9859c45ac8549bc51f973ab05c61bbe772329f2d
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:36:52 +0000

    fact shelter reject arrange impressed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
