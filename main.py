8d0fe849caeba4e8d4f681dafd1c7c870f01b280
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:40:57 +0000

    strong new lookup through badly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def add_numbers(x, y):
+        return x + y
