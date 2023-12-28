9d100b840b69b60e202e0c53ea5d6267ff99db5d
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:54:47 +0000

    sit among steam listenfor bye

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
