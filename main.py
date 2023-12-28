9436c4c55a38775c4ff3f13edb5769875d93f274
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:54:50 +0000

    sit food else hers turnaway

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
