a30f1a5901312038d1753b5320b59bd0cea76c1e
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:13:47 +0000

    television limit grow goout sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+for i in range(10): print(i)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
