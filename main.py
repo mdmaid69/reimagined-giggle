5426e0ed5854dcb30c5d86f61f6a8903a5123bec
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:31:15 +0000

    dig hard cry hit busy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_length(array):
-        return len(array)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
