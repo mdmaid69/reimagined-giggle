f41789c30ccd28d9628ba5ce2e550516e56728ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:15:25 +0000

    thanks warn people smile mad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_typecode(array):
+        return array.typecode
