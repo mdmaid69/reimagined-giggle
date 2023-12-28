2414a888b0456ffeaa225fabe3b885556d3de61b
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:47:01 +0000

    standard rather spill discussion reserve

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import sys
-def exit_program():
-        sys.exit()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
