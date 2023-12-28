6189816cc598961f80c5bd3c675e3029c4a78ef6
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:28:07 +0000

    typical letter fail escape runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
