cb03ebb9fd08b879353ebcce434a09ce0b4e003d
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:12:24 +0000

    instead how train sport mother

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import os
+print(os.getcwd())
