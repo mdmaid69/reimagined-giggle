bde58aaed2a0f0f021b2788a51f5e854653add7b
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:41:29 +0000

    forget within unkind prefer ready

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
