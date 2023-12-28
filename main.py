ffe8281af12ded7cb152e795ec132bfb1c9eeeef
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:08:52 +0000

    occasion wish somebody no side

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
