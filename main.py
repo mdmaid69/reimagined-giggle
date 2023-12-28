edf9bb1bb5432282bf0333542ce8706cd614951e
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:50:24 +0000

    nice wet one adjective towards

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
