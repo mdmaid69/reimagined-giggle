c77bac957e6fd1a8a30bf60750b0706a236a033f
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:24:07 +0000

    heavy main niggaacademy finally load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
