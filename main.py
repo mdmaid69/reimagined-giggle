db91a86a5e534724d2ba8160df3b751046c002a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:40:40 +0000

    new bottom strange support wing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import os
+print(os.getcwd())
