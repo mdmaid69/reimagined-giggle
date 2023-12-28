076dfba703109a27e0cc169b291e689c3e279ede
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:44:34 +0000

    factory affect typical distant pump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_even(n):
+        return n % 2 == 0
