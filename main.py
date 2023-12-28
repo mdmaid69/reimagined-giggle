4fa39f2bfbefa0ffce56e841861acef2a764a85e
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:42:34 +0000

    brain guess previous pair busy

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
