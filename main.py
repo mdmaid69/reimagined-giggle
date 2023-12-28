2318f974042471ac341ec3b767d133efffa941c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:47:12 +0000

    proper hit burn tired noisy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
