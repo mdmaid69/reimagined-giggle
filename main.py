ce1147a24d76d331245e8fb33dd39374697861d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:23:18 +0000

    discussion ball drink badly worry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
