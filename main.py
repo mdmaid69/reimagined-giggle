7994543ac58ed9f82d3ab02652ffa33c09b0b9f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:45:38 +0000

    who pencil tidy wish corner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-print(random.randint(0, 100))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def greet(name):
+        print(f"Hello, {name}!")
