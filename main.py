e989a55922ead55765c04ea8d9d40db3b4231f47
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:01:30 +0000

    discuss suppose bringdown pronunciation 572enter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-for i in range(10): print(i)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def subtract_numbers(x, y):
+        return x - y
