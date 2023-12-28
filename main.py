f24779a275e138a878cfcb732e069bd2381f3727
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:46:12 +0000

    cruelty speed small compare lookup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
