6f9cdd356b751f9f2020f0b557cc97f4745cbf25
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:57:36 +0000

    official runout attempt above than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
