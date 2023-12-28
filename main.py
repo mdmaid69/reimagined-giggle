49a965cfac054382f34dc6b21c9e1bc10b73f5f2
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:29:48 +0000

    runover constantly rubbish obtain great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import math
+print(math.pi)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
