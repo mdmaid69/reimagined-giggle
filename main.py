2936244aa197c424025e5c8c2610239381e448ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:53:20 +0000

    start ourselves on word star

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
