df72e1a2d7f9b2999f2b0a8747fab294c3bc9133
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:59:21 +0000

    first become computer odd nearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def convert_array_to_string(array):
+        return array.tostring()
