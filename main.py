2e1722ac0f61b2f5669b572308ff26ef21cf9424
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:52:26 +0000

    fault form keepoff thankyou direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
