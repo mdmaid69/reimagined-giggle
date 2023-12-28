dcd9bd4f944312313942aa16518aa662627a75db
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:01:07 +0000

    pipe earn suit lost secret

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_density(mass, volume):
-        return mass / volume
+for i in range(10): print(i)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
