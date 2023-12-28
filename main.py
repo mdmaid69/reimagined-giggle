8e63a4af1c24ab0b5717066467bac8094d65d432
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:33:34 +0000

    smoke just share embarrassed female

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_area_rectangle(l, w):
-        return l * w
+print([x**2 for x in range(10)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
