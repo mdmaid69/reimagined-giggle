2f32a0932803d1b12ffef3cbb1359db044d31994
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:32:11 +0000

    practical direct bath fear neck

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
