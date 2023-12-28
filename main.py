1eb2f78ceeefa5f0f520b0afccbcf2843334e359
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:24:05 +0000

    far copy programme hers lead

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def split_path(path):
+        return os.path.split(path)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
