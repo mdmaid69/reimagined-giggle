b219acef712e289bccc060680139d991de2f3a54
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:38:17 +0000

    yours divide careless smile surface

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
