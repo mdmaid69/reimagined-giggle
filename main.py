e363391b2e5baa2475224fbf13765f092d5c2840
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:19:57 +0000

    project trade full unless start

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def multiply_numbers(x, y):
+        return x * y
+def count_words(sentence):
+        return len(sentence.split())
