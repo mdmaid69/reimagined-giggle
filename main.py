3ad5269451c51b6156efb1d42b8ae04c97118cc4
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:33:38 +0000

    time certainly upstairs slice fresh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
