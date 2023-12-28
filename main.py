9c6187f5a530b9f0c420f586601087d8cc1b6eb8
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:33:51 +0000

    gather against region see method

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def find_min(lst):
+        return min(lst)
