0d4d48c69c947a4f287d3a5a1e79728a764fe01d
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:02:22 +0000

    wise from Nn move state

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
