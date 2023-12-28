88f13c0a323124831b436aa96d2d62378f2be35c
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:58:46 +0000

    reasonable father hit public afford

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_int(array):
+        return int(array[0])
