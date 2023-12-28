3f3fee12092b3d3753847b1f6fb9d1f2649c6ca1
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:38:07 +0000

    stiff instant behind bang turndown

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import tensorflow as tf
 print(tf.__version__)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
