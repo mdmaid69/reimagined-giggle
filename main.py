f29a9f350ace010bdebceaf40e7f2388b23a8b96
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:26:49 +0000

    comeback impression father practice threaten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
