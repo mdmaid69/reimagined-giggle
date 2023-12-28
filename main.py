7c97e7b7b5cd5d6f4e86bc44d855d9806e1b72be
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:59:17 +0000

    far after case war closely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
