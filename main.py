ec8a531602dd0b69a432104236e4b0b99e0ca5ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:14:09 +0000

    from pattern blue typical bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
