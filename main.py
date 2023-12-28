4aff43208a9ddc373c3ed605403ec84a5f293660
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:12:53 +0000

    thinkthrough Oo explain clearly on

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
