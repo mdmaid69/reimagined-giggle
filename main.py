7f9a98e85655a913b1254e90aa374cf3991ad6e8
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:01:22 +0000

    apart furniture way breath appear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
