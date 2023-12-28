06784627260ecaefc8de6f6c67c61527c6627dfd
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:19:15 +0000

    space grand every wind equal

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def split_path(path):
+        return os.path.split(path)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
