65cdb8ea2f8e9ad1fb951ed4caee5411e0e5648e
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:46:48 +0000

    pull care tense collection key

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
