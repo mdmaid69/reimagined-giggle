f0e2d4b5b5a8eab3dc87b31e4852104c21fffca8
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:20:25 +0000

    sick consist weigh stage rank

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
