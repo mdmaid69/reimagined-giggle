4fcf01bf98fa8fbe96bf9c381430d6da5117d710
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:02:01 +0000

    finally old life around point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
