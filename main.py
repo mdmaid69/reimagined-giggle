6b7a2530b79f59c0389700977b83d36f105d5c99
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:30:50 +0000

    snow computer we wet dress

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
