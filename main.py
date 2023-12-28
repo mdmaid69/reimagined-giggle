40fbc58b7b78f99924ca7b939edba17f04590653
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:07:52 +0000

    join progress familiar autumn ocean

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print("Hello, world!")
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
