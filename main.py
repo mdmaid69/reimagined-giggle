2908264539024f88f661614de89e1104aabe4c33
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:56:11 +0000

    arrival organize giveout be phone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
