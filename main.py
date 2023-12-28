b6f9ee521f1bffc73018d63871e22587c487e55a
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:00:43 +0000

    pour by apparently fond limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
