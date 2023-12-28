48ca5796092c25943ddf9546bc3aa0e3d4857ad6
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:17:02 +0000

    Vv opposite regularly interrupt breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_typecode(array):
+        return array.typecode
