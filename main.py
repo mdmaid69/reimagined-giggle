c321f4132beaaed13aa58ebd8e4791d189e1501b
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:36:53 +0000

    order possibly best under dress

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_rectangle(l, w):
+        return l * w
