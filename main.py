9a0b824e44559385092fa8d9dde543c2177c669e
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:30:36 +0000

    fear friend mail relief thirsty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 def calculate_quick_ratio(current_assets, inventory, current_liabilities):
         return (current_assets - inventory) / current_liabilities
