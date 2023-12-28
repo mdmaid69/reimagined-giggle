1078edae0a1c829d16e4744a5302673cbf8349c1
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:05:32 +0000

    equal late it afterwards similar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import math
+print(math.pi)
