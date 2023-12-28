9d5dde039008904cc37ffb80013135891afa2a5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:43:52 +0000

    strike form command spare future

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
 import array
-def get_array_item(array, i):
-        return array[i]
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def check_if_array_contains_item(array, item):
+        return item in array
