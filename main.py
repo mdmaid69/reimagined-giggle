01d9d454f59a9029c58d475def535c8432ea606a
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:17:55 +0000

    movement getoff under union once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def convert_array_to_list(array):
+        return array.tolist()
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
