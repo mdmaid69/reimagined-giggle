eb6b40431ed86c397d06928f32dd929c8c16edc2
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:39:22 +0000

    warm place official hole divide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
