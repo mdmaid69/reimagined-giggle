9b38a3ea6c1ff32cb8a9a96605f2830112b689ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:12:53 +0000

    lesson succeed fetch during over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_binary(n):
-        return bin(n)
+  def count_elements(lst):
+        return len(lst)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
