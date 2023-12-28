628b3cf4a7114ac45f585208ec348a1df66a4fa6
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:26:20 +0000

    free similar ear suddenly to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
