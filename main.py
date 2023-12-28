495b15f1abf1bfbc4f863c38a25c71c4f18adbc4
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:35:38 +0000

    distance evidence mail sister free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
