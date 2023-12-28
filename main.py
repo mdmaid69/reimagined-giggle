52c9e7bba1b6382cc7d373b1ab011752b5f44250
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:12:57 +0000

    business picture available best disapprove

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-for i in range(10): print(i)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
