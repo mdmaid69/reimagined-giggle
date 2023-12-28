d8d5baf9784fd5515635348cd6318fea000b1dc3
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:37:35 +0000

    thanks report calloff order last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
