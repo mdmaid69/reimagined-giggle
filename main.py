5f991f81f2cd86823010ab93be62072ff829177f
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:07:52 +0000

    communicate hurry rush limit miss

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
