0b43a04508f8e201ef70fd14e11246dfe381d11d
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:50:59 +0000

    organization sea turnon damp failure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
