bcbddb5eb503ae4a0c760a35447b2760c0a6b497
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:40:21 +0000

    reaction 570enjoyable acceptable closely book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
 import re
 def replace_pattern(pattern, replacement, string):
         return re.sub(pattern, replacement, string)
