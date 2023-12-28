7ed8bceb11309f51cdad49f1ce65163e8a280c72
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:53:03 +0000

    professional available care fight upset

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
