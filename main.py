1247997ef49745b142b4956271e6ac416df17c45
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:38:09 +0000

    same opinion education fall holiday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
