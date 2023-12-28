116624baa74ecb5132155e7f9cef57fcb02c86c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:31:11 +0000

    money classroom him worse independent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-def divide_numbers(x, y):
-        return x / y
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
