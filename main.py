53fd336a33b2ce98946f3d67a9bfd0dc42783915
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:46:43 +0000

    choose turnoff display programme he

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+for i in range(5):
+        print(i)
