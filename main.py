b3370564d1f3f7e1e3e594bbd7a3b856c35ebdce
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:19:27 +0000

    operate mind holdup very sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def square_number(x):
+        return x**2
