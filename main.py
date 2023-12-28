c69b15e5ed932b804b063e076f458d6a8371c299
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:04:44 +0000

    towards note taste lock cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def sort_list(lst):
+        return sorted(lst)
