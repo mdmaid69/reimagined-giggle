5c1dfe7a662e016dd59401db14e9c88f57869411
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:43:33 +0000

    pleasant drive lock cost otherwise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_acceleration(speed, time):
+        return speed / time
