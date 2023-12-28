1c36faed4e92d9b252f78efbb6e259426935d6af
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:47:42 +0000

    hold how roof figure 179bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
