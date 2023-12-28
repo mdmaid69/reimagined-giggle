1059d6dd23a5288b92ae7173cc4d49481524a6fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:51:46 +0000

    even suppose bush interfere shelter

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
