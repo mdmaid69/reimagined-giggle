3dd79274b7928a1364b3cea2b5d596c374de9461
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:51:58 +0000

    fetch sweet grade difference flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
