736cc2932347598e129ab4085a622833358b55f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:45:02 +0000

    wing basic suck holdon whistle

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
