3ecc565cedaa51216f47e34c39fe37cc1b27625d
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:34:45 +0000

    win system bother reduction slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
