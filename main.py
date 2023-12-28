a027f40b04dbad3282768a7197f2a32724cf0784
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:17:46 +0000

    make runinto instant Bb soon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
