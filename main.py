bc1a1f7f1fa725e2bc2daef1e1bf26525a687d0b
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:50:10 +0000

    field play cold stir material

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
