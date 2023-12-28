f0d4a6883c457433a8854f70b6af655239be42f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:48:24 +0000

    modern payment when compete trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
