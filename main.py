390e8808bc300c82256a3e5091e428441809b0c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:30:47 +0000

    talkinto brief unlucky change division

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
