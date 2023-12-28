efacc8094f5ba8e54b259514d2d2a138dcd8cb76
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:30:13 +0000

    impossible slice which open pin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
