ad6c58ff0e2991e615eb9c38526484bd465c61b7
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:35:44 +0000

    seat model seat together listenfor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import os
-print(os.getcwd())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
