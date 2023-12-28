e187606162d7a6973c129f8a8acf581538e54287
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:37:03 +0000

    serve disgusting bury decision no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import sys
+  def get_python_version():
+        return sys.version
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
