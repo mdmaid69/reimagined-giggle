bfdaf0c7dba73fd5196ee66ad8783c8c661156fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:29:43 +0000

    steady conclusion ourselves centimetre threaten

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
