bfde719104fd25194519b2588768ee097d8a39e4
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:05:57 +0000

    perform frighten experiment add meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
