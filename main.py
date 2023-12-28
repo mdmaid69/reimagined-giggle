c209cf77706dcabc1592efdec32719298339e65b
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:06:22 +0000

    proper scream after block top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import array
+def get_list_from_array(array):
+        return array.tolist()
