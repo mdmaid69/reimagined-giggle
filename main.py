f09e3c417a8536a57e961ca7e7f44c357bc20681
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:27:03 +0000

    sudden comeover minimum Tt regional

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def delete_file(file_name):
+        os.remove(file_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
