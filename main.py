ac0722522ce22b053eabf2c68b0338a38fc9032d
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:53:21 +0000

    tough reach front hair fresh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_as_str(array):
+        return str(array)
+def reverse_string(s):
+        return s[::-1]
