95c0fe3af1c76e83f8802ba438c45230eaceaaf8
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:15:42 +0000

    transparent become although Tt favourite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_sine(x):
+        return math.sin(x)
