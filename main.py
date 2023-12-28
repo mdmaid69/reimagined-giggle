ef89767db456ebfd0d179438732588a97629349a
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:15:35 +0000

    mistake criticism powerful service land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def square_number(x):
-        return x**2
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
