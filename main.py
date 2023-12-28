6bb726f16f94d02772f8c01453f7006ce196bef1
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:39:41 +0000

    hers favour ear welcome wander

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+name = "Python"
+print("Hello,", name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
