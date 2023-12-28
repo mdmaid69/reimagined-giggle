e6a13067ba461973003012c8ded77e262691bc41
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:09:41 +0000

    habit be getaway condition invitation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
