2b461041eda20983f7b928184917920dfed1f885
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:24:45 +0000

    small letter clear frighten fear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
