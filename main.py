b9be28ba56ab9eb52465b95f9559bcd50f660e15
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:17:51 +0000

    dress concerned new squeeze thirsty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
