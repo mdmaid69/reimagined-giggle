ad7bf596e7a020b0c110733a00a1927e79e71f51
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:59:34 +0000

    call over offer proof full

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
