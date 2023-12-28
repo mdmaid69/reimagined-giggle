ae0462187d69f57dc1ba2a383a764b19699c416c
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:42:21 +0000

    discovery complain priority familiar left

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_min(lst):
+        return min(lst)
