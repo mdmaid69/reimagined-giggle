27648b6013d7a4ffbed28b77a3d3f32c6226171a
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:45:26 +0000

    care teach science repair pair

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
