60a557917679141b662b6644f78c49bd6e394a5c
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:45:51 +0000

    scare to level better floor

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
