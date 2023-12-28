b37e1ba2d7ad5bd10d5dab7adca3db6c48d6cee4
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:26:52 +0000

    soon frighten lunch break sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def square_number(x):
-        return x**2
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
