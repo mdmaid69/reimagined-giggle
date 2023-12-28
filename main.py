83e98b6345bf56519eaf9d90e29221aad3482e98
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:06:20 +0000

    fierce hello boat squash behaviour

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
