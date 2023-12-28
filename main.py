44af1e89e18fe7a5f9c08dfda6d482c7111eea25
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:11:56 +0000

    condition wear walk raise eager

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
