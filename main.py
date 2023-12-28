e7d156353262b83d1eb795eba4546a9c1327c8c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:02:51 +0000

    satisfactory person single minute second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
