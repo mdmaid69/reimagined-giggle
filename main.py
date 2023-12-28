0ebed14ac7ea7b37b295bde51e802edbd00eed34
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:14:24 +0000

    language the holdon comeover loud

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
