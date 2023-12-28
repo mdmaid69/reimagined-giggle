fae592d1bbbe121b5e8e2ade7edfc684e0b6c5d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:58:02 +0000

    name bush people traffic comedown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
