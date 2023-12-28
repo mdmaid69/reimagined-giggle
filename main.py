158c0fcdc7000fd28d4c33d8adfae0708aafbad7
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:11:38 +0000

    scare ill experiment number knock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
