3c238f054c1ed577d986f92b1129814ffdfd0874
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:21:25 +0000

    front alive onto comment goup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
