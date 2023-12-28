08dae82bd206bd31f68dc5295512f8a3c0534507
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:30:42 +0000

    offer drive mood comeover freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
