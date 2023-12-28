afc39dff4adb5e4e2514f6ac109fe14a6d7b2888
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:01:27 +0000

    around physical low respect offence

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
