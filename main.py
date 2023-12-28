fcdbf8a6eb7bd0d2da36e43ce6ba7d104370433e
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:25:38 +0000

    serious buy shop trust occur

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
