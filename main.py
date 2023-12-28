56d9919590e56b65f18399c550187119173a08bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:51:12 +0000

    his turnup actually do might

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+for i in range(5):
+        print(i)
+text = "Hello, world!"
+print("Characters:", len(text))
