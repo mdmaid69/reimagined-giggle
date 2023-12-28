57e76ca8eb2047f627941be0a6ca77103a0a9735
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:45:10 +0000

    boring invitation imagination taste little

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
