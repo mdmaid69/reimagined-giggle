b12b41d569eacb4322593c66e5366d5c86b7f009
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:15:15 +0000

    private set worried blind basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
