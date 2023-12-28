1f0fab35f7a0778158e9c4e93c08b6fca85c5cdc
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:14:02 +0000

    train of you taste blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
