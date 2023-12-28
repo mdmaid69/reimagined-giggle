918080d3543171f5d50e8d27f7a55641c9f63a00
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:16:51 +0000

    wear will splash object bet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_palindrome(s):
+        return s == s[::-1]
