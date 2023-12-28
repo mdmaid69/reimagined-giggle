48c434979bcdf1668e9d59613edbd5f283463767
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:21:00 +0000

    touch dear mistake notice stamp

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-for i in range(5):
-        print(i)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
