278623564d50f5ff46ee2047f3950ba227de9c21
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:50:23 +0000

    getout scene or guilty argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import collections
+def create_user_list():
+        return collections.UserList()
