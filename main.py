8076bcf60323e913cc06ced8ce1e2a396f50dc12
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:34:17 +0000

    familiar well genuine gooff consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import collections
+def create_user_list():
+        return collections.UserList()
