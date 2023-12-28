1ffc417525a391710f6a9bf00b9f5ff23b2ab0ff
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:22:19 +0000

    apologizeapologise mother spread deliver couple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
