89a166cff5f66344bfeff61b03598f9e57a41be8
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:16:13 +0000

    special ill keep near wide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import datetime
+def get_current_date():
+        return datetime.date.today()
