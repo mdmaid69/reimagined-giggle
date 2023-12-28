c7fdc5180eda8909b4c25ce907272c576809fcab
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:30:02 +0000

    turninto hospital fall holdout pleasant

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+print(sum(range(10)))
