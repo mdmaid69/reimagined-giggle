27f46b51f3bd61580f8b85a208a05970349342a2
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:09:36 +0000

    really book characteristic change draw

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-def calculate_power(work, time):
-        return work / time
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+print(sum(range(10)))
