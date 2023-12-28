ce8d75d83a3a7813cd133c27ac09592e840be788
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:04:14 +0000

    slightly substance performance home father

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
