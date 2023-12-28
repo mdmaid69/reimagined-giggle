000bcbb6587d532288c1040284df849d78d91575
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:35:03 +0000

    shame either service arrive car

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
