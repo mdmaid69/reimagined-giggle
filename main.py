97eeed7d6500c1c10500c86f7d26a06ebff92c2b
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:43:29 +0000

    down enthusiastic list stone deal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  def convert_to_hex(n):
+        return hex(n)
