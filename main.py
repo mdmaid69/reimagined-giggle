61d40252a629bb19e2a66ac35c3bcf53f5853cc1
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:45:49 +0000

    bite content callback Rr after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
