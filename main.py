5f0c3c3d82b92165352db872e0888b851412843c
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:42:08 +0000

    inside claim mess arrange cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+print(json.dumps({"name": "John", "age": 30}))
