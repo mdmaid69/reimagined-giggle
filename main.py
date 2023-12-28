f359a3805d93fab6713db0653fe3f82f9d0b8549
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:56:16 +0000

    ill exchange broken suffer suppose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
