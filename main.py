ae04a0fef7f239d0982f430155b5ef755ff214cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:30:12 +0000

    sting plain interrupt appeal suggestion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import logging
+def log_message(message):
+        logging.info(message)
