7274839ffed025ed1c560992d883940048a93038
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:00:32 +0000

    performance modern moon reduce swim

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+for i in range(5):
+        print(i)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
