cd3065b659d3d4765aa612bb67139471e596d232
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:29:17 +0000

    artificial trade niggaacademy feeling trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
