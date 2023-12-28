da4682dd2bd849a0cf7c03d0e121c52ec782bf27
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:19:54 +0000

    comein turnoff clean foreign mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-print([x**2 for x in range(10)])
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def square_number(x):
+        return x**2
