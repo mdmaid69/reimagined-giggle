cfb4f721060749b4bacf076efba9945c731381d5
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:10:19 +0000

    lookup congratulations call violent sticky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_work(force, distance):
+        return force * distance
