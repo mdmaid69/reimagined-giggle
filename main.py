7483fbed33f4d1bcb2afffcbfedb080f5e0831da
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:53:39 +0000

    stare surprise difficulty guess uncertain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_acceleration(speed, time):
+        return speed / time
