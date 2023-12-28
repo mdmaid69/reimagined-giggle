9ba34e3ef36c6504404858729735e9e453636b97
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:19:12 +0000

    stiff influence expect letter explanation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
