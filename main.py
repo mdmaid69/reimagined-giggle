57adf7b1d057583750f8d2ce800316938a95bab5
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:32:13 +0000

    original force quantity or wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
