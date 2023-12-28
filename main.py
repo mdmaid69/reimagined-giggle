456e7d377c68e7bd38b812952d43e6635bdaa3c3
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:45:44 +0000

    above book large Yy on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
