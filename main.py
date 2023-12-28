cf53d8f6147bd37b2372ff805acf4d69db73e4d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:21:29 +0000

    whistle comefrom switch book cheek

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
