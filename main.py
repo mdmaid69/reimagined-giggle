da9be030436377d0ba3866de72e5fb2d73e53cb1
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:36:57 +0000

    expect suppose fast none partner

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
