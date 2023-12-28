2a943300b694ce1e531ea11857922d25eece29fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:01:32 +0000

    least hide plenty cool suddenly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
