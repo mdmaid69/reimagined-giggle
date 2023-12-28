f7f090decd10540c9810434087775151aae4784a
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:03:44 +0000

    embarrassing workout miss protect bother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import getpass
+def get_username():
+        return getpass.getuser()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
