f512f1a0f20b54bbe4ce927ad8cda7bd7251c53a
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:43:55 +0000

    around hurry keepup speech pass

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
