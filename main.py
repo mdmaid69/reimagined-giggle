327790f04f0313fdde85f3e0268f5e2db26d5ac2
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:45:47 +0000

    surprised dress club system ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
