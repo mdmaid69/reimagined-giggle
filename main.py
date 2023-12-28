69af773a69df4700ca95053e02de0d614c18ecb5
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:02:09 +0000

    activity hot meet somebody hard

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def square_number(x):
-        return x**2
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def find_min(lst):
+        return min(lst)
