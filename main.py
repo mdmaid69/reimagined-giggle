8bca0b35d6f45168f497f5824ed86a1855dfdc4a
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:05:56 +0000

    busy suffer stop tape spill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
