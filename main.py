8e1a9cbde6d03d94202c3b9af4d674e4b39b13e9
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:57:03 +0000

    promise Ss somehow ashamed makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def print_python_version():
+        print(sys.version)
