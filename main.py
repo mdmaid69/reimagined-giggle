c96a4c08c74b8c6164c1e78a1544ab0330ec657d
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:20:39 +0000

    taste recover minimum run split

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
