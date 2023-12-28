7427dbfd776704f2e31e72f156fde0e253871df2
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:17:11 +0000

    black turninto bite active discussion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import os
+print(os.getcwd())
+text = "Hello, world!"
+print("Uppercase:", text.upper())
