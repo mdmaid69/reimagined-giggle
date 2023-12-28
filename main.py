3d2d55eb4af3afd5d8bfd5c6f2eff2cf58c0bae7
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:46:39 +0000

    right depend twist deaf field

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def subtract_numbers(x, y):
-        return x - y
+def calculate_pressure(force, area):
+        return force / area
+  def convert_to_binary(n):
+        return bin(n)
