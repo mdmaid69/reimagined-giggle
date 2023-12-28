0f386106ed336c5873af83a739eadef504d38fb1
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:26:12 +0000

    private face justice supply below

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
