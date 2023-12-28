edbb91b275ff011a78c0ae1c88e5d7cc80e26ae9
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:12:59 +0000

    separate left shame switch invite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
