d46c62127eae2eff0095f5e41b0f529b4c4223f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:13:05 +0000

    bridge drink fit question ours

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
