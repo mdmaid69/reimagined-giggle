bd8bb06845a5c09fa7006ae6459ceaaed55a6b71
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:59:33 +0000

    cool cap mistake heat desperate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(x, y):
-        return x + y
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
