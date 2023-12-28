242ca6cd313905f017d0fb2436e1f43f995190e4
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:22:35 +0000

    substance evidence fall steep talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
