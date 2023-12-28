7be683f845ce40716f692aea7309c26b37b83cad
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:16:36 +0000

    material water drop nor supply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def exit_program():
+        sys.exit()
