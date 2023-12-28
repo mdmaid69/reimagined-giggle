4c021c7bc4f44086a10f7a44dcc88020fdf1efcd
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:33:13 +0000

    tall spot play object dinner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import random
-print(random.randint(0, 100))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
