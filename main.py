f92b008dd350f52ec6d76c5c18cc9eee80dac794
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:29:30 +0000

    colour visit admire minimum neat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+from collections import Counter
+print(Counter("hello world"))
