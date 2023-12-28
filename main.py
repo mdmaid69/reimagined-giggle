17e06544a6dc14465279fdb5bd0eff1e79b384ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:52:58 +0000

    Ee Zz collection notice content

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+print(re.match("h.*o", "hello world"))
