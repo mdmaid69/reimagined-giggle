cf0eb16f428fa08468f276c343f96b2ab7f6d651
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:45:22 +0000

    determined control count squeeze cheque

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
