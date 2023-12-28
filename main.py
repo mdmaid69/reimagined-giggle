118e981a721590924f94ece8317f7c3ab7f7248b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:51:59 +0000

    drop delight properly team cross

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def add_numbers(x, y):
+        return x + y
+def calculate_power(work, time):
+        return work / time
