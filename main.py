7d7409e830aa8a554503a9bb974a47d7f3a9340c
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:55:05 +0000

    slice turnover defeat sink communicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+print(sys.version)
