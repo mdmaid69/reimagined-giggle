799ba750977ddbe0c45a7e022aa0183362621a34
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:44:50 +0000

    section considering reduction defend trip

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import numpy as np
+print(np.array([1, 2, 3]))
