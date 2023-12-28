398f5b526abc5cdde2ec6643cc0141a1d885b982
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:39:15 +0000

    radio sing warm straight depend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 import array
-def get_array_item(array, i):
-        return array[i]
-  def add_numbers(x, y):
-        return x + y
+def set_array_item(array, i, item):
+        array[i] = item
