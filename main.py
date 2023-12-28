9f63efed0623b85997d1bfd69bdab91176648199
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:11:37 +0000

    in model hate beside flood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
