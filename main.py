72fa845cb8ea8970e847eb08737c701b49fa81b8
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:31:30 +0000

    press small spread consult callup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
