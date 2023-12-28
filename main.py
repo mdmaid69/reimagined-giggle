3830c28a30c75a56a96c2c911b950168bba39c4a
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:27:33 +0000

    report gap environment line heart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
