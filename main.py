13de74d134f2deff6b9afaacd58f36f70590ad3f
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:29:32 +0000

    fail physical explanation block on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_density(mass, volume):
-        return mass / volume
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_acceleration(speed, time):
+        return speed / time
