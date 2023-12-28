dbafb4527cf3d3fc8af992a2af4221ed4e464a56
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:51:45 +0000

    come shallow declaration recovery height

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
