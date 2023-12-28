d1865c3b3ff8085f36ef937e663bd2b89fefd5f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:39:52 +0000

    have above see accidentally spoil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
