5c29a30e64575ea97adb6a674cd16fb3f958c87d
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:56:02 +0000

    note art likely crisis nowhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
