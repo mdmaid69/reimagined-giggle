d3db0a37d5ac0a31aaa516388dfdf3b4e5c0bde9
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:26:56 +0000

    often account admire instrument arrive

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import sys
-print(sys.version)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
