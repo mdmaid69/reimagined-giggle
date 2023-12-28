0cd81db7eb5aff35b92f1ff79e264a676c226c3b
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:15:02 +0000

    coat bright quantity comeback print

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
