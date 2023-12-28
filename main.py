e3f140794dc346748f895beb30fcc4f0a82da684
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:23:21 +0000

    steady polite brave pure pity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
