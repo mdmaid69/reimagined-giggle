b55fc95987d7c25c2cae6c59c10e11a99769fcaf
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:10:56 +0000

    maybe wonder reject against block

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_acceleration(speed, time):
+        return speed / time
