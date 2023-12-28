2a3648838dc549cc67e6a6188527ff4c68227b52
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:46:35 +0000

    that method everyone common much

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def sort_list(lst):
+        return sorted(lst)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
