b88d66babf6b4aa2dbeeb60a22af91b9c9a8e033
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:27:30 +0000

    study opposite might or since

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_acceleration(speed, time):
+        return speed / time
