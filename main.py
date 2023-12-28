26c0aa7b3c51fc576a3b8a5b417983340157eb11
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:08:41 +0000

    fetch language platform public vanish

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
