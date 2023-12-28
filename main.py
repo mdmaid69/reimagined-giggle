155e016b0211b7bf87fb5d0bee4b9fd86a11f9ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:14:13 +0000

    off along fair grow sore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
