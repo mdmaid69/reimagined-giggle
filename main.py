b31fd4ccda255f4c5ce70cb31176f1256b511c62
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:58:05 +0000

    attention reply demand trade ask

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import os
-print(os.getcwd())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
