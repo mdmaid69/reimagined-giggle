be74bf38873ed3ef937e3f581048b1097306cd1d
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:06:20 +0000

    smell save education lunch respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
