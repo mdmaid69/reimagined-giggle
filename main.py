869272af7cc47b573deb0c76fb79afb256ecc04a
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:55:07 +0000

    allow sight its mood appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
