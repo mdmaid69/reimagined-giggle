b520b7fbb9bb63caa67ecee8d1030aab38d78727
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:03:41 +0000

    add respect suck passage line

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
