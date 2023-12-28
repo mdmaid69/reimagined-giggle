cbedb84a1f2095b0256e2e7d60bc0ff97649dcaa
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:16:37 +0000

    form part combine under stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
