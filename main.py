700eb2c2cd3870f8cf639e90963a3861c05be424
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:19:56 +0000

    rubbish holiday Uu comeout section

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def remove_from_array(array, item):
+        array.remove(item)
