5061c6985b632108e458bd5d6e9de8d1e650e6d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:12:21 +0000

    precise again see thinkof Gg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
 def subtract_numbers(x, y):
         return x - y
