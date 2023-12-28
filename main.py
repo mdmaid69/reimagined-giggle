e2d64fc98f460acd633e0797980d5c6807bef123
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:29:22 +0000

    possession behind capital journey slide

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
