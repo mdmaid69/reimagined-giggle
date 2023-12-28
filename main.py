421913c41ca85bbdaa543e5759b4217ff3d56f77
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:27:06 +0000

    cost touch attractive nerve our

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
