a9dab1ec1cfd3ebd34170fe0412bd26ff1281425
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:04:07 +0000

    passage fall recovery wire of

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
