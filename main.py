b591f4bb9e02f611e6288bcd7bbb57ca72e8ba03
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:10:41 +0000

    maybe although standard collect glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
