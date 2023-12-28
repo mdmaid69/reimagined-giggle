3ab53490655a598a0c0ae38c935fe2317cfa38d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:18:38 +0000

    transparent simple general law standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_complex(array):
+        return complex(array[0])
