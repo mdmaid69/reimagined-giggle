dd241ba38a496824abe76bc363e64bbaaedaa922
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:45:05 +0000

    own plenty peace cruel difficulty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(5):
+        print(i)
