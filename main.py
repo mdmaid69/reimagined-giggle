3c4a83cd8388965c8ec6c5fe57d0cad64268deaf
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:51:51 +0000

    mysterious test later bad stone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
