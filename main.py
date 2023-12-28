973d4101eec6d5de5a0e6b7b7b5f09482c94d849
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:53:51 +0000

    watch combine criticize shopping gooff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def square_number(x):
-        return x**2
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
