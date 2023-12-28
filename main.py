9d130e048fda465ad6de621627b51ceb58db27fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:02:13 +0000

    excuse pile deal mark sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_base_name(path):
+        return os.path.basename(path)
