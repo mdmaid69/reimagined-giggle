e5fdccb76b3a6e1607c55037c12e16fe99923581
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:59:09 +0000

    land pass valuable lean recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import sys
+def print_python_version():
+        print(sys.version)
