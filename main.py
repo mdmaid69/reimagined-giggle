5e181c5c271a7d30b548a4f729699402d68bd3a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:55:43 +0000

    risk over example waste attract

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
