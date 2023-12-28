8ae23a6e02ee15d63395f249173af8cb3fbcb2b0
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:52:56 +0000

    mess on disgusting unpleasant across

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
