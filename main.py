62cee4490587546b5ca93df0c688b9ebf45e3450
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:39:42 +0000

    hope strange moon sea contain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_array_as_str(array):
+        return str(array)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
