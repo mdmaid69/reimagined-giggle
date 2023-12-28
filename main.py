fbae10d07f2e2a3863c109d146bfd6b73cd6c899
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:02:03 +0000

    attract cut Yy past post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
