a87d4d48898a84c6f64cc146d397dab3f00956bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:07:28 +0000

    prefer bath single great plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
