ac6f1bc014411c5abaebf4ec31d5c2a30697e266
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:13:37 +0000

    job need juice ilovedicks name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
