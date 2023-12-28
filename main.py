345fd6ffa397211d4e44a469b250a59e7c075d17
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:22:58 +0000

    relief style if only official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
