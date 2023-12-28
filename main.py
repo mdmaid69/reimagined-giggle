d7b2ef8410ec68c5885d4cd1a356503898c91425
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:30:24 +0000

    tomorrow slice worth side period

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
