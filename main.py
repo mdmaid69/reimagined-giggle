41bbcc61da36bd67905b1d761d8241952f1c598b
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:12:30 +0000

    zero Qq moon slip murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
