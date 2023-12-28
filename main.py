a4bc1be55cc574b1deed05efed00758934162e01
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:05:58 +0000

    apart some close pool taste

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
