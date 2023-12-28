b1902d087b2479f972179aa8dfa014bffc71c378
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:17:48 +0000

    care about speak shop ball

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
