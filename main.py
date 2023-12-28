2af81d5eefc2ab96228ba0197c8252b7883fe4a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:22:43 +0000

    spot show maybe apparently immediately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
