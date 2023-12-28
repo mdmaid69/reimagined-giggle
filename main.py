ed81927e29282fd0d2c7a2a3091a34bbe8daf169
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:40:20 +0000

    over tooth design respect snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
