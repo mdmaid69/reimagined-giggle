9d91c7c74037c53b2cb46b1d01ef7431499c3411
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:52:22 +0000

    generally can fly own partner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def cube_number(x):
-        return x**3
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
