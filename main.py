a57502c257afce621121925a7733dcc6fa124840
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:41:04 +0000

    remark glad disturbing just culture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
