f61582d207f3c9c732737cc8fffd6d674d2efdbb
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:28:09 +0000

    bath bottom people respect part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
