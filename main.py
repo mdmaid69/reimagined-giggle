de8bc171167d24d7744e5f6dc373420a36d48ffc
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:00:38 +0000

    fond shopping simple rich clue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def get_array_as_int(array):
+        return int(array[0])
