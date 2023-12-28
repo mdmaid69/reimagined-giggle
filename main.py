088d9cdb6160e04adcbe40b540121f9dfcc38154
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:16:33 +0000

    order invite beside furniture before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
