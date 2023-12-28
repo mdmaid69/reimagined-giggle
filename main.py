78022b4f64d296cc9f9a63efef7ee637bd0485ff
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:12:35 +0000

    embarrassed burn simple curious know

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
