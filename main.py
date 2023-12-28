e178099b0ca9ac5975682aeeba95830ff9703191
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:10:57 +0000

    honest pass foreign future programme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
