5274fb4adeaad7fd8db20f66e06e84a656ef4100
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:33:09 +0000

    lean post goon instant duty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
