9720d056eac5a5eb23cf2139f0603caa470c9121
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:35:44 +0000

    used kick type rubbish damp

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
