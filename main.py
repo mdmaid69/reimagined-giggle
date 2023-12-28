1edf4255621ec5c041478a6deb60980551abe3f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:33:57 +0000

    impatient under everything service violence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def count_elements(lst):
+        return len(lst)
