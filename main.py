3827af837dbb368a12378931bf9edd13b52e892f
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:03:50 +0000

    squeeze trap lie side where

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
