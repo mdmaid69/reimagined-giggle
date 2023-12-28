cf82e447f1de7ee8c9982dfacff9497c695f4033
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:29:06 +0000

    afraid so business play rarely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
