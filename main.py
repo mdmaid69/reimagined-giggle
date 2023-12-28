4eb641f5268b85fae692f737d545949056649efb
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:25:31 +0000

    guilty blow forgive proud that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_pressure(force, area):
+        return force / area
