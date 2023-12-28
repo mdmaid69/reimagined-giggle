9f8e40edf5a501b9e350ea01193437004c695ea1
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:32:27 +0000

    cut commit chin control hope

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
