4ff3e75c0886793dbacbb96e8f1bb40b78649440
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:31:38 +0000

    match tie relation Itself apparently

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_bool(array):
+        return bool(array)
