2098021af8bda77fbadd57107a28b32b09ece9b6
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:49:22 +0000

    beside memory complain blue tip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_speed(distance, time):
+        return distance / time
