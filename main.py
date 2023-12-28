4204953d3be27d85c3a296c678660f4004ad1499
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:20:33 +0000

    another bunch connection bad home

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
