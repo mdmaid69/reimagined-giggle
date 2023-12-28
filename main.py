88d3d0387a27d6c3fe0f2e3f9861de8b89993aac
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:03:35 +0000

    convenient street grow consider think

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
