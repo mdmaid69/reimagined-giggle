9e3142a23dd97d65e48725964a401f44be174d57
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:55:49 +0000

    wherever effort getout grateful equipment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
