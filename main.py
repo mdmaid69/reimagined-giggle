d317a765137ffe9d4fea7703cfb96c8fcda462c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:22:49 +0000

    quantity then dry thinkthrough workon

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
