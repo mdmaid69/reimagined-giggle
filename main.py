1645cea7ee0a06d966232992a5d92f954b6cfc6f
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:12:51 +0000

    tax tough genuine imagination reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
