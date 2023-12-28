c1f3b033d172d414446da61001b83b40cdf27858
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:44:56 +0000

    beside what confuse door fire

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import time
-print(time.time())
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
