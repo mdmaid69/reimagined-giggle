ad93ad87c9545e92852761f9417bfcbd66ce9d9f
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:21:21 +0000

    complicated deliver good splash disappear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def split_path(path):
+        return os.path.split(path)
