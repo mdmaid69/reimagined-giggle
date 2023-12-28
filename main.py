a2d20795899e60422a5818a74425ce154b936bae
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:45:59 +0000

    Tt idea respect turnback crazy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
