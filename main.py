29a10ac7229fbe6b096a0e870159f1f2fcc4bb0c
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:15:38 +0000

    dick computer possibility not recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
