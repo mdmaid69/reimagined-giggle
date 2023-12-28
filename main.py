bccf7b6aa1f5f9d7a184db8ed1ec54a57dc1893a
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:18:19 +0000

    money hurry plastic onto squeeze

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
+  def plot_graph(x, y):
+        plt.plot(x, y)
         plt.show()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
