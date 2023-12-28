bbae1376ccb5a70673921ee090ef7a987e64f09f
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:25:10 +0000

    insist disgusting decision there passenger

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_work(force, distance):
+        return force * distance
