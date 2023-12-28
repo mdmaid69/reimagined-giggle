acb9a159b56dedbc474f5a146c5efe4e5ca51b97
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:34:28 +0000

    television behaviour same family begin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
