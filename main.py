58969660a3184f32155f71659d454b1380450389
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:08:08 +0000

    before cool ability meeting purpose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
