53e34c350091985382d802d34c7b78ee740e8e92
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:29:01 +0000

    thinkover outside everything correct blame

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_volume(length, width, height):
-        return length * width * height
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
