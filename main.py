56c0f941bd2af36a89d665977f6c7e072c298c16
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:42:48 +0000

    interfere bush build hang so

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
