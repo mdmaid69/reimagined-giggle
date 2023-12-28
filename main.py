0d72e5eaedfc331030098d9464de16937550f193
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:24:25 +0000

    distant pause tidy surface cry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
