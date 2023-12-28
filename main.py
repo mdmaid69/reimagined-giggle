b2413be00ab0b881f7325be38235b0e45330883c
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:28:29 +0000

    be grow leg tidy weekend

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_tuple(array):
+        return tuple(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
