2787d45cf7687a6504b36659f87b6df6b8ad8695
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:57:57 +0000

    sail visit public soon material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_as_list(array):
+        return list(array)
