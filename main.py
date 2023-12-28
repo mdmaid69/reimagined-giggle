dcf4faecb90752ecc953806c240765da702f749e
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:09:34 +0000

    collection round hold wet independent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
