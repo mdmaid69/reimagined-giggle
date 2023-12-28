f2e0b2672e808a286045c173adeab2ebb4a2ab62
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:06:06 +0000

    reduction taste school exciting attitude

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
