13105ff077e421cf41559e9c9bd0c8adad9e906b
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:32:53 +0000

    him bit later show walk

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
