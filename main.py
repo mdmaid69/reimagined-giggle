1e3d937b8edf626a729410313e0acbc3ab0aea75
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:04:44 +0000

    run lend precise different let

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def calculate_area_rectangle(l, w):
+        return l * w
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_sign(x):
+        return math.copysign(1, x)
