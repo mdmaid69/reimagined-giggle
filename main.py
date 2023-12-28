6fab00a4f828557ccb370f9121d51ceb578e39d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:51:56 +0000

    Itself keepoff nervous unite thorough

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import datetime
+def get_current_date():
+        return datetime.date.today()
