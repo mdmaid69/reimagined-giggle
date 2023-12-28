a0364fce390903598f8149fb5f63481dfecab3cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:35:18 +0000

    sea science lunch judge present

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
