82536bc7c7ed34eeba08437b33147c10e8ee0d9c
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:18:18 +0000

    trap indeed distant worst rarely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
