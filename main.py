f143e5a4085d5bbe3e4a6b38fb9df8337963494b
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:18:52 +0000

    future guilty congratulations provide sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_item_count(array, item):
+        return array.count(item)
