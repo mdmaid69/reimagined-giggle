d81fc2e581646d43335f85b12b87f38c9086bc2e
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:40:05 +0000

    polite kind total great communication

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-print(sum(range(10)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
