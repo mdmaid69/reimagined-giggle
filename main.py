a71e96406c0621bf2ae0581c9822bddf18f28332
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:46:08 +0000

    row wait hit score tax

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+print(os.getcwd())
