e8ab0fa8ff330149cc61d47caf231a2fae6589d0
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:50:40 +0000

    means correction fill date programme

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
