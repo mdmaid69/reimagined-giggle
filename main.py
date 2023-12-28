c1bf3d56f88bf206468e10e10636171fc1e928f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:00:35 +0000

    bar tragedy belief strike keepoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
