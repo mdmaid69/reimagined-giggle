7c6c3af2f9447a11f101c46d56372169d908b953
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:33:40 +0000

    division bringforward power war while

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
