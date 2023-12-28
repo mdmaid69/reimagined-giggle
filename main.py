f8aae0051ad055566d788907be95fc8fe51d76ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:12:10 +0000

    maintain face story any beginning

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
