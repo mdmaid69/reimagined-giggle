e6e57322233d87a709c1b00d9a1d05011aaac83f
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:38:47 +0000

    talk violence confused criticize floor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import sys
+def print_python_version():
+        return sys.version
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_memoryview(array):
+        return memoryview(array)
