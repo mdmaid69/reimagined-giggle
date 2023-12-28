8c69f2135ec8a659e5985b3a79081e21fb213d32
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:25:12 +0000

    there which circle very community

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
