bf81f8d7ff15339d410b16399b857b7ca599cba3
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:32:54 +0000

    what tend breakinto letter face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_as_int(array):
+        return int(array[0])
