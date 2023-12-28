79de234467a15b2278854ae40a112cb5821c41d4
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:15:12 +0000

    breathe any complete mine hole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
