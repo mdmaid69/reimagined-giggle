a7c2a7804fc752a59665ec0244ef8e0be47ef2dd
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:48:05 +0000

    beat company emotion also again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
