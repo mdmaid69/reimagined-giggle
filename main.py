4199b9ec39c9d9de1487617917b239257715eab0
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:01:44 +0000

    equal criticize programme breakfast trick

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_as_list(array):
-        return list(array)
+def convert_to_hex(n):
+        return hex(n)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
