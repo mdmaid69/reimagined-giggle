e6c62fa7457896cf24192add29491d1c213dd572
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:56:56 +0000

    activity they slowly idea better

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-name = "Python"
-print("Hello,", name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import numpy as np
+print(np.array([1, 2, 3]))
