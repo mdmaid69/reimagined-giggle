d66d82c36c68238e765b945aed5125d003034d11
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:22:41 +0000

    creature protest pull afraid difficult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
