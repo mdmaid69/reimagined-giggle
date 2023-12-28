fc292c1a2a999402dcfbadc46c3d7b83790bbdf1
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:37:33 +0000

    free possibility dry region record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
