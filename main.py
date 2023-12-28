535e8486ba7e953fe094f37f5b584582a92d8c1d
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:44:33 +0000

    order workupto road lock him

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+for i in range(10): print(i)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
