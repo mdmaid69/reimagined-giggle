6e7dfcb20a4e28f99f98cf0713862e55ff64ec40
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:01:02 +0000

    uncomfortable poor splash spray underneath

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
