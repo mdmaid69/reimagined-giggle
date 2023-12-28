e95d69b960ab9aee02d70e082dd2c6ff8387db23
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:55:56 +0000

    curious divide among common touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_work(force, distance):
+        return force * distance
