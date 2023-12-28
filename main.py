87a678d2bfede1b53560d9b5ad0b125cf8674410
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:47:33 +0000

    split score cheap breakinto medium

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
