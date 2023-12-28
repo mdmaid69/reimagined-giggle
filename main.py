371a77375b7f95106c256188f51876115fb15a05
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:12:07 +0000

    else post official ours use

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-for i in range(5):
-        print(i)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import collections
+def create_stack():
+        return collections.deque()
