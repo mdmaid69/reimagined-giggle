37123be479185fd48180673b30d71c6d21f985e1
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:23:14 +0000

    deal worst Vv difficulty favour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
