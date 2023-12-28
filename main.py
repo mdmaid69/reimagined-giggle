50ee870deef6e0ee3cd3374f30fbb1a1e2d97d32
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:29:51 +0000

    before fire cold everything generally

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
