32bab1bf3d8c84e02ecbab4ec64cfecd9552b0c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:29:46 +0000

    old damage to getout check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
