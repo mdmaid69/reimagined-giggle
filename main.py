3de78caa9c585d794f9f4ecf9f80b97eabb25354
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:02:56 +0000

    special measure do thinkof over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
