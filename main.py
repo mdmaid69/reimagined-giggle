58f7773ebd36a21eda9bb09e4f2268c7b8fed545
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:07:54 +0000

    base Zz workout nerve future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+def get_current_date():
+        return datetime.date.today()
