34e8019c68880d75e9571d798b9237fe902acbf0
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:06:08 +0000

    platform week their declaration grateful

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
