65c5173b98dba3d8b5e490a1cba74bd643f70e96
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:09:42 +0000

    happy might before lock necessary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import logging
+def log_message(message):
+        logging.info(message)
