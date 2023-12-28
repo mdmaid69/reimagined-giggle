e5e4c04c5e63d2f1d54c5bdf5da885efe0afaa7c
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:23:45 +0000

    mark band pot custom soil

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
