a280d93bcc6c40e47191cda6df95874b69f29c11
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:07:59 +0000

    float calmly name dinner war

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_acceleration(speed, time):
+        return speed / time
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
