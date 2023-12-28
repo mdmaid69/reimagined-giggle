6a3f4a92ea535cbe53ee43775cae4f3cdb4cfd99
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:45:48 +0000

    safe none inquire extraordinary great

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_as_float(array):
+        return float(array[0])
+  def remove_duplicates(lst):
+        return list(set(lst))
