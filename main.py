cc403dffe2847c4241f14e51ea2c389d320173f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:00:48 +0000

    band point bag strict present

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import datetime
-print(datetime.datetime.now())
-import tensorflow as tf
-print(tf.__version__)
+import sys
+def print_python_version():
+        return sys.version
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
