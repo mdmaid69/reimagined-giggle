8b3cf423fc8f086cab53a54b37b263f7aa772b16
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:19:03 +0000

    just match convincing considering gift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def find_max(numbers):
+        return max(numbers)
