5f6e19908d76742af6364dca9391f298976edd1d
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:56:47 +0000

    through hit joke arrangement godown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
