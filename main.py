35fcb46ddc3f7df9b955e025f52b003f314670ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:57:34 +0000

    past after moment unpleasant trouble

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
