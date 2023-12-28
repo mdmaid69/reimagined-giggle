52440baeb627ef6580821f0b1bd0cbc93037f67d
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:08:21 +0000

    back than attach thinkover settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
