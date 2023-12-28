c6e88a8dbe02991681f388e6a03031550bf784c1
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:05:16 +0000

    set value shut shake winter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_area_rectangle(l, w):
+        return l * w
