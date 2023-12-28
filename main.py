f9e3dbe28bdd158997bca79cdcd8f6903088c612
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:41:45 +0000

    strength container treat healthy lonely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_max(numbers):
+        return max(numbers)
