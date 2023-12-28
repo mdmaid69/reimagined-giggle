d961b00e466bbdbcbae7122c5580e79c9f91e26f
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:43:56 +0000

    its tonight stretch bath apart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import os
-print(os.getcwd())
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
