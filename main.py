b276ac4a43e91e3cb8e222a780398334339b68ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:39:17 +0000

    high part degree satisfactory whom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_repr(array):
+        return repr(array)
