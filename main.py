025a8ceb5896b13477891ad66eb81920aee3d6ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:07:40 +0000

    opposite hope near discover sister

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
