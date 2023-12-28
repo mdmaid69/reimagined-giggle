a1700d75581f5f0f39242d1b0c6406facec87c1e
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:40:37 +0000

    cross glad idea appear ourselves

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-print(random.randint(0, 100))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import os
+def get_current_working_directory():
+        return os.getcwd()
