07731ff188bc4c7e538ea5ff0140f430a20c281a
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:09:12 +0000

    cheque match inquire build remain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+print(re.match("h.*o", "hello world"))
