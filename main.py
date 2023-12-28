ae75353871d391e517020d4ea90a0f577845a3cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:58:13 +0000

    valuable mean tear top trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
