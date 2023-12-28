6a9f4a449ccc7bea30527e70cf3336e339bfa267
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:36:51 +0000

    hollow famous reduce thirsty get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
