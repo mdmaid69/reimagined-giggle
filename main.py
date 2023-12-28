49117454690588f708dc76a9c82b80684f29419f
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:56:43 +0000

    talk odd mention area game

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+for i in range(10): print(i)
