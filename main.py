af9f2c8a13c2be89204a41e564a5ab4339e696bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:23:31 +0000

    left fall will breakout radio

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
