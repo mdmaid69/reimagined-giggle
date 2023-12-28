73ddd7af5369e5a425023f64ff9a37cf514bc0d5
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:52:58 +0000

    holdout crash listen regular normal

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
