7b4fb8f7eb87a851ad52adb519e5e3ec6549ab82
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:53:40 +0000

    pretty Nn man fill apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_density(mass, volume):
+        return mass / volume
