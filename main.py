cd3cb660e08f413f5dfabbedc6f8e203c9cb985c
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:19:33 +0000

    well estimate angry runinto front

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def subtract_numbers(x, y):
+        return x - y
+import sys
+def exit_program():
+        sys.exit()
