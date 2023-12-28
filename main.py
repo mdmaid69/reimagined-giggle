a19f396df7619afc1dda18ede92434acd76baabe
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:35:05 +0000

    score tap calloff no deserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
