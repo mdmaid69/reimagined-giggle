863454ea5fbb55755441e1ac6fbeb044966a4797
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:42:26 +0000

    sweet union notice food public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def add_numbers(x, y):
+        return x + y
