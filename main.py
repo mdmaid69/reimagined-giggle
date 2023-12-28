cdfb9328f732e76cfb79c00d0f2f347a16173a92
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:31:36 +0000

    true design pretend mail church

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
