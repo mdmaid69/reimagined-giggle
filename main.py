a9ee8aa0ca61de54fe9b93cd295c9bb0253b5da2
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:35:42 +0000

    warm still complain laugh anxious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
