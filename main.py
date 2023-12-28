cd76f243cf212f0a29c03969bde9843b6a61cda5
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:09:31 +0000

    relation always friendly forgive pile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
