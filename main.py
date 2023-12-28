c5d0a9590058bdd981b1aec1b7d2e26bb1f25816
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:01:20 +0000

    separate sting born accommodation date

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
