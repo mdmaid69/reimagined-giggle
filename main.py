6a914d53ffd9b80f1dc7e83cf4613c45a4b66ece
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:18:00 +0000

    vanish sea steam tune sticky

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
