f8be41ae83d4fd130c763181f085d280d8cde5fd
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:01:10 +0000

    extremely point display peaceful uniform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+from collections import Counter
+print(Counter("hello world"))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
