3987d6259a66e994d1534b2e933b86a56d2c57a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:12:52 +0000

    care error split radio point

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def count_elements(lst):
+def find_unique_words(sentence):
+        return set(sentence.split())
+  def count_elements(lst):
         return len(lst)
