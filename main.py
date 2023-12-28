8629ee9fafd9400705d92765f233d784509d2e96
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:01:48 +0000

    hit woman heart lesson crazy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
