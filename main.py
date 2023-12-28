963a46e5824b4cae7dacdb7fc281fdcd9f2608a6
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:27:38 +0000

    about before generous shout love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def reverse_string(s):
+        return s[::-1]
