be52b33b26eb1f1e831d1d6d8ce6afa19b2191b8
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:16:04 +0000

    perhaps sorry concentration goaway tax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def get_array_as_list(array):
+        return list(array)
