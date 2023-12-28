c0db08204b3a7f4f923c82135f719a7263ccfd51
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:31:28 +0000

    use hole sweet attitude transparent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import collections
+def create_priority_queue():
+        return collections.deque()
+  def convert_to_binary(n):
+        return bin(n)
