ed22baa158f811107929944f3ecb7b11ead62959
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:17:37 +0000

    window clue buy considering between

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_current_directory():
+        return os.getcwd()
