92ecb6f00f2734580b603a0279fbea78daa7e72a
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:20:44 +0000

    closely except apologizeapologise strong thinkover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
