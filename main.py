ed1190c1b194c03e42e9feeec14a1964b286c7c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:55:58 +0000

    getup share become once modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
