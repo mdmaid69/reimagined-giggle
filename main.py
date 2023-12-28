c1cc5c09e3f13f8c7e0ec5e9259ea6f795d36f75
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:29:50 +0000

    way activity legal beat crime

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
