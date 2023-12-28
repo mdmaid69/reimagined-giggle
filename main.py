3913a96e3a9dd8c95bafd137fa374dcc08da9c21
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:22:26 +0000

    copy eat train show patience

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
