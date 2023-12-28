2ebb92f7a3f9f7e19f9bac768caced85b16b3de7
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:36:06 +0000

    soak back winter open regard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  def is_even(n):
-        return n % 2 == 0
+def calculate_factorial(n):
+        return math.factorial(n)
