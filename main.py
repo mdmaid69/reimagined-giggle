dff3d8dd0f21c4257d02e4e2411a38eec53ce994
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:37:11 +0000

    proof pleasant runaway education juice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
