2acd5a871a06c26dc407b73823c14f1dc986dffd
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:22:21 +0000

    discover shut necessary foot opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
