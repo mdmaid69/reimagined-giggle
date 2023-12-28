888542d08d0289d83b00e62cf25514c148a6e700
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:38:05 +0000

    onto relief alive swim catch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
