d66ca3523fc97f7f397d09c4eb54f6e0b0e851cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:00:00 +0000

    tool butter sleep tap noise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
