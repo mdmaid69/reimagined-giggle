c67f616dac85afadd2dd989b5dcdba5b473bab52
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:07:44 +0000

    moon heat surprising break abroad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_pressure(force, area):
+        return force / area
