0847ca058a01a3c9099f6e6b80b80d4375e27d76
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:33:32 +0000

    some regret announce each fond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import numpy as np
+print(np.array([1, 2, 3]))
