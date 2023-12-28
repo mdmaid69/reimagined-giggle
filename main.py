848f66ba5fa81fe5092b3deedcf029e3ebdd7526
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:44:03 +0000

    which tape like stamp risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import sys
-def exit_program():
-        sys.exit()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
