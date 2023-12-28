1befa44c8de78e3ed6d04c63f06a716c3e599ca1
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:32:10 +0000

    one well beside bite achieve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
