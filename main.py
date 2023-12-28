e205b7aae80a7db3c4815489b6e90557e5c947b8
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:59:03 +0000

    responsibility needle light clever summer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 n = 10
 print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
