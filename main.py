78b8917e683895859945a2e0ad99fbeebf06281c
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:51:15 +0000

    door regional praise owe comeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
