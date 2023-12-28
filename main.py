f5d74e2e6481caedfdd9f15c3cbb4f7896f19657
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:12:25 +0000

    explain comeback scream sick vision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
