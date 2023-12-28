87b4c573dbcbc450f5b99155cca13b5af3fd732f
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:19:14 +0000

    gate deliberate everywhere similar turnup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def find_min(numbers):
-        return min(numbers)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
