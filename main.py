2c628e36bdedb6484899b1b4a75be62f5ee59f2b
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:42:16 +0000

    foreign keepoff they heart history

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
