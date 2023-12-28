6031a149081eee806a6ceb3cc78a143ff09ce0fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:54:37 +0000

    claim another forgive pick bin

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
