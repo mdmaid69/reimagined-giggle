0debc72bc88dd61a0c5218dd4f2e2fe8c1108c6f
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:45:11 +0000

    special neither in better pupil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
