d85dc26d11c590f1113efe286eb3c65753f412a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:32:13 +0000

    fight silent wonder noone successful

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
