ed43a66690fd47efa5f924a34e6a4c1664213bb2
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:20:45 +0000

    since peaceful business confused scared

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_length(array):
-        return len(array)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def get_array_as_set(array):
+        return set(array)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
