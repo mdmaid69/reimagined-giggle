7c2c893dcb8c4f93771052de389b95431e21dcc0
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:52:05 +0000

    achievement shout excited picture just

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
