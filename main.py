26c36b6707f0b6dc952c689fc04839e29ba31a1b
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:05:13 +0000

    shop before avoid the float

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def sort_list(lst):
+        return sorted(lst)
