ac490d3656f9d77bbb80ae4c967c79d370650ab6
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:03:32 +0000

    mine ashamed passage smooth appear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
