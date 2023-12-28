caac614d9c452f91e6efdd11e133a1dd083c9125
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:05:05 +0000

    solve couple pressure find fall

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
