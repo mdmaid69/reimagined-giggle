cfa591744398b8584a20d3fbae3df12b3c15fa49
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:02:12 +0000

    certainly concerned wish where together

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+for i in range(5):
+        print(i)
+import array
+def get_array_typecode(array):
+        return array.typecode
