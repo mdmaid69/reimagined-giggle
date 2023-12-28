33a6590e7f7b5724995c0acefe6cd42f26ef54b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:44:26 +0000

    far claim sometimes strike snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_volume(length, width, height):
+        return length * width * height
