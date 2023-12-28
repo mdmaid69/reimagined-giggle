550e3b5d677e2f08709780b0d61bd93767fcadb2
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:13:44 +0000

    injury whoever judge satisfactory be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
