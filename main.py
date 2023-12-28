da41292c58cbecca13274ecfdb4004c522c1fd22
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:02:12 +0000

    patient big actual call handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def remove_from_array(array, item):
+        array.remove(item)
