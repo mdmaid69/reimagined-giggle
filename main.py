4959e97e75551cb75c658d82f3a5742feb3b5448
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:25:31 +0000

    least know slice practise even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def clear_array(array):
+        array *= 0
