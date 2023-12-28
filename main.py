121a22cbe057c71f7684ce1106c7eb4cbd75fa16
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:39:04 +0000

    second swimming squeeze ill mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
