6fc373f39ec9733fd05ec72d86827a87dd1c03c1
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:02:36 +0000

    ring stamp mean middle advice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
