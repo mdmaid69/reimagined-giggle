cd2716b556b15dca837e979fc11848eac488d4b3
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:09:34 +0000

    unless storm without alive light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def is_even(n):
-        return n % 2 == 0
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
