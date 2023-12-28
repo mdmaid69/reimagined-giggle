eef11eb31583d2b172f0d2ccafa5faa031b4b071
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:21:15 +0000

    stamp people part full lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+print(math.pi)
