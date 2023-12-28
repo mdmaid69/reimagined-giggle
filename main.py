a04e20d93dc6a5d82a72958d2dc45f3c21b00dcd
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:35:22 +0000

    what love achievement correct inside

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import math
-print(math.pi)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+print(json.dumps({"name": "John", "age": 30}))
