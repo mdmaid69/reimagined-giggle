54e54a2ac876d69f38ba4b71d66ba600f1a5d4d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:06:41 +0000

    protest scrap hurry she blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
