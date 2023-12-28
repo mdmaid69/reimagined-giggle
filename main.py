77d6bce5cdf2ac5493fcd2a32314a1868122b30a
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:04:05 +0000

    event part offence unit thick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
