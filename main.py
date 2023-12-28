a97b98611d5f6b98ff5c7bb0c058ddbbac4e2991
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:05:53 +0000

    crime finger wash concerned throw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
