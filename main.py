aa3314ef13cd79934453e4d2b523ae82307484ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:28:56 +0000

    to name plant language chance

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
