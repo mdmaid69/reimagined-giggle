937103304c7254b44e62fb88d6ac3b23647f0ed9
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:35:11 +0000

    charge noone pleasure move view

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-print([x**2 for x in range(10)])
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def find_min(lst):
+        return min(lst)
