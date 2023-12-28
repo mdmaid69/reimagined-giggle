188eed5356771b46d9d0f9e9a73ae7766fe3a2a2
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:47:40 +0000

    yet something strike appearance purpose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
