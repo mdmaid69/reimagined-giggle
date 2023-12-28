f3ca23dd3589604ff531cd14005944ef1269e557
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:17:17 +0000

    for escape hit react low

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
