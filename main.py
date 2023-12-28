d8b45bc610fb1e9156cc379325623376b397cd99
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:02:21 +0000

    use string worse book factory

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def count_elements(lst):
+        return len(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
