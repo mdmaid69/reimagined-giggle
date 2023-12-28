fb7f0eca089afe7df9e68689e75aee2a5a21ced8
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:57:55 +0000

    look basically short forever takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
