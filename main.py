86e33f1a355e8312ef1e5d07e11df7cbb817ede0
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:41:59 +0000

    month run different start congratulations

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import getpass
+def get_username():
+        return getpass.getuser()
+def find_max(numbers):
+        return max(numbers)
