05469f477cc6fb2bae89d6d9ce120f7eda3eb729
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:00:13 +0000

    advice full succeed guide shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def convert_to_octal(n):
+        return oct(n)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
