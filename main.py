38981c9382748e0201fcd048ba36027cc2b5d4bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:19:19 +0000

    dick mouth private except plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
