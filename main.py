298f5511c8d7fc1f96afa8c205efccbc1f70be43
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:50:41 +0000

    bin think stage taste with

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
