ffc2d063fe132bdf20173d44212a5214baae2bbc
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:19:56 +0000

    reduction physical build little word

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+def divide_numbers(x, y):
+        return x / y
