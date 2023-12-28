5443ae43dabaab413f078b0204cb99b315f4f4a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:50:30 +0000

    attempt range cut climb left

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import os
+def remove_directory(path):
+        os.rmdir(path)
