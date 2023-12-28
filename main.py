ce5a24caf2154ccd230a30104ccc21d1b8a374bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:37:11 +0000

    original expression turnon union achieve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def remove_duplicates(lst):
+        return list(set(lst))
