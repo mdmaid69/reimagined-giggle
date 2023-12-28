d871401054f0a93a0287861fb5f215f98c051607
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:38:41 +0000

    negative error mention believe organize

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
