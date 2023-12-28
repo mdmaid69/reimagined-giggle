4a2b7d0cd04ed006cf48574b8e0dd04922e77f87
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:25:42 +0000

    yet soft tape rather sell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
