5d4200c4e592eca4c756df97666667a0f932eef5
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:24:36 +0000

    passage 565end treat who ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import sys
+  def get_python_version():
+        return sys.version
