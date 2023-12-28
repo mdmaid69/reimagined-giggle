8fad7237cc3231be965febe577b4ce1fc2717958
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:28:55 +0000

    means pupil kind battery belong

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
