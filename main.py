066cdb5044a82976b3430b76f0e00a84cf1d13bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:19:53 +0000

    wrong command deaf teach pressure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
