4d69ad942eb1b9a7c6ef7cde91dd4e59a22bff26
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:12:18 +0000

    letter whatever under lot herself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
