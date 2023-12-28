c5d302a35c95b6adf3bf62e2822c542c6f539468
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:23:46 +0000

    tall fold raw off anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import heapq
 def push_pop_heap(heap, item):
         return heapq.heappushpop(heap, item)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
