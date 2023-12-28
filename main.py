d3b6a361c009a493531722667901b08507ade561
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:08:32 +0000

    case satisfied uncomfortable clue guess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def count_words(sentence):
+        return len(sentence.split())
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
