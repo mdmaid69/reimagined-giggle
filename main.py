f4346dab8b2e967253d6c7ae31a4e5378c6a275b
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:20:44 +0000

    process butter your explain Oo

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
