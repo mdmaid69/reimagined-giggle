b406c0f81c01276a5226a25a654ac75b56fb737d
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:06:15 +0000

    unfortunate remember instrument no lost

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
