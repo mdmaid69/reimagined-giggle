e15835f46784b3ccad434d6a8a67bcf65ae81ac7
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:51:42 +0000

    top breakfast night raise lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
