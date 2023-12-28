9e03371a3c2abde8784004deddd3e0b97681c6f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:44:09 +0000

    hear forever intend shelter comeup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
