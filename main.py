3fa4f93621dedb9e85fba879157a9842c9de54c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:21:10 +0000

    serious usual show identify strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
