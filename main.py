0fc857880bd1e68ceddf2933e98bfa4ca79e42bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:00:49 +0000

    total call supply control visit

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
