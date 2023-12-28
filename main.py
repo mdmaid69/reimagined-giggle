0f30bbfe2c687ba32b7ec72be9051f3a3b17da03
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:09:22 +0000

    live confused term foreign cover

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-print(math.pi)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_hex(n):
+        return hex(n)
