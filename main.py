562cdb7064fb2e5b1b133b634f490c9a0bf8d922
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:54:53 +0000

    generous attention trouble chance 179bite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
