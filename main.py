71556b4605c80dab121fdf8df294ac1b9a6da136
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:14:18 +0000

    fear mood culture behind fit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def divide_numbers(x, y):
-        return x / y
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
