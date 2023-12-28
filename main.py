dcc99098b71c790a004d1110bf1bb48442152e74
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:59:41 +0000

    safety discovery past plan cross

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
