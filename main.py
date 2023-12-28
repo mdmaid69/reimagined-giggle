c9a83b127b7cc2d3b51eec91e7091b42dbe7baa8
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:10:59 +0000

    watch moon entrance shame official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
