fdf80869cc43d67384742a4513d3b2797344a423
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:23:20 +0000

    out main heavy adjective meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_current_date():
+        return datetime.date.today()
