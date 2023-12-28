95925e1c2601ad9c55c7ead70348350525bfce04
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:22:57 +0000

    Uu regret hollow cap mention

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
