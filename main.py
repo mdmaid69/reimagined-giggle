fca889aff4a6237cf0942513994f47a3f174b846
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:48:07 +0000

    slightly shadow line Dd remember

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
