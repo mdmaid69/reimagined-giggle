fc8d7d0c992463556598daf771746e64df4b04cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:53:39 +0000

    chief fast hungry smile fault

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_acceleration(speed, time):
+        return speed / time
