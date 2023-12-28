bd378c69364f5ecf83fb012ba7a00c44b96071f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:12:19 +0000

    criticism fetch which whisper bottom

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-print(datetime.datetime.now())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
