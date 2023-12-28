b949d0f9a666c51fe8bea2e14d4e69c40e143626
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:39:49 +0000

    female petrol feature keepaway fence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-print(time.time())
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
