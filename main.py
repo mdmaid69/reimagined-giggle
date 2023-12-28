4f63addbca6e65f4558761b7a399c123d218478d
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:57:58 +0000

    cap few sky listen condition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
