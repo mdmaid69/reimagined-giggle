b0a523d88ddcd377eb556cd9b98f60247d492d33
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:11:35 +0000

    advanced dry shut mysterious hour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def remove_duplicates(lst):
+        return list(set(lst))
