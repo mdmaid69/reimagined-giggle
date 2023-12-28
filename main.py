5dc5f40ac2bc43b20f29af41e129d899f6228e2d
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:49:25 +0000

    breakout spite time exactly of

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+print("Hello, world!")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
