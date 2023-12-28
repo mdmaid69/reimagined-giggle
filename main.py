332db69883f2a80bbabde60ae3f01699240c78c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:04:31 +0000

    theirs row die how than

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
