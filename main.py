7b27a11b72d4d88e8951e558493fa317448c2377
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:04:08 +0000

    offence sleep yesterday moral dream

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
