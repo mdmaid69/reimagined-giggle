0d219bf00dbbd5b9900d9ab5f3ae6337ff323932
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:55:46 +0000

    nature address middle willing traffic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
