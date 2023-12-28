affe7fe48c00abc51f32bb324ae2778635b9afa4
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:25:57 +0000

    best to control period help

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def sort_numbers(numbers):
+        return sorted(numbers)
+def remove_duplicates(lst):
+        return list(set(lst))
