0ddfb3bf9e215c6abcb8e26a3a3690bc7e63d78b
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:28:09 +0000

    tooth drink patience wash sting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
