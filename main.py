59d5f37e3ef03ed43985771f81ae7f8bcf3ddaf6
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:21:47 +0000

    hurt know force price close

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
