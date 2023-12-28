77330052b68ba59f3549f45a7025b8c76212b2d5
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:51:20 +0000

    prevent drink weekend downstairs experiment

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import re
-print(re.match("h.*o", "hello world"))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def convert_to_binary(n):
+        return bin(n)
