1791761cf556752ddc53e8c82842e8310fa7ea26
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:47:54 +0000

    floor it typical callout loss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
