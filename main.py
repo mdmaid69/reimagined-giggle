169fa5d8ad45e8c9e5cefe2a33af2731afbebb79
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:39:22 +0000

    rank paper though have translate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
