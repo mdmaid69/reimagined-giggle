415f66bd2c650d903b026e9694c085a824300263
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:08:41 +0000

    exhibition dress cover trap shine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
