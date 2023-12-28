7f1a32337211a7a5ad86ac3bb487a5b3098cf359
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:20:12 +0000

    strong next laugh what gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def get_array_as_list(array):
+        return list(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
