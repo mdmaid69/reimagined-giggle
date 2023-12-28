4ff8dbd9e3ae253845e17b57ec55500fc933d1b3
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:48:59 +0000

    while 571enough mother born release

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
