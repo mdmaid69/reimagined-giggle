4ddb8049e9639703b22923200a36a052507bae48
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:35:39 +0000

    line basically earn zero rarely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_speed(distance, time):
-        return distance / time
+  def get_current_working_directory():
+        return os.getcwd()
