9488beac94168fa99f48dad53d230b4c5ed491a1
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:39:23 +0000

    plant Vv call experiment fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
