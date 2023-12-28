e8c936dc2e8ac417e5260b548313bb72af1633be
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:04:40 +0000

    advise spin Vv height beginning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
