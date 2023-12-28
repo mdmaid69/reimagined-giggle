dff23af5efbe0d68cbe764423a5aae40acb6153a
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:08:58 +0000

    mixture track risk obtain date

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
