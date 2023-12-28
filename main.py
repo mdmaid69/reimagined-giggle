1633bafae9a7bd4a36f345f14aad70b9a9873bc0
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:14:12 +0000

    contents that rescue story radio

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
