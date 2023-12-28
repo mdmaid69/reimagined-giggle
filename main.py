79193f83665949423196fef70af1f51398f9c43e
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:28:32 +0000

    rush vote yesterday supply class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def subtract_numbers(x, y):
+        return x - y
