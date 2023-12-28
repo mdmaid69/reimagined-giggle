26fecfa0c6ed3a3eb682f614399bb9505cf06381
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:45:38 +0000

    turn otherwise her through wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
