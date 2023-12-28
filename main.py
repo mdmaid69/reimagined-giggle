3eebd36239c66b8889614754d4c8dfe0a7dc3046
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:55:42 +0000

    wide fact plain callout away

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_work(force, distance):
+        return force * distance
