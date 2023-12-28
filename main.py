fc0f1b11e13eb8500c5fe4fa086f08e113036d19
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:14:53 +0000

    stage shine obtain steep thankyou

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
