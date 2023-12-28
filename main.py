d640718bd0aa169e33c5b4860a3d878a975020bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:05:09 +0000

    town contain advantage fun usual

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_float(array):
+        return float(array[0])
