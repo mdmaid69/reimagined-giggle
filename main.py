623f77b86b50ddd4471c17db0c968ca672e83308
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:08:05 +0000

    dig bang central tone walk

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
+        return os.path.getsize(file_name)
