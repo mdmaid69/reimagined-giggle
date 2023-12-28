91e60434407f4a6e41b62993ff6782847ade4761
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:33:55 +0000

    hurry clever rub count fast

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def add_numbers(a, b):
+        return a + b
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
