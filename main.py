0732251693e16461591983838075bde715f21368
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:04:38 +0000

    rest paper cough loose announce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
