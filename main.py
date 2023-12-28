0d17163981f6862a964fd7f1ead89c9c2901af1e
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:45:48 +0000

    normal pull along breakthrough ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
