aac91c5f3eea0a076cbd48e36776f4e511c48b5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:23:56 +0000

    total closed except especially connection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
