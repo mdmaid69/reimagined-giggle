f67966d90cd2d8e76b4561509d3dc3bdc94bd060
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:58:52 +0000

    Qq brush bus also wave

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_area_rectangle(l, w):
+        return l * w
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
