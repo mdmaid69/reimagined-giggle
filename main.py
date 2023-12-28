6357a794bf8b570681d75568bd6b96a636c87c10
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:58:16 +0000

    house while further after habit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
