a218e67cbf787d7d5f0a51df1afada04a8fd74fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:18:14 +0000

    fast only suckme expression shopping

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def add_numbers(x, y):
+        return x + y
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
