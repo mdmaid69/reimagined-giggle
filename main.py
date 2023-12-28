4e5f22d7ba92b19deb47eaa52c1bf18d5f7ed6d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:35:12 +0000

    spend middle home giveup splash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
