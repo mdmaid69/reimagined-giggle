589540c54b5e65e6855f0598678d24017f8bce7b
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:57:17 +0000

    unkind smell callout dance figure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
