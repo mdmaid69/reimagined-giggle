f38f2b7f081d0fd2c0b030f41868487b2494b801
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:11:48 +0000

    extra battery mind appear disappear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
