212978565a32f868324dcb1f6e2cef0f37bf6f38
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:16:03 +0000

    pot slightly sky Uu promise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
