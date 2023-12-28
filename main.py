0e4f3fe05a661bef32061ed127a76a277864dcc6
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:08:20 +0000

    hungry moment principle fail play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
