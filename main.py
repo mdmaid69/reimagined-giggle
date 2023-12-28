3aee5556229a11e588a933977099a8bd1bc6e587
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:20:32 +0000

    load mystery this cheque error

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_binary(n):
+        return bin(n)
