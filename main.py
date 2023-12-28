73ae735591a79da840b9a4f869c005d05d9f7c24
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:38:45 +0000

    suckme interrupt pen trip invitation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
