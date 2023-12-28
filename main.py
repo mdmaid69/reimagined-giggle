a01dd189c4d64dd7f41198298675f1ce67036e1c
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:47:06 +0000

    clever ago mind still mind

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
