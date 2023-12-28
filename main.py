87de9935edaace600b3989798a615262c6a5575c
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:07:51 +0000

    naturally related fall power exploration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-print(math.pi)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
