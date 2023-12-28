43e0b48e491882082aa6a3fe9ffe4f30a0c9afe9
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:13:47 +0000

    complain tooth pay succeed attempt

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+print("Hello, world!")
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
