1dc9b1b06dccaf293a5ed0430479aea5cbcc8b99
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:55:06 +0000

    afford real reduction really weather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
