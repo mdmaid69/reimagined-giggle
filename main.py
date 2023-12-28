bf00a3ff5bbf27a499ba5e57d84bc6941837f228
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:56:18 +0000

    whisper suit safe Uu run

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-print([x**2 for x in range(10)])
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import datetime
+print(datetime.datetime.now())
