2b4196fb0618fb9dce7d6a55d97e0473c47eb143
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:20:00 +0000

    brush cross beside too underneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
