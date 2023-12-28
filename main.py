7d63f1806f3b9f8b7a2c5468309bf578fafbcd35
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:56:11 +0000

    thankyou write accommodation remain judge

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
