c60108998ce8183de67e7881e06fffa43dfc3ae2
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:24:29 +0000

    giveback rub nearly near love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+print(sys.version)
