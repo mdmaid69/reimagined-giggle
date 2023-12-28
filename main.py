59f4d05ede497daace019ac6414447562403d8b7
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:37:05 +0000

    shrink pretty centre dead side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
