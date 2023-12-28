d8672a746df4e39115b35632abd279cdd2502da7
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:47:35 +0000

    advanced pleased risk negative laugh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
