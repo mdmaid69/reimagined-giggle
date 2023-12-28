0da62acce2113d7f588c7106cdee89bf19cd1a38
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:35:42 +0000

    friend connect sit invitation naturally

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def multiply_numbers(x, y):
+        return x * y
