4411bad6490069332cd58c2e09c5a6ed9416cbdb
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:46:24 +0000

    179bite quarter borrow table advise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def multiply_numbers(x, y):
+        return x * y
