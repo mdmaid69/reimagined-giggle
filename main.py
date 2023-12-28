e186d31acc948b61fa4e1f9b2b5d049311a70095
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:34:01 +0000

    disturb season easy clear play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
