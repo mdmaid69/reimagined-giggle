3b97cd8f0e02c345dd6c4dff02a39fa2b3d85a43
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:38:54 +0000

    community Ee almost safe fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
