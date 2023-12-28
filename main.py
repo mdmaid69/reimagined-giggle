392ed85d39132200279d2a566402c3e33393460e
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:26:32 +0000

    length seem hide extremely hide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import time
+print(time.time())
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
