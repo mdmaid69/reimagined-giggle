30cdfa180c405aff00ebc95f978c6fe96b28b7ec
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:28:31 +0000

    law warn fierce fond rate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
