d39a4b8c27dfdbe574def7e8ef11e1bc830b7505
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:33:37 +0000

    bottom murder congratulations stupid because

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
 import datetime
-def get_current_date():
-        return datetime.date.today()
+print(datetime.datetime.now())
+def square_number(x):
+        return x**2
