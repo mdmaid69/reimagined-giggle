b23389c0d77672440ee541b78162f0af510c4077
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:09:53 +0000

    character body concentration hardly add

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_density(mass, volume):
+        return mass / volume
