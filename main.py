84e2f27405eae8fb480ad7ddd10e23ff60d3ee8e
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:04:35 +0000

    touch fasten deliberately translate tonight

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sys
-print(sys.version)
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
