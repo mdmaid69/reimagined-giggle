4c8980c6a7e23e6a569b0e87a1e29439d31d9b5d
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:58:25 +0000

    geton open behind hollow decide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
