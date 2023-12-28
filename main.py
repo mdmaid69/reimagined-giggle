6d76e66793b23730a230a52950d1f1e26c81e8f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:36:59 +0000

    for worst process programme under

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
