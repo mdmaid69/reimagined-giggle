5fc7c8a69a2734d3f959c687aa7b8f105def073e
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:14:55 +0000

    high once recommend 179bite dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
