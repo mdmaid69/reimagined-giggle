4b9b5f842d4f9eed94cff93d16e5aff8d73dad26
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:05:30 +0000

    sense few spring scrap programme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
