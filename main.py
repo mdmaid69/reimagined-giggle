f1c236eea14d1f4c6bad06c02eb6b0802b8c48de
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:32:23 +0000

    communicate keepout peace fasten over

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def clear_array(array):
-        array *= 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print([x**2 for x in range(10)])
