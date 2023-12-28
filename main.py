6c0f60770fa915a744c6cc4bc89a6c5e5b1f3a8c
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:47:24 +0000

    mad swim contrast off should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import platform
+def get_os_info():
+        return platform.uname()
