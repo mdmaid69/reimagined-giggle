d6dbb7b262930a82590742f33c2e8debce2e085d
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:05:16 +0000

    memory command exact expression finish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
