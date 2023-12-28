4c343e3efd32a3f7e511dc576ff5b7372d462d4c
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:25:24 +0000

    throughout season accidental strength seat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
