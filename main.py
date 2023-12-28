9e9881f88d29b0ad18e60a51f02275dfa53a7605
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:52:01 +0000

    attempt regard especially sorry callout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
