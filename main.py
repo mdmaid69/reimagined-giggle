9c0dc8da8ae9b7d9501148729798c3e3268e58db
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:58:33 +0000

    cover slice much smart minor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
