744bfe5b3f767c5f8f69f70fdce36d45b5c8ac53
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:52:21 +0000

    size excuse square tap shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def subtract_numbers(x, y):
+        return x - y
