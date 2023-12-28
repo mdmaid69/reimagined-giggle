b2cb200422e38bfe19bfea23a47880226b7b9e40
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:10:23 +0000

    sail organize communication practice deal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import collections
-def create_user_string():
-        return collections.UserString()
+def create_counter():
+        return collections.Counter()
