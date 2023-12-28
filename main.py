e4f0df72855127550bdfbd917e5f2b317829bee1
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:43:23 +0000

    neither wide circumstance note return

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+print(math.pi)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
