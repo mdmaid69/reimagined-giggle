f6490f276e5fb561eb65f17e9cd3dc97b7f596e5
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:52:22 +0000

    refer wonder report fast talkback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def reverse_array(array):
-        array.reverse()
+def pop_from_array(array, i=-1):
+        return array.pop(i)
