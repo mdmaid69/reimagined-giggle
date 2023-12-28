4e836e308a472eaf33faebd1e4c0650baca8b6db
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:16:30 +0000

    speech along comein loud no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
