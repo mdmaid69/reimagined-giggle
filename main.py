74ae2e79ab83993b9e4d2d7cdc175912ee685cb1
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:56:19 +0000

    behaviour step yesterday ball before

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
