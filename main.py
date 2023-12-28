bd5ef61024a6f904c16f854447bf8c7652044bd5
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:15:34 +0000

    either wipe mix meanwhile space

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def reverse_string(s):
+        return s[::-1]
