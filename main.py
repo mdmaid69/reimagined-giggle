6b83d6f3eef60485971327da94758dd4400e5063
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:56:03 +0000

    meaning culture clue slightly confusing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
