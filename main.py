fcac7723685f1dddf86af4e569130735c3cd8028
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:43:16 +0000

    search on plan term across

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
