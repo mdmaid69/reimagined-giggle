c7792e2f10bb24bd2ecd3b434099a973ed3c1d56
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:38:37 +0000

    drop earn every takeout will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
