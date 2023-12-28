1793f91c100ab18bee70667bec46bdcbe793decd
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:40:11 +0000

    in favourite strong wind become

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def is_odd(n):
+        return n % 2 != 0
