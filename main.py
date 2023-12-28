6d1aa518c3c4e06f0d088390633e24b685207423
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:14:26 +0000

    past also their and fix

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def remove_duplicates(lst):
+        return list(set(lst))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
