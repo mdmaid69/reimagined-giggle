dd0522ec9df0cebefa7f51b8ee5c202256a809c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:23:52 +0000

    sign home ball sleep claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
