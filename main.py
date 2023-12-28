c269be2506bc2a14aa351c5673feadb2aa966251
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:30:29 +0000

    home want smell build unfortunate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
