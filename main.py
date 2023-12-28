a716444db8e9f7d4344fb906e27fd148b7ebe1ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:21:26 +0000

    steal persuade limit fly objection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
