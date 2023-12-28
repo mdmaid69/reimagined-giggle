0e7e845a20c2ccc7bb31dbe7edb8ad0b42cf13f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:02:33 +0000

    way successful keep order male

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def convert_array_to_unicode(array):
+        return array.tounicode()
