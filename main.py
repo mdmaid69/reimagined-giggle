fe77969b04f7ec200aef907a585649a1e320a5d0
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:09:47 +0000

    situation he make period should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def get_array_length(array):
+        return len(array)
