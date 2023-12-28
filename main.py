911d90fb12f349f4c2b0cdfc6e4adca7a484618b
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:54:38 +0000

    wrong equal down through hear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
