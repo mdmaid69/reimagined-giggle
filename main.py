dd9b0e02e5d7c6458670b5ec10bd1f1cfd7fafc7
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:14:30 +0000

    less intelligent bang this demand

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
