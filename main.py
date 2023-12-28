25828e1a021014fd02235feb249f93a794ed7136
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:08:17 +0000

    inside somebody uncertain none left

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
