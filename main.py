abe0d666f9bbd76dfb86bcb6e0d8126612e98762
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:10:32 +0000

    strike sleep first Uu compete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def square_number(x):
+        return x**2
