05f5e2efe4d0d37cf7248a1f97ee0d692826c65a
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:14:13 +0000

    fire basis nature whether course

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def sort_list(lst):
+        return sorted(lst)
