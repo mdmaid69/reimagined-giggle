11546296ca3191e7142a6080a8879bc55bf87183
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:13:29 +0000

    favourite word holdon reliable command

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
