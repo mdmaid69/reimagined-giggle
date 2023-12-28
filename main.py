1d4469cc2ed0817e47aab9bf7a72406cfef444eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:16:56 +0000

    formal excited degree laugh march

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
