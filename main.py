7152ab3f3f5d1ad4ffc0b28d3fe718aa775449be
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:19:56 +0000

    swimming store lay tune maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import numpy as np
+print(np.array([1, 2, 3]))
