eb88bf7a82353e5d4e583c51101792028623c47a
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:52:59 +0000

    juice freedom wash impressive defeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_area_rectangle(l, w):
+        return l * w
