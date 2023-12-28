f563b005018e8a85789c4ea54fd1574821db3b4e
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:59:21 +0000

    exit tongue fit shut keep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_acceleration(speed, time):
+        return speed / time
