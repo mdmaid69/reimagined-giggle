ad127022d7dcb775731cf0e17c703bb2e2d1c2e8
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:11:55 +0000

    depend face lift tune quickly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
