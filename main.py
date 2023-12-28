4b1a430471f1d0c87b923bad979f3dfe32d294aa
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:59:52 +0000

    worth disappointed narrow means persuade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
