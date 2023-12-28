3d8ba027f9af2a02deacea3d0e2386efb5327d21
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:15:40 +0000

    face love means busy make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
