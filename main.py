b922d9c072188d94e387133424ee3424d84c10d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:50:20 +0000

    slide exercise the thin fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
