eabed9a0d1ac927fa66af061c6874c48a5540df7
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:33:15 +0000

    broken lose anywhere often shallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
