576aeee6fce39f790a1123f01d8ab4efe10702d4
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:49:45 +0000

    doctor offer image shadow range

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import collections
+def create_user_list():
+        return collections.UserList()
