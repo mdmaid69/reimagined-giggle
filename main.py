fad946a4da4ff509b308cbafa5d92c3a3e63e03c
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:06:33 +0000

    attitude they main sad escape

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+from collections import Counter
+print(Counter("hello world"))
