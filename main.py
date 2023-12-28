a4ddb375f7f8ad674cfb129469080daf7d164853
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:06:01 +0000

    guess Dd angry drop thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def count_elements(lst):
+        return len(lst)
