411671ec308a6f9e1ef0f44216b18e44d4b4f521
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:01:08 +0000

    public Mm serve turnout process

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
