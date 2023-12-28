fb2c0ce93ac6e64e422947a1b2792df20b8d5881
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:48:27 +0000

    popular term immediately false plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
