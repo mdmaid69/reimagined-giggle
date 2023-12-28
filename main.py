3c7ede5e62761d883cdb7b000228825bcbe87bd8
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:44:13 +0000

    everything unkind dream accidentally minimum

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
