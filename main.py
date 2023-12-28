6988b2338c1d580764119769767f925b65555924
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:19:51 +0000

    twist tread steady bang get

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
