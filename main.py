56e8aa3f1805dd3490ea661ddc2d7bed6e713aba
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:24:03 +0000

    own finish mention store sad

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
