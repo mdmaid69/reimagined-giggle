5bcbe65e083181573be1c41a21c152c253ee6304
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:05:59 +0000

    care none compete apologizeapologise exhibition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+print(sys.version)
