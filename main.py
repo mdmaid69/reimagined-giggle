07cd880d9c264907a7aa11d693cefba3b54fa840
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:55:48 +0000

    company dish sensible responsible relax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  def remove_duplicates(lst):
+        return list(set(lst))
