8fe0ddf0456594ca64a1e44f2b7ec8b301fb8fee
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:36:31 +0000

    there happy accident battery cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def remove_duplicates(lst):
+        return list(set(lst))
