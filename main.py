04af7df638bee4f04d421dfccc637171bac710eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:48:26 +0000

    step city Uu turnout refuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
