313f8745630d23ac0d4aedd9e24242edc4c0353e
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:05:04 +0000

    help 570enjoyable will nonsense side

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def find_max(lst):
+        return max(lst)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
