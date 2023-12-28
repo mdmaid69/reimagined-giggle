3d0e553180f730955344a918c1ba337e7ad15ce0
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:26:49 +0000

    clear turn tall broken wind

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def cube_number(x):
-        return x**3
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
