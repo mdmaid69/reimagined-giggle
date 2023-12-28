93994720d55d5dae1e0364951a0771f728c38ce9
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:04:22 +0000

    Oo nature Ll generous themselves

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
