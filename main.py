e5908e1205076f33fd07b60372b7521b46f8816f
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:32:42 +0000

    message cause nowhere carry sudden

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import time
-print(time.time())
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
