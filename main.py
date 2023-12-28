814597fb5bac20eab7ae38751c5c5ec47b48447f
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:46:27 +0000

    runaway rare polite complaint convenient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import platform
-def get_python_version():
-        return platform.python_version()
+import os
+print(os.getcwd())
