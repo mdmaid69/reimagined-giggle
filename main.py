375d52124cc5dd76a363319ba587c0f845e037f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:38:57 +0000

    level pool strike Tt push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
