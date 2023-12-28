183bcd2a5464d667e28995f11fa0d5285d5a2fa1
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:11:49 +0000

    spread follow satisfactory mention aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
