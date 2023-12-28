2a45148234fd4e30d1e85dc051bc20e523dee291
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:54:37 +0000

    correct major rest feeling breathe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def sort_list(lst):
-        return sorted(lst)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
