837d8878bc379a23eb7982182855cf6231b729a6
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:09:50 +0000

    harm goaway Ii key fear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
