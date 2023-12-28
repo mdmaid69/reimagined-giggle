6b839fe6d05ec14cdc096edac5a01746824d9391
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:49:44 +0000

    sleep broken comeout path crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+def get_today_date():
+        return datetime.date.today()
