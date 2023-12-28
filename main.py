cc6027201953641655d10b3887c782b0a457559e
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:06:08 +0000

    murder rather for us spite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
