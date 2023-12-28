644436873634791d902867ec79daf0dde90165ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:12:42 +0000

    occasion accordingto bored hard 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
