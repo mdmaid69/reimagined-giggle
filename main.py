7442f53cfad2b5787fa01652f45c2cbd293e78e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:53:08 +0000

    soak convinced vote attractive right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bool(array):
+        return bool(array)
