3eaa79b5a2b87e85b066803cd6de2fcb36c47a68
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:13:27 +0000

    crisis either paper mine hear

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_pressure(force, area):
+        return force / area
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
