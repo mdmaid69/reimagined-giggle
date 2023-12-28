750a153e7be6abfeb88eb9a3047a311fdf0365eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:33:47 +0000

    suit bet expression reduce seriously

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def is_odd(n):
+        return n % 2 != 0
