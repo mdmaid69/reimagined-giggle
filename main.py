0e0e9cfe289ccc6bf5d5d7615857bd3455713ecf
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:34:47 +0000

    fault holdout straight complete she

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
