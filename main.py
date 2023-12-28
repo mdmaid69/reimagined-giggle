dfa4df90001e6212ac7e87748469ee25ad99b9f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:04:06 +0000

    neither summer correction convenient treatment

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def count_elements(lst):
-        return len(lst)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
