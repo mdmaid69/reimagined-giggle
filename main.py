77fa08c4f3bd78a3bf8e1a372a9b8c4d9655a172
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:45:36 +0000

    like score mail catch takeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
