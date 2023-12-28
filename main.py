3ca8bb46dcade1e925ecaff53508ef761d72df31
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:57:38 +0000

    centre behave minimum thirsty foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import time
+def get_time_since_epoch():
+        return time.time()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
