649ca5642c20b331259d5d607c48efdff0e77a61
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:44:14 +0000

    duty thankyou mouth arrange most

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
