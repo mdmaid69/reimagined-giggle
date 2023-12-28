61863de89779ea930753e27924507868ffa56f68
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:40:32 +0000

    order cap straight push impossible

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def convert_to_octal(n):
+        return oct(n)
