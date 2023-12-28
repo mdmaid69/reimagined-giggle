e25036b88e919eb42c7285ae7fdc87de0a51f2bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:43:07 +0000

    foreign very rough sort regret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
