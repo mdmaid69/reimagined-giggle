a9f6cee1e20c0d4b3316db65b28c838ebfb96329
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:19:48 +0000

    talkinto task stick contents fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
