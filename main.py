76c77ce20db296be10c89a0db367b7e822ae5d96
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:39:56 +0000

    quit general flavour you acceptable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
