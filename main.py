b78b8d4dbc9ed98d1a3899792b3bec0bdc6573a1
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:02:02 +0000

    shall set warning surprising frighten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
