7f0309547d15aa1ed2aca3115fcd029e09f7b800
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:27:38 +0000

    country plenty direct likely splash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
