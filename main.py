2e926061b62e5ab4793d9fbd614abd2bc2e4eb61
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:10:00 +0000

    rare body demand activity conclusion

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_list_from_array(array):
+        return array.tolist()
+name = "Python"
+print("Hello,", name)
