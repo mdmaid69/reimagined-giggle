4d35afa4fd6522fd26c97f16c9109e084dd4a25e
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:35:59 +0000

    answer base 179bite breakfast priority

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print(sum(range(10)))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
