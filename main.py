c4a5673759f00cfc0143ae6a1840b729ba249d70
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:39:34 +0000

    wish meet operation cheap connect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def get_array_length(array):
-        return len(array)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
