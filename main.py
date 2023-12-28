4879e65f6bd7edcc96f5f5785461176a843c8f91
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:49:56 +0000

    luck risk land surprised fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
