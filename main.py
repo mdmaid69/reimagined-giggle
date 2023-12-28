9723f32e1690312e385e1bcbe7257c8222ac52f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:13:30 +0000

    story down community preparation claim

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
