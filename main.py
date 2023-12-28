65a4d2013c59d448d9377a398059d9e6f39bd220
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:28:52 +0000

    eager branch spoil invitation wing

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
