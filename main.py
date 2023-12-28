2992d0e2b5c9aedbf737beb870d1b0c8d67864e3
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:24:20 +0000

    yet pass embarrassing undo there

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
