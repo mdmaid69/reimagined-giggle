191deba483ceb6c48898bdd0f62115ad9cd8e50d
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:13:53 +0000

    collection wear language considering history

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
