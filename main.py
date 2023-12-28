ba0def2d7faa8bc7ece228bc9c6f981249e98244
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:13:55 +0000

    wipe trick proof activity insist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
