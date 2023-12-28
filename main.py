777e70c18636dcd4061ecd78509b059264a418e9
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:28:13 +0000

    test expect should eye foreign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import sys
-def print_python_version():
-        return sys.version
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
