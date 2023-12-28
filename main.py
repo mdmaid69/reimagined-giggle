86923da97d63fc4e4c73d9b5d2854ebb45f7528e
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:05:08 +0000

    likely argue hurt concern advise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
