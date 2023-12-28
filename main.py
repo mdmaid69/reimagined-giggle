b320de037caa32f5fa513e786ca34063e00fbd12
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:35:09 +0000

    favour month swallow they moral

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
