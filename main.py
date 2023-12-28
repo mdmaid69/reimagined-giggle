a501f2eb19d49e69e8e392d041d4f0141384b4a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:15:56 +0000

    position measure answer lunch secret

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import time
-print(time.time())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
