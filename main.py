78d04ca0f253765d7a7cb91b873fa836fac7c57c
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:44:30 +0000

    fit collect hot race back

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def get_array_as_set(array):
-        return set(array)
+def count_elements(lst):
+        return len(lst)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
