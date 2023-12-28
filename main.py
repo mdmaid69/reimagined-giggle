fba7379edebabbb31bfa3022319a485d710dca52
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:31:42 +0000

    difficulty bridge rare surprise vanish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
