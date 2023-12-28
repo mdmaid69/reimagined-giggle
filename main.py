f3ff1ae0a71b65902e2c29d2b9db980686ad1ae1
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:08:08 +0000

    when brush correct stay nature

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_work(force, distance):
-        return force * distance
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
