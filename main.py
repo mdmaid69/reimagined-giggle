79ee71e6ebbec6e7802506f67c4dde521c25588e
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:15:27 +0000

    on attempt bridge uniform include

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+for i in range(10): print(i)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
