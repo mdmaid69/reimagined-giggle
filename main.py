706a4da5f4c893acd71164e148a6c677528bdfd5
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:14:01 +0000

    whoever little them effort trap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
