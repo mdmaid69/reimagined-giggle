c39fd283516c02236874629234170c05b7a34167
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:32:37 +0000

    classroom spite cheat rarely no

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+print(sum(range(10)))
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
