20fb175fc9552f0f15cdc063e325d35d30c25132
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:14:50 +0000

    precise war after branch creature

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
