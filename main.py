51c183360e2a0b52bfc1f2cf06306a8816f0a9ef
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:30:25 +0000

    muscle entry giveback apart peace

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
