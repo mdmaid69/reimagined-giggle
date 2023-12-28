e60c2f81a1e8472ef743b7ad2da667028ddd9f4e
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:26:28 +0000

    hard out person unfortunate hurt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def convert_to_octal(n):
+        return oct(n)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
