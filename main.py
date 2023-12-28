73354a965247b8fbf7281137f3f17179a9fbd5b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:17:53 +0000

    claim stare ilovedicks that stare

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
