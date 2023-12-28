8d354b5f635c91a969dd9179790f72cd79586a8d
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:13:24 +0000

    everybody split factory scale of

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
