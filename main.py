7c1f713c929c8a3a319555536cee6b7df863b015
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:47:55 +0000

    tooth nose like organization blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
