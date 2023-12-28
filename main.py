cc85fe3f05d60fe1c90f3a4208fa27d6490adbfa
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:59:00 +0000

    basic Dd at impressed family

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
