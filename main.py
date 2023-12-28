63538afe6399584c5650a2c56f8e6daa3d7f62d5
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:21:08 +0000

    operate sail century near glad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
