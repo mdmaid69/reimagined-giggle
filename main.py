82a783fb1685b312e928cf41952c477008cd2c39
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:41:20 +0000

    respect interested tie will turndown

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def cube_number(x):
+        return x**3
