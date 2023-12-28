fc254f985468dd549a10aeae47640b240ead41a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:13:24 +0000

    advice wander clothes summer shake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
