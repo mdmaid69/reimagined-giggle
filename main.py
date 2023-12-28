1930e54333a476e2fac9460aa8fa5093ab1b5099
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:22:37 +0000

    appearance record programme complain after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_density(mass, volume):
-        return mass / volume
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
