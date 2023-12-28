7320c25bb8547f17a89b46a1dbaa849f2c08c901
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:27:21 +0000

    pay 564encourage tidy seat fail

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_power(work, time):
+        return work / time
