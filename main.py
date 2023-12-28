4eda98e444ccf687c0d2de10b7f1b760b65ead0f
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:58:41 +0000

    public scrap disturb thin arrangement

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
