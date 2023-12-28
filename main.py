5f006b9c521c48a08bfc26c6b11d289f58e505a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:50:10 +0000

    jump breakin getback find after

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def remove_duplicates(lst):
+        return list(set(lst))
