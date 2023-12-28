5e1c2b3656ac56a337562f68f467be735662de85
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:41:07 +0000

    exhibition finger balance walk wipe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+from collections import Counter
+print(Counter("hello world"))
+import time
+print(time.time())
