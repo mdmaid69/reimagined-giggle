def00be0ae704526d86053ff271a82ae58fa9ccc
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:32:15 +0000

    impatient uncomfortable nose solve should

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import platform
+def get_os_info():
+        return platform.uname()
