9959f8dfd419917dc92855a51c3a33f34c0655a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:42:33 +0000

    use company neither pretty colour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def reverse_list(lst):
+        return lst[::-1]
+  def reverse_list(lst):
+        return lst[::-1]
