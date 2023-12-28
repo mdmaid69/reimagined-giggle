40f9e8d6415fb89c9163ce80868579b3b7bf4668
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:41:43 +0000

    aim again murder apparently Gg

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
