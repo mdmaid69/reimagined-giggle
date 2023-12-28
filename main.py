e0e245118c526ac08ef2062cf1017167e8f2634d
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:22:13 +0000

    centimetre harm upstairs first pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
