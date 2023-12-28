81a9cb6f96fd00eb23d8a29ce82033767c8a9045
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:57:59 +0000

    shall still apart occur hers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
