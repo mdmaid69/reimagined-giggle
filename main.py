1b855c14961a2b03093d31d902ea2c5f2543370e
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:10:32 +0000

    comfortable announce plant open dare

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print("Hello, world!")
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
