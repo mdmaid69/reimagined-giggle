0c41afabd7d4d5d36f043b28de788af4e5fda418
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:50:15 +0000

    treat yet sport exam punishment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
