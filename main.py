31b5d57897682ce2b55d1bdc1cff4d05c83f213f
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:54:48 +0000

    firm reject nerve view impatient

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
