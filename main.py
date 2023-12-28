cf744a589762c64f764e4a674b2cdb4b2ab78e84
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:35:41 +0000

    Oo explanation brave turnover apparently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_even(n):
+        return n % 2 == 0
