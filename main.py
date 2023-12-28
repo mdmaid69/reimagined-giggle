ecea719fef3b2446d3d927034ccd59745240f578
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:16:48 +0000

    urgent while happy past rise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
