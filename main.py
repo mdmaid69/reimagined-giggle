e5a4c75a246d6b281853bfe0d0d41877f1e1750f
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:01:42 +0000

    find crash whom exchange 572enter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_as_int(array):
+        return int(array[0])
