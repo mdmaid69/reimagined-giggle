24799c232f90f256ec1c5d34ec2b9ea121bdca58
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:16:46 +0000

    ball country shake improvement keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
