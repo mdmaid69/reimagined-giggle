d215d967583cba40f286d61c3257da5164d1b34e
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:14:58 +0000

    thing plan story house tax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
