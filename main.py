7fa7ea9a4cbfdd97a5f656d503b6566ce7c0c8c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:16:02 +0000

    drive top go satisfactory shut

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def square_number(x):
+        return x**2
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_sign(x):
+        return math.copysign(1, x)
