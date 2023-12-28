26af7b03dd3712cce36df9e5dfbaf0d35ee6015a
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:43:13 +0000

    little somewhere fold finally play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
 import os
-def get_current_working_directory():
-        return os.getcwd()
+print(os.getcwd())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
