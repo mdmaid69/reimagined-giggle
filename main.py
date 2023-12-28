c3b07a5854fabbcc757d6974ec4afe9fb6f98718
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:23:37 +0000

    hardly release claim string it

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
