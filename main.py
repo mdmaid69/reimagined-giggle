777444b0346e2f98eecfb43f5099fba3a2def0cb
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:28:02 +0000

    may shock angry tip offend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
