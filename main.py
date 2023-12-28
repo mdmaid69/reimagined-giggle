5e8ca1f52f5b38a6be8bceb5af33ac7e6823544c
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:16:35 +0000

    allow gather swallow right division

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
