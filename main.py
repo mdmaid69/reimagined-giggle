dc93687fcf847ce8996bd8f97f11b8c7c37c6c4c
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:25:22 +0000

    fence square satisfactory smile snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_max(lst):
+        return max(lst)
