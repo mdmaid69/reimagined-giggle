bfef1649f787bb1a4debb9e701cdf9761c2141b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:48:03 +0000

    paper even hold any importance

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
