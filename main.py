7e19f29b142d1f59f8e11b602326941fd440ad3f
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:05:43 +0000

    sentence the risk Vv lot

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sys
-print(sys.version)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
