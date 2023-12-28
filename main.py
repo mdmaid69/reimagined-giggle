377e6dce55b5a854e38f2374223e05b6c35a29ff
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:32:40 +0000

    drink bet respect or gooff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
