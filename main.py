e29a6af5baaeeb3b6b7de9f9f4246ddf201a509e
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:41:20 +0000

    through error mixture argue read

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
