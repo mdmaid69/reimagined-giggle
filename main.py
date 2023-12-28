87960128007bb31841ba4e7e94a3dc3a03fc31cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:33:40 +0000

    risk finally traffic eye sting

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+name = "Python"
+print("Hello,", name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
