ee3d4c21b3c568a49f3d9552a54830a4a4a305ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:53:45 +0000

    arrive front piece ourselves think

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_octal(n):
+        return oct(n)
