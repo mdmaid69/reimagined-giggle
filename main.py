38426f3cfed2329f9438d781044c3eea3a164208
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:38:41 +0000

    exaggerate fine brave limit disturbing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def get_array_as_int(array):
+        return int(array[0])
+import logging
+def log_message(message):
+        logging.info(message)
