644c17edee7bcc01fb94640c4fbaeb45e577c7bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:14:21 +0000

    keen create left own fairly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
