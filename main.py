3fcfdeb8a83c9d82e4cd1cb03da4897bacca1978
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:48:07 +0000

    tend process plan broad summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
