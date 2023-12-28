8ad2d7e51aa4c47497de65b92a66196e14a37c7a
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:20:26 +0000

    crisis simple warning pattern year

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
