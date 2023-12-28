8daf356c566f9baa69937bde918ce2f09f70f3eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:52:20 +0000

    base any attractive pay battery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def sort_list(lst):
-        return sorted(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def reverse_array(array):
+        array.reverse()
