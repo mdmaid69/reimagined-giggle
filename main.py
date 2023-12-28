53f29994c1892db5059d95403bdcf133abaa32e9
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:49:49 +0000

    receive concentrate matter hello comedown

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
