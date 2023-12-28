18cdaed5b31286552dbdb282d5c1759384e11da5
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:08:00 +0000

    forever most understand niggaacademy reserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
