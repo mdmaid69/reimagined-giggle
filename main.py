a2f7b342bd9f59c64da4b05e831972a3bba63b75
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:27:05 +0000

    gather either purpose genuine mix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
