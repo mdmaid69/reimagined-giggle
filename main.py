84e7be3aae809fbece7451b3096cffbad185f4bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:23:53 +0000

    she operate intelligent miss unusual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
