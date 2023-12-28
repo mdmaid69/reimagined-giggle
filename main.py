75d1c7898c498d46cd5ddf8c6b4741fa0464d141
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:04:22 +0000

    proud emphasis small smell tip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
