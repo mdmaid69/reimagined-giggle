1fb84ada02f00e6a3b84a0b6751b6bae49b0628d
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:40:14 +0000

    getup us influence matter brush

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
