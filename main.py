f4cb175358081a714f17449b4a63fa74f00c1963
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:10:01 +0000

    mail determined worse takeoff past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def get_array_slice(array, i, j):
+        return array[i:j]
