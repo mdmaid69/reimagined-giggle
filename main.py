bab8e81b26e5613e3f0bca01affc146e76bf712d
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:20:36 +0000

    middle score shame fork damp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
