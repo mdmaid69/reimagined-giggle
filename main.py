3d9cd1d21e02b6ca4b6f263d82322e1b34f17009
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:45:27 +0000

    song thirsty everyone authority turnoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
