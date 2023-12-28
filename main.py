8a769de4352c1053d9ffd4c15ca85f0961a52ab3
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:03:04 +0000

    middle Yy please instead draw

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
