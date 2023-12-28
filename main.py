bc3ac72bd0798185c66bb3bc587702ca62c921e1
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:17:02 +0000

    count especially responsible body like

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+print(math.pi)
