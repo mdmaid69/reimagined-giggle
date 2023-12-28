f886270a5b2b906b560a77fa8c150887df938e35
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:07:24 +0000

    Ll press regularly hate argue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_acceleration(speed, time):
+        return speed / time
+import sys
+def print_python_version():
+        return sys.version
