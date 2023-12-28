6f97fa4a9a9a359244720bbb4a1e42995b586425
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:16:20 +0000

    sting foot suit happy judge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
