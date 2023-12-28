7c7dd7affed018793688f68756cce00140289a8e
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:46:20 +0000

    operation class character 572enter happen

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
