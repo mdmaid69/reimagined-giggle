003332dd149e785a7e8d747081663bb850ba96c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:51:06 +0000

    experience art best unit strength

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+print([x**2 for x in range(10)])
