e0bc905d302a114229e7c07477ebcc682402b5a1
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:42:58 +0000

    forever about father square smell

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
