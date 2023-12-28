1677e484a86ba25630fae7a8c71de8acb6ab9a4d
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:51:08 +0000

    everybody file phone petrol pattern

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-print([x**2 for x in range(10)])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
