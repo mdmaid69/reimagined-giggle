afcc617339fed3a101dc13d6197a0fa2a7fce22a
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:08:30 +0000

    yesterday love takeover I line

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-  def square_number(x):
-        return x**2
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_string_from_array(array):
+        return array.tobytes()
