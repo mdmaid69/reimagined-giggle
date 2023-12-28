5ecc2a12a1c2014a18a0869eccdf3f38417f5f3f
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:42:34 +0000

    thick confuse stare common pump

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def is_odd(n):
+        return n % 2 != 0
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
