229af0bd1225ad1f44480c39df29bf727fb2c310
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:24:46 +0000

    contact just circle breakout child

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
