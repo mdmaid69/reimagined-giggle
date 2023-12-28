359c516b405fdbb305f96620d491931b488dc2d0
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:38:58 +0000

    other force second wish opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
