20d29f16c165ec52d65a23499f5e098043b74616
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:22:36 +0000

    fair declare fault common teach

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
