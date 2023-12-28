518937d7b225c85106318ff8018d669ca2a8cf6f
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:57:37 +0000

    remain his with stage write

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
