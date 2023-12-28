45c1735dc47652d57763ebef609e3d430fd8c686
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:05:17 +0000

    loss roof left confidence patience

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
