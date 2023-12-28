26ab6a12c35d4d48f9939aff182508c7a56e18de
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:41:01 +0000

    deep other comeover correct friendly

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
