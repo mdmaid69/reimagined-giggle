79e46874ed3b004b4b14655cd0dabc4b7b1c70fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:26:52 +0000

    snow this strike arrange know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
