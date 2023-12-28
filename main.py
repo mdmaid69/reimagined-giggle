ffd8ce524026ab9600e417907372ce068e5ff997
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:25:33 +0000

    matter join guide collect experience

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
