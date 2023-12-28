c9effbc8457d3a1fb04420d76d05c5010cd01661
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:36:23 +0000

    nervous basis country happy buy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
