22c3bc97b3e89aa89516ee5dda305107d9b21e31
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:14:23 +0000

    value detail out related bringdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def reverse_list(lst):
+        return lst[::-1]
