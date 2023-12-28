02e7aef1c41528be76d05d135f9db374000a72b9
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:56:21 +0000

    skirt also unkind impossible suit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import os
+print(os.getcwd())
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
