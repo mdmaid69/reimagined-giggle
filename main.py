0478087b308663c555aae329e002dd1f0f5f77b6
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:15:35 +0000

    class accordingto believe sometimes book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_bytes(array):
+        return bytes(array)
