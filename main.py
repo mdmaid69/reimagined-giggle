9411f8877c998b1674014ff575868677618fb25c
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:27:28 +0000

    wealth chat carry stand limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
