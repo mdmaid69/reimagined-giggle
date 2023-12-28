b976f04b0e5a20c4d4f6ba8e5fdd5ef69605aaff
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:44:52 +0000

    brain emergency claim last reach

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def print_python_version():
-        return sys.version
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
