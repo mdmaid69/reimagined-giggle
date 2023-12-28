4f893cbf029bc46ce2dcda36184ae037178254a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:16:35 +0000

    deep plenty attractive interest order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
