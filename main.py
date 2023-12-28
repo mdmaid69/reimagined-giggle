976e62db782dc776332e12524a590b0067c604be
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:51:48 +0000

    passage active interested criticize object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
