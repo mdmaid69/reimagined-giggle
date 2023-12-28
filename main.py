c0f64faab66e9d30f31f3250ae9301c2d9957b99
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:41:00 +0000

    summer so way famous bringforward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_as_float(array):
+        return float(array[0])
