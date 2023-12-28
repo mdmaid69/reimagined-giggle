5c6f8d08fab5d178f81e1d4987579f27a42dac67
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:46:34 +0000

    thumb man person build prepare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
