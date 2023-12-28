72d91df335ab5b4d3a624be167a4b75e05692090
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:30:03 +0000

    spoil immediate probably bill clothes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
