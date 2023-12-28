4f80333979994ee86dfc885989d4fc886c3f5a45
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:20:17 +0000

    remark admit maximum blame light

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_pressure(force, area):
-        return force / area
+  def reverse_list(lst):
+        return lst[::-1]
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
