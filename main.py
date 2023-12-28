e2f4d505bc4283816492d72989bd1e594d1ded18
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:59:10 +0000

    autumn flow understand result fight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
