cf8bd2e09644ef7831f2599abc2ebd3446c871cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:30:05 +0000

    ashamed fine difficult fix brush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_acceleration(speed, time):
+        return speed / time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
