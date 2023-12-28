61bb14ee71ec58ef20d28b919e3a67659610d49c
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:49:18 +0000

    top protest plant spring matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import time
+def get_current_time():
+        return time.time()
+def find_max(lst):
+        return max(lst)
