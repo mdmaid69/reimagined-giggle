c6ca3c240377e3537bf0c9bd9b5cf7d5b32e5eb7
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:23:32 +0000

    door marry issue container tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
