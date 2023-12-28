3beb407d615733f51963d54477ccc8a54fc8549a
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:08:28 +0000

    raw will cost without temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import os
+def change_working_directory(path):
+        os.chdir(path)
