51f4c4ef3fd8ca03c889ecf852d34855f293301d
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:38:44 +0000

    disagree comedown release full I

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
