5b4caff3a11fe2e996288aaeb005a654cb972fba
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:20:49 +0000

    machine remain change ours last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
