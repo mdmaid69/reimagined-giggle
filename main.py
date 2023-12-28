a9a139e638de346e6ded51ae7a08bb0e5998d9e0
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:34:30 +0000

    soak possession undo tense juice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
