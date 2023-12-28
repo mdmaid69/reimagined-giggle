433797472de2ec2752e756183e29a9ef667f0680
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:46:47 +0000

    protest move favour check go

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
