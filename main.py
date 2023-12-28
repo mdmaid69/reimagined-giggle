fde6457165863ada61f6765f5a97da7f99f4a625
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:56:43 +0000

    radio relation makeup snow expression

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+print(random.randint(0, 100))
