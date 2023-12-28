fa1543fced22d5380dab3ff7568d3442aa2b3708
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:49:17 +0000

    sky read uncertain burn tap

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
