c9559485c47a25b58b4641fd512eeb5b9531aa61
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:53:34 +0000

    breath later admire scare justice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-for i in range(10): print(i)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
