28caac4b4af815b83d5086793af8d618d9a6973b
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:07:42 +0000

    mind brave taste concern set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
