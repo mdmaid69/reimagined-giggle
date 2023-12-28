eee443770cae4e9c467631317de55e2b4ba666f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:20:19 +0000

    mess hardly joke rush bit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
