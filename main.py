2cc2c00e7910cbd317d602335e5a9ee85c5c31a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:05:25 +0000

    cause guess straight anywhere tongue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def convert_to_binary(n):
+        return bin(n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
