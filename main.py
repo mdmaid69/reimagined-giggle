5f7beeb35380363032d96245265bdb505a7f120f
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:43:45 +0000

    crisis desperate hungry cost follow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def sort_list(lst):
-        return sorted(lst)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
