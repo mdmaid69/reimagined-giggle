d6b91b3d996ccb985606a95c9e6053e04350728b
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:21:42 +0000

    neck mystery but keepup hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
