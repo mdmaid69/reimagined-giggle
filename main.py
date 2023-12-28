5bdee716520354c45c84f578771f0e0e3ee696e4
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:25:43 +0000

    represent brown blow also earn

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-print(time.time())
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
