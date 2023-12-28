42f7292a08bd380ac8fe0a67f6eab56b763a15b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:39:51 +0000

    hope reduction lock ask patience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
