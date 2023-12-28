7f5d687550ddad0e6e867ec298dc38180e72d78c
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:13:03 +0000

    force call open they slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def print_python_version():
+        print(sys.version)
