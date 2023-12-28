78d9c1cb7e90c31f94a7961b3ec85fc70f624bf9
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:30:44 +0000

    emergency split answer share temper

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
