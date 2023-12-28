69783d461decfe1153c559d3663f237e581c98d2
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:02:46 +0000

    only by rule usual fit

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
