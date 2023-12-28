44c20eab58e4b4d68f068b76b01769d26db1e15b
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:59:52 +0000

    exhibition compete calm since wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
