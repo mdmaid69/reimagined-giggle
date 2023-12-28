0d25c52d75687715b1777a1ed55d6fe6729d3d91
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:13:11 +0000

    book accommodation wet priority bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
