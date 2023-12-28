2396cd7392636d2146d3eb8648e4d2fa1bc781f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:02:41 +0000

    contents except pure wide same

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
