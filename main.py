e653d6610f6544bb45f746b27a0f9ef5bd36b1de
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:23:01 +0000

    feeling phone twist violent normal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def split_path(path):
+        return os.path.split(path)
