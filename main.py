cc474356b4d26944142b717a7a6fad60efaa4cb2
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:35:06 +0000

    disagree precise listen street preparation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
