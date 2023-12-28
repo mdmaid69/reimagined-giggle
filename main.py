74177f68bcb75bb45958e1a7dacf36ab0c3cd0a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:06:54 +0000

    smoke honest command press tense

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def convert_to_binary(n):
-        return bin(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
