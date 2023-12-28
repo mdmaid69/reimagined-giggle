f66f8b12ad01f7031f6a77f3f005adce370887ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:43:00 +0000

    stop door dark pure listenfor

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_length(array):
-        return len(array)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
