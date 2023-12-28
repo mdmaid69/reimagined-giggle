221c140a9d616bc8daaafdd9ffa4c17beca48234
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:53:21 +0000

    exam shame lay stare we

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
