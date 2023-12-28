615cc228f7131395bf7a16bad353b16ebc917ffe
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:50:36 +0000

    contact bush spend along spring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+for i in range(10): print(i)
+import array
+def get_array_as_repr(array):
+        return repr(array)
