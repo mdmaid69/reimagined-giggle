0239597c03f9a826b39acbbb5411e213c50d3c4a
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:40:49 +0000

    field plenty goout broad steep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def is_odd(n):
-        return n % 2 != 0
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
