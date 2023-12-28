8e4d6d9c1305364be70ae8756223065d21a3179c
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:35:31 +0000

    breath now change female shadow

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
