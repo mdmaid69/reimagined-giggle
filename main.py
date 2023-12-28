0d0cff19aec877d58e5df1ae5edbb2d95561a6fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:01:00 +0000

    yours cheap town pipe lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
