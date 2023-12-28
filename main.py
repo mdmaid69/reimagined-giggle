df38a682b4863adfc8230057c1e0c0d9280a12c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:51:29 +0000

    shop performance father afternoon exit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
