2b149043b4d6e97b6bddaf6af04bcfa055aa2d6d
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:33:42 +0000

    keen makeup book port science

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
