c4ec1a941c1507fd302614be0c29562c91af7ad6
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:33:34 +0000

    perform yesterday familiar kill getup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
