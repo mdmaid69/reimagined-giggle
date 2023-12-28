0381396924619780670894d58b89183233a17a96
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:21:41 +0000

    maybe equipment colour clear search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import tensorflow as tf
+print(tf.__version__)
