34b1b2900f6dac27997042489245ab96e80f19f1
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:31:59 +0000

    after serious turnaway leg concerning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def square_number(x):
+        return x**2
+import time
+def get_current_time():
+        return time.time()
