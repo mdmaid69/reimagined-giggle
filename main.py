517c353082ec364ff4c5f1f81a327406f6d4edb8
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:55:09 +0000

    test react machine hurt whether

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_absolute_path(file_name):
         return os.path.abspath(file_name)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import sys
+def print_python_version():
+        print(sys.version)
