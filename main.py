f6e821f123c6eef0bf817483ad03b768c46ff2c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:36:57 +0000

    whole poor join guilty machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
