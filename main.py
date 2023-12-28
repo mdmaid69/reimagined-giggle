e5a535c41225df83410f5a36e25b6e9a47d0107f
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:35:10 +0000

    pipe takeaway roof maybe object

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-print("Hello, world!")
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
