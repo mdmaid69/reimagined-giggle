0c7c81386475701bdcfaccb1380b4c1311671240
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:39:36 +0000

    job exam exhibition his last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
