0b26ff43374c371d6cab65255a76bc84e8ccff0c
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:33:24 +0000

    makeup accommodation getaway situation shade

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
