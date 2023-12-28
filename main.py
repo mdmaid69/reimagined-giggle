bd0bedc8b372ee4f24d68e4f1991bd592092cc5a
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:14:28 +0000

    turnout dry owe acceptable dress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
