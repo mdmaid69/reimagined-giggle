4204a87bc613b6b890dde342c026c0c2b8195cf6
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:03:14 +0000

    care join thinkof neck other

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
