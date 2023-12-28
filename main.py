23b1cf42a482291abee15e7335d951bd58ebdef5
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:49:44 +0000

    talk test change model sail

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
 import array
-def get_array_from_string(string, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
