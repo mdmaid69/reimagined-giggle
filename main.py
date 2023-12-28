df8a1130a2cbca7a86c874694f9272410a87bf8c
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:54:12 +0000

    neither slight street attention ought

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
