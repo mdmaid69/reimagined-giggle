6800f181683c8b18a1c636bb0d152ea31452fb59
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:28:15 +0000

    turnoff owe attempt after build

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
