818b8dbb0b3df7c3616debc26d46ede211a53280
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:17:06 +0000

    land anxious show any nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
