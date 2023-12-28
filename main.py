a37fc27c73e4e854fb2f3c1c9a404f52f2c8d1ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:26:26 +0000

    present stand inform size course

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def is_even(n):
+        return n % 2 == 0
