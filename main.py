5114723a9125019635607cf86b42390a9f74b53c
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:24:34 +0000

    callup although sister house mind

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
