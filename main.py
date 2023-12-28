5655478585ad4a91d92cbe5dd15b1c464108d9c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:13:07 +0000

    pleasure good sight degree up

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
