1a54aea469f68536f1e44e3fc5d3eb4aa44542ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:00:45 +0000

    waste patient instant noisy too

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
