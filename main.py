708e66d8af55b99dc7809666a02958f0f7a40d04
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:59:09 +0000

    regret train activity book injury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
