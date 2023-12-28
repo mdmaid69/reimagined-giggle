d48b3222a3f03eb4cd4378c0b414f4a7f1226353
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:34:06 +0000

    cost scene unhappy concerned art

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
