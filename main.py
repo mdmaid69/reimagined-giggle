01f990fe23c2549d2fce4fef9dc26e8414ce5a32
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:10:59 +0000

    concentrate pile gate pronounce practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
