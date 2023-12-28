79af1eb88571e5512ab8008f0f9fbd86f15a1006
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:19:45 +0000

    pot recovery none trip phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
