f443511df90fa69e814c78dbb7e4668f44978487
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:44:26 +0000

    fire effort willing mail face

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-x = 10
-y = 20
-print("Sum:", x + y)
+def get_bytes_from_array(array):
+        return array.tobytes()
+def count_words(sentence):
+        return len(sentence.split())
