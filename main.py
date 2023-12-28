8075a222ab231074b4bf70943316b67c47660b04
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:13:40 +0000

    amount different far flow influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+def get_current_time():
+        return time.time()
