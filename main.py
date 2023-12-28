b4b0cf47bd3f24af776938197b2eeb74d3d2319d
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:23:37 +0000

    dig chance wide wing please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
