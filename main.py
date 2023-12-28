8c6eb862f854ce69be103ae58d6d284cff903d0b
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:40:34 +0000

    as to square impossible squeeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
