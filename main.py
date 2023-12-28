265377b7e05114a4c47693e87710ff24eb44b4c1
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:49:25 +0000

    practice bury declare spend heart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
