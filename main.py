5339e43e2c1b026a1c1fa0ebd8de7a16d37dc162
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:18:11 +0000

    search mess steam after right

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
