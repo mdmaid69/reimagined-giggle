24fb8119ceec2ac7c8065afe3cbea51b78d71685
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:01:41 +0000

    union increase excuse duty broad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def reverse_array(array):
-        array.reverse()
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
