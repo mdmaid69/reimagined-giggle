8575e8a7864d3bb36074ce4b2a68ccb688997a62
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:00:06 +0000

    also whistle now getout threaten

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import queue
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
 
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+{body}")
