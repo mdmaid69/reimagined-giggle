c83db04beffc1128a5995e61159615caed2798e3
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:42:53 +0000

    or central song direct sweep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
