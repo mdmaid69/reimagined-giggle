48bf6a9f6c45998817de043f86490726598ded66
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:44:07 +0000

    someone care enthusiastic fall list

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print("Hello, world!")
