74757738273a1c08a17d784e8a62da537a5a524d
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:07:10 +0000

    bit gift quick probably female

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
