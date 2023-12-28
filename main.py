03d1c83a7e1bbd2925e569568d42cf19f05e7a06
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:21:10 +0000

    minimum exaggerate near worst busy

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
