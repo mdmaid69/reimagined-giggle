f71924e4a28a2d023e0ae0552b3c1221aaebe797
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:51:47 +0000

    far smart there challenge back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
