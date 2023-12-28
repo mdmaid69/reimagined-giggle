c5b6d8dbe0d4648986dca4d6a6eb47e3b1ebdcc2
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:57:14 +0000

    570enjoyable pain protect 566end thought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
