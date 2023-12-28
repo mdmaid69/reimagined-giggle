08f48d9f4d99c64b3af4e3f23aad24c062df688d
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:39:55 +0000

    underneath basis feel workupto punishment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
