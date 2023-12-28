10a6a43250ebe9d0f43c876ff47857f1b99ba99e
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:23:56 +0000

    air wheel attention learn produce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
