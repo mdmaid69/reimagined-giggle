6b392677c28a566e05c14870b0a3c31d18ebde69
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:15:00 +0000

    range train rub excuse contents

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
