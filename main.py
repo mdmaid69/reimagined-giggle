4f3bf06791935e627e8af9af1a5a8b2356c8a5ff
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:42:41 +0000

    exactly note gap hollow smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
