2ab6cfec797cc693c17a24a0991bfa12d800932a
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:37:28 +0000

    and apply considering happen generally

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
