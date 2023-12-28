79638061f2abb01b8e7dd4cdcc92fa2677e403e9
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:43:48 +0000

    rent healthy near pain fold

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
