8b7d5d2447593aad0db99234d5b86426f514d0fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:32:04 +0000

    stamp worried string remain bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  def cube_number(x):
-        return x**3
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
