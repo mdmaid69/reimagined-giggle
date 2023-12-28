fc3bee1e88f63cf493336e85250477bbc0125324
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:08:10 +0000

    brain kill dick butter cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
