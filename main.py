177ad856992731e6550d5ee3bd3a1cd4b1f31190
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:50:24 +0000

    stone idea laugh sick mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
