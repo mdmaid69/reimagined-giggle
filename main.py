ec9abe72886174de03573381256542b762de6b2a
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:35:37 +0000

    original brush deliver everywhere dead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_acceleration(speed, time):
-        return speed / time
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import json
+print(json.dumps({"name": "John", "age": 30}))
