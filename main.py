211cd2f0d66efe568645b3ffe2474f1568a73f8b
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:04:59 +0000

    lay example accidentally meeting try

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
