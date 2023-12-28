e06c2e5494449dfed344abaf41b948b9128ecae6
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:21:12 +0000

    height separate oil mistake month

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 def calculate_mortgage(principal, rate, time):
         return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
