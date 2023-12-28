09926935a42d270612452ce9d1b4838d569a721b
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:28:33 +0000

    mark grow knock debt children

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def roll_die():
-        return random.randint(1, 6)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
