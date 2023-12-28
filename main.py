b59e50b90fee0a167fb20a7b63a08102fda2d78f
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:52:44 +0000

    rub thing concerning perfect police

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_odd(n):
+        return n % 2 != 0
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
