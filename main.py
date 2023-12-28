3b63247a39078ed845bcc670afbe3d2d38084ca4
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:01:33 +0000

    even create desperate not personal

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def add_numbers(x, y):
-        return x + y
-import os
-print(os.getcwd())
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
