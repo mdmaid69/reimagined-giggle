8500e4aa907527f3cd6eb07fb075d1d5e91f586b
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:54:06 +0000

    nervous in poor keepout car

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
