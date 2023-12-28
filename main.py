5c6397282fb545cd570fe6034e491965628b3021
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:25:39 +0000

    war holiday heat affect comedown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
