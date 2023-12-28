abcfaf5fcd490b788d9909dbf3b1a84b786d5e41
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:26:21 +0000

    speed programme cover actually bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
