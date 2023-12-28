5b011f1bff2209dea23ba4c8692f8f29f91959ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:22:27 +0000

    lucky cheek loud equal problem

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
