007ea75a5ecb19ac678a275bfa49224a6a5136e5
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:23:15 +0000

    demand crash tap band cry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_average(lst):
+        return sum(lst) / len(lst)
