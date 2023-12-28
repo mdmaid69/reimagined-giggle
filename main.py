ce51bae74a77409131f26c22f5f33f5522b57ca0
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:10:48 +0000

    frequent scene comedown sensible flood

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def greet(name):
+        print(f"Hello, {name}!")
+import os
+print(os.getcwd())
