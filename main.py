bbb6922ec4f73c85efd253a147a7fd0f1eed0627
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:06:22 +0000

    listen tradition float opposite now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
