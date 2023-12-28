99554e1d2a46ce200f19b5c114cc8f224a743e77
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:08:53 +0000

    high post full like think

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_pressure(force, area):
+        return force / area
