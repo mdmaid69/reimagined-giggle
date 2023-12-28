ad0e2e7f64ecab804aa0c244789db7a5a9cc5247
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:38:41 +0000

    practise soil through forward listenfor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
