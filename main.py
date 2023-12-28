52e0671f2329fcd4dbc75456a2c349db5b778c70
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:45:17 +0000

    list female below used right

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
