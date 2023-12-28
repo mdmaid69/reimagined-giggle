c6c1fc2c9bcf9aba418d6d51798940c4bce4f0da
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:51:08 +0000

    meeting unhappy relative team opposite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  def square_number(x):
+        return x**2
