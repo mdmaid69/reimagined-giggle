905258dec2ce951061a012887b48541827695bd7
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:58:12 +0000

    remark statement down underneath lean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
