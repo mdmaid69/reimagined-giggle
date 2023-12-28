aba910f573f533ae0c68752022a12f5ae6ade7ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:38:01 +0000

    suppose war reply since what

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_int(array):
+        return int(array[0])
+import sys
+def print_python_version():
+        return sys.version
