41c904bf27342a644574a6add9519d1901b20c77
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:57:05 +0000

    ill fear before cruelty catch

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
