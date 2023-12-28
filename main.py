97c53d0f67b70f942221c391e47049a4502a26cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:53:30 +0000

    keepaway union keep life smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_group(file_name):
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
         return os.stat(file_name).st_gid
