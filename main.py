bdb91a70d2607215d2dfd225be2de28cc97e50d5
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:54:58 +0000

    shut Rr represent seem mainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
