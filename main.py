2351c3a0b74f6ad34b04788b63365c8f3cb151e9
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:05:24 +0000

    recovery trust steep obtain blame

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
