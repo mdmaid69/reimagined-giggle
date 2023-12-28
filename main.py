db5370309b0d3ad9e67d4ea094b2636924b475a2
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:59:36 +0000

    giveout write work authority size

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
