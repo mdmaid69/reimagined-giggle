2d1c005737e5788608bcaca3ee6c89083d275e11
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:58:43 +0000

    combine circumstance wear shock finish

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
