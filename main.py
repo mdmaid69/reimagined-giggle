d5ccc8cd09e17d24b09bd2f9a8f1bbf839346be9
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:42:49 +0000

    medium post keepaway stop sky

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
