6cb78cfd29ad495e6e64390e79897b414a9ce1b7
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:25:06 +0000

    upset claim instruction uncomfortable uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
