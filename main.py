f8986c50fc1dccaf89739ce730e1e1e1e291f179
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:41:07 +0000

    passenger often Dd reach getaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
