342cce5e467973ad126fa015705730507cbf2edf
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:10:12 +0000

    pattern film test probably badly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def square_number(x):
-        return x**2
+def reverse_list(lst):
+        return lst[::-1]
+for i in range(5):
+        print(i)
