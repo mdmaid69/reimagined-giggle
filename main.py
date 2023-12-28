865808021cd9c1f397e391a5d6f7b3ed500f5110
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:13:12 +0000

    behave callback cum near relief

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def reverse_list(lst):
+        return lst[::-1]
