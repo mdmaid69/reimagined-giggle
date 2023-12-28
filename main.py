44575c3fd50cbb881c4b7d346a22ec3620a8762f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:38:22 +0000

    as regret perform brush happy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
