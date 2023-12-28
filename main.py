ece62d4af95324d797d22b33c0e9a4575b40f1c1
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:52:05 +0000

    hard original treat commit speech

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
