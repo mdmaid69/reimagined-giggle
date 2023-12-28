7f0c1b5886ab36abd0f3d0e8d9a2bcf8d84ada0c
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:17:12 +0000

    ocean disappear tense scale on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
