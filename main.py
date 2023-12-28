58a84525258666888b271840f7ce3049fa97b6b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:19:24 +0000

    dress talk possible soon guard

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_min(numbers):
+        return min(numbers)
