c3e5328f96c13e0f8fbc86e26774085c61943aa2
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:02:59 +0000

    survive coffee interview stare tap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def square_number(x):
+        return x**2
