65bf50fec0148dd1d5fd9fa7adb7239a569da4d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:42:21 +0000

    noone private estimate explanation education

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def count_elements(lst):
-        return len(lst)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
