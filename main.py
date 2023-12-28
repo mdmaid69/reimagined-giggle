712693f70c0acf67635ee3433ed3d14c4c225db6
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:07:15 +0000

    view however can say if

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
