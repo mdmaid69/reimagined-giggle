78ae7694891b7077a9ac611b167b109b8f3922f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:27:24 +0000

    cheek build fix speed risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
