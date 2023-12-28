9a749173ee61a4fef8cf9b1d0bf4cfb167db4a3b
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:21:45 +0000

    journey region responsibility undo spite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
