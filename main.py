f006ff461f0fceeb3714918fb6be0d229f1f8da4
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:53:29 +0000

    concern theirs fire great deserve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-def reverse_string(s):
-        return s[::-1]
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
