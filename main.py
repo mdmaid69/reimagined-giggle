97c19909c10165c1b6f02d90d89129d22efdebe1
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:59:23 +0000

    mistake friendly thorough far call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
