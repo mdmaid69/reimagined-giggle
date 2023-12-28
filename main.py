b52b8673482181c07641eba9db2d38ad07f8864e
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:09:20 +0000

    meeting apply standard operate snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
