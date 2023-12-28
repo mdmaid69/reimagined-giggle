71e6e5f80a6f582d96e80ba5656ade95a661d6a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:44:51 +0000

    exhibition instruction hurry ahead warm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def clear_array(array):
-        array *= 0
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
