b71d6e8e646aa6739c218be2746f017ca52a001b
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:28:22 +0000

    such concentrate used sit why

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
