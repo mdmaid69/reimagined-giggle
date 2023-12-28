96df4af40458b447a1226c60f2940282edf50a27
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:13:53 +0000

    beside improvement scrap pure memory

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def divide_numbers(x, y):
+        return x / y
