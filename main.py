17ea4b2bb506acf45387fcae68ff2888892dddc7
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:45:46 +0000

    strict shrink spend machine far

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
