627d9eb42b3649da62d22f612c41c9e42bc0244d
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:10:05 +0000

    than find survive 573entertainment slide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
