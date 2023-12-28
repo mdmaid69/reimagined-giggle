949694c89e32a6654a136a9a972f288e47cd3479
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:18:04 +0000

    sore bet case lunch live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
