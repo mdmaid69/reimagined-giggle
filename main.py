f4bf0e1391b5416a4accb0d0ada4704d415b9642
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:01:38 +0000

    personal laugh change then hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
