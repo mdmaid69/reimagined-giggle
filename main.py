57cd0a55dbad0eb21dc9eaae3642139f5ddcd189
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:34:58 +0000

    admit whenever during wear yourself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
