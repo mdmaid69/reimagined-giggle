87dc1e77f1682d6fc4bb0ed4a53993d096ba5f5a
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:02:54 +0000

    introduction lean soon closely start

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
