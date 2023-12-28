b4b3d6598a322174efdff2c38eec1a4a5aceb22d
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:27:00 +0000

    down definite constantly scare worse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import sys
+def exit_program():
+        sys.exit()
+import os
+def remove_directory(path):
+        os.rmdir(path)
