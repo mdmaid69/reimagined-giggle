ebbab67bfc830d2d4553b2f8438e8c4bb33ca0d0
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:59:23 +0000

    interview fine practical consider obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
