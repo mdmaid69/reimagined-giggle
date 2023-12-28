2bf843951ef45c50006713d4244b04e19472bcd2
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:40:43 +0000

    Hh round bear blame separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
