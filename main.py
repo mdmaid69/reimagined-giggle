57a1669da471cbad03221d3b0f0c67e3b76bb35d
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:21:35 +0000

    warm base manage culture tone

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
