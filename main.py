a8b767cbc2abfbc93555d83fc9c6ba0f733de66b
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:39:44 +0000

    grand repair embarrassed walk set

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
