7944b86b04392465460458dfc8266d3a6d23442b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:45:03 +0000

    different naturally for great right

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def multiply_numbers(x, y):
+        return x * y
