7cbc2bcd0d9f10aacc545bbe2088ea01101b7d1b
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:01:07 +0000

    mess bored train rule shallow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
