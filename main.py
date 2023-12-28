a9d73b172ffc0a04384b9dbbd360d07736002ed3
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:54:52 +0000

    some point 569enjoy amount crash

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def cube_number(x):
-        return x**3
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
