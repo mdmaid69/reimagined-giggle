265cc7e1ab5d6cb050626f46f03613e2e686de04
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:40:40 +0000

    market familiar thirsty Nn getup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
