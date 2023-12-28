8ebe676deb9c7fad9747e1fb57f6836eff502366
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:02:36 +0000

    necessary unit question plenty money

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
