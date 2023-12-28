53a229e96cc1438422dc00ea4419d24a7ee02f72
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:20:39 +0000

    advice reduction meanwhile slide cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_base_name(path):
+        return os.path.basename(path)
