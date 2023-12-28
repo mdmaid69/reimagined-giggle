ac556fb356eafef8b56445f4a91e80dc8afc0712
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:07:36 +0000

    today produce university zero lie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
