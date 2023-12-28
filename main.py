6b85c22960f5c57672487a0d5231eab53774731d
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:42:34 +0000

    block development worry before solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_directory_name(path):
+        return os.path.dirname(path)
