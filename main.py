e763f97fc71a5636849a809ded63aec93b8b3dda
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:00:12 +0000

    cool especially everything care hear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
