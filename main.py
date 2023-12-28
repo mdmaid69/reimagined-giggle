7d33de6a7e2a5aeb599d00b7edfe31e60c737830
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:41:53 +0000

    usually usually kill base awake

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
