2fb0f9801f209ff1c7fc08efeb5aceb3d3e8832e
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:41:03 +0000

    sister sleep unite contain rubbish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
