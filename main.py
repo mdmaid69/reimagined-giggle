e9eb75f79b08dec441cf914ff3969e32a0e280ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:50:22 +0000

    difference belong about interrupt ability

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def convert_to_binary(n):
-        return bin(n)
+import time
+def get_current_time():
+        return time.time()
+import collections
+def create_counter():
+        return collections.Counter()
