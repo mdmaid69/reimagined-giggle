03cc9daf603d1d3da94c368ae9825f96c89b3ec1
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:13:06 +0000

    period inside grand live worse

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  def convert_to_octal(n):
+        return oct(n)
