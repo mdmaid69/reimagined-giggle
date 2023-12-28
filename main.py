f934bf395fbce361c3770eec10b48d46636fad44
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:59:33 +0000

    its pleasure giveout room precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
