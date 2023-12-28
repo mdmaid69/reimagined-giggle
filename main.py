eb15a14851589c75ed06234aadd1e858c098e66c
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:26:50 +0000

    bread material lose important married

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import sys
+def exit_program():
+        sys.exit()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
