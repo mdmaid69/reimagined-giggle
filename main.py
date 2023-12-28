7d9eaaaa3b8ae703555d1349403687e311133b83
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:11:43 +0000

    think compete getback accident alone

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
