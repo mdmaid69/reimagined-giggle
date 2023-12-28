311aa353423531488ba1744de03ba368f9ca7791
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:25:49 +0000

    necessary guess guilty bottom medium

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
