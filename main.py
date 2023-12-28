317ad13a61ae7abff3ca217bf39d2c08d768e91b
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:51:39 +0000

    high forgive medium right stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
