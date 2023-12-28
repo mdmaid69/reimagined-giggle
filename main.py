f5180e8ea879ef80d8da1a5337f6a8bb42400d4e
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:21:21 +0000

    fight grateful total behind large

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
