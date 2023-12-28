4a2ef955dfd2a1b88747626031dfa00cbea8b9f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:47:01 +0000

    welcome beginning store learn tradition

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import collections
+def create_stack():
+        return collections.deque()
+import array
+def reverse_array(array):
+        array.reverse()
