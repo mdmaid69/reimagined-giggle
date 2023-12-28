def194370d32b3b1d389ecc419d0000baee0cc5d
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:09:50 +0000

    correct reserve entrance rubbish machine

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
