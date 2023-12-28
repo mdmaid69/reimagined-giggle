ab1c551503abfd14285522193504d40cfe29f837
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:11:50 +0000

    water greeting scare risk neck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def remove_duplicates(lst):
-        return list(set(lst))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import time
+def get_current_time():
+        return time.time()
