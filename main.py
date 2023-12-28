b958b7bdd73c72d27635d61bc9d456d2af6ef39b
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:39:31 +0000

    indeed fear travel love will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def reverse_string(s):
+        return s[::-1]
