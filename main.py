db27b3e5aa89ec673dcdc66c1fdaa753f343c37b
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:59:26 +0000

    back claim deliver your traffic

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
