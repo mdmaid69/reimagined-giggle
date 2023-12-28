418ce34d78ab4d121dae704a02c16a0aa5f82423
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:21:25 +0000

    content rush frightened lookup unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
