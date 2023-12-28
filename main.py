42369e91cc29faf57c48c16e708be83b6644ec6e
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:42:46 +0000

    anywayanyhow rather important achievement choice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
