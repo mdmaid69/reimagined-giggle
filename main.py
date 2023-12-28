02e6eb0f8975f6eab27ab7e17f3fe35fd03b4017
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:18:23 +0000

    famous position narrow temper juice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
