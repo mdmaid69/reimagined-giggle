534a7bbe6e0c05de8bdc3d8e6ff6f8b7e8f4d2fd
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:52:45 +0000

    class regional direct to crash

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
