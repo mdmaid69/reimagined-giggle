5abb0d158fa5f236565b468d8a76c06044cfd3c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:32:55 +0000

    door here face straight impatient

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-for i in range(10): print(i)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
