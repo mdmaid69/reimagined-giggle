9eee301d412cc4692465f6b7342e972b3b3b76c1
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:21:49 +0000

    preparation tidy narrow expensive dinner

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
