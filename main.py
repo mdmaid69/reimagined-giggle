2c8d29429a0d3ddd36c35c332ba9ad25dda7f0c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:46:08 +0000

    bend fire Ee pocket favourite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def count_elements(lst):
+        return len(lst)
