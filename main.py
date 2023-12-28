d3195464cb15f6e7f6803f426c0017c33b841f57
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:42:00 +0000

    along apologizeapologise way slightly produce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
