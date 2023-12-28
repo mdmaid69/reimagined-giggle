4c1ba8bc1e0943dba5abb9fb0e0a85ee85b3452d
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:18:08 +0000

    off plan spread quantity scrape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
