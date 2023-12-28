22a6e968c97b0ea9c7c6fa5b82f3a54001886ffb
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:51:19 +0000

    catch right noisy squash legal

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
