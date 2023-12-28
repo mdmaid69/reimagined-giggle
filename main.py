f6ae5b0fa16224785fb9e44bacd7bd86f3bfb14b
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:26:21 +0000

    call else male mix arrive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
