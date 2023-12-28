189913a4b443ae27ada4e16e7901895e358c1450
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:06:49 +0000

    chin equal along both rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def reverse_array(array):
+        array.reverse()
