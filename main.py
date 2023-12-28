9c49f371f4c440d8dfb9d15cab4ba443b5e0def4
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:26:45 +0000

    medium spill gas loss rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
