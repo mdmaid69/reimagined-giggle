eecaab2f32a28c617a50ce07cb96b07ba5071406
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:56:18 +0000

    tradition think show profit shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
 def divide_numbers(x, y):
         return x / y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
