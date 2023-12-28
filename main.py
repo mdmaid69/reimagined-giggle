4dccfb3e7f610a7269f1a99537c4ccfca5241fba
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:32:36 +0000

    ago takeback damp fall fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_counter():
-        return collections.Counter()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
