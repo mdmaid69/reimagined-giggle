cc8e993d156d38cd1f3e483f70daf1525d4f7172
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:13:59 +0000

    everything interested estimate judge kick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
