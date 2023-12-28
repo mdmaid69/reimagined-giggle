165604b0dcba718b49a9a31ba7b4965a49db2d9a
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:15:22 +0000

    worry passage account thick sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def add_numbers(x, y):
-        return x + y
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
