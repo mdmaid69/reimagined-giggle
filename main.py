cff7e84364f3b350a3139d55bdf1a748aaf6111c
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:05:09 +0000

    guide rule probably bread special

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
