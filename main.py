c3e080ae30dd67b0f1b192a0768ddb40b9d8d6e0
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:03:21 +0000

    abroad shopping answer trial extremely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def is_odd(n):
-        return n % 2 != 0
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def square_number(x):
+        return x**2
