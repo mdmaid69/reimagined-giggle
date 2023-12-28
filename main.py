2fdac8f0bb646f5a736c279745fec198bd4584c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:25:38 +0000

    Vv gather convincing all shrink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
