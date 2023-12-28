41db935918ea72cb9643ef4aa9e4cb2d46f0f464
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:06:59 +0000

    live say switch proud gift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
