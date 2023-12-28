56f71431fe4fb00224a4e9fc1edbafd4c884b678
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:12:25 +0000

    surprising talkback respect issue close

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
