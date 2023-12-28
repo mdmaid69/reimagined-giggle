297b3baaf7948718a3282a54ee26a8343b6b7703
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:34:08 +0000

    journey nerve operate subject hear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
