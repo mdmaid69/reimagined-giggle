122267330fa95bce3cd92e3b904f3d8bebe66e3f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:40:28 +0000

    patient bill odd pretty giveback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
