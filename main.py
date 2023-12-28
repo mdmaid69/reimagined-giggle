908d4cd01ef56af38f03d264a1d495bffa0cdfdb
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:34:21 +0000

    pleased getup pressure drawing square

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
