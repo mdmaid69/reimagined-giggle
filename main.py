c60757c53387effa99928b69325beaa113f6697f
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:39:10 +0000

    advice young destroy complete tall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
