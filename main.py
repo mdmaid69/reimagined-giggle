a96dcb5a53509d5f8808a869df068325e54cd8c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:00:45 +0000

    opinion flow off think unlikely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def find_max(lst):
+        return max(lst)
