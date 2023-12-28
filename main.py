1023e76111fa35d8ae735df738beef01eb31dd2c
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:07:50 +0000

    throw on medium colour amount

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_typecode(array):
+        return array.typecode
