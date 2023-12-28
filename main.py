0526e51f21c37fe9a6c14c34f7fe484de871c19c
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:58:36 +0000

    meal private keen dead concentration

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
