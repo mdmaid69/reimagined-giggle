1a9b17a160aa1e477b24aa3b7352cca68386ddc0
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:06:11 +0000

    object summer peace lead decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
