619304c24e29dd61f95627bda8bd94411ff2a2ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:52:01 +0000

    179bite several computer basically close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_current_directory():
-        return os.getcwd()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
