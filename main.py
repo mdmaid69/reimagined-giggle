e21853aa79f870f64e2b5cfa889d3b047fa566d2
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:50:52 +0000

    doubt shout help emphasize hate

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import re
-print(re.match("h.*o", "hello world"))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
