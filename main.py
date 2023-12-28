7e709e62fa0169544bc0ce95d1f89cf506a9d918
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:27:31 +0000

    whether communication press deal reject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
