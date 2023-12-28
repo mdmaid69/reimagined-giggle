0340140dc6a64e0cd999132a68d84beebf40ec8c
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:21:07 +0000

    classroom couple pay brown skill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
 import array
-def get_array_as_list(array):
-        return list(array)
-def find_max(numbers):
-        return max(numbers)
+def convert_array_to_list(array):
+        return array.tolist()
