341b5fce376e12ee537c1bc6cbe1889eb10694e1
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:55:25 +0000

    opposite burst furniture spot size

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_list_from_array(array):
+        return array.tolist()
