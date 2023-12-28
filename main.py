a940f2cf9c35e1773dc6c97e70ce2ac6e01902b3
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:50:35 +0000

    close sad wear goback(to) tape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import sys
+print(sys.version)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
