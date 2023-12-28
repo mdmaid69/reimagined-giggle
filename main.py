bc0ca300dabab84cc4a67e214dfcd44a6b7cf557
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:31:05 +0000

    switch face shine certainly sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_combinations(n, k):
+        return math.comb(n, k)
