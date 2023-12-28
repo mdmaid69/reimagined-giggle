deb7c0f0499ebcd5c5bc5d7ce15473ecfe9ff802
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:08:23 +0000

    beneath persuade example protection here

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
