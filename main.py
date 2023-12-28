e98dd50090341d0bf2da2d669eb501c910ac9df0
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:30:44 +0000

    store write butter responsibility image

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def get_array_typecode(array):
-        return array.typecode
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
