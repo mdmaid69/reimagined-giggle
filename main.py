c44fe14aec06b68e09d89fa8a575295c6db40fce
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:09:11 +0000

    through minimum mainly unable thin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
