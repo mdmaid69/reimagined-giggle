2d47d4d862b7ba8df6ce5135a16c6d7bdad3276f
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:48:32 +0000

    should matter black increase shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
