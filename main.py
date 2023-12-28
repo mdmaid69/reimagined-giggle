d697a6a61f859c1659d755af89934d18460d9c9e
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:01:28 +0000

    shallow read any Ww calloff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
