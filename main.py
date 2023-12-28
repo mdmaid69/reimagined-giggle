c1ce2a9920c1c480e5e923249101eda3fa429ebc
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:39:40 +0000

    indeed freedom prefer level stir

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
