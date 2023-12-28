c1f4251d4db71c8360ac1c86c0d6139deeccce39
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:03:31 +0000

    admire vision purpose excuse task

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
