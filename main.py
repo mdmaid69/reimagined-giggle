cf06fa4007db7621bbeeda5d8465f9d528ebf4c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:38:09 +0000

    slow bury father above model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import time
+def wait_for_seconds(seconds):
         time.sleep(seconds)
-def subtract_numbers(x, y):
-        return x - y
