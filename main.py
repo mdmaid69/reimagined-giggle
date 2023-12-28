51e65b79e5212c3427a64befa9a93a4d39724e2b
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:22:11 +0000

    regret getback invite matter doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
