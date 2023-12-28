d179b38bc4cbda72bdce2ec75a67e44ef32c4243
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:06:09 +0000

    answer mother for just considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
