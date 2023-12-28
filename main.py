9497e1f8545b872fdccb1542d51c850a56816f67
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:41:09 +0000

    hurry failure getback aware identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
