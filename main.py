aeb2daa4b1b8371e70c70c9461d22bed1e2a6e65
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:21:39 +0000

    Zz main concerned last consider

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
