8d7cac85999983e27169c829e2b28710cdf6da8b
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:37:26 +0000

    forward lot charge card schedule

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import datetime
+print(datetime.datetime.now())
