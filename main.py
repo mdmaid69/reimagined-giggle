6edd3e3c3cc48c213a3bb47acddcb46c016c80ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:24:33 +0000

    smell continue pleasure immediate weather

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
