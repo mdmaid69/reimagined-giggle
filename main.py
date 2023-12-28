1a4a2886d43a79ded8647e6f358b1f3555911a0f
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:07:16 +0000

    careless threaten television afterwards hand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
