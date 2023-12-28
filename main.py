f35998b5f43c58db089c94dd849e4414cff59728
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:56:11 +0000

    trip trust radio takeaway science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
