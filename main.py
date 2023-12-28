9e8e3fdd10a93a823f6eb84dfebeecf6efc1f7b7
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:18:18 +0000

    whistle office gentle comefrom appear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
