470ab982d5d26ded97dc8546274401582e2d52be
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:07:21 +0000

    badly its wish emotion slowly

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
