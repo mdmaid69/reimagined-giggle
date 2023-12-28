311ded8b8295db875b656686ea5ffe58b2a3013b
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:12:04 +0000

    confidence likely emphasis instrument attitude

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
