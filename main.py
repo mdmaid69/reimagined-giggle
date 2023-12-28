f91bc02fe6dd2b61f87baa9b015339048efed179
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:08:22 +0000

    shape fall foot concern hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def reverse_list(lst):
+        return lst[::-1]
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
