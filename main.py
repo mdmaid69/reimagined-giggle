0da31edc910a983aecf73cfa1d75f2b4f8e39e20
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:09:55 +0000

    rather aim difficult unite top

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
