ff8833f4fd623ad57e555fec2d7820c25bf28ca8
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:20:29 +0000

    disappear bus who accordingto total

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def square_number(x):
-        return x**2
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
