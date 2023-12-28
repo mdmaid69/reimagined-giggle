7ab5fb8715da236053e1f72cdbff24741663311c
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:57:09 +0000

    properly dick cruelty apply several

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
