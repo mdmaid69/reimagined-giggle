516aa05cf882a1bf88f0d55b03d5071bb60fcba3
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:32:22 +0000

    behind allow special attempt separate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
