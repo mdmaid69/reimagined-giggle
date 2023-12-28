f8878df2311c966d4aaad3f59471db580ab95b7c
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:55:31 +0000

    convenient arrange command suit project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import datetime
-def get_current_date():
+def get_today_date():
         return datetime.date.today()
-import array
-def get_list_from_array(array):
-        return array.tolist()
