7c2842cab144d9657e1219fed1362ee91b9295a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:06:00 +0000

    bringdown one cross cough proud

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
