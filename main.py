8f82cd1c4c7b0e805e84c597bdebc6989d877039
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:54:30 +0000

    Ee message knowledge development short

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
