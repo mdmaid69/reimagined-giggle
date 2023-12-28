2c502ccf98cc5f08d60acb7da418503145c344c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:24:39 +0000

    fairly occasion pin impossible handle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
