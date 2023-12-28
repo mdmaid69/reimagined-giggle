268b36eacdb746dee9674c8c88bcf3d517d19b16
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:35:31 +0000

    thumb cross wash pleasure visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_work(force, distance):
+        return force * distance
+import os
+def change_working_directory(path):
+        os.chdir(path)
