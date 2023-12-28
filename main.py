ad75ffe545474f25ac9b83bab7736c27c9e58d1f
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:58:15 +0000

    heart distance permission fly rain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
