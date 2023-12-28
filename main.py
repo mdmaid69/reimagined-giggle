52cf3a52d2556ee25f6de62fe80952d32360fbc8
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:26:52 +0000

    spin spill fold type branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_work(force, distance):
+        return force * distance
