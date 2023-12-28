0e84b0c8c35bbe76ef9b133562aaf62f90704dd8
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:49:57 +0000

    involve anxious nor public change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def get_array_index(array, item):
+        return array.index(item)
