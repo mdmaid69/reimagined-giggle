7dee9b1a6b261894e898b2aba18228b1f76d1c40
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:03:10 +0000

    giveout unless her price left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
