e1959b25662e13d1b11b7d96d9969d5aefed212a
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:06:41 +0000

    point much there constantly her

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
