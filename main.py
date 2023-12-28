ab0cc44cb9c3a27df4570e04eb2c2b98a673c368
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:46:10 +0000

    begin instant by either copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
