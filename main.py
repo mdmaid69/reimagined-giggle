05c4d1815b0c63e66d9f18d1cf268031f8de569b
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:16:41 +0000

    long Ff team realize immediate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+print(math.pi)
