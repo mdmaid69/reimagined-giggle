5b6cd697a4e97db5fa1f6011dad646771d89c823
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:22:40 +0000

    this pride stop suppose failure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
