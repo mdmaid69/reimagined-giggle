e9847b809c2452baf07db5eedc54fa9f071827ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:39:55 +0000

    office tip spill beautiful stage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def is_odd(n):
+        return n % 2 != 0
