c04d8342a0dc791518b0ae20b6e2f915381a4ad3
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:09:03 +0000

    turnout exaggerate drive value young

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
