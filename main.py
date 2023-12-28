7d42f01589929526c8e8a69b7729a04351645975
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:37:17 +0000

    call further than understand giveback

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
