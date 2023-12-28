3866b810f45ae029ec657561255b1b927b4c5ed9
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:40:57 +0000

    high hang cheek common culture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
