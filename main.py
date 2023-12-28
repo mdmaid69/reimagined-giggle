a2f36730ae2018bf3a3291c471dd47354bf44a4f
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:04:08 +0000

    conduct dinner represent turnup without

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
