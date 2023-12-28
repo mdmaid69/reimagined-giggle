556468517c9ade35bbb6bb41f817ddbafe00a15e
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:22:00 +0000

    copy patience sail develop favourite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
