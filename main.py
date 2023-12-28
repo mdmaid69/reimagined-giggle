ba6b375cada8a4f44ed46928f3395a1353bf000c
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:50:39 +0000

    lead deep new transport clue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_cosine(x):
+        return math.cos(x)
