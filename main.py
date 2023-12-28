5cb126655348818bb16659600fe57134e415644f
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:37:46 +0000

    proof suit worth find promise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def add_numbers(x, y):
+        return x + y
 import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
