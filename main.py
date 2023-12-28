8428f33c817d2813dc45b724ecb82f925f675893
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:37:26 +0000

    come train small foreign trial

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
