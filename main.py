8827b96e3fdbf4a567b6344d8459ed5fe13877a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:09:22 +0000

    wire quickly anything all lock

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_speed(distance, time):
-        return distance / time
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
