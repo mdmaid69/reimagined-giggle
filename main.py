b385931a133f8bb485a138379533178ce30b20d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:15:10 +0000

    them pretty father comfortable hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
