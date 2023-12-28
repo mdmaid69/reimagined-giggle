f7c7266dbeb0be7c379ee6068423a9a546c35371
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:50:33 +0000

    various coat goodbye Vv flow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def convert_to_octal(n):
+        return oct(n)
