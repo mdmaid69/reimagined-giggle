ab421c67e3ed09c9a992d4c18b2fec6a917a1cb5
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:08:36 +0000

    next giveback bet used goout

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
