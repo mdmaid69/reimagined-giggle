91adc27a6b9aeef2a297638bfeb5a18fa7e764f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:08:51 +0000

    roll pressure like dress past

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-import sys
-def exit_program():
-        sys.exit()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import os
+def remove_directory(path):
+        os.rmdir(path)
