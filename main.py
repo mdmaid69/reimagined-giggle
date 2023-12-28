14a8c4883d4e6c207fbf16693cd45b95e8b2eb98
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:31:17 +0000

    measure later protection mother dark

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
