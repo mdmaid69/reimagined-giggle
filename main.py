f737d8af3f5510b68287d1d1b3237b90aa4d2a51
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:34:59 +0000

    responsibility Kk how regularly fun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
