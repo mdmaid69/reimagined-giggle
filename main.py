56de7b9a06650f92123726a484169aa3e6fc103e
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:53:45 +0000

    situation trip cinema stay spend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def subtract_numbers(x, y):
+        return x - y
