2563f3d869d70e0e1b76b680c16f346356011ef3
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:15:49 +0000

    takeout unite fond eye satisfied

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
