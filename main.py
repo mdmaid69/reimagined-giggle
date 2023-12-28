fa3be09ce541b53d45db9416a575addee33e9f84
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:32:50 +0000

    stand mess disturbing drive shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
