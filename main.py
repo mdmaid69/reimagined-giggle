fe08a6d113be8c73566b23d52ce6a0386142d31b
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:03:45 +0000

    evidence except wander length characteristic

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def greet(name):
+        print(f"Hello, {name}!")
