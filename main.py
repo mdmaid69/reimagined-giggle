b3cd09dae0317e4da3313197c2ddad7bcc1b4321
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:35:06 +0000

    doubt description number tidy talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_directory_name(path):
+        return os.path.dirname(path)
