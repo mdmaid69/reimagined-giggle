c09a0c1eeda1583cb799babc86170065fc4f820e
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:14:17 +0000

    marry drop save worried clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
