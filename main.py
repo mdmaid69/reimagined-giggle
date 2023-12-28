5e58db2398c41ad1039db6051c3dd2093429731c
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:49:03 +0000

    object furniture talkinto people there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_array_as_complex(array):
+        return complex(array[0])
