47d07b50d73d8720a81a62be518ae64269b2ef9e
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:36:16 +0000

    transparent embarrassing organize judge summer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
