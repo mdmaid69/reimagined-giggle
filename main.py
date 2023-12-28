d750b518787b4a20c0afd509bad969b5cb7a3d7a
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:42:15 +0000

    comeout shoot night ear Nn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
