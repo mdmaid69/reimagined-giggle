52926a3b90e4f8749bc05666bde2dbf2c9b13f85
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:49:41 +0000

    copy get afterwards between place

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
