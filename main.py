b2e1d43822a8c3de735efb139d0e204c2c7facf6
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:37:48 +0000

    insist score measure tidy criticism

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def add_numbers(x, y):
-        return x + y
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def remove_duplicates(lst):
+        return list(set(lst))
