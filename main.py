7273601afced1538bccec27ce7450b91d72bb755
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:05:43 +0000

    bring separate approve represent sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
