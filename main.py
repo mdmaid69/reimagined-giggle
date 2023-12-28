e139bc7333c226f0883e5cefcfb5108a00e8289b
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:08:31 +0000

    cry minor sentence spot spin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
