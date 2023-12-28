a95792c85b259b6f5767a7c8eeb360fb0b2bebd4
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:11:22 +0000

    comeout mysterious offend be bye

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import time
+def get_time_since_epoch():
+        return time.time()
