2f2c6b80bcf4b16244b13ea81c5a3e27613c649d
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:39:05 +0000

    loose joke independent similar inform

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
