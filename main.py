e0d32e0f303b6d2ea4ea30884f765d1fd1caf390
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:34:47 +0000

    dish dear story pause answer

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import collections
+def create_user_list():
+        return collections.UserList()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
