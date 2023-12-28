3bdcf2818e730f0a672e7ee4f6aed45695be35b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:21:00 +0000

    hot awake object poor Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def is_odd(n):
+        return n % 2 != 0
