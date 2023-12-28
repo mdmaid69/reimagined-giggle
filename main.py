492b378ed7f3e9cfd9056b0d0d053af343347b2c
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:11:52 +0000

    partner minority common active persuade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import time
-def get_current_time():
-        return time.time()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def convert_to_hex(n):
+        return hex(n)
