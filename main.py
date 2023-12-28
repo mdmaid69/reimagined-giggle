162be0d64ec868e87f35e7493465eab0974c46f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:20:34 +0000

    really coat roof indeed difficulty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def get_list_from_array(array):
+        return array.tolist()
