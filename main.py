324ad82a5370493630ff73b1d79eea7ce9c4bd6e
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:08:44 +0000

    that flat forward beautiful means

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
