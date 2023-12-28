46bf31b2bd60b46b0a1b68e034e7daf0974994ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:03:33 +0000

    advice nose plain miss cope

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
