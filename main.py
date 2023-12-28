6351775c9419a3f81695252517ba76105cda6179
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:49:05 +0000

    father permission noise instrument sentence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
