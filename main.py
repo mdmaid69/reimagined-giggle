7898469a2ad09639487d57b95f88cae330cbe017
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:37:25 +0000

    half tomorrow wash bright personal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_cosine(x):
+        return math.cos(x)
+  def is_odd(n):
+        return n % 2 != 0
