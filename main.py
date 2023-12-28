d40e9df8df9661102181916b4751fff81bcc018e
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:37:34 +0000

    use drop heavy port spoil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  def square_number(x):
+        return x**2
