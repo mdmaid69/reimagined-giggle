1e94636d2a6edd1da2c492d16ea1b8c593cd642d
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:30:43 +0000

    guess temperature exit approve doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
