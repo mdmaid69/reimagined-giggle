a0b3a850a98543203b369ead75c3db5bd8719c9f
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:56:19 +0000

    course goaway many wish hard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
