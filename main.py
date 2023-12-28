03ffba72478ac5aab1e59864eb652ff59f4c54a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:34:53 +0000

    impression every spring suck connection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
