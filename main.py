b335b0d024832c20a7bb31d06a644ff28b27f149
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:20:39 +0000

    nerve sense Yy pause well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 def calculate_combinations(n, k):
         return math.comb(n, k)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
