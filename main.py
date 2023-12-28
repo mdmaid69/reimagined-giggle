a4c3642e47c777f1c083929da3d0e404de925de6
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:31:44 +0000

    rely inside cope hello like

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
