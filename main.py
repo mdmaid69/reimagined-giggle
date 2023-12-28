41046846f0c755d2b0789be19c0191989a090156
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:55:17 +0000

    nearly clean urgent hide beside

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def sort_list(lst):
-        return sorted(lst)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
