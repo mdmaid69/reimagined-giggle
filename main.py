9604edadbd04ef247cc86e4598ebe88518f3aef0
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:32:50 +0000

    mix once stranger exit teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import os
 def get_file_modification_time(filename):
         return os.path.getmtime(filename)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
