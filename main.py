f05ad823cd43ef35be120db4dbd33719c88c207c
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:03:42 +0000

    watch ashamed smile fall relax

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_work(force, distance):
+        return force * distance
