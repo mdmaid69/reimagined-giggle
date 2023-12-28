731e713a8af6f71c4c0c525cb3cd0c8ac22c9e72
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:27:10 +0000

    quiet order sign contrast pride

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
