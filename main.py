1a619c2a4284887a1f37cc617bf6be446ecfb8fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:06:42 +0000

    takeback off begin cook dinner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def subtract_numbers(x, y):
-        return x - y
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
