dc69e5d8ce1fd4947d0ea168106376dcc31ad984
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:19:09 +0000

    fence treat enthusiastic calloff influence

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def count_words(sentence):
+        return len(sentence.split())
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
