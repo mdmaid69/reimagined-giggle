ca9ab982a64799a82c257f609d234cb2695c38ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:49:43 +0000

    wait nothing involve repeat please

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def is_even(n):
+        return n % 2 == 0
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
