14c682e1a51f1144069e6acebc564d1d8ab74ebc
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:43:45 +0000

    shop complaint yet pull crowded

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
