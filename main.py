265d356334c32191f258fb151439f266c3c65753
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:15:34 +0000

    harm file bridge type Jj

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
