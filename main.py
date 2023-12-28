d9b230be6697d19fd4d24f68521ed0d661101675
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:52:50 +0000

    slip independent clean hers vanish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   def add_numbers(x, y):
         return x + y
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
