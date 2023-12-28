beec8ab3db493332d301e9e4023d25a3ee582e71
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:15:31 +0000

    inform continue trust surprise form

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def cube_number(x):
-        return x**3
+import datetime
+print(datetime.datetime.now())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
