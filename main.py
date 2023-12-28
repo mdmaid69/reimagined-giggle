a6893defe9cb57179e2307b6cc298b22bd64dacf
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:28:38 +0000

    belong all famous divide female

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_current_directory():
+        return os.getcwd()
