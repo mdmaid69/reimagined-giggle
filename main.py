1a8401bdf52e1a131a8a00bd5c20452660209a4d
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:41:11 +0000

    flavour getaway field sensible juice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  def is_odd(n):
+        return n % 2 != 0
