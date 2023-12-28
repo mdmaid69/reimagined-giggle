aaad29c80dcc53da8d8b536eed211091672fae48
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:06:52 +0000

    reject better line work question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
