b236f7e28165382c99fd64b30abec1d53515c185
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:04:24 +0000

    whichever luck smoke copy person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
