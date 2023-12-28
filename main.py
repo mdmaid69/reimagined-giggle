672e94a51549a0dde7fafaaab124bfe4468a573d
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:04:14 +0000

    regional switch passage Cc shout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
