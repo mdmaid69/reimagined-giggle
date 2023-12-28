f2a53089cfad44b0bae33f0dce4690a9bb0782bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:22:37 +0000

    skill hungry steer quarter take

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
