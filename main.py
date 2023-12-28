e60d8a832064cb2cac806cc465951b4fed2adf34
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:25:34 +0000

    share doctor freeze beside ability

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+for i in range(5):
+        print(i)
