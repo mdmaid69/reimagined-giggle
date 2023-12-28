54b105b94f850f3662f52eaf529c446aae5dbc68
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:56:25 +0000

    quarter slip prepare private there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Uppercase:", text.upper())
