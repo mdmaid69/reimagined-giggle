4bf577ac78bede67c56f2f31c560958b47755c6f
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:33:14 +0000

    work change takeback rescue Ee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_logarithm_base_e(x):
+        return math.log(x)
