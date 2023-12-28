9a2e80adb4ec374c4c4a1a47b17a9e3c1d5f3fc0
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:35:01 +0000

    scream lead name dull window

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
