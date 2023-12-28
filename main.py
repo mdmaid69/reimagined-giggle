cde67d7ff5313d4136c50bd75be442f85c8c6aae
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:27:54 +0000

    usually phone test care culture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
