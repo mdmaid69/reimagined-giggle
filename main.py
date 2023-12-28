53be138bc0b5d7b3ad9b3a5c63467da3af043a50
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:12:55 +0000

    confusing condition kill age combination

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def convert_to_binary(n):
+        return bin(n)
+for i in range(10): print(i)
