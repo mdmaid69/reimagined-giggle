40fbb8285fadef5b765ad640c73a9f1f5b6c8642
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:44:12 +0000

    such different switch knowledge opportunity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
