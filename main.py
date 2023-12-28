dd26f7f381e017aa43058b5ea2b5397c05a825e8
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:03:16 +0000

    block leg introduction matter trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
