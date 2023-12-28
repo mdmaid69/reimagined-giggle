240aa36e41e8deadf90a3ee6a93d87d9d93a7478
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:31:42 +0000

    power consist turnoff shut gradually

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
