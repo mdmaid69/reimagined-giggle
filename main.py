fabb1d58305716cf45b7aa2d964a8ecea656eb02
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:10:56 +0000

    forward principle solve worry lesson

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_str(array):
+        return str(array)
