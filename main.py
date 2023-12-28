aee269e9d0d7673b3d2b83e6cc531d5d675072eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:46:06 +0000

    pain concerned recent eager reach

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
