a8d84bdca58ecd2e97d9f74fb1ca1e3312ff5938
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:29:25 +0000

    year rise burn nonsense difficulty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def sort_list(lst):
-        return sorted(lst)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
