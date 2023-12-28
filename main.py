6d8b504fda4f05021b384c9a00d94c4d979432ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:43:09 +0000

    we when mark tip Ii

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(10): print(i)
