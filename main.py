6278551cc2788f5b9f95e6c6386a1b153794f7d6
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:18:04 +0000

    moral can poor sure else

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
