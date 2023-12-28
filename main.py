02f6d399d9519531740a04e34d8567766fa7e01c
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:13:34 +0000

    mention above she swim general

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
