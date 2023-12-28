5c469faff9ccd5b5f058f6fda55f09f914731045
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:20:34 +0000

    news rare single road building

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
