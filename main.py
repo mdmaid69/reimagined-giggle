13b43ff476cce757787e46a7c11c8a635a86bc52
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:15:42 +0000

    thick present funny run dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_hex(n):
+        return hex(n)
