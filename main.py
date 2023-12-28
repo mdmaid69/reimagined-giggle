a7a7310553de65a4ee6b416eb8e375a50722fab4
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:32:20 +0000

    height forward fat block write

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
