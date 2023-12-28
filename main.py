5bcc645f010588bdb730d7df562a8a8fd3b2d277
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:33:21 +0000

    capital beyond tough fasten glad

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
