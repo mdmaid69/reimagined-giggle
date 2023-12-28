66ea891ea6c40a0e0ed7b9a51eedce2fd197b1eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:54:37 +0000

    substance late section different worry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
