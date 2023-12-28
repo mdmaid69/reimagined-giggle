1f7eb5138269998490883d504fb6b621fa789b45
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:11:26 +0000

    wind uncertain chase so objection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
