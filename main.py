ce0278a34e9a56f7b36086994c5d1f71a4eecaa9
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:10:15 +0000

    base cold spring heart check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
