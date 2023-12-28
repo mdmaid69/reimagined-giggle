4547594e926aeb981374be565f3ba9599d71cf5a
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:53:26 +0000

    getup innocent silent lucky idea

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
