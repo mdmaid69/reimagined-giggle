1d230ed7d35a76b2c78207ec53edfac384b7f7f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:57:02 +0000

    firm goodbye study add dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
