7b806ab350cd581fa0dd2ef1b69f2bacf888a70a
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:51:31 +0000

    listen rub some low suggestion

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_work(force, distance):
+        return force * distance
