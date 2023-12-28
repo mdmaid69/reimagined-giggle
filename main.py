a5f7bf9e4217f78f324b084f52162a19106f2089
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:00:21 +0000

    Jj thick who callback water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def sort_list(lst):
+        return sorted(lst)
