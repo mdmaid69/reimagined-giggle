9d9b840e99ced5560a73dd886714da6e8ec7df4d
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:45:06 +0000

    interest greeting gate copy fold

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
