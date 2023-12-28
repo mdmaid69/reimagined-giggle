52084655d3675a012db56d98b66ecfad3874d455
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:24:29 +0000

    mother 570enjoyable spend out bye

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
