8dde38392c363e7e967be18c40570516d272447d
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:27:32 +0000

    example class use floor set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
