6a6b2a26015cd9d9062280cd35da2a1d38827790
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:00:50 +0000

    buy whistle dress object fasten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def add_numbers(x, y):
-        return x + y
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
