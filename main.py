1f4cca34f7dc8bfffc24e972126e329927238b7c
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:13:49 +0000

    partner taste thirsty design brown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
