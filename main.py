9f55ac2947da3422f6947c42dc74ae526f034e34
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:14:42 +0000

    travel prize mean happy tune

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
