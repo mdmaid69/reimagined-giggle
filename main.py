a0ed40ad4277c948dccc84c97b1eb762573ad4cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:44:31 +0000

    knock party comfortable continue avoid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
