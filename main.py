5e5d10a6beefa5f3de709c6a16c01fb67f755cac
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:14:08 +0000

    up cost profit better own

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
