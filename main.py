8ea0472b99b6eed74cb03350a46f663f327cff1f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:46:56 +0000

    suit dick love obvious answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
