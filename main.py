992955d110b8431aa7171bf0a360d23574dad93c
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:36:43 +0000

    fit total guide say position

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import time
-print(time.time())
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
 import array
-def append_to_array(array, item):
-        array.append(item)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
