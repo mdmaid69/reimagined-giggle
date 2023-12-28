0502795b4a6ab7f28130cc326f492e187ec56d28
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:37:34 +0000

    pattern journey low great brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
