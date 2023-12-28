8bb90828f762cdf3ace7ad1b8518c8b174df451c
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:46:28 +0000

    place matter doubt dream pause

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
