cbe23b93ca52d6c6deb2a608bc5b86bb8c5927fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:53:00 +0000

    world temper lie point bar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_binary(n):
-        return bin(n)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
