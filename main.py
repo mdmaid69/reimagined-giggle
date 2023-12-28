9cc665fae6799f28009afc680a114cfc0f026eb4
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:47:53 +0000

    blue aware hang probably trade

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def check_if_array_contains_item(array, item):
+        return item in array
