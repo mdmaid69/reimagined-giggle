93850908b7addf7a66529006964c5db4cc9340c3
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:37:27 +0000

    peaceful conclusion contact extremely side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import sys
+def print_python_version():
+        print(sys.version)
