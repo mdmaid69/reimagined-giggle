09f1c3320a27f385f63bcf2cd563745a9a77adf5
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:22:18 +0000

    used me bright argument complaint

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  def add_numbers(x, y):
+        return x + y
