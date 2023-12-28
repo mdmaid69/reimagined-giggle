efcaba6074ae0f960b9a74bd8783b953fd2a5dcd
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:57:17 +0000

    maybe young give extremely include

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def multiply_numbers(x, y):
+        return x * y
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
