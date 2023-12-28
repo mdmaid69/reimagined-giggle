1ae996aaddc951968d2d04081e493b17fc1a1f68
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:40:34 +0000

    pocket hope regular succeed off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
