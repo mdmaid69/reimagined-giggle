8ac05b7ae5c193d2005aefbb9f58aa7f4713ae3e
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:52:24 +0000

    forgive opposite remove trip Yy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
