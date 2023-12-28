245670152b26be12996cbeef5c43056fdacecdae
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:49:14 +0000

    lot goback(to) one attractive land

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sys
+print(sys.version)
