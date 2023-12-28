c46d820decc31396aa13d5476a7960e245c354db
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:36:46 +0000

    nose operate Nn above arrangement

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
