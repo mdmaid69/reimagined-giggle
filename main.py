1dd6bcc1dc1d6d93bd0875f84f15f86743319001
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:47:23 +0000

    nowhere present pot test during

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
