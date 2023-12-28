cf2ae737aebcd58a46a0ca2c5e41cd53257d25c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:55:55 +0000

    later train by year best

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def find_max(lst):
-        return max(lst)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
