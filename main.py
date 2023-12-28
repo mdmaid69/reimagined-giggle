8cd7e77c7d999835f424dd93dd78634c48393ec6
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:17:05 +0000

    lead dish grand roll argument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
