a5a3be97a1ba1b2383d4b91ffb89679bd983c220
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:12:53 +0000

    share patience instruction needle usually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_directory_name(path):
+        return os.path.dirname(path)
