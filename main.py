6598f253fdf103a804a2a3b0dd78f46234800cea
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:01:26 +0000

    Zz fall joke relative soft

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
