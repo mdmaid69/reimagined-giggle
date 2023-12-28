9e6238066ee1653a58c9509962f0e7f0a6e8ba81
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:31:23 +0000

    nor rare goon float and

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
