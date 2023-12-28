04a6f939ee87ce95ddcded87869b781be3c9ec9f
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:40:53 +0000

    workon keepup sudden throw shape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
