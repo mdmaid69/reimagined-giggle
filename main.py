8ee7dc03a74dc8fcf366f262844bbe070e49b66f
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:20:06 +0000

    noise price cover approval nowhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
