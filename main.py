4b9d58fc1bb831f40253fbf3001652480700c71f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:57:21 +0000

    surprising no trade join accordingto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
