f01859997d8ab543dac1bb55242b807f8384e7c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:23:18 +0000

    butter file emotion until modern

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-name = "Python"
-print("Hello,", name)
+import random
+print(random.randint(0, 100))
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_repr(array):
+        return repr(array)
