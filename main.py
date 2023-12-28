031aa28c8bf0209babb019ee51abc5efc59ef929
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:17:29 +0000

    common negative judge successful print

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
