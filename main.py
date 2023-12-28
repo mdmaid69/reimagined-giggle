731591ac5cd030cb4f1b487fa64df990d07bf50a
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:39:18 +0000

    hurt many mind protect call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_acceleration(speed, time):
+        return speed / time
