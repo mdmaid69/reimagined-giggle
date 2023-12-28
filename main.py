425018da37703a7e9951cd87a0e25b46c5db021e
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:21:54 +0000

    declaration cut post get generally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
