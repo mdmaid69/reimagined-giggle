43fd52bce2623a1269f157c810f2cac349ea155a
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:32:06 +0000

    limit experience concentrate bet long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
