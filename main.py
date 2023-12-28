63f40bd1e033fed2b82724b5b01c3372242bef80
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:11:26 +0000

    land carry arrange mess congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
