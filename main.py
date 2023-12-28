bd13f3fab8e7038410287566b28036c6f6195513
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:29:46 +0000

    chin fasten anyone cool tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
