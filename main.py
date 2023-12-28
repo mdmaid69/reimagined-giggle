2fd0c8a15a8325660868dcf73d6f14b45b3bc4a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:46:58 +0000

    taste comeout minimum whether used

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
