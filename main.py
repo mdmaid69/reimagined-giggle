d6856b792306912331d44224b774422bd93e63a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:12:45 +0000

    sight consist talkinto mention afford

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-for i in range(5):
-        print(i)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
