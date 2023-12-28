44735370c93d8c22842e4c54098b1b1f5ef6c701
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:53:46 +0000

    between mother laugh everything draw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
