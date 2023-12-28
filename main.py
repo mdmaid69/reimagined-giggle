6dbcb65c090f67b156aa1ffd6e3dcdd5531d484c
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:58:29 +0000

    high butter test getup because

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
