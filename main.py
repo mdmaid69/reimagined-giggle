dc9a2d5dc53ecfa4bdb94647499078e7fd6fe528
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:17:51 +0000

    finger detail there breakdown arrange

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import platform
-def get_python_version():
-        return platform.python_version()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
