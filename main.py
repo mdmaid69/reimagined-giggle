2b90e166be1c26ca1490ac6af37e912fd202b46d
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:08:57 +0000

    consult spring spite private that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
