16d55bf87056e3b4c4f6669cba612a72fbe68269
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:18:48 +0000

    gate back sink safety show

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
