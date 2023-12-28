9af43c39003bd78a9c70b54868d152dde4222e07
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:05:31 +0000

    so comeover month education fold

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def divide_numbers(x, y):
-        return x / y
+import array
+def reverse_array(array):
+        array.reverse()
+from collections import Counter
+print(Counter("hello world"))
