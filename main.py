c8c4af0e6ac96009f67df53b21c4a5cb9d1f2e64
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:52:32 +0000

    earn familiar skin acceptable left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
