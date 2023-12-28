d98a2566e2c798d6123beddc3907188e0c196664
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:51:26 +0000

    recent dear sort attempt opposite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_unique_words(sentence):
+        return set(sentence.split())
