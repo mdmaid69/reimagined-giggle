8bff09d37bc07676a524c40f40d276862a251979
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:17:16 +0000

    birth gap back right vanish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
