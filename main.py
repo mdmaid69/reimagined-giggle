d2cdc10dc4bd54ac885ef7a1f75c64410ce51c24
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:58:35 +0000

    Ff sign hot demand train

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_floor(x):
+        return math.floor(x)
