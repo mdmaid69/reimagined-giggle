cf71eaac3597f6f9c3fed2be2f18b5a9d4ba2713
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:19:53 +0000

    ring father introduce operate grateful

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import time
+print(time.time())
+def convert_to_hex(n):
+        return hex(n)
