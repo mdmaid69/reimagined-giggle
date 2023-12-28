30a566cbdf24a2b82ec210e3262ca81e354b2db9
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:41:46 +0000

    Tt characteristic everyone recent turnoff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
