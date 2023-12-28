0427f28c4a1dfa4c790956c7d6f116e939817ada
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:33:43 +0000

    buy recommend afraid prize from

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def remove_duplicates(lst):
-        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def convert_array_to_list(array):
+        return array.tolist()
