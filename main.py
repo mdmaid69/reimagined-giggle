60d1708696eb46f7aae419848a7983c741d16643
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:42:19 +0000

    can club conclusion life considering

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
