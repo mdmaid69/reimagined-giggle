ca0717fc852029cbf3fda2ede402850096cfb1c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:58:34 +0000

    aim dark introduction the comment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
