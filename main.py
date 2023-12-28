39e9d02f4734f7a0aa167666e675a3171f122c29
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:27:57 +0000

    not turndown substance independent forever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
