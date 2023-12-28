103fe3c54b5668bb5f0bb5bd73a6eea7e9a93757
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:52:34 +0000

    wave impressed safety tidy me

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def is_even(n):
+        return n % 2 == 0
+def calculate_speed(distance, time):
+        return distance / time
