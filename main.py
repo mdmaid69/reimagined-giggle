9da932a5978a03961140fadd14f1625e48a0361a
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:22:50 +0000

    skin callback smoke miss division

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
