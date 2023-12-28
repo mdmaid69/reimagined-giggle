ccf4ce3af67215b5f30ce4f709e6917cdf54b4d6
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:14:57 +0000

    shake if exact share load

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
