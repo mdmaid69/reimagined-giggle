04c7404bd7018ee8e43c92723ca66ee6923d0687
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:13:49 +0000

    Nn foot immediate next level

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
