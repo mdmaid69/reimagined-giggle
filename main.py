ac46fd6d0ce8f88423bd344026c67ac85a56c97e
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:30:35 +0000

    count accidentally need Nn past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_odd(n):
-        return n % 2 != 0
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
