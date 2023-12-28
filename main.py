9c20857e0f9e61f1f3d936ce016b6e9915158cae
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:31:27 +0000

    sight agree importance himself ought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
