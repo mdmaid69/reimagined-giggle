566669a1a509999e93dbd7a1c5a13f0b77e45266
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:22:35 +0000

    error minority afraid unlucky lonely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import sys
-print(sys.version)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_density(mass, volume):
+        return mass / volume
