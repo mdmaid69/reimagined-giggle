e5fc08b5a80a94503e15ed96cadffd1471582bbd
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:15:11 +0000

    slice goout prepare edge you

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
