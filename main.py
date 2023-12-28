371cafb3d0f39463b5e78cc05e49a6faa20b0580
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:09:28 +0000

    lend inquire exploration emergency model

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def square_number(x):
+        return x**2
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
