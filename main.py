b00c5d099c9971ab662dfd450c0a599a87196f1d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:43:13 +0000

    hers always least drop picture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
