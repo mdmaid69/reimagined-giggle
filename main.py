261ec2ea920a29f300d1f7c1c43be999e48ea39d
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:09:13 +0000

    him miss fault container use

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
