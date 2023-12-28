5d84a6d7d18522a43e985c41101aecca3d3b1c52
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:25:29 +0000

    payment steer spend ever thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
