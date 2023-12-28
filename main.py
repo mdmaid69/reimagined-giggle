29490867c075a7acf919317e971fdcff895afc84
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:04:32 +0000

    bear though neither punish welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
