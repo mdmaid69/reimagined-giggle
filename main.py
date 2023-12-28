b3a02e9ea051ebf2e5611e08da5d4fac47760684
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:12:19 +0000

    any smart care preparation pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
