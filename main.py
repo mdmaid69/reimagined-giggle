b2c2e00feb3b1cda36fe33f0ed292f047a3e0a59
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:09:48 +0000

    keepoff goout table discover person

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def remove_duplicates(lst):
-        return list(set(lst))
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
