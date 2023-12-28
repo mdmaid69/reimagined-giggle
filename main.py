1b90fb4c0376cfe40561c1a05e4dc884e769ecc5
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:26:38 +0000

    yourself group compete city wind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
