d80fb76302623a8d94478baf12f50b8582e49d5a
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:23:43 +0000

    duty disapprove funny state callup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
 import sklearn.datasets
 print(sklearn.datasets.load_iris())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
