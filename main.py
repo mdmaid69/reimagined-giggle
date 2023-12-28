090257fd77582f634e9293e577de6bfd66262bac
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:56:15 +0000

    truth plan dig wide steady

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
