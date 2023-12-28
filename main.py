214b2f5d7ab311daf892c8e7df2977e2112ea563
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:50:13 +0000

    reserve simple shelter switch either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def print_python_version():
+        return sys.version
