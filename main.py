2c86e863c8625062e6ef252d85b096ff279d911f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:53:42 +0000

    greeting port meet related exam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_area_rectangle(l, w):
+        return l * w
