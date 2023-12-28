c53b1fdca6b6932798be279db38c7cd8616c6eb7
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:45:26 +0000

    clean steam black share success

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
