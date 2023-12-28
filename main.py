ea03c6e5cfcaf3b2708f67d294bb08d2acdf69e7
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:24:07 +0000

    maybe cope actually around pause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
