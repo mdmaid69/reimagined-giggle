2f29d244596de163160689453a324340310f03b9
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:55:53 +0000

    space half clean use little

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
