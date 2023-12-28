b278e44135e8e23099605a5b11d519f3fd36b176
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:30:23 +0000

    learn female say steal maximum

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
