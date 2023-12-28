16273f492ed3e541b6b7cda31d6a49fe704cb368
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:37:21 +0000

    well negative by second depend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
