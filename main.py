0f66dd7a455dcdc1ebc1edfffc3ed2814c2244b0
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:31:29 +0000

    mean suggest lower keep opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
