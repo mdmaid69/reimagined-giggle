e479f91bc1ada1625f34ee78e3aaacecf0975068
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:27:42 +0000

    leave suit nowhere floor chance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
