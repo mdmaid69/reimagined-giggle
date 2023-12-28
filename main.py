ce244e18ad50dd6f0fc75416dfb40af61ee8ca33
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:09:08 +0000

    copy me stay finally arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
