12d855882dfc1d43e72ea68f2d109cf86c362d48
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:22:22 +0000

    solve benefit lot hour fight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import collections
+def create_user_list():
+        return collections.UserList()
