d4d38b2d8d4970511c0578c5f6edb8ade5a30b01
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:48:30 +0000

    wind little full kill dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import platform
+def get_python_version():
+        return platform.python_version()
