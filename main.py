5927fa4cc0890b82eb9842455843c5091f26a22d
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:20:58 +0000

    hard blind oil damage in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+print(json.dumps({"name": "John", "age": 30}))
