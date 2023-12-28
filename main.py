6234756fff859b27954ae26b1479ed3b2498cb0b
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:26:08 +0000

    best wish wide go exhibition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_palindrome(s):
+        return s == s[::-1]
