09287ff877d70f1d8db0bfa4030aa8ef1a0f6f05
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:49:08 +0000

    development shall example profit priority

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
