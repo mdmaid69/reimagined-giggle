aa6ab85e0ba67fe14d2f68ab7c45cad6eb5819f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:15:05 +0000

    trade pull Qq must congratulations

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def is_odd(n):
-        return n % 2 != 0
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import platform
+def get_os_info():
+        return platform.uname()
