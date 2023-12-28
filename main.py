3bc2ac6393fd8bbc0803485e28a47158effef547
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:18:55 +0000

    curious loan thorough learn country

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def add_numbers(x, y):
+        return x + y
+name = "Python"
+print("Hello,", name)
