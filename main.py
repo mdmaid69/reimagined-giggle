bfbe932c852f1229b2f3cc413b4492c6d89d3873
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:38:48 +0000

    separate report together talk until

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(lst):
-        return max(lst)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
