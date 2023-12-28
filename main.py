0cd17aebb1b4bcb022ca47b30f2b95400e50e9e1
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:19:10 +0000

    hair argue trap acceptable chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def convert_to_json(data):
+        return json.dumps(data)
