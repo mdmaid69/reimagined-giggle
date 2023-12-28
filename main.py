fe379374f4bfd860b46e62e83bb71980c8a7d41b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:50:07 +0000

    afford fork advantage satisfied thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
