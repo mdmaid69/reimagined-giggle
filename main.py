ea09813e5df8058722eece5f7f42cf313fc69fe1
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:45:54 +0000

    goodbye cope set between switch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
 numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+print("Sum:", sum(numbers))
