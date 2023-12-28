bec54b010b5228a47f905062c1514f4493ee9248
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:24:33 +0000

    estimate flow fail still Bb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
