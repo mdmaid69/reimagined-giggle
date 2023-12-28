b180f7dc9ecf7f72b2b24ccfd93e04c3386c2932
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:07:09 +0000

    catch stiff throw remove stage

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
