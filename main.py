ebfe075ed86df4cb2a859bd2e59794dfc5e8c416
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:07:56 +0000

    spend refer fence save combine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
