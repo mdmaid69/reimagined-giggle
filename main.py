db165ad5e3844b3463bb8a6ea12a8272541f2913
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:08:22 +0000

    wrong answer kind illness far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_sign(x):
+        return math.copysign(1, x)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
