9f94bea0a8072b3062e5b3e09337ab5a7ac1666b
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:01:29 +0000

    build behind star somebody comeover

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def convert_to_hex(n):
+        return hex(n)
