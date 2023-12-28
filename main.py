8c431e48e1fb55243ba569e1a2c8c861a745dbb0
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:25:10 +0000

    tie believe scale thirsty remember

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
