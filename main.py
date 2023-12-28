27e43631d30d53ae15aac0c6da190935859b1418
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:51:46 +0000

    wear cook makeinto scream turnout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_force(mass, acceleration):
+        return mass * acceleration
