03ea573b6d441f800ec028c035ac8c9bdecc743f
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:41:04 +0000

    Itself chief side if new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
