62d91cfe34f41bcdfa8878d7670ca6606565b2ec
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:07:22 +0000

    character careless comment advice convinced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
