bfc1c79e723c45013cde8cfa99929f957a041827
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:20:59 +0000

    worth train considering Ss corner

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_array_as_str(array):
+        return str(array)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
