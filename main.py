23a0e1aa80ae225ca9b92fa86508f7d42e331bb8
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:47:37 +0000

    underneath tone preparation crash kind

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
