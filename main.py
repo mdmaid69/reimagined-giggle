47a265a42ebc1e1ad29a957bf3b2426f17b7d1ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:08:36 +0000

    high high special Nn place

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
