e86ee98efb13d2d0ea2d84b030e2747d149a0c04
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:03:47 +0000

    exciting calloff offence certain mail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import platform
+def get_os_info():
+        return platform.uname()
