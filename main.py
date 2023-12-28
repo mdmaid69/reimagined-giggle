660a8c10005744719e4f88fd3b8f80757f30b3f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:41:04 +0000

    goodbye success send design Vv

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
 def calculate_product_of_sequence(start, stop, step):
         return math.prod(range(start, stop, step))
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
