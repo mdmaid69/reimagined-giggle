dea17e60e150b3dfb427324588904a1502920d98
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:43:52 +0000

    lookafter so offend tonight instrument

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
