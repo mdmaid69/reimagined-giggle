b237ff3a43f28d5c9ef5d58ddf2a802610dcd886
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:45:09 +0000

    recently vanish getaway check whole

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
