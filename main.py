b620bf7ea2ed564e248b98ba4e3f05ff6ce4ca02
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:34:05 +0000

    glass rob go command attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
