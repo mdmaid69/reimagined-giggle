fe1236bf61d23d267504044c3a183fc349ff0215
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:51:57 +0000

    public possible 565end yourself excuse

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import collections
-def create_queue():
-        return collections.deque()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_acceleration(speed, time):
+        return speed / time
