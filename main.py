232775785ef16bd091eaa12729510d4acb4e7f3a
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:15:42 +0000

    to hour butter buy blue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import sys
+  def get_python_version():
+        return sys.version
