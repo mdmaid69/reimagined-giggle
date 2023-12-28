557d0504c61a4dc10bead37c16f9ea0adfee33d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:38:28 +0000

    keepfrom relax happy dark would

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_current_working_directory():
+        return os.getcwd()
