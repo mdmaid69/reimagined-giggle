0f7d2ed1f415411875f685dec1fb6be8de7b9b39
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:22:24 +0000

    certainly disturb crisis Vv minimum

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
