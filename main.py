afb2d2b5afe977daf0781323b1383ed0b32e87a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:06:29 +0000

    shop climb black sure frightened

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_volume(length, width, height):
+        return length * width * height
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
