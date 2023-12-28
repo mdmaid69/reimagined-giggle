47a228c32e70a8158ba47ca3a2ca3400d1a86276
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:53:37 +0000

    on tune pause glass can

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def square_number(x):
-        return x**2
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def reverse_list(lst):
+        return lst[::-1]
