c4da897f6d9b165c34afb28bba5b0fb92bf73b31
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:12:46 +0000

    dinner 179bite danger keep active

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
