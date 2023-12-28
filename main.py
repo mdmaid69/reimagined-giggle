fb8beb94ccfdb9a9be461e0c325b10ae7f28b2ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:50:45 +0000

    confidence live strike work well

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
