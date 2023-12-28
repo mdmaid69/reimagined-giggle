7214e83a619c467819c6871788038ebd45c2d3cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:37:13 +0000

    lock spare skill agree car

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import platform
+def get_os_info():
+        return platform.uname()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
