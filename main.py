3566bc2902fcefc12d76a649c635fb4e91c03db5
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:22:08 +0000

    autumn comedown dinner off listenfor

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def cube_number(x):
-        return x**3
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+print([x**2 for x in range(10)])
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
