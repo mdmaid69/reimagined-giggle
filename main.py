901de793368368228a6be43de8e0e4817e0006a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:58:04 +0000

    anywayanyhow nigga plant view square

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sklearn.datasets
 print(sklearn.datasets.load_iris())
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
