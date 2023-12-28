475dcde76fb46ec011846654648180392a23b841
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:13:37 +0000

    turnback fill circle work breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
