a45d481b69119a3ed95ce74d67e7d4f17e719e93
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:10:04 +0000

    afterwards attempt another content fairly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
