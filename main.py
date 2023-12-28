47bf4e052cc5802fed8b4dc818a53f3f484dd402
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:58:38 +0000

    long cruelty example study operation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
