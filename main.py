48a0c6056a8eada1970d8f67a8d4690097414647
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:32:56 +0000

    centimetre club Vv pass runover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
