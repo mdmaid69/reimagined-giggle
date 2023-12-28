7a1c52efaa802c511f474fcac617369e40a417d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:27:06 +0000

    quarter scene long collection callat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
