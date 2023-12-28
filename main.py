67a66d25d96b6ca29dc856075d4f2ddef0e42206
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:39:58 +0000

    Bb relative guard kill hit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
