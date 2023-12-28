057a38b3a9e934fb2e34aa4184d956df415f2b21
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:43:55 +0000

    point introduce last throughout keen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
