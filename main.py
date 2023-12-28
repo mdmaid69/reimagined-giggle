f0a7ce02216efd97d2fe86985898688b69fc95e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:16:40 +0000

    advise platform everyone later invitation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def remove_duplicates(lst):
+        return list(set(lst))
