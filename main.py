3bd4723163b9a48ba01491fee60b9bdd2b78d5ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:21:37 +0000

    succeed direct consist believe wall

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
