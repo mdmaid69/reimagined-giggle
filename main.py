244d34503a26324bcbe74626ea4be7ee585c69d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:08:57 +0000

    179bite square help rise escape

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  def multiply_numbers(x, y):
-        return x * y
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
