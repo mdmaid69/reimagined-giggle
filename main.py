ae488e33d1e57704ef37a63762e20be17cd4d29e
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:15:12 +0000

    show anything free only loose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
