8aabddab9892f5df820b83a95e4b96357618987c
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:44:06 +0000

    party original invite convinced comfortable

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
