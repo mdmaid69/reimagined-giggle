30267f5f3795e9c6bdcacc19ff7fa72ce5a6f4a2
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:27:09 +0000

    impossible involve polite bridge last

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
