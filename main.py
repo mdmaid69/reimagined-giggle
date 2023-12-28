23c18daf693bd85c68f47bf5e73f34195bf82ab9
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:02:59 +0000

    she valuable wrap worth exciting

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
