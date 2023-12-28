43e300a3c194ec9299ecc0fb520f02058c6cb25e
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:27:24 +0000

    declare major lose frame load

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
