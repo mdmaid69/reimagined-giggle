f3220fc37deb3cfcbfd0ea3c199e0f493d2bc46e
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:42:04 +0000

    fly my interested rob figure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def cube_number(x):
-        return x**3
+import logging
+def log_message(message):
+        logging.info(message)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
