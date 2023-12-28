bca2e6326c8d4ecdd461e5c0fa6d674fb63aac69
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:09:40 +0000

    zero bin steal bush break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
