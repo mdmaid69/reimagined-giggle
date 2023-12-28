152aada9863d82dfc2789dd60d68ec236dc10b00
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:46:17 +0000

    trade us development wait pile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
