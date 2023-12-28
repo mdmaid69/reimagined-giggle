1ea9d9a98aa13c62c1c5dbde22d51246379b5f7b
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:22:11 +0000

    tax fear congratulations discuss birth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
