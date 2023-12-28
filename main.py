cb21b13e70e9447feb06ebda6bdd32acf3025612
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:05:03 +0000

    odd none wave body business

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
