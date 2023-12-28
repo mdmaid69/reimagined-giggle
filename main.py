f26f2c85c2716bb1dc389d43b033778923b3eb38
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:48:46 +0000

    neck business smoke repeat chin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import collections
+def create_user_list():
+        return collections.UserList()
