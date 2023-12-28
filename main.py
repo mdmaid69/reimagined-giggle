674b76d56b213591fa5c4c4a78ee4eab6d23044d
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:06:28 +0000

    smooth attract disappear Gg breakinto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_factorial(n):
+        return math.factorial(n)
+import platform
+def get_os_info():
+        return platform.uname()
