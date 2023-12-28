5fbe9a9edef86c740a8eb727d5204f7557157824
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:55:31 +0000

    no general carry miss movement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def insert_into_array(array, i, item):
+        array.insert(i, item)
