8561b06d3f9ae3e37b9303550ef6a0d9bdd9abb0
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:10:45 +0000

    side impressive swing man string

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def count_words(sentence):
+        return len(sentence.split())
