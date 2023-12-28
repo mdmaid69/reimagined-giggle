d22a8915a153f0f194819f327ff633daeb682d3f
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:55:44 +0000

    advise gift press active market

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
