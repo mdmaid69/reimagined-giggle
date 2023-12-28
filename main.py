b16cb05e72d7d4bc8aa9b3584cffadb084b12086
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:25:11 +0000

    there clothes top comment several

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+for i in range(5):
+        print(i)
