a5f295bd0b523b73e9b46f82668534cd0abc4b79
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:46:46 +0000

    heavy perhaps past them wealth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
