f0c50c1cb8d199764c83b417de6dd08e2426c924
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:15:36 +0000

    note emphasis divide bringdown bringup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import re
-print(re.match("h.*o", "hello world"))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
