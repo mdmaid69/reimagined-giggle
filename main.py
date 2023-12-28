bba1116498c1266e9a66770b86324cb13be19141
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:42:23 +0000

    close weak either draw remember

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
