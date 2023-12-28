6aa0c23aea02131f81043f01be0fbbad8778c498
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:42:36 +0000

    fine sort relation insist left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
