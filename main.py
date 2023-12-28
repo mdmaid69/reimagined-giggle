fc8b354e1690f1f4affec850ae7f2886c13ff355
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:50:45 +0000

    handle exactly telephone improve so

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
