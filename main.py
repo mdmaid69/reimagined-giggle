9d99db21b106613ca94da36cd04919e8426eafce
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:33:42 +0000

    shame Ff pour Mm class

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
