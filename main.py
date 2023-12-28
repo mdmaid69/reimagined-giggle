cda3572e3c2ef81ae28c6fba51887aac5835d491
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:22:29 +0000

    fear mad owe legal heart

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
