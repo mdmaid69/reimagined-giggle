95731f1fc18b4097b16e766c8cc4c53985974c5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:35:28 +0000

    stretch 569enjoy plate match callback

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 n = 10
 print("Square numbers:", [x**2 for x in range(n)])
-  def remove_duplicates(lst):
-        return list(set(lst))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
