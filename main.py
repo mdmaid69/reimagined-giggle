98ea5c5a9d340452034ccaf8deb949cc2970e6ff
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:13:39 +0000

    ours loan although Dd niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
