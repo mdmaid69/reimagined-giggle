95e05c0c3734c1b2e11cde621656bcedaf91db9a
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:53:19 +0000

    imagination distance typical damage muscle

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
