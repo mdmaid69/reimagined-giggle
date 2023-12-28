dda12574763b5ce60fb4dacaaaa9464dc678831b
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:34:33 +0000

    this his successful ride upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
