d1311b2c73e52f09cfa2091866bfcc21c1f065d4
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:17:38 +0000

    attempt attitude furniture pretty keen

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
