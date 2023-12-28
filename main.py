babe330584c1e0dcc41722f402ca7a9daaa45a64
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:28:30 +0000

    measure shine juice although desperate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
