2b9cfe625d73f4b07862791eac0f5310549a99f2
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:37:20 +0000

    left seat prove calmly Zz

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
