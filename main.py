3bfe0e770c9e246074b53a57261a1b6d76d3cb06
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:22:13 +0000

    correction name black various left

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
