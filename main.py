18cc497af72d47c81925eb99e3c31de67b7dea60
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:04:55 +0000

    original Ee ride promise suffer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
