1a20d028ca9d4f030879ab1d10454d2020b12802
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:59:12 +0000

    prize hold appear search shoulder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
