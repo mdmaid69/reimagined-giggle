baba1970960bc622e4137136fcd61f399a7105fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:43:50 +0000

    uncertain anyone tonight first office

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def count_elements(lst):
+        return len(lst)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
