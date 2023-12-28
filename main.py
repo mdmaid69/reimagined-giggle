f65940d67ae26d3832455323ef6babaffaa16a8e
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:11:50 +0000

    off picture open refer fence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import datetime
+def get_current_date():
+        return datetime.date.today()
