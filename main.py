5b2e72465a3f26c549de6c73cd76ea1cbb9ee4e7
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:29:39 +0000

    prepare perform ignore realize nigga

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
