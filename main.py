687edc71b282bc843f3fada942e03db1c032dad7
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:59:15 +0000

    cross during temper middle back

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
