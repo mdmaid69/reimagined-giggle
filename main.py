cf243280db3bb702e33b023d1e63bee72d74d48e
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:02:05 +0000

    trick afterwards sensitive relax payment

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import random
+print(random.randint(0, 100))
+import re
+print(re.match("h.*o", "hello world"))
