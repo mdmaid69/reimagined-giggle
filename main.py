28933568cdbe5637b150a0dadd3b7de3864ebab9
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:46:39 +0000

    intelligent term school soak famous

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import json
+print(json.dumps({"name": "John", "age": 30}))
