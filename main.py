25a022cfe39cef43baafbc47d2ab6cd54d621a14
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:23:28 +0000

    suck so straight tooth late

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
