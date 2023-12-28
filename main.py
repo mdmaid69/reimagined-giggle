423f9dfec71d9f20e97d85154661fcbb51c688b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:54:12 +0000

    spill Pp drink right action

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
