93beec9e3ab76066d8b33b5dd0db5129c0f6f5d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:31:57 +0000

    conversation people less ilovedicks me

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
