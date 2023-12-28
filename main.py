5d15618d83368cf51bdea134f6438a4eb6edd6c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:46:52 +0000

    for she unit post plain

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-for i in range(10): print(i)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
