ba19037a23c8ca7f4943f023453069b3394c52c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:46:35 +0000

    exactly downstairs shine cost children

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
