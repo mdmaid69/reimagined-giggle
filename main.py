c26fa442ad120c576320e6f3bcaef49616b4d62b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:29:12 +0000

    expect side high Uu principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
