a01cc725f994e2949a9bbb6524c387e16dd3fe86
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:37:09 +0000

    second person software lost usual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def is_odd(n):
+        return n % 2 != 0
