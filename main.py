5494037b7d36049ba1a9ca95fc263ab9f187fa66
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:56:18 +0000

    design bound fortunately play confident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
