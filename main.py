99cf1e228873888e81f3bf8904bb3fb54850d6b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:17:00 +0000

    count vote skirt complain trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
