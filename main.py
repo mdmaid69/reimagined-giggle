f1418c2e8fad3fb9ffb4add7ed153eb5e4cb06f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:45:50 +0000

    regard point whistle tall keen

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
