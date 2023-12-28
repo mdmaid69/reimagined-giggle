0a7c2de6e1867865701a166a9c129426775b12e2
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:52:44 +0000

    level honest cut possession shoulder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def convert_to_octal(n):
+        return oct(n)
