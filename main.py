d745db65f2bdf4fc20eaee36eb0918f3d773c475
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:09:15 +0000

    hole cost emphasis sticky comein

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
