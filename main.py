5e83e4f77d7fab1993d9dbd20eede7b1ec5a2351
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:41:57 +0000

    say use consult detail date

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def multiply_numbers(x, y):
+        return x * y
