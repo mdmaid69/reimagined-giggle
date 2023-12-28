5ba4efc85c3007f87d468938940fb7704c11b0ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:36:03 +0000

    after could personal obvious office

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
