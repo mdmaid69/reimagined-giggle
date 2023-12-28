4c81614be90e636c854c61cfc58f1d42dfcfe718
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:30:03 +0000

    square freeze solve getback 565end

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_density(mass, volume):
-        return mass / volume
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
