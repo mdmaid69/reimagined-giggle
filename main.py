d88bff8b524f3677c7e93a3ff25310c878be9b28
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:59:06 +0000

    course breathe mouth cum pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_max(lst):
-        return max(lst)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
