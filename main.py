c4abbb786156b1117a2de0c6d0dd23e29b3c3b83
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:12:09 +0000

    welcome miss table long offence

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_palindrome(s):
-        return s == s[::-1]
+for i in range(10): print(i)
+print("Hello, world!")
