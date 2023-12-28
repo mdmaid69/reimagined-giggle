c1101968242a64e51642805071fbb7f445a82bf3
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:23:43 +0000

    cook can reply pay like

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
