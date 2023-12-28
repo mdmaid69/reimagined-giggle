7b73e8868fbaf4cc6241b51d22cd561fa7812466
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:15:08 +0000

    hello we bar would march

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def append_to_array(array, item):
+        array.append(item)
