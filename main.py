a024cd580c16615921f13e82a3980b5dc1b75d17
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:45:41 +0000

    phone excuse announce once comeup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
