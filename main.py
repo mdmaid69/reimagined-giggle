89e0dfb61fb1df3608411632efe8a2e305a986f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:55:03 +0000

    minimum simple form her skill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
