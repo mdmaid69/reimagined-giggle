4bad200d39934b13873165a7c04cc8cde2c2c83e
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:27:06 +0000

    father dick past fierce difficult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_speed(distance, time):
+        return distance / time
