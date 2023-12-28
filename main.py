139cddcccf3320a87fae724161ab95be2f070057
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:31:09 +0000

    various actually damage intend wise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
