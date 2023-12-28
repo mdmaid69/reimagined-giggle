121fb252ff7690b09c12cdcf602afea8c0b9ae88
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:59:45 +0000

    come creature rather combination 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
