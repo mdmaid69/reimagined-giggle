b702f5455f268b5ee07e3789b9d6203bca51fc6d
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:10:41 +0000

    stick learn matter section area

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def find_max(lst):
+        return max(lst)
+import datetime
+print(datetime.datetime.now())
