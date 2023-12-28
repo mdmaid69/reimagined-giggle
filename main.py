e49c0ccb27b004e0e6d7f6a81a880566e9b48760
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:18:55 +0000

    use autumn use paper easy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
