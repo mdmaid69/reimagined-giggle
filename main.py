c49315ebdbaafc16dc8a13f4320cab4173aa95f6
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:42:42 +0000

    turnup takeaway thinkthrough spoil workon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_work(force, distance):
+        return force * distance
