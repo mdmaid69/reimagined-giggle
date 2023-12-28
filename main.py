cb26337610a98f629a9dda6fd44dd899ebcfe65d
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:11:46 +0000

    clear full distant float love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
