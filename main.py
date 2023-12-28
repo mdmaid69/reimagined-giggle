823addaeb0e1f21b2ffd75f445d63cb9d85b94c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:37:25 +0000

    entrance yes break quit shine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def convert_to_octal(n):
+        return oct(n)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
