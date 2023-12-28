f224f891be2c4bbfee644011c9322b281f24eebe
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:24:47 +0000

    separate appeal closed copy chase

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def find_min(lst):
+        return min(lst)
