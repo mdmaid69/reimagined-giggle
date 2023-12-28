41a454d1b037930da493f96e18969d566ab53469
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:08:20 +0000

    dream passenger until bang several

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
