cac7f7184fbbfb586f6506d5c84bba9a5d6275eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:13:49 +0000

    new promise level them explanation

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import collections
+def create_counter():
+        return collections.Counter()
