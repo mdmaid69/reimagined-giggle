9cfe30420e56e72b84e6ac5bd179f34871131571
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:38:28 +0000

    calm shelter either owe naturally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
