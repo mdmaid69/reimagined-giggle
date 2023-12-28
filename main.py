a6563e7df75034ff992a255ab9f8f6cdfecbe61b
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:26:43 +0000

    beside bar Uu organization near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-for i in range(5):
-        print(i)
+def calculate_gamma_function(x):
+        return math.gamma(x)
