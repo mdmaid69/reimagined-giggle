b5c94c44530b5fe927f7fd0e2d99ec15ebf9bf81
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:26:04 +0000

    feed successful Kk call television

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
