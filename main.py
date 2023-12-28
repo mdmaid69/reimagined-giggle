5b4f303adf61e819a379ec5db861a6d315add33f
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:25:43 +0000

    effect achievement dear worth convenient

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
