13f30e0c4c5befeeb670b1bc62f43081ece3f227
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:17:15 +0000

    polite pause similar order evening

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
