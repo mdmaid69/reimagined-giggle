c32002b111fa2bbb44ff3b799dbd0c1dcef4f105
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:47:02 +0000

    pupil thanks outside popular touch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_floor(x):
+        return math.floor(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
