c80d65ea33e2635802140d105efacc0379620f5a
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:27:54 +0000

    join least confuse trick direct

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
