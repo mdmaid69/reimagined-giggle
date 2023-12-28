b51591e843084a6fdb1673040d4783963c5ad9d5
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:54:23 +0000

    face period thirsty goup fresh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
