7bd3875a48b9d14904bd273806fccdab8cfd5f43
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:11:17 +0000

    slightly depend use unlucky boil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
