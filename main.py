6fa7248f33ce8ae13961e58f65f0947fadfb0f1b
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:55:54 +0000

    die range involve charge load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
