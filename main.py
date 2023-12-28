1f2e02c5e87d15670e96e975e7f90f2b0f5704e0
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:01:37 +0000

    artificial hear join brush wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
