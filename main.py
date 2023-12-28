257fc258b02d975b94b646a72b59dc959be9f26c
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:50:23 +0000

    thorough short there urgent visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import platform
 def get_python_version():
         return platform.python_version()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
