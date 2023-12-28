d390865f6590bd24c9809e7f97de37c0cd8797d0
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:11:49 +0000

    show weigh Rr necessary depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
