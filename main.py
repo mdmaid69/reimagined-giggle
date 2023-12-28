2e032a4c84997cda536718fc50a82b45d512638e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:37:54 +0000

    film immediately cross behave statement

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import json
+print(json.dumps({"name": "John", "age": 30}))
