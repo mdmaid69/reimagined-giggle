5307f76bafcbc1f50cc7828c541923272480d611
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:43:29 +0000

    stop whisper before real duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
