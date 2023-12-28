bbe10a152e2d114c1219006fdbd2c59143746f38
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:50:41 +0000

    cut Pp convincing march last

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
+  def multiply_numbers(x, y):
+        return x * y
