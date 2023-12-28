067c2ff536e0a1012bcae4c69c6b02c24a3507a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:38:53 +0000

    shoulder frighten form combination wrong

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
