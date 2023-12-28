ec8cdbef6bf846a478b97497e5de2e87154329e7
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:09:30 +0000

    next measure unless refer bringdown

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
