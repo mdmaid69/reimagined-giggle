3e2efe984fe1d30e1319e771bbdf3830bfab9b22
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:48:03 +0000

    film share trade flight quality

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
