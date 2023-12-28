c9c66f169721b169fc4c8d7918b2316cfa4ec31a
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:22:02 +0000

    description fold takeout minimum interesting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_length(array):
-        return len(array)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
