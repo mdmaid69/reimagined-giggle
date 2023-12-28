d254475b4221d94d77e230500df9e26220db81fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:06:55 +0000

    rain any shallow maybe order

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def cube_number(x):
-        return x**3
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
