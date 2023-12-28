f398c3cf4bda98fefabf35b3b7523bbb0e62b52e
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:59:04 +0000

    from disapprove benefit my raise

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
