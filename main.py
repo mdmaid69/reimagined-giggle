52056bda14798ec241265c6e645698b5f7b6264b
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:46:47 +0000

    belong getoff quick unlucky life

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
