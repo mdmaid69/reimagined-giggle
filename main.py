5fdb0dc0bd4ca8bbcd70f5f44906ebbb2ec80628
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:10:32 +0000

    mistake delight relief promise life

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
