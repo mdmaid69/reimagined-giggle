dd44de8330da19ccb97787bb31fb896067f8c554
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:36:42 +0000

    think waste from innocent realize

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
