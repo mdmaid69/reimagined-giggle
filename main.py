43ae5daa127b65a6047c61ea27a2bf78b892e885
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:58:52 +0000

    collect see sell need cry

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def convert_to_binary(n):
+        return bin(n)
+for i in range(10): print(i)
