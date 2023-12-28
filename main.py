04b8223f10baf951cafcc49cab897addf7bad93a
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:41:05 +0000

    up under conduct lower getout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import os
+print(os.getcwd())
