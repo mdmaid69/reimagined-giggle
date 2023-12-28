1642288d0f4ff904f58b2ecc1b2f0bbdc7065722
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:28:50 +0000

    workon conclusion way neat possibly

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_current_directory():
+        return os.getcwd()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
