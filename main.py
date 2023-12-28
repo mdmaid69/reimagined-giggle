b623ee874b291046951725d2fa3b600ec9c52af4
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:43:54 +0000

    share concentration belief delight brave

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_user_string():
-        return collections.UserString()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
