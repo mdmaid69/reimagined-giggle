945c07961d3ea33510b2c49b2b1eecdcb7121b83
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:57:35 +0000

    heavy rid complete pick compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
