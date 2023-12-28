ac888f1683f8762f87026648fd03494c7de04ee4
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:15:25 +0000

    pause imagination care post chin

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
