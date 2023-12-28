70073fc77b079122c146d262c729ce208e707c01
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:47:11 +0000

    while properly embarrassing tell big

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
