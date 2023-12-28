af7237eaedf9eb669591cf2375b1ae7cacbbe296
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:35:23 +0000

    bet thinkthrough exciting feature once

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_area(radius):
+        return 3.14 * radius * radius
