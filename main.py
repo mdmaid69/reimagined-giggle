a8e156caf876eb6fc3f604477f71e11b1db8e3b7
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:05:21 +0000

    use home part knowledge information

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
