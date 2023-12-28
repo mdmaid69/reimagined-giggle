d2245bf2d753b5df3c2e592424b00aec863c5366
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:59:50 +0000

    collect own straight underneath organize

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
