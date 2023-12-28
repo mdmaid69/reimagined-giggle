23c2ca476cee27d50bc57f2f360ab468773f9c4a
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:41:20 +0000

    threaten steal full many another

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import json
+print(json.dumps({"name": "John", "age": 30}))
