2ce361802a1d73f44d2fc02e99ca08d7f401d2f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:26:58 +0000

    pupil hot judge cause dig

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
