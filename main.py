24b9aa9403f9db1104848f2f4ec6a883101c20cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:00:26 +0000

    tense regard tie wind view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
