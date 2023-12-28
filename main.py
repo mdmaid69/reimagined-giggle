916b9b9878d230cfd4ce13fe098e1a478baf089f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:36:57 +0000

    anything straight actual letter exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
