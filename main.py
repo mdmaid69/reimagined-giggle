f20fecb45d2f0433332a04794f93a295b55fe586
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:39:31 +0000

    sweet regular content strength away

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
