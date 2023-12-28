3b8e67df410979d38829efd0ade256b84e1013c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:55:01 +0000

    meal pattern slightly watch grateful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
