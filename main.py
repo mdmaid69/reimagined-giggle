3ded18cd92a71b6cd49694567b2b7dc6dd4745be
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:39:08 +0000

    best offend rush wise work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def clear_array(array):
-        array *= 0
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def remove_duplicates(lst):
+        return list(set(lst))
