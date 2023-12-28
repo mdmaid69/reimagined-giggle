ebc4d5b49773fcde32fcf90c775c8779cab95f11
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:08:21 +0000

    pleasure earn confuse once think

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
