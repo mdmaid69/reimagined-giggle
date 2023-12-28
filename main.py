24fcb4d20969c957d94bc4e124fecfe6510ad734
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:32:06 +0000

    large disturbing table value they

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
