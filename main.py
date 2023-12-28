4e9a3e1070a2b6dde6bf5e11b553c94a94c70b63
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:10:09 +0000

    like contrast without wander weigh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
