91723824ce07c317bb991a4dbf29b7d39ff17883
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:44:34 +0000

    congratulate major represent length experience

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
