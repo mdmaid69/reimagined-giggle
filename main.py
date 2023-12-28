a0bb7d83c245d4141e2f23f1f49b867681c7c8f1
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:24:15 +0000

    programme blame uponon average will

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
