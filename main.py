547e57c2809d8925f27709e681ba63cf7ac8bbfd
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:03:42 +0000

    off who worth experiment skirt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
+  def square_number(x):
+        return x**2
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
