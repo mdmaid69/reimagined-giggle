3243cc843712113165c0ca4c246560b782dbdabe
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:27:45 +0000

    serious popular 569enjoy choice thirsty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
