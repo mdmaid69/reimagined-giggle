c0f4c7090c7b641d568e68a0703aa1a2b15613f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:58:41 +0000

    treat especially detail sit motion

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
