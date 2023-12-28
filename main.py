baad3dc740c40a66479cac4d0e7b54a0dcc5e7b9
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:54:40 +0000

    relax tune kind invitation better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
