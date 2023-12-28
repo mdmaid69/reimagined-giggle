a6916233128afdc3b8804b2418647be68a03413b
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:05:25 +0000

    underneath education pool burst influence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def clear_array(array):
-        array *= 0
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
