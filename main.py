0a06c48c31b3cf2ffad65aed8f77885d04edbb5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:36:07 +0000

    friendly scream memory tough interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
