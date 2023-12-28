b0c34d22d2f7701789279ade227f844fdb26a4f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:05:33 +0000

    advanced spin statement lesson though

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
