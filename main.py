40a399acfe42f2041389491cad7db7cf8010d3b3
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:41:22 +0000

    Itself the exit arrange breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
