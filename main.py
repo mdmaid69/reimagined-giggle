d4b8e1ae811860c6c6b82fd8e14b708645d7856a
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:34:04 +0000

    load precise modern blue clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  def sort_list(lst):
+        return sorted(lst)
