19e3b406abd14f9ac4389b119d013bc243c22b4c
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:42:13 +0000

    must as solve tired important

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
