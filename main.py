3569bb5ada60ecea00205f8ffb1648eabaad3e3b
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:35:53 +0000

    central tip talkinto secret declaration

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_characters(sentence):
-        return len(sentence)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
