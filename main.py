af33c1770d65db89b2caa0dc180b6737f7b07547
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:14:38 +0000

    tell recovery imagination safe kill

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
