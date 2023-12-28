4a9055db6217443b5f2199cc1fbbc05312a226f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:39:38 +0000

    unusual getaway laugh base pipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
