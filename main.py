f05f755cf1a37f16526004bac3bbd22abf8e120f
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:20:51 +0000

    all consider tour announce something

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def is_even(n):
-        return n % 2 == 0
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
