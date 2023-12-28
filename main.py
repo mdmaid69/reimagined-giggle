a45b893f9870a96b6d79da5859ad4d58e3c94d24
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:30:50 +0000

    wash where various evidence tragedy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import platform
+def get_python_version():
+        return platform.python_version()
