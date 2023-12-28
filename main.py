2e0e23802b7a630a316f168ae92261c3af933c82
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:51:49 +0000

    she upstairs level study whisper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
