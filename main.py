fac494fe660481406e447fb3faaaef730bad8b29
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:59:54 +0000

    off come system space ignore

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def count_elements(lst):
+        return len(lst)
+def convert_to_octal(n):
+        return oct(n)
