e1560b43034de08456207ff4d18d1c2957db595c
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:48:44 +0000

    cheque wing matter head takeoff

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def remove_duplicates(lst):
+        return list(set(lst))
+import json
+print(json.dumps({"name": "John", "age": 30}))
