c87bcba41f69b60e8faedcc66b831ccc19cb9fd3
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:32:11 +0000

    stage argue chat stiff correction

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
