c456aa434fd42d07ca3279fcc3f1c890a0afc3c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:43:25 +0000

    previous take continue sentence turnon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def square_number(x):
-        return x**2
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
