df2ea691af9af4f20d04761d21af2909cce645af
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:36:54 +0000

    beat later steer better gradually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def reverse_string(s):
-        return s[::-1]
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
