337ac3729e6eeb511a71ba8ab9534f00aa680029
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:28:05 +0000

    noone guilty present film careful

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
