7c0b2ca84b41e32be39bf28e5b7a7a5d2136d8bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:36:47 +0000

    excuse building skin join get

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def cube_number(x):
-        return x**3
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
