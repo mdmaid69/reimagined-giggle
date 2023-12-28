9a6e1cafa5fde81d7d13afc72ece41d6feb126dd
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:49:02 +0000

    thing abroad after come plenty

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
