1eb7a527831446e08a0739a674731ee406d76f29
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:24:15 +0000

    below murder common section rob

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_max(lst):
-        return max(lst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import getpass
+def get_username():
+        return getpass.getuser()
