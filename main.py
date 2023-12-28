ccd419029e74ef5712519f26422a222ce91687c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:35:28 +0000

    computer schedule workupto benefit equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def sort_list(lst):
+        return sorted(lst)
