10429c053502084752d24597b6bc1e364db11118
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:16:36 +0000

    failure mouth sleep sort sit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def reverse_string(s):
-        return s[::-1]
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
