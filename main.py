06a705c23152df3cc8938cfc1d3f7dab04d1e453
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:06:36 +0000

    harm position reliable false slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import os
+print(os.getcwd())
