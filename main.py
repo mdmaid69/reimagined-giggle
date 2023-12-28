1dca6e15c5e5baf35ae20c3a39a337108334d435
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:04:54 +0000

    centimetre cum conclusion myself permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
