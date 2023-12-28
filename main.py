b78e568f3f0b04cbe62812a881326429a8e3631e
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:55:52 +0000

    surprising education better on sharp

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import sys
+print(sys.version)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
