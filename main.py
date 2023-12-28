f8c5e44861038f96616b17f39be7f0eeb8250373
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:39:18 +0000

    most past statement soft write

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_directory_name(path):
+        return os.path.dirname(path)
