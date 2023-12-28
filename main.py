c519ea2d55d013473669c746b8becbcc93c8ae94
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:12:59 +0000

    telephone vision fat pay freedom

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
