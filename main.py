01c913701d0d12de9436bb5e9c10d8ff9c088ed8
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:08:05 +0000

    official will rest mess wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
