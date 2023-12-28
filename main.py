c3c27f1db98171731425ae0d4d6cfc4cd0eaee45
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:13:48 +0000

    hungry colour spoil listenfor appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
