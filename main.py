ab5ba5782cf3544eb04fe4d11639e5f81a87976a
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:46:57 +0000

    discover thanks damage near Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
