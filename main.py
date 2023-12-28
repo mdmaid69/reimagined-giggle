5e3f869f8587f3b41d14ca621f4665082b30bca3
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:34:57 +0000

    past arrival imagination silent unlucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_elements(lst):
+        return len(lst)
