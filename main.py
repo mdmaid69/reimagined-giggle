434c5449ff939ba5a8fa705923087a4afec50776
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:20:16 +0000

    bored reserve turnon play reliable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def get_current_time():
+        return time.time()
