272b7bd41197e5470221ff6f375439fa19a21464
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:46:15 +0000

    further feeling build criticism possibly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import random
+def roll_die():
+        return random.randint(1, 6)
