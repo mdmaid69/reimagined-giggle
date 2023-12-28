2a1c730098a32cd659f0c931f0d2ac87dd475721
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:45:12 +0000

    same Ll lookback(on) average error

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
