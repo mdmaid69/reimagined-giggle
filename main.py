ea17783f8828a609765dc585bc35fd42fe33aa17
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:45:19 +0000

    temper taste port bush holiday

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def greet(name):
+        print(f"Hello, {name}!")
+import array
+def set_array_item(array, i, item):
+        array[i] = item
