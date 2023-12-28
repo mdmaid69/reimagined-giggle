67368b6b8b2196ccace62ca4b4e01b0ad72b119f
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:43:52 +0000

    store fact passage test hard

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
