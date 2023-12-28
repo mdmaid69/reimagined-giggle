443a350c2e3e932d0e5801db3b87931292053131
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:32:43 +0000

    trick cruel comein programme type

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import platform
-def get_python_version():
-        return platform.python_version()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def count_characters(sentence):
+        return len(sentence)
