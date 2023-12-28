2238d4bb9d0babdfed942af6bdfe4fce97eadf84
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:01:09 +0000

    blind correct he action second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
