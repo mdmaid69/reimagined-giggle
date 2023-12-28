8f78f76d07ee4cec80cba3829c5bfc03775467bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:18:22 +0000

    skirt enthusiastic himself ourselves on

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import tensorflow as tf
+print(tf.__version__)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
