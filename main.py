5790ed497e7605a2c3b256ff3d0bbe3c17c169b8
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:13:51 +0000

    call operation snow sure trip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def subtract_numbers(x, y):
+        return x - y
