be674ce216eb1e533e80c3817e0901c8af5705f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:27:24 +0000

    nurse string rob demonstrate bread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_elements(lst):
+        return len(lst)
