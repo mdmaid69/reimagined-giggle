77184e2bd372572174d0f4cb1d857721fbd9db72
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:47:00 +0000

    giveout slice getback ask right

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
