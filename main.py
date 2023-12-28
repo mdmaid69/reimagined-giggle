147dc063f8856367b131e9d06bca8755ebdc041b
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:31:34 +0000

    convince means breakthrough mark goout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+print(math.pi)
+def calculate_work(force, distance):
+        return force * distance
