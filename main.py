7f1a4c4964e0f09b00fdad8398e9bec2c7f2115d
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:59:32 +0000

    letter description somehow sink undo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
