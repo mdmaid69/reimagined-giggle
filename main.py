e5e470ec9610a48bee9bebd5c2441361bf36720d
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:00:44 +0000

    kind lower conduct simple official

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
 def calculate_return_on_assets(net_income, total_assets):
         return net_income / total_assets
+def cube_number(x):
+        return x**3
