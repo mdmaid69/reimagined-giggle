fcaa96e890638aa243dfb7165309ce4f0ad9ce78
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:45:47 +0000

    bill show although waste stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
