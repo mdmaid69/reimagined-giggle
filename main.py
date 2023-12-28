46e0e0dd1bd2e282ba7af8421b31f0d666c0d378
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:11:49 +0000

    polite bring broken feeling make

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import time
+print(time.time())
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
