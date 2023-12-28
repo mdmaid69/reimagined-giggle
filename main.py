6f7b848d1175c7f71c1ee3af3c36946ff433a3b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:29:42 +0000

    secret border giveout government lost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def clear_array(array):
+        array *= 0
