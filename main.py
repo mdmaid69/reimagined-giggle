d9a9304f2591cdf0aa8c25c238dfcb9c3a6a90f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:02:27 +0000

    underneath free thought boring evidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-name = "Python"
-print("Hello,", name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
