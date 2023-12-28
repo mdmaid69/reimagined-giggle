b4b3060d4ff7a6464b9554df4b9905576efb4709
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:34:46 +0000

    thorough declaration skirt support understand

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+x = 10
+y = 20
+print("Sum:", x + y)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
