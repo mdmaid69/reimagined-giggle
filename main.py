773559bd3af578f58727f13e243ec21e02214db8
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:02:31 +0000

    beneath whether danger carry my

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
