5cab7f4b9264d493d5bf85918b852b3968e216ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:41:57 +0000

    sink habit answer yesterday wide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
