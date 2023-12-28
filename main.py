fdfe76d8092c8be792a84e3564e242eec843dd1c
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:02:18 +0000

    regular anywayanyhow block preparation sorry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
