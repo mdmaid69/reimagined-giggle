19b66d33ecea7c825e5eeb18431f7fcc2a5c66c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:13:48 +0000

    mysterious its practise example arrangement

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_counter():
+        return collections.Counter()
