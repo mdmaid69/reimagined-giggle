9363faaf3f660cfe9214899cbb3b730cef6c4487
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:11:29 +0000

    depend freeze city minimum study

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-for i in range(10): print(i)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
