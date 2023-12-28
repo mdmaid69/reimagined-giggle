435068bc302c625aec5d470de96060d462ac0ea4
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:25:57 +0000

    range form side among one

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
