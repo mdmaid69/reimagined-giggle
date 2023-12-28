e5e59e1e3f8352bd8f3b08da82ba1e39b556975d
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:41:17 +0000

    expensive after some knock dream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
