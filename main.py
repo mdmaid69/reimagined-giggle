a2c66043606c75045b5a56b2c4c5c5ffcd02eec9
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:42:55 +0000

    compare right angry slice very

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
