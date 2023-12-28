7f6c776cb1d5b694cc642f0ac3c30a88a6da3dd4
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:41:45 +0000

    geton know secret long when

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_area_rectangle(l, w):
+        return l * w
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_cosine(x):
+        return math.cos(x)
