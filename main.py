65fcb1c7883639e7493cef724b62f7bb75afbc20
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:36:57 +0000

    solve thanks rid block opinion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
