62167414cdc0fec219ea5d5f4f7ece27d08d53fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:29:41 +0000

    mood bet sort reason that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import os
+def remove_directory(path):
+        os.rmdir(path)
