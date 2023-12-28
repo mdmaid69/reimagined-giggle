07661b9fbaf4034e88bd550783269a8f0cab64fd
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:41:45 +0000

    treatment discuss change hard hollow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_uid(file_name):
+  def get_file_owner(file_name):
         return os.stat(file_name).st_uid
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
