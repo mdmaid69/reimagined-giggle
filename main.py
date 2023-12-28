68aa58faf60835cfba6b8f2c28f97b0f58844069
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:50:03 +0000

    weekend description pain wish history

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
