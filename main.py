fe9db695d351c199801a1238833e3db51c4f39c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:26:42 +0000

    name under warn respect height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_set(array):
+        return set(array)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
