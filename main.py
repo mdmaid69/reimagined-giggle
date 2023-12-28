0693f99838eebfc1b2d8eae184e8a9d1765beb53
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:43:03 +0000

    used meanwhile turninto hold otherwise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
