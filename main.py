b51b0f308989a96ede8f9490165faa2b54b214ff
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:29:11 +0000

    than surprising true mark unlucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
