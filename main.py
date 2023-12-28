b932fee076d79c8b8942ed769c229ccc56befbb0
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:36:39 +0000

    file Ee unhappy human claim

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_string_from_array(array):
+        return array.tobytes()
