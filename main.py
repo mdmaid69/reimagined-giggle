bca85d950208cef1125570282f373f0fb3e01054
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:06:26 +0000

    dress relationship welcome name month

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
