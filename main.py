006a65cefffd61a88b0229be75350cd28699c658
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:08:41 +0000

    frame discussion woman lucky arrange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_min(numbers):
-        return min(numbers)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
