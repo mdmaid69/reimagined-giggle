cc48726b2ff93db8ca77e1b2c6d7595767429d41
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:35:42 +0000

    target drink club theirs loss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_time_since_epoch():
-        return time.time()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
