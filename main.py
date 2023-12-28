a49f07d5ee0dad989b9167eb9c7e53837e47624d
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:24:39 +0000

    transparent speed more ground hello

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def multiply_numbers(x, y):
+        return x * y
