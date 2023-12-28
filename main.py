5da11b36c1f3a4e48f5045148e50088d77830d40
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:56:49 +0000

    idea temperature slice comein side

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_str(array):
-        return str(array)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
