473ae0205cbe1f292c67407281aea20a7eef58d4
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:06:46 +0000

    level sensible giveout rent exit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
