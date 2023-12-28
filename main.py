2dfcc7198321f848ac28f03aa82b39afdb7c1861
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:01:01 +0000

    view pride buy connection cover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
