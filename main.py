d235a33fd28048b4e27ba28d293f776f36b5aea8
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:14:50 +0000

    building almost unlikely range talk

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
