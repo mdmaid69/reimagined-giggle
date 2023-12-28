3d1dff6a7d4f46550cca81722b12d06c68d204c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:36:29 +0000

    sometimes regret so benefit stick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
