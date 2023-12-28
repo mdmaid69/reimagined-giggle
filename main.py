8742ca306ce4c66f499855fefa1f30fa77ef4ca4
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:24:21 +0000

    each purpose quit after except

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import collections
+def create_stack():
+        return collections.deque()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
