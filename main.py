d7f4953c91ee171285a907d64db77dbf6cc26c06
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:25:34 +0000

    touch spray 566end ignore high

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def square_number(x):
-        return x**2
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
