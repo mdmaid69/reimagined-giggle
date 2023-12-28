845ebde164812cf1f89a655b97f899016788a0a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:53:00 +0000

    dress prepare motion copy cry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
