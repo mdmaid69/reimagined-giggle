ff409ba0f6a98c0ad57d6a5b60cee418902376f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:35:34 +0000

    petrol cut contents cruel child

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
