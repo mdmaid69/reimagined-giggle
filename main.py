3c84454a92790bc380021feff56b092309cb475b
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:00:12 +0000

    genuine justice substance plan pattern

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def get_array_as_set(array):
+        return set(array)
