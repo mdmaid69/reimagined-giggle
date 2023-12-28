32a18aac66acbb04b9127e49a9efcdfae560dff0
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:28:56 +0000

    address wild bottom central right

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-print([x**2 for x in range(10)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+name = "Python"
+print("Hello,", name)
