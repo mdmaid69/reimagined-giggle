908523ab1a848ac988392821e3869136b7235ca3
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:06:25 +0000

    familiar tell lot level declaration

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_power(work, time):
+        return work / time
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
