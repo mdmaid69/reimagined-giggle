9384820c8a2ad1296de17f44f8545f3d187aeaa1
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:20:22 +0000

    odd could style effect culture

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import datetime
+def get_current_date():
+        return datetime.date.today()
