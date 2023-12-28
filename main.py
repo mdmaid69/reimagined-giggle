3647523c2d25da2706e15c676ced939064e18f2c
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:04:45 +0000

    trouble tape unlucky shape alive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
