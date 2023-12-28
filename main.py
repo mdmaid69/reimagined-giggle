fab2f07dfc9bdbb8bb94a65760169975fdd0a0cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:38:03 +0000

    crash turndown everybody base limit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+print(math.pi)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
