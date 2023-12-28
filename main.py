286f593535ebaa03aaaf23d08a8328c4f596de61
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:43:58 +0000

    always holdup fair confidence Ll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def find_max(numbers):
+        return max(numbers)
