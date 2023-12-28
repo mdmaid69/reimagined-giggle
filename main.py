4112d23bf30d605957f091e0d10c6b95532efa42
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:41:26 +0000

    calloff result charge comefrom steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
