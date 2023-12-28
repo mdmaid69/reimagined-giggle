aefeb4bb3ae128b640fdf449d97f3df31a32c5cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:11:33 +0000

    grow similar actual praise later

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_average(lst):
+        return sum(lst) / len(lst)
