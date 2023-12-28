d22df5ec16e05840cffb01894014da5be12b8652
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:59:10 +0000

    straight quality disapprove night repeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
