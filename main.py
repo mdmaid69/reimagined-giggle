57d3ac6bccaa6fd61d1028b5b37693e3ec86bff5
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:18:49 +0000

    repair shall power exact file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
