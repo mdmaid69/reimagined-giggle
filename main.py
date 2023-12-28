90506df037eb475c0a0283e3e4def089747d77f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:50:14 +0000

    correct scare rough only cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
