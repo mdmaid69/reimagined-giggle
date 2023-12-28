f313f9a01e934b276bbe68ad64b40138d54a5be7
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:52:56 +0000

    full turnout environment getout course

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def reverse_array(array):
-        array.reverse()
+def set_array_item(array, i, item):
+        array[i] = item
+  def calculate_area_rectangle(l, w):
+        return l * w
