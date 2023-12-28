8766e3017b11df777dc0859027984efbf7022f03
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:11:59 +0000

    cheap common interest Ww disgusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
