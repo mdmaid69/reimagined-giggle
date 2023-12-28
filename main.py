1255dbed3aa3981751dc7fc39916598ac3b4c9a1
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:55:42 +0000

    jump tomorrow fear warm blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
