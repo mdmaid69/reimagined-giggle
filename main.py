2406aa5af0e22b5b3187101ef11cd08f658c6cb1
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:27:12 +0000

    respect petrol original expression simple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
