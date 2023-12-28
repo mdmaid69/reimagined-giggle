af421dcc3064cea83e37ad615ab71600da1c109b
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:31:18 +0000

    include loss check cruel firm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
