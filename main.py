d16f03fef4c617bd21d0fe45f5a9f3476e619ecd
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:37:19 +0000

    violence coffee class exchange smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
