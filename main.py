fcf2b8c6080e1e9bc2b351bcb6ebcd94afecbab7
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:45:28 +0000

    somewhere case treat soil hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_item(array, i):
+        return array[i]
