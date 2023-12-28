b6d6c415b9b7f7528dfc792433e64ce6ec10bf0a
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:12:42 +0000

    wind rain feature public conversation

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
