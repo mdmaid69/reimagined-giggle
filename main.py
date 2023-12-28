c625dd5da40978e408a91afd26fa4bafec2b0855
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:28:15 +0000

    tax know list opposite from

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
