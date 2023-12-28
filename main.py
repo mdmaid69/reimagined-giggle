fbcfdcddd9af27a9db28369541fba05f46d03a07
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:34:06 +0000

    around cause appear thankyou weak

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
