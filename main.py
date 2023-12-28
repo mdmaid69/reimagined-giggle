f14d53db196addf3aafa980dd769153a1225f4cb
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:06:52 +0000

    worried entry Tt weekend terrible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
