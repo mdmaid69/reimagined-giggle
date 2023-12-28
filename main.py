14e7077946e2c15318c26d9bd4cf9ea1ed0f1102
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:57:42 +0000

    squash comein container steady release

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_list(array):
+        return list(array)
