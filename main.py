f172e8718402128e7a909f7e4f345288bee14622
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:20:50 +0000

    sale female cheque on upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
