5d7e229528d45e57b106eb4c54f45223ccf66308
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:58:51 +0000

    worst fat below it name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
