4d0810db68e48a364d63026e45cefaada3d10389
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:12:10 +0000

    measure another give takeoff hers

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
