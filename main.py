c55b9058d1eb2c952421f2b93bfa7e142b9665ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:36:07 +0000

    gather back record inquire attach

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
