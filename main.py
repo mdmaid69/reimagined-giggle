f400d0181104b3eb6532ddc819f433bada34b210
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:11:41 +0000

    impressive pay help do town

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-for i in range(10): print(i)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
