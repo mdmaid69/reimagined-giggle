9f2b39feff63a99e6182ac642910faad62a64bb2
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:24:26 +0000

    friendly broad suffer finger spot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
