49e9659d9277c7420f41e74329995bce6b1933bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:35:30 +0000

    instant forget Itself wing wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
