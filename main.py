f75d5c344dccbdb00de006ea531e70c6c5871fe7
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:08:51 +0000

    grand when situation remember petrol

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
