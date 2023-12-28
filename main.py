5d20b09868132944f5b48b1d9e1c43a9a32b072b
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:47:18 +0000

    borrow feed black worth way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import sys
+  def get_python_version():
+        return sys.version
