5bdb42f367df9860ad8fceb9023296d9ebe62be4
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:25:55 +0000

    suckme whatever harm birth dick

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
