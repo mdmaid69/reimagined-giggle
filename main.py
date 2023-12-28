c4256c41d0825073a2aab0afc7e240a212b4e9b8
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:25:43 +0000

    temper look will priority reduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
