00a36061b96bf1ec25ecebd7a67059b7442567c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:40:48 +0000

    everywhere form direct right sleep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def is_even(n):
+        return n % 2 == 0
+import sys
+print(sys.version)
