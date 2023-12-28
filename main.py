75742e5f3a40f55d7def1b0cbdf688dcf7947c46
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:19:01 +0000

    obvious equal 566end interested unless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
