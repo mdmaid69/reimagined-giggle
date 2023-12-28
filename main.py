97af0cc8838f60c5553e2e5a3424055662351637
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:38:58 +0000

    round software unusual copy leave

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_work(force, distance):
+        return force * distance
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
