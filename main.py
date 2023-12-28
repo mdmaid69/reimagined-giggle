54ba8a1035ac041ef60f28526beeeaca5cb398f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:52:03 +0000

    admit walk offence paper correction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def find_min(numbers):
-        return min(numbers)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
