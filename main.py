d1648a6198f0fcbd9e5011785b4ac251ca98afb9
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:11:36 +0000

    left impressed powerful takeover first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
