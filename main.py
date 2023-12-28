ca5f141332f924d4d81d480abc16883290abf482
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:48:06 +0000

    mind balance influence subject discuss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import collections
+def create_user_list():
+        return collections.UserList()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
