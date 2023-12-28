45008479edeb9f29c419e8e85d27f33651bd789c
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:13:17 +0000

    nose equal hold goaway study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
