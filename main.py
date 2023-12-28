9e7fcafb5edc3ba9336cf6b0595cb4e079a13ac3
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:37:02 +0000

    thinkover dream mention deep history

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
