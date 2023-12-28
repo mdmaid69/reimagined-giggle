5d7ee31bb08f3f04a1bc46c4069694d07342c2b0
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:18:09 +0000

    bag appeal fear force healthy

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
