0bc138663c46537d6f482c69ab11a9cbbc383983
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:25:20 +0000

    soon window hang bridge few

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
