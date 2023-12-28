6fe11999d5b98d4dbbd72a4d091aaaf19a517fb3
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:54:02 +0000

    score he land marry charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
