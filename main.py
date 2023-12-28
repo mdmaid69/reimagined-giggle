6006d0ffb1c79d869ab0dc189fd51b4ef2af2dd3
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:22:58 +0000

    pool play shopping open arrange

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
