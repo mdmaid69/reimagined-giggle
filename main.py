211873ea36558cd98daeddc73f850cf603abfe8e
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:51:43 +0000

    interfere doubt certainly earth shallow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import os
+print(os.getcwd())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_base_name(path):
+        return os.path.basename(path)
