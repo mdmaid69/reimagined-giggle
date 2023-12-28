cacbd3237ecbad77d00aaba7de4bfd860804af9b
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:47:04 +0000

    present lookup float base think

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
