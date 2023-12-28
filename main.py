13f3bbe154a0f0b66455266035dde578cf136ff7
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:15:01 +0000

    recent believe any shape hardly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import logging
+def log_message(message):
+        logging.info(message)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
