658e90ddaedcf54460e8b35397679a6b8fa2f2b9
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:00:24 +0000

    well behave callat Ll else

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
