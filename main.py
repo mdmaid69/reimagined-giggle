4806046cb9bd59f08b96eb9865b63a97dd82cc57
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:27:07 +0000

    develop telephone act contribute air

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
