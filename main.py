3012d7a3f944b22c7e74592dd9e6d7c963bb2600
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:56:24 +0000

    takeaway pleasant getoff zero comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import sys
+def exit_program():
+        sys.exit()
