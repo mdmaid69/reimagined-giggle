224bd40d36d3b0cce3ecfcdf542878d859e44807
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:25:32 +0000

    profit need protect arrive survive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_buffer_info(array):
+        return array.buffer_info()
