b8037048224214987c1ae55b1cf10f877eedc60d
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:31:57 +0000

    there arm gift circumstance unit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
