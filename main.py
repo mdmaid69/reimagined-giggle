0160e11dbf7ce35d651d5d01c312dc76da550db9
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:16:14 +0000

    unpleasant conclusion no telephone add

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
