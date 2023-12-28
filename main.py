5f2a0e4f7bba20ab63d9d229dbf492371f819e2a
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:22:44 +0000

    unless ring whistle ago wrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
