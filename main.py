8b40a153bd326938ba6387830592391948ea96d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:21:25 +0000

    attention section minority myself single

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
