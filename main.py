4234984af12876bd730ba73775d6eb003a88b35e
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:12:57 +0000

    offer length it ride outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
