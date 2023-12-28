6761b29bd2efd1712ed447c21e682866a31d427d
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:36:33 +0000

    back squeeze pause rush quick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
