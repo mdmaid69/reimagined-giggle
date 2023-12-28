762339169fcb106ccb1b76bdd99ef4404e619fad
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:15:48 +0000

    present increase badly run people

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_base_name(path):
+        return os.path.basename(path)
