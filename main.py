42c8e8d9b9fc6ce9fc4fdf4e1e3a18006b8049f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:11:31 +0000

    state Ee but beside decide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import sys
+def print_python_version():
+        return sys.version
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
