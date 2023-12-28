0bcbb392820aaa483390e7abac6cb33cd8315ef6
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:31:32 +0000

    raise mine loose positive design

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
