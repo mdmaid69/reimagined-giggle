5e2cc18fe9fa2c20d3225797e3a855b79dbc2bc8
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:46:47 +0000

    clear genuine sink ear traffic

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_power(work, time):
-        return work / time
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
