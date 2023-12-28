243d915139c2bc6b40e090091bfff26f1892aab4
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:07:58 +0000

    difficult mess pay defend suffer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
