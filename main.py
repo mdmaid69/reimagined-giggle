04d52aafb748c100229615d3e6c7a137f7905ed3
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:13:37 +0000

    conversation furniture piece guess dead

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+print(sum(range(10)))
