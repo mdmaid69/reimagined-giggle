4d251f4d584b5d676dc50a3556d3b68ccae12c0b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:17:51 +0000

    dry schedule carry software where

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
