ee60be665bd1ef9442b46629714366709b63628d
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:50:39 +0000

    home may throughout old police

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
