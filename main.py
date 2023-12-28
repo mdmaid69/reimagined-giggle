63da7d1b2e819b00e7c9b96efc3eae13b2910214
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:11:06 +0000

    takeaway collect car intend lean

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def is_odd(n):
+        return n % 2 != 0
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
