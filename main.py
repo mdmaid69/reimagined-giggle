7859c53087baf7651a0c7f2ddb4a0b9bb7951fcd
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:14:04 +0000

    healthy feel cut instrument feeling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
