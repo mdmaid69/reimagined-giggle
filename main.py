bda772f25022489b43d465be97a79a15acd0bdb0
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:09:59 +0000

    middle punishment smile join makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
