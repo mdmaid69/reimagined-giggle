5b9768fe9b33f68b2447e5d006506e2a4ad1779c
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:26:59 +0000

    kill total excited must increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
