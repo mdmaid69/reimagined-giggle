fbadc872e17e5742ffd0a2950f559c5305c011a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:53:53 +0000

    honest exhibition include children announce

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def count_elements(lst):
+        return len(lst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
