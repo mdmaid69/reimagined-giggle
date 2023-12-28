bd97c9c934f27a106150c255ecf913072e9c04f2
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:05:43 +0000

    regular rent number takeout refer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 def find_max(lst):
         return max(lst)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
