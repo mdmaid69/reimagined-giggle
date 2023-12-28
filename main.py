57a6f63bfff67cbb349515cfd14e0d5654e37bae
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:43:22 +0000

    still someone behind room remember

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
