b38586ccfd761707909b4d0847c7c94fefbc309a
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:57:13 +0000

    strange steer none switch bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def print_python_version():
+        return sys.version
