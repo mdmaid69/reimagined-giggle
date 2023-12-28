e9fa59d6f775a9b50d1d7993b385f37c30b52e30
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:17:15 +0000

    sink any would suck journey

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+for i in range(10): print(i)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
