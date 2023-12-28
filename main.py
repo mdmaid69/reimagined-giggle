1ad94259752e461567487994802da21d48ca03cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:38:32 +0000

    comeup name pleasant her confuse

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_current_directory():
+        return os.getcwd()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
