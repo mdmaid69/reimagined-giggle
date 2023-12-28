5b7941cca32b4b13367d1b1198a9df9e18cc4ab9
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:31:03 +0000

    trade spot suppose sail rush

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   def remove_duplicates(lst):
         return list(set(lst))
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
