83836a54c8906eb449177b11ea9733feaed25dfc
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:22:39 +0000

    major equipment only suit rough

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_area(radius):
+        return 3.14 * radius * radius
+print([x**2 for x in range(10)])
