65e30afa3d815804f74e94e4c2205c57f18f6f68
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:20:54 +0000

    may other either badly excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import array
 def append_to_array(array, item):
         array.append(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
