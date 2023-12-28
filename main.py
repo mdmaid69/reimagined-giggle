7876e3bf44e32578f3c469905ba7d572b30efe07
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:35:32 +0000

    thankyou powerful whoever border hole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
