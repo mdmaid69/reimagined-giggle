d43321fb36433c6314f1a6e3a08b52599002073c
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:12:12 +0000

    tape 179bite react no look

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
