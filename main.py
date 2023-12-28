462ed9e4d52ca0d3f5ff327fbb5ebd9726565bc3
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:00:37 +0000

    so draw refuse entry quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
