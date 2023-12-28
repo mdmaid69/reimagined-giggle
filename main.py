a35e121b1e66db05c50f12047b3cbe20db7b482a
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:57:15 +0000

    keepoff thinkof exciting insist the

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
