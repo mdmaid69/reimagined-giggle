c72911ca08b52e48c7ba83ebccf2703e81c2009e
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:08:42 +0000

    feed shop eat action usual

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
