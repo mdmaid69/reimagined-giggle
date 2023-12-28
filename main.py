7018cb5dfb748ddf0d3c0bf78139512b2a975568
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:22:44 +0000

    intend mess clever long copy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def square_number(x):
+        return x**2
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
