96ebae9152eddc2fb7da39451bdd3e3c0d8051e2
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:14:41 +0000

    takeoff keepup song fortunate container

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
