15336104c75bbdf71e66fed116c86c90601b6963
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:10:43 +0000

    manage circumstance continue mention please

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
