402130ea2dc748e08e8e2daf9ce7acdf9c695b9c
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:22:05 +0000

    describe fortunate drawing sorry breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
