472fc6abbc91627ae8979f4502684fe6d2b20657
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:53:12 +0000

    holdout swim test 572enter divide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
