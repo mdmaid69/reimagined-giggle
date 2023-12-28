68a646439c4e64ed8c0af100de150f8d43642065
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:14:35 +0000

    thought pencil before turnout though

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def roll_die():
-        return random.randint(1, 6)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
