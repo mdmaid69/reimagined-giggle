d21de66ec7091c9cd67cefe3b9dd69f256f5f534
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:26:02 +0000

    surprising least basis thin noone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
