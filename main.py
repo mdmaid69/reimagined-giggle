f8af5e7235694cef002a21b2a301900a93d8ef18
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:02:59 +0000

    own person kill plenty branch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_max(numbers):
+        return max(numbers)
