7b6b894b07ff8f4d66a85cef96c404e6139e55a2
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:40:56 +0000

    load blame cry keepoff means

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
