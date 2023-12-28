d49852d19e2ead23eca5d6aa4332e499a162d25e
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:20:48 +0000

    along apparently first somebody star

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
 import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
