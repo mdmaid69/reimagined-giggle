33b8647acddf145f7b8b19a7e711e49cd58fd879
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:15:00 +0000

    in complain us share statement

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_octal(n):
+        return oct(n)
