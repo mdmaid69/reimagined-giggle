0f3e3904e16714ec0c58ab0e4da74659d5724fde
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:48:27 +0000

    drive chief start Jj settle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import collections
+def create_user_dict():
+        return collections.UserDict()
