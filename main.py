620ad502727855af99a3dc991261b92f862b150c
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:19:14 +0000

    bottom permission worry freedom light

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
