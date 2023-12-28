14936377424141b09c3dd8585b510b892e656cf2
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:09:40 +0000

    interview protect immediately respect against

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
