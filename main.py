82af3fcd7964ba8129ee57fdb26ee8b114478b55
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:03:30 +0000

    avoid club unpleasant purpose murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
