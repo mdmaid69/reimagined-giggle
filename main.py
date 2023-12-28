63ecf81cd5553462255f329f907ec4784aeac2e5
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:15:47 +0000

    complain port spray separate aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
