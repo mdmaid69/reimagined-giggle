225537c4d2cf690af08b70c5cfb708dd42ecf39b
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:49:31 +0000

    intelligent offence breathe tense hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
