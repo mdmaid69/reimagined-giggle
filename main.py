968359237635cbdd2f2842d3a6a9d94e7a8a2dd3
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:50:49 +0000

    stamp foot lost phone usually

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
