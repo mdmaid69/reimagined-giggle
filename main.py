14e59288c1603a51427df8fea3d653f88f7999c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:17:53 +0000

    rubbish principle gentle hour arrival

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
