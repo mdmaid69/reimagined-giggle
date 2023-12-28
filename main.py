9779aa6b1e7ea5597785c76200c86a92177e6338
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:34:44 +0000

    tired round force line until

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def convert_array_to_list(array):
+        return array.tolist()
