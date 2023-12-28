a1ef44d54222b1b5f42c0d7b17cb53a0e2e7bc04
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:58:59 +0000

    makeinto factory floor trick machine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
