3b5a4494537420d2b170559986c053fc34c202fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:08:25 +0000

    exist pronounce only female prove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_current_directory():
-        return os.getcwd()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def subtract_numbers(x, y):
+        return x - y
