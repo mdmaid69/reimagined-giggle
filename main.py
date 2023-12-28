914630061ac8e1dcc66001c82a418306c358585a
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:13:37 +0000

    rest quick uponon maintain flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
