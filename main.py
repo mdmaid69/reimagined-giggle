a4a48b593841f4b49c8377a20fea461d5d9ecbd3
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:08:30 +0000

    become steam surface several figure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
