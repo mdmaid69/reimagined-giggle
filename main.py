acb22ddb6b26b48e1e6871589b57f05380779c75
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:31:53 +0000

    bother model often disturb steep

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def reverse_array(array):
-        array.reverse()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
