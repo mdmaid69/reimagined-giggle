b9ee4c753bc44fb43fc89e5c9645f20a13c4b683
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:38:43 +0000

    insist steal compete slightly gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import logging
-def log_message(message):
-        logging.info(message)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
