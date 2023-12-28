610122700b23441320ede2fac1055f10b8635edb
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:24:33 +0000

    badly here mess design partner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
