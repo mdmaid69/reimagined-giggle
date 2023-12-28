e93d0b5b9bf597678ae2c00958d7664764808ad5
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:40:45 +0000

    broken of phone child organization

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
