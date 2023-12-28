7990c496101ae38f8703052d34cff763e67a9dcb
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:15:52 +0000

    story advice accordingto brother score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import datetime
+def get_current_date():
+        return datetime.date.today()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
