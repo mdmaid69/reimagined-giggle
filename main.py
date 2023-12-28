633734fd6f1729a985f185a5c75292325f94b779
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:44:31 +0000

    building show mad keepfrom commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
