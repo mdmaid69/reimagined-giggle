3f6f30d3807d2c2f48940ee339b024367bd5534e
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:58:36 +0000

    swallow pencil wide could lie

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
