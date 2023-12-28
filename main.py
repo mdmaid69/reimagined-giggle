ea45eb95a9320a7c74e15773bfb355e25a4e089b
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:02:01 +0000

    flavour anyone store shallow wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_exponential(x):
+        return math.exp(x)
