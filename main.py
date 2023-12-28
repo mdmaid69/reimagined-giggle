2900c9041c1fbfbc8366f93784a4e24a7077916e
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:37:56 +0000

    think declare seriously breath mention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import re
+print(re.match("h.*o", "hello world"))
