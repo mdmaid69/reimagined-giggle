f5d25e19480c08a1132767639dcc688cb1e4a40f
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:27:59 +0000

    dry tour habit violent relation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def remove_duplicates(lst):
+        return list(set(lst))
