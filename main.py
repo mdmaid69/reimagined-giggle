ce79f7c31b4dbd471cf989935d199f3ed2523156
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:47:34 +0000

    anything branch 566end temperature happen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
