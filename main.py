be79d823a22759acb4ec3449cb2f1c7f6c5ff5dd
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:35:46 +0000

    popular operate easy class serious

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
