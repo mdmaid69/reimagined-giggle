236bb97152f4b296b3485ba6fb1c0042106334f6
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:27:09 +0000

    touch lucky mind possession shine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
 def calculate_present_value(future_value, rate, time):
         return future_value / (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
