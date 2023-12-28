28ac3fc04b5e72d45cf798e7d877754951bf6237
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:34:39 +0000

    thirsty friend keepup tongue error

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
