5c891b36b6c794f0e5599c6788204fdd990f7c68
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:12:22 +0000

    way peace hurry problem use

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
