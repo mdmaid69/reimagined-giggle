5bf61ba4bb22f7e6255be05915a8f14aed6c22b8
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:08:26 +0000

    confused meaning boat view pattern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
