575d2122730c9c99d711a1507f72e3bde18a93c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:20:42 +0000

    calloff cook near round prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
