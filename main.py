051e60bb3f4ca7b16b502262597f52f7aa259e8f
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:52:54 +0000

    weak demand herself wish exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def add_numbers(x, y):
-        return x + y
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
