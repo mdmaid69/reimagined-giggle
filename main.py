1b495158c340f031ce338812d1926cec978e361d
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:32:08 +0000

    fix invite face act direct

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
