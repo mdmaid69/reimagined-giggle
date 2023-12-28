d3f2032a3d905b97977542e2ef488c8e13470123
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:49:49 +0000

    combination crowded hard now large

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
