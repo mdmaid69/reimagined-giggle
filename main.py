8bf06bc770794db2c5ddffdd42b0f4fdb2ed9949
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:26:34 +0000

    government again importance independent fairly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
