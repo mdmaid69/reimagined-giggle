cca3641fe0b976362fe55475e3e0030d01d4ceb4
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:45:32 +0000

    behave spring solve shoulder little

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import platform
+def get_os_info():
+        return platform.uname()
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_bytes(array):
+        return bytes(array)
