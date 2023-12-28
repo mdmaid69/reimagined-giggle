aa3fdfbb8bbdf9a61a4dff0fe8504742b57fbe22
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:35:16 +0000

    blind illness tip forget shape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def set_array_item(array, i, item):
+        array[i] = item
+import logging
+def log_message(message):
+        logging.info(message)
