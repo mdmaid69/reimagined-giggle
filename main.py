e40fdd5b6675b8382a7a1c6c57f154b3a8be2b0a
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:30:54 +0000

    cope comeoff purpose war bring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
