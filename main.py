e7b7c611363e26796bc1e29d26c94a742cfb50ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:53:42 +0000

    sticky play description emphasize better

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
