0a550155d940d1085bc3422e398ad0c6befa8fb7
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:01:28 +0000

    sail crime practise train fun

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_list(array):
+        return list(array)
