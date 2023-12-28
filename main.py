e3594cffe415152ad63f63fb0c41c2b7e5e7d5f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:10:16 +0000

    freeze sport cum bus convinced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
