f5b891f3778dad139a65565cacf398ab5be253bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:08:44 +0000

    contrast imagination wait impossible state

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
