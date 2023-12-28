d3fa26a626e13d74433fb4b611211c39ea1af9f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:47:54 +0000

    butter geton square suit sky

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def get_array_as_bool(array):
+        return bool(array)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
