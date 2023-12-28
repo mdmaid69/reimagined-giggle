4eec8d46399b1f79ce770f8a7a9346e82cedc3b9
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:11:48 +0000

    boring uncertain strange still last

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
