b3d1a92ecc4f42c19900efebcfcf456d3814b477
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:26:56 +0000

    stand real uncomfortable port all

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
