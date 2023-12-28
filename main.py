c185a707136ef603ffa624a9791f7cc803141932
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:48:37 +0000

    suppose straight contain force other

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
