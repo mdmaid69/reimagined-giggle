7f62c4dbc68572b32d8563acd1d4726895228aad
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:14:55 +0000

    nose bang insist tonight snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def get_array_as_str(array):
-        return str(array)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
