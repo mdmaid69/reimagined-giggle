75d61a8c09d4eee2c2baaaa357f3fb11fbaf98c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:44:35 +0000

    operate vision be slip warm

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-for i in range(10): print(i)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
