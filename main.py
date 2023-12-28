18f368e7afed7b12775a4c066ed8f3828cc03ae6
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:21:40 +0000

    holdout runout by sport judge

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
