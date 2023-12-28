5c4ababd4f99546ed3df8527a2d8d0b82010c0cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:45:57 +0000

    phone check thank just sharp

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
