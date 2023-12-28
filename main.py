b7387e85e8aa8600b35e1181babea1604319ba6c
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:01:26 +0000

    character sail relationship some challenge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
