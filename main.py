9dbac14d63f7ca178c0218804a12b39042e18ed5
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:21:57 +0000

    worried print lose rank ability

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_density(mass, volume):
+        return mass / volume
