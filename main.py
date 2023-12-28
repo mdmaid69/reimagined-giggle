168611ee0cae0650e12e2c47243686638dc71c50
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:31:32 +0000

    provide feeling grow capital otherwise

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-name = "Python"
-print("Hello,", name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+print("Even:", [n for n in numbers if n % 2 == 0])
