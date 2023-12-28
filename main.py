3efcabc8c5b98f3b86ee9e220c18fba194844c63
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:35:58 +0000

    complete storm visit ahead peace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 def reverse_string(s):
         return s[::-1]
