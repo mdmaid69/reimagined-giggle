6df336bab32fd69344e17ff97b11df0f64528cd0
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:08:09 +0000

    touch bin cut back view

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
