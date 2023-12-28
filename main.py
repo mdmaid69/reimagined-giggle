ad870e760463ea71407fb387a77b34b75c52fdcc
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:53:25 +0000

    poison grade hit major temperature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
