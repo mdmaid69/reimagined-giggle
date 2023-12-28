c69afc57172fb28a10dd55c650e2703b2bc7b5bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:33:29 +0000

    unhappy concerning size he noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
