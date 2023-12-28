0ca8356cc338f02c8a12f1d2e154f9c84c221948
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:56:35 +0000

    laugh lost plain police patient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
