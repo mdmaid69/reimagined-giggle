f94cd21f3406b642d9d6873e9209ba4dec81baa8
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:28:13 +0000

    opinion work breakup state breath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import collections
+def create_queue():
+        return collections.deque()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
