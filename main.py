5ec2dd9c3ef28126726b3a223f7492b8fc52c676
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:01:40 +0000

    storm so sensible little lean

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+print([x**2 for x in range(10)])
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
