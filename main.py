dc25b71eeed8722667b77f7f4784dcc0bce60249
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:20:36 +0000

    crash bag influence Ee guide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
