25352200eb8ffb1f584fbbd79a72067a29eac037
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:28:11 +0000

    convinced innocent police hospital anything

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
