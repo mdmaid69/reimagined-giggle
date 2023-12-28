e19807f3dc2266bb34ef6f02a33c05643be20aaf
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:48:10 +0000

    consult tense quarter understand long

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import datetime
-def get_current_date():
-        return datetime.date.today()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
