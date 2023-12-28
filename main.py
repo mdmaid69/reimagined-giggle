d07922e6d2b978e3faf9f92496eecde6d03228ec
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:05:44 +0000

    there direct ashamed unhappy half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
