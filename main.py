d53ae9afee9d6725653465fd96fee5ec6c4e74d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:32:53 +0000

    vote touch Pp about wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
