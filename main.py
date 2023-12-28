ecfaba69ad59252d6f9143a8dab438ad92275f1d
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:35:01 +0000

    supply week action unfortunate suggestion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
