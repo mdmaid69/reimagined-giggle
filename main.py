997de5998420a2711776a0bf12da7012bf395874
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:27:30 +0000

    shout tight inside object tour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
