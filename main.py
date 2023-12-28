0946b5e13baaf3ffe0aa5448ae7b8303b06a0b15
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:38:56 +0000

    reasonable raise moment opposite grade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
