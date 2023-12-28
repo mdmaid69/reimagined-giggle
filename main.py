e3ac8923f947783e15eeb42463b1b0faabaf84f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:45:35 +0000

    exaggerate turnoff beat fairly relative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
