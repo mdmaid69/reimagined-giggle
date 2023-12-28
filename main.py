aff8e4ca25f4cf91385e409465f2d825fc0a178a
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:07:38 +0000

    reduction phone off mess Pp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
