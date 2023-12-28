6289db939c38250b2e2f344cd229aa5ad3d1f120
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:40:59 +0000

    emphasize several factory cry hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
 import datetime
-def get_current_date():
-        return datetime.date.today()
+def get_current_datetime():
+        return datetime.datetime.now()
