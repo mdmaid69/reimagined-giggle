70e3b8d9ab9bf6d4c7bcffc7f89d7bdbe7ad7bee
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:39:51 +0000

    bringforward lead rarely rush again

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
