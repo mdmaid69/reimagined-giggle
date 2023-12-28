930db86d41a5362b2974ad407f5238dab6e65fa1
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:07:11 +0000

    wheel tight chin onto accident

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
