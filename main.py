46948845629310e3e8eacb269d36ffde7d52e712
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:59:27 +0000

    except anybody thought conclusion reduce

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
