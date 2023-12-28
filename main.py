fa082ce36514ce8530a35441c6df42b8fea31242
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:03:40 +0000

    fasten stop bringback steady dull

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def get_array_as_frozenset(array):
+        return frozenset(array)
