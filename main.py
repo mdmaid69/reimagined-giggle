b7b09666ffd826c23abd0e9d2fbfc95a64b35c8f
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:12:13 +0000

    smile plate lend unfair blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
