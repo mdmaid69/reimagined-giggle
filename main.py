c09e4855443ad5c7195e7f0517f89a5365618453
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:39:14 +0000

    several order ahead successful small

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def reverse_list(lst):
-        return lst[::-1]
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import datetime
+print(datetime.datetime.now())
