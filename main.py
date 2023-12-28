49195879e2dbe7afec91dbb906ab19cbf8562cd9
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:18:39 +0000

    565end talk whoever move intend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
