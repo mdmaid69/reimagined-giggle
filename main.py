41ee46743dc19806462ab6953f94bb9ca852cd4f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:49:56 +0000

    famous responsible have nerve far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 sentence = "Hello, world!"
 from collections import Counter
 print("Word frequencies:", Counter(sentence.split()))
-import time
-def get_current_time():
-        return time.time()
