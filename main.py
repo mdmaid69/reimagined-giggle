0ffab64a5b061ee744a3a4a1b2d0d0c177d32b4b
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:04:25 +0000

    mysterious play temper along by

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
