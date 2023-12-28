4d7f2665e40be450d9c675c478aa7f6dba8d6830
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:02:36 +0000

    regard lesson effort extra change

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
