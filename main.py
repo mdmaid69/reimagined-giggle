0229b53c04ad8aa825a8544b53a5c56fa5186354
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:26:43 +0000

    break regularly variety quiet me

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
