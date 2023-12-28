1e4bbed02fa8d7318aff78cb544759926d1acc6d
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:20:10 +0000

    fix communicate thing spin unkind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_odd(n):
-        return n % 2 != 0
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import sys
+def print_python_version():
+        print(sys.version)
