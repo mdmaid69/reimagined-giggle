192a3f6f04bf692c974703b8ad982a5be25c351a
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:07:05 +0000

    roof holdout giveback any explain

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
