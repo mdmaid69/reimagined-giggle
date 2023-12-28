ec02930a0d8df3c7f5c7b11a2b29edcba16bbdbc
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:34:20 +0000

    so breakin he responsibility excuse

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_average(lst):
+        return sum(lst) / len(lst)
