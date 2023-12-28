8f8bbd00abb0b2047e39289cf9f694297543b073
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:32:56 +0000

    squash angry convincing damage declaration

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import platform
-def get_python_version():
-        return platform.python_version()
+import numpy as np
+print(np.array([1, 2, 3]))
+def find_max(lst):
+        return max(lst)
