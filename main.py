5a6aedf431d394fabc10b27cfc60f2b852bdad18
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:26:07 +0000

    still date warn sight culture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
