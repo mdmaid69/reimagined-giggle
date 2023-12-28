342ce56a923ad1951feedc6d975459e49fd3b16b
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:52:34 +0000

    mean country language branch ourselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
