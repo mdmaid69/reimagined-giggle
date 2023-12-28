4f63d9bcfe9a1d40817299fa69f25077f2d8aa4c
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:11:32 +0000

    act regard workupto often actually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import os
+def change_working_directory(path):
+        os.chdir(path)
