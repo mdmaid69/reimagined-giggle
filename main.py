25a79fab64ab587eccfdcdad5e109e83fb27ccf2
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:25:42 +0000

    me load unfair shut rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def add_numbers(x, y):
+        return x + y
