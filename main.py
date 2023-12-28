6c4deac4c19a684fd1ea494003218b7a5ae2f129
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:06:26 +0000

    street yet single behind sell

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
