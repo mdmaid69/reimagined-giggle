3774c936637502c4abe24bb068a5346481c0140b
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:17:30 +0000

    tough pleased fence glad callup

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Hello, world!")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
