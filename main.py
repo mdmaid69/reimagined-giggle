0f6561abb6a232cc2087860f23643830f68426b8
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:24:24 +0000

    keepoff crisis instruction argument cruel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
