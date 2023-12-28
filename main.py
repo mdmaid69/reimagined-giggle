46262bd4f45573070e906f2cf74e10475c3ad616
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:07:26 +0000

    loose before nervous squash bad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def remove_from_array(array, item):
+        array.remove(item)
