7c9217fdbed55ad50f06c0597ba21fc6279fd7a6
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:59:04 +0000

    please accept reliable way better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_max(lst):
+        return max(lst)
