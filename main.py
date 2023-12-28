00e4ced56c3d6a90d0b4e60864b76373fa6b7f19
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:19:16 +0000

    movement illness grateful degree purpose

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def convert_to_hex(n):
-        return hex(n)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
