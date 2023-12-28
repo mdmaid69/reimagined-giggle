e56de9f88a3c48d24c19af201474a3d939ede5af
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:10:03 +0000

    pretty foreign home callat stand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
