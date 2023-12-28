5d5531624553d393ed8d1238fe8dbae31809f3b6
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:25:25 +0000

    thing splash identify worse attempt

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def convert_to_hex(n):
-        return hex(n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
