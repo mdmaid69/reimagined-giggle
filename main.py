f4e8f8816c3f15e43dec32cf2ea1b9d528cb0e4e
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:22:56 +0000

    declaration out wrong beginning sleep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
