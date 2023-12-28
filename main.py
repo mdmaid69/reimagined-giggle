0e7e6f4056d3b630d143971303d338fd0bc763d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:47:55 +0000

    show excited square gift pot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
