70fcccfc3602cb33c75932807dbde542de62a522
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:10:59 +0000

    pressure sleep light trick start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
