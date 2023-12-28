8b584ecdba1cf4be19c973e8ce6b726ed61a3eb5
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:25:25 +0000

    guard warm tax gate politics

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
