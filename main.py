629577d329620100ea3ad4d3c621b5dc2f21b316
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:53:44 +0000

    fortunate birthday know reply moment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_speed(distance, time):
-        return distance / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
