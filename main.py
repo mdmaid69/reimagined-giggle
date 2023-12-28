b9f99936f3ddee1001ac7d5886fd8394f5d679c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:46:32 +0000

    communication people software work art

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
