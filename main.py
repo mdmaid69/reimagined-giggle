fa4126045ff783d8fb5047eb080a85debdd9ee42
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:45:28 +0000

    ignore lookup mention mine herself

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
