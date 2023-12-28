a20a4a0cdbc86e215323ea413d07aa0af50b967f
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:30:36 +0000

    switch just light problem club

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
