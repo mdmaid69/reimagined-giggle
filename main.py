db9c4aa56fc902f979a7492cf077f173673b3c94
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:27:19 +0000

    language quality comeover him discussion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def count_words(sentence):
-        return len(sentence.split())
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
