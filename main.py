7fbd8802f611a974ec6bec3b5b48381a9ea3b212
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:13:15 +0000

    mess put group conclusion child

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
