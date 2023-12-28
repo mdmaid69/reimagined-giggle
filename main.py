0bca921a08ac5a4a3744500a8708289f4aa1f63d
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:44:07 +0000

    basic start unusual beautiful image

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
