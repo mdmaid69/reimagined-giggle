b04043b62d4784c4a6ce15a3510c9a7022e764fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:45:06 +0000

    read muscle memory stiff healthy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
