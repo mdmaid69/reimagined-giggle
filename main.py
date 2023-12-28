ccdbd4118ac26cf2e4963f7252037d79b846136f
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:23:11 +0000

    long official without satisfied appearance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+for i in range(5):
+        print(i)
