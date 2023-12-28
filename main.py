e9c49dc2532a1ab88fe769f33ea8cfd8daaec947
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:06:54 +0000

    coffee fault art talk show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
