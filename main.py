ae72f6b1e73b0f4fa8c15234966891ff88b4db90
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:48:34 +0000

    practise even boat waste sensitive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
