baf5dd0907d17ff348ebeb3cf1980644bb8f1dde
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:02:09 +0000

    flat attach rain whistle short

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
