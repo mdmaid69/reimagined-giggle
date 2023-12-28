7504eff4974ebda88c72e87b6b167b89e3ca8e31
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:19:39 +0000

    butter twist shut soon trousers

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
