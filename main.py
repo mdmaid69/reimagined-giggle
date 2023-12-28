fab5fe6ca358c941b80b9ce656daac3b58a49f38
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:58:13 +0000

    patience anyone ordinary purpose weekend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
