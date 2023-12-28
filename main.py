3cc1e4139a9ac6535320fddd2e588d854769dd9e
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:37:33 +0000

    mother moon anxious appeal ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
