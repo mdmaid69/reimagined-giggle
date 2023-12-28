766c58716a022420ad4a6638836849b2206c930b
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:55:53 +0000

    embarrassing warn break early around

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import tensorflow as tf
+print(tf.__version__)
+def find_max(lst):
+        return max(lst)
