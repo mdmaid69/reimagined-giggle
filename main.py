a6b360353c50a6c8644de31761cd1e1186a1b23b
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:21:45 +0000

    flavour welcome degree instruction introduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import array
+def get_array_as_repr(array):
+        return repr(array)
