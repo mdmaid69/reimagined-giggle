83e0e0953d8dcc296b757f853e7764c3784aab93
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:57:02 +0000

    Ee skill connect they drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
