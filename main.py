7b9ca7f66da03d68ede3b39093f4d15faf7dbb65
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:02:00 +0000

    until anybody many commit married

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
