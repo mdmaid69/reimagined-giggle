007e42df39fb1ecf8ca72111855abd9586ce56e0
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:19:09 +0000

    cry goout smile treat underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
