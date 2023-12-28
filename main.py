bd60d9efcfbd2e8d576de39ef23bd640178ab5d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:44:37 +0000

    before free long last far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
