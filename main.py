41fde9db8dfdeb2035ec644efa1272989b267178
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:28:57 +0000

    hate poor between statement walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
