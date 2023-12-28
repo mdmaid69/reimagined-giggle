3453c2d23ecab185342d1bc2ae83a1e18d9ecc1c
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:45:08 +0000

    bar plain sink same mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
