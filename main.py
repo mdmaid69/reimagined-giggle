ca98869addbef65144d3058585d4cb75e02955d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:38:12 +0000

    hate fortunately pleasant preparation ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
