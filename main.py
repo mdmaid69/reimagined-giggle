ea5f275891e84ed82ec9b91406eb54dfa05c3984
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:07:31 +0000

    fault steady thick polite behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
