20a15d229a4867dbe2b8918b614a3ea06efa41af
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:00:49 +0000

    telephone argue object move hold

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+print([x**2 for x in range(10)])
