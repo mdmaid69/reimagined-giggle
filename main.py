d8cd092ceab8a2f8487abdcc912aead0aa43fbf7
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:23:42 +0000

    force Bb trial silent completely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import datetime
+def get_today_date():
+        return datetime.date.today()
