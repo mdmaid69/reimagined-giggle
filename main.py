abef2d09834216f370aa2d58a4dae156f9163bc9
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:42:37 +0000

    meaning drink answer fast wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_power(work, time):
+        return work / time
