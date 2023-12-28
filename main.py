dfa11e88478d18bb4564bef2202e5fd18c0f5d9b
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:26:10 +0000

    sight used hide left size

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_logarithm(base, x):
+        return math.log(x, base)
