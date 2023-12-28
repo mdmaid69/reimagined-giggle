37457d03ed279ae1f16d8820f37178dfbc36043b
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:05:17 +0000

    thinkthrough mean play ignore confuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
