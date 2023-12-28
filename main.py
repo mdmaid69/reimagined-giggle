d155edec8cc7860f42e5a91fa298b9dd5b9b3c39
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:03:37 +0000

    family too sea practise invitation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def count_words(sentence):
-        return len(sentence.split())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_power(work, time):
+        return work / time
