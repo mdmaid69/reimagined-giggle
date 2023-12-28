00218c8628b7da678881e001dfc2efb6b9186bcf
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:58:10 +0000

    judge control container bring such

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
