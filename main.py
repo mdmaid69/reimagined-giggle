0d8e1ca2c5caff9bb22c1cd2622e3bd4fa687647
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:15:19 +0000

    punish business sign step usual

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_pressure(force, area):
-        return force / area
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
