d312dcdf8bf277a227228746eb1671090d314e27
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:45:28 +0000

    expression guilty thought pity creature

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
