c7ae8092c4c6c88d29b4a380544e79f8b5b0fab8
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:14:56 +0000

    smell combine pay company control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def append_to_array(array, item):
+        array.append(item)
