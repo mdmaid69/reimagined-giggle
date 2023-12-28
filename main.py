dbb888250ba419590f928a4247ebe7be707b7a1e
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:16:50 +0000

    hair equal clear near wake

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+print(math.pi)
