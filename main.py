b90da3f98835443c437901ca907eafe4c5f4e013
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:40:23 +0000

    well bound herself price turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import os
+print(os.getcwd())
