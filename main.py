58571b7c357ee1e19fa05056b66119acc7a9a71f
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:08:23 +0000

    criticize desperate getback long surface

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def find_min(numbers):
+        return min(numbers)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
