315efcaf23676d003af0ec6784b42d76f904eb77
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:36:55 +0000

    failure spite raise tool dream

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+print(sum(range(10)))
