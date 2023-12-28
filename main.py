450c30b52d52f38d27124a738f6334862e5d0901
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:25:54 +0000

    bored bus tall getaway degree

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_current_directory():
+        return os.getcwd()
