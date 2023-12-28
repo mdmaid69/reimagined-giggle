7f88897f090792e7a29534d8f05f4596f3960582
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:38:04 +0000

    pull after far pull obvious

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
