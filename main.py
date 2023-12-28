68d8057c3d5f45378343df1cae6df735a6ad4641
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:44:45 +0000

    direction than which church would

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
