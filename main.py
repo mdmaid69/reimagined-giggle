d7f8704082dc03869c2dbf143c5183f9298a077c
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:48:28 +0000

    sick draw watch objection active

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_power(work, time):
-        return work / time
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
