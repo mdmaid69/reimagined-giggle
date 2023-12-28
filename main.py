e9a856c35590c98cf8ff183fce4735f5118242c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:10:03 +0000

    step either excuse pair plain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
