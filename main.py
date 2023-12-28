01e90bac84e21d3f1e459a061b7fbbedd75cd228
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:55:00 +0000

    family afford down appearance wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
