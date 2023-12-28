7a21c1fd9441b94d10ddf404965ca8f2331f6c4a
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:41:31 +0000

    feed ready new greeting dry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
