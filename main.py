8a3776911b0bf11fbd8aa77a0cbe706dea4ce236
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:21:16 +0000

    ill plenty whether film spring

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
