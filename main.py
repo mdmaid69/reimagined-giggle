c3db17baf39315d804d8d8c537d18840f66a265e
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:31:37 +0000

    badly point loose put they

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def change_working_directory(path):
+        os.chdir(path)
