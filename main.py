8c949936c024db0c4e265589f415c22ad36093a5
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:58:07 +0000

    Ll share task contribute drink

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import collections
+def create_user_list():
+        return collections.UserList()
