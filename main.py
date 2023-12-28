fcb1f2de29c8f0711f914a8793788ab713f51e90
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:27:08 +0000

    pleasant difficulty national class judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
