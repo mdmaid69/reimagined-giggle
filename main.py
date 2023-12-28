424943bde4dff6e7b27a231aa2b85ddb669d8af8
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:10:44 +0000

    stick habit deliberate clever clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
