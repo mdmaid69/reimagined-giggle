00df4e96544488aee3c09ee4b9df568e6c94232c
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:15:38 +0000

    pronounce finally can meet stage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
