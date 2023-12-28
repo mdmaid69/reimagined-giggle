136407ce57a3ea50007a237bce5c1c0fbd318816
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:41:01 +0000

    exist swing process warn wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
