1f815875fc784bb667f4e2003866bd780e1b9fd8
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:13:51 +0000

    unless hope model fault moon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
