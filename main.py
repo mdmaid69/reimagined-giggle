3543b9d67d815b7dd5cd18f4276461a11bc4bac5
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:36:12 +0000

    jump obvious keepup public brother

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def count_characters(sentence):
-        return len(sentence)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
