7888ece5fd3626e6fd6ccd31ff8feda553794146
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:46:46 +0000

    explain hollow in hers suggestion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_pressure(force, area):
+        return force / area
