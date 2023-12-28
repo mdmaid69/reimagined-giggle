48f2661cc518badc93a0a9e083c49e6faa2ca71e
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:52:31 +0000

    bank post read crash cheat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
