64ac18e5e6c6014955ed471899d9aa57f8efdea6
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:30:21 +0000

    male mark walk getaway fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import getpass
+def get_username():
+        return getpass.getuser()
