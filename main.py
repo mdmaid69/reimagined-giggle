1670fe3f88d9be73d43bfbb3f698e386992e807b
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:52:36 +0000

    borrow male still suggest extremely

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
