4bcbe038695ea02931a6770a58a14957f2d7cd81
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:54:21 +0000

    correct protect detail callout intelligent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def subtract_numbers(x, y):
+        return x - y
