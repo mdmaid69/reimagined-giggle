ab2e9ba67ef19abb7939cc467a49beacf99317d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:18:54 +0000

    shop throughout it jump arrive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
