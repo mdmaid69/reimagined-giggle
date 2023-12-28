032c86d0447c6b15dde69d3d0758f1715f04558d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:51:29 +0000

    treat description poison environment scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_set(array):
+        return set(array)
