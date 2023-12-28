a8c7a076167fa651604bc101f08aac8d41d9c70a
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:49:15 +0000

    region notice proper frighten more

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import random
+print(random.randint(0, 100))
+import getpass
+def get_username():
+        return getpass.getuser()
