867376aa458385645e9dcd5c9f196279cac0883b
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:43:19 +0000

    near outside develop event noone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
