cc81036370daeff445fbfef1f13362b1d8fdba60
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:11:33 +0000

    dark include murder together flight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def sort_list(lst):
-        return sorted(lst)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_power(work, time):
+        return work / time
