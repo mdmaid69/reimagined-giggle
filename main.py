f6929871fb7d889b9ac17115347cb9101e446c24
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:06:51 +0000

    or load also sort thick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import collections
+def create_queue():
+        return collections.deque()
