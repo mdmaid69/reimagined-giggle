4c232cf0a54dcade0e61b7f8b4632dfbf71781aa
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:32:57 +0000

    myself way shadow see agree

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  def convert_to_hex(n):
-        return hex(n)
+def find_min(numbers):
+        return min(numbers)
+def count_elements(lst):
+        return len(lst)
