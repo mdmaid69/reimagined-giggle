4414c5e277c924f87678521b53ac74526b34d6d2
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:54:46 +0000

    quiet this OKorokay control please

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def add_numbers(a, b):
+        return a + b
+def count_characters(sentence):
+        return len(sentence)
