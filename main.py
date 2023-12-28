19e83871d9798855ac597f7b31ac97e69766d1f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:51:51 +0000

    broken concentration kick recover importance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def square_number(x):
+        return x**2
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
