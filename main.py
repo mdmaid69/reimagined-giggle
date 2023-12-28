4e93d5ccc6f968c4895d669d8993c07224449a5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:55:40 +0000

    chance embarrassing surprising car muscle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
