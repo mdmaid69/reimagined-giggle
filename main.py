63e62e68aee599c63cdbee1e3a003bca8424ec7d
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:48:26 +0000

    but result side word use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
