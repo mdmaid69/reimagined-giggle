dfac78f320ac18c16169e00f924670a6121b8702
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:26:06 +0000

    journey whole move once weekend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
