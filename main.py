a0f3209f44bcb4e5eb0bcdeb887a027ecb7bf680
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:17:05 +0000

    lower breath foreign furniture healthy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
