173dd03215c97f43f794eafe8f2dc35f6d7b3a8a
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:07:36 +0000

    hers substance interest similar loose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
