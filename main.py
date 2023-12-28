de33340f6a923c79692bfa0139d7231846d05380
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:08:43 +0000

    pay possible point odd shut

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def clear_array(array):
+        array *= 0
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
