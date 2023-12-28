aae011c7e9c38927069a8a9d6b45c31c5fdf5dc8
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:27:41 +0000

    hair lock behind not pile

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def convert_to_octal(n):
+        return oct(n)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
