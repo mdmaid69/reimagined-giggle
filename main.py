b544f787103dbfe202b77db7a687fe8edc2542cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:33:30 +0000

    talk fortunately wave smile region

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def is_palindrome(s):
+        return s == s[::-1]
