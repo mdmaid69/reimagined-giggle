7c11af211ea7bdf699d0a1128d27ebedd3f46f7a
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:27:40 +0000

    reasonable neck position practise mess

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def calculate_area_rectangle(l, w):
+        return l * w
