a4c19d4d43f3b514be951a0f6147d3235829c3a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:38:55 +0000

    cry whichever impressed service slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
