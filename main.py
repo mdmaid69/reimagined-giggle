383fd2e9bb94299c231829db04987787c4a5b7bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:24:55 +0000

    disturbing later area myself unite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
