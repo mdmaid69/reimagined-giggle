422841208c306ad403606ccee3996a5607948d97
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:02:07 +0000

    giveout measure story cause content

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-import re
-print(re.match("h.*o", "hello world"))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
