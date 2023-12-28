22c762050ee1f2008ff460efc5a44610f90760c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:11:06 +0000

    beside afraid convincing interruption up

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def add_numbers(x, y):
+        return x + y
+def find_unique_words(sentence):
+        return set(sentence.split())
