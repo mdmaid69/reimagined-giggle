dfc9ca3c1c3673b78292af2c158038d4ef0bd8ef
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:41:20 +0000

    age border consult holdup fetch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
