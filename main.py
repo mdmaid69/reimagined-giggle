f04094b76f8fd99956a695a096e099a335fa0e0a
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:25:36 +0000

    activity imagine rare throw so

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def cube_number(x):
+        return x**3
+print([x**2 for x in range(10)])
