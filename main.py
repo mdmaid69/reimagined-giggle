0e5c241c32edc7e34c565b0fe4cccd027cef9fcd
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:21:02 +0000

    rough try never confusing average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import json
+print(json.dumps({"name": "John", "age": 30}))
