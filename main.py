e401c9a871453d83edd91483a397566bd1f74b7f
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:44:22 +0000

    also news stone direction smooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
