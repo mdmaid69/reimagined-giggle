56f4569566c52fd6a6af41231970eb498cac7e28
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:19:20 +0000

    negative stare comeup 179bite close

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+for i in range(10): print(i)
