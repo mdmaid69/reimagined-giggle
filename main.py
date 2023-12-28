3fd478e10cd85cadbf85502b050212dcef69c007
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:23:33 +0000

    call persuade violence suggestion heavy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+print(random.randint(0, 100))
