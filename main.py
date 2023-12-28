bd079c9a171d0cc8a9255e2726ec445c9b551aa9
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:34:54 +0000

    bang frequent kind their fetch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
