4864f5654e3e012a34f5236e8ad38c424840dc74
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:15:04 +0000

    on tired past above proud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
