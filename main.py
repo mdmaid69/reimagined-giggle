9fca40de38e32a122795a6e09a0bb14bdb4208e8
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:03:47 +0000

    protest explain convince silent brush

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_int(array):
+        return int(array[0])
+print([x**2 for x in range(10)])
