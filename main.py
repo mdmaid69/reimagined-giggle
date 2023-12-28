72ea25e117d1235c29521ede82f22c59794e783b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:12:13 +0000

    during question left black peace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
