0aacaec80e08d82eb0c1536c2bdd924af667857c
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:31:50 +0000

    comment motion mail handle blind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import numpy as np
+print(np.array([1, 2, 3]))
