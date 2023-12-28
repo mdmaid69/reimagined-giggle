ea92913e26c69408ab7d7097e7e6ac4e7458c82d
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:28:41 +0000

    late mistake water so frequent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
