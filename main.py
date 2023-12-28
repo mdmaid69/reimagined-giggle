dbe36cbe24425d8cb9fb4b0110aced172f85a7bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:13:13 +0000

    workupto trust terrible damp hard

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
