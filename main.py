5caac48536e8da44fb8de5a3d2b420b4a47d6865
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:39:11 +0000

    figure relief prize team feel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
