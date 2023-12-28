84e149644d7519c8a644af82dd1256b3bedf75c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:12:41 +0000

    punish respect finish rest behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
