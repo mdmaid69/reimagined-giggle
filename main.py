d6360076d5335cd81da35c964d78578b426b7284
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:33:47 +0000

    in final memory foot measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-print(random.randint(0, 100))
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
