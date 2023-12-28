ebc301e4581034aa838c0f50b992981b31933004
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:50:30 +0000

    concentrate ear interview passage float

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import time
-print(time.time())
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
