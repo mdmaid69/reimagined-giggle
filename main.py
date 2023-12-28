2ad2b69edcbee772a0b06844367d9a6eeb83a882
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:51:09 +0000

    tool least remain serve separate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
