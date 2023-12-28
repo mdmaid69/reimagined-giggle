bf8732070ddcb5688701276b91b66c1f5642d3bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:07:22 +0000

    ordinary meet bet goaway board

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
