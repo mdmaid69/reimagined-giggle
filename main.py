a99986bad9a5e05587216858adafaaff39ad72d4
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:16:49 +0000

    involve game search know pause

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
