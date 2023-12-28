63bb1c11c0c28f7b782e0ea3824040c688ddbafa
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:01:11 +0000

    serve hide careless this honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
