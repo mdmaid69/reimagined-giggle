1f3691f2dfdf09d5c955b400130e6be61e2a0cd4
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:21:18 +0000

    behaviour programme fork safe lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
