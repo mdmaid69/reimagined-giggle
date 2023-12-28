0c272664e7bde372b3d8f3db1eceab3ea176e0c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:12:07 +0000

    below crash note neither absolutely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_palindrome(s):
+        return s == s[::-1]
