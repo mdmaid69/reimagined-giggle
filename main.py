c0abe6cfcf7b5d05734c41769874cf4adf771267
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:14:23 +0000

    deaf plant exploration splash remove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
