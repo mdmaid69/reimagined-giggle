39fe651835d86a2597415ea4623248c4c21d7ff6
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:38:35 +0000

    own tap custom force collect

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
