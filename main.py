99c5fe99efccb9d0a5ab1301d0b0713860d14798
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:57:39 +0000

    active quit explain criticize since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
