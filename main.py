77a69b16336815353094eacea0ada6c9d676ec5d
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:38:13 +0000

    advanced spring cook professional raise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import sys
+def print_python_version():
+        return sys.version
