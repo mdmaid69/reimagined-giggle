7013e7b4a962138852cbdd58ac759c03e997ef6f
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:11:22 +0000

    build settle fault after between

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+for i in range(5):
+        print(i)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
