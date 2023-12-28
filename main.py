2ae9a093e2c6c7bcea05c5591717bf831222b6ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:52:09 +0000

    temper shake dear cut age

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_power(work, time):
-        return work / time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_min(numbers):
+        return min(numbers)
