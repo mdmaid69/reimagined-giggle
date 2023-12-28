8249c46e11d355cdaeb1fcbd68c55c6e6783e2c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:37:17 +0000

    strange touch after approve rest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
