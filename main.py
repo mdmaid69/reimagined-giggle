9e1c6ecb03714718eda4d9c191cbf12699938b62
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:08:05 +0000

    tend minimum ring funny giveup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
