71f89c97e5d738a251af0a3f2e2a199c170a13ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:30:26 +0000

    satisfactory behind excuse bringdown suggest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
