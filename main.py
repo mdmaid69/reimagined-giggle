cff7134b4f17705122f1b6f765ea2a90508abe01
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:04:51 +0000

    subject organize future even people

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
