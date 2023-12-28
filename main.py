78e53de9f05cccf003c598315840e49cf87a7a8a
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:56:50 +0000

    spray sea Cc bringdown spread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def cube_number(x):
-        return x**3
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import os
+def get_current_working_directory():
+        return os.getcwd()
