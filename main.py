8925023a6e3ef8bbad14646b3c159ad806891653
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:26:29 +0000

    comment nature care keepoff sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def convert_array_to_string(array):
+        return array.tostring()
