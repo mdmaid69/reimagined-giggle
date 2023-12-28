28f82f0f2f3c7866b1e63b4ffc9e7d03edaefcba
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:20:56 +0000

    progress successful possibility underneath even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
