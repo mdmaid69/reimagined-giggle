5ba06fd00d1dff056289ce198a36a3595a4061ff
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:31:05 +0000

    stand respect previous praise wind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
