04117b6183bb9a270afce337946cdc8b1273bba4
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:56:40 +0000

    brief healthy instrument hard board

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def find_unique_words(sentence):
+        return set(sentence.split())
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
