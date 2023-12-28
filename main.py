554e32038486d9b475ba2b5ca1e6662e4b1cfca4
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:39:22 +0000

    dull drawing responsible pin free

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+print(sys.version)
