acb415a3c8c00de1fa2c3c563537962be96dae0d
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:50:50 +0000

    unfortunate comfortable allow turnover smoke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def subtract_numbers(x, y):
+        return x - y
