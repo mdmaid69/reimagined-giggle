5df06ee1d0d7c8f149dc70b4ffb402b557b385ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:22:22 +0000

    unusual children shopping fail level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
