ef1c2f0f245bb8e13d60b19dacf2d2d0fb8ae66e
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:26:22 +0000

    breakin pattern run do freeze

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_base_name(path):
+        return os.path.basename(path)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
