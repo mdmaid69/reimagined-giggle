fda4a9d5be0f96028f46ecf900f5f5bc4a3a3724
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:13:02 +0000

    side turn can range giveout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
