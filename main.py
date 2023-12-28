e237829f5ef52269cd1fdb521e545bd264b3a699
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:19:07 +0000

    your thinkthrough takeout considering finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_as_str(array):
-        return str(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
