24519662c607881448ae0b4ab89f82e77cb3a5ff
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:57:22 +0000

    path cruelty Dd uncertain burst

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def cube_number(x):
+        return x**3
