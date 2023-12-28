162f3fa1f2e4f32187baeb44b6a3b55186c7d104
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:55:22 +0000

    objection improvement hungry pleasure odd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def cube_number(x):
+        return x**3
