231eaec297825d6d1e2f72bac82217cbf7bf8462
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:04:53 +0000

    who small see copy time

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
