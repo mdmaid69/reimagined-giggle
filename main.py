d7406011bca3c5d1aa1911b00b5b0c570580fa13
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:06:20 +0000

    clean act borrow unfair software

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
