9a08cec6863dbaeda56d24f4d860f213015d9035
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:38:32 +0000

    seriously remember turndown reserve passenger

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-for i in range(5):
-        print(i)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
