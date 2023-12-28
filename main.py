a69121e9071c8a1569b0f7c84ddea7237b63b7f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:24:58 +0000

    niggaacademy partner into spill range

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
