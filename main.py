2efa63243535827bd9086b2bc0b1ef6c4165d926
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:58:12 +0000

    example name soon think butter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  def calculate_area_rectangle(l, w):
-        return l * w
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import re
+print(re.match("h.*o", "hello world"))
