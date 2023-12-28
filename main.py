a437c02144bd3de4a37f882dde153b9ea2d80899
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:40:24 +0000

    language temper tool worth concern

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
