49892c565e2d46f0b0771ce93a51b35c7aec5a0b
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:17:08 +0000

    start part judge condition benefit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def convert_to_binary(n):
+        return bin(n)
