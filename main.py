0308050736ee880e8286dcffc4de6298ace23be7
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:37:12 +0000

    lucky calm Mm crowd chief

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+for i in range(5):
+        print(i)
