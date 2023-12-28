a4ede574af77b3c71190a4adadbf5965204b0e19
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:40:45 +0000

    please legal material plenty free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
