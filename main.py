7ccab5fa9b8762577b0d3b8fa9921ea748bad151
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:04:08 +0000

    entrance hurry study production just

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import random
+print(random.randint(0, 100))
+import random
+print(random.randint(0, 100))
