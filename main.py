c050a26df7b382be7574d8d43ef246b36403d7ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:37:01 +0000

    present burn thing mess example

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
