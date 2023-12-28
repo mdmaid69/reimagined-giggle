3bf6bdfd7f8eb192918d81836e795db40551de97
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:26:50 +0000

    talk stretch best where dress

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_acceleration(speed, time):
-        return speed / time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
