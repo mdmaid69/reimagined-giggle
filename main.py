47092d8c52a05ce9509a1e4440d6065fa60550a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:52:02 +0000

    healthy hardly heat mind along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  def subtract_numbers(x, y):
-        return x - y
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
