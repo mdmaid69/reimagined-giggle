dd481d7462c6be0a2fc99b2aa0528c6f1092daaa
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:56:47 +0000

    tie accident national pocket emergency

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def cube_number(x):
-        return x**3
+def is_odd(n):
+        return n % 2 != 0
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
