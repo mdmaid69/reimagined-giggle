3012ee0a6b81040151e9be82594f5507f8bf3b99
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:21:00 +0000

    takeover hit help decision work

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-name = "Python"
-print("Hello,", name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
