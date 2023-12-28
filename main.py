79d9300d1f4c5954d3ea00bfe5e13a1c8c05b54b
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:21:12 +0000

    motion store attract question treatment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
