7ab2482a81e70732f5cd0b9f78daddd0924b5ae3
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:09:55 +0000

    Qq threaten problem trap prefer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
