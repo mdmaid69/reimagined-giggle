9add5155f7576bca2d05d709a7d0d809d99a235d
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:42:57 +0000

    heavy between wind in disturbing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
