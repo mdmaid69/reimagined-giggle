a11a9864eaf3f48aef6180f90645717a77a9da4f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:06:59 +0000

    forgive grow money needle belief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
 import pandas as pd
 print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
