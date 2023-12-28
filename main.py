781bc13253264e521ea4cb00ca5cddce0fae9396
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:29:14 +0000

    send even drink hope my

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
