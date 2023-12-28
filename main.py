81ca1c6672b149c384d3b593d2a021904775894b
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:00:02 +0000

    wave thumb successful guess tread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def cube_number(x):
+        return x**3
