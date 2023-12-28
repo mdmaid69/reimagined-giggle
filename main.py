38687480e05dac553bd737e140a73f47420784d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:39:12 +0000

    scream such fail plan please

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
