6e40d35d33ede04033e35437d88729591bbab44d
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:00:01 +0000

    great centimetre coffee important hungry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
