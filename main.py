e29e76b285f4b8f808c149eb2de857d4421f6ae3
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:39:10 +0000

    Tt gate dance broken bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
