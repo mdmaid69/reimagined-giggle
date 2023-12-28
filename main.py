8aadf42512bd78f363ab3b584e3c2192f1ca1977
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:45:20 +0000

    introduce university previous sad keepoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
