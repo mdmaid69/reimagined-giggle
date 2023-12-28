6e1c3f3eaea26efee4393340911ea6f2921bf52b
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:26:22 +0000

    beneath main left stretch question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
