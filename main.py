a4cf586354e56c64fe82d126a4d966b65bc55777
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:38:13 +0000

    my tragedy container please inside

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def add_numbers(x, y):
+        return x + y
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
