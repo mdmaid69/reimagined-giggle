df5348fe07188108353f05a42ab8e840a27e0c9f
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:19:51 +0000

    door record crisis talk disturbing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def sort_list(lst):
+        return sorted(lst)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
