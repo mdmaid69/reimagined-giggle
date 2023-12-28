dfd031d3e1b5493f708cb1d5618cc43bdf7996d2
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:31:46 +0000

    reliable bush stone female top

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(x, y):
-        return x + y
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
