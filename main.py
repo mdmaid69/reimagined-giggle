fae2c306e1f0119e1e8e7a1b2cbfb8898437690e
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:12:17 +0000

    fasten factory bin empty human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
