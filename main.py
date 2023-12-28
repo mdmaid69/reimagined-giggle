7c604097320e0f5b8f90d66571df56ba7a80a1c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:36:22 +0000

    raw everywhere back date view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def divide_numbers(x, y):
+        return x / y
