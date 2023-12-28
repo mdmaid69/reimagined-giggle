aec85b428d9acf1990c6d0d428382d705aff3439
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:52:13 +0000

    human treat uncertain put always

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
