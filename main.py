382e8daf882882ccc63f2bc23fab2f0e4e33594b
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:11:09 +0000

    comfort sit expensive blame first

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_volume(length, width, height):
-        return length * width * height
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
