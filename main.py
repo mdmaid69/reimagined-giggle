b671b4b6f9fa3bb986f6b568e10a6c8fa33e96c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:02:57 +0000

    reject mess only argue excellent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
