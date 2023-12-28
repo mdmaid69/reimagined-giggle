1db08fecd4d44a21541f3f5975fa9ef7a123b655
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:09:18 +0000

    factory wash song serve height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
+        return os.path.getsize(file_name)
+import array
+def get_array_as_set(array):
+        return set(array)
