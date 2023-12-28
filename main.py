5dda02aa1ea14f89acef0f5f7f219281568c2ab2
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:12:29 +0000

    limit sense owe though satisfied

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
