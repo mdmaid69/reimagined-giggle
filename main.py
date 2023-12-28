39673de54bcb4ccb1e0968641bfaf097cc3704c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:01:46 +0000

    praise store past move smoke

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
