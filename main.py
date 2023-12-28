1509bc67de48f3155e1d45b6bc12103b0d4652ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:04:20 +0000

    place foreign we crime slight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
