d99af38c6b9cf462b095cd27f13514ada126fa6e
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:35:59 +0000

    refer pump arrive pause communication

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
