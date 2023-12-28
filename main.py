e0afe207a3dd5842cd7b6fe32f8679afbf88d7a5
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:20:34 +0000

    fault decide gather cry rub

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
