907bd05b901e63e3d20b3bef73e765f550213bff
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:24:31 +0000

    Rr wash pencil run container

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_hex(n):
-        return hex(n)
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_index(array, item):
+        return array.index(item)
