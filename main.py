eafce07311895e97dd64b04f435eca0b9c029e1e
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:45:58 +0000

    equal murder injury rare plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import time
+def get_current_time():
+        return time.time()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
