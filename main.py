c06a2aae01fd7afa26964ba2dd152d5a177e88aa
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:53:09 +0000

    mouth their strike light agreement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
