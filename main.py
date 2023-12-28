46e3121bcdaa9f34a9e4e4a7ac6d4766846886ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:40:07 +0000

    566end taste punish connected everybody

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
