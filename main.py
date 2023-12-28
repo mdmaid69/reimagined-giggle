0a1581f311035cbdf9df000c9cd3229971ce7e91
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:35:27 +0000

    anxious so rid accordingto beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import sys
+def exit_program():
+        sys.exit()
