fb0851fde2370ce5d3326c9185747a82d0ad153b
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:35:13 +0000

    anything order adjective would eye

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
