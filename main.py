592e460fdf99fb976bc14d29357cefc24d48ac0c
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:07:46 +0000

    towards gooff eye also opinion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_set(array):
+        return set(array)
