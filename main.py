143df0098994c56fe131fd8b5e86243cab7fffb5
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:46:33 +0000

    upset room pinch shadow different

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def reverse_list(lst):
+        return lst[::-1]
