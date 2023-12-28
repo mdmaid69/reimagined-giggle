f970ba143561e80e1ae0768432fa4fd56312a8fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:31:55 +0000

    date afford persuade rain comment

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
