6d6c81da41588b5158dade98573b2c3f8f0fef51
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:29:14 +0000

    ticket run interrupt shape drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
