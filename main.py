8c2fd8b8cf701d84628340fdb90ab2028d3cf768
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:37:34 +0000

    away pronunciation might shape cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def square_number(x):
-        return x**2
+def add_numbers(x, y):
+        return x + y
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
