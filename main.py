f180e5e811610ae26eff4766d9a7800e0f91f8c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:07:37 +0000

    apart divide continue drive trial

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
