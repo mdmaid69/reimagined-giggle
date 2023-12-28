4b9f8fd0b9f2da52be2bcb479a10c94e92308808
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:34:36 +0000

    place card taste free description

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_floor(x):
+        return math.floor(x)
