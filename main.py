d1c99a39445843fe290ff93542920296d5ad16a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:08:24 +0000

    desperate balance back area expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
