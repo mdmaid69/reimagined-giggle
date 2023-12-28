b57e6e54cd5bbbba92d3df9a303b6649060ca5ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:28:11 +0000

    rise sea lend judge note

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
