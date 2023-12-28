6998879a80d1fea4cceb4dc38bbbdd5963490666
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:22:09 +0000

    pin holdout voice because before

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
