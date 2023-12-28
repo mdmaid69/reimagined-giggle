53a6b8867bc4a6bbff0ea8203d5860913abe6561
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:44:24 +0000

    put skin ourselves foot lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
