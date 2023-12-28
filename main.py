d26ee1c962823edc9169e2405382330a688121a1
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:37:46 +0000

    hard declare influence problem snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
