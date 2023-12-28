3ba9efb3614501a49602a27eada2917aaf2c27be
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:23:28 +0000

    wing runover half price information

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Characters:", len(text))
