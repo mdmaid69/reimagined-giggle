2397b559c6ae5e1901a5e2f9ab90b64b872c02f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:35:54 +0000

    exam trouble ahead drive surprised

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+print("Hello, world!")
