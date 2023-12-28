e8ca447b67d633a2d37a39f688a65e0eeaddb480
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:42:57 +0000

    go frame principle bread hand

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import sys
+  def get_python_version():
+        return sys.version
