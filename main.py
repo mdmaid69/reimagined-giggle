7598b3fca8da580455520e42fde62f855ec85536
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:20:56 +0000

    considering record lookback(on) progress normal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_sine(x):
+        return math.sin(x)
