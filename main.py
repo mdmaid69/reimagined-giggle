eba0ceed33684b8880cece8790458d43fe23d761
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:27:38 +0000

    shine unfair myself also rent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
