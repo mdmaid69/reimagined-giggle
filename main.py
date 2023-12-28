3483aafdf0ae3ecb1bbb224c575ca51fd2fa559c
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:07:16 +0000

    today slice less runover swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
