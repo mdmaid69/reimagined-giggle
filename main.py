2ad13ebdfdf5c1b15f3faff8ecb8d3ccb5f99218
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:37:01 +0000

    idea Qq none star beside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
