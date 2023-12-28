0c7b79dc18116eeb720c0e63816b1e3892bd66e9
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:57:31 +0000

    shall closely considering 179bite disgusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
