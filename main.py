25d1078046035406166120681b05de3e1a5656b0
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:59:32 +0000

    mood port shock practice store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
