0b077d9a1fd5d2a68a2ae6f96c72441be9d06b69
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:09:55 +0000

    shelter them able future who

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
