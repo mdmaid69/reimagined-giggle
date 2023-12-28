03c106c87ac9347ad6953222b536ca185cd21405
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:01:56 +0000

    calloff leave important if terrible

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_pressure(force, area):
+        return force / area
