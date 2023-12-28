570d6f75a1a8f59e955a930f6c0771b835682f49
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:33:12 +0000

    joke with play sore snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
