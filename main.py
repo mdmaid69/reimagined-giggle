60f7472c2795be60630e6f5de87d7ef2f9f40797
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:25:15 +0000

    handle holdon behind have forgive

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def find_max(lst):
+        return max(lst)
