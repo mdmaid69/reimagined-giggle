5e8e5f3f566607ca4e4aae429f22e113c9258bad
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:30:00 +0000

    error mark trap home lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
