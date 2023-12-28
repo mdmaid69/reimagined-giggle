0c6cda3462faf01c73c97a55ef2a266ca529b238
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:01:51 +0000

    564encourage next no obtain Jj

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
