8d6dc3fe5f91098c990f706b9b57f42828715a6e
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:09:33 +0000

    but past shoot whatever half

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_sign(x):
+        return math.copysign(1, x)
