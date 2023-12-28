28e9bc00149c0195889ff96792852e010952d3eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:59:09 +0000

    closed uniform offer character Pp

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
