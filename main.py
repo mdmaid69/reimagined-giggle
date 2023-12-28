37ac58a4c2ed0b1aa639ae06435f5214bd7f8ed3
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:06:56 +0000

    medium room completely class couple

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def is_odd(n):
+        return n % 2 != 0
