f32866c3eb7815823457723760dd9d286b1bd4bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:55:41 +0000

    appeal record except tight spring

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
