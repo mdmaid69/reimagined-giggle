7d56568b1008e136bed49c691f04fb4393894eaf
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:14:43 +0000

    scale pretty holdon deep broken

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
