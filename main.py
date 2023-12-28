dfffe37539f787060e84dbd27edfa1ecf91815c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:26:50 +0000

    connected cost his use circle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
