c7096cc019d985cb77ba053490c48a9033b6c938
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:54:28 +0000

    mess so today picture warn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_priority_queue():
-        return collections.deque()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
