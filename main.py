bd3b3d7c79ee794fec428a9e31af1470a125267f
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:18:24 +0000

    undo on shame rule recently

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
