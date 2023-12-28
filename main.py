f3c5e16e6bfa33cbaa562046ac524cf07da6f2e9
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:00:51 +0000

    now term transparent strong until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_as_list(array):
-        return list(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import sys
+  def get_python_version():
+        return sys.version
