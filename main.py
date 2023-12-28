4cbed4d7a975e9c80a0cc3f020de9805662f93a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:16:30 +0000

    more apart miss strength badly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def find_min(numbers):
+        return min(numbers)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
