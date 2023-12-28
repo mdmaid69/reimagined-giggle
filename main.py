7bed0cdc35106fcdddfe76e82ad20105a68be0a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:52:40 +0000

    important middle operation demonstrate together

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def remove_directory(path):
+        os.rmdir(path)
