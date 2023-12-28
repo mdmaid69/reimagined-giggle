ea7731c6a475e033d14efae540cd81baa2337fcf
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:02:29 +0000

    voice system after party light

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_volume(length, width, height):
+        return length * width * height
+def calculate_area_rectangle(l, w):
+        return l * w
