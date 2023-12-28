25cec1f82a4b4c8d7a57427b1bf82ec5cd529da7
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:14:55 +0000

    contents impossible suck familiar bin

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
