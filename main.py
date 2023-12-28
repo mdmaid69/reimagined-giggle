e0c7a851bdab6df9e4c11ba9d9bc5b8afc580168
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:40:38 +0000

    print range push put breakout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
+  def add_numbers(x, y):
+        return x + y
+  def convert_to_hex(n):
         return hex(n)
-import os
-def change_working_directory(path):
-        os.chdir(path)
