e95dc084e18d3da81e8ea4580f3de6fb6813302c
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:16:47 +0000

    type actually maximum basically reasonable

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  def count_elements(lst):
-        return len(lst)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def count_words(sentence):
+        return len(sentence.split())
