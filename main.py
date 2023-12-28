246fb62e9726f587555fc8ef03d6a1f3b664970c
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:01:30 +0000

    inquire crowded shop anywhere there

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def greet(name):
+        print(f"Hello, {name}!")
