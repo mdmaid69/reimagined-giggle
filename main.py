0ea38ac19a7e3a7f1dae6519c5f4f30967c31284
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:15:45 +0000

    Jj wherever if score goout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_even(n):
-        return n % 2 == 0
+  def convert_to_octal(n):
+        return oct(n)
+def find_unique_words(sentence):
+        return set(sentence.split())
