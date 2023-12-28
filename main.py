49fb32c8b4902d17ceb4ebc86df4e2ca77f67d98
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:13:20 +0000

    stare regret deep music give

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import time
-def get_time_since_epoch():
-        return time.time()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
