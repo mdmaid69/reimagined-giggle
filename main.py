49752036fc77ad852e24f5ae4d085714d769d455
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:41:58 +0000

    recommend agreement sale breakinto imagine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
