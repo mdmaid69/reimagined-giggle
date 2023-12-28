d19b1b25821dca73978c653846ae59ef3f12f6d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:50:46 +0000

    concern force chance anyone 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+text = "Hello, world!"
+print("Uppercase:", text.upper())
