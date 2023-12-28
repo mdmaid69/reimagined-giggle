fdcef4636ef2b4b53e7543e1304cc9c727569fc8
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:45:56 +0000

    before brave store beyond back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_density(mass, volume):
+        return mass / volume
