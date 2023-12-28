04166dc3861e90022bebc121d265045989056cb3
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:28:05 +0000

    equipment late hate best pride

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def greet(name):
+        print(f"Hello, {name}!")
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
