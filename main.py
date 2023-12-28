37076efcb0ba2fc7cd4489cf210dccd7f4a1659b
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:59:37 +0000

    stir popular minimum defeat although

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
