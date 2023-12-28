39b80e067586bf931c65ba50a944c174541dccc2
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:08:50 +0000

    winter makeinto difference behave total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
