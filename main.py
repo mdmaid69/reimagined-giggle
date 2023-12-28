751b03e30eec73170a9a12f2005315e59b6da6a6
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:18:53 +0000

    steady or narrow interested service

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def square_number(x):
+        return x**2
