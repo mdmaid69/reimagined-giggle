02f8d0d3d6d537f24e671f6ad8a07c77f2e6a9e2
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:01:23 +0000

    behind read fault interruption at

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
