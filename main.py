1731ae6b149987bb43adc0e680d8377bdbb02258
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:43:41 +0000

    favourite confident anxious generally you

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
