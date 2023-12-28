f467c17aad8a70f807e48149878f42fb583e578b
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:13:36 +0000

    present mistake exam blame this

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
