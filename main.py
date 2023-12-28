f5045ac846932b36694360f43c12929e320e4160
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:58:02 +0000

    left side fix tragedy agree

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print(sum(range(10)))
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+print(math.pi)
