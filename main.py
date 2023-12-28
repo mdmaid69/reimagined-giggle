6b8116a13e706cf3fe7fd6783829fc7c7939e6d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:54:59 +0000

    load die lend flow concentrate

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
