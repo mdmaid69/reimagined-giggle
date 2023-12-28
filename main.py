e831667d627052cd3ba908bbf0b92ec904309524
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:32:01 +0000

    ground comeover present fault harm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
