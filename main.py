bd68371813175a6e759a41960d44b506433be324
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:11:31 +0000

    slight table subject whether achievement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def sort_list(lst):
+        return sorted(lst)
