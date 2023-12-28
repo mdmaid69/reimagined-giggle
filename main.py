6a6578efec10f5ab46b86b615aee8930c779f3ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:48:21 +0000

    surprise exhibition anxious scale deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
