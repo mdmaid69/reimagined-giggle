b0e76d76080bad8d19c1322ca6608428a8356d52
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:16:51 +0000

    tax vote learn major finish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
