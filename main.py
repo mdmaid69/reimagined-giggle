69038c6fe604f2802a008947b872e60d5684ceb3
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:43:57 +0000

    like defend centre equipment keen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
