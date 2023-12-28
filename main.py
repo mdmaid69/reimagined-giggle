5e23a8524d9e00233d4f1ffa69cbef88c2bdb274
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:02:43 +0000

    cost traffic confidence art phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
