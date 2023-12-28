9402c1e8a624cde7f6ffd994ae80be7976ad749e
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:13:11 +0000

    beat way Oo blame danger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def find_max(numbers):
+        return max(numbers)
