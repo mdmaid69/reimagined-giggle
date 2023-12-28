b7b6a09474e97f44a1ea0e694bd5ab753f1b00c1
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:07:35 +0000

    sometimes smooth hurt divide front

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
