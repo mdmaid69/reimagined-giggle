5c80bb028de13d755319e25ac4e2355aa5ef68dd
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:10:05 +0000

    base soil hurt health next

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
