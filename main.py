778f9ebc19c619db2017824660ab1a9ec6734ead
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:13:32 +0000

    declaration children broad unite opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import datetime
+def get_current_date():
+        return datetime.date.today()
