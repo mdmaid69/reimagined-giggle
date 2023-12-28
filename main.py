80a6784271cb781ad4f6b00cc6795acfe1738bc0
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:02:54 +0000

    sound crazy excited clean snow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
