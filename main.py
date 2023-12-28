2c3c26d4680b620ed83f8b382ebbe9c07f945c93
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:28:20 +0000

    public turn born build survive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
