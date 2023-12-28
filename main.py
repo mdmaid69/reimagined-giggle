4ae57963898a8792200892a7588905ad5b3ace2c
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:12:52 +0000

    that open exaggerate exchange cross

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(sum(range(10)))
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def reverse_string(s):
+        return s[::-1]
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
