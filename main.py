f76ff16794af6b7496c2415099e21a3dcbbd0698
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:15:10 +0000

    kill pleased punish free hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
