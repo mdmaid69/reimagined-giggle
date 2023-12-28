3564e4edbcb3c6a77e5592cca69d053cc320f282
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:38:36 +0000

    cope thinkthrough cinema mainly wet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
