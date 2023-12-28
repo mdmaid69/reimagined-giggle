b55a0baea1891c1c76ae5fc3e98e6f94ee8475cb
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:33:49 +0000

    sell slowly noisy rain behind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-for i in range(10): print(i)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_density(mass, volume):
+        return mass / volume
