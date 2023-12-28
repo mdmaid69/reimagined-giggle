9372a7019f0b64e052b2b99d52d2cd055bc41a9b
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:32:52 +0000

    bring meanwhile everyone hand half

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
