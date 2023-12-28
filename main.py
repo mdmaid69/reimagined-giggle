68fb5863a593bac1892f39cd0db04144eb9d259f
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:59:58 +0000

    back afterwards purpose forever smell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
