121b729d6d5acce4a085f36f3ecc4fbae92f4b0e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:54:38 +0000

    strength speed list heart scene

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
