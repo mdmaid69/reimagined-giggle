c141efe2157edec89a75cc8aa4164f2265194289
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:34:33 +0000

    pocket church stretch turninto production

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
