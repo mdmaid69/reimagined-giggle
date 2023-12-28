29340e95b5e4bec0c68429d6c86cfa287ca5753e
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:34:52 +0000

    him generous bored really Rr

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
