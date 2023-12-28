7191451c626e4985e4d1be47f953f20ead26cf3d
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:54:48 +0000

    can accept young statement him

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
