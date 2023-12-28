38269bc6db5d6b6b851603f4fae61d4f5e0b369f
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:57:44 +0000

    skirt close appeal tone translate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def remove_duplicates(lst):
+        return list(set(lst))
