a73c98cdf3652b090698ba560517d9fa4058ad12
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:01:40 +0000

    name complaint pour insist ground

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
-import sys
-def print_python_version():
-        print(sys.version)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
