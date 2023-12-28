e95412b7630c7a0baa8fb487263720dc44a9deba
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:58:43 +0000

    confusing total pipe towards information

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
