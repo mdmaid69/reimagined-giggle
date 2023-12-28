8650949099657635fb2aa57dfb487278ce3bbee0
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:42:32 +0000

    material measure neat breakinto activity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
