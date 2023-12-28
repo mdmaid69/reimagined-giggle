8ef67968182713c801200d932fb5970d6b7b8f66
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:23:34 +0000

    remember agree trap strength replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import platform
+def get_os_info():
+        return platform.uname()
