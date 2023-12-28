9f026e7934317d895c45ee19c36786a0b8a61e84
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:12:10 +0000

    welcome tidy block tax 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
