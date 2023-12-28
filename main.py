36aaac9b1c859c5c980c5c9caafe810167926bd6
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:17:52 +0000

    trip better personal raw finish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def calculate_factorial(n):
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
