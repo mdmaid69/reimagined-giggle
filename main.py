c98e7f1dec348aa4e4be2da19232262a2858bdf8
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:59:05 +0000

    mean front pocket deal trap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
