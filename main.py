ba05e8245f2801790073111b5bc1bc0b4c3c6513
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:16:25 +0000

    look glass afraid so check

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
