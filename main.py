9a76ef8c4151a24cbf419050b36a86eb4e3b89a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:04:30 +0000

    centimetre free weekend ball everywhere

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
