4d924b04b84a78cee4a738b6b17e1dbbeb07ee06
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:53:29 +0000

    less remark join term turn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def count_elements(lst):
-        return len(lst)
+def calculate_factorial(n):
+        return math.factorial(n)
