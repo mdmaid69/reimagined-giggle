604541b18b93ccdf97e77258385bfd0bbbcd4f91
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:46:32 +0000

    theirs cover book down operation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
