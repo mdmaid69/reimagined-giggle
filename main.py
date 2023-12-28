4abffa97508372e47a4e260168030d4196cf0b02
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:11:59 +0000

    choice force question giveback contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
