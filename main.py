9ea642ee774696247cc4d2f8494886215e6cdc76
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:25:05 +0000

    inquire reduction develop fairly conversation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
