edb1dc5b2c0c8adeaf93ba16448965c108e302ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:08:18 +0000

    disappointed adjective getout Qq sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
