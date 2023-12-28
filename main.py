5a4a5615640302a77fea82f07b9a1780bd0389d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:06:05 +0000

    trust myself train step drive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
