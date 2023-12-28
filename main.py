5a5b177d5176b95fd492d2d43ec904fa436ad275
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:06:40 +0000

    mine violent relative recently there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
