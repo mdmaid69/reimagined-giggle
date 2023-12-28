0fe0a4bfe7ba9990af52dbffec41b43f9d847575
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:12:15 +0000

    dead record arrival lead attention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
