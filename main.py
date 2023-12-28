8b165fcf2ec725bc7ef27725364cfd3fba885d5a
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:00:33 +0000

    help completely pick television minor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def multiply_numbers(x, y):
+        return x * y
