ad29ee327502c78d7b91ce0286601c84c4b38c2d
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:15:44 +0000

    group kill disturb kind their

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
