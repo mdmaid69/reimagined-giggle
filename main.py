6f49436b3bc6bdec8b8ec4ff19cae652b042fafb
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:33:08 +0000

    circumstance judge answer will shoot

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
