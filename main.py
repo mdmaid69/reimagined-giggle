86a2b64ddeb06a11aa320db5dfe4af471f27ab9a
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:09:03 +0000

    mine pinch legal meanwhile Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
