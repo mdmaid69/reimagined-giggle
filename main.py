5cb5db4ae8a88d6a778a10abe95c1b898b1406fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:18:17 +0000

    plan card active familiar record

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def get_array_item(array, i):
-        return array[i]
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
