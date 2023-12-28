ec894efeb2cc0ce16189ac781f3a5ca83fd6fa6a
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:52:09 +0000

    disappear safe same wish wise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
