e17c6adbb9b3d81be304784fa7c394357711a427
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:40:33 +0000

    consult play clear brush later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def find_max(lst):
-        return max(lst)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
