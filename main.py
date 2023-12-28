e718d0d92f7b99fe05eeb03dd230c7537dc81295
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:16:20 +0000

    tight one dish ordinary his

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def extend_array(array, iterable):
+        array.extend(iterable)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
