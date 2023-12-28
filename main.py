2a8cf2c28d9d99c7be0785923c3ae839fe7f1f15
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:15:04 +0000

    more beginning 566end we relative

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import sys
-print(sys.version)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
