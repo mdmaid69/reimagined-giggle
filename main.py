c853999e1218b1a01493be0c3654c1f1deb464dc
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:03:48 +0000

    cry sensible cinema receive runout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
