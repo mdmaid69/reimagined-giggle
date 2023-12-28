203a9777b17ba5c747972046d0086f69465fd8c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:10:40 +0000

    crowd interrupt although standard noisy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import sys
+def exit_program():
+        sys.exit()
