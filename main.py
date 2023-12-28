be915da707d92f749449c02881d96193f0d20cb1
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:58:31 +0000

    moon acceptable direct tip address

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import datetime
+def get_current_date():
+        return datetime.date.today()
