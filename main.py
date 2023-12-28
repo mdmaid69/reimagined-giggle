87852c43fe83f745bdcd25cf912b9c1f6722a9fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:26:19 +0000

    block typical comeback increase punishment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import random
+def roll_die():
+        return random.randint(1, 6)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
