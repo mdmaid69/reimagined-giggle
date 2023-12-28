b9e23f5027ab6a1c676319dd0f2953d5b7e71d98
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:06:37 +0000

    patience whichever cap comment forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
