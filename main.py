bd42125d73aff527a55450a9c8fca54194154414
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:51:08 +0000

    rubbish weak circumstance free spoil

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import sys
+  def get_python_version():
+        return sys.version
+import tensorflow as tf
+print(tf.__version__)
