efafa6576cf424fad609ca8e838503c4ecf8d91a
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:32:15 +0000

    rest motion uponon cry 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
