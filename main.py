8596e5187fab02268ae9b85069f90ef7793a8b25
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:46:52 +0000

    understand interesting record sit matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
