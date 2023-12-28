241fec93eb09ac0984293a089117f31607a224c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:05:14 +0000

    sign OKorokay slice foot ring

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
