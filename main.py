8f97c712f7024424fca3cdfb8e3857e163767b35
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:23:30 +0000

    trial computer breakout weigh party

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
