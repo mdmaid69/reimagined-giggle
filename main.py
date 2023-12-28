22fc6a787e3ca9e8bde356d97dd49cb09388a482
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:44:21 +0000

    shallow advice anxious concerning artificial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
