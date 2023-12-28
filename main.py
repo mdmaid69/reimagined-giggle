81e41f8a496b62139a4b9e6e8bd332b38a9c7b5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:17:42 +0000

    pinch interrupt process long style

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import time
+def get_current_time():
+        return time.time()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
