8b9dd1cec53868cac36eadca0261397de3326ee9
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:22:05 +0000

    kill tradition habit plain clue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
