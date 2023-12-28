d9d03e9093cb123917089877f8a7650cf60779a6
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:53:08 +0000

    design yours ill comeback line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_current_directory():
+        return os.getcwd()
