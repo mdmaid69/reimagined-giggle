8e1a1bb9486a3ab0d02c994d036266850961d9df
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:31:36 +0000

    breakfast possession thorough crash children

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_density(mass, volume):
+        return mass / volume
