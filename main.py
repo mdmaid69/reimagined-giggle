b9b5a16731d79cde89c03318734608a86baf7abb
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:31:04 +0000

    handle tight feeling stamp shall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def square_number(x):
+        return x**2
