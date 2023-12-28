8b3a02690e5a125d08e1ef5acd5b476a8f210fa4
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:39:23 +0000

    connected rid talk goaway comeout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def sort_numbers(numbers):
+        return sorted(numbers)
