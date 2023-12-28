43e163f057498ab5c4d8c8e46f8bbd64bd1573ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:55:54 +0000

    Tt firm escape marry split

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
