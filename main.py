1f9d04dae2a85f1de81b186be2145e51a884aeb2
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:39:56 +0000

    smoke recovery sell total behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
