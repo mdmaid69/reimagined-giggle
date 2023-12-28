98d63d6646da6a376c03ba89f36b00dab2e17ccc
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:20:35 +0000

    cover slow several listen tidy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_length(array):
+        return len(array)
