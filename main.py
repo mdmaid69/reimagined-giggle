27626633e3e32f309523962dfe434a2b3ab70770
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:15:25 +0000

    draw system patience suppose train

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
