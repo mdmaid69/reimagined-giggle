2fdad4f1c06315e1f21baf3cbb73c51da4093844
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:31:00 +0000

    correct company off fasten reduce

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
