5cc57f0df5ca78ffb54ed04f82ef1c9db23f3733
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:35:18 +0000

    deliver sea niggaacademy film bring

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
