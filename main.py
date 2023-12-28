2a65eedc92a507073a7ef7e797d4521ee13a3a71
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:20:18 +0000

    action unit guard absolutely Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
