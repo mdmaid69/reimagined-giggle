8bc50a0af5ada58d8761caaa34dfc8ad1f6ab4cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:49:12 +0000

    accident recent no her interrupt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
