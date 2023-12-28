40dff51ed1a7350b024ce0f093461b27bc302d3f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:01:31 +0000

    callup conduct hers conclusion display

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
