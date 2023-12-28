e36271ddda27e2b0c20a0e1fecb05f0ceacc1c83
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:28:19 +0000

    underneath underneath then getup unkind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
