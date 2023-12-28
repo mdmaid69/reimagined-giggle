fdf14df443e08d6b3bab7fb6838502074d449682
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:25:38 +0000

    feed unhappy feed excuse the

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-  def cube_number(x):
-        return x**3
-print([x**2 for x in range(10)])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import collections
+def create_stack():
+        return collections.deque()
