3317d3f07c58611d839059d7bd39a29ae6a24252
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:51:08 +0000

    seriously how connect case centimetre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
