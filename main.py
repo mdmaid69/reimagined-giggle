fc654aeb01329ffb25eb3635c8c9139ec8b8f454
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:51:38 +0000

    culture famous strong shout force

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
