db1b3e386f7f38f7396f369bef34f308895769b7
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:09:00 +0000

    spite freedom love opposite carry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
