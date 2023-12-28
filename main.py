585b2d040d926596fd611b1a6dec1d647f85d289
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:34:40 +0000

    limit object Yy activity reach

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
