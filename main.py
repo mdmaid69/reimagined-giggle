0f10641b391c6328777144c1a0cad7ee42def291
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:09:14 +0000

    gentle complete peaceful mystery appeal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import os
+def get_current_working_directory():
+        return os.getcwd()
