8e02414514f2531105d71abbddfb7a748804a2f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:33:24 +0000

    time talk sign Oo abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
