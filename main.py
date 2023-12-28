9d7f7b24466ed4ea2d91d9213d34c1f21e53c3c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:22:06 +0000

    address fear shopping rub attitude

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_length(array):
+        return len(array)
+def find_max(numbers):
+        return max(numbers)
