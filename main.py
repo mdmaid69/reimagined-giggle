5b01ebfc7dbf3c562ebc17220a86c84e5232a07f
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:24:27 +0000

    ever load other tragedy turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
