8b1316d5c12483cf266feea922eaa4589e575cf9
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:54:33 +0000

    rent ring train criticism shape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
