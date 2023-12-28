7466f21efb65279aea32bef95e82e2c21e10341c
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:11:19 +0000

    now police callat wash turnoff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def reverse_string(s):
+        return s[::-1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
