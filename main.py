8978542c54904c453e14f6fd0fee63d8ab9ae3bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:14:47 +0000

    best morning difficulty weak book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
