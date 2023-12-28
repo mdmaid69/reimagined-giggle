9181381ead9a71ae0c2d98ef1a3f32ab7fdd73ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:57:28 +0000

    giveback plain corner label production

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
