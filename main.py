fb9126025b859569b19b1185fe8f02f56065c8e8
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:09:04 +0000

    answer matter strength ear immediate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
