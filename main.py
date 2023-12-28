a18e194364057d9f94ae7044ec79e651790e725e
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:20:33 +0000

    pocket guess listenfor alive level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import os
+def get_current_working_directory():
+        return os.getcwd()
