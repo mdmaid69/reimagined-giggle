b91b2cc2361ccdee2c562bc5ff8053ded9229869
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:11:09 +0000

    bye cook shout his tend

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
