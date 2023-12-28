5d61e434d09ccfdb2aa4732aea611849add76bb7
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:50:29 +0000

    here onto our danger question

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def convert_to_binary(n):
+        return bin(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
