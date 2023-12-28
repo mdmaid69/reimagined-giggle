5aae5b117ba64e2fdbca3ac1db8d8e6ccbf9d06f
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:56:26 +0000

    principle worse sore across legal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import sys
-def exit_program():
-        sys.exit()
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
