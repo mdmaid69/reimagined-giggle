1e1c66d54432951b3d8627d9fda4375151f6c38b
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:41:48 +0000

    please knock themselves Uu position

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
