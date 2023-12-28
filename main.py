87fb4188dcf859be671cad25e183bddc158f0fdb
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:21:23 +0000

    figure tidy soak label add

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_array_length(array):
+        return len(array)
