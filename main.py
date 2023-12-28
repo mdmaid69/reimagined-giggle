eafbe531a07bb62c37af97b29cfba940c7beb96c
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:51:47 +0000

    country straight spread goup evidence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import math
-print(math.pi)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
