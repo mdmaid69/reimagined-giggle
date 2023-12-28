a6a6ca975b01fd13ed680f6f94dc7b18da26adfc
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:18:06 +0000

    takeout low breakin book disagree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
 import math
-def calculate_sine(x):
-        return math.sin(x)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
