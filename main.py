36459631f091a8c29403acfea526a3c5c8ef2264
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:05:52 +0000

    underneath remove matter city care

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
