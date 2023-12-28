f4a5f42778a6ce78a18a6675617bfb5152d3041a
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:43:10 +0000

    deliberately damp nor miss crime

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def sort_list(lst):
+        return sorted(lst)
