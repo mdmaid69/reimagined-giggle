8c0dc81ed193e98615f7d900983eab1c770bd28a
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:36:56 +0000

    must pollution naturally find process

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
