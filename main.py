1664d21fb176595f035cb2bf8a8a8bebba57b165
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:03:13 +0000

    sea say ago influence thing

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
