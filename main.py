b6f6e10b35e0de5faaef469385a7d5d6c447e24a
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:08:49 +0000

    first famous impressive last front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
