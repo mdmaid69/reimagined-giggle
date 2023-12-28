fffb53b5d5e2a0235e900b2d000c96c0912ffca7
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:49:31 +0000

    your tidy account name finger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def print_python_version():
+        return sys.version
