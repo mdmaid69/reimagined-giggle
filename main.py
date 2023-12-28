b812f6dc9adcf079d370e332239ad34d5ae91cc1
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:24:44 +0000

    example wing own across slight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import time
+def get_current_time():
+        return time.ctime()
