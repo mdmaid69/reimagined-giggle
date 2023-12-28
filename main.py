a57360d7f2c98c755c79b80a29fb6d31bcd30957
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:28:10 +0000

    suck influence recovery shock burst

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def convert_to_hex(n):
-        return hex(n)
+  def convert_to_octal(n):
+        return oct(n)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
