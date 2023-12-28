526d41571263ffa59ab69b7e4cb0a2ab98ea4d2d
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:17:00 +0000

    bound reject acceptable well nurse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
