f5b7dcff0ab66015fb292b4b82571dfd5f435259
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:42:21 +0000

    wide note minority with rank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def multiply_numbers(x, y):
+        return x * y
