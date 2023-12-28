edc625519df36b3c32597864b7cd116c485cf1db
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:53:39 +0000

    owe view spot letter little

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
