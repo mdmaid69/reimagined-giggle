a80dc94cf5bec53edf5e1edc914955bfcf74017d
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:25:51 +0000

    worth in condition worry lead

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
