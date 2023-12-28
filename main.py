d5d716289f6e3be64e21538c08b2a7460a556952
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:22:04 +0000

    Ss nose cheek high little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
