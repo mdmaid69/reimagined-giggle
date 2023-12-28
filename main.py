d1be1aa4684fa7d44294eb88710ef450a071a758
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:33:47 +0000

    label fit wind none file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 text = "Hello, world!"
-print("Characters:", len(text))
+print("Is palindrome:", text == text[::-1])
