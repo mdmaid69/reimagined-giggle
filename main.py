596bd3ae8789eda8f42dec6d7bd2e8d1389a08fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:34:53 +0000

    threaten open thumb pile information

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def append_to_array(array, item):
+        array.append(item)
