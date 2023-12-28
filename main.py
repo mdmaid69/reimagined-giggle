e72d8a19dc3bb7afd17a6eaf2739cb6a31d37fe5
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:28:23 +0000

    scream interfere official report comefrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
