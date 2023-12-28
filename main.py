f0c71b22a661d7dcf5363cb795a8e701bcc60fa7
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:17:30 +0000

    whistle learn build smooth difference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def count_elements(lst):
+        return len(lst)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
