61aabe37aae6c5aa861fbb6366e926fa24022c7c
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:19:55 +0000

    runaway to year light minority

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
