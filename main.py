f9451d0e51841f9f4b2e6c179d61858ca3ff0243
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:08:43 +0000

    scale chief equipment may dangerous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def count_elements(lst):
+        return len(lst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
