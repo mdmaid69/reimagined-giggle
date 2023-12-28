eda02447f17a2bde232d6613f12c4c65418c40b9
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:26:37 +0000

    really copy century average record

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
