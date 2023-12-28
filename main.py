39e9e8a4d3bdd7f56bbe865b5ee2242d9db9c971
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:52:20 +0000

    embarrassing makeinto independent passenger obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def convert_array_to_list(array):
+        return array.tolist()
