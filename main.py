440a4522057ae261043e14f6405d2773b413d1e0
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:43:53 +0000

    early through favourite takeaway pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import collections
-def create_user_string():
-        return collections.UserString()
+import time
+print(time.time())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
