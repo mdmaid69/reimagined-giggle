ee983b67dd402a7a6e0c5bd9d22c6ebd7b2efa27
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:03:25 +0000

    sister since kind programme keepoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_item(array, i):
+        return array[i]
+def calculate_work(force, distance):
+        return force * distance
