b853b5764a16dc194319f841478074f0bcc65475
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:25:26 +0000

    drink beneath conversation failure sail

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
