ddbfb162088fc56c7b311e7fba1f3fed3f4347c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:21:31 +0000

    handle pleasure uncomfortable force belong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
