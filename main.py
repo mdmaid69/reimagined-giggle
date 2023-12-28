aa8268292f8e089abab17d7c77a93984c77bbfd0
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:17:11 +0000

    total expression message boil square

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print("Hello, world!")
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
