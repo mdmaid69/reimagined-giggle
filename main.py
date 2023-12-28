6a90c918d642df21972a019d98539ac789c7ecb3
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:48:08 +0000

    declare peace careful disturbing wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
