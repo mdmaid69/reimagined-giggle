67911637b79c89e898d4f621bf7519289af52f85
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:00:46 +0000

    569enjoy sky quantity sea free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
