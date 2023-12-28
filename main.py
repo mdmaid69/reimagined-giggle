07b85f6c84b4b25a933055177d98d4d2e7f4b18e
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:51:36 +0000

    sign love mass over manner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_file(file_name):
+        os.remove(file_name)
