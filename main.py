3bf480015e0635ca536308f11f8e3ba5c6ac57aa
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:57:53 +0000

    shut worse spin properly free

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_power(work, time):
+        return work / time
