28fa63c01a107ff696c6af20017eb97814a17d1b
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:13:41 +0000

    port used obtain 570enjoyable little

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-  def square_number(x):
-        return x**2
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
