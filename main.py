c0ec52d3cb3f0294b575b690f2682a4316d88571
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:43:34 +0000

    destroy lie odd estimate owe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
