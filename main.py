fb2aee21c4ec9efa3cf59143de89c35ab8dcec39
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:20:22 +0000

    mistake thick fit smart balance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
