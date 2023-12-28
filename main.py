f7b4a29f19cba8b6d197aa29c7af2c310da34593
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:32:46 +0000

    steam someone tune to anxious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_min(lst):
+        return min(lst)
+def convert_to_binary(n):
+        return bin(n)
