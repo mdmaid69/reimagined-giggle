e83bad89ecdef463f4bdbb7269cdce8a509867ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:50:12 +0000

    swing film carry careless mad

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
