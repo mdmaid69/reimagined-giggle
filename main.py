7eaa1949315b9502edb7d0843546e43cd9d4601b
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:28:29 +0000

    spread cost factory breathe impressive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
