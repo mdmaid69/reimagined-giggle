a5439686c62865c8ee30e72cb7e1aa6d8e0f71aa
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:58:18 +0000

    image container expect compete anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_area_circle(r):
+        return 3.14 * r**2
