36d687b5e0828b67f637c837217e13fcc521037b
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:52:38 +0000

    creature live that it dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_counter():
+        return collections.Counter()
