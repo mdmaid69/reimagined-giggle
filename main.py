d4bf68655c20a0c3cb076d4c105a8c0ebaf6eb01
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:49:37 +0000

    bend theirs inquire even when

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
