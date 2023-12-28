1b0a8fc8088c350ab2796499140c7a8b636f1be5
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:40:37 +0000

    certain equal next bound of

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
