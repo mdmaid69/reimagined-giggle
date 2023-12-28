76119dfc37b52e92ea2e94f3880fb2f8ae84e0f2
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:52:20 +0000

    intelligent Jj branch debt thanks

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
