a30bc48559ad8f48088d8447dcba64bcb55196c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:58:21 +0000

    mass duty remove length hold

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
