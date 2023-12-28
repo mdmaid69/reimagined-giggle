746e628fa98246fab9fb502596603b7a03f28531
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:46:54 +0000

    turnon mysterious contrast not discussion

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-print(sum(range(10)))
+text = "Hello, world!"
+print("Characters:", len(text))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
