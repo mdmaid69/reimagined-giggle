1117dcf994a67d735928dd849f587d4a5356568d
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:49:13 +0000

    protect division impatient pair objection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def set_array_item(array, i, item):
+        array[i] = item
