9bbd7ae2f8a4335000c211ac9d39b0ee5f40fff0
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:45:50 +0000

    sick swallow opportunity believe comeoff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def convert_to_binary(n):
+        return bin(n)
