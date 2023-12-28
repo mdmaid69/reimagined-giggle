b52d86f61ca522d812f867d5b4eea71e6b87275f
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:24:38 +0000

    repeat advanced memory trust grateful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
