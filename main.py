c7420157ccf2661d4524ec75871d1c95c98856d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:29:07 +0000

    pretty about physical slice lookafter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_min(numbers):
+        return min(numbers)
