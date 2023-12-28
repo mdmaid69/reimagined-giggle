cf4c7891423b573c6f50ca185e593e8a566a56d2
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:38:55 +0000

    section circumstance then appear rarely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import re
+print(re.match("h.*o", "hello world"))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
