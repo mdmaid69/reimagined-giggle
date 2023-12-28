b85132e7c4ba025a6cdcb24fd9aaf0bc737e1907
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:12:36 +0000

    door lucky fire commit lead

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
