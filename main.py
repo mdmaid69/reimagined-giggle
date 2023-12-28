149a2e3c6c4d784257a898388c2df14cdc810081
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:09:19 +0000

    pump me lock favourite community

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+print(math.pi)
