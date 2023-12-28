1cc737cce9719aa2214c170b00406e111630dcf1
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:20:24 +0000

    shop kind relief wander determined

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_max(numbers):
+        return max(numbers)
