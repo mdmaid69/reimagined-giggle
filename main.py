8bbcbe574cc88b9cb042296808cb43d449d34ab8
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:28:46 +0000

    birth month money occasion drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
