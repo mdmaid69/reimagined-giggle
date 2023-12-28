76a9db4dd5b0243de84390ecb1cc87573c9755bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:58:58 +0000

    sea extremely 179bite shelter breakin

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
