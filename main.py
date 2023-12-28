95a2c2598defdabd7260991af600e23603a7d2ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:18:52 +0000

    market live education share recover

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
