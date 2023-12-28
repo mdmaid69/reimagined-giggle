934d2071519b04ccf6b966fefc70573df15cb723
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:44:22 +0000

    Zz reason 565end amount sense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
