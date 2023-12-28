d9ca404b3f2d6fc1b55d166726afcd5bf177b08c
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:37:27 +0000

    journey unfortunate safe minority free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_max(lst):
-        return max(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
