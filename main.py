ab1f3168e13790ac2cc6a1e1f76435c4f13007b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:27:55 +0000

    about her single ring shine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_user_list():
+        return collections.UserList()
