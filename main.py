961ac57e5e6668c56a558be4640ba9aa5aea9996
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:01:31 +0000

    skill godown runinto rise joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
