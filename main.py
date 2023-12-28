958a123476f262b31f6b664fc08a5294ea011460
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:33:37 +0000

    bound join callup he consult

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def remove_duplicates(lst):
+        return list(set(lst))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
