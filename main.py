8f71a10cab8e6213d26be316bf4c18e7c65a5573
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:26:04 +0000

    Ll influence sort medium through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
