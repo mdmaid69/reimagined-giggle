d67b1f149df02e338ebd2c3cbfae0f70ae0f0839
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:04:10 +0000

    correct union sensible problem lay

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import random
-print(random.randint(0, 100))
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
