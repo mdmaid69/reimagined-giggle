89e434de0616cfdc1998364d8f50535bde865112
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:18:42 +0000

    side instant fun bringdown only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
