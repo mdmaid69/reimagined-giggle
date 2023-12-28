00b03d91bb81c98a4e4dfea672bd7f19d86c6b38
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:28:49 +0000

    congratulate trial shine term complain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import pandas as pd
   def read_excel_file(file_name):
         return pd.read_excel(file_name)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
