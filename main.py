67a8052289e3b09684378792d346d814f4e34d05
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:53:23 +0000

    command waste have become call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
