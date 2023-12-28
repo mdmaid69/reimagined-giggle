0612cb7e0bdbebb5197c23fa72b3ca4a6aa4b0ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:10:17 +0000

    shoot risk order maybe clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+print(math.pi)
