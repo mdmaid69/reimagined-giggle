4adf973836334ae8f860a1452ecab7517f937c30
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:27:59 +0000

    meeting considering immediately 572enter thin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_item(array, i):
+        return array[i]
