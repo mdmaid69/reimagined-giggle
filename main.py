513284b7b34b935bdb6b71cd32e0a922eb22df66
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:36:17 +0000

    between advanced similar point danger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_queue():
+        return collections.deque()
