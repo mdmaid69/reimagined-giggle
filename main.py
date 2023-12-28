8e2ff1ff11c09a20fb2269f8f372dfa1b171e613
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:52:45 +0000

    choose black present he broken

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import collections
+def create_user_string():
+        return collections.UserString()
