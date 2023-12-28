33c8bf1379443565cf209053e307cfaf5685c0ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:53:49 +0000

    centre possible feeling front on

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   def square_number(x):
         return x**2
+import sys
+def exit_program():
+        sys.exit()
