85f38a295a18f603b7aa8287a9396e3902a95a07
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:12:52 +0000

    round dig push afternoon proper

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
