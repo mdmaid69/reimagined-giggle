c7b07036793835fbaa7e76175371a3c6eb205b4d
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:52:45 +0000

    very considering nice muscle criticize

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
