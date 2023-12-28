6b95e663752a3ce5db4cd80ff622d0ec743ed6e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:03:36 +0000

    lot come waste nice spring

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print("Hello, world!")
