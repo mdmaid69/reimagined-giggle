2b85e9ee4480fbed5582e37350929f6b6d429026
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:03:05 +0000

    water reply scared treat sort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
