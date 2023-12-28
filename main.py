865aa781ce608667548a93bba38daa3ea6bae257
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:15:29 +0000

    attitude form onto physical necessary

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
