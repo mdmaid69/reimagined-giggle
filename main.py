6a93c8832ce1d102a0c40399bd9a047e3bd79e5b
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:30:56 +0000

    approval hurt show date range

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
