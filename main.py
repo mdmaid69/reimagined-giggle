843ffbb34094b090b10dc0ee37b721b4a8261bf2
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:20:20 +0000

    change window somehow feeling that

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_power(work, time):
+        return work / time
