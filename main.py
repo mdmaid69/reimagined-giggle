d33418984ae507945dd199cbb06ab88459739ee8
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:37:05 +0000

    whether by holdout dig fear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_area(radius):
+        return 3.14 * radius * radius
