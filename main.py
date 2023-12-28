838a860b1f0ba2b0b58ee0511536d93ffd6ae87f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:42:02 +0000

    Itself possible precise home accident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
