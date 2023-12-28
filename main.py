5d525d06a7cfb8801cd9a570556826113f96a1c3
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:23:34 +0000

    write anxious cheque advise over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
