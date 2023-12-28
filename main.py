37dbf428fb6ab0e5971110793970b2c8f707e1b7
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:08:06 +0000

    burn allow public slice breakinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
