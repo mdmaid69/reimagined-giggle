050a2062afbf54816c3c284f6577401763ed234e
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:17:35 +0000

    sale satisfactory make somebody rush

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
