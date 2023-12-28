25f4c80eefc934583543ae150d429ab58a3a56d5
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:08:11 +0000

    adjective once load middle description

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
