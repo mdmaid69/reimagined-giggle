9c8d69b40a957aa7cdb540420e062ab942b730cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:51:59 +0000

    pity among strange blind influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import array
+def get_array_as_float(array):
+        return float(array[0])
