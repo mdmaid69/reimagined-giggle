1a3d5b2f63d3d19219535fcfc3d3fd5eaf6fade4
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:45:50 +0000

    feeling rush occasion through Ee

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
