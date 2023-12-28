a78e7612ecd54c7d2ac619b9a2d4291c42960bf3
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:44:15 +0000

    centimetre join pretty develop disturbing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
