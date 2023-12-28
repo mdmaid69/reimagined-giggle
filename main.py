7c70270d3cd8c5e5a3d96ace625da355bf53be67
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:44:11 +0000

    change if smell hide lean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def remove_from_array(array, item):
+        array.remove(item)
+import os
+def get_current_working_directory():
+        return os.getcwd()
