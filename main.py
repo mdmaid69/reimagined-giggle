41000cfa219a4136dbdb001284d7fbb82bf37484
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:26:42 +0000

    practical control cope make tradition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
