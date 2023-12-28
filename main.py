a3e0d2a2da5d675f8094e01a2d8cbe4ea4db9d0e
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:56:18 +0000

    giveup ring bored happy act

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
