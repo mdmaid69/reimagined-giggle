aab47ebf343fb52326e7d6145a9dc13648a25e55
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:21:27 +0000

    base within reply rid behave

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
