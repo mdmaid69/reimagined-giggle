7e76f1a6abe9bdf6d330ce332ce608c62cacca08
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:45:44 +0000

    charge give smile laugh show

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
