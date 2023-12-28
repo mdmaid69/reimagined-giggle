04f8f18653c148630583cfb8137b713c7a6de940
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:39:36 +0000

    office completely how while help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
