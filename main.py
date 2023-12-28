90e4b3f4e32ccc6b666e07f273d1f6093206f482
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:41:45 +0000

    pass 567enemy sink pour juice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def sort_numbers(numbers):
-        return sorted(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_density(mass, volume):
+        return mass / volume
