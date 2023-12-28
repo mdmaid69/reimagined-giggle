386136f5e8dc580f080451c1afaf86be253923c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:10:43 +0000

    fail common careless original road

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def read_from_json(json_string):
+        return json.loads(json_string)
+  def cube_number(x):
+        return x**3
