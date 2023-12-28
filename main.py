319852102c3cff88bd96d6eca9cf26666c7b24f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:56:42 +0000

    tough share temperature callout suggest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_as_set(array):
+        return set(array)
+def add_numbers(x, y):
+        return x + y
