a03919f39983777ba75ac40d26442867ce543936
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:37:02 +0000

    house opinion load trap thank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
