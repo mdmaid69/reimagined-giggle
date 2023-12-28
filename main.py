abc0be38ae5c9a32656056aa5dc78aaa7de8d904
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:00:40 +0000

    properly house approve tight physical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-import platform
-def get_python_version():
-        return platform.python_version()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
