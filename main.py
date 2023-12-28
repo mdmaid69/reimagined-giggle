9534a95cba4fbba441e439a020a23fa6c71b0144
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:43:29 +0000

    easy frightened tip see pressure

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_list_from_array(array):
+        return array.tolist()
