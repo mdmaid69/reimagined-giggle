1d81bb9578d0e9d75818bbe06b051fe1f8d89676
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:35:00 +0000

    properly software well occasion Ww

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
