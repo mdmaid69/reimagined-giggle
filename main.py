8d3efdd330f4baee5e488296621ddd5db8b4dbf9
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:06:33 +0000

    female search treatment still broad

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
