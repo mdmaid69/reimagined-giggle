3e02376848312538eba6b6aafe34bd0eb4cedd22
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:18:44 +0000

    day reason considering write violence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def cube_number(x):
+        return x**3
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
