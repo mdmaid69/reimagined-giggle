0f5d505d5a4bec59acb4aa79a0cd6994f3649192
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:47:44 +0000

    loose dry house except lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
