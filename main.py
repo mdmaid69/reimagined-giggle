0d96dc41ea9a98652025b9920c7991112c0a489f
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:50:30 +0000

    warning old wish everybody repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def remove_from_array(array, item):
+        array.remove(item)
