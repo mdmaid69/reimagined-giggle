06a515eb9d04f252062d3e069930a90b10898eeb
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:10:33 +0000

    eager speech true stamp fortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
