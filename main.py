2ec6690f621390935e953fdf977b72b76f5407f1
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:02:50 +0000

    sick born study taste risk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
