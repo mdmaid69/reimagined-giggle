e39e6bd5a5241e712e473e79d2769e4e01baaef9
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:46:00 +0000

    disappear only direct of cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
