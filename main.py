500261cb9d6a7813409d7833d5f79f6981c71890
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:09:17 +0000

    difference ordinary bury basically plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+print(json.dumps({"name": "John", "age": 30}))
