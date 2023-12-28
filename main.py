4788a1eec99629288440d71d6548cf7550a5d86a
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:06:11 +0000

    description sad 567enemy something low

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
