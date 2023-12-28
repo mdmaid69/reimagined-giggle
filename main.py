232461a7491143e602af25644823fbc66eff1a75
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:16:21 +0000

    passage sensitive inside level motion

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def is_odd(n):
+        return n % 2 != 0
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
