72c8094f28821fef5e50b83594ee625007728a65
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:02:46 +0000

    ourselves allow smile too comfortable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
