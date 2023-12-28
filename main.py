5f9b79243b6cf6f3600c797d961d084a1c6f517d
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:08:18 +0000

    Uu before hear summer scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def append_to_array(array, item):
+        array.append(item)
