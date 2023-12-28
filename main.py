f79831093cdf89d55697c766d115155417aeb05d
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:25:43 +0000

    throughout edge maximum offer crisis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
