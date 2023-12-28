38ced246353fdf01e0d974d83e24c5bd6384912d
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:45:42 +0000

    cough to important actually exact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import random
+print(random.randint(0, 100))
