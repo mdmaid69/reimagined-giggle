a329886f17b09a7a6a14f8768e548d2efe2667c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:27:15 +0000

    risk thanks our past attractive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
