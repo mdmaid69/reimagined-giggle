f4d6b914a96b26860eda07622c3058781d0ddb8e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:00:03 +0000

    education delight until mine relation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_repr(array):
+        return repr(array)
