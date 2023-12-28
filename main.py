1581a41cce11a121c9c01e5cc8eb9ac7f043a2bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:03:10 +0000

    health embarrassing turnup there gooff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
