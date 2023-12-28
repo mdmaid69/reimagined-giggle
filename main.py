7329eb9a6db211b5e9c88e8ea7c11410493081df
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:37:07 +0000

    organization surprised your welcome cruelty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+text = "Hello, world!"
+print("Uppercase:", text.upper())
