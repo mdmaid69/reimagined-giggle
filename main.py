89ab1e59e090c59f928d3c63aff33264555bc6f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:50:23 +0000

    comment expensive cruel getback language

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
