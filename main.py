2dd52e48c9e3594c2a815a7c38c67636db5826db
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:17:10 +0000

    quit ignore final bury level

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
