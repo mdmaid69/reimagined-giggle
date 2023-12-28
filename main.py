6bc0a41d468bcde5d8417269e29823ad077909c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:37:20 +0000

    contrast machine have say recover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
