712cbe4d61c0623c85be6035ebd488170e42d24a
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:45:55 +0000

    his force right eye ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
