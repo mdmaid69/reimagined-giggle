48e0bdabf656fcb7d7f56868b14023cdae3a3359
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:35:41 +0000

    hate environment mystery line appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
