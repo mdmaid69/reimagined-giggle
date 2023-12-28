ef21b373d1f8a102a32bcc2a31b24c68a6c936d2
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:38:19 +0000

    rely conduct turnon guard keepoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import tensorflow as tf
+print(tf.__version__)
