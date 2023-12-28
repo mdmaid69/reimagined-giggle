5808c386eb9c959a4343a64385e500b8e67e3d42
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:04:05 +0000

    disturb arrive face centimetre prove

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
