18882f77124ecc7952480beff6131ca3b047504c
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:04:15 +0000

    better promise feel noone everybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
