9c5ea68e1ae58881fe1eb1416df08f400968cb87
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:22:26 +0000

    almost underneath neither any breakup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_binary(n):
+        return bin(n)
