87ebfab7ef88e8c778e15b8ef8ab77c1e504bb41
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:33:05 +0000

    turninto have matter performance ear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
