26b03b91a12265d4f1c94721f5ff21143868e9b6
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:11:50 +0000

    close somehow twist range professional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-print(math.pi)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import re
+print(re.match("h.*o", "hello world"))
