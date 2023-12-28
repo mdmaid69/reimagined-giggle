838aba7b32bac529e8091a681bd382a5a37f1ce0
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:29:11 +0000

    price walk smell responsible material

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def is_even(n):
-        return n % 2 == 0
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+for i in range(10): print(i)
