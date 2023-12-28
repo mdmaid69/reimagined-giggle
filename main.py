70e56bfdb74ef818e115f8df73307665d8e8cb5d
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:32:30 +0000

    Rr Oo see along fond

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-print(sum(range(10)))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
