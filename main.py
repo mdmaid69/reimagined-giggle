1f8fdf77a329db618e6f3629eca0c7a52bee0970
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:12:29 +0000

    married callat later land oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
