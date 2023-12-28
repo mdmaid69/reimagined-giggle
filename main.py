274ce60a9129e8e63fc38b38fdcee7f787bd9b32
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:15:38 +0000

    lookaround fail towards both justice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_array_as_bool(array):
+        return bool(array)
