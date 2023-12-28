4cd8d16e419284e0df40caf77dfd49bbb8873686
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:56:08 +0000

    sink into near coffee trick

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
