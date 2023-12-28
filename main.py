a37a24de0b5d42e04f9d097288f531088e3d3609
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:54:42 +0000

    work cause exchange majority final

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import random
-print(random.randint(0, 100))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
