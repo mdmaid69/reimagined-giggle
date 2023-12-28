5b46314e7c2787a3edf30bea863d79ec64e42bcc
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:32:09 +0000

    slice church direction comedown active

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def remove_duplicates(lst):
+        return list(set(lst))
