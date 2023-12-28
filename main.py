8df3abe0c77eb41379c197602c040e9d4350bb37
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:57:35 +0000

    eager public software art mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
