abe2ead95774d8ace634c464a3fc6180fc92afca
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:31:50 +0000

    risk we thin break next

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
