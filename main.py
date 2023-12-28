89dd9c509765d151e3d7f85aac247fc2710bb141
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:50:47 +0000

    whistle herself ordinary whisper flavour

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
