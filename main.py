728add248eec35de10ae1bf068f9c679f397c87b
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:26:16 +0000

    trust reduce frame right moon

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
