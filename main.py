7419a92ffcfe1cb7822b72b33f4aadc13c71041c
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:09:07 +0000

    whether strength smile public can

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def cube_number(x):
-        return x**3
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
