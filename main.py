335e8cadd77602beaa3b39405ab42be29977cc9f
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:52:06 +0000

    name involve beat obtain hair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+print(math.pi)
