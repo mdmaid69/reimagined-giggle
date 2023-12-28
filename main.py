0a770fde53f651abad5747a06561b03c667ccb07
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:38:12 +0000

    genuine opposite wash rule sweep

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def square_number(x):
+        return x**2
