b360055d4c05333aa3dd7b390a4ae3bdf877890f
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:29:11 +0000

    tell trap snow inside die

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
