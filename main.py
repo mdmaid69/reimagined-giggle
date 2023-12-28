88d0b3f40eb146a896dd522854a07adde99089f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:11:16 +0000

    present plan successful 570enjoyable meeting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
