6f5332b4cdb89d01196333f1c1f386b5ebfa791f
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:55:46 +0000

    giveaway meaning disgusting pool pleasant

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def count_words(sentence):
+        return len(sentence.split())
