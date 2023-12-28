610fe1a41cc588691e6cc5b9fc33c793b04dc241
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:33:57 +0000

    culture moral maintain give emergency

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
