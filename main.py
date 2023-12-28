5ad43aaf263b4997542400bbd9b4af32c0413423
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:13:54 +0000

    issue indeed cap how bet

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+print([x**2 for x in range(10)])
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
