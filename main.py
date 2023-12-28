984e1900a40496ba6e428e4edd6bf7989db03291
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:09:48 +0000

    set land ignore sense again

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_palindrome(s):
-        return s == s[::-1]
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
