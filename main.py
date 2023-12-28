3b7e014b286b5484974bbd2ad2405110de21b558
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:23:40 +0000

    perhaps mine bush realize shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
