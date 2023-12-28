87b6bbc4f86d2a8825aacb6cfb833a1edb7b0238
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:53:24 +0000

    write worth still drive explain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
