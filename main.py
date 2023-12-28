8cb6383e1b6b8ee864e000dde8d11a014417e072
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:29:50 +0000

    tidy valuable failure themselves stage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-print(math.pi)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
