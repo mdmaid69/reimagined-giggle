33dfc85ba714ae3474b73aa6c021d63c6391b165
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:24:19 +0000

    dull display tell contain sport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def find_max(numbers):
+        return max(numbers)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
