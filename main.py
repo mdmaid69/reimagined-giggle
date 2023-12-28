7fb74e85accebef7315aeedff940a0d673407fd8
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:12:35 +0000

    repeat fill border part to

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import re
-print(re.match("h.*o", "hello world"))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
