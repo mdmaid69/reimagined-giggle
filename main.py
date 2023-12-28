9b08246e27d3221931b2e797b798aa60645087ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:30:56 +0000

    giveback backwards disagree complain release

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
 print("Common elements:", set(list1) & set(list2))
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import os
+def remove_directory(path):
+        os.rmdir(path)
