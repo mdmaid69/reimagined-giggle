5b56512f019730c59a6d7057d902e3b96edec8cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:53:39 +0000

    line sorry avoid match spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import os
+def get_current_working_directory():
+        return os.getcwd()
