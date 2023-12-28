491a80a97ff0ce3e7b0f3a7b161f207546808ca3
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:38:47 +0000

    as deaf home nurse wet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
