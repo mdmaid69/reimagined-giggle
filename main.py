0c1652daa8fafe711940bed602367217293f8fda
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:17:56 +0000

    bin shallow crime stand forever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
