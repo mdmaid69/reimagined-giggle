b58171b431c9c4d470ac3e9531db536789a9089f
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:26:06 +0000

    dangerous ride brush wind they

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def convert_to_binary(n):
-        return bin(n)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
