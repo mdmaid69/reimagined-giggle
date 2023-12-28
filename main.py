682be57915a7d59ac9eaed21485775e5e877b207
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:22:28 +0000

    rub laugh willing depend tough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
