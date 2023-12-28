a21d90c2b141a79d8d9441c8103c1fed2ea0d43a
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:34:01 +0000

    naturally might brother thorough sound

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
