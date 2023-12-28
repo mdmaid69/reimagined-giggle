7914f653e1972c8a964b20b203e0bb49d2abf3ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:11:20 +0000

    afford division stamp runinto shout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
