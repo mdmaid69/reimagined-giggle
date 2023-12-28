19a867fb769335afbbce8af277f9ecfd5d35064a
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:12:55 +0000

    contact hardly keepaway table centimetre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_int(array):
+        return int(array[0])
