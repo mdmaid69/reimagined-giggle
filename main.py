16d262c2591e22be4d2d21533f1669874575f417
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:09:52 +0000

    attitude colour pressure Ss pleasant

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
