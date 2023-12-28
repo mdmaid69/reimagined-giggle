bf498c37a82db0e0cba60cac1e2f0ad5e4f11fba
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:51:21 +0000

    smile shake tear this blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def reverse_array(array):
-        array.reverse()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
