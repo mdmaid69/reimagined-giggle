703e2a4065bf85e5eeb0a468ad9317d69255cd77
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:42:28 +0000

    whistle earth underneath discover air

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_pressure(force, area):
+        return force / area
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
