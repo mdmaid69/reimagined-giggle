0c5d53902ba49b2d594c038995080b46659c9926
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:03:14 +0000

    once unable measure threaten period

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
