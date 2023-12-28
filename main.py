5506e2bb2bd18242c408b5886f2f7e560c012b88
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:22:08 +0000

    shake accidental single steep tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import collections
+def create_priority_queue():
+        return collections.deque()
