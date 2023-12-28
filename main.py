91526c8ce2a1730b27e755c6c7fca3fb5547bcf0
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:34:31 +0000

    comeback represent feeling tie possibility

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+print(sys.version)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
