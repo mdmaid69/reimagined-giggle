3ee4b5f959314f8fc42cbfab12bf537258b36dcd
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:38:22 +0000

    threaten official feel top flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
