a17764a603cbdeddc29e47be4638ae89a450cd9c
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:15:43 +0000

    demand otherwise swing discussion change

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
