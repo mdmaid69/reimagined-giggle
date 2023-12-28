80f75cc97550af20d317e8c8fdc2cb5bb1356eaa
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:04:10 +0000

    tragedy work tap drink Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
