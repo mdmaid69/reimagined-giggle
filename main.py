87bd405673f77261225939deba3519cb5d134ab1
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:09:07 +0000

    unkind close practice 568energy lost

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def find_unique_words(sentence):
+        return set(sentence.split())
+import sys
+def exit_program():
+        sys.exit()
