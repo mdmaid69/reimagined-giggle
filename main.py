1c2ccbda9fc6a25d663862e08da434607e049b54
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:58:37 +0000

    cost offence he contain less

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
 import random
 def roll_die():
         return random.randint(1, 6)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
