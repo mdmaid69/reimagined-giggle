5bca9cfc4813c473ca832c4386bdebd2584bd5e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:14:17 +0000

    callat approve colour pile follow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def add_numbers(x, y):
+        return x + y
