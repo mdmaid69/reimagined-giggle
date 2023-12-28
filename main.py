1e2fbaf4bd3b4f38474d236910e7bb78ce4ba6f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:46:47 +0000

    communicate giveup patience sign service

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
