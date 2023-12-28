2db904b678fda3fab9e9a5285f64381c79abde97
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:15:20 +0000

    free heat question since run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_directory():
+        return os.getcwd()
