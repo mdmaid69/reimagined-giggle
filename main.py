116c8665bea9fc4ff8a1e45d81873b55dad1cea3
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:01:13 +0000

    persuade he married unusual importance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def greet(name):
-        print(f"Hello, {name}!")
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def sort_numbers(numbers):
+        return sorted(numbers)
