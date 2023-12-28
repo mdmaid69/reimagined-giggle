3ac5fd8c795d3126607894fa3dedb1766d3b97f2
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:23:31 +0000

    sorry begin service shadow whom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import collections
+def create_counter():
+        return collections.Counter()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
