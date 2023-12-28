9dabeb2eb3ecb1d01f72e14e3f774d746a5d080f
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:22:47 +0000

    owe scare Jj available outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
