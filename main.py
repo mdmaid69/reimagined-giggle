47c8525269d81fbaa047afe8efe11dbfcb6d0a5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:22:40 +0000

    recover voice loss shame play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
