fe577984fd512c63830bbdd77eb19a4a32d85503
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:09:48 +0000

    this sleep post whichever that

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
