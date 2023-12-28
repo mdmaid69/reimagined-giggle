00da07d9cea93622baaa345b2fc95208f14acf47
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:17:44 +0000

    count cum half hard proud

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def greet(name):
+        print(f"Hello, {name}!")
