78c5691691554ab3c5226805e0620d1bdd8dd30d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:44:03 +0000

    modern strength purpose 568energy business

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
