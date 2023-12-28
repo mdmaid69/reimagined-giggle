aea9cf8a20da8b3858c10eb12b544ff0fd15c2d4
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:51:25 +0000

    invitation bringback season face famous

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import numpy as np
+print(np.array([1, 2, 3]))
