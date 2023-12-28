f5a87b47ac5bdd2b395be120bf2cc20871db561f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:21:07 +0000

    into meanwhile discussion history mad

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
