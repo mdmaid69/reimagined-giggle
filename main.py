321fd0ae4924641bb9cf226ef3f7de5312d16ab7
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:12:09 +0000

    breath regard translate lucky interest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
