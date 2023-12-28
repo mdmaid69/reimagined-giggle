4fc91a432464182d823a3acb9a51de490e1bbd4c
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:39:26 +0000

    safe combination conversation turnon top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
