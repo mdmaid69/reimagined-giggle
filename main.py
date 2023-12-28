2d638195fe1bb2348548a7f3f4c0f9533e0390e0
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:21:31 +0000

    invite limit come creature treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_current_working_directory():
+        return os.getcwd()
+name = "Python"
+print("Hello,", name)
