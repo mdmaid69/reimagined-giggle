154fe604a1e7227677aecfda000b229c7b4381e3
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:40:50 +0000

    plan failure stupid all class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
