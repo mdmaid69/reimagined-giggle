32a388e1bd298c22ce17c5e702e4ad8dd4ab297a
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:18:26 +0000

    keepoff deliberate uponon Vv female

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_min(lst):
+        return min(lst)
