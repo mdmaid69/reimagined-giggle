bd3ed5bcf8578199044ad2133fc8aae03651be15
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:22:18 +0000

    education holdon make exactly giveback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
