16040d938c70716ee3b5f77a2555d1ccc38d25b0
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:22:51 +0000

    gas confused year but long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
