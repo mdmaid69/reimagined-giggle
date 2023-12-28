49d5b878fddc1163e99d366697c93ca7dbbad330
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:51:17 +0000

    tear have pretend justice crowd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
