cad8b112da630d69189bd1f2e3efd789a1ad181e
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:50:57 +0000

    Ww order within change wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
