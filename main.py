342245f4a6ceb27a23068c368fb43c5ee3585ad7
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:16:44 +0000

    pain arrange small dear transport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
