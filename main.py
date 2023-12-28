e2e6f54e9045319719f84ebb70fe3d3de9de2e96
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:47:52 +0000

    appeal tense charge anxious address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
