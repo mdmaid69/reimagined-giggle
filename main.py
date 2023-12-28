5139a13d2c5f05a2feb7ee1c5bd5f64e762890f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:34:08 +0000

    odd basic strict mess conversation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
