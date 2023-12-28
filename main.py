2ec04b9471618c5973c9f7dc351266b575ecd53c
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:58:15 +0000

    maximum fix rain full combine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
