37b156c4c3d6f1e7bc61c96fa57bbc6b3795257c
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:59:02 +0000

    mistake passage effort luck drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
