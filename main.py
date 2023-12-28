db539362ac27c60c3ed70833c7bf55486443b7f2
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:53:02 +0000

    maybe goback(to) kind across realize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
