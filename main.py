6047f10f877a79c456719158ce3d16463ae71e79
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:35:37 +0000

    minimum wander situation trust male

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_speed(distance, time):
+        return distance / time
