6655bfcc2f44048d38493e9bf5b1f98c40b3d092
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:36:27 +0000

    willing quality conclusion urgent practise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+print("Hello, world!")
