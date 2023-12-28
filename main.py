4664b5c554362bea440a26b6ad3c853052c094e7
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:20:49 +0000

    model walk stranger as friend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+print(time.time())
