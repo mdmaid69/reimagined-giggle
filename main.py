035525f9e99f9b28ad06e9e396940d7179d96eda
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:43:24 +0000

    considering design hand act cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
