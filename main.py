0f6e38ed36f3bd0fb1ea898bc2fdb0e0bec4a084
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:12:23 +0000

    guilty so dream visit late

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
