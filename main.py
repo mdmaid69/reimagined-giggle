bd186a564e41ffb9622c02e3dbb1d65006140a54
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:34:12 +0000

    mine wipe normal along cap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
