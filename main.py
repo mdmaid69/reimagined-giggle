e25c9f232388fa9bb251174e89c3f918a674c360
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:45:20 +0000

    confusing children ever term plant

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print([x**2 for x in range(10)])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
