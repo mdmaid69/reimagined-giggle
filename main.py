8c7bfc4a0e97d275ec6cac99604f8946357b970b
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:06:49 +0000

    sign secret uniform positive special

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_factorial(n):
+        return math.factorial(n)
