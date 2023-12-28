6e54976578621968aac62720acd784158a187627
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:04:38 +0000

    less holdout service crowd ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
