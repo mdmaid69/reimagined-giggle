0d4460a9dc7ecaae621e0bef7fb320d421d57c9b
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:49:32 +0000

    final nervous afterwards on talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
