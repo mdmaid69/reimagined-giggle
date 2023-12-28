d33e50a4f69dcfd831f919a7abc86972abf7a775
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:56:25 +0000

    breakout part late callat innocent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
