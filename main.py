5d57a2e5241b9c03e6b1976f163823e20a9edc40
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:49:24 +0000

    fit beautiful until mind private

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
