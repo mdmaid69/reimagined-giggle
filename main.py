93127160d20bab797f6d5bed6efb85039727e7ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:57:42 +0000

    trap protest tool steal tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_length(array):
+        return len(array)
