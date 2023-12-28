ff5d4b919ac6f19617a8c5cae175547e705005cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:40:24 +0000

    march waste ought lookup blame

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def square_number(x):
+        return x**2
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
