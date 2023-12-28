02df3c3541b2d9a546ccf8d0e5c5c0284fd86043
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:15:32 +0000

    how cause sink sport wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
