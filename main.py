9e9d223645417d60995915b3dbfba1be47d22dae
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:28:43 +0000

    expensive design state song satisfactory

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def multiply_numbers(x, y):
+        return x * y
