4507e74b9f8c4b44e5540c285385a72fbd8b35fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:25:49 +0000

    single distant strict swing blow

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  def is_odd(n):
+        return n % 2 != 0
