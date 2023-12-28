4f00af34b1004a91107a5f7ffb851bb37765cac0
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:34:58 +0000

    regret cost breakdown quantity push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
