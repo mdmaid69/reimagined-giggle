69431948cbc65f3709242512715a4cce2dd87e4f
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:52:11 +0000

    Ww lucky moon scheme fix

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
