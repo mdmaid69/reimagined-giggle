cbab4850e4a82e25bea4aa3163a9fbd815855a5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:44:50 +0000

    represent maximum across section beautiful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
