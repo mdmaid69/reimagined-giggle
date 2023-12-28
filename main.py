692053407ce84153994314a72645a8de3cf51684
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:03:28 +0000

    within gooff frequent challenge yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def get_array_as_float(array):
+        return float(array[0])
