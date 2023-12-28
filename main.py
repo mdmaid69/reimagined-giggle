55cae330afb4cd703bead5d1759ae4b2f6a977e3
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:14:39 +0000

    rain splash throw taste future

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def reverse_string(s):
+        return s[::-1]
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
