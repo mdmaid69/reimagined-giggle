d976ac2af1441ff40588b132e709705f33d73c2c
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:21:50 +0000

    action how example use speed

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
