aef5027e7f16764790bb2a7c28797140e69c0b98
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:02:02 +0000

    cheque impressive avoid reduce large

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_work(force, distance):
+        return force * distance
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
