0efadc7ca06bf5a7fd3101547410ccbd85567f87
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:39:45 +0000

    among relationship underneath opposite way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
