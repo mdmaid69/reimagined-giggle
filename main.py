7ef8d3f2e88c242a3c05b3b1fc68b1a8f6e0fe3b
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:48:03 +0000

    game wire yourself book left

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def square_number(x):
-        return x**2
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
