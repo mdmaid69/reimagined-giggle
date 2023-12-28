d63eee2183bc8e00834b3f7a85ddc03725de50af
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:53:10 +0000

    past please professional light food

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def convert_to_binary(n):
+        return bin(n)
+import numpy as np
+print(np.array([1, 2, 3]))
