8a374226fb46f14ea73f079d661c654318a39349
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:26:04 +0000

    estimate outside family habit immediate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-print([x**2 for x in range(10)])
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def convert_to_hex(n):
+        return hex(n)
