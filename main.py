ea0bd7f046ae0b7744a1318f1c8c3923977f96f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:09:16 +0000

    top suck silent block cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import datetime
+def get_current_date():
+        return datetime.date.today()
