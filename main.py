aa68209efe26f034c3f70d34981eca4c38fec466
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:38:54 +0000

    sudden Zz clear achievement flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
