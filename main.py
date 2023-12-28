8da22ebcec86fdafb1cfbe5f60c7678a01c82ebc
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:36:48 +0000

    Dd busy repeat noise smell

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def count_elements(lst):
+        return len(lst)
