ab27a555c54c692e8234460257bc5466db02b5e5
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:00:48 +0000

    wave gas band single and

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def get_array_length(array):
+        return len(array)
