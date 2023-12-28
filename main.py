5616516b8699c79f66baa122fbc5a51e710d4193
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:53:28 +0000

    sorry sign insist organize workupto

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def reverse_list(lst):
+        return lst[::-1]
