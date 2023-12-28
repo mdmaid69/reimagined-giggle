6b8cb0ce803cbf2add66dbb968fc42f0c2dfe695
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:19:08 +0000

    every unite mark for follow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
