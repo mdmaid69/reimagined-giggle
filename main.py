5ccd41ffbb2308a007ce720147bdf69a36e29336
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:02:41 +0000

    minimum bang kick hurry please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
