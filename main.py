78293b7ff86fa810d1d955ce28c67fd8a0d106d0
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:27:49 +0000

    petrol offence sleep behind anywhere

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def convert_to_hex(n):
+        return hex(n)
