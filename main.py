d03f08d2b46244a9d8f836055b71490f519128f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:56:46 +0000

    somebody remove set sleep live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
