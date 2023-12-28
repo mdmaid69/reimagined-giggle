480186ee5ac97e1998967bf5f79f204b2834fc17
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:11:09 +0000

    listen actual callat spend truth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
