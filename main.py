c59501a01df94ae7c691830f412a58b104ee53e7
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:03:23 +0000

    excellent risk afraid ever weigh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def square_number(x):
-        return x**2
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
