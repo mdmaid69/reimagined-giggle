a3ddf48d293f804abc0b414bac33e3e927b41e87
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:01:50 +0000

    upset unit have touch develop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
