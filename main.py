0df20e3bc9330470df8ab8f83b06394ef058ac46
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:34:58 +0000

    raw spill also taste roof

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
 import csv
 with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(["spam", "eggs", "bacon", "ham"])
         writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
