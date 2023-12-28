0aec2579243d7eb38d1863bf984e6767e319ec7c
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:09:38 +0000

    round holdout yes love system

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
