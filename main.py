8624a95b87f0f23988181d3d96a4b89e410f3416
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:27:55 +0000

    skirt makeinto its smooth respect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+print(math.pi)
