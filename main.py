ce908a17a1bf40f13b1d6c3bbfaab9b9cffd9271
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:29:15 +0000

    high hate 564encourage run smell

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_work(force, distance):
+        return force * distance
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
