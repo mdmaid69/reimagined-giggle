51cf3f19248f05a1ebbe5dfe24bcc0a7b7c1abb0
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:13:06 +0000

    impossible light war force nurse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
