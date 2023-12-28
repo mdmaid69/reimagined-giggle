29f5f24aec41e396aa5e87bf81408152c1ca04c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:14:34 +0000

    characteristic hate shape wonder spite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_work(force, distance):
+        return force * distance
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
