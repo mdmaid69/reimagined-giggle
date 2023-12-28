2fb5348afaf790e753b5dbdc5719ff20d68753fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:16:00 +0000

    creature use mother save wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
