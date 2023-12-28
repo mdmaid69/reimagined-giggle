724c7a54a4626c795724f9f1952de9983551d6c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:09:51 +0000

    answer for children lead squash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import sys
+  def get_python_version():
+        return sys.version
