889412d91df8f680d28ef23b51b67e0ed592f637
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:03:54 +0000

    complicated independent pick once door

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
