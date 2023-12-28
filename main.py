91ca9e2246c41dbd95f5ed969dc7446adb1b1e04
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:32:42 +0000

    water turnover before glad himself

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
