9806454b314a142cbcd571dfac0aeadb1425e0de
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:34:38 +0000

    trial accidentally cruel challenge even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def insert_into_array(array, i, item):
+        array.insert(i, item)
