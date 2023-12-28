5ae74ce171376a752012fa8f3b6ad92d421591e7
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:15:31 +0000

    blind direct hello pot loss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
