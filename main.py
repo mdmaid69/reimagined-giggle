ce5e86f426fd1bb72536610663031699bd353a9e
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:57:20 +0000

    ashamed keepout cool prefer spare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
