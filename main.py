182110a65cccf8d308a749a1d0767f5aec972c96
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:16:44 +0000

    achieve on still comeup anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def split_path(path):
+        return os.path.split(path)
