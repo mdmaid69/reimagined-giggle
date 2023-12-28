c5ecab78030541aac74eed788297e588e8dc9fd2
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:31:03 +0000

    record flight fairly giveup shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_itemsize(array):
+        return array.itemsize
