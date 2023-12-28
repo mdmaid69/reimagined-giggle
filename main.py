b29fb744020131433d49736e16bc9e828e268b98
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:10:24 +0000

    talk idea comeoff confident switch

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_array_typecode(array):
-        return array.typecode
+print([x**2 for x in range(10)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
