8f2ee02897cd8eecb15655c36a1ea025d17f961a
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:51:05 +0000

    joke however bang familiar character

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_power(work, time):
+        return work / time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
