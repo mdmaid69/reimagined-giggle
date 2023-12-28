8939d6c95a2f402e566a4334b04e529e70c9b427
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:23:48 +0000

    smell bringup check hardly straight

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import platform
+def get_os_info():
+        return platform.uname()
