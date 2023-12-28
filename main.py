2cf8d059cc08d5273b38e0b69c00fe0c0932bc50
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:02:18 +0000

    thick my will Zz attention

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def change_working_directory(path):
+        os.chdir(path)
