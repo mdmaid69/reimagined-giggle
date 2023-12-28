a3420ce6e24c18c4e24db760588b370c0d4d16fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:04:57 +0000

    taste twist flight interfere around

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_int(array):
+        return int(array[0])
