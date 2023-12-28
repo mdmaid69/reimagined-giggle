2e3afda69d429da6f332c787321c4a3565e1b590
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:23:24 +0000

    development content achievement illness cruelty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def convert_to_binary(n):
+        return bin(n)
