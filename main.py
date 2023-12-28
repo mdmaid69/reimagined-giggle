72da72dfab805f301d5dabbd83c418b45e200bdc
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:47:57 +0000

    by common edge visit getout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import sys
+def print_python_version():
+        print(sys.version)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
