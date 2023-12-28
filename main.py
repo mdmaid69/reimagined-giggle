e763ccc590edd4f3f0d3bd12870cfb6fb1db2238
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:33:10 +0000

    print motion uniform will relief

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
