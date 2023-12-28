fe60e62624106c0c4c78f3e4aad2845b7bb93eec
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:47:54 +0000

    exchange peaceful interest thirsty Jj

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-def is_odd(n):
-        return n % 2 != 0
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
