90c3e720ae389b4d1a577700711f47829cf5c126
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:28:58 +0000

    if than twist uncomfortable curious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
