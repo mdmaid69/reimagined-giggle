8223cf2f40fff8e836aec3dd953df5cde1fd4e7f
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:49:42 +0000

    advise gate noise answer persuade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import getpass
+def get_username():
+        return getpass.getuser()
+  def subtract_numbers(x, y):
+        return x - y
