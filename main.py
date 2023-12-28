3d79ced9d555669050ae196962deb727c33b7053
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:33:13 +0000

    let minimum story off responsible

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
