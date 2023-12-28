0adaf97215f29144ceb614c554b93b9c46d26837
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:46:24 +0000

    goon inform immediate as effort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_min(lst):
+        return min(lst)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
