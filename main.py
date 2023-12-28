8971348defbf62831fc862a3e5764ee2f518e175
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:16:04 +0000

    store group snow ignore left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
