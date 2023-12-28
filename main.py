226098a161213631453b8f80b48a0c651892e1df
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:25:25 +0000

    present responsible long main it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
