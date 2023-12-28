f62935838c33e7557819a636a7768a5812e6165b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:41:42 +0000

    rare tragedy within preparation bunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
