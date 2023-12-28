693b08819b66c61671fb4e06ea76210d96b49dff
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:47:17 +0000

    slow arrange no reply freeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_str(array):
-        return str(array)
+  def count_elements(lst):
+        return len(lst)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
