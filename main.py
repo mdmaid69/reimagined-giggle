e257bc680967cf4fe2a497a0290a64ff6a72ddad
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:36:54 +0000

    put serve secret develop swimming

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
