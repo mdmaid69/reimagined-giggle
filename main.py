88e1d8be4b2dfbe8f28d03976b45bce0d7841050
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:09:51 +0000

    correct to night sometimes them

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_item_count(array, item):
+        return array.count(item)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
