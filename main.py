0478c01b9431c321c6ec2aee0cb3bb38ac75c407
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:34:16 +0000

    hole there top whether prevent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
