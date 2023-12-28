fd4bf86df15724e7c653bdf6a58a27f413a81834
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:47:42 +0000

    task only whenever sensitive job

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_list(array):
+        return list(array)
