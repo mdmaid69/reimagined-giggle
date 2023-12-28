87920a9f3310892110a63a88ef59f3e18676a2db
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:49:58 +0000

    beat rise bush history furniture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def get_array_as_bool(array):
+        return bool(array)
