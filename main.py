2f69f794a35eeb333e94ab1a4afb1a350bfdf211
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:32:10 +0000

    person sticky file after throughout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
