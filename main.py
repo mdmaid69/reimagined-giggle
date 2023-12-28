d0f09883fc0ecec8b92e61908fab2fabb7149c30
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:20:00 +0000

    from reason price successful similar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import os
+print(os.getcwd())
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
