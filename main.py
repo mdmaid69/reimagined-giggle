42847d26495a40e7487700e2f1fe5d7b07e9bf4f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:19:46 +0000

    soil glass wall normal apart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
