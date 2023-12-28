0d7984620fc3305905f3a97f485f265af1a932f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:26:28 +0000

    pleasure low disappointed major immediately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def is_even(n):
-        return n % 2 == 0
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
