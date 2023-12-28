41b3ba1652d7289eed00190bfb278c7dcec7f108
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:38:08 +0000

    physical comeout sink closely callat

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
