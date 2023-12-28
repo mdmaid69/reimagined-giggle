861d8681d314f6ccfca4ba6536d453aeca2d6438
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:12:54 +0000

    mixture never age smooth finally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
