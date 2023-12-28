4949bcaf931b972ecdfa0f084915dc5882202d2d
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:53:35 +0000

    tax off smooth separate now

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def cube_number(x):
+        return x**3
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
