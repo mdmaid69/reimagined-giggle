bfe1294066a19f339e6490a8fd8a789bf4be4296
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:36:44 +0000

    mix plenty container to during

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
