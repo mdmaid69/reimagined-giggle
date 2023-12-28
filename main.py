9befc616813a3c04eb521014be1cd0d810154db2
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:08:40 +0000

    lower giveaway concerned hard however

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
