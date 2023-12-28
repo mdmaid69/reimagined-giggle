2d02d4ceb721be1d716415d7cb384af1ee6738a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:29:43 +0000

    decision male cost but save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
