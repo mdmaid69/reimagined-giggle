d8fc27f1e3a02fe98f63eac05b54db78a9c7f3a5
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:06:54 +0000

    ordinary still regularly fat makeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
