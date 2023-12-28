d41da252fb57249f2442b1dac1988304abc9d1a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:32:46 +0000

    exact office delight whisper excuse

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
