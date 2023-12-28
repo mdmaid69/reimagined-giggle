c008564192dae315d3e99eb52ecf787b8e1292da
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:37:30 +0000

    lead plan free unless discuss

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import platform
+def get_python_version():
+        return platform.python_version()
