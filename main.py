b80ffe9b861efa149c5dc446c13425e5776b46cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:28:59 +0000

    important bath heart holdon genuine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import math
+print(math.pi)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
