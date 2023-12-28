8c2953f979eae7fd18457365a2e3bdd3f785651a
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:45:38 +0000

    rid level bread sore wrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
