b6936ea226b48f72eccbcdbbd42009099274c4a5
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:24:21 +0000

    weather Yy wave scared sing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
