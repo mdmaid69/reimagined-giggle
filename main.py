c55e017c6649748926d404a0f5a3df6316a69847
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:13:40 +0000

    leave mine comfort doctor always

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
