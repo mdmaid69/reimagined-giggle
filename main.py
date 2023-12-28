fe6f04249ba68823e4b7a06e029be02983c7d707
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:02:35 +0000

    deserve married Dd worse score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def divide_numbers(x, y):
+        return x / y
