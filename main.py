3e76dd1f7d710a66cda3019d9234a9c6d6271bf3
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:05:48 +0000

    innocent myself do safe one

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
