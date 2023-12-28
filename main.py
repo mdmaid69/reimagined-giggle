fdb8610fc5287e7cfece53980c9ef9992c3aa529
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:23:30 +0000

    rich survive interfere total centimetre

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_unique_words(sentence):
+        return set(sentence.split())
