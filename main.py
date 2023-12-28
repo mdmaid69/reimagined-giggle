e8a9e21619c98b72042ae8076a9e7514558c0c57
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:48:31 +0000

    advantage translate where exactly taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import time
+def get_current_time():
+        return time.time()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
