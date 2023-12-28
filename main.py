98d2e0202b01023b8349aea58f29a3f25cf86480
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:53:37 +0000

    act male neither cinema split

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
