59cd4e75e8b18c8463ed35b82006e180e1b6983c
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:38:26 +0000

    abroad afterwards pass black house

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_max(numbers):
+        return max(numbers)
