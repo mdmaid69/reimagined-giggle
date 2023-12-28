4d5c1ac89e8e73deeda0dab8bbc966ef32385ad3
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:07:22 +0000

    Jj squeeze range sensitive shall

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+x = 10
+y = 20
+print("Sum:", x + y)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
