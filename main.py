a816eed965d69bcdb794fdcd52e4678e4f7f675f
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:21:52 +0000

    future account talkinto boil 566end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
