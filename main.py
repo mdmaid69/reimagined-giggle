44657099000f8dcf98aae1c19eac54d4e9d6d528
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:00:11 +0000

    increase stage crash immediate front

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_octal(n):
-        return oct(n)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
