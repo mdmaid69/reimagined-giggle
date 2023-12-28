1b5b3991e550caf42e3ca39ce250287b7592bd1c
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:22:36 +0000

    tread such maybe taste nose

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
