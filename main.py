7dc82f0bdaca203cac58d498fe5c8dee642fb104
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:09:41 +0000

    rescue shock health rain birthday

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_speed(distance, time):
+        return distance / time
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
