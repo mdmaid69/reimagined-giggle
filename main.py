d2fe1357a0d88040419abb44c2c19cd4f2ca9491
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:59:11 +0000

    borrow lost part last think

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
