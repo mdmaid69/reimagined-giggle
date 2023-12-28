8c13c6a52202105a51f12976a89e7ad43c212575
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:05:02 +0000

    their 566end club reply steady

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_tuple(array):
+        return tuple(array)
