80bb5055e8884165efd12db7b6e4d6402aef92ff
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:23:22 +0000

    pronounce confusing when message after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
