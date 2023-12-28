431865ef0597ddc5239f44a9e66b5e07f75edb8e
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:26:41 +0000

    provide occasional carry ear sharp

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import os
+def change_working_directory(path):
+        os.chdir(path)
