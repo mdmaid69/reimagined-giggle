2f6fabb0481227cc8398751d5ac0d58736b4f7a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:05:32 +0000

    lookback(on) thinkover gate clean bang

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
