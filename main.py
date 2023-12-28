faaec4a23f9c1953a40a25b5fd3a71f40d397eb6
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:33:26 +0000

    goout neat form record path

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_user_dict():
+        return collections.UserDict()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
