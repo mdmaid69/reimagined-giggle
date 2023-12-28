825d65928e98220f467ed2084a2a96d6ec272bb7
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:09:56 +0000

    leave declare read miss over

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
