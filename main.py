1e48272a2f85074d778fe9aff58116d5ed6c52d6
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:55:35 +0000

    piece fortunately unit left measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_as_str(array):
+        return str(array)
