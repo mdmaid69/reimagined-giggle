8d5f975b04a089e48a7e82f88fd991194288bd40
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:49:11 +0000

    provide hurt for tool side

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
