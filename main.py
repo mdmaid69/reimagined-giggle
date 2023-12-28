bc99bbea18f7b40cfbc70e5555eef9c411e3df49
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:08:57 +0000

    time otherwise Kk true process

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
