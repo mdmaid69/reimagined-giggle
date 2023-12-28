34c894a2393c26808cac93ffe3e4cbf510168714
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:00:30 +0000

    more organize aim learn strike

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+print(sum(range(10)))
