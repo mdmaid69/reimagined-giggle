19853b669e7efef144059dd4c6bf7a05c5d099d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:20:19 +0000

    excellent plant knowledge upset influence

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
