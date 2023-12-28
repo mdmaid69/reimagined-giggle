cdd5c814b245f31cd5af0ee4152ba962cee201d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:32:52 +0000

    calmly day need relation organize

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def get_array_slice(array, i, j):
+        return array[i:j]
