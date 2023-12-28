e4abfe5c289cd783abb7034258f4b9aacf7cdde6
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:07:13 +0000

    pretend trousers cheque always care

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
