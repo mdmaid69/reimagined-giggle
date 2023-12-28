517295775a2bffae38dd1399f16999c6168899c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:47:47 +0000

    interview hollow appeal walk return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
