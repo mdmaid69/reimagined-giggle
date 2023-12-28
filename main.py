2fc7ca248618dcfc535eb26a42e1a612faf82527
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:45:55 +0000

    rise shape suddenly opponent search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
