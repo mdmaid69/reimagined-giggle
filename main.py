fd519d09411bae971ca589a88aeed46383769a86
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:28:43 +0000

    fit undo gap quick disturb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_length(array):
+        return len(array)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
