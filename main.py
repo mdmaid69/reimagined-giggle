a5631b538b4c953152d9f21ca03905e1c88aaceb
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:37:58 +0000

    godown Ee march march badly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+for i in range(5):
+        print(i)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
