8488c828c3ddd82d237ad5c658ad2d2d9b7b8099
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:52:27 +0000

    dry concentration muscle translate rain

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_min(lst):
-        return min(lst)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
