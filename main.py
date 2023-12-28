791a6eb2f586a936a10fa694c4ea6b5ee8410431
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:27:18 +0000

    search false truth pressure nothing

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
