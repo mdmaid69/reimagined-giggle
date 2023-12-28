81368f542f46fc7eb3f337ae03f26a3e3f0838a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:22:14 +0000

    ask comeback already just breakin

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+name = "Python"
+print("Hello,", name)
+def find_max(numbers):
+        return max(numbers)
