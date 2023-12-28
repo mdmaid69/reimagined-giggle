3de8b473757652391bb702c6ce093a581c7e60b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:33:53 +0000

    179bite large law offer heart

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def get_string_from_array(array):
+        return array.tobytes()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
