d4986d247772099934cb433d9e8cd3f29bb5a436
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:13:50 +0000

    beat book dead stare boat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
