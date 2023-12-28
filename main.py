9309f518bb18a1fc10ba0100ef08d059fbce21ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:03:47 +0000

    take comeround defeat fact poor

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def greet(name):
-        print(f"Hello, {name}!")
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
