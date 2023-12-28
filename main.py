39a5aea0fb011935a33dc536157d40891448fc7d
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:08:19 +0000

    nice Oo glass needle satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def is_odd(n):
+        return n % 2 != 0
+import sys
+def exit_program():
+        sys.exit()
