3e2ff720098718d37c538ca5c6c3c26347fec4be
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:42:21 +0000

    confidence pure makeup comfort square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def clear_array(array):
+        array *= 0
