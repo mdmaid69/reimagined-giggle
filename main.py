7a3288a2c7a61cb8f56c462033edb32c726a85e3
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:11:24 +0000

    success father computer official top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
