66e0212836f2c1c99b74df433008849b9dd15a40
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:42:51 +0000

    basic debt shelter respect claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
