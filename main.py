4a34d7d51cc3f1f9958e4998100fc27df54baab8
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:59:56 +0000

    willing sport trouble allow damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+name = "Python"
+print("Hello,", name)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
