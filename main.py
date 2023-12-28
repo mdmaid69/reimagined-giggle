2f9db94f1142281ea389a7e71cb3b76a2133b92e
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:14:19 +0000

    start juice keep stupid car

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_set(array):
+        return set(array)
+def square_number(x):
+        return x**2
