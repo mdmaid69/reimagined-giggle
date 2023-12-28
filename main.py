9794fed0eb8afb3836b072a75ce8794d924740c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:22:48 +0000

    pleasure improvement gap worry variety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
