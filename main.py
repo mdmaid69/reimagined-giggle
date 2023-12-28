3dd967882df59173566de64324490af5a66789b9
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:15:21 +0000

    comfortable murder hold rest protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_length(array):
+        return len(array)
