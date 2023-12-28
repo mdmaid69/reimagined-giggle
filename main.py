f4894696a5f9b05b7f524013cc325bcf35ee662e
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:56:54 +0000

    disgusting comeout fear rough over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
