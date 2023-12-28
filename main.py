2d94a5e03b322bf17bbe4a5bb934ae88d01516ec
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:27:37 +0000

    emergency bright direct ask preparation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_set(array):
+        return set(array)
+def remove_duplicates(lst):
+        return list(set(lst))
