679c0b46939f0f7f5a18198b5dff249c0008fbbb
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:43:59 +0000

    person shoot well yours sharp

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_acceleration(speed, time):
-        return speed / time
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
