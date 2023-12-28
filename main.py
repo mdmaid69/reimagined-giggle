ab3901d2bcbc7f8410a9c835bedda96488fb45eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:00:22 +0000

    arrangement worst cruel explanation forever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def print_python_version():
+        return sys.version
