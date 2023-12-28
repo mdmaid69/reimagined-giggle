78c5007c2f13e400103da7aa92c5a20c48448cb8
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:21:31 +0000

    such brother pity beat mail

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_min(lst):
-        return min(lst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
