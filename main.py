4803e54b1ded34376e2add92436c1520a51b66e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:32:46 +0000

    571enough pool night bath blind

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import platform
+def get_python_version():
+        return platform.python_version()
