8409f3f128bc086ce29c03fa45e332d1593c60f2
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:23:10 +0000

    cruel burst heavy desperate air

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
