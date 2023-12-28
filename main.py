42544542ba9fd55789a6c58d10b06d9ed8152fb0
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:25:29 +0000

    takeover apart after cover strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
