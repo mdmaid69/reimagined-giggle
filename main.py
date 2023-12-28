491096cb58bb55fa79000cfb12cedb15ecbe9dfb
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:23:14 +0000

    appeal feel runaway cold high

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
