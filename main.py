63ce353eb21eaaa1c9ac56c1823ec7f713340e78
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:46:25 +0000

    send area accidentally perhaps form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import platform
+def get_python_version():
+        return platform.python_version()
