b74986f700b54c7058d2e4fbfc877679f62eb4c3
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:28:11 +0000

    spot tool lose zero aware

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
