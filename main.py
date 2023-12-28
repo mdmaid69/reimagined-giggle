aefa86ef9bcef5e6e8fed31f956c38fb578982aa
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:52:37 +0000

    main Kk smooth them invite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
