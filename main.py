afd4690aa6abbec12a9d4aa4eaa2899481320497
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:08:08 +0000

    draw calm general possession getaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
