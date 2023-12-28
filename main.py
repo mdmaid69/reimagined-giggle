47fa8f8513843f3a8c921b0c8517ed98618d65af
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:23:08 +0000

    somehow combine time area everybody

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
