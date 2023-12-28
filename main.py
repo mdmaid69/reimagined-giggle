5dfd21b75d53dcaa8d0f6374138ad4aa2811bfcb
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:55:06 +0000

    sit announce private early process

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
