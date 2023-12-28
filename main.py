6e9d777b38d8c996bbc70961f758dc0eec403f22
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:23:52 +0000

    long dark comeout Hh rush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+print(math.pi)
