6547f231007b857890bfc7a3c894df35a5e8f078
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:47:50 +0000

    prepare violence basis relationship friend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
