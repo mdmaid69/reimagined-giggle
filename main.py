8de43d3d302d6b43f2bbb5cbcd6f5623daf05776
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:17:12 +0000

    tonight uponon pocket correct scared

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
