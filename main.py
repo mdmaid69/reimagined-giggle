8330ccdafd32935134d742da67fc1e24f82d35e0
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:23:03 +0000

    middle land for opposite work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
