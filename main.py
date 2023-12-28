8cd82c175a53a3400a41501b737c2d9d1b7fdec6
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:57:15 +0000

    great lonely brown wonder no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import sys
-def print_python_version():
-        return sys.version
+def exit_program():
+        sys.exit()
+def convert_to_hex(n):
+        return hex(n)
