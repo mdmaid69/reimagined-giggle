b162797938d6dd5e37bc8d014592b2967ddf29f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:58:43 +0000

    late unable breakup fear season

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
