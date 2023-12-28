bb2a763b167d1dd52fe32bdf56a73382b48dcb71
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:09:01 +0000

    unfair join behind passenger while

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_set(array):
+        return set(array)
