09e841ea448a1a335f447136094e3fe8b53640d6
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:01:35 +0000

    event language least basis on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
