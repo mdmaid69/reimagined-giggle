cb71f7d66854c60e7977f947dc761dfbc85478d6
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:14:22 +0000

    bother shut Kk pot keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
