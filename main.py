136a597057c37f81a32bd4175d209eb4d316689d
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:43:05 +0000

    improvement pool doubt die extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
