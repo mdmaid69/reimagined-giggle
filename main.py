7718dc1b9d9981c42c4323fe057fdff59894e7b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:12:21 +0000

    ball draw attract fall absolutely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def split_path(path):
+        return os.path.split(path)
