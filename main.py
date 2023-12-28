b04f213266339b7cb0e7feb7f9b1d941b14acb14
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:45:11 +0000

    afford arrangement joke music hurt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-def calculate_work(force, distance):
-        return force * distance
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
