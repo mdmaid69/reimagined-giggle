41e972f273a05193c0f272b5a7a1976bbd9449aa
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:22:41 +0000

    ride rain squash turnon anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
