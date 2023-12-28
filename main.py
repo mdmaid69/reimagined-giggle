e755c027c2a9380b3fc4a7f5228d61007a3d5db8
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:21:40 +0000

    safe but last declare full

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def remove_duplicates(lst):
-        return list(set(lst))
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
