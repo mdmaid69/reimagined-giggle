e206595a87a9e5a7cee6a017f4228c8db3ff3450
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:13:47 +0000

    past instead along content rely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
