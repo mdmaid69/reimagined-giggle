025b21c95499cd7a6b412048f1bc0c4b84ad5ea8
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:28:15 +0000

    rain large tradition oil exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_current_directory():
+        return os.getcwd()
