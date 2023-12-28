f53f56487d056def5b094b19c8ee19031be726bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:01:43 +0000

    hers private drink later touch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
