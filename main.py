d5f52bc2f1ec075bc2a3d5bc133e5194dfd8d519
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:26:33 +0000

    interest sing protection authority old

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
