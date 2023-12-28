f17fae450460a14eb9ba075e0b7f08e29aad1dcc
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:06:14 +0000

    ground mistake us imagination height

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
