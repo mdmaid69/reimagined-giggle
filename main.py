cbd74c0f4487f0bc1926b2ba538b1b05219033b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:38:18 +0000

    community stamp regret left announce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
