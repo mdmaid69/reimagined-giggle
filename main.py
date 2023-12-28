bc17ac68f980795d6aa97d5ff39c6a2372f7fa03
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:04:44 +0000

    strike kind talk nervous catch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
