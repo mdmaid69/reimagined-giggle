11ed6ddd8ee3f508586769c1d813c0b45cbe3dd7
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:43:28 +0000

    design sort available criticism business

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_max(numbers):
+        return max(numbers)
