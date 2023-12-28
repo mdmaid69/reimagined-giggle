eebb74ff636f03f9276c5ccec67949810540e21c
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:29:54 +0000

    block Pp history minute back

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_work(force, distance):
-        return force * distance
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
