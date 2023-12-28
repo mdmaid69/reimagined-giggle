c6704b56f34c2bc36f593076d16ea2e17bece69a
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:56:48 +0000

    cut among callat interview something

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def cube_number(x):
+        return x**3
