290c7919856f95b01ffe6a852e2322fafad2ae47
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:59:28 +0000

    yet priority read protest land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
