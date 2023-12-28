788e4104f14a2f003e920957efff5540982620b8
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:35:43 +0000

    wise excuse damage round ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def is_even(n):
-        return n % 2 == 0
+def cube_number(x):
+        return x**3
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
