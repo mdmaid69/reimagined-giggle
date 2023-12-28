0f61f3bd8dee3ed6fc10934058fd9df8c40dcc1a
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:41:08 +0000

    backwards rank clear smooth ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import time
+def get_current_time():
+        return time.time()
+def find_max(lst):
+        return max(lst)
