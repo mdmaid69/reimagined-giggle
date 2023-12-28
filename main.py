870c04325683c0a7cc54f4f958c927f997692a93
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:26:54 +0000

    cause see word subject against

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
