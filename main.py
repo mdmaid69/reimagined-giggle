51cda0cab5a36cf086a4d4628b67ce272228c96d
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:19:28 +0000

    chance mess table bridge difference

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def remove_duplicates(lst):
+        return list(set(lst))
