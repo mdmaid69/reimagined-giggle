664f1b81a564032d707cce9467fa2eaaf876a32f
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:42:40 +0000

    expect blame average objection colour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
