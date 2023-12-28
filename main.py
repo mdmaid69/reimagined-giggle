b47430d9f50250ad891c9829ab760f1fe64dbd9e
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:11:05 +0000

    reason movement pride type getup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
