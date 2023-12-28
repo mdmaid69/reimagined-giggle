3a486257408992b9b20ede896a2d92f86ae58676
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:12:12 +0000

    situation Yy expression remove false

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
