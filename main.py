4d44318b93e334af9cd16494fb4cc24bff6fc36b
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:23:03 +0000

    possible mistake inform lucky visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import platform
+def get_python_version():
+        return platform.python_version()
+def calculate_work(force, distance):
+        return force * distance
