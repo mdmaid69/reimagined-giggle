fef6df7932515b8eb026555c16a06e3f75e0b7ff
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:38:16 +0000

    correct bite variety call squeeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
