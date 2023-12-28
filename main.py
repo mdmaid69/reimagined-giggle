4db16b30d1e85ba2b119acc168a67e45c2260d62
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:47:39 +0000

    deal oil still physical attempt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_cosine(x):
+        return math.cos(x)
