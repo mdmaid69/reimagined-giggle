8dae988b8a3afaf9c87eb0ce55ffcc63a2208cea
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:55:19 +0000

    needle address shock through study

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def find_min(lst):
+        return min(lst)
