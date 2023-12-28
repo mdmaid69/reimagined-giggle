103fdf16ae3c4ae86b152188d285653d54ebe566
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:19:01 +0000

    be hard flat suggestion mention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
