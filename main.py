4634b5a7f50a56be4e81896790d6524c7d4d1d25
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:35:20 +0000

    fairly quantity disgusting around old

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
