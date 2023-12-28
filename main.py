6794a5bfa727fc629cc3b26a210ed9b0bb2b25f6
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:25:46 +0000

    respect children fairly lunch thorough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
