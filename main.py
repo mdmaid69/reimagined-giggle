dc81097ef767414882aece7a714fc962fe0d9403
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:00:49 +0000

    lonely not hold soon thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_density(mass, volume):
+        return mass / volume
