815582dfbc149d836805f7336247eabe3f9018c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:43:16 +0000

    base Rr able month much

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_exponential(x):
+        return math.exp(x)
