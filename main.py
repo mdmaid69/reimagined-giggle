0bb2cd5c6c44b4ce9c3983fd7c4b97286a8f5013
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:32:49 +0000

    large sometimes control receive emphasize

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import platform
-def get_python_version():
-        return platform.python_version()
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
