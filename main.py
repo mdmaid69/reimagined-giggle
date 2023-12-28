deec2f125a5cf217b7afbbdcb046aa7be8422e77
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:42:54 +0000

    bank run turndown muscle give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
