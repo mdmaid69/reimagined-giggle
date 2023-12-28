1b407ba8962c575967609545b95dd2790a914ca7
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:38:21 +0000

    constantly anywhere sink likely front

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import time
+print(time.time())
