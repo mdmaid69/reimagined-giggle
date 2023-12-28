9faa14665e4b49c7677fbaaaf0816974ec5271ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:20:15 +0000

    language split quantity please dangerous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
