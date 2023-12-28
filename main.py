acc1ecb729b032e14754b8bb2b837777e5226423
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:28:20 +0000

    appearance kind club able goback(to)

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
