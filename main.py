ce8cae9ac4acf462bc6168127623b2317f3bdbed
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:01:10 +0000

    571enough easy circle guess patient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def convert_to_binary(n):
+        return bin(n)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
