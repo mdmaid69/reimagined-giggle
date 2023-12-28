54daf9de9520740352f187fb107e4df191834263
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:02:52 +0000

    rubbish hurry second point like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def count_elements(lst):
+        return len(lst)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
