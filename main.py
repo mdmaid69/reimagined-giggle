6b823210883b440313b0388aae03f80c88e89e0e
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:19:45 +0000

    brush flow pool reach seriously

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
