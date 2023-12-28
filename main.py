9878353eebbb4017a418810e19c032c9c7bd8d42
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:00:26 +0000

    very action talk chief correction

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
