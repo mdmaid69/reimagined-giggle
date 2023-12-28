4d7760d6e6c80be3c5292c849fc47576ff01655a
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:15:44 +0000

    press radio record party play

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_list(array):
-        return list(array)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
