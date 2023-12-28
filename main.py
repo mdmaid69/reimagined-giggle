367842a40edc1a0f802c5353b8d391f1678f37e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:01:19 +0000

    rare mistake organize correct practise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-for i in range(5):
-        print(i)
+print("Hello, world!")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
