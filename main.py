f61d1905a337a287e4a426861927c973e1438fa6
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:26:11 +0000

    fall twist state matter base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_min(numbers):
+        return min(numbers)
