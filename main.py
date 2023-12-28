e98f234342c45f194d2f7c264cf12135b8514041
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:50:22 +0000

    Ff cross hurry answer various

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def square_number(x):
+        return x**2
