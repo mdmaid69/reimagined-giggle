34ba32c62468dbbe42b6d88afbfd8b7cd8006b2e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:56:14 +0000

    straight cheat concentrate suffer exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
