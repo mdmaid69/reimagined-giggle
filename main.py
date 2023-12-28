f7341de229d14c4eb41bdb7243f4129e3a43378e
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:27:36 +0000

    surprise otherwise involve bend wire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  def is_even(n):
+        return n % 2 == 0
