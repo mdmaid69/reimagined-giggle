6fe9c5a8b8fb686307c620c4fcdaf4f82f3f8e54
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:41:49 +0000

    chance convincing drop chief sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
