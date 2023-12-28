da1ffb3add7aed54527bb553b21b4765794a92a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:55:31 +0000

    doubt manage cry recommend tie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def get_array_as_bytearray(array):
+        return bytearray(array)
