ecbb0fdf0ee9c6318b8f7ac87ff70cd29d1ecc47
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:49:17 +0000

    Ww touch exchange risk unkind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
