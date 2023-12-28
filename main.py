c157db274b1d8e3e0e442ce72bd6b4170dbca608
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:36:50 +0000

    may freedom tired sting year

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
