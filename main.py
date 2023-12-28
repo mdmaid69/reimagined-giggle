a33bc4f55700302ecbb97b8fd6a86378d25b6507
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:40:18 +0000

    direct win improvement inside mood

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
