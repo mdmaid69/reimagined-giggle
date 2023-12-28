4c6f25f78038472e400701d0b053ae3696bb23da
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:32:53 +0000

    chin control complaint mother suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
