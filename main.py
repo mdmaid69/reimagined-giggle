6fddec211627c575a6be193636120d78737736d5
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:43:12 +0000

    concerning eye Gg raw year

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
