0f67c0c99c48fb304d193971bf9f4b75e81be933
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:00:18 +0000

    watch view only corner afraid

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
