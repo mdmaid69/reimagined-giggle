5c7528ab0981af6922f1bd56ff3d48658684cd87
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:13:50 +0000

    shoulder everybody copy mass juice

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-print(sum(range(10)))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
