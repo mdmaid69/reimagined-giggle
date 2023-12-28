6a6220d0ef00ff10a47b5480dea8a15463b08d44
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:25:20 +0000

    bunch soak every persuade tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import numpy as np
+print(np.array([1, 2, 3]))
