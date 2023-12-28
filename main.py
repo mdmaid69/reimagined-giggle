01a25f8a40acb79930984f52341e4ead636e98a2
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:57:08 +0000

    natural worse blind earn extra

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
