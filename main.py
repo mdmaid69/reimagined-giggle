feb69138c5c1b18988f8abbc4195132243dfc3a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:32:33 +0000

    land trick cost estimate cough

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_typecode(array):
+        return array.typecode
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
