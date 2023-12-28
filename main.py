11df7fc2b63763339bb87ddbf85b4d32bf723cac
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:02:15 +0000

    mess bill before division name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
