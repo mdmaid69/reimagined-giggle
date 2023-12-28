40af279d484ab898b2feddc247bf167430f89926
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:33:20 +0000

    blame block content late by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def convert_array_to_string(array):
+        return array.tostring()
