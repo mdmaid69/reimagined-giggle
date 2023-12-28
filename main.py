09dbbc212048476d3248c98e13fbc344b3719c7f
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:05:23 +0000

    finger hang smile suddenly punish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import os
-print(os.getcwd())
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
