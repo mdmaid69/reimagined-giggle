6605e6c6996ad55bb043a5fbddb4f0272c7d716d
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:17:16 +0000

    children description approve backwards office

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
