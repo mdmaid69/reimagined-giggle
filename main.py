804e94ce8209892b8231037772a04cab15f65ee2
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:04:34 +0000

    border butter wait cut hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
