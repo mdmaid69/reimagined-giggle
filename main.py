4cbc49609eee38df9ec32791686262c0c61b59dc
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:07:00 +0000

    whichever period expect obtain frighten

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
