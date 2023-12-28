1f080f787ea61952393745419ec3c2d80ea3b724
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:31:15 +0000

    vanish fork pile goback(to) air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
