8a0b97e3c26b6729633fc1aacc4af5b472326941
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:21:40 +0000

    compete safe himself suggest pause

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_density(mass, volume):
+        return mass / volume
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
