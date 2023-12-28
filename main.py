f78bde2a321bc281ec5ae8747d35403c247558f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:07:53 +0000

    find light result odd choice

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def subtract_numbers(x, y):
-        return x - y
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
