c42114ff219baffffaa4a3dc231a14d927d9ed1c
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:53:20 +0000

    until brush Dd uncertain fair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_string_from_array(array):
+        return array.tobytes()
