0d42185a2c6f533e8902ad65d73c69c4cce2540c
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:36:28 +0000

    fear compare obtain communicate yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
