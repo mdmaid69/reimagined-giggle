9234de5f7cc0d13eddba6da1328f022d1a96cb2a
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:16:22 +0000

    slice cruel recommend anywhere reduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
