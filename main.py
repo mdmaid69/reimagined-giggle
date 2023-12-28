0f529e235ddbc2dd5fc9b0d8bd147d8056eb2bc5
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:40:33 +0000

    Mm battery while track tight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
