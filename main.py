db0513a1a7f48b282c69bb3819aa4cc451664122
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:42:56 +0000

    rank already there themselves decide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def square_number(x):
-        return x**2
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
