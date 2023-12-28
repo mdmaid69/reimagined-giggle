02a61101213b5cbb9bb31aff6d935529a7eb0079
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:06:10 +0000

    thought age godown disgusting nonsense

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+for i in range(10): print(i)
