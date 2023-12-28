014876ffb57ac3722fee871ba79234bcccb8c519
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:07:21 +0000

    get cheap right kind proper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
