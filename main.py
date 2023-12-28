7683bffb44b39be5afc4d3e74cb7734a83071080
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:54:10 +0000

    character think favourite pile hers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import sys
+print(sys.version)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
