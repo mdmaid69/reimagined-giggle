3fccb0a6ec85479d9a2ba0faa2429875fa3e9bf1
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:49:00 +0000

    like discuss home cheap reaction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
