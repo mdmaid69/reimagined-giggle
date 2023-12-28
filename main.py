93fb01a58d0761044243f26ed575cb49e60a50a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:24:41 +0000

    birth embarrassed goodbye find hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import platform
+def get_os_info():
+        return platform.uname()
