dc5530d069273e90fb0a3e62eb76da51fb571935
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:21:41 +0000

    on bang necessary basic comedown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_unique_words(sentence):
+        return set(sentence.split())
