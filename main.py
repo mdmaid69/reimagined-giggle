5f8d67d76d9ff36a14fb7c5e5871212aad3be1aa
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:11:38 +0000

    hour copy ignore pot affect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
