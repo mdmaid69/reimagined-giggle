03a4da6943c26031015fab4a375ef916f8468abf
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:00:07 +0000

    laugh lower bit freedom control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
