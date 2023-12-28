9f02db85893a2d39776048d1fd9e880f0bfccc09
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:19:32 +0000

    standard dish wonder sense nonsense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import platform
+def get_os_info():
+        return platform.uname()
