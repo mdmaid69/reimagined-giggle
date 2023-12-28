ff670ea58ad99d99323477efdd7e23b1658dad60
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:35:26 +0000

    order rather form runinto handle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def get_array_as_repr(array):
+        return repr(array)
