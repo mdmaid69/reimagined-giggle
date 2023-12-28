39fea350578fd2e5494c94792fee4bf366f7890e
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:54:58 +0000

    best callat negative before either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
