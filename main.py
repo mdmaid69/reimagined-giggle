4ad56e6a40402cfddb00fc4580912276145280f1
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:18:50 +0000

    through dish whatever tradition replace

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
