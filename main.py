56d4f0fb9c638fc64ae86e85e6aa782ab53f9d31
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:40:58 +0000

    lost aware phone hard arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
