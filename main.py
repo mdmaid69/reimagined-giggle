5564725a4b7b896324adebbd181916ab92f4b5b0
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:02:45 +0000

    bang too hand memory sore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
