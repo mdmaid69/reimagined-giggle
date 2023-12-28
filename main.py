5f1788903909e5f03d1e24d35f81cec468c625a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:16:00 +0000

    grow stupid level consist need

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import sys
+def exit_program():
+        sys.exit()
