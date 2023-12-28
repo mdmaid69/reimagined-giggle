f23742b709a9b55e79eebafe5eebf5fa758e6e6a
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:30:45 +0000

    picture careful note conclusion public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
