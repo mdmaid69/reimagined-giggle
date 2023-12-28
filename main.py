636664b44fb07f61df550ecc9c40fbb9c40dbf3c
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:59:23 +0000

    row rest friendly poor slow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
