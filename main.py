0edb8ebbb580b2d510b446e24f0c168cdd6702ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:48:54 +0000

    equal stop tidy herself tradition

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import time
+print(time.time())
+print("Hello, world!")
