8d48c39a8371dbc7694fec6dfaa6e7432ea063df
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:40:49 +0000

    town bet used start search

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def sort_list(lst):
-        return sorted(lst)
+def calculate_pressure(force, area):
+        return force / area
+def calculate_density(mass, volume):
+        return mass / volume
