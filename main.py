a430df24f43c78085cecc9e8bab465a2f9ff0c75
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:50:38 +0000

    whether know sign somehow common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
