9b9285e7ef5e005e1240fc9d20f187448dab5c2a
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:47:27 +0000

    female want basic strong boil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_factorial(n):
+        return math.factorial(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
