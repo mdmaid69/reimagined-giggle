c8999f9177b27b5920e0e4204686a8ae54fef455
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:39:07 +0000

    care less trip bored slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
