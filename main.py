c3ff220a6900a4fa7143cddcfc8af9656ffb1044
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:34:25 +0000

    prize pollution appeal sure health

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
