3208d57ef6529c1e09d06a70f63e3f22df8f8ecb
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:29:00 +0000

    lend shop firm himself common

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
