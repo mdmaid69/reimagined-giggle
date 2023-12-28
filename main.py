dca5ffb0e94e9d59baa108ae71a347640baf1f86
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:29:18 +0000

    commit thinkover country guide breakfast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
