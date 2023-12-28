b7fb66fb2cf40cbd1c40a80a403a135af031aab2
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:08:17 +0000

    slow market impressive power fetch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
