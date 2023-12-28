fe96ab3cbd902996376d62894769e3b9de05cb16
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:59:06 +0000

    centre disappear wish damage buy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
