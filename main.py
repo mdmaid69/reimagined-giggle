f69ebc69f6c576891882fb0e920ef3d64a7ce11f
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:27:59 +0000

    letter contain skill know charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
