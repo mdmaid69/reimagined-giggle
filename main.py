54f63d44d1fbf48a1d3bf49e4639e6b286899968
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:29:44 +0000

    flow happy search bet tear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def get_file_size(filename):
+        return os.path.getsize(filename)
