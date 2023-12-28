b79638f808173d9b3a7cc515e1f5a112e8e4fa0a
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:44:00 +0000

    kind heat sign under fact

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def append_to_array(array, item):
+        array.append(item)
