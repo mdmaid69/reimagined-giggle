609429411c0afe71bd96af946a942b04023d685f
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:31:07 +0000

    against class walk guard voice

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def find_unique_words(sentence):
+        return set(sentence.split())
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
