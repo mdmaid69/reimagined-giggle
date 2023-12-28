bbb2ee77a029cfbe6c7b82ca8ae312325c50144b
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:03:08 +0000

    sound 568energy reason skirt minute

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+text = "Hello, world!"
+print("Characters:", len(text))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
