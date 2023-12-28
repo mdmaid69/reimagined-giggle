39878f884f01b70de6f3066c5fa61e1b36d68199
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:04:25 +0000

    Ii edge at geton love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
