aec48fe928ff1787c09b7ef0a7d1cb28c3d37da6
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:29:30 +0000

    nervous coat pretend holdon proper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
