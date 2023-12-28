6ab8ed6f4a4b1b105c5862c9f30cb93b62466c2e
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:57:45 +0000

    keen interview any town spoil

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_current_directory():
+        return os.getcwd()
