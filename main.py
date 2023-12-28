03007800ff72fff2e91ce6cc251a86446068edc3
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:57:13 +0000

    slip short burst giveup clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
