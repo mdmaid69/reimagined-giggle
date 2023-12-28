38c254844701dc9f38b0da04a502b9ff2e743f08
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:43:33 +0000

    build goout commit turninto false

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import platform
+def get_os_info():
+        return platform.uname()
