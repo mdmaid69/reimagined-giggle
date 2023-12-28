dad110d6f3928b535de7ca6f287776d9e93d9632
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:29:54 +0000

    trap rank winter call confidence

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+print("Hello, world!")
