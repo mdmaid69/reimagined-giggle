c01de9434a2972d59526a8450b4f15ddeb338a83
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:54:12 +0000

    complete Hh feature dark listen

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import logging
-def log_message(message):
-        logging.info(message)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
