c62d580844b40e21b07036a8f1bc629b8873257b
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:05:49 +0000

    practice word force afraid face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def square_number(x):
+        return x**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
