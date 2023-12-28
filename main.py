808e7613cd655a0630f4a341cb2dfabc8fd32b14
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:38:43 +0000

    achieve bye bet pretty mad

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
