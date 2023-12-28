2d01c0ae12c21f93b327984adc63ebf878d2e51a
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:27:03 +0000

    wonder office apologizeapologise dull hair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
