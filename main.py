288e88426e7fd756eab0fa47e2f2272b410cf329
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:08:51 +0000

    message punishment often follow protect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import os
+def get_current_working_directory():
+        return os.getcwd()
