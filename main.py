bdd0341768da9afa57efb30be182e31e40fc5da5
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:34:03 +0000

    mess nice close 570enjoyable feed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+def square_number(x):
+        return x**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
