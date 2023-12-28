de35534290cd3f16a8233860564913120b605487
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:08:14 +0000

    smell however list he evening

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
