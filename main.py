d2c4e536c9ed84f34af347be6f715527e86c55ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:00:48 +0000

    bringup well abroad certainly pot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import time
+print(time.time())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
