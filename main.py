51b2be0fca879076ff39138fb14b52ac93dc11fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:19:28 +0000

    different travel mix effect correction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_min(lst):
+        return min(lst)
