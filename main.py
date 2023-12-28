2f03f85daa3b6867de5f468e229116446bc923a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:17:38 +0000

    will majority afraid complete must

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  def multiply_numbers(x, y):
+        return x * y
