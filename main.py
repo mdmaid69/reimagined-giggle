9eb7e908bbda69f269dd589baed8f508be183534
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:59:05 +0000

    share flight situation realize house

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def reverse_array(array):
+        array.reverse()
+import platform
+def get_python_version():
+        return platform.python_version()
