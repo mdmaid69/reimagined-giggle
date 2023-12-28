a7c3000e6164b14583c4dbc7cf5057fe20229fc3
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:12:26 +0000

    fashion space such escape union

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def square_number(x):
+        return x**2
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
