1c56ec5b18bb6229d5882aa0c92deccfc41bf4e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:29:05 +0000

    defend probably good equal forever

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_work(force, distance):
-        return force * distance
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
