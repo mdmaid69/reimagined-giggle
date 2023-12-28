a5076ec6e6f36bceaa38acfed95fdce3141ccb90
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:14:36 +0000

    reliable tight afford town with

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
