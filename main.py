0a76bbd065ae0e0e27537841bbf9ad3a8e0b91e9
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:41:28 +0000

    minimum neat choice ought careless

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
