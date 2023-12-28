b4d6abdcae7c65efc11296a136a5abcd7a8bf9fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:26:40 +0000

    nearly smoke worried fill luck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
