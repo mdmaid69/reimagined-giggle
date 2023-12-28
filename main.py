f661e6cd7ca3fb37faba072b56ec73608f5e76fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:51:52 +0000

    future hospital foot safety as

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
