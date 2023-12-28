8b4937a203f1744c5310fae9ed16cfb4a3e97a43
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:55:50 +0000

    law determined bottom out bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_as_set(array):
-        return set(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
