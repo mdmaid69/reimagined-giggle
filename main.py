aed5d47dd8d514729bd4e3ee869501fb5062fddb
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:37:13 +0000

    from friendly respect matter section

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_list(array):
+        return list(array)
+import platform
+def get_os_info():
+        return platform.uname()
