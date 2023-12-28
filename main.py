47448837b5c6c7db8856d67d4f379a7736a76c55
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:17:33 +0000

    thorough wide hurry wrap glad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
