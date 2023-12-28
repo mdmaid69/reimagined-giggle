5ae6ff8f263d5d4d399d20a092a5b5c42cd91a1f
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:51:27 +0000

    please inform ill wear check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
