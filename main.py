0489d1fd46faec90be8588f84ff82c8f60ff286d
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:34:54 +0000

    say print shelter crowded young

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
