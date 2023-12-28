5485b206201bed314401f8180621a80baf974e85
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:19:05 +0000

    grow expensive estimate call takeout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
