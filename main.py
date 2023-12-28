7b184fde5a8fb7d7c0ad2233d24016076349d624
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:15:22 +0000

    agree clean skin naturally makeinto

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+print(math.pi)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
