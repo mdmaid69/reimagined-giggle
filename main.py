08f2664074c21b30f01b55cbdf913f4beba68911
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:27:54 +0000

    me only near dear awake

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def find_min(lst):
+        return min(lst)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
