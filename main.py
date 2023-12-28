0e70acc116c3d3984df4a78296784684f48844f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:12:58 +0000

    want long calloff education proud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import time
-print(time.time())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
