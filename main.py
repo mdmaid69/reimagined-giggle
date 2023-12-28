b714ace61e354803a5bb3b0f9816205fe6d5a10c
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:46:55 +0000

    stay attempt male test ever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
