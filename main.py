23b128dba2ca10445c919ee84c5a09f37858e2cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:30:06 +0000

    direct fast sensitive hungry complaint

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
