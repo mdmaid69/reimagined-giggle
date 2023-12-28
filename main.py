b0fed7620d2c37e7f6c2f534f1cedfc35130b689
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:13:00 +0000

    team quantity choice lookafter suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_work(force, distance):
+        return force * distance
   import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
+  def plot_graph(x, y):
+        plt.plot(x, y)
         plt.show()
