2dcb6f34f59768c7e256e08d5f2240e72ae8f2ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:04:29 +0000

    term progress impressed care bringforward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
