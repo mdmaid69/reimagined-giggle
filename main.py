ce28f937929984ec5f2e33f2faf3b3fecf021d35
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:37:52 +0000

    let prepare flow food live

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
