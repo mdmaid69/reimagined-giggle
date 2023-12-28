35f536b763c22d8de9315e800a30e4617fdf03fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:09:44 +0000

    decide so comfort board nonsense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
