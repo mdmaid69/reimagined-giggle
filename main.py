ae23f458577230f505abaf0c45060e6a55a4e83b
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:49:00 +0000

    comeover politics now cross platform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import sys
+def exit_program():
+        sys.exit()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
