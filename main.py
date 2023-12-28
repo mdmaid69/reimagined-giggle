216385daf94d7734e253704bb352f02ae8d5f874
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:43:35 +0000

    turnup benefit shut always flight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import tensorflow as tf
+print(tf.__version__)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
