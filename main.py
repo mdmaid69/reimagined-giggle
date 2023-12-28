336eb417f8946f737232498c69058dc32988e517
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:52:02 +0000

    judge sentence protest collect makeinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
 def remove_duplicates(lst):
         return list(set(lst))
+import array
+def get_array_as_bool(array):
+        return bool(array)
