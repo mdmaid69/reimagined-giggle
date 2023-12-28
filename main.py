01f3504edb76307cca44fa792ade5bf8677a7545
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:41:05 +0000

    summer seem earn holdon opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+print(datetime.datetime.now())
