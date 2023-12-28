c993b4bf4227969ff05b557341d50278a451bac5
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:42:21 +0000

    keep give bringup state stop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def reverse_list(lst):
+        return lst[::-1]
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
