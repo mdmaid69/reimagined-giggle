79fea95edda848db290cbc7aae787c0dee96bda6
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:04:36 +0000

    understand invite result avoid ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
