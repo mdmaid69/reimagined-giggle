27ff47037e8f37b5870aad405aca64bab547c674
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:02:45 +0000

    summer form flow regional hard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
