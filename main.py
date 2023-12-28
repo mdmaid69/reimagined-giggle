f7a69fe1b400f9f6e9fdc08372bea580a2cfca2d
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:26:54 +0000

    thanks over difference legal repeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
