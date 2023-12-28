d33686d54dc33b98bc5efdbcd9978255811a9f9f
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:03:37 +0000

    system still pretend confident every

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
