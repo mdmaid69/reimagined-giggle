eba4ea950e2a32bbfa5460778bec9866d11035ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:12:49 +0000

    badly cough special issue beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
