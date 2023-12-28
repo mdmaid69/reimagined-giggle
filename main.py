d30c4e4e7531f3c5cb7ed05d6917a5d83d16a1cb
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:16:43 +0000

    success perhaps afraid without recently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import tensorflow as tf
+print(tf.__version__)
