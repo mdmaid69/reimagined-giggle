c4896ab34e4abbdaca1dd8406550a03f8f270286
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:30:52 +0000

    freeze tip congratulations show absolutely

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
