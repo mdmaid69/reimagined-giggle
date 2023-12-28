9d084211c9bcc754acbe244e2b90009029e033b0
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:51:44 +0000

    nice appeal niggaacademy sweet needle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
