e706aa6b9b79eb113011edabfab791e14cde3486
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:03:15 +0000

    division now fault head ourselves

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
