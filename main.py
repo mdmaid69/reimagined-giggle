97a7bd1bbf2df8873a00b4eecd8d5e243298e1fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:07:00 +0000

    minute sore after leg getout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import os
-print(os.getcwd())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
