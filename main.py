b28965dfbcb619e1e911442af6e2dfedb51d436d
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:55:52 +0000

    tip trouble yes better personal

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
