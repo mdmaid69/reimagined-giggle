26f4603c31208b23b54d1e172c6247dffb24bd26
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:11:27 +0000

    defeat collection long development flat

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
