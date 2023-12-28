6c50bc9421ab0ceaa3363116a5cab59c54ee84ba
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:35:18 +0000

    method consult congratulate boring long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
