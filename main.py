6ab4c36e9d37a757dc9c84e59402dee46e7ac63b
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:41:41 +0000

    travel lie small moral reliable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
