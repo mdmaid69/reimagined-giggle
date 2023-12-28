0302bd354800609c42c22fd50e0c2718300c8ed5
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:02:14 +0000

    achievement skin Ff and allow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_acceleration(speed, time):
+        return speed / time
