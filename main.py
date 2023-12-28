64e856896ca3013ce42ef75c99c6d9cd571b75cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:11:37 +0000

    advise something sell sensible yesterday

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
