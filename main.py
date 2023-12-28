4ae9a360522c64170973c3c83260455aeb88c2d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:25:59 +0000

    arrangement distance steep secret onto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_pressure(force, area):
+        return force / area
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
