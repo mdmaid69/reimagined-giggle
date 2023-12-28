5e81171c3eb520b39df0f264d372e7cd15d7fa04
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:57:46 +0000

    bridge shut slow sky earn

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
