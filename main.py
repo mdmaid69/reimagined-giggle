3aaaefd62780530a4add694bff38690cca49fb7a
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:03:55 +0000

    meeting loss smile phone also

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
