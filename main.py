557e2ac3e8141d76171ee9c940fac90519025154
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:18:29 +0000

    father content turnover aim everyone

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import getpass
+def get_username():
+        return getpass.getuser()
