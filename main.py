055127175caee2c7eda9a945427f70682a1f22c1
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:26:45 +0000

    this neither holdon television born

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_speed(distance, time):
+        return distance / time
