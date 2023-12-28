912002794e923956949bb3f153a01ddd4026b10b
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:02:04 +0000

    sharp evening under sound future

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def get_bytes_from_array(array):
+        return array.tobytes()
