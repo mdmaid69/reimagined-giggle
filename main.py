e6cf0cc446bf80502f21349f69e19aaf5f6e5c9c
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:31:55 +0000

    statement machine too snow whom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
