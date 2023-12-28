235951bcde8dd8f44fc2fd513fb9d324396b6bd5
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:34:56 +0000

    cool knowledge thick compare direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
