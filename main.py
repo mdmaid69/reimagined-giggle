9bcbf37899709e2dd24a3b8c5e21b8905aae7244
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:32:38 +0000

    snow major flood order prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
