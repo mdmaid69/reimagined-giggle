74da63a6d91830247a25e7711743e5bac1526d51
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:42:54 +0000

    concentration tear year demand attitude

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def square_number(x):
+        return x**2
