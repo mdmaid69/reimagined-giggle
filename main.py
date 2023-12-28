2d2f288b25e834db92f614d18199aae169953768
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:40:28 +0000

    party foreign however ability soak

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import datetime
+def get_current_date():
+        return datetime.date.today()
