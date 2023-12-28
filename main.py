e6f4c7c6bc43862a09424626505474a7da666854
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:00:00 +0000

    tidy geton disappear control lay

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_union(list1, list2):
+        return set(list1) | set(list2)
