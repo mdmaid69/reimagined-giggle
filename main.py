db8f1e74db323d0722996526cd9c5b5f3d95ceed
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:38:17 +0000

    anywhere decision strike operate exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
