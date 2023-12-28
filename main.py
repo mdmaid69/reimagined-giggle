44b6cb31dcf698f8ed7da04638c87d5c786fdea4
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:15:05 +0000

    fire positive inquire schedule thorough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-for i in range(5):
-        print(i)
+name = "Python"
+print("Hello,", name)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
