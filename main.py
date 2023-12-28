6cee1ea6883c051ee36c083d683f1c907cb9ad82
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:19:44 +0000

    turnoff hollow repair lay listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
