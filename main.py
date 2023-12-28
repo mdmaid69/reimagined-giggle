2c024529aecb02748c30244f686a960999cefaf7
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:34:05 +0000

    survive bush reason shock month

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
