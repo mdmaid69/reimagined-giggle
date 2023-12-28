1a1f61cd90b05b6981dfc8dfdcb825c90ab2538f
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:22:47 +0000

    just single education rank doubt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
