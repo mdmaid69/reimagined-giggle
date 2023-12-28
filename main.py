db4615535ba9241e217f74312bb2ef98171df5d6
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:26:41 +0000

    proud tomorrow blind case weak

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import os
-print(os.getcwd())
+for i in range(10): print(i)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
