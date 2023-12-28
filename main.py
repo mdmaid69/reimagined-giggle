611c452fe5edcfefd000c345da5dd6bfc2948a8e
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:56:55 +0000

    goodbye supply dry comeup crime

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_str(array):
+        return str(array)
