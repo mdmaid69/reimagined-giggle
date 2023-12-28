38118cca36c2dd89d79989ec9447b6e45f2c308b
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:17:12 +0000

    without position pile wide fall

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import time
-print(time.time())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
