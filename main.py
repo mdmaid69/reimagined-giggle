ac6e0d14d0de01dd0a9061f7a8725870cde8bb63
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:43:49 +0000

    Ll expect mix break holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def remove_duplicates(lst):
+        return list(set(lst))
