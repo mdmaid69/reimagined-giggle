9c3a16203a1f7c1d5424b11b8f4ba70514d1f72a
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:37:15 +0000

    shall brain month opposite control

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
