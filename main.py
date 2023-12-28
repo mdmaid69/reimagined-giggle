632c6f37c8108022452ab2b713b223f17ca5e419
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:48:04 +0000

    true several flood show area

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_power(work, time):
+        return work / time
