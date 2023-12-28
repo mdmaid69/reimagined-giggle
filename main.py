7ecd0c7c7f53f37c8cacc09c698d598571c240e4
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:58:43 +0000

    makeup idea comfort secret comedown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
