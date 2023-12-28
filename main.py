c4e8eba8d84ac4129f11af7555c74dec992bad9a
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:10:31 +0000

    guess culture thinkof part exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def pop_from_array(array, i=-1):
+        return array.pop(i)
