1d83331c4e59e9e33ba4e833b278a9f4a955dca0
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:33:09 +0000

    miss Pp guess pass forgive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+from collections import Counter
+print(Counter("hello world"))
