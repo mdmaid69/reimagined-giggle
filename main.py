f9f94fc5c0981ef550ce28c1a1559f7fdb30664a
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:14:19 +0000

    mixture cheek height Oo level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
