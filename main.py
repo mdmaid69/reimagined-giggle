6d5067ff1e6f05f9fbd639d0b69e18c139d79c46
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:09:27 +0000

    border light equal thorough show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def convert_to_hex(n):
+        return hex(n)
+import platform
+def get_os_info():
+        return platform.uname()
