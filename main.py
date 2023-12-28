4fe8181116b6080b0cddaddc23a746f59aee082d
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:46:39 +0000

    minute reason frighten beside actually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+print(sys.version)
