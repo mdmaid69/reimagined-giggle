9fff0c22c95c9b09c850599a23fdd0a6046adf42
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:17:07 +0000

    trip comeoff politics watch hold

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def cube_number(x):
+        return x**3
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
