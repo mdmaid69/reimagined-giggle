e0bc489632cace1705e54af4d34ae43ced3db52a
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:18:00 +0000

    personal definite destroy upstairs only

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+name = "Python"
+print("Hello,", name)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
