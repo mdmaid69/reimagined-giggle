6a0845713ca4d2b675b339fc718cb434fc97d3aa
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:34:14 +0000

    pupil fire fault instant contrast

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
