4449e7bfca0719fdbcc1c4a633f15f1b902ed0f6
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:07:12 +0000

    culture peaceful left stick suckme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
