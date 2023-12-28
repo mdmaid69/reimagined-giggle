aa5756835b187856e2add580e5ea2ac4320698eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:25:24 +0000

    loud anybody arrangement round face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
