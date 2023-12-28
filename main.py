bf5d5d33d2c1e1803048ac175dfacdd09665203c
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:40:19 +0000

    hard odd love on scare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
