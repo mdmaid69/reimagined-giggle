2fb7634732b92bba5e5301ded38ba160a68ac1d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:12:28 +0000

    spare face street order ocean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
