fd570af9f2c1039a1385c46f870aab9fa96ce039
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:24:50 +0000

    contribute pretty song concentration modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
