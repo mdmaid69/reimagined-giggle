9edf6fa09e38c50ccb1dac234106ca20e36d8cf9
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:26:08 +0000

    swallow Uu slight quiet pass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
