02dcafefc5f041150db6a60d785a7d2fd354f503
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:05:45 +0000

    that scared wind hurry happen

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_octal(n):
+        return oct(n)
