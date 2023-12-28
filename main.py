abb94d75f9b4bdd5b5affa288fe7a85a5794f41e
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:53:56 +0000

    relief earn listenfor suggestion cheek

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
