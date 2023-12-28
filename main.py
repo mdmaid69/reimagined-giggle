8cfbab54655fc531fb6da425acb82a4bab2140ba
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:47:40 +0000

    like race as objection study

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
