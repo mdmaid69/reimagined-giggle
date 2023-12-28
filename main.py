0ae744557e0e071e24584ac02a14465eaf598b61
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:20:41 +0000

    lay test split drop improvement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
