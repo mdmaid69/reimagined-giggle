813452e6ae48e79ba76fd06dc18a12d15cde462f
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:32:34 +0000

    medium thankyou murder scale passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
