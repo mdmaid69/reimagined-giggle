9414ba25117626794e1396ad93728d36864b928c
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:49:57 +0000

    least song when increase regional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
