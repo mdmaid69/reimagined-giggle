a650bd486bda42e000b4ff81710c39fa286673af
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:14:55 +0000

    foreign fat wire agree remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
