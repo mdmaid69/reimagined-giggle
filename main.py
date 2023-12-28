8a00fe698e454c254011adc569da219284379e9c
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:59:26 +0000

    nerve crazy word team feel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def split_path(path):
+        return os.path.split(path)
