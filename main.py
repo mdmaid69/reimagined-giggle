120b280e3d1678f29e150488275ed74105041677
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:30:07 +0000

    throughout little strength maintain include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_length(array):
+        return len(array)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
