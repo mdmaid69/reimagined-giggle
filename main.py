f2586d02e76cb8032b82dc80b77a185452bc3471
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:56:15 +0000

    back cheek day morning path

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
