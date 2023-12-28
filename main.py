fd3ff9846a1afb3c5e899ac38b28c4974105048e
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:54:17 +0000

    deliberately final brush reject considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import numpy as np
 print(np.array([1, 2, 3]))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
