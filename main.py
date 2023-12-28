183f29213048a31300b8c0e483e0d620d8c8fcdd
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:52:43 +0000

    lie protest shape mistake list

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
