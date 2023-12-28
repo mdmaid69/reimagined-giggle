b4e3dbaaac885e88e929789e3b93bde84c77659f
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:30:48 +0000

    music quit moon forget roof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
