a3e57ea134724e1a05f3752f53bc56f4ffa9dfbd
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:51:14 +0000

    throw read exaggerate legal turnback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def calculate_area_rectangle(l, w):
+        return l * w
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
