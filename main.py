94c7b526fde2400fae5f95e42a9c5dd3c8114703
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:41:19 +0000

    present nowhere contents rise report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import time
+def get_time_since_epoch():
+        return time.time()
