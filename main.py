552a5b7a4623aa9952ad9d99d9031fe8da757403
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:07:39 +0000

    lay estimate habit pleasure bear

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def reverse_array(array):
-        array.reverse()
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
