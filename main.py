b3604af34b86158ca59673dbbe7f173a15e86e98
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:26:42 +0000

    development female stamp week splash

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
