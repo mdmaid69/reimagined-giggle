fad0af0de888931cc420ed94e0a2718a8335d52f
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:25:45 +0000

    chance stupid waste class getout

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+print(os.getcwd())
+def calculate_pressure(force, area):
+        return force / area
