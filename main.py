743a52a78fe35af9c2ff26d76530e23851c5a3b8
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:38:29 +0000

    sing help nice everything pump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
