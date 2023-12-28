2087088ebada2baec223d084c2b54d18d019b6ef
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:15:23 +0000

    brother rubbish share alone towards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
