a2a37f17b04ad6a676798821b775158258dfcc1d
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:38:58 +0000

    object order drink fit act

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
