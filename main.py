1aff49e978baa5be37155b06661ea7b3652622dd
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:47:15 +0000

    rely sure getoff wake dead

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+i = 0
+while i < 5:
+        print(i)
+        i += 1
