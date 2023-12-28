5e3e16eb396a9bcac5a940bb44ee9413bd480784
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:31:19 +0000

    party gift matter however scream

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
