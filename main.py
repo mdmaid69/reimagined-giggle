11668e024b59081478d0239a266bd454eaa9f66b
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:49:38 +0000

    573entertainment immediately unkind mistake decision

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def find_min(numbers):
-        return min(numbers)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+text = "Hello, world!"
+print("Reversed:", text[::-1])
