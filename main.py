77192671594e58df870e90fda6152573cfd7640d
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:19:28 +0000

    excellent once minority important hospital

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def cube_number(x):
-        return x**3
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def subtract_numbers(x, y):
+        return x - y
