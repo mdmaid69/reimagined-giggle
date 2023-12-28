b305a5a8a396661a56d1cbbbce20e2a4350f756c
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:11:47 +0000

    reasonable strike picture human increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import math
+print(math.pi)
