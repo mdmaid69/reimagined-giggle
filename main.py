ad334d067c22365c51570b32c65b00f7be604421
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:21:35 +0000

    pity follow getaway cope schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_base_name(path):
+        return os.path.basename(path)
