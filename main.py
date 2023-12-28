c3286d90752085b148470b0496a89f23187969db
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:22:35 +0000

    pattern container bear depend closely

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
