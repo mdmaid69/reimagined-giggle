27e5493cebeaec95d5f52e344e07b3763c2917fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:10:18 +0000

    run cheek ahead plenty tip

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
