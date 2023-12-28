abbaa81de16481b331a1f68944ed1a1ae75718e5
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:33:56 +0000

    confident joke Kk risk rush

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-print([x**2 for x in range(10)])
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
