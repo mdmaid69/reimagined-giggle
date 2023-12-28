9000e0493a24fb983b9d62e382468aa292352083
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:40:13 +0000

    comefrom room praise upset dream

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+for i in range(5):
+        print(i)
