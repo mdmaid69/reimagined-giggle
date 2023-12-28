6f97d966235955bbc2e83b26fbdbdc51fa4c89c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:03:19 +0000

    hide sea smart statement butter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-print(sum(range(10)))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
