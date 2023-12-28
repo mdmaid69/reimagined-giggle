ff6ea93bda23812ce922273e03658787c4f84bfb
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:44:14 +0000

    future pipe will such angry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
