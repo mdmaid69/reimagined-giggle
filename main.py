12550489525319a7f3d880226076266df7320e9d
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:18:46 +0000

    punish evidence backwards judge score

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
