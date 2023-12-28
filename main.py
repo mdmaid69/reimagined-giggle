005b2dd9afe321a435748f1542df2af74ac47b7c
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:33:29 +0000

    night successful cry this distance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
