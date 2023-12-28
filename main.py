a78ba2f4f9e03e1ef68154c593769bbcde47d5c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:16:26 +0000

    shall never on squeeze year

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
