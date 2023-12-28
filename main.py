4ed895ae307359538b5a2c79dfcf3f01f3aa4496
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:48:31 +0000

    production generous away work little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
