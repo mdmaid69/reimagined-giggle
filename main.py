62dacde807a27a1fc20b0dd620c4337ef3804d5b
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:08:20 +0000

    concentrate bringdown start control for

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def find_min(numbers):
-        return min(numbers)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
