bd0473d42f76dd1cca874891a2b4c8ece02af76f
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:42:33 +0000

    swim impossible ticket tall surprise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
