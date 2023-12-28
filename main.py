37bcd97abf8c394d9352e54f2eb2090c21d58cd3
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:56:09 +0000

    market development wrap fact 179bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
