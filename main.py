da7e04fc094706d63884a218cc82e91e431f091f
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:46:09 +0000

    bringback at about attach fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
