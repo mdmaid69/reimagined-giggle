bb11b7e1fc6c4a0dc99cfba4b9dc54ca7092b90c
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:22:54 +0000

    back comein part scene workupto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_power(work, time):
+        return work / time
+import array
+def get_string_from_array(array):
+        return array.tobytes()
