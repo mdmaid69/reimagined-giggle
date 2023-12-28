920a62502b693574811a6ca4d75d5517a7dabf27
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:02:04 +0000

    yourself apply lesson yourself afraid

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
