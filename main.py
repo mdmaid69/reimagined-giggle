3b0cc140dee747c850ae05c12262de737646bd23
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:48:21 +0000

    noise push influence damp no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
