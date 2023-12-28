946f64946fa6d0f175c542238c127870793e2ed2
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:26:05 +0000

    compare introduction out comedown birth

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
