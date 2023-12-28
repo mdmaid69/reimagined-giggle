48cf851746735b7d2384d4d839b4ba377164f3f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:26:31 +0000

    cruelty share declaration bye sky

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
+name = "Python"
+print("Hello,", name)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_typecode(array):
+        return array.typecode
