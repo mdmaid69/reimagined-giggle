861e71dda97f4f4150613e8e61e55a4116470263
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:06:44 +0000

    meeting foreign ourselves rate study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
