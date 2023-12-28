c3049ef232c7ee82b18c5ddb062822ff06036f00
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:41:14 +0000

    jump possibly threaten medium nonsense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
