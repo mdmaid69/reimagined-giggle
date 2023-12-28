0094122edc6b72602d47d0e406f2296385b56c35
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:55:33 +0000

    exhibition push sad successful spare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
