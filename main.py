8c6759fdcc1c26b8d812d5ee5ac0eabc4284c354
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:45:28 +0000

    tap rather change shadow test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_itemsize(array):
+        return array.itemsize
