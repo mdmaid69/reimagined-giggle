41563c3106fd4dc55715288af24878f503a1fbf9
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:52:21 +0000

    Ww curious shelter workout unhappy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def is_odd(n):
+        return n % 2 != 0
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
