668c6b61440262f4637ace77edfe5bba86394ba8
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:34:06 +0000

    rough pen mind pride blue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
 import array
-def get_array_as_str(array):
-        return str(array)
+def remove_from_array(array, item):
+        array.remove(item)
