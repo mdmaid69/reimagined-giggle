19954abfe1af5569b1499435f67ca6166b73d8d0
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:05:38 +0000

    about worried takeaway clever accordingto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def remove_duplicates(lst):
+        return list(set(lst))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
