6dcbb837472cf0d745226115487dc14582c513a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:48:54 +0000

    over objection getup shout fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_floor(x):
+        return math.floor(x)
