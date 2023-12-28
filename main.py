cbb8d70c0a66a77c1bb36de12a83459407dd8099
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:04:45 +0000

    from minute reject discussion slightly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
