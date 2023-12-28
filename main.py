957944344a9c820d8247201536f7da67337a43e0
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:16:22 +0000

    forever imagination scrap protest later

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
