dd967626730dc559a66e0539adda4ee9febf43ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:45:56 +0000

    language distant flat equal fasten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_working_directory():
+        return os.getcwd()
+import collections
+def create_priority_queue():
+        return collections.deque()
