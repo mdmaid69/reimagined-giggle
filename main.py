9f56ed65590f9e98a123a078243f927135a4b839
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:14:31 +0000

    politics behind plenty effort picture

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import sys
-def exit_program():
-        sys.exit()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+print([x**2 for x in range(10)])
