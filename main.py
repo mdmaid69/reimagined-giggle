f94dddfa3600506b419314c33e7ad1ef3b55943c
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:34:47 +0000

    for tall there beginning aware

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
