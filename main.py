d2fea34d4ea020506978eef45f781a53035147f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:32:46 +0000

    frightened system natural bad government

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+print(os.getcwd())
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
