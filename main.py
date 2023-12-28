f51d8f1a77e064419bd72ec645470580e6b262b5
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:04:53 +0000

    find uponon reply comeup Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def find_union(list1, list2):
+        return set(list1) | set(list2)
