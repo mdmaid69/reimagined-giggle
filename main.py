8dc4821e3c8795eac36719442e1171004b43684f
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:19:51 +0000

    properly frightened blame pressure smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
