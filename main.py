94eb6872ab9245b9d014eecdac42dd9df718730d
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:59:34 +0000

    union speak plan nurse burst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+print(datetime.datetime.now())
