6b740b668c858bb3f4c8ddea6cd2a05c9a79877c
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:47:53 +0000

    lose no language its certain

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-print(sum(range(10)))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def remove_duplicates(lst):
+        return list(set(lst))
