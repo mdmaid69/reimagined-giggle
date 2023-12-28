14d1265f0afa002709441eb2b02293913b66ba99
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:12:18 +0000

    heat cross instead escape minimum

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_current_working_directory():
+        return os.getcwd()
