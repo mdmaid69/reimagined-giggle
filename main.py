3479eac7b046f193b69a2a3087eb003ad4aa85a6
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:22:57 +0000

    straight base some vision nerve

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import tensorflow as tf
-print(tf.__version__)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
