bb23c75552b6b3bb0001395bea0bb998bf3e5225
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:28:19 +0000

    ocean share prepare false open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def convert_to_binary(n):
+        return bin(n)
