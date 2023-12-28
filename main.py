fd0726b64205b3b54052be8f413e310b18662234
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:43:10 +0000

    stranger free stick old entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import platform
-def get_python_version():
-        return platform.python_version()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  def reverse_list(lst):
+        return lst[::-1]
