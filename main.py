6533190a9fa03f5af2cfc0df69ba5234179afc8c
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:57:48 +0000

    reserve sensitive there set since

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def remove_duplicates(lst):
+        return list(set(lst))
