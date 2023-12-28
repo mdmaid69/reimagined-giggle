5431124280387fe586331dc9103197448387a42d
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:44:51 +0000

    spring ear mind fork bound

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
