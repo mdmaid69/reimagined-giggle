3611ca356e57d35b93691e448384a86dd48f9be7
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:56:34 +0000

    rub and push practise or

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
