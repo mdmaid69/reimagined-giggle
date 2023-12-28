96f9e2b8ec705b6c2ea4a2cf0e15b314172ebd54
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:39:17 +0000

    manage nor along confidence destroy

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_area(radius):
+        return 3.14 * radius * radius
