5c122d4adfb18d67cbc1f839c36a3644f622ba3e
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:42:11 +0000

    advice round anywayanyhow impossible succeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def convert_to_binary(n):
+        return bin(n)
