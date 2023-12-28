a480b927e1681f10998f2d8f07d3cd3a98062a15
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:19:58 +0000

    air first central term clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import numpy as np
-print(np.array([1, 2, 3]))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
