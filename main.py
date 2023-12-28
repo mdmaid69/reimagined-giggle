f9e626e9beed79ef90016b655e4d45f853904e2f
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:50:41 +0000

    squash quit pleasant commit squeeze

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
