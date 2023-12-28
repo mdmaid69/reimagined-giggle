bca92383b4699bd541527c388aece54e8e9c2848
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:47:15 +0000

    mine joke star avoid face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_str(array):
+        return str(array)
