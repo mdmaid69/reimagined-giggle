e47d4078bed548efa70f750105e70cd794337aeb
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:27:17 +0000

    Ff hand together do basis

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
