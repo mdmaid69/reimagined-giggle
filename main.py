434f36d2fbb0d9c4f34a95acbe7be60b031ed145
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:46:32 +0000

    vanish reply warm close straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
