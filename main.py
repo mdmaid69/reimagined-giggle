cd02c3a28e5e35c5fef34dd9ea798baf3f8e08f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:00:12 +0000

    pocket force all because only

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
