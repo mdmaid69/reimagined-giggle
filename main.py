0424a653f41982c9b5929397828eeba89bc68fef
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:21:45 +0000

    underneath break fine concerning splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
