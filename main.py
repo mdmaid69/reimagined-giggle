3632d07fbe67a7e486574a8df3e449d79a6202f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:17:24 +0000

    so mysterious square quickly anywhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
