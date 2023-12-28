299c37030cde041188dfc9fe211a27928b8cf585
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:55:13 +0000

    scrape display platform violence bush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
