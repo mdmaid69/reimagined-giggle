079287a4195548f12b798d54f9af466cc6347916
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:27:00 +0000

    sharp error arrange spray match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def convert_to_octal(n):
-        return oct(n)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
