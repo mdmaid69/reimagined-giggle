1f9f8b13dbd6fd384f77fb96dc40849b1ed522d4
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:08:13 +0000

    dream since against convinced develop

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
