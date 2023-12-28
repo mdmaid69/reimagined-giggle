0289c66f8adefc78cf0ad35f244127dd073f99d0
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:24:00 +0000

    embarrassing benefit either smooth spot

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import sys
-  def get_python_version():
-        return sys.version
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
