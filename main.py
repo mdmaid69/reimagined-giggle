f8cfc77e6d2f00ea7277c1404272cef073c5158f
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:36:07 +0000

    fork image pretty week same

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
