cac9d6fbda88146806b1b2bb58a5326f6c6b713a
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:12:48 +0000

    dance before vision lie memory

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
