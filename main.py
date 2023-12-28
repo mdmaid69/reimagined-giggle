70f8f4f7bcc3020cb328d173049f14106e9f077b
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:03:48 +0000

    cough under introduction rarely Pp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
