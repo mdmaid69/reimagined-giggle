76f45705ed8432a1038d817b179942de3c141eb1
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:27:15 +0000

    stranger importance suggest telephone contribute

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
