8505b75dbcdaafc0b4fdbce77bfc80edbe34d223
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:57:23 +0000

    interesting solve heat last sleep

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
