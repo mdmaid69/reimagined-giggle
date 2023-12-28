7737f59f9fa57488292207b5bdcf96425880018f
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:29:43 +0000

    whisper advantage advice permission scare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
