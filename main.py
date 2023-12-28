4c1020862945fe5c27ed87f67b502f4627f9857b
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:13:17 +0000

    look display body motion pipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
