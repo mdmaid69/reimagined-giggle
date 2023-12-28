30e3759266ebf813abf464f6ec55108a88265f1d
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:59:05 +0000

    view check send interested top

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
