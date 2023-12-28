185ea1c910df68e0a99595d39b5148a88de782b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:58:07 +0000

    dick below fix scene under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
