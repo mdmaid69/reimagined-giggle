efea88a4b1ced06cbec0cfbaa88d770fcc41f924
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:39:48 +0000

    rescue order glad attach Bb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
