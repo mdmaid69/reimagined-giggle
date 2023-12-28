3aa4da27b9c7fc79b3f358aa377fac891d0b6dc9
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:42:04 +0000

    entry confusing neither church admit

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
