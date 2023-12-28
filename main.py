95746c9569118e70f4edc16612620597a1282728
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:37:38 +0000

    disappointed any comeback shoulder word

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def reverse_list(lst):
+        return lst[::-1]
