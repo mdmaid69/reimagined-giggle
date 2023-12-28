0165c2e83725dd3119ca3a6dedf6a32e8ba0eeb7
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:24:17 +0000

    goodbye add well free appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
