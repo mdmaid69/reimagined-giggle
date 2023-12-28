b61047e822940e86efb67f6788c030e1541adab8
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:52:10 +0000

    rank cry I feed deliberate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
