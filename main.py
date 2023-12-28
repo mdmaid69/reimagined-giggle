a561dda3454edaccc1c371cc7b4aa20c67b61859
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:05:57 +0000

    date one name transport plant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_current_directory():
+        return os.getcwd()
+def sort_list(lst):
+        return sorted(lst)
