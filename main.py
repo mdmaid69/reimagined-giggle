3eef891734db06191cd46f7619b0f9fd9f083e03
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:32:58 +0000

    cap temper wind available occur

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import os
-print(os.getcwd())
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+for i in range(5):
+        print(i)
