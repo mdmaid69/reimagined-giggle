88254dd3a11d8beecbeb4a829eb70a532efa2134
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:48:59 +0000

    fast ring post centre honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
