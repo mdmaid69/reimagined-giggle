8ede7319a13619e75d46e74377280dc84ab20214
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:17:52 +0000

    cross about separate replace exist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import sys
-def exit_program():
-        sys.exit()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
