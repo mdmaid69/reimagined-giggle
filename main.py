c45c22585591e26c89e49a5a9a308bccdb59a3ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:25:40 +0000

    light partner country recommend object

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
 def get_array_as_float(array):
         return float(array[0])
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
