c811b8723b48515a627da9cd1a8744521a699985
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:03:54 +0000

    take project unlucky meet opportunity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
