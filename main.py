47d7e047bb5d3b65aefbc165c561778af4c9d85a
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:26:45 +0000

    tear large benefit embarrassed remain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
