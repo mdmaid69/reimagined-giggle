9c40d9788087068aaa58220a84e6830282499b73
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:18:10 +0000

    worry race today match interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
