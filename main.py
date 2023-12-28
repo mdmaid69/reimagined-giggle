fe88bd0d13b6a5260fc5bfe2b4e13f69796a321e
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:30:22 +0000

    simple interfere themselves too consult

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
