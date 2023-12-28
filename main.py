d62854d53e0c88097f078143fec979bd2b37171f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:26:33 +0000

    runout complaint that maximum division

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_min(numbers):
-        return min(numbers)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
