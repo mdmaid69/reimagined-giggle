ed08b236081da3fb9f9e612929a8b11b2dcb8948
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:15:09 +0000

    float scheme pure getoff takeout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def find_min(lst):
+        return min(lst)
