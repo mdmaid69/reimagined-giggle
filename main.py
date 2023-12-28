bad4887b7e5b888bd2eee4b72b545146157c74c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:02:32 +0000

    bother tight giveout bad add

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_item(array, i):
+        return array[i]
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
