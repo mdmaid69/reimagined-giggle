b6974c298672687643ed63edc9edfd5d8c693cb2
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:57:01 +0000

    why complicated wait children food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import collections
+def create_stack():
+        return collections.deque()
+def calculate_area_circle(r):
+        return 3.14 * r**2
