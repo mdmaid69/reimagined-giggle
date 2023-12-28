b472efda896ff7da2fa0bbe995eefcc0bf5b4895
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:54:31 +0000

    arrive calmly tell pleased spread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
