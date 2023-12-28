7cb59ccd3e0c820d4235dfe84895ac8f1281f1f1
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:54:18 +0000

    repair ill cut display necessary

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_current_directory():
+        return os.getcwd()
