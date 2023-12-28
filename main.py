20b5aaafa473059825836458ffeab9dcb0e0b8e9
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:15:40 +0000

    what Mm ought rush shelter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
