fe2fd4077482e0bdf5486ffbd0410fa9ec056077
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:14:54 +0000

    splash boring safety sensible something

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def find_max(lst):
+        return max(lst)
