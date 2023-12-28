6ef3cc7cc235e80baa7f6236c46a2cd9a19f1b48
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:04:40 +0000

    attempt beside to expensive drop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
