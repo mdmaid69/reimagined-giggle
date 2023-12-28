460178c49c2a32a8bb6239d352fe74a1d3602a6b
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:32:52 +0000

    report example along swim journey

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import getpass
+def get_username():
+        return getpass.getuser()
