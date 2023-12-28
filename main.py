716de6df7652762c4da36f8e51939761f8f37db4
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:31:56 +0000

    trade question share telephone wear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
