27c190bf416b9f0c6069623bb8eefc85b861f396
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:49:24 +0000

    room classroom quit crowded apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
