eeb4b6fae719d1caabce9f93a6f1f2a1737a18da
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:52:10 +0000

    enthusiastic label better thinkover work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_time_since_epoch():
-        return time.time()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
