f2e76cd61f835ffbdeef5db91fb845a39eeaa8a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:08:06 +0000

    responsibility impossible failure smell mean

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
