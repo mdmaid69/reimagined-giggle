0d09eea092bb848be4972d06a74028de0a1a9829
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:58:00 +0000

    classroom arrangement step strict total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
