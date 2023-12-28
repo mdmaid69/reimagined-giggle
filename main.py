bbbc97676505a66355e289ee3c2d4055d7e1262a
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:36:15 +0000

    loose past reduction and long

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
