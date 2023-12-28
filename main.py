a40e3b803d7107db0610a25aee4b6715df3617b7
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:05:37 +0000

    mood clearly complete sleep row

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
