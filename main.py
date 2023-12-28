edf05f777b5beb6b8b1c8faf504a08fe33a345df
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:25:57 +0000

    eat expression once personal stupid

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import sys
-def print_python_version():
-        return sys.version
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+name = "Python"
+print("Hello,", name)
