4fb8a10364b5afb4046b8ff44abc5e3ddaf49043
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:59:11 +0000

    represent recommend honest instant smell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import math
 def calculate_modulus(x, y):
         return math.fmod(x, y)
