2698112b8db1653e52146e649eca97a43f697a5b
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:45:26 +0000

    risk whom brother emergency thing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
