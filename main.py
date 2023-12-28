6e788d9ab9be9be1faf4efb07645c74794f7784e
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:33:51 +0000

    true of from workout want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def reverse_list(lst):
+        return lst[::-1]
