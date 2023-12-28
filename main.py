20f01609a44bf1e26e1a70d299098a4eda514a31
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:49:47 +0000

    measure condition average matter fasten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_arc_cosine(x):
+        return math.acos(x)
