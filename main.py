399c0fd24af0dafc38cb209b60c1d3f63e3ec8f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:51:02 +0000

    recently sign score bunch dinner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
