3be86744709ef71c65a859f5d0e52071baccdaa4
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:31:53 +0000

    part letter march partner sister

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import os
+print(os.getcwd())
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
