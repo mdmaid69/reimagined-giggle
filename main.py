b398aa860fbe9d35d8bb5204cc1e60d63dbfd210
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:51:35 +0000

    hello base complete material explain

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
