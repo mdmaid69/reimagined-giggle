1a38561f0a29efb8c72acdcb30b4d2a7dc962b03
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:09:59 +0000

    nothing frighten closed choice getup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
