4ca172cb1ab582153709b02d4fae31df151fe2f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:52:26 +0000

    fit judge underneath smile promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
