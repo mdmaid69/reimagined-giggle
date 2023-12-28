921dd12497a1a910a02e43e7dfa78356e1e77550
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:12:46 +0000

    thin appearance match night control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
