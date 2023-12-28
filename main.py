d8e21f36a944a31a5d05efa543213c3b9280f85a
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:13:22 +0000

    calmly fair warn draw anybody

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def is_palindrome(s):
+        return s == s[::-1]
