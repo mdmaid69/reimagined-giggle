69cdd5a37d00e11934d843848c4ec54c28705fdd
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:43:19 +0000

    hear purpose well regret exist

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
