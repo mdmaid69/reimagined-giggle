1b5875cd0a45b1b8a3db6a475008c8184d6c32a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:29:52 +0000

    independent go jump unfair interest

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import json
-print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
