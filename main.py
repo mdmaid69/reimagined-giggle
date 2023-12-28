e813ea9dbf1f17eedc0f10600e5efe4eea2c0d09
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:08:05 +0000

    slightly bang tune separate with

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import array
 def get_array_typecode(array):
         return array.typecode
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
