b0b7f1df98ab0cf614cbc93e5e9e9ce49c3acbfd
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:07:00 +0000

    accidentally expect relationship bite mix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
