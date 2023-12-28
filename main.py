9562222c75f230b874991536c8dc993b9812a450
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:07:45 +0000

    concerned mad burst something always

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import datetime
+print(datetime.datetime.now())
