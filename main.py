0ea3e070ebda3b6ae3c233deb364b576e76011c3
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:32:36 +0000

    tonight wake persuade perfect like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
