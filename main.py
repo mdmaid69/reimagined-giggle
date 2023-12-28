bb044f3edec6e3c7f40a195d740ee789f3cf4cf7
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:29:33 +0000

    arrival future surface woman rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
