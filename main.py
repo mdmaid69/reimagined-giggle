c49c3dc825470eee749886498b2f60f3c438b7bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:36:16 +0000

    culture talk contents store key

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_area_rectangle(l, w):
+        return l * w
+def convert_to_binary(n):
+        return bin(n)
