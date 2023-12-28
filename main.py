09bf4bab70059d3d8c69571de027f8f44977c320
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:24:28 +0000

    lucky match guilty embarrassed swing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
