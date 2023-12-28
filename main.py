4ea6d8a82815d38d6ed0444475de75666f95b1e1
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:30:32 +0000

    firm press strike entry body

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
