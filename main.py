6f42bc27677347a86ecdca21c0c93517dec828cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:50:35 +0000

    pull suit conclusion dig tax

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import sys
-print(sys.version)
+def calculate_power(work, time):
+        return work / time
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
