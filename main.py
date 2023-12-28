575055c51b015d386d44ab51ab5451425ce92a25
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:10:08 +0000

    recognize exploration heavy height free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
