55cebdf0b3fb26301d69247b961212688d52ceae
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:44:18 +0000

    gap light platform plan train

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
