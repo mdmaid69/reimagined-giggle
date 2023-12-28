e9443604d6e2037374902340885724ea40ac869c
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:53:32 +0000

    by justice clearly same stretch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
