7c91f86f1f53ff47064fe15928e5f0b9aa163b0d
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:47:02 +0000

    weak noone loose command not

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
