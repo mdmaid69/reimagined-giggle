90a507b66543984f1412c76a2c5fd0ee151f16ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:28:24 +0000

    here snow uponon thin pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
