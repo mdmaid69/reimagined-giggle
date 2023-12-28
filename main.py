fcefe1e72f10486025a5b46f525f4fd23fde2052
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:43:19 +0000

    smile ignore clear cold play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import sys
-def exit_program():
-        sys.exit()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
