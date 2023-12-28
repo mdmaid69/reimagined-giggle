afd5894ae411e99e6f7f9d25af4a6bd32c73b244
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:37:26 +0000

    bring especially visit front offer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
