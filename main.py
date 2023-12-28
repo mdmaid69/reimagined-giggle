b9645980384326049f59ac6fdc824717ee902b77
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:32:14 +0000

    interest machine prepare throw comeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
