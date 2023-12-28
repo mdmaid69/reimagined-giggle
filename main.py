a9803f630488467469189c18a5d5373eeca89c86
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:41:09 +0000

    advanced perhaps then second fight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import time
+def get_current_time():
+        return time.ctime()
