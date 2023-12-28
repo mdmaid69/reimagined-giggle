f7ed476d1ab4f68ec9cff6024476d383309702f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:00:34 +0000

    power success opinion beneath late

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
