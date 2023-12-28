e2328db565b9da96bde9be3caf5071878723090e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:22:13 +0000

    for suffer snow remark mainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_volume(length, width, height):
+        return length * width * height
