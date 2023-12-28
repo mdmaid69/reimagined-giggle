51740b8d58cf7984602b4b6ff8de880f52d0704e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:17:57 +0000

    area their precise excuse own

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
