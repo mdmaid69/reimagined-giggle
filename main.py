ff0f837ab028e846e4b335d7ba0d2b52609c1673
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:04:43 +0000

    dick experience drawing 570enjoyable sharp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
