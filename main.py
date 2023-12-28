e8a32c39d45d49b798b7bf57bc6cb09dd96b2908
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:41:11 +0000

    through what accidental treatment test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  def add_numbers(x, y):
-        return x + y
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
