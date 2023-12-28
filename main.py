4fda9bee7ecb2413e1e5e4d5a7dbe2a4af7f55ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:02:43 +0000

    play congratulate across before get

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
