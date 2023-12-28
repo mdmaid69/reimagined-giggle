21dd6362b436ec10eac5196d250e83b2da515e03
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:57:11 +0000

    schedule wish bread steer say

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
