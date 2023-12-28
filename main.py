cacb6156bdc860d316db57d785e4151c9af55d75
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:19:13 +0000

    aim gooff yesterday vanish terrible

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
