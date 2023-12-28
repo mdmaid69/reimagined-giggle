788ce90af86bd1c9e6476c791f39d53108d46365
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:33:29 +0000

    sharp receive reason transparent cover

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
