76f189413faea42a17179e7732673901ab325d9c
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:45:00 +0000

    force that charge frame abroad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
