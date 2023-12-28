e0c08864751c087e04927f6a9a33f29ad8cdd124
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:38:11 +0000

    rest several extraordinary instrument Ii

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def subtract_numbers(x, y):
+        return x - y
