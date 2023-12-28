9f14e8140a78ef6d56f1b29f8df395e23a0033bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:43:13 +0000

    aim unfortunate experience now she

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+def exit_program():
+        sys.exit()
