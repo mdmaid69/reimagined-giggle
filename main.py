b99d90952a3b7fd3e578e12e435bb64a31aa558e
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:08:07 +0000

    murder yet somewhere start easy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def cube_number(x):
+        return x**3
