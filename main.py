81b45ca88275b8d2dc13e37bf266775188649df2
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:06:29 +0000

    point shadow book post pattern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
