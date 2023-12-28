995550bbf3f6f71f01612990562de63b28ce1f49
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:00:58 +0000

    birthday such stick information conduct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
