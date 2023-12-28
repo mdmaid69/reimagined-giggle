3bb93cf57cf4312bd2e829b9d49700db974dfd67
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:41:20 +0000

    ought ask measure burn recent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def square_number(x):
+        return x**2
