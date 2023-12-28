345da3bad01e7de624c26a387d5b290f361d675f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:23:30 +0000

    lead recover ask quiet feel

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def split_path(path):
-        return os.path.split(path)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
