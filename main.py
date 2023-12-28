cdfa4466a7eaef83ad62ff72af01cad7536e631d
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:06:04 +0000

    exercise cover bite silent agreement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
