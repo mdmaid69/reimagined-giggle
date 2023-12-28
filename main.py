fea6e2dd9bfaa16a143de723937f240d20fea939
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:09:01 +0000

    dinner string across sting sing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
