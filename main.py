161e54850e43e4618e5b396d5bbd7e84d5e57fb9
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:04:32 +0000

    getup strike operation slice extra

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
