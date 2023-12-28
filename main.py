407f0807777cf33e40549e9e45d9d0be5ded8faf
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:50:38 +0000

    but short free tell include

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
