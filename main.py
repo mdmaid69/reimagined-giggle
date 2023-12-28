faed3ed6b97d41d8074363eb790ab8391fe01baf
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:55:28 +0000

    shock violent Tt spring quality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import platform
+def get_python_version():
+        return platform.python_version()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
