eb69c0a63e046e0bf26203b2289118f94901b2db
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:51:25 +0000

    hear avoid government doubt rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
