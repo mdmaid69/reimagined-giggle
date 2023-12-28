147880f3d08ccad8d99e95f6d9bdcd62a78eddfd
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:20:14 +0000

    business shop platform callout turnaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_list_from_array(array):
+        return array.tolist()
