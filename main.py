d114f45f6e1e64aa384c2c99aff24b52446496e1
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:05:35 +0000

    contact environment lesson exactly curious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
