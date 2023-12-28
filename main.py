60fac2855b09e39bbb8cb654862f974683da08f1
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:11:32 +0000

    spring replace whistle water give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
