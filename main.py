c03a99fe9be942c9d3050c66938a6117456a6062
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:43:07 +0000

    fierce perfect wander act weekend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
