792b05af62f27627279ac564424d22990ff98a71
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:25:44 +0000

    meet flow task hold middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import sys
+print(sys.version)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
