3d3c205fa03d4ecd81780648afbc9f95d15a944d
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:20:49 +0000

    wind 179bite tongue nerve correct

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def cube_number(x):
+        return x**3
