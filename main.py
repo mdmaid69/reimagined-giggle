beec73b73d50dc65b703512d45304fd9416134da
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:08:26 +0000

    squash food otherwise drink suit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
