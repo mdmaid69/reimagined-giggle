6734643a3f51c392789e01eff82c2c7e3d9a5b27
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:57:50 +0000

    sore sad date after on

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_user_dict():
+        return collections.UserDict()
