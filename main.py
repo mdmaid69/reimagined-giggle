e685bd8d4afa06d7e149a35141bcd4b0953202a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:42:14 +0000

    high even direction skin nice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_current_directory():
+        return os.getcwd()
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
