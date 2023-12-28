8fd09bed5a327dfe073085e16ae334dad1fd6bd8
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:29:05 +0000

    weigh early plan address block

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def is_even(n):
+        return n % 2 == 0
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
