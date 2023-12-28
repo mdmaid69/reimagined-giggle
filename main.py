c3c8994868fc7a478293216e262ec02ba5606c99
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:38:18 +0000

    sometimes actual exactly total argument

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def count_words(sentence):
-        return len(sentence.split())
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
