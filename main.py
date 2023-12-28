53d824f7c2a824c8c86cec83fd5f4125331409e2
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:12:05 +0000

    open stone identify father content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
