72761c42c01d92717242c375487aeb25fd88ba0d
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:48:43 +0000

    fast control related crime machine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_density(mass, volume):
+        return mass / volume
