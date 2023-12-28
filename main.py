52fbf987b9bc8276f3731753e9198b0c523d94f2
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:39:20 +0000

    apply miss might left peaceful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
