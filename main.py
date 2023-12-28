b6e8c3c6123022b0f39e2c80ea0fed21f581818d
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:14:18 +0000

    steer dry guide letter black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
