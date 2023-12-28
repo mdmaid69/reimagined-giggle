ef4c0a4225fe80cdbfee7a22cd71e8ed5818ab43
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:23:52 +0000

    mad aware tune black floor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
