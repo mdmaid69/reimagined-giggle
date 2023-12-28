c422a1497e5dffa487f69a41fc286c954b840627
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:59:34 +0000

    other can uponon for polite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
