cec9f3abd51f49aad92fdd7f27edfe3e75e2c9c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:02:28 +0000

    film reply black tooth notice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def print_python_version():
+        return sys.version
