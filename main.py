8fa6fbc4671e333096c92dac20b08ac313f67e24
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:40:13 +0000

    none sleep plenty temper exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import os
+def remove_directory(path):
+        os.rmdir(path)
