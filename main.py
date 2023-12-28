2e9b5e2a4722f1e30044eb80b3b42c3146fda174
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:11:01 +0000

    butter report season bath reason

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
