e11859107baf81bf833e07aca14a66e0a43f5c80
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:15:32 +0000

    priority reliable criticism food alive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
