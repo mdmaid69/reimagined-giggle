cbd673348a51a95c62e8dc37507f0aa0a988b248
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:00:11 +0000

    possible sure downstairs Ii mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
