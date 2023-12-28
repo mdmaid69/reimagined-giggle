60e9accd175260ac9e97da67549caf9dd5766aed
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:15:55 +0000

    thick interest drop port lookafter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
