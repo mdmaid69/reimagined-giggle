b21b19c01a7052d9ef7bffd74e0653ae0df41e37
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:53:18 +0000

    situation oil make situation feeling

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
