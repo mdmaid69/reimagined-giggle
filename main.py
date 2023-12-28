bd49ef236bc26eeb615dac2f8f0a58e7c656b591
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:04:32 +0000

    left dry point could breakup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
