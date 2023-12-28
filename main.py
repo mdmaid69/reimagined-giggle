61823d85b4176604116fc662f347db45900a7354
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:04:23 +0000

    depend chat set mother after

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def reverse_array(array):
+        array.reverse()
