4e311c8bec0b5fdb2d059c9e99e8558e62deeefe
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:12:54 +0000

    careful measure fall together disagree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
