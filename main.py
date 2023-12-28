5c40026ef53029a7ac7da15ef597de176b9d4018
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:54:53 +0000

    runinto already flight sure typical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
