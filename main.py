a01b5094fb309dc2f4efe423a7519ba563d49ef2
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:54:52 +0000

    average distant reject tape yesterday

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import logging
+def log_message(message):
+        logging.info(message)
