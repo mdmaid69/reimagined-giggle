5d2902af207471656e6163f102852d9187272e74
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:13:57 +0000

    ill answer popular news terrible

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_acceleration(speed, time):
+        return speed / time
