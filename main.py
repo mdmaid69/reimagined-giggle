a6df9390b49beac92dba6b4390678a4c251d1980
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:02:31 +0000

    shout order press sticky similar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
