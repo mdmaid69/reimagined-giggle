bef6cf8652e0722f068c5a525b276c72138416dd
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:32:59 +0000

    base spray pleasure row ill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def find_min(lst):
+        return min(lst)
