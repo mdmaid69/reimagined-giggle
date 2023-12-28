265c67f4169b807ae793d1383ca2ae9e8bef9a56
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:22:18 +0000

    turnback else foot sit thin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
