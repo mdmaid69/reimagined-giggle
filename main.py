0bdb6789a92e01b2855be8ad9eae138fc979a223
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:06:31 +0000

    hello wonder safe opinion event

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def delete_file(file_name):
+        os.remove(file_name)
