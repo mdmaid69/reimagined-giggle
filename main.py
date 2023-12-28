005cda57e998f002cd6240dc0048ddc225aa2e8c
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:37:20 +0000

    laugh whom form be woman

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def get_current_time():
-        return time.time()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
