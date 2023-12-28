2b2f37d438bcb2d81462122e51e3af25e807eae8
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:29:52 +0000

    pretend correct name talkinto slow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
