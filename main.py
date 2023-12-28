82a20a1397fa31c72496d78e4316473932b6e3fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:07:18 +0000

    want law pull worry alone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
