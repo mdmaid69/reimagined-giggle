0dbb26ef92671e91f4d56ddaba23c6b91302dc3e
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:23:35 +0000

    relative aim niggaacademy control board

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def square_number(x):
+        return x**2
+i = 0
+while i < 5:
+        print(i)
+        i += 1
