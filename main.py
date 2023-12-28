2b572e1287d5bc67b55cf64bccea156c3d0bc228
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:28:51 +0000

    congratulations cut temperature strong until

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
