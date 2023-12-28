22f93ed6c3d9e625b9d1507a5039c9a3e8194c40
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:11:40 +0000

    low entry swimming report tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_even(n):
+        return n % 2 == 0
