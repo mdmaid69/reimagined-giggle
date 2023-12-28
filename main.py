9cee16952d6a302985e50ad2dd5e4b051aee3d09
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:36:29 +0000

    common total ability polite only

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
