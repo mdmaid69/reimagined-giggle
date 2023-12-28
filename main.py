8a1f6a0be63a8422538e3f7016ba515f7ef03ba5
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:35:49 +0000

    compare concentration goon business splash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
