add9495357927fe2aa64e8e9df85968cb724444d
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:07:22 +0000

    score welcome tape hand relation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import re
+print(re.match("h.*o", "hello world"))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
