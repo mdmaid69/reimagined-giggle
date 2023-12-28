31d004cb91bc98ca50f23cbc9d8567536bfa1fd5
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:06:55 +0000

    sad gas choose steer allow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def subtract_numbers(x, y):
+        return x - y
