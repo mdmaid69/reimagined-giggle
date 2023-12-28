0eb3a629a79f734ac63d1b67ec07847b66f8eb11
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:53:37 +0000

    finish present emphasis send borrow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
