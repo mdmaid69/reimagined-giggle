812bee2e8331342d0ffb88003a6047f0ca9497a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:43:48 +0000

    among likely protest argument today

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
