c610a2c7f9d4edadd60e605ecb32bbb3d4142612
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:27:16 +0000

    difficult nurse dead bill length

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
