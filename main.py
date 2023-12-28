e2cf28d4487e7a1ac37d2dca8ae06841db2de6ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:38:04 +0000

    only once how suggest keen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
