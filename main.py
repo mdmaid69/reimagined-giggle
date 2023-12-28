8e5b01004284825fca8e39471c9a02a436b9a0fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:24:00 +0000

    birth little justice as further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_user_list():
+        return collections.UserList()
