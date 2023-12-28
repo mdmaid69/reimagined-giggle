d48f4a1c4b377d9c1a5378379a1cf8f148bf75c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:05:58 +0000

    search march whatever turnoff present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
