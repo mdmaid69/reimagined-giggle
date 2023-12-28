6807ece7b3610944bbe1c1b63224e83f6fd5d30d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:20:15 +0000

    shop runaway by statement care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_power(work, time):
+        return work / time
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
