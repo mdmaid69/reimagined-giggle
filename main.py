ff6bdff6ecc523870cfc1672514ef60ee0fc9389
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:23:38 +0000

    smart mistake approval description maybe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
