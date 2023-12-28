d0250ff36ddb25b7203401cb30519728df4a1fbb
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:52:30 +0000

    worry unfair shall at sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
