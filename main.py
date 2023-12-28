80ceadc1d7592eb6f4eead3275a65ffa845ab96a
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:56:31 +0000

    trade interested yours more comeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
