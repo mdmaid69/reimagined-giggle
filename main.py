0d781e8e44c705ae9fa49ceab7a52519b9f0d31c
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:45:36 +0000

    565end easy disturbing order negative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
