aefe93777b1d0959341f26d285725695e031a72b
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:02:29 +0000

    mix slowly load movement scare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
