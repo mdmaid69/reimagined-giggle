1d956376ac4bfbbd50844a8416124b5d750bf9c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:18:59 +0000

    set style key separate pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
