7781d35c3c2afa49505f7a2c0521d3f6292131fd
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:48:10 +0000

    like score position collection represent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
