a90c0e78b7e26abf26fe5f97e71ed16d83626b5b
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:53:05 +0000

    slice tone note defend everyone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
