3e8782e399ef5d4bd4484abe76cfa7cb35345a01
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:09:56 +0000

    solve treat exchange flavour expect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-import datetime
-print(datetime.datetime.now())
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
