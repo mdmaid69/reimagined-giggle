ebf8be0fc6e73d332fe026ba824533281fa229f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:58:56 +0000

    style street turnup natural contact

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
