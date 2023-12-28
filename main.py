ec6ca340c902f297def3ff14f8fe2d347c019976
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:58:49 +0000

    fetch shadow goout meaning surface

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
