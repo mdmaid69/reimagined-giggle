f7546ee3db576dd2f0dc8a6f0308b97b654dd513
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:19:39 +0000

    there closed month match mention

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
