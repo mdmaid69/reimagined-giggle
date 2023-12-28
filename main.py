57024a50d05736fefab18742f0090d7290d91d93
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:28:32 +0000

    subject thick rid brown everybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
