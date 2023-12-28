37206757feb1f28c6cbcd8c274072e18741f38af
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:06:00 +0000

    behaviour getoff yes look excuse

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
