bcbcc8dc08d8e387894ba7415b89881bb16a9a45
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:30:07 +0000

    plenty since majority face demand

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
