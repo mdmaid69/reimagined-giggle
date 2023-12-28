c22ab12b788613954450d1d7849120e19ac0daf7
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:24:44 +0000

    cheek undo trial exact clear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_as_set(array):
+        return set(array)
