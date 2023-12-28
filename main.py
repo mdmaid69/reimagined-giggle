02e174d1ef4ab8116c672b63bae203bff897360d
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:55:12 +0000

    compare rid busy add spot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_acceleration(speed, time):
+        return speed / time
