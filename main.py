d0afb2b6f5ffb637e4a347e91b35d2b05a3b05a1
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:31:43 +0000

    sleep combine getup thank comfort

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
