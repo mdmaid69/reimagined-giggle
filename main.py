14846b5eaebaf3729b3eff3d1971c9943ed47933
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:11:52 +0000

    cross terrible building fresh through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
