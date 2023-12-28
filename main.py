3d0fca7a3dc5fd898accbe2a9471fc67aa9cf663
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:36:36 +0000

    road bear mouth star manner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
