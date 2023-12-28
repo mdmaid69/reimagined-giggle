6ca20649c62905e0ee68db96ef18fae0fb189088
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:23:53 +0000

    sometimes cut back complete attract

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def is_odd(n):
+        return n % 2 != 0
+def is_palindrome(s):
+        return s == s[::-1]
