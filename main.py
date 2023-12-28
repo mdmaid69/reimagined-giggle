19e14879736b1d973bd243e2fe45cdf975561c10
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:01:05 +0000

    relative step help dig expect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
