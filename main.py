e8f093464b68d6e9cfebf53dd48b9dcd1355361e
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:55:33 +0000

    talkoutof treat plain scare dress

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def add_numbers(x, y):
-        return x + y
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
