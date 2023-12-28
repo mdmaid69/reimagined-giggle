875251a9cd2bc57a29e46ecfd3b0ed5633907d2c
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:57:19 +0000

    quarter attempt rain brown them

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import sys
-def exit_program():
-        sys.exit()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
