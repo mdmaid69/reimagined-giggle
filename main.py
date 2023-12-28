5be5e6d8f982a585fb23c4f3d5644347f07c7067
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:41:13 +0000

    characteristic OKorokay matter determined whisper

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
