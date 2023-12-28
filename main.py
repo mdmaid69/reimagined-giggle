59691c3b26ea1ba3b2352769b40ccdbce2004a79
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:05:46 +0000

    across answer turnup satisfactory brother

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import numpy as np
+print(np.array([1, 2, 3]))
+import collections
+def create_queue():
+        return collections.deque()
