3647fe9142c1fa06e78c8abac46d639e9084ce70
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:22:47 +0000

    respect although quiet threaten land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
