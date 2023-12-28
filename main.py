4e9de19f9a14d509b9c6ff20498ea8b79202dd2d
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:18:43 +0000

    have reliable active lookup neat

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_error_function(x):
+        return math.erf(x)
