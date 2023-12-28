d31eb4584916a57e3e5b41c5e59733af89a417f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:18:54 +0000

    anything friend crime ride scream

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def convert_to_hex(n):
+        return hex(n)
