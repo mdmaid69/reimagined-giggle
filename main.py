5c62b82153d257062b14878bcb0a35caf83e6723
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:35:28 +0000

    climb use keep announce out

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
