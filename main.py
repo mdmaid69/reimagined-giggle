905ddbf6e38b4ef4666858e75c31c13289e8519d
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:11:15 +0000

    strike about though leg inquire

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import numpy as np
-print(np.array([1, 2, 3]))
+def convert_to_hex(n):
+        return hex(n)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
