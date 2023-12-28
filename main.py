179be007ab65d0881193891382fb7ec2499ac7cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:02:25 +0000

    around shop onto mine sleep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def find_max(numbers):
+        return max(numbers)
+def add_numbers(a, b):
+        return a + b
