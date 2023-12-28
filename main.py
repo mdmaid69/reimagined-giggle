a36364725a3546d46727082db8afb2cf91a72102
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:30:09 +0000

    row pain decision measure steal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+text = "Hello, world!"
+print("Words:", len(text.split()))
