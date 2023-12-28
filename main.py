e957dafbabd61a48c1c426c0d19da821dbe2c2e8
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:39:32 +0000

    hurry downstairs bang special rule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_array_length(array):
+        return len(array)
