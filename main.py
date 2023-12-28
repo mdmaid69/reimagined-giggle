d98fe8a47a05bf56deb1feddd3dd7b482fb7c3e5
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:05:19 +0000

    study tape limit hardly but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import datetime
-print(datetime.datetime.now())
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
