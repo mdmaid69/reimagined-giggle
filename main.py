2b21db6203554d23cfb33b1da3065e75aefa7020
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:17:56 +0000

    neither while stir keepaway easy

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
