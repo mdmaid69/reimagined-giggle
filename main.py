1d9b5ea32042d323ba98912dd97b0145ae2b39d4
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:17:55 +0000

    move shut trade black base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
