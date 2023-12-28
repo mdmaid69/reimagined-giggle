402436b9304137479a67f8ae2c234860690892fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:21:51 +0000

    congratulate slice later criticism difficult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
