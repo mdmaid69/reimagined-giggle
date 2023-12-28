7be5e841a78640eec51cc54d0858a699339de266
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:55:25 +0000

    ring along shop television Bb

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
