c1a94be8e21a61a2429dc93527381c1cbfbccdad
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:38:24 +0000

    rain free schedule prevent moral

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
