28cc1d93a3f69721f5e1ad851e84963b0f986c52
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:26:23 +0000

    sort passenger during goback(to) could

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        return sys.version
+  def reverse_list(lst):
+        return lst[::-1]
