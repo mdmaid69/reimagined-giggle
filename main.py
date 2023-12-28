1d2c42c14cb77b87c09868574ce4972d1475f7f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:31:59 +0000

    callat wash chief create home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print(sum(range(10)))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
