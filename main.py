5deb861b17cc476ff2be2c7011a80a7e6ec02160
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:29:07 +0000

    Dd wonder achievement forgive dull

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
