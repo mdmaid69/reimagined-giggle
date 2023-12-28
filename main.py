bf70cecc1e058a5a01c266ea571cf7e02f6fa66f
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:43:19 +0000

    deliver bear present arrival debt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+x = 10
+y = 20
+print("Sum:", x + y)
