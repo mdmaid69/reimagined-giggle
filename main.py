60aafb9e486f78f3d4915ac44e1ece45d6d20aba
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:38:31 +0000

    sell small note importance earth

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
