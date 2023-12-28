8c0009f9df73c0f6e66bfa6723e6aee3e5b59e07
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:01:28 +0000

    sensitive opposite switch walk team

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
