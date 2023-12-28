fda0c69eb07e157efac81d2821c7e0620815a42a
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:27:44 +0000

    manner rely everybody from giveout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def add_numbers(x, y):
+        return x + y
