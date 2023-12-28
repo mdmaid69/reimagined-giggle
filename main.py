7c62b289d5eee6367573cc88ba51312961c1a3f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:00:59 +0000

    getoff step hear plate bit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
