c3e3fd4d4d26070f15b119776ce5be071e0685bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:27:50 +0000

    expression honest geton smart level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def count_elements(lst):
+        return len(lst)
