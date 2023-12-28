1c3b6b0baa321163de44ad34f334f71d72d38ae4
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:53:06 +0000

    break brave excuse touch matter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
