d1b6d44e507ed43404557d817cba7e189216f666
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:55:38 +0000

    turnoff contrast large bright abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
