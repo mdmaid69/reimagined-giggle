260a24a6006064129731284b837ac46146cf5a01
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:32:53 +0000

    sudden national exchange tense callback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_octal(n):
+        return oct(n)
