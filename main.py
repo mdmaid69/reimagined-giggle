6ed321d81f6dccd40560362167aabbbbed97fec5
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:14:52 +0000

    tense rain yes pipe damp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_itemsize(array):
+        return array.itemsize
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
