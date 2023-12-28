519294eb8c6bd2b1d828c54f60a7cd9d9e06e717
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:18:28 +0000

    guess beat pay spot rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
