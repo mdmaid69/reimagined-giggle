ed42ed75554c6abffef7c0a165f03dfa7815da18
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:11:40 +0000

    seriously honest method quantity interfere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
