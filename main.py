d4a6bd7d7b1ef005128d52d5056abde28f4efecd
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:26:22 +0000

    left Yy frame dream flat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
