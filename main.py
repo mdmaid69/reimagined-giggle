98fb4262d083f88fec3ea6c16a0bc3ff142af977
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:13:44 +0000

    unit feed broad recent takeout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_cosine(x):
+        return math.cos(x)
