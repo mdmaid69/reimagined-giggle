f5e5a7687ffa1113d4c0ffb1dec668536204f428
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:14:25 +0000

    cold comefrom gather argument promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
