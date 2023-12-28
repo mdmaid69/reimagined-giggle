45ab5d4dd67c93aca60d5a29752742c9323ad07a
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:39:06 +0000

    rub card pretty vanish relation

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
