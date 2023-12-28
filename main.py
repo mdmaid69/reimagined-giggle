a24899aa2813d209b83677974bd093865f5cb737
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:02:59 +0000

    manage pull team guilty lost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
