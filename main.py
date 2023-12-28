a008db3bcb32943c25c2c6d49ccdff0ae492e477
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:21:04 +0000

    voice value house once check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_area_rectangle(l, w):
+        return l * w
