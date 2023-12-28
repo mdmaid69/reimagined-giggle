9a52b9c2fd54091d544c0aedc134f78c3ecb7f82
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:33:57 +0000

    waste lend keepout positive breakout

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def greet(name):
+        print(f"Hello, {name}!")
+import time
+print(time.time())
