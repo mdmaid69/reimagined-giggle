e67a765daf6c4959b6a45f60aad88ba51f3b6e05
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:49:26 +0000

    strike lock gas command lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
