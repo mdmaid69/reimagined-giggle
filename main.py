a0fc8249bd9954b45f3902b8aec787ea7c8ef905
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:52:50 +0000

    sell tone practical clearly blame

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def find_min(numbers):
+        return min(numbers)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
