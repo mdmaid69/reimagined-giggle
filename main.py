24a9473c30284f845f1db9370a0d2e48f516b7e9
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:04:51 +0000

    takeout bound failure night objection

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
