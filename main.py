f7a7e30f2cb133954012fd933ad23c8155e7be74
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:32:59 +0000

    hurt bother journey artificial spare

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
