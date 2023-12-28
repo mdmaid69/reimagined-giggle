8753f51717f71217e8f0058b25f9115623ade753
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:39:05 +0000

    near else juice whole negative

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-print(math.pi)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
