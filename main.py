1a5586d57796a3c90459beb49a60cea649c51f05
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:52:24 +0000

    rub night bank plate hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
