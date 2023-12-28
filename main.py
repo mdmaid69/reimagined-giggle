dac368fddac561a38b6623866ce8cddf57e118e7
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:06:39 +0000

    recent deliberately duty start imagination

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
