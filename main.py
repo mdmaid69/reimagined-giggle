9278383fdf6e6f78cae984739ecd96d4b7deb4ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:44:11 +0000

    best yes of ticket coat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
