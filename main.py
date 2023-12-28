535f11c7d6dc662c19a5eb6f9a7369479f6af1d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:24:54 +0000

    mistake blame completely remain evening

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
