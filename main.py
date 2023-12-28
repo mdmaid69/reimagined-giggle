52ae1d736cf34bbfb2ea259ec7d76aa6320901f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:28:00 +0000

    Hh ring spoil band politics

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import sys
-def exit_program():
-        sys.exit()
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
