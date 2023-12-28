673044f079e93afea03ced1d84ea6309add323e4
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:57:44 +0000

    grade drop reasonable able division

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
