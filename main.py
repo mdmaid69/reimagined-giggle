1e849b391c1bbb6d2b61ad6a127be3f3f8974a77
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:24:21 +0000

    often out unite respect scared

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def reverse_list(lst):
+        return lst[::-1]
