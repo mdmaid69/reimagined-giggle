3b7b717c93129aed84ea920db4fbf86434c3b627
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:38:42 +0000

    stare old question giveout dangerous

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
