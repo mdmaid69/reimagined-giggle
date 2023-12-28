59fd344606142f8945fd72642877ed71f94611e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:47:34 +0000

    party necessary thirsty slice middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
