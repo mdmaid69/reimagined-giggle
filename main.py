ed5a9ff693d75ab4ea2c4dcaac8d5c34ee0ed43c
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:27:49 +0000

    dull loose sick square ilovedicks

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_speed(distance, time):
+        return distance / time
