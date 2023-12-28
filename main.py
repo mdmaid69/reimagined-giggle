251e6ead27426abc104ee5c120527732ff116e1b
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:39:55 +0000

    question strict coffee trust comeout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
