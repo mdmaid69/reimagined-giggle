f62576774d0b97854b0e3d14f38d838bcb21eb34
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:50:50 +0000

    mouth dinner stage head occasion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
