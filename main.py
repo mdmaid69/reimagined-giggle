4da9bb546d31a9d037c1f5e6f7046fde2bee82f1
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:16:53 +0000

    stop where left complete passage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import time
+print(time.time())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
