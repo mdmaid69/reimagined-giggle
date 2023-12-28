65a6d7c17dac48cbb5dbbe2e954a84a7918a81f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:33:22 +0000

    freeze cut accommodation pride point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_rectangle(l, w):
+        return l * w
