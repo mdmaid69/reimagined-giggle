8fc7d6f6e2622613258e276ddcdc95ff17100ebd
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:52:53 +0000

    score rare responsibility stranger fast

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
