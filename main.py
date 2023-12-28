970950dc689ba85d591799738051088c9085a1db
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:36:11 +0000

    light hello comfort become less

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
