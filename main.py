d721c0f27782cbc77d4ae4e1fa33084406375b90
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:41:32 +0000

    jump block Dd stage influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
