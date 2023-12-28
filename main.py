b4ade22dbcdb13be58e64094decaaf6e6cc68bb9
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:25:55 +0000

    listen callup clear genuine sound

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
