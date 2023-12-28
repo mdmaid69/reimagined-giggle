02726493c163210362f7eec3a1b5fe01040ccf77
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:30:12 +0000

    forget future activity everybody hole

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
