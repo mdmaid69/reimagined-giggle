49b27bf2132a8c84c060fdb220dd789d70c3902f
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:50:53 +0000

    common my voice accommodation impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
