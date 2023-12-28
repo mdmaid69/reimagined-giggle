a29fe5401ebdd58fe862ebe7ec531ceff04428b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:37:45 +0000

    slow silent wide weak stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
