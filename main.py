2354698f9a3b47964a0f53bbfd271a8c5af76636
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:12:46 +0000

    realize pressure minimum snow minority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import datetime
+def get_today_date():
+        return datetime.date.today()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
