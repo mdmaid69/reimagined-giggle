ced59334131e44b689f4ffbec6e3074adab00c66
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:56:12 +0000

    bite independent door active holiday

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def reverse_string(s):
+        return s[::-1]
