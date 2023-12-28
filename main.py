4ee7f52fc80d05179d20ece733759f2e887727b6
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:24:00 +0000

    pleasant judge light hard surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
