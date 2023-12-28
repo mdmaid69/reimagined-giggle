81f0199e59a92530db1b7a6df19f3fdc49be52bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:46:38 +0000

    neither pretty regard their demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_current_working_directory():
+        return os.getcwd()
+def is_even(n):
+        return n % 2 == 0
