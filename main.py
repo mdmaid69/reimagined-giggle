1eaedc40bc697b2e1b91c8ff2a4e3654d2964de4
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:05:35 +0000

    sound centre confused basis eye

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def clear_array(array):
+        array *= 0
