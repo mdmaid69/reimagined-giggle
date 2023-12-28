919f0ad55703bac0698e52e98cd3fb890f578c66
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:04:34 +0000

    target birth Pp catch nurse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
