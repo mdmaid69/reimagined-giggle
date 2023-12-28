fad9d569c8ee8f5596c607cac250db37ba1b3897
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:09:08 +0000

    workon runinto pause handle book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
