19a8b871bf1e9f5ce4476b8fcf6f34a85a906fab
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:52:44 +0000

    container he murder deep slight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import sys
-print(sys.version)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
