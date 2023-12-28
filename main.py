3d3b75b91d2d5ef1afe5a363cdcaff605d071af8
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:11:31 +0000

    cheap matter slow spot match

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
