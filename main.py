70752dd90d2f1852f8cb5058d2a921ceb73dc905
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:54:09 +0000

    noisy makeup mind callback turnaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
