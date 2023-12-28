6b4613d3ea59cbd1f757cc19b70e174f9d673cbd
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:07:25 +0000

    sister ordinary appearance lookback(on) attach

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import time
+def get_current_time():
+        return time.time()
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
