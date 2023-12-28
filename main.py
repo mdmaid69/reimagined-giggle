a53ee31745fa9eb1af3e8a134effd36a1553e9f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:08:06 +0000

    discussion where there usual advantage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
