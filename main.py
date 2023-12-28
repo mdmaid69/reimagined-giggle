3c65269b0bd1535ca86de0ebeb46ab0da1462360
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:14:10 +0000

    pronunciation journey fast factory moment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def count_elements(lst):
+        return len(lst)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
