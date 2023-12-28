99eab1cff09584741fb06a6cf276f940b2cd4f70
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:31:48 +0000

    closed state giveaway habit bunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
