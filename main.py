624747c0c349bd0af186dac8c7bab4137f7bcfa8
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:10:46 +0000

    new exercise progress contact Pp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
