269c2bc0b64389d07953a9c5f376e2a270701668
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:22:32 +0000

    seat Jj neck pencil fact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import sys
+def exit_program():
+        sys.exit()
