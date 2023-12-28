b00ca95c5e80924027747ceeee5814713e5ee35a
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:05:33 +0000

    blow concern stick favourite runover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import random
+def roll_die():
+        return random.randint(1, 6)
