f9683f7c27b4d40286b58332a02d28c500858572
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:48:18 +0000

    out slow clearly lucky ourselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
