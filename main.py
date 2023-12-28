13c93bb7d043209b0141b1ce2d8101d041f1a172
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:34:30 +0000

    compare impressed than excuse present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
