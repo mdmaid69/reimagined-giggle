e894a1463c9ea6ab8d3d1178eec1f51186b3d65e
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:54:16 +0000

    complain distance needle share official

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
