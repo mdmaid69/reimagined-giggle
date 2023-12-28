f10faec6610ab35797032ba51d2a2f7464dbfd17
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:29:15 +0000

    talkinto worst exam lookup makeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
