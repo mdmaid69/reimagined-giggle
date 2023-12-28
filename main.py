02fd482e8320bef14c3cac75f134e9d2d33e0689
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:48:01 +0000

    opinion miss division giveout bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
