92d9c7c68aa42608faad261f6e36dc3176346107
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:21:26 +0000

    skill stare wise increase hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
