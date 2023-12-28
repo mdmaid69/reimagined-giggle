5788b358ec01009cc7c18105df00e7d9c4c6c931
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:41:08 +0000

    skill slow wave kind equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
