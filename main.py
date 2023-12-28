c64398618f59b2d2c39b556c48d624e2e03b25e1
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:46:39 +0000

    round circumstance common law quick

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
