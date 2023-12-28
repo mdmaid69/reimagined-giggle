1f3dce89b9813be51d052c489e5aca4888b273f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:20:37 +0000

    centre last authority could pause

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+for i in range(5):
+        print(i)
+import random
+print(random.randint(0, 100))
