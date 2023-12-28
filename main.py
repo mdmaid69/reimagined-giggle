4a492c099441c81473b182f0123de92b038a0751
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:17:07 +0000

    sticky breakthrough me loose cruelty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
