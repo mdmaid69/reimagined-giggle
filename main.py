11018529130852ef1c6932945b49686c75d8f888
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:26:42 +0000

    stop please suppose inquire better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
