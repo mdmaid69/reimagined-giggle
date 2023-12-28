72fa3efcb55712a81570a795751f1af7a04a32ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:36:04 +0000

    confused call usually scene clean

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
