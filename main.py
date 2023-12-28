b31edba07300a4c8478da97d61d9b23bfc41095b
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:17:04 +0000

    except substance wise otherwise taste

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def divide_numbers(x, y):
-        return x / y
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def calculate_area_circle(r):
+        return 3.14 * r**2
