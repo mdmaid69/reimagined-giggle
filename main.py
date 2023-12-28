71bf8ea000212307f86c4239dbe2736c8b19b9b0
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:18:35 +0000

    operate sorry cause difference listenfor

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def is_palindrome(s):
+        return s == s[::-1]
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
