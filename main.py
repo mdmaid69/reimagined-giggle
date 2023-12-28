1b36252d63b7a904c0d457530ce6cf77c190d10e
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:02:27 +0000

    sore however thinkof possible modern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
