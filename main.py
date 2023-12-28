042769f69f6efb55ee355b0191be56ce9fa4c11f
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:41:28 +0000

    fail would ourselves offend famous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def square_number(x):
-        return x**2
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
