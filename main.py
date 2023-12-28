a1be0e47b9b2b6304da0b9a1439463dff1bd3aab
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:44:07 +0000

    your show buy show pleasant

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
