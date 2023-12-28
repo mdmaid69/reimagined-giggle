9c510ae6a293489a72224f768d86f0a349577afb
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:40:02 +0000

    trouble workupto children priority might

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
