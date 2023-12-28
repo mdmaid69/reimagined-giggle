1945eb5afa3e27e2807cc0a2e77369a1f0d4390e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:55:40 +0000

    poor home dead flight acceptable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import random
-print(random.randint(0, 100))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
