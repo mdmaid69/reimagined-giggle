259d19608b57168824246cf7979ad5cbcf887e6f
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:19:37 +0000

    affect bound crazy warning add

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
