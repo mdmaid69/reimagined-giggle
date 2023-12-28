ccf33ed6a95e323f93e130090c4d398547d70448
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:30:23 +0000

    callout slide violent buy cough

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def check_if_array_contains_item(array, item):
+        return item in array
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
