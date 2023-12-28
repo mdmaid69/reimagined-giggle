8eefdcf1907cd969aa16e1a4eb99b866a5ef0566
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:44:22 +0000

    earth joke what shame tight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
