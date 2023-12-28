0877968c0c33b47e0c688a1e81877ca27daae624
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:22:08 +0000

    one through right kind brush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import time
-def get_current_time():
-        return time.time()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+for i in range(10): print(i)
