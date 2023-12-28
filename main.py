4a30f1f6f018e2ff571d067a2cb2cd1b41fdf1ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:21:48 +0000

    provide build order search hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
