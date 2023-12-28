585ceaa40b8368e864db0c2d92ceab9e24b0dfa4
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:45:32 +0000

    trouble importance punish prefer harm

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-print(random.randint(0, 100))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
