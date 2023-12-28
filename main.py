0ef6d7f3b75565d86ea14df6de5bb69a1a620069
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:19:16 +0000

    learn recognize cold awake piece

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_current_directory():
+        return os.getcwd()
