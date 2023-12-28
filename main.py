ae1264214ee047f53d3cb4c9be7a0973e4a66b94
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:00:36 +0000

    pupil frequent health temper right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
