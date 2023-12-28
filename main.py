5f19fd1dc0b21edeb2a5ccb37aad3716639e8fb3
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:59:40 +0000

    phone stick cost meeting flat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def find_min(lst):
+        return min(lst)
