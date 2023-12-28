e725a983be982bfa9cc153d5130d3acd86ea91f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:16:26 +0000

    soft extra whisper position office

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
