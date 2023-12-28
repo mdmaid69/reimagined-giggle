8d4cbbe4cce527bd00ea7b967f6acd6a9b0ebbcb
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:07:50 +0000

    broken fierce see move test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
