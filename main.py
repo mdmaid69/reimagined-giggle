2f024ee0830f7bbf850cd8f543ff5cc5321f9dac
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:26:44 +0000

    separate sit spin turnback copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
