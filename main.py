c2dc5a67ed24f75b5043ad8ecf27e3d645d1377b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:29:08 +0000

    ahead size tax through cover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
