dcc9029694c558a663cb2fdf62583af96676d247
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:02:42 +0000

    kick hurry blow properly cheek

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-print([x**2 for x in range(10)])
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
