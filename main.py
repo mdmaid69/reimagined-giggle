d6501f8c1df334567bd874e794c68db676c5e48e
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:47:05 +0000

    charge boat extremely before call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
