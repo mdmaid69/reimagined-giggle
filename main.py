19caf34736f6629e481c40ce12324f72ef193eb6
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:04:25 +0000

    hurry breakup afraid various suppose

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+for i in range(10): print(i)
