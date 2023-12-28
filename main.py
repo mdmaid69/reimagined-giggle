2ba216a484faff44c4cbe2353d4bb90166d7019b
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:27:39 +0000

    burst join eager lower press

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
