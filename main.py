950a27440848c0a5c31ae0dadcbef6f5b972d8c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:44:06 +0000

    lay rob book lunch intelligent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import sys
+def exit_program():
+        sys.exit()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
