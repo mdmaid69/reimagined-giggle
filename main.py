7086c8b7c6d9268773ebea8d4fe875a9c5ea9e71
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:13:26 +0000

    bottom contact boil damage come

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
