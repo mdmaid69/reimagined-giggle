9121d7689d1fa5cd2496bfdbeaa735b2feedeb01
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:42:15 +0000

    later song hair while punish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
