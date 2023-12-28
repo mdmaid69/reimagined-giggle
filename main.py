9cf450af033796b40e0cb9699c54dc72b2e4f993
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:14:38 +0000

    start explain dig ground cheek

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
