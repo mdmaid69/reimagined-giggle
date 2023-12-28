f4bd824fde0e17843b17cb22301a60518ecd3b7b
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:16:26 +0000

    holdup male polite squash failure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_even(n):
-        return n % 2 == 0
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
