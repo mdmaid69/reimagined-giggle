9dce79019cd56df0c513b74a8d80b11a90821216
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:05:45 +0000

    science bet beat anywhere once

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def find_max(lst):
-        return max(lst)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
