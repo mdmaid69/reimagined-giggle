d216532fb885654c3fa6d189fdbc7474b525b1fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:38:00 +0000

    visit think reaction custom fat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_as_float(array):
+        return float(array[0])
