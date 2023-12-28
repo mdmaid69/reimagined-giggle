109aa65e26da9b3c6ad7dfb485999e4de1070828
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:36:56 +0000

    reduce fire hers whoever succeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
