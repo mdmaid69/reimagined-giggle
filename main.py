81c37df81b68ff660f814f1f85373b5798ed2e54
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:49:58 +0000

    start full question at Uu

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_item(array, i):
+        return array[i]
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
