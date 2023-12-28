c899986caf40f4e4be61b6dccd0d5ba37ac8a14e
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:56:27 +0000

    yesterday describe government scare tight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
+def cube_number(x):
+        return x**3
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
