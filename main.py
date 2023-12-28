8684d5ce0af88506776d7e7085ae847adbb4087c
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:32:32 +0000

    achievement male ball protest natural

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print(sum(range(10)))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_as_repr(array):
+        return repr(array)
