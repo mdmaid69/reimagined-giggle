6cd42669f8b170f79a992e5fdce9e07fba33ef10
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:33:00 +0000

    through week minor opposite edge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
