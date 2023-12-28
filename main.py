07051e8b418abd6c1e407e231f549ffd74e94884
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:31:56 +0000

    unit mind keepfrom Ww collection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
