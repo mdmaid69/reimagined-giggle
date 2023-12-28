0919da8f7ee4adea8cd9e145c95eaaf0a86c1b03
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:57:27 +0000

    provide raw block confidence original

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
