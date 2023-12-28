38ece995dbf66e22639830a3147d3fc21861bb23
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:09:59 +0000

    idea unless bin know cry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_min(numbers):
-        return min(numbers)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_area(radius):
+        return 3.14 * radius * radius
