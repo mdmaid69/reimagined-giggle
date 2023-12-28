8de3ec08f4111cf80bc82f6a9da09dcde30c08cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:39:06 +0000

    change dance base war them

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
