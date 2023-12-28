ac05688be33aaa2ceef9d7e1eac9090c410ecb4e
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:04:43 +0000

    project last history remember frightened

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-def multiply_numbers(x, y):
-        return x * y
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
