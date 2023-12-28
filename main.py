f2848d39b52f85272999b454430a88dca79bcc11
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:25:45 +0000

    pretty smart unhappy vote wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
