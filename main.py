66edcbdf26020a37fd5a2c07a79fae1bd41dfa84
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:58:30 +0000

    advanced scream comfort degree call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_typecode(array):
+        return array.typecode
