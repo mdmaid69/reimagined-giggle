fc8c512255a466b27d4bd1061c8905c6ae604e70
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:50:46 +0000

    flavour wall land certain better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
