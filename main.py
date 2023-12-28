5a246daac7bb1d58a26ebcfdf4960b4d0b5bfbc6
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:38:59 +0000

    space string lock forever mixture

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def multiply_numbers(x, y):
+        return x * y
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
