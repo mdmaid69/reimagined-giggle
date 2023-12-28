28fb68ff1147ecd12ff73f4bbea9ed96b311563f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:10:41 +0000

    getout target Ww solve instead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def sort_numbers(numbers):
+        return sorted(numbers)
