bc6b0181024134ec8c12d2b8077be4cac0fd3f33
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:43:42 +0000

    cap display success deep on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
