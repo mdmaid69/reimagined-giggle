538f91ca6d56d94a37bf77c38810d17c847c81c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:13:35 +0000

    concerning wrap some safety afternoon

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
