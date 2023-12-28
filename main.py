cdb0cff6dc2084c2bb958e86fee95450c4c87d01
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:39:44 +0000

    peaceful laugh keepfrom hurry live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
