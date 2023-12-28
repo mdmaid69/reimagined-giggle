5da15cfb2d67a273d4f0ed62fb38df5e7e596071
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:09:30 +0000

    also calloff mend preparation debt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def square_number(x):
-        return x**2
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
