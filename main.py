44830cfb33dc53440267dadf83d94e1bf5754d2a
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:29:26 +0000

    birth whistle image mess personal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 for i in range(5):
         print(i)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
