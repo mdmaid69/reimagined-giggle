fabd7df84ea51d8da9f22955a874b0428bec7d28
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:39:41 +0000

    less pencil chase shrink sticky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_item(array, i):
-        return array[i]
+def divide_numbers(x, y):
+        return x / y
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
