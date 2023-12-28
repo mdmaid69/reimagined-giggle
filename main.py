7a241653c12075ae85963135fcacf13e469f65ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:29:09 +0000

    ignore begin please Mm nature

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
