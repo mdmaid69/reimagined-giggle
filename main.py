614337e4df1eb1f578ef5a02065604ab8aa18740
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:44:21 +0000

    story once dry previous measure

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-print("Hello, world!")
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
