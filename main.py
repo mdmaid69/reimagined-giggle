ba4472173f8055a234e79c7a48aa8ee565284bbb
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:12:33 +0000

    comein class stamp tonight public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
