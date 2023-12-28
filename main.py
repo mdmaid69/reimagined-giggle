74b17492b801e20fd8a748998dff7691310b856c
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:26:54 +0000

    control term heavy equal return

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import sys
+print(sys.version)
