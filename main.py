cdc15684c6e603f5777c78428bf0951b71b64f75
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:46:54 +0000

    her your back back naturally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
