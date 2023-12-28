d7af1d957e32263b410877da8f358790631d8d08
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:05:49 +0000

    crazy forgive rob pride touch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def print_python_version():
+        return sys.version
