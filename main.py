67eccd187288eff09951106e76a67faef75ec70f
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:32:29 +0000

    sail defeat serious waste remain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
