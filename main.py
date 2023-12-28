015fa92147e0e94fd50e03c324bea1eabb516c92
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:25:44 +0000

    right Cc butter of hello

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
