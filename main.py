b3f489969e7dd3faf9787095e9862e8593f35265
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:32:30 +0000

    expect calm stupid block couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def square_number(x):
+        return x**2
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
