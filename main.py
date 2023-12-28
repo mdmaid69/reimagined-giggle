a3508d1450be7ea047186a1e372658eceffaf675
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:11:36 +0000

    honest pile OKorokay fill fill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
