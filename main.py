ffe3983010d682387f9a20ef2330648d862eb1a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:21:01 +0000

    broad development mail second unfair

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
