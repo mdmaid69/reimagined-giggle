180a27924c17b47f3868235e7f7b6ce083aa5576
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:29:45 +0000

    treat exchange school lot might

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
