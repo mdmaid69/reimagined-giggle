abef3599eeef2245178d105b0b67b3e7717d9330
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:19:26 +0000

    giveaway attempt everywhere wait unite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
