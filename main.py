9f35d42439969f5b99573a4a148e585c9b5963d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:44:00 +0000

    even bunch mess miss wear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print([x**2 for x in range(10)])
+import array
+def get_array_length(array):
+        return len(array)
+def sort_list(lst):
+        return sorted(lst)
