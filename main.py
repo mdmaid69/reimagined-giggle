241946b250bb95d7b1bf62c73d6ea9b953194884
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:06:16 +0000

    runinto phone weather case favour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import random
+print(random.randint(0, 100))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
