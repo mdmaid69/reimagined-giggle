3116028ff1061f17b44f7ce51484f49bcb77feda
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:39:53 +0000

    bridge slide human comefrom direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
