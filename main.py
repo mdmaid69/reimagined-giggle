00b9bc3f37313647b556f182e0ff1a26b3e3f2b9
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:26:42 +0000

    marry sweep recognize here university

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_area(radius):
+        return 3.14 * radius * radius
