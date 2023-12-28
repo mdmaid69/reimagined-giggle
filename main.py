538d601d63699258170637a40202c229b6dd21a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:11:30 +0000

    pull safe whom Pp air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
