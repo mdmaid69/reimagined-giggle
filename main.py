2f3cb9df15ef0b2d8d8413f24cf2001c8a75ee58
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:59:32 +0000

    stop surprise until wet light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_as_set(array):
+        return set(array)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
