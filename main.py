513882675227654c93e845763ad5f8b1d9f0538e
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:18:13 +0000

    defend that lookback(on) give trial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
