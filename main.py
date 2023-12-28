64e6b253d77f57a9bf97057139a6581cb5d8f4c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:26:36 +0000

    Dd subject destroy bottom earth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import numpy as np
+print(np.array([1, 2, 3]))
