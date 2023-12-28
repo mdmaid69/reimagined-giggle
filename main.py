d5afbcc393c0b0567ecb96ba3391d6ce55d1203b
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:12:16 +0000

    himself print fire mention build

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def check_if_array_contains_item(array, item):
+        return item in array
+def convert_to_octal(n):
+        return oct(n)
