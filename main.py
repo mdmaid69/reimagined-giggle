976ddce5b95b07522840c13351911c38a87936a1
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:51:29 +0000

    566end slight spend whisper worth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def cube_number(x):
+        return x**3
