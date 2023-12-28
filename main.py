5380a1eb6b07a0e01466f4f6d246303d5b0782e3
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:56:39 +0000

    introduce continue sleep female himself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
