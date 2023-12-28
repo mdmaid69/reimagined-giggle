b208ccc4d3d46633c1223894c6b10ac9cf5aa026
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:11:34 +0000

    evidence imagine uponon stamp well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_array_as_memoryview(array):
+        return memoryview(array)
