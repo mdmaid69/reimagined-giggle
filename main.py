415ba9a5eabb9a5886f7bc43fa6929b0f71ed592
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:53:14 +0000

    straight workout male decide experience

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def sort_list(lst):
+        return sorted(lst)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
