79ef9f5382b3e15e80e887382b5cbed2c832f605
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:36:20 +0000

    recently equal thing dear until

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def add_numbers(x, y):
-        return x + y
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
