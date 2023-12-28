8a1a311325cb4301482f75038df35e17bcafdae3
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:44:58 +0000

    ashamed side active reserve leave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
