718d51545add106010c4a74f0d026fda8e7a918c
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:47:04 +0000

    formal catch exchange sorry raise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
