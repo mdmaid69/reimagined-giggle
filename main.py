d8f8775a455a6bb5fb70152236268d8dfbc59530
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:20:37 +0000

    pull phone write beautiful welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
