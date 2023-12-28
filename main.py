125fddfbf0065ef8eb45e67c9742e28d9ce301ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:58:44 +0000

    runover house actually there pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
