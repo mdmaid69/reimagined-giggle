68925d2654d0f8ab2a3ebebe43764b282add21bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:11:28 +0000

    everything stupid least drive most

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
