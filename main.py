1174c007dd61a45e9ad79b4d166e278b4bfefe89
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:11:01 +0000

    card passage busy contrast delicate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
