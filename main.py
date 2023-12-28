2162e090034215ff6c3a700d7125595c2a8f3b75
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:03:46 +0000

    rain either bringback lunch well

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def convert_to_hex(n):
+        return hex(n)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
