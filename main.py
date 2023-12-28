1b62d1d33b98acfcbce1da2358ba2fdb0bc891cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:47:26 +0000

    confident trousers taste foreign raise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_item(array, i):
+        return array[i]
+import math
+def calculate_exponential(x):
+        return math.exp(x)
