1040bda1faef306bb87b5cbb719972de87b1f886
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:11:57 +0000

    expensive pile woman stand brief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
