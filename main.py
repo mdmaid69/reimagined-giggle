fec7ae840cf91593114cca18305ba037ef7f7c35
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:50:51 +0000

    laugh front escape bang knock

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
