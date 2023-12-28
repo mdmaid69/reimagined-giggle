64d691004227786ab2db1d204432b2bb656bc462
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:32:14 +0000

    nice cost themselves keepout freedom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
