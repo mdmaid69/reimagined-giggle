7e61f6960532d041b31e4eeb8edd7777f01d58f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:13:26 +0000

    exchange slightly giveaway mainly bath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+print(random.randint(0, 100))
