b3fe22ba83b8209db39aa992edf136a428025081
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:20:41 +0000

    divide tap runinto might later

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
-x = 10
-y = 20
-print("Sum:", x + y)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
