1ce19fe17990cbf6534431a71988413f5a774b2e
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:25:49 +0000

    previous frequent old computer steam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def is_palindrome(s):
+        return s == s[::-1]
