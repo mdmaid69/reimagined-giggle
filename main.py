927a4e10f74d8928d18520edd4d0e784432377b9
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:25:24 +0000

    on back car swim punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  def reverse_list(lst):
+        return lst[::-1]
