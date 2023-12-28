794fc66c8da5b139096f3fcfaa9af4576b880242
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:13:17 +0000

    right strange needle everyone cost

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
