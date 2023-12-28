0562b3ee39cc0800a2c8d28cf4f3967aae0294ec
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:08:49 +0000

    action job reliable position children

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_list_from_array(array):
+        return array.tolist()
