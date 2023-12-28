0b5366a1912675f144e9e40ce19db5345855bd3e
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:52:06 +0000

    recently punish admire takeover regular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def is_odd(n):
+        return n % 2 != 0
