5a2f0d8c7ba6eda920b3e182818bed00cc30e43a
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:19:26 +0000

    terrible modern instrument takeback contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
