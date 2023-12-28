28be18044794a4787d837f34f61833078dd0d3f6
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:08:28 +0000

    thinkthrough ground attempt awake if

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def subtract_numbers(x, y):
-        return x - y
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
