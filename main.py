9929d6a6094d0e998cad93b8f1c73a854cefe6a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:08:28 +0000

    serious patient extraordinary man impossible

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_typecode(array):
+        return array.typecode
