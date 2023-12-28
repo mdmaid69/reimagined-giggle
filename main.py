7eae83facf35fb2edad8b82363165a1f14a0ffd6
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:17:11 +0000

    pocket dance rid tooth happy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_work(force, distance):
+        return force * distance
