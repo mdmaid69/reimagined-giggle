34409d9b6b9c4b0eb4f6c76446af69b7d6830238
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:27:56 +0000

    late back material hard connected

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
