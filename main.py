e4d46e0248054eaf7ddbcbaf261d904d1b878ba1
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:24:38 +0000

    terrible principle weather brief rare

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
