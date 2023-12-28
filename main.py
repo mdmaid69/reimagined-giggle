cfe2d2a06ad474e946adef6dd52c66aaf8a59a9d
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:06:25 +0000

    sale tone refuse slow dangerous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
