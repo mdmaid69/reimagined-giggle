4a7aee2f8f14383b3608bc364e13e412b8f326d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:44:10 +0000

    brother open except much spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
