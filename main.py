f970d8d10b774e1c9c2bac1e681820221910d5a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:44:25 +0000

    watch just embarrassing noisy judge

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
