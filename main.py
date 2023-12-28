abb46f6294c596d218f49bb64d6b116438af7050
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:20:39 +0000

    bear city confused tall become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def find_min(lst):
+        return min(lst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
