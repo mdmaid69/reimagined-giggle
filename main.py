ef96337783fbfb8e01709e2ea73e50be11d4fe3e
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:23:58 +0000

    translate Ii error temperature emergency

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
