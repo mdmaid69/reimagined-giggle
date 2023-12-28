16aa3acaa8e787a8fa4ee7b5413fdaab7e829147
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:12:38 +0000

    party almost fasten among even

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+print([x**2 for x in range(10)])
