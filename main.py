1e816d7e74a6e89b28b7d9879c65a1b8edee7f5c
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:17:12 +0000

    experiment suddenly so effort yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
