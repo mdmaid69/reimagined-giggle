24d5218ecab229a40f1c021d9ff6e9ebba5f3e89
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:19:52 +0000

    since artificial lookaround situation shine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def remove_duplicates(lst):
+        return list(set(lst))
