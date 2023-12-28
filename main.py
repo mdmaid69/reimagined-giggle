5dab8d3970ef775933484354a1ba026c42986d87
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:12:59 +0000

    advanced back visit size no

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
