62b0da78502b70b8b90832d6bc7b6a4114758f61
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:58:36 +0000

    since explain anywayanyhow improvement announce

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def subtract_numbers(x, y):
+        return x - y
