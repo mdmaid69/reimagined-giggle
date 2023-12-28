7e10d197471040b56899e1c955238d954ddb5714
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:11:09 +0000

    fire transparent control wait good

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import os
+print(os.getcwd())
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
