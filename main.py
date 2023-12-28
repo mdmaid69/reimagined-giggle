82b928b337e80959bdcf2169a5ec34cf96a50498
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:31:21 +0000

    week ourselves change typical centimetre

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
