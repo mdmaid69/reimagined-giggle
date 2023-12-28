b9a6a24e23c61c7daef25a9b00ebfd71a268ad6e
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:51:56 +0000

    shout like interview separate wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
