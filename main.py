f48d18a51795a955cfc28dcfed1a3c6fecbc8d6c
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:28:57 +0000

    turndown eager disturb mend press

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+print("Hello, world!")
