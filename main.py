f059c7a8e8e91ac6cf18c705ef2e3177f8708efc
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:39:22 +0000

    dare your cry come cook

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
 import os
-print(os.getcwd())
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
