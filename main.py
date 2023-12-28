d612ead86e611607c99c5bfaa1ad18ca3adf8658
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:45:35 +0000

    record light bag performance odd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
