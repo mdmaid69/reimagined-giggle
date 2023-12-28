cac9663424469b51c0bb78870f5242fb995334f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:09:43 +0000

    his damp few but persuade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  def sort_list(lst):
-        return sorted(lst)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
