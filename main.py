ebd477b09f5c4de5e7bfd8c5be1fdd88a3baf075
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:41:56 +0000

    unusual sport maintain birthday judge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def find_max(numbers):
+        return max(numbers)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
