a05ac0b613544290af4cd6ee2303e977c8b45ea0
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:39:17 +0000

    fast snow sky complete late

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
