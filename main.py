73b647d28bd6f2d8b33bce026c3c021215dbcd33
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:43:34 +0000

    dangerous bridge fair return usual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
