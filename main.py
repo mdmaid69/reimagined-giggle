7302c079e696bfdd42377e1fd46868338d98deda
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:58:48 +0000

    straight straight alone food eat

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
