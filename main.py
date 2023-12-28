cfcb49ab3b2b5a4fab959ee2f91af76e4712806d
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:18:13 +0000

    effort poor healthy recently step

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
