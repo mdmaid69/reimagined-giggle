75d25bc6cd976482d9c2471de782a213e589d106
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:04:10 +0000

    far plan touch active advanced

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def square_number(x):
+        return x**2
