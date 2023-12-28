dc15e0a5dd5bd957c46667e08c64b2a7c4225408
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:21:39 +0000

    goback(to) tooth nose plate season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
