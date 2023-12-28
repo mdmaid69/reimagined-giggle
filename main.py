de125b5a4182f2c0481370d58eb77a2dc0dc38f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:33:15 +0000

    news number strange accidentally accept

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
