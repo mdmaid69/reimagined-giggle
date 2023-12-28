6365f7cf33d6304233a78e262888df67d27df679
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:02:39 +0000

    border variety attempt ahead involve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
