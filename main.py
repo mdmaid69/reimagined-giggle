8e490e4d7c3c546f7ee55f76ae64e48774834b54
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:26:10 +0000

    niggaacademy however over fond better

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
