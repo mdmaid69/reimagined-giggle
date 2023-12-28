73a9b880648ce5ee63d0196a92b05a5812b52270
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:48:08 +0000

    along characteristic height bear disagree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_area_circle(r):
+        return 3.14 * r**2
