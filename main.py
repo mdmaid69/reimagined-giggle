0308cd10816afb0622edca9a9526d8ffc074f183
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:55:02 +0000

    alone whenever gate state passenger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
