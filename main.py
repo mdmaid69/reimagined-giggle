617d6aab6b2927741464766381f95a92288a9736
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:04:49 +0000

    goup but hospital hospital 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
