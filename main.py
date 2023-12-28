3294b129f9f50d8307ed5730320bfcd182678929
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:50:09 +0000

    impressed accept control clearly contact

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def count_elements(lst):
+        return len(lst)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
