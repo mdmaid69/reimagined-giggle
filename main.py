5d78d7db8610899c9d3f00de8bb7605ae7c7728c
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:12:49 +0000

    tie size appear book niggaacademy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
