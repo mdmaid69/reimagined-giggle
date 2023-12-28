825f677420d3d0b93b75cd04ad402107be5c911b
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:40:59 +0000

    reserve place improvement up positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
