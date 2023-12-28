07850adf1aee81d534c0f3ede016577df5a8d0e5
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:08:50 +0000

    earth discover this run copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
