c65f2a5e5d22502154ffcb76feff01eba8a49e25
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:29:20 +0000

    wonder great progress getoff spill

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def is_palindrome(s):
-        return s == s[::-1]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
