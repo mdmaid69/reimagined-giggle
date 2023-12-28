07764867361d186a728bb0bd14427587f4d77eab
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:36:08 +0000

    relationship country prepare phone dare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
