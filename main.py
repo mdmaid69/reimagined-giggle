8e1efdbc0206e9d09e9a3678f5120d02cf5e8efc
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:46:49 +0000

    demonstrate relationship vanish completely image

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
