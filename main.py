e0d0495bff2606956a5bcf7788523351820fd0c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:39:50 +0000

    count strike measure rely best

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_floor(x):
+        return math.floor(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
