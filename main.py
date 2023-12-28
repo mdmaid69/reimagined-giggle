2b239630d04b564cc270cc5bc7f15862f78dd4aa
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:46:10 +0000

    care suit frighten trap bringback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
