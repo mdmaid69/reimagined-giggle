d95140b73b02b9a499a928e5718df4cb97ca67ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:07:44 +0000

    face moment business public it

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
