3bdcacce21d000dbc6741203303545e00e7a4b01
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:21:15 +0000

    responsible burst store during result

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
