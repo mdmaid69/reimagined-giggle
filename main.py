88f3afc37e76ac927c25afd8d8f78f3938b89a6a
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:52:01 +0000

    exercise backwards breakinto careful factory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
