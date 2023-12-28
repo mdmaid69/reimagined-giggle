ff048b4326a35ccba23fd9a54756f3a8cb5fd0ff
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:05:28 +0000

    second vanish weather sing authority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_typecode(array):
+        return array.typecode
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
