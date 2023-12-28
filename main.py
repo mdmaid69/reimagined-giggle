cddc70fa5ab8f2ddbd684da1794e18bbab87cf26
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:23:38 +0000

    purpose argue exchange plan bad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_min(lst):
-        return min(lst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
