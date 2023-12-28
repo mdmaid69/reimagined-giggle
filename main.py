75422b9ed2de7b89242444dce226cda579a83a44
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:33:32 +0000

    instrument cruelty myself past swing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
