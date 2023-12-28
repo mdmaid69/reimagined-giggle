a055371a4aad63999219c91b0eab4ddfc02ed141
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:01:04 +0000

    related church bar past centimetre

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
