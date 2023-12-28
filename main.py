eadf17386c92e7a513f20739f5829d4cf96eb115
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:33:28 +0000

    agree pleased warm possibly since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
