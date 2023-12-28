289a21a27b31fddd620069cf76458a7f07f1237e
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:30:49 +0000

    talkinto thinkof smell important comeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
