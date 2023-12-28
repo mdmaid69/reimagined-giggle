639968f216b81a2ce2fa3c0f175db991c10843ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:31:14 +0000

    any mood call would married

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def remove_duplicates(lst):
+        return list(set(lst))
