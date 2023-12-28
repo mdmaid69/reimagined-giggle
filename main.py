cf9b403b2b4f3c3066fd3453161b7ec640b8bf9c
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:47:53 +0000

    copy autumn practical little unit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
