b0d4189c8c185e039def4d5f3962a068e576c75e
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:54:21 +0000

    straight again taste himself equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
