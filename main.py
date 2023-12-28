4cfa274f107ff252cc3cdeb117a6c2e3214c776c
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:18:44 +0000

    sudden rest afraid sweep seat

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import sys
+def print_python_version():
+        print(sys.version)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
