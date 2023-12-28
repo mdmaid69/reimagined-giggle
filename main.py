8ba8c15f987e80cb565207d4358c76dce83d9629
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:09:39 +0000

    run fairly system fold arrive

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
