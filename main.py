5d0b6e45cc7373761ddc2f48a8312b964650cf40
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:26:25 +0000

    goaway wheel famous phone goon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
