e21e29d33d6b227e7f01eb6a31299a65bcd29003
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:42:08 +0000

    refuse niggaacademy tape often surprise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
