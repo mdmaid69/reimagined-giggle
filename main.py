5528deaf6a06b558453e13dfbec950e45197573d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:07:54 +0000

    floor will year especially actually

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-def find_max(lst):
-        return max(lst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
