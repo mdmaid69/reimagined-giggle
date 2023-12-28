0b1fbd60696e2ffe1d0ab5bdfdbb3c67bfa5d630
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:11:23 +0000

    Qq suggest comment normal bang

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def square_number(x):
+        return x**2
