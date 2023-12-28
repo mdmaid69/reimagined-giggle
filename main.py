4f5d9a0ee0eef5d21186ff64984e0472be85291b
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:06:55 +0000

    at quit murder report cheque

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def split_path(path):
+        return os.path.split(path)
