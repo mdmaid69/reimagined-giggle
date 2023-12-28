9cdf87dbb7a20efcc1fff5318ee0103c6b4b00d0
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:24:02 +0000

    should ride cost present still

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
