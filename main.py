360f4930016077233621fdc1cb90bd0ffc181060
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:52:59 +0000

    high weekend on train few

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_volume(length, width, height):
+        return length * width * height
