73102cfafa0d545d91ca24c5ee47f85a1cfc30f2
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:50:59 +0000

    range considering ride flow bored

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
