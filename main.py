a9f8346168ad17c6929da754b68b858dcac5b281
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:35:30 +0000

    Itself why bush scheme actually

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
