f6cf9075b0b9e48def094a54b1f5e7585e2ecf89
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:41:37 +0000

    blue person if safe company

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
