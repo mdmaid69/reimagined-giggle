48af48c41e74dd66f8dcf32593de0b87beb359fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:41:32 +0000

    call instruction tell space relation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def multiply_numbers(x, y):
+        return x * y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
