26ebde21744aba56a1333a8d0397ae099822ed73
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:20:49 +0000

    trust Ll step point he

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
