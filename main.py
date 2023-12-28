40f35c931531267d27d61de9316adb3b94d58f5f
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:04:49 +0000

    makeup state talkback country approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
