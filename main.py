58e74e2b4a152952272e1a9ce2a113bc82e23b4f
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:03:44 +0000

    lucky favour near flat comedown

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_str(array):
+        return str(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
