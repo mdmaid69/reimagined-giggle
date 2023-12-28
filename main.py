1a13fa41b73a957605c3b52bee1b3df621a312e4
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:07:09 +0000

    mention hour threaten which fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
