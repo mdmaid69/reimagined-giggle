5138434fb7f812ee02f2c8e68c3ef2b4bfe8d838
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:21:16 +0000

    relationship hold strong professional hardly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+print("Hello, world!")
