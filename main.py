d18bb7ec9b1bc3c1050eab5a215f53c37d7d2ad3
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:09:16 +0000

    extremely entry grateful season replace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
