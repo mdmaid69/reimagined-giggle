47bea594ce2bd277702d322e411fd4018c4da702
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:52:13 +0000

    hit front introduction future field

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
