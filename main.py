2841d77dfdfd170ec430fa29691dc32eef0f62fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:43:11 +0000

    development threaten convenient dangerous pinch

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-for i in range(10): print(i)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
