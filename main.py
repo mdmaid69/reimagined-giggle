6d8a629c90195ac626fd94ea98e4bee9840618cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:35:36 +0000

    window satisfactory whichever pronounce remember

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
