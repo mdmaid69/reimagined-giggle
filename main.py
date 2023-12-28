0030624cdd6094ef254c3962b0f3ab36af2750aa
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:00:35 +0000

    time large loan load couple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
