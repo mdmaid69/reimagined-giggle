78031465a3af1e2768d42f5a504bc459f08df9b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:49:55 +0000

    lonely emergency cruel law effort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import logging
+def log_message(message):
+        logging.info(message)
