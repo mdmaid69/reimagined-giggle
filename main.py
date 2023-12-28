67146266183fa2e3bb4215dc72ca3787db8b8a00
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:29:02 +0000

    odd pick cold manage deal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_density(mass, volume):
+        return mass / volume
