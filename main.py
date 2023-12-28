8395edbc6cc4b82505e24bbba762296961362488
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:40:35 +0000

    actually dry mark bye bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
