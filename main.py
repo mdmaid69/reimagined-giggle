851c17473068be6a51c90987bd50004e0f52e608
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:56:06 +0000

    grand shrink deliver variety temper

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import os
-print(os.getcwd())
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
