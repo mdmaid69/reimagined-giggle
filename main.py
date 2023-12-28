b974dd04148b889665eb4fa864370632c2a6c42e
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:22:24 +0000

    opponent measure important sensitive centimetre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
