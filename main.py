ccb73e5397e2b5aa367871db2eba4ef1cd76064c
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:44:43 +0000

    nose godown workon punish real

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def find_max(numbers):
+        return max(numbers)
