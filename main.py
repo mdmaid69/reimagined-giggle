9e9ad556a141b1d3c2e2672dad8d0be7d2b281ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:45:24 +0000

    lift strange lot thumb fast

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_ceiling(x):
+        return math.ceil(x)
