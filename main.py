36a6b5e40cbf05442d98eadc821fd9e0801ff73e
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:34:53 +0000

    load fix surface freedom giveback

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_work(force, distance):
+        return force * distance
