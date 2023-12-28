b17ac772ed8fc1278a61002e650811cda9cfd863
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:07:30 +0000

    themselves frightened count in air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
