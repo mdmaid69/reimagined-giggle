56875ada91abbed4b8f5595b959eaf5bf3107892
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:12:17 +0000

    ground order weather politics deliberately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
