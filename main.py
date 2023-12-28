fc55b7ba5fecfe1c5eae871652554e15ced8878c
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:36:29 +0000

    sure suppose bringup when hair

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
