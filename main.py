2d364ebd12a1f513d3f3ddbdc447bdf6fa7cd98d
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:06:37 +0000

    advise 568energy treat remark owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-print(sum(range(10)))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
