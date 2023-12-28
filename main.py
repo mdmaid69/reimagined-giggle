579eab4740f881de5d2c0fd7f5948074a5d8c441
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:48:29 +0000

    reason wind damage keen repair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
