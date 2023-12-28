fd631b3b39004fa13b131121e2cd2ba8d8d138b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:57:35 +0000

    earn pause bring bringforward wish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
