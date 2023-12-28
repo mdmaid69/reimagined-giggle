a2d70313bc1fa075cf8fe387bd377db07a0a1b7b
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:22:30 +0000

    slight description interruption top admit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
