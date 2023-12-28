c02c941ab9bf7e3df281de600ed71278de8ac24c
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:17:55 +0000

    claim equal doubt his murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
