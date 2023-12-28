55b1be1f1d3167da1715d47c0ee6698c987423c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:49:33 +0000

    sometimes than clean convincing climb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_typecode(array):
+        return array.typecode
