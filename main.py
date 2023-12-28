b1c37e110f45d2d16df52818eeda4345db4f913c
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:30:01 +0000

    takeback country except avoid unlucky

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
