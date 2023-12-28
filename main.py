c76421de90f4e76c7dad304670e34a2fbc42d78f
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:44:59 +0000

    apply interesting over later moment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_volume(length, width, height):
+        return length * width * height
