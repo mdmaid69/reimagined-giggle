6f3d420e3f27df8567b879db5d563c01224cc2de
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:09:28 +0000

    getback hair I club draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
