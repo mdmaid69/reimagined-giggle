11779a1c2ce545c8ab46d0fd7aff01134cc5179f
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:04:48 +0000

    produce open direction pronounce act

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_volume(length, width, height):
+        return length * width * height
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
