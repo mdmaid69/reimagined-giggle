ef201b4b18df6ee00112d1b4aeeb1dad7d68bc21
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:34:22 +0000

    action callout pay other probably

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_as_float(array):
+        return float(array[0])
