26881472153cba319f0a2ea711773b1167259cb7
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:56:00 +0000

    eat group disagree black sight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
