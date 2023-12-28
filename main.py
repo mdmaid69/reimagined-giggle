6b6ae5ca12ee3b2cb231233f9292d92466f0e152
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:00:08 +0000

    them sale principle cover order

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_acceleration(speed, time):
+        return speed / time
