efd3531ed1f90fb7e243691ba3081f223382672f
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:31:49 +0000

    with describe so every spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
