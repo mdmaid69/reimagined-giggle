89883fd448c215cb8c96fcc22e616cb09d84b8a5
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:45:50 +0000

    perform call only ring visit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
