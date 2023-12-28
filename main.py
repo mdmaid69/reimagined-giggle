ee198b2c114a130452f12d12ce0cb20343d99e68
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:26:39 +0000

    dinner final try wind actually

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
