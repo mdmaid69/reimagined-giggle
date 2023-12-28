cf5b3fa4d087a13674cbaa8ca7d548ba65077499
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:36:52 +0000

    appeal excuse lead lend reasonable

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import tensorflow as tf
+print(tf.__version__)
+for i in range(10): print(i)
