8a3a40dfbbe3356cee5b43cc4fef00bf4df8e5ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:47:09 +0000

    advise even single hers basis

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
