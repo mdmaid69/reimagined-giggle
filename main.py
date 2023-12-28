92f376ab067275ad809f62dd0c4681dd68ea776a
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:44:55 +0000

    film follow for idea mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
