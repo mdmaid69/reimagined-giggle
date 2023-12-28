8b772bdbfe57813996196cf89e395d9a01652ec9
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:47:21 +0000

    band mistake dream comeround certainly

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import sys
-  def get_python_version():
-        return sys.version
+print([x**2 for x in range(10)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
