fcd935b2fbe85bf03adce3978a6b9a8b032cb5d2
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:35:58 +0000

    put drink wrong reason warning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import datetime
-print(datetime.datetime.now())
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
