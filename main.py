641b6553afe802b4050cee0622f96af3c428145c
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:32:16 +0000

    past burst congratulations accidentally dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
