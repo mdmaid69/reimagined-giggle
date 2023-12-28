bcd8ef0ed93e41f63e64857a196a6d7de3e226e1
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:32:38 +0000

    display male Ss cross Rr

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def remove_duplicates(lst):
+        return list(set(lst))
