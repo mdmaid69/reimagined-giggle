8fb18ad124cbc7f82c5b0e62c521bed023639de4
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:48:43 +0000

    wave front free square dull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
