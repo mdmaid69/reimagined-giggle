f6fc3ed06082d8de79109bfb8acf75ca454f4ad8
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:29:08 +0000

    pile instant tell estimate frequent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def find_max(numbers):
-        return max(numbers)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
