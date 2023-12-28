39c3083a4952c703a17d822bed2a8600f6eba59d
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:43:42 +0000

    moment stop belief complicated rub

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_pressure(force, area):
+        return force / area
