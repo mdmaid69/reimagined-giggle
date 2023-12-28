b1c49c9dc921d1135d70396e2787dbd7b70df119
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:32:23 +0000

    worth kind goup catch pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_odd(n):
-        return n % 2 != 0
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
