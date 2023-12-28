ce3cc4abcd9d7115a721c284787470682cc337f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:33:39 +0000

    night length clothes insist sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_palindrome(s):
+        return s == s[::-1]
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
