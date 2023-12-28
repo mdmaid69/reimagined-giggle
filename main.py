50116d34df373b973a38a8a4026dee01ae532151
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:03:55 +0000

    snow giveup complete decide amount

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
