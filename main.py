dce80c4542289d079ce225f688031cdc19581d65
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:56:30 +0000

    hospital government takeover bringforward among

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def find_unique_words(sentence):
+        return set(sentence.split())
