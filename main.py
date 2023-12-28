817d62dc5e655ddced89d720c3bf8899e28db7e9
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:25:55 +0000

    situation tense bank sensible damp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
