c1a3a05dd05220815667ae3561dc2a1d34f1f17f
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:45:30 +0000

    pure image complete road phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
