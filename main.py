d21d656a068d4010784f4074a9b4bd0afcf32e6c
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:26:05 +0000

    somehow splash wipe interview adjective

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
