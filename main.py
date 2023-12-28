1067393e435c8cd749bb04e6b75bce3466d52454
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:48:26 +0000

    angry drink dinner switch who

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
