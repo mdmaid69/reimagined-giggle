3eb5deb43865eac5930d5dd9dfe99e3b8f6faa8e
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:53:08 +0000

    justice prize deserve clear Gg

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_min(numbers):
+        return min(numbers)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
