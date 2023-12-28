30462903abbc2034094e311456b4c68252893bf7
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:05:00 +0000

    contain anything custom poor expensive

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
