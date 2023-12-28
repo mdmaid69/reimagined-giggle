2926af15c9c697bba1424918ddea0729536780d4
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:45:50 +0000

    task table example further mail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
