dfa8c141546efdef0106a686fc029bc22b729f83
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:47:02 +0000

    different discovery question eye ought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
