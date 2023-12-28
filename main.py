4d73bf47ef3c7afbddb7d84e033b49d30c0ea61a
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:33:23 +0000

    unlikely wash have personal joke

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
