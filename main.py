8db1180d58a844b3ff48911e8154371eb321a517
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:21:48 +0000

    hello heart shock forever downstairs

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
