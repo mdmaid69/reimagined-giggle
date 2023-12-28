8e534033c0eb9278f5a69b341982e6e6035ec211
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:42:54 +0000

    dream able success difference hour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def sort_list(lst):
+        return sorted(lst)
