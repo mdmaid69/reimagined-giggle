74788796cfc158962aa7699a5a05696cca1ac527
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:38:53 +0000

    glass operate lookafter murder between

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_min(lst):
+        return min(lst)
