31087518242bd47b8986b2b938da9ee0e905ae46
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:24:23 +0000

    live area failure trick because

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
