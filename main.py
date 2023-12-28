ab4cb0e3873acb0c32fa2cab584a5c31a06cb249
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:53:41 +0000

    earth grateful field physical proud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
