2488bb085412b248bdf0c05800e32d465ae2f801
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:33:09 +0000

    can someone guard order sore

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
