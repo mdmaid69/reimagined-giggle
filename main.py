91cb990994084bc25183155d9ce50a82460195e4
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:20:08 +0000

    muscle advise illness trap trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
