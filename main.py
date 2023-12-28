d570b39748056fa0ba41a60b848df080265535d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:36:43 +0000

    sound keepoff wing bank clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
