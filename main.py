2188fcd8dca619aa1b472e1790af5f605e0e2a4f
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:57:12 +0000

    note whether cross to without

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
