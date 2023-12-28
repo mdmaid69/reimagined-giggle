6d5803edfb12ec7a7af3f7e746aa572ac5dedf30
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:07:36 +0000

    fear perfect method shall steady

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
