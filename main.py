067b7060ba8771f123dd3a69edbb189ba5b5b827
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:58:40 +0000

    statement mistake flow walk exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def cube_number(x):
+        return x**3
