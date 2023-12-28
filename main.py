e109497393417c2b0dd7c91dd1fd964a3d3198e4
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:18:34 +0000

    kind judge should also strike

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
