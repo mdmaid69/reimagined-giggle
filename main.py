be9011bb687a7cc404c2bb45b407d5e8da66d737
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:53:55 +0000

    before sentence blind pure accidentally

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
