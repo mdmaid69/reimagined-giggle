5ed26a2608cb83895479756a91eb62ac1fcdcf5f
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:13:08 +0000

    Ee praise tense party left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
