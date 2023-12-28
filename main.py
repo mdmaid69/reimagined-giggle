e9425738758241b86a832bf60cefaf099c1407c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:34:00 +0000

    complain ring protection several bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
