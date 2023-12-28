4ecdd5d8d45bbcd4e6c63df2f6e0e1b821a2bd8b
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:09:32 +0000

    emergency what emergency zero medium

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
