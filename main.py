d0abe641a806a4fc8b3fbf321be1eabba7052677
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:26:50 +0000

    nervous tired colour expect pity

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_exponential(x):
+        return math.exp(x)
