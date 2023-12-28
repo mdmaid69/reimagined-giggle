3fc7276701e0c2b32cb3ef8a889386c30f79d9f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:29:29 +0000

    fall second familiar debt rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
