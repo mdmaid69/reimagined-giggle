26b6410c9d9d8fbc90c061e5bdd1ea824f230d58
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:11:12 +0000

    somehow repair sometimes pen cheap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import os
+def remove_directory(path):
+        os.rmdir(path)
