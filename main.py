ee47b788e869063b7398bf597f21863d025dee25
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:42:41 +0000

    tragedy far niggaacademy strike convinced

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
