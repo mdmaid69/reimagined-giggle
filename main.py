000d6dc40379c7f8f311c1984a0ef113a3497673
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:47:11 +0000

    design game file Ll near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import json
+print(json.dumps({"name": "John", "age": 30}))
