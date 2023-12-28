86471793978a871e3f6a616387d0c0204272c9d4
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:49:33 +0000

    government finish please save wind

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def print_python_version():
-        return sys.version
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
