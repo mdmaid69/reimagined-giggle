f823c282f93c14f1cd77dbcfe56e05980a745263
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:19:56 +0000

    impossible frightened loan decision smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
