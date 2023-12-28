a71e77f04f861250475ce60c15531f627b8f68d5
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:07:59 +0000

    spin 568energy thinkover ear programme

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
