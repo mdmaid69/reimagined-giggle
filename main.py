ff2e099251598de5694936d8a7f978a219f9afb9
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:04:57 +0000

    manage same herself state treat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
