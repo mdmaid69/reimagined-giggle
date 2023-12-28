5da36cb06e197aebfc223f6b852fa862ad580032
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:00:13 +0000

    both pride convenient body praise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
