68d588eda11c04c90cb2753b7e23ceecfef71bdd
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:24:36 +0000

    name collect yours explanation emotion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
