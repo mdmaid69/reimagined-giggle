37111891782ba10cb429d0097decad135d472d96
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:35:48 +0000

    available emphasize exercise read pick

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
