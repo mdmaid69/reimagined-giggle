910f9eb60cae9eb0933ef31192c48bcc3ca2516f
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:10:36 +0000

    even feel no treat story

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
