fecda2290164c8059c1591e30396b55d70ffe7d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:27:53 +0000

    rule give now hear develop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
