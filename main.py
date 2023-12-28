bffd9142994279ded2156d4e7601575e713c77ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:07:26 +0000

    error star free borrow early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
