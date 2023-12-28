6c1cec951817e886751a3ba80f8bf022e12f62fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:41:51 +0000

    refuse phone about anyone month

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import datetime
+def get_current_date():
+        return datetime.date.today()
