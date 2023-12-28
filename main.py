5aab7114abe74275da11d5d7cb08e09349eacad4
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:45:22 +0000

    grow mass comefrom perhaps car

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_min(lst):
+        return min(lst)
