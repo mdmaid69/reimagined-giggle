07aabf21e62ce2ca916385fbd73821f2dd18bf8d
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:54:27 +0000

    callback table marry complete air

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import datetime
+def get_today_date():
+        return datetime.date.today()
