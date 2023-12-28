5d429a4eaee8ec49530285747cf07c55d33211b8
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:06:24 +0000

    guide road lot star interest

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import datetime
-print(datetime.datetime.now())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def greet(name):
+        print(f"Hello, {name}!")
