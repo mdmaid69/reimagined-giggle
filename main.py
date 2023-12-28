2e0f01fb9682bf312244099afae2394057f84b90
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:39:50 +0000

    beginning add care formal border

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
