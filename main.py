1cb271a123d88084940e5f1050a3043e9da58daa
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:37:12 +0000

    565end grand from any row

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
