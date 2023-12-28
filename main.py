e832c21fea679651862d6e7c8196a024305652c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:12:46 +0000

    walk advice drink release water

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
