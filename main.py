219512340825cec1617312d508a57ae8256e907b
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:52:19 +0000

    whether oil too base justice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
