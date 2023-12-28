f26e9b71d9cd303d5b8459a26a0f75849ddafabe
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:28:32 +0000

    argument bringback rescue buy grow

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
