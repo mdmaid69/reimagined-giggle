67383ea9ed94411cc1cbfbe9517d235b0a1951c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:45:29 +0000

    key quickly stand talkoutof careful

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
