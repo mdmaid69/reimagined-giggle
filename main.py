b9fe90c1fb7f6d8d64e3b48503715f4b1f87d044
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:05:41 +0000

    spread extra office someone bridge

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def greet(name):
+        print(f"Hello, {name}!")
+import array
+def get_array_as_bool(array):
+        return bool(array)
