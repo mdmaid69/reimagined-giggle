d4083b9dbe6d409997274b23016f6a832d137af2
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:17:55 +0000

    motion meet speak season the

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
