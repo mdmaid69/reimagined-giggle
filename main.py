344a1aaef792541cebaed7d6f8f3a5aa6863ce5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:01:48 +0000

    another onto job persuade contrast

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def append_to_array(array, item):
+        array.append(item)
