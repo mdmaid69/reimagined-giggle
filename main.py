3379ba4675a915a8c4f04791fd618ad156c3f3ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:38:16 +0000

    be work cut wrong trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  def cube_number(x):
-        return x**3
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
