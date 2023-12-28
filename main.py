867270db38d83baaa4d701d423d981f2cdc37768
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:30:16 +0000

    free write limit new though

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
