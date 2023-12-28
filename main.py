31f1787c30a6ccad37479fb725c5cfd43a822622
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:34:23 +0000

    straight image persuade bill thirsty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
