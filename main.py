73308b87167547b63b85d091ceca8cff76e0b0c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:13:06 +0000

    blow experience address over gap

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
