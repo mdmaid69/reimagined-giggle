e62092f803564680625863d640a49f269d1acc43
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:36:59 +0000

    raw appear dangerous well always

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
