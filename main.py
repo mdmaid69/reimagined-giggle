5df91b5b81d12c8e9853ea1e184168c4bb698154
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:37:09 +0000

    decide cap cold study promise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import numpy as np
-print(np.array([1, 2, 3]))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_area_circle(r):
+        return 3.14 * r**2
