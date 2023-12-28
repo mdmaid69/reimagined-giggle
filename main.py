5cecce806abf10f55ba31ab385824176e9155da6
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:31:02 +0000

    general store spin your check

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
