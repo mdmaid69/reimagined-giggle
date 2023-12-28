1c7ec03edba9c7cb88c81c363586e90169f3eed5
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:22:30 +0000

    turnaway wait suppose near bright

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
