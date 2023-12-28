f8102314b755bac09de315b84c75a635581b4a1d
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:26:24 +0000

    suppose object gap idea study

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
