26c4574fc1c7be5bcad14a8d5011414e24348bd0
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:52:12 +0000

    turnback mine balance leave want

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_work(force, distance):
+        return force * distance
