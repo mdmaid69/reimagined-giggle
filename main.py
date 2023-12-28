5f26df12a44b9d2b60a8ab7a0aa13042e71893e0
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:59:07 +0000

    game acceptable defeat dinner thinkover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def find_max(numbers):
-        return max(numbers)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
