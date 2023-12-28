ad914dab9d3f1cafb7f5522905214a90226a2feb
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:23:35 +0000

    reject meaning business Nn sale

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
