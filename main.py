fe677bc00234b13ad8373049eb255ae05b62ff84
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:47:24 +0000

    hole minimum break cap grow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
