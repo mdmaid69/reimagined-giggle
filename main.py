0ab560cfb5e7103afab138dfeefafca293e7ef60
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:40:29 +0000

    rub while result block trouble

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
