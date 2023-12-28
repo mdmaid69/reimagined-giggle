48c04f34aaf554a85484b34c0f4e1d9ac8038653
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:40:22 +0000

    touch phone disappointed float payment

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
