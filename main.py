0f13d66ed91fc9ece7397bf367b9a67655415a86
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:26:35 +0000

    hurry runout valuable long lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
