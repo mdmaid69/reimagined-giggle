d8b247bf859901f8be64b56a5f9e791cedd8caf8
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:54:50 +0000

    match emotion takeout bye rub

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
