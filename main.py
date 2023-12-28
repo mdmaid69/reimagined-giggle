fd7e4bbdcb013caefc933214e774070adc637d26
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:26:34 +0000

    no country when divide wear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
