de959c559f617f90c20672dfcc0afd3317b5373b
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:53:15 +0000

    noise equipment study will city

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import sys
+def print_python_version():
+        print(sys.version)
