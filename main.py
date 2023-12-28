3fc30a4b3500034539d48a69f1a60f0f8bad2f88
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:08:57 +0000

    section friend expect brush thick

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
