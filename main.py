d1038675911870fac14aa77be292543d610d4129
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:18:33 +0000

    mystery label brother turnaway size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def reverse_string(s):
+        return s[::-1]
