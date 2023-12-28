fa78d38a55b443dd0ab6bf4d2421e395c98c2a0a
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:46:27 +0000

    through invitation brief square guilty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
