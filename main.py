3d86308adb58116c5a6af1f69ad4778283a51bb7
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:32:33 +0000

    distant charge development Uu tip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
