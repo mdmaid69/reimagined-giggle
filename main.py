c8dac93cfaf56f72afbf0d59c8a67bf3fd9e61a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:36:57 +0000

    fairly anyone work correct excited

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
