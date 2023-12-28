d9d3d315bfd17bcfa7360fab39923a4fc58f7d3f
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:45:07 +0000

    thinkover present spring ours difference

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
