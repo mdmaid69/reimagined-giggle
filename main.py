1c51dc326f8ff973779bd322bfd5763ca8fa8c71
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:17:02 +0000

    address fetch apologizeapologise ilovedicks out

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
