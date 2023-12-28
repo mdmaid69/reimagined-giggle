a89b9f625f38b7895c602b9fc1f31d32d0203d1b
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:11:11 +0000

    discovery gentle motion listenfor trap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import sys
-def print_python_version():
-        return sys.version
+print(math.pi)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
