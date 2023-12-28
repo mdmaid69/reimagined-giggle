4138ef353905cb079954da152b348404f926031b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:13:28 +0000

    joke steam scare confuse advise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_area_circle(r):
+        return 3.14 * r**2
