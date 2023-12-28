ad2715e8c1da0891296d146e4b951d41228b1751
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:24:00 +0000

    morning near vision mend sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
