2a2a06bc83d82e747514b9f48417af5cc7b358a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:48:14 +0000

    poison safe earn to arrangement

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def find_max(numbers):
+        return max(numbers)
