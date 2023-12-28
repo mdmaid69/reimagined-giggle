0b6d9623da966b84ce38ab81c3c1a8ce1578adbe
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:55:35 +0000

    along squash support introduce letter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_as_int(array):
+        return int(array[0])
