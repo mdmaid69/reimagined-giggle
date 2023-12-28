511b419249cea6d8111e4eb224d6d685110a94a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:55:46 +0000

    lead intelligent under natural future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
