5b186f8d5f27110479f7b59a3835119a89b80dad
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:27:20 +0000

    actual blue gooff Jj building

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
