3395bef15f304990749c08a48aeac52d2fbd3ea5
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:11:21 +0000

    sink direct ilovedicks confidence change

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
