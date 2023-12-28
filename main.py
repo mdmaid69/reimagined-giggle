35301c0e609624906c9cbb34a835fb5ca931c671
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:36:42 +0000

    motion style hate complicated who

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
