c9fba9395c9e58c6acc28cffb8c5719b3ad489c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:18:19 +0000

    careful importance ring rank distance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
