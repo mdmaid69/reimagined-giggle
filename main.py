f16ad23cfaedd1739f2055513d7b6c38824edd16
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:57:19 +0000

    tidy sensible calm vanish hurt

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_area(radius):
+        return 3.14 * radius * radius
