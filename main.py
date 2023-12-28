557fa8f0c4c485d6aa688ad80c938e84d8f81bd1
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:01:32 +0000

    cruel oil identify defend temper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
