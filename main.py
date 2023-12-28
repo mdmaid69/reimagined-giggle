44606a38f58cb31ae3428fef38fa155c3bebdbc8
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:09:16 +0000

    Qq heavy unkind buy conduct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
