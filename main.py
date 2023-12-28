a12bf5647bde96b41e9f86ace1e64fc33fa0ace9
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:23:23 +0000

    breakin especially uncomfortable lift company

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def square_number(x):
-        return x**2
+def calculate_area_rectangle(l, w):
+        return l * w
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
