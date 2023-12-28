8536e135f0946606bb7c146ce08bee34ec5971ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:45:58 +0000

    backwards bread poor figure collect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
