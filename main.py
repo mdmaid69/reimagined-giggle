f17404f6a1722f1ece78b5d9b394fec2763933a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:27:25 +0000

    present well deliberate past yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
