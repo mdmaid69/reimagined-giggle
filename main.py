b817f35f1173d3f8662234e5015ce42f070a7048
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:40:44 +0000

    allow correction type measure medium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
