29d4d0559cfcff5734c3049ca65d0c90b600cfed
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:38:20 +0000

    march repeat little lesson children

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
