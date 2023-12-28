78bdf8c9fedc067645012172d74033abd999dfe3
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:08:09 +0000

    strong person taste temperature round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
