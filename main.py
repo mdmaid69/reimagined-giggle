7785fc28ef0344047f28de283f5a91fcf34b7494
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:35:45 +0000

    Ii hope geton confuse smoke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def square_number(x):
+        return x**2
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
