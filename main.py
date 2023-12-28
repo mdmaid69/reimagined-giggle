da5abea7092a37babcbdf9291001d5363db99d5f
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:11:23 +0000

    bet discuss loss tip pass

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
