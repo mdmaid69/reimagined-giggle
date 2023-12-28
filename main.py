6aafa62b79e481981362b194f4011229077bbd35
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:32:01 +0000

    autumn country join subject protect

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def get_array_as_set(array):
-        return set(array)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
