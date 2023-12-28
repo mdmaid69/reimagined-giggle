03c77ff1c41109183f22f9706f1121f6ab4be3a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:10:08 +0000

    office someone little protest callat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_float(array):
+        return float(array[0])
