43da24d5725dafbd9dad2eaf4194836dbedf8b57
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:53:55 +0000

    impatient well wonder flat check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_item(array, i):
+        return array[i]
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
