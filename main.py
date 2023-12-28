eba6b850701509616c77400eca65d1f5de367de6
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:52:39 +0000

    towards flat slide cheat pressure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def is_even(n):
-        return n % 2 == 0
+def sort_list(lst):
+        return sorted(lst)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
