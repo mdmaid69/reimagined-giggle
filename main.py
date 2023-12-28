160c8fea5f51287ecb60913231c8efccdd00f917
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:43:33 +0000

    godown cheek limit stay show

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
