3fcabf666d5bf1b063a6c1dffbdbef0cd22fb6df
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:38:01 +0000

    middle ashamed exchange poison practise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def add_numbers(x, y):
-        return x + y
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import time
+print(time.time())
