8a4a3e92019f6bbcbd2ce05339b856244917f58c
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:05:07 +0000

    children switch exchange better maximum

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
