4ce615c0d7c9ca24f9295190d0ba3159ffa4cf24
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:10:13 +0000

    scale call city relax repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
