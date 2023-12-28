eaaebbbe50d029467c9ff09a3d555c7dabc1a536
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:06:05 +0000

    related front force check several

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
