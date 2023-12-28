1ece1e252b3661fda0666537f0e6e411bcf7cdd5
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:58:38 +0000

    be awake arrival rise principle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
