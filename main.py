14a03a3cc3f82d81cc4811d645e62081b1bff23e
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:57:38 +0000

    university state provide direction somehow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
