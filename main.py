df6031e1fe8aaa5c17089a33a966874c03d3ad01
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:07:22 +0000

    worried callout use where little

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def find_unique_words(sentence):
+        return set(sentence.split())
