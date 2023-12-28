2a5592af2e59a901173d4bef2ae5d16adf6c36c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:37:31 +0000

    holiday through picture interested sad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def square_number(x):
+        return x**2
