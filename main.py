13f11093671cd013ca3e2119d6fbd7800645d20c
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:21:38 +0000

    night instant play square city

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_power(work, time):
+        return work / time
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
