3a93b2451940838534fa3a5049f5a0881f39e8c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:30:46 +0000

    if comfort comedown small sensible

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
