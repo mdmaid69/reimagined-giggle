6cd0eba99b39c3ad8e670c377c6ad18a9ec37d1a
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:49:05 +0000

    various wind group book concentrate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
