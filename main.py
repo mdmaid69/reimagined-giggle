d1e02acd1c26c060c0d968477b4ca36ea2e356e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:00:06 +0000

    basic just so with use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import os
-print(os.getcwd())
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
