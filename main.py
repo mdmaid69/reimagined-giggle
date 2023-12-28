a797b6409e0365b44b6ca7a81e8088633bbc759e
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:30:51 +0000

    run crowded upwards short wipe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import numpy as np
+print(np.array([1, 2, 3]))
