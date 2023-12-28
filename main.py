d70228b2d612d967d5a4ac7d97d9c5a42964abcf
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:07:41 +0000

    name front border row fear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
