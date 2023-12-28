706d59861dba234bfb5a53b6f744058b67c32106
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:05:16 +0000

    safe hot ignore congratulate careless

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
