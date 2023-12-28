9ee60e88b5550b456ae1292d3ae8c7176092ff17
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:58:24 +0000

    small interruption supply now taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_owner(file_name):
+  def get_file_uid(file_name):
         return os.stat(file_name).st_uid
