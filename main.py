b5db91dccccf167764bec2230107754b5934306f
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:38:38 +0000

    central sell answer flow trust

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-def reverse_string(s):
-        return s[::-1]
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
