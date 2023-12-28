6fb98f6845c18c7bfe0a44502e7e21f4983e39df
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:28:11 +0000

    shake search make whistle tongue

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def convert_array_to_string(array):
+        return array.tostring()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
