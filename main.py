7f2cc17a8e13107f297bce5967b700d82f134143
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:20:39 +0000

    secret smile steal aim variety

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def add_numbers(x, y):
+        return x + y
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
