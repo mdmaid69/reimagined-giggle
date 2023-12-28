d6633c29cc7fad3f330d58d4288781b821729dd7
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:39:57 +0000

    favourite quantity separate floor straight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import collections
-def create_queue():
-        return collections.deque()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_density(mass, volume):
+        return mass / volume
