15f5d4ea8d294e656e4c996cd41151361916dfcf
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:36:48 +0000

    dress door partner opponent concern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
