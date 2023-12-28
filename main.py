c790cdd66720fb6a21b3af6538b0c5e8127bb08d
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:48:47 +0000

    actual skirt later close ready

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def sort_list(lst):
-        return sorted(lst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
