cedfc4c93f18dbd7c5e701e99f5fa08f75684425
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:32:09 +0000

    point anxious fast curious tell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_length(array):
+        return len(array)
+  def remove_duplicates(lst):
+        return list(set(lst))
