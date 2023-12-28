70347677b5124fc66938ff2d291c4df4d284b9a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:32:56 +0000

    total exaggerate strength drop minority

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def sort_numbers(numbers):
+        return sorted(numbers)
+import os
+print(os.getcwd())
