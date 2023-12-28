030c09306167f5a0547384d2c4efc067cf1246f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:59:47 +0000

    town too art lose flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def get_array_as_str(array):
+        return str(array)
