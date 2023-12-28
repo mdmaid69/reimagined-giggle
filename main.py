4c878b2a30adb01b3688e5ee61fa05c347c9fdaa
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:03:34 +0000

    defend university apply set interruption

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
