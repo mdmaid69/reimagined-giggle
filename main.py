2c2d494c2d0fec5b3305bdcade50a157278bd4b8
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:42:01 +0000

    memory mind dear through ordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
