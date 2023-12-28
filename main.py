f48fd34b66cff51a1f96b5b22e40ae3ed6b3e5ba
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:26:55 +0000

    somewhere phone pure giveback declare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
