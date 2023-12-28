6ba285da89e1ca45aba1f42e0d2fd2710873b466
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:08:34 +0000

    pleased spread swing real holdup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
