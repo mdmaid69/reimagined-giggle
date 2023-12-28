633db832a697449b4ff83c7e38076522661136b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:28:45 +0000

    complain centimetre phone dream piece

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
