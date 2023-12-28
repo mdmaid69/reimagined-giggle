55320754ddf55d993ec3167104eb92dbd5a71b77
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:12:40 +0000

    connect share dance sell cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
