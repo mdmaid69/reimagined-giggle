d45eedc5943361ca9a31c9afca81a45e1a1900d0
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:32:26 +0000

    careless thinkthrough draw goaway maybe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
