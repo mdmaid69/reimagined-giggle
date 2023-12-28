4392cf6db6084714547fb046b10e927e485fc382
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:02:29 +0000

    godown furniture after custom view

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import sys
+  def get_python_version():
+        return sys.version
