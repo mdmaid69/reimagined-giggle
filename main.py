0b47c5a798c3e8b433743083746e647beedb1da0
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:30:09 +0000

    private traffic shoulder grow punish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
