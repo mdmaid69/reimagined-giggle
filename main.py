5306bc952e36a0b66fd953284ef5b6b2220e970f
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:43:51 +0000

    intend border cook sudden conduct

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
