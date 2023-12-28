897ab3613c4f4a7744d8402d567ad2a6e51fa4ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:43:13 +0000

    please godown catch sad wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
