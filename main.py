8b200a5ed5440c24aece9b33cbf15757515e29fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:52:57 +0000

    date write include fall patience

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
