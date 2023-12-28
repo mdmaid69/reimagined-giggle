5bbeef7d1b74f96af6152bdd98d5a22ca7c8f4cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:33:18 +0000

    fix no burn describe herself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print(sum(range(10)))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
