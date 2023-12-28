06144fd727d00bef0af904ff10b106d24931380a
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:37:52 +0000

    today destroy rob balance corner

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
