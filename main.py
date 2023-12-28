dff8af8b5358d68e931ea3cb92048eba57b711f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:30:01 +0000

    deserve bridge left story recently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
