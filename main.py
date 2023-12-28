941870beb9ee3f93a8b3b5d97e394621db145219
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:58:53 +0000

    bear report hope language finally

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
