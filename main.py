7363f000b708656b081cc091cd0727e35a4a1afb
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:23:00 +0000

    equal swing little priority bear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_as_str(array):
-        return str(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import collections
+def create_counter():
+        return collections.Counter()
