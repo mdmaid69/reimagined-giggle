54652f64611992562004a3b26b0ee5a2a4529196
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:33:59 +0000

    arrival touch traffic gather city

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
