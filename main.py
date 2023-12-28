2185eb32811ff37c8621b9d435ff8045ca738317
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:48:28 +0000

    upstairs star love lesson crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_min(numbers):
+        return min(numbers)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
