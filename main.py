3a279457d47cf853a603b701132a5505a3f08e66
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:20:20 +0000

    young later raise principle swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_area_rectangle(l, w):
+        return l * w
