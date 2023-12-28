be90c74bb5a81512804e55b8cc38ce540aa1cc01
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:33:30 +0000

    Ll trip challenge direct ever

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
