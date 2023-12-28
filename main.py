17934f208bc6723d8f95fbc7118486ab2b5e8db0
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:53:15 +0000

    determined set runinto replace present

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
