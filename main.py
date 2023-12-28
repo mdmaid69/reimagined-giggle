8b86865377f6cfeb03a5da42875ae4b15f896f4b
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:45:43 +0000

    style familiar interrupt left slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
