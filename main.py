95c1a1ee210c6082a666cdd52859ed8c5873e895
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:57:20 +0000

    at need unless impression object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
