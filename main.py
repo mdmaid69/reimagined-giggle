58f4aae150bed06a63b98d301832be7db8d9a2a1
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:38:49 +0000

    sure calm straight solve child

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
