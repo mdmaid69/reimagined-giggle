5115fb3cd185ca2943c443cfaf45e135e0dee4f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:56:31 +0000

    this total squash record concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
