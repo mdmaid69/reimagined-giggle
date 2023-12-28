fc93a9f4aa3ad8ea517af81fe89bb3682d5f15f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:39:01 +0000

    closed clearly present you collect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
