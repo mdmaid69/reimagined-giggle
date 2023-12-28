42508c1dae07155197629abc81b7930a54a9bfcf
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:15:03 +0000

    extraordinary comefrom border sad thinkof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_acceleration(speed, time):
+        return speed / time
