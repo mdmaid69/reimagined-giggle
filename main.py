bacf48b058f896cd21992e8d001fd8d11a2b79b6
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:31:20 +0000

    respect success invite speed over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
