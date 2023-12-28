a9d72ef26347a9007f2ce20ecbd6d972467a75c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:20:58 +0000

    somehow public typical holdup dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_volume(length, width, height):
+        return length * width * height
