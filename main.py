25dd62442f0bf2be0d31ab84797c23ba52f86288
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:53:25 +0000

    near glad bread lend attractive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+from collections import Counter
+print(Counter("hello world"))
