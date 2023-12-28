af7564504c25bba0ec764914548726ec443e3560
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:09:33 +0000

    ilovedicks better free regular wear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
