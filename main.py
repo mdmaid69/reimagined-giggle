cc6b28cdfd2771c0cfe30f9b85419992c119832d
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:58:41 +0000

    breakthrough sister reduce shut its

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def count_elements(lst):
+        return len(lst)
