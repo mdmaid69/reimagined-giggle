506db148f4608a3a82b840e99cb5279c3387b0b9
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:09:20 +0000

    564encourage past popular anywayanyhow cruelty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_current_directory():
+        return os.getcwd()
