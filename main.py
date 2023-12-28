38f668c70ba8ece12105b1ac7f4ae8ba122f9422
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:21:56 +0000

    character house abroad proof deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
