791b703a6753d102040da380b0ddbd6283b3dc3e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:22:17 +0000

    final Bb attach side earth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
