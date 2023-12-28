7473647924330b90c1c43fa6815dbb2b27a8635a
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:59:53 +0000

    sea should punish top off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
