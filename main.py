9c561ccce37a3426b771a26b00a53b4b66a7d0af
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:07:48 +0000

    safety container strength turn correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def sort_list(lst):
-        return sorted(lst)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
