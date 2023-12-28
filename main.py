c02316f3ef2bbd8cc6cd81e003769504d127ca6c
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:01:16 +0000

    put attractive across cross sight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
