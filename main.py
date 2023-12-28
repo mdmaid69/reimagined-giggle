9bf4e5335af6b351e0f1c1cfe48e7e90c452b928
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:11:18 +0000

    steal excuse cost keepfrom national

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
