6a5b8480819864579a0df73eef54addafac61c50
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:33:25 +0000

    concentrate impossible cheap attract squash

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
