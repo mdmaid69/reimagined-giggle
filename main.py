2e6794eaae08a8596f4c81c5eca36fc7130c5bde
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:10:40 +0000

    fall whisper punish cruel strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
