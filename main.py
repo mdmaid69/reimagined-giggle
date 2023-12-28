9e6154c29ad96ed75e4299aa3ab8fda3bb665b6e
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:13:52 +0000

    interview uniform year brief quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  def convert_to_binary(n):
+        return bin(n)
