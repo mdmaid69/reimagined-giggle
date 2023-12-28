bc5940c867bcc2b8e6fbd71e5f2ee2553bf02e75
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:55:44 +0000

    break close nearly habit other

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def convert_to_octal(n):
-        return oct(n)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
