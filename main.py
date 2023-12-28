020ae6f88f991f3dbb99577fe58ec6cf6df91390
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:54:07 +0000

    nerve answer autumn might professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import platform
+def get_python_version():
+        return platform.python_version()
+import numpy as np
+print(np.array([1, 2, 3]))
