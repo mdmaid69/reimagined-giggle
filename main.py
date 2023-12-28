b9ef777980006804b63b6c48272cb0ab897f3358
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:22:36 +0000

    keep ilovedicks glass sing direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  def convert_to_hex(n):
-        return hex(n)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
