2a29b21fd6a7ab0a0b5f398cf5ea5c12e4aa0775
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:37:53 +0000

    plan plant because 567enemy himself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_pressure(force, area):
+        return force / area
