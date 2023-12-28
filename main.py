32d63c9f8ff79896089ef517bf98ee85f49ba626
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:07:23 +0000

    up 566end exciting pin trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+for i in range(5):
+        print(i)
