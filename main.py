6dd1c1661cdf70d795e9137c92702fc71b61bc50
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:47:30 +0000

    tradition feel further phone home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area(radius):
+        return 3.14 * radius * radius
