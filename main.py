d6d665d1d740da18d341ecf86a239ef28df88078
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:03:10 +0000

    spot quickly embarrassing enthusiastic yes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_base_name(path):
+        return os.path.basename(path)
