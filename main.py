86baffaec05aad9ec4f46111db6d3eb36ffc4550
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:21:10 +0000

    bunch scrap she speech rid

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
