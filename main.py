b30814d27841b607bd5fc28878da2fa4ded444ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:06:50 +0000

    nothing floor by familiar prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
