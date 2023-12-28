62720f002222bb5883f49c696818af9a15e5ea7a
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:22:13 +0000

    mark disappear broken heart but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
