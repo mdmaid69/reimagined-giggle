81d7f604a1f7a10619ff5beb5b060b3ab0d91965
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:17:55 +0000

    accommodation regional sick fail aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
