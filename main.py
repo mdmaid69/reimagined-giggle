67832142027508bc71718dde5be081793be1c897
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:06:31 +0000

    field concentrate live rest design

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_length(array):
+        return len(array)
