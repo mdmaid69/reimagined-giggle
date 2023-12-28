2c8fd450454484e44b05ea96415bd6ca1e396ac8
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:27:43 +0000

    change ago figure breakfast precise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def remove_duplicates(lst):
+        return list(set(lst))
