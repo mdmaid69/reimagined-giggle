567008e87d47fbb21dae86ab183e8a5d60ea06ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:05:34 +0000

    total so life line motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
