099f90d25bbbf9695a24ea47a10da1ddadb18fe9
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:40:20 +0000

    confidence block pupil speed lonely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-print([x**2 for x in range(10)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
