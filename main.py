6a3ebd1ca9de9deeeea41f45296665f2bb21ffd0
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:31:59 +0000

    evidence decision stage visit available

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import array
-def get_array_as_float(array):
-        return float(array[0])
