cd6ce67977b78dc610948bda60a6ba2d33ba8057
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:10:14 +0000

    delight ask result conclusion disagree

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-for i in range(5):
-        print(i)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
