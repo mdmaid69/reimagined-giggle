a291e868539a799db947a058b4ca3b3f47d69775
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:42:02 +0000

    art almost spring Tt object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def cube_number(x):
-        return x**3
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
