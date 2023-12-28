2b42db6164bbe93ebbcc807e5e19f15f3039107d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:24:00 +0000

    might polite could tread tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
