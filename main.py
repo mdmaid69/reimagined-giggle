fab4157997a482af5d9e6f960480ae0eca420173
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:28:26 +0000

    view to objection remark dance

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
