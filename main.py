a736d6c47e260a3232319f066310f9320ff44076
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:30:17 +0000

    basically sting call warn middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import tensorflow as tf
+print(tf.__version__)
