300d29e7ec83b88adf50536cf27d09a44dcd4f0b
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:01:01 +0000

    game pretty point truth object

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
