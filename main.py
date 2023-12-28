52ad86f49794d5e6ec020008b303490a0eea85f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:50:38 +0000

    agree brush case factory easy

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import sys
+def exit_program():
+        sys.exit()
