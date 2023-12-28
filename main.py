5f96835b04dffac7102c634be00cc04972f5910e
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:57:50 +0000

    crazy peace city trade here

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_array_as_float(array):
+        return float(array[0])
