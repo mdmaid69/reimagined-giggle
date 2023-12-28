f803eaddcf53b2e54f0eda43c836ee035660d82d
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:47:05 +0000

    lot cry subject press declare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
