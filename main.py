58b344f3e3232767875caf9be48c4b378bc47c7d
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:15:56 +0000

    general wipe name step soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import numpy as np
+print(np.array([1, 2, 3]))
