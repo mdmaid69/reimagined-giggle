afc47b4cbb7110402c8d487f67802e9cc97d4435
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:09:27 +0000

    pot bury punish hard meeting

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
