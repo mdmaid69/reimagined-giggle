bc8ae6381bd293fb2779e9a17d24b6ab7c225a98
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:49:53 +0000

    worried curious light match direct

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
