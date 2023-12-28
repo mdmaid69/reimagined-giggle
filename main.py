cc9a434ff8d87093c5a3cf890f6b486928224476
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:05:40 +0000

    favour forget giveout spite frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_as_float(array):
+        return float(array[0])
