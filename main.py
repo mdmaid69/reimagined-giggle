96da91a9ed5a8c8e18236997937dace49e848272
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:20:28 +0000

    listen possible concern invite hurt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import random
-print(random.randint(0, 100))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
