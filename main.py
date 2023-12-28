0876af56739101db69948f6c92922a1c784488c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:33:41 +0000

    trap stand must sister onto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_cosine(x):
+        return math.cos(x)
