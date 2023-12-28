0709197e100c3c50dc199a6ae789d869790d9d22
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:47:05 +0000

    city since reason walk even

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def count_elements(lst):
-        return len(lst)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
