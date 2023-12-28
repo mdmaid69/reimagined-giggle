a3e448df3ae582d4a82308fa8fbacac858c74e8d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:10:08 +0000

    hello drink ticket opportunity finally

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
