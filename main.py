30a326d16f9944033b7204b6b71b545f0a470d06
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:52:29 +0000

    remain separate accordingto full instead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_density(mass, volume):
+        return mass / volume
