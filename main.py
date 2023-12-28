d7058572c6bf49fb67724151a1c61c06124872aa
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:46:13 +0000

    contact as every worse answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
