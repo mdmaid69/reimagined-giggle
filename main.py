7aef4e0ef1603a8191e7c1db554d335ebe011120
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:29:50 +0000

    male convinced chief break shadow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_area_circle(r):
+        return 3.14 * r**2
