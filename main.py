7a2919ec7f0b1490dc1199e93ecc75e3133e09fd
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:31:47 +0000

    range responsibility save run surprised

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
