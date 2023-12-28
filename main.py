ddf35c62fe209585abedc039e75b85687992131d
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:43:51 +0000

    autumn tidy usually sensible centimetre

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
