b64058a3df9ff5d541e26d3656680ff6606fa27b
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:35:18 +0000

    seem disagree sort seem block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
+  def plot_graph(x, y):
+        plt.plot(x, y)
         plt.show()
-name = "Python"
-print("Hello,", name)
