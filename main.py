bf03b06b002fa6df974d96141b0b562ec5c85972
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:08:21 +0000

    Kk tongue value secret write

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
