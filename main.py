4cbf8f7259ba98fa5c020c2fbfabce7d6e9fbce0
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:05:17 +0000

    important face design through but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
