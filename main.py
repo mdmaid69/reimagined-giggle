c132407c2a25d84074bb68be9a33d7f839aa50fd
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:33:38 +0000

    one getup teach to lookup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
