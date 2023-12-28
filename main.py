d1002ffe6b1d2ca98133fd924140b616d71bdc6b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:19:52 +0000

    exit available dance giveback might

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+from collections import Counter
+print(Counter("hello world"))
