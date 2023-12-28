2038fbb7f88f0bfac7c7007acfacf67a4b6e9294
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:24:40 +0000

    mine prize fear cook broken

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
