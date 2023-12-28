7c35b37e69f97fc6a1c2a16a6cb9b347faae5774
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:08:12 +0000

    trick label direct inquire want

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
