8cc99a13c95067dcabf3ed387b3bdfa6e40be219
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:36:58 +0000

    negative unkind print influence Pp

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+x = 10
+y = 20
+print("Sum:", x + y)
