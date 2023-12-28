63e22bcf99d4ca96fd53ea3260a14c386782349d
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:10:59 +0000

    right call wise harm owe

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import sys
+def exit_program():
+        sys.exit()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
