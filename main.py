946b983a0edc195d66b183906669fc02639004b7
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:16:59 +0000

    view operation true your correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
