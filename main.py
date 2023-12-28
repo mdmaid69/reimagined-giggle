f63d6e5da2573666da761b74b791f73769469b35
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:24:04 +0000

    beat dance cover claim beside

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def count_elements(lst):
+        return len(lst)
+import sys
+print(sys.version)
