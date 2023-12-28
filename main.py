cc57bf276f25b0b22868613cede3cba2550fa607
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:38:17 +0000

    escape spring one wave pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
