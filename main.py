64f69327de6cf020e67991b2da3c32b55be5ca1b
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:46:52 +0000

    spin switch complete unless expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
