6b72213bbf6dc0b08adb282acffdb6bc07bf9077
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:37:46 +0000

    drive goon fortunately across quarter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import numpy as np
-print(np.array([1, 2, 3]))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
