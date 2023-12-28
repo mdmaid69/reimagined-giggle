2d7893cfd2572e4fea0b920760867f6fa6ae2fad
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:11:49 +0000

    meeting impressive position notice such

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
