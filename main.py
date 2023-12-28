81b5963060b2599e8ca4575b03794b34bd6cfe0d
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:11:13 +0000

    exploration injury Qq left pipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+for i in range(5):
+        print(i)
