c29816c0fca6d6413ae09cfecf606ad39b645e2f
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:38:47 +0000

    frightened arrangement introduction people say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
