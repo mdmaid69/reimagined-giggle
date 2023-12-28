d99132135c63928d8494174af4e2e84b78175265
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:37:53 +0000

    share glass comeover advice every

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
