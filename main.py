c965234a09d85edf373633de5412ffd5e74071a1
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:58:56 +0000

    factory provide giveback satisfactory cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
