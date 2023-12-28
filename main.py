bd7bf5ec49ee2b584fe929e581137f13a481a7bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:32:51 +0000

    book unfortunate drive government absolutely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
