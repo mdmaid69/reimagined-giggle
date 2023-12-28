1b73373a2e5c2645c7b8a3ad97408538f2a9758b
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:51:59 +0000

    best score colour whoever no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import random
+def roll_die():
+        return random.randint(1, 6)
+import getpass
+def get_username():
+        return getpass.getuser()
