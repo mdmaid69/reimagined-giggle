0223b817d46a2e6ec5a9c27882f2e44880020f9e
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:47:30 +0000

    effect cheque punish great adjective

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
