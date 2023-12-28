07bc9eb4469441d86e8564e1086a2739ecee4263
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:09:18 +0000

    soft wander frame tread talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import array
 def convert_array_to_list(array):
         return array.tolist()
