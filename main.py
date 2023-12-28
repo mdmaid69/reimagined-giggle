f33100cfb1d2b03c1d08f10f4d123bc30ce46e81
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:26:12 +0000

    later business guess also undo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_array_length(array):
+        return len(array)
