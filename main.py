76ce42b91cb395f398032fc02598627cfd31bd06
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:31:47 +0000

    month room pinch pain gather

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
