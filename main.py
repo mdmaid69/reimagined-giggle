b545a9fe26b56a879d58589ee5e4ea8d4efc70ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:46:10 +0000

    wish rob offer correction unusual

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+print(time.time())
