2d7cfb5bb6917af747673574d2ea91863464b9aa
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:47:02 +0000

    opinion muscle method mass inform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import sys
+def exit_program():
+        sys.exit()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
