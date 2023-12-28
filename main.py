a45202411577cf2257628bc300c988f2ef1c4977
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:12:19 +0000

    terrible star telephone upwards popular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
