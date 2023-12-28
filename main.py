8bf968a4d80bdd2e7f217c414ec8c234cae13e3e
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:31:54 +0000

    part food programme throw spring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_birthtime(file_name):
         return os.stat(file_name).st_birthtime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
