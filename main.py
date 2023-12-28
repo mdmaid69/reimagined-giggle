b8c318c65149a540c20f4dd6debc0f97219def7c
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:08:17 +0000

    check class book disagree send

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
