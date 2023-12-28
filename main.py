ea425da5245bcdd3bf77d3a4b7be599a0f3f820a
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:12:03 +0000

    everywhere certain check tired less

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import sys
+def exit_program():
+        sys.exit()
