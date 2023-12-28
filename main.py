73dbfc685c83c3b1afb65bba3fc804140ed0b379
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:07:57 +0000

    summer talk bright advise rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
