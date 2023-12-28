2ef3c7f9866265d3aacbf50f2a7a442c5b905897
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:08:44 +0000

    example whether factory dangerous proof

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
