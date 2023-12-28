3b0fa3b7372792538d9740beb95ab52581ff5df6
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:00:28 +0000

    sorry base considering convincing store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
