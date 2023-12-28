41f288abd341e32a6953d5e6fad0ec773e27deea
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:58:59 +0000

    experience treat music exciting object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
