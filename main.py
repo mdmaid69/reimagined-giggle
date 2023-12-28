3840b4b9d4612f403ce6c340aa3686b877ad4f33
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:46:55 +0000

    tense experience load food relation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def remove_from_array(array, item):
+        array.remove(item)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
