2310c615f6b4616a75654f24314a1b940dd5c7ff
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:11:42 +0000

    after appearance shadow statement nice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_acceleration(speed, time):
+        return speed / time
