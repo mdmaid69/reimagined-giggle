4957cf83ba8dca8c58d1ab9462fae9f5ad299dec
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:22:53 +0000

    square chance space wide see

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
