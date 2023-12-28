e71c81c0b06d6c643e290c81e0f16d391398cf06
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:49:46 +0000

    that listen illness voice today

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def cube_number(x):
+        return x**3
