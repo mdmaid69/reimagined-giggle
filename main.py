0956a542a4b6826ec279943bb5839713a5f90450
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:16:09 +0000

    maybe wild port sentence getback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
