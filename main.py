1ab25491f01671d45e2c4080dfe3f32bc726ca8b
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:05:09 +0000

    lookaround transparent machine enthusiastic off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
