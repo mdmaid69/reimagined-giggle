92dbd832c792e5dd173f0d7be50370b7dfa4d83e
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:07:37 +0000

    maintain throw Bb reason control

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+print("Cube numbers:", [x**3 for x in range(n)])
+  def add_numbers(x, y):
+        return x + y
