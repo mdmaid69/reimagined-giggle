5003a12f45218bc6759a29e4e766ce792ebaf274
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:08:55 +0000

    mistake pen obtain breakthrough turnaway

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
