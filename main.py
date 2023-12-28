bda8fc425f36e3a24ccb30ac98b2f563b382fd60
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:10:07 +0000

    comfort report capital test one

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
