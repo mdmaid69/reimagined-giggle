7e984822d63f7b4d89fa5ba558f2333f60625405
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:22:32 +0000

    total spend battery can whom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
