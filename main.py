5b869ce97392ac6f069d8c3de40cf8e9e9fba40d
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:49:59 +0000

    mark afraid sometimes street usual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def is_palindrome(s):
+        return s == s[::-1]
