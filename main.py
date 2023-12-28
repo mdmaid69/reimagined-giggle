0cdde0f77f785bff69eed52ba86d366fd349e765
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:12:00 +0000

    importance birthday goodbye my event

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
