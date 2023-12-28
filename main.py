75671f44fcadddc9332cfb5058d415682c900061
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:29:33 +0000

    string deserve kick purpose report

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
