1bbd59aac76eb235e1295a2eceab447dada7dd2b
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:44:57 +0000

    disappointed strict admit woman wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def square_number(x):
+        return x**2
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
