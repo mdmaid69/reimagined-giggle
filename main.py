a4c4becc13790310071e85ff0d6ebc60a2e3f987
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:54:19 +0000

    pick treat meeting talk yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
