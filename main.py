e75136d2972a7ce7acf47f31a3316f047cda94ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:58:38 +0000

    backwards suck row entrance thinkover

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_power(work, time):
+        return work / time
