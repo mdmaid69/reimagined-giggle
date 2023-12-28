d386c69114d0e338e503c9fe57b3e77977cd0c4f
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:10:16 +0000

    drawing use communicate first lay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def add_numbers(a, b):
-        return a + b
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
