12488df6770842807b82b8f3c4476f5ff07f9455
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:21:18 +0000

    statement offer transport touch equal

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
