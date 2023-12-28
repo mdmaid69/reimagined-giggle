0960f40d3a9eec8b2da82d59f212e6a1e38a015e
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:47:23 +0000

    lookback(on) road heart talk book

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_hex(n):
+        return hex(n)
