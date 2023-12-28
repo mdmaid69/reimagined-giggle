128bb347ff6f2f04991d29eabda4a67ee49ff600
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:15:49 +0000

    exit shall thin talk accordingto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
