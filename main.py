3ce24979d4544664bbcbfeda31147abdc7b217b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:05:07 +0000

    likely hold can careless slip

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def divide_numbers(x, y):
+        return x / y
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
