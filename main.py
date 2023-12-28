490659fc868744e6f890deb14114597f759d3b54
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:53:49 +0000

    oil getback mine mine listen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-for i in range(5):
-        print(i)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
