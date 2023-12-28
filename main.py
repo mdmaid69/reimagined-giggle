89b9c7a72ee509d2b25136ec6cce0a89ae904596
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:25:16 +0000

    section fetch circumstance time play

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
