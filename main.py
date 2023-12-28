75dbf7704a694822c134b5df6d39d413314f493a
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:02:37 +0000

    beautiful complaint check Nn leave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_palindrome(s):
+        return s == s[::-1]
