8e2a1eab7b1f656b0f92129ec414c6808645b41b
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:53:58 +0000

    about towards criticism brother Ll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
