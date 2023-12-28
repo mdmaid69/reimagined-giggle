42910650ec6ae357c8d9c88e0c8d887d2014ed3a
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:45:43 +0000

    dinner previous no during spite

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
