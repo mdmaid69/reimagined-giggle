efcc308dc6e48b43a5ddc1bb27de44a1836b4ec3
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:36:49 +0000

    copy present distant imagine Qq

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import datetime
+def get_today_date():
+        return datetime.date.today()
