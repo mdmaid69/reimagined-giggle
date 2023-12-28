c1dabb2bae38d2032a78eac597a8278930a55812
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:17:11 +0000

    scream ocean delicate uniform muscle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def convert_to_hex(n):
+        return hex(n)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
