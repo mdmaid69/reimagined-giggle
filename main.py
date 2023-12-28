93a6dde29dcc7c2e0ef85b2bffa1496c645f4ffb
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:36:33 +0000

    against situation ago clear entry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
