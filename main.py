4b4690653b8f5e390102d4087a588c5b123fbcbb
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:35:05 +0000

    activity congratulate whatever several uncertain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
