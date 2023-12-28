846f5abbf324997a6c056555484c5bbde2368b15
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:31:33 +0000

    shop geton scream talkback getoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
