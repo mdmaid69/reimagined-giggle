2cef32edf350014fecc32c69485b13a9735e4b28
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:53:47 +0000

    opposite handle connected thanks film

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_octal(n):
-        return oct(n)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def square_number(x):
+        return x**2
