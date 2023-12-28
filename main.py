639b770852d307ecb17185628703c1113ac09d63
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:52:41 +0000

    believe bridge spare from join

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
