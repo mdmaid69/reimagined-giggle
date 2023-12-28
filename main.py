38a59914a08068c4ba7752377490a2e8010a863d
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:17:24 +0000

    slow callat unlucky comment late

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def is_odd(n):
+        return n % 2 != 0
