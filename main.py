319ed73deef12c56cf81d7c6410a1e51e3302820
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:58:49 +0000

    first job behaviour compare foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import getpass
+def get_username():
+        return getpass.getuser()
