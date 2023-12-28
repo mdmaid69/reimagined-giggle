d8fbd9e55c16e98f202c6ae268a41cca7860dae7
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:17:08 +0000

    uniform plenty be medium section

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import tensorflow as tf
+print(tf.__version__)
