7e19dfa2b0143b39700e5bc5f3b4418825937f5b
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:06:55 +0000

    close bright keep other confidence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
