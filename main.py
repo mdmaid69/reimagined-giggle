ca9a7f03542cd1ba1e365125fe2038a0fbb14128
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:27:20 +0000

    survive often her convincing ocean

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
