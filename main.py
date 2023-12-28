8f1d2a851b9d65345a527817dca4cbc684f2a5c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:13:42 +0000

    whoever interested nature meet bunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def get_current_time():
-        return time.time()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
