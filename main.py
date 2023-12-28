8188dbf3a75c1c33544a55a9ee9ac12e5c153596
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:12:54 +0000

    shrink estimate worth scared regional

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
