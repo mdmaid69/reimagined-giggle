d1dd8c1b16dab8804bfa6fd542ac7374d42599d0
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:00:45 +0000

    its grade zero turnon generally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_volume(length, width, height):
+        return length * width * height
