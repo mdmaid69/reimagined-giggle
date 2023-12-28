8d34acd710718b10e2ded9a688a0204ba04ed062
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:45:12 +0000

    broken duty keepoff spend giveout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_pressure(force, area):
-        return force / area
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
