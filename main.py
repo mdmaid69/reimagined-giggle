5445c457f2cfe7f06a7d570c1b483792374de7e1
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:45:22 +0000

    at ride light waste shock

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
