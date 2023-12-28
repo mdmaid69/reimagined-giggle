1c7aa2f916febc18c139a7d0e882b12b3499875b
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:21:57 +0000

    create add convince occur figure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def calculate_area_rectangle(l, w):
+        return l * w
