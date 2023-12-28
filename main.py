fef5d449d31c58491f005daf38af5c27c0121f80
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:19:52 +0000

    gap comfortable 570enjoyable unkind coffee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import time
-print(time.time())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
