4dff60319b8db134a8221d75a8fd1bbc00101af3
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:34:57 +0000

    mark nature country spend regard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
