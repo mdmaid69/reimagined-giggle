27ee2f3b7409c762b9c3ee126ca2ea3210b9eaa1
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:22:32 +0000

    rub field talkoutof terrible chase

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def subtract_numbers(x, y):
+        return x - y
