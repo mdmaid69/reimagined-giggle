ff1f0b36603defe26f2aec132abd49664a7739ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:57:15 +0000

    know ignore bored copy style

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
