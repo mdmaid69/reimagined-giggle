e7a90e5683f04fc8ce21dc4ebbb407490f61e57a
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:50:51 +0000

    sensitive mood each continue interview

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
