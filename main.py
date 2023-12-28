1db8c3130355ec73425e184474885da6176bda49
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:52:58 +0000

    me benefit juice raw replace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  def convert_to_binary(n):
+        return bin(n)
