717012b7c8340d74f7bf47062f5b7c284dc1f9c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:43:30 +0000

    smell tip hate depend out

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
