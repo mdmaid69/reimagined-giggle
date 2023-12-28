48e6722b2a4a820196720767c37fc50ea195c8f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:38:28 +0000

    tight apparently note this impatient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
