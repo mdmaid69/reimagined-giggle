6c3f4eac811c7b957183f5a4aa12b49430d3c407
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:09:51 +0000

    giveout without relative nurse street

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
