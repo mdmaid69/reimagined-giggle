ffd2d923f966b28e1cff475bf4cc9d4cf9f88a98
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:59:43 +0000

    section shout Ee downstairs closed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
