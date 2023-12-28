784f7dccd512c7df038c3c79440c99a340f83c00
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:06:23 +0000

    last angry press positive valuable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+print("Hello, world!")
