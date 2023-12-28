996842e18498d0c37ed4a6d50317d5a688068173
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:19:28 +0000

    flat preparation like reduction touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
