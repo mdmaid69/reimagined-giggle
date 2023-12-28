a231ae84045eddc08369bd87bb3af7614511245e
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:29:41 +0000

    false pause shadow rush everybody

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
