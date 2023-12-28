b1f784c7413410b93415dee43e6e80bb9d7cb85a
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:02:13 +0000

    type possession low colour person

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
