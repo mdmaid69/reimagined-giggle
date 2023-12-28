2add269ccf049fb9119c51bc38e23d8d44761951
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:18:54 +0000

    claim trip vote child pleasant

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
