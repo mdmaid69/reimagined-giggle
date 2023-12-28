bcdf32d2e81a939c96fccb09b56d27caea184b29
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:05:30 +0000

    class mother once centimetre good

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
