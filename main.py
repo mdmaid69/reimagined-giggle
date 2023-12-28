993f7969196e8af4ed8188cc0748ac0b004e6734
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:38:09 +0000

    window group morning ourselves write

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import time
+print(time.time())
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
