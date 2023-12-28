9502f4316cd97db26fa7137cb5bb548442e72cc5
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:27:47 +0000

    wash shadow squeeze name finish

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import sys
-def exit_program():
-        sys.exit()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_as_int(array):
+        return int(array[0])
