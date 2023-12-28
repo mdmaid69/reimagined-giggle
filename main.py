687ac5b8b5815458de8f321905ec450835d58c86
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:58:44 +0000

    central confused anybody may blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
