60cd59c7dc0823d2b3b8f35f2b7ca87f30ce4281
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:00:56 +0000

    difficult hurry bin town and

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
