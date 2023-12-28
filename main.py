cb443eecc9cf6a306c6c7eb732b24f95f2d51fb9
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:40:38 +0000

    call behind connected corner spray

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
