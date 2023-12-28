fa63425da26e568c69ccb75ebaf59e3d65816dc1
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:13:31 +0000

    lookup the tonight dress square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
