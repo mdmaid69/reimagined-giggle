919bddd57746580a6e241bf39cf9a0320fafcfc4
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:14:48 +0000

    complete shut open feel frightened

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
