e4bcc7933d359802806519901945a3b3f76b228c
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:06:52 +0000

    turnback touch bit march apply

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+from collections import Counter
+print(Counter("hello world"))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
