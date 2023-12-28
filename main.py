85172dfc2987d2e457e4b8c363ea8972e1d6dd9a
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:22:09 +0000

    tidy charge innocent shallow polite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
