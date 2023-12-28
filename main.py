e2a29fd4c5d65857cb4303b9e4bad097d35af570
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:24:31 +0000

    announce remain motion ability mind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
