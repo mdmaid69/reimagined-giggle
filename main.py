321bdba1bb657670103a259662823a09fbb1102e
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:53:31 +0000

    supply tip manage decision early

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
