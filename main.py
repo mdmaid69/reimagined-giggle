6272b1a3faaece561ff083d4f01531069181b9bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:34:26 +0000

    operation Qq warm load 566end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
