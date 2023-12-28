bb68a2a56fa7302b99b543ff81e8aa3151222ac2
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:16:49 +0000

    let sing quick listenfor description

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
