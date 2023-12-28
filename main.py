88a4a2aefdf2d05f2b8517e8eb266df6851cd0a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:13:35 +0000

    instruction warn whenever succeed remove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_speed(distance, time):
-        return distance / time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
