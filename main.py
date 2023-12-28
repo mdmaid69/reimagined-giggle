91b91a4dc131de65217b9243ff4dd86225d4ea6b
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:10:39 +0000

    hit goup party broken contain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
