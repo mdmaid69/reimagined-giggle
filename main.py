3e79892248897b3e411c1b2235d72e14e7258f58
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:42:59 +0000

    brave regard lie try ability

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_str(array):
+        return str(array)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
