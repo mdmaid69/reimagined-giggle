a3e8d26eac85c6e540c7e6e37f04a43cb2af05ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:47:22 +0000

    refuse its point object passage

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_pressure(force, area):
+        return force / area
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
