61a195e4b1a8ea327b960cd64f825ee2a975cdc7
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:44:36 +0000

    our runover room weigh scale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
