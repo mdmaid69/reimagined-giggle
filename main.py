05f478ef56c01784caff0b7ac3894319b2cc2e85
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:10:40 +0000

    equal shout squash cope room

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
