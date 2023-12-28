156ce00c2461dbe5cb3cc4334ea87b18dd1456f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:47:51 +0000

    suckme difficult word fit instead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def add_numbers(a, b):
-        return a + b
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
