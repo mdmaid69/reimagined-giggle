584d424a43d92d69cf479eb1d1c04b033a17137a
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:53:09 +0000

    danger description study own view

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def find_max(numbers):
-        return max(numbers)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
