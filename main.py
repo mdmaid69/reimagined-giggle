334373d6a9602d8cc30578756c2437287f4a5933
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:01:20 +0000

    paper bread entrance fall upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
