c5f3bbc0ac7a8291a27a21e70f68ead3b27e60f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:43:39 +0000

    skill young interrupt birthday consider

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
