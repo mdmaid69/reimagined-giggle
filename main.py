c124607918c399f2bad19d2c94409b474af718c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:24:51 +0000

    truth university dick greeting force

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_power(work, time):
+        return work / time
