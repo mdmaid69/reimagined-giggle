b830d56313322525ed5127085511d8487ed3e4e0
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:41:50 +0000

    upwards runaway point reliable anxious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import sys
-def exit_program():
-        sys.exit()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
