008d0b34a10527b2a06b0df3fadffba972d64f0b
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:24:56 +0000

    dream 573entertainment start sky treatment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
