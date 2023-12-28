8cd332bd4280b1042b9bcdc17da5e26c6f73fe2e
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:10:04 +0000

    write petrol information let guide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_speed(distance, time):
+        return distance / time
