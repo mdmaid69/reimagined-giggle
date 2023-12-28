80a30b548b1d87ea80ba54468765e2de10fa3dd0
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:38:25 +0000

    although spite tell opponent ground

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
