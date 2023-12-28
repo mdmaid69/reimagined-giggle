2865fc3492818b7fa6cfb540a3eab257c96b63f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:57:37 +0000

    flow lesson least bus confuse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
