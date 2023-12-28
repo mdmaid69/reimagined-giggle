bbda4147c73890578d57fa564ad52ba114389d5d
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:25:20 +0000

    nurse distance cause children claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-def find_min(numbers):
-        return min(numbers)
