e187f38c2238b8800a2ddc6eaf4c966567490677
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:16:17 +0000

    black sink common evening succeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
