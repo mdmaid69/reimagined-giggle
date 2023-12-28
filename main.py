3939f38cedaa553002cfa5aec82af4df85f38578
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:38:58 +0000

    in invite tone hard tell

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
