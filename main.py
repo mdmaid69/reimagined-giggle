ce5e2d51cd5b94e97bb747a1d84c1ef580fcb8e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:31:54 +0000

    settle straight muscle ours voice

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
