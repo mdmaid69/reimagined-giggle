e8865550c59bed020b76433340e8c876208f8388
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:44:54 +0000

    571enough free edge onto callout

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
