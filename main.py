05b2f66b647afc3c94c43c5190fb293912d38719
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:22:02 +0000

    ashamed over pressure admit anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
