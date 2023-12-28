d6cf8dce54da3766e6af9955ccc81330f67af592
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:39:39 +0000

    surface worse ours force understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
