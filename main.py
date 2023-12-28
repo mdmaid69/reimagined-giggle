85e9c83747e61c7199502daa38293cb29b64da02
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:42:19 +0000

    famous bet ill find admire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
