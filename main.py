325a1ec0813e721ea8514c4b3a3e18ffc9d2d026
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:40:09 +0000

    favour work tape important hide

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  def cube_number(x):
-        return x**3
+def reverse_string(s):
+        return s[::-1]
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
