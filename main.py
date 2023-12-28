92a0a3b2c8df3d94e692e7cf244019eb36012879
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:27:43 +0000

    relax mail pump nose meeting

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def iterate_over_array(array):
+        for item in array:
+        print(item)
