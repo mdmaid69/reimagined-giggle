cf3dacd7a045451e3acc3211324da3ca02e97d9e
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:02:44 +0000

    cut unlucky accordingto persuade circumstance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
