d93ad1920ef7cdb28838b188af37d449110fdf10
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:32:26 +0000

    exercise crash worst interfere himself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
