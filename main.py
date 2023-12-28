d834402345e42bdb9af4158f65316d7743f46176
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:56:35 +0000

    I weather direct Cc match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+print(math.pi)
