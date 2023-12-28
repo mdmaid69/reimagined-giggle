32cce69cc35affee5d524a1a63752736c350235e
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:37:49 +0000

    knowledge accidental switch scrap break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area(radius):
+        return 3.14 * radius * radius
