96215d57bca9413ffa7bb17737d4a4d940f4c283
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:04:25 +0000

    building letter whisper succeed kind

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
