d67d3fe8ffbb6eacdb7150f101f0f52e1fb323aa
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:07:08 +0000

    bath violent square step traffic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import numpy as np
+print(np.array([1, 2, 3]))
