bdce988fc59856b52a2b82d8180a7070ed80afe9
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:01:06 +0000

    bad indeed near chance beside

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def cube_number(x):
+        return x**3
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
