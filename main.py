310fcbd4e6f522412728084bea35d86704dbd906
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:57:50 +0000

    prize month measure before lookup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import numpy as np
+print(np.array([1, 2, 3]))
