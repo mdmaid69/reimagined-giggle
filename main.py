7c353b1fc5377bd279c248297ebf20883b92f3b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:16:32 +0000

    cap explanation welcome flood mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
