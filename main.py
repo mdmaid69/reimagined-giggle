054bee92e4886aa4fd461e417ee206a3be7a5cbe
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:36:47 +0000

    tune moon meet not simple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
