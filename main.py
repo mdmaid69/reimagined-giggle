eda07de70bbc118ef94f8b461daf908d2159fc42
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:10:44 +0000

    one path address card behave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
