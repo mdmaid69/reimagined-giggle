78735d12f7450d98b143ff41a350e584ad6da18a
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:40:11 +0000

    left office pair sink quantity

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def find_max(numbers):
-        return max(numbers)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
