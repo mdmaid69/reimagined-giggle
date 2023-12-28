d8467748f196ddb2701487c8e49b921d16c42c85
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:58:26 +0000

    turnback power die shape belong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
