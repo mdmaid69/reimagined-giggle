6f27d01e8f2e9797aa9c5b36c27e151833bec67f
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:04:55 +0000

    obtain Hh project forever study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def clear_array(array):
-        array *= 0
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
