0c6d5709a5b7bc35508f3cb71da812aab1eb6d5c
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:28:39 +0000

    manner better any get sleep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
