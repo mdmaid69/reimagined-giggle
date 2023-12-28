edad4cd23a30b4cbae11f38e9f1585de87ec6e2b
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:04:54 +0000

    machine grade priority Pp limit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
