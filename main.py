f6dc26ba025ea9d6653b89a63b358264552b37f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:01:21 +0000

    find approval cough relationship respect

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import random
+print(random.randint(0, 100))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
