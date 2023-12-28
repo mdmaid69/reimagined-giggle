7608a1d7619c94d97e19839f363e72d507c1bc27
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:08:08 +0000

    hard sting long goon aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_base_name(path):
+        return os.path.basename(path)
