66e498233a3dddbe4823f4c2ec0c7282121d3916
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:53:54 +0000

    funny difficult wave plate bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
