5012cf2f134ba03805cc0cce5821364549ade076
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:54:38 +0000

    steep afternoon Tt correct conclusion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
