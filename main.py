ac26dc220254c228cba8cdbb2ec36e24340fef79
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:54:56 +0000

    mixture sad there compare Nn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_max(numbers):
-        return max(numbers)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
