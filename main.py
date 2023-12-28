b0dd15cccb1294b6e0c7e047b551b87ae57b0613
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:27:13 +0000

    Oo muscle same battery complete

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def cube_number(x):
+        return x**3
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
