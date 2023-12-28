1cc6d3ecb38c5983b13349b6c62172d37ac9870d
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:55:46 +0000

    fork innocent swallow opposite formal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
