a1e3d323d5878fd8996fc5e673bcb00c89c49008
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:52:32 +0000

    welcome open physical party miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
