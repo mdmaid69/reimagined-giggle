c60a4c48511b990724ebfe2990c8cd76f6a03736
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:01:57 +0000

    regard exact try grow through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
