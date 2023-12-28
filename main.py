c82502c98717fb8fdb344f376b96abaf1aec21a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:52:11 +0000

    use study bite conduct bunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_typecode(array):
+        return array.typecode
