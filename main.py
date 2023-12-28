2bf9e1ce849d946bd880d207155ab7259707d0e5
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:41:20 +0000

    put programme ours exciting regularly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
