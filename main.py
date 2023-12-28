d2e2e9ab4a79acba05fd91793631bc8e3371b432
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:15:57 +0000

    getback build remember around reply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def divide_numbers(x, y):
+        return x / y
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
