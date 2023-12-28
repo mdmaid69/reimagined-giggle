64217d6db625b34b3d1d4ba0cb4a979e09c01559
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:15:21 +0000

    turnup splash ill Cc open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_priority_queue():
+        return collections.deque()
