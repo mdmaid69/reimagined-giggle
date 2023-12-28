aa3194ff1edc5fec97dde793f518d1607aaca6b9
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:58:52 +0000

    responsible compare force cover final

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def multiply_numbers(x, y):
+        return x * y
