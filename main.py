aaeef48517b19476e05bc18e07f309e864d91210
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:37:10 +0000

    nor whole until few ticket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def sort_list(lst):
+        return sorted(lst)
