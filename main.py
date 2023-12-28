d54b593ee9e4fd10fd80ed8e7252db36424f5e59
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:15:19 +0000

    stage tragedy show pressure admire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
