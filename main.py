16651182041294602ea9e7f5b369c6c5201de7a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:49:55 +0000

    spill company Pp pour justice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-for i in range(5):
-        print(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
