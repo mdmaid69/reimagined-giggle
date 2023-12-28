9bf437816c590876fb464280dd53fd8b2c9bdaff
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:50:25 +0000

    heavy gate away road need

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
