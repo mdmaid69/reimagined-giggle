e7b94334534b9149ae797a1aca1d263cec464ed5
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:08:36 +0000

    difference word vote face exist

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
