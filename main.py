f1e98ce39471a1c6af6448893aa735a240ee3005
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:02:55 +0000

    treat trust as meanwhile several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
