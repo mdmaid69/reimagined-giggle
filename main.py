ef2b56e146d59b135394f16f0d759130eb09dd28
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:31:53 +0000

    excited fall this street doctor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
