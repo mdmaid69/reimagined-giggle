2f0f441da03af2024677e0cbd9b4b3a4cdd75790
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:16:35 +0000

    cruelty calmly release question visit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(a, b):
-        return a + b
+for i in range(10): print(i)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
