a47c2eaeb8f9da06b389a4df7ff53997444162a5
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:26:38 +0000

    nearly inside thankyou talk well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-print(math.pi)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
