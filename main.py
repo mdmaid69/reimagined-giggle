9f75ecd1749ac478e26681828a1bad0cd0df57b6
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:09:41 +0000

    sink house bet save like

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
