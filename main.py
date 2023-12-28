0123673799122c6461807c565ce420c0c4ab62bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:08:49 +0000

    most hot left plan scream

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def find_max(numbers):
+        return max(numbers)
+import math
+print(math.pi)
