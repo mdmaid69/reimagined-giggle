bc334abe4c9baa658aa7e5be06ab26aae71db24c
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:11:07 +0000

    after lose interesting round professional

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def cube_number(x):
+        return x**3
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
