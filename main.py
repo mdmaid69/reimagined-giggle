22f11c6a5b692a5153e038729983a1469ef96210
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:20:09 +0000

    recently load bin nature star

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_floor(x):
+        return math.floor(x)
