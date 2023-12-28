ca834fa55c36ca39df3a382f2dd704c3d8ce18c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:42:35 +0000

    pretend off occasional play hers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
