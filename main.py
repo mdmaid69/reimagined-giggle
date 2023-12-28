5c1b7caac46a9e66e5040b4ae5581fbb5f7efea1
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:56:40 +0000

    large base my other even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+print(re.match("h.*o", "hello world"))
