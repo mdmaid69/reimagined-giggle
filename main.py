7841c2e6469f086c603f80d4d803d6b2cb53e3b0
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:19:21 +0000

    today towards picture hand characteristic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
