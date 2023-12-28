ab1f140b59899b5840a696257e6d5c8508bf847a
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:48:38 +0000

    dare strength art catch tired

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  def is_even(n):
+        return n % 2 == 0
