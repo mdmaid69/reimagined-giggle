d052c5ee86556cdcfdba80728947b65acbe956f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:46:58 +0000

    principle squeeze of and Tt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
