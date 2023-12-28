5c79e878ffc89e43703993f5b023dfe5e89e50d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:54:33 +0000

    themselves generous artificial although healthy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
