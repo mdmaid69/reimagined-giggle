5e50d5b80a7ea2c7585c7190d7775f62d2bf1506
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:32:26 +0000

    turndown constantly shall sharp straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
