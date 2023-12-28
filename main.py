ade6588013e7b477cd369f899c020dbd54645f4d
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:33:36 +0000

    excuse introduction acceptable any reason

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import sys
-def exit_program():
-        sys.exit()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
