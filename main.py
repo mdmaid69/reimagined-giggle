a9d40ff327e197c3e4a42979e528157659614347
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:26:26 +0000

    spread usually impossible pick badly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def square_number(x):
+        return x**2
+def remove_duplicates(lst):
+        return list(set(lst))
