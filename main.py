8e1f599030887fd0c4628b8db16b127c32c6136c
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:05:29 +0000

    include break skirt measure strict

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def get_array_as_str(array):
-        return str(array)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
