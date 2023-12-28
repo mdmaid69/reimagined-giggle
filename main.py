12094e36fa37bdf32577f0a1faaf1b7c401214c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:38:05 +0000

    behaviour too punishment loss moment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
