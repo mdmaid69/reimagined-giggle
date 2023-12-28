321d5c890fa7f155715978dd80c0598de05a92a2
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:52:33 +0000

    brain conduct delicate 566end offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
