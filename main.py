54a7622f3135e78a82c8983c894b0e0691282dc2
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:52:27 +0000

    spread pleased forget cover them

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+def square_number(x):
+        return x**2
