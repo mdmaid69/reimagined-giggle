08452cfcae0b2cb0775b37d334607dfea18a2dec
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:57:14 +0000

    suppose trust anywhere gradually cry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def get_array_index(array, item):
+        return array.index(item)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
