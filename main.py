e54305d56f0ce515cdf4bebeeb9119e123874890
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:55:08 +0000

    laugh may creature turnon public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_base_name(path):
+        return os.path.basename(path)
