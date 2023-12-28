eccfa977d67086089974df37366056016c2214ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:32:55 +0000

    unite disagree give 564encourage stretch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
