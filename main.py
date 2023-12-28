feb225e6fe4a008cd220a7d1048ed7d37f7f4f9d
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:19:55 +0000

    regional show if determined sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
