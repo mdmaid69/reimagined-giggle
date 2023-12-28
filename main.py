1bbe578abf8dcb3c32ffaf64cea0c3b8fbe3d9d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:49:30 +0000

    plenty treat between confused recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
