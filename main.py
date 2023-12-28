69484e9c789e3ac0a0ea482b1536785783bb11c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:48:06 +0000

    guess by confident concerned along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
