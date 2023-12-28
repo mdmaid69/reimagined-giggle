f5df8ed3d84c79feab740cefa76c819eceab0380
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:43:57 +0000

    air Ss splash whole breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
