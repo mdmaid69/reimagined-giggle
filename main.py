0c5ef456f88fa296efeff52a772677a41400c47f
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:25:37 +0000

    ought everyone plenty niggaacademy phone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
