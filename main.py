d8f80cc9669737cc28db0e145c348a10e8878935
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:01:44 +0000

    rain contact successful comeoff lie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  def cube_number(x):
-        return x**3
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
