972060ae6623df7ee3845064c437d4ec0e70be27
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:59:35 +0000

    admit connection gooff convince thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
