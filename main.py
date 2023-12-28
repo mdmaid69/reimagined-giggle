8259cc7e42fda1709c5e6a6c12b04273407aede9
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:15:37 +0000

    legal well comeoff able press

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def reverse_list(lst):
+        return lst[::-1]
