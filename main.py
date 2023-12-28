95b9582ee633f84d9e9da8f637c59bb2b5b6b209
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:19:58 +0000

    teach limit hers month temper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import math
-print(math.pi)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
