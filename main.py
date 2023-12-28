387a0d97519a86100ea9baf9b0621206a73e520b
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:31:32 +0000

    entry fork hold field hurt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-name = "Python"
-print("Hello,", name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_to_binary(n):
+        return bin(n)
