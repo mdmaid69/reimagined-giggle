dbbdecc056d5effbe128c0b6ab7ed992dd1e16a2
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:54:40 +0000

    trap movement visit no throw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_error_function(x):
+        return math.erf(x)
