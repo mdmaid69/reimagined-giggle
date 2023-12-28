f2473a562089d9f44206a86147465b47155340a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:38:44 +0000

    power contents convenient unless nice

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
