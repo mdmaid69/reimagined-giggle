2ab9c74f69c6d0bd22df35f47f85f661a5972ce4
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:15:42 +0000

    no poison block reserve gift

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-for i in range(5):
-        print(i)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_area_circle(r):
+        return 3.14 * r**2
