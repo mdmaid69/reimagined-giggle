29b3ad9461bac8179c6c41a2452eb900e5e363fd
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:50:58 +0000

    light speech somewhere keepfrom copy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
