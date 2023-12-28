1c226bbdde95dde42ae94dd4bc88dcd3cc68bbd6
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:24:55 +0000

    effect tragedy deliberate yours brief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
