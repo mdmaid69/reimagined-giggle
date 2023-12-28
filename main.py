ea6707be76bd905d49947488498f9491a357be4d
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:00:32 +0000

    mix sale accidentally although pain

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
