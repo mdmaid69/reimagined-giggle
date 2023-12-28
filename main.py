21a3bf4d62da92ef7bcd4e22f2be5e97be6311c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:43:50 +0000

    shine everywhere increase variety month

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def subtract_numbers(x, y):
+        return x - y
+  def convert_to_octal(n):
+        return oct(n)
