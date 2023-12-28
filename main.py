46b8c34460212667ddd556968d97c95f7d7523f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:37:55 +0000

    blow future lock instant character

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
