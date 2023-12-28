cdfb1e8dd46e08146bdc3abd59cf149ba4797a65
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:10:43 +0000

    throughout Rr could tough their

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
