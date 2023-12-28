341469130a95a4f2b8e1fbe0865656886d02b7ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:18:58 +0000

    unpleasant mix indeed steep year

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def exit_program():
+        sys.exit()
