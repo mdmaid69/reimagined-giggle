ff27be2b9d6f973e7074d9f4964e285caa3d10d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:04:42 +0000

    violent naturally hate minimum delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
