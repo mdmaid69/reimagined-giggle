8fc7f694260de34e5300b6db8af2490a2f120b99
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:52:45 +0000

    tidy turninto that roof carry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
