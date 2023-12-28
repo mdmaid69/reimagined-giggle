45fe99f1ce09b102e6b07bb302584d6c5ffa31c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:39:30 +0000

    behind guard wrong thanks now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import collections
-def create_stack():
-        return collections.deque()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
