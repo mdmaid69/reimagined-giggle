35f5a4f6be9008aa36a5c9e4d6cd517b49b1e5cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:33:39 +0000

    deliberate bringup strength equal meaning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
