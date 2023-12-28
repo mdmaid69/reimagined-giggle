28bbb04dc56625b13d5a70f3d82db487ce41af09
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:07:42 +0000

    live tomorrow also copy defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def split_path(path):
+        return os.path.split(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
