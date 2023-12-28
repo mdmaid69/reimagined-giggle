0b43ab048c07717e33b0cf82fd4950cfc8419af4
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:45:01 +0000

    square silent runover break seriously

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def find_max(lst):
+        return max(lst)
