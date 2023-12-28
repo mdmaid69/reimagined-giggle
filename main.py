dae509475bb5ad08e0df9f1cef1397e1496ffa9f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:10:53 +0000

    him trade getback level combine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_index(array, item):
+        return array.index(item)
