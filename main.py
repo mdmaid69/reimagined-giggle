9b9ca33746bedbd4be0c82d18e4513316f74d39e
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:54:36 +0000

    forgive window look spin return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
