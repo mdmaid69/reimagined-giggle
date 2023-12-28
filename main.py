637213ed87ed8173606b3fd12d6860786d1f092c
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:22:08 +0000

    wear cook pollution switch sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
