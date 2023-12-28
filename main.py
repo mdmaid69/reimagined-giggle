17fd6ad85a8d2d50f402aa5198937d4947dcdd5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:49:52 +0000

    middle now respect borrow woman

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_volume(length, width, height):
+        return length * width * height
