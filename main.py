aee8192cddd49c9d909ca7f204f07e187a92eff8
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:47:03 +0000

    opposite runaway here explain argue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def reverse_array(array):
-        array.reverse()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
