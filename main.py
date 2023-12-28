e25517fea41291b08365e072d1a68f6f397a53bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:41:22 +0000

    today middle typical jump instant

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def cube_number(x):
-        return x**3
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
