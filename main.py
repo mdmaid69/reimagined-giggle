2d79368e02fa9fd51bf63eb8e584c14650c649f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:22:33 +0000

    whisper course form believe course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
