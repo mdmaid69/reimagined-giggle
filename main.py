762fb68141ee3898b65add3fb24da55fa3933961
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:21:46 +0000

    deliver tomorrow use neat attract

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_work(force, distance):
+        return force * distance
+def calculate_power(work, time):
+        return work / time
