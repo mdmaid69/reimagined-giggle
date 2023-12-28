3e4ca7b11d29d311acb62fefe569359320427528
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:46:44 +0000

    hold beside square keepup straight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+print(math.pi)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
