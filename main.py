755c8e1c324d73c87c462b894271ea21dabb626c
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:30:34 +0000

    duty doubt flow treat right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import collections
-def create_queue():
-        return collections.deque()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
