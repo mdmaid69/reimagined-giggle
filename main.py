6db2c3e3d1dcba8ac976fdbfbdb0cbd25a251823
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:48:39 +0000

    solve react kind bother only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_str(array):
+        return str(array)
+import sys
+def exit_program():
+        sys.exit()
