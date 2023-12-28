930a991f356401b5ac05d45311558e66bf9271bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:47:12 +0000

    sound slip common onto godown

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def square_number(x):
-        return x**2
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def reverse_array(array):
+        array.reverse()
