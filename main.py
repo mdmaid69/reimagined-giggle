3f3564b2180775fa3c7fab62cf3ea3772592bedc
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:29:17 +0000

    back turnover Qq protection modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
