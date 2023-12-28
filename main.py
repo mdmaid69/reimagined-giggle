299c2a9adfa7bab5edb6b1c9aeee26f7166e0fa7
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:13:12 +0000

    pretend method the attach undo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
