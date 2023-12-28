b22c8326f581a5555011af7bb2304d61792df27b
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:04:18 +0000

    the name crash closely less

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def sort_numbers(numbers):
+        return sorted(numbers)
+  def is_even(n):
+        return n % 2 == 0
