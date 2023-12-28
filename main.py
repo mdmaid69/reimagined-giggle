89af6613a00d4276a405e111625a74f8750b70fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:20:55 +0000

    might promise goback(to) police suggest

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
