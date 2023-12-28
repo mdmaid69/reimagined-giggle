0aa4d30d85d1af103b6a5979f99513540ca53fa3
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:57:26 +0000

    reply month difficulty but gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
