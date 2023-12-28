0c2b1159422add725a4fd769d3fc3abe4869784d
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:26:42 +0000

    thorough phone single care steer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_speed(distance, time):
+        return distance / time
