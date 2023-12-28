309aa1430110f81424386464ab42b2f9bc7e430d
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:10:49 +0000

    about keen beyond proper goon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_perpetuity(payment, rate):
+        return payment / rate
