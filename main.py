e5d0969be8b4a9d8f251a223b4b82256e13314d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:17:17 +0000

    thin turn force that slice

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def remove_duplicates(lst):
+        return list(set(lst))
+  def subtract_numbers(x, y):
+        return x - y
