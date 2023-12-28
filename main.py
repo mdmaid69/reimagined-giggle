fc32266d9df4807a4edfabd1c0b379bc70ee9bd5
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:52:40 +0000

    lesson remain apparently priority trade

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+print("Hello, world!")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
