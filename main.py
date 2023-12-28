79c1777a448e97944fca8bf70cb1524732e2eb71
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:48:19 +0000

    though flat win little extra

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_even(n):
-        return n % 2 == 0
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def reverse_array(array):
+        array.reverse()
