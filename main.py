07a9860fa2c749dceb8bef8682e07d6287883688
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:56:58 +0000

    turnon angry pain Hh ability

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import numpy as np
-print(np.array([1, 2, 3]))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
