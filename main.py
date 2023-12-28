8cf2cc52dbf24cb1b445ed77396a7fe01ce7a154
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:52:15 +0000

    even front private heat comeoff

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
