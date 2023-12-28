9ae9f483e10a67194969468edc48e9184040cb36
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:57:26 +0000

    record do unusual fall hope

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def add_numbers(a, b):
+        return a + b
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
