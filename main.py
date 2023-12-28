d60d9c3117a43e576af4267b085d0fc2d85de44b
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:34:59 +0000

    miss carry way job shock

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
