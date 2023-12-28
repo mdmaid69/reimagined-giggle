cab2a61dfb9772cca247f35ed69ba76b25ee749b
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:14:08 +0000

    middle defeat fasten line listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
