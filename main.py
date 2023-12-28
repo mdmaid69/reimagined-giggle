c680ccd15f8f90f535a224da90565c5c85c4974a
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:01:01 +0000

    degree tax callat laugh especially

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
   def get_file_owner(file_name):
         return os.stat(file_name).st_uid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
