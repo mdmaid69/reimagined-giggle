ecba4849627af9dd716a632793a0e2bb7c1d6675
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:21:59 +0000

    explanation tongue breakin stage human

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+text = "Hello, world!"
+print("Words:", len(text.split()))
