a322808b3f571cd867ddf4797e23e29b8dbad6ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:06:27 +0000

    world spend film 573entertainment reasonable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
