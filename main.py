399cc553c2271496251ee71c482d70c38ba0621b
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:45:20 +0000

    cost rest jump concentrate extra

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
