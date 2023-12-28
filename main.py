1806dacea1325267eb5c45924dbf36312665231e
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:38:22 +0000

    stiff great foot sleep reliable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_pressure(force, area):
-        return force / area
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
