a638fd3b669a2a294d9a8f3e0fab7effd0cf799c
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:50:09 +0000

    scared hope disagree acceptable or

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def sort_list(lst):
+        return sorted(lst)
