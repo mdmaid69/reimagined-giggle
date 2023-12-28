c5b5eed702658f46fb68067aee1b6091e29bf008
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:38:02 +0000

    copy conversation fit takeoff fold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
