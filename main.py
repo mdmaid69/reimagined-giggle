1c9c51dfcfc79f85f56ae3fa0ed53ef1581c1abc
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:41:09 +0000

    must character little reduce treat

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
