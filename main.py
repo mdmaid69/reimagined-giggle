8ff0f62d45dac1b2e658fc3843db196447fda416
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:21:07 +0000

    real record capital hurt squash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
