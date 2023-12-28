080c6cbf22504ab2afdf1fc9e4bfc4e00e6a5c5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:17:31 +0000

    rarely tidy left great group

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def count_words(sentence):
+        return len(sentence.split())
