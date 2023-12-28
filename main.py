c062972cdf3683747c431746191a3898a3e5d537
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:24:57 +0000

    no crash memory favourite game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
