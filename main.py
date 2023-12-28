7b2731eb74a5aa8131d28a2c43d615752b7da2b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:47:55 +0000

    satisfied set strong equipment invite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
