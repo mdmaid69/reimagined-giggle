4cc0633aadc96dbc12097f5ee6b0df9f0a9180ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:42:51 +0000

    gather thinkof become makeinto convenient

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def square_number(x):
+        return x**2
+def is_palindrome(s):
+        return s == s[::-1]
