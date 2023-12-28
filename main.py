95e39cf007f49ccd79c3fcd3a89a2f4ba6040ab0
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:00:09 +0000

    lead wave lunch brain able

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_octal(n):
+        return oct(n)
