d482665eb4ded14f8293fa6f39d4d3728bbdf854
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:13:27 +0000

    centimetre concentrate getback will pay

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
