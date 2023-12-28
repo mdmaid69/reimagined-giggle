e53f061eeaaab67a0b7d8b6147d6d2a699df6767
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:49:41 +0000

    comeup sort eat otherwise tread

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
