61b3dbe70db4819e2971197a5555b7e1593b000f
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:50:51 +0000

    run proper satisfactory goodbye now

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
