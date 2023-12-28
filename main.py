64a5ad3559a3ad92ed9e2ad41f74035dad344b6b
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:55:22 +0000

    original hard holiday divide pretend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
