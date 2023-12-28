47d5ae1b05351a2d294e63fce5fc82883d98ebed
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:26:04 +0000

    big swimming support Jj wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_speed(distance, time):
+        return distance / time
