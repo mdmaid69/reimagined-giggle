4062e2395596c23d0f7c2592ac22d6393b413cf8
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:35:00 +0000

    better feed love serious positive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import collections
+def create_user_list():
+        return collections.UserList()
