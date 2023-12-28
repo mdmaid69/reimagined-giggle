ba0729109bd4546d34d0ac7e087163202c12d253
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:20:53 +0000

    neat indeed moral pleasant invite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
