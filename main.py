e098af84418a08fdd50843540a892e2349484214
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:41:53 +0000

    really bringback cut loss love

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import platform
+def get_os_info():
+        return platform.uname()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
