8b4392c1393fd18b01c9ce77cebd28535c23b336
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:55:20 +0000

    unpleasant talk maybe pity store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
