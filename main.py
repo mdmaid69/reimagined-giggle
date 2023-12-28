e3c45365d4cdb6f1d98753b97da5ff00dda76e9a
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:40:19 +0000

    miss wonder owe land cool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def is_even(n):
-        return n % 2 == 0
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
