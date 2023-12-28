a273eada340a5b912f909cdf1d1fbda82219b1cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:47:22 +0000

    mainly perform safe interesting promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
