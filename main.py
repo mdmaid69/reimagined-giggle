d952cc8a28463110df2bbf66a82cd24f0d4526a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:40:04 +0000

    anything protection getaway honest tragedy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
