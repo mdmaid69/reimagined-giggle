f17f8af3dcf130e60513bc50f8bc551d55073253
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:37:43 +0000

    moment reduce before fit spend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
