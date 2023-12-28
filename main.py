b81e989bc20e752f610e5d4bd29c3a97cd73c54f
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:32:24 +0000

    blind rate unkind naturally fix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
