97912a3d1a77e1437015829336e4da53251a2cd7
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:38:54 +0000

    scare good lie manage walk

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
