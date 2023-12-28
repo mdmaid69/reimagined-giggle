030ca6ddbd646f812dbb4bbcb715f6ae4a952be2
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:23:50 +0000

    cool top brave breakinto proper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
