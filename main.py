6962c0b86d96592561e2c8d251f453478c202a11
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:34:05 +0000

    confidence knowledge sit watch central

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import getpass
+def get_username():
+        return getpass.getuser()
