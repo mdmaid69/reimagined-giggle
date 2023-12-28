d91dc710be4c52ef18d3dcd3ffb45bdcf954c2b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:04:48 +0000

    swimming who reject she reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def count_elements(lst):
+        return len(lst)
