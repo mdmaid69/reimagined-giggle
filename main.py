2ca7cabf63e5c6e4ce5bf7d714756c11fc4e0e64
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:30:13 +0000

    mysterious art grow mad praise

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+for i in range(10): print(i)
