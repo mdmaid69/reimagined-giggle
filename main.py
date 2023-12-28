b28eceab07a86fa9289ca4641fb5c5c26486b0ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:35:25 +0000

    frightened careless bring week uncertain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
