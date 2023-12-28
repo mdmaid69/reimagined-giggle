8e7efc8e27496ff44827acd0108cd897503b5e1b
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:42:30 +0000

    valuable top excuse blue whichever

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def find_max(lst):
+        return max(lst)
