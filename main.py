db425b767f150e57f569fb868f2cc6806279af28
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:23:53 +0000

    underneath cut bringback choose uponon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import time
+def get_current_time():
+        return time.time()
