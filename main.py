84702e645ec9d28b2b364c60b82baf87184d2339
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:49:16 +0000

    our lot adjective bad tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
