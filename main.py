a423a50e4b280947e534644a502ac8445f07236d
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:48:04 +0000

    basic makeup imagine holdout recover

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import collections
+def create_user_dict():
+        return collections.UserDict()
