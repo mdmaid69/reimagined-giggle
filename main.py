79d70bd6698f5751e3a0bea72ce1610b47bd8b67
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:50:07 +0000

    change bin programme Yy borrow

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import tensorflow as tf
+print(tf.__version__)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
