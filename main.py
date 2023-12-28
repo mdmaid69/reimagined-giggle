8bfd51ece3f7d9b6eecc8058e34a3111c0b31217
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:20:36 +0000

    especially rule who figure set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
