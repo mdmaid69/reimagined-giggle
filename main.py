8ef4c9c7217ff935e958a2d750dab04b2cdcc480
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:37:12 +0000

    blame sentence rough decide almost

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
