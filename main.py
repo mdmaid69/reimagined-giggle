1d25a20548671fefec7257b850fbe8b3c0608a70
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:19:48 +0000

    place correct world majority imagine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
