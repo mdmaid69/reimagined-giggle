ab64a5588e73314b28d2df2bdf8d6d6f510d855f
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:57:05 +0000

    different if manner draw material

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def print_python_version():
-        print(sys.version)
+def reverse_list(lst):
+        return lst[::-1]
+  def convert_to_octal(n):
+        return oct(n)
