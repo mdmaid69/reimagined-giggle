4d02051465c22f9f95504830011292a315f455ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:55:50 +0000

    fork rate interview arrangement what

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-def get_current_time():
-        return time.ctime()
-def remove_duplicates(lst):
-        return list(set(lst))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
