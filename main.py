3a40e5f118c3d96cff3ba672cbdc2926e43f7656
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:56:36 +0000

    fierce cough over floor hurry

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
