49a437e006f5dae5df80cb532ab717cd6573b909
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:17:14 +0000

    mean judge almost rob ever

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
