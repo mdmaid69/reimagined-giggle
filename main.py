442d5fd5f7d832df27377f55aa26c0d66fd35b74
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:34:10 +0000

    tough complete shake quiet instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
