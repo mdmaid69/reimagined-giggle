e7a9e8d9ded37094d9ac4a93033e7ecf53ef24de
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:08:08 +0000

    connected convenient furniture lookup say

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
