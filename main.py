4889f990ce18087fd8f7ac1e4034e9f8b0aaecd7
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:47:09 +0000

    press rule circle sound rubbish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def cube_number(x):
+        return x**3
