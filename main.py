3cc70f9468ac0be500d8e65a36418ade297cc84b
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:28:48 +0000

    shut test consist command repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
