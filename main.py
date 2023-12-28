a0cc1ab111bd9f648dba4cd43454bdb262e28c42
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:27:00 +0000

    flavour shape influence world tear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_index(array, item):
+        return array.index(item)
