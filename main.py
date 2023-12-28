c2e751bfe08c4ac6d7352f34a4f96f8f8998554c
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:29:04 +0000

    upstairs division move traffic uncertain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
