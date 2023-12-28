cf7fad3432d7161c16e7e64723407ca464e32acf
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:56:38 +0000

    risk spring confused earth trap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
