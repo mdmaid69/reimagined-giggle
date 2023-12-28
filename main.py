3470a29cd1ea73fa36c5a019d858faca63260006
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:57:01 +0000

    bottom case without table copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
