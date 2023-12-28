6ba4db40f4c5861f5a39dff8cdb8fb1278ef3365
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:01:55 +0000

    bury respect attempt story turnover

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
