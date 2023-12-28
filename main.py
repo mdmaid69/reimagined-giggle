fc407746f909421eb45ad26d879badbda85d3225
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:16:58 +0000

    stretch power treat separate protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
