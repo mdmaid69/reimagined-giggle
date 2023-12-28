e26fd687a65a713c4d32e6d6acdf72123882a83b
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:34:44 +0000

    hardly sweet club at maintain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import collections
+def create_user_list():
+        return collections.UserList()
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
