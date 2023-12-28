a19c75194a1dda7fa844dceae6935f1ab6470153
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:43:46 +0000

    much knowledge harm throw organize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import collections
-def create_stack():
-        return collections.deque()
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
