d365329baaa2cad0b8ea25c5d68ec96fb83327b3
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:19:48 +0000

    seem understand public search throw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
