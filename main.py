cc144cbb386e802d569c455063d1f36026071bcc
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:29:54 +0000

    use band develop miss brown

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
