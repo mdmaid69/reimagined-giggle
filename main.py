6fc98301f8b459ef829bb14b6b8fc5686648e3df
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:57:12 +0000

    grow danger consider keen actually

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_min(lst):
-        return min(lst)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
