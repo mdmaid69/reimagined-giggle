b5cf44af170008ddb701213da647fed0ebae8986
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:59:21 +0000

    unusual once wave accommodation pretend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
