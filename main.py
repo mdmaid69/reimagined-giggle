c86f788a9118bc86716ba25df647dccf2114689f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:26:39 +0000

    to apart ground card normal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
+def sort_numbers(numbers):
+        return sorted(numbers)
