39e7b768ea32f78ada49cc844d14281bca915a1a
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:23:56 +0000

    laugh getback nothing face sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
