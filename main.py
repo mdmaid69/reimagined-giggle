629edbaf57be80adc97d5572cf2035e5c3b759bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:03:08 +0000

    shut claim war place sting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
