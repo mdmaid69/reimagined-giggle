52b87fb79f4a530b70ba873fa0b5bec52ad790ba
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:17:10 +0000

    spread further hate obtain increase

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-print("Hello, world!")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
