fa3340f8068189f97b7506351b5631416278e99e
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:47:45 +0000

    term turn benefit sport slowly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_typecode(array):
+        return array.typecode
