7da9dca64f14f5dd7bbe5cfa6d5099ebcace9e9f
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:38:41 +0000

    his idea address square case

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def remove_from_array(array, item):
+        array.remove(item)
