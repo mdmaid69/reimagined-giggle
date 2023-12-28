b0afcc6e51004bd86286533da808fa16a0be8736
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:22:21 +0000

    praise report string unit substance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_array_to_bytes(array):
+        return array.tobytes()
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
