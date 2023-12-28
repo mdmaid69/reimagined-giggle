c5defd968e18d5cc36bfd47fc64ea9c52da02a7a
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:48:03 +0000

    force consist about even bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
