6d18fe895caaf31a7037c85fe7fe6cbc27b62eb0
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:20:26 +0000

    wide present next mistake ourselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sys
+def exit_program():
+        sys.exit()
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_string_from_array(array):
+        return array.tobytes()
