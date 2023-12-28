d2000d906320c49a44e33a0f3451bcaff4ca82a6
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:25:53 +0000

    passage main breakinto wherever miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
