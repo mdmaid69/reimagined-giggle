a4ccb4f16234ed12ded61fb6b6b90d87292ecce4
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:49:04 +0000

    include dance sensitive serve crowd

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+i = 0
+while i < 5:
+        print(i)
+        i += 1
