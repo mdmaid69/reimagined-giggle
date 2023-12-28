17e29b63830c9abb53e918f52f2f8d83a8362910
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:45:52 +0000

    rate listen throw introduction anxious

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
