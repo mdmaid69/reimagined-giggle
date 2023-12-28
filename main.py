437a6787635175d807de41ec09479258c126a3f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:59:26 +0000

    boring 571enough squeeze favour favour

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
