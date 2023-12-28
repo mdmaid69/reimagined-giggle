7d1ef2c546dff7a1fe859fb6e7aae949949f8a69
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:12:44 +0000

    appeal mark brief work embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
