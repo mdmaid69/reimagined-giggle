14714547d1d756c4c603da69aa2ecbbbaf9c7b85
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:59:36 +0000

    home steep 573entertainment release apply

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def add_numbers(x, y):
-        return x + y
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
