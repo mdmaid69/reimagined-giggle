826037203868f3dd25ded9777c1a918c5eea3074
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:28:32 +0000

    during laugh 564encourage medium allow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
