3d49a28312d78902faa9296eaa67428e1b6be8d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:38:51 +0000

    indeed equipment guess great tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
