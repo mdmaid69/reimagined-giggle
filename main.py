0623e2ea5232341e8302235f11470e4e0453b882
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:47:18 +0000

    dull centimetre smart lend guide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
