5d9b2aa767ea10dc97f20042d395d59c4832d6ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:49:35 +0000

    disappointed aim our bus me

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
 import math
-def calculate_sine(x):
-        return math.sin(x)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
