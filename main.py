4b8a04f1ff065c348457a64892beefb1aa16ba7e
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:46:26 +0000

    no apply claim bush contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
