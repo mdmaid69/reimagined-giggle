86467844838c5306f3d868d9fd313b0458c100bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:13:55 +0000

    noisy more pay wave thorough

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
