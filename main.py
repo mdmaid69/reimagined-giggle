a0cf7426375208031c0cdfe4aea79d2fc3ffe655
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:18:43 +0000

    meet possible strength shop valuable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
