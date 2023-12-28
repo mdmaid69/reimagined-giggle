fbdd130008b54d077776cce0c168264013f42099
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:43:04 +0000

    secret turnover holdout laugh ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
