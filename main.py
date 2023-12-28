ff7197822036d6e41a2e7cbebf8e355957d9e181
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:56:51 +0000

    contents yet them water never

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def count_characters(sentence):
-        return len(sentence)
+def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
