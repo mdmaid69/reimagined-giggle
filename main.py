4c09766a24481180e9a706720d3c137c06763c03
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:19:51 +0000

    reasonable wish talk idea when

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_set(array):
+        return set(array)
