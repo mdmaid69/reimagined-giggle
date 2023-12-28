47be6c10e7182b12aef3a1a04b43ecb34df11cac
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:24:52 +0000

    case congratulations wise regional event

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
