d2db8c0b9e5f7c07439138fa7e9541ce31f45807
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:20:18 +0000

    better surprising print hold destroy

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
