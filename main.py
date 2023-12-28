971b2505c675d3adf77caf4fff2e1844bc6ba407
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:21:44 +0000

    getback runinto phone country not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
