d5d796befee16a3263a28e40a85466b1f85595c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:19:33 +0000

    balance run herself fire alone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
