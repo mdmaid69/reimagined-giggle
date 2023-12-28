e4978a5880e95eb4bf2001d867c3343c8b90372b
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:52:25 +0000

    reserve large broken bush everywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
