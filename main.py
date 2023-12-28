be535eb026f52a244db89f6e0f0576c640f09d40
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:23:54 +0000

    emphasis rest home legal able

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
