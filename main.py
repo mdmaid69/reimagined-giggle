89c303f7de6dc2ae53e38b67d35201da2e646c67
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:54:31 +0000

    difference table live office harm

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
