27a369b89b867f2224d639bd4cd764f5b4604e44
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:43:46 +0000

    hand dull breakin control nervous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-for i in range(5):
-        print(i)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
