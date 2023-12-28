d9050ca92a3b88efc89281d90c8ebcf15f3db5a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:45:51 +0000

    fence keepfrom authority responsible bush

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+print(sum(range(10)))
