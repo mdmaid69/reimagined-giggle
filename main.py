be526490edb0a60f99b835f72e52b542a906360b
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:41:30 +0000

    cough her negative choose fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def print_python_version():
-        print(sys.version)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
