d85b80c7ccc170a6f66b9de2350eec001f7adf23
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:44:23 +0000

    size limit press way disgusting

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def is_odd(n):
+        return n % 2 != 0
+print("Hello, world!")
