74611ef2e98df6342bb04a285f46cfa72bbbd497
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:14:31 +0000

    silent body speed hold shout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def split_path(path):
+        return os.path.split(path)
