6b8054a8de418e5cefb56703aee44ae5d8fac29e
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:01:24 +0000

    hit to shoulder few marry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
