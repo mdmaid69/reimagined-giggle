a05f953886ab9190ad8a38e905ae1000fed7bd44
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:50:21 +0000

    file bound church hole secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import platform
+def get_python_version():
+        return platform.python_version()
