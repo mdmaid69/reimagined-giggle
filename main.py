e2a238c8d646f046bf54aea3d875eb1c43da5cf9
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:46:41 +0000

    statement pronunciation order press friendly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def divide_numbers(x, y):
+        return x / y
