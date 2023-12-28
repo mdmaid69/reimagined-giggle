b4f82aea5661c66ee99e3f4b89ba918df2afdb2a
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:06:27 +0000

    transport introduction cry race deserve

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def convert_to_binary(n):
+        return bin(n)
+  def is_even(n):
+        return n % 2 == 0
