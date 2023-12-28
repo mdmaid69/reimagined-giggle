2e7502aedc238d722ab4103896752b1a2861b157
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:27:58 +0000

    dick apply judge remove come

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
