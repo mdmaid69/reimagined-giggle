300a6f59059164fa9b3efe5ef287134975de381b
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:18:10 +0000

    accept thankyou tough town shallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def reverse_list(lst):
+        return lst[::-1]
