2a331aa64aba1f5528a1a00c9f90c0ed16443e0b
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:57:44 +0000

    mind rain claim wrap whichever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
