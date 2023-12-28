ea87a8635eaed2af91c79a9c0ff682fe59918b74
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:35:40 +0000

    spring development at public recovery

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
