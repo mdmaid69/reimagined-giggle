628a231cc2524bfba788775b5897ff45b5f8778d
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:25:29 +0000

    otherwise occasion accident each cut

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+print(math.pi)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
