c9dbca98a8eaa3deb586b069b33480bbc0f53500
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:14:22 +0000

    feature ear little minimum get

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import sys
+print(sys.version)
