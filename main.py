52fc612b302103d45733df286031208d4f1c8592
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:22:56 +0000

    tired powerful have typical judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
