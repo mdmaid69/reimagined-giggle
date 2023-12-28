fc33215e9541c3923da5dc1bc969549b0c7234cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:58:57 +0000

    idea spin squash into convince

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
