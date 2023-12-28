6180ea1cc7933684382e0822a97ee08e9834c942
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:37:35 +0000

    level mind blame represent flow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def is_even(n):
+        return n % 2 == 0
