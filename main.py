8229154e005c6345cee58415fdac0cbd7d7e25ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:14:23 +0000

    handle bother often place boat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
