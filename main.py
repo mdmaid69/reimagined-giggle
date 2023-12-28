f5c4dad525b33e9cb3eef345209d44859cfd4be0
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:14:11 +0000

    only push actual time pity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
