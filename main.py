7bcea148b3db92df9b8981d7f63771413afdaa4f
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:50:04 +0000

    however sign hurry twist message

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_string_from_array(array):
+        return array.tobytes()
