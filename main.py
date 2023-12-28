72d5b33db8e4bd9f2bc3bed0415c12706b3bbb93
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:33:41 +0000

    bright professional score risk fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_force(mass, acceleration):
+        return mass * acceleration
