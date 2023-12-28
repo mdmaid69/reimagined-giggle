4c30d0f683921ca0a89aa10044ee503d2177cd13
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:58:40 +0000

    obvious surprised characteristic design drink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def cube_number(x):
+        return x**3
