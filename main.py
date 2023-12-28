34005ae4331b9eb9c4e115b0b3a94a818952d1d6
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:20:23 +0000

    fast flood exaggerate name foreign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Characters:", len(text))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
