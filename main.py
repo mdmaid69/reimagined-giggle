e5b50bfafc712ae7ffdbfaa55da2f6493e53cdb4
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:45:09 +0000

    giveup increase square music also

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_acceleration(speed, time):
+        return speed / time
