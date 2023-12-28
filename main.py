1be0db42ff40b482da35938ca388f671f197af63
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:11:22 +0000

    length otherwise letter exaggerate hole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
