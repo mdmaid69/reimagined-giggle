2172254ea07bba9b44ca1a6c0bef09a7563ee866
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:24:26 +0000

    pocket should communicate night sink

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
