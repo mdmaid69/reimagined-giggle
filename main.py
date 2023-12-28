7a330b542df6f5e8abd3a4ec0b414e21eb0ba779
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:54:07 +0000

    lead key breathe actually little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
