ad9c2f4bfb96bf6daf9e92e920f4916a0f0a191e
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:24:17 +0000

    plenty shall cut lose male

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def square_number(x):
-        return x**2
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
