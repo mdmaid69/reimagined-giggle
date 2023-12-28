b0aa0a0f18d40068db1d9ea902c89afc5b8cbcdb
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:10:51 +0000

    eat habit stir use getup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
