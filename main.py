12b57a164490cef4670b6c59c7019ccdd6020cdd
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:29:19 +0000

    any plenty invitation tense settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
