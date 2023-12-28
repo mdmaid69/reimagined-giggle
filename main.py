c6085fe5ca49d91c73d73e910123063b290c41f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:35:59 +0000

    ilovedicks operate that wait bye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
