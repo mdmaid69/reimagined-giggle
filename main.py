d014110001cb0746e28ad6c608d09eeed57f1b6d
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:01:59 +0000

    see exercise why shut arrival

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def convert_to_hex(n):
+        return hex(n)
