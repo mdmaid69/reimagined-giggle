b79c52405a7d4eb6124964f4b505516be506ed76
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:55:08 +0000

    bet refuse Ww mistake listenfor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
