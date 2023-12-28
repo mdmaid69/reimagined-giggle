a77c1f5ba3997d90617ef0b79565fb0c8d54d881
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:08:09 +0000

    happen birth splash risk sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
