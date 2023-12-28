eea4c0fe6e68a897c60e14d961ba24ef0b04c38d
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:45:59 +0000

    pin apply hers emphasize out

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
