89df969b8f88111cefd6f1b69f6732b07e366808
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:42:29 +0000

    foot other mess thing whole

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def find_max(numbers):
-        return max(numbers)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
