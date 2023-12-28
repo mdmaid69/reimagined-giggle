53fb27a3631f8b4e264135850a7cab7e8a723bad
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:50:44 +0000

    minimum continue answer teach congratulations

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-def calculate_power(work, time):
-        return work / time
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  def convert_to_binary(n):
+        return bin(n)
