dc7b6f400d01b145889741a7d94107760872e125
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:15:37 +0000

    secret false set definite window

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def is_odd(n):
+        return n % 2 != 0
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
