d1aac8b8c16194842b351aa28183567e95ceeec3
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:10:14 +0000

    cool machine basis present win

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
