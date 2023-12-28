2524577fa67d229d9044d65b18374db4a2e877f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:38:41 +0000

    responsibility so getaway turnup beginning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_itemsize(array):
+        return array.itemsize
