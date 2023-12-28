3ba48fff32174739a6d44d8a333c6cb8bbec4ffb
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:25:48 +0000

    birthday rate move pressure record

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_work(force, distance):
+        return force * distance
+  def convert_to_binary(n):
+        return bin(n)
