caf09506f8b7bb311e9a4463fffc186852f007ff
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:31:26 +0000

    relative or comfort nose above

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
