d1ce2a2538a9025275cb23727a4fcfeadef0319f
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:59:53 +0000

    feeling fat criticize finish letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
