553de43d680fd91e8c9d728378f29baceb7e6564
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:17:48 +0000

    consist considering murder waste shall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
