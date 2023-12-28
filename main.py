bc58c4f38323bd42d92d188c89d0a93b4fc4ccd0
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:06:40 +0000

    arrive character stamp wrap play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
