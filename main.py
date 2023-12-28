e01e912f00728797fc3a0cceaf2bb7dd5b26d025
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:49:32 +0000

    command theirs innocent free impressed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_error_function(x):
+        return math.erf(x)
