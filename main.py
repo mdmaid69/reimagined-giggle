a2a19fd2bdb553b830620da8eca910fa38fda4c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:14:23 +0000

    test price union shape smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
