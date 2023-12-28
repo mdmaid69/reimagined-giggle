99eaaaa564dad6b20898438dace6e07a3577ad87
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:45:31 +0000

    autumn minute cross giveback possession

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+print(math.pi)
