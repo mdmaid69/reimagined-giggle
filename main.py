f50ad3754935d6265359a07419e331b7590b9d9d
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:16:56 +0000

    entry visit heart spare if

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print(sum(range(10)))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
