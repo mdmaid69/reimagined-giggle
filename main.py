e32c5e0f7f4246f8f8e0a793dccd999ae5065cef
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:31:05 +0000

    actually thing conversation expression workupto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def remove_duplicates(lst):
-        return list(set(lst))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def find_max(numbers):
+        return max(numbers)
