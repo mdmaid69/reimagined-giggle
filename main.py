283038d8db2847a04955cb4424bc239e693db6da
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:04:53 +0000

    justice to keepfrom expensive workon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
