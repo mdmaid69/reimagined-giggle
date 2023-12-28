cff65c0156256e770398eddfaedd36ded04b6709
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:55:18 +0000

    frame could no forgive across

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_density(mass, volume):
+        return mass / volume
