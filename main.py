bea415c0972f1ae729b2f73a8340d48740eb1b7f
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:59:25 +0000

    underneath herself common hand whatever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def cube_number(x):
+        return x**3
