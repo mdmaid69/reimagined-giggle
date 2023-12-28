d1d937f6fb5277a02a9beb846f3bd582306c31b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:23:43 +0000

    height escape pen letter dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
