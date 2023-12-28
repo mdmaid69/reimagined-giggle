1e77e78060c1f7c236b967bd492058ba3afe1c53
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:19:32 +0000

    land tap effort pipe tax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
