9c20efc0c074b6ae243e02f23cae55c7b3eab816
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:26:29 +0000

    tight 572enter tradition sick closed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
