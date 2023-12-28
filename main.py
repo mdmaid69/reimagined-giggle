e9f143190f76dea8db44d7dad0abb30b7b221148
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:12:13 +0000

    fault figure typical sweet shadow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+text = "Hello, world!"
+print("Characters:", len(text))
