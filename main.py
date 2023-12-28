daeed51cc34c432c903aa442eb8742adada6670a
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:23:12 +0000

    broken his sore more worth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+print(math.pi)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
