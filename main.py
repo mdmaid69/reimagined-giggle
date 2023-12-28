a0e3b1d27b56e4083ae45b337b07f9ea22c38c05
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:43:05 +0000

    close abroad person little leave

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def subtract_numbers(x, y):
+        return x - y
