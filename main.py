e87d8e300b9765c14cec113135151153a6f7f62f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:09:04 +0000

    stare sensitive closely pin swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
