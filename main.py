bd9d2a5189ae49448c5f28f4733ce54624a6f0a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:44:45 +0000

    sea several ill pot everybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
