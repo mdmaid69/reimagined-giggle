36b09cdc2653eb90a4f89d4d2ed78f264d90b90a
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:47:48 +0000

    taste project approval breakinto collect

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
