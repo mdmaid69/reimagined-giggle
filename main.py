4aeabc951a1ba5d88ba25c14609c55dc1d299d24
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:16:53 +0000

    keepfrom boil under battery considering

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
