723325c9af91526d0dbab6e32a185c37c32666f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:39:53 +0000

    my prepare goup skirt warn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import sys
+  def get_python_version():
+        return sys.version
