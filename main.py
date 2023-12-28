3fe6ecdb8faa0ac6de79c520ef6ab6227e1d4d33
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:54:54 +0000

    pay string fact this sign

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
