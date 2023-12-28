a504818132b10bdfbe41d0b632f3d8196ab23f0f
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:09:09 +0000

    town kill follow regular flavour

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_as_int(array):
+        return int(array[0])
