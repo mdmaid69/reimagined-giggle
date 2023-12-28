d5c98ca8b7f9d7dd06b464b87cfe475ae14bee8a
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:21:54 +0000

    squeeze colour practise process measure

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
