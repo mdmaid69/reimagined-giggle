96379f16c6689c0bdf8bf71a14929109519c1f88
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:22:55 +0000

    fashion hang model earth catch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import tensorflow as tf
+print(tf.__version__)
