4e1992da5cbd35d98a794c4079468bb372982b31
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:25:28 +0000

    profit whistle letter crash nearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
