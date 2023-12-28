9102c41b999e5043fc405a64645e7c9a4d494df4
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:57:31 +0000

    display slightly soft music chin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def add_numbers(x, y):
-        return x + y
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
