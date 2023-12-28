44d192ea93d4e88b5e2c656f48e46220f7e1dcd1
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:35:34 +0000

    address prevent large neither independent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-name = "Python"
-print("Hello,", name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
