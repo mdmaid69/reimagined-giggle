7c25ad616ab970f78e9fa63cfe5b68b3674356e2
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:53:48 +0000

    pronunciation coat class without use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
 import os
-def get_current_working_directory():
-        return os.getcwd()
+def remove_directory(path):
+        os.rmdir(path)
