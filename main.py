089fbb6cf4e9c67cfa801663f8801c6d0da9c05f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:22:55 +0000

    holdon weak suppose event my

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import array
+def get_array_as_set(array):
+        return set(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
