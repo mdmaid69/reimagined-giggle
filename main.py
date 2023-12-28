cad2d5989491833ce17fdfbfa65281d9c1303497
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:22:33 +0000

    trap trade concerned aware art

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
