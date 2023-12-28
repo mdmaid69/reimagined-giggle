b2a99060ad8307161e9276d049e664beb0a2a3c1
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:35:21 +0000

    none intend loss insist defend

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
