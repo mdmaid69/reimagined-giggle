3887f71f6e043967d86c3b2d00feb4b3d0da408d
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:30:03 +0000

    move marry respect purpose run

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
