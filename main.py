13fe67afd858a0c9362cbfcfb109512b819e49cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:14:54 +0000

    figure wealth pick right breakdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
