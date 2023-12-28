1ff9563fe65a58318693e5d54f97abcd43f87183
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:23:50 +0000

    hit content quick view breakup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_work(force, distance):
+        return force * distance
+def calculate_speed(distance, time):
+        return distance / time
