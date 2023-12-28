0ddf02f87626244e7388596d63d576fa573fd6d0
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:21:13 +0000

    wish tight choose bet unlucky

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
