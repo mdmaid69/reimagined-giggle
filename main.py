4c26cb71a172a81865e95543ac4dee91c227a523
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:42:50 +0000

    belief love pretty very deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
