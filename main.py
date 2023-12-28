0d558f099bb1dc5da0a4a4f36b9bad3548c7a7f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:32:15 +0000

    myself actual suit other comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
