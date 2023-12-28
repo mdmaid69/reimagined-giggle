8d5ff07434e72565387b9a53d1af923f1d69f109
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:29:24 +0000

    rubbish children no concern Ff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+print("Hello, world!")
