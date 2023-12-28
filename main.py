65131525258a72045189caf4f0bdc789fae3d67d
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:15:07 +0000

    tradition fit pipe stamp opportunity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
