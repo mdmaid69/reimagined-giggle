2b8c3924943a5f9de10523ff2e0d565a3f245d96
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:23:24 +0000

    someone collect less threaten use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def reverse_array(array):
+        array.reverse()
