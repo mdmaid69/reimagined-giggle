627eb68d831170c411870cc14da2e3af7cba2043
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:30:09 +0000

    memory argue 571enough lost offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import collections
-def create_counter():
-        return collections.Counter()
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
