9b6984a3af8185b3acbc106474e550aefc46928e
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:32:57 +0000

    strict regard terrible occasional completely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
