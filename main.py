f8d72b68419349e01d0deebe2aebd6d0c445cc50
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:06:41 +0000

    plant slight sure big home

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
