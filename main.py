dfa3d3c89663405ac4d1a48229665862c98bab71
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:25:42 +0000

    situation knowledge under protest interested

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
