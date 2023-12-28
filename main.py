47075800d49e8de5501cbf9c7c763aa67f6b32de
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:41:42 +0000

    pump poor burn smell justice

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_acceleration(speed, time):
+        return speed / time
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
