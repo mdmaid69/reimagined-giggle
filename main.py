99aa6692a24f0a6728cb0f6450787ea0e0be87e5
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:23:24 +0000

    giveaway sound design contact last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
