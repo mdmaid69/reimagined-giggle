eca6d3b2ac5936245be5130479e67e4b280475be
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:46:45 +0000

    sit shadow runover table less

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
