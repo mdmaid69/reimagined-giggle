75e78c67ba1db6a061394c34169b47d3abcf6e9c
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:34:44 +0000

    runover innocent reply slowly mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
