e397316bbe4a1981e44123098de47ba96bd0f403
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:32:42 +0000

    kind attention splash drop so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-from collections import Counter
-print(Counter("hello world"))
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
