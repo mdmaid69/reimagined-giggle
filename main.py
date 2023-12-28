3692c0659e3731d8b71a7a6dc3248637dd1c31ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:50:10 +0000

    its believe please underneath convenient

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
