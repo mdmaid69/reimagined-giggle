4bd7e595afada38c076a96399125ec5032d0af44
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:44:47 +0000

    characteristic recover visit letter comedown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
