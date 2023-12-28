6f07b3ac566951f42376effb05432b2d0b394feb
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:54:14 +0000

    come act influence take wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def remove_duplicates(lst):
+        return list(set(lst))
