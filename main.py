31ea7ec5d3e84b68d3dad9b46d10d0c5cc186e87
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:05:21 +0000

    upset fair surprising bank happen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
