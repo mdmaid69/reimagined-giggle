30dea4887a22f359c86286cf96dc083488d43e6b
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:53:27 +0000

    commit shelter dance meeting Nn

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
