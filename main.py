91a756a6f390346f42e7933ff0632bb6d6b32ec9
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:40:45 +0000

    close trouble them Uu sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_rectangle(l, w):
-        return l * w
+import collections
+def create_queue():
+        return collections.deque()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
