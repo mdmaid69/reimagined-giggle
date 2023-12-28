ac282c57064b6287627fca560271bc07221c0601
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:27:26 +0000

    choose tip hide regularly edge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
