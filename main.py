53c9ca8945e366d8d03d9c0ead688c7723bd7ff0
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:54:27 +0000

    here from afford concerned support

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_user_list():
-        return collections.UserList()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
