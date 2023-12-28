8b18dd49e04e94667449ccabaf57ada94c14687f
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:48:14 +0000

    pity comment custom ticket seem

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
