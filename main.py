12ae979bbbdbdd578b4467b178351714634672f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:23:12 +0000

    know flat slice black callat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def calculate_speed(distance, time):
+        return distance / time
