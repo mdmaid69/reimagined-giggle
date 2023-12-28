e017341de9448692e2585ebce754809480ecddc7
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:13:59 +0000

    it bit offend field substance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
