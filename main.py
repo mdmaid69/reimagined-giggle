1a1e0fddc7c29187cb9c3bdbb117db4ec0ec36a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:40:24 +0000

    interruption substance sign guard of

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
