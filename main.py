c9f3bb4c81af08a6b2566703bdfbca5151281c37
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:23:25 +0000

    nowhere spend deep room afraid

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-print(time.time())
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
