5408d3da907d9d476e69690c23db8fa6fa135c36
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:33:34 +0000

    use Uu her cry seat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
