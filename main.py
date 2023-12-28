e5420a631d3ab760f7d05adb185d8a3b0f97580c
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:54:16 +0000

    respect commit have need regret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
