076e9fb9e925d926331d5c9fe84231b60f724cd5
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:06:07 +0000

    front table sleep Ww when

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import os
+def change_working_directory(path):
+        os.chdir(path)
