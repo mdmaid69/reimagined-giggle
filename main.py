095f4ccdc61890740cd74ed8ea3649de0b101c56
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:00:43 +0000

    control motion release minority speed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_area(radius):
+        return 3.14 * radius * radius
