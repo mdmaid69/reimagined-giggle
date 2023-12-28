a6fd1f025bd1fb6857206628e081b0e458749e92
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:53:49 +0000

    negative address Oo pinch medium

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
