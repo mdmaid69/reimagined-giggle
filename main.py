d3f030dec91925ca8400ab693e3041c56e26d92f
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:50:01 +0000

    lost write sing introduce some

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def find_max(lst):
+        return max(lst)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
