1963e8c235e14ac5ace8826a8265535fe0073ce7
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:23:37 +0000

    tradition sure nice our police

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def square_number(x):
-        return x**2
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
