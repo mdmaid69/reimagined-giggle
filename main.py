c0fde1ff11419b4c9e7b2d926e04d39aac4679e9
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:00:06 +0000

    wheel battery anxious be country

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
