03c7d1d5ba59a15bcc5335e1fe8b45774eff0121
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:27:45 +0000

    goon petrol clear dream I

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
