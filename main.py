d058e420a3d1c09f0a3a08c897fd23995758104d
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:22:55 +0000

    571enough confusing roll slip below

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
