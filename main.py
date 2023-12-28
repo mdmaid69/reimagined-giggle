3c55b8e355f1a86a46c1869ec29cb6f975aacbfc
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:53:45 +0000

    Zz speak yesterday your church

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
