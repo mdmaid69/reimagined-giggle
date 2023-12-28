31df439d948cb2688773e2a65de0844718e72e91
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:47:43 +0000

    else form spread hole spare

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
