0c94e810275e910de92899345e9090a2f8c8f253
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:58:56 +0000

    once pretend fast instant stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_array_to_list(array):
+        return array.tolist()
