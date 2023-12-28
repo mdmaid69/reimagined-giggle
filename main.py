88c9363223da7186c69dd4eb5e5c4134950d526a
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:46:09 +0000

    regard sticky care lay edge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
