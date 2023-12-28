89e19e5ab129ba978837eaa8a8e3e0662efba848
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:47:38 +0000

    deliberate actual material swing number

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def remove_duplicates(lst):
-        return list(set(lst))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
