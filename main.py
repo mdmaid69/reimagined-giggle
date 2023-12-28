49a6d67009b3f124f813eb55625c3e54305c340f
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:01:42 +0000

    badly distance mind towards order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
