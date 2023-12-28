aa116993e06d4b5f2313fc2472af75c42874b9e7
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:11:39 +0000

    respect rush blame apply note

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def multiply_numbers(x, y):
+        return x * y
