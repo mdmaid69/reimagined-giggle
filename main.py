178e287e323fe76fa54b408ad2151cb4b075d794
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:40:49 +0000

    fetch contents strike ability outside

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_work(force, distance):
-        return force * distance
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
