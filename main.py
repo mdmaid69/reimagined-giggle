b43580d864f79a18854d5caf5978891bd6963190
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:20:59 +0000

    work win soil spin face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
