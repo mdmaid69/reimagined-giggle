cbe6b30415962253abac131930d1878fdf708ecb
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:24:42 +0000

    scale hardly something separate some

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
