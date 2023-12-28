d43b84913df3aa71f45c5c3e400de33574124104
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:11:11 +0000

    Uu ticket flow live politics

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def reverse_string(s):
+        return s[::-1]
