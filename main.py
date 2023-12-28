b69c162761f0b016d5a610ad552c88246fb6d867
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:54:56 +0000

    brave disturbing lift confident wrap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def is_even(n):
+        return n % 2 == 0
