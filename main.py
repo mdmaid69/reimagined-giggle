873d918c4a80c65b29fa69514d3111fe3d0a7310
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:26:06 +0000

    wise peace female juice have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
