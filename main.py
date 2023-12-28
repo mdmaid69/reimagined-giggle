0fe4f8b57817e94fcf74d530738782c24e65d20d
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:32:32 +0000

    tie shop proof sticky hate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def square_number(x):
-        return x**2
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
