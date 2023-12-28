8ab81e6f9ce9b9896cda1a40e93f7460933eb70c
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:00:10 +0000

    indeed tap make somebody this

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
