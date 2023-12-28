5f6ad3cb008d45b813581682e9caf10dc17eeba9
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:16:58 +0000

    usual remark frequent up rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
