3ed137e62d86fa2dabf16afdb2a764bdaaf765cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:58:15 +0000

    minority apply typical will freedom

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
