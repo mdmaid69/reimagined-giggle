43ef4a53ef8ccdff69a3b9f20c3487719fc83de0
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:03:04 +0000

    factory politics whenever bear until

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
