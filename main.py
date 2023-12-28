9ffc79dab8e580cc1967083d6698414c6954a37d
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:09:12 +0000

    wake quarter oil foot model

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
