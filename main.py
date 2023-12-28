f4e74b4f60e2d6b8c7266938da3d94e7e6537ddf
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:25:16 +0000

    just constantly challenge reaction tread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import os
+def remove_directory(path):
+        os.rmdir(path)
