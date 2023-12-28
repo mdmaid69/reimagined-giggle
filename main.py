3ca4535abb28138c37503c96f50de589eba82948
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:19:15 +0000

    distant sensitive easy neck total

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def cube_number(x):
-        return x**3
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
