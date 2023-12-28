da85aa37cf7ffb48c2e8b88f57ea188789ac26f1
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:38:24 +0000

    rise scrap reasonable manner but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_as_repr(array):
+        return repr(array)
