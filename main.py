0c83263e67fb2972eb2b597cff4b156caa58f707
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:14:47 +0000

    sensitive mean tax surprising shelter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
