fcb81338d68f012ed1bc0899d08fe4d3b3989225
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:17:59 +0000

    paper promise perform warn payment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_power(work, time):
+        return work / time
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
