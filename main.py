6ad332ad685a93cb85dffd3971f6b277b5177120
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:18:53 +0000

    drop thanks show make afraid

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
