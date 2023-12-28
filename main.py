725c31c9e0fdb13d8aae83a679c7dde843377f1f
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:29:44 +0000

    branch operate shoot mean beyond

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def sort_numbers(numbers):
+        return sorted(numbers)
+  def reverse_list(lst):
+        return lst[::-1]
