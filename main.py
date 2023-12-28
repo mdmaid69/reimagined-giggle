da1132c54ff006ef34f73e1f85f57666afd2c870
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:01:52 +0000

    risk plenty female interview spring

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def is_odd(n):
-        return n % 2 != 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
