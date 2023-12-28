a3f2987f0a82b30f9807894e2a3fc904abca5961
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:48:05 +0000

    petrol will sight instrument trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def append_to_array(array, item):
+        array.append(item)
