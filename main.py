a1e83e80a511632c5b94d75a44c046018e1c772c
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:36:19 +0000

    name culture night worry organize

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
 import array
-def get_array_length(array):
-        return len(array)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def get_array_item_count(array, item):
+        return array.count(item)
