1300def904322628f92690c7398a7788ced47fae
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:26:11 +0000

    father test no ground back

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
