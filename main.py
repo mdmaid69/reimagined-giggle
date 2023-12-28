9930a729d0b6eda4bfdef8816a89a6f5c85267bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:57:46 +0000

    feature wish succeed identify drive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_pressure(force, area):
-        return force / area
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
