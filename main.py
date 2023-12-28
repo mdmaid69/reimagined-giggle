8e335a02a4e1c84d3ba8fd16d60ce08f061580a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:08:40 +0000

    advice front feed precise exactly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
