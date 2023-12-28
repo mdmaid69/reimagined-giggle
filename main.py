94f260fcb3078b56ddd76faeed45a3c9bb745969
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:34:49 +0000

    soak whole float getaway house

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def convert_to_octal(n):
+        return oct(n)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
