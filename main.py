a26dbb09b7492193c244b856a48fcb07283a6246
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:37:14 +0000

    finally apparently angry public far

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
