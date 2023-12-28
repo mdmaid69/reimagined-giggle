d9632dc30f1011943374a24fadc3c8067183dd3a
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:42:58 +0000

    sink love pile left contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
