bd735c972855219304f8570e88298eeed8214cfd
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:09:10 +0000

    gentle stand one failure stretch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def find_max(numbers):
+        return max(numbers)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
