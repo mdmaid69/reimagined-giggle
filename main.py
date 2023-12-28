379d7fef18c309e4ba26d8035c00e068959baece
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:32:38 +0000

    band may fresh damage habit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import random
+print(random.randint(0, 100))
