12b59b8f889e89839af8d816ddc12eb8ce2e864a
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:10:22 +0000

    prize hair house smile communication

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
