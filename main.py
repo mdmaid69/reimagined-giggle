5c7bca90be57e0154f33f17a3b9bb827dd246d6e
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:58:50 +0000

    push worried distance think general

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def square_number(x):
-        return x**2
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
