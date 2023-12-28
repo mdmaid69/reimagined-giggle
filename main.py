1d99ea8171022318deeb859b5a4eb22bea929b5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:56:56 +0000

    callup callat balance copy neat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import os
+def remove_directory(path):
+        os.rmdir(path)
