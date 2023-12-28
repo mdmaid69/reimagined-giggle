6a0798c00fc8074a5cf61dd8b0222d72f8ab2b58
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:03:59 +0000

    wonder getup geton production getout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
