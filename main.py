1a866c66eae7b2bf8d070a0201e151ff5abf8781
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:17:11 +0000

    silent bringback smell idea do

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
