6daa8d081e19129bbae34a4af16836c96b18ed66
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:12:43 +0000

    home opponent regret shall joke

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
