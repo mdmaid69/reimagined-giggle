1d3abb3ed01fee610ba28980faffa327e909a232
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:06:40 +0000

    speed open estimate oil check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def subtract_numbers(x, y):
+        return x - y
