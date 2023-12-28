75f91412a1daa121a97a101afbd98d941902faae
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:08:34 +0000

    excited offend original hold communication

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
