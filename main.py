57c5c161ad351854e7ee53cce55b174109b84a29
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:03:26 +0000

    danger drink dress advise run

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def count_words(sentence):
+        return len(sentence.split())
