fcb42fe6deae4730137aa2f761b1a24b0a6bfc20
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:03:35 +0000

    that radio survive fill defend

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def find_max(lst):
-        return max(lst)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
