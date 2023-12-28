8de3c6856cc56fe766c74d05812afc4e6f5ee25b
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:21:15 +0000

    ours grand bank physical window

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def convert_array_to_list(array):
+        return array.tolist()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
