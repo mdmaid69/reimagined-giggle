436c093470ea97efa24fb9ecf103cdf8e1fe74af
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:02:25 +0000

    thinkof valuable opportunity Zz live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
