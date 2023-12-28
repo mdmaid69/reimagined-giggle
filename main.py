a452ef76f831ab0653e441adc7f973eaf91472e5
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:35:27 +0000

    style face congratulate mine interruption

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
