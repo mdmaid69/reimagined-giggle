e46f0f2df0f0863342a45ea8fefcc3a89ff29171
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:05:35 +0000

    some paper responsibility turninto fasten

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
