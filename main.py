62d6d4562fd364da37fea90d229d4744c97628bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:19:02 +0000

    curious gather noise habit fetch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
