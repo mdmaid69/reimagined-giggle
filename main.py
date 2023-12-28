7a41d5a896b00530f75ace1db483947bde9d8164
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:59:14 +0000

    Ss belief wide up rank

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def is_even(n):
+        return n % 2 == 0
