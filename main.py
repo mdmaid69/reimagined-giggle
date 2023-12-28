25cf4749ff4725b493bff6c4355b3fb6b5935570
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:28:52 +0000

    promise breath borrow day healthy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def count_words(sentence):
+        return len(sentence.split())
