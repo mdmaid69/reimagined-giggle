83f34a653ef8080c8b094f7883458b7477659e32
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:34:45 +0000

    terrible concentrate contrast occasional autumn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_binary(n):
+        return bin(n)
