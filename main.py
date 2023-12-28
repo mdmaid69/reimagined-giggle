1f4bf068710aa929291dc6190ed64789e35fac59
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:08:35 +0000

    name pull prove container record

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-for i in range(10): print(i)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_speed(distance, time):
+        return distance / time
