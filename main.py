4a958c786063fc479f3772b5cce5f233fd575856
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:56:40 +0000

    union pretty strength window miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def change_working_directory(path):
+        os.chdir(path)
+  def subtract_numbers(x, y):
+        return x - y
