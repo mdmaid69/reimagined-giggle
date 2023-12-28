86d77a5017e8f6978deab8688bf3e58f474630ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:09:42 +0000

    plate underneath own 565end cheap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
