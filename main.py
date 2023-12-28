c85d5e9b93aa07d1dc1ad5ec146556471b710d0c
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:33:34 +0000

    order use undo rest giveback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
