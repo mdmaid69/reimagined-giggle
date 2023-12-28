d26775b3628cd6b61b8bc5e2899ba90b8c9e5947
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:02:53 +0000

    570enjoyable threaten schedule maintain smoke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
