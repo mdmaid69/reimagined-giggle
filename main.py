5d928920967b7f33dc78857b9eb72ddafcd24ca6
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:56:50 +0000

    question cut spring may start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
