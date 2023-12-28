8291e48e426b67f6a69a2411d514b97bcb77301b
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:56:04 +0000

    find cheek obvious general path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
