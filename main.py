2194a17e829501a1d72cd962e631ca42e6c3b3df
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:10:28 +0000

    sharp responsibility belief now patience

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
