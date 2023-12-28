32bf1b7fee003ff0405bec2df647ba6bea75d389
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:31:59 +0000

    confident young entry through soon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
