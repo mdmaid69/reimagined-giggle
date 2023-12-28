fe2ba5ff493b691d599abb15ef101055260c06ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:38:20 +0000

    couple measure survive material immediately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
