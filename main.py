0e4b8b7b58a685896bccda136a6b5ee38049b199
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:29:03 +0000

    classroom finally failure size difficulty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
