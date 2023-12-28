7548fd048c9348c3772972d6674099b189aaeeb6
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:24:17 +0000

    string black mess funny city

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
