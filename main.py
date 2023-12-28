59423e027771a5f2141067e8ba12809333db1a11
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:03:00 +0000

    hang popular punishment seem some

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
