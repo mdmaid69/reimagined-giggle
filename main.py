c1521b81fec8dc7baa4253710945c012c780ce69
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:44:07 +0000

    prefer sure here post roll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_file(file_name):
+        os.remove(file_name)
