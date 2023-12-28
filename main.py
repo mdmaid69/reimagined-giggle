78142f62d0ba337f5acccdd825813538efb9ff6c
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:55:34 +0000

    congratulations immediately sail as trap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import tensorflow as tf
+print(tf.__version__)
