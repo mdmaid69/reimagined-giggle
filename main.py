6fe875b618e7f8195492fa8e637b923b3648da1f
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:05:27 +0000

    tip which present effect attractive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+x = 10
+y = 20
+print("Sum:", x + y)
