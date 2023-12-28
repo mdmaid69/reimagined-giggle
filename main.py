d0a7d6cef2104a186b149a023e89c465796b28bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:39:01 +0000

    friend measure scrap pinch instrument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def get_array_as_bool(array):
+        return bool(array)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
