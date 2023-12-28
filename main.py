ab951bb6c9781587f5b500874472e5e895f4161e
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:11:10 +0000

    rush spare closely slide careful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_index(array, item):
+        return array.index(item)
