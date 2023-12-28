247e39fe6c29980b3e4273700c6f4e7e20956228
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:13:16 +0000

    earn polite for reaction form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
