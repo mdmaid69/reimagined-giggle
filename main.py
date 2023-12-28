ba10617c731041cfcb95e217dd6f6d79717f4595
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:02:58 +0000

    yourself ahead comeout Itself move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
