f178beda3c035214b29ef1d1566a3b23569a48f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:45:40 +0000

    film result meal talkoutof reason

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
