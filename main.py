922209a38aa100fe4102074901e2e80408e54f9d
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:11:53 +0000

    set measure getaway worth completely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
