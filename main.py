ad0dfafa236358889531dd8e1a8106f41d38d4ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:40:03 +0000

    brave bin awake fairly before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
