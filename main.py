be2bd25508361616e266dc4dd8621768f8cbfdf7
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:28:06 +0000

    rain mind prefer book bill

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
