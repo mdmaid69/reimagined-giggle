f056bca7d9630b5d86d5b026788fe72bce91a307
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:02:09 +0000

    back regional tooth emotion breakin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def is_even(n):
+        return n % 2 == 0
