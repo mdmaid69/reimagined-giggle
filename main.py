6521e146fd237b10d62e0f323486aaa5bf236283
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:30:59 +0000

    pupil wrong quickly healthy direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
