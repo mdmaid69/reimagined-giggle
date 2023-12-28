caff77d227417afee1c2da6efb82745e91e0d628
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:20:40 +0000

    learn somehow rob aware boat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
