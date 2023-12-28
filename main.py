0e1be8d9e0c4082f79bf45bcf6c44b27a8778567
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:23:04 +0000

    small protest bury ask skill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import sys
+print(sys.version)
