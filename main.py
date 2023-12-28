610fdadd61dba36225325f92dd9b3693f65a60c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:09:54 +0000

    dance dry runinto sign feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_as_int(array):
+        return int(array[0])
