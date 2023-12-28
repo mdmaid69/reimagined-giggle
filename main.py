1544a555bc43654609ee9d8892834870103aba02
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:14:17 +0000

    unusual base off clearly pinch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def convert_to_json(data):
+        return json.dumps(data)
