ada25a75b105ec4bb3ecc5946b6be2deab9f59be
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:32:36 +0000

    be experience combine unless sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
