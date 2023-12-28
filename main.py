a830b47eb7fc17ec6934687617b9d15150f77ebd
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:28:01 +0000

    hope compare pretend roof so

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
