7edc2807e59db43ff7ecc58520efe31477f6b94f
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:35:36 +0000

    unit compare ilovedicks branch mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
