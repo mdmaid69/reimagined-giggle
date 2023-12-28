50aa8a520f3b7b7a48f5bb8db7b4f904230e8be5
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:59:52 +0000

    sign once remove finish stupid

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
