4d8ef88bb9bdd241c099b962450c07dc66d7d905
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:32:19 +0000

    exchange shape moon loose prevent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import time
-print(time.time())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(x, y):
+        return x + y
