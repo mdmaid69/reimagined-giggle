aca6da8bd5b915735a2a65e8d68fd15c7f07e676
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:17:40 +0000

    form yours turn area match

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
