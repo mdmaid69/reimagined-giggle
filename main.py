43ac1af23ecb2c53d58f284e2df176e7a318b08c
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:18:57 +0000

    rub once section arrive rescue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
