9f5355647fb251ac94da699e05af26b9eafdd745
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:06:33 +0000

    balance quick please squash during

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
