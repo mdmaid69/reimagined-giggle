1e6c1a4b961c1499fa0ea7b9cd4b8acea5c7c5bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:44:14 +0000

    quick size nonsense roll Tt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_speed(distance, time):
         return distance / time
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
