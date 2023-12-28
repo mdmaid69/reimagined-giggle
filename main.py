cd53cfde101fe850edc3f0ebad9ede436feddd19
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:03:04 +0000

    advanced refer possible thought speech

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
