e326223177b4ff47dac7bcab338327dcfd23ac7f
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:29:45 +0000

    apply mixture no inform look

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_array_index(array, item):
+        return array.index(item)
