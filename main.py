749ba523195fbe6f923c7038ca5cea5d23d8c069
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:56:44 +0000

    sometimes discuss themselves there reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
