267c201551c078e646a76d4a124e19c5c4c168ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:24:38 +0000

    inquire theirs pick somewhere pupil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
