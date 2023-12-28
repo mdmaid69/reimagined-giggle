7bdaad57f5ccbea45f1cfb8356fd17b434859975
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:06:06 +0000

    rescue equal desperate compare unkind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
