5d0b6c12547ebd086f16b9c7ce16f3961dff7da4
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:12:08 +0000

    undo news print complain move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
