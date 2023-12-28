99b97ae3150171409fc4fcb8dac3fed7f8086410
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:54:47 +0000

    lower miss suggestion goup praise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
