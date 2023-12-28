3ce863b3a95f57f9fb57528f210f6d56be365dfb
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:32:45 +0000

    nearly holdout check scream study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+for i in range(5):
+        print(i)
