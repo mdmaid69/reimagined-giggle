1a3b2db8852a8b3061c9ed6532559f3284b02be1
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:11:00 +0000

    everyone form Pp usually crisis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_acceleration(speed, time):
+        return speed / time
