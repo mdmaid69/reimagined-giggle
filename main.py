fb854899b4b87e189e1f15a5eba567d01ca69c64
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:57:53 +0000

    idea argue only take usual

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
