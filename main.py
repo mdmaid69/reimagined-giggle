4925ad4fcd2414f34f57ae9d083e07b9f7b8f700
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:04:19 +0000

    excuse worry language keepoff stop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_palindrome(s):
+        return s == s[::-1]
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
