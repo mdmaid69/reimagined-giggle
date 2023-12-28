ba42e5227dec748d69653cb1ddf0c587197ffb70
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:05:38 +0000

    cheque action sweep forever ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def subtract_numbers(x, y):
+        return x - y
