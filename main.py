02ac309c6e6d3cb31d5bb5de896d758cdf977909
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:52:51 +0000

    place slightly education towards story

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import getpass
+def get_username():
+        return getpass.getuser()
