3f2a524ad26cd4de062177a6040a7626f12662f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:30:40 +0000

    swimming worse rate activity crazy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
