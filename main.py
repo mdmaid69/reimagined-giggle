255dac8630574f0fbfc45f53172942d042e80636
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:15:27 +0000

    artificial hurry friendly often middle

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
