2312d55d13c6708296c2e5afbdeadf3399dbc754
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:20:17 +0000

    thumb show country shop material

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_palindrome(s):
+        return s == s[::-1]
