e919aa94be945524bfc33e25401a0919f04f7aa0
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:05:59 +0000

    brief he whole note shop

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
