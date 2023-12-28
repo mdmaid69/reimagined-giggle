bdc3dc318d31969b6c2c9331f7f48677954217bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:34:53 +0000

    storm pencil success holdup if

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
