95eb653251f0fbeb3a21062aa937e3fb4aa6107b
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:17:50 +0000

    if near distant congratulations minimum

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import datetime
+print(datetime.datetime.now())
