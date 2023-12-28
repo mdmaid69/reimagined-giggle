4e583945e746e8249f41a33a4d09db1962f85624
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:45:30 +0000

    whistle design while later story

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import sqlite3
   def close_database_connection(connection):
         connection.close()
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
