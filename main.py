1a7c3b3900a80d869240563daffe4089dcd285bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:15:12 +0000

    uponon pollution goup born fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
