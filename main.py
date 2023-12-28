8a63f95dbc8c617e94d22ec5f268c3782ccbde99
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:58:54 +0000

    suck government sleep sore raise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def remove_duplicates(lst):
-        return list(set(lst))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
