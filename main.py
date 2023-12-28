910c647d336656c4e57bd45abc7fe01eb959b361
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:02:02 +0000

    borrow worth improve satisfactory anybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def convert_to_hex(n):
+        return hex(n)
