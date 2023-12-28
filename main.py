3c656eccedb5a456fd4cbea4abb8f2f5ad332619
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:36:40 +0000

    equal related disturb still switch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
