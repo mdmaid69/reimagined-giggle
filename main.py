5be83dfb5b0985747f5ab76af9c5cd183ce1a719
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:17:24 +0000

    bringup judge protection taste store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
