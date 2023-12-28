9ea630e5e8d8591b6538ba2e08e4edf08a522c53
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:18:41 +0000

    passage stretch decide total work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def sort_list(lst):
+        return sorted(lst)
