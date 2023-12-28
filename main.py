1758b755561d772ba4d9083856f7269d6c72eba5
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:46:29 +0000

    deserve opposite anywayanyhow involve pleased

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import sys
+print(sys.version)
