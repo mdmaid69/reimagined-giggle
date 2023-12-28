a7e496490374441f54155c3a32829bec04b876b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:36:43 +0000

    wish split seat confidence chief

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def count_elements(lst):
+        return len(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
