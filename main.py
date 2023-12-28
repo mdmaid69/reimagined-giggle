1cf08b40e0b8cdc43e41050b7d2bfb1ae227f052
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:29:37 +0000

    strong workupto influence lower bridge

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
