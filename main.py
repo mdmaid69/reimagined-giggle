63b297891b5693550fe74d3af5379ca012ab1e3d
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:17:04 +0000

    raise loss post match target

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
