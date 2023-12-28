8fa7e928deab0091f7a513418649b50c25846520
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:34:32 +0000

    fall minimum shadow also rich

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
