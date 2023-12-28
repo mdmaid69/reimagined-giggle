113f9f27ed5e1d8d6c9eca3ec0e0763a789a6704
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:47:19 +0000

    side present develop lookaround sale

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
