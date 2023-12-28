bf035fa73a961a4c6f85c0dfbddd3acf5bcd3c02
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:36:12 +0000

    sign ear shop grateful hot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+name = "Python"
+print("Hello,", name)
+import platform
+def get_os_info():
+        return platform.uname()
