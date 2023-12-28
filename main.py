6ae769b9aeaca3491469c50a7f10e68b794011c1
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:40:44 +0000

    eager under development shop normal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
