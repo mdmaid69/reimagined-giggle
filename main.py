859d23f33335e37650a99e86a514127c89fde9b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:43:32 +0000

    appeal account serve blow book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def find_min(numbers):
+        return min(numbers)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
