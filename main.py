74d7df61f37b6bfa1b138403c3b928a3b761b465
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:12:21 +0000

    practise distance back spring really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import os
+def change_working_directory(path):
+        os.chdir(path)
