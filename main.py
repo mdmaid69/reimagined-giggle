16510db820bf3fe6ae2d919213de4a36009f5007
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:22:47 +0000

    punishment cry admire actual will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
