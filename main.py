89389e0b83e9290f4afe36b07d7dac7893c5ebb6
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:47:20 +0000

    unhappy thumb lost pour hot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        return sys.version
