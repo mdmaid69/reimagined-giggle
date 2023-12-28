f36bc290fb5d21f511d1eb14cdd3ca9202cf6dad
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:12:23 +0000

    quickly many crisis method surface

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_list_from_array(array):
+        return array.tolist()
+name = "Python"
+print("Hello,", name)
