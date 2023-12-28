1003ad0fc0daadc36b89f1bea2268a9fa6d8b234
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:48:40 +0000

    male uncertain ever creature difference

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_density(mass, volume):
+        return mass / volume
