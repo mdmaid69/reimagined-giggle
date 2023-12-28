fef1084f51a67d7d1d2eefc0936ffe5966a00fe8
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:42:52 +0000

    warning guilty whichever lookaround terrible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_union(list1, list2):
+        return set(list1) | set(list2)
