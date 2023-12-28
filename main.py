8807e9948f9f935e2b7f82212c455300e0d7da42
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:35:32 +0000

    all excuse bet proud frightened

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
