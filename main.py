7518126023b1f2ce75e7aea541716ff38023c97c
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:35:16 +0000

    remember old money mix runover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
 import array
-def get_array_as_list(array):
-        return list(array)
+def remove_from_array(array, item):
+        array.remove(item)
