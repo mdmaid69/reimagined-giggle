ad3e30dfa931010f0ca85c54314854cec4847ba2
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:13:19 +0000

    forever sweet secret promise wear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
