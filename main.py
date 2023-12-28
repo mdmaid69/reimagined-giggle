13ac48fa0399a1b57d97013d6e2881aa3931e28a
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:01:53 +0000

    away concerned brush thank sentence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def find_max(lst):
+        return max(lst)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
