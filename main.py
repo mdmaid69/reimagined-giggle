b339cb52b9c9d3018960be45db6433b02001b7ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:33:27 +0000

    mean record behaviour statement accordingto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  def is_odd(n):
-        return n % 2 != 0
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
