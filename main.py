73ebf226012d6d46010f22fc06ba8a5e6f9baf50
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:50:39 +0000

    regular record wipe after needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
