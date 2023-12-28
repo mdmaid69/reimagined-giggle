6b2ad2005bc7e2c40fb803da9d99503f7626cf32
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:32:34 +0000

    view instrument effort start payment

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def convert_to_binary(n):
+        return bin(n)
