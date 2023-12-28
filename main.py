7ba495397ef633100fc629fe552227a4b83c5ae1
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:17:21 +0000

    flood most nowhere mystery press

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
