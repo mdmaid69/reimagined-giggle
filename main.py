d08efacbecf39fa0a08d3302b2ab88cffe78ea1e
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:04:04 +0000

    beautiful phone inform light equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
