fd62e0c334debdda7a1647a3ad379c0a27d9cf79
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:25:08 +0000

    win congratulations rid excited worse

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
