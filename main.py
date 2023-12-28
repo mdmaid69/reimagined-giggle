966e7747d0aabfddb821aee961002a45cc6e328a
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:20:10 +0000

    Bb except cheek influence Ee

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
