43e95574eea13eff7d5097105cdf9c5fc002520c
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:14:13 +0000

    flat care paper talkinto apply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
