8ec688f65fd261f7fe7d0f1f63ee191879980fbe
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:30:18 +0000

    little prefer forever range calloff

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+name = "Python"
+print("Hello,", name)
