b60eb5894e1f4df5ca9b1a13ab6161f9514994d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:37:29 +0000

    offer accept awake habit cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
