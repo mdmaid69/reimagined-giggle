844271e72837717de5015d04de34e48fb42c37be
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:31:35 +0000

    clean speech once forward ride

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import time
-print(time.time())
+  def add_numbers(x, y):
+        return x + y
+  def reverse_list(lst):
+        return lst[::-1]
