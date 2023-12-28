d258dad500b83671a3b707d587ab18d77c60f7c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:59:20 +0000

    care calm quarter answer foreign

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
