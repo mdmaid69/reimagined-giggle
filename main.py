c4b830e2831daef8d7f7b08491d992d238508e70
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:38:59 +0000

    quickly year careful matter noone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
