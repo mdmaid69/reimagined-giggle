4263b7f6093589506246aac9ba819f11638251e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:11:32 +0000

    colour put scared though account

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
