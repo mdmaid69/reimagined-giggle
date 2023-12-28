ad087816d5bde051c8e31df2860e5caf1ada3262
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:58:02 +0000

    extremely influence copy record store

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
