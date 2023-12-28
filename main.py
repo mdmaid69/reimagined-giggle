4c57db3f0d1a00f7126e348b30eb2d3b91e36321
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:46:35 +0000

    polite relation week fact laugh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def greet(name):
+        print(f"Hello, {name}!")
