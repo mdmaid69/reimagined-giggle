fcfe50ea71bfeb5dd04eb0d1e71c5abef19be775
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:32:57 +0000

    except happen blow crisis sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
