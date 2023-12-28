201cd5639eb18727f70229e0770d53f6edde33b3
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:18:46 +0000

    Hh healthy draw border swing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+text = "Hello, world!"
+print("Characters:", len(text))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
