fff3321c92348dd6e07d74aaaa2470c063d7f4ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:37:34 +0000

    nervous train practice character ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
