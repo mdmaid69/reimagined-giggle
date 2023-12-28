89025bc171e91b768dd15bf216505be15ee96879
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:55:52 +0000

    teach blind creature before inquire

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-for i in range(10): print(i)
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
