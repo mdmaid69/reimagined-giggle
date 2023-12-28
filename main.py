df9f39015d3e902369b93d81bbd3a57143230c0c
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:40:09 +0000

    union statement different live dig

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
