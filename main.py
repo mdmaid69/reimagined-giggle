4b63b1897782e28c733d86cf7364773c766ee1e2
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:49:31 +0000

    future undo control usually deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def add_numbers(x, y):
+        return x + y
