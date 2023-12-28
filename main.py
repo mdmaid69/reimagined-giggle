c978576b30ce44525247ae883b6bba79242eb5ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:40:56 +0000

    shrink such convenient welcome moral

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_max(lst):
+        return max(lst)
