cfa23b211671e7a162f0c8634196b4572c5ba4a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:33:59 +0000

    one raw underneath mess no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_speed(distance, time):
-        return distance / time
+def subtract_numbers(x, y):
+        return x - y
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
