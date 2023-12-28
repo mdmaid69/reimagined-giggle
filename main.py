2af01b840173cefe01da2f9c296448cee18cb976
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:48:22 +0000

    fresh fresh however you blind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def calculate_distance(x1, y1, x2, y2):
+  def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
