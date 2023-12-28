2742b89dbe6638bbadfccb360ffa9f88ec7b861a
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:40:01 +0000

    dream reject happy block comein

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def add_numbers(x, y):
+        return x + y
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
