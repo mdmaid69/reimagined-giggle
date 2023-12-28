2ea8c6f9537fc821ea075c1fa353bb2f61825a43
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:54:48 +0000

    against across partner to trial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def convert_to_binary(n):
+        return bin(n)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
