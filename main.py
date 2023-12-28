570784eeca67dca6008f35f083546dd9648d9473
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:51:27 +0000

    mix dance violent temper hair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
