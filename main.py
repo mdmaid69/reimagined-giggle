7d6797d71842844266141230292116404deb24c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:24:49 +0000

    wide grade off turndown responsibility

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
