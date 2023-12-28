34b811e50a8179acad51ead641ffd304d09d8ef1
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:22:30 +0000

    dinner his floor pot place

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_bytes_from_array(array):
+        return array.tobytes()
