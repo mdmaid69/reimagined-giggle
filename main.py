cade9648946734602cc6d6dbbab2af056a5d2b8a
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:47:53 +0000

    sign usual satisfied frame satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
