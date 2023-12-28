3140c1a43bf17c7d5f2e42486df2620dbef986c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:55:28 +0000

    near stranger appearance danger gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
