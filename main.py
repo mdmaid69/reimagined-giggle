78bf73286d61827b8f4b5b271ef599897e2fe16b
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:03:11 +0000

    sound promise zero any Cc

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import platform
+def get_python_version():
+        return platform.python_version()
