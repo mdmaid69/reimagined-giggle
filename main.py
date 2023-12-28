22602ce6b431af38df3de182d9051d75a23236da
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:59:29 +0000

    uncomfortable exaggerate favour building connect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
