b368e49af2e865e5f193eafb48cc95984976590f
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:19:28 +0000

    share satisfactory stiff criticize pain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
