9f2b669497a74349d0102c6686506e3f2bbece70
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:05:42 +0000

    date like only refuse regularly

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_acceleration(speed, time):
+        return speed / time
+import datetime
+def get_current_date():
+        return datetime.date.today()
