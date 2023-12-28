1dd88e59b4322b7e0283d7228719ede0768fa6d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:52:54 +0000

    breath may cinema statement machine

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
