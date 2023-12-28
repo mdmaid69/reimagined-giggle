cedf3ec4baf5fc81543480c938aa3d1c67bbf9b0
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:51:33 +0000

    embarrassing never opponent snow port

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
