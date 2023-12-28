96f1d370f714ab456c236361c156c222e301cedc
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:03:51 +0000

    stamp usually development mine risk

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
