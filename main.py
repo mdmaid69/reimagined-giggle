1f73ad699b755af2a685c80f0a2cb9b2a44f2018
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:20:41 +0000

    price save forget escape runover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+print("Hello, world!")
