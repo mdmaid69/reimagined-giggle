31c9869f214343053b65a79ef065efcd03da5f90
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:03:23 +0000

    delicate increase through recover danger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def get_bytes_from_array(array):
+        return array.tobytes()
