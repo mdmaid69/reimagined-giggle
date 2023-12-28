2aeaf3b73bb1d100afbfbce069b0e42a813c37c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:50:40 +0000

    address laugh man protest load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
