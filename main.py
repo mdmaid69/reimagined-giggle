5aec337c1bf814d5178a9b4a16cedbda35b6bcfe
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:29:30 +0000

    small mysterious far smile measure

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import random
-print(random.randint(0, 100))
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+text = "Hello, world!"
+print("Words:", len(text.split()))
