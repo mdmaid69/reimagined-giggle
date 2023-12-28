5ebbefdaf0b21f4a405e468905aa8ec11d434956
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:18:54 +0000

    neither no neither prepare meaning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def get_list_from_array(array):
+        return array.tolist()
