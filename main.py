638bcb60557bad3f29ba3c857ae79410dcd9fd4c
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:37:49 +0000

    thinkover secret fortunately command fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def clear_array(array):
+        array *= 0
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
