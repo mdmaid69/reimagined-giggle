aea398a123d27d2c3fe4853b3b89a94c3057699f
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:04:48 +0000

    ordinary demand way whether pretty

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
