0ad36cbe6e44e6c9476c18cae0036d6d714507e5
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:37:35 +0000

    party progress reserve good left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_current_working_directory():
+        return os.getcwd()
