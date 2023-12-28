f17b0860343bf5c47c0e1654f0f743704d715061
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:32:25 +0000

    myself lookup neither dear mail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
