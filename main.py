14f24e48d4697a4ac0ee74ead0bf970e0c95ccf4
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:13:19 +0000

    ourselves favour modern type easy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import getpass
+def get_username():
+        return getpass.getuser()
