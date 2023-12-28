ded8d67c91d8750f6f83577828ee6c0b2a343e90
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:58:55 +0000

    at ours material genuine start

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_as_list(array):
+        return list(array)
