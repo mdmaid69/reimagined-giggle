41733c01fde3e6c5e5200efbb6b98d6ddba9373f
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:16:47 +0000

    conclusion switch border future march

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
