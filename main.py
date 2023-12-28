0cddfecf4f7daacfab4facdf865f646ce819ba18
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:38:26 +0000

    available like yet ability congratulate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
