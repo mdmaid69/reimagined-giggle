e936ff5065cc98fe95370c3fc20927ea36768e9a
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:56:11 +0000

    play display interfere point pollution

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
