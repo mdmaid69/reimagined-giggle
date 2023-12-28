9f5ec5bc22af132c5e1a1478ca66ce147d6a4e3d
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:11:53 +0000

    interested tidy section geton name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
