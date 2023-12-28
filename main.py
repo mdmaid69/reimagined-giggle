93b6cba9004621db82820634fb31be8a198bf03b
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:35:47 +0000

    Tt trial level measure wire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
