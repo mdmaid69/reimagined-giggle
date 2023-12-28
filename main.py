d2c78ac5c63aec78e5606c35978d0c6d8eb56501
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:00:11 +0000

    guide bag quality failure convincing

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_floor(x):
+        return math.floor(x)
