bd9a1492a171d9454f3f544c6043cf3c69fc24ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:18:09 +0000

    respect fun quantity touch way

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
