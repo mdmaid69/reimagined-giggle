2cd498528a7deee6e81796633f7e837c2a885f97
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:52:42 +0000

    image runover goaway several rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
