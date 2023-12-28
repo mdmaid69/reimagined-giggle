5201453b3e475bd22d6f3a76259dd3116fa1aa20
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:19:22 +0000

    sail straight care makeinto name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
