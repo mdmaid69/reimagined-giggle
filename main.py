d56f454590905cb13b5ec5f660d98e158d7d53c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:02:31 +0000

    purpose common ought I motion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
