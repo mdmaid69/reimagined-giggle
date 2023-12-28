ccae70b4e6364c66c66ec229f930d2baf862a4c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:55:33 +0000

    we instead art few everyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import datetime
+print(datetime.datetime.now())
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
