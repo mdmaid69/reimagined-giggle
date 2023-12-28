4a949bfee6e5e5e340a67fb0f9e62cba51ac8883
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:47:45 +0000

    sense content live base perfect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
