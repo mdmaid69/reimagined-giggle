f5b4d747ba82e2febb35df20cfc72035ff55c9a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:10:30 +0000

    shut square gift murder quiet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
