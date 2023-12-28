266623685a491d15285406a94b3b334372a7150b
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:19:05 +0000

    scene consider imagination value comfortable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
