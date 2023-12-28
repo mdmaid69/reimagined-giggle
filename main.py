c6b193fefb276dcd046338c341e6063a19b6baaa
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:29:52 +0000

    check false rule advantage joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
