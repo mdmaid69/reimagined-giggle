ca97899b52c701c8db5dd5f48a926d05481e4d2e
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:15:34 +0000

    perform pair character advice become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
