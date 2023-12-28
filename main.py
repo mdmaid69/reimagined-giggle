d175eb0d9a1edf09115299f0e8af70b118abb813
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:51:17 +0000

    benefit say help crime weekend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
