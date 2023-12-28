c4c19fdb60ff474e75a682926c1f77c4dc18b13a
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:21:56 +0000

    stamp mouth runout worry speed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def reverse_list(lst):
+        return lst[::-1]
