51e9de2e7d315cb6fc2c01ea29cf889227f89e96
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:50:11 +0000

    zero damage instrument herself go

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def sort_list(lst):
-        return sorted(lst)
+import random
+print(random.randint(0, 100))
+def calculate_pressure(force, area):
+        return force / area
