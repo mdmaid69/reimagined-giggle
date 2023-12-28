479764d142e2196ca4ab3512e8679330a6a95ff7
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:04:02 +0000

    rescue suck tap trip affect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
