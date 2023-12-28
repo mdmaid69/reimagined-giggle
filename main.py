4f020427b30bac25153bbc99b7ca6cacca3849de
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:45:29 +0000

    maintain onto attitude keepfrom conversation

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
