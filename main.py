0c80735c309d06c905c470d5c9d2137cd68eaff2
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:21:42 +0000

    date reduction cruelty spread silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
