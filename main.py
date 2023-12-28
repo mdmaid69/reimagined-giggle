284eefdddfafe8f03c2a39165ffd7cac0a77b305
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:00:49 +0000

    sound must takeaway frame wise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_list(array):
+        return list(array)
