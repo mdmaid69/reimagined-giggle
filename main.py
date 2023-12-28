690577c236c2a7c054c2f319a3770fdb9b5b1562
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:21:25 +0000

    our acceptable wonder block exhibition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_octal(n):
-        return oct(n)
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
