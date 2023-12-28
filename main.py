4c94c12b213b537f18e141653400fa1abbd2a8e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:01:03 +0000

    prevent Qq hit runaway brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
