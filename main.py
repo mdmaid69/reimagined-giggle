14f57bf4b47703377fd96cd7bf7ffa5e895530d5
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:52:20 +0000

    maybe freedom free violent laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
