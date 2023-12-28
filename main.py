eb820e37fec3fded285da9e92c5944549c6d2946
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:52:56 +0000

    ought up scrap keepaway care

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def convert_to_hex(n):
+        return hex(n)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
