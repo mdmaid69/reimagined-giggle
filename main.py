65810da72724dcd72886f1cbcad73d7f30a88840
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:11:45 +0000

    show grand load sticky comeround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
