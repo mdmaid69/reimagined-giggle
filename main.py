96e98cad1e282668d53fdedfe749d816fa68492f
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:50:03 +0000

    allow authority vote among argue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
