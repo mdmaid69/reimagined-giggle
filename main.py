0cec14819c7ad6f884afb3aac3e30f964be6b0ec
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:24:54 +0000

    probably exit goaway quickly flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import collections
+def create_priority_queue():
+        return collections.deque()
