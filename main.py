10183f12110c71f8f8964ab88e13f12d7ecac3ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:53:35 +0000

    history compare use cut check

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
