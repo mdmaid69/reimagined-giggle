a0cbed60c1b92897c9734ce2273d29705be55dd7
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:54:27 +0000

    mention prevent brown Yy thank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+print(re.match("h.*o", "hello world"))
