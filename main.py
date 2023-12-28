4ee86b27c5ea8688c24ff3d460990e59d3df22c8
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:31:26 +0000

    into weather sentence thumb father

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
