3a22982911dce63fa93c5b10c297c0da48f7e8b6
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:10:35 +0000

    afterwards region winter practical last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def convert_to_binary(n):
+        return bin(n)
