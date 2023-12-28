80529f135be5c6a2519e4d8e0b5669af3113661a
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:46:15 +0000

    cough fear punish top empty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import os
+print(os.getcwd())
