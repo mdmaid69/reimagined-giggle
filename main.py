4928259e5f12d3a88ec004845999afb63c488276
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:28:32 +0000

    model length ashamed square previous

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
