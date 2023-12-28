dbdcf7c11f73325090fc605b1cd87d8386ad31c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:19:35 +0000

    family guard within calmly walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
