324ed426d1881869fc956444356f286f366dfec9
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:36:01 +0000

    put teach active rare meaning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
