9511efc9a8b3835e6082872f08ddf33ce56fd84d
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:26:55 +0000

    hole effort who lot brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
