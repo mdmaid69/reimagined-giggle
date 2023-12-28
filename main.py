13199e096e89987c697d4b8210dbaa777d50f155
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:31:46 +0000

    child book brave load neat

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
