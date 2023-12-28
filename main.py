655ca5da62061530baa6a451f02069aa4fbae297
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:16:05 +0000

    father 567enemy break include can

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+for i in range(5):
+        print(i)
