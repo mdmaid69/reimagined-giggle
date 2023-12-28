a305ffcba1e73749ee576103c39b767a6b2821ef
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:33:37 +0000

    pronunciation secret best bar copy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import tensorflow as tf
+print(tf.__version__)
