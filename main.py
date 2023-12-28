b8927fc5c6cf78dc308042e35547d698f1135963
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:03:27 +0000

    appear goaway create attention meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-def add_numbers(a, b):
-        return a + b
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
