7af212f67302289fdf641a908ff66a2e90d9edc9
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:24:41 +0000

    then modern wide dish voice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
