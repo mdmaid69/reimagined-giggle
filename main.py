d33a268b3c306237a27cad2d09b1423d648e3966
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:50:58 +0000

    side 569enjoy mark strike part

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
