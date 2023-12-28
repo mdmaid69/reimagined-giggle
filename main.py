84da180cf3d9870118af66a0ab434c8e0130f0b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:04:46 +0000

    used light deserve runaway quiet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def create_counter():
-        return collections.Counter()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
