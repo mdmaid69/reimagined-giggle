c4c1811358ba604d9bc9846a77cc302cafc22cfc
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:57:46 +0000

    attempt cheque division content surprise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
