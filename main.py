36681484be66a7978ef317072135f63b90cf3e1b
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:24:39 +0000

    party part embarrassed platform friendly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
