676c58ccfb247b75ab92663f5931e39553098ac2
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:27:57 +0000

    class actually tape stand preparation

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
