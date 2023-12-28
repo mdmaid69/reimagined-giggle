5114aab2016ddec2a4eb437081ae4453b14ac092
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:02:24 +0000

    eager him fence store taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
