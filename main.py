d51ceb388b5e0c58f241dc1d645c6e915aedc774
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:41:19 +0000

    good comeover hold city fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def change_working_directory(path):
+        os.chdir(path)
