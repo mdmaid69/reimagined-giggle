6ce2ee44b827b796aa4945717202584642ce9781
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:57:29 +0000

    out opposite profit until add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  def add_numbers(x, y):
+        return x + y
