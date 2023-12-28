8a5de7cddaf8fa429dfa357ead9c325a4c9fe309
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:22:52 +0000

    shake track because smell material

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def cube_number(x):
+        return x**3
