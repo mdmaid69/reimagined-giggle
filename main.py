1539649395aba1a2155ebce36ca3bee5be6b90bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:48:07 +0000

    typical outside will from male

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
