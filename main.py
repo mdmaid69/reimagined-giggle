f47b34956178e148b8a646e3bf53734fbe282942
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:17:18 +0000

    swallow mind black mother simple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def count_elements(lst):
-        return len(lst)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
