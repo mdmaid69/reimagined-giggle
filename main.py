7120493c15e748230f40fd52033e7fd41bc43c5b
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:29:30 +0000

    keepup fortunately rough square comeover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
