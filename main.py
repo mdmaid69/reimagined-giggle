88256325e9d3f49a99f391b08e916f99c6b5f155
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:46:26 +0000

    defeat somewhere deaf nice harm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
