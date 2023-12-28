ed95b848e91098dfdad6f79e733addf9a998a8be
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:03:11 +0000

    forward definite leg joke loose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_octal(n):
+        return oct(n)
