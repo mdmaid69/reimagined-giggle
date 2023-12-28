3ca79b4df6b473fc26c075b52ce7f8c81682fd44
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:12:42 +0000

    correct interrupt effect interesting false

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
