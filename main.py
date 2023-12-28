271889d75b810551ea06c0229aaeefc940a7bba1
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:08:04 +0000

    luck mass sink walk best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_binary(n):
+        return bin(n)
