af402a29fc6eba7529d13516bccf47cb8d80c091
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:43:03 +0000

    568energy spoil ignore suffer whoever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
