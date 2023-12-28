7d0910b5b78a4eea04e1c56f31379b03c535b816
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:55:01 +0000

    calmly grand stone little bringforward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
