f744449f58108f24efb9b051a8abef631742c77a
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:20:59 +0000

    apply deserve dinner shade development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
