d70d4b2d9ae51beed4d23c465e09bcecaab9d8bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:32:52 +0000

    prove where busy consult disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
