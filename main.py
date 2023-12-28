eb63516511c74045f67292bcc7e9da332a31fc95
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:29:17 +0000

    weigh follow rise proper people

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
