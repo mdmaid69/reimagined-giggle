7d07417894de943ce81f41558ca25d3f67b5cf0b
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:01:48 +0000

    path lend sign sister pain

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def append_to_array(array, item):
+        array.append(item)
