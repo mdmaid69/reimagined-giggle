fed3df94302de94c454b0c607d8db41816f51175
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:00:36 +0000

    because negative minimum tired club

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import sys
-def print_python_version():
-        return sys.version
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
