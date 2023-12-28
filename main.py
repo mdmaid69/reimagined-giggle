001c9a1c07c8fe529d0859e0e8810014ae9c7757
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:54:43 +0000

    kind few delicate between fill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
