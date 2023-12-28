8beba88e6e9ab14d5674be918ac071650421b505
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:05:59 +0000

    edge temper loose tight getout

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
