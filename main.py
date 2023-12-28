516045b5a9e3f44ef54981963f9d57e2afa4b06b
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:50:17 +0000

    show ignore start ilovedicks innocent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import os
+print(os.getcwd())
