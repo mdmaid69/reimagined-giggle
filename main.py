5bc35dab1896e8a6d4a4c78c831f36e3a0a96823
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:03:23 +0000

    strange detail suit discuss back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
