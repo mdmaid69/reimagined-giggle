9878bc68d316bf3af35d42589e0a37668594fd34
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:23:39 +0000

    there shade content tape voice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
