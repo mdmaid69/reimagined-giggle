049adf4709fd8891a904ed73f0e3dd9439f0530f
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:31:17 +0000

    equal miss force and impressive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
