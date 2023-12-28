e986da1462bec6d9a8becdc6c293557dd4328577
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:02:29 +0000

    public time success seriously once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def add_numbers(x, y):
-        return x + y
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
