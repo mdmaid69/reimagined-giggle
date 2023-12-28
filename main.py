d3e91e9c75d1f21e0a5047970d076864f1e76a3f
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:40:35 +0000

    occur repair trick brief past

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def add_numbers(a, b):
+        return a + b
