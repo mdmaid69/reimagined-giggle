2eda59df0f19220b83bc9a8d2dc7c59cb527a6bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:20:24 +0000

    murder cry concentrate into detail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+text = "Hello, world!"
+print("Characters:", len(text))
