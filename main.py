6b903b55b2387d694c3e5d3093682874b25f5eff
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:36:16 +0000

    interesting please book stir brief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def delete_file(file_name):
+        os.remove(file_name)
+import collections
+def create_user_dict():
+        return collections.UserDict()
