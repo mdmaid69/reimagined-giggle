75259b9831ec04dbb116b33e377fc388fef73331
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:48:15 +0000

    later transport appearance keep afraid

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
