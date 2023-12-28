56254429ab462175873be220ac45fb0f9d584a81
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:56:37 +0000

    line legal cause help back

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def add_numbers(x, y):
+        return x + y
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
