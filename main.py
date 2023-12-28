e0707a0c2b23d112c151f5bebfd71c123aafd067
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:58:00 +0000

    close chin threaten burst hospital

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print(sum(range(10)))
