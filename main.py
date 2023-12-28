ff41dd187370b769e5bf278751251473c10dbe39
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:59:06 +0000

    goaway late you together condition

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def find_unique_words(sentence):
+        return set(sentence.split())
