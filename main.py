6d7152d45908bf2f64254d3e473dc6e762fa4ddd
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:44:01 +0000

    rush theirs spring weigh tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import time
+print(time.time())
+x = 10
+y = 20
+print("Sum:", x + y)
