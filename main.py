0d4fec862a1ec65658d8a5e4b004f4007999dab9
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:37:50 +0000

    possibly confidence serve calm deaf

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def convert_to_octal(n):
-        return oct(n)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
