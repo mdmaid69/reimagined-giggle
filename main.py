70695e388a92ceeccc02e2ecbce61140cb979207
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:02:48 +0000

    marry claim talkback positive distant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
