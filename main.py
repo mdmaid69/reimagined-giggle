dbe40fe459744d300bb8751fa515d9c78ab8d5b7
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:05:32 +0000

    dick breakin steer thorough treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
