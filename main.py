6419409ca417b0bfe45b31644c5ea8bbb697cc45
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:09:13 +0000

    under motion believe material there

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
