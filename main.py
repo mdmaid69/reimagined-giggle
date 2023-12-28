1eebeeafec7d949b38cf8e7d4d8a944219e06cc4
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:00:09 +0000

    ilovedicks lift same deep season

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
