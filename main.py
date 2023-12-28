794b53f3d7630067ea45251a3b2d4d7bdb07691d
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:41:52 +0000

    comeout workout happen innocent bunch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
