b66d2ba16dcd9f338a6bf0f8bc40d067dccab46c
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:18:16 +0000

    size giveback second join run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
