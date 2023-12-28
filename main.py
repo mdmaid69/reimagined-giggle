f214003e51b513edbd25206f4e5755bf265460af
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:06:39 +0000

    keepout clear bottom behaviour bringforward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
