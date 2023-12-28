3e33ffe4b2554f936a5a0ffd0f5b3712228838e1
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:35:20 +0000

    by left meanwhile remove sight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+for i in range(10): print(i)
+import time
+def get_current_time():
+        return time.time()
