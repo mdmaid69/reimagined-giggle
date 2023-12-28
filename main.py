164259ce563f0fcec6d1315b9fc6044e9402c0a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:50:50 +0000

    scream takeout combination uponon ear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import sys
+def exit_program():
+        sys.exit()
