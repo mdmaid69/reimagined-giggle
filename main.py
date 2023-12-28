f2649cae1788a12d9afe6f4a20ecd984891debe3
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:09:32 +0000

    workout band illness ear dig

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
