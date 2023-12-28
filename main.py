30711852d55fc41dbc1cee67cd6eb33b4f861537
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:24:36 +0000

    mind petrol free pick teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
