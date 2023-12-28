0a0d7b4289023ea4dd307b946ad1286fdd42c2d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:26:31 +0000

    dark clear father declaration coat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
