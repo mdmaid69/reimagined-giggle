baf6e7ef4a5bf850bef94c265965bd3a6e69747c
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:22:34 +0000

    little clear interest fact extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def split_path(path):
-        return os.path.split(path)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
