1921bd872e3792f4b1cb8a28861a59560e364f61
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:53:51 +0000

    production jump favour length whole

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def get_array_typecode(array):
+        return array.typecode
