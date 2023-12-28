aeaa3ab1de493e364c08eaefbbb80fe147ce701d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:00:04 +0000

    guess familiar thinkthrough Cc argue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
