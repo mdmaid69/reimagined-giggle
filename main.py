03b8b94b879f3dd5d8f6693a711274575cf5cbdd
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:53:25 +0000

    wander thinkthrough push mind either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
