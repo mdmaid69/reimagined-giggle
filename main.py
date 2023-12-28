d0a83bfff292f1f889a1b325a14619c53f330708
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:43:34 +0000

    down getup getout song shut

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
