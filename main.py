6d9b185fb74a39c3b546d0f7ce84755770ad50bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:06:21 +0000

    smell bit crisis across come

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
