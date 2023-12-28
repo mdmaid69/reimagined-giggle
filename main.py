8e2155dfe6f71c3c9f27f1fc79c827e06ce03668
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:47:50 +0000

    practical how exhibition cruelty passage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import sys
+def exit_program():
+        sys.exit()
+def calculate_density(mass, volume):
+        return mass / volume
