9c37fe67d5a679410a928e90108c26f750b3ba87
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:34:54 +0000

    weak know cost attention house

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
