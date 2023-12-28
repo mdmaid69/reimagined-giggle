d7e7bdc7733cc3e0236ddbb9b57ccc36270a3068
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:25:11 +0000

    may simple education season rob

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-def reverse_string(s):
-        return s[::-1]
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
