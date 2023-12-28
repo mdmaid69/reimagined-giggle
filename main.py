a9d975ba027418735fb3c48e05bd334b768ecd92
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:14:23 +0000

    workout shoulder destroy respect anywayanyhow

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
