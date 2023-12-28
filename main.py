e48377008dd16cda6e73a276abad8661bc15cdc2
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:12:46 +0000

    ought main reasonable please black

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
