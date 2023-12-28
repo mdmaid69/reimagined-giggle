122422eff46d9850205c1cc60e245ccfd30c318c
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:02:09 +0000

    explain play angry take unusual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import sys
-print(sys.version)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
