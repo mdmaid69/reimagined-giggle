428c81fb4d252d967d6188c091979b9e47c7a80a
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:34:08 +0000

    reasonable tongue thinkthrough sister autumn

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
