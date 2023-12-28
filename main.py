8b3489c849681cffe52cb16abb14530ad2fbf36b
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:23:45 +0000

    case confusing father allow open

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def divide_numbers(x, y):
+        return x / y
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
