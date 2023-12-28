e35b93491a80e4dfbb52069ff54d5efe3ef1daa7
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:59:46 +0000

    split refuse although anywayanyhow language

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
