8a42f9373f92445e72e0366d8fdd3cbe08aa3cea
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:49:43 +0000

    relief capital dream single floor

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+print([x**2 for x in range(10)])
