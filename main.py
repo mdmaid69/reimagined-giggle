6bfa33f80c60eb35c717446c40c5abd682b54ce7
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:25:30 +0000

    else very thought mixture inform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def reverse_array(array):
+        array.reverse()
