c9c40cbe30966a15523f1717013ced01f1e7038b
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:11:29 +0000

    understand contrast temper unfortunate nowhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_working_directory():
+        return os.getcwd()
