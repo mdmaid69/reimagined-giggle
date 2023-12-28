aac7707c11f596084bdd82a1411d740419192c2e
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:53:05 +0000

    argument comeup callout rush 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_length(array):
+        return len(array)
