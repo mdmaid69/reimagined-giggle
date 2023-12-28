4216f1aec1ddfbbe96991d0c3ed83a9dd21fd2af
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:37:13 +0000

    practical maximum both wave anywhere

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
