fd8ac4a3d0ce27bc49d25559b08c8052a39e9c7d
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:42:42 +0000

    study guess dish workout so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_min(numbers):
+        return min(numbers)
