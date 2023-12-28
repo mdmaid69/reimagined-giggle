10e8edac5fc1757255ffad801f92da9f6d2d2d14
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:37:32 +0000

    loan similar whom shock ring

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
