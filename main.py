9c70ceeea7c379190c6e9cfbfa4777b31b05134d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:29:04 +0000

    public force dry copy line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
