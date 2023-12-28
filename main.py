071c2afe3f1fed5152a420d06258ade5097c5eed
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:28:20 +0000

    behave beneath minor please false

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
