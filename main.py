cda2e2afca381f43dd9117ab638453a3bf937b96
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:47:53 +0000

    burn room breathe call crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
