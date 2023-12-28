a841154e5ea1958dcc7d63776ce432e154fb3580
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:03:28 +0000

    downstairs fast closed vanish impressive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
