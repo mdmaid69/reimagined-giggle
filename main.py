7c37ac267d3e366cd8cdb9cea97bd3c26d60bd23
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:04:08 +0000

    plain meanwhile born mark blind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import platform
+def get_os_info():
+        return platform.uname()
