531172b16492f46d19c9079fece8c2f78f0936cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:57:44 +0000

    morning offend shoot runover look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
