da2ee289b8de1dd6ca76d4b551b6ccdbda504cd7
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:12:38 +0000

    thirsty offer agreement spray acceptable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import os
-def change_working_directory(path):
-        os.chdir(path)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
