94a103e5b9cb1a7fadba6fd78228ee840db6156d
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:34:51 +0000

    treat ill can calmly influence

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
