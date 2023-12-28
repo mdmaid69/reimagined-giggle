fcebf3962b1cd0a282a919efaf07dc9a7823b6f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:27:41 +0000

    should sign various oil perform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
