32842451496f4bb9d4e6a6de515f2a5d0394c8a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:57:37 +0000

    real false warm scared delight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
