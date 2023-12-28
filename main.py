7dd97b67fbc32d2f774db818c7990788ad353b88
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:09:12 +0000

    since pity bottom field guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
