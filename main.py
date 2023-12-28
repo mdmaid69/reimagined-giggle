da1a8641cdf15429536f0e0b6a228b0a08f655fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:18:54 +0000

    impression steep refer me unusual

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
