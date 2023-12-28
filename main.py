e71cc958bbc404762b1f51686a5b493d39a59a78
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:40:33 +0000

    copy dinner jump burst gift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
