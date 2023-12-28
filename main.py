fc77f364fde9980c5df27f50d68ac3f3f9d702ff
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:00:30 +0000

    dead adjective offend nigga eat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def greet(name):
-        print(f"Hello, {name}!")
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
