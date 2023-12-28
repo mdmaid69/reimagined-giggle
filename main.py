e43f5357495965dcbe7d65af646fa02dc85580c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:59:07 +0000

    uniform narrow theirs room frequent

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
