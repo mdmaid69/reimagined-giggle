38e621b7eba930302d691705fd7d44c3799f56ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:49:31 +0000

    fast spin blame smile rate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
