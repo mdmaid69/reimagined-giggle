32788b558294791d767afe0f025dcd3411cdfee6
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:49:34 +0000

    freedom throw balance complete simple

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
