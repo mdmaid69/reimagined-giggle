3be7226b10ae9b1c8ce983e0806c07e8d6504cc5
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:30:44 +0000

    majority strange fetch report charge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
