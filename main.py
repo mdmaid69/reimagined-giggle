e1cadceee3618b49c25092763e97b79fad8b7ace
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:07:35 +0000

    bend else case calm absolutely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
