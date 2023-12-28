9dc89247e3e97de63cd16190c23882119810f0bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:30:50 +0000

    children ours valuable giveout pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
