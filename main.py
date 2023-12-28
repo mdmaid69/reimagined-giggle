f4eaad0aebe4503752fd8198718dbbb2b809aabb
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:49:46 +0000

    because what expression appeal neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def remove_duplicates(lst):
+        return list(set(lst))
