522fa48f11be510458a522c7516a3077815d2699
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:35:57 +0000

    bringback translate murder back affect

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
