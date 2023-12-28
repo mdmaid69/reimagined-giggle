c505d9311e7e5f9679bd35b199f323689de9962d
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:50:48 +0000

    quickly turnback government communication battery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
