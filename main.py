0cc725ee8a6e5c0a1bf6c9781714353f0be2eed8
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:32:09 +0000

    warm cry next exciting real

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
