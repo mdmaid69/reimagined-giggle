42b478e5fa8ffe689e2b92151651af919661bf62
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:00:21 +0000

    protest decision content plenty so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
