c26b493ddba6e76fa3b6333c3f47fdc6329502ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:23:43 +0000

    combine deserve development acceptable right

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
