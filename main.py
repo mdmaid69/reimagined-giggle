6550691e653d15c6b8e9d2102dbd217f8558bc11
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:04:05 +0000

    difficult aim chin think back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
