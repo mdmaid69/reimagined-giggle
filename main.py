618009318720150b8c9094bfcba3bad24365a22d
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:26:19 +0000

    even condition include earn float

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
