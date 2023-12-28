36ea651ef330c70ef3d901dc96b90dcdc15b2373
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:19:06 +0000

    rent ours hurt worth win

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
