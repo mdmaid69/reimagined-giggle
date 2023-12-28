64cd2f9d7e7764709875cae8b4932a7ae6bf30f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:50:00 +0000

    communication sport successful heavy pipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
