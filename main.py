00bb9ce535b81d805bf657e04c0b52ec6d82c88e
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:23:28 +0000

    extra cost scheme phone hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
