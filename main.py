b83e97fad71aa189c9ade864e435e9a9828d59d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:38:37 +0000

    ear manage kind turn what

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
