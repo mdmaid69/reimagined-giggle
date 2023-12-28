da571e20ad77a77198bb66fae8383abc6a04295f
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:48:07 +0000

    turnback suppose further future protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
