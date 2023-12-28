f616419956c01e8697af4e9c1630029462081380
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:34:14 +0000

    comeup gap uncertain shape put

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def multiply_numbers(x, y):
-        return x * y
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
