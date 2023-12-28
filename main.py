9545ca5598c235668188a52acc4a287b7721cf88
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:04:46 +0000

    sweep pen increase information responsibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
