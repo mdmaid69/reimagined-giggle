e24681ab8d518ddd0fc7d0048b32d9ba605e1fdd
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:14:02 +0000

    official relax blind autumn exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
