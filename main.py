2b5319acd93243019edcd61e767fc87607939872
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:10:22 +0000

    wide claim contact science possession

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
