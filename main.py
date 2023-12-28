f450e23d6cea071c903de54812fcb8a2a142c44d
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:21:02 +0000

    low pen old explanation comedown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
