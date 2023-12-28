69d3dacd11b1bded5940e6ba8ddca5d521f69ce7
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:28:05 +0000

    use sky emphasize begin secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_factorial(n):
+        return math.factorial(n)
+import numpy as np
+print(np.array([1, 2, 3]))
