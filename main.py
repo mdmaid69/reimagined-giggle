af76c73c54c60350ec915065b4846dab1ed27877
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:47:06 +0000

    hear set should little future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
