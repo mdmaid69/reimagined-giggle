8e7d4b1440f06606135676f48ed274209c731565
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:58:17 +0000

    pile smart set hot attention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import time
+def get_time_since_epoch():
+        return time.time()
