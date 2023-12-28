b627d9846b95e43ef741ed22a4f79379f9ea8d81
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:26:03 +0000

    declare can one maximum hot

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print([x**2 for x in range(10)])
+def is_even(n):
+        return n % 2 == 0
