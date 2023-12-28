6e9a0f7b59179be427f6ddb81dd02271ee0b5110
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:26:04 +0000

    pick act guess tap late

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
