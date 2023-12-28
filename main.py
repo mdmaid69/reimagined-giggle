2bfb7918945963d9f74de7aebfddaf1543ab9966
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:07:27 +0000

    difference plant rain worth opposite

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
