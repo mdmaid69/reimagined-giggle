5bdf9ae50048ab6191bbddadb7684e029206cd9b
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:01:52 +0000

    lost arrangement convincing next sweet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
