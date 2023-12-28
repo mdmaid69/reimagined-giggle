4146f31dd75150da0d1cef23e81aa2d08959cf18
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:29:05 +0000

    strike present at seriously anyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
