0b52996834f492362d00917f4807e548190858dc
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:19:46 +0000

    holdon thought hello last praise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
