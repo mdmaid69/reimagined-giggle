7c099b817dc1aa942dce79e6f7f4db1ac06297ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:57:28 +0000

    Itself rush thank remain excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
