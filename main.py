669fb92ea1ee6502c97e5410d84e6dfa0095e9d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:18:16 +0000

    scale view ability relation pretty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import collections
-def create_user_string():
-        return collections.UserString()
+def remove_directory(path):
+        os.rmdir(path)
