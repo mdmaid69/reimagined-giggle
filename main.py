bb707fc0c5021892fb6156295106c31ce1d1a013
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:54:49 +0000

    sad bath deserve famous Yy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
