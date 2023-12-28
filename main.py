a8284b700bbefc659452b93919374bf795360849
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:10:04 +0000

    measure flight blind name language

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
