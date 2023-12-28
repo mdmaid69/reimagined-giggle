311f3077cfbea380ec35350ee0c8535a29520302
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:08:04 +0000

    pain Qq appeal trust special

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sys
+  def get_python_version():
+        return sys.version
