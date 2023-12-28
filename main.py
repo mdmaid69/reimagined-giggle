722b1d18107befe300af0fb6d24f9890dcaa69eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:17:36 +0000

    reply recommend watch door introduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
