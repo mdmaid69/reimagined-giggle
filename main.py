5c1f6e66bdca426ec05038151673e875cb63b0c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:27:21 +0000

    convincing crime fortunate material build

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+print(sum(range(10)))
+import getpass
+def get_username():
+        return getpass.getuser()
