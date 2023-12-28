7524eb7753a1af2ec19600f454a032184582aec6
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:00:37 +0000

    some accidentally reason dream related

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def append_to_array(array, item):
+        array.append(item)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
