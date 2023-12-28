da86858ae7b55e69478f75ccc540ea57a75100b7
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:18:36 +0000

    ride get then hide lookafter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+name = "Python"
+print("Hello,", name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
