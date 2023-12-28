9847ab7fce044aa75af5cb4f742ad101e877568d
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:44:14 +0000

    grade spin clearly pressure general

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
