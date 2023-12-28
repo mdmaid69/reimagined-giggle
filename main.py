bc05e6d4eca108c64ef420c7a0341248014a6976
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:50:33 +0000

    guilty Ss free snow profit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_directory_name(path):
+        return os.path.dirname(path)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_floor(x):
+        return math.floor(x)
