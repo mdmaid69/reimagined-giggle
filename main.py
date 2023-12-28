5cfae0559fe0b711f6746a202b70d3b366f957eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:41:24 +0000

    cheek slowly why light bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
