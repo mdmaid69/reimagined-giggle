5f3499097ce272edabf98e3b186731e253988fc2
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:01:16 +0000

    temperature conduct opportunity result paper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
