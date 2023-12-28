310a0f68819e8867b229bd12abdd5a29b9ce6316
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:56:35 +0000

    cover who look branch issue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
