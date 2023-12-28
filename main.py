e1602b969629bcb3c5170cc9bce9473b031d3789
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:00:52 +0000

    point finally century Rr no

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
