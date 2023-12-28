752647db3c5d4525d5f3663bf236244f3c6abc1a
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:54:54 +0000

    badly who half indeed thinkof

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
