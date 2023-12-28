b6fcf11bee787d1bfcafdc44198e385b7f2ed2bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:08:28 +0000

    board final tooth fair than

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+i = 0
+while i < 5:
+        print(i)
+        i += 1
