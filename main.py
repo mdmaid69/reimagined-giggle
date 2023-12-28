e463ac6315e4be1ac6ae2451f0ba1bfc35f8b530
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:51:19 +0000

    protection level onto feed record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
