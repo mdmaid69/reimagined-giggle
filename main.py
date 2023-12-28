b0c1af453fcba621b3f0bdce5665246598b35577
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:24:36 +0000

    post interfere travel announce I

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+print("Square numbers:", [x**2 for x in range(n)])
