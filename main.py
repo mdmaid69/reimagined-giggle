6ef177985712412d813bdf652a0441835b0d5c6b
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:55:07 +0000

    stone border free interview form

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def is_odd(n):
+        return n % 2 != 0
