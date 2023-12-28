8e3bf36059a4d993d6c5f62667410d1a1e6381e3
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:33:18 +0000

    connected display extra block buy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+name = "Python"
+print("Hello,", name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
