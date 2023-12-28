142252344ff6dd9d03483e238d5c9a1656bda131
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:43:38 +0000

    strict lonely funny pride confusing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
