473fed71e86245935c0687fa2b5b574dad7482fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:56:02 +0000

    bend bag man mess little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
