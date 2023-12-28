71d1d45ea4eb85850f316ba7255a8c6c07d67e87
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:29:04 +0000

    later improve union possible claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_speed(distance, time):
+        return distance / time
