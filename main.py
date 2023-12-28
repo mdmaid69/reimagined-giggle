5627b11df2521d9acafe3c96365e63fd33690465
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:09:28 +0000

    traffic usual area defeat shut

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import time
-def get_current_time():
-        return time.time()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
