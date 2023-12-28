ab5e3c880357e4933817867d9e9f24bb6d15e352
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:41:12 +0000

    past value make discovery passage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import logging
-def log_message(message):
-        logging.info(message)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
