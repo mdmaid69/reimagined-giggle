b826a17472a781eb71b7b2256fee8948300b5be9
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:29:02 +0000

    ours apart considering bush major

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
