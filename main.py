2b4a49b07fab19692273a0c6d088ec710b3d2fcc
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:12:48 +0000

    interfere ill blame course few

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_union(list1, list2):
+        return set(list1) | set(list2)
