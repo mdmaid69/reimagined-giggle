b2999ddcefa541261edadd23fa3e2eaebb39d8cb
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:14:45 +0000

    inform open settle inform even

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
