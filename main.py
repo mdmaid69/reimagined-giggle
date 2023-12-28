e5408278f8fdbb95671568678a56b324ebe1090a
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:26:02 +0000

    past seat knock hollow hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
