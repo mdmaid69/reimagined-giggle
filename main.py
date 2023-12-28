1bfda37b8828f860d36c1eed231e55992ff5e74f
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:20:04 +0000

    friend cap usual bit silent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
