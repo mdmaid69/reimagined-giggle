3bd22f9ca3347170fdd526ef739e84b3e506e432
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:32:09 +0000

    indeed home somewhere they sentence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
