3d4746cbde4b22ae347366be42fe9e2bd6255057
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:29:48 +0000

    believe usually hardly Uu display

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
