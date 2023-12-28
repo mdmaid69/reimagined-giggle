8221449f5251202e02e7dd51e016c71e3d3d96d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:09:20 +0000

    thing tour crash capital terrible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_list_from_array(array):
+        return array.tolist()
