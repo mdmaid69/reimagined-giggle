213b1dcb329093bc52e76c4c1c0a9460093fcc87
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:29:05 +0000

    runaway suggest something frequent hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
