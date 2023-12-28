b419b4b35e2b59d483638b06c8647be78c1bdc0a
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:09:11 +0000

    march ground destroy comment downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
