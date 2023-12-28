de05f109e06466be87b6e61a51122308f05a1f64
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:27:01 +0000

    science 572enter fire leave fear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def sort_list(lst):
+        return sorted(lst)
