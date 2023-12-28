19c93f02f79ba0a83deafeb2c00a90081b89f704
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:33:36 +0000

    range appearance cough chin ground

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_density(mass, volume):
+        return mass / volume
