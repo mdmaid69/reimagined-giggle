7e1320f459e1094ea7d44ef62bd49fda98b2d4b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:30:50 +0000

    project shout rich happen her

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def divide_numbers(x, y):
+        return x / y
