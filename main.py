f69446b7181a379911fe9c9302760b98c6cb983d
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:11:08 +0000

    introduction task love quick discuss

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-for i in range(10): print(i)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+text = "Hello, world!"
+print("Reversed:", text[::-1])
