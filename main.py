86dbcdcb7fa2511e98c76a1f5771d789ef5c3d8d
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:05:54 +0000

    active suppose ourselves street support

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def add_numbers(a, b):
+        return a + b
