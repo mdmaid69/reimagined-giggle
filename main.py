37a44cfb93b56e8267283516bd692d6e001176c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:06:25 +0000

    side value everyone brown protect

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
