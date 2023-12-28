6ce31b33496e14a787f3b52a1d0945f86ff361cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:15:18 +0000

    weather school chin write word

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
