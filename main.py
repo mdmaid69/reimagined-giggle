69c7c99eb76001afd1957b7dace4f4291d0402a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:44:32 +0000

    suppose study union fierce bound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import datetime
-print(datetime.datetime.now())
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
