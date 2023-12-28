0f38771e4c7b414d140f81dcff85020f45e17496
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:29:42 +0000

    record borrow spin offend number

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
