f03420fe602d68b2807eb4356712088c3197c983
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:05:30 +0000

    completely total damage Ll usually

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+print(os.getcwd())
+name = "Python"
+print("Hello,", name)
