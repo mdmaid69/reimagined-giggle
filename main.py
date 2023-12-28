915421ad953753a4e5e13abe263145046c3a1f69
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:27:36 +0000

    564encourage discuss wipe trick immediately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
