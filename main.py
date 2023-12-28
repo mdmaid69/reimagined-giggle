b1ef299a846399613374316075436f19fd49620e
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:29:04 +0000

    centimetre freedom increase keepup suggestion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
