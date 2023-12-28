77cafa55ca6364f2a56834926a5079f7bc9f3aac
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:52:39 +0000

    favour good proud cost argument

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def cube_number(x):
+        return x**3
