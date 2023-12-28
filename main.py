3f6dc68bd62a11af66c83b0883fcafbd182a89b5
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:24:09 +0000

    giveout slide stir thumb situation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
