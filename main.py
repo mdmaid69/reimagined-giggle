25475e0ef9039b7bb4e2c3bd4c4693ae3b9fab4c
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:29:24 +0000

    urgent Oo spread excuse only

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_acceleration(speed, time):
+        return speed / time
