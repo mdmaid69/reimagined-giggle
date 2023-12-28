b372a8d83b8fdc93d40d60d0fbe7267579543b10
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:40:05 +0000

    result pencil large send path

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
