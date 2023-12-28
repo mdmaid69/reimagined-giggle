3aeb03567a9dd2a6e86aec76dd83ad6050a7769b
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:29:43 +0000

    stone young agreement offend damp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import tensorflow as tf
+print(tf.__version__)
