8166e0b219f29daf95851ec7a1381df80a008b0e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:17:04 +0000

    respect exploration style before except

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-print(math.pi)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
