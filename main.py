417677286fcf0b2cd6a278f92adec65a6765bc00
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:17:57 +0000

    neither realize talk answer rare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
