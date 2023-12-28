4acc37b1d257996b91a6571ef8aa5d4fe8784abf
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:03:27 +0000

    false achieve sorry always area

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
