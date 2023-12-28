98fb2167b6fecd11883e7c54da7d0351ad734e72
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:24:25 +0000

    wash not workout simple shop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
