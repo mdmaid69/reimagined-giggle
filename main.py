395774a766457ee0e56febf6822a32661a27d249
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:35:42 +0000

    advice age work judge finger

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
