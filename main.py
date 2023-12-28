799d364106f23ce567bc6dc1f4ec00d5e84b453b
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:38:40 +0000

    worth limit agreement estimate prevent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
