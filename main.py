58a5c3f070c937853f2c9341c9bb268623189c6a
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:59:48 +0000

    tradition exchange prove blind I

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
