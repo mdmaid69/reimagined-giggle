92e7035f4be7d02c1012ccadbb7dcc022817306b
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:02:53 +0000

    clue call mend dress sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
