a006168ae1fd3621d3e9c2dc6b18c0769ae098b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:23:11 +0000

    real grade eye bag fence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_min(lst):
+        return min(lst)
+import time
+def get_current_time():
+        return time.time()
