b739cd04715df008c671a16e8b54e6df4bee618e
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:22:49 +0000

    point blow belong nice amount

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def square_number(x):
-        return x**2
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
