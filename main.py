a8a072d511d251269b40a191e3c0059823d36815
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:33:55 +0000

    guess estimate promise matter survive

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
