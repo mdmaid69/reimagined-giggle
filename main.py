6475dcbdc0e798fdbc43af23f3eec93cf0c55713
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:37:23 +0000

    mind car lay answer teach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
