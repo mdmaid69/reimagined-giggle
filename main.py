8cbba587972e6f1dc3aefb75676e8220a4813d97
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:08:07 +0000

    lot determined strong manage giveback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
