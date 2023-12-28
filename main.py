fd0f415ffc0c2a49e0d28f6fb6fe0da6720b626a
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:46:06 +0000

    day disturb ride solve complicated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
