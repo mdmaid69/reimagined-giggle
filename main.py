6708314749ef8b834a6e89fad6bbacb1611b0185
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:39:15 +0000

    smell ought unusual she repeat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import collections
-def create_user_string():
-        return collections.UserString()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
