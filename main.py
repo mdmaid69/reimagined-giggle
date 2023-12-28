7a72e411e5acd709408da921b5125cbe209b90dd
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:25:36 +0000

    getoff loan payment building scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def subtract_numbers(x, y):
+        return x - y
