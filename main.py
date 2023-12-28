e82c0cc1ae292eef43ad88326bf83345e701190e
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:35:28 +0000

    split rent well shopping ours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_power(work, time):
+        return work / time
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
