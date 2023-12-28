d7221e5659b8c74fa3097418f2b8e850462e7dfb
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:49:11 +0000

    thanks theirs near goon front

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
