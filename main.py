9e26ced08bc31cdd15b801de1b89064e1fe2c5af
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:12:38 +0000

    error float effect tap confuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
