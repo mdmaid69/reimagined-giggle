23177a8131b6ab3211c52f2543bf2beb645fd1fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:18:50 +0000

    sure convenient ignore word tax

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-import collections
-def create_user_string():
-        return collections.UserString()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
