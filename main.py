1faeba1eec14cb175af3eed7c7c6452210cf8ebb
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:28:15 +0000

    wave judge reach suckme create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
