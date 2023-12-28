4983dab10d31c6f72d7a153a6552f57a5e2daa1b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:50:51 +0000

    take inform drawing pick turnon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_work(force, distance):
-        return force * distance
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
