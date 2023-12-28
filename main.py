9776d83a333d2bd95cb4178e987a281971d05a17
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:19:05 +0000

    of surprise slip compete wheel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
