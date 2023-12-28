97ec8870b1bf582ca27172f74b13a93d1920080e
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:11:58 +0000

    old there roll very secret

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def subtract_numbers(x, y):
+        return x - y
+def square_number(x):
+        return x**2
