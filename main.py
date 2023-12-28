4e7e69e145918f6b33d535cb3b6c55d5037bed27
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:40:48 +0000

    community light chase attempt stay

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_list(array):
+        return list(array)
