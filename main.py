d0c4ae27477c110ac33ec609e8abcc170b5f3261
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:24:10 +0000

    to settle achieve goup perhaps

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_pressure(force, area):
-        return force / area
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
