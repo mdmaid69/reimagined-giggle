5edcbe0f265c3af7ca2c6d6a36e05e1e6847288e
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:16:54 +0000

    towards patience in traffic several

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def subtract_numbers(x, y):
-        return x - y
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def count_elements(lst):
+        return len(lst)
