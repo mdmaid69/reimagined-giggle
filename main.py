6a3263255765fc7bbdaefa83eec8a9a21c8a414a
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:57:03 +0000

    used close father hold bank

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
