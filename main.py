e79fc997f7c4dd5aaf112490a8010eebb49d35a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:53:06 +0000

    refer maintain push belief show

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+print([x**2 for x in range(10)])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
