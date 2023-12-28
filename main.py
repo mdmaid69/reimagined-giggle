80b18b7595c7e184410e5e937bb2160572b62dd9
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:35:55 +0000

    hurt hungry crowded female match

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def find_union(list1, list2):
+        return set(list1) | set(list2)
