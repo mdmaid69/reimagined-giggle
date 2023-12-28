522afcbf4b700a0707c047a5f01c4d0db7689758
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:57:08 +0000

    climb turnon judge discover hole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def cube_number(x):
+        return x**3
