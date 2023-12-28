56ac4747a8bc825e731b256938194e6333dd34a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:34:25 +0000

    description check strength hard building

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def is_even(n):
+        return n % 2 == 0
