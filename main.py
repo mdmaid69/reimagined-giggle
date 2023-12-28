26fb4cf2d6e9caa9ad2e5bf5b48c0d07d7b8de05
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:15:14 +0000

    that waste born spin brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+print([x**2 for x in range(10)])
