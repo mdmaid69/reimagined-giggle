2a8428c5800c29db7c0a2855550f26a8bc1e4e2a
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:46:58 +0000

    write type work offence confuse

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
