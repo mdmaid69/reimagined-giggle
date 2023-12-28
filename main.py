1822501163bc3faa0211c9bad735d07bddb8c09d
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:06:09 +0000

    guard physical person eat even

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
