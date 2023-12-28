f359bbd79367a696f1ffc912d20150355a4e5649
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:54:45 +0000

    top lie succeed 566end ability

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
