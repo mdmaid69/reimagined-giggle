49602080f66195e207f6dac44b3fb6834821feb8
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:10:28 +0000

    lay tongue doubt container guilty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-for i in range(5):
-        print(i)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
