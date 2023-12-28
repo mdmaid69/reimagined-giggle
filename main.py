0e9e826766595bee67f8ed394db53740520e7858
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:50:50 +0000

    smile smell plan nothing turnoff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_area(radius):
+        return 3.14 * radius * radius
