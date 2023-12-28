7df126d539a1dcbdff82e2de30810ea3cc181f72
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:57:50 +0000

    chase radio bound task sound

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+name = "Python"
+print("Hello,", name)
