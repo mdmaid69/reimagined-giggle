58a5dad8c5dd3e25e1833d777fb84ee1388d486a
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:59:19 +0000

    give claim business pair convenient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
