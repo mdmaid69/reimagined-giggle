cec28126237c5cc00f68d0020dc27a55c71ab860
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:11:42 +0000

    deal ocean bet mood slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_even(n):
-        return n % 2 == 0
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
