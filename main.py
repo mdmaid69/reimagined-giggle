fc426c50d065c3e70581e5d1b57917021b43610b
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:54:05 +0000

    excuse cook laugh mark shadow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
