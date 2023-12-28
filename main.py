9ecf3f5c456e45df3be24dbbc05f542cc09def56
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:58:06 +0000

    born through add connected proud

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-for i in range(10): print(i)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
