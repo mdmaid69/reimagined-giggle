f6984797ec2a5e6df09ddddeb3faca694add4711
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:31:09 +0000

    568energy nearly believe wake reduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def count_elements(lst):
-        return len(lst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
