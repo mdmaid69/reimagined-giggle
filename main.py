48a90c9d12365bcecd9b69938068b2f1218aaaf5
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:27:14 +0000

    reasonable whole push watch repair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
