f6cff89bf4aaca56b6e7dac79dc9481e4b39c5fd
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:10:40 +0000

    content follow stretch pause expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_area(radius):
+        return 3.14 * radius * radius
