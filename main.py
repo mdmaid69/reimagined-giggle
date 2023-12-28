3b4ffa686cf49f4d988796bbf50e17761d1fa36c
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:35:43 +0000

    serve hair face town meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
