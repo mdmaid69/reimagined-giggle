508211636b57e5baa9833827c6b9e94af8ac64da
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:35:29 +0000

    large could skill choose hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import sys
-  def get_python_version():
-        return sys.version
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
