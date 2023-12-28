7e405bdffe6fc9b13d927686740f781b109cedae
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:15:33 +0000

    means original tone national explain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def square_number(x):
+        return x**2
