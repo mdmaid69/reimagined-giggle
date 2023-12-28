0e6a3dbc8ad4175ab192c50b543565af35beb29f
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:03:19 +0000

    smart smooth power dream promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def change_working_directory(path):
+        os.chdir(path)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
