324316fe7334846398aeea46ce5defdc3c96c880
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:39:30 +0000

    temper scream tear regard mix

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
