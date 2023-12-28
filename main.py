56415893cbe4ffb75bc11d793a63bd51e85b2283
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:27:59 +0000

    statement naturally small sight improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import array
+def get_array_as_set(array):
+        return set(array)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
