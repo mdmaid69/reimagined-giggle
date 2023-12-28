9af6663fedddb31d736494e41531b1b2f9972b75
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:15:43 +0000

    bill to play sticky force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
