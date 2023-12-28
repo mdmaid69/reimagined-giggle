1aa31c24aa6ce5031ef0963b7dce8f134682fe54
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:12:19 +0000

    soil march middle rather train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
