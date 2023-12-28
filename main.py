fbed89addf7c8a5d81ec751d02055483c482c60b
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:13:56 +0000

    whistle wave alone coffee pot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
