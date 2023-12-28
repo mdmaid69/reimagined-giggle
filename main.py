0951cc73bd68dd8c78c5f3e074a0405eb43bccdf
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:29:32 +0000

    possibility benefit every anxious enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
