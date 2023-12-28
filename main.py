ec82ca5ad715bce460be0f984753ae3ae5b61f15
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:26:50 +0000

    bend greeting scream thumb for

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
