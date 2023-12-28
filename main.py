44b85e0ed757b2f6e82fb204f79d28daae4ac798
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:28:54 +0000

    weak region certain emotion bus

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_sine(x):
+        return math.sin(x)
