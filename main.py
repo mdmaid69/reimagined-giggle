d85bb37e0d7db9c36243d461a7497dac8a670b96
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:09:26 +0000

    Gg sister transport finally advanced

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_str(array):
+        return str(array)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
