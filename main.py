8d87e0ed8c3d36a04c55ece03da84cfadb3eff9a
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:56:09 +0000

    getaway sort goodbye talk score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
