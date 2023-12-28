7013aab73f8b63787f569f3c8ca90c4bee6db0d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:25:34 +0000

    doubt fly couple collection thin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
