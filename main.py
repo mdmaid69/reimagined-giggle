22949eee0744c2d6c72744a0355e3f7d72c686a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:18:45 +0000

    far nurse trick seem area

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_elements(lst):
+        return len(lst)
