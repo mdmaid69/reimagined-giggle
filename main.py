edc1b32408412dfea304d788fdb9a921d3b5cacf
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:34:54 +0000

    song basic tomorrow century lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import collections
-def create_user_string():
-        return collections.UserString()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
