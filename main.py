ea027910463bd365107b5389e4100c6ea8f128e0
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:33:41 +0000

    point circle door error fairly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import os
+print(os.getcwd())
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
