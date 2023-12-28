058c7a7260ab6f185b7b227f7a5cb075e6385509
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:33:03 +0000

    skirt possession heavy lost damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_as_repr(array):
+        return repr(array)
