6706c53731647e93690b7cf22a108db46cd8dce9
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:17:08 +0000

    badly instead 568energy desperate thinkthrough

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-from collections import Counter
-print(Counter("hello world"))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
