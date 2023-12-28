3627b075724db32c17a59ab229ad8a1b1595725f
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:13:30 +0000

    long Ii badly search murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
