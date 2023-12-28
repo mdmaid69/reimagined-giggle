bc4ca369954061565012d630ee8f50db49a386c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:21:45 +0000

    around suit free narrow Vv

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-import platform
-def get_os_info():
-        return platform.uname()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
