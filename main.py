cf5a939631d5e7788019f533804aeea1e9b1f293
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:41:06 +0000

    mistake by born before match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
