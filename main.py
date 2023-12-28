5f953d870d80348cbf84a8c00d8f788ab64b6648
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:53:18 +0000

    quickly politics explain mark anxious

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def remove_duplicates(lst):
-        return list(set(lst))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
