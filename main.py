cfea6beff74756e823adf7fdf50ff7526952545d
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:03:49 +0000

    pause prevent mainly we note

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import sys
+  def get_python_version():
+        return sys.version
