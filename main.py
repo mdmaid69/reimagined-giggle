b6f4ff762609c405f7a8dfc96170a00fdb282048
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:55:03 +0000

    post practical this within pull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
