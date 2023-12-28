02160b556e1088d132132ea40f3c1e24dc07c5b6
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:24:09 +0000

    art protect indeed calloff increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
