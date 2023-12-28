c6d6b54cc47d94b137319e39a4ab28bef2815ec4
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:37:34 +0000

    hurry tear left tomorrow workupto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_work(force, distance):
+        return force * distance
