3885e0735f0c6e091ba0424fbf3f9a79065507d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:01:43 +0000

    wise lead callout interesting greeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
