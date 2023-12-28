7a75868d444503d9497b0a629ed6c4d8c9a9618d
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:03:20 +0000

    cold recently achieve favourite object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import datetime
+def get_today_date():
+        return datetime.date.today()
