b3dc404abfbd31d3edacb1a26ac8e8dd43872a5a
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:50:52 +0000

    know in sit fire sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def subtract_numbers(x, y):
+        return x - y
