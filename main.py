27fd528a04558d76a9ce5b9746d48af03d001a1a
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:36:24 +0000

    content argue sight yes workon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
