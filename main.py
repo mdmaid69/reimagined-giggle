c6484df73d22a3dd812da363aebf8cc9886987f6
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:49:34 +0000

    cost high grade development weak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import os
+print(os.getcwd())
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
