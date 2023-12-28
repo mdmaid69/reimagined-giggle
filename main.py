b17016b02df80b35b23f29c69028b46682683828
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:29:37 +0000

    murder bag matter time person

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
