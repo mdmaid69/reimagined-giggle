dec57e8bc16b7217e8eaead269d04a13a7b336ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:38:54 +0000

    rent live sick almost stir

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
