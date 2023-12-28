ab9ec173e429b705799faca2565b534da3df0cc9
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:52:49 +0000

    issue experience body that low

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
