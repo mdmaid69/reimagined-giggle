ebc3608bc312e3eec0eaeb6ed40135fd5db15749
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:20:49 +0000

    break display imagine punishment kick

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def get_current_date():
+        return datetime.date.today()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
