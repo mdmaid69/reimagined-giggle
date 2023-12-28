7b3391e8e76d46d220241e9488036228af06db20
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:49:06 +0000

    somehow on thirsty drive illness

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def reverse_string(s):
-        return s[::-1]
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
