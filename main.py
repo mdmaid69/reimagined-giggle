12d74e1e199963a8b69b5d5211fb3663730dd114
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:15:25 +0000

    introduce position profit recover protest

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
