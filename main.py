b296df07f7d2ec597d5d95fb24438b1e3ace8d24
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:18:37 +0000

    where bend cheat town on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_item(array, i):
+        return array[i]
