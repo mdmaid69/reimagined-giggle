88968a8f7312a1f64ae6c394e498bf396cdd8adb
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:17:28 +0000

    rise star theirs pupil pile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
