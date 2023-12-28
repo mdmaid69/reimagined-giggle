480507534148e00e0f8f2c4a2fabf8b70b0f3ee9
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:57:04 +0000

    famous opponent lean ability straight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def find_max(lst):
+        return max(lst)
