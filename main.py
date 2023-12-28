202dc1b086a4fce5325934fb57e28264d4010e50
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:53:26 +0000

    pattern couple be base suddenly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
