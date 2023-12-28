9dd7be963122612c80a551df9b982d8aa6106b4e
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:25:48 +0000

    fit fast usual work Kk

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def add_numbers(x, y):
+        return x + y
