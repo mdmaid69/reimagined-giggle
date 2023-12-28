0ce8bd18d753384a16e7cffc37bf2f4ab4cb376a
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:23:02 +0000

    view hurt bridge black Gg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
