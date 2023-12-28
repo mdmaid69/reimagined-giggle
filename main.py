823fbf4bb330d37c94b8edb156255652ac4ddbac
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:52:22 +0000

    opposite summer yesterday direction time

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import sys
+print(sys.version)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
