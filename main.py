d281c4f9645677bf70e0be09651a29a83f851e13
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:12:01 +0000

    comeup hard variety reply criticize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import sys
+def exit_program():
+        sys.exit()
