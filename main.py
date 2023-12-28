24d51ebbec06d43aff60f8ad4225e7328b3ba4ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:23:27 +0000

    sick excuse middle boil pay

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
