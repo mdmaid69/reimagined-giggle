738cadd14046f9f963e045e53cb07818c0a90556
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:21:47 +0000

    clear profit patience hold sell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
