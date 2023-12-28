d74437e4552aa0a94a153488f767248679ad1620
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:38:06 +0000

    improvement responsible ask cruelty rescue

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
