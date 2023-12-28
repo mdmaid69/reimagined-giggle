cb6d3bb49c8e03e72f0a39c7dd1c6069466f25d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:53:22 +0000

    passage male entry nowhere flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_current_working_directory():
+        return os.getcwd()
