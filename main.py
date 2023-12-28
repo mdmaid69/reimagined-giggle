71eb227adc633d5f93c9ef345c80d189a13c059b
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:18:02 +0000

    mend side similar organization turnover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def convert_to_binary(n):
-        return bin(n)
+import collections
+def create_stack():
+        return collections.deque()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
