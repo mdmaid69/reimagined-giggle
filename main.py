045bac28788195dd816c17fb166a5c25922a9eaa
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:43:41 +0000

    attention with no bother record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def add_numbers(x, y):
+        return x + y
