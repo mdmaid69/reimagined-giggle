fe8cc9a838dd285afaf6b3f88714dc188a898dae
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:31:03 +0000

    arm favour march sticky hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
