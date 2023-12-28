bff6882b5011de09a1b4d42abd3a3c3ffe6eb98c
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:52:38 +0000

    human about smart worry recognize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
