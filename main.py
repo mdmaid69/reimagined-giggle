7166140312715ddfa65159b2be72ae7032ffcc02
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:36:39 +0000

    previous shrink extraordinary before building

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
