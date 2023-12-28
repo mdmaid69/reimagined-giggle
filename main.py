50ca07dec27acc576e741e7e2f7336f5d7c201c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:39:12 +0000

    control ticket steal shop low

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
