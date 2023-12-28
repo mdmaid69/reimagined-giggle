dd87cec0472d0869ec45fad68244650b0a161081
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:49:10 +0000

    complain house gentle strike belong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
