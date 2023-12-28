4c491b75f5c98df07c89b5f545de19984d8e6d08
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:18:18 +0000

    expect heat reject private moral

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
