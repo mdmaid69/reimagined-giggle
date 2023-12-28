d2863aa4a6d1e97e172c159eeefbd8fa7f152f73
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:20:38 +0000

    blame side plan bank sight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def reverse_string(s):
+        return s[::-1]
