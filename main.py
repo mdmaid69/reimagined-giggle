9e1b1e3550d9893df778a49e9f4378de7fb4fb0f
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:29:07 +0000

    change dick damage whom when

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
