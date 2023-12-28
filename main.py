984c5390517c570c243937af9b7a816a2dce5d68
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:55:23 +0000

    brother pass on watch care

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+print(math.pi)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
