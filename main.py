18f421f2fcf455da146e6eaff36fb86aa4e572f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:18:31 +0000

    makeup develop report fight criticize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import sys
-print(sys.version)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
