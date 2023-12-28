c3e70b982ef05e10b36795e4ae323ccd788aa2f6
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:03:38 +0000

    maybe greeting ago relative believe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import sys
+  def get_python_version():
+        return sys.version
