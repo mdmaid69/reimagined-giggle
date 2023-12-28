c080f43651565c5120c050bdc65361d2194bc8db
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:20:53 +0000

    snow collect trick cold wing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
