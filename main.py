7b4e6c945928c575c366613e5e1bc46f06072aa8
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:45:15 +0000

    hit keep funny shout breakup

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
