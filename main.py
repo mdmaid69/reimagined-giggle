7fd7a2b1f388a51e0879e3f4e76e8d9146b7b025
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:05:46 +0000

    reject embarrassing something dry group

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
