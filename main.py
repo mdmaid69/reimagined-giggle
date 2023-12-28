82fca91d3248fa270fce7a1d0b4591372556d178
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:15:01 +0000

    marry shame coffee early regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
