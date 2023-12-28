f175b662dbac248a378dc92ad1cb5e9f25e4d2ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:51:57 +0000

    much young constantly or such

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
