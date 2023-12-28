4cc24f8b0bf9efb29b13980e64cdf390689597ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:15:24 +0000

    think remark uponon stop regard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_length(array):
-        return len(array)
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
