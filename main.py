60e7a186db2f25c95896ed961b1e4277a6b97d93
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:53:43 +0000

    firm go travel programme label

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sys
+def print_python_version():
+        print(sys.version)
