4883800f0dcbd9081a0ccbc5dd333b0b1bacc29d
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:43:32 +0000

    hot skirt disgusting tread combination

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  def square_number(x):
+        return x**2
