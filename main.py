285b0248ab1e69ef0d520399d1771b48675da703
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:37:56 +0000

    avoid anywhere giveaway watch married

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
