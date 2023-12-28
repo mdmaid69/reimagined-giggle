7cf7a91b96619da92990deabd950e2b9c129f491
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:35:38 +0000

    workout may key man winter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
