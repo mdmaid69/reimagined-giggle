9b8df5993f173895d10ed59ca73d6bc7102cb22c
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:26:32 +0000

    neat left book nor announce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area(radius):
+        return 3.14 * radius * radius
