8007117be25c1a84daaa0cdab8331dcf0ad35108
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:42:48 +0000

    uponon seem arrangement miss brown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
