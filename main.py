dc7e3e995cf9a9ae3166e03fb771ad874d9058cb
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:29:12 +0000

    region future curious fight factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
