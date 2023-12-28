7872fc939edd60762ec796f11c005d08ff2b5ca4
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:47:31 +0000

    temperature whichever operation somewhere tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
