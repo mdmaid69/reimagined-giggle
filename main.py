ffab6561f1bee3fd7723715058ba565d9207babd
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:48:05 +0000

    criticize Ss responsible flat catch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
