21714c712a2cad8ea6bbb480391f3dfc172e463a
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:50:03 +0000

    try him pretty brother throughout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-for i in range(5):
-        print(i)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
