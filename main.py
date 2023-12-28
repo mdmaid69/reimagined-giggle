aa78496cbb5a57d6924d0300b7514dcc56d640aa
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:41:06 +0000

    reason float relationship whoever pile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
