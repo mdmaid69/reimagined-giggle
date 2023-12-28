6535a0620a4981d00500a247beff39a49782ba51
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:35:03 +0000

    free pain 570enjoyable flow born

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
