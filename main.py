c124a0ae367190b9fd3aa750fbf75facb45c708a
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:12:35 +0000

    once once laugh variety strong

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_bool(array):
+        return bool(array)
