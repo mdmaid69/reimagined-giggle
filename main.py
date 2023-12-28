7a9bbc312f36ba239e2dde2eca4884c38031f3d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:02:05 +0000

    disgusting hang spring autumn hurt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
