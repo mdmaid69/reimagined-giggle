09e3abf6c8521c6bac97bdb86db7af406ec32438
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:25:31 +0000

    straight table size morning medium

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def square_number(x):
-        return x**2
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def multiply_numbers(x, y):
+        return x * y
