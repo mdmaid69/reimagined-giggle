539d16cf6fcdc907590612829463866b837f7957
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:54:17 +0000

    worst mean bunch chance buy

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_perpetuity(payment, rate):
+        return payment / rate
