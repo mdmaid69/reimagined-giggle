fb9f49c73147ce671a8708cdfc0b718eef94b79b
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:42:42 +0000

    second face worried demonstrate apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
