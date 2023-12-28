1d47290478a572e903a8bd89b71c2099ac56540f
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:54:48 +0000

    instead turnon always wherever break

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
