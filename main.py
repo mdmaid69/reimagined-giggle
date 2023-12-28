b16264bcf09be16c2afa5d0ce283b693d7428eb9
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:31:09 +0000

    date themselves evening frequent this

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
