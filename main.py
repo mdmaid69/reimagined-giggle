24009982f9f7720a1b54e9c4df0e5c108f82cc04
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:11:08 +0000

    home aware recover wet gradually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
