416990bfc9d575e4b14e5251729ca52488e47818
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:16:19 +0000

    live welcome whom conclusion pipe

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import tensorflow as tf
-print(tf.__version__)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import json
+def convert_to_json(data):
+        return json.dumps(data)
