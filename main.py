f52f7e2e9be7430342eee7f274638036214ec1fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:42:23 +0000

    makeinto worth read admit shine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
