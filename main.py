561821e4393145c108ed9e541653cc53c26858f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:40:07 +0000

    regard excuse 572enter tongue room

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
