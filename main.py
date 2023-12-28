b1bcdb428c7b3c7927be2323c257d4a3f96c8dd2
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:49:13 +0000

    fear boring work number he

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
