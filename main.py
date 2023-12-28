3b80e48e1909b56e62f02837a8f9e520ff1c6ce8
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:28:09 +0000

    wave tone would either roll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
