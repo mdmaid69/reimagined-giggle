1ff116ea30aa517f83fd111a9730cb4083c0aa30
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:40:39 +0000

    boring us position quarter beside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import collections
-def create_priority_queue():
-        return collections.deque()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
