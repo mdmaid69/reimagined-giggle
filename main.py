18aebe8f4f51e8a03bf863380b4592c2bb99531d
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:33:02 +0000

    uncertain shelter bury back person

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def find_max(lst):
+        return max(lst)
