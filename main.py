2c8b685831e735f70effe3c8ff37a3cc5d6088b5
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:57:25 +0000

    table finally degree question sort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def multiply_numbers(x, y):
+        return x * y
+import os
+print(os.getcwd())
