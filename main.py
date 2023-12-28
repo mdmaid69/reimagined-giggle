4e1375cbfcd8ababd019ccb55a84e78d63cf87e5
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:32:29 +0000

    frightened from may copy peaceful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
 import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def delete_directory(path):
+        shutil.rmtree(path)
+def subtract_numbers(x, y):
+        return x - y
