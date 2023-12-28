23ad6e4b8ed79451878189f6f43ebe4db3f3f12d
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:58:28 +0000

    show conduct house scheme equipment

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
