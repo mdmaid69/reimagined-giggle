5e9c107581212eb60a6ca1ac4bf252c01aa176bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:04:51 +0000

    round break 566end last load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
