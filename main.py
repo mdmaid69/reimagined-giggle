d29126cbaa0d4de6b5eeee95d826847eee32ce4c
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:41:41 +0000

    control profit disgusting call show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
