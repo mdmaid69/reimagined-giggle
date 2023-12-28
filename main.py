f9a42a462251d525fda9102b514dc02e5c2f1a09
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:53:52 +0000

    keepaway affect clothes marry absolutely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
