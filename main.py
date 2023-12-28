5fdeb262cc0d225714157f647c05e26c2c9b590d
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:10:45 +0000

    national degree about advantage hardly

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
