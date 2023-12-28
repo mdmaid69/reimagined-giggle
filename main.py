8530e88043b8fd520fcc9818836469836085f1ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:29:51 +0000

    dinner mood water concentrate breakout

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-print("Hello, world!")
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_work(force, distance):
+        return force * distance
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
