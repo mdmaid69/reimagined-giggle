2454d1bf368f5095b36746caa98b94a6b42b5113
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:57:22 +0000

    worried offer glad hot sale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
