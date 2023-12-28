202ed725080b19c274d06d27c6ea7627a53dfc65
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:20:15 +0000

    old waste creature market hot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
