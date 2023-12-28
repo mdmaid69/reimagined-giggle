2a7474f94d57ce39265b1c1f14e717ee82227c30
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:48:19 +0000

    improve science Ff wind actual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
