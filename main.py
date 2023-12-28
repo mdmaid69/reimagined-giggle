96dea7f9ad115755895a0f9b4abf3a6017fd66ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:11:14 +0000

    breakout body very grateful change

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
