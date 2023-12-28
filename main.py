a305cf816f6449b9586f0f8f25550b8c165ecafb
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:41:14 +0000

    sight call team subject meet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
