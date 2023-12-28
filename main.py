d223a441efdef524d593416000583f2bf2f5242e
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:08:52 +0000

    pride grade love straight apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
