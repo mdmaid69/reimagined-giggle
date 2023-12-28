eee26e3408598aac95c44e593ed4f7f3c434a9db
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:19:47 +0000

    seriously clean clear no start

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
