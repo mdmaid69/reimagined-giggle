b91e531e3b80cfe15fe0ebe0c46e98447fb7e72f
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:03:44 +0000

    summer bush brain people curious

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def add_numbers(x, y):
-        return x + y
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
