775c53f5da60df7f88445ddefda0fafe2cae8ad7
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:51:40 +0000

    past to let inform account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def reverse_array(array):
+        array.reverse()
