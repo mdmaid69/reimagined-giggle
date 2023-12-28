329f1e005d4faac134211905fbfec0a68708f29e
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:48:44 +0000

    advice black extra freeze peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
