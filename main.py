60191f5dd622d419f880a6755ce7b75c9035d94d
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:38:09 +0000

    add system blame now instant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
