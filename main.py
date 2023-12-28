bf6c6340b00bd3dab9ee5532ceed11ece73f2dba
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:00:59 +0000

    piece outside scrap foot prize

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
