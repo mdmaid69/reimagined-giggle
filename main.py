b75737afa860ad6d4304641d8de6ce21a184e015
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:29:19 +0000

    giveaway ear top sure on

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print("Hello, world!")
