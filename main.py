7ea8febb8eb79f3e8d1e753e1c0da86366198529
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:26:31 +0000

    building prevent suggest child handle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_min(numbers):
+        return min(numbers)
