32d1bbd34f7b157f65c11fe2d3b47bd3ca9a04af
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:59:22 +0000

    quantity attach across ago movement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
