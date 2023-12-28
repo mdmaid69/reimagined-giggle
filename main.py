8ce7c6bbe40c63bcfd5c6ed45689eb092bf76b85
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:01:23 +0000

    schedule police behave behind drawing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
