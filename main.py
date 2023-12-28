ba969043310e32b7207b3c5810f210c8deab549e
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:38:21 +0000

    much score wherever design march

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import json
+print(json.dumps({"name": "John", "age": 30}))
