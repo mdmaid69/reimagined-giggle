bc56164250fd22537e70ec77e7df47cd24b18263
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:52:16 +0000

    opposite positive share cough deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
