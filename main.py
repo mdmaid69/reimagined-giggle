d90c744dfc7abf579f03bc02587cc57a0398194c
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:00:56 +0000

    general smooth Kk heavy factory

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
