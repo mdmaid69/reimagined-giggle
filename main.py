fbee900bf5d49537ca5eb6f57883bbd2dcff8ac5
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:48:08 +0000

    instant perhaps clearly behave during

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
