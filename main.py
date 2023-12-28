7529644b9f77cf4ccddffb7417763a5e6da6df7b
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:37:00 +0000

    convenient goout relation regret scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def remove_duplicates(lst):
+        return list(set(lst))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
