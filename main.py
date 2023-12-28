457362b47398bac2559fbf663b2e1e044f4b7478
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:10:45 +0000

    dead bit apart then pass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
