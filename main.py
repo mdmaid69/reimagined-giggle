ff99477a9b596539c192b2036e4392a52cf55672
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:30:32 +0000

    lost poor rate influence upstairs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
 def calculate_amortization(principal, rate, time):
         return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
