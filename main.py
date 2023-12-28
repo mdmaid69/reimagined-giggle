5ed928d1a50d3cdad96a444172684f2efb03b126
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:08:05 +0000

    safety how anxious definite artificial

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+print(sum(range(10)))
