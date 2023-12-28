275ced4bbd12db948128838871e0a13ddcbf93a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:42:41 +0000

    design remain choose telephone forget

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
