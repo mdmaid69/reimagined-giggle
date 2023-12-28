2f71ef9d97583a730143fd7ee91acd88f4a4a329
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:12:37 +0000

    unlikely there contents present fault

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def convert_to_octal(n):
+        return oct(n)
+print("Hello, world!")
