638f7caae96876851b130cb10737c15bf7f27ed7
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:37:01 +0000

    wave underneath minority allow Rr

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
