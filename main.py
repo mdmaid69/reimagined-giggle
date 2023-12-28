27aa5a7bd4636d8023f575a78daf23b81a1fd820
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:57:09 +0000

    branch mistake usually kind joke

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def convert_to_binary(n):
-        return bin(n)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
