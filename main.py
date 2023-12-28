7ccd0b728522194ce0d0c76b2d51a0fc3c202d03
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:16:47 +0000

    immediately perfect perform violent notice

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
