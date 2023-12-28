969ab2f85667afdd3c94a93bdcd1ea5cb76098bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:54:04 +0000

    surprised confident effect often protect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
