3a6fbe011f319ef2a195b0ed52fdeb8663975cd8
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:15:52 +0000

    opposite lonely mix control between

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
