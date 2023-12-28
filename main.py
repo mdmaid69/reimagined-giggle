96ac779bb55ebaa1ffbe7cfdc901f60a5e34c4d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:12:43 +0000

    file figure chief speech clean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
