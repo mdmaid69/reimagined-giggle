372c54bf48e54b05aaeb9b7d854ae0fb48b67470
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:13:15 +0000

    brother them general across dull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import collections
-def create_user_string():
-        return collections.UserString()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
