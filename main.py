60ee241529baf94e9785a4ceca9ae8f9eee4de2b
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:43:13 +0000

    to care reduction touch gradually

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
