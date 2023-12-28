66be9a60b2b3f5b48cd9ea2c10426c39844fb4f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:46:14 +0000

    approval once kill part something

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
