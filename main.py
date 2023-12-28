6ddbc95047d9a5b6066d2c443fe2630144d6420d
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:23:32 +0000

    sound speak report strong dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import json
+def convert_to_json(data):
+        return json.dumps(data)
