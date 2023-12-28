f6e497f1a96ed145d0f95f548c9df59b02315ad3
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:54:03 +0000

    frequent conclusion holiday swallow not

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_average(lst):
+        return sum(lst) / len(lst)
