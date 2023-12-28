bae21f2e65637b9c8593bb6835579b7c8d3a786b
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:00:48 +0000

    maintain best bear set meaning

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import collections
-def create_user_list():
-        return collections.UserList()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
