26ab9bf26f6d2dc18894d3692003bad54a629b21
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:57:12 +0000

    problem suit guess fortunately turndown

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
