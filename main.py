dfc08be991cca0e0068b708f2c56d92ff19f9595
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:50:27 +0000

    state house slide target clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
