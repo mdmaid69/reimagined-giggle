6d7c1404c9d627b117ebeef73289a594dc799cf4
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:08:27 +0000

    view moment explain progress satisfied

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def sort_list(lst):
+        return sorted(lst)
