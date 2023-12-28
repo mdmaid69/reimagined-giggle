c0e6c347e6df168683f789f7b1699a39406765f1
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:46:30 +0000

    draw fire impression official low

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def convert_to_hex(n):
-        return hex(n)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
