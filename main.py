9ae603b41f024c47c34473fbc08485898a79e4a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:05:16 +0000

    about workout bang niggaacademy operation

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
