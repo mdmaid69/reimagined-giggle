dbe6f101061f3f1e53c05429e24bfcdf3ce15bd3
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:23:52 +0000

    by finish medium sign cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
