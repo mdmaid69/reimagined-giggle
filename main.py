726ee5fbc5c397163e3814c2164e6a070c8a8900
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:08:30 +0000

    mainly process break add my

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
