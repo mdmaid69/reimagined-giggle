146abea48e80cedb9d0a78036579fc5e0cb5ec93
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:10:27 +0000

    organization argument accidentally jump stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
