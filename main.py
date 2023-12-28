f0c4f008a7902caa95a984a53252ef7333eefbd6
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:22:25 +0000

    warm lift fine throw buy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
