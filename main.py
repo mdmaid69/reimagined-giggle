4176416ee035fee8a18cb4485ebb749476c0c4d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:45:51 +0000

    doctor peaceful getout throw through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
