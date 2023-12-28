22114b51d6abbb6249c59fe2c227babba43a9456
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:10:26 +0000

    skirt aim tend fortunate section

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
