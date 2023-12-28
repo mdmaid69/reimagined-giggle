4a247adce263d9b5db2bd229a92f9576aa9a01e0
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:43:47 +0000

    unless tongue nurse strike now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
