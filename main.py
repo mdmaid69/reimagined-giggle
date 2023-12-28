c42c233456f2dc96b6172c9b20c00c59c7a03df6
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:23:39 +0000

    year market difficult grand pull

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
