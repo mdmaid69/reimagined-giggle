2a1b74e734fb9387ab425fa73b4b946575bcde52
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:42:16 +0000

    trick burst definite aware beside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import re
+print(re.match("h.*o", "hello world"))
