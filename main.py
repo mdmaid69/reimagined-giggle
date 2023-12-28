0a4bfcb3c51652a77ec48a6675e814ffddb0428e
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:43:18 +0000

    temper empty abroad Ll join

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
