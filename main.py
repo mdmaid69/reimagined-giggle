aab062efffb5918142e6edf03872a90a77b813f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:51:31 +0000

    oil pleased lookaround shoulder evidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def count_elements(lst):
+        return len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
