717e28b7d380aac302088265bc3bf036283b1963
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:39:34 +0000

    level game safety whom flow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
