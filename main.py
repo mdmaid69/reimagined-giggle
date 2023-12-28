2dedcdf375cf4e600cc0669a30bb8410835ba95c
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:38:59 +0000

    case boring there pity add

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
