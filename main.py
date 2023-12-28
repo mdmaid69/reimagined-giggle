01d80a9a34f40aa405afe1832ad3f4e31f2d0eae
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:51:58 +0000

    rare involve lie into matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
