3a81304bde5dfd1420773a576cbe2c212abc9f4f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:02:17 +0000

    correct smooth interrupt measure pump

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
