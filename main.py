05d1eb4d01900eb51a987b94dd4f2bda29c25425
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:36:16 +0000

    breakin fight cry wish hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
