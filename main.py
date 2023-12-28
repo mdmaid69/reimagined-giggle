f1f01e7678107ea2ccd42026e3fc8cbb7444ce43
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:59:56 +0000

    truth part account question exploration

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
