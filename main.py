bf2884b5893ec7c2f67407e1c116e41dfc572abf
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:00:23 +0000

    angry necessary tense track practice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
