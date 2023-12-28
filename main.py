08842ef3a6b77488cebc8cfdd30c22f797a00ca0
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:59:04 +0000

    ring her form operation strict

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
