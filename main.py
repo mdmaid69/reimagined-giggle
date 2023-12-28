f4a6fb2bead9431e8c40709321d2f13093b25e1f
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:39:58 +0000

    wide mean port callat discover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
