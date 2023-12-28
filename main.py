86f2c7a1555f20990584a13ed0287187ce1e5e75
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:11:11 +0000

    subject getout exact sad male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
