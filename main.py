8f7e768a7d0391398f7d7e350fc474210094b983
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:46:03 +0000

    wind nurse Hh with swimming

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
