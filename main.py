0aa5ca16d60f8670d6d44ec4877ab91c71f32fd9
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:44:18 +0000

    damage year noone entrance card

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def find_max(numbers):
+        return max(numbers)
