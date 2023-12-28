b1027c1aa19ccd4e2edcaac1ba9701d135348b1e
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:44:48 +0000

    hard temperature task herself far

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def calculate_area_rectangle(l, w):
+        return l * w
