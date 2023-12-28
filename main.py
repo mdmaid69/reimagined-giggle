4129a2bb881c6ab0571f3353158956fbe6768450
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:27:16 +0000

    bringup rescue grateful reaction cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def set_array_item(array, i, item):
+        array[i] = item
