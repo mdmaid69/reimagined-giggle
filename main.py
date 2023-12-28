8f132accc2ecd701f5696b8cec2115f502062564
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:03:55 +0000

    yourself runover contact so flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
