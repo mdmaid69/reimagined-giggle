fb940411e9d48bc2d6204b3834df6afa1b2bb9a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:31:00 +0000

    late nurse thorough own believe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
