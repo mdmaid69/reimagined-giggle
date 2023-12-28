2c360a2ceeb44f8bbdc68396cf55d2c3e11c8829
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:35:24 +0000

    anxious weekend view mysterious pencil

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
