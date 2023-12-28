0b5039452253de62e2494c88624823b7fa87a8d6
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:03:39 +0000

    shut sticky cause equal suddenly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def reverse_string(s):
+        return s[::-1]
