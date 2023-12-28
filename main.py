33fa5543814a26187fa0062be510952a30345d66
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:29:35 +0000

    open connected this ground search

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def greet(name):
+        print(f"Hello, {name}!")
