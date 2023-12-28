9116a5d37b1e061f08e2ea4e9d71b26b7156f3f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:00:23 +0000

    trick later breakup Rr thorough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
