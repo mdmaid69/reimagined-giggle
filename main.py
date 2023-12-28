6a13f872b2b58a1326e6c2f84e4c7b8b73ab534a
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:34:28 +0000

    alive convenient meal health everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
