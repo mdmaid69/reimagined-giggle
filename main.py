5d3e2f467a15b50abe3878351be79f3d981addf7
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:49:56 +0000

    ought together terrible such professional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
