5594381d6e9a2c7dc41170783d1d24253af96c1e
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:34:15 +0000

    fill listen attempt badly office

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
