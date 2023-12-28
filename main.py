38777ed229dd3735fac14d83dc5225b644bb9033
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:18:55 +0000

    hope town himself substance leg

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
