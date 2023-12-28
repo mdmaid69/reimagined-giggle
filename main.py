12db1630a0624071328ec34d9f841d8a893c0663
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:15:53 +0000

    please there bringdown however natural

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
