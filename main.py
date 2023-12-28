cf93244e33139da1f6b72b56db7ac46fbb4d140d
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:37:49 +0000

    tense just trap result touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
