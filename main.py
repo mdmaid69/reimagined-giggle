4f6a0af8f61b5e11b8230ade2c4157fcf3dcac5a
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:12:05 +0000

    anyone dress skill wheel new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
