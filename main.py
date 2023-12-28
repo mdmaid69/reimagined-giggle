ecdb9f186aef245fe5e51bd3e4c0e8fd0e80880b
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:17:26 +0000

    complaint slice approve whistle desperate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
