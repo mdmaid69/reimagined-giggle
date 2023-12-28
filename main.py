fe0e7f812d2eefebb370f2990c3edd65580bc97b
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:58:36 +0000

    bend ought however sweep image

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
