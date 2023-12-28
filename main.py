96c6354704ae23261a7e8a370c8d1e1bb8d13be8
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:21:01 +0000

    number progress occasional get practical

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
