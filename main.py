21bbcf6100cd87b408c58b089efad43f234e594a
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:48:00 +0000

    out consult advanced winter house

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
   def get_file_modification_time(file_name):
         return os.path.getmtime(file_name)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
