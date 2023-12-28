c62ee49dd9e947eb13dcc705275906eda05a02f2
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:06:21 +0000

    guide as moment summer fire

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_work(force, distance):
+        return force * distance
+  def calculate_area_circle(r):
+        return 3.14 * r**2
