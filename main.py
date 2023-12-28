7049c76629bbdff5392af63fce48cab1fcffdeba
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:17:34 +0000

    worried aware past score bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
