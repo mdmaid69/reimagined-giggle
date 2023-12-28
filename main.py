f7bdc8b2208a380b09df2e86e54968896f939c36
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:30:13 +0000

    easy class approve comedown floor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
