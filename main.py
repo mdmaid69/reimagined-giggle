43b7df3c0a8305d93bc0b7a6af3e914ec84ff9d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:32:55 +0000

    plan plan nerve last shall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  def convert_to_hex(n):
+        return hex(n)
