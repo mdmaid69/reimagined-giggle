d24d0dff6644b696bd9e85e34073d1947fa1fab2
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:28:21 +0000

    valuable theirs mend wrap classroom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def greet(name):
+        print(f"Hello, {name}!")
