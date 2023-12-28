c34c41fbfe61a8335823d4a10005046fe926de4b
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:39:48 +0000

    please temperature Ff fence invitation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def square_number(x):
+        return x**2
