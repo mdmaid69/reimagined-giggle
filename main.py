68bab20cdbac10494109ff30239bcf751c73e12b
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:55:12 +0000

    convince holdon suckme everything used

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_current_working_directory():
+        return os.getcwd()
