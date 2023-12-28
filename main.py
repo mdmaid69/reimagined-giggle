de7b402ba80dd05b6ed4081ab13fe9aecb851187
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:38:56 +0000

    whom moment steady why note

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def convert_to_binary(n):
+        return bin(n)
+def count_elements(lst):
+        return len(lst)
