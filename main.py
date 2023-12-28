154b87482f15f68445f5e4f739165639770121ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:07:02 +0000

    operate home thumb wind doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
