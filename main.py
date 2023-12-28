4de8a7b28fea264d58ba00b263a714de9816ebe1
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:10:45 +0000

    care interfere sore check appearance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
