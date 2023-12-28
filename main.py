e27933bc9daffe2f61706e7b28dd462518206da0
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:20:12 +0000

    score moment target fill who

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_length(array):
-        return len(array)
+print(sum(range(10)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
