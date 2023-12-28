1e3e48b67c7293d4ad1a8a00bda8db23c23545b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:52:57 +0000

    song heavy truth inquire burst

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_hex(n):
-        return hex(n)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
