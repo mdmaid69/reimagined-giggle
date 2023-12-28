a75f31bf3dbd961301cfe0f0ee5574106db30b20
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:36:04 +0000

    pattern walk geton fill harm

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_acceleration(speed, time):
+        return speed / time
