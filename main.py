0947ed3c90cb71e6dbb779bd6b02b498ff42a5c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:53:52 +0000

    raise that plan respect stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
