398e30a782104f5cd1010e0058fb93736eb61fb5
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:03:49 +0000

    field thumb occasion influence brain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_int(array):
+        return int(array[0])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
