6576eecfc4fee9714e3fca562c12b99ffc45baa7
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:06:30 +0000

    maximum you feeling ought in

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+print([x**2 for x in range(10)])
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
