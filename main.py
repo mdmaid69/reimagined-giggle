4a4c645faf75cd1aad718057ca86ef6a2ba4063e
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:27:32 +0000

    trouble behave before the environment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
