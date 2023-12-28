18971d5550ea5b69aaeba263c97b80f569ed3b37
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:09:54 +0000

    machine back material ground car

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+for i in range(5):
+        print(i)
