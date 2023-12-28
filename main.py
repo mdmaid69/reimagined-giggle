63a4b84541e876125507e185aec93887eda1723a
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:20:39 +0000

    fire decide may limit not

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
