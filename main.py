9a0a79d04754b6f2e58b5749502cbdfbf24a8946
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:52:15 +0000

    government practical suggest very mass

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
