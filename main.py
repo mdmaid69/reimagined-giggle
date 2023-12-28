2961febadf0b82914802401313fb4ed76f31c7b5
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:21:21 +0000

    talkinto please when gather show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
