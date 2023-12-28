9ddc8037cffc8da74233886b99dc837fb8322f93
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:24:40 +0000

    side interruption danger poor bear

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def square_number(x):
-        return x**2
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
