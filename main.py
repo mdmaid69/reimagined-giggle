7672729443259bec33ffb7f2cd03f8ccbbadc9e9
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:03:55 +0000

    bored wash nonsense turnback sure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
