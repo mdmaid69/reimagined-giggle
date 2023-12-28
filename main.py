47628eea26c1766e8b2b29028f71f654c732c846
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:09:50 +0000

    year Ss offer swallow emotion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def is_odd(n):
+        return n % 2 != 0
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
