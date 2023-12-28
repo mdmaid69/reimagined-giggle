ec27e3e52c8725784e42d85ad010f15436aaa169
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:15:50 +0000

    date anyone absolutely rate report

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import platform
-def get_python_version():
-        return platform.python_version()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_acceleration(speed, time):
+        return speed / time
