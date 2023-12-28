d614a243a7df7ffee872a56daafd2606520e813f
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:44:26 +0000

    side represent side pour Bb

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
