d1995591f474a8dfd22e15e9267bb574fd8b24b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:33:23 +0000

    its plenty plate stupid basic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
