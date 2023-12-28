5ef295f995905de1655ab4ca80d4de8c70401ef4
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:45:13 +0000

    than partner muscle voice weekend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
