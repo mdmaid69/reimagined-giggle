c13be1cc4ef08e3e111cf5f7e14d3e07e870add4
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:07:42 +0000

    kind join through address row

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
