1a5e174aec6300e8bcf2cbb0d5e18ec30a9d08d0
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:46:02 +0000

    noone decision disappear with definite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def find_min(numbers):
+        return min(numbers)
