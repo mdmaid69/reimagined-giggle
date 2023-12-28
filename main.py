f89fe759322e8844440b90a6b9e0a3013b483c15
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:19:57 +0000

    born professional account attempt difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
