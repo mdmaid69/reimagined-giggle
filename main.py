01b4ec7bd2d71f1cc5bf36dee03b25a7404bcefe
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:20:04 +0000

    fire taste lift bend nigga

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
