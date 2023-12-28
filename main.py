375f1cb77bd9dab0658ed7590cec628ab65555cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:59:42 +0000

    pride smell raw shall keen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
