a8d53ad9e9b0e532d484190e26c1e4aad58effda
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:34:53 +0000

    tune high amount coffee must

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  def is_odd(n):
+        return n % 2 != 0
