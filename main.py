71fbb90e5d2e0ffcd516a3fb2138957f376314dc
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:39:00 +0000

    pen major position deep hear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def sort_list(lst):
+        return sorted(lst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
