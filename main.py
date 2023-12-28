5abf06cef28e105a90c4a19381a42a4e5b7c283c
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:25:36 +0000

    hard punish cheat sentence fly

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-for i in range(5):
-        print(i)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
