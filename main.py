ebdae025a124ef85d4b8f45f34326ac9b0bba678
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:24:25 +0000

    love yet what past model

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
