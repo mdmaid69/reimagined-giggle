831ce2a8e52be170f68d56d3ac6f62c4aba44dee
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:00:30 +0000

    think far in without desperate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
