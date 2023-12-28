fb05aeffb4a32811afa658faff001d868c61d919
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:14:23 +0000

    excuse trial abroad defeat false

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
