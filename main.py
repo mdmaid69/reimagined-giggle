aa9ba3b817d2f693d42ec31a5821a7d27bfe5d43
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:33:56 +0000

    ilovedicks communication pollution criticism Qq

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
