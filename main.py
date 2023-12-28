6ac021242774aec908bc1567e262723e5bd93931
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:11:39 +0000

    several shame connected number recently

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+for i in range(10): print(i)
