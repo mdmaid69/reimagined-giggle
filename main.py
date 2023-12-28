bd99b9b841803c20f19f7df12d9e083e5b0d5489
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:04:50 +0000

    old short maybe face before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
