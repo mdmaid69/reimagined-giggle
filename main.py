0ba4d180ae23de399abdeb9185053f3f928e1ec7
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:44:03 +0000

    otherwise excuse stir ocean spin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def count_elements(lst):
+        return len(lst)
