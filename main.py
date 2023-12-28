f0da21fd1afcd7565faa2d55c94b749d3c63e9eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:50:56 +0000

    as successful who closely pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def get_list_from_array(array):
+        return array.tolist()
