51755c9afdb7ea66f39d756b6c9ce87d626cc4e8
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:04:57 +0000

    target scale tend mood left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
