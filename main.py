debf96d8f060886f6096ea09c13918e2bf326633
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:24:18 +0000

    no treat loose shock region

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+print(math.pi)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
