3bb71d0181264de22a24ac60a2a5a9d76fdc69ec
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:25:32 +0000

    object limit fail frighten turnaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
