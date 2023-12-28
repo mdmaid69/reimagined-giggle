4b5461c4a8d74981ed34b1a4db18afa8c7461bbd
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:12:59 +0000

    approval friendly needle friendly scrape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def remove_duplicates(lst):
+        return list(set(lst))
