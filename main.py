cecc69594b394d0f9d0108b2e48c2a0da5b72cf1
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:25:06 +0000

    feel never use form while

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
