8ff7a16d5260974540212c3f85f0da8dd29aee68
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:51:31 +0000

    be fine crowded sense giveaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
