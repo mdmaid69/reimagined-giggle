b684a08cd0b636f2b6a01aa6c1a92d23fb3aec41
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:13:33 +0000

    violence unkind spend famous shame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
