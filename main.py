6b7b002d0360c9a325ba425a5a21959bf00a45f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:46:12 +0000

    popular bring face very substance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
