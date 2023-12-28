b02716b8734d3827ae675d700f2b87677ad9eeeb
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:49:38 +0000

    weather theirs limit performance rather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_power(work, time):
+        return work / time
