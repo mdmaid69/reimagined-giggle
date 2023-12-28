be5afdd7a4d5fb02b02162ec151f48a67f86f7ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:39:30 +0000

    control weak afford science sore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
