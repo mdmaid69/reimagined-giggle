f009242a9f3790fed2b6e67291c8eff385e57754
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:32:20 +0000

    dance water trip describe I

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
