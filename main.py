13fb24f9dc5bbb1a50dbc998550aaeeb43d7d952
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:48:39 +0000

    flat show night relationship interesting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
