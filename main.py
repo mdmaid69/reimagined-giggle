120c3fcf7c0b88ac5ac9876a4cdb6c51b73e4b2c
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:40:09 +0000

    slightly illness scrape pin sea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+for i in range(5):
+        print(i)
