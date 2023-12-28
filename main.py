e4e7ee8d271c750c022d37a3b7d4dc89d5307d82
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:30:57 +0000

    straight commit free minute divide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
