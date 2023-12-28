5b3698f9d40c60a987d24cf2d7b432b1e4cf3012
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:41:24 +0000

    goon bringup image so better

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def reverse_array(array):
+        array.reverse()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
