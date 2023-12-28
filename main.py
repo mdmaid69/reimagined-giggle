8b16bdc7764ceafdb1a814839848929ef0e16592
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:16:37 +0000

    move fix last excuse boat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
