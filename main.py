a49a0707f1c710b17c781032771ed03184ab1bde
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:48:53 +0000

    even light couple enthusiastic record

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
