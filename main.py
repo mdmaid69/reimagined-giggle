3b606aa46f8ee1d40ed897ca9b1925f278b5b430
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:21:10 +0000

    conduct nature burn gather anybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
