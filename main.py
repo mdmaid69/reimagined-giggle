17264ba994fb795c603b706abdf8498f05f5ce94
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:20:51 +0000

    gift vote correction float let

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_binary(n):
+        return bin(n)
