4c1d2ac0028ee975abbe1fd5cdf003cc33aacf59
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:23:33 +0000

    usual regular case set Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def greet(name):
+        print(f"Hello, {name}!")
