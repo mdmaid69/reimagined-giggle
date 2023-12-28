cd2481bc59d7d20bd3310cbebaede7df14b75fde
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:23:51 +0000

    skill order platform past splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
