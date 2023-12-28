0ca9f6213356616306ddf5762d38f674945933ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:04:37 +0000

    quit naturally intelligent flat thumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
