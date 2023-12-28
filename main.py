57dc951a00d94d0c2d58a52ecc9652ec42a2c297
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:13:30 +0000

    memory moral match action platform

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+print(sys.version)
+def find_max(lst):
+        return max(lst)
