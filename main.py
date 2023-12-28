417243f0be43255dc14bd2cc9ae18f39668f7c57
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:08:48 +0000

    nowhere hurt away risk shake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def convert_to_binary(n):
+        return bin(n)
