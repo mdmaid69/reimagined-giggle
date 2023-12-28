83aea14f3b6f53122a79a1dcee901f8da3228829
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:39:58 +0000

    guilty bang view surface entry

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_power(work, time):
-        return work / time
+for i in range(10): print(i)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
