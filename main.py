6ec6e67c0d5da1b4347ba539b7e009c3a25a0710
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:41:51 +0000

    search realize keen foreign hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
