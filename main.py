fb18190d3a75b97df69eb35c5197ce6dd9e6d56b
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:01:07 +0000

    reserve year second leg too

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
