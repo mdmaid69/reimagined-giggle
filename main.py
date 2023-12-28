1b581dc0b92e09ba36d942774e872e83da8391d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:30:15 +0000

    best solve proof match wrap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
