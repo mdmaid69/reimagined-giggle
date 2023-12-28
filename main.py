c21e1f55f85cdb1050181d31afcea12580a57eb7
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:16:50 +0000

    table sort whatever tomorrow respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
