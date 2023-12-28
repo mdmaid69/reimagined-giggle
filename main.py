d402365ac201ccce9d6ce1faa679232b1000ddec
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:11:48 +0000

    telephone pile house sale so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
