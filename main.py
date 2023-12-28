43cf42f1a6364b337dbcd01c5525f376dcf9d89d
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:50:26 +0000

    simple fortunate really neck company

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
