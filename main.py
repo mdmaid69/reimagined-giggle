474ecbbe52f716a00a5d4d2b0a5e9c48b3f8fb20
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:16:24 +0000

    chase slice inside naturally wander

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
