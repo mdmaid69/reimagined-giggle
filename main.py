bbaafd58300f476d2ec409bc1cbdf78157263281
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:40:12 +0000

    heat flat wipe lonely worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
