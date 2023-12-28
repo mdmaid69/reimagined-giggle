54923f4095e306840d01f56b3a6744190099b46a
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:09:36 +0000

    worse school accidentally shine ring

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
