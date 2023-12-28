330ba084897efb85a06b6770a35f67660a2b43f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:16:21 +0000

    door 572enter stone room negative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import sys
-def print_python_version():
-        print(sys.version)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
