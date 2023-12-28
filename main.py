ac9faca2819feedcb586a62f766cd4a7feaa4572
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:32:40 +0000

    job excellent whistle kick seat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import time
+print(time.time())
