d4eba85caea271c69ff98ede005933a40d8863f2
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:08:41 +0000

    empty challenge dream dig for

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
