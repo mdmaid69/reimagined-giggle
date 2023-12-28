217775f9c7c37037e8e0fd541286709cdf0cc949
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:14:27 +0000

    harm within middle can instant

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
