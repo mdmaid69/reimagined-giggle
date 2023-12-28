11a15fcc06c51672418fabbcd1d87720ccc0edb4
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:21:29 +0000

    interest split fact please over

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_min(lst):
+        return min(lst)
