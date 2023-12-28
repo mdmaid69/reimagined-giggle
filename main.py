a628139724045d575a7a89c7c326fcf4cde13e72
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:13:01 +0000

    prepare safe callback before space

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
