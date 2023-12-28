c6490393014011ffbc9115f0f723b2ddbc6a1296
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:54:31 +0000

    guilty long stick closed food

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
