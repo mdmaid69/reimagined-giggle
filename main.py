2dc85d495a73dfb35c251671f4b39dccbe03c61c
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:56:39 +0000

    artificial easy school name himself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
