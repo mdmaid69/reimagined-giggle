fb7e09baf1aa0aa60db570ac32ab27dbcdb376a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:11:58 +0000

    runinto formal pity juice concerned

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
