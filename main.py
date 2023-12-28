738f99fb02c43041c8b9e6d5f55d43da3418d5d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:56:15 +0000

    some become treatment entry suddenly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
