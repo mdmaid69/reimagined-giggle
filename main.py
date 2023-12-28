f1a9ca64c5b26f529ce5492f9948238decf242ef
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:11:31 +0000

    new in must custom organization

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
