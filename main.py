8e2da172b3d3658baafcffa0771d169177ccd301
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:13:25 +0000

    situation trip past pause effort

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
