d1694f60a5dae0eb4b7ce3a6592fd12d0615e6b7
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:53:28 +0000

    put luck Ii union university

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
