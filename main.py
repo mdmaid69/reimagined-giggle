e1dcde195e4769b141eb7b694ddaa052277fef8e
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:58:48 +0000

    various sign rise shout 565end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
