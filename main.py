a4b755f4426a141d136dd0fb760cc07de4b07384
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:50:37 +0000

    beat minor march check pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
