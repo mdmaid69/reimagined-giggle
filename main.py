543283a9c6af18710614ef857d8ebd9769e84d97
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:47:12 +0000

    go dare natural spill fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
