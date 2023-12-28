c52d07fa5c9de1648ecc9fd36c88a89c00baeaf5
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:18:25 +0000

    later almost neat so whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import sys
-  def get_python_version():
-        return sys.version
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
