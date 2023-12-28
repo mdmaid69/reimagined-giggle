3c6f8151b3302478fb2ba6f0d99d2e9c4b38c2c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:04:01 +0000

    meet tonight respect owe trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
