e54fcc3339c6adbc659a041090ac7cb69d767f43
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:59:38 +0000

    boring glass custom bet dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
