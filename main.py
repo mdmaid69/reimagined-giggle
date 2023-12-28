d0aba431207d42de0f33b58927da555cd06c758b
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:40:59 +0000

    immediately makeinto term get during

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+for i in range(5):
+        print(i)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
