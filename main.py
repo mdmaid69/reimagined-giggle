c0a53210673c6fb4c4f16853877f2a7b02c3c644
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:38:45 +0000

    179bite play declaration complain sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def append_to_array(array, item):
+        array.append(item)
