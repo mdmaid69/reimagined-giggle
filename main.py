e1b6aadb25cdd23fb6dde12fbd8d2a8b09ed906b
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:39:31 +0000

    letter needle love history dangerous

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
