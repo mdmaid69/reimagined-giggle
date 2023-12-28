51b205e2031cbd587aff93814cf39f7bb9fe61cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:39:34 +0000

    regional life equipment government empty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_pressure(force, area):
+        return force / area
