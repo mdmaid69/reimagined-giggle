87cf232fcf0170f951db2eec5e48df7a47e5eebf
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:23:56 +0000

    me display dull prevent dull

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
