394b0b5cc666cd57f2cce83589abeda538aa326b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:28:07 +0000

    mother fork lead Oo correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
