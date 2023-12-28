5a67714649a6a7fbb99c16e7fc21b61c3dacd3b8
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:27:18 +0000

    entry frighten whatever mend table

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_acceleration(speed, time):
+        return speed / time
