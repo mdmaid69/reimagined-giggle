58fac163bb0b03b00c4710d7144e42e513c78fba
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:36:12 +0000

    defend pay friend hurry choose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
