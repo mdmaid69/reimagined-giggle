4d5378a3a3156aec8b8d224492aeae89e67ffc76
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:53:35 +0000

    565end name feed proper sea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  def sort_list(lst):
+        return sorted(lst)
