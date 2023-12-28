6bba175b4b31acc6f5cb2df8afd48a7a7422ed58
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:51:55 +0000

    stupid answer remember opponent boat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
