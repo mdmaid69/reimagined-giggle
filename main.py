0657259a6e51ed9554db49298e11a6045f8779c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:15:08 +0000

    base yourself goaway mine apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
