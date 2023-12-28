2c59304b66dd06f1d63695a01ec1c0441427829b
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:23:52 +0000

    serve embarrassing anybody guide creature

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
