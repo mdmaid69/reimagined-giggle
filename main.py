1d6e87d0d5f92bc63b50e41efca05c857205f850
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:08:07 +0000

    patient Ii best whoever drawing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-print(sum(range(10)))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
