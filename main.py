25cac2566bac6e7f389215b49f7b88273f58f863
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:33:07 +0000

    callat stand pinch necessary of

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
