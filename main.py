7910554a240842512c123d2db8d763484bbb3341
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:49:34 +0000

    train supply nervous programme long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+print(datetime.datetime.now())
