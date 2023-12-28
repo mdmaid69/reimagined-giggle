b9782bf2b8593da7488d68b01c9043ff46a73686
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:51:53 +0000

    you principle sink war city

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
