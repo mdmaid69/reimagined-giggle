c2173701ec59fc85f3092d0167ae2c189b49f6a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:32:53 +0000

    acceptable arrange influence mystery noone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
