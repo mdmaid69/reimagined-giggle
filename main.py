b8f988bdda79843d29075bf06ef93137bc580940
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:53:16 +0000

    priority major criticism ashamed shock

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def sort_list(lst):
-        return sorted(lst)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def reverse_array(array):
+        array.reverse()
