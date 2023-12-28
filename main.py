713b3b243d080fe0942718664395845ad15ac1c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:34:44 +0000

    correct regard runout previous machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
