d5623b131579015640f01aed86b7620069081727
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:58:21 +0000

    post play team stretch luck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import tensorflow as tf
-print(tf.__version__)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def subtract_numbers(x, y):
+        return x - y
