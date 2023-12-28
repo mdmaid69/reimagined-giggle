35539cdd7e109fa22cb275b2f54972491a6b309b
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:26:12 +0000

    answer apologizeapologise partner mark loan

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
