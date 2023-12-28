3a03587795971d0b2dc2d775c539beb1f4e4b287
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:18:52 +0000

    pen dead strong slip 570enjoyable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  def add_numbers(x, y):
-        return x + y
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
