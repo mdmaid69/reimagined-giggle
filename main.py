fa7f3cf295e57836f9118dc39548eea1357a9b0e
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:35:57 +0000

    various obtain delicate cry thankyou

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
