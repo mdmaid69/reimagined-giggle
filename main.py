e8aa02236b52be03e5e1044c22578e8ccdb9fff0
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:48:34 +0000

    attractive need ring fierce doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_average(lst):
+        return sum(lst) / len(lst)
