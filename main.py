bc16ab12c8a9aee5c0a091deca4d49ef58b18c68
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:37:40 +0000

    opposite recover telephone health whichever

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_max(numbers):
-        return max(numbers)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
