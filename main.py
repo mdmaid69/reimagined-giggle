96a97df186629705fecc42da3f51b89779796aec
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:32:58 +0000

    neither persuade circumstance reasonable pile

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
