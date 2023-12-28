8600a1f3dd0df8cc25b0a71fcf03686f569ee0f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:10:27 +0000

    luck exploration crowded his tough

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(lst):
-        return max(lst)
-def count_characters(sentence):
-        return len(sentence)
+def calculate_pressure(force, area):
+        return force / area
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
