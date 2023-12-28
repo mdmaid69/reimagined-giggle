76fa8e1006e169f425ddfb2fa9752437b8e36d24
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:08:08 +0000

    hungry satisfied record borrow getup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
