28f0d25704fcd33998adf3fd0d5a3aac33936c11
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:09:26 +0000

    whom stop branch this bringforward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
