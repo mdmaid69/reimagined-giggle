fb79d4d452f8b8fa68922942442ec31807f44edc
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:55:59 +0000

    flat raise system wish shopping

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import logging
-def log_message(message):
-        logging.info(message)
+def is_palindrome(s):
+        return s == s[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
