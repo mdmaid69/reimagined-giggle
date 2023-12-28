8c0da414852793d065b4a4b559355a0196783a9d
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:01:59 +0000

    become different sky doctor medium

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_power(work, time):
+        return work / time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
