2342d7c19edfef6cbd7de6c417d521a4e28e175b
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:13:30 +0000

    notice middle short exam hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
