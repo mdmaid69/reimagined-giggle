7c0b09eed4970d66e7f96ac791715fe08dae6d18
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:20:43 +0000

    bite sorry kick detail deliberate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
