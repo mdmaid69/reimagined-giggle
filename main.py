a90ccec8f17e27959033f340e65e14fb005988fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:08:40 +0000

    obvious seriously afford main hand

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import platform
+def get_python_version():
+        return platform.python_version()
