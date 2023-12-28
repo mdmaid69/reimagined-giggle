9ac7b2dea5b13bfcec18520bc5bad6ead16bd19d
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:43:34 +0000

    right computer surface machine traffic

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  def square_number(x):
-        return x**2
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+for i in range(10): print(i)
