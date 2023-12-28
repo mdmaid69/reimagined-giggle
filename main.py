4763cdecff9606454523444a2f9355d9f25b3663
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:15:20 +0000

    guilty pupil classroom embarrassed hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_item(array, i):
+        return array[i]
