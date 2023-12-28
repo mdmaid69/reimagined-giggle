d936eba149cc274c39daab65bb4e6d379d5a0907
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:17:42 +0000

    angry punishment private favourite trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
