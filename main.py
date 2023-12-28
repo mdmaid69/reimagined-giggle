3bed3d816bb633794e70093d0a66de872a3c7584
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:04:03 +0000

    account only path scare defend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import os
+def get_current_working_directory():
+        return os.getcwd()
