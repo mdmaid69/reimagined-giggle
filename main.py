d93b0507670715926a2f7eb63f05f80d8d242bd9
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:19:29 +0000

    to deep loss in warn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def convert_to_octal(n):
+        return oct(n)
