eba0f144dd9285e9f29f18d28d9c36c7cce3a9ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:44:30 +0000

    forget impatient century shut commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+text = "Hello, world!"
+print("Characters:", len(text))
