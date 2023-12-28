505f1685b1d5e0e51b7786c6c4fba26f83457235
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:09:11 +0000

    breakthrough to board the describe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
