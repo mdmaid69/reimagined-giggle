72a00e22fe11ca3a6e34a263e654bf5ed397c149
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:54:05 +0000

    unit immediately shoot perhaps factory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
