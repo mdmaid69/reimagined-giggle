de0c5950995ac8e108befb829b437e14b71e76c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:48:24 +0000

    strange instruction shock sorry standard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
