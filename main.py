2f9e71d384798ec9eab2ba2e8c9040925dcc8aef
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:28:48 +0000

    public holdout brush guide government

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_exponential(x):
+        return math.exp(x)
+import array
+def reverse_array(array):
+        array.reverse()
