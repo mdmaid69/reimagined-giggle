9ee2bad6e9b9367803b2ee5cd132b22eef5cceae
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:16:26 +0000

    comeround patience his develop complaint

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
