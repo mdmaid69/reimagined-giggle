1b789d99ede1e16c44c1c2fea8ab108e9db6b210
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:55:01 +0000

    pain bored century impossible open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+from collections import Counter
+print(Counter("hello world"))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
