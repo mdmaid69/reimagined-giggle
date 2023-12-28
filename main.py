f7cec558b066cc9ad2d9d6aba01df6e6c5252a2b
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:30:30 +0000

    near careful angry content intend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
