04f079c9614625d374b57bf79a03e8d6dcf719b9
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:43:33 +0000

    change reaction getout disappear talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
