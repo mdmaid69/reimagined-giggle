4b31c634fc90356bfc972a1b78cf300890215f07
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:56:45 +0000

    him scale compare effort shake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def add_numbers(x, y):
-        return x + y
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
