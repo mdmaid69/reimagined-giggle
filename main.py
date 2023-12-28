7a903ad60eeb10b73ad4eb16e3d48c4cb3c812fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:37:47 +0000

    allow fall zero back form

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
