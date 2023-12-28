78d0954b5d725570cb4ed3bb4583cbc13664829e
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:38:37 +0000

    shade law valuable excuse neat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_set(array):
+        return set(array)
