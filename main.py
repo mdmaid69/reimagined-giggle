7f8b387065cb691388c85dc44ba410a8ee3392dc
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:34:37 +0000

    there shoot guess cover anyone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
