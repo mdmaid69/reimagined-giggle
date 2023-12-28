2bbe58e16a14946492b7bb9e48db00197227d6b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:48:39 +0000

    sweep mean human spring mix

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   def is_odd(n):
         return n % 2 != 0
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
