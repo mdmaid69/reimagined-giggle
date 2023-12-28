136975a8b79d15a79179755031c40286b6aa9295
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:57:18 +0000

    ocean wheel freedom Yy season

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
