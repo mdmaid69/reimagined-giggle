a323a297187d6e95043fe336684d68bf4f2f50d6
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:37:07 +0000

    deaf share plate contact minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
