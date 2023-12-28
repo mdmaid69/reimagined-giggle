f870e99df35b219bbfa559325560c911efb8f7f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:31:25 +0000

    black fortunately view blow suckme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
