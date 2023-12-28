f93c5a8dd28173064357292c5a5a53c667319a80
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:22:30 +0000

    good evening squeeze sign secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_acceleration(speed, time):
+        return speed / time
