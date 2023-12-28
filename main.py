93b7f2cabc2c087dcf823f88c7986dd21e5e2437
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:20:07 +0000

    tell table sign night cum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
