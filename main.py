67801ecdc29f0bd440bec5d16213d2019c4af0a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:32:04 +0000

    mistake physical finish corner nurse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def convert_to_octal(n):
+        return oct(n)
