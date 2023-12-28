19f76e757329c06fbc4775aef85c57d6c917ec31
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:16:19 +0000

    on slip service road quantity

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_index(array, item):
+        return array.index(item)
