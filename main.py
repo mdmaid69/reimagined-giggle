710d4a304357e6c38fc7b1129ccd10dd99c3224e
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:12:05 +0000

    project clue paper address act

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
