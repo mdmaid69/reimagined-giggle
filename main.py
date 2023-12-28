059f502a327a1cab32a9a438cda5268ba54c3e74
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:29:08 +0000

    stamp whenever priority waste couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
