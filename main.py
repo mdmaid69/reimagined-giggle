89712ac6e4da840f3cc8d6d835df2f350c03b834
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:42:23 +0000

    blame grow makeinto obvious old

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def multiply_numbers(x, y):
-        return x * y
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
