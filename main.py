4a79bdf57898c6d5b7e7b586180daaff35a11527
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:45:16 +0000

    connected man plan reserve join

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_item_count(array, item):
+        return array.count(item)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
