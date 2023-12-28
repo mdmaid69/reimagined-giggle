69b3bc8f3b889912642f0b9808a5bb621da1736b
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:12:06 +0000

    urgent measure quantity may arrange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
