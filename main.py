f785269aca1f5d24b68a7ed31de2d811499cf662
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:39:45 +0000

    because go comeround common delicate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
