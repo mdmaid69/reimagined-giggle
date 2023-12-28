264d548e4eccae223766c82a0ae4c9911a636ddc
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:55:11 +0000

    argument try stage record worry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_base_name(path):
+        return os.path.basename(path)
