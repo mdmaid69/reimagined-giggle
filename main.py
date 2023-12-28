95032e44babffccf5f71f894dea39331e53ecccb
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:22:01 +0000

    division disturb belief flat sensible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
