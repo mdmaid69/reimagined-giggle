c8f3f90e2ad4a62ad5396f124004f7481c3455bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:16:51 +0000

    nerve identify callat careful satisfactory

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
