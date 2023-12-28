df4c1b76bd009f3be3b36c9346b5f0fd491bef68
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:24:20 +0000

    boil list Gg less trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_as_bool(array):
+        return bool(array)
