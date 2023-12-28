1f7cb6ad50c4e7c7cba9b36bf7a8ff4c500f2021
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:42:19 +0000

    positive on bush safe face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
