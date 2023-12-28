aba43eb0264c03ae44150a91bf2f7da20ae8541e
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:40:41 +0000

    wall give hour almost constantly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
