1123db2aace6cb7e316f13a2dd30c9c5f7ce5afc
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:13:47 +0000

    shake possibly work quit part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_power(work, time):
+        return work / time
