3400df2a5ff28e14b2a7aecc93acb30f1a4abf10
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:19:33 +0000

    measure actual under forget temper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
