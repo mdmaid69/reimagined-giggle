8c28ba941f30856c376e368d6cd9cc66b5d6526b
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:06:00 +0000

    accordingto autumn track tip whom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_as_list(array):
+        return list(array)
