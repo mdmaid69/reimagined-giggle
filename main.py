a79ba8d8a9c2ddfe256a42fd1a9e9c6558e19524
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:10:26 +0000

    rent computer style boat together

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import datetime
-print(datetime.datetime.now())
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
