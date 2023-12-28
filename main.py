f17da47cf8c01186a62637197a3a2316235d0735
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:55:58 +0000

    amount close deep sweep shine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
