ba78b42c21f7d4bd9c33546ab6cc3c2afa3f4c68
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:45:13 +0000

    keepfrom behind schedule person Itself

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
