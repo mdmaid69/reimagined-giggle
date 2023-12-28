fa1fd1fdf52ff4392c181da6f5274857dfeb8304
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:01:01 +0000

    store weather age formal brave

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        return sys.version
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
