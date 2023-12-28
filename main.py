a8510cc60cf2696266bdf3e54a03adb409c2eabd
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:35:45 +0000

    block opportunity fold present improve

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def cube_number(x):
-        return x**3
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
