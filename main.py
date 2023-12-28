443540cc9aeaa6d6397dd6da7d0a8f60b2cd71ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:50:39 +0000

    top boat tight at water

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
