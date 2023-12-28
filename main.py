37b25a4acb7b691d5e7ff61a0cbc3f845f3df418
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:36:02 +0000

    century contrast large average mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
