c16c505be97afe97692016c0a9aa2df88bc83543
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:33:25 +0000

    culture surface appeal disturbing breath

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
