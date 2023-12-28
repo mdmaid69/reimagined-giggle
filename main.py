e710be3cff0141cfe1db72f55b6da838097e62ff
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:37:10 +0000

    unable destroy charge 566end damp

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
