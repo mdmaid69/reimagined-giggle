a320b92a2b3bfc605e84a487aac0ee9deeaf2f23
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:52:53 +0000

    worried reply variety blame anyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
