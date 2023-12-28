5644a4b63ab5ebf563bed0df79e892e213388e82
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:01:38 +0000

    future everywhere wrap respect arrangement

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def is_odd(n):
+        return n % 2 != 0
