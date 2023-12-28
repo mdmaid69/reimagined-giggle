b990d4471b47926c0a7fbda0e086988e566b69a6
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:16:34 +0000

    then recover exam this paper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_str(array):
+        return str(array)
