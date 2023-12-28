5a9bda1f2cd5c311b837ac7691a871b2e5d99cb6
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:57:10 +0000

    do organization day every hour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def get_array_as_bool(array):
+        return bool(array)
