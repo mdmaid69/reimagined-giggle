c0c72add553ef1277b895942f8121067fef3aac0
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:47:57 +0000

    achieve anywayanyhow excited trial borrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import collections
+def create_queue():
+        return collections.deque()
