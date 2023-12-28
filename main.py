b17f56541498759f0a9a6c4a5553eaddc4137a35
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:54:39 +0000

    picture instead shut year promise

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def multiply_numbers(x, y):
+        return x * y
