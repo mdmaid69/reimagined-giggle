5157f69acacc766bfa4fb80cfc290f3f8f4a4f60
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:51:09 +0000

    guess stupid gradually cheap write

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
