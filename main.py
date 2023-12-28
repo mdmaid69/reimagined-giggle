897ae19de6e4d4830f03169df7f22ec66425c268
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:59:07 +0000

    tune raw fortunately slide noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
