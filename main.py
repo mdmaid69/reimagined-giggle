295cbf0157aea16e0199afea15584a151159be5d
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:56:40 +0000

    grade face people combination you

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def find_max(numbers):
+        return max(numbers)
