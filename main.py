7c4f9c51dc6c353097b70b1df1202ed8284a6c63
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:09:21 +0000

    turnback gap separate attempt favour

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import platform
+def get_os_info():
+        return platform.uname()
