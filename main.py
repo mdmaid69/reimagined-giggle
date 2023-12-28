5661dae3122d1bdb9a86d4203878f15193e404c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:23:12 +0000

    artificial real maybe price score

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+i = 0
+while i < 5:
+        print(i)
+        i += 1
