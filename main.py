2897d196bd707bdfaaedc1762831602ac272b52a
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:58:21 +0000

    joke 567enemy gooff yet since

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_power(work, time):
-        return work / time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
