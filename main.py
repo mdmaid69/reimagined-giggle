0abc613be26eae0c7051a200802cd78a8311d2c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:16:42 +0000

    release between female twist pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def append_to_array(array, item):
+        array.append(item)
