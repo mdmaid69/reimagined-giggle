970f1429536f5d6bf5735fca1d195c547e83527c
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:28:08 +0000

    punish while taste statement moon

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def convert_to_hex(n):
+        return hex(n)
