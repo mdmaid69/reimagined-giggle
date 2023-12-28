34205ede4354e9680ff67e035221044b2a89daab
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:31:31 +0000

    comein difficult otherwise search correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
