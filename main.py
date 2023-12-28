3ca682baecb6adb9893bec7dd5d74d02131bed7e
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:04:03 +0000

    stare count apologizeapologise tidy regularly

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
