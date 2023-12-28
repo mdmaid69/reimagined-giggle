d334e828cb0edca2a6a3feabf79c08bcff6e6015
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:09:05 +0000

    every cut goout sweep impressive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import collections
-def create_queue():
-        return collections.deque()
+def create_user_dict():
+        return collections.UserDict()
