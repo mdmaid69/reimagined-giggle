a08e19bde7ea59b1ffeece5af24b3803bfdf7148
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:08:22 +0000

    welcome cool only cry collect

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
