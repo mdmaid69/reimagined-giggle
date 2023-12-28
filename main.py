ecc14f9c129dff300b80f41bfbe79d7bb214e8df
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:46:18 +0000

    final blame morning thankyou sensitive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
