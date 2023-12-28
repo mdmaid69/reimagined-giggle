a6240e3bf6388aa64d963659b92474ec640539d4
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:21:49 +0000

    genuine rain coffee home fat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
