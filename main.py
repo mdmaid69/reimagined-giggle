bf43878488a1b9b2017281fcda06b01ca426860a
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:22:50 +0000

    cheat offence top control mainly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
