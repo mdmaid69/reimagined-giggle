66a7c32adced16c0212132ac2e8f7e8bc7d4d360
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:17:59 +0000

    equipment bend respect represent whole

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
