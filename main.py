d5262c94c58ab46c5216a479e54da4b0a643e37e
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:50:07 +0000

    go survive slight instrument when

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import sys
-print(sys.version)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  def count_elements(lst):
+        return len(lst)
