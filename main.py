ea75538a27b7c16f9af479296435cec0bf18e098
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:46:50 +0000

    Ll getaway guilty private wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def greet(name):
+        print(f"Hello, {name}!")
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
