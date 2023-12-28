56b28882eac5f7ab4400206c710a46540cb50533
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:20:09 +0000

    swing unlucky search woman expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_directory():
+        return os.getcwd()
