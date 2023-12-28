405da92389c87cbb9e15f96e526326c955011eb9
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:15:56 +0000

    foreign care ought here major

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+name = "Python"
+print("Hello,", name)
