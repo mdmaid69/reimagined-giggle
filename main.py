33e2b08ea0e991e054399e79ce57baa645f5fb5b
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:27:12 +0000

    hardly pool fail sick tooth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
