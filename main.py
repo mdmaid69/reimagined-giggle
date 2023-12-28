29ac5cb511f45b5eb3c7ae62b4f9cf76e1bc57d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:51:00 +0000

    suddenly Hh basis chase wave

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_item(array, i):
+        return array[i]
+def square_number(x):
+        return x**2
