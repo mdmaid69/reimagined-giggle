ac4ac778cf54f27092eb6c84a3af2307b33fb978
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:49:49 +0000

    pity cheap story ahead company

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
