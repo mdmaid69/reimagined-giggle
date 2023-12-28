94a256c68eb0f0e38418ce6682d49935cef9c3df
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:50:34 +0000

    lead stamp myself calmly journey

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
