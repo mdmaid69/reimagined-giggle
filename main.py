f6acfa92cea13168f93fa1641c14ec204714870c
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:58:23 +0000

    lower belong spot grow total

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_octal(n):
+        return oct(n)
