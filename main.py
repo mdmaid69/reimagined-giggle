ecd93e64ecc057d6c91e776ce548c889c07f9ff4
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:57:50 +0000

    mass fortunately off achieve invitation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
 import array
-def get_array_as_str(array):
-        return str(array)
+def reverse_array(array):
+        array.reverse()
