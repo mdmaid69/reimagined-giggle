f90f35c594a1c4cbbfef45bd825e82360ae11cfc
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:42:53 +0000

    scene getout demonstrate apply basis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  def square_number(x):
+        return x**2
