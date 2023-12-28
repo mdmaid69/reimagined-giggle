2d13d2993a7f5cbd58172933d054a27999b846a1
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:16:22 +0000

    majority imagination stretch thank yesterday

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
