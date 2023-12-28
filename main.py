af9daeec23e67b6bf2c1c76106cea31d03eeb597
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:29:38 +0000

    proper familiar violent nigga bad

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
