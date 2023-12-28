5f9bb63989e90627a817ffffa01257bfa918f57e
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:48:36 +0000

    its create general charge loss

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
