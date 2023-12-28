fc180f7051a3d1a580f3d2010397c00b35febd92
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:16:07 +0000

    harm appeal yes priority about

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
