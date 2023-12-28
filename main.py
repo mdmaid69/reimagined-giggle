f3491d87d0d4911c08939304e91f223baf95a4f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:59:12 +0000

    consider clever even year more

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def square_number(x):
+        return x**2
