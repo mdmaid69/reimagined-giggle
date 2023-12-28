c5bebef52f144a66eed306b05c83341ec664a07a
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:21:06 +0000

    uponon course instant car dinner

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
   def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
-for i in range(10): print(i)
