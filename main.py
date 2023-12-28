6b70e76b815eb0a305bfdcab0c06cfba53f90c71
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:25:04 +0000

    accept like ago once strict

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
