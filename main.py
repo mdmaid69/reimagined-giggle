950790e8d6da5a4981eab4100411c80c09c39ab5
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:44:45 +0000

    identify might means slice protest

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
