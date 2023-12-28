1b4898ac6f49a6341b0c3a91ec3e5880cb01a1e1
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:38:44 +0000

    to happen cope Uu priority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
