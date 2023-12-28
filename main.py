2462db63a320b0ab10f3047a99178053b515ee04
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:50:59 +0000

    round their draw seat keepoff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_density(mass, volume):
-        return mass / volume
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
