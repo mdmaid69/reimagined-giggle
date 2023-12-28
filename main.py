b9a18f4bed36327150e0027370d98d01f420ea9e
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:46:54 +0000

    sure rule value difference even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+x = 10
+y = 20
+print("Sum:", x + y)
