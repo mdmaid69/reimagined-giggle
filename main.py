131de8851048e118045e05a65da4d6be92432bef
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:29:30 +0000

    yesterday Kk geton today will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_binary(n):
-        return bin(n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_as_float(array):
+        return float(array[0])
