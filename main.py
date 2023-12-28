891b6b4c37fd7c4b0931ac5449ea2f161df0d519
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:23:12 +0000

    trouble rise team general bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
