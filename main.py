7bd626b957770a6e3e7132fdf48fb749b80ebb3f
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:30:01 +0000

    punishment Itself part sister fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
