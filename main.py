060df669c15a777bec55e8462c7194565ab08700
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:05:02 +0000

    active word post board person

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def find_max(lst):
+        return max(lst)
