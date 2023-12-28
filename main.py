9917e3934ce83347dbb35ef8b6ead9861efa48d4
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:47:00 +0000

    soon on left disagree sail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
