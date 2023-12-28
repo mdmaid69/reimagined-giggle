1d8f6da860a1bb0aae782c06f89700812464bacd
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:19:03 +0000

    escape on copy acceptable constantly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
