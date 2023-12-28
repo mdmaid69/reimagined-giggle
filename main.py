b7c93d998326d88dc4a16218369f06ade9e581a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:12:06 +0000

    rare cry passenger sound evening

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
