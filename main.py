606e212de01876fd65729bef9e7d6ecccba0bf73
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:09:06 +0000

    care window since opposite hope

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
