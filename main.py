baed5f6048f162d732d11972dfd27517ee4939f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:30:57 +0000

    greeting dry uncertain off best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def split_path(path):
+        return os.path.split(path)
