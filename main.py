6fb8dc31dee0e9d2924885bb5b00316332f5ff70
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:24:31 +0000

    happen last violence contain degree

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+from collections import Counter
+print(Counter("hello world"))
+  def square_number(x):
+        return x**2
