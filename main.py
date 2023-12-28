85d2de2da64d2397fff39cabd59f558bd5aee62c
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:50:21 +0000

    cross crash activity Ss though

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
