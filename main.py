e1c01f609768603515946a62295246d556bccecc
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:11:02 +0000

    where giveout suppose hope cap

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
