52274f2748c1f5a6538221438e9b22ffb36e02cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:32:54 +0000

    destroy nonsense contrast unhappy punishment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
