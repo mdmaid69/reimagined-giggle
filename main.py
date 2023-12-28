f6f91f18d73ab87b83b24c131be1c5a33169884b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:28:03 +0000

    cheat post low OKorokay break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import time
+print(time.time())
+import time
+def get_time_since_epoch():
+        return time.time()
