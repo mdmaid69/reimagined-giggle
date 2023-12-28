ec3df2c5359657778057846ca40879783801d44d
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:01:28 +0000

    chase put lay stare down

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def add_numbers(x, y):
-        return x + y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
