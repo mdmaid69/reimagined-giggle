38c91f712df2c363f0d72db56004b7a88761efa7
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:20:42 +0000

    bother rid strength scare combine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
