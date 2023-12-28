a8d94a63427234a7ddab4246c42b58a60c8481bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:22:24 +0000

    takeoff film Cc serious otherwise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
