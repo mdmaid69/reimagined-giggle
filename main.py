5f77441f36ceba5230d856ade6170b041e5900cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:49:28 +0000

    away fail speech news female

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
