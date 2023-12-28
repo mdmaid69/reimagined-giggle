461605db36c43edd2b452b0469c440913d895e60
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:36:31 +0000

    justice by glad disturb possible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
