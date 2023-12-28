46f9c331ef1c798e70a7cfbca168d2acdcfefc8a
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:21:33 +0000

    free onto whistle during this

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def is_even(n):
+        return n % 2 == 0
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
