3476742fe8a764098f772e2fa167da3e6084cf44
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:18:13 +0000

    fall 572enter please defend left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
