1e77887d35d08efb4d25fbe201280ed92c004b11
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:49:07 +0000

    picture yes trap politics relative

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_power(work, time):
+        return work / time
