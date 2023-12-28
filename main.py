4a51f4271427397707b2921a074c1bc8a7685c13
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:28:41 +0000

    joke length him brush direct

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import random
+print(random.randint(0, 100))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
