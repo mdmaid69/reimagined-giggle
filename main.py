d59e846ea114466218832ec13afc88a8d43e50fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:26:54 +0000

    string record by grow offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
