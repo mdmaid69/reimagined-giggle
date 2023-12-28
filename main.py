5fe49fe1810cfad08004d014df7a15405324900d
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:56:10 +0000

    offend any production where dress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+for i in range(5):
+        print(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
