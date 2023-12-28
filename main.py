b683e224952b96b4cd966c63ce26c0ba9396616d
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:39:16 +0000

    pull children steep joke obvious

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import datetime
-print(datetime.datetime.now())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
