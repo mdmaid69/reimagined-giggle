4b5641c38ff4277a9bddd0b6b11ad45390c91b9c
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:30:54 +0000

    dream amount trick evidence surprise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import numpy as np
-print(np.array([1, 2, 3]))
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
