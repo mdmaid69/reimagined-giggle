11afbd728f0a4a0845d6cd86b79b5c58573fbaad
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:47:34 +0000

    thirsty regional right go performance

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
