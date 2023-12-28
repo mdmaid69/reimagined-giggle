a34f1aac504461c4cb2a2b268f4a25de545dac12
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:57:00 +0000

    refer experiment hole add see

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+name = "Python"
+print("Hello,", name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
