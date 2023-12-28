9ce20ded6430d2f98619b7fc8b814cf3a59f9824
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:26:17 +0000

    rise gradually stop finally sound

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import os
-print(os.getcwd())
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
