dcfd7efadd734413be91c86f4c3cf1d4bbe898f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:08:17 +0000

    reject beside turnon tie market

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
