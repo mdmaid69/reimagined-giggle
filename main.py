99cf6faab329f0a90b913b973cd7ee046e922ce2
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:42:35 +0000

    equal range 568energy post open

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_logarithm(base, x):
+        return math.log(x, base)
