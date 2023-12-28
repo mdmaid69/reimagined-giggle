eeb8faea6baeb2481f26db6c842ea064de67dca6
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:13:30 +0000

    spring support prize wing somewhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
