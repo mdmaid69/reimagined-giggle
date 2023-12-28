09fa7a83e58d93ac94fae6c363cf7ce1d68983ba
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:38:37 +0000

    hurt connect obtain display properly

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
