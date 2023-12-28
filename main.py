88c85141ea5161eae0b94084a7f106d465c5759b
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:25:01 +0000

    turnoff past declaration equal arrival

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import time
-print(time.time())
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
