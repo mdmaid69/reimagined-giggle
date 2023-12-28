34a73da54a095476dc82981f009b0e48f38555b3
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:10:06 +0000

    bye good so join opposite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
 def get_array_as_memoryview(array):
         return memoryview(array)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
