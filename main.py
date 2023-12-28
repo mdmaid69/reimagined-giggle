c8af3dd8d3b765eab0e58b549e529c186482c5e7
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:49:07 +0000

    attach run miss off OKorokay

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
