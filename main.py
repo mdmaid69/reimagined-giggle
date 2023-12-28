d930bb023255790c83793559d17f15d87853d650
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:12:30 +0000

    in some old illness failure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  def sort_list(lst):
+        return sorted(lst)
