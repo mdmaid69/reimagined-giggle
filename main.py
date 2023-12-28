84e0243f7ad457a584b2539e3603b331ac33b17b
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:59:14 +0000

    distance influence case plenty recover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
