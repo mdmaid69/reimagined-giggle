b80b7d18ff23c8385dbd611b329fc330a1d7db02
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:14:14 +0000

    notice tough try by comeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def sort_numbers(numbers):
+        return sorted(numbers)
