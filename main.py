461513a127ae0f32266e0951025379bee580c177
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:16:46 +0000

    write regard provide angry calmly

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
