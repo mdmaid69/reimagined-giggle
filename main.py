ae97bffbcbe379c3c711232d044d81da77e948db
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:12:57 +0000

    before unkind raise hide unhappy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
