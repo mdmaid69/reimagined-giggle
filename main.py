7748f899ed44ef67c87f9e3dd89a21084c4876bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:11:33 +0000

    Tt exist attractive correct worried

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
