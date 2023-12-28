0fbb73dad2e0a51493c8ac5b8e8387601d9eb730
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:45:29 +0000

    odd workupto politics play country

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
