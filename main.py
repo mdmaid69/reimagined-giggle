8b47a102e2f4761460958e838d856df1e8fa82f6
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:43:21 +0000

    advantage bush chin if beginning

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
