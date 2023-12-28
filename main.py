acaaab6bc5a7dee3fcf519cc04cd3c91969176f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:24:42 +0000

    roll introduction figure eye clearly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-print(time.time())
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def sort_list(lst):
+        return sorted(lst)
