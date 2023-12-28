13abdeb7579c4ad53c761aa154428fb46445d28c
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:11:04 +0000

    low accident fork even conduct

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
