e6efb969ada01cc92b74ad6e2d510dbb7740b287
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:23:07 +0000

    tend earn exaggerate pick seriously

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
