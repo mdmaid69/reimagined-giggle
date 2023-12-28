7e8876a1da72b079c32560a156fcb7058165dd8e
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:22:58 +0000

    correct exploration tomorrow cap creature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
