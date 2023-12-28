b7e0fbc596c3dd3ef17b3fc6e3eb5860d9fa2379
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:34:57 +0000

    few congratulate tight better nature

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
