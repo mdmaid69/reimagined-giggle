4416aac4e07f7dd8161eb870e36fca55d72634e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:30:44 +0000

    clean production Qq zero row

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+x = 10
+y = 20
+print("Sum:", x + y)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
