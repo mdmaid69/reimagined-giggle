b490bb6b38a3216a433f34120146d8779ce63c2c
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:08:57 +0000

    turnup plan permission measure 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def get_current_working_directory():
+        return os.getcwd()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
