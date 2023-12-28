fcb9c45919ac594c17e974650090ad5b4f3633ef
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:12:22 +0000

    talkinto often fortunately spoil fun

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def reverse_string(s):
-        return s[::-1]
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
