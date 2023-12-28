991687d00bf1a169843aaf4f45b05b2845abaf5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:23:55 +0000

    criticize destroy Ll picture hardly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
