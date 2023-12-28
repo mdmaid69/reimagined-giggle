b01f343846d3cc8815163fea7d230bd4c2668868
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:41:32 +0000

    short rid also below sense

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
