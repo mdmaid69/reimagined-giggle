3bd19de053e8adc68cdaf9c7d5846267a35b934b
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:41:10 +0000

    he rich appeal over gas

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
