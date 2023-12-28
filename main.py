ec45eea36cc8c95d8c74893e1cd404a13f051ba9
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:01:49 +0000

    condition language sudden dinner personal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-print([x**2 for x in range(10)])
+import random
+print(random.randint(0, 100))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
