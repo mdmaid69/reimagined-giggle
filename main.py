bb9757c0086cf05576db4ba87c5aa6abbb37a619
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:25:06 +0000

    lead either same who alive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_current_working_directory():
+        return os.getcwd()
