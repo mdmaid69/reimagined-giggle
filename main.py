5a7dda57df71211d49eb3c991a8e3ec463f3d0ef
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:03:34 +0000

    along emergency every wind nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
