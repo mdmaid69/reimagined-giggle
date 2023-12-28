8934c3c79ff6a3284c3cba1164c967a0ce1b0c37
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:23:25 +0000

    goup curious foot towards case

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def count_elements(lst):
+        return len(lst)
