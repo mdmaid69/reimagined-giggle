c9ab0c1c883e14041d092e8223f494f22df8ddb0
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:10:25 +0000

    dry protest well behave nor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
