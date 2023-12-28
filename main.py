656516fe7e6cb63929d3c0ca0ab618530ca3743b
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:21:16 +0000

    involve common silent record cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
