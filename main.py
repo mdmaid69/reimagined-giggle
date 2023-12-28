4e8146422755a49b5f66beeb5e54606155a2aefb
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:22:19 +0000

    fast succeed content shopping should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
