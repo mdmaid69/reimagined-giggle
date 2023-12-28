be1f8c371c7894ab545ceadb59a4b1fa468e330b
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:53:19 +0000

    minority union compete realize nonsense

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
