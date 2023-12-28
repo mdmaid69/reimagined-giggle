c8b9be6c538ae1e184aa2941537d664e05b165c1
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:29:45 +0000

    criticism start age finish temper

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
