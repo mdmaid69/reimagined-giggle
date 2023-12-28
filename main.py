1f54f422928d1e605a9cd1968f4d55d3d485f763
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:01:53 +0000

    many imagine behave matter spin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
