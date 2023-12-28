20c93ba08f86c2ccd582b1a2707eb1ac21ab8c65
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:35:46 +0000

    unlikely challenge cook shut basic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def get_array_index(array, item):
+        return array.index(item)
