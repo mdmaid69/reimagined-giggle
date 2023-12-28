b7b2958fe33c23bce9598bddd55deb2a7096e3f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:55:50 +0000

    improve expression church direct open

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-print(time.time())
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
