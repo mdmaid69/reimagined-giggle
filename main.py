6cd1f628d5c538d5abfb99838696c8553f95070a
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:55:09 +0000

    hear process bag sport light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def check_if_array_contains_item(array, item):
+        return item in array
