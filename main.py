ea36deffeadd45395ee93185e242a3134babfca1
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:00:26 +0000

    accidental mainly single fit length

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_bool(array):
+        return bool(array)
