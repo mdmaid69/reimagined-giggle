75256d0b0531bf06bf9bb48cec9ee2c13754f765
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:59:01 +0000

    wind contact generally rid scene

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
