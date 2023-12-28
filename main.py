03492e297a1b669b8379ffe424d863fe7609774d
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:12:15 +0000

    catch surface separate sight stamp

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def square_number(x):
+        return x**2
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
