0a1239551daaa1976fb350a27dfe958404fc103f
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:23:14 +0000

    drawing runaway course bringdown raw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
