4e8ad34018ffd94fe1ed6db44318bf991911bb97
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:55:19 +0000

    holdout single period pair convenient

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import json
+def convert_to_json(data):
+        return json.dumps(data)
