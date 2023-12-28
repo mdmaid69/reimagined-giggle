eb6a2bc323ad06ba2f3b982fcfcde8f91e6b215f
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:05:16 +0000

    move tap especially persuade afterwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import numpy as np
+print(np.array([1, 2, 3]))
