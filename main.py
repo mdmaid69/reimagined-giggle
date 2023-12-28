6431fe0d2a812c01f64eb3dd6e174ca9dfe7095c
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:51:26 +0000

    interesting plain nowhere comeback trial

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
