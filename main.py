f3a6e9f45afe09011cb1e90635e8b03cb243b49a
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:33:59 +0000

    gooff immediate free hot bend

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
