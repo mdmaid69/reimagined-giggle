1fe2e0b411ed748ad5efcd3f03ecb8a67575bb40
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:01:44 +0000

    watch fit dick spare but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def subtract_numbers(x, y):
+        return x - y
+import os
+def remove_directory(path):
+        os.rmdir(path)
