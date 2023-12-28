9d1b4a1b32a4ff81651b36f2396bca5d14a2b4a5
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:39:32 +0000

    lonely easy kick demand walk

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def find_unique_words(sentence):
+        return set(sentence.split())
