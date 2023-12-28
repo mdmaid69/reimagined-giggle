fdfad9fbf94ea3979be5ef7d54bbe324df8d7176
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:03:01 +0000

    post criticize turnout past side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
