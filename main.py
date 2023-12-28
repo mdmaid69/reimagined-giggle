ac571987606d51022ea338447744c10fcf3cb6d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:16:27 +0000

    lead reduction succeed second warm

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_array_typecode(array):
+        return array.typecode
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
