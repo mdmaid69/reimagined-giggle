b2665efae3965d9126d98c676e549d87dd1f3bdf
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:18:25 +0000

    discovery so instrument base control

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def subtract_numbers(x, y):
+        return x - y
