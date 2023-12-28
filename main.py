5595eabcbf46e93a48e73e54c2306d7f416dffc5
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:49:17 +0000

    dance leg 570enjoyable joke matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import tensorflow as tf
+print(tf.__version__)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
