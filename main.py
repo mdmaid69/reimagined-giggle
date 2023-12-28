db8178600b94b73e6e76a6e96cd0eedc55b54da4
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:49:53 +0000

    buy pretty Zz injury without

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   def calculate_area_circle(r):
         return 3.14 * r**2
-import array
-def get_array_as_list(array):
-        return list(array)
+  def reverse_list(lst):
+        return lst[::-1]
