2d5dd5cc3ecf8a2e4c50026e5bbf56dcd8d40318
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:13:34 +0000

    hospital pass lean couple confident

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_volume(length, width, height):
+        return length * width * height
+def is_palindrome(s):
+        return s == s[::-1]
