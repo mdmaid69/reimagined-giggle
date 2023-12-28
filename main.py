aa909c654a04ea81decdcf91f2784b3802f007a2
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:11:59 +0000

    admire start so load when

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def subtract_numbers(x, y):
+        return x - y
