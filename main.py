7a656941de6d31a0a9be6466f304979ba904d9fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:05:56 +0000

    spite size sorry small advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
