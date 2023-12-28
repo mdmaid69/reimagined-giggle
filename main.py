1c31c5bf70dce8f3472fe9bce2447b4d3308a292
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:06:50 +0000

    love mass concentration pride direct

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def remove_duplicates(lst):
+        return list(set(lst))
