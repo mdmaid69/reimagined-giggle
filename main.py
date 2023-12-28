8733d11f8326343ed771f0f6359d42a9ae239e4a
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:11:54 +0000

    Vv later probably forget picture

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def cube_number(x):
-        return x**3
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def calculate_area_circle(r):
+        return 3.14 * r**2
