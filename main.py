bf8f7c08b7a4711aa5f067b0d63e89284ca2dfd8
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:16:08 +0000

    raise year calloff vote 568energy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
