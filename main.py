ce5b5e1da8c25f7f4f454c182b12a14ed7d7ea20
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:11:02 +0000

    remove mysterious goout afternoon eye

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
