5eb416d2068119742e5b5473cc809f4c9c6fffee
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:44:11 +0000

    upwards standard meaning degree slide

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
