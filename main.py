9a754778a32c87d72dc05a7adbd4c440585be0dc
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:42:16 +0000

    backwards black stone dance spray

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
