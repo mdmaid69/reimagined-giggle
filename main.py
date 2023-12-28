561d7e3ef9a585c282f476c84bf5808622dda71f
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:41:34 +0000

    concern command war beside reply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
