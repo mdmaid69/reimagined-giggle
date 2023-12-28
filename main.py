7c977277f0c15e822d440189f5b7a8c9c6cb13a5
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:11:57 +0000

    surprised cost fortunately attempt door

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
