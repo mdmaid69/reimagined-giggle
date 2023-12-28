517ff953a816565c6a5b73bfdedb028accdff8c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:17:29 +0000

    turnon every taste bill wheel

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
