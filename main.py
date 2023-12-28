08f90c9c099087ecfb857b1a6b73c85f79a16a19
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:17:59 +0000

    correct same spite giveout getup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
