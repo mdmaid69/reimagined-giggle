5107d28bb64425a23222ab2f1e711adf6e1988e2
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:58:13 +0000

    risk amount careful situation club

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
