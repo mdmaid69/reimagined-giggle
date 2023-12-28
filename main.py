45aa33871f04cd20569666ddcc986691abb5ba90
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:37:55 +0000

    offer far exist nose recovery

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
