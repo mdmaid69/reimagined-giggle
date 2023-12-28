31deb51775035d4dc01ee8240e7938c5ab1d8b0a
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:00:53 +0000

    lookaround Cc bill conclusion damp

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_max(lst):
+        return max(lst)
