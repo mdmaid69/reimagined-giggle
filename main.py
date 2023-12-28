230dc09d161ebd11337dfcd874e5a7b3f9678ef9
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:49:16 +0000

    matter object 569enjoy underneath match

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
