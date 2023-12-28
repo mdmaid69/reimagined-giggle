a35839d5730b9b6cda7d625ce441ace4e47295cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:40:38 +0000

    operation married Cc from aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+x = 10
+y = 20
+print("Sum:", x + y)
+import os
+def change_working_directory(path):
+        os.chdir(path)
