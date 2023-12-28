042ba311f08f62d874167082f6b1515cbaf113a2
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:40:38 +0000

    course hurry valuable arm makeinto

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-print(math.pi)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
