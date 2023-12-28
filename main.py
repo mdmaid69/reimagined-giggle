635d30b19d0bed03b022cefe9de329c50ee836c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:49:45 +0000

    afternoon awake tell smell combine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import numpy as np
+print(np.array([1, 2, 3]))
