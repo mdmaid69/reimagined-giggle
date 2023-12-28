fc022d9d270888b86b3b9169e5a0b17bd35bfe60
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:52:33 +0000

    thin watch smooth keepup thick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
