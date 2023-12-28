bc8bcd7c862b036ffbffdb2c4243ed9c2507313a
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:11:25 +0000

    guess cough start general occasional

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import sys
-print(sys.version)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
