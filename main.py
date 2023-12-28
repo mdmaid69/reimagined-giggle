9111149da0cda1ddfc488734e1b251858f843052
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:08:42 +0000

    paper news himself fix intend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_union(list1, list2):
+        return set(list1) | set(list2)
