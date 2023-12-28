3c8fca8e08e061ce51f74cc89be84c899c0a70bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:34:41 +0000

    maximum extremely perfect sometimes scheme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def sort_list(lst):
+        return sorted(lst)
