9fff818906faf3562c68d73ec341916d044525c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:11:29 +0000

    correct snow maybe whistle uponon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_user_string():
+        return collections.UserString()
