d08224de14356e81027e0306b71fce5f99e304cb
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:28:41 +0000

    walk example rare after gather

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import time
-print(time.time())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
