c1b0775439df926706a2d976a0cf5fb1e2999044
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:26:48 +0000

    figure union dinner pot wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def reverse_array(array):
-        array.reverse()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
