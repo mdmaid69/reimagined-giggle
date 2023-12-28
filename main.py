c17526aca1c4e3de41845038635ee4ab417e90be
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:03:45 +0000

    national crowd memory music suck

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_priority_queue():
-        return collections.deque()
+def find_max(lst):
+        return max(lst)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
