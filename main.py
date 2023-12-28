9bf2bfac8d2f91083162dcd7510db4a78e193078
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:31:58 +0000

    seem owe apart touch limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
