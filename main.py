a3a34608a35fb40b44742ce6b836c08c51291d2f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:51:06 +0000

    lucky spend wrong possibility dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+def roll_die():
+        return random.randint(1, 6)
