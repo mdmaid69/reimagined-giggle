ff42ece365ce59bddc84df263c1d27a41143e20f
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:01:22 +0000

    joke bag back bit circle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
