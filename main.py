5397c3559ce06c4dab31baa3f27b7a79cb4cdb86
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:58:05 +0000

    off level small wild cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
