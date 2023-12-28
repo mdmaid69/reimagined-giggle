377bcfed5a0790eda318eecd851ba39bc191b5bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:59:56 +0000

    nervous stretch always not breakout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def count_elements(lst):
+        return len(lst)
