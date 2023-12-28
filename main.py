4eb4116822d179b3a2ed4890cf2905b613f17648
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:58:03 +0000

    just regard bank this deliver

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import sys
+def exit_program():
+        sys.exit()
