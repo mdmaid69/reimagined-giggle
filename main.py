ffc7155641a5de9c4e8b89d9993f54ad6ccdde96
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:28:57 +0000

    branch introduce under stamp neat

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
