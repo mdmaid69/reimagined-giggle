a490f126b3ae50da087562df462da2ec5afae603
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:10:19 +0000

    gradually knowledge edge bath now

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
