539211e0177cb796f639805e2a5c6034d4e84749
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:44:30 +0000

    year sale push file sticky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-from collections import Counter
-print(Counter("hello world"))
+  def split_path(path):
+        return os.path.split(path)
+def calculate_acceleration(speed, time):
+        return speed / time
