52108c4adb735b249f8349541e807348b08176f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:43:45 +0000

    shape possibility closed myself track

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  def remove_duplicates(lst):
-        return list(set(lst))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
