3e227eb99a088089781a3c65ef291b1d0c8d359e
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:23:54 +0000

    store ill autumn tragedy attract

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def reverse_list(lst):
+        return lst[::-1]
+x = 10
+y = 20
+print("Sum:", x + y)
