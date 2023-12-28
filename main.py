2e21b4917627965544827687b4f51b5e904a0682
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:55:24 +0000

    marry comeround factory disagree container

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
