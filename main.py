b173c4a7b28b78a218271603a3878a90ceae1608
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:52:43 +0000

    have no average more unlucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
