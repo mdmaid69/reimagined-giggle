cc69416523f617b54fa3bec64118b72e16a1c744
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:51:11 +0000

    score joke destroy breakfast satisfied

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def convert_to_binary(n):
-        return bin(n)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
