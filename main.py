92af0914050810e1d6743486bd04fc0fb76fa8a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:21:22 +0000

    argue crime commit nonsense welcome

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-def calculate_work(force, distance):
-        return force * distance
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
