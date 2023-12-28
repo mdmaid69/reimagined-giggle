976a08838da39e5a834f2b4705a79d62595ce28e
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:11:43 +0000

    meanwhile scheme neither 568energy pair

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
