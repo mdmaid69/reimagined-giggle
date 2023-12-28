68646bbae4bc76e43064aad1483ce66a78a31435
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:35:05 +0000

    convinced live giveback reply bag

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def exit_program():
-        sys.exit()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
