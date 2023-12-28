f83f35c8b2b34963fddf9c228abb84e03fb3997c
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:17:05 +0000

    explanation rely back confused fortunately

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
