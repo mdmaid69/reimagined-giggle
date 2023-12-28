0a03b521d40fb95ef5f3f239f6ce8d2a56f66551
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:12:28 +0000

    dish long dare clean excuse

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def get_list_from_array(array):
+        return array.tolist()
