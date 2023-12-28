a69b8e9abd677e552fea054449556a470e9a3bc6
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:37:32 +0000

    to date Bb turnup country

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
