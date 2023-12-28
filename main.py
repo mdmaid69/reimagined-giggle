38b62f222656e1955f25d1b7ffa6f0e952b6521e
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:49:12 +0000

    steady stop history study much

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
