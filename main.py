971e6fa57ff472218f30a9603c6ce261fc2920e4
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:25:16 +0000

    main knowledge complain whichever natural

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
