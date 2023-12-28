65afd166804dfbef6c20c2c2d7874b535ad64212
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:08:17 +0000

    include watch calloff fast row

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
