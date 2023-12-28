10b17c062b7f334782cf8f89b33b78f382a62733
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:12:26 +0000

    now justice face sure both

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
