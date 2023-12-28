6847fe06ff9b627b8dcc27426b81d70ef043c79f
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:07:56 +0000

    stone get undo interrupt improvement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
