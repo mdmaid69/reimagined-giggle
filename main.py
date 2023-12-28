e7cc4cd923e1ddabe98fc69b2c4004958007db02
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:10:53 +0000

    store tough afterwards sad upset

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
