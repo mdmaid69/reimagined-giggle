d40908dc33d46c758f6633a5356379b4e96ed35f
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:04:11 +0000

    lot tap borrow Itself calmly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
