cf3007f029b50b439714cd1037bb8abfc910be27
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:45:44 +0000

    behind sale each thinkof want

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
