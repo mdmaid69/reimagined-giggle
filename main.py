9653e5a70e9957ed8e04582b9ebf487f892f6baf
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:42:06 +0000

    stare interest along so free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
