9e251c99493d7bc4ed9872d445844b30a7cebff2
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:04:19 +0000

    late rely refuse takeback sort

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
