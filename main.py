3d400c57fd0c67a365df46894657643f70dd868c
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:38:32 +0000

    issue runover beneath weather tooth

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def remove_duplicates(lst):
+        return list(set(lst))
