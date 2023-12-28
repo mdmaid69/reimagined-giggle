54a7fc0254756b2082872f7270d9a08fc7a79d2c
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:17:34 +0000

    hear buy formal giveup squash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
