b03de645907c4b45e8b8853788bcfe01f861a9bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:23:00 +0000

    amount embarrassing over below unhappy

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
