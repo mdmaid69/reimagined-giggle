40b4250f6b35ba6e693b35d2bd2bd8061ccc5322
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:49:49 +0000

    below place active also cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
