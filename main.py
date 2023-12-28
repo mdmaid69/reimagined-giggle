5826498d9b1e0fb37986f4201180fcd3d5b90b77
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:23:30 +0000

    willing brush desperate breakdown ask

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_power(work, time):
+        return work / time
+import sys
+print(sys.version)
