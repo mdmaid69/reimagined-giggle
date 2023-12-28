c14a5df867aaf22fa0db79adb5b662db6645c715
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:04:29 +0000

    question dead on note tap

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_counter():
+        return collections.Counter()
