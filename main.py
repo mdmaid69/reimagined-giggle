8d8a27527ea879ff7c7813155fa77c84153865bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:36:04 +0000

    embarrassing job arrive principle early

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_power(work, time):
-        return work / time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+print("Hello, world!")
