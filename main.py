dd19749bb1ff597a4f4460c06c4e59b4ffd66605
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:52:31 +0000

    rate undo burn soft miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def count_elements(lst):
+        return len(lst)
