d2f6c0dcbddbb36e37f5c3404964556ab823a6cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:26:24 +0000

    show themselves similar pour getoff

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
