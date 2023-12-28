705f04895edee2d4d65c62721fd508b64e7c2ca6
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:34:38 +0000

    justice strict press crime become

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
