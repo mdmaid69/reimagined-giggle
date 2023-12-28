ff4d93a01734aa788a5d0960fcf9fb3975d0f2eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:10:52 +0000

    570enjoyable trip open habit picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
