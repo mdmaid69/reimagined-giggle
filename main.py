28bf8b03f5aa1a2656e87043b4f3e31ea4f90643
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:43:20 +0000

    meanwhile shadow community dress live

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
