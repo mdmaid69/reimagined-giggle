c8adffa7c123197eaaa9771b278f4d3e25a69108
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:48:47 +0000

    crowded along within stamp discover

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
