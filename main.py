c2824c6955302c04e4194ba347f2c3f88f10a8cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:55:31 +0000

    suckme laugh trousers leave average

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
