e17e981e883da5a3e13efa4b5b23db23ebe64a5c
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:25:43 +0000

    patience account low near design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
