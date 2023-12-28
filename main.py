4043554dbf97396a201b83dfc80acee38ef3872b
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:48:47 +0000

    instrument remain burn risk teach

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
