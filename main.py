7f75220a5dcf1e1127121ed9cd92946db3f5cc0d
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:07:18 +0000

    entry underneath tired definite load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-print(datetime.datetime.now())
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
