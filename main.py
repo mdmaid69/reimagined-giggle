1bf003e30720d7b5180debca9c10a13bc9344f6d
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:27:42 +0000

    card friend opinion suggestion independent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_acceleration(speed, time):
+        return speed / time
