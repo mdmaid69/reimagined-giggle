f02b583c060d17c071b07bf389847706271dea8b
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:01:14 +0000

    uniform drive record house track

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_min(lst):
+        return min(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
