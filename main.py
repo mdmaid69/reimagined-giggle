d852aa832ce225517d2db56efb574f0de2f017d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:18:49 +0000

    coat degree straight shadow swing

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  def add_numbers(x, y):
+        return x + y
