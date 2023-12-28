431f0f58b3354de6295f811ce8a85c639936bbe0
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:40:53 +0000

    rise lookup move offer sign

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
