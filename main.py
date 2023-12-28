18babb67f0ece0fde8c4b8134f2b8c85c84434ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:16:15 +0000

    lock estimate breakin weather note

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
