0a085f628f351726017ac32d8f8d93dd5ec6aef0
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:23:48 +0000

    mention realize gooff narrow turnout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
