2a3d3c2fe8b0072506043dd0d8e1103aac4526f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:42:01 +0000

    noone healthy like music trap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
