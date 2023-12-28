7289165e64cf52dd858f0c68508996ab2216a313
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:42:51 +0000

    turnon walk realize what rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
