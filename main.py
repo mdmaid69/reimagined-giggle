cb4889bbba3d2407ae11b5f8a948ef3124563a62
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:31:15 +0000

    bank wave once pipe air

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
+from collections import Counter
+print(Counter("hello world"))
