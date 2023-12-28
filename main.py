6f34105bd7382e4a5f9345fea86fb5bfbc2ef875
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:15:41 +0000

    trap reasonable region still relationship

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_as_str(array):
+        return str(array)
