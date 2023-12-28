beec0f8f4170bdcda13011995e4b848acbd15407
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:34:34 +0000

    relative hurt exchange patience sudden

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_as_str(array):
+        return str(array)
