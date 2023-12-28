d42998d5d837cba845f5758c31a57e9c43bb1c68
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:42:46 +0000

    workupto fit underneath so adjective

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-x = 10
-y = 20
-print("Sum:", x + y)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
