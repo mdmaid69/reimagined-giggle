2ef767f6973da3786a618e3d36f1260f67883694
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:48:31 +0000

    used see proof indeed develop

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+for i in range(5):
+        print(i)
