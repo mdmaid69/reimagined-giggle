be8bcd1e5ad0ef8dcbb162f4ed7a5b97ea612a2d
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:34:39 +0000

    miss difficulty wire turnover mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
