2f31a3fbdc55c625c098633c8066094b0211d3ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:22:21 +0000

    sound conversation action rob party

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
