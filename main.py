93639cea8aba20dc67709395d38be2857ad6fbbd
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:01:26 +0000

    accommodation evening breakin inquire respect

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
