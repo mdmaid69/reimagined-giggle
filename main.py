a58ba29fd3959ed7f5dd6d9f0df183e41d898e8c
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:22:53 +0000

    waste suffer branch bringback honest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
