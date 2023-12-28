c82122d2ce359977f2e80741a7b5098e95a0d2e1
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:46:03 +0000

    deliberately big face table suddenly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
