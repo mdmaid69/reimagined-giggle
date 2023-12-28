23136c1b8e6f10bdfe2ca4ce6fad7b8710b5ce51
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:28:51 +0000

    nothing long empty service time

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
