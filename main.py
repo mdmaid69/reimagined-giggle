7bc344b51fc0af48d878a419de2ae1c30131247b
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:26:39 +0000

    error long give law story

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
