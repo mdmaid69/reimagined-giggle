b5cd2539f2320530f892654c07a6fac21ff6af7b
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:54:41 +0000

    piece coat formal sore address

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
