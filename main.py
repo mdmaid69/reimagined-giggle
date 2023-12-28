7482d2c559f36190a190897029f3d6acf389e207
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:45:28 +0000

    deliver battery drink either shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
