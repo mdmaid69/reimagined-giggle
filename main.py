d697d41ecca7a0156b5f355651c60e3a53875df0
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:19:18 +0000

    second further 565end collection light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
