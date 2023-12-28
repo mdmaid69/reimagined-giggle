f1a0a241f64d5d5c3ef36867f6a70171b11d3be2
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:51:41 +0000

    account callout act together myself

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import sys
-print(sys.version)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
