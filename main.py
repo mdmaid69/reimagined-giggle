e69568e28d4c8350be50488b199cd7ab3cfdc6ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:04:38 +0000

    such impressive anywhere point storm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
