03ff57819de970f76416a5f51b2a34415e90a1aa
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:20:09 +0000

    Ll watch scream lie copy

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
