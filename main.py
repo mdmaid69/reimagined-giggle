21e57a931078fb580cbab66b2fc761fa02f8787c
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:48:56 +0000

    straight shock basis name and

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
