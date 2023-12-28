137f1ea5e679295b894c33a1185b292359d9b5b5
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:37:43 +0000

    unpleasant meet drawing late enthusiastic

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
