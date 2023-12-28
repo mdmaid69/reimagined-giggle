57aceeccfa537d4f8fe2c17e0f8a776d5aead164
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:07:19 +0000

    strange frighten sore event border

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def add_numbers(x, y):
-        return x + y
-for i in range(5):
-        print(i)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
