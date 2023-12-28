d91c0d3be49aef33b7b4256a210f4d77cd169539
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:56:48 +0000

    care organize keepoff motion the

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
