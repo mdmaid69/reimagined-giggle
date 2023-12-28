ea2b1b5e97e8340462d0bb6b652a86cf382f3d94
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:33:13 +0000

    fresh wake so petrol exciting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
