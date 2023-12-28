1ffbc4822518f411e06ab36246ba062e516b0108
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:41:41 +0000

    sharp among imagination refuse happy

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
