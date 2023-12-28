3ea8b928b35bda8f413a5f193eb610552eb51a35
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:31:51 +0000

    compete cause protection moon careless

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
