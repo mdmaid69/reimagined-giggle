ec4e6bb1df606319ff6fad1451380f2ef598fb45
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:36:46 +0000

    realize bush news stiff pupil

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
