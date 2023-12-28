c0635dedadab72cbb27ef6c063258744924450ec
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:13:43 +0000

    wealth wet shop bother art

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_itemsize(array):
+        return array.itemsize
