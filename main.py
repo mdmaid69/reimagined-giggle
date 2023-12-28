b799c57737b04ad58d07898fd5c8cb77305a772a
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:17:11 +0000

    score turnoff debt call thanks

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_area(radius):
+        return 3.14 * radius * radius
