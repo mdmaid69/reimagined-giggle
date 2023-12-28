68c2d38a78095ba90bccc7a3ce7837aa63bc3c07
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:09:06 +0000

    ordinary tense fair pay bringback

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def convert_to_binary(n):
+        return bin(n)
