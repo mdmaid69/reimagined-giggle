b0faa7524debe15981fc2a611cabe698b9d59b6e
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:40:57 +0000

    radio whom proof even Ss

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import time
-print(time.time())
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
