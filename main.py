5332f97caece86e2539697164f598cd47d1af0f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:29:05 +0000

    shock recommend lesson smell custom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
