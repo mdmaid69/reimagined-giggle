4e2b2fca961537073681469541fe7839b7367da7
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:52:31 +0000

    share turnback contact loud float

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
