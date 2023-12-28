4b89dfb3164748e69693923f54c7dcecf0d3bd04
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:52:14 +0000

    compare tradition belong shut blind

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
