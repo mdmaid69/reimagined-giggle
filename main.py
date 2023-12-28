6612afcda8e57e8e8a46ce32da86b006aa7e28d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:40:10 +0000

    unhappy familiar taste hello turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
