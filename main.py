622ad03713f4bd7f0dd3515cee1186c5ff254f60
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:21:22 +0000

    name workout easy importance machine

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
