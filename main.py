45fb9878ad1ca94d1ed4e533ad785d3f07cc8c0d
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:53:33 +0000

    both turn child join Ss

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
