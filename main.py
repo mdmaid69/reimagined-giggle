ace24d9dffbf096b3c69d3a8aa3d9426bad07cec
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:09:02 +0000

    interruption stone equal proof cruel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
