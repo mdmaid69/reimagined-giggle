4211442d3a4f34ebb8237f348a419fbd249bb5d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:53:18 +0000

    claim contribute estimate address us

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
