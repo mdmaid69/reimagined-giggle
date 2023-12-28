98dedb2bc842595ce92938ade347c777ca47b9f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:34:47 +0000

    eat male lose great change

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_speed(distance, time):
+        return distance / time
