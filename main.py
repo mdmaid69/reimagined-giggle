5a1a387b2e6492d14ef5dc740162352e27c41b9e
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:38:46 +0000

    firm Cc switch surprised dangerous

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
