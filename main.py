f303a5697a87d7212e227cfc78a4978d939c0183
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:34:37 +0000

    keepaway back juice lie copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def change_working_directory(path):
+        os.chdir(path)
