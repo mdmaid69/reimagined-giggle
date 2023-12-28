d60886d43a8d355a2142211abedeac886b4e59e7
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:36:00 +0000

    that very flood region pour

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def reverse_string(s):
-        return s[::-1]
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
