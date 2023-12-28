02fd9337add602c974046bac187ecc9b3261507b
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:34:45 +0000

    tie stamp law Itself formal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def cube_number(x):
+        return x**3
