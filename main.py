e699703943bbdc8d3a8cc7265694b9094ba86b9b
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:49:40 +0000

    ball second stick software work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_power(work, time):
-        return work / time
+import collections
+def create_stack():
+        return collections.deque()
+def calculate_acceleration(speed, time):
+        return speed / time
