c8d714c58f16e39e5366fb5e2986c9ff3aed2712
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:37:22 +0000

    lean home wind trip rather

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
