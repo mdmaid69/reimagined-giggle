9663e255003d6d3158035b8e817f80ebb917a562
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:06:29 +0000

    star Ss journey my knowledge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
