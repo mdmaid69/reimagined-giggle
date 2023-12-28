9c872f5f66b197895c309c5fd656d1a8fdaa6719
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:52:26 +0000

    flat ring basically school sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+def get_current_date():
+        return datetime.date.today()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
