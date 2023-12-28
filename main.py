c00dda2e4e914e83f0bd24df3f94d74909643453
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:06:19 +0000

    base deal 569enjoy wide comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_user_dict():
+        return collections.UserDict()
