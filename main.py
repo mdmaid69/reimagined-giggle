49ed580c10684910590f410353585da6ba7259e8
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:23:06 +0000

    breakup successful fit cover conduct

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
