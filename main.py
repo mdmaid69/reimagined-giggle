fbd4956cb6c01b7e0da2de284fd8b917111dc114
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:01:18 +0000

    wear test right later about

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import random
+print(random.randint(0, 100))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
