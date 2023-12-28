ae80231e5f57a9f2e200c79086ee8bff73cf6b63
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:07:10 +0000

    software climb connect over new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
