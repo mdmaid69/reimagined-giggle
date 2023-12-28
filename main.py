c9e3d8445c89fd7bce51599c94764f26d27524f2
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:25:47 +0000

    fortunately season about knowledge dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_current_directory():
+        return os.getcwd()
