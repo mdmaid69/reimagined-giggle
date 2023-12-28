3ab04644477c1c86f7387823f8196692af38b826
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:26:41 +0000

    obtain land wing trade centre

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def cube_number(x):
-        return x**3
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
