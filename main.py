1bdd3d5e725ecbf574e5a2c9431004c3aaad5f80
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:43:30 +0000

    move bill help country clean

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def find_max(numbers):
+        return max(numbers)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
