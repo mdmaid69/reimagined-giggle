bf519294b1393143193e5ea74c8dd1848f4f6098
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:39:01 +0000

    work fall perform string keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
