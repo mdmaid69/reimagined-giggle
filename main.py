c92d330a66714a761ca8a4363b74b6fe720705cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:51:21 +0000

    future 569enjoy wind drink something

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+print(math.pi)
