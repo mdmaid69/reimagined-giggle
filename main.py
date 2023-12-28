ee961f41dd529e84e8716c5ace318a8102427008
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:07:37 +0000

    prepare wait luck across get

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_density(mass, volume):
+        return mass / volume
+def reverse_list(lst):
+        return lst[::-1]
