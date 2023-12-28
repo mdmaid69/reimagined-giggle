0658f4c7067e025dd1ce0ae76de6adad10fa7c10
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:52:38 +0000

    morning defend upset sting even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
