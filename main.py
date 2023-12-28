d3f4d399533abd7e86193534c59a4f5c899f7d4e
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:59:03 +0000

    shrink call path scream sound

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
