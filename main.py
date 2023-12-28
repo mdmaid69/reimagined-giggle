f0b5b2290e879894146ad6308968038b48314c77
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:32:01 +0000

    dish occur pocket this look

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
