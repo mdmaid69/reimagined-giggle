771db91fbb92687ed3772f43d6d3f6f59ae3544d
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:31:47 +0000

    wind choose cruelty cheat stop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
