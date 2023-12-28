e74ad5786757b51e90633d3159b345f6d8e487e0
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:00:27 +0000

    anywhere firm off suffer shame

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def set_array_item(array, i, item):
+        array[i] = item
