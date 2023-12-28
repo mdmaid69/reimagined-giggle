8f5103bcc7579ae357513d4da097fc53da74adc9
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:54:40 +0000

    fold information nurse vanish everywhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
