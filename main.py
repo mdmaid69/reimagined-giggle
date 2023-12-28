a09327e8ee89404a37242e858ba28af17c6e32ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:10:35 +0000

    transport building raw Uu mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_error_function(x):
+        return math.erf(x)
