cd2ba6b63fb01f7f66561f01d4391472754b6f3f
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:37:49 +0000

    comeout top especially port lookup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_power(work, time):
-        return work / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
