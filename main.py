266d55949aab9333b3ff6db0e255839b30fe86d5
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:44:35 +0000

    prevent fashion anything frame undo

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def find_max(numbers):
-        return max(numbers)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
