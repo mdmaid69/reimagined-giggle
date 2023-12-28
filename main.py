5d630881fc338a6aa218f0981559a8fdf5b907e3
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:33:57 +0000

    male shopping future soft earth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
