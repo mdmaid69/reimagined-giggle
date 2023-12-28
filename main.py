80c9b4156893b36eb812ebf443eb4ad58098c518
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:15:16 +0000

    computer port powerful provide mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_base_name(path):
+        return os.path.basename(path)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_repr(array):
+        return repr(array)
