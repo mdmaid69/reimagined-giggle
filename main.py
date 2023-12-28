507d2c1eaa1f90b1d1b7f8318e5a3316a6e138ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:34:36 +0000

    arrive drink spot final opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
