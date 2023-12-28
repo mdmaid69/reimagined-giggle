e511a18367086efaa95f520d637e2dc060a4e22d
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:27:19 +0000

    balance further store dress mess

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_pressure(force, area):
+        return force / area
