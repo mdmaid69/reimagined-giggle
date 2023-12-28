863fa33b7e54326eba8d27579b2f05bfe0cb69f1
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:33:40 +0000

    history final teach best convinced

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
