b60cba9d7bcd43eb12955038c0017adf8a548cce
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:28:18 +0000

    practical callat turnup respect reaction

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
