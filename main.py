fd47867f5b3de20988acf83e2b2a125c13501d9c
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:21:02 +0000

    beat everything neat yourself rush

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
