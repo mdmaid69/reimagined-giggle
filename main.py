0433f4524c78c211dc312af497ad21cba50ae84d
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:09:44 +0000

    convince shake wealth matter order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
