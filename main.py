f12ce01867c7520f8d7b8b8b03438278c17f217a
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:23:20 +0000

    taste traffic progress step problem

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
