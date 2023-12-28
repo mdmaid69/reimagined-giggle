12a4bb53dc2e16ffa9528e606e8be0b0a240045f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:23:29 +0000

    name justice proof uniform sport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
