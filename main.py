a07a5c6de3d1d057a5c46eb461596c84a261aba2
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:31:23 +0000

    I beginning fit feed tragedy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-print(os.getcwd())
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
