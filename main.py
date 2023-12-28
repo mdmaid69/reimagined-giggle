9fe53d34f154a78245363098dadcb3b0f3f873f6
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:02:05 +0000

    visit close recognize difference destroy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_pressure(force, area):
+        return force / area
