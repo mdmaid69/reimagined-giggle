12751d427413d63398d9d5e2abb4f888daf5447c
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:19:15 +0000

    flavour brief foreign convinced know

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_density(mass, volume):
+        return mass / volume
