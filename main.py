b90aef5315322e6eb595a5dba007dcdfb2189ca8
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:02:06 +0000

    Ii closed how pipe collection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
