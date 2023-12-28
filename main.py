f2149fdad17924d0dcc090e50416bc6ac8adf238
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:22:26 +0000

    negative noisy turn shout morning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
