c6ef35a40b6ee5b4c9c504cd725d4b42a2b47d12
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:58:42 +0000

    date respect bunch rid getoff

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
