64a876808f16d07e2de606ac8ead77c7ddd9d224
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:35:19 +0000

    use test defeat bringup teach

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
