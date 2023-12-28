3afa83c447c04e7adf4b0c5697a28c7e39d35fdc
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:33:21 +0000

    angry yours proper road thankyou

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
