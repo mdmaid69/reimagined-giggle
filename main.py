8ad77effb7275320bbbeec777fc9010c75cef639
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:23:56 +0000

    scream century cause both repeat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def find_unique_words(sentence):
+        return set(sentence.split())
