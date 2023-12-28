723ee1b38ed349865448b5b25fa7d7fff7f3b872
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:34:51 +0000

    birthday treatment both intend somewhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
