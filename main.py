e4acdf057a89ce45ab95050dee1d7daa63c38341
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:47:25 +0000

    count world whole still address

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def cube_number(x):
+        return x**3
