82b981337dc08d5238896c89ecf6ac890cf2da90
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:48:34 +0000

    Vv view during mail content

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-name = "Python"
-print("Hello,", name)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
