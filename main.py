0a7da23ef03c896067a616e1be6287cee8e53d74
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:19:07 +0000

    reaction act sign invite anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
