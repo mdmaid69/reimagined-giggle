3b9d79509c4aadf8346ce5915e85f7c49c01ea06
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:48:59 +0000

    boring bridge important band since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
