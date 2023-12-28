f650dc3db9067c7820432762a6d0fdfa3bc8ddde
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:46:20 +0000

    ignore onto deep win cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_force(mass, acceleration):
+        return mass * acceleration
