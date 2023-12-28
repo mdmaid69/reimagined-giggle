13b023a6764141fd8b46e0ed9bce20deb4f7cd13
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:53:38 +0000

    strength bunch opportunity needle before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def subtract_numbers(x, y):
-        return x - y
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import os
+def change_working_directory(path):
+        os.chdir(path)
