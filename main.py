693bf063e4e62daf971179b16cf80185c7d91726
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:24:56 +0000

    guilty whichever fire Ll shut

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import time
+def get_current_time():
+        return time.ctime()
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
