891023f274a2c3c4fce3dbfd59308a27566fae8b
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:19:13 +0000

    content suffer special unhappy dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
