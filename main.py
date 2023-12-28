4e9a30a7edf92517ef3bf6e42ba45ad1e635e904
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:52:55 +0000

    border like basis towards place

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
