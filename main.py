f41a2bcdd71479df175e8835cc5677a4b8a05bbf
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:32:44 +0000

    talkoutof talkback travel warning meal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
