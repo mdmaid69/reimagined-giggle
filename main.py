6419a42d93c6b44123726086e7099845c07bb5d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:07:38 +0000

    promise rain violent limit relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
