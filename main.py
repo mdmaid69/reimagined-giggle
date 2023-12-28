c7f840e5439009142003a25bda98330817b71e3c
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:25:58 +0000

    that create shout hole most

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_speed(distance, time):
+        return distance / time
