5f5487c6baca67a5c0155cad9baa794c0ca9ed18
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:28:02 +0000

    negative guess excellent man night

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
