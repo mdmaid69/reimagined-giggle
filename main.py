145a8e8cb8dd20ad13cbb577241f3c094045e5a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:05:30 +0000

    goaway fond another own complete

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_item(array, i):
+        return array[i]
