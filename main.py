7f3084d0da98a10e0b64bda17b28b713fcc1ba15
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:35:47 +0000

    government tall handle this dig

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
