dafd29d35b417ab0c47ee0c3bbad7c1491fafe78
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:45:44 +0000

    burst advantage bang female surface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
