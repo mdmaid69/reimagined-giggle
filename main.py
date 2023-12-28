c3214f90c4d18a7c538cb701ac37888366ccaf87
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:25:53 +0000

    clear emphasis brief bag practice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import random
+print(random.randint(0, 100))
