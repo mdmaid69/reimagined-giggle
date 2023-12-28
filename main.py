7ce65a0e8d139daafb53156193852487496a2a79
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:45:42 +0000

    convinced event ground guilty attempt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
