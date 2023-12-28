a90c99baf447def2db02adf7005b53f6a96ca1c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:59:21 +0000

    myself guess dear take funny

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def is_odd(n):
+        return n % 2 != 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
