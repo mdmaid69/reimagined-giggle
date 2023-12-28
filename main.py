40d370cf026a693a56c297ec7cd2fea582f1b24c
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:49:51 +0000

    breakinto expression look future instant

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_directory():
+        return os.getcwd()
