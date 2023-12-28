3ee6ddcc1e799a4513cbb5c051fa8446adf53f04
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:05:17 +0000

    voice wrong relation low goodbye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_min(numbers):
-        return min(numbers)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import datetime
+print(datetime.datetime.now())
