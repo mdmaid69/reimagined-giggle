ad8fa05e1bcffbc3acf315d68a9f1322cfb0546d
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:01:55 +0000

    complete demonstrate trust thinkof heavy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import tensorflow as tf
+print(tf.__version__)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
