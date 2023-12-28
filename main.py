507b1e4d0aba28d79eab0b7dbfcdbd6fab8f2585
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:38:18 +0000

    really money attempt odd brown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
