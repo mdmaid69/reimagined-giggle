d476d9fb23b7e02226e410611df08af10166133f
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:07:04 +0000

    run refer turninto breath immediate

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_floor(x):
+        return math.floor(x)
