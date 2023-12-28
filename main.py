b9744a57996d63b9971118ccfb7f1abef294fb6e
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:54:12 +0000

    memory callat finish worst healthy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
