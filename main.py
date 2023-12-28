9547ffd21b7134759b022f69915825671397d907
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:13:24 +0000

    gap the funny kick load

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
