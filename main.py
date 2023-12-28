1b34900138aeed3b927231cc4e9ae516b95e30a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:27:19 +0000

    meet protect guess reject window

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
