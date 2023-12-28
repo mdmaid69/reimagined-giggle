1a85ba47073261796f2561343d4af00e1259016c
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:42:25 +0000

    serious conduct battery payment habit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+print([x**2 for x in range(10)])
