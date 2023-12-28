25785f90d34f1b28caf41b9ce208dd16eabcc5f1
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:35:00 +0000

    choice callup report deliver study

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
