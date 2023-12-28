79a3a940978edf5e27eba23965126cd5e0748c1b
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:25:02 +0000

    reliable be might formal study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
