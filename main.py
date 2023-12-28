bde9f129ee21c0ded29d3a456c6480352fde29ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:12:05 +0000

    fat holdup means common under

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_volume(length, width, height):
-        return length * width * height
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
