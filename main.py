bd7cc6c412b4673046af514b954755cc01906f57
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:46:49 +0000

    manage make happen manner land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
