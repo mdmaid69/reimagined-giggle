b8ffd261cc0114844f3e513cc7f3fa839f969e9c
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:36:39 +0000

    wire rush fold protection after

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
