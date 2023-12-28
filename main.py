6dca171dd499809a040591ff0700c8db4ddc34c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:57:37 +0000

    see grade present total bother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_union(list1, list2):
+        return set(list1) | set(list2)
