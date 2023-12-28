792c29430699849a66ce5ef8c3bc7a863f530be7
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:25:44 +0000

    myself relax opportunity distant suit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
