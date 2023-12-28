c03f52f2c0369253fd3b18a54e4476af598e697e
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:56:38 +0000

    whom pleasant grow behaviour pain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
