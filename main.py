fa6d7ce570bc37f96344a80a6cc1dcaa03b3568b
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:07:04 +0000

    tone hope feed patient become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
