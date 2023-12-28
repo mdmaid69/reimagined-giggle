4e13afaab74bcf8e5b5fbf11b5734cffdad8c1df
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:20:36 +0000

    smart shoot do about laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import sys
+def print_python_version():
+        return sys.version
