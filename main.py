282cb916cebef01aa2325deaa66b5a72fd19cb0c
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:59:23 +0000

    difficult style post sorry boat

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
