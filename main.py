2cbc06134da5678ef73f8dfd5d16b90c376397d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:45:31 +0000

    run wait read nose value

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+for i in range(5):
+        print(i)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
