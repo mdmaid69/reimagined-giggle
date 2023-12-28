a6e1a3b0a8f28dc5f75c4afc3aed6e65c769fab8
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:43:09 +0000

    wheel seem pretend know interested

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
