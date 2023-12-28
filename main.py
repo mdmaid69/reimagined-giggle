e825967c81cf881c371a2b30fe1d0a1d5a76777f
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:13:51 +0000

    slide probably value air ocean

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
