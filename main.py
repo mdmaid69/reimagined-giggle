28cc9b716a420c4ad840c3e90f3aef5e936df176
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:35:12 +0000

    anxious minority familiar acceptable breakdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import os
+print(os.getcwd())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
