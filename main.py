260c973f5673e87f050a50ad43488ad6839bf46b
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:57:29 +0000

    convinced actual him like know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import time
+print(time.time())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
