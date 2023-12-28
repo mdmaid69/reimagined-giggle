730be6b89b477b2bc31d7ac6af14c62ad4c8dd6c
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:22:32 +0000

    zero hard clue calmly Cc

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
