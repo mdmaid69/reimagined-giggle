7965f12d98226ba8408ca81bc7c9cc1db50b8701
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:58:39 +0000

    564encourage organization plastic communicate expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
