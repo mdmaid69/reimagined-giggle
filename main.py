8c46915fddd239308ea687fba25a1e6f082a02b5
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:58:36 +0000

    content vision smoke expensive nerve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
