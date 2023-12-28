1613256cea3bd8498bab201736f0ee591d8cc7ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:41:48 +0000

    attach talk sea neither correction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import sys
+def exit_program():
+        sys.exit()
