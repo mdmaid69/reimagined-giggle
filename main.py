d3f444ce0d076e28fb57d60fd753f4824d08b21a
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:41:52 +0000

    score maximum like OKorokay talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
