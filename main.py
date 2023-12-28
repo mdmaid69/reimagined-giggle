4acfc534e00e6e6d7a245f4b2595e298a9920b45
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:15:36 +0000

    tight crisis address scale especially

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-print(sum(range(10)))
-print(sum(range(10)))
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
