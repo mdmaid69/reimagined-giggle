ee0b61023a182309e5c65923c866b32ea6559b46
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:44:43 +0000

    give grade mind Ii willing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-name = "Python"
-print("Hello,", name)
+import os
+print(os.getcwd())
+def find_min(numbers):
+        return min(numbers)
