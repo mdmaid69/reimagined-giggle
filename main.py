af2fc2370388f701b6c3491eda0db39f7c14449a
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:14:02 +0000

    everything advanced thank strength task

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def divide_numbers(x, y):
+        return x / y
