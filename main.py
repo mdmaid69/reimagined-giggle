28eef0bfc04f441cd05e9b80f9dd77627c96c2e8
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:42:07 +0000

    drive inquire innocent form process

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-for i in range(5):
-        print(i)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
