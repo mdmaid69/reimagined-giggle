1825a25b2ea3d76feca8b30c9247ad276ba54287
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:51:00 +0000

    keen welcome environment last convinced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
