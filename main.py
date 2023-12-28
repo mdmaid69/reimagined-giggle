79d683205b966cd38b4806572e29ecc5cf980ea3
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:33:44 +0000

    young refer correct ticket trip

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
