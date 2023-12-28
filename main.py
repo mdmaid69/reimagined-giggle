5545c2932581c1d9825d10a8e8eb7722750a79ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:20:42 +0000

    nerve man voice also rate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
