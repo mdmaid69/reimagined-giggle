702178ddc903ae2ad74ec572f7da755ec5cb1789
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:23:30 +0000

    rest list activity professional lunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def pop_from_array(array, i=-1):
+        return array.pop(i)
