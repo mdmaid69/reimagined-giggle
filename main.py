cdcb6f5a7cfe3e1bb152687c1c2cd242ea1a90b7
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:08:23 +0000

    head accidentally advice wave except

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
