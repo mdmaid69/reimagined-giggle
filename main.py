43b2b4a2a836ae03ba73193f62a828007cf6fab5
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:45:35 +0000

    sad die every in settle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
