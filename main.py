5bafb35c0452cb70dad342d670ecfdb242c33cab
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:42:56 +0000

    curious takeover laugh post your

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
