d58e7049c84d6e54c667b797c07db3a83ec1df68
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:00:06 +0000

    strike skirt eye exercise correction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def remove_from_array(array, item):
+        array.remove(item)
