6fb20c3c2e2bb1642be54d34a282fb23fe4098be
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:56:34 +0000

    disturb motion workupto work neck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
