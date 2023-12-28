99599e8736c0d3330cbe3a997ca961123602d7f2
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:47:28 +0000

    now represent object tip stir

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def convert_array_to_unicode(array):
+        return array.tounicode()
