771096f188e9b68a0e068ccd2b4a712eaca44e80
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:49:37 +0000

    inquire wrap always he until

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
