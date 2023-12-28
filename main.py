e97710489c42deb288709a451f1f79ecfa3bf2d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:33:50 +0000

    what record say makeup community

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
