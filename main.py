3b1b86818037ec7390d8ae010c231591eba854b5
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:32:12 +0000

    party welcome underneath calmly convincing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
