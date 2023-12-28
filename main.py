82f78ef27ed88e055636a511cf877d8f64c867d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:31:05 +0000

    bush exciting present quarter wake

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import time
+print(time.time())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
