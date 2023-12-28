a571ce0a4c07dd3ffe7b262b37e925ec3cb2855e
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:25:47 +0000

    tomorrow hate feel save loan

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
