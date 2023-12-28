7c8c8bdfafbc398d04c8dca84868ecf87d34534c
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:48:08 +0000

    for indeed steady stop really

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
