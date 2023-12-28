6be1037c8fec3f872287e7c3a4c30ce0fba8d1ba
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:26:36 +0000

    feature party ring swing nurse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
