ea596a1efb653c91f4b336cabdd5a3f7b5dc067b
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:29:16 +0000

    be disgusting emotion solve hit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
