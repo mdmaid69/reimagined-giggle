137dd1af62b18a90e519678b9e55674444b3b297
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:04:18 +0000

    possibility meaning lose mind gap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
