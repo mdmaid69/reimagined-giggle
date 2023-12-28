7c92319d57ffdcc753a067d1370a90f8934c9d7b
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:41:33 +0000

    life brief their steep write

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
