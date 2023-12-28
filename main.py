f4bed396652f9ed3f0c3b6ec99231a3a545b308b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:37:36 +0000

    blue discussion offence any bringup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def is_odd(n):
+        return n % 2 != 0
