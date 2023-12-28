96237d90e30a6be5f5f780fef0fc6ddc5c110038
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:26:18 +0000

    world familiar listen body protest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
