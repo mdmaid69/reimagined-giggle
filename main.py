82f122a78406b876bd9904f876e03ce549396806
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:13:15 +0000

    criticism zero better advise grow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
