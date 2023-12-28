81bc4a98384bbae0b319f2824191bc2919efb00c
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:29:10 +0000

    sometimes pen butter thirsty cause

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import math
+def calculate_tangent(x):
+        return math.tan(x)
