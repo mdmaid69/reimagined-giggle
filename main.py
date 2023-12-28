88129fa3981e618daa51f2c7cb0b2676ca9c052e
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:15:36 +0000

    wrong complete there side nature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
