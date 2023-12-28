01f127eb06a8b7be2898672a16dd94026acde0d6
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:33:14 +0000

    gather ability price contain trip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_force(mass, acceleration):
+        return mass * acceleration
