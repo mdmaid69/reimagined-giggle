28010418eafc1c1551b4c9010b0f2402f7abc272
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:57:59 +0000

    mood so wide flood lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
