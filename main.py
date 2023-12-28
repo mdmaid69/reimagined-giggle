9262400ab2949f70435546b37bcf9700bb48cf11
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:39:34 +0000

    address our cough century only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
