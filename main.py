faefc032168978363e1d292bad96ebbf97f6b7de
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:38:37 +0000

    free persuade dear their wet

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
