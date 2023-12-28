b1edb76f141b76d59b501af8e381f763569764ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:52:35 +0000

    apply lower nothing question noisy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
