38cfeefa6bfc4096abb7299f4fccda81b912d918
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:47:13 +0000

    about threaten train possession female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
