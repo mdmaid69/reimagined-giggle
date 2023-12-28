1da27f6f2b1fd74c991f4f1a88f75abee30e1e1a
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:47:22 +0000

    regard remember market needle day

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def square_number(x):
+        return x**2
