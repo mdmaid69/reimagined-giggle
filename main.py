ccc07139db63eb49b0fd89e6606b8c987dbc7884
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:59:05 +0000

    appear suddenly loose close Tt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
