24b845cd6d6646e79636cd60b068eecdebf2fd92
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:29:28 +0000

    comfort comedown tense call abroad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
