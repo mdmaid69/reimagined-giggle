3e1f0f5986fac144df2522315d15e886c30cc77b
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:13:07 +0000

    upset dish official ought protect

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def is_odd(n):
+        return n % 2 != 0
