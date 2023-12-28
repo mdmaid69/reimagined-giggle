e6192be2cca3a29cd3167aa7d9620bcd0988c74f
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:49:11 +0000

    sure forever tall drink emphasize

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_work(force, distance):
-        return force * distance
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
