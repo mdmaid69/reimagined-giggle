90fbe9acc6cb6acfde36e2aca8183a793b18f474
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:29:22 +0000

    belong die arrival reasonable off

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import datetime
+def get_today_date():
+        return datetime.date.today()
