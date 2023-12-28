abe0b89a7f4c385d1dc3819d61d645db156629ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:26:10 +0000

    spring slightly order brief condition

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
