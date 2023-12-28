38a372b40f956138b17af5c99dd1cd8f6c53c04f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:48:22 +0000

    bet heart everybody lift instrument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
