4b822dc8c95ed79f0762d39c72d7138789a3c00a
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:01:34 +0000

    out average male explanation exactly

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import socket
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import unittest
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
