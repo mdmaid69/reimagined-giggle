c94f368af2beebbbf9bc72116e1f2cd4d2569b21
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:57:38 +0000

    total conduct choice slight means

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
