110c052d5d08da6e1b4c20666ba8f277a62df471
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:38:05 +0000

    available delight telephone regional official

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
