4b45e84d8125e0eed00edb96e5058c8c1717f950
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:05:15 +0000

    compare show brain exploration lost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_min(numbers):
+        return min(numbers)
