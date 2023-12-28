86607d7213eb70f8f8e51b9f354f18f4ac24512e
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:49:40 +0000

    marry unable pencil blame appearance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
