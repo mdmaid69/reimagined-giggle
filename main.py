a401166c34f73134522e0cbab3f0375750c1a31e
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:54:15 +0000

    reduction battery arm Vv instead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
