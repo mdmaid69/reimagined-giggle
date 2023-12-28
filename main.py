95a376b63dd09ff3b79db2905e0b28cc7a56203f
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:34:21 +0000

    turnaway cause acceptable finger name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def find_max(lst):
+        return max(lst)
