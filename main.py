ba995464e1d5c495fbc9b2d412de3c946ed7e653
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:35:37 +0000

    no release certain dry question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
