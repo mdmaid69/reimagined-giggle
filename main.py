c908fbd697055f278bf2bd24631490187a381fc7
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:10:54 +0000

    abroad knock worth refer joke

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
