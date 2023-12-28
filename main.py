55bfdcca40a28c7d5f5ffb581def1e8b8b803db0
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:43:25 +0000

    again 569enjoy tidy appear busy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
