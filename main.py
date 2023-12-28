38d173b56b4cd260660575a9874e622bc9799690
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:49:34 +0000

    achieve goout world reliable back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def clear_array(array):
+        array *= 0
