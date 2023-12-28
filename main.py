18b39c9f64c982c192022b789faad53aa7c1ae20
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:41:28 +0000

    get damage different country trousers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
