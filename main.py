eb4b1db60194177dcabaabb3b2a0fbf3ec295616
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:15:48 +0000

    blame trust slow score material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
