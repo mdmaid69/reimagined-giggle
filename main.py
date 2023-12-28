e5e02371fca0396514694e1ca5da4730949768a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:40:59 +0000

    opportunity branch dance unfortunate company

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
