aa68679337a37cd68bbd721fea23cdf670009eaa
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:53:17 +0000

    use hers closed in respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
