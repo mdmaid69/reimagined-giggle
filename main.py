fca242e2a77bfd4b1cf17a0bb9222296ecdcfc06
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:55:02 +0000

    loud runover swallow goaway minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
