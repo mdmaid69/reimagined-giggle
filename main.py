bf53049d29a34ac421fb730feae500b6386b8e95
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:06:48 +0000

    nurse share 573entertainment hello nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import json
+print(json.dumps({"name": "John", "age": 30}))
