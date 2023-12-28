27336d373ed1497e0718c0c4aaaaa828288bac1f
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:55:02 +0000

    well ask sort off past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
