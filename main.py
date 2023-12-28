2e5b9ea311d775e99cdc072bb2ec19bc2d99cbf3
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:23:22 +0000

    rely perfect band guilty describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
