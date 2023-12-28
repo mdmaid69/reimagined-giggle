2c0e02d00669ab9ce22844c932b8bdda3d46fcc5
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:18:47 +0000

    drop story price follow mass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import array
+def get_array_as_list(array):
+        return list(array)
