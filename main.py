34b5652f823826d43f195fe9a632c8986eec64ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:27:45 +0000

    new waste set shock politics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
