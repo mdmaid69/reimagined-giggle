dacd7eccc98cf468acc169cba471a87b9aa9ae09
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:14:51 +0000

    cut means across hope table

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
