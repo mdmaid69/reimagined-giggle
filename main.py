f790d886f4ba328d883974a16a17b0ca91b943f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:13:05 +0000

    indeed ignore firm go callup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
