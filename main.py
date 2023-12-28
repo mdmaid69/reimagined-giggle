f660486b399c099833b138d297f812e6313893fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:28:57 +0000

    dinner there wherever thank tread

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  def square_number(x):
-        return x**2
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
