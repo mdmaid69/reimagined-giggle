eee3d175b1a76de63397cf208f8b3b4fb14ebf47
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:35:21 +0000

    modern seat cover curious dig

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
