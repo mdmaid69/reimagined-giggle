dd0b13cf3da1df40cf8602e32feb5f24142182ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:38:11 +0000

    free single float bringdown confuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import array
+def reverse_array(array):
+        array.reverse()
