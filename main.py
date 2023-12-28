70ad874d1d09199ad34be99c1b2633010eee3c5a
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:29:46 +0000

    hold charge check please equal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
