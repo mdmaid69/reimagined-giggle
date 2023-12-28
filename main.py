8b4daedafcc6234f7d9a218a45f6c4f8604f3d0f
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:54:34 +0000

    attach usual backwards flight stupid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def reverse_string(s):
+        return s[::-1]
+import array
+def get_string_from_array(array):
+        return array.tobytes()
