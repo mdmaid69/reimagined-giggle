8e059481b2e2e5f0361dcccc9eed5ac0a808e4a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:28:36 +0000

    high hello lookup callup centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
