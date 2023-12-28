9f448bc325534be975a378f0fb55c8eb93f0c4fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:53:53 +0000

    stamp climb high gather when

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
