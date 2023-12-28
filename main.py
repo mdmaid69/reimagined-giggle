39b9e1b00cdac96a432275e7a7f2ee0b3a96693b
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:09:25 +0000

    accident war talkback ticket battery

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import random
-print(random.randint(0, 100))
+import datetime
+print(datetime.datetime.now())
+print(sum(range(10)))
