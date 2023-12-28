24ac70fa5aca9b0a7b2d2c32c01092bef5dc7e32
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:26:07 +0000

    bus earn flow sore trap

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import time
+print(time.time())
+name = "Python"
+print("Hello,", name)
