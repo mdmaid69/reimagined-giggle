30b1656f14bddce72d4298241c74d371942bde5c
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:36:38 +0000

    rest create either runover far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
