7a2b78c6ab458bc0d59af2c4f2abfa6c1f645757
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:23:22 +0000

    fork such afraid circumstance themselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
