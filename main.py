c702577f242c54e767aac9496d4164ebce5352a5
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:22:49 +0000

    much bag sick her happen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  def remove_duplicates(lst):
+        return list(set(lst))
