fc4efa4f9a5ddecdfb2ded31861c3e123ab8da30
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:31:41 +0000

    share form little unfortunate comeround

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_power(work, time):
-        return work / time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
