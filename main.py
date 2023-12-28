78dc327622513b151e43b086fe1ab3ae199f26bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:44:40 +0000

    mad communicate clear eager will

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
