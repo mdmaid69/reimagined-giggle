1ec64aec88afb4d348dd3e602cbbeeec84ce3bb1
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:30:29 +0000

    scale visit effect slice close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
