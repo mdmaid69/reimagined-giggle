840dae5d161b953ed37a8af176c568c0cb4f3188
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:09:11 +0000

    middle bunch ourselves all call

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
