7b50a1991c71dd19266c915c9601a835505eb613
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:25:08 +0000

    Dd manage wash smell receive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def convert_to_binary(n):
+        return bin(n)
