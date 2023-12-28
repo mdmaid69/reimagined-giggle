47f9cf2d6151d8d8f81402fae2f9b764a4f02a34
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:20:45 +0000

    rule comfortable long direction odd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def multiply_numbers(x, y):
-        return x * y
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def reverse_array(array):
+        array.reverse()
