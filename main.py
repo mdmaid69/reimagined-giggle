7698ce276720fd4cbdbdf5544afb65ad35612e59
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:48:46 +0000

    ocean mark welcome sick borrow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
