73c096d16ea89a67354894441a37d9626d9b4859
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:51:00 +0000

    scrap equipment compare man noise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
