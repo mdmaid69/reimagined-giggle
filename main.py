608e198920641a03175981194bf315e0cc5c43e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:37:56 +0000

    worried recent Kk slice goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
