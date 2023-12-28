292fc66b8d6bc58267270ddb3e42560eb8f92101
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:46:20 +0000

    age crime estimate degree because

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
