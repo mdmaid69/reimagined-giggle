4c6b6575a68ce94e3e6affeb88d0cab595b2681d
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:46:00 +0000

    meanwhile forgive full day married

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
