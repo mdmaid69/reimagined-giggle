cd5e52461c52968e4067bc7706ee8080640406dc
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:31:52 +0000

    rest bag pause blue sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
