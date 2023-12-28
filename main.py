66528d2c916f27a4e5332216d0ce35c4ec03fa7b
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:08:17 +0000

    effort closed better noisy hear

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import random
+print(random.randint(0, 100))
