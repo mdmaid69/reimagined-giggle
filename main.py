77d2bccac3620737de0df1dae6b27b5e7295b356
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:28:10 +0000

    study obvious finally finger tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import time
-def get_current_time():
-        return time.ctime()
+def count_characters(sentence):
+        return len(sentence)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
