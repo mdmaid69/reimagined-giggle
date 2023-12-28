35ca627fa9338b0dcdc0d3cf59ad0b95e4d3bfcb
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:49:47 +0000

    handle take pride flow extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import numpy as np
-print(np.array([1, 2, 3]))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
