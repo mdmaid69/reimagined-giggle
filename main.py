524befdcaae26b09c160100d6d00addde70ff4d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:40:20 +0000

    birthday goon row related blue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-print(sys.version)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
