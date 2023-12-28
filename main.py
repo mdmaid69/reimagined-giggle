485317fdaf23a8df3e3a4fc83990f886b626f7e5
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:00:31 +0000

    pick programme excuse sleep scare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_typecode(array):
+        return array.typecode
