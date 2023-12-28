81cf18a475eafe8d7de66b8fe4212bd9e8915803
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:14:39 +0000

    under spring attempt goaway favour

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_typecode(array):
-        return array.typecode
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
