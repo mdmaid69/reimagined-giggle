0e4d8df11422929c44344e3eb76f8e28499a27b7
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:50:08 +0000

    have good explanation Ss mouth

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
