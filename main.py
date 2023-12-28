b7ba8abb0824ce9b2c2ebb28924faed10acf5393
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:27:49 +0000

    force project sort damage exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
