908a8f9505c33fb739018d17b657787e4d78cd74
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:02:30 +0000

    block think which willing her

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
