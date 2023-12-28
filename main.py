8a9991db830b1db0e0f16f80b2f1e1dcc6fe6cdb
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:22:00 +0000

    right feature phone hurt worse

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def is_odd(n):
+        return n % 2 != 0
+def find_unique_words(sentence):
+        return set(sentence.split())
