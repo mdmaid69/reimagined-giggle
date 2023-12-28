8643ac048d6a6badf1627f74df91005310ac489d
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:13:33 +0000

    official sensitive fortunate think study

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_speed(distance, time):
+        return distance / time
