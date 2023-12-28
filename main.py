f5a10846c2087434dfb665a14e57146ff05b998d
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:17:48 +0000

    keepout declare coat sister rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
