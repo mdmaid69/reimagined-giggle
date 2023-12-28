b68e579cdf6400c9811be220ddfd3a3b7f7c5ee5
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:07:38 +0000

    introduction country your fail whistle

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_work(force, distance):
+        return force * distance
