9e77bb59600a5f005e55c7776a8bf04e18b52af6
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:20:56 +0000

    heat punish soft either attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_as_tuple(array):
+        return tuple(array)
