e8449f2184428d68d4a9fef86b52747a0111f5b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:17:26 +0000

    brain history workout adjective beat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
