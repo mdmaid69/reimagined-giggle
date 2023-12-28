bc946445fa17bbda4e49fadfb7bf7c542217e8f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:59:00 +0000

    activity workupto soak naturally noisy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
