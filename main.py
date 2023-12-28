15b69245cfdcca57189f21f91f6804fabd18bea5
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:19:05 +0000

    once stare complete voice spring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
