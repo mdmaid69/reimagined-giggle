535d65b886b9a2ede7dea6439b7807facd71b944
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:14:27 +0000

    store everything trust tired number

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import time
-def get_time_since_epoch():
-        return time.time()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
