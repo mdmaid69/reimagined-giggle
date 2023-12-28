0f7287eeb28e3e6cea5e37925db8c6ac03f93080
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:02:48 +0000

    complaint takeout sorry owe bang

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import numpy as np
+print(np.array([1, 2, 3]))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
