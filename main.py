c3d4bfd5f947f6e7aaa1988bb9a31b75ee72ee22
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:29:05 +0000

    satisfactory lookback(on) noone idea uponon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
