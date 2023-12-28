cbb20c53fe9d04958eab1a5f73496ec7cc5c2dbe
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:31:42 +0000

    birth smile sight smart reduction

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
