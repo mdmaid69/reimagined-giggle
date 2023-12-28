cc669e07d0fee8524b832852b768015c41cc51dd
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:58:56 +0000

    in especially culture organization workout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
