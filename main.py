cb786d4a0b6eee3e6000c36dadd33f07da4b733e
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:46:59 +0000

    load shoulder feeling mad comein

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
