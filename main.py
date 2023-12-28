e8c7ec9a3cd36db191672e6cba90f60c86ac5cdd
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:44:46 +0000

    ear dear tune swing truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
