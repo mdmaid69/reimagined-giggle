c5771b86e9ec047c7670a2f58161eb2132c1e44e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:35:53 +0000

    summer imagination improve change complain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def subtract_numbers(x, y):
+        return x - y
