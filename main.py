2296958ba07b2b7e13388da6d437bf871fcf07b9
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:01:20 +0000

    unite guard comeout kill hold

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
