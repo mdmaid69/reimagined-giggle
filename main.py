e2b1a54136e6497eb294cb5e1a976dd6110d6999
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:43:34 +0000

    turnback punishment serious artificial hard

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_volume(length, width, height):
+        return length * width * height
