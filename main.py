7840028e836db872fe36a489ed0871b897b488a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:24:29 +0000

    share because please shoulder breathe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
