053132322138b1a799b791a247051106060ee386
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:47:16 +0000

    obtain label city object sticky

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
