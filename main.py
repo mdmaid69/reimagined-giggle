8e0073221645114d31d9cb46e1d4b4160919542e
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:41:27 +0000

    doubt building light file music

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def is_palindrome(s):
-        return s == s[::-1]
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
