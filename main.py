aa3fe0d9fef1c3a3ebf67f728d36b4ef120c04a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:13:56 +0000

    else study shallow hospital when

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
