b670cc733d9f1f2a593e712572dfabfd929f2df3
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:53:26 +0000

    size nature justice entry hurry

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_area_rectangle(l, w):
+        return l * w
