bb518d0ba785057b2bacac221f0c4bf18b79d93a
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:51:08 +0000

    convinced choose you stop official

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import os
-print(os.getcwd())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
