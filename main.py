d358af2bafbb8d1e1c18653a4e4177bc1ef2ee32
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:20:25 +0000

    Cc pain hand its leg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
