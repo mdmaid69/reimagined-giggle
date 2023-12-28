8d4fae8425d4e735b5f441ffbbe413b7a8c2e413
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:41:50 +0000

    lend test brush whichever cum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
