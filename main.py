2b23b158fc5da3771dcf6425be4e401e00804b1f
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:21:56 +0000

    chase meeting suggest defend goon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
