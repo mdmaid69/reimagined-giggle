9b26a00c20ad7568ac022825e72827a42fcc4790
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:16:04 +0000

    brain precise 571enough behave matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def reverse_string(s):
+        return s[::-1]
