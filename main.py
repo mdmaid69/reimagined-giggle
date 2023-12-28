cdf723fead7c0e6ccd75991afc7b4117f92cb2dd
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:47:14 +0000

    reaction recovery exercise order rule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
