9eaab59b2dd073ae7320073f02d99d0e13ea3d61
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:06:09 +0000

    reply steal exam determined people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
