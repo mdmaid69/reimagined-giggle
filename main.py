8d761229cd47db5c229fb245b2d82188a099458f
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:50:53 +0000

    hurry voice fortunate exploration event

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import getpass
+def get_username():
+        return getpass.getuser()
