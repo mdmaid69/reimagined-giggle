3bb8ae837dd1e6d9b2a24f00cfcbad75c5b1f758
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:37:27 +0000

    realize term strength brush paper

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
