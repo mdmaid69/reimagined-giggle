8770ecb4a7b7d1e7c757279d77bd2acf2ad375b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:05:43 +0000

    lost us neat invitation precise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def find_max(lst):
+        return max(lst)
