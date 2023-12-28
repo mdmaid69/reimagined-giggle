ab29e6b7ec1a79c50ecb56bb658fda43892f5628
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:12:21 +0000

    little success prevent include command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_as_repr(array):
+        return repr(array)
