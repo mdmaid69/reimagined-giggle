ddb566546c435bf87e17a14837062a5afd020530
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:14:08 +0000

    scream talkoutof wet protest conclusion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
