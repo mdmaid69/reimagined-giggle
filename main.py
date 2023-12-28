01cd302fbb52a3f5d2ee0494aeacbebdc431043f
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:20:20 +0000

    middle care sense full now

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
