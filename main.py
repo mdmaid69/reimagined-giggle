e2c5796603ec54b1740602f5104366147c1c20f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:11:15 +0000

    Kk hurry estimate after will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
