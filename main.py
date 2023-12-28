2f631e0112edeaac48970bef404b98caad406edb
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:04:56 +0000

    book understand autumn disapprove fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def square_number(x):
-        return x**2
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
