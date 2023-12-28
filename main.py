e7479c4143507fc40e83369c562dc59ea68b1c57
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:27:25 +0000

    blame file some plain declare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
