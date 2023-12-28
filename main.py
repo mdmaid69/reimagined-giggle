2dd2885d6dfa37d3075c8e669a7b41d881ea3128
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:05:37 +0000

    flight introduction rid pair change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_length(array):
+        return len(array)
+import collections
+def create_priority_queue():
+        return collections.deque()
