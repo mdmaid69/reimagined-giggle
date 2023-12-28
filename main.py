f9fc88588c9e142d20e6554cee088fbcf4355a8e
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:21:38 +0000

    money like satisfactory around 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
