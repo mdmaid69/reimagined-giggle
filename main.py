53502042f880de62e08b49d55b80dd02dc2cc7c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:27:26 +0000

    too welcome command herself how

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def clear_array(array):
+        array *= 0
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
