1021973e6533296efe4a81de2f25d422b294d9d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:29:13 +0000

    eager party possession somehow pair

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def remove_duplicates(lst):
+        return list(set(lst))
+def is_odd(n):
+        return n % 2 != 0
