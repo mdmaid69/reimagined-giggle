c367c8d3783d02d6d1424cca96b6f6729d49a66d
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:46:41 +0000

    bin prove worried agreement low

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+print("Hello, world!")
