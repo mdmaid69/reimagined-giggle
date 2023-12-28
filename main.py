30b826f1d19985519fe9285d46a394ae16d222e4
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:35:55 +0000

    tip anxious imagine complete pretend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_area(radius):
+        return 3.14 * radius * radius
