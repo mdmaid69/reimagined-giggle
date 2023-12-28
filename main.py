4730f0df16cae3f06b84506505886f838fe38bd0
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:06:18 +0000

    detail stop old invitation turn

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-print([x**2 for x in range(10)])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
