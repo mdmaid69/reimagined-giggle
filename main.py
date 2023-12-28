52cc234ce59a1644ac9f1eef6be5e8d068254db4
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:44:31 +0000

    ocean forget rush frightened plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
