5c73455781b65a74b12e74756a0d9fa9afee7fc9
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:06:42 +0000

    prevent star year surface unkind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
