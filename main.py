777d14e1cd27284fd40bc50547f95fb2229d12ec
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:21:16 +0000

    ocean follow could off corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
