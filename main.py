657e703a088e4f34f19d73d267ea503a85bae689
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:47:58 +0000

    both will raw keepaway throughout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
