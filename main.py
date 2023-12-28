87dcc3c554d1f47b211bcde6604b037c6124534a
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:54:35 +0000

    guilty concern rob sing spread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def remove_duplicates(lst):
-        return list(set(lst))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
