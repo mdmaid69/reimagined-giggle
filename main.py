f2715a04f115ba8d295bab2ea7a225a15a760edc
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:58:32 +0000

    correction ignore raw the embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
