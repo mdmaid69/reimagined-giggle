b63afc7d2043100ff4675e195ccb792f5c11a2d0
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:23:50 +0000

    arrangement frequent for advanced to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
