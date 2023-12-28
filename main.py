cc5f70979c483be04036cddfd3daf703ea764669
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:28:40 +0000

    comeoff tidy whichever performance steep

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
