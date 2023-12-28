fa824d3fe9c660658f41317ad4ccf7febcb52249
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:50:31 +0000

    telephone hungry base Zz threaten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
