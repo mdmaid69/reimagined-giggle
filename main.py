f9e253bccc09bd1d2316f4d7bfbcc3f4943f40af
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:47:47 +0000

    repair callat block shake lean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
