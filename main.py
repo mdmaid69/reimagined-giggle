1760fb8f260ed22599cbcfe99e1fb03474381806
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:46:42 +0000

    nose authority drop in shopping

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
