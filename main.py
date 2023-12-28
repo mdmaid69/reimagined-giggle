8d00fa1fd9110740ad87575a1faca80627540044
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:03:21 +0000

    teach serve truth beneath violence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def cube_number(x):
+        return x**3
