6ded5356505f2a4759477912daff356964548d49
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:58:24 +0000

    passage strict world day point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
