17ffad5d4b95399ae749d6e91cf679152415eab5
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:13:43 +0000

    plant only sky dry lay

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def count_elements(lst):
+        return len(lst)
