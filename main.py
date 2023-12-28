866dd3d6e2403076416fc13fce8d241522be2c82
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:06:51 +0000

    none soft score road relief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_str(array):
-        return str(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+print([x**2 for x in range(10)])
