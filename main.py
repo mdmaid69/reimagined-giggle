a019f8e3987eeb8c63d4a2f3edf93fd2a4e45e09
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:28:03 +0000

    invite because dull chat apply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
