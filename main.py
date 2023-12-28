c425810bbb98dcef5a91ebd00930361e16c2f2c1
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:14:32 +0000

    pain continue stranger name date

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
