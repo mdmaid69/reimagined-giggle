dbfa54bd9ccb853ffa35700685b5fff492e4aed7
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:51:58 +0000

    develop sure set Cc lead

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
