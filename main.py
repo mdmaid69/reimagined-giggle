e7e9f3e005b6f4e2d1dea3d5118471b7a17bc41f
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:09:35 +0000

    used confident arrange confident comment

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
