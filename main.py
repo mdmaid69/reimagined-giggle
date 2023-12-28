5278072fc3e059100e8167cde2b3038f44d0cd88
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:03:04 +0000

    scale minimum main trust probably

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+print(sum(range(10)))
