18bb85edc13a45c93184aad448e6acf35cb58a36
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:47:23 +0000

    major who copy little length

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
