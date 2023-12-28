02b9f6a0683c509cef8fc3a751f0dc28da003c58
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:37:47 +0000

    average fill positive ball hers

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
