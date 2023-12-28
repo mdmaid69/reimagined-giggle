e61879bfa47b9d3f8a1abf7dd63263606610d75e
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:43:59 +0000

    day several describe peace turnback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_unique_words(sentence):
+        return set(sentence.split())
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
