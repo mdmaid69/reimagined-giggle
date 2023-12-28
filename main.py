cdc26e9f7ad959f6797cf465a7d6db374d5e616b
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:25:01 +0000

    unlikely possible memory neither group

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
