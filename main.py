b92d9fa5ce391c8e8c022383b4c945a2431d273d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:06:47 +0000

    pool really dull children getout

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
