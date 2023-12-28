aafdbe6cc7331f52788de65545810d146219086f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:59:38 +0000

    different knowledge trouble flood anxious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
