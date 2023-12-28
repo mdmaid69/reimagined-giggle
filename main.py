7a1bbc5221f2202de77cdf2929fd5350435eea09
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:18:42 +0000

    defeat comefrom treat holiday tread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
