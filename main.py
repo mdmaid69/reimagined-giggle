689e26f64af91e2dc477954b1d2e8175ff3d1e33
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:50:06 +0000

    plan fond left reply downstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
