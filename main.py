d99d6622ba1370f730da7cbbd680af92a8103b40
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:04:53 +0000

    year walk close wear edge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
