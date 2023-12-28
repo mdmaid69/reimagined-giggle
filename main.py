89503fbebc9cd6ac946416b6e3d7c296fa12c95a
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:20:39 +0000

    else takeover anybody taste crash

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(5):
-        print(i)
-import random
-print(random.randint(0, 100))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
