a73aef2fb5c4a1280dbaf229f6027d81aefba156
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:23:06 +0000

    issue space lose meaning change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import sys
-print(sys.version)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def count_words(sentence):
+        return len(sentence.split())
