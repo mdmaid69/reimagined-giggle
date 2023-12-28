dd970568f10d70f5ddfe45278e665bb2ca3ea690
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:17:37 +0000

    challenge crash confident doubt doubt

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import datetime
+print(datetime.datetime.now())
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
