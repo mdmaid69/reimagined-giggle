69e6d15eec8286ee57562bdfa116d2a77f4d3ffd
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:14:24 +0000

    legal makeinto corner week nigga

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_current_working_directory():
+        return os.getcwd()
