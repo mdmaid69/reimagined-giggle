4eff40bc8221963813de09deb27e59dbb2fa668a
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:24:27 +0000

    turnup class education popular sleep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
