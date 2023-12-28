bbcae950e474fab07e61215a057a67845227c62b
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:28:58 +0000

    keepfrom OKorokay thirsty polite store

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
