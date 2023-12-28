48a8c6fd3bd29398c084798508db043ae1c71fe4
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:08:27 +0000

    pollution suit head about carry

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import random
-print(random.randint(0, 100))
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
