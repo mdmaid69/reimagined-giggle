c4caa6dd1e90df39ab2fc97a7b894c6b7a35a905
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:42:56 +0000

    pen tall back drawing insist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
