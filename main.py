4aef1be0af46257d011e434315434e9bd312559d
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:54:53 +0000

    taste high bored form sit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
