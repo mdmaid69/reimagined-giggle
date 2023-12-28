266e7c6060619cd9452c089f7fc2d987545208ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:35:50 +0000

    callat concern listen quantity suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
