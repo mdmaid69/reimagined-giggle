33b18db9cfc2fde57342e802ec6be727d2ab85b0
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:32:59 +0000

    feed usual choose either wide

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  def cube_number(x):
+        return x**3
