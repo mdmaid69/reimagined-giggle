cce9bc89e7be6131fec36a02ba5361b5d40d720c
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:07:43 +0000

    laugh getback paper stand exhibition

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
