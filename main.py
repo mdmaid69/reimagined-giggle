273ebb85e45d4f6faf91e8392a0770cfc8e3b109
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:39:02 +0000

    experiment wake advice alone depend

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
