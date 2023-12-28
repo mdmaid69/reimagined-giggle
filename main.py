731a1feed607e64b3eaeb5ae2f193c62f9390e25
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:08:10 +0000

    matter funny original warm shallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def sort_numbers(numbers):
-        return sorted(numbers)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
