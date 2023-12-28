c12d8a87bd7c6e1590e2c4fa282363e511d4e967
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:09:59 +0000

    disappear although powerful willing song

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
