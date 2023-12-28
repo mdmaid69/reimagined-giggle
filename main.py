0ff599cf07dbe4c41f65e3c3464b04cf3d764d1c
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:00:48 +0000

    lock organize learn unable wash

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
