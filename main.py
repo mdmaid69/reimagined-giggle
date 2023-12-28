cb5bfc31b719918adc941af84f7a587021cd2598
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:19:18 +0000

    worried as moment anything back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
