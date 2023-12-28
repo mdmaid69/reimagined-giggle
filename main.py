1d8041f64fd09a63cab66536be2afc45d7e73808
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:29:35 +0000

    keepfrom now become advice anywhere

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def add_numbers(x, y):
+        return x + y
