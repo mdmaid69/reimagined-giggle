d9e30ee95ade905e8f074a1fa89770e0927799fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:36:02 +0000

    challenge culture best apparently example

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_speed(distance, time):
+        return distance / time
