f06e2ade3d1438cf6756dc6aced21e4e225322da
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:45:53 +0000

    address pay word definite beyond

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import os
+print(os.getcwd())
+def calculate_power(work, time):
+        return work / time
