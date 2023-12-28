4fe7506b3cfeb61d2519655391e6e6e13d2b482b
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:23:04 +0000

    refuse load them lesson declaration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def square_number(x):
-        return x**2
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
