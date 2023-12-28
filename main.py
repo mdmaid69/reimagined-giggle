28b6c61cf668f926fe3f6a9245f719c5eafa7196
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:57:39 +0000

    rare store display surface match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
