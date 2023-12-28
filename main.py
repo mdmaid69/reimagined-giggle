8e3bf3dc0464479878eb7882964081516c616d78
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:07:37 +0000

    like smart whisper cross further

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import time
-print(time.time())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def is_odd(n):
+        return n % 2 != 0
