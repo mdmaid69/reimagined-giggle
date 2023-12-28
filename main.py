7acf9a03fb0a2f63efe040f4fa058a4961b8677e
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:19:20 +0000

    by grand male breakup achieve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def square_number(x):
+        return x**2
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
