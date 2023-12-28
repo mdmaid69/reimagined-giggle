6e71bc8b5447ee3ee7e2093622b78b5779ba6c62
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:25:06 +0000

    issue lift Dd sound several

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
