0a0644cd25e08e1ef15159be3ab2ffdadc8ddd44
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:56:58 +0000

    display introduction tap none direction

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_list(array):
-        return list(array)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
