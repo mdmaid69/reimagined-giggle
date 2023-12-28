77a1ebf7557e6b278064e276d151362ba268145f
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:20:39 +0000

    handle freeze the holiday practise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
