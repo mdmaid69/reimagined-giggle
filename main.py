6781e1e1be53739a874680fad4edabb6ab5ae697
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:00:50 +0000

    show sort street mend own

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-for i in range(10): print(i)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def subtract_numbers(x, y):
+        return x - y
