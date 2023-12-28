bb73ffb8e06e9d6fddcd699c07998d7804326fec
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:16:05 +0000

    giveup thirsty better Hh list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
