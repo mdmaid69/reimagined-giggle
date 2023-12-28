0ee9f686fc53df6b3dc20390458749014b24b8d5
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:53:52 +0000

    support hurt climb ticket road

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import collections
+def create_user_list():
+        return collections.UserList()
