cc630581720adb8c8dbfffd21536a36077f8aaf9
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:37:29 +0000

    except phone welcome safe pity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def get_current_working_directory():
+        return os.getcwd()
