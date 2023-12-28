fe22d7aeab7bb92ea634ccfba8005ec2460650d4
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:29:48 +0000

    past statement bar name lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
