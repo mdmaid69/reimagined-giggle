81df7bc0f227ea14b42b663497ba5098f445bef5
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:31:47 +0000

    chase forward sentence rest kick

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_acceleration(speed, time):
+        return speed / time
