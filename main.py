c497cab69360e16fc19340356284d01cc56dffb0
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:35:38 +0000

    police ought means image flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import random
-print(random.randint(0, 100))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
