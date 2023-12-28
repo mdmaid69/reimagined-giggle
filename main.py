8827c5b2cef2e73a58c2aeed8400d125e2b2ae45
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:47:51 +0000

    really compare wrong confidence trip

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(10): print(i)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
