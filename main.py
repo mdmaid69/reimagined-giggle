6b6699b146d6b334b19c664097a96fae14c4ab44
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:12:25 +0000

    turninto cope black high disturbing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
