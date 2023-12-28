c9fb44623a3a104020dbf8f79217435e9aff30ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:10:07 +0000

    steer whistle fall destroy involve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-print(time.time())
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
