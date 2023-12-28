21ecafa0f90cc20e5c992aa19c17c248a118371c
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:52:50 +0000

    floor mark detail brief point

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
