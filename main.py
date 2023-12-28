25b39106484e1dbfbe36cc38d541c48cde95374b
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:58:22 +0000

    march small drive accidental demonstrate

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-for i in range(10): print(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
