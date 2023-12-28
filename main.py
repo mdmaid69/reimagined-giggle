72bc3487e6ba43bf2562988515554f24e0f7f741
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:29:27 +0000

    injury nerve dear balance keep

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
