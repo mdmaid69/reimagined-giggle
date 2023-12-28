3b726c9c44e2a907e48767a421ba91e4a292a31f
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:53:48 +0000

    pronunciation or answer soak future

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
