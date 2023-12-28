8ed3500debaa5975ecedfc3e312f81b5c075ca87
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:04:07 +0000

    whenever suggest trip frighten getup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def cube_number(x):
+        return x**3
