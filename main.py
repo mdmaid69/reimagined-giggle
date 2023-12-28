b007d1cee88b5a06e64664f3163319ec384b9909
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:14:45 +0000

    light drop night silent expect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def is_palindrome(s):
+        return s == s[::-1]
