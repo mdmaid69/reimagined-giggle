74a78cf7b780afbd5199c57cf8ca902ebd662045
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:46:41 +0000

    careless central arrange bother busy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_power(work, time):
+        return work / time
+def calculate_speed(distance, time):
+        return distance / time
