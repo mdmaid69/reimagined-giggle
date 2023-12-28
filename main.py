ed9ce640ef1fcade4dbe732cea789eed1e6dd9bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:44:07 +0000

    better switch turn laugh sweep

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_pressure(force, area):
+        return force / area
