6eef218f4e28093b7f5905ecd0b9478451708c4e
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:33:06 +0000

    software year possibly clue expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_speed(distance, time):
+        return distance / time
