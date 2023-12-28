ef0bb8f0a05b2dadd944fc1d017e230431b2b3af
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:43:47 +0000

    please drop declare alone back

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
