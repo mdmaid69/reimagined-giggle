92982aaf356f2f683f85d80c8d0849e9ca84f512
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:32:53 +0000

    owe need preparation search bread

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def square_number(x):
-        return x**2
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
