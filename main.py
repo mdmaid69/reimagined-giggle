2c054bc94d32ad58861e413dfe9d0e149e398f68
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:12:01 +0000

    track later whom shade bridge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def sort_numbers(numbers):
+        return sorted(numbers)
