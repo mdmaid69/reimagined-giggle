c856ad9143d04e7c5e81c54ea6f36637562c422f
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:50:37 +0000

    anyone design comment worry excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
