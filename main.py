0534b93a994e23ca977fda2f5b3764ffca8357d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:00:38 +0000

    provide disappear holdup modern drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
