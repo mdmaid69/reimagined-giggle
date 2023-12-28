a2b16c652cdc800827974a0091ab3443e4780d0c
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:00:36 +0000

    sorry comeoff band mess apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_priority_queue():
+        return collections.deque()
