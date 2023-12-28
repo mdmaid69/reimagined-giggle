ff0fb99504dde4dfeb7a71d3501c86f3327dab0c
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:45:51 +0000

    knock emphasize fortunately home complain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
