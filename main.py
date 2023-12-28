e1e53571f6cfd5003ddc0a88a734d77f9e81982d
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:00:40 +0000

    forget love idea lookafter television

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
