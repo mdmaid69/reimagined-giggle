196ba8f745816dc76224c8691a7dfb8294076d54
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:03:59 +0000

    telephone sticky hope claim food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_circle(r):
+        return 3.14 * r**2
