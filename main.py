ffe306c6c954b020454e86dee705a8568317fb47
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:38:01 +0000

    journey face duty left sell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
