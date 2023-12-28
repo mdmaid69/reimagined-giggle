3e8f7d34d6f39bf4214098d55f2d36b0e5b0204c
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:38:50 +0000

    behind protest apologizeapologise exercise vision

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
