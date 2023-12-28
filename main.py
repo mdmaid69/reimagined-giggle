c4104ad92b40a5782f2a2df4d3b8667f09c54a1a
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:07:05 +0000

    inside weekend opinion supply shame

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
