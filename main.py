ce4a90ae4258edece10e5b3cab500cd0e5180702
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:16:25 +0000

    niggaacademy offer previous goout shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
