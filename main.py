cc138394a59856c0b9e2a341a1f955a988e4312b
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:26:16 +0000

    loose present target without proud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
