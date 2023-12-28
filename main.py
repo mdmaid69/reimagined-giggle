2f36f198a24707a98ff71e758f69fa0a4a759545
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:36:29 +0000

    above point fortunate school heat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
