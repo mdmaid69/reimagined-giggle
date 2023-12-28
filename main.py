a13e01d6fc24574c602bf7a4cba86fed29d62dfe
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:36:01 +0000

    command contrast lead he hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
