eacb653314df78fddaa15cdbda49f71797bc2333
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:34:28 +0000

    question earth software song mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_work(force, distance):
+        return force * distance
