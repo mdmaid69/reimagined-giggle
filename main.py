de4129a9e2b1f779efa3f5e8fc7e747764dbec81
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:42:52 +0000

    brain borrow subject this shape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
