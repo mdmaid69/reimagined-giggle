e8fe1de381c02f826fa5f2720fd32746b16dd882
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:04:16 +0000

    frighten cost experiment ride thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
