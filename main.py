99528ea0a81219de81c371e10ced4418b6c70a11
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:58:54 +0000

    journey about rely rank score

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
