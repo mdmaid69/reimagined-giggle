d5f8b7a7e94f4205991d3713b5fcb6731093b70c
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:28:05 +0000

    themselves possibly show crime of

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-print(sum(range(10)))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
