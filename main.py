b0335bfd4312624a244b9ab19b26736fbb7bf7c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:35:37 +0000

    opposite pipe mainly matter Qq

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_speed(distance, time):
+        return distance / time
