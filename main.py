e0b1f4846700564db27c2a49032c76295dc5fb28
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:36:35 +0000

    certain office criticism mainly quiet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_current_working_directory():
+        return os.getcwd()
