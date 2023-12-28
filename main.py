8f67c46c188ffc9d5da462a52a220b1e9f1573b9
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:36:28 +0000

    remain touch dark bush minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_max(lst):
+        return max(lst)
