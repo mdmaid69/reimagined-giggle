894b26e90f450136f4092cc7ed3c196c288c7dbf
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:31:40 +0000

    by sometimes live piece popular

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import tensorflow as tf
+print(tf.__version__)
