dcd62cf71685cca2dad962bc793eb4646e68a6a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:40:24 +0000

    unless anywayanyhow need wing brush

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
