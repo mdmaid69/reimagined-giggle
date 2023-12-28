2e8a5c0c4eff2095afedaadab213a312248eb5f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:31:07 +0000

    whistle concerned payment wide culture

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def square_number(x):
-        return x**2
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
