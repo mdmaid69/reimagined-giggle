c80c13b57de01e06ea426b93cdae801738a5be30
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:59:10 +0000

    than busy invitation tradition already

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+print("Hello, world!")
