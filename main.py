275fa57513824742c4330763f0ceeff212770405
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:33:46 +0000

    afraid compare time theirs embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
