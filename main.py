a1fa4e9bee405e5c84bb290b710135a439b8d393
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:42:23 +0000

    runout laugh unfortunate recognize hers

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+print(sum(range(10)))
+def square_number(x):
+        return x**2
