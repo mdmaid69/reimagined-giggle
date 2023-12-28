bca322999317028ad876acf1e79c11d0ab553da2
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:25:51 +0000

    home fight bit visit limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
