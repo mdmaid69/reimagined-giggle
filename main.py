effd7d8ab9582821f8ec84c006a252d68958268a
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:59:59 +0000

    reply deal front here suck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def subtract_numbers(x, y):
+        return x - y
