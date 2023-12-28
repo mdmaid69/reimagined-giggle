1afe7e92139c6fcb7f324d6ba6edf8cb7dfba171
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:16:30 +0000

    tape breakthrough dish insist start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
