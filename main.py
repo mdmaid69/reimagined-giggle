46ff7e9b06e45b5bb1c3f1466ac794b97a02e1dd
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:02:09 +0000

    once table black major can

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import sys
-print(sys.version)
+def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
