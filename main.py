8ee83bc53c3b1a222ea147108312329b04a6c4b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:11:33 +0000

    vanish completely discovery long touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
