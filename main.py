e04238cdbab9a4e6e13c3de45bec2e75a2b93e80
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:39:42 +0000

    smart hour also harm coat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
