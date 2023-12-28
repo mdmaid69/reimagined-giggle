a73fc40c22454cf1bc695e8db90fd5742c3d3b05
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:17:12 +0000

    steady frightened thing jump understand

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
