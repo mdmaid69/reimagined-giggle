8358523637de912397c9cb217fc4f71b54aa1a34
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:12:07 +0000

    clothes steal flat drop balance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def reverse_list(lst):
+        return lst[::-1]
