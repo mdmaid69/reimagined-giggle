592d7fef471abf28adc5d5f207a27cc89f04ac14
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:40:16 +0000

    slow niggaacademy swing turn long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def is_even(n):
+        return n % 2 == 0
