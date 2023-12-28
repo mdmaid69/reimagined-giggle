6a8899c050f1041d8ccc2cd3f6742171b2f420f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:18:43 +0000

    fetch repair meeting turnoff demand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
