fadd41a7f1fec627eb5ef53c550bb1c843bd5e83
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:11:54 +0000

    window negative awake last opinion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_exponential(x):
+        return math.exp(x)
