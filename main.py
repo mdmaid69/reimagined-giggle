3e414b8f4b7f7dcc975f60eb79f5526e5f66401e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:41:56 +0000

    actual apart cope equipment work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_area(radius):
+        return 3.14 * radius * radius
