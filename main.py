189a103babda44949058a4140e4c5f859c9cac77
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:47:43 +0000

    muscle want ground see sort

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_speed(distance, time):
+        return distance / time
