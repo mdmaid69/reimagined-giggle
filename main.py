6363ca15b5443ebc97f66be3aac705a2e38e0dc1
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:59:54 +0000

    fall shop write watch cool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
