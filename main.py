4a7dcde2020db90c77cf54ec2043f67eff5745c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:51:48 +0000

    hold yourself room sound else

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
