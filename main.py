910863854149b7337ea998e1db0ed76256096869
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:04:21 +0000

    method have improvement they material

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
