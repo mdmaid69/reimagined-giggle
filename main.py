cfd0334c3c4721321e2f75f04d041cea0b950836
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:26:01 +0000

    order power city sound giveaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
