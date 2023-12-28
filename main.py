c9e9fd28dae54936e82886a7ccec8a32f7038de6
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:09:57 +0000

    field quarter steam unless comeout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
