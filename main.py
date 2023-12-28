9ad83e70a9fc9877e48a8bb8f5a9bf826bf68ec8
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:30:36 +0000

    promise season would profit exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
