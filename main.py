ae0f5580db876cd8605ed33a89258b1e683e62ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:47:32 +0000

    frame tragedy instead press spare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
