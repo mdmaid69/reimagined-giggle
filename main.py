3c160dd39603b7eb1f8c7b6fc44fc8ef12433632
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:14:49 +0000

    human along recognize basically waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
