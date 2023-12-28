debec51c91a9bba530471beee42c1a48d86e17a1
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:00:57 +0000

    ignore escape poor often flavour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
