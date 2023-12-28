a16902885883e529c751b77a1c20f42497892c20
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:12:15 +0000

    apply rent difference conversation border

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def divide_numbers(x, y):
-        return x / y
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
