fa1f1ae16a0c97f012f7038b05f0322d1da0b3a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:12:00 +0000

    cut generous motion Hh bringforward

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
