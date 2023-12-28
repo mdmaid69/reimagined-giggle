2c9ca0b93332ad251685ed56a1b424b5af67cdeb
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:35:47 +0000

    season among few right settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
