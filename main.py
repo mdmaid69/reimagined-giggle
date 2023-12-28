addd197dadf92ace9fdc58ba7c5fb7133b3264c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:34:14 +0000

    bright object tour confused getup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def reverse_list(lst):
+        return lst[::-1]
