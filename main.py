bcf636cb577dfe53b59010648f6d13f0856a71d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:10:55 +0000

    stick appearance deliver scene music

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
