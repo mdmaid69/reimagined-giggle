90d0903c297b9f9352496d5546b485a6fd19ae92
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:32:24 +0000

    shame effect secret hit transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
