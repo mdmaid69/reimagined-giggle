fb1a01c652dd0276e02b6b4f81188d24404df286
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:32:22 +0000

    measure contribute software ready shall

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
