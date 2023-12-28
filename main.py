ff91e78b960931aa30ad3a0a553785a5ea4702c3
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:21:02 +0000

    cause pay throughout someone exaggerate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
