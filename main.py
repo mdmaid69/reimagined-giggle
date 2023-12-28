3aad4aafc95641d8449155774befce5c2804428a
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:09:36 +0000

    desperate apologizeapologise low aware line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
