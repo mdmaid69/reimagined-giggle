8aa080f8c38b5135d00dbb93c05225b58af2aaff
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:52:56 +0000

    charge foot admit game wander

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
