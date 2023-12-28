0e9b350abb1a94873101b9759a7e4632695e1d10
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:37:44 +0000

    lookafter treat emotion foreign danger

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  def subtract_numbers(x, y):
+        return x - y
