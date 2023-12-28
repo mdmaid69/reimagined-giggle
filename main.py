aabb9c33fd6543cbe383dc51b5c7f0790cb76c89
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:19:37 +0000

    may remark sense key approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
