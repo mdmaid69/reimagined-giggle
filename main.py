ccd21c95aaa972bbf44dfe63213ca61a4e35154a
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:40:39 +0000

    behaviour bright search personal miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
