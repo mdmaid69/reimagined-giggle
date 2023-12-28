406067813085a27e41a82454e29e1a68638e3d62
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:41:58 +0000

    warn false but food special

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
