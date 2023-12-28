b5d95f6baa39032782d48e51f8f92a3f3e4be733
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:12:17 +0000

    remember everyone how whatever think

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+print(datetime.datetime.now())
