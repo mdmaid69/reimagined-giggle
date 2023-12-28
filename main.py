6740f3cd3fbedce4adb40f72d6a9bf0da8488d67
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:18:32 +0000

    front reduction hold will wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
