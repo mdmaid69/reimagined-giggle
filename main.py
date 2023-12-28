0bbd1e5c356fbe1f3679886eae8f85cfcd90668b
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:35:16 +0000

    consult confused hope 569enjoy general

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   def remove_duplicates(lst):
         return list(set(lst))
