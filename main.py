f530e308b975b04407108a5ed44b44c2beb92cf1
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:36:16 +0000

    themselves check which sight operate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
