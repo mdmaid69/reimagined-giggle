dff217bcb45896176858dbed600626d7bf7d8547
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:06:23 +0000

    myself patience foot pleasant brave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
