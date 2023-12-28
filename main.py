8c20fbba597314caf520bd1564b26458b33c7400
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:42:58 +0000

    work car if plain round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
