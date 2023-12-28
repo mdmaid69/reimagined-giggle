4c6d2fb2e7fc0938bd5806b7ad8c58e31e59eadc
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:55:13 +0000

    recover hold fortunate equipment calmly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def add_numbers(a, b):
+        return a + b
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
