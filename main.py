0bb5fb9514c7d32126caccb8d018d7004fa1d79f
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:41:26 +0000

    effort lend correction return simple

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
