95b9d8c49ecadf8428a47f5210e30e494e53f66f
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:30:33 +0000

    side shout only call survive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
