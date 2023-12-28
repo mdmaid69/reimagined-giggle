a1f53365df2ae1303ac408910121f55b464fe099
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:11:31 +0000

    dry remark sentence increase accordingto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
