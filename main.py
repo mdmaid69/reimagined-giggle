9edd32b6de0922861c0d1d8bc1b548a02cf01ce8
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:09:53 +0000

    guard takeoff pen attention fierce

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
