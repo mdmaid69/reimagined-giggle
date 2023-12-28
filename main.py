0c44f3443584d2db8d5546512f74c52f8f57b0ff
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:26:24 +0000

    joke read 566end fasten geton

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def cube_number(x):
+        return x**3
