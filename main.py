18aafb9b27a1debdb955a09e46251f9c7d1a9450
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:53:47 +0000

    beautiful near cut young stranger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
