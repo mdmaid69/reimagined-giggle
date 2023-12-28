88f265c262016858ebcc114bd4563ce6f14e1c4e
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:21:45 +0000

    rain touch journey news create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
