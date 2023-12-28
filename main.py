a8269b9b1d30a82d8ef4cad12a4aced814bad174
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:05:29 +0000

    sea dangerous attractive happen answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
