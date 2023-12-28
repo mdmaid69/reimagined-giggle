a2ddfe645b06973aab6137626bc2860ebbcb82a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:36:57 +0000

    general small mess instant under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
