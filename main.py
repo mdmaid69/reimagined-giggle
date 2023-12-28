a84f645fab8ff54e3f09e63bce06d0570a059263
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:20:44 +0000

    concerned coat off nearly without

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
