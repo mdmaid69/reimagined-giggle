f0345646c797802adb6452d730fc60c4eab4a95a
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:07:33 +0000

    task thankyou accident prepare high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
 import array
-def get_array_from_list(list, typecode):
+def convert_list_to_array(list, typecode):
         return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
