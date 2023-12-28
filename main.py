f0d3ad1c30524e14904865ea11f2625fc5fffb7b
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:43:17 +0000

    mean visit complain both narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import collections
+def create_user_string():
+        return collections.UserString()
+import sys
+def exit_program():
+        sys.exit()
