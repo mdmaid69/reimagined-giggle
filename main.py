904747aa22f6665fc28848cb1e46e1176dc00026
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:46:40 +0000

    570enjoyable any foot left foreign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
