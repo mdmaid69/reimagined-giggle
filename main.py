e8241fe157cf56f5534a87767db4bb26d071f453
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:13:28 +0000

    section shape emphasis watch natural

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_base_name(path):
+        return os.path.basename(path)
