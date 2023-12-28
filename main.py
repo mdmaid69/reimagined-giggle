07d036d3345ad74ae745df629b09fe1d823e4412
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:59:33 +0000

    guide waste dream pay walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_odd(n):
-        return n % 2 != 0
+  def get_current_working_directory():
+        return os.getcwd()
