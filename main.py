1acd2a5a6ca849246cf7351df4dd3d8af047bd37
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:54:49 +0000

    undo last teach cry order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
