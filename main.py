dbef1dac20b78601cce14770c7a58591abf55ba2
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:51:37 +0000

    avoid free conduct capital clearly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
