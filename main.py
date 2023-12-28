f96ea09ab09ae89664b26c7625639b5cb9b7aa3a
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:34:30 +0000

    excellent equipment shout open he

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_volume(length, width, height):
+        return length * width * height
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
