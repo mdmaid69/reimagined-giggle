b15b07a40059e90cc9573bb8fa6d4b66b53f8ee1
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:08:32 +0000

    place wonder estimate Qq pipe

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_work(force, distance):
+        return force * distance
