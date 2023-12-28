5bc4722881abaecedeb76aaa6d7c9d82b24e4b89
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:18:05 +0000

    float hollow protest possibly borrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
