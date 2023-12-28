b2bdbc0d472b465a46c318495580174fea11f7d4
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:56:53 +0000

    last both callup dream moon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def find_min(lst):
+        return min(lst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
