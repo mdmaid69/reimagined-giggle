d503801190e422b47b4d66fec1193e3a02e941a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:03:26 +0000

    object between relative touch confuse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def count_characters(sentence):
+        return len(sentence)
