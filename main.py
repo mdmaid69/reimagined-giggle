5924f3469a5a11792b00e717d1f997c456d2a076
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:32:21 +0000

    nigga love previous child pipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
