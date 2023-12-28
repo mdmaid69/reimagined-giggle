b4eeab6fff21da4f1c63efd83852006f2868219b
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:34:51 +0000

    basic tired many or start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def convert_to_hex(n):
+        return hex(n)
