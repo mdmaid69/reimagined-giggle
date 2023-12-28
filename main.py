21abde28455bdc5b6ea50815e0892566ea2bcba7
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:16:07 +0000

    nowhere danger mine bound dress

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import sys
+def print_python_version():
+        print(sys.version)
