c7db9aef521510aedffae031c6c7c3443dab739f
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:27:42 +0000

    complain accept trap breakfast meaning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
