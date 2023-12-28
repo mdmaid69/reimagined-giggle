ed3fef267827713b8279f92a02d4f3d28c7489c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:31:25 +0000

    outside display problem clever introduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def cube_number(x):
+        return x**3
