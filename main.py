871330898ff470a1ecb2698b284e2440cbf3b9a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:58:18 +0000

    land runout top account weigh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def remove_duplicates(lst):
+        return list(set(lst))
