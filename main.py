be9c8b508f1d4d78be1b3caf7648382981c67338
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:58:51 +0000

    shape pot where lot bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
