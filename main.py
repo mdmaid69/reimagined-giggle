4fbc4b8ba619f4065242417fe1bc542d66dab0bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:24:05 +0000

    rid respect comefrom regional rain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_octal(n):
+        return oct(n)
