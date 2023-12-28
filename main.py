42bb3eba8b3fcd4db9ee75c91bdc4ddb31331c73
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:08:19 +0000

    getaway only judge tidy project

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
