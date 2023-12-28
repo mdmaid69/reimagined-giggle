365fbf7753185752196f53b3cc9eec271afc928a
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:30:57 +0000

    big ahead equal compete keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+print([x**2 for x in range(10)])
