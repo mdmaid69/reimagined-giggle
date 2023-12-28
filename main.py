9997b069bfe0705cb1643b43b18a7ef7d67efb68
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:52:11 +0000

    combine least tomorrow dull lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  def remove_duplicates(lst):
+        return list(set(lst))
