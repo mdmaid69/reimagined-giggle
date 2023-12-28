9ab018c2bf2cc92aafd0b16075da4c90668a3b30
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:26:53 +0000

    create kill issue try limit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
