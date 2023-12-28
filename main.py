36ecd776622aa866af535383bb9538178aceffd3
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:08:30 +0000

    rescue secret machine at Bb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
