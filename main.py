c1807d3bca8774e59a34cc0a6ca8e6c42cded207
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:05:10 +0000

    fight strike train hope agreement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_min(numbers):
+        return min(numbers)
