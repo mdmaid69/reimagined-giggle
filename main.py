bcee4571ff2d8386f289c8b89d9cc86c2f177dc7
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:48:04 +0000

    length breakout move smile moment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
