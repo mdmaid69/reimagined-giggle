d689c982d33cce23328e41a55109415bb5f9e93b
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:06:41 +0000

    lost marry mystery wait fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import datetime
+def get_today_date():
+        return datetime.date.today()
