941f33186c51015cc30d46ea5d6b5124cf03aeeb
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:08:56 +0000

    afraid offer under head talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
