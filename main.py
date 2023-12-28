edf16b767f996d968d3c9d47ccdc28b2b68de2dc
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:04:09 +0000

    remember dress hold Oo science

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+for i in range(5):
+        print(i)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
