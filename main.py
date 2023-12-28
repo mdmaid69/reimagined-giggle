fa1ffd16227cf2d2f45ea663588009fc000abf91
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:56:48 +0000

    key couple police appeal cruel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_repr(array):
+        return repr(array)
