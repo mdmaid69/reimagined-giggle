0077b3936e08099cf27b9268611486e5ad4c869a
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:35:05 +0000

    telephone getoff callout stamp hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
