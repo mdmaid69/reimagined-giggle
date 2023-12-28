4415a6b9a149abe6cd5342f726be3b7008f6bcc3
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:32:30 +0000

    apparently that floor good cap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import sys
+print(sys.version)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
