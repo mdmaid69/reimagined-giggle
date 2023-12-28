39f215bbdf83c05bcd1e66295d47acbec8840295
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:22:07 +0000

    hour sound performance benefit actually

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
