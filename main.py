7c91eb299daa6046581bedd39627003d91897a94
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:36:48 +0000

    whistle goout that neither keepout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_area_rectangle(l, w):
+        return l * w
