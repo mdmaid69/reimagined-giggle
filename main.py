4c1cf3f9b21bded843ad3987c2ecdf7ef24ae97b
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:23:35 +0000

    day failure turnback live negative

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def remove_duplicates(lst):
+        return list(set(lst))
+def remove_duplicates(lst):
+        return list(set(lst))
