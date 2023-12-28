436890bd7068830c95ec339695cab126b0e59a52
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:46:59 +0000

    workout strict touch where fold

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print([x**2 for x in range(10)])
