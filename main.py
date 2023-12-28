cffa9d7407fec5c227bc13e2228aae027e2a7824
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:55:56 +0000

    stupid might thirsty pencil peace

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+from collections import Counter
+print(Counter("hello world"))
