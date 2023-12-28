bfe4249a3bcd2cb58a5d7ca99ba2bac148f6afee
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:07:15 +0000

    between keep weigh course clue

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_exponential(x):
+        return math.exp(x)
+import sys
+def exit_program():
+        sys.exit()
