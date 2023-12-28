ffef9b4c13c1729687bd002cd7558b765a050278
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:10:05 +0000

    reject actual scene attitude would

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import time
+print(time.time())
