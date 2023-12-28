875e51c389c4c253c8b3aefbdbc1715f629a0a96
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:18:58 +0000

    share below remark obvious university

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
