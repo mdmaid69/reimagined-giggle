13ac7be891f196600f9ab94d66849e1525ec0dc5
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:49:58 +0000

    else negative smile 566end pretend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
