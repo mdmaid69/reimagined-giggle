31d95b4dccda38b1251b75f99e02fbeadc2b29a6
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:28:36 +0000

    discussion smoke use edge edge

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
