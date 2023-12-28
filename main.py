1fd4e72c086c4f787a9f5a296927485f9345e9a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:28:50 +0000

    many old well suit chance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
