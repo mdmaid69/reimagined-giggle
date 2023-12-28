f6de31491ca37dff563c6d0a39188d1d463196d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:28:01 +0000

    fine dream shall polite note

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
