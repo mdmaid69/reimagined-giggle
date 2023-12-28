07a158d78197d64a6c60fc7bead88c334277e027
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:33:46 +0000

    little love when mail wait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
