e9b6e83477fba27e5169f8ca29b79b94221d80d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:57:52 +0000

    decision trial now affect police

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import platform
+def get_python_version():
+        return platform.python_version()
