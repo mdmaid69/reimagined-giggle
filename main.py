aefbec2df687dd576e690a7bd6eaedd9e11c737a
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:58:59 +0000

    pick include release measure clean

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
