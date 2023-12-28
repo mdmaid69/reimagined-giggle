776c9d0012f29a16054c70bda54e481db8ece752
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:53:02 +0000

    quarter road thick afraid television

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
