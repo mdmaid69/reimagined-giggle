7e58e8ee843b83c1fccd6bbec97a4b5fa49ae661
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:55:10 +0000

    post shadow process street sweep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+from collections import Counter
+print(Counter("hello world"))
