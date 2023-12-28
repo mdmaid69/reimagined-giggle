0c0847291f75125a0400a9fd23cc4e01356839d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:04:06 +0000

    warning owe small flat path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import math
+def calculate_sine(x):
+        return math.sin(x)
