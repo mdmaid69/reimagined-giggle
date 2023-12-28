aeab6e303271ed7693c16ccf1eec4ac1a2f91c97
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:48:30 +0000

    yesterday trial weather suit not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
