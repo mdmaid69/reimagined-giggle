33c04d391c38001d2ade712474fd3b67f40c969f
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:30:57 +0000

    careful through light constantly relationship

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
