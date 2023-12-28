2e71206e35806b53fca557f135abdba4c7506af5
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:37:04 +0000

    lookback(on) available various generous discuss

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_pressure(force, area):
+        return force / area
