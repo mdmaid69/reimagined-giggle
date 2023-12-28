251d92f10fb606fb009d3be7ead54fd7752118b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:38:51 +0000

    create hope trouble pause not

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_floor(x):
+        return math.floor(x)
