a14759d7790aaf470e6e0421117aaaab8adde50a
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:14:24 +0000

    whom Tt position thick wonder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_typecode(array):
+        return array.typecode
