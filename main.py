0aa2e561106eeb8624c78c89fe1a4af793fb29ec
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:19:57 +0000

    nearly dear roof practise live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+x = 10
+y = 20
+print("Sum:", x + y)
