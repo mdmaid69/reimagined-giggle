2d597694a0437ffc0c91d3e2805a888930af68a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:21:06 +0000

    ordinary authority giveup tool interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
