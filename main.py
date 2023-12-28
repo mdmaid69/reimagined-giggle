88dfc5794ca488aa910442a2f7499d8be4120c5f
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:06:56 +0000

    lunch job fall bottom one

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
