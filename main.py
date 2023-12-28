2ddcf60da6bc11c7af7ab486ec3ce32fc7fa7c8b
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:12:31 +0000

    emphasize teach vote problem birth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def exit_program():
+        sys.exit()
