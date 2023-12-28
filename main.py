a187c0b1ebc045a3d448ee86eb509b415e77ee96
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:28:47 +0000

    win crash content mess return

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
