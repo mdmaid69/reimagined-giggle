4aad94ab89cb1e9b3783e12058f8ecd62a2c7569
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:37:08 +0000

    value hair jump arrange turnover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_current_directory():
+        return os.getcwd()
+  def convert_to_octal(n):
+        return oct(n)
