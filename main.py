b7a7f314eba0303af33087e084e130e3b939426d
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:29:46 +0000

    error really murder keepoff keepoff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def print_python_version():
-        print(sys.version)
+def convert_to_octal(n):
+        return oct(n)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
