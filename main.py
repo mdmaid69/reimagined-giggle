832780da8645fe98cf3b67bbdc301c2efbae5152
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:54:32 +0000

    takeaway amount bother difference positive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+print(math.pi)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
