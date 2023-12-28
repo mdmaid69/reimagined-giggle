067d9d07167979e96d66439fd3c3e0de1f7cf7ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:41:38 +0000

    dull accidental very stone badly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
