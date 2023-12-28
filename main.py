532e4a4e68cd21ee209f0c5cf025b5f4b030d486
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:37:40 +0000

    suppose moon wander wait occasion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_density(mass, volume):
+        return mass / volume
