5130e78f734c422c1bcd916ebfb3825a8ae01519
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:04:21 +0000

    everywhere play fall expensive Ii

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def convert_to_binary(n):
+        return bin(n)
