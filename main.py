3a7b44081fdf7528bffecffed7811c1e6aff468e
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:35:41 +0000

    definite reduce why guilty accommodation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
