5d9ca03170e7432e2948b5b2108a9de63b2e5559
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:01:06 +0000

    trial completely office follow usual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
