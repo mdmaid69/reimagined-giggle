6cf2d84f3a756afb0acb3d0613788666ff0b3b00
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:13:49 +0000

    must difference step offer principle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def sort_list(lst):
-        return sorted(lst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def convert_to_hex(n):
+        return hex(n)
