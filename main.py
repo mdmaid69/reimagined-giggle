b0ef069ea407028ee3586e08b5646809134b7077
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:07:39 +0000

    ask poor smooth sink free

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import tensorflow as tf
+print(tf.__version__)
+  def remove_duplicates(lst):
+        return list(set(lst))
