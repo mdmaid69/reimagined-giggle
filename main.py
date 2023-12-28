8ac9a931ee868c63c9d5818a6d37865f208f40fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:10:26 +0000

    bang study until single unable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
