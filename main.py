e7ae1f1eb4b176889a0c33ef4752060a07a84694
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:18:44 +0000

    minority patience base squeeze switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def is_odd(n):
+        return n % 2 != 0
