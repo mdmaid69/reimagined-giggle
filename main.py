be789e48416a75716ed7b64eef2c52529f3a2d6b
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:36:25 +0000

    ride university answer stop picture

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
