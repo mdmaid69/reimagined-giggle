f3aeb4b79729b19c9b0b3f7ebdf6ebb70df67577
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:14:18 +0000

    past to patient reasonable getoff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
