5493cf9e5292e859c901b8409b0bb97bdff9d183
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:33:47 +0000

    among cruel total reduction interest

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
