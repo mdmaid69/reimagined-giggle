e50713097274993e006313ca09f572d348f41935
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:54:25 +0000

    Rr Ll often board talkback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
