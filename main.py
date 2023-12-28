193acd89461d720490f36c1b3a2c940b9a829c41
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:28:46 +0000

    broken definite issue plenty term

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
