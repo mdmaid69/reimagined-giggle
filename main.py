90e4db7d44ee8541fb16871a961f6393f9416886
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:41:45 +0000

    record order miss nowhere language

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
