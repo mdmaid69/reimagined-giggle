afc3a494cc9335609ac895db494ca9c971612633
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:22:19 +0000

    instruction getup callat load owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
