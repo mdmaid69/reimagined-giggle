add7edd0e115c4a3c92f2fbd02d30cfedb6d14a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:34:32 +0000

    lookafter find defeat check shape

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import sys
+def exit_program():
+        sys.exit()
