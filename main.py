bd06c4c3a2a6d78f3e7910d9bd817f2e74a7c480
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:41:24 +0000

    best high shelter silent recovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
