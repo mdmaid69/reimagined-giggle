5903f68df92ea437f484c8762599c85bd08ccb84
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:02:01 +0000

    past sky bringback commit badly

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
