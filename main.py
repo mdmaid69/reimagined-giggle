94cf1e49ee9966fba17b06e302b34a3acfbaa528
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:00:51 +0000

    less official swing wheel keepoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
+  def get_file_owner(file_name):
         return os.stat(file_name).st_uid
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
