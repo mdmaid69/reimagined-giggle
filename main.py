2583f9567bb9f4e47494c6dc878767e444c6d0ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:37:23 +0000

    list workout develop love blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_cosine(x):
+        return math.cos(x)
