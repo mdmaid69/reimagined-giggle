4a4709d9eb1b26d9ad3a682281427640fc7f6e3c
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:37:27 +0000

    country chat earth university get

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
