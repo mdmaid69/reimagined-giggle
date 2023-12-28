06d571131cea657619c4fc3de4c5c23949170b52
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:56:07 +0000

    month sight cope whistle interesting

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
