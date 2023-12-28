dc8be8f9fa259a6c54309deff7397c2edfa843ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:49:11 +0000

    appearance study precise glass future

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def count_elements(lst):
-        return len(lst)
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
