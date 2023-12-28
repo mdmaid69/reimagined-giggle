cf80278a7da438fe349475590ac7528730fef39e
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:27:04 +0000

    introduction pressure organize and switch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
