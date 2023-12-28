7e21e3489b0d0d054c937f06081170beedad3ecb
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:55:32 +0000

    think force care lean hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
