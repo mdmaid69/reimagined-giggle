db3fdaa80b75aaa3f3670eebe43a555245ec9694
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:48:08 +0000

    steady type disturb stir sign

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_from_bytes(bytes, typecode):
+def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
         return a
