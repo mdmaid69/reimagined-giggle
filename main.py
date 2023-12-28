7dcb479dd0fd628b16712aecb55cdca5fe42dc84
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:58:13 +0000

    concentration piece wrap slight message

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
