8bbbb7ff4b5f744c7c3d4870cada9bcb747512b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:13:49 +0000

    necessary please official period supply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
