66630886d012de534ff794c582b32538fbd67b39
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:54:40 +0000

    unless hospital under hurt pretend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_as_str(array):
+        return str(array)
