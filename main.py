e1dc3edffe5bdecaa2cb9bab6ee2dd6e96e22f92
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:52:48 +0000

    thick argument star maybe say

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def count_elements(lst):
+        return len(lst)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
