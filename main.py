e7c935ed7569bd0afdf5b81d70edb725e84e42e8
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:44:56 +0000

    number behave callat back house

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
