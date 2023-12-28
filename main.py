7047b5e522e6c38cc714aac5a7f2c6d44995600d
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:53:39 +0000

    roof spoil certainly share wonder

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
   def get_file_name_without_extension(file_name):
         return os.path.splitext(file_name)[0]
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
