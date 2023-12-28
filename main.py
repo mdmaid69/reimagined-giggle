ed91ace1911ed47fbd8a8417487b94bb4aea63ff
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:21:20 +0000

    male smoke arrangement finish mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def divide_numbers(x, y):
+        return x / y
