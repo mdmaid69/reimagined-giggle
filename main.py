63829672425366f7c4e510745389cc87288019b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:06:43 +0000

    power cruelty school comment meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def square_number(x):
+        return x**2
