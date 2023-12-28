081092987fb9a2139b60cfe4afb300ef47caf54f
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:06:02 +0000

    mention light intelligent roof cinema

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
