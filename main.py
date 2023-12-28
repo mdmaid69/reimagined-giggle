a49ff299b0c8072cc0d785a7930dbf062df0f801
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:48:37 +0000

    call involve settle beyond keep

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
