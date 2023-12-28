29c6af08f190c7c66028f96724c39183ea26b6ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:20:35 +0000

    they street there glass black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
