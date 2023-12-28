cb4fb0907771140fc83069de5174aad68eebc726
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:05:38 +0000

    hate used touch address event

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import sys
-def print_python_version():
-        return sys.version
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
