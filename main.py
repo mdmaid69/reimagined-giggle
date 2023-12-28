e8eca98063c070036cf8d9e869eacdaa179da842
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:15:55 +0000

    contribute makeup shape inside clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
