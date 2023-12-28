7351c332420d59cef9a83bc332e1c3b8f3b71016
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:59:29 +0000

    calmly carry contain trade mass

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
