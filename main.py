9746429a35419d24d2d5e2ab35ebc0797eeca244
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:33:45 +0000

    sign burst somehow behind pocket

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
