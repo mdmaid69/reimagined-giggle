0b49e17e3e4a894e06bebbe7130e1e1c4df5ab63
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:47:26 +0000

    tidy comefrom form ours wise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
