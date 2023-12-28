c82ac99e73e1679d0d4bb1bc2c09e9ff680bea19
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:33:03 +0000

    unfortunate joke ordinary note bear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def split_path(path):
+        return os.path.split(path)
