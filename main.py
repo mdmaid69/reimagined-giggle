d03dfdd742a68fa0cfe7e2503fdd488aeb29fd17
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:22:35 +0000

    transparent geton mine later soak

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
+def get_array_from_string(string, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        a.fromstring(string)
         return a
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
