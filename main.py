67e0f3676fda0f0e303fb13d100c3487f6abe36c
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:05:05 +0000

    breakthrough learn summer bin gap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def add_numbers(x, y):
-        return x + y
+import array
+def reverse_array(array):
+        array.reverse()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
