4915ff48bce89a17dbd3c198a902c3e771ee6932
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:46:35 +0000

    give worst dead team naturally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
