f2c6e2e9612346e2a55a6d818c6b600874807c10
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:43:20 +0000

    large model press bad excited

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import numpy as np
+print(np.array([1, 2, 3]))
+def greet(name):
+        print(f"Hello, {name}!")
