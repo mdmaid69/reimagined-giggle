55f94a2625fa706cec7e01635fd493fe8b1564fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:15:44 +0000

    possibly zero scrape true report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def is_odd(n):
-        return n % 2 != 0
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_speed(distance, time):
+        return distance / time
