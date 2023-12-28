edd9f60192051dfeae2711cdf2f94eed5ed5e7ec
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:23:51 +0000

    certainly lower write draw pipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
