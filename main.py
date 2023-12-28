1705490608da233dae8b1b1590dafea7043448b9
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:49:30 +0000

    spare book replace clearly smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
