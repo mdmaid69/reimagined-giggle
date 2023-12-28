9930e8ba418158cf7de25314eac3421223faf849
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:17:32 +0000

    unlikely year kind by breakout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
