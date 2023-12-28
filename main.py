5e55f62575ff4e62297aa7af86f636b5c9d083a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:37:26 +0000

    ilovedicks need priority scrap introduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
