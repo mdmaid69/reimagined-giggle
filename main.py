de862c0411d4dffc8c05c030679243f760f120f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:17:44 +0000

    recently otherwise complete sign around

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import time
-def get_current_time():
-        return time.time()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
