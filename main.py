57a15a89c2b016bd903434ad779b31422904285c
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:51:04 +0000

    recovery prize touch maximum crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import numpy as np
   def calculate_variance(arr):
         return np.var(arr)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def count_elements(lst):
+        return len(lst)
