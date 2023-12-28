ca11c60dfebb561f40c070af733faf1ce46c508c
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:16:12 +0000

    address stranger ball why arrival

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
