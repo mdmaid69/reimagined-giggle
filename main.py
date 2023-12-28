69d248fc68a69c31a0b25ef2c9d0c048c7860b9a
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:28:26 +0000

    ours claim yours send hot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
