efc15a8021ab11a7918d84f159b6f43bd8f80b06
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:43:11 +0000

    advantage value reduce careful callat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
