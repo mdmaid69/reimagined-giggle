029dd9f492ace4cf9dd684b163c770c56950ec8c
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:06:42 +0000

    peace expect odd sad machine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
