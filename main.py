21390b84deb2f8efbf0a417e35b2b0c9cc3ecd96
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:52:30 +0000

    length detail otherwise again forever

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+print(sum(range(10)))
