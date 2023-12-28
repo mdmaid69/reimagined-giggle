4c7549231178ae46cad671af8e1b0bfe246759cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:59:48 +0000

    fit relative deliberate reasonable track

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
