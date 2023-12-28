deb65e16fc10d764fb62df7f96311c365708b270
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:18:19 +0000

    Ii deal watch determined opportunity

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
