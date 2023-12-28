2617d816e03cf6dfdf9be938391a6b0abaa118a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:46:19 +0000

    skin walk passage rush broken

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
