5081c3a3bac6a46e021efe13640cca58af1cf6f1
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:45:53 +0000

    floor freeze apparently heavy somehow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
