bbd27706a818a900254ebc44dc76545c2ec97f9e
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:16:33 +0000

    square crowded send book key

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
