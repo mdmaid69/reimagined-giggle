93949bd0016b800b538c5f3b86dc2d2d54281e4f
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:26:24 +0000

    ride fight use sometimes think

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
