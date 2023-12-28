debf80e003c05dfc64dc05c4f443c3fa9b713177
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:23:56 +0000

    correct recognize relation goodbye collection

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
