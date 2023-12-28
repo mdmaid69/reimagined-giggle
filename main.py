780676e27b448e473b42ae95e383b3febf9bcfce
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:34:14 +0000

    community load situation conduct fix

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
