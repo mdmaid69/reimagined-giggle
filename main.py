75cbdf246c4c56983f3e68439c4cf4475cdd7cdd
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:59:32 +0000

    appeal wrap if fold formal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
