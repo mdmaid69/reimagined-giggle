71e1f1e482b513730fc43a1faf6a0857ca6e14fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:37:49 +0000

    turninto cinema cause science drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
