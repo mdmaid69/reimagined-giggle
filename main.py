9029fcab68347c642c227fa8b9cb1b8525932a3f
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:56:28 +0000

    communicate lean thought murder reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
