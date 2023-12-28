46e390718526f043493c5618dbd2cc3ce48ee3ee
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:51:27 +0000

    spread him recommend from comein

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
