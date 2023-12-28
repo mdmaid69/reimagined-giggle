6dd1900d3b6c4d6384f4eba58ff7f10869fb4b2d
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:50:12 +0000

    poison impression use cry month

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_bool(array):
+        return bool(array)
