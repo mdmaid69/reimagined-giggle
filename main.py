9dd01a62602c3ce89888cec6ad54ea5f79b41f93
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:06:27 +0000

    picture like smoke false for

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_odd(n):
-        return n % 2 != 0
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def get_array_as_repr(array):
+        return repr(array)
