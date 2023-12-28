c0aefd4d6f72adc63b5fa031bdd7ed71429c68a5
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:49:02 +0000

    breakfast quarter sail tough catch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
