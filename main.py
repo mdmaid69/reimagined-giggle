1686b86fdb7f909249ff3ababb26d7f659e14431
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:08:32 +0000

    mean brief print winter both

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_list_from_array(array):
+        return array.tolist()
