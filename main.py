bad0888181a1ddefb210e596a0b093bdc036a543
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:32:39 +0000

    disapprove free dull offer raise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
