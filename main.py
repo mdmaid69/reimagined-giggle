982be7527b4cabbca66ce075c1526ef3dce12255
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:38:24 +0000

    sharp switch stranger argue payment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
