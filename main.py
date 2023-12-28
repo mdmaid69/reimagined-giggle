5bb2987519d07198a9f0c52cbe60d5fd4c22d404
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:50:14 +0000

    switch hurry ashamed port goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import os
+def get_current_working_directory():
+        return os.getcwd()
