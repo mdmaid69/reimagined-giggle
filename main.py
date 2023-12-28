748c217d59961a44bb571d7e6c3032f5ff37d0e3
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:57:58 +0000

    copy takeover warning consist along

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_max(numbers):
+        return max(numbers)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
