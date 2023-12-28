cbd32fedac84d76c7dabb32dbe29b29c57345df0
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:54:14 +0000

    design hollow circle relation anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
