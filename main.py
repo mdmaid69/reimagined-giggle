e28b9bf03589d3feddedae577f1f158a0ac85a77
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:52:39 +0000

    steep mix shallow basic quick

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
