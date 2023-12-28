2ccccbf144a4c8cdfbe4ec86b6e612d382399a3b
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:41:37 +0000

    begin deliberate narrow once choice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
