2885596924bf7c3f54692ca626e5af7252e5baae
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:54:33 +0000

    safety comeover judge actually Zz

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def remove_duplicates(lst):
+        return list(set(lst))
