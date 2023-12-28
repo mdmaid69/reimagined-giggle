4f5eefb5e61bf66cd8229e3239a5526e7acd2c49
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:57:06 +0000

    Jj check discovery connection success

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
