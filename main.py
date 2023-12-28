f7a933dca4862a1bf8d4fe762e1b89a6a8d7d952
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:48:34 +0000

    572enter embarrassing pipe much getoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def count_elements(lst):
+        return len(lst)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
