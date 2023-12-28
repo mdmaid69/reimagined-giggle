a25b142867980171097ee8d36c25970efe31eab1
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:28:31 +0000

    breakin avoid cruelty excuse challenge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
 import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def read_from_json(json_string):
+        return json.loads(json_string)
