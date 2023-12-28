abd9376ea44157c0ac03bf8f9f465484e5899c7e
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:06:44 +0000

    smooth already limit exchange related

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
