e2ac515f914356b6d772388230f065149d6268c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:04:36 +0000

    example ask equal possibly acceptable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def greet(name):
+        print(f"Hello, {name}!")
