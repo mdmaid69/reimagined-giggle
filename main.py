36c89068220084f415f843e31a5acbcd44e9bd42
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:49:58 +0000

    suddenly family report dress act

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+for i in range(10): print(i)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
