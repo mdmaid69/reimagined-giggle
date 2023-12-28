2ef2c9c9456056e204fc5e5aca2a789253dcdd9a
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:33:25 +0000

    exhibition plenty lead terrible dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
