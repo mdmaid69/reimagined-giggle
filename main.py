a30c3cc0ea68eebc332fdd0c40523db9df79eaa7
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:59:17 +0000

    punishment finger check future careful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
