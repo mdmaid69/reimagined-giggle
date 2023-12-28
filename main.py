fe654f097c98f0d0ce00de47d4587efad431ab96
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:56:25 +0000

    exciting blind hurt 571enough design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+x = 10
+y = 20
+print("Sum:", x + y)
