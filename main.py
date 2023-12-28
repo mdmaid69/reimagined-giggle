a2a32e72a872271414f0c323534febe93334153c
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:15:17 +0000

    guard inquire discuss protect major

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
