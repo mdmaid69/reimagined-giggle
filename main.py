d2e04ad901206a1c2d79949ac34f2473a69f1ef2
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:49:48 +0000

    all turninto switch insist door

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def cube_number(x):
-        return x**3
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def append_to_array(array, item):
+        array.append(item)
