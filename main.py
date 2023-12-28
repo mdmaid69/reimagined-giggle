996848b253d8c2bc661d4dde60127b1cde433803
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:53:02 +0000

    best unhappy aware above cool

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
