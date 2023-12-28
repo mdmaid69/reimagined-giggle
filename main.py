c3a0f03a028087b3921b579e4b03f3813cba2bc3
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:11:45 +0000

    realize often judge slip judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_power(work, time):
+        return work / time
