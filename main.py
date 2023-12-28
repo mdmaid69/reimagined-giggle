ee355d28c8147871b5a82dcee0c207fe0160c020
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:04:04 +0000

    wish bottom write track tough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_density(mass, volume):
-        return mass / volume
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
