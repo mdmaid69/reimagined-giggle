27e1c1c7f882954dc5ac8a53f6ff0bc4ab6174d0
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:54:54 +0000

    next home ride perfect blow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
