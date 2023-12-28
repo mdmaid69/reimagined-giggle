21aa6ed365462a1b9990140917788f7a4fd8eef3
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:13:52 +0000

    national Kk me confusing lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
