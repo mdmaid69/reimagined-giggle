199a9ff3c726268a89e862428cca490ab8e2bc09
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:50:55 +0000

    interested fact relax matter subject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def is_odd(n):
+        return n % 2 != 0
