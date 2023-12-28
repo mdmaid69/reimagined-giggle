1b9440755921ce71fee49ca5fb0b18e194516129
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:27:53 +0000

    ask immediately across least cap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(10): print(i)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
