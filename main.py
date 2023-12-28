d3bd48a684e46ebf4e47c2de7998c2feaff8be40
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:40:55 +0000

    part worst alive common worry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
