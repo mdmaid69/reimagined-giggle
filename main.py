874314bc050610b3faa16e4bb748b2efef46d05e
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:45:15 +0000

    guess lucky straight side attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
 import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
