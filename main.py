5b330ad806d47b9f1e400d2e76c4c79ea4cc9167
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:48:20 +0000

    sweet unite side check listenfor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_bool(array):
+        return bool(array)
