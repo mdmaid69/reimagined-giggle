db6ed772fc1d0479705daa3528bc7d2703cd1fdd
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:21:13 +0000

    undo stop trust club responsible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
