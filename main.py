4a5befa086f3862d97a736889c7ef3b1a2a163cf
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:32:14 +0000

    hello holdup disgusting almost save

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
