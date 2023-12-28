bb4481fa84896aa18271135465dcd6d6e0215e19
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:27:28 +0000

    next achievement scare ignore fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
