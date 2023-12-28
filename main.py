d627f1db775ab8550a42db4940d0e982c57959a1
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:18:16 +0000

    game wish blame sign warm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
