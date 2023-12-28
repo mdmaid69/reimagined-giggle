b7a68d7f76754caa94faf57d17dc5aea28282db1
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:48:24 +0000

    lookafter space bringup absolutely nice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
