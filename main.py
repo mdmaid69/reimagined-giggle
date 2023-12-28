4b7a5728803b73f366c9cca248bb87a864a76fe6
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:35:11 +0000

    consider unit influence coat loud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
