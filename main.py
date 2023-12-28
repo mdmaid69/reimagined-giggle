e160430c508519b3cd914bc2411c130e013d7dd2
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:22:15 +0000

    correct breakinto serious lookback(on) taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import platform
+def get_os_info():
+        return platform.uname()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
