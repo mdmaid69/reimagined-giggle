d02e5f917bf1020864088edf3288faaf120360c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:47:40 +0000

    butter protest play criticism old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
