fc2d082e497443c6db4955d2846ac20a0588acee
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:41:40 +0000

    desperate pipe close pair or

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
