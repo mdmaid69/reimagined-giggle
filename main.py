69fce90fcb8011f7a044a352539e19e947852749
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:50:02 +0000

    swing unhappy neither damage Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
