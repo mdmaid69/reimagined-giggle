f6c3cb44230ac0dd5fe1515ee4f199e5cef56ea2
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:31:07 +0000

    furniture everyone visit separate contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
