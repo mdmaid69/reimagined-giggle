38a96593f0ea3ce051745feaef41650f044e6bd6
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:12:51 +0000

    mix 179bite sing bear care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
