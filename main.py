9554bf4f11b449da4a84aae99f3cfcb85b57acb5
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:15:09 +0000

    twist flat welcome thinkover dress

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
