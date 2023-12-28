4bce65b445779239219130cc0db4fb410836c2aa
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:51:22 +0000

    strong accept split brave group

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+print(sum(range(10)))
