b0ea6f2eefa3252cb851c6c1bb924e8e32f9eea3
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:08:26 +0000

    money children less estimate from

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def convert_to_hex(n):
+        return hex(n)
