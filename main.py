3d23d02f95979a52fd7bc617901eacb571eab776
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:50:54 +0000

    late protection sore trip division

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
