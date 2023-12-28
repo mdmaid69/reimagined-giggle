c223cbace96e673bbecc35439922f24938095dbb
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:23:05 +0000

    division whoever snow worry into

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_speed(distance, time):
-        return distance / time
+for i in range(5):
+        print(i)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
