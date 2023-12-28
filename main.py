7fb76460067dfcdd3aedc043988c742005a8e5c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:29:01 +0000

    tap payment imagine tip trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def cube_number(x):
-        return x**3
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
