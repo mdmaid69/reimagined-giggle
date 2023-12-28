73b2e330eefa8212d5bf0f427ff517ee2b3b114d
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:18:07 +0000

    tidy custom thorough visit exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
