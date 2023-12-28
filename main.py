4d7ad129cf07bb3913955638908b9461761cf04d
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:22:05 +0000

    quick not pinch exercise lock

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
