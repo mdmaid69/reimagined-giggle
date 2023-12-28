6f60d5a37a46e1900df0d44bf2ea43d3cc9d61eb
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:52:25 +0000

    takeover least practise case sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import getpass
+def get_username():
+        return getpass.getuser()
