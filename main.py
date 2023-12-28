3148a2bba7a84ae026c984c97aa751289c905238
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:15:57 +0000

    price extraordinary sore stand unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
