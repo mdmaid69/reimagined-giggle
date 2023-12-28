d68e338ac9c6b534fa514c6f6465a03df4185060
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:01:27 +0000

    after hers reduction worried forget

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import sys
-print(sys.version)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
