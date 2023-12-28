415c91a9f014671ecb1ec4aa7c3aa9c604fb8fe7
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:42:29 +0000

    trade detail frame tip law

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import os
+def get_current_working_directory():
+        return os.getcwd()
