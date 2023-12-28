e04bc323e143512cdf764066a6125a77477133bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:59:55 +0000

    fortunate wish hurt save exactly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_volume(length, width, height):
-        return length * width * height
+def square_number(x):
+        return x**2
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
