68b6cb8ec7fd7ccfbce576e1833ff21d44985e21
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:42:58 +0000

    train sort similar since join

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import time
+print(time.time())
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
