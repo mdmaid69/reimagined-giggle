3c5daf57b8207fc601bb76db940728d7aef6f652
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:06:48 +0000

    turnaway defend outside plain conclusion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
