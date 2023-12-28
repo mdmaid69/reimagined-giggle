4f187a9f2a8df034f4c36e27800933236db49bd2
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:41:34 +0000

    design protect Ll big zero

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
