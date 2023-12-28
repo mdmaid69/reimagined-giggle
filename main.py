ad7e8af08bbe07c195bcb26aad4d83a0dac8449d
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:16:29 +0000

    pull affect fast importance stick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def cube_number(x):
+        return x**3
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
