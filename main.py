1c76ee29c0e100610833feb2dab48dc73bcdccc2
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:45:17 +0000

    use notice regard contact air

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def sort_list(lst):
+        return sorted(lst)
