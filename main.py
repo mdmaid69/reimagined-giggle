3a2ae3d11494e527256d6acb8fac6d4071df6f76
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:49:23 +0000

    everybody suggestion affect consider right

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
