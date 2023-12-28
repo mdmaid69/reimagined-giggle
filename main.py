f52a41d372ace9464ffbd491bbe4ca5d20d380dd
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:33:47 +0000

    motion bury theirs refuse muscle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+for i in range(10): print(i)
