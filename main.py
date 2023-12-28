b82b32cf68e5fdc164805321c1f184edae73240d
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:42:02 +0000

    broad push combine cheat show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def convert_to_binary(n):
-        return bin(n)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def cube_number(x):
+        return x**3
