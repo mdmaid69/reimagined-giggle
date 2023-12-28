0b975aaf3c46f2387cd4d8a71fcfad14141b2088
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:17:21 +0000

    surprising pollution draw question confident

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_pressure(force, area):
+        return force / area
