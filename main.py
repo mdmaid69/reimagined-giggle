213a0f7b5a588c0b41cf4af3cabed57e127e5b7c
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:31:51 +0000

    undo position thinkover time pay

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+print(sys.version)
