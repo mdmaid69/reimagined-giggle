564145abd9f492221fcb625f33db219bc33ff722
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:43:12 +0000

    sensible stare protest tragedy unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
