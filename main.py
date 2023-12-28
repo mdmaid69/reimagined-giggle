3873262fea089bf8b0ae236a033ac03dc24a1846
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:28:21 +0000

    accidentally land godown bring pride

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
