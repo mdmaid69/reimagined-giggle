13ef68fc1f95d543fc34698007412c34c6946991
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:33:26 +0000

    yours break owe control look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import random
   def generate_random_number(start, end):
         return random.randint(start, end)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
