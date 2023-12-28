566993afe9bcec99a98e41d5bc10475cb1f2343b
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:50:45 +0000

    swing prevent absolutely free party

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
