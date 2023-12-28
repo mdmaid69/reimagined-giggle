9f65ec14ee97e767ae8a471547430f33e87cbd8d
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:55:11 +0000

    exit main finally otherwise thanks

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
