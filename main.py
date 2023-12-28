73bef4bc4eafacdd86741cf881b7817a70afea93
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:21:55 +0000

    married successful hour good exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
