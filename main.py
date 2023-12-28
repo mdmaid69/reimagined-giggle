ef6e508c9c52f1e75cdaa0aa527914fa93ae3ba8
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:47:26 +0000

    pride promise related worst spend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_floor(x):
+        return math.floor(x)
