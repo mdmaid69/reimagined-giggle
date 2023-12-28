894cc5abcc89213492f047da0e9282bec1c27785
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:22:52 +0000

    guess too muscle show juice

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+print(sum(range(10)))
