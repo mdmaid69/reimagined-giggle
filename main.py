d72059ba2bb53fc07cddea5421cb8c79e4f8d83c
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:52:59 +0000

    Gg criticism shopping broken force

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
