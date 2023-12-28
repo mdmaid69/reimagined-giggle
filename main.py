a55f95696eaf0e36e7b809672e6b2f86bd525f98
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:23:20 +0000

    about sensitive he pleasant bringforward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_memoryview(array):
+        return memoryview(array)
