34811192565a86ceca564e8e630038696340b435
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:25:29 +0000

    sorry plan whatever late pass

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
