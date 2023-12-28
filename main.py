0ab77828ff807e9f986bf0aa308475178824019b
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:00:36 +0000

    fair check exit kick whenever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
