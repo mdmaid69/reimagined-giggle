2bd2eebbca06a234600e2374b34779e191014c8a
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:56:46 +0000

    from callback stupid trousers relief

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
