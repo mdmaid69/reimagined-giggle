eb03f7785cc29ee54d92cf28641c89268beb90a1
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:13:10 +0000

    office Gg replace track ourselves

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def cube_number(x):
+        return x**3
+def convert_to_octal(n):
+        return oct(n)
