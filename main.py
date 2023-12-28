e145c41d140cfe8d6315a59bbf7bba59791dd862
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:10:54 +0000

    telephone little willing Ll mystery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_user_list():
-        return collections.UserList()
+def count_words(sentence):
+        return len(sentence.split())
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
