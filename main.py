fc7321ec3aa67783362d82dde2259fee1df480bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:31:27 +0000

    callout stay Uu always birthday

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
