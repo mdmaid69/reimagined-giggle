1a0335ff28a2ae052236ccf550dd1bb3b8e015bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:52:05 +0000

    concerning back bye bag lunch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_pressure(force, area):
+        return force / area
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
