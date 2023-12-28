c1448fcfa5f41b36f23b9d39623310b691a92f01
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:07:46 +0000

    difference television knock hit only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
