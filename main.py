ec1ad7a4a013d8f5153a4bb6e0505fdde14ec439
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:57:01 +0000

    stiff cook probably wonder soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
