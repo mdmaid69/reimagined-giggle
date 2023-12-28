b99ed8b1c5f5cad1e1d546d9c5e1fdd422e849a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:49:23 +0000

    behave Kk front bring exactly

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def find_max(numbers):
+        return max(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
