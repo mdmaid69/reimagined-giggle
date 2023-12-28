eae72eaa82bce90406f5cca553b314981f4e0a96
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:15:32 +0000

    spray practical height block so

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
