f6dab4e5622c3ffda1d9e6637169ac36f193ed7b
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:44:49 +0000

    mad pleasant expect remember job

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_acceleration(speed, time):
-        return speed / time
+import datetime
+print(datetime.datetime.now())
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
