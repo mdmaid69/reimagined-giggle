6228d74350b04b950e13b4771df8a36510487475
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:51:29 +0000

    true decision quiet water increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
