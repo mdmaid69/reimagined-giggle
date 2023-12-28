326badbd427cf83b8d909fe446e302609420f442
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:14:03 +0000

    slowly strong else building worst

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+for i in range(5):
+        print(i)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
