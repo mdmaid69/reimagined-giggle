d72851a59ec321d2d91c10bac3d4af0066c8ba44
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:06:05 +0000

    increase none but dead far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
