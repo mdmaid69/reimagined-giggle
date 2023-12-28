bf438c1ec370798dda2410031fd91f4a0aaa8aa2
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:52:18 +0000

    fat willing this build slightly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
