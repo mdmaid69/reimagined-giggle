9a2fb7e7a57e22cb1a86fe5032a67f87ad760520
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:07:16 +0000

    difficult warning near do getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
