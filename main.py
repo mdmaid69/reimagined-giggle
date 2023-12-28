7d9e41f9a7cc2cd4cfb768138d9eb836260c42b5
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:34:54 +0000

    standard public pronunciation air receive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
