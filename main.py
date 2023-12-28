d08c2ef03b9febc4350cbee608a681f431f00a50
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:20:17 +0000

    star round bottom book storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def delete_file(file_name):
+        os.remove(file_name)
