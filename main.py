5295e896ca47546561283685b89d0be7c5910608
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:38:43 +0000

    show limit issue except tooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
