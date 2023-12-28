edc972f13aaae34b0bce2d982279b42d0126a880
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:02:01 +0000

    rob cover autumn central shock

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import sys
+print(sys.version)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
