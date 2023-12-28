dc357baa300255d9b487a98ccfe36c18caa9de5d
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:34:41 +0000

    immediate lower shine area pair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
