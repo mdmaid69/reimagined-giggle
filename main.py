b78da33e8f7cf71e5fdf2bcabeba2914f9c82b53
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:03:38 +0000

    back left between set laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
