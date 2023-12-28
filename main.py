7e526ecfa9df2f8af845ee8882d2f33218d2c338
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:17:45 +0000

    ocean when confusing stop common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_base_name(path):
+        return os.path.basename(path)
