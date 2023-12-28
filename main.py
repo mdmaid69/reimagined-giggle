ce007711e52c3da8a58ff1927b8b283848ca550f
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:50:27 +0000

    trick through figure want string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
