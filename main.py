92516dafb6f00036af8b75c9eaf3f1092ec18dc7
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:23:37 +0000

    minute will Uu view since

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
