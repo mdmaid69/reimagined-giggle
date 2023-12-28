0f0b01db3b72b54e95ea3f36ce097ef8c2e6835a
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:55:21 +0000

    work either climb crisis breath

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import os
+def change_working_directory(path):
+        os.chdir(path)
