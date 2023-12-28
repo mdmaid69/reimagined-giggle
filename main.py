8069860bcca67d8a80cea722c3654222bdfcf611
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:55:54 +0000

    godown urgent great rubbish kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
