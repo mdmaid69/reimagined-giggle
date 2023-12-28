e552644f779dc7e3326dc3520a8d8b4fe3548dbd
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:29:10 +0000

    butter tape only laugh from

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
