b9cd269e85b5abf3c61b623262716ea75593b17b
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:40:09 +0000

    repair unless noone hello occur

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
