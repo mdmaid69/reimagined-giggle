6bb15b5b467c97aa45f8a9fd08d942997fbf83fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:18:41 +0000

    believe study food left anxious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  def cube_number(x):
+        return x**3
