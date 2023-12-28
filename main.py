59fc28cbec110d98a6e0fd921f4837bbbc842fed
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:09:13 +0000

    stay instant problem inside respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def delete_file(file_name):
+        os.remove(file_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
