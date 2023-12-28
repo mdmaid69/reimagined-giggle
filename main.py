b0c0d95a1b156320138c2697861cf7784933b762
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:11:56 +0000

    flight practise appear comein broad

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
