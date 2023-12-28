33e5e8eaf13d154ccd8f37ecf76622022e524976
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:13:00 +0000

    fear foreign explain design contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
