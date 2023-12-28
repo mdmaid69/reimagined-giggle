b4f35159e963c7c4b08b8ef936be33a632b41582
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:49:04 +0000

    disapprove pay knowledge problem whom

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
