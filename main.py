73ed764e0c73307d1818d2df2d51fa459bd518cd
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:40:25 +0000

    shelter cool film argue advantage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+text = "Hello, world!"
+print("Words:", len(text.split()))
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
