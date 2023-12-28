347c1ae65fd5557f1f327526a7c41031f36b4696
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:26:35 +0000

    call heat bus under mine

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import datetime
+def get_current_date():
+        return datetime.date.today()
