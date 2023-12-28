6eae60f007038ae02c1187697c42b09c1d61c9ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:03:18 +0000

    announce flood distant ashamed concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
