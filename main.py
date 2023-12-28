5cdd51cf02c0831ca10f10ac68f36b32c87bd582
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:57:47 +0000

    whole rely mistake check smile

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
