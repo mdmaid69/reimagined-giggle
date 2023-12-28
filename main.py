f45789990d87c5b2e4ea9ba0e01498119d1db359
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:28:18 +0000

    runover apologizeapologise worst disturbing minority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
