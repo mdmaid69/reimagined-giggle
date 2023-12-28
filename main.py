68f4f887c50bd559b13e4f257833767b265eb6d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:11:39 +0000

    protection content practice marry about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
