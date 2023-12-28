8fad1b69ab34137d39606e85fb3558451c9c4df0
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:29:05 +0000

    actual boring risk skirt use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
