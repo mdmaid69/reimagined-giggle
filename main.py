b4e36266897ca831f9a5e638afb1ea0f90c676d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:33:49 +0000

    roll excellent gap principle satisfied

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_current_working_directory():
+        return os.getcwd()
+import os
+def remove_directory(path):
+        os.rmdir(path)
