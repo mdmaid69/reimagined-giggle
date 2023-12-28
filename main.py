bae5692665a6515fadc4fc3a0a88b2785e1c7672
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:54:23 +0000

    tend do finger step sticky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
