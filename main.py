21fab56134277ae997fc63af92fd7a2006d64583
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:30:21 +0000

    nerve study content awake slip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
