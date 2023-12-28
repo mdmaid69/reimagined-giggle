e93b2b2f61c260016de33932f1e7233ee65170de
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:19:56 +0000

    instead control straight now boat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+print(math.pi)
