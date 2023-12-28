3798dcab644a254391b97a831d989c43eb965c04
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:30:56 +0000

    argument favour be lose possible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
