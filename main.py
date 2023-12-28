1742b0bac1f5c41f83e7be7a3dfd22de64892d31
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:43:56 +0000

    payment comment search opponent border

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
