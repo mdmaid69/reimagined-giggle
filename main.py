85eb229394e141551b1cab202b0842bfe4a54b87
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:22:30 +0000

    sure row clear fashion influence

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def remove_duplicates(lst):
+        return list(set(lst))
