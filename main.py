605aaf44b7c1c3dada32bc942a44f7937650a936
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:12:01 +0000

    front terrible bringforward stick sink

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-def square_number(x):
-        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
