1368f8d8e14137ad24ea0ebaa6c614638cddede0
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:45:07 +0000

    plenty conduct beside model mood

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
