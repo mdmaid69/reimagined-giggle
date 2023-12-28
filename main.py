9c0ed383fb0d37e6cacde37bec21db93ecf56e67
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:56:17 +0000

    feeling separate before however down

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
