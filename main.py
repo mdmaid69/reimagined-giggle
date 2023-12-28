9978232e0c77c659ba761a60e6768b15e782798c
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:03:29 +0000

    system middle greeting determined last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def square_number(x):
-        return x**2
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def set_array_item(array, i, item):
+        array[i] = item
