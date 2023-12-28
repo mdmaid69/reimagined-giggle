d8187e1d0aee1e28925522e401c7f551056a981d
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:00:43 +0000

    off cost emphasize hers in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
