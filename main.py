175a4b64b31bf939813575b731f3dee67e666d98
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:31:34 +0000

    male giveaway improve geton tidy

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
