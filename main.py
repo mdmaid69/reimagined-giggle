8eb11618afebae6c685a2f98f422eb553230b3c1
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:16:50 +0000

    about cut for boil famous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
