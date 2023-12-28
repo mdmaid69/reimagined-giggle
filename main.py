9354e27b351cad4147a3ef3da757525371651a66
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:31:51 +0000

    enthusiastic realize sticky main forgive

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
