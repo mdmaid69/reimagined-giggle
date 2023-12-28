f855d632996580843aeab7d0ddbfa720301dc399
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:45:14 +0000

    accommodation border telephone trust shame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
