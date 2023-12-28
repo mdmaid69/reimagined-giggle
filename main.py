ef98534ed2a70c16365d96cd9570de46c2b89de3
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:04:14 +0000

    shut afraid flavour protect story

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
