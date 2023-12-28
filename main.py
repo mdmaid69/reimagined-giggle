162a80871b90fc4f37d52b8b5167e8250f78cb45
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:53:02 +0000

    risk noise than lose tax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
