cd63ffd73be78c5ac879defb6d8f4ebab09c566e
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:55:02 +0000

    usual study only mess valuable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
