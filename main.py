b538f3500fbbcd3377a199fabf1f426db6ef6d5b
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:52:54 +0000

    unless either university common lonely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-name = "Python"
-print("Hello,", name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
