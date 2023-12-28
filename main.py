04530172580b58df1349e1f558f549e60b8befa3
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:54:16 +0000

    thing sight practice politics sweet

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
