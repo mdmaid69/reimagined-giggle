535bac2adce9b4d77897c79f210a78d7ea16e89a
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:01:57 +0000

    lonely everybody worry measure apply

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
