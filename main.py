21b0d7c9bd18749b71a7bf88821eec7425fbd716
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:17:22 +0000

    regional because send style true

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-  def convert_to_hex(n):
-        return hex(n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_set(array):
+        return set(array)
