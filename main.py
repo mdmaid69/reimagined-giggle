d1eb37e46cdf642e9efe88008aa1c4eb7aaff273
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:47:32 +0000

    at sentence warm up open

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
