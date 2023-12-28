cde9dbbbe3d287302c42110f9ae21bcdc7f8e280
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:29:57 +0000

    day male land strike crowded

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import datetime
+def get_today_date():
+        return datetime.date.today()
