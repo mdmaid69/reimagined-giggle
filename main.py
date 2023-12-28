bd64ad2aa39b6790a880ad269bfa013d6e46c806
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:31:10 +0000

    mad about impossible single wing

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_array_to_string(array):
+        return array.tostring()
