d6061776bd9db437f3fe05fac593c28a873d885b
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:36:59 +0000

    eager spoil almost climb pronounce

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
