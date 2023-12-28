9126fe2eaad20c9cbce4e6ade92136cdc783e227
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:37:15 +0000

    stiff accidentally skill rarely strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
