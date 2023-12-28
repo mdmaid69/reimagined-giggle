37bbdb7389408ef6511068b21adbc644bfc66fbb
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:10:37 +0000

    admire uncertain giveout soft beside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  def count_elements(lst):
+        return len(lst)
