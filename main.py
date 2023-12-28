49e69e3e03222fbc127a6f3a8efd39b345689bb6
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:42:03 +0000

    pay taste thumb to style

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
