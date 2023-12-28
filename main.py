f5090a03d9400c959bcc17644ed94c45519e57fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:36:42 +0000

    shopping let go free lost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_power(work, time):
-        return work / time
+def get_array_as_frozenset(array):
+        return frozenset(array)
