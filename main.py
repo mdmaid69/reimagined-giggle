bb1edd2b57ea1cebe398cdf65b03589058141b7a
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:13:45 +0000

    sick lookback(on) mood pump damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_str(array):
+        return str(array)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
