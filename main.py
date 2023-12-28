7ed2e000bc857a261e98d87de9677d781e682a86
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:10:40 +0000

    border over create tune praise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
