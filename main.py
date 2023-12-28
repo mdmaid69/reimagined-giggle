437f2f7c6f6e65535c17e9e05d234b1e0e3466b5
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:52:49 +0000

    afternoon positive matter ready modern

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def count_characters(sentence):
-        return len(sentence)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
