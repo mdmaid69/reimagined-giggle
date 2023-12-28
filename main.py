8dc37a1007710361dc5a7e5a25c1644259dad96d
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:35:56 +0000

    generally takeoff punish ring disturbing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def subtract_numbers(x, y):
+        return x - y
