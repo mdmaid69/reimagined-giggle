7e9cad007ef4fce11028fb8b28ef1a1ed62437c9
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:20:49 +0000

    be Hh beyond original slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
