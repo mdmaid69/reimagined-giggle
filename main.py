603d868cd6817b48d683c534fb5846ccc80a1ccb
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:34:55 +0000

    trouble purpose think machine recover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
