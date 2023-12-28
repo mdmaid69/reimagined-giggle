75a18ed4803869a6868c50150158e45564369c99
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:04:48 +0000

    clean charge cross regular our

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import sklearn.datasets
 print(sklearn.datasets.load_iris())
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
