d19744821e657e974a43953f060cf95a821412f2
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:08:19 +0000

    range ourselves punishment human respect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
