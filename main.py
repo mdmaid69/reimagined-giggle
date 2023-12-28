f7e84afc8e13f838985c79ebe1424d908e069e9e
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:36:37 +0000

    flat splash value yours intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
