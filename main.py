25d58e51128cec6d8a58d30e878bb5a4739a0648
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:01:51 +0000

    excuse relief emergency absolutely steer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def remove_directory(path):
+        os.rmdir(path)
