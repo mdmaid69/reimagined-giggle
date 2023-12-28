e52ee008b55c3e1ac8d1c87ad7619e49e2badebb
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:48:03 +0000

    interest awake sound anyone intelligent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
