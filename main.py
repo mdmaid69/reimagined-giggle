58dadb774411ed1a223b8880823b86d38f419a60
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:55:15 +0000

    schedule century flat mind instead

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
