9e4fb122657efd70e15fce42565d28be99e440a6
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:02:41 +0000

    finish nothing star quick clearly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def subtract_numbers(x, y):
-        return x - y
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
