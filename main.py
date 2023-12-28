ab4e077656be46aa07b010586bf9f8dc26e2d9a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:32:13 +0000

    pretty tense unable mainly dick

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
