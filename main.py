c3e780da2d944ce2e313447f09155b927b62d4b3
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:03:02 +0000

    by name roof smile body

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import math
 def calculate_bessel_function_of_second_kind(n, x):
         return math.yn(n, x)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
