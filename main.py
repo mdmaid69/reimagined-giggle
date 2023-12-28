1e3ee7c8ee06d8a92f8cc13701214171d36702a6
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:33:11 +0000

    goaway road back bet broken

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_as_float(array):
+        return float(array[0])
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
