1c18382662ef33373eceb8cecc7264abac4c304c
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:30:13 +0000

    play loan fine science poor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
