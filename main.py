98978390070da6d73270b49cfb832af48ae7419e
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:34:44 +0000

    will respect so relationship stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
