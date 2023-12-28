6ab24fded969acfabacc0c19fb72a5689d9a380f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:19:17 +0000

    drop yours wrong high embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
