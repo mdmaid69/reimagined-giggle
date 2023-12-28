160b1ac5fc0def8f9321b80cb30a2101462380b5
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:12:43 +0000

    hit nerve thankyou thinkover sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import math
-def calculate_floor(x):
-        return math.floor(x)
-import sys
-def print_python_version():
-        return sys.version
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
