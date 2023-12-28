aff5468815f5ac1224b9008a7ad6a53345a1994f
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:15:33 +0000

    566end keen comein copy body

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
