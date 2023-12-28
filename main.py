5aa2ec67335474fb532d2627be070f7ecfffcea1
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:07:58 +0000

    571enough now course advantage until

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_work(force, distance):
+        return force * distance
