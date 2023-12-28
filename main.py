a0e6c371176e4dd6d9e7504d22efbab8136043ba
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:04:59 +0000

    later watch bill pay violence

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
