45d7344b689df9cc3a4ba3bdb928499da328fd43
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:38:41 +0000

    travel fit thing wrap real

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_density(mass, volume):
+        return mass / volume
