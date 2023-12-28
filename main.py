09e66f2915be5ec7a010f12791717439889f015e
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:42:54 +0000

    stamp plan fail rise chat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_length(array):
+        return len(array)
