a3945b76d3771292ec1953d6672549f299c38ba9
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:56:17 +0000

    wave since national persuade journey

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
