407c6977b8f1c331664e36727c10fbd5b2f1e87e
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:28:22 +0000

    bet holiday sister any interfere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_bytes(array):
+        return bytes(array)
