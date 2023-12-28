f81f6540433b27da0f0d68b70e90b2583c7fdc13
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:58:45 +0000

    read material basically it name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import collections
+def create_priority_queue():
+        return collections.deque()
