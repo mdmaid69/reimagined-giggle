a09aa193ba7ce7a7a3794dedeca353d5b887d734
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:46:21 +0000

    blame no knock frightened what

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_itemsize(array):
+        return array.itemsize
