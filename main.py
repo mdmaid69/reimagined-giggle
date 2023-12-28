72aeac1b1f431d5dc5fcad4a6833ec0aba0a21ec
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:09:29 +0000

    permission public bringforward store calm

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
-def reverse_string(s):
-        return s[::-1]
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
