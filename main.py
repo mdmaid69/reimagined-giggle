667867bea12cf9edb53f877cc63ec25c3f6562ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:16:02 +0000

    next hard taste Gg perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
