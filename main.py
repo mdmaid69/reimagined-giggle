459a4b956bbeeffd45dc42680c35794bdfaf44ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:39:14 +0000

    vote underneath tidy holdout slow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import sys
-def print_python_version():
-        print(sys.version)
+import os
+print(os.getcwd())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
