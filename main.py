8f6f911a0a7b8191aa00d9ea3ccab750fb5d9ecd
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:29:48 +0000

    Oo middle insist pretty than

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
