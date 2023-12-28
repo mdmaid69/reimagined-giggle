d45e5c3f9862a0787ec0a751c2baf8d01762e79a
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:39:37 +0000

    average combine recent fast supply

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
