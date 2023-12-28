dedc64c6a5f164f13557336cd574508c1ed13ad9
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:59:30 +0000

    fall waste part week public

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
