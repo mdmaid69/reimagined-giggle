44f3a9c11f040868cf8eb39d85970d0fb2f5e1fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:10:06 +0000

    estimate several rub word key

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+x = 10
+y = 20
+print("Sum:", x + y)
