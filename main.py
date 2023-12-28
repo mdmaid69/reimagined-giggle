15fe6881c249eb957eb131661ac5da5fc7e47219
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:49:21 +0000

    previous welcome community communicate cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
+for i in range(5):
+        print(i)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
