7996fe4dc052f704cc071c1c74758e4c8cb35766
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:46:36 +0000

    fresh advanced sink cause claim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
