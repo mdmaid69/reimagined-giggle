accc888f9c928e4ff614b2d62cf030e620a616f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:11:01 +0000

    peace thinkof call brush Ee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
