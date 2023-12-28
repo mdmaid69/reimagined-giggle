695283fdb77dee8aabc8c7db97d123782841f95e
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:01:15 +0000

    tax rarely ring reaction connection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
