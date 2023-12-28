a9da15d1f69496e978ca842d102388f1c67a3165
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:39:21 +0000

    opponent basic contrast stick turnout

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def find_min(numbers):
+        return min(numbers)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
