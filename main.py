486a7fc0403603f5f211ddec01690b34e390b58a
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:15:42 +0000

    exaggerate wear whenever his spray

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
