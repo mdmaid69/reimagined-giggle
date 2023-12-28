aa874a7fc71536e793e9ef5d36389d81a3e0eb54
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:30:12 +0000

    constantly block profit breathe secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
