53eaf3d9b6b0adb626c5ca55927968045f62be1a
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:23:50 +0000

    emergency correct rather often together

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
