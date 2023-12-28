e7c5b964eaf38715b54399f93c846854058c0e0c
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:50:20 +0000

    spring callup lose offend live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_max(lst):
+        return max(lst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
