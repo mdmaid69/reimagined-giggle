e893969f8e5c5596f5dc329b258dae2d2f15e75d
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:36:53 +0000

    too old sensible Nn base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
