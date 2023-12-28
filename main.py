c5a4b2a7e7d72545dea06d79c86bf26d48ba89f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:11:29 +0000

    block actual against moon guess

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def convert_to_binary(n):
-        return bin(n)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
