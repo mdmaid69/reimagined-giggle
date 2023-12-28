d2ec14271a04e4b871be14415aedf25bcbef8ff6
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:00:31 +0000

    whole admit comein defeat air

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def iterate_over_array(array):
+        for item in array:
+        print(item)
