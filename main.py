7564dbf6df7f5b82d7fd570a4d65d03c118603ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:04:22 +0000

    term comeover before truth organize

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_as_set(array):
+        return set(array)
