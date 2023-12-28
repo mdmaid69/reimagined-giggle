bf0dc496f7e76762d39ed18ef4947c2dc79fe342
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:40:56 +0000

    question march theirs important bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
