6a6917bc2d64dd82a1099a0dd6141b724f5454c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:24:40 +0000

    brave load sign suit criticize

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
