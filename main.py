d873faad4ff7780ba61ea0819fad1f18d1a0fdd5
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:19:28 +0000

    shout disturbing pleasant choice instant

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
