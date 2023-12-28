b821773e701df29e0ca5df0b890592bb36e4fa36
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:48:12 +0000

    ourselves tear complete programme convinced

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import os
+print(os.getcwd())
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
