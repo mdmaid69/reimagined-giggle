531a04ecc863c7325d4fc9de87fed7f9f28a82c3
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:03:48 +0000

    either pour consist Tt body

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
