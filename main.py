6d81de9aff2bb6c9e1f978f918ca1d93246ecf1e
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:54:11 +0000

    save congratulations comeout luck often

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
