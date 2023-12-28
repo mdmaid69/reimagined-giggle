4f5a37aa13f0b3f7496b7d58fe5d68040e590d66
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:36:05 +0000

    thirsty sign sky believe flow

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_pressure(force, area):
+        return force / area
