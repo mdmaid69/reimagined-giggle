4c6fc832153041fb51cee0c5940a430e852e544f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:34:54 +0000

    Tt male threaten front operation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
