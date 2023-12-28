cc34af05b2b0c7a7988fd9adbc4f9e8f2ed5f0f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:29:13 +0000

    declare guard pupil battery tidy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
