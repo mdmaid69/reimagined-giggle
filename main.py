0fc0cdb38b21e2dce6d999a9e78290ba9af6a403
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:49:06 +0000

    fresh rather decide fight drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
