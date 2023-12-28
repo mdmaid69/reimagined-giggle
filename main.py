0b1e51f15efe607a227721d4a3dce5493387d617
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:42:50 +0000

    fond ago friendly kill Mm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
