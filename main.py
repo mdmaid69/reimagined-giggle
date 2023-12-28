e70a7bc88b0c5c4e433498674ff5bf43712ef3bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:12:25 +0000

    round until advice conclusion design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
