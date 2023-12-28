a6209a1262c12717865dda2f9683e95d184ee24a
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:21:08 +0000

    promise towards sign grateful market

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def find_union(list1, list2):
+        return set(list1) | set(list2)
