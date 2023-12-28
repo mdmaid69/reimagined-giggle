70ffee3126263715f2176b12807f7e24e958336c
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:09:05 +0000

    better second wall holdout ride

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def cube_number(x):
+        return x**3
