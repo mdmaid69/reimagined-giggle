7af1d2f6374b91a2eddf13b9e4b47531c53415da
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:44:56 +0000

    respect point class relationship unkind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
