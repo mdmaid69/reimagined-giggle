52ca157ab9e707670741297ec153cdbe83bd87b5
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:43:49 +0000

    record something persuade surprise lock

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def cube_number(x):
-        return x**3
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
