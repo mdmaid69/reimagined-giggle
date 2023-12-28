eab9893853aac1341218c5eec9c605132386c043
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:28:19 +0000

    pretend knock light mine drawing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
