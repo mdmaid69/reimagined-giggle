9740c24874fc8cd4c66dffbb527d4d9cfc13ed92
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:26:33 +0000

    hospital city so male take

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def check_if_array_contains_item(array, item):
+        return item in array
