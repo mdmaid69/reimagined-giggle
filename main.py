a5ab1fe56df18c49fa30f28b26dc4acddc2eccba
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:12:07 +0000

    vision goaway accident experiment show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def clear_array(array):
+        array *= 0
+for i in range(5):
+        print(i)
