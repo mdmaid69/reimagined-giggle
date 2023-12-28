724ff34ddb188ef34d9f7c82505d46719b40c25d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:03:46 +0000

    goaway top accept lock idea

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def find_max(lst):
-        return max(lst)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
