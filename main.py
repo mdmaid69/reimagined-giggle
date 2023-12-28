9d39fde57ad8c66df85a541fb5e8d7b62920d170
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:22:05 +0000

    closed authority slightly urgent bottom

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
