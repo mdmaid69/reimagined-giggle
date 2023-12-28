08a433e1a8291c9212a717bffc1c1a0ed887accb
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:50:25 +0000

    aim about tear know attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import collections
+def create_user_string():
+        return collections.UserString()
