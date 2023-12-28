4011eadc332d262dbee60aaa62deafec4a4edb14
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:08:54 +0000

    way charge excuse definite can

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
