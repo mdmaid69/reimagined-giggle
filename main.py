33749912a3588806086aee3c20a7ed989df19f46
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:04:40 +0000

    Yy secret concern fit money

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
