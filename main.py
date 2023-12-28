4923e0b38b4ab53ed9f458fe8fe23f8f5bcd07a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:41:29 +0000

    kick battery I nothing Hh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
