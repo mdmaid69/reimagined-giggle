fadc9b504f6bb4b0372004179ad6d9cb2322a30f
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:24:59 +0000

    pour reliable against hit affect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_pressure(force, area):
-        return force / area
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def sort_list(lst):
+        return sorted(lst)
