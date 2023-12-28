3bfec51e837352eb51b1f04350b547755f4ac76d
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:40:46 +0000

    pinch sticky surprised clearly become

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
