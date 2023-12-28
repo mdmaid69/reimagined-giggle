26724799235633cea8e748fe96e1be51ab65b7bb
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:19:35 +0000

    callat will absolutely deep interesting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import sys
-def exit_program():
-        sys.exit()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
