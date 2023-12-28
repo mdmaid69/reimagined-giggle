d97b09bee94e1f9a3ca31633990f6d897964f028
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:17:12 +0000

    trust hard uniform classroom drink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
