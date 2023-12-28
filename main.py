e182e3688b568173d32daa74d69545a3278d59a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:23:08 +0000

    try believe line its together

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
