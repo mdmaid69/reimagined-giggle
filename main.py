9a0bbdddbb231e6ce2b623169ff3bfd3f387356c
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:11:30 +0000

    past drive business region stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
