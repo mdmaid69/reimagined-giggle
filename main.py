d9474d340d7dde996757a8679e2f6ded6c260d85
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:30:48 +0000

    well transparent simple change skirt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+print(time.time())
