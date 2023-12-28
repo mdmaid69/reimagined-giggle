f007c60aa0260bf8a4e7d7e62c52adc25a9c687b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:17:05 +0000

    form formal kind geton plate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
