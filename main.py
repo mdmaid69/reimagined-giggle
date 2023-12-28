1034a1d2a7d7025ad84886e9fa8959b1159b6a7d
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:46:24 +0000

    under fashion minor crazy little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
+  def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
