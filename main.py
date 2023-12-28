6743f0127150bd1cfe5ad81bb393f60c8e354acf
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:51:55 +0000

    plan affect pool easy why

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def add_numbers(a, b):
-        return a + b
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
