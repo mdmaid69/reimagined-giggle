06bb23c7ca813db6824540253adcea7ac044d568
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:54:09 +0000

    slightly everything success discover machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
