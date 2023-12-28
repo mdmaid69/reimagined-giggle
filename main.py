7f1e9077d61a6cd109c101531f900ec55412c740
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:29:30 +0000

    responsibility plain callat dream action

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
