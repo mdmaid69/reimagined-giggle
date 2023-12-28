a08710446b5774c1165a7b869b19e7f7abf78503
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:47:14 +0000

    could urgent neither shut string

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def find_max(numbers):
-        return max(numbers)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
