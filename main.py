55c11385baa37e9ffcc65237917d4864c2f93d2f
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:46:49 +0000

    important suggest lookafter say rather

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
