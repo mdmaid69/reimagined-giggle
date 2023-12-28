d81670677cc0f6b73cc5f378a4e3ffadce6f8886
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:05:50 +0000

    really harm statement plan left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
