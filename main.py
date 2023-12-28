10180d5856df114e9b4345eaf52cf532106d2830
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:20:02 +0000

    measure tooth balance over once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
