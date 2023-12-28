f751f0529cd88f7d5dc95dd60914cc73097ab8fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:00:15 +0000

    sort open disappointed careful trust

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
