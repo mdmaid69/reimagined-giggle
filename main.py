b807257991f8572ae403219360094961b2c4a6b3
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:44:38 +0000

    heart suppose payment himself tell

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import sys
+print(sys.version)
 import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
