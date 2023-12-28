9753ec133e6f05d26887de5b7843edabbcd6b403
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:48:20 +0000

    come play thirsty save thank

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def find_max(lst):
+        return max(lst)
