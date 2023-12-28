6b22ee0213623987ad68f2f3e10b633912f43ed1
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:05:26 +0000

    everybody join dress learn flight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def remove_from_array(array, item):
+        array.remove(item)
