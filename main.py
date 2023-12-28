f070cd8d2a72ecc0babd8279aa0e6ee5baf834ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:59:12 +0000

    compare discuss tight vision beside

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
