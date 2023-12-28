00fbb3b819f3491da828fa89d532998ca1b06d12
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:37:27 +0000

    callat keep surprising choice lock

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import time
-print(time.time())
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
