0f0ae4732ae1c5e9109684287693f764d593ad02
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:30:04 +0000

    clue comeup careless importance refuse

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-for i in range(5):
-        print(i)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
