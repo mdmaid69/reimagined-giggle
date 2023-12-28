a91fb70627b14319cabe2e3f1fb6129f5dd03fb2
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:22:30 +0000

    close judge few squash apart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_arc_tangent(x):
+        return math.atan(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
