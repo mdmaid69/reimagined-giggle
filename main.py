83b7265f90da42d49ecb4114e3ff260877b8fb17
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:13:34 +0000

    through bit consider fire success

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
