7355f8c1c198eabab19df77be177c59112a6f5fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:01:17 +0000

    past warm related soak trial

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import time
+print(time.time())
