b22c58cfb52568cb6a24e7da3bc6f1b9d8f46b5a
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:26:01 +0000

    breakinto impatient steep stage takeaway

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def convert_to_binary(n):
-        return bin(n)
-def cube_number(x):
-        return x**3
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
