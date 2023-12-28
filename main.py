2ee10b591067f744fe20feb0a7b5b8aa238d0621
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:12:35 +0000

    sick during conversation stiff concerning

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.time()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
