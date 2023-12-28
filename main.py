6dbc7d2517631cf866fd4860cc589fff57dc16b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:10:21 +0000

    sticky cool arm rest church

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(x, y):
+        return x + y
