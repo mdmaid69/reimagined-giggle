49bb62891df2fabe611f8a4e43c35a7581047e0b
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:45:49 +0000

    trip admire afford sense visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
