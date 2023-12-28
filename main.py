585dd66f0c26abecf1f17a1b299769046a5fc919
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:15:30 +0000

    spite practice advise foreign throw

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+from collections import Counter
+print(Counter("hello world"))
+  def count_elements(lst):
+        return len(lst)
