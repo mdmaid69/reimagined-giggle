157bbb2ecc259eaa26d6fd7a2b6013f0d1b6b08f
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:23:49 +0000

    perfect smoke reason left recommend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def is_odd(n):
+        return n % 2 != 0
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
