1cd07d98b9f60eec2917ad75b942158293e42eed
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:59:43 +0000

    below serious wander behave adjective

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
