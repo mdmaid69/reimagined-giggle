fc9aeb6f6927aa4f4101c9e1d9b1832c833af4ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:21:39 +0000

    father patient right telephone mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def get_array_item(array, i):
+        return array[i]
