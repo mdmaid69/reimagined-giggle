5c2f5d143a1709e3c6696c291d81e1b8733c39e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:26:23 +0000

    start load give mainly regard

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
