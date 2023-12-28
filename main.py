35f967d07cd4bda306e4a2aa2118bc37a21aeb32
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:09:42 +0000

    chase limit build sore fire

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_index(array, item):
+        return array.index(item)
