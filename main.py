874f6f9e79f1150f3586bec181e9a636bb9a7c93
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:41:58 +0000

    even ordinary workon computer position

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+print(sys.version)
