32467d497caa0b862b0929429871785a69df88fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:26:06 +0000

    ought interested goodbye comedown store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
