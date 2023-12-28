f3ac2fccdc4eab5f02ed24063a3f4dfd88b3ad06
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:25:42 +0000

    get hope honest comeup ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_itemsize(array):
+        return array.itemsize
