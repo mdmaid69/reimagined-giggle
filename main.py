315083fe044449966b7080e3f30db25e6a2f2d33
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:51:25 +0000

    watch position under use goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
