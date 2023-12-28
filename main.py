0aef1becdf663470804543c85a2964e7a4e956dd
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:08:01 +0000

    let thinkthrough announce bill across

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
