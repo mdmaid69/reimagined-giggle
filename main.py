582b9afd513f67785660131990c8acdf68551130
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:05:55 +0000

    rubbish sky protection among figure

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_density(mass, volume):
+        return mass / volume
