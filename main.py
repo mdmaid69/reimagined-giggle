957c7fa938e9c71f78efe4ce090675cec764bff9
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:32:53 +0000

    develop whether excuse once remove

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(10): print(i)
