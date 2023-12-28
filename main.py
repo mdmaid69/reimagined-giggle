c1041e7dae373b826dc653b73310e0da5be02621
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:08:21 +0000

    punishment later shop disturbing bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def is_palindrome(s):
+        return s == s[::-1]
