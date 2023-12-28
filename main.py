a7e929281bcf375f8cf64433d06eebf392351871
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:13:55 +0000

    development where confusing pretty proud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
