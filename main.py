e749673afcb9f04a38c7e65014ae7501f4f7fc5d
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:36:39 +0000

    uncertain chance never spare report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
-def calculate_sine(x):
-        return math.sin(x)
+print(math.pi)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
