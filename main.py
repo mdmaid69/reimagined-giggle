7ed8d011fc34f5a2aa3ba8976d2abe342e21dc3f
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:28:07 +0000

    stupid position disturb loss protect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def remove_duplicates(lst):
+        return list(set(lst))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
