1be12df5552f1eb284968d4a181651f643c5fabb
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:57:39 +0000

    bridge reserve pollution scared behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
