6ffc8fa83589188a8be1c2a74c4a246a70793398
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:31:07 +0000

    late male radio attention serve

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
