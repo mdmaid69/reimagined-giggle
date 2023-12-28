ba9e260c1ea2b55dd550978e29659051edac6542
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:44:18 +0000

    direct congratulate active alone crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_pressure(force, area):
+        return force / area
