dd3843e5165a041bdca4aead6202ced799d955d3
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:21:08 +0000

    raw innocent takeover violence grade

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def reverse_string(s):
-        return s[::-1]
+import sys
+def print_python_version():
+        print(sys.version)
+  def reverse_list(lst):
+        return lst[::-1]
