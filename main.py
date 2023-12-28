939e44f601e6ed649d9c194afa2a3e8e383679c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:42:47 +0000

    general comment defeat company use

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def split_path(path):
+        return os.path.split(path)
