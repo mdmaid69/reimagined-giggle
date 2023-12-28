c9a7064e8199535354d77b59ba02bd6f5fd84747
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:04:42 +0000

    complicated shoot view connect window

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
