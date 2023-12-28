46672bc676e998b97d2d37c7e68cdf1c90fcce23
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:14:09 +0000

    suppose debt about neck determined

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
