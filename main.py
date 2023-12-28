35f231afa37240b17e7dc071a542da8ef20f762a
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:51:47 +0000

    score exam allow for stretch

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
