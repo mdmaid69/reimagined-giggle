1dfb21fc36bd8f074ee0b8a77da56adb886ea378
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:49:22 +0000

    Oo beside pure drawing trousers

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+print([x**2 for x in range(10)])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
