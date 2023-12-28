d07aa0bd3d08a330c8724e6e8a6c2a9b78563462
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:34:30 +0000

    fall store interfere earn whether

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
