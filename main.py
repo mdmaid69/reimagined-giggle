9293fed017ee55308b66085aa485157f60a5e73f
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:19:32 +0000

    various trick someone forgive remember

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def square_number(x):
+        return x**2
