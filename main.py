a5415c04eaba8ffefc2354787d826438e894a9e1
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:12:42 +0000

    unable replace excuse contact discuss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
