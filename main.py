bf1186686290fab77b79bcd87e374e978836dfd4
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:42:21 +0000

    build Tt sign star contact

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
