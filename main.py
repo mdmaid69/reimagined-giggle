8d0bcf8b455d23746da1205b5429347f61ca004f
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:44:42 +0000

    central post slip plastic best

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
