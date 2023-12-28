7240f39ef85132447e82db86d3f9591c392ce78d
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:51:27 +0000

    tread several satisfactory bridge talk

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
