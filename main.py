cc4ef766c60a68b4b1ff7b639815db6ab611f287
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:23:29 +0000

    water similar fine notice arrival

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
