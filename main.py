b6bd4683cd69ab28f7cd10fdd7b04827e4de0c40
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:41:31 +0000

    steep 569enjoy tight patient telephone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
