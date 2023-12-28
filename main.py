2421ad6b0e8aaee1d9b509d80595e4c5f793be97
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:21:07 +0000

    community short worst loose debt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
