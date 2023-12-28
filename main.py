3cfb60ae68823ce28f1bc487ffe7250dc77cbedc
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:31:57 +0000

    cover book ride mark move

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
