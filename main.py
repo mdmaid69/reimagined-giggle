8f0d968b3bd6ac3adf035ca4a661910540d28853
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:56:08 +0000

    thank without favour exhibition whistle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+print("Hello, world!")
