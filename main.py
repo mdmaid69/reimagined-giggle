72db784fedc1fae1e725b4422413c388f0721937
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:06:34 +0000

    breakup recover shout Tt fear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
