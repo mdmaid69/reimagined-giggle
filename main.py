a47b70f485b8ec6b2aceae01fc9e4d07418e86f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:17:25 +0000

    spite interrupt patient whom criticism

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def append_to_array(array, item):
+        array.append(item)
+import array
+def append_to_array(array, item):
+        array.append(item)
