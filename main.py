290942c2bb22ae4e0ef6807040291c370b067dae
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:30:25 +0000

    problem conclusion strength bother reason

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def reverse_list(lst):
+        return lst[::-1]
