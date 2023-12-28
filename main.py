9ba28bd2b09c21ed4a02b2ae5a2904d050735d95
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:15:36 +0000

    there interview place control disappear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
