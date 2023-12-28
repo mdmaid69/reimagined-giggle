0d0a223d8fad01774ce83a47fd8b7c1d1ec33ef8
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:00:23 +0000

    soil load teach cool punishment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
