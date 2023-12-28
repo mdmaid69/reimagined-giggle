61b27825b5badc2761e6e6540369cb032da99031
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:02:14 +0000

    straight black where never deal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def get_array_as_set(array):
+        return set(array)
+import collections
+def create_priority_queue():
+        return collections.deque()
