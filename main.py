0259a3dfec14759dd49655b15365035d31da5278
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:19:31 +0000

    after sticky point your lose

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_octal(n):
-        return oct(n)
+import re
+print(re.match("h.*o", "hello world"))
+def find_min(numbers):
+        return min(numbers)
