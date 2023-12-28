a6f01d1d3d9e4d80dedf63b1380c67068ad371ef
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:19:44 +0000

    hollow cook do content test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
