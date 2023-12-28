fc9de2f54b96ad2be63992246eb5b61738f199ff
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:36:45 +0000

    trip list moral shrink interview

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-print(sum(range(10)))
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
