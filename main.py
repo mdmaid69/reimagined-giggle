08d2f1f365888deb3f98514344c14fd7153c89a6
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:12:36 +0000

    mean fine score around comefrom

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def reverse_list(lst):
+        return lst[::-1]
