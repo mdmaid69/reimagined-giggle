cf5dee7463a0bc6c03a0dc59c000264e572ae5a2
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:27:31 +0000

    danger sound clean middle sea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
