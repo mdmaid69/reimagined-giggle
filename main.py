6d1680ea06689e9eb372350eac14160a137dcf76
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:48:40 +0000

    turninto gentle yesterday congratulate relief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
