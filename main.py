818a3ef12d2dcd36d540968fff5cff60cc65ecd9
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:52:02 +0000

    then lucky sign light left

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
