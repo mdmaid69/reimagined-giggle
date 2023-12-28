f89c59f7bb503cee4f369ee5ef69da58393a9be7
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:37:17 +0000

    thing few exam join steep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
