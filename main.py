261e99e323e4775caf9da80ec100e7e2a1306896
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:43:00 +0000

    choice print shame most turnoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
