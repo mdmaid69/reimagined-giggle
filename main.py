41c39afef2885ccda35213e58028fa6804ee0398
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:04:44 +0000

    pot sensible law advanced recommend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
