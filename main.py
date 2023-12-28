cbc7cf70c34cbbab1920cdb2ed3de6a1764396b4
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:48:36 +0000

    pronounce knowledge letter house likely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
