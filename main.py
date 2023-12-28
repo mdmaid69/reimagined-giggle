50513ad7f703f5399cfe7bd6d148b7c6c54f720d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:36:54 +0000

    skirt broad mother matter finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
