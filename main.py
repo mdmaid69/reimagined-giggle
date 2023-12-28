14a7cd5c01225df330418bf4066927fb95542492
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:20:24 +0000

    wish confused clever increase offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
