87a1db456bc17a206d37f20e1ddb184a9de67bd8
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:33:06 +0000

    word genuine replace turnback pretty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
