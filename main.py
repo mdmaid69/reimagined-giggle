3a44d74015a712417ee34d9ff254ea6467fcaffc
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:15:30 +0000

    cry switch strict borrow getup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
