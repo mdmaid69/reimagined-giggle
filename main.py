8d45278292c2afc32b70c8873e34fc4881cbddf1
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:36:18 +0000

    proper notice too nor theirs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
