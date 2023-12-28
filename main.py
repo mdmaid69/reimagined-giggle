3870c8810a271836c97630e103161c864289bb0b
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:04:24 +0000

    murder deep oil bread trip

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def sort_list(lst):
-        return sorted(lst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
