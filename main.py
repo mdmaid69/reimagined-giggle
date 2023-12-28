50b12c6521cfd6a3f64c32123b9737011a837327
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:02:44 +0000

    join Itself also free thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
