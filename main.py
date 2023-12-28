83e49d4aa09271e43b5cc20b4b8303b89c53476e
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:48:32 +0000

    trust form gooff touch takeaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
