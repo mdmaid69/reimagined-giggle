336d46fc42bb89f50b377012e828c65a7321c2f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:57:14 +0000

    comeup clear drink and target

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def change_working_directory(path):
+        os.chdir(path)
