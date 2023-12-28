018aa8497c5f505239840d1755e5aea8d653cd1f
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:24:06 +0000

    whistle married value more holiday

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-print(os.getcwd())
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
