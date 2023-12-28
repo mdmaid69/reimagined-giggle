5778f75bb6c5d0438d59c5454dd0f933d7706ec2
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:39:19 +0000

    wonder disturbing 572enter mind wish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
