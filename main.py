1d747bd192556d5115b4812e50df71e5230e9ffb
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:05:06 +0000

    charge possibility capital crowded culture

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def convert_to_octal(n):
+        return oct(n)
