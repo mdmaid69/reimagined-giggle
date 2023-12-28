79c88dae6cf24f3aa9b41b77cd3eddf195952e6a
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:53:05 +0000

    breakout appeal motion recover wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
