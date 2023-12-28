4afe718a051b0bf4d74f09e403846433a54e0328
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:42:54 +0000

    pot children touch reply unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_area_circle(r):
+        return 3.14 * r**2
