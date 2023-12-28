da1dde964501412a6a46dab57aa4222947e5eb1c
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:46:06 +0000

    pretend cope holdup equipment disturbing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
