1f82781613df478c61f09bbc44633245bff9caca
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:43:05 +0000

    mix before truth absolutely pocket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_inode(file_name):
+  def get_file_ino(file_name):
         return os.stat(file_name).st_ino
