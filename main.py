19f87501ed9663ac54ca9faf8f1d73db40b6568c
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:45:09 +0000

    other pay style bridge view

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
