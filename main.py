214aab0601b3341ac4f958e2ce021a2dd9c706f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:37:45 +0000

    glad effort there welcome hungry

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def count_characters(sentence):
-        return len(sentence)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import collections
+def create_stack():
+        return collections.deque()
