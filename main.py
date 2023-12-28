cfb8b56cc412ac297efbdb951db60c8142ce110f
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:48:21 +0000

    ready remark yet past narrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
