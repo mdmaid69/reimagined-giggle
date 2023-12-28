14c59023a086ead03207c909e1aaa8f9caaeb994
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:31:32 +0000

    sign plenty convincing another pressure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def square_number(x):
+        return x**2
