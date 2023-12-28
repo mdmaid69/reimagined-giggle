8467ce23fafff3fc7b89f5b790379f722c84d805
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:05:54 +0000

    might squash past hope possibility

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
