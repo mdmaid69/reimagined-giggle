082ad9c5fde244332330e43850fb26a07112e068
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:57:48 +0000

    achievement word breathe business expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
