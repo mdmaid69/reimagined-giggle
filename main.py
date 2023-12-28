22b0db4d5d01d41a8b6e6892088efd4cbbbe9771
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:04:24 +0000

    swallow wander cover tap thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def get_array_length(array):
+        return len(array)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
