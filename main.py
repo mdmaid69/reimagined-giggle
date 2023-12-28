40680e1249741b1655a047699da5e06c560bad4b
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:16:55 +0000

    dick not refuse out swallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
