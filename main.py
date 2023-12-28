882d930b04b40e548eb2ea0b0e7716139c4c9bd1
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:36:11 +0000

    thought defend appeal bother straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
