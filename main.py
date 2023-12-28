ca89c8de93b59a7fdafcaa99118b2ef0ac7bdaf2
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:54:35 +0000

    previous century intend goon into

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
