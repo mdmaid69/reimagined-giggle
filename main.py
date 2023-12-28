da303f7c69fa7a88d5afb972e959c94e98a0b577
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:00:02 +0000

    provide stare welcome operate thinkthrough

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
