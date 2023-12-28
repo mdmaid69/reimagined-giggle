d6c2dd64aac1e86d64e38faffa191082e81a31b3
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:34:31 +0000

    substance noisy wave blame pile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
