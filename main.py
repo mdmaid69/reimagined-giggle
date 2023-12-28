27f195e951666ca711e2610d2b234e5720422af5
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:51:26 +0000

    hate feeling before post form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
