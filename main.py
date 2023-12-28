0d567d1c9969bc5736d9ba39ecf745897d561352
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:27:41 +0000

    over if will blind phone

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import datetime
-print(datetime.datetime.now())
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
