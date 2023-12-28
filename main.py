df08e2e5b184d57e78f4167488d82d45ba2f4891
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:00:36 +0000

    copy lose pull gas subject

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
