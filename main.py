375faea9751c31fa749d747900ca5a7ed463087b
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:31:14 +0000

    about home thought fear talkback

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
