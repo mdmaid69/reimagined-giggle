7f5315353eda7e5afb6858052483ce5c7ed2e515
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:16:18 +0000

    begin them storm board glad

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def reverse_list(lst):
+        return lst[::-1]
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
