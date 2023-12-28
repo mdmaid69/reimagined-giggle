5397382ed9e4ab80873bc4d594470b19d17deb3c
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:22:19 +0000

    wash will return accept tight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def find_min(numbers):
+        return min(numbers)
