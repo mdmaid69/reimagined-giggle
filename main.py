c98ee037b391d3488d00bf5db41f9506bf4df452
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:34:05 +0000

    listen switch bite disappointed attempt

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
