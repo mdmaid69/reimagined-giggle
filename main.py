262bd9950fcaa22630bad72a30b5e9af72c7f2ef
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:45:40 +0000

    threaten seem steady thought after

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print([x**2 for x in range(10)])
-  def count_elements(lst):
-        return len(lst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
