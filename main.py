2d32e4774995defb8db9ee9bc941e6136b97359d
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:29:02 +0000

    satisfied petrol decide if operate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
