5511f9fb55b77f8896528c3157fa7095388183fe
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:32:35 +0000

    rate age carry second environment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_error_function(x):
+        return math.erf(x)
