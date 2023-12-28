fffdbb8ea08577d63240926346c0ccc7eb3f4747
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:30:16 +0000

    actually confuse aware rob switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
