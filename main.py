ac4099b310a190b13cf06df343a32585e09883f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:06:37 +0000

    battery honest free nothing thin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
