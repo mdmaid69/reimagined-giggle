5faadd9779b641a96bb2e13255601bb5fc7891f5
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:33:09 +0000

    sign loose state message clear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def get_array_item(array, i):
-        return array[i]
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
