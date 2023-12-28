b6d930518fb3602e2029bfebaa9d4e38891a5f31
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:59:47 +0000

    fit mistake tall wheel pretty

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def reverse_array(array):
+        array.reverse()
+import collections
+def create_counter():
+        return collections.Counter()
