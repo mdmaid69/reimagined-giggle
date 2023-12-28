e1496c12fa9161261360e45e605ead8c5957715b
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:34:43 +0000

    write goodbye so recently tear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
