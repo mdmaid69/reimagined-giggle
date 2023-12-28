0916ddb365ccc8e417175803f5df32fe9a2d3291
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:04:45 +0000

    basis police duty collect touch

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
