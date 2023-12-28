199e1d5226ad8a24deeec08daa0cdf3c448e238e
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:46:58 +0000

    fire boring protect goon popular

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
