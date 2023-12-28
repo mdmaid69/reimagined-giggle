f02ccaad56ba11e787a86cb5d124f12995069573
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:20:02 +0000

    explanation maybe order sorry model

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def reverse_list(lst):
+        return lst[::-1]
