f6ff0273f342497edec68f4d4a0747f355f582ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:29:07 +0000

    hurt calloff lucky borrow you

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+print("Hello, world!")
