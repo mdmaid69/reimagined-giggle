1ba9f6400461f93c3e86cb03ba1281dfc0a07498
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:48:36 +0000

    holiday genuine sudden pause repeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
