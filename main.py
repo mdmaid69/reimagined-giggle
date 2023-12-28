3724fe64959fc4f8d1e464118834bbd323d2a4f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:37:32 +0000

    show affect address no together

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
