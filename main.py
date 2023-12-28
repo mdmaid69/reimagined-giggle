268e7284ae50545ed6b7f9e89dfa12978acccc05
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:52:34 +0000

    like disagree squash famous find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
