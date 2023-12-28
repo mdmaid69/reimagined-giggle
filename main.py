e52736667d1dc78fcfada724c4f352e5036734d0
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:49:04 +0000

    development accept achievement base possibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
