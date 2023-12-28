5762b92026b9cdf208029fb2710705880cff7ba6
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:03:42 +0000

    drop situation pollution be instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
