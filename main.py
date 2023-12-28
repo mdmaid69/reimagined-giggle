5498fe97aaea011cb01897b38ee89f18070e9dfc
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:25:35 +0000

    Uu stop breakout name convincing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
