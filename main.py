5f3775a20e2e2da73dab1323589c1b4ff81527bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:18:21 +0000

    loud tie though bunch development

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import sys
+def exit_program():
+        sys.exit()
