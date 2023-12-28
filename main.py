c6fa0a5b852eafaf8baef7215f02338954997aee
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:29:39 +0000

    centimetre police command scrap disappear

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def is_even(n):
-        return n % 2 == 0
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
