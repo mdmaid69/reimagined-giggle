98dc19aab3e336fc01c4e78496d76614fd8bced6
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:52:54 +0000

    wind direct health memory reduction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
