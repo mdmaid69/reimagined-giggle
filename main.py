353023ab8d76fe20acd6b6556b2dde58a2e3e504
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:39:58 +0000

    basis dream thing leave niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
