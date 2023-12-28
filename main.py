2acb6189f43a7a388fb1770b03ddc7bf9a95bda4
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:05:57 +0000

    mood reserve last advice bound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def clear_array(array):
-        array *= 0
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_acceleration(speed, time):
+        return speed / time
