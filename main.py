d53f7aaabeee666e34039269bac11ff85a5e0dd5
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:29:47 +0000

    wash bush scared neck breakout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
