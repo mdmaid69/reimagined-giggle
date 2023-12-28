9eb9b362a2be9cea7657266fb7af7a96d9f4c056
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:43:27 +0000

    shock birth possession beginning control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
