70fcb37563beab625634e3c481542aea013b1da2
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:50:36 +0000

    personal university rest rescue report

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
