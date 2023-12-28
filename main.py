ca4cddcfae3447ada7f2a3cceda1e64b4889a78f
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:56:45 +0000

    goodbye bright bang loose react

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_item(array, i):
+        return array[i]
+i = 0
+while i < 5:
+        print(i)
+        i += 1
