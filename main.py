7d117f76e7d21bc1802287428f5dc6d7b372d9f1
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:27:51 +0000

    say kill skill dream consist

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import time
+print(time.time())
