135369bad201d7edbf3b829493e44d61cbec045c
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:25:44 +0000

    trade level none calm niggaacademy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_density(mass, volume):
+        return mass / volume
+def reverse_string(s):
+        return s[::-1]
