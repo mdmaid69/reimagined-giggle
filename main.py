fa3210919fe237f2ead9b234b4f8b676abd0f0bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:13:53 +0000

    long your bridge life activity

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
