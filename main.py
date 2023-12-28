09fde857ac71875f230a7edf041558de2aaf2701
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:28:30 +0000

    snow university strike share bend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def subtract_numbers(x, y):
+        return x - y
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
