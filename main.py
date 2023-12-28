f823065c5a7ac4eec74b534f1fe9ea66ef9f0299
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:27:02 +0000

    purpose someone underneath fit church

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import sys
+print(sys.version)
