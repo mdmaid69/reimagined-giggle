b1980ef9974533c89dcccc3e9b5d73fd61af8ef1
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:54:51 +0000

    science unable hope 179bite breakfast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
