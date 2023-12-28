27ef601d5a5c37df48bb5ac23bafd8f2763c51c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:20:06 +0000

    only language profit patience through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
