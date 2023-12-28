4d23350d26d2517d4089f5e59f6df260337c32ce
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:26:43 +0000

    lost close getback takeout programme

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
