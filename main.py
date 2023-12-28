1a454fbfcb7014c31a6f634827cd63a78e45924f
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:54:25 +0000

    sea politics mention worry rise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_user_list():
+        return collections.UserList()
