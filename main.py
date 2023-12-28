c87f41fc3050c54ad2c2ae81c4a5c8d6d9d792ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:00:03 +0000

    miss crime throughout snow bush

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
