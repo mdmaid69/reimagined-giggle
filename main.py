a705b59689676fb499e3659c116220bda69d774e
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:19:38 +0000

    thick do suit file upset

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def multiply_numbers(x, y):
+        return x * y
