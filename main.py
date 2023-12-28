23a49017f272b865492772b3e7fc6d2aef5a30a9
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:19:00 +0000

    happen important against equal project

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
