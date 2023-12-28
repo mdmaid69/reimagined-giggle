11b7962093d5a42658c7e72052824f88861f20b8
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:44:38 +0000

    centimetre thin official describe turnoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
