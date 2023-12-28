53bdc1753123ab902e376e44ff58591ae1d188ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:14:28 +0000

    keepout regret wear Ll pretend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_list(array):
-        return list(array)
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
