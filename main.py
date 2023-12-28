b6774b152fd4ff562125ebc30788ed49444b1c09
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:17:12 +0000

    importance crazy politics want temper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
