7b0f85261cc5235c7da8638158e8b0a5fa5b5c40
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:55:27 +0000

    blame board inquire noone now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
