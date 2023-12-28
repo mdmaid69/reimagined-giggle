d86a2b7b8a34e18dd7e4eda690eaf209e1f74638
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:00:45 +0000

    couple 566end advantage criticism cinema

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
