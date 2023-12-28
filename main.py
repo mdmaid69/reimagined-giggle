e51ff37e115fd6a427a9adcf08aa6b8725cf50fb
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:25:49 +0000

    only ahead attempt tape indeed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
