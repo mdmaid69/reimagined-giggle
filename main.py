41c23136773d374ce8b7f79438ce7c2ccaac0dae
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:59:03 +0000

    grade tidy appeal joke moon

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
