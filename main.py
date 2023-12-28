f04aae4dffe891e5bc873720a47a5bca4409284e
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:26:43 +0000

    precise bringforward smart war official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
