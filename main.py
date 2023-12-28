09d4c4163ca39a4a9fecf4f38ca4938c7df33052
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:35:26 +0000

    yourself injury education tape runout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_perpetuity(payment, rate):
+        return payment / rate
