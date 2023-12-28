fc4bb4e665a006800761a854bff0f7ca808ef11e
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:59:28 +0000

    tidy under whom marry body

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import random
+print(random.randint(0, 100))
