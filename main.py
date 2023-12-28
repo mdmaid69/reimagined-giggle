ebc695a6e4d38fb48c593dfb9f31d728df717b08
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:06:07 +0000

    ought true shout leave lend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def add_numbers(a, b):
+        return a + b
