4d89d9b96d64c0cbc0fa3d6a74c5e9780096c681
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:10:14 +0000

    every state remember talk badly

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-print(sum(range(10)))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import platform
+def get_python_version():
+        return platform.python_version()
