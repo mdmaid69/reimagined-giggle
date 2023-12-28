d2bc1fc622400a85688b18e822fcd165cccaea0d
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:23:16 +0000

    opposite approval legal high shut

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
