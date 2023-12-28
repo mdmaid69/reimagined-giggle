774ac33bed91254b542af2169a53e799958bfef6
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:58:20 +0000

    season score argue wipe fit

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
