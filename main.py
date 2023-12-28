5de3463b57041c944993bd0d1ed7dca99debaa26
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:42:09 +0000

    fit loan lost circumstance experience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
