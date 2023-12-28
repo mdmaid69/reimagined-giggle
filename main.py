38b999dde2d8c06491561cd9e71878aafdebfb39
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:30:44 +0000

    note clothes relation trust payment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
