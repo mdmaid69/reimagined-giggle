d74d16623dd1890043a0da65184ef11699295e0c
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:30:38 +0000

    hole both above measure cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
