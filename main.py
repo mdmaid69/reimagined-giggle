7956b26ea6513e4cdadec8ffc633dc8b33588da9
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:08:59 +0000

    possession here intelligent set path

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
