505b9c4eab362d613e238e301b200ad6e319b074
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:14:13 +0000

    legal cook century rain number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
