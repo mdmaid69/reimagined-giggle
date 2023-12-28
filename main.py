d8547d95754c3b502830f3223a3d50f1fa33cdd5
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:41:00 +0000

    convincing reason genuine need breakin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
