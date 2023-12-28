776c27143315ad6e60b49a0667d1d981c8af8ceb
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:30:05 +0000

    strike reach drop thank skill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_unique_words(sentence):
+        return set(sentence.split())
