b4beba9eedcc52fcfc9b4f9b5d6ad1f837eb91c2
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:59:29 +0000

    appear closely scale settle take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def multiply_numbers(x, y):
+        return x * y
