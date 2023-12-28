f8602e7a8a21efacddaab873fe0772664113b4c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:27:40 +0000

    file move sad week declaration

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
