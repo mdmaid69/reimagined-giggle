fadb1f21760d79e254c30804457b3797730f4c85
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:27:20 +0000

    defend pride future aim spin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def add_numbers(x, y):
+        return x + y
