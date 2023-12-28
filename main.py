21aa9fc0e379152d667adf46613096105fb7ee79
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:09:48 +0000

    surface pen work embarrassing target

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-name = "Python"
-print("Hello,", name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
