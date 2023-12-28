c6d5dc70d6914cf81a2d9187b1a8f9b999d59596
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:54:25 +0000

    explanation will most tight soak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
