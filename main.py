4fe5b30128f4f68a28a6604358a34e4b63c61f4c
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:56:28 +0000

    however knowledge steam remember let

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
