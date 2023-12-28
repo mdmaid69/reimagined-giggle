3200342130f25d66d65e44fb349988ee784534ef
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:10:14 +0000

    advanced risk government willing punishment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
