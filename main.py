2423190c05ec4c10edf73de8abb80c7d93ad916d
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:47:43 +0000

    disappointed protection back home score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-name = "Python"
-print("Hello,", name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
