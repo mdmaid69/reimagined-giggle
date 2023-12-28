df4c92211bcedd6bd034c57dc8c2f1719f250305
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:59:33 +0000

    runout ride almost best worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_user_list():
+        return collections.UserList()
