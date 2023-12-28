dfc99adddaf7f0a840c5269c431f37d5a676b7f7
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:47:51 +0000

    keep mad window comment fix

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def multiply_numbers(x, y):
-        return x * y
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_int(array):
+        return int(array[0])
