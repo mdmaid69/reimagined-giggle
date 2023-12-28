b43a2859787fdbfff600fbedeaac3fcda56cb64a
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:05:16 +0000

    involve tap trick usually callout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
