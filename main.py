8441c063bc4121205c1a31fd1c3207b7d28492b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:02:55 +0000

    young pretty feeling bunch height

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
