80220713ceafc7322b688444803ce433c6b2bb59
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:45:11 +0000

    activity finally treat objection badly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
