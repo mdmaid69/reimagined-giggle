966a0c679b7a5d8fe948699630072c341f270e9b
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:15:49 +0000

    keepaway original result rush in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
