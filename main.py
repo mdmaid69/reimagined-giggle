cf35b76c2998b2aa4c9f75294b030b5d3415d615
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:51:47 +0000

    arrive stranger wave computer organize

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+print(sum(range(10)))
