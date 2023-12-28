25d9b815654f7ca44c7c4490329fd9bbe556ec7e
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:04:00 +0000

    foreign one lucky brush forever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
