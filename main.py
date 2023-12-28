11a408e952405d5fe9dcc2d9a033491e7529fb0f
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:04:21 +0000

    goout visit damage bringback classroom

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
