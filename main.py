dc2caf7deecc405911cb2237525242138abc589e
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:52:48 +0000

    spray shout lot honest emphasize

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
