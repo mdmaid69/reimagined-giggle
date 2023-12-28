a84b974d24a4f60b659df2c4a358cc6605d27b2d
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:27:05 +0000

    female remember window follow dream

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
