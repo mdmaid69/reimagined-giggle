53d5b2d5dce6349f5fe3f42d213a8d0fc4cc34a5
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:24:27 +0000

    afternoon hit entrance different will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def convert_to_octal(n):
+        return oct(n)
