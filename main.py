23548edb348f23b7a864b705156eaf1e69e0de5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:06:37 +0000

    loan period occasion really official

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def count_elements(lst):
-        return len(lst)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
