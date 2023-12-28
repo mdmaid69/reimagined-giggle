63a9e07dcc3457bb11ae288dfbf230c4147cb357
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:25:07 +0000

    wave best meeting profit pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
