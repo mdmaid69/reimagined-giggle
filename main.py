44e45769405db362532d0860c7b7d8ca3f3e2573
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:09:41 +0000

    ashamed example argument deliberate valuable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
