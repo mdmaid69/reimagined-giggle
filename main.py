00fa9732d4bab5825da4aaacdf31c291fff8b09a
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:24:22 +0000

    better experiment quarter progress pure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def clear_array(array):
+        array *= 0
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
