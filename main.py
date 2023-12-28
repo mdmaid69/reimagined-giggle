388faaa7f10f9d6c5b9293392d478d39f48d7e0a
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:47:58 +0000

    leave prove where stop mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
