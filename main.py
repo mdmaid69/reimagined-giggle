eff778f8955acd2123b6dc1069d91c45a017fd43
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:46:53 +0000

    normal long since interested impression

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
