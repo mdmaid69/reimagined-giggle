81b4824a3ea3b07da65a750b8ebbb6d5e0d22b45
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:04:33 +0000

    level charge second wealth mood

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import sys
+print(sys.version)
+  def reverse_list(lst):
+        return lst[::-1]
