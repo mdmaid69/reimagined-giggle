e9a69e97b148693d0fa9f248db20f76f8f0e9ccb
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:00:50 +0000

    file promise recommend hurry make

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import numpy as np
-print(np.array([1, 2, 3]))
+from collections import Counter
+print(Counter("hello world"))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
