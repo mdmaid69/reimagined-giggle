342204964402f6dc37f0d48fcf0804297987efdc
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:03:50 +0000

    pleased anxious confidence yes naturally

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
