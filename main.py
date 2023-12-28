b81d28e4e324de32839dfba89f0bed8fbee6be84
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:39:58 +0000

    please behave deliberately escape storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_power(work, time):
+        return work / time
