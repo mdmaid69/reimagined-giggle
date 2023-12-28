7a6413a7f4cfa3ab39fceca960d323e86f6b3180
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:03:37 +0000

    active stamp stamp fast shine

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
