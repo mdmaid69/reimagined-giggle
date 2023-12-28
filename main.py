24b2631473faa8e578d6402b1a488faf9f4247e4
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:16:58 +0000

    as exercise centimetre block behind

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
