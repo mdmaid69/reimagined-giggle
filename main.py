7ac6f8ea23c1654530ebb5440b2c15bf4248fc8f
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:59:50 +0000

    influence dress advice consist affect

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
