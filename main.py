53a50b664f8f7b90ef35e9ff481970389fb21c41
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:05:57 +0000

    ask smart woman terrible smile

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-for i in range(5):
-        print(i)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def remove_duplicates(lst):
+        return list(set(lst))
