a27cc8bddd6900b0577a3b425aafbb40350526de
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:14:38 +0000

    accept pause already tongue share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_float(array):
+        return float(array[0])
