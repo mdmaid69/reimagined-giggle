4ec68a849c7a7e08efa7f3caf34b3ff97e6e12c1
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:12:55 +0000

    forgive fear wall race before

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
