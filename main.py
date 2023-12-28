981e417469e8e83772d1b9ab08f33e24c15fcc62
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:30:06 +0000

    problem at measure everybody shoot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
