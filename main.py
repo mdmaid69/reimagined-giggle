afab4a0d4e7eaee384b13f86909d8791951c1f5d
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:50:32 +0000

    disapprove fine love notice your

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
