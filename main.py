97ac8ccf405d20f32e5ce3805ac2ce52c31c648e
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:56:21 +0000

    round square number key promise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import sys
+def print_python_version():
+        return sys.version
