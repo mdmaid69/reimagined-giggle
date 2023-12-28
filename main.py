4f5a54a168514eaaba661f607d9734ba3253df28
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:30:45 +0000

    tight theirs fat unite dick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
