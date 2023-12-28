2da539fc0739db6258de9f86f61e1e59340a3210
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:51:26 +0000

    satisfactory earth show Oo rough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def remove_duplicates(lst):
+        return list(set(lst))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
