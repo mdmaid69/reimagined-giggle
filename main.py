902d47646fd0ee9107695b6091f979d5f71f2189
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:59:51 +0000

    raise never what emergency bit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
