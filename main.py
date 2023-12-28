030353a14d928bcfe3e88998e170b012e64cd126
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:57:57 +0000

    while idea almost match wish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def extend_array(array, iterable):
+        array.extend(iterable)
