da1410f7b66d4ec2c2d48d1b418e1b66c7792d57
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:04:18 +0000

    regret past smile actual interest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
