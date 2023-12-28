18e760ca6280a596a077e10b751eb00865654829
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:52:14 +0000

    hide catch leave notice deliver

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import collections
+def create_user_dict():
+        return collections.UserDict()
