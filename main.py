bf526ca439c02c074568b299d3e2639883dd56ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:14:44 +0000

    ilovedicks picture supply trip skin

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
