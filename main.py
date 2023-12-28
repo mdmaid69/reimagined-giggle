44a60c5f62bd7fc0577d8b6e410eb5a3acecf21d
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:49:03 +0000

    nose show there design unlucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
