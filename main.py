a8bf5b7d2ecb73c19241c81bc5e24c1ccf19fa05
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:06:33 +0000

    turnon interfere keepup sometimes soft

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
