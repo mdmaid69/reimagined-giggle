2aa682feb391e5f8342d831aa63f1e4c4f12ac6f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:09:28 +0000

    bet but else bring pride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import platform
+def get_os_info():
+        return platform.uname()
+text = "Hello, world!"
+print("Reversed:", text[::-1])
