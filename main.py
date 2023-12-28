ed929dda599c10f2fb8a5d23c313dbe6c25b0cdd
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:12:09 +0000

    star cover to noise before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def reverse_string(s):
-        return s[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
