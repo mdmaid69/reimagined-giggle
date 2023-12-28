e78c3d1a153302e3667f9b3331cf8044bb335ad0
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:16:58 +0000

    dance cough onto about sense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
