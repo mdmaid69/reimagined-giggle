004d40f9ca383ad8c4930abb8e101a42d7637f71
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:31:05 +0000

    by breakthrough temper successful sore

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
