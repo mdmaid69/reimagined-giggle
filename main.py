479c1789299a30701d788daef9bed9202c7b7662
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:14:11 +0000

    calloff entry callout beyond excited

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
