4c5a2e27eeb00701bd59fbfca00ea8ab82da1167
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:34:16 +0000

    excuse speak occur woman survive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
