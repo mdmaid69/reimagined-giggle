2626ab9fecbdd5230fa0d2f64636c7a855cc96ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:28:36 +0000

    apply have bar value invitation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_area_rectangle(l, w):
-        return l * w
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
