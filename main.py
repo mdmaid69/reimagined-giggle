6071ac84287e9e2d7c047d87135e3d4ed534d011
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:51:07 +0000

    compete warning tidy attention dress

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
