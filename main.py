dcf2434e9921c31f7cbdeb89d90af1f8226e48ec
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:38:26 +0000

    bang getoff mark priority pool

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-print(sum(range(10)))
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
