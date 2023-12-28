90814368237846e2a7b305142ba96de6f05c5fab
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:52:42 +0000

    us conversation upwards get shake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def remove_duplicates(lst):
+        return list(set(lst))
