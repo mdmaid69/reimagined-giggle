2cfc2b46b76b96cf265e9c1d0cec0072c2be4c65
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:17:03 +0000

    move impressive organize borrow continue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 def setup_logging(level):
         logging.basicConfig(level=level)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
