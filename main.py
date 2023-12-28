fc3e3229309f6952b52739789ecf718cd7ea8b41
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:31:11 +0000

    position almost middle getoff destroy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
