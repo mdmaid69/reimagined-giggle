9e862931a73d4e1fc4197cc989a099afc5da1873
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:05:21 +0000

    lunch goback(to) upset avoid female

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import os
+print(os.getcwd())
