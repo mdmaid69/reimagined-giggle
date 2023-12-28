27916f75865de046cc5ece8c7f2ad56067b8fdd7
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:37:12 +0000

    pencil takeaway noone should preparation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
