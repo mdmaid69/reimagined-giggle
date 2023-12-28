769b05a97b6471970c4762de4ee16c1f4f4aae5c
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:32:13 +0000

    mainly central smell block point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_pressure(force, area):
-        return force / area
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
