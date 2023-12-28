e50499cde65bb70eb1e7476b616c52dd10cb19be
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:20:35 +0000

    expect accept holiday century shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
