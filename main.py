6468efa5830547a162b0e98a9a2d452a41612a09
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:09:30 +0000

    different reject Uu or grow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
