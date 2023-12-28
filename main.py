25e1e6dc71e7fcd2ba340f9c33b8577629f1dda3
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:28:52 +0000

    pool paper close relation contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
