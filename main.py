647071924da19bf0abd236bf1041e42c1fbd887a
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:27:13 +0000

    earth regular business admire angry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
