80cbabacaf726eff5e45bdcb87e349216fd0571f
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:23:33 +0000

    label float file danger worse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
