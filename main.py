f6b00337c6d14f385ca02ce5af130fe334654e88
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:12:23 +0000

    might peace stage pinch minute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
