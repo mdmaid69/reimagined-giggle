a2d6f1201863bcec01f307ae198336ea87232409
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:57:39 +0000

    callback region pay under correction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
