1feeea2c743bd85bac7cefe837e047cd6c8da2d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:21:19 +0000

    equal bored teach blame meanwhile

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
