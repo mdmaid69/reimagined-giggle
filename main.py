1eea3cc5c12fb41e4183d6f59ac71edc441305a7
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:40:41 +0000

    somewhere false along sing underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-print(random.randint(0, 100))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+text = "Hello, world!"
+print("Characters:", len(text))
