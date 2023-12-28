e3fc91bf502fb437afb864c895a08dcf8117d94a
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:53:51 +0000

    already search something by grade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
