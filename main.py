4d81edbcb4f98633559f1671e308161732277f44
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:17:46 +0000

    chin call quarter power excited

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def reverse_list(lst):
+        return lst[::-1]
