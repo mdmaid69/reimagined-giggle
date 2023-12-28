87118af54c72ebccbda57b75f9a5f0707c6ce294
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:50:33 +0000

    wear push century definite anxious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+print("Difference:", set(list1) - set(list2))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
