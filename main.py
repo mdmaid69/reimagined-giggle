f1304584ec770b008aaac418ecbd3eb642cf7ab6
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:19:26 +0000

    thing ability opinion Ff convenient

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def cube_number(x):
+        return x**3
