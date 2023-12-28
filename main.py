0b981fed49fc231e1bd8230366c28200ac7381a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:10:48 +0000

    solve goodbye join read reserve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import time
-print(time.time())
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
