c53ecd702f69cc492b73d7a0d03836c8e1174c96
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:39:55 +0000

    neat rely guard description breakout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def sort_list(lst):
-        return sorted(lst)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_as_repr(array):
+        return repr(array)
