7e8186d5d50c3989a711e771d3acb051e9e6bab8
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:39:32 +0000

    upwards message tidy music first

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
