12f17e142afd94d97f84ace526c5b8f4d6aaedb7
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:09:02 +0000

    well declaration before above complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
