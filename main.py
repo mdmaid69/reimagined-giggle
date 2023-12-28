78d9ed80c580c488af49220a570b5e657ff7a60b
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:22:43 +0000

    level height to live sit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_error_function(x):
+        return math.erf(x)
