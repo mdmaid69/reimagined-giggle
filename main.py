be694e2ccaa3f193b7f21cd9bb336fc270222539
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:00:43 +0000

    music shout scream lend switch

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
