eca2f8d7a1398de5c91927d1f37ee4e59131c5d1
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:42:04 +0000

    limit fair dry constantly centimetre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
