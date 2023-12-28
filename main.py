98249a685b2eb8adb2b5a588fbdc73bfba859a66
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:51:48 +0000

    tomorrow you female organization band

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
