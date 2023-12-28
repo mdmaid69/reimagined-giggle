0b7fb72f8c0964c5dc9344af2b48b1f58070b11d
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:16:14 +0000

    bit blame once swim degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
