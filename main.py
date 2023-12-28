5a520ec5b3f90a3ce0ecc7ffb9ec3b28dbb7a85d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:02:18 +0000

    shut trust fasten group convince

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
