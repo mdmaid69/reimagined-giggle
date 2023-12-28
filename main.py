57687343ad1baa5195c88f76f3deaaac56c93a42
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:17:36 +0000

    quickly truth bound cause interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def add_numbers(a, b):
+        return a + b
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
