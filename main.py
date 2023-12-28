071b70ee1c1e671406b48d8c8b63f926af5b0bf2
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:22:11 +0000

    crazy mad start conduct niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
