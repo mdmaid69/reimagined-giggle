b9c73f44fd483ba8becdc27d940d78df333797bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:38:01 +0000

    ilovedicks anyone fair bet far

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+print("Hello, world!")
