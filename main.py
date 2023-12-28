2b34cb9679abbca9ed1d8e73bf7e520336d1521d
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:09:11 +0000

    cut country event introduce weather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
