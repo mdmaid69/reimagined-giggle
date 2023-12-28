59c36f6366c285edd560c9432da1c9443dbbc7ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:37:17 +0000

    official lucky runover nearly object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
