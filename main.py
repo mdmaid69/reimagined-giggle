83fa2a164592913e5d271d6105a266769bc97673
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:45:38 +0000

    proper equal of unfortunate guess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
