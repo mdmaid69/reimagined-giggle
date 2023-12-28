2ceb9fff125d7d30f1392ad3151decb244296a4b
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:02:26 +0000

    round conduct sure impressive mention

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import datetime
-print(datetime.datetime.now())
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
