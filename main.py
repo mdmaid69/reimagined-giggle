c8b2c5b8c575069936eea96c601efc662c76d6d2
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:40:02 +0000

    will getup clearly hold wash

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def convert_to_octal(n):
+        return oct(n)
