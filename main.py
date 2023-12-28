922b9dea5aadcb107f79d9776462eb617e483b40
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:45:25 +0000

    this little shop success spin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+print("Sum:", sum(numbers))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
