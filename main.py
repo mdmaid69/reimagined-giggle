142ea7cfafa5bdfb85c591ffaef5a372adc16dc4
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:56:55 +0000

    discussion travel keen breakfast remain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def reverse_list(lst):
+        return lst[::-1]
