f88799efd366f9facaed8d41562e441b9c6f5c42
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:06:24 +0000

    bath official congratulate date open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
