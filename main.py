f7a0dd770c0adab3c01254b54f81bce967323774
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:35:11 +0000

    truth lock pay file city

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
