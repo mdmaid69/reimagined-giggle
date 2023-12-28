592a8e3a5f51f182b7170a6d020a5f55ef77e33d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:17:47 +0000

    cruelty workout contribute legal operate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
