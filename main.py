f66e1c9bd53211b8231515b053f6dd6215d37b31
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:13:35 +0000

    keepout pull wealth air come

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
