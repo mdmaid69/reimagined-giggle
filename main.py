1f0c91bdc3c2df106cd492daaa11284d34ae8b25
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:28:37 +0000

    group wrap autumn brief persuade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
