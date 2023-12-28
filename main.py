e908aa201a99c76a1df0c2f315e47eb6495ac463
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:07:07 +0000

    lie rich stare plant fit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+print("Hello, world!")
