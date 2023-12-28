65ce45ce9a1f8c414d6f45e5866955413f477101
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:05:44 +0000

    corner take space tap miss

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
