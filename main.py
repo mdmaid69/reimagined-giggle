0d06c40bd46f522738302878dc6bce6bbc1ac93d
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:22:11 +0000

    danger length Gg she test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import logging
-def log_message(message):
-        logging.info(message)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
