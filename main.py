76334de69c42e390d6bc143f2ce4e35256351d67
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:11:59 +0000

    aim on chin demand round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
