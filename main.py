9425bae01a34af1327412e94f5e0fe71f161ab23
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:35:51 +0000

    translate hope food sometimes chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
