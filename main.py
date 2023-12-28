9bef9b3ca49ff5b5702cc56640a2375c7fe3b6a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:50:14 +0000

    brother bite extraordinary she bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
