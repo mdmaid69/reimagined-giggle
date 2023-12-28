74a9348ef08de71a997d6711a98ea24a2ebbac15
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:14:41 +0000

    close independent walk church one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
