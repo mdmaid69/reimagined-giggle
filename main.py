8087aab7cc321d0a7d2a371223b993e0e27e9a40
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:34:07 +0000

    winter unhappy quick demonstrate appearance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+print(time.time())
