c27e1e6f530191fecaf9c967ac744521d131fdc1
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:36:39 +0000

    thank surface adjective cope mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
