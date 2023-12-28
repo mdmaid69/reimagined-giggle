5f136211d02362af228afe7a1fa6fa900cf762d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:50:38 +0000

    copy slow test list gas

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def sort_list(lst):
+        return sorted(lst)
