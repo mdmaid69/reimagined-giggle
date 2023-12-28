23a52ecc51c3b41b132e181c24c512011d49a5ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:16:14 +0000

    physical noise getout whom improvement

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
