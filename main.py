09bee13a99cf670cdc58bc591ef0aa000d0b43ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:53:11 +0000

    by wire state sorry long

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
