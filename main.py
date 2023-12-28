b66894fc6c34ff9b3d4e664b3acae8f9d2a4f304
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:41:06 +0000

    Cc opponent full space table

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import json
+print(json.dumps({"name": "John", "age": 30}))
