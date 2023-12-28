b62b91d4f1eb0ecccbe00f8e4baf39c9aac100c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:03:08 +0000

    ball protect opponent makeup debt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import math
+print(math.pi)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_item(array, i):
+        return array[i]
