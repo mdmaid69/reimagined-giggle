9ef970f6745164ba6405c4f05adba6664e9ddb5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:05:24 +0000

    black typical careless invite shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
