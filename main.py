dcdca21fb8e4a0131db066281e068243115e14d8
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:15:30 +0000

    test last brave trade somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
