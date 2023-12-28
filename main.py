21409de41710b4f1e4a87b9b6b93e91a8a2a4a40
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:23:57 +0000

    comeover talkinto spin meeting influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
