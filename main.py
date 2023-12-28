f18c2fa2441223da8981a32f3089f4ef21c23132
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:12:00 +0000

    introduction impatient conduct stop circumstance

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def cube_number(x):
+        return x**3
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
