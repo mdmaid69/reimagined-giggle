eda0c748711af33c2a915e89a1888eded1b7aefe
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:42:38 +0000

    achievement scream immediately complain poor

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
