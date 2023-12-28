bb8d5b1608706e009438e7a89ec5c20552ece6a2
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:52:00 +0000

    drawing much house uponon food

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def subtract_numbers(x, y):
+        return x - y
