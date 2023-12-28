932324a2ab36d3b0cee5cf6f2a158f8643b2e91e
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:58:31 +0000

    onto shame bend threaten drive

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
