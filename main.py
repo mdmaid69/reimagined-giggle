818ec860ac8981e419a6e4341b55b72d22b48f95
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:04:57 +0000

    description health criticism obtain limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
