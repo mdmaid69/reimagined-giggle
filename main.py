ab0d9ccb45a8d364e04a6e0aaf4b7657ae1f0aa3
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:43:55 +0000

    lookafter clean disgusting inquire book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
