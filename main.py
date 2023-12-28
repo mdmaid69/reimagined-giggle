cde71efdab92562152ee40399f023e5b8b2e20f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:57:23 +0000

    regional floor solve university sing

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+print([x**2 for x in range(10)])
