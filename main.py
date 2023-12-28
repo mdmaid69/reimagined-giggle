00f590a3bcc29a6c25e08646f5b8def4684fc28f
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:19:23 +0000

    spring break debt use dare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def remove_duplicates(lst):
+        return list(set(lst))
