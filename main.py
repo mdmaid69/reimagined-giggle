50e67bde8256a72f6e5021a8f5629f1e858cc269
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:10:38 +0000

    admit ring address exact godown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
