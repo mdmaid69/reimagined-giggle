7b90269fe3ce3571dd9f985c52c1fbc283bacb56
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:51:48 +0000

    exactly check usual afterwards round

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
