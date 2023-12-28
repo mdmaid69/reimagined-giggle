eca847f49d3b7dfc82ddb103983004bd814393e7
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:15:32 +0000

    disturbing offer agree beginning post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
