65fa2338a89b4710ab1442e84859bf397588d235
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:09:39 +0000

    mine brave clever act general

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+print([x**2 for x in range(10)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
