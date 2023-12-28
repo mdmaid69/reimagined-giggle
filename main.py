93737a40a91ffb243cc9a58b733da4da76001a76
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:51:43 +0000

    leave stare godown plant people

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
