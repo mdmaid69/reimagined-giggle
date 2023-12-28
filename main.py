fd1beaa6ae3ebfa973fa90f7c91c175a87029c15
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:34:29 +0000

    fly best lean cinema market

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-for i in range(5):
-        print(i)
+def calculate_error_function(x):
+        return math.erf(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
