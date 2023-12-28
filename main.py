c4d2eb5a9000750a4eed83b5902ed8c10bd52b10
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:17:04 +0000

    holdon getout intelligent himself finger

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
