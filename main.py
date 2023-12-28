53f7195c8f6f11fae44b6608e312e823f9209a1f
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:03:27 +0000

    clue further funny spray unkind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
