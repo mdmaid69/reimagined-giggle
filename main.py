6886ab0d466c145d091746ad3fe27ecd0b29dca4
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:10:11 +0000

    pleasant bush rely straight priority

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+text = "Hello, world!"
+print("Characters:", len(text))
