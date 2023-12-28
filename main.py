195f27146b3ce1c037d555dc1c21ffa10096c736
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:44:49 +0000

    complete concentration popular programme unhappy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
