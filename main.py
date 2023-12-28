6f73d8b705d341d065202bd62913e8a0cd6db474
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:55:19 +0000

    wealth anybody sure any uniform

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_average(lst):
+        return sum(lst) / len(lst)
