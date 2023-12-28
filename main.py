8013b3f57ce8c92fee60d557a416d58f1fee8d94
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:56:16 +0000

    air nigga suit communicate cut

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
