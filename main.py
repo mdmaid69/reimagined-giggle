41155255c1e4c2915a14911aaaf0be76be9c2fde
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:37:51 +0000

    substance peace bother up sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def get_array_buffer_info(array):
+        return array.buffer_info()
