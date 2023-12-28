f77d121649c1e7b41033f897332e87582010038f
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:04:31 +0000

    communicate dance material frequent mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
