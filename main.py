ad996773aa2b75b323f8ce5db31bfaec18edf14c
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:49:36 +0000

    Nn owe environment seat tip

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
