0df1e57b128d4569a41f83ab81b70bdb3c7f66d7
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:07:35 +0000

    pay goodbye organize skill when

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import time
+print(time.time())
