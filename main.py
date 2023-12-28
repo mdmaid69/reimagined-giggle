fd3d0a0aeea7159124f67a30290d5339a54299f4
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:21:29 +0000

    moon kick show such statement

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_density(mass, volume):
+        return mass / volume
