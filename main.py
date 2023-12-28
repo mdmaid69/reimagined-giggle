9c2be61acb03a686f8a0574c5cd617cc45460bef
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:39:20 +0000

    communication exact congratulate bear mood

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_acceleration(speed, time):
-        return speed / time
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
