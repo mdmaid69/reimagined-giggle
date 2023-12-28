1e49e3134bf75698772cf7aa5147824ed5e1e89b
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:03:12 +0000

    any great plenty report Mm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
