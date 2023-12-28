e46b8ca246ff48126a89cadf747002de13fd5bcc
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:23:58 +0000

    survive suggest shade Gg search

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
