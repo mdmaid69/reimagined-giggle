85ae80001859d2c22a2464c5d00d1971e1444beb
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:42:49 +0000

    deliberately declare pupil rise consist

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
