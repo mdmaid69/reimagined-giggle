db021e9c1add3f5855184b3473e9846277a4d85e
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:07:12 +0000

    manage crowded unusual buy knock

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
