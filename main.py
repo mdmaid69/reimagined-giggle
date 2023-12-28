d688b13004e85d5cb0cf071136c60aab7fff3c59
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:23:52 +0000

    foot holdout thorough consist tax

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
