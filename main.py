fcad73a13cedc47fb5128e25ee8124b7ea6c353d
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:47:38 +0000

    ride pool present lost complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
