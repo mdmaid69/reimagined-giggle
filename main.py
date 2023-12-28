30a951d4117cfe0a47889ac2406d97e3bed3c8fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:15:56 +0000

    pin science uncomfortable concern hear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
