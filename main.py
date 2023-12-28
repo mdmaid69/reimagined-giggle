c951f5eeabec3275100587f1b1e10f9fad96e5c3
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:26:06 +0000

    opinion sticky mention which measure

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_power(work, time):
+        return work / time
