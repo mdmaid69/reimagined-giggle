4a4be1e3f310e98c6a23584c99a8a684ddeaef2f
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:31:00 +0000

    opportunity cross peace level uniform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
