6769bec59dc332895a8aec6ee4564ad07b8536a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:26:37 +0000

    injury every rescue consist passenger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
