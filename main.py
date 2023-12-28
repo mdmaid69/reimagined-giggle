69485a9cd328be67a1bca7f6e23b21b89b0f7097
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:54:32 +0000

    politics sort communicate cope priority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
