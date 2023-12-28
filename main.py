1fd1e5d3f87d4e14fad8831d67f8d91a304adb5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:22:39 +0000

    radio increase share him about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-print(sum(range(10)))
+def get_array_as_bytes(array):
+        return bytes(array)
