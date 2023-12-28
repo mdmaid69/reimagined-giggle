41af3e675d15c0ebb36b4d08a902caf2fa4fc306
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:04:32 +0000

    pay completely reliable possession lot

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def cube_number(x):
-        return x**3
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
