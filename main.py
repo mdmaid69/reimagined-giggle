42fd7c96e6ed767920a642106129bbf30592e77c
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:54:28 +0000

    zero appearance possibility breath transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
