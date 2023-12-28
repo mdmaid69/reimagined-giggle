89c0a43ec11d42fbc60b3efd7b9071f6fe5c7e11
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:44:13 +0000

    definite without somebody period bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def divide_numbers(x, y):
+        return x / y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
