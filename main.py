971b905ce75d6a6b2fc824664c9f45115114c8f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:50:46 +0000

    satisfied thinkthrough complain odd scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
