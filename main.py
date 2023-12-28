19f1d343b23d1fb8bfaa1e91decf51af00c74438
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:27:44 +0000

    event top year along healthy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
