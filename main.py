43891cd7d9b6964ef2e272260fc83061ab5d5327
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:21:57 +0000

    charge plenty switch properly guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
