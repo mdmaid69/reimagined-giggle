851cc7c73729613e4de4160ebb524de371573f62
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:49:59 +0000

    physical want contain attempt lay

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import random
+def roll_die():
+        return random.randint(1, 6)
