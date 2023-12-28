01c45b585a43aeab0bf320c311eb1e8e078ba942
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:00:09 +0000

    whistle air produce run point

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import numpy as np
+print(np.array([1, 2, 3]))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
