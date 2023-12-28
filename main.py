bb443fbf53e6063ab781c05591da64c04c8d4531
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:58:03 +0000

    OKorokay left organize estimate splash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
