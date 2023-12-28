56b2aba9098ec3aea649ce51bbe48ae3c485d594
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:40:00 +0000

    fast direction soon backwards trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
