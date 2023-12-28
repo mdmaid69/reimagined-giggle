89d5bb792ddd5b6fdc1426aabcedc9e2f429a065
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:24:48 +0000

    his nor any believe convince

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
