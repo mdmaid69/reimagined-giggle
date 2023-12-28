e5f626748fc210b0f0293690a17190ffdd27562b
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:01:01 +0000

    argument complaint worth best target

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_power(work, time):
-        return work / time
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import time
+def get_current_time():
+        return time.ctime()
