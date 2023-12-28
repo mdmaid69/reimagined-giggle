68fae383652f16828e101ea886ed19a549236823
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:05:53 +0000

    workout second sad here cough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
