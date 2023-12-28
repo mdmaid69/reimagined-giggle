45b45275b755fc7bae8bbb08f30f168635809cae
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:25:33 +0000

    takeover most below communication word

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
