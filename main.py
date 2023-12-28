6f0d66d9a365be5f9a15dcb157de68a172f9f2ad
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:06:08 +0000

    string challenge punishment brown study

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import sys
-print(sys.version)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
