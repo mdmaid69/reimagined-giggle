e034d5294b25830d56d3bf371700a878d64e730a
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:34:39 +0000

    react last occasional wander themselves

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
