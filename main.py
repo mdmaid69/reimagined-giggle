cdb6e453b3d96a41d7cc1404d3231b9ae6c8428c
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:53:09 +0000

    ball task central getup condition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
