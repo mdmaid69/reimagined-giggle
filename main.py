38e65f51bfbb0923284ce8ebfcb2da21e6440bbf
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:51:29 +0000

    commit praise solve depend pressure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
