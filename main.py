d9d508fd86c041b032fafc542ce540becdecae48
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:20:59 +0000

    size lead hope convincing permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
