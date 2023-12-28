12c838c759bb7e07f48973b4cb943edfa5d5f41b
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:22:46 +0000

    attempt cold many condition conduct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def calculate_variance(arr):
+        return np.var(arr)
+import platform
+def get_os_info():
+        return platform.uname()
