50f2224fefe4e357431d70fe9fb187541f4df1a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:54:39 +0000

    throughout performance classroom Itself call

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import datetime
+print(datetime.datetime.now())
