0cab37fde1fde92f29a24597f49f09e5f312ef6b
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:04:08 +0000

    father part behind valuable intelligent

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import time
+print(time.time())
+def calculate_speed(distance, time):
+        return distance / time
