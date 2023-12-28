352d1d365a893ec1f7fc3d24f38b58b3858e8fbb
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:48:30 +0000

    health complicated smile press seriously

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-print([x**2 for x in range(10)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
