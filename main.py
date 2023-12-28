e164c08bb4c6233b85f1fc34d65e3c9f2d1eeb6d
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:31:15 +0000

    goback(to) radio affect smell year

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import collections
+def create_stack():
+        return collections.deque()
