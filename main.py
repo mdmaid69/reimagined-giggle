77fefec9d23ffd5a6a7448619cb60ae148d29389
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:54:21 +0000

    just follow drop recommend find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+def get_current_time():
+        return time.ctime()
