d97fb30615c6b9956f4801b3d86ac5eea6771151
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:53:46 +0000

    disturb workon address company ahead

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
