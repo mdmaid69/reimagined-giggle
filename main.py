56d7de395ac627167457431bf85f331216f91e4c
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:33:15 +0000

    loud sting never famous father

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
