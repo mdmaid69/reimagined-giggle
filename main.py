765df484622fd62595c8e1ed0da095d0009dfd43
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:20:08 +0000

    568energy lead embarrassed similar talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_str(array):
+        return str(array)
