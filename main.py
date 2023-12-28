85f554b257e92f3bd661bff9838ec6cfac9d8b92
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:43:17 +0000

    case condition attention foot bus

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
