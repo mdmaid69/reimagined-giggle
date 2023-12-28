2d2a77a2aa999cdd732af55e1625ef82de4cd359
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:59:19 +0000

    use bye truth damage paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
