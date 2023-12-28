f1764215b91dbbd59e4c72c2d5ced6af423afcca
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:44:34 +0000

    thanks me hate active closed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+print(os.getcwd())
+import datetime
+def get_current_date():
+        return datetime.date.today()
