e6a3939e4082b63700295133bf6c9c4f58144a84
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:23:18 +0000

    length much process happen break

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
