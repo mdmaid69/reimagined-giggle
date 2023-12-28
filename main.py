d341909a15ef9c80a0b4bdebca0072fa8c5d9e8a
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:55:22 +0000

    rise inquire find trade music

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_perpetuity(payment, rate):
+        return payment / rate
