549b680227584227a376feaa10fe8d079a8318f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:14:45 +0000

    permission yourself weigh certainly equipment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
