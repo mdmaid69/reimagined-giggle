377583fc2d01228bea6b66aead004bc38fd6716e
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:39:09 +0000

    disturb love score prevent getaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
