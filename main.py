a53c75caa3859e2ee799ddda8f154c9bc744683d
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:15:03 +0000

    plate suckme how care blame

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_tuple(array):
+        return tuple(array)
+def is_odd(n):
+        return n % 2 != 0
