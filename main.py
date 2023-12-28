7055138ccc04672de5178ead963fe523ffa89715
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:37:09 +0000

    exciting environment muscle whisper advantage

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  def remove_duplicates(lst):
+        return list(set(lst))
