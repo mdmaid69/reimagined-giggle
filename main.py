2038644571f5f34df03a8db609b5c1aa67867db6
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:46:24 +0000

    which call this along makeinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
