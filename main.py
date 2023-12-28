f31f996aa50f00b6fce869a1e218652515562c51
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:26:15 +0000

    admire concentrate impatient plan record

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+print(sum(range(10)))
