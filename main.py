2fba3d9b0a92da211f9ab1617d506eedb4a70575
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:01:05 +0000

    569enjoy chat in standard moment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
