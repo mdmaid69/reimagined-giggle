c5fdcb6d719b4ba494f8075122c9c19c161e6eff
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:44:40 +0000

    all shame recent strict nor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
