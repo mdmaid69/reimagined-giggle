5670d9f39ecf773b429f8cb5c18bd8742c9720a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:23:42 +0000

    scene before forever finish exhibition

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
