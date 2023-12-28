e550221be7d6e98471c974f79b6686ca8092abea
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:36:24 +0000

    ocean path thumb bath inform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
