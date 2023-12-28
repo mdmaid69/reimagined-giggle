6b8b2e1131ce7ca423c9bb3c308461be1c3f5e5b
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:51:17 +0000

    border honest thinkof somehow course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
