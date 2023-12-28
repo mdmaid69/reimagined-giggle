ac077422c51a2d7485c58ca0d1cefc64ed3f3251
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:17:20 +0000

    speed born behave satisfied simple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_array_as_set(array):
+        return set(array)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
