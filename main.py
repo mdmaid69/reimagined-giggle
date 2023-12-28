92f00bb3aba299629607f4f40dbb5d8ab04d104b
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:47:59 +0000

    face past demand of unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
