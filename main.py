b7a16e5f6496f7e81624bccb890e8eeaf927f8f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:39:10 +0000

    narrow happen way tape lay

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
