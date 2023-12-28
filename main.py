5b97b7fcbe77b782e9db6acbf818bdcfeac8f3e2
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:39:29 +0000

    hungry takeback 573entertainment turnoff many

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
