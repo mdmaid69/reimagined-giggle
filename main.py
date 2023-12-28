6bd3eaa5de967fcb98b9a7fad82b4afbc3b5d53d
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:14:33 +0000

    we post national himself since

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def convert_to_octal(n):
+        return oct(n)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
