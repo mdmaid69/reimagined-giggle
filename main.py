0a5fe4919e3fd21775ad6413e33a3e7b0302c6d5
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:26:09 +0000

    none steer tap position cause

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import time
-print(time.time())
-name = "Python"
-print("Hello,", name)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
