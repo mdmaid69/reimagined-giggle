e2ee244feb665a00a09e31dc3778c6380427cc8c
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:31:21 +0000

    interesting willing neat middle stay

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import numpy as np
+print(np.array([1, 2, 3]))
+import collections
+def create_stack():
+        return collections.deque()
