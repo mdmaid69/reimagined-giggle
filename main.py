6a4696e005d6f0795787028ba598324a04d813fa
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:58:30 +0000

    drink workout add dress another

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
