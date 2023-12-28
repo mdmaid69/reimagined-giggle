298966c160058cbe221be7049fab0f623b0fe59d
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:23:04 +0000

    comeoff uponon estimate common our

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
