abaeb579c94b104d773e94f925f226a5aeea3270
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:06:37 +0000

    bend side care recently not

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def add_numbers(a, b):
+        return a + b
