beca492db67bf11f9adf0ad9388da9758a6a4811
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:54:15 +0000

    busy tip belief pronounce wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
