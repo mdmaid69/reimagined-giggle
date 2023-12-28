9554dcd3d5d0dded9350bd231f1435b389e1315f
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:15:52 +0000

    dick set wherever well actually

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
