21b6e312c854a7c303fef0a2666ad84d7602c2ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:07:19 +0000

    fond instruction neck connection motion

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
