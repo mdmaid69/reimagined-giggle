46182ec72cd1151aba90eb9d8b3c799f141f7970
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:10:28 +0000

    secret herself shopping break failure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
