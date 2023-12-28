a70a867d41eaf0f3eedd68490756608541888eba
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:26:35 +0000

    several book thumb understand wrap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
