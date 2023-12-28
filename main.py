c50d554e1afa30f96e05fff67b04be7244f72679
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:19:37 +0000

    I curious fail Hh taste

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import sys
+print(sys.version)
