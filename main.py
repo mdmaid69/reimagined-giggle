b538f7e630697937a01fad99ad5389b84d687c31
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:00:42 +0000

    distant accidental crisis Zz comein

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
