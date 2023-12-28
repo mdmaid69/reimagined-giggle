c9b332fd7859c613bc9a768f3abae63e783bd724
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:40:24 +0000

    home later failure join Ee

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
