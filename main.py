2adbf417d5dd8b80f296d439e3deb8dc8dc99fb6
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:39:56 +0000

    office somewhere voice advice combine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
