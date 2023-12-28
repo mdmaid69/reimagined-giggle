3c9bc9fe799116f76415a06a447e817f69a78a81
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:40:09 +0000

    cover exchange though though pot

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_average(lst):
+        return sum(lst) / len(lst)
