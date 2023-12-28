7545d0a46a98b1b18ce03f3be80ade4996ef63af
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:20:36 +0000

    noone firm reply believe noisy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def reverse_string(s):
-        return s[::-1]
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
