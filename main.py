769a3e2cbd28fefd60d601e55893c387cd816276
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:18:55 +0000

    agree body knowledge difference simple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
