dc046dd00cb8c5b9807c3dfabffd693cc003b7a4
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:46:42 +0000

    improve tight wind disappointed style

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import sys
-def print_python_version():
-        print(sys.version)
+def convert_to_octal(n):
+        return oct(n)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
