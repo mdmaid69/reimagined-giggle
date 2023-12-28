ba435deee29c48410af2e6d99bda86c140891b82
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:36:04 +0000

    good like fix likely over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  def is_even(n):
+        return n % 2 == 0
