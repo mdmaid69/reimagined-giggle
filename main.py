9a3dd31a5fabee1959ff5e91ad41d8ce4dc6de1c
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:35:55 +0000

    rarely better firm fond absolutely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+print(json.dumps({"name": "John", "age": 30}))
