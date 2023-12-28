976e001ef13aec37c4131535916ea7386addcbd7
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:00:55 +0000

    broken turn concentrate ring trade

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
