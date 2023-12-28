78db19e2a1ef280da9ecbea1484ed068391ac66e
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:55:33 +0000

    means prepare cost emotion whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_bytes_from_array(array):
+        return array.tobytes()
