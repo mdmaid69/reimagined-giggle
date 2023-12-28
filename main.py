f8509a693d0850c521b8977289622f7b48a86cf5
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:43:07 +0000

    challenge alive laugh software inside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-print([x**2 for x in range(10)])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import sys
+print(sys.version)
