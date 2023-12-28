9a246675c086e1979cf70af8b50b0bbb6c37a486
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:54:58 +0000

    meanwhile thing private term difficulty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
