ad1ff7f0d781787c1d11e78c957b60b080cfe5c4
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:30:02 +0000

    create university raise rise star

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
