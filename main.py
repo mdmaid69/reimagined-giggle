c232759ae0735406692d45ce27bde45367fa8a62
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:54:02 +0000

    rule hit takeaway ready pot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_exponential(x):
+        return math.exp(x)
