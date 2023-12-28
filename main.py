0a99ff76ec02fed56c36f99bbe581ee8c08ad177
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:37:06 +0000

    rate before fortunate risk us

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
