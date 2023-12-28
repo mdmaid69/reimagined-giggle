f7269d5bf5c8499e3b1ec4c5684bc04226cddef3
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:18:05 +0000

    whoever edge tomorrow early scrape

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def cube_number(x):
+        return x**3
