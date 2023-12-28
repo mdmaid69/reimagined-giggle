a85b3c1ae68364c5964d72ec261d7a7415bd2dec
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:26:18 +0000

    scare seat study since win

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
