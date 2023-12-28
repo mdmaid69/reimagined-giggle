43514fdef99c09ba33b33a7e123e70c8b0f90e92
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:03:30 +0000

    definite complicated split modern write

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+name = "Python"
+print("Hello,", name)
