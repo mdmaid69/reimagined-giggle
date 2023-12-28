8101269f92d634cfc4354a76f5c4f6d3511b55b1
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:20:19 +0000

    production close food gather prepare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
