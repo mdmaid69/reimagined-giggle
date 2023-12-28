89f61316cf3dce7d3ca875fae1c7a29da5739766
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:31:06 +0000

    machine tape very hospital joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
