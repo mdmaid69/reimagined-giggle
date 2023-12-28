ecbd9ad0815f933e37d1f9cc3e80d0b39fce8369
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:28:35 +0000

    anywayanyhow start already passenger shall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
