a80ccae77eb2967fbc5b3d4e9e383f4421eb9689
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:04:16 +0000

    what nonsense repair score touch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def convert_to_hex(n):
-        return hex(n)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
