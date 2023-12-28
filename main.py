b59f5c2f34834a888b028e733140e0dc1bf0e3a3
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:03:12 +0000

    face emphasize Jj tax have

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
