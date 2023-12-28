fbedaf40f4312f5156d398628968a5311c818aa3
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:26:20 +0000

    something present transparent straight swing

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
