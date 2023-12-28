6b24ff12bea0a7fa537dd610b4f905f26b0fc830
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:23:35 +0000

    description actual related protect deliberately

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
