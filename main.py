2f9b0f01403f5442357d36a18437c32e06642b13
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:28:26 +0000

    old mind start market behind

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
