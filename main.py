4ed680ae60537624f71fe40a282f59cda3629aa9
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:52:01 +0000

    cope black dark speech obtain

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import json
+print(json.dumps({"name": "John", "age": 30}))
