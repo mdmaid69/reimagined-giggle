bab89f1047421becc4ad27a89fe485bbcca87d9f
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:16:38 +0000

    act trip clean your return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
