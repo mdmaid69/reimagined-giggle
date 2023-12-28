2c938edabeeff8e4315c8d1f9cef989771eaa0ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:32:03 +0000

    short game steady fetch soak

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
