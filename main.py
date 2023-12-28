c6368d2f8959dcd6bd2adb1e4146ecd28ac343c5
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:40:17 +0000

    argument either opposite contribute hope

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
