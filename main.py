338671dd2cdb095d753fb4de972576919ca7bcd2
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:35:40 +0000

    organization oil section how switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import time
-print(time.time())
+def get_time_since_epoch():
+        return time.time()
