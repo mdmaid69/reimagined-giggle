b3773ca512813b7215d4e25d71e9bf5e639a31c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:46:07 +0000

    instead exploration smell trust if

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
