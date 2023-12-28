fbad4c56da2ac4062fc43f7dd09d951f901e7373
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:47:10 +0000

    profit power back mention fire

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
