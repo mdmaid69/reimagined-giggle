ab07850979ad8bd047b319b490c40c75a1bc5a02
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:38:09 +0000

    rule keepfrom gather Qq fond

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_density(mass, volume):
-        return mass / volume
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
