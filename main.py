efdd7009d16c1a9ead6a8416a9c7f64459860a07
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:06:00 +0000

    any count back recently different

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
