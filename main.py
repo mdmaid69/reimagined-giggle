fc86e462b112f58423fa3e3fbed5001fe2ed729f
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:35:42 +0000

    several body blame pick body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
