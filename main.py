1a98d9bad5850976d9b7840a8eb1696e4b317ec0
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:37:54 +0000

    wave drink ear swimming future

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+for i in range(10): print(i)
