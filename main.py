ef7c204466b3de131646b705d69ab3ceec492fde
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:50:42 +0000

    hear about chat dance explanation

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+for i in range(10): print(i)
+def calculate_power(work, time):
+        return work / time
