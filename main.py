3380eccca3b62811c46edb8a87f083b7d174ed75
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:41:54 +0000

    float keepout rise come path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
