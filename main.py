8267aec8db0be0b12716ffbc222e7fa47cb52685
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:22:34 +0000

    moon there front clothes ought

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
