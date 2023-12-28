3d4b21edbc83ed76b74e675f51c21a326ad11ac0
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:56:08 +0000

    town reserve drop introduce port

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
