6798e786c923df754ef30e4c555fdadf71ace200
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:07:35 +0000

    fetch deserve group view Yy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_work(force, distance):
+        return force * distance
+def find_min(lst):
+        return min(lst)
