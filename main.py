352406a12d3356e131e864372c5db08f63bd2a6b
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:37:37 +0000

    region scream runout achievement rid

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def is_even(n):
-        return n % 2 == 0
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def reverse_list(lst):
+        return lst[::-1]
