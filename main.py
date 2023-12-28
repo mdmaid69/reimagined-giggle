c3449a064ef1fd4095dc83b6c55e0d3249f36571
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:38:48 +0000

    trade tend honest burst achievement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def square_number(x):
+        return x**2
