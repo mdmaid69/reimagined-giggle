b6d8919fa45b0a00c03e99682101e6f7613697b2
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:57:29 +0000

    temperature that actually floor handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
