1cfbccf5d3c5ab915f6be8aa345adf561207fe15
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:46:00 +0000

    land pile hurt if quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
