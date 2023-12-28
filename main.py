3afdf163f281aadf12f9c4df4f5000e46a2970a8
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:25:28 +0000

    cause Vv react exam extremely

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_tangent(x):
+        return math.tan(x)
