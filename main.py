24f51c42fe8a29feca4de732538d7e3de978f4ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:43:09 +0000

    spare progress responsible people path

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_base_name(path):
+        return os.path.basename(path)
