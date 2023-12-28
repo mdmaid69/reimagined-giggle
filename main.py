2f8a60814a627d9361e5092a2b94f256a6c94e28
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:46:25 +0000

    true necessary success carry scheme

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
