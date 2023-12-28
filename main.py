b195623d005b5bc8d66a31ab0aa3620831574944
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:09:33 +0000

    weak design towards ours finally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
