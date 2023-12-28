a8bb7576a8051fd249fd5b90ca287c473a9f632c
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:13:24 +0000

    maybe dick drive sleep fun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
