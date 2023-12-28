ef3e9ea00355017b69b497c389fe39a8b6eb1cff
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:39:16 +0000

    dinner ignore run detail characteristic

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def convert_to_binary(n):
+        return bin(n)
+for i in range(10): print(i)
