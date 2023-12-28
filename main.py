5e2aac10c1d2dd72e7696718203262d4bad6935f
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:36:06 +0000

    beginning past pure press comeround

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
