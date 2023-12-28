e7cfe9600860db01d05d1409cea695cdeb1e37df
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:57:51 +0000

    appearance at back guilty soak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_min(numbers):
-        return min(numbers)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
