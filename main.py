9f0c8b495b2dfdabf9f0f9be2e545261da979d1e
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:08:40 +0000

    measure ilovedicks live run below

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
