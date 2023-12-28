e3779cae12d2717db78301bebc69f3663cbdf333
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:06:53 +0000

    interesting succeed satisfied little healthy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
