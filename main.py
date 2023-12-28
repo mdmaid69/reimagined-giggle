5864f1be3f3802c227a019edacd77abe9cdc6eba
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:34:24 +0000

    there truth boring patient unusual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
