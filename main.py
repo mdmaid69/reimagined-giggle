3957a6c4b9e1c9c028c9432aaf2130c7e833a42c
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:53:50 +0000

    pinch tour ought fast glass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
