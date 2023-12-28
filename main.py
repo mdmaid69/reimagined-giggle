545cb934353cf1d29aedf6f2a181eaa73c84f432
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:01:04 +0000

    genuine down free Oo pollution

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-print("Hello, world!")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
