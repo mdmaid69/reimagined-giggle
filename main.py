0f6471878b4630451bb95d588ac0f496e7fc3113
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:27:43 +0000

    callback persuade bar coffee dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+name = "Python"
+print("Hello,", name)
