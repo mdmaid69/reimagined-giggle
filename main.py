7bfb6bcc3f00079bf184c968f6614490c0b7ee97
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:39:26 +0000

    pleasure correction turninto basically area

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def find_max(numbers):
+        return max(numbers)
