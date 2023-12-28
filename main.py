64ebe389b87ddc45a7237e178949fa6fdb7ac6fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:49:29 +0000

    just come hope finish thirsty

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
