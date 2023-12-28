0818c5e34f961e4d8b5ef05915fee64ac98b5b77
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:23:35 +0000

    move complicated behind mail pity

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
