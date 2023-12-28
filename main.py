6ce88c0596d976f7738b4a72be5ec72d8654f8d9
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:55:16 +0000

    everything rain return side yesterday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def find_max(lst):
-        return max(lst)
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
