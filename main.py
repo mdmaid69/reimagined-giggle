e7b0ca89e5310717ddaa00526669138e2ac0f609
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:10:37 +0000

    change name impressed reduction I

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
