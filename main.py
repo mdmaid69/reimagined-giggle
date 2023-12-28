15dc92432b250f730e9c926d9011d953ee529df7
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:29:58 +0000

    thought drink heart yesterday operate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
