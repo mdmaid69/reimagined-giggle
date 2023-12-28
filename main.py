3b70ad465385f7cc219604689e578c7f9b49e94e
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:35:36 +0000

    exactly weak study produce however

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+def print_python_version():
+        print(sys.version)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
