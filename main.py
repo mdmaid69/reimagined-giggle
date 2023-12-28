fd38f02f494b6fbd8702f8ddd2d2014f1f4b665a
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:57:17 +0000

    prepare runover division card imagination

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_cosine(x):
+        return math.cos(x)
