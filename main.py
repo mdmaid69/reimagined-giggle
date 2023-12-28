4c885ca5a79912e4b70ce636e0138c33485e6e1f
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:30:53 +0000

    stop strike consist he ground

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
