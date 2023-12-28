5d790ed62c782badb41a19b621000b55b2efa779
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:21:23 +0000

    emphasis compete suit ignore accept

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
