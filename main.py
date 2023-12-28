7c40e9d3b18a84ae20bfe73932742ab28ea2ea58
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:30:30 +0000

    payment suckme exaggerate mark off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
