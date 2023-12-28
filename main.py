c82ae6ab103667efe0fb3b4824dc7fa6513cc1c6
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:56:19 +0000

    geton getoff sick runover consist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
