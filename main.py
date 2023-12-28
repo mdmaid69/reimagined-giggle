6496b0a1a475a1373d0ba450b1c223f938425d37
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:01:17 +0000

    system respect greeting swallow dare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
