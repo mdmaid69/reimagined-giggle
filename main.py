440131e2c2ce49a034f6515347ef8b7451bd386c
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:45:23 +0000

    dream boil rush smoke block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
