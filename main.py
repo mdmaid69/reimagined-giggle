17814a7cc1536714efb9555d900ca2fb5e1fa3ea
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:58:22 +0000

    him clothes thirsty control shallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
