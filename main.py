9a5afef5577964d763d3e205f0d2dd0e7d395f58
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:53:15 +0000

    target taste sorry bush pool

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
