3f8da43e518230d29489c2ae750fb618fa57eace
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:01:20 +0000

    size reply public inform service

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
