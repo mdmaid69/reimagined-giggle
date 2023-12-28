ecf0ffb74d97da68e148329300f809e513f35599
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:03:39 +0000

    rescue I square smart miss

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
