95cf93641fb7cd72a8692a97767d6bf439776532
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:50:10 +0000

    phone man dance sit government

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
