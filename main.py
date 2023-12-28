7f9ce45b0e6c62308f0de5bfc2c5866652c96c23
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:35:25 +0000

    war partner circumstance Dd land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_bool(array):
+        return bool(array)
