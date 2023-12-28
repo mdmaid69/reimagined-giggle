067dce278afa3a13e8de76ea6887384c0d1e5879
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:42:52 +0000

    free trap aim before accommodation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_work(force, distance):
+        return force * distance
