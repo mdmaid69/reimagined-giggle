633cc3bb5add6f173e363c8f355c2193869a916b
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:19:57 +0000

    congratulations how excuse takeover lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
