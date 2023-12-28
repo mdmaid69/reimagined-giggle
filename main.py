9311b6befcafd7b856b42d09e63637d84ffd0a8f
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:43:43 +0000

    late but wide weak offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def sort_list(lst):
+        return sorted(lst)
