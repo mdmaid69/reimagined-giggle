ae980b9d2549cbfdc01757ae79d904838cf398f0
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:11:19 +0000

    cost where Oo wide steam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def count_characters(sentence):
+        return len(sentence)
