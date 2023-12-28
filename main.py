a94b0250605dc1b1c4568a0eac15799e525d35de
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:55:44 +0000

    drive up wheel lucky emergency

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
