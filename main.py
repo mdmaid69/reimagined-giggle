6997eb7f225cfd3c5a872c3e0a135c8e7115705e
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:46:56 +0000

    dark kind relative improvement part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
 import math
 def calculate_square_root(x):
         return math.sqrt(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
