2f111e53253414551629dce326de626f05a2a54f
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:44:04 +0000

    to declare taste debt everywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
