169fa83308f07d2e250096ff984cc7442fffa3f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:51:20 +0000

    pour subject watch provide description

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def convert_to_binary(n):
-        return bin(n)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
