064110f612f6205779835563eb1112989bd1aea4
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:33:27 +0000

    classroom whom raise bring although

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def count_characters(sentence):
+        return len(sentence)
