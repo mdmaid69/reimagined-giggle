cca8bcd7ccf91717831b4d042ab4897a63ccd915
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:10:29 +0000

    past scream promise meet confuse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
