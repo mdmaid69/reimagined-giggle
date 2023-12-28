9be7b74f9f6596a1623be0f287779f244f77eed7
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:27:57 +0000

    hope good across maximum just

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
