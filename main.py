b0e9311998b26c46002d4a4b209e64148f4be51e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:56:58 +0000

    band really traffic professional practise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_string(array):
+        return array.tostring()
