b9c3b8076405233f3f020fbbd4235179a5f37560
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:58:39 +0000

    worry condition flat offer gas

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
