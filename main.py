0849b1e2d3929c58240c958effa761c8c2a15e1b
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:32:28 +0000

    free burst on public acceptable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
