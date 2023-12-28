6b55c6d1a9e21dd0201a1200fe44a074463d5ff2
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:23:52 +0000

    love warn height lend equal

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
