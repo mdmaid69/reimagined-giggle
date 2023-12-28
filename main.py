cf2011e48932aaa91bf71c51853fdf5d1f7cb816
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:55:23 +0000

    trust trade fair project obvious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def is_even(n):
+        return n % 2 == 0
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
