a6675f36e3d1102cb60b5199e54ecc25ef96ba4f
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:46:50 +0000

    sink news behind about worth

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
