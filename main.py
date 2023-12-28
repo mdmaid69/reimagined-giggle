13ec2540d3b3acc49a902aff11820bab1da97d08
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:41:37 +0000

    pick think shake room clue

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_string(s):
-        return s[::-1]
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
