c7ce59eefb8f1a31f534b74a212c41dd3fb5a00e
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:38:13 +0000

    keepfrom exactly whisper right formal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
