acf58c0a2bd3b5fbe9224db3dfd4efec3451c22a
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:20:39 +0000

    professional difference benefit suffer concern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_item(array, i):
+        return array[i]
