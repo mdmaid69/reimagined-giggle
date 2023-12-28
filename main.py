80a8d2730430ec28cbd061bb6138e55c7143985d
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:33:56 +0000

    soft wheel cool arrival pressure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
