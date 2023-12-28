c8b18765905a0b7a612bb7589f127ecf9dcc08e0
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:03:05 +0000

    underneath police expression rid adjective

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def find_max(lst):
+        return max(lst)
