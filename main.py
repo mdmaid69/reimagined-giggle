f4fc28687f9a08581c5448da4313d9857d63b287
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:06:54 +0000

    finger morning challenge find dream

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-print([x**2 for x in range(10)])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+name = "Python"
+print("Hello,", name)
