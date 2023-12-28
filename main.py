6ee659cde88ac2db34c57ce19e528d04a83c5886
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:18:16 +0000

    consult accept wash steep minor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
