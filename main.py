f30bbe2b2090a1011f18c917c84ffc48460f646d
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:53:59 +0000

    purpose its thing worst failure

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
