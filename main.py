ec4228eb0d6f978716bbd9f1df3f1055947525bc
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:13:05 +0000

    find ocean ahead chin dare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_str(array):
+        return str(array)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
