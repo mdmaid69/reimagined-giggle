fcda78f2a1314b67c8dc57ae885b9ef1ad2c13f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:12:33 +0000

    passage far contact knock reserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_acceleration(speed, time):
+        return speed / time
