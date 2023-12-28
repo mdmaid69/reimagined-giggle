56c681035a08bffa211ec9c23259a94362f27d3a
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:19:00 +0000

    grade pollution progress mass far

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
 import random
-def roll_die():
-        return random.randint(1, 6)
+def generate_random_choice(choices):
+        return random.choice(choices)
