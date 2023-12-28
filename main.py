9447521a868904170cf2a5f33b40c330885a00ae
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:55:21 +0000

    tidy talkinto practical which since

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
