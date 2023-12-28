859fd001024a727b1f9b9602dc21203738451986
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:37:00 +0000

    recognize jump run gift once

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
