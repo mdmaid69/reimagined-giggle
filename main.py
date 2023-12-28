23b51f8316e292c6be9550766ae84658d39beeb2
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:05:25 +0000

    present cause evening callback add

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
