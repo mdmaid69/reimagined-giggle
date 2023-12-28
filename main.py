e8a5a7fccc9ea63eec9b89a0f8c4ca4a4fada52f
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:16:23 +0000

    upwards family hello scale theirs

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
