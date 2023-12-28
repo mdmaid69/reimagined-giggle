d7aed84c335155b352dbc24b6cf02d5a322afe24
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:27:50 +0000

    surprising passenger interested honest how

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
