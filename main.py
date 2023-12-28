5422a3cc11b165b5c1a7741e4870748bd487624b
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:40:32 +0000

    getoff passenger joke unpleasant sport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def split_path(path):
+        return os.path.split(path)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
