9681005e3927e7797d1a50482d203b04338b34f9
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:36:45 +0000

    represent plan spin tight or

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
