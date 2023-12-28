e6f54bc23a7b957ce2f02d30829b2d2c7e9cbb62
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:59:15 +0000

    shall contents talk possible cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
