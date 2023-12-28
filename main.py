c02bd4876fa220f59c1ee390939d5cf481f9f277
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:03:15 +0000

    569enjoy sore leg empty should

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
