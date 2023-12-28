776f729cd70d5240518a50596791946d60cb16c7
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:09:28 +0000

    flavour little sound exchange occasional

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
