b7a793a57eb17c64df44eb327d3343b81c2c1c1d
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:34:48 +0000

    temper eye talk collect comefrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+print(math.pi)
