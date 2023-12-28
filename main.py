dc3dc61c4b53e9df86c236f07392a1a801fe2df6
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:27:46 +0000

    last catch trip push threaten

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import numpy as np
+print(np.array([1, 2, 3]))
