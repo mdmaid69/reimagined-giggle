3b8a34226efbc7bcc85b144c7ebb768a3116ea4b
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:53:12 +0000

    slice force although concerned passage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
