03150669cc753ff04c34ca227272d7ff085e3207
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:06:16 +0000

    smooth its copy geton petrol

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def is_palindrome(s):
+        return s == s[::-1]
+import datetime
+def get_today_date():
+        return datetime.date.today()
