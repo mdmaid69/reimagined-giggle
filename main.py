b6db00a41ea1d42b86c0c3dd0c6adc5de69f6d4a
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:45:45 +0000

    right happen rich anybody emergency

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
