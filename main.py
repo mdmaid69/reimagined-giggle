123acdc148cc5b86b9c80cc0f892d57a276b10f1
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:57:37 +0000

    task part makeinto move keep

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
