daf78cb536e950f5e3209244fd80909d8eb661f3
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:46:22 +0000

    judge equipment drink part himself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
