edfe40798ea9a9455ec169fa7b8f5e307b05dd8d
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:34:31 +0000

    poor moral independent blame language

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_pressure(force, area):
-        return force / area
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
