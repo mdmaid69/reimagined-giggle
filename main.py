a1df07882c529eafad794b72cc4578a2edc2e2f8
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:20:47 +0000

    busy window own name blind

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
