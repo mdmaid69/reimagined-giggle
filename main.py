5b263d724d371910b873f3599e8d99f5129eb40e
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:01:23 +0000

    kick blame choice nervous branch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
+def calculate_force(mass, acceleration):
+        return mass * acceleration
