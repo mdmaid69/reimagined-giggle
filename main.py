ae6d5c7c4d7594eba4d089cced443212e3d00c51
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:04:20 +0000

    further surprising development dull flow

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import tensorflow as tf
+print(tf.__version__)
