dd59c94fbfb43176179ea8023685f1ca6618d75e
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:05:18 +0000

    unlikely discover lot law shake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
