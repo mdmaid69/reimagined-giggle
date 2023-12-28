61b540fd875a9da301fe0be8e889740c3250f132
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:55:14 +0000

    sort pot walk leave talkoutof

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
 import array
-def get_array_length(array):
-        return len(array)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def get_list_from_array(array):
+        return array.tolist()
