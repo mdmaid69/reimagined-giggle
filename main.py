8ea0ab85a25041e3e5a6b861b939f98f076f97c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:00:55 +0000

    prepare really her forever change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import time
-def get_time_since_epoch():
-        return time.time()
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
