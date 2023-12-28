82de7baae64de8ed59d3d80d9f0cb1f6ee594be8
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:34:20 +0000

    fit soon command variety string

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+print([x**2 for x in range(10)])
