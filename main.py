81a678e2397ab89063fc1b7bf87dd04dd6930778
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:55:01 +0000

    happen he tense taste than

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
