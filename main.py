3a31433e9749cf5e588a32191f5e8adf39e7eb95
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:05:28 +0000

    comefrom protest boil comefrom temper

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
