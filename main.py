a4cae23e112af6b548291f67fbff8deddfa17644
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:04:27 +0000

    family taste heavy before never

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
