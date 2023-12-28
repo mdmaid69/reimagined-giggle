8af566fb53b940333835d9295ef650161d78d500
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:45:54 +0000

    hope instant anywayanyhow error tragedy

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_itemsize(array):
+        return array.itemsize
