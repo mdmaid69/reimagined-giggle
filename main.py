1d3c0bace5a43caeef6a2bf146c3290b70d25789
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:31:12 +0000

    float determined race slice seem

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def convert_to_octal(n):
+        return oct(n)
 def reverse_list(lst):
         return lst[::-1]
