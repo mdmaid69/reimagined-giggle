a59e010d67b1136a380e1ecad8b2bd8e9132c834
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:15:02 +0000

    stop guess wet somewhere question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import sys
+print(sys.version)
