b614b49039cdc5db6a9e81cdf8123a0100b78b0b
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:49:20 +0000

    introduce consider scared waste achieve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
