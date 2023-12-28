33d2c099fd074390948595e0581160ebb0359eba
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:14:26 +0000

    middle turn turnup sleep confident

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
