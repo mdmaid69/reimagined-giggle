25ae991eb058b9fd62c2149271edb3b49ae093d6
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:56:24 +0000

    object act threaten band popular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
