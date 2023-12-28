ad47fdbf4aebc56da505e94f8b34c64d7882e252
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:18:22 +0000

    future 572enter original disagree runinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
