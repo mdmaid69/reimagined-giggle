774ef59d4a82ad90daab3143d8c37de3ac3995bf
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:51:22 +0000

    belief transparent special cold care

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
