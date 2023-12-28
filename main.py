a1a4270549872181b7794d083fa94ff08aa3b51b
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:34:05 +0000

    area skin pride private error

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_power(work, time):
-        return work / time
+import time
+print(time.time())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
