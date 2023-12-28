e63338465f824c751b5afdc9469430cc2a6029ed
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:41:52 +0000

    breakfast protest callat noone come

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
