8c5fa8f960993724cbe97e5100c8d399bc1f93e3
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:34:14 +0000

    like tall beat fix argue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
