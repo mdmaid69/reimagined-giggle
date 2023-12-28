16a65598154104758ef4ec0dbed3410010a53fe1
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:49:46 +0000

    wipe recover clearly press recognize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
