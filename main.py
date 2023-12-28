1ba731c9a698b02c60a941cb10ebedf0cab9eec0
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:21:14 +0000

    soft report 572enter among slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
