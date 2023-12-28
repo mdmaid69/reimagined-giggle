4a85e0bec6c82e2ab192cc03e44b694f9f3f2970
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:57:08 +0000

    container past workon underneath tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
