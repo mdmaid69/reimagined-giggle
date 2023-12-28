75eec759a644f60793dabb1e6841af3c5df4a2d2
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:05:25 +0000

    squeeze empty keepaway cross embarrassed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_acceleration(speed, time):
-        return speed / time
+  def square_number(x):
+        return x**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
