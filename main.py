f52531bf55354f0e24bf089552b17d31b6b4489d
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:45:04 +0000

    exciting goout patient sure turnout

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
