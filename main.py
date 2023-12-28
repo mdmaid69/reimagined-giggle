2a47d0c5773d0b90a0718e5a4c7ebccf952b2b83
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:57:38 +0000

    by interest justice already generally

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_acceleration(speed, time):
+        return speed / time
