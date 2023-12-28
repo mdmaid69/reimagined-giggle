a185f693cf7dc2baa5cf133537848c538ca10a82
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:57:33 +0000

    collection bye indeed block city

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def extend_array(array, iterable):
+        array.extend(iterable)
