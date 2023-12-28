a677e703a31493e27b9fd3b4c6ddf3f881e4fc10
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:46:30 +0000

    inquire care turndown dream relative

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
