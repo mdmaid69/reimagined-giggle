bd8ee3d23f0caa06cef552a8fe37d270ad458721
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:22:10 +0000

    deal fall after nowhere themselves

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+print(sum(range(10)))
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
