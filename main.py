d3ea5032ac800fa803729175f84cdd5a42f0e449
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:31:30 +0000

    far profit supply fact culture

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import logging
-def log_message(message):
-        logging.info(message)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def convert_to_binary(n):
+        return bin(n)
