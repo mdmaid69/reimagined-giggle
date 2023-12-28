60d26c3325d249c8aeb8dfb37475275389d2a33b
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:54:56 +0000

    practise minor rubbish still touch

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-print([x**2 for x in range(10)])
+def divide_numbers(x, y):
+        return x / y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
