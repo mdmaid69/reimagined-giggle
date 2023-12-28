af243921bd26add2e72c475b0ee8a511cac4af4e
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:06:46 +0000

    result mistake keepfrom tune peaceful

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
