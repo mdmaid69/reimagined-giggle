de0f0c0a0855570324220ff3ba2c9453ffcc16fd
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:53:48 +0000

    set pull father across person

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
