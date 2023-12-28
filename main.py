949602f6e28ba6f0a91d16ec13144d86799b7b91
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:20:11 +0000

    go ours some right waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
