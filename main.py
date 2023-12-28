9663a4e16220b598987f687adcd125dae22ba052
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:13:28 +0000

    fix distance rid another spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_absolute_value(x):
+        return math.fabs(x)
