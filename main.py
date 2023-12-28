8802bc2e42db8cba23db72291784f93a2525edeb
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:06:58 +0000

    work workout smart too I

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
