4b8c2931b5c25de38f736953cfc0063a38be4403
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:20:15 +0000

    noisy fierce wish voice rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
