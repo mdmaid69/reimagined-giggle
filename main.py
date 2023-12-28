c7d584c7168ac41e3c66f340c48707179dde14fc
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:55:58 +0000

    treat post stamp needle mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import tensorflow as tf
+print(tf.__version__)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
