5fbf77f6e058b30437ab70e84bccacc419729af9
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:43:58 +0000

    right scream convenient feeling mail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
