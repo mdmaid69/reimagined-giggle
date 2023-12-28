a299c6700e3d1bae69f2ac25da1bfd08c8bc235c
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:45:21 +0000

    schedule bin experiment throw priority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
