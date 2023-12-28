28ab7485fb3fcc388a7a4f4dd0b4091823be320c
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:10:27 +0000

    represent switch move relation enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
