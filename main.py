93acaa197019fb53d75e76f4522c9932b9a3ec7a
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:13:52 +0000

    occur frightened ours nigga just

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
