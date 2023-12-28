1861f109363573f4b181b50842c6749a3502e707
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:18:34 +0000

    already play Oo of perform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def convert_to_binary(n):
-        return bin(n)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
