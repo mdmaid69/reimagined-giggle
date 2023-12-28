52acc32677c39f06277d8ef196ee7544d9566385
mdmaid69 <robot@alexsyw.me>
2023-12-28 05:57:34 +0000

    fire spoil dangerous chance pipe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_acceleration(speed, time):
+        return speed / time
