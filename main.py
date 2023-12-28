19221d37125b1ddfdd1d7feffd02fa9e2b44632f
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:41:10 +0000

    impatient design catch spread wall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import logging
+def log_message(message):
+        logging.info(message)
