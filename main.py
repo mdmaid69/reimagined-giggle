00009db0c3ebbf25361bf17566865828e9dd8270
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:37:29 +0000

    practise her Cc patient offer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_min(numbers):
+        return min(numbers)
