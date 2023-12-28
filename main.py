84a19daa05be1abf67fae9976bd4a00da0e6ffa6
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:19:54 +0000

    shout push thinkover break stop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-from collections import Counter
-print(Counter("hello world"))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
