89ef4ffe8df5179cea6b207cfa20be0a99b251b7
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:54:02 +0000

    getoff Itself criticize joke university

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
