e4e5571dd61fba9ef0da61738622b02bdace612c
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:28:53 +0000

    right sensitive whom alive her

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import numpy as np
+print(np.array([1, 2, 3]))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
