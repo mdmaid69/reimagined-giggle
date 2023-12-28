7cce3e75766c55be13e4488d40d4025672094b5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:04:08 +0000

    shadow birth until attempt by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import os
-print(os.getcwd())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
