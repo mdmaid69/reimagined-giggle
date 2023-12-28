bcd255754445279f60d4942b981d19bbd1ccee52
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:25:53 +0000

    down that consider cry oil

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
