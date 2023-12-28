b5003dc016c154ac3388aa3e823f74cc271af236
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:12:02 +0000

    perfect moon escape sticky confusing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import collections
+def create_stack():
+        return collections.deque()
