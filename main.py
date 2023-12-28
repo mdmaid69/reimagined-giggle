1bd4ac2bd295cfc61e3d99f7608f3ca483f8cfa2
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:42:44 +0000

    start able fix progress another

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
