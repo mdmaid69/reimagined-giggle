808ac682ad682d2846e407c64721024f9ad7587b
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:35:56 +0000

    OKorokay path introduce also smell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def count_elements(lst):
+        return len(lst)
