c2600f72aafe7974e8831589335b205a67838386
mdmaid69 <robot@alexsyw.me>
2023-12-28 06:58:19 +0000

    embarrassing boil beautiful same anxious

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import platform
-def get_python_version():
-        return platform.python_version()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
