f4ae243cfc5820de79ec547f85ccb0d86d51b3a0
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:34:50 +0000

    pattern Ff stay mouth load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
