e607e3d58cca1368523a35fe42734a7bd3053124
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:20:13 +0000

    pity means big impressive violent

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def find_max(lst):
-        return max(lst)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
