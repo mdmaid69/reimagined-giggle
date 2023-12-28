ea4ec19f4f5f846ef92e403cce91d69cd0141bd9
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:00:07 +0000

    meet sorry reply priority but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
