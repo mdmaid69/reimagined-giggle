d3a4cad7e228ef82d510dfa7945a726ae3dd6122
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:15:54 +0000

    might anywhere left drink bringback

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def is_odd(n):
+        return n % 2 != 0
+def find_unique_words(sentence):
+        return set(sentence.split())
