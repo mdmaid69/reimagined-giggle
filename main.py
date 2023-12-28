fbc82ecf632ee577800ea3b649ef44b65165729b
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:39:52 +0000

    among pain time grow estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
