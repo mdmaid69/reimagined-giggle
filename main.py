7b9ee3d3570367515bfde697f9ec4be6ea4507da
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:14:11 +0000

    568energy bit expensive sign exploration

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def is_even(n):
+        return n % 2 == 0
+def calculate_perpetuity(payment, rate):
+        return payment / rate
