26e4e0a5bb331bc6c7975e2d7b20512e23991610
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:29:42 +0000

    perhaps get right song rare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
