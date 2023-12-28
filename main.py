9fdda9459b4d8c3bc29517aa36754d90176ad337
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:25:48 +0000

    loan crowd circle most teach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
