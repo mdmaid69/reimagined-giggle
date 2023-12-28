96a1714aeda5ed0d739bd3b239cf7a311942174b
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:21:22 +0000

    afraid tend wide criticism pause

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def count_elements(lst):
+        return len(lst)
