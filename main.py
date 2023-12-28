a96a042f9839fe84fa004ab1d3888c93326271c0
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:37:18 +0000

    star somewhere importance drop belong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def get_current_time():
+        return time.time()
