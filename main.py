b493a71ddcb0b91015d4a8f6f0bc65acbb79106b
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:14:52 +0000

    why furniture conclusion treatment judge

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
