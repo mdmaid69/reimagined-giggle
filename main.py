45c024bba04fc59ab9645465dd050cd62bf4fb38
mdmaid69 <robot@alexsyw.me>
2023-12-28 03:14:34 +0000

    address ring communication this supply

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def find_unique_words(sentence):
+        return set(sentence.split())
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
