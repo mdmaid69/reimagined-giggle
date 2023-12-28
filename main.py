1aee5b0757cab0e43406b6d0484785aaaaa66590
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:09:59 +0000

    successful anywhere Bb godown tear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
