c6f2b6718523a88738a20e319985ef3cb06cc43c
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:01:55 +0000

    correct deliver practice fresh sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
