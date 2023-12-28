7d13c9d12ec52a0b5a8502596f1cb2331e1c6ce6
mdmaid69 <robot@alexsyw.me>
2023-12-28 23:47:18 +0000

    report bet scrape once emergency

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
