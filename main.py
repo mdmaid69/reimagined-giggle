32ba0d317a6dc0c73fdcec17128961747479cd55
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:37:57 +0000

    limit book runout wire lend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def add_numbers(x, y):
+        return x + y
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
