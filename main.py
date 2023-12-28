10e4f7723fe8946ee23f6732b1ce93254fbfdca0
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:30:46 +0000

    design transparent dick shake Cc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def find_max(numbers):
-        return max(numbers)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
