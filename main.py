61e1f84cabaa9a24a289cb693dbe76cf13c37a74
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:03:03 +0000

    recommend job watch rely frequent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
