43f5f1239f8404c70c84cddc26696637a28ded5e
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:59:02 +0000

    show best sleep side shelter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
