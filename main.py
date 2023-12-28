e03a46aa0714e1a5acfcebd3b658fac47ddbba65
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:51:13 +0000

    simple try centre should teach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
