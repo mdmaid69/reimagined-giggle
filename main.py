9fb2382f2055e7dff334d826463f44648aa7377e
mdmaid69 <robot@alexsyw.me>
2023-12-28 11:32:49 +0000

    long almost slowly instrument attempt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
