69b80ea3802ad33849a5d6082f3ec8e7518f06ac
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:39:25 +0000

    flight lock fashion knock drink

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
