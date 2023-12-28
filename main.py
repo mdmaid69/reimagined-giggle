cd006eb2d0a1e8f528a9944cc1d20def9e4719cc
mdmaid69 <robot@alexsyw.me>
2023-12-28 07:53:45 +0000

    usually bright next family related

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
 n = 5
 result = 1
 for i in range(1, n + 1):
         result *= i
 print("Factorial:", result)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
