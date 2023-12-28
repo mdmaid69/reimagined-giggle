b78de8a21602fd3c08f1b5b033901e100b7ddad8
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:15:53 +0000

    play noisy region interrupt position

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_owner(file_name):
+  def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
