9553039e5d464c37cf75779d9707f93201ac1cfa
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:38:19 +0000

    empty influence throughout stamp trap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def count_words(sentence):
+        return len(sentence.split())
