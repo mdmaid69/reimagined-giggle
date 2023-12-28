bf10eb0a9376381e001c6e1a8a555f09e464c873
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:06:37 +0000

    set mind announce moral copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
