c764047be55a73be8c05047d70350b2c3a445220
mdmaid69 <robot@alexsyw.me>
2023-12-28 18:21:55 +0000

    embarrassed someone outside rule must

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
