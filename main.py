95154828311d948558a90afa59c0d81030a43a4b
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:37:37 +0000

    bus burst still than cinema

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
