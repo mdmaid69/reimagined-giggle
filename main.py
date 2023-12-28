b045b3916ac0e07920806d5f6ebfa5c09f9c54bd
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:02:24 +0000

    move interesting trial turnup club

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
