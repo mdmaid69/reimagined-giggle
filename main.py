3acf072e19f72b2bebccee41594c574ba6fc3011
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:45:22 +0000

    any solve mainly share compete

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def calculate_area_rectangle(l, w):
+        return l * w
+import re
+print(re.match("h.*o", "hello world"))
