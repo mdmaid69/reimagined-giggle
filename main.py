e28d1a623a020083464f70496fd8803839d2c178
mdmaid69 <robot@alexsyw.me>
2023-12-28 01:37:13 +0000

    peace trip 573entertainment some increase

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-for i in range(5):
-        print(i)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
