391311ac3b22db0ea170cb926a11a8de7b712353
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:15:36 +0000

    lookup thank punishment division polite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import collections
+def create_counter():
+        return collections.Counter()
