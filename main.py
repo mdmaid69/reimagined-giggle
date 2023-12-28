0efb55c0dd44074db348204f1f8497a6fc27ca76
mdmaid69 <robot@alexsyw.me>
2023-12-28 02:54:45 +0000

    contribute Mm we book happy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
