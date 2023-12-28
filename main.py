d124caafed5ebb8f663015fd7ea751b4d3f87f9b
mdmaid69 <robot@alexsyw.me>
2023-12-28 12:36:16 +0000

    hair list discuss improve sometimes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_palindrome(s):
+        return s == s[::-1]
