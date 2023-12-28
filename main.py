ae6c8655b6692697b8785f7cb138077ad8915400
mdmaid69 <robot@alexsyw.me>
2023-12-28 16:59:15 +0000

    trust game once house more

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
