d7c91ae0f6982f5ffc079cb762d6086ac053c218
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:01:35 +0000

    less emergency busy violent post

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
