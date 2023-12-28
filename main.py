f16e06f9daabf6a4165ad9f7c016422bffa06835
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:44:54 +0000

    scared reply content advanced fresh

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
