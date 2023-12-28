e6ac8a1571d7341ff0b858b28633d5cf949e7ffd
mdmaid69 <robot@alexsyw.me>
2023-12-28 20:33:14 +0000

    importance choose team characteristic perform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
