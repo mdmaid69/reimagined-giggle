a0071b7f38f0c87a4c0babcb19e2558e0a2467e6
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:53:08 +0000

    loss count cold murder bite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_index(array, item):
+        return array.index(item)
