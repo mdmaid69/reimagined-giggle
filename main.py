95809fd9ecd27a7ac90ad1b8161411461bad613e
mdmaid69 <robot@alexsyw.me>
2023-12-28 00:49:22 +0000

    listen butter not advice organization

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
