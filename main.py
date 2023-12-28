9d14402ec76be8e82d7c52c28c676be3ecb94ea1
mdmaid69 <robot@alexsyw.me>
2023-12-28 22:25:03 +0000

    cost smoke Ff meanwhile charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import platform
+def get_os_info():
+        return platform.uname()
