15d198121614e3e109b1a2156454d4e26eed13ab
mdmaid69 <robot@alexsyw.me>
2023-12-28 13:00:54 +0000

    holiday spite Nn little hole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
