8ca567d5594986874c960ebb2e744e929fb228e2
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:12:30 +0000

    emotion total together noone grateful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
