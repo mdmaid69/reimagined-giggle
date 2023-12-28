327a4b1e471299c149ffeda7d29ff3af5bd04620
mdmaid69 <robot@alexsyw.me>
2023-12-28 04:53:23 +0000

    market flow eye trade comeround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def greet(name):
+        print(f"Hello, {name}!")
