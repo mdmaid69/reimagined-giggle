fcae82c05ab0ec05dee41c80c996111c04d55309
mdmaid69 <robot@alexsyw.me>
2023-12-28 14:58:29 +0000

    back central swimming 571enough prepare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
