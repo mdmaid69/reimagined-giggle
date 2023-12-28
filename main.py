fc04ab8eb58070dcee8f60bfed5cd08bf0a3d7db
mdmaid69 <robot@alexsyw.me>
2023-12-28 17:51:17 +0000

    consider apparently either comeround price

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
