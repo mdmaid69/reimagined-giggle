950db5c99db13f61ef27f8d7a594c065ba7b187a
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:05:07 +0000

    their legal dance passenger mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
