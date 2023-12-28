7367d8a47aeb2a21ad94f3409cc69bf5ad85a5ca
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:31:10 +0000

    amount justice still translate crisis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
