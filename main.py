19d0dd59fc894679af426a8ff044f577c27b8ecd
mdmaid69 <robot@alexsyw.me>
2023-12-28 10:06:51 +0000

    subject young like main convincing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import time
+def get_current_time():
+        return time.ctime()
