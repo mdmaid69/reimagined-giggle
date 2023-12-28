abfa0bd702d2131b22996728466bb059e67897c1
mdmaid69 <robot@alexsyw.me>
2023-12-28 21:39:35 +0000

    play after night look table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
