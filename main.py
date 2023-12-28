07fa8c0cee642f1e317e06ed154f541c799ac15b
mdmaid69 <robot@alexsyw.me>
2023-12-28 09:48:17 +0000

    weekend concentration surprised underneath should

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-print("Hello, world!")
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
