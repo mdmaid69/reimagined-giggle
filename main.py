57466f754efa83d11cc0b40c968c683cd81c2de3
mdmaid69 <robot@alexsyw.me>
2023-12-28 15:48:22 +0000

    persuade precise tie on rid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
