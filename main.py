49b83d0f7b335926baaa5da74aa279873971e00d
mdmaid69 <robot@alexsyw.me>
2023-12-28 19:23:27 +0000

    wave judge blow project middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def get_array_as_list(array):
+        return list(array)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
