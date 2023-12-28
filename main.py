61c19cbd7320d8f97c0bf81cb4be666b32ee082b
mdmaid69 <robot@alexsyw.me>
2023-12-28 08:52:23 +0000

    runinto file noise tape begin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def find_unique_words(sentence):
-        return set(sentence.split())
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
