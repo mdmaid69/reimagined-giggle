7ae8da7923e5ae7de8f7069420aa5948e154d9a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:27:45 +0000

    steady explanation image container education

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
