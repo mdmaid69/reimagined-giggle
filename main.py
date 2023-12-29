821680b8c1d0f75871bb3e70173968fe56ece1b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:56:22 +0000

    worry pretend dare offer control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
