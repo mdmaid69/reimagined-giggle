d928e69833dbeeda51b69657fb477adf4a835d8e
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:58:59 +0000

    small regional typical choice commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import time
+print(time.time())
