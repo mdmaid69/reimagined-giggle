c8429993bffcf21cc17f21e07fa6f775881eb87b
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:17:48 +0000

    land off instrument bridge broken

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
