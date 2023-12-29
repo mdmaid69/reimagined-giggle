9f7c04ae3d7bae3b59215dab4361685b1e6ea073
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:22:18 +0000

    constantly area switch trade sad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
