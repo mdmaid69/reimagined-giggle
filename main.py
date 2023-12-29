334caa96c8256d8d4bf57f812c364c90bde9bc6d
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:25:15 +0000

    angry Dd scare pride computer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
