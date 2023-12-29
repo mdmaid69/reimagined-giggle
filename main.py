3c273dcfe24a4b1957af3a6d3b6a7eeb493f08a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:56:01 +0000

    star accommodation seat final unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
