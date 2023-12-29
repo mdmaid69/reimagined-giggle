bf6ffc1249162a0b09fbca650b273a7e2a321dea
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:23:29 +0000

    moment suggestion I passage born

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def multiply_numbers(x, y):
+        return x * y
+print("Hello, world!")
