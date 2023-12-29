02601e9ffb1aba3de77b8b76efd67aeae3adaa40
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:12:06 +0000

    thick he however wash top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
