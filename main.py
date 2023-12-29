d73502b3dfc688715df4e0fb2e6baadbb91bd0cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:48:58 +0000

    watch thumb just majority dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
