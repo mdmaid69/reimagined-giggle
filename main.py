7594e2677f72c3dc32f7a117811fe55f67bcbf23
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:43:36 +0000

    face bored arm straight leg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
