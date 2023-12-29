9881964a435fa5095412e27451f49b708764727a
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:14:26 +0000

    switch command emphasis this firm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
