75f88fed8dd0e3cc8c62085ce4f85350e9cd83a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:11:47 +0000

    mean character straight extraordinary thought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
