e7df8b97e63a692c48df2cb0be1cabdb9bccb7e8
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:12:21 +0000

    future own responsible experience question

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def convert_to_binary(n):
-        return bin(n)
+def find_min(lst):
+        return min(lst)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
