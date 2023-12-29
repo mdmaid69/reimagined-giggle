e866a5c0d26d359e333b207d355dfeeb9ee34d55
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:30:30 +0000

    naturally keepfrom obvious event appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_index(array, item):
+        return array.index(item)
