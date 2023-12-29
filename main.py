e85ac7e8d3a8e5c990d81d2f8cef3fde45a4a721
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:25:25 +0000

    probably report present count mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
