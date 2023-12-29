b7b5ac4761b00241bd31e4effe90e0581078b396
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:11:49 +0000

    cheek brown feature makeup unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+print(os.getcwd())
