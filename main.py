d3c39de54a2ca67d8eaaee69513baa261ffa400b
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:17:56 +0000

    to brush law die belong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
