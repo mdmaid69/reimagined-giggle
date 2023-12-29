2eabe5ebb1c9a05a76dad0fb8aeb77541e5c0fa8
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:24:24 +0000

    custom thank warm knowledge perform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
