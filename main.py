6f48e74237041555dfdd28391befa457e7d7557a
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:10:46 +0000

    sight makeinto pleasure behind callat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_queue():
+        return collections.deque()
