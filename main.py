42aaf3ba53e79b9b3d09af613a92c71f555f6745
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:30:46 +0000

    last around return path murder

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
