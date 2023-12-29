2a348a73d9a337b2a25103e5164e4630285ee3f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:13:03 +0000

    pour runout gap soon issue

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def subtract_numbers(x, y):
+        return x - y
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
