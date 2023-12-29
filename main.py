e9f032cb5a23f1c32e57f7920c988191cf8a1758
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:08:40 +0000

    can wise union area discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
