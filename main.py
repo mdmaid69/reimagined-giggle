e35a0267588e36f76eadfb8bc0c9a2e2beca237c
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:20:38 +0000

    comefrom cum powerful care friendly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
