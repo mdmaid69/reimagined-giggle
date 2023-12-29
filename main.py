a779e14dc6d8cc0050bceb2e9d154fd45c3ea56d
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:42:31 +0000

    shoot opposite afford depend regular

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
