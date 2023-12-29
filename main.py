0d93321d5724b6f06b45e74334dbffd5841def8f
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:50:02 +0000

    part taste night dream conduct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
