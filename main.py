29833483096cb2b1bcb7abeeef7b135477cb9364
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:21:17 +0000

    drop tread drink precise square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
