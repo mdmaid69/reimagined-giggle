8263f16a5aaffd30a005dea2fa1222f1264af510
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:34:14 +0000

    brush grateful steer production there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
