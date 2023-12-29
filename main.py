df4275da067eb1b289d08ad9d4a608f94cdf1209
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:31:04 +0000

    between along plan slow skin

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
