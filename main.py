7e9c997d73c11e1da44055f072b8e27813166f1c
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:30:02 +0000

    win evidence spread turnoff fortunate

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print([x**2 for x in range(10)])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def add_numbers(x, y):
+        return x + y
