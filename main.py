4fed2dba05f83d301b4ff506f4b7503eefd066ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:25:01 +0000

    thought negative comeround 566end unlikely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_sign(x):
+        return math.copysign(1, x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
