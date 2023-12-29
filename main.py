95dae82f609353374cd1c2b44ef251a5028ff2c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:24:58 +0000

    physical accommodation group swim react

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
