bb0a249c5f393e946d0ede5f8ac7956ad2aa6795
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:57:12 +0000

    window know worry mine clue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def sort_list(lst):
+        return sorted(lst)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
