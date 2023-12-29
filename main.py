7e1cfd434615287b510ab437374bccc04325ca8f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:48:37 +0000

    ear physical height makeinto family

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def sort_numbers(numbers):
+        return sorted(numbers)
