066e8a33029e33dc8941522d21bcb9944fa343c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:35:00 +0000

    left problem figure mixture embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
