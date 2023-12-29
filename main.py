8e13b54351525ee33d2b0bb66ef1e6b9612b2e03
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:10:08 +0000

    collect print confusing beginning relative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
