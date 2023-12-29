ebc1ee4c6834da7caa29d3c703d81bb76cf8bee4
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:10:13 +0000

    still lead comfort face petrol

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
