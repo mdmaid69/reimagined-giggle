80f9bf5eb67a2066c2f4dfe002066b9b300f408e
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:12:12 +0000

    visit safe confused blame well

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_speed(distance, time):
+        return distance / time
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
