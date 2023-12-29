9d5b4af7fa971effe8ed0fe0bd60c19ab402e511
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:13:25 +0000

    shallow pleasant welcome bet question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
