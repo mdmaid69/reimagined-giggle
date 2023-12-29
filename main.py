4cfabf160938a9a293e2936194914df2ee026ce0
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:37:51 +0000

    Mm wire store wave brown

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_power(work, time):
+        return work / time
+import time
+print(time.time())
