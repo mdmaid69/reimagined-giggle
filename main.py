1ec32b7972749df785e055f9357abd21ddb2d411
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:54:52 +0000

    admire pool smell whistle sentence

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def get_array_as_set(array):
-        return set(array)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
