2efea4138867f486925774446b5cd6ef10510511
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:26:03 +0000

    while turnup lunch through situation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
