7736e84e5037aaf2efc2eea2ba7afa693f7f3d5a
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:43:49 +0000

    tight copy conclusion apologizeapologise and

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_unique_words(sentence):
+        return set(sentence.split())
