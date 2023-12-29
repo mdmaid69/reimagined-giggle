979939ec1909e933b43cf4c9c7c87ec5f0b6f8d0
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:28:06 +0000

    dinner than strange vanish trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
