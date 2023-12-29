e9c445b1fcc31de7a91fb8dbc55e05c5fd073262
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:12:55 +0000

    behave label opponent move tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def count_words(sentence):
-        return len(sentence.split())
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
