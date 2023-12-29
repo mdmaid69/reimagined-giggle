6b30fc4f70423669e7acff0649de854b43dee409
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:06:40 +0000

    this dance rent official rough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  def multiply_numbers(x, y):
-        return x * y
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import os
+def change_working_directory(path):
+        os.chdir(path)
