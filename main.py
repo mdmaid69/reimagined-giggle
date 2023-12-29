9eb8678f7538eac1bf7eb2f90d2fdc4c6d7edc91
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:42:54 +0000

    hard gradually noone agree tip

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
 import array
-def convert_unicode_to_array(unicode, typecode):
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_from_bytes(bytes, typecode):
         a = array.array(typecode)
-        a.fromunicode(unicode)
+        a.frombytes(bytes)
         return a
