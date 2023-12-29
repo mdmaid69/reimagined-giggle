7dbf564cfc0161db0ffd98ae9f093676fd991c14
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:25:38 +0000

    scream demand complain interview travel

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
