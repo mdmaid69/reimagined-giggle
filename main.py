3aa704bde55af4abd6e7a1334416e76633e4654d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:56:45 +0000

    smoke getup sad could table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
