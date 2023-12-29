72fb081f479c4b1e0ee89cf9144eef7d902df9e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:01:05 +0000

    cheek tip standard evidence neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import collections
+def create_user_string():
+        return collections.UserString()
