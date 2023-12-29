5c04cd0f70a614d9058707299d2b03f948d3a756
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:28:05 +0000

    especially confuse attach each complete

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
