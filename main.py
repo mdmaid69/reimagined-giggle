9a55a35712fd772414ec23d7d357b37b9d770ab9
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:19:22 +0000

    prevent mysterious alone damp cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
