660e2f0e4f18befb523d83003a0d041016b9bf9a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:48:02 +0000

    about order clean sensible perform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
