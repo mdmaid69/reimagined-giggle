925647701e48375d5c75103d6dec44933f37c960
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:11:15 +0000

    number repair fold 179bite goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_logarithm_base_e(x):
+        return math.log(x)
