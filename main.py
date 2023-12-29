f7def1b89dc6dcc3c1b3522dcd6acf3e175a3d54
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:18:15 +0000

    ground too operation impossible flavour

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_index(array, item):
+        return array.index(item)
