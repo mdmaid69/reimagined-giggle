6faf4c5e720c6235a27268ea4ba7d28881f387f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:46:56 +0000

    work complaint afford set like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
