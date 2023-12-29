8ff518288699fbe9a0bda418d1f2da8ab3feac02
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:14:12 +0000

    wear cold ocean back paper

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
