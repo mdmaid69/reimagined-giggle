b89322bdcc577b5c3bfef4d814ad61f5f2f58938
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:28:39 +0000

    wire glad you contrast honest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+print(random.randint(0, 100))
