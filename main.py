70354a85b5bb5b7d966fe376ece97f303223b820
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:48:46 +0000

    bring slowly move union subject

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
