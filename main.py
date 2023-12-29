f993df60e594ac2e5744f9e38f3d4319bbb9a70a
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:00:08 +0000

    punish active worried ball people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
