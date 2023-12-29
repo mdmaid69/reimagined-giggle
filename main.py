560fcecb94fe6bd44d5429e49a167281f04f9841
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:46:29 +0000

    brief seem fair below instrument

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
