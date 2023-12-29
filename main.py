8876dc79e1bd66a6d2a5bc75cd492528529bf4ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:47:43 +0000

    radio shade hour scene copy

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
