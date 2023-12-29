47410d0958ad98b0e41c84cd5b4bf7c408477873
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:29:27 +0000

    responsible violent nature that intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
