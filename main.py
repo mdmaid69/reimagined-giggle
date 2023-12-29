2c6e2e2b863bc0c134d5706bd62f75af6cb82ca9
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:48:18 +0000

    also copy phone treat fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
