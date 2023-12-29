b56a5c796d8d206d33daaef9a18d7f52e09796e1
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:50:31 +0000

    very your forgive everybody rich

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
