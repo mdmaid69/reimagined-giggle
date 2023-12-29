4e9b4d920fb085cf3fe70edde0fc0fbf0004a557
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:14:21 +0000

    rescue government history region natural

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
