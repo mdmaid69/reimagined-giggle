22670b9e29488a61db6a56071d8c02eb3fbca8f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:39:50 +0000

    interview grand frequent uniform weak

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_work(force, distance):
+        return force * distance
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
