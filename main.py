a8030de12c03a69171d3676cd7e0decefa9ea065
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:50:27 +0000

    scene see settle school spread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+from collections import Counter
+print(Counter("hello world"))
