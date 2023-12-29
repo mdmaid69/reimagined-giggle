1dcd76a12186e47d12ba761a03fb918df6302baf
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:00:34 +0000

    extra side steal impressed fence

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
