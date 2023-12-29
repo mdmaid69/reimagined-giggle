dab1306dae11e3f29ccfaf53b37fb3d5fd031c35
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:09:50 +0000

    education check whisper practice visit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
