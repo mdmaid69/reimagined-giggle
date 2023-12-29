8dded9f6dd6e4a8a11d4dc733162c233f999ee77
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:52:33 +0000

    promise profit comeover interview fault

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
