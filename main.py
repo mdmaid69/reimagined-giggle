b2d33f533ccd6893a14ee63f56aa4be59c74202f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:37:08 +0000

    crowd chief offend blow excited

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  def sort_list(lst):
-        return sorted(lst)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
