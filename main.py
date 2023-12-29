63d488451241a0f7347ab2237ccbefa83ba2a65d
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:48:16 +0000

    drop concentrate Kk under fair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
