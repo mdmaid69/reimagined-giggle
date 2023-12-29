d3a15f68fed6396512d628f29634f5f6a09c31cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:00:45 +0000

    or habit happy consist joke

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
