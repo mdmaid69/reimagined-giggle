1cacb36cb7652de936fb8f5ef32623e7754a1f0e
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:03:51 +0000

    introduction achieve influence human separate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_speed(distance, time):
+        return distance / time
