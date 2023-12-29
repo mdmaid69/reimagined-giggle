21c3a6adc209545c1548d58ae08059171fcccc0a
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:41:12 +0000

    mistake far shoot spin mess

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import sys
+def exit_program():
+        sys.exit()
