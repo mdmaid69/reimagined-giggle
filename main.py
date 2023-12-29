ec0d14bb92552f2459ea37ac80af859f4581c50e
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:54:29 +0000

    squash minor basically much rent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def subtract_numbers(x, y):
+        return x - y
