085e2843156e253ee989512a38d9642d192e12f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:18:09 +0000

    control wind cope concentrate peace

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
