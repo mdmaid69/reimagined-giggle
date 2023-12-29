fececad16eddc96abd06f000a92d7f0aae007e2d
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:36:55 +0000

    type invitation scrap ability bear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_current_working_directory():
+        return os.getcwd()
