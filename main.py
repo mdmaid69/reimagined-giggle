29f8a8a6feec89b88d4a1b95668bf250f0273d9d
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:29:16 +0000

    nowhere warn chase comefrom sound

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import os
-print(os.getcwd())
+def square_number(x):
+        return x**2
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
