203b6b2896fad95cebb9a99eeb6de1fcdd3d7744
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:48:47 +0000

    age plate hard that organize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
