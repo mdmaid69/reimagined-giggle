7c3dfa63001eb607f78182d512613deb4a81e195
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:40:02 +0000

    surface choose persuade hope experience

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
