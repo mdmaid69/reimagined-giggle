39504a92ba582940a91b1de0e0c03439b1569fed
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:00:33 +0000

    autumn slight tread second group

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
