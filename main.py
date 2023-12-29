a545508c472a3a8bc217b10522f215a5581df532
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:38:41 +0000

    lucky air jump usual past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def convert_to_octal(n):
+        return oct(n)
