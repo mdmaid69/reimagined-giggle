bc2aab2e20c96f43097c5fdb882788b7defcfe10
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:27:40 +0000

    none tend pity niggaacademy open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
