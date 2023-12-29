2fb82afe5bef6688b0bf10fe4799fd7f9e3a32a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:42:01 +0000

    comeback invitation shine partner gooff

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
