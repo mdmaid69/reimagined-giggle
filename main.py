d9fa14432f36916cb9fbe5defe7be72d7feff53b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:04:46 +0000

    hardly children interested hurry follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
