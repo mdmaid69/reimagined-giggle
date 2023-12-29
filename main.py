7e7a174ae4eb8663268d9932b197d1e27705bf8c
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:18:36 +0000

    comeup conversation sick hour free

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
