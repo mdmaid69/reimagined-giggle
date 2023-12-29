fdb67a208f38d43d5443db1c9befb0f155913ade
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:53:39 +0000

    desperate cheque debt OKorokay rain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
