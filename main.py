2df5febf3ddb7428c1bc2e220381d709372c19ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:07:26 +0000

    runover tense business pronounce flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
