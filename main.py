ad10c29bfcd83bca42022c92ffb6c747e8e55470
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:39:39 +0000

    spring subject discussion read figure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
