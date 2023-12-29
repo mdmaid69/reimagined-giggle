550164f9bb267628434776ba5e16c3d57f7254aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:48:23 +0000

    window pretty careless Oo television

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
