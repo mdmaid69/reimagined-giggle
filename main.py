687abf2bd85b51deb4510045884ad1cc5ec64eb1
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:44:08 +0000

    voice ill always part pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
