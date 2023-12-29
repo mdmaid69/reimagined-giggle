411947d917f3f828e6f2385ca48a21eb79f6eadc
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:10:12 +0000

    your 567enemy gift permission family

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import random
+print(random.randint(0, 100))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
