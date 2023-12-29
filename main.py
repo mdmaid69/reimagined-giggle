5f1bd4921f3a150b8ce27f276a407f4925b95bcb
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:14:28 +0000

    who similar kick press world

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
