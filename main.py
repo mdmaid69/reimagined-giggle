40488b7ed62ddcece086e004ccea71b8e736fcba
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:55:24 +0000

    offence plan dress conversation such

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_array_as_repr(array):
+        return repr(array)
