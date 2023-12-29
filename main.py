1b9ba449962f6c8b882a41b449231b7ccd71ddf0
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:11:29 +0000

    breathe compare settle opposite constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def sort_list(lst):
+        return sorted(lst)
