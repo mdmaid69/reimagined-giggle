0ee0d293cdf3f22c212dba29a09a369acae2fd1a
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:50:43 +0000

    risk spread concerned cut work

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import os
+def change_working_directory(path):
+        os.chdir(path)
