17abdf394d2b671041c874ffdb2119d6f1555e7c
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:09:08 +0000

    grateful matter flood tell certain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_density(mass, volume):
+        return mass / volume
