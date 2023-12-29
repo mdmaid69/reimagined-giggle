26f5db0fbb5a7105d316dd4d01ae0e7ba5b5368f
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:47:12 +0000

    room lay snow recent guilty

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
