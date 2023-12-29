b3305e601be18c1da3ace832ef14d4bef992f1d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:49:19 +0000

    father rescue comefrom fast himself

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import socket
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+{body}")
+import json
+print(json.dumps({"name": "John", "age": 30}))
