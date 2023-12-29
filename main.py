b12b94cd54772d126f21c79833c14a1b92364121
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:25:32 +0000

    play stamp part him before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def get_array_typecode(array):
-        return array.typecode
+def reverse_array(array):
+        array.reverse()
+import random
+print(random.randint(0, 100))
