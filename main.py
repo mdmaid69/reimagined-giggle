2e1854918c7356772f3e684b3f1cd1f6b3288d2e
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:36:49 +0000

    forget dick escape remain suggestion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def count_characters(sentence):
+        return len(sentence)
