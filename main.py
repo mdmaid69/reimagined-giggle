69a353b1b5ef4c0cb7f54bc91e70466de54a2e3f
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:17:34 +0000

    even sky funny arrive objection

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import sys
-def exit_program():
-        sys.exit()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def change_working_directory(path):
+        os.chdir(path)
