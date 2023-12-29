c50c9f7c06c4aa93d3c0e36e303fdf8d89ef5298
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:41:03 +0000

    rubbish practice sink patience independent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
