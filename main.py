3f7274c18b562d47675122a1d205821e8fb20725
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:43:27 +0000

    damage neither birthday this slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_directory_name(path):
+        return os.path.dirname(path)
+text = "Hello, world!"
+print("Characters:", len(text))
