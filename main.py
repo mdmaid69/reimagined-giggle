0cd0dba80b9586dbb5689a7b4780a8b05de3ed79
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:09:17 +0000

    destroy autumn rough tone consist

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
