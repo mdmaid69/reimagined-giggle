109010ddfb7cbf3dc99cf026dec5dc7f30a3354c
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:56:26 +0000

    willing reasonable correct view fear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
