2aafdcd11e019c89cc064b5cbe1fd14a6a7e3325
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:41:04 +0000

    lookup board suffer mass card

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
