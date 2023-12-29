2d08587c67bd41c680f420afb3e3142ac0c32c4e
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:39:32 +0000

    issue rest smell share label

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_speed(distance, time):
+        return distance / time
