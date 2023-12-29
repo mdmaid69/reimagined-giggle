8bcc95aae16871c95fc86a68cda6784b5833418d
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:21:14 +0000

    otherwise store turnoff charge callback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
