37a9422b334888279467125f4875b51b01981140
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:31:35 +0000

    into drawing steep glad travel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
