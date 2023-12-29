c687abc061c493a75366cfd6d5aeb83a0cd8255e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:42:22 +0000

    slide 564encourage let search sorry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
