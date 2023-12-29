e2938fd8d684342be51b565d3f72bd134227bd13
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:20:41 +0000

    travel dance birthday nose answer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
