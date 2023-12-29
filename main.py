2388dad64e54b6226b5fcf6dbfe24c75b69e9574
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:45:47 +0000

    holdout cheap maintain room material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
