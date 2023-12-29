39ec721a180e75a7e74cec45b7417eaf02f0adde
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:34:48 +0000

    regular vanish equal mind successful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
