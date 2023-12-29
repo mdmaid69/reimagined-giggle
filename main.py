6139f8ff262e920ba2039e001acdd276141bfc09
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:31:19 +0000

    less exchange damp run set

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
