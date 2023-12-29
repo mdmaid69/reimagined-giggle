66dca337001687624929148a330983be17b699f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:35:47 +0000

    choose responsibility shape quit sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
