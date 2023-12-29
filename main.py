603bba62b43a4d58ebc6af68589ffc0890ecbef7
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:55:30 +0000

    arrange callout share charge occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
