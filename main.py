91e075c8370a618c1b90775644dbd0237dfe597d
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:29:29 +0000

    inside comeround effort imagination up

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def count_elements(lst):
+        return len(lst)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
