d7b27de27a73f6c56c089879074443ccf3c74f63
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:39:20 +0000

    pool smell hear key accept

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+print(random.randint(0, 100))
