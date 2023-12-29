0460ca3436f98aec74b97816f088baf43f2192a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:36:37 +0000

    safe knock return centimetre exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_current_directory():
+        return os.getcwd()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
