6f3bcf5b940d59f407edcea4f05e12c75bd51c2a
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:15:13 +0000

    centimetre giveup on car indeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
