71f21bd3a192f06dbc1affc8e5bfce0fa4f08485
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:36:30 +0000

    familiar limit together limit partner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
