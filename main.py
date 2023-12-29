9a2494693cfa2c4d850fd42a4644a19178e9fdc7
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:45:39 +0000

    proper last scene practise group

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import collections
-def create_queue():
-        return collections.deque()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
