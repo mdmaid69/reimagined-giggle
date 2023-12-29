169ead8b7c509b58b6d245ead4180f3164e97609
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:23:29 +0000

    strict healthy march power successful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
