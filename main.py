2466ca25a4ef111b4d1c67f79fa62a77999eeca7
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:22:02 +0000

    thought near limit confuse flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def remove_duplicates(lst):
+        return list(set(lst))
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_as_memoryview(array):
+        return memoryview(array)
