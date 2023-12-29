8b582b1a8219aa01dfcc357a4eb7d2c07d834ca8
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:53:41 +0000

    scream manner eager statement shape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
