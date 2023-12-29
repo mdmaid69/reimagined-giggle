a37764fad4d659d76370a1269e55082582a4bf12
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:33:39 +0000

    what part experience plenty fair

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def square_number(x):
+        return x**2
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
