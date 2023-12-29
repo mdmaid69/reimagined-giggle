25b26c0d3e8cfe79f0840873b105b8737bdd4ec5
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:25:36 +0000

    which normal window cut forgive

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def clear_array(array):
-        array *= 0
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def find_max(lst):
+        return max(lst)
