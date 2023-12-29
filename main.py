8e0e2de81d478159e39b84a0ff253364f85cd09e
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:19:11 +0000

    home butter heart ball getup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
