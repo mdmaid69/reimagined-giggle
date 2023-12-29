09cafbb66523c9676423fa2fa5b2bc952b68fa40
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:59:27 +0000

    comeoff stone spite forever beginning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   def find_min(lst):
         return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
