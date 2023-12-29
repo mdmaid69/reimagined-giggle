798c00e85fe135a593e1b040a389a365e41a68b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:34:31 +0000

    Nn drop under goaway comeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def divide_numbers(x, y):
+        return x / y
