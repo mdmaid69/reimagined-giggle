d5d3c6122f634b20104986cf27da9393f672e18b
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:14:01 +0000

    complaint pocket exam not clear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+print(os.getcwd())
+def subtract_numbers(x, y):
+        return x - y
