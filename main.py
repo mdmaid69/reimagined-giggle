4e467842f612887ccdabc08f992e61b4c00f620a
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:19:08 +0000

    movement decision reserve difficulty feature

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
