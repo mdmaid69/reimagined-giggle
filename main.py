ce71ea22c615a8b258343906d0d67f932b87c139
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:42:20 +0000

    score breath soon clear interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
