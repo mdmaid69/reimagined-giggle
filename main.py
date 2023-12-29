bff2f7e5905fa358b97633d7eb1731d1a8259f4c
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:14:33 +0000

    swallow regularly whom ahead Ss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 def calculate_acceleration(speed, time):
         return speed / time
