e7e51a15e2c46426111f472aa1d9246aa26cc2c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:59:05 +0000

    refer together spare free community

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
