bec88edc4b602cdf1ab5dc2203dcedbc8dfe9c01
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:59:47 +0000

    pause would system comeoff spend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_power(work, time):
-        return work / time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
