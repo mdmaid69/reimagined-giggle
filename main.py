7ee2214dda1bd57a620a6b0ef2b3562bc87a0976
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:16:39 +0000

    under information Ii spread book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
