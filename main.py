58e88cc3218ee5339de91cd59fb49e371522bc85
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:47:23 +0000

    replace prepare Ww hang lookaround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_speed(distance, time):
+        return distance / time
