35b39f3541cc4672f00d5b40cbbfb1cdc25ad889
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:06:01 +0000

    nigga certainly fence around cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
