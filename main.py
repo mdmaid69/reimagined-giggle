0297bd4037ace5e51de5929e297154a4e365c9fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:33:05 +0000

    sister ago natural natural dress

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def square_number(x):
+        return x**2
+import datetime
+print(datetime.datetime.now())
