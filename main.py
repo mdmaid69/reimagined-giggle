64e19293a1cef27d0a85ae6a909cca884eb1a8fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:18:20 +0000

    foreign apparently learn centimetre central

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def reverse_array(array):
+        array.reverse()
+def add_numbers(a, b):
+        return a + b
