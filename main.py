cedbf373ae7436560165d19b90879b7a4e91a3bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:56:29 +0000

    die difference left fortunately direction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
