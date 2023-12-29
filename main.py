0aa86e1b351f426be734cff0c230348409291e34
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:00:33 +0000

    confuse protest funny quantity your

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
