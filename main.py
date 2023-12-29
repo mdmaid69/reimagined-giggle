ebd8a851a76d28fa0f6d668a7fa6ef2e49f21193
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:47:50 +0000

    woman phone anywhere nervous drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
