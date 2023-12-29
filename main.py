a5f1decdefd5f1ac27b400f8613eeb5e6c9b0963
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:20:40 +0000

    interesting world recently both head

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
