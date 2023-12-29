46cf522ad6abe7a0c802a85947f666affeccbb02
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:07:24 +0000

    doctor pattern favourite earn ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def count_characters(sentence):
+        return len(sentence)
