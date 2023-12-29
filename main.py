bb9593a5a0edf4a8c50749bdac735d99a9b56d39
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:11:27 +0000

    history dance change beyond interrupt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
