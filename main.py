8bbb3163bae2509a5bc5193957d75669333a6201
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:10:12 +0000

    lookback(on) pinch regional satisfied act

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
