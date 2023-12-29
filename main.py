2db043c1f2e7bdd06c169be67db3a1435a5d34f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:38:46 +0000

    wash aim knock that attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import array
+def reverse_array(array):
+        array.reverse()
 import array
 def clear_array(array):
         array *= 0
