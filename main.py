26ef940c5c564689fbf3b3984402de1042e331a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:31:07 +0000

    treat whatever clue cap card

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
