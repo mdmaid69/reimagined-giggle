75e0580f11256af1ddc8b6d54ea18d9a9ce28d19
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:16:45 +0000

    knowledge turnup offence neither content

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
