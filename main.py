abbc87395716a343c3201e61015b1eba3580d139
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:13:36 +0000

    wherever rich fly dream bus

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
