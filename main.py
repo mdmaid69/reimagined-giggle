7f26c078d8a4211ec3cd86e22c28c11e70dddcf5
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:25:46 +0000

    careful busy fun mess convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
