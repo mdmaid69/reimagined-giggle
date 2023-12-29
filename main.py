f08e950e20aebe5a5cc8c8068b4f846700c3639e
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:21:48 +0000

    lie advise exact add wherever

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def square_number(x):
+        return x**2
+def subtract_numbers(x, y):
+        return x - y
