c2910b88634a8422e5bb5c5151b2b971556ed064
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:55:36 +0000

    platform protest gas mess telephone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
