e9ef3336c751b06a294b2a211d0fb1157ceec392
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:01:57 +0000

    rate turnon Ll young dream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
