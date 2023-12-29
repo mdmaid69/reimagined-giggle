ec98e1038ac637909c1a5afccc15f121fb60e833
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:14:26 +0000

    sensible breathe miss transparent peace

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
