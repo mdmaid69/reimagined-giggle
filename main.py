871a0cc4015b04c3f7b12b6ec22ffc98659d62dd
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:18:41 +0000

    very fight pass summer comeup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
