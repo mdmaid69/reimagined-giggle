fb5edcb2f5b36422576261ba106de6ec329670f3
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:09:30 +0000

    contents equal easy basically impressed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
