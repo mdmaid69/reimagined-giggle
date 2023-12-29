702d594805d3b0f255903b0bf71ae5d301c9ad18
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:20:54 +0000

    think grow probably bring birthday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
