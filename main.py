7d9c80bcb1f582e110ea601ecf4cb3aaa7c72748
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:31:33 +0000

    breakinto address claim standard pump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
