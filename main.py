5775fd6abb688393e5f6d1753358289251f830ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:06:46 +0000

    mainly ear miss steam lay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
