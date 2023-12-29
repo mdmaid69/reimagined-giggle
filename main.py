1b6d77572e7ac10b2fd259316f27675c67087bb8
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:59:19 +0000

    expect far discussion case win

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
