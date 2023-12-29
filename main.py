6ccd48374a7eba6244a45a3ccd3392280b2a26ec
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:12:21 +0000

    square to right generous gentle

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def reverse_list(lst):
-        return lst[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
