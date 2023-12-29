d46adadab1b651e6c62950009dc08f724412bd39
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:52:39 +0000

    exhibition with wear use doubt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
