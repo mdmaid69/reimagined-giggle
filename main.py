10736277e2d95a224603c15c34cb7c7ba66fb81c
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:33:15 +0000

    kill grateful unusual secret comeover

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
