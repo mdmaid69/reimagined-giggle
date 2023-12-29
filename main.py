17110c0ed267e4c892cade4b47b92ba5b9b86140
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:31:56 +0000

    takeover sleep affect bush takeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
