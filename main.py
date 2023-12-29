615d97c2929297b256b1ca333cf803738e43a522
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:08:22 +0000

    or change around meal increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
