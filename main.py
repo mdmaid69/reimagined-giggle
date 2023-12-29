9e4f6cfcc56ae568b1a542565884401d0730f3ca
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:48:58 +0000

    region considering neck people confuse

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def is_odd(n):
+        return n % 2 != 0
+name = "Python"
+print("Hello,", name)
