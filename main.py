8a3dbef48a823a4eb8c2155de148abbd4d0bb6ee
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:20:06 +0000

    doubt deal heart on contribute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import tensorflow as tf
+print(tf.__version__)
