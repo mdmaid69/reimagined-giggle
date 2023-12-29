d06b59450dd181daff53f5c7c2841d94d15cd015
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:06:08 +0000

    future state trade neither opportunity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import sys
+def exit_program():
+        sys.exit()
