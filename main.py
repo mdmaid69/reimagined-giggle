7877acd6e990ea5195a0f586eb43428b9bebc944
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:05:40 +0000

    sort duty much closed comedown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
