725415ccbb9da37f7e299282ff30fa2ce24027ec
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:36:48 +0000

    part fit scheme sleep Ll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
