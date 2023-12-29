2de82bba13dc28f5bb64a53e6a0ebb078677097f
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:57:16 +0000

    connect authority reach cheat along

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def count_elements(lst):
+        return len(lst)
