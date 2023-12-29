7c0ee64aca5ac91e358c955d72690d87b2fae50c
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:57:51 +0000

    plate aware land birth half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
