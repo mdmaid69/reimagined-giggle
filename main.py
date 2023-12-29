0ad8e18ac0bffd88e06470f7363d5da5e58b8420
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:46:08 +0000

    also receive thankyou border hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def remove_duplicates(lst):
+        return list(set(lst))
