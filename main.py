e6abfb384c08075083553ccbe9d14196ca41140b
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:03:26 +0000

    family language snow fail satisfied

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_typecode(array):
+        return array.typecode
