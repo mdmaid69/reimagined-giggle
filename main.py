f736600883e79c49f320e2d1596a9f28f9b54308
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:24:12 +0000

    stranger row change nor professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
