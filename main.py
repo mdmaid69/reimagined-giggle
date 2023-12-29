636d8342a4e312c26362126ec9b34376e5cf40e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:52:39 +0000

    doctor before alone manage weak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def multiply_numbers(x, y):
-        return x * y
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
