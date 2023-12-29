3574b0603c3a77abb90c73cd7ca6c555c942ee3f
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:15:24 +0000

    vision harm money shout past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_length(array):
+        return len(array)
