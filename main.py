58b4762cc4253385faba27a243b62be1219df147
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:31:30 +0000

    address petrol complain immediately basic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_acceleration(speed, time):
+        return speed / time
