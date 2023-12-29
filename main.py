2b88ca010e31eb4ded12852ca1517c63273f95d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:23:18 +0000

    hurry deserve record frighten immediately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
