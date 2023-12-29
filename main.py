3e28e413f33b4f254fcc39bbe534cbb74315b1b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:38:42 +0000

    hair accordingto Ll whistle protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+text = "Hello, world!"
+print("Reversed:", text[::-1])
