1bf972770c5dfcdb0bbbe2e12273f55fffebe0ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:46:08 +0000

    computer where relative only combination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  def add_numbers(x, y):
+        return x + y
