723d182def6f51d86507977d7d1c3d4e74d9a2f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:19:59 +0000

    everything turninto form join recommend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
