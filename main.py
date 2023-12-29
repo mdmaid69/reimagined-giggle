d0bdd290fe93e0fe7299b27a7fb6e0b4de8f969a
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:54:40 +0000

    reply contribute mouth holdon breakin

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def calculate_volume(length, width, height):
+        return length * width * height
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
