096f7b25e45f9ae602dacf1b86456181d3ed9386
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:47:25 +0000

    comfort image simple test from

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
