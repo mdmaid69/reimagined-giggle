9c2efd024c9235dcfb0cf702f6fced2cb763c527
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:18:05 +0000

    Jj cruel else contact custom

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
