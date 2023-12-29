2a85f6199627dc1a9f78a3400ddaf7fd8b0a2865
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:24:57 +0000

    affect cold characteristic rough label

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+name = "Python"
+print("Hello,", name)
