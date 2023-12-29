3f43a21c15b48948a99cc81b68228ae905365a74
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:07:58 +0000

    much once who fast flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
