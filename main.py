67dbed6980cab295343e049bf9a0fb3be23fe504
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:43:11 +0000

    information statement snow new calm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
