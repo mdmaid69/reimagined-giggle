19c1cb9b3f231e5699c6fc8f12c488ffe2f70445
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:03:16 +0000

    cruelty choice memory improvement cough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import collections
+def create_priority_queue():
+        return collections.deque()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
