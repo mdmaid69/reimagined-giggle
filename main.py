b46525a1275c6fa84a21d1e7aacbc8a3a40b3d47
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:26:31 +0000

    callback safe roll search plastic

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
