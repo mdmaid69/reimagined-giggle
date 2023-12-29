dd125c8a3ff8b07dd3ad1394b1b4e5c168fb4319
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:42:47 +0000

    build pattern surprised lucky born

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
