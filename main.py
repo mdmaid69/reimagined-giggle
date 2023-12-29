96e475b246baee7540c6de45e7e456bb6724e206
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:09:00 +0000

    ago negative even fast decide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
