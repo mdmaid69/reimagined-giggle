31e08a18a15f1c5ee8f21d0ad6464783836edc58
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:22:24 +0000

    feature later course strike aware

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_max(lst):
+        return max(lst)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
