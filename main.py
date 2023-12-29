1593a289179873d3e23e264d542d671ce871ec38
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:40:59 +0000

    sight 179bite mend touch strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import re
+print(re.match("h.*o", "hello world"))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
