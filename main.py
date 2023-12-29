aa08c6f3f3f0c4bec9a231b60f0d60325e3318d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:02:41 +0000

    crash to release excuse I

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
