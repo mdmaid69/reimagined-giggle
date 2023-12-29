5e7d26eea65e277e092fa7c60ec46e1f93a0b3fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:28:30 +0000

    attention band cough comfortable benefit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
