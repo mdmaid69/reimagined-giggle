5a46979b0b34e12d48e6edfa1a297f9715ea6088
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:47:24 +0000

    drive spring shine sudden common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  def multiply_numbers(x, y):
+        return x * y
