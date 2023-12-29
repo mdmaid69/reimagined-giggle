86565f929c00a9671175c405fbc4c223ccd6e514
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:17:19 +0000

    thin thank comefrom makeinto earn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import platform
-def get_python_version():
-        return platform.python_version()
+  def is_even(n):
+        return n % 2 == 0
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
