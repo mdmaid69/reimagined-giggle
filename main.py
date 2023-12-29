0105503ced9f0314abeca467a836ca57b55e3fcc
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:17:22 +0000

    rough please under holdon love

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
