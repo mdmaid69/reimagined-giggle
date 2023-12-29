b35eaae00c080cd9b975b07e4a0b43b634a38761
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:26:45 +0000

    how final about although want

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
