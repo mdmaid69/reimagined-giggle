245cb349f3b98b859c2be4da86ccd95946f1389f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:02:35 +0000

    large myself poison tune trousers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
