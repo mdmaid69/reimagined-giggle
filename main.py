06acdc8ac4aab0f113bb183190a9cea44d0c461d
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:37:14 +0000

    beat upset transport scrape century

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
