a3cf52245f7e4d31c37cefbef45f1e9ee47db56c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:26:28 +0000

    scale bridge press train powerful

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
