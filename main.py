cb0dc6bc65f992153ae513b014f3446a2221cfff
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:54:10 +0000

    comfortable comeback flight rise please

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
