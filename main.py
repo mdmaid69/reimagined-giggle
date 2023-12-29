452f0b97a8f6b4787f69fb252ffc22ba17997a26
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:03:44 +0000

    respect dream priority trip anxious

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+for i in range(10): print(i)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
