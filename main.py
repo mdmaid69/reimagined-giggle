f6f04a2ce31357865c8cd2d42ca1945f5dc5be8c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:56:13 +0000

    burst lot earth regional equipment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
