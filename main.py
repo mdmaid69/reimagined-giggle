97cc425a4141b9c8dd7320409a132d5b3fcfd755
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:00:49 +0000

    soil clean owe hole post

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def square_number(x):
-        return x**2
+print("Hello, world!")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
