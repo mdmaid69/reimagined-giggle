e473225de7b291833aea6589cee41f7ec2440658
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:56:32 +0000

    pleased like goup dig ordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
