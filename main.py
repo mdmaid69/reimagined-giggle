339399746c978ce0a6882cc6a9ee6258d4361c0c
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:49:36 +0000

    especially takeoff another contrast tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_max(numbers):
+        return max(numbers)
