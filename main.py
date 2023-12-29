9b46117ff85fd8cdb892ac288d40c6b7a538d9b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:19:57 +0000

    wind bother trial if available

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
