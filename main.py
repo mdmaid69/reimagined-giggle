3515bab83a592692c5d1d1974b4b2b4132819962
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:55:42 +0000

    sense rain like pride fly

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def subtract_numbers(x, y):
-        return x - y
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
