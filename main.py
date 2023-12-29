c9144caf1046d99025a573a5dbd3b3a6c0029f54
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:26:26 +0000

    order cool immediate pity approval

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
