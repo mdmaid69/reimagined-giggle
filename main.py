00ecb613bff33ad24267f5790092ba479d9f93d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:28:59 +0000

    cost narrow sail draw wish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def reverse_string(s):
+        return s[::-1]
