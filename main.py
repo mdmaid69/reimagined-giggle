022000cc5b9c41046feb18e7d624ec2ce34eeae4
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:08:43 +0000

    to vote giveback thinkof Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
