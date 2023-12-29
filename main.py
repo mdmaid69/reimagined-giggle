fec637fa22f481788fc046f2004852afa15db678
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:38:33 +0000

    contribute race roof perhaps mind

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
