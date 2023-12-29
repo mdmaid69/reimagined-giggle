086a49bc9585c380bbac4bdd4f5f25b602c33f4c
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:16:07 +0000

    gooff exercise pull neck peace

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
