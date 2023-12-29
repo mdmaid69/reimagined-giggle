26873e98c7e450a1f88a928f5e405cc4a8351f7a
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:55:12 +0000

    slightly himself reaction design really

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_sign(x):
+        return math.copysign(1, x)
