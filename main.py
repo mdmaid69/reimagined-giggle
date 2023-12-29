39b9f0331590a124f6daf14e8a8ea2f5341ad2ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:19:05 +0000

    crowd delight warn beat regularly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
