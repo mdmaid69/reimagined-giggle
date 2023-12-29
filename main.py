62df945189dc72215185dab46be808ec9a632288
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:58:00 +0000

    guide press once forward them

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+text = "Hello, world!"
+print("Words:", len(text.split()))
