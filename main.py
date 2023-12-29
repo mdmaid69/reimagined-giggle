1bfbc05bb159d9ccc055fcdb3c49f5292b2f1048
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:22:14 +0000

    post tip content bother event

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_current_directory():
+        return os.getcwd()
