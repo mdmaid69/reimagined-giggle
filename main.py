0d2e94eb4f2e38334fa21eb81573729962c607f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:54:15 +0000

    circumstance fly remember bound some

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
