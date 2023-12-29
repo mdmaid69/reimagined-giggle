d47a7b85ce03d6bb268e5aeb4628d73c7eeb7706
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:49:55 +0000

    worst shade except except question

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import os
-print(os.getcwd())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def reverse_list(lst):
+        return lst[::-1]
