28be6fd7962cf03608de26a363a431af9e7fabc7
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:16:23 +0000

    lie embarrassed quiet lift sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
