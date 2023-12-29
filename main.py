a7b5b101d8e0e1ead5ec2868ef4bc10509e71f91
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:50:32 +0000

    question or obvious confuse criticism

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_list(array):
+        return list(array)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
