1bd324fc290a1d5e28dcb4cb7b483ac3332ec22a
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:04:31 +0000

    keepoff shock total Ss crazy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
