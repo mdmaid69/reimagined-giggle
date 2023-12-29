a4a0428fef5e8d549e75c57c3332ba26edd9a412
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:04:00 +0000

    high watch undo snow character

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Uppercase:", text.upper())
