61e55778a377081fcfdaff8ebfefd7ca6950d69c
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:59:01 +0000

    control ground get artificial work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def convert_to_binary(n):
+        return bin(n)
