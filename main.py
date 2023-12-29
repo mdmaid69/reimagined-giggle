c90101eeaa4a0327afe8b268c83420b460673526
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:26:28 +0000

    strike bother fear properly relationship

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_str(array):
+        return str(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
