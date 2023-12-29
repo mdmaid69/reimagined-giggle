af4296e9d101a2d70fc0c00dbccaebd9a4b8f69b
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:55:52 +0000

    children evening rest tour storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
