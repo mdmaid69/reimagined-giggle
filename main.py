96aaa7cb2e5640df9ae2a054dfd14be1b2ca1cff
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:46:29 +0000

    estimate wonder ignore rarely middle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
