a5863078d9ee9cf510c291da465cfdac5f4a9fcf
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:03:37 +0000

    television dead completely Itself punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
