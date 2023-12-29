98404b55eeff0861966d32dce4d9fa607fd1deb7
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:00:01 +0000

    skirt pupil minute survive boring

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
