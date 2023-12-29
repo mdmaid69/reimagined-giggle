ba26ac7914f54d3230f64e797bb6f41648acd782
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:20:43 +0000

    lean characteristic condition story badly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
