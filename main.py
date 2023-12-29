9660d4af8530b0565464e9dd6686afd98201c6f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:43:26 +0000

    poor mention activity even bar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
