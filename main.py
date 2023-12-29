27fbb6893fc8ff374dacc13681fd37799fe5dad4
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:35:27 +0000

    pretend region cough refuse apply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import re
+print(re.match("h.*o", "hello world"))
