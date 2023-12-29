4bcb691b8c551329383bfc9b4bac5489cd27fa33
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:08:20 +0000

    poor entry win area friendly

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def get_current_time():
+        return time.ctime()
