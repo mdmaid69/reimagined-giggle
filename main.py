e63a9409d19f788386cc944137b6836f9555d07d
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:17:08 +0000

    long tragedy ask fly pronounce

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def remove_duplicates(lst):
+        return list(set(lst))
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
