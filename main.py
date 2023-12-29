44a4af4e1c88af635a8b2d4f8a14a665f40745cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:24:46 +0000

    peace whole exact exaggerate beautiful

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
