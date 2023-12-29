fade1e8ea5c655e797207370952e104d4b4391d7
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:42:56 +0000

    bad stir friendly glad towards

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def reverse_list(lst):
+        return lst[::-1]
