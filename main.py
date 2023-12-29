1a6c77fabaa4a3ed076e80a88e256ebc43a9bacc
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:10:01 +0000

    please adjective convenient point second

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
