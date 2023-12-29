334dabb2616a1034b4d138c98edd4edbf07970f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:51:37 +0000

    proper place Bb issue scrap

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
