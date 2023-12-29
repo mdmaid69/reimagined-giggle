3d469e0b276133fbd5c89e9b6c789f8432565cc8
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:42:42 +0000

    official lean fly tragedy mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
