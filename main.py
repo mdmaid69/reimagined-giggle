6f209a1ddf958a740b0dd59f36e13c44727763c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:53:33 +0000

    block success clean change dead

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import time
-print(time.time())
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
