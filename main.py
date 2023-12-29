8be1612f2ba1ab1b8da635cfeed78a01a974ed84
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:59:30 +0000

    base ring hold round no

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def add_numbers(a, b):
+        return a + b
