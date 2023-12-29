8587ccdb3a7c671a00af7b7435ff401746eb74b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:24:01 +0000

    slide adjective turndown fortunately which

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
