378fbf6a0b9706e13e505d8fd999a71b9e9318fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:09:08 +0000

    coffee steep call lot affect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
