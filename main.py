13ccb96a7fa6d3f6e7d24d383a193dc49a72469e
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:47:03 +0000

    brush forward power sound forward

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import time
+print(time.time())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
