74591713e9c4d4b706f299c03f16ffff6b52b6d6
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:24:39 +0000

    legal whistle wide 570enjoyable somewhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
