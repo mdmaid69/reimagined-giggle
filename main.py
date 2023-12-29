1c04bfff577abcbbe7229aadc3d1bf93d43368b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:31:26 +0000

    except occur further add upstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
