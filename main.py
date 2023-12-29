4c9aa0bd0bc7e005455508afedc29a18668ae04c
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:07:59 +0000

    onto transparent her shout top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def sort_numbers(numbers):
+        return sorted(numbers)
