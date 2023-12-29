44bf81be7c2ca252b6e65c0e73d69a423594f937
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:53:11 +0000

    curious life nerve anything precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-for i in range(5):
-        print(i)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
