c29bf54b05adbaf00fa255f90d0ae40f19e20197
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:45:35 +0000

    enthusiastic and juice plain match

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
