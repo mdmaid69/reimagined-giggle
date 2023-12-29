1cfe6141d2b968b589f096c30153146655b2558f
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:10:18 +0000

    stop fire blue strike confident

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def reverse_string(s):
+        return s[::-1]
+def is_palindrome(s):
+        return s == s[::-1]
