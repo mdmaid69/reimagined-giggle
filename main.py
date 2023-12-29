76ce73be11732c310795efb0ce88320c38bd95aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:11:58 +0000

    finger floor today guilty contrast

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_exponential(x):
+        return math.exp(x)
