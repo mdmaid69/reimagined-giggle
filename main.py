51f425754346b547362a4985fc4843b4ba5dd1b5
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:41:42 +0000

    force month turnon communicate home

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
