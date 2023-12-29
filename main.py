c19d134103ad9edaeb6dcd4535478ae061521ed4
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:40:27 +0000

    petrol school workout play control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
