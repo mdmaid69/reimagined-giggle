9324be1fc063bf1c63f6eb711624d968f2787647
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:15:54 +0000

    cook calloff or light skill

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
