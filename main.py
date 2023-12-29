7052c3c8424bd1893a1bfa4c18bca0267fc53d8e
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:26:47 +0000

    major answer Uu splash equipment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
