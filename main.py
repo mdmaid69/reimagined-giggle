77fd661f4d710628ca23beb220fb04b00c692d47
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:10:44 +0000

    regard pot pure evidence bet

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def remove_duplicates(lst):
-        return list(set(lst))
+def add_numbers(a, b):
+        return a + b
+import os
+def remove_directory(path):
+        os.rmdir(path)
