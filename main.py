f5e970350b299a4ecf37d5b2de4555e4b8b7c6da
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:00:38 +0000

    check star shelter shelter mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
