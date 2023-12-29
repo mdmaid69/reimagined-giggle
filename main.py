0905a5d71f8360c7183710ca04c0bdc7902fd681
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:18:06 +0000

    build everything fit centimetre worst

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_max(numbers):
+        return max(numbers)
