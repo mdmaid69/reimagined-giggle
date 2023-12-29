40850da82561845f761b72b69a2497e695ba28ba
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:31:39 +0000

    point pull cold why bar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def print_python_version():
-        print(sys.version)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
