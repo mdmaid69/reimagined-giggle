63796d81c289fdc19f15f3065a69f2ad080b6692
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:55:44 +0000

    shelter arrival comfort offence bath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
