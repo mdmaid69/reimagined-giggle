7dd39e58c4adc441e927777da72e50bcc7fbd713
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:09:51 +0000

    runover funny line century start

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def get_array_length(array):
+        return len(array)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
