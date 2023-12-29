b60ebb947aa4bf9dcc4f3670e24a15ad004a63c5
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:51:48 +0000

    compete anxious birth getout connection

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
