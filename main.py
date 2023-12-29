87f9c11b33c6430ec70eb50b6ac797bcddab0bfb
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:37:31 +0000

    towards right help calmly spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_user_list():
+        return collections.UserList()
