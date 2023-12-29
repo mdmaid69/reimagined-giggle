c137ba2337253491a2e7b9a47f0d4214ede9da35
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:01:59 +0000

    common lay secret suggest so

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-for i in range(10): print(i)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
