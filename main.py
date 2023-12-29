71e56d4b6a45bdbb22bffd5080e92d8fb2dba5ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:03:04 +0000

    please trap away last demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def add_numbers(x, y):
-        return x + y
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_density(mass, volume):
+        return mass / volume
