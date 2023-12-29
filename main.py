0fab396a90f1f5aca139abf42a1391ad5eba8c4e
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:53:29 +0000

    reply way relief mad place

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
