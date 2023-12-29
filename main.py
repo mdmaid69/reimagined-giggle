8c380c44de2171488df8f10e4f0d4731291427a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:19:03 +0000

    lead common bury bright many

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
