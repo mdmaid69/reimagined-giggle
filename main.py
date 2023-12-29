1673c0e2204331f5618eb7b18116595fe7759d56
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:36:10 +0000

    estimate ring whom similar suppose

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_int(array):
+        return int(array[0])
