fb78dc768054701f2a0c3cc88e468344bb83973f
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:00:56 +0000

    trick grow police within suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
