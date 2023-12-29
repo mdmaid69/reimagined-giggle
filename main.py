8e0711cb7e7a8296abc44948aeda120c95dc97f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:45:21 +0000

    importance outside side live permission

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
