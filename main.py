3158cdfe1ab893c0821611c1f12f081ed4f01cb4
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:57:27 +0000

    back exaggerate achievement name spread

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
   def get_current_working_directory():
         return os.getcwd()
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
