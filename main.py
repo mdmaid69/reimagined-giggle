1ecead62bfb9e63a9bfd1573b8d2bd1c40e175c8
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:48:12 +0000

    pollution ago term suggest manner

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_power(work, time):
-        return work / time
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
