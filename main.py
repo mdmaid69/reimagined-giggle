a4410f85aab0980d0da0a26666fcede19aa3cec5
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:36:37 +0000

    heavy show welcome introduction crazy

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
