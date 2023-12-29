d9495d5ba86d6cd505adf9db8a522afa0d84dc02
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:56:03 +0000

    walk either introduce connect lower

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
