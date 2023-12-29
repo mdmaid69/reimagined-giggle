dadbe5eeb1928ba20cffc3a073802b3b36fdabab
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:06:21 +0000

    neck suppose shelter Bb game

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
