a220d2079f687b21e65cf67be02499e1d641a043
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:46:27 +0000

    mother year fail cause principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
