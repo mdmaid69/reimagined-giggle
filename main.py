3c1f22fe03ef428a79b3d8af6e6b52ff1ddd9af9
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:03:22 +0000

    birth fetch basically buy who

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
