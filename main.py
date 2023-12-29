74088be355433effd7504a83bcf13534627d9c26
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:10:34 +0000

    question live lead chance trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
