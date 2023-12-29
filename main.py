a4e6f3ffe41a6f2b69c837a445035cf12a158fdd
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:38:31 +0000

    guide motion suckme smile dish

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
