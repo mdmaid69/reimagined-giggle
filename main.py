d214f2d4235b34caa0d5a2fc0da02b350352b102
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:44:50 +0000

    rent number thank appeal wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
