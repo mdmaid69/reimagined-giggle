5edc93923676a850d65c71ebeb2240c6db5ca0d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:21:34 +0000

    young smell precise Vv switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
