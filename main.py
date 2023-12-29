fe320afe99f5f1b8ec2745ca63962abfbe0bddf0
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:44:36 +0000

    file wealth direct door whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def reverse_list(lst):
+        return lst[::-1]
+import os
+def get_current_working_directory():
+        return os.getcwd()
