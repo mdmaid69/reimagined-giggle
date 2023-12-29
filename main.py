b35afda2ef6db829f0b57a5970ee95fd13f1aa95
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:49:00 +0000

    seem female accommodation necessary accommodation

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print(sum(range(10)))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
