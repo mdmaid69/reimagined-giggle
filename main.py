17d01a4447ce30a1b2d88766626824bb64040220
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:55:17 +0000

    formal force contact risk father

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
