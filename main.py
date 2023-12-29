524ea80989d27a491b916b98a2255c36f81d2658
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:41:38 +0000

    design birthday science variety safe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
