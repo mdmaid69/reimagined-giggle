ee7a0af1f8a9700239b4681831aa883f208760f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:35:28 +0000

    cry computer mix what determined

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
 import array
-def get_array_as_str(array):
-        return str(array)
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
