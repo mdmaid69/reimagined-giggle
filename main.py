014dafe05efc53a31c2e934869f6b3fe9ddbde1e
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:33:12 +0000

    ill worry you crowded practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
