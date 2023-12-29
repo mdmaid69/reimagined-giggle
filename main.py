c9b91e04d152bdb39cf1c41b741852106895b532
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:37:31 +0000

    fact damp floor the room

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import re
+print(re.match("h.*o", "hello world"))
