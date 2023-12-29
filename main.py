5a0b7727c8c47f1f0bd8c02104f62a925558d585
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:22:50 +0000

    holdout confusing key everywhere plain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
