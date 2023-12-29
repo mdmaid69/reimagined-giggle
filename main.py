4ff85e9ca681fc73760c493ee770072105599ec3
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:52:55 +0000

    quantity bill deliberate split apply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
 import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def convert_array_to_bytes(array):
+        return array.tobytes()
