c43cc538add1eeff713b0d157ccd884ca45a0502
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:23:14 +0000

    really secret innocent container operate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
