2e2f1bf26054e2ceb35262b34ef57e2311e34a34
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:19:57 +0000

    switch modern afternoon across cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
