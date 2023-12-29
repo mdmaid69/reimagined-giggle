beb12aaf9a50beb17905b2276c6c7f4429cc62fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:18:47 +0000

    bus television beginning shoulder genuine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
