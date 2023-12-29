d6df06fc5eacbf1e4ac71855026011911967954a
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:40:45 +0000

    sometimes sorry come weak truth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
