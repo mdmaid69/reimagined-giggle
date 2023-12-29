fb3f37e6b0d20cfbef3cfbaf86f2795476774a42
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:04:43 +0000

    bringforward sink generally book spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
