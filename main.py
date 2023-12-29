b2a3903da059e9099db747a0cf887f7c53970727
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:24:53 +0000

    process correct play blow tough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def convert_array_to_list(array):
+        return array.tolist()
