73425930c9b9cb7b71e927cf6009bea94426261e
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:33:10 +0000

    mine whether truth keepoff allow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def reverse_array(array):
+        array.reverse()
