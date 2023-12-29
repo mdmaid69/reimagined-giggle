7cb74ac777c31f95db38038ce021d982139aefb7
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:16:33 +0000

    narrow alone failure release spoil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
