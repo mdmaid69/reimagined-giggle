ba5d04c1ebe93c4206e88b76242c4389a3ec35d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:56:14 +0000

    nearly transport argument myself before

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
