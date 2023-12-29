2b33ea71129644b26d11156f99a24dd4f19d0a00
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:27:48 +0000

    embarrassing forget peace drive remain

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-print("Hello, world!")
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
