f3cd54c5e98186adaf3167c009914dbf5d7c2026
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:08:33 +0000

    regular anything sleep combine compete

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
