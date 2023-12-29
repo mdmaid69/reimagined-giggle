7a8ecdf41c4d2a249ae093b9a6a8f858221788e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:51:31 +0000

    pin lie display goout return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
