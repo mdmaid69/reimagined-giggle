55b2a82ed97ec8cbd9721105aa24fc1131aaf219
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:27:18 +0000

    work offence before breakthrough wrong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
