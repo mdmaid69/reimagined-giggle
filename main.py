906153342820a6b452e22639be5e3cd3f2ada27b
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:25:59 +0000

    whenever together responsible key grow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import time
+def get_current_time():
+        return time.time()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
