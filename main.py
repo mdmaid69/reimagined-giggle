696f5f07c6f435c3d76fd5505407577fb1365b9d
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:58:00 +0000

    beat act light mess he

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
