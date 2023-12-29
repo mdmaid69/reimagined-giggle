87afea721462c0468191e55552137ed797bc349d
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:35:23 +0000

    definite uponon square wipe sound

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
