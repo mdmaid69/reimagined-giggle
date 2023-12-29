5866717b53f2cd1c285a8e0360950522b55d50b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:13:29 +0000

    confidence familiar priority colour make

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
