a2bcf2adeddf2c8d771da4aa917100ead48af76b
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:25:57 +0000

    punish drop far air fill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
