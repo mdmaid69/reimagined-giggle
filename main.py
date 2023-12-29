88c39b00ccee4de461cc294a0d3a7a262807d254
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:07:06 +0000

    exact clear shout inside arrange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
