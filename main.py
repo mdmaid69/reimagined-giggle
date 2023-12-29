e3d4e43e9679f46255db1975c740516237b7549b
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:00:40 +0000

    combination unhappy lend original unite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print("Powers of 2:", [2**x for x in range(n)])
+def divide_numbers(x, y):
+        return x / y
