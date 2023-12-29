d76ce6d90311226eef74da2de3151b005fa7009e
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:16:03 +0000

    stare might tragedy finger moon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def sort_list(lst):
+        return sorted(lst)
