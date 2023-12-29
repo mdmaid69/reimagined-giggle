14f05db66f18f43ae79c6c07e439a7064f6e2a01
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:56:20 +0000

    question steer balance cross appeal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
