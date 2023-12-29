2866971a8a20dd667a10b9d500ddbd11664adb45
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:57:24 +0000

    advantage drawing cause strike strength

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def reverse_string(s):
-        return s[::-1]
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
