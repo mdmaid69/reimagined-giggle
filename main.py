51aa81fa786bca951c8cc4957df931965d482e43
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:03:43 +0000

    spill body danger week male

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def is_odd(n):
-        return n % 2 != 0
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
