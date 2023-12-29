cdf97adf467ea849e08f7acadce8b8e143b24fd5
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:56:34 +0000

    instead type string nose power

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
