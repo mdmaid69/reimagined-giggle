73710ed07e5de39f9ede7d8c37e99b4aaae3d0ab
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:30:56 +0000

    company dream such settle kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
