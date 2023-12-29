98dc88d1ea0cde36d099f4e7c981d864db2c740e
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:57:50 +0000

    aware vanish among post bright

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_volume(length, width, height):
+        return length * width * height
