8b19458aae88c20b2c8e5ecc6c285dedccd9b3b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:03:06 +0000

    all appear improve burst comeround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
