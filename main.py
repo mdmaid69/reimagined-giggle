a138abeff09899ee2807ef04039ddb9cf999532a
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:53:12 +0000

    major reduction still lead getup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
