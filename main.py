0ce03670553b3df5b37b1966b9c0cb5f19d9f09f
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:57:16 +0000

    develop fail emphasize ours need

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
