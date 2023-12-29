df7b477033ca00f726240b57488a84203f154cc8
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:15:17 +0000

    receive environment price cry pride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
