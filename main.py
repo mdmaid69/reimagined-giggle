fa222df29052eb8761fa780456c08d0256899d7d
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:41:22 +0000

    left fail grade could unfair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
