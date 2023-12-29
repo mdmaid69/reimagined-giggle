06d29c200d3253d420e1a1045efa8e1992e0e607
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:56:48 +0000

    turninto we someone dream clue

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def cube_number(x):
-        return x**3
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def count_elements(lst):
+        return len(lst)
