f29dfc9d2bdd072259ee9b977790963a41647396
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:02:06 +0000

    now check runaway centre together

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
