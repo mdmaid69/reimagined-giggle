d3d5abf0c8e362302adb47aee2469cb4a3a00a2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:15:50 +0000

    low famous before clear sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-for i in range(5):
-        print(i)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_length(array):
+        return len(array)
