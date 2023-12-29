1a1c6a90407ed6237bfd4bbc2ecd8592a00ccbb3
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:23:08 +0000

    wind give bringup myself shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
