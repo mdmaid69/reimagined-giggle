c9335d241b1467343cc4fe2fa75bf27c800c2fd4
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:37:22 +0000

    whichever measure clever several Uu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
