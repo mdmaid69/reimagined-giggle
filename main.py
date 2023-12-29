c5091033b2a11adb094250ce56e459f2fa467287
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:59:37 +0000

    embarrassing related step target most

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
