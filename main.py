7f547532bee4cd7f75c6164396edf0af2b09e068
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:08:28 +0000

    clue advice little thinkof comfort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
