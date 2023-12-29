21eb53389bdfd0bd7bf1702955e5d08450224972
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:22:09 +0000

    fun pity pick model swimming

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_area(radius):
+        return 3.14 * radius * radius
