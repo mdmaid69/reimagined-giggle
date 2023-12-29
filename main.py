3c4d2278a03158c0a303ae3180231acc11ebf7b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:59:15 +0000

    gas interruption truth accidental fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
