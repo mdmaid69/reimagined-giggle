4dc5e80ce832fd3c98be55f8d7d2d00da085ac63
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:59:48 +0000

    possession successful lead this polite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
