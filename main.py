25d9e8d9d1a972b9d3aecadb5741217845590978
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:04:08 +0000

    soon distance relation kind during

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import collections
-def create_queue():
-        return collections.deque()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
