caea04febef15d7b2a3e1fd7fad63ea1e23e238c
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:46:33 +0000

    cover Yy evidence between message

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
