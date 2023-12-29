cbf806550d4671063789ed80c4c8a7f0bdb275b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:27:01 +0000

    burn glad hurry Gg bus

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
