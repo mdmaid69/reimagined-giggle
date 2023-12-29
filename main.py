afde49b1a9853f4c57fefa63285788f5286993ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:28:18 +0000

    wing know stranger hurry beside

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
