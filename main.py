36b15ba994aba479fe8e6d7edffaa945144152fc
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:23:36 +0000

    dear bad determined return whole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
