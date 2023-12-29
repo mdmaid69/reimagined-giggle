9001366adb65d7d4194ec4e9f2c4a464a84da8ee
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:50:05 +0000

    squeeze lesson one previous afraid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
