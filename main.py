ab114a126fde18354c295d86c22bc645b085b0c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:36:06 +0000

    swallow lose skin fire peace

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
