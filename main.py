78db567ebcc2be4c4d487a5d1b1dd48d7e344057
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:09:26 +0000

    pool worth poor smile under

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
