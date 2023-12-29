2e5c956d0b53b0ac32c91c4dc4a916f6aac0eafc
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:47:09 +0000

    search coat afterwards properly leg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
