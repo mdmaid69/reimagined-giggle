8aa07ef3acf93e420f2b3442189274753312ba56
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:00:35 +0000

    realize difficulty reasonable Itself speech

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
