afa7d6e2c253125614f9b5381f7e2aa8f958a04d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:05:56 +0000

    street practical regular increase lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+print(math.pi)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
