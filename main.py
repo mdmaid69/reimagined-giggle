8ca6f56254f89ab9d40a0e064f9eb8942376171d
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:53:10 +0000

    to sea right stop sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
