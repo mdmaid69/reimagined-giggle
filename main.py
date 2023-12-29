fefa3729586d33a8f48b4c38a92c08fbb689e1a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:58:17 +0000

    tip patience tonight thick appeal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
