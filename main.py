dba26fbbd05247634bc57f02f5b1291250ea0e74
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:35:58 +0000

    favourite capital nowhere until shall

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_volume(length, width, height):
-        return length * width * height
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
