bd522890895aea521ad74bf382f05d43a35f7a93
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:38:26 +0000

    squash long prevent wealth unlucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_area_rectangle(l, w):
+        return l * w
