fd84808beb637f07bd8f2be7debbb83d87603958
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:57:10 +0000

    face beyond detail myself twist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_list(array):
+        return list(array)
