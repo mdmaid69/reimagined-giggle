2f36c8f355a0e428c72f01d5c53f6dc89f1e137c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:57:42 +0000

    tragedy fear something even gap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def remove_duplicates(lst):
+        return list(set(lst))
