c16ed53a70313a5a58e8b2d98269ae53e54b00f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:56:18 +0000

    least talkinto depend up exaggerate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
