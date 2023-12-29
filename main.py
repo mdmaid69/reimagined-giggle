d1cdfae812a633fdaef6d86cd88db1e96ae87728
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:48:07 +0000

    one lot fit cruelty tool

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+print([x**2 for x in range(10)])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
