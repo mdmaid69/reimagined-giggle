2990956ab463289e00da28add799e84cf11630e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:50:26 +0000

    rid love excuse breakfast somewhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
