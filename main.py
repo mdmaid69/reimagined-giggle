f8baaa99ea00a55e6fad8e629f7d0873626ccb86
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:56:39 +0000

    travel bin wet safety behave

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def cube_number(x):
+        return x**3
