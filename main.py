762f78dc994078db5f2eb693e4c906de79b8fabb
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:09:48 +0000

    I unfair innocent earn song

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import numpy as np
+print(np.array([1, 2, 3]))
