38673a00435d0ee192c9b904dfc96ad9434c26f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:27:10 +0000

    theirs learn still successful similar

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
   def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
