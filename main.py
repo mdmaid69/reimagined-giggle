3a3d0a0aae17c48d2a6b02b1c7495838665717cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:57:27 +0000

    around unpleasant man calloff represent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_area_rectangle(l, w):
+        return l * w
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
