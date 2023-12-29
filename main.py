233836b4ab60d44e7ebec1220b1ce4ce182a14fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:26:11 +0000

    as use name spare room

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
