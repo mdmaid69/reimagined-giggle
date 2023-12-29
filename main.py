812e60594855b2615ee49e917c3caff96abe9587
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:32:58 +0000

    skin broken blue one become

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def multiply_numbers(x, y):
-        return x * y
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
