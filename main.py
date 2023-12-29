9ceed3bbeb164c6593454a632d798d64400a32c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:57:54 +0000

    progress field scream store independent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-name = "Python"
-print("Hello,", name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
