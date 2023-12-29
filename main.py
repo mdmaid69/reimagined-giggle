99ba8693e35da446ae3f7698c8df4776ebc2ef93
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:17:04 +0000

    muscle trousers suggest square rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
