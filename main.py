6b2bbc6db9fef36194ad520060a04a0c86c448c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:46:39 +0000

    search month family whole hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def append_to_array(array, item):
+        array.append(item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
