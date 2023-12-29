eee8180f7daa6cf658296215aa5c04714924f152
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:31:28 +0000

    prepare cheap nigga sorry trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
