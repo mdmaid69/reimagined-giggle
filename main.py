76559bb77b2569f936d13cf6a80cf18c5165f202
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:56:31 +0000

    strong wide sound 573entertainment worry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
