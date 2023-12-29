ff2a3ec3f477fc5463023dbc754b90e164518955
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:01:38 +0000

    intend maybe use comein convinced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import numpy as np
+print(np.array([1, 2, 3]))
+import sys
+def print_python_version():
+        print(sys.version)
