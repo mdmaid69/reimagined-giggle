d6438a7b2a8a5abfca79cbfff0f20cd8460c60ab
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:08:56 +0000

    silent model wire catch exit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
