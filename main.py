10f11e5e896849f46a192004ff10a6a745f42673
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:10:30 +0000

    brown straight thumb slightly ever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
