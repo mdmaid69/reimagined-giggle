e60d77a4c0effa584458164e9eaabb0b6a8deeef
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:45:00 +0000

    stamp fit hope better crowd

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def cube_number(x):
-        return x**3
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_length(array):
+        return len(array)
