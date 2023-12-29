dd560c54e7953bb5071a475b1e3cf419cda830d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:39:34 +0000

    practice mess dull shelter cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
