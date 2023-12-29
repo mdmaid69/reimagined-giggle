3f6afc111268454fa2252e49118fec55d9697f6b
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:21:37 +0000

    development gentle spill brother slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def convert_to_hex(n):
+        return hex(n)
