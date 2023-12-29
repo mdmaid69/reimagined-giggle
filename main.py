97cb572ffa9610974005eb62159f0d65b27a66c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:21:04 +0000

    city permission scene decide quarter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
