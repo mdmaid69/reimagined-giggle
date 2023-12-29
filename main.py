c0ffb152a91a81e62568e2fb25dc7c12fb3ec027
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:53:36 +0000

    affect book appear star quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
