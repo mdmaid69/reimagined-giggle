8c2dfd016eeef41c40087b4b29d393538cfe8a46
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:40:49 +0000

    trap opponent return accept wet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
