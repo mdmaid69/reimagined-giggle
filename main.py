df1bf639abe1d6ffd2de9f060f2182cef58ca607
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:43:55 +0000

    concentrate Ee proper clearly cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
