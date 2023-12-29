4600e836d752289d25777d7dad9e6dbc1ee83738
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:08:49 +0000

    physical violence fine strange handle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
