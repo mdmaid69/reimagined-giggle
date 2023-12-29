2051e4f8651a063903e536a3df3583d5134610e1
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:21:10 +0000

    part ought spill label boat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
