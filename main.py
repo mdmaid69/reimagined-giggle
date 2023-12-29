b31aa13324de7fe3837aec80126efb0ac3805b51
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:27:29 +0000

    thanks obvious mystery lead condition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
