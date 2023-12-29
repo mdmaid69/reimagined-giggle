e8554fe21e03c9dd545b204d658bd2c19aa83573
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:38:22 +0000

    here want jump organize objection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def find_min(numbers):
-        return min(numbers)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
