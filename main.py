8ad627dd258f9b889df2bf0c12ffecc11125ff1e
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:45:34 +0000

    sail smell plenty ring bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
