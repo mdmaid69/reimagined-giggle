e09c28908c23daf362ad14db821f5365d0e05f51
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:08:38 +0000

    shade pencil do radio gooff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_array_typecode(array):
+        return array.typecode
+import tensorflow as tf
+print(tf.__version__)
