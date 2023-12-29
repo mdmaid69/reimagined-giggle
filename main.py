6c23c932dee9b779543a6e6a9e19e6b4783fd942
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:10:14 +0000

    bill slice fork mind argument

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
