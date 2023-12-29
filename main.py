4f2d5c72a8ffacb6d8e967c0a4828ff864e51429
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:27:29 +0000

    evidence up minimum town rid

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def square_number(x):
+        return x**2
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
