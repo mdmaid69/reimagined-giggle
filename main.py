7f247ff384cbefbebc45dd3cba16ada46c4661bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:16:51 +0000

    water Hh transparent deliver ordinary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
