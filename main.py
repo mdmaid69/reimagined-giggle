a7838c39e48ef4d681d32030271543ca88c272f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:05:14 +0000

    crowd repair like good shopping

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_as_int(array):
+        return int(array[0])
