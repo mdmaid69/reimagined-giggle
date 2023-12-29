26dc87dad1a034d58580b9a9e37bba1c1fd980c1
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:34:52 +0000

    praise open straight settle low

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
