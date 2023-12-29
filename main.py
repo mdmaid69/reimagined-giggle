4cec313b4349817cfc0c3bc201862312dc3eec54
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:26:21 +0000

    street fast description delight concentrate

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def sort_list(lst):
+        return sorted(lst)
