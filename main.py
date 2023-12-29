4145f4de1d7fe09a05032abfd8f98fc1144cb561
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:00:41 +0000

    even deaf study better may

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def count_characters(sentence):
+        return len(sentence)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
