cbeb4257435a4b2cd046be0e8654fc399138a662
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:21:52 +0000

    music expensive simple fun behaviour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
