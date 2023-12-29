bb3f2a66ce3259d80ec0454cf9fcc5bb1d428b71
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:49:26 +0000

    culture high food satisfactory pain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
