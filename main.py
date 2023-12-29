ecc45ab2a8fcf4229084bc89847042d335f54835
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:11:58 +0000

    lower picture lose manage refuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def find_unique_words(sentence):
+        return set(sentence.split())
