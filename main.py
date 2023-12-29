6f92a391e751abc6959508780c4ac35ff19e6b2d
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:55:53 +0000

    side world remove sit throughout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
