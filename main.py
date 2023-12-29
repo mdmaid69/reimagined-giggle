b0fe4367c749374552d7c519c11440101c787e09
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:37:43 +0000

    fold soil Ll present unlikely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
