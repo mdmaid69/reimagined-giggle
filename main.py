c447ada4cebb966637f53c74337676150d76d13b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:24:51 +0000

    pure intend on hello partner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
