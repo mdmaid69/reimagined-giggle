9f0551e824160ce8b13203e3ab674fbbcfe787fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:48:31 +0000

    cheque Bb shake sharp interrupt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def find_max(numbers):
+        return max(numbers)
