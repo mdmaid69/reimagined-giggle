0a9118b45e7bbfcca0d4c9f45beeebac5bb0d88d
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:36:33 +0000

    but divide positive now better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
