e0d74667a2a0246c6dd08f55b96ec54eec59db88
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:37:19 +0000

    start cheque smooth city future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
