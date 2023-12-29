12ba013454f1066cd1e6a4f265dc2733282ecfc5
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:50:25 +0000

    lose smile mark safe disturbing

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
