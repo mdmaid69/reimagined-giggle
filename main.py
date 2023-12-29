1eced0b4ac977d0f71b08f26b0d0f58b51d4d59f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:34:01 +0000

    dream rob understand wheel plenty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
