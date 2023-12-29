dda963461c62a0a24256906f48b40f886972a28d
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:33:17 +0000

    present previous or inform stretch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
