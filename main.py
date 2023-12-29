91b53d95c3281882940877a7a9ba11f657eecbee
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:16:26 +0000

    waste share female normal yourself

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_ceiling(x):
+        return math.ceil(x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
