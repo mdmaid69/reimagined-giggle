fca45c777fa989360e71eb057cceeda42e908f4d
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:11:39 +0000

    ashamed whether forget Vv object

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import collections
+def create_counter():
+        return collections.Counter()
