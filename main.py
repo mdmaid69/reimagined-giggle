e1a6db5bb2e9e6785678cb36451f29f082ece0db
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:08:35 +0000

    crisis value touch turnon line

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
