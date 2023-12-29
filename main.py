7226f2e399b43e660bf84f5f3155966db72cf07d
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:01:57 +0000

    interview variety culture try stir

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
