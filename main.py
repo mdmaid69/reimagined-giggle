70f00c7301d93e2455ee327aff5ca4a9095e9b0b
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:48:53 +0000

    usual male problem can spend

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
