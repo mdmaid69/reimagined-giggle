65c439fd3bad1d8f8368460e845a4ae365443a62
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:38:30 +0000

    along study embarrassing house treat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import os
-print(os.getcwd())
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
