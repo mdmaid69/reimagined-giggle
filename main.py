571e1b8b8e1d6c20f162babed0dc89e0bc797759
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:36:32 +0000

    want cheque noise debt gooff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
