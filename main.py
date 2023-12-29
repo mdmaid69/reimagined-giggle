bcc1efaeb7c1cd5fd385c0c6fcd6ad251d771591
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:02:52 +0000

    this thirsty squeeze know detail

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
