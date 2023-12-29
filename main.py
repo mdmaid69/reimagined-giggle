dd17155c25432dbb26bb16a89df8225d904697be
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:05:44 +0000

    Zz secret drop patient congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
