412c251d9185eeb8b3e52d828629f16f7ae60a1d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:40:04 +0000

    lonely crash branch charge cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
