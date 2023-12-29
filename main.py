b992b5c15d0eda9be69afacc38a48d6a104a318f
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:48:24 +0000

    comeout pen plastic music pool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
