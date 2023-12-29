85b1ffbed91327bd31973fe85d7acdd421a8a25a
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:27:34 +0000

    admire section fight correct write

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
