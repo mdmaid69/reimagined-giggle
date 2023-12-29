eec53624d48a88a5e82457a36d2a187e6bc534c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:52:21 +0000

    switch suggest loss building far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
