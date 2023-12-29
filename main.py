6f07ce754a1991c1d2d74a94436da99931d54586
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:05:18 +0000

    occur tap plastic report create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
