cf7e6a796e485d9b69b9c33a7c7e8c09fd948c6a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:08:15 +0000

    well convenient sticky because willing

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def is_odd(n):
+        return n % 2 != 0
