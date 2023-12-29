097b6292cb3289cda4ab4d32f58316db040bb215
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:35:59 +0000

    Uu nowhere pollution base patient

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
