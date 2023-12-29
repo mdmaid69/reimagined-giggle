430c73c40aa2b8c14bb7feac1d8397c18c76d03e
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:59:17 +0000

    stir only cum post calm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def add_numbers(x, y):
-        return x + y
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
