e77813c066036ec21af5904548034be07e9dbe1a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:32:23 +0000

    succeed breathe tense game pleased

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 text = "Hello, world!"
-print("Uppercase:", text.upper())
-def convert_to_hex(n):
-        return hex(n)
+print("Characters:", len(text))
