c2ef8610092d68883cc1cae35ac16e0a9978f17d
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:33:55 +0000

    fix suckme aware alive die

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
