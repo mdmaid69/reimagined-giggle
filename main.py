f15a53f74cbdd82825131c6644e951d634ccf1af
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:38:07 +0000

    powerful trick sky continue control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_set(array):
+        return set(array)
