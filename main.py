0109c6c7643f910a42c0db82c03de25c10ebaebd
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:46:22 +0000

    mail out embarrassed surprised embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
