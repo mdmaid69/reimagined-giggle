f2a4b2cd9468c4bb06a9e47a1857d943ef300abd
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:12:18 +0000

    stranger bend belief clue fair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
