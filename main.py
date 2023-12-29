5732f51a1a488983f1b1aa0539ef68b92efa0e62
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:27:39 +0000

    exhibition nor intend seriously mystery

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
