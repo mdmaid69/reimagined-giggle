844dc4ecf5d7eb851b725a1ebd8c9d8c95371e3c
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:14:19 +0000

    finish pinch whole far complaint

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
