c79e455dc7256a9ca9878afccee5ab9c6b29cbfe
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:28:00 +0000

    control still better control environment

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import sys
-print(sys.version)
-def convert_to_hex(n):
-        return hex(n)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
