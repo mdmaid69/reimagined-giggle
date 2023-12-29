10a6775b6422924c165c9d17616d2cf4f022f6f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:58:16 +0000

    idea past woman memory difficulty

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
