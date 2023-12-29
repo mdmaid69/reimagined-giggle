4961a971619bb591f59ec7fefcfd86e08a58cf0b
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:07:50 +0000

    sky somewhere attract obtain nowhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
