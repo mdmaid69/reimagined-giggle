ef8eb190c34944e8bc80924be42d1d6f58c4f644
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:55:10 +0000

    watch important sick lonely month

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
