8a25af98420e49b20ddf0fbdd090928132e06575
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:59:02 +0000

    lonely maintain sit basically before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
