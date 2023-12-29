1ab585223a00e3c95be5155ddd60210b70ac97d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:06:42 +0000

    welcome paper loss soon memory

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
