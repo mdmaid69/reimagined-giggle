0fc2ba755a7bcf847a303f0c23c752a6b13bde21
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:43:33 +0000

    unkind display calm attempt impressed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
