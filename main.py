5eaa73b3dd4b711afe853d00294f522cd85b8b6d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:55:15 +0000

    advanced experiment rain turndown him

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
