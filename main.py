32e8142554e8d97dc7c5efbed51f2e0699eced0e
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:39:01 +0000

    explanation course characteristic danger occasional

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
