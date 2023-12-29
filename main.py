1f71ad730f05cac90127e9c74da979423d7a8653
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:30:24 +0000

    skirt 565end flow them innocent

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def find_max(lst):
+        return max(lst)
+import math
+print(math.pi)
