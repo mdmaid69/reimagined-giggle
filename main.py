dcdc7f3d9fcff0b38a7ed7adec087d2c0fcca1ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:22:48 +0000

    attitude part giveback contrast completely

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def cube_number(x):
-        return x**3
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
