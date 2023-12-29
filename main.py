e45658f5cad0dcf9e6f4ead65a63ecb9fe4dd945
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:01:46 +0000

    time permission blow notice each

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
