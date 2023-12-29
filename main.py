d78ce8f9809999b671af4cca0dadf75be2af0460
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:04:24 +0000

    572enter plastic callback strange sale

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
