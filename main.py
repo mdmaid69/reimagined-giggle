a448d1a6bbea1e31a4d910bbb746efad217bf6f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:42:41 +0000

    suppose worse stranger risk means

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
