203a0ca5a1c66cce8e4c822aa8be4f9105b583a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:55:31 +0000

    cum callat moment thinkover lead

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
