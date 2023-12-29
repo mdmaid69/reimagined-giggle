904a3eaa67f3451d5acf42253f0ac5ec5ab504ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:30:17 +0000

    difference major somehow turnaway sorry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
