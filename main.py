7cf7e0e9b7cc68cc5306d38f8404ee7171df25f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:41:38 +0000

    way welcome few cost tough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
