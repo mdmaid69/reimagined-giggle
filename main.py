1586c892553a90120a1ff3f76a53c807a66c36b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:43:49 +0000

    again obtain breakinto breakdown in

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
