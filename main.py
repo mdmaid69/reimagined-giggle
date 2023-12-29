0e7e84a20534d4e5fa20c87823acc8718973a9e3
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:49:18 +0000

    correct needle clean this regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
