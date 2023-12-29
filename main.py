62e17eb1521c122a44a37191cdbc4fadd0c2e7d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:17:34 +0000

    star happen my teach rent

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_acceleration(speed, time):
-        return speed / time
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
