aabcb1733b7351677843c25993599950024c2133
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:12:39 +0000

    possible stiff generally bite accommodation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import collections
+def create_queue():
+        return collections.deque()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
