dc699c46dee61053571db57e4bc263e4b2b43739
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:14:47 +0000

    about brush take his shallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
