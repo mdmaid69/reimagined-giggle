3eaf6192a834e2a6262a5448c839eabddbd09b6f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:19:45 +0000

    roll factory week geton experiment

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def convert_to_hex(n):
+        return hex(n)
