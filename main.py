05f31a2b9291b1b65c6a71c8ddf218337ba67580
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:11:00 +0000

    politics play find continue between

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-  def reverse_list(lst):
-        return lst[::-1]
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
