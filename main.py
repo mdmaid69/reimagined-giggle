cef6d7806fd67776b7244c3f4438058cb8ebfc0f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:55:21 +0000

    dangerous move plate name Kk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area(radius):
+        return 3.14 * radius * radius
