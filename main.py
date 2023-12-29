b32a04348fe6592980da9dba6d823d27bba31ee8
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:03:29 +0000

    fork compare hit too carry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def sort_list(lst):
+        return sorted(lst)
