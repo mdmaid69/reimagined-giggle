7de153063356fc97ee524bb18cf0d9bf31b5d07f
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:12:52 +0000

    fact fire tape declaration aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_str(array):
+        return str(array)
