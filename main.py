c61a03c958a74abdca699eeaa01bbe404089cf6a
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:11:12 +0000

    next term against acceptable truth

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
