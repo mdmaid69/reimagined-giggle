29978d948b0e62ce04d515fb8f5ef684a12a620a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:27:03 +0000

    everyone play you early recent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
