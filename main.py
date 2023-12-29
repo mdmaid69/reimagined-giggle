12bec95e3b2b3ed4ee49821c920b15e717d28c30
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:35:02 +0000

    now possibly afraid finally beginning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
