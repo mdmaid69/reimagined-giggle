ce219c8530fd0f69d3a058425577157f2e1a4a1c
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:46:05 +0000

    right surprise shoulder statement sick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+from collections import Counter
+print(Counter("hello world"))
