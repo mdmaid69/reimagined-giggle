edb5199b576a9efd6a1d7493bffd91a0e288fab0
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:33:36 +0000

    row wet weather Dd hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_current_directory():
+        return os.getcwd()
