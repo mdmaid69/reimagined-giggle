cd64c317dc97d66799ca07f1d6dcd358438dafc5
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:09:35 +0000

    public check slide comment guilty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
