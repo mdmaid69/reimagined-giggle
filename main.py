daede5d18b867f22288bb927ebb7aeab9b25475a
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:22:19 +0000

    attention everyone afternoon dull inside

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
