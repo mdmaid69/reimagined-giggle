0094cee74e321e0184419065c990dc6f91df1e9e
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:24:59 +0000

    black file increase less 567enemy

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_pressure(force, area):
-        return force / area
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_area(radius):
+        return 3.14 * radius * radius
