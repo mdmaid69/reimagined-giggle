9a863f5dd4e3718da848085882f618d0c6a74bd3
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:53:31 +0000

    chin strict exciting alone minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
