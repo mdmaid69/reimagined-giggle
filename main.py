6b91be7158af97e1840dd39fad2d481b0d40e67c
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:17:45 +0000

    have though brief help comeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
