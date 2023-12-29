114630931889197f72ba7706607ca9aa7914f977
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:31:28 +0000

    search scared that oil whisper

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import collections
+def create_queue():
+        return collections.deque()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
