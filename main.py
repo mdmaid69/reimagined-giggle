63102dc1859c85169992ef0138fab57daaa82d0e
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:46:14 +0000

    failure fortunately close frightened who

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import platform
+def get_python_version():
+        return platform.python_version()
