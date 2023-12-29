d91b6f96e58e773451fd6f812fd142353b02a195
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:29:26 +0000

    clever comeup race plate raise

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
