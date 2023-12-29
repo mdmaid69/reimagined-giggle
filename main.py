18b1b2ccded70983cd4b81c5114b543ebd7689c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:52:28 +0000

    face Ll safety belong like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
