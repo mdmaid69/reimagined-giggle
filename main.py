289e4ec10334fb0778c580c420804dc6d1b42a1e
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:28:57 +0000

    quickly whisper Uu disturb roll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import math
+def calculate_floor(x):
+        return math.floor(x)
