803448250a597bb5166c370bb76ad5b580ce290e
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:48:34 +0000

    very fierce book schedule choice

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
