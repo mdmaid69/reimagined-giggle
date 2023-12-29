218cdb78d09b138a9450143242c31ef5d50e22ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:33:26 +0000

    group far bringforward Pp run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
