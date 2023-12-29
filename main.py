08a7e3f5dbed18b49af230ae36c78e5d7dd138db
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:11:25 +0000

    block needle imagine cheap interesting

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
