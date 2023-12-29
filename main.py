2ff8948f2decae49cc8ec33919146c41dba846ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:42:57 +0000

    tax hers speed joke brave

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
