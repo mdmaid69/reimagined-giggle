2d79e473a412a56b76e68582583d171175ff443b
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:05:07 +0000

    swim sensible strike join hurry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import time
+def get_current_time():
+        return time.time()
