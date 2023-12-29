32845e18573fc386c2c1cf92fe955da2affab50e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:40:11 +0000

    runinto ride top yours basis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
