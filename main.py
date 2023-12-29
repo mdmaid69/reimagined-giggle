b1623615d6130a4be43a246be0c47446da77edae
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:05:23 +0000

    way left relax check nor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
