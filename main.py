fbdba47e0a99bb4102720948b96f6036e4cf611b
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:21:04 +0000

    pity slightly judge contrast fond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
