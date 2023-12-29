7e781c2bd7e0cf18e00307d010612adf7e93a086
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:45:53 +0000

    attempt possession suffer cinema bus

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_power(work, time):
+        return work / time
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
