dc4bfb9f55debcc5f52cd244932dfe4d65bd164e
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:18:47 +0000

    closed bet majority second they

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def subtract_numbers(x, y):
+        return x - y
