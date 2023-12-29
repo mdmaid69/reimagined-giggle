4e66cea5ed14e17691e2e66086f24d4d5b27af97
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:49:52 +0000

    more treat turnout believe correct

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
