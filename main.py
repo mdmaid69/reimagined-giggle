79d708a422a374ad15d17105370a44ca0cf157d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:11:42 +0000

    straight fear cheap 571enough runaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-print(math.pi)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
