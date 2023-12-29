f68cebb37e9268c11cfcc583b5af08b457b47a4a
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:22:24 +0000

    detail immediate after over tradition

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import datetime
+def get_current_date():
+        return datetime.date.today()
