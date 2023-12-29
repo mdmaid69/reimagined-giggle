32f4e9bdf9d6306aa67b68789fbe0deccb832cca
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:25:58 +0000

    write escape relative sing compete

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def generate_random_number(start, end):
+        return random.randint(start, end)
