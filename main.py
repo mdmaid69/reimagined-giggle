8c3459a54b3e7ace2f9b1a0ebb077bb85d04ab2d
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:32:50 +0000

    cheat demonstrate warning deep trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
