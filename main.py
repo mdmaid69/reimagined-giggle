cea70478f033d3f658e7fb8b2b6bf743cdf8f45a
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:15:09 +0000

    Hh takeout shame reaction weigh

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import re
+print(re.match("h.*o", "hello world"))
+import os
+print(os.getcwd())
