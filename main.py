b033fc97854182d3b4d4b380bdd86e7c6d73a3e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:53:01 +0000

    below produce remember any compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
