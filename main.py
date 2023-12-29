e02d6e107f6fff5d1d02ec3afc5f576ba4b87a97
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:48:19 +0000

    scream both behind contents beat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
