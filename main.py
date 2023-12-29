03e3c802fb789a398b3acb84f9d16365ab1dd0ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:21:45 +0000

    prize pour crash exist trick

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
