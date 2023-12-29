48e95814fbc8206947e6d325f9ca691bb9d3b5ec
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:48:22 +0000

    closely mean pocket quick attach

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  def cube_number(x):
-        return x**3
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
