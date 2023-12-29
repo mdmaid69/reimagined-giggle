65636a4a4f9b2abbdcb919bac308ec9ab0aa88a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:38:41 +0000

    behind coffee complete book post

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
