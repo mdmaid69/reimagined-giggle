2214e2397584ce3564a6d4da91a3641b606495d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:58:10 +0000

    section old usually young often

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def reverse_array(array):
+        array.reverse()
