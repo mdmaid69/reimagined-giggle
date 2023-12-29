2f8fb417aef9091f0ee9bee0ec12a560c80afad0
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:00:49 +0000

    aim head fence turnout Cc

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_speed(distance, time):
+        return distance / time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
