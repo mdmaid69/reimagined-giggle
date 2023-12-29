c9a6b91d20a4f3928a81136f41a7f33e0dc4037e
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:17:48 +0000

    deaf offer deaf positive break

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import collections
+def create_user_string():
+        return collections.UserString()
+def find_max(lst):
+        return max(lst)
