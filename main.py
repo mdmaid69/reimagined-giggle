e03de85e6b02af5ddd2765b153f5c0ee884871d0
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:12:54 +0000

    immediately discussion recover floor wear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
