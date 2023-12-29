af802152cedd6822e6128b89b78f5f6ab3e49d45
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:28:03 +0000

    bit voice book makeup hollow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
