6e888753e55f6bdad991fe5b71919786c6bc537a
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:50:28 +0000

    connected load taste plan them

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_itemsize(array):
+        return array.itemsize
+import os
+def get_current_working_directory():
+        return os.getcwd()
