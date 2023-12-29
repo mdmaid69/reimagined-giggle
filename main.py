ea6a7fb42575f14144ef132e3722d36c9b5b5530
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:07:16 +0000

    alone education declaration all section

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
