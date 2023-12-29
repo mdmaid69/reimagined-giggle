c2ac297961b377341aab415f907ad60f7ad4e49d
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:30:37 +0000

    recent entry shrink collect pocket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
