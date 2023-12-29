e4c1d080e213d98bbce77d8bf7d5c519325ea13e
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:17:54 +0000

    drive identify major compete Hh

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
