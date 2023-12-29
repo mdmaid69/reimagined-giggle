318746a80112029e90513417b4bca934a0c5a6b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:20:33 +0000

    side clearly sorry nor want

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
