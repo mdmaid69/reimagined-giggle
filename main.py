5df6f8353b69fb9841c35a7bcad4d5fd35838022
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:37:58 +0000

    play measure support they female

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-for i in range(5):
-        print(i)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def find_max(numbers):
+        return max(numbers)
