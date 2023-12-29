70944046de7b105215f2e11308d647267f9218b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:13:35 +0000

    education throughout burn pocket travel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
