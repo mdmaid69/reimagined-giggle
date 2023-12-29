f9220aa949072ad6d8c5056051efc3db2441d308
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:12:16 +0000

    scene it well uniform mail

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
