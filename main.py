73b4ad64c6e694ae49acc2a42affb0c7ecfc4805
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:31:33 +0000

    cut full travel deaf loud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
