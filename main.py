0c65fcbbcff4708d8af5293e2842b9b162985ba8
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:40:41 +0000

    generally circle set sure expect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
