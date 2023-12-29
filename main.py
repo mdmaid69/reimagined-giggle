3bd37929c7a2b2e80c67f6c467bb7382b411517c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:09:48 +0000

    front production object spin edge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
