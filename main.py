451d6d40fbef023da550d30346ec75b9b8dc6e86
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:53:38 +0000

    worth completely apart curious search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
 def calculate_logarithm_base_10(x):
         return math.log10(x)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
