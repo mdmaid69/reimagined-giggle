7654b9bf268bc2ad851b1407c34b4fed8ac4e42e
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:10:45 +0000

    professional lend satisfactory shallow act

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
