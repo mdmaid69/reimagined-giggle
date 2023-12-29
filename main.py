520072ecfacffcb281377447a2f4267ac3658df5
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:58:07 +0000

    head deep call holdout play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
