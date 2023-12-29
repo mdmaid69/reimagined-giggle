d57298a4d46d128768dddbe03fdb5969a94a72f9
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:23:40 +0000

    would stare bother alone write

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_volume(length, width, height):
+        return length * width * height
+for i in range(10): print(i)
