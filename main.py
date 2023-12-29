a88187193821f991fd086228c5e8d450486df181
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:28:39 +0000

    inquire arrive spring switch silent

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
