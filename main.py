477343954fea8fd9a2092f7ea1d032de6b5307ee
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:44:10 +0000

    through total practise we plant

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def convert_to_binary(n):
+        return bin(n)
