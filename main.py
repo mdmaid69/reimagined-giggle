7b323559e844d0a348106e695ae9c463aba49cb4
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:10:49 +0000

    action opposite acceptable level special

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
