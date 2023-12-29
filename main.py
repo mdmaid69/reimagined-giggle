c1c7ef8c5c91a761af6a160105fa241ba29cf404
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:47:16 +0000

    loose odd roof base talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-for i in range(5):
-        print(i)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+from collections import Counter
+print(Counter("hello world"))
