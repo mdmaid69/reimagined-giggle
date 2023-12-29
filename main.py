fa07b3449a38698003d8fecae4adf40d6a5ae0c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:59:54 +0000

    be problem discuss trick exam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
