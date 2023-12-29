802d5e12d6bf17c9a289c7c937d5e538e0c5b2c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:46:47 +0000

    authority no nowhere steady smart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
   def delete_directory(dir_name):
         os.rmdir(dir_name)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
