85e7b12eb0ca3bef23b3f1015fdb89ee650cfb65
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:31:38 +0000

    somehow full yes slide shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_list_from_array(array):
+        return array.tolist()
