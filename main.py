c3115a60acc7befcf5148e33c096244fcd59d883
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:11:32 +0000

    old sort war dead sad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_length(array):
+        return len(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
