fdd437aa52f207484df3068951e8b29584de0d34
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:36:39 +0000

    view among edge would afterwards

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def count_elements(lst):
+        return len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
