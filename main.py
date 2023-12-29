75c5f2bb6b0012e5f57754100d44c592e446beda
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:42:28 +0000

    stop art system absolutely weak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
