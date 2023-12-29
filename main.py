8cdd8c6d15dd961afdae95a205acc1b64e5f7880
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:59:16 +0000

    giveback check pull rush challenge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_density(mass, volume):
+        return mass / volume
