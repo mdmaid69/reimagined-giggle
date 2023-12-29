0626c2ce846638ae7c0b04884af2f9a6f5a62e1e
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:51:18 +0000

    load talkinto twist stand wind

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_power(work, time):
+        return work / time
