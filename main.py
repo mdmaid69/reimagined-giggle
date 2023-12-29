62683120ed450d1a72d63026cba628814851b928
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:05:55 +0000

    equipment this actual will split

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
