aa97f0a03b4bd412b949f6188c24fc20b6019a9f
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:42:09 +0000

    past pencil takeback accommodation plastic

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
