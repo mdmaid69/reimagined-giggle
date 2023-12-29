2cb9c4306b03046c452423d00b4b5f265e239cad
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:21:48 +0000

    paper land false greeting outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
