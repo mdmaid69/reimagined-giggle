1765a84e720939e7d1f4d327613f3cd3df5bb4ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:55:11 +0000

    classroom seat big question soak

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
