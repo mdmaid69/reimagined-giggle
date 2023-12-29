dd196694594ddc9efd634845738f7584af4b4ef9
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:16:10 +0000

    difficult pain whole unit copy

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def count_words(sentence):
+        return len(sentence.split())
