e97e33e63d6be101dc1e12376bc222d147713570
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:09:15 +0000

    public plan creature character through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import json
+print(json.dumps({"name": "John", "age": 30}))
