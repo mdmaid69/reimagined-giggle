c2bbf0ae5cb375cb14bdae7ece960162469f25d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:54:24 +0000

    mess physical peace owe sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
