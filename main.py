0ed559ed0306e06256f13a3269fc7082c020499a
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:56:11 +0000

    dear promise bury scare repeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_octal(n):
+        return oct(n)
