db6fb7625d75396edc0a972c935fa08e75ed7402
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:56:49 +0000

    extraordinary break separate shelter front

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-print(random.randint(0, 100))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_binary(n):
+        return bin(n)
