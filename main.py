dbb1a6b64f516962a16ed4133a538d6e3b54a6ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:07:11 +0000

    consult terrible warm rush jump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
