e127eb519e3773bdd1e940e99669aab331a9e8dd
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:05:27 +0000

    any born exploration 570enjoyable long

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def is_odd(n):
+        return n % 2 != 0
+def sort_numbers(numbers):
+        return sorted(numbers)
