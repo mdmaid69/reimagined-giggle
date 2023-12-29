31fc8220e9c7db8c940c2b4bbeb2c919dcf0c77f
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:30:26 +0000

    relax kick basic scared least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def add_numbers(x, y):
+        return x + y
