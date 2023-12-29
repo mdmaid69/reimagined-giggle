e444ec9b00bcfa7f0c89891ee4b68a6bf8d25993
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:32:18 +0000

    tour shout laugh job argue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_user_dict():
+        return collections.UserDict()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
