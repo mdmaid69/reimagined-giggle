f07e34ac0e49d398f97e6357fc7e834a2560a0f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:36:13 +0000

    ourselves heavy dream sister invite

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
