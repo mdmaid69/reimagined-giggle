23a6cb13ff7bd43b6e2cc3cbf4f9fb06e5f78497
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:23:59 +0000

    clear sink not my smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
