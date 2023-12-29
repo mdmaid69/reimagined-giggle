2784100288ad010512efe2c18d695c2d45ad0483
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:20:47 +0000

    instead bound machine bury contribute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-def reverse_string(s):
-        return s[::-1]
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
