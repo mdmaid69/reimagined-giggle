0633d5698217a7d4ccad4ff7c63592068b204617
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:19:44 +0000

    pause face breakinto behave period

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_power(work, time):
+        return work / time
