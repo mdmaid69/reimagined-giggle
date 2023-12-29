a73398408acf4d3c2da98946816ac2232f0c7679
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:50:37 +0000

    sweet down workout any name

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
