9192b78db778cfbd05387079cae58a4a8cde6f0a
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:02:48 +0000

    protest true proud break direction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import sys
+def print_python_version():
+        return sys.version
