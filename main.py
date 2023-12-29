3ec42eb7a2ec4199273b45d817aa36d3fe32362c
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:02:56 +0000

    comein poor flavour against work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
