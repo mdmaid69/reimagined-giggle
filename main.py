fc373c05d4a752c8f0115675948b602073ceafe8
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:17:02 +0000

    shock risk contrast general tall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def convert_to_octal(n):
+        return oct(n)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
