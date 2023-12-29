b59416625065856ae74a108633a3394f06761358
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:31:45 +0000

    business container takeout that least

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-def subtract_numbers(x, y):
-        return x - y
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
