9845bd527d535eda6314203d7589811ae9c7310d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:13:10 +0000

    upwards cap minimum flat rid

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
