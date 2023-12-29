d1af32ae486aba814f7a65358b7318e74df5b5ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:03:56 +0000

    worry date invitation bet air

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
