18a51f99e10fe0f427ed66d17bd652cab456d67a
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:34:14 +0000

    feel turnover train anxious out

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
