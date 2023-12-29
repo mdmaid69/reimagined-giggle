189d7046bcc6a7a387f3ec824d3ae3bc9849d588
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:55:17 +0000

    base different tongue demand tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import os
+def get_current_working_directory():
+        return os.getcwd()
