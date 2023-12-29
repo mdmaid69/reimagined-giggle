88494fba6ea600f901dc54e82b44c49cb04cc058
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:39:37 +0000

    slice once each scream day

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import time
+def get_current_time():
+        return time.time()
