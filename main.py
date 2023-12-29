9e306a9a76ff86e52f38e6fbf2bbbb391fb8d855
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:24:42 +0000

    content bottom fashion size morning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_base_name(path):
+        return os.path.basename(path)
+def is_even(n):
+        return n % 2 == 0
