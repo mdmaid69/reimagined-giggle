da3aede3e948cbdcb5534321ca5c244079484c14
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:44:52 +0000

    trap choose somewhere afternoon defend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
