ba5c0a5ea421d79b00e6e75e5e7636fdc29a3dc9
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:18:17 +0000

    again prevent all pool direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_list():
-        return collections.UserList()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
