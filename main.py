d37a3b155fa92ed5eb531210055ed68b8debc10c
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:10:58 +0000

    press vote education different your

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import os
+print(os.getcwd())
+  def reverse_list(lst):
+        return lst[::-1]
