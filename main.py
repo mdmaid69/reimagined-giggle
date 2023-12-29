4900029152671ff12c1fe4d12d4e841fae6655f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:53:24 +0000

    stamp sign towards public square

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import re
+print(re.match("h.*o", "hello world"))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
