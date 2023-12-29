d098f30a23c67cd4a1180f3ca0296f9f2671ecdf
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:11:28 +0000

    next bang stay receive language

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
