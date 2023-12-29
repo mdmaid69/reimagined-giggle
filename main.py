89bd1ed59e6357a967d19921c951a0cd1fb42dee
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:19:20 +0000

    immediately practise better prevent discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
