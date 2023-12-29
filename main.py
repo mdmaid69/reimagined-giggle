5fe9cf8978848407ce68531d4e3dc3612d391b99
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:25:26 +0000

    left count blue mistake second

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
