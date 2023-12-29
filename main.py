633d207fea2c3797f306384a2d9daa6d653a5baa
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:53:05 +0000

    hurt few way world ago

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_list(array):
+        return list(array)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
