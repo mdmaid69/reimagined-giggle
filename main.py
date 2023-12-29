67d9b83a129c4c56c79b4c660a48c94dd6a04cfc
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:00:09 +0000

    quarter such win congratulate level

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
