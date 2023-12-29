8508ada5a7b57069c7ddd6982ba5b7dd269f30e1
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:42:49 +0000

    recognize switch interesting Vv there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_counter():
+        return collections.Counter()
