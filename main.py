3b1b8538b417529f34f5ea7d396de7f98479be84
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:40:59 +0000

    funny good plenty Bb reduce

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
