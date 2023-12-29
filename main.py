457bd21a95e294c63c149de526002a26aee7f8f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:56:09 +0000

    goup pain consult back unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
