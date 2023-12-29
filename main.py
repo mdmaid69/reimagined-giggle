0a06fb74adc685fa24eb15a489aba53d509bf13f
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:25:48 +0000

    oil offer lie finger store

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
