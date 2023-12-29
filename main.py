672bbfde34533ad99172a01b3042a582a72c1ecd
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:13:23 +0000

    capital whether race cut view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
