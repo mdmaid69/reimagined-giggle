b70df5f73ee6fb3df71b89c5dfa73093d97de7d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:58:36 +0000

    godown interfere perform achieve callat

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def square_number(x):
+        return x**2
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
