f67e7e2a12e025651507f662c4be5471671f3f4a
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:13:23 +0000

    say gift pinch delight noisy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
