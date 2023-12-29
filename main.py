5ee613f15cf36ff7dbc14b535ab4edb38693561a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:28:27 +0000

    shoot independent whistle trust much

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
