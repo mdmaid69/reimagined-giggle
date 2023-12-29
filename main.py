01f784527fd774a19399e8342c066e3eb7abe5db
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:53:58 +0000

    project rub do teach star

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
