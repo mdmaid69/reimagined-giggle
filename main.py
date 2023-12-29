81eeba19d2ee6703adb0ef9a7c7df0b86a050ccf
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:30:46 +0000

    fat centre its gap allow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_work(force, distance):
+        return force * distance
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
