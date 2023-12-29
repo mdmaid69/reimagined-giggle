d7d2cd191fd76c59687587450c1dbdafb504b1d7
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:19:24 +0000

    none feature decide summer earn

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def is_odd(n):
-        return n % 2 != 0
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
