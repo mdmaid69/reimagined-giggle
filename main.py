07279666b207e375d5c1ef742caced3c56a213e1
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:02:31 +0000

    tend medium wash funny copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def cube_number(x):
+        return x**3
