06bacc4b8956f7d064e4e7f8f39581eb3df12475
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:18:38 +0000

    city length face runover combine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
