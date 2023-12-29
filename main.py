7b1addf3f1270ec63a4d78590f5cbded290fd45d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:09:35 +0000

    alone oil full bear convince

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-print(os.getcwd())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
