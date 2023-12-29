c0488d864a1f5df105facd9d34568a68ea9847d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:38:55 +0000

    plastic smell poor pleased delicate

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
