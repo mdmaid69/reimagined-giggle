d531ee32b861c8a93b688013cafac98fe4d549bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:29:00 +0000

    rest speed bread knowledge comeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
