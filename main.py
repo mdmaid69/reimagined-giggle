4a46608bd51b6e7865b29c77d7fad3601e462acf
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:07:32 +0000

    defeat similar away speak already

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
