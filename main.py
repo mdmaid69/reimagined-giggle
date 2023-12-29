e5349e0632690bdf15503f94a76b2393f9aed794
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:14:38 +0000

    tidy myself scrape constantly spare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
