8605dead464e08c968d02a7823bfe0003bcc105a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:57:29 +0000

    long of waste task label

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
