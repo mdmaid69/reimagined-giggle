2329bc382cde49d7f93c23d28cbf7923b603ee5b
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:09:44 +0000

    niggaacademy so head really vanish

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import math
   def calculate_square_root(n):
         return math.sqrt(n)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
