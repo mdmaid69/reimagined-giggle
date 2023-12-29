e64c1c454958003a09c966b4db05a3161092d3af
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:18:08 +0000

    test finally save forever talkback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
