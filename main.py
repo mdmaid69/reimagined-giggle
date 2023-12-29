1beed53eb08d29c5e6bc392d999e11128a3e6ec9
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:41:58 +0000

    exercise collect instruction smart hardly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
