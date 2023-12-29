92cfe7293496f48366e845e459f1c07417a15e2c
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:27:24 +0000

    Zz curious contain fat forget

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
