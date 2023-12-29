7828183cd60637cb95738c8ff7e927102f3b2c10
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:58:42 +0000

    bear turn design extraordinary tragedy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_density(mass, volume):
-        return mass / volume
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def remove_directory(path):
+        os.rmdir(path)
