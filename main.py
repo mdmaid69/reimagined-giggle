11da8c3ec234c35c0f8e01c74752aa7699253af9
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:59:01 +0000

    thought remove promise pause display

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_density(mass, volume):
+        return mass / volume
