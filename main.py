bf15c563ee661501cdf717115c0339063e89fcf2
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:58:12 +0000

    fat tool over harm float

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_item(array, i):
-        return array[i]
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
