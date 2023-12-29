21d076d4b57dca0a3aa73ff45b55525f52f39665
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:51:07 +0000

    straight clear protest rain Ll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
