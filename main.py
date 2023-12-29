e86e2121360e36c11dcf4186ba5dacd44a526e34
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:30:40 +0000

    this fold shall switch short

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import collections
+def create_queue():
+        return collections.deque()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
