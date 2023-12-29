65aaa9c4dcbbaaf4f9277bb9c24e94d73a6420a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:51:35 +0000

    mother else lock quickly to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
