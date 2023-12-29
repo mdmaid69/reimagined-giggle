fc5a16a9925e6dd7a45a184605bec4ca9cf11005
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:33:50 +0000

    judge accommodation comefrom scream reply

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_min(numbers):
+        return min(numbers)
+def calculate_area(radius):
+        return 3.14 * radius * radius
