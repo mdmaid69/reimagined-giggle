07323c7a97d0d39647e92bd796f8a21ae7f7e74a
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:51:18 +0000

    care allow injury skill unlucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
