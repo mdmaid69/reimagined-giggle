2b064bdab66b26a57030514db6157baf5a08924c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:56:31 +0000

    dance lesson record hole rob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_arc_sine(x):
+        return math.asin(x)
