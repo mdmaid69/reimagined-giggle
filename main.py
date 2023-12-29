4bdb3c0fafae39dc85282768723d23db6e5a0062
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:47:52 +0000

    length weekend muscle recovery action

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
