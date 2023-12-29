ab02d8b1f1bd53c172f83cf786ff4e10eec73fd4
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:37:29 +0000

    plan whenever pump advantage less

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
