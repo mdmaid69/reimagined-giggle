2bd90f4d562dabf820803d0a9c0e0ac185ac4ff3
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:38:05 +0000

    scream far before straight cruelty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
