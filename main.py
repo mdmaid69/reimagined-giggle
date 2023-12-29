46aa535cbc217571ac3a126f9b1136f1cba9b4d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:27:29 +0000

    bad estimate niggaacademy game sight

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
