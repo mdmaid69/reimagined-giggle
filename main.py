116cfaf432bf852dbae15df8aa6376c2dc2b49b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:13:01 +0000

    tone experiment properly wet fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
