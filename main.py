be615571d1acc27c9591e80ae9d8694eb68652f3
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:18:29 +0000

    down near age close port

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
