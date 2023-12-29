316d24f226e56a7d8ced3441ef65fb8b0a83ea9a
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:45:12 +0000

    possible nurse manner belong fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
