d687d0392651529a1c37e6f4029dd3e80c20b576
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:20:07 +0000

    healthy godown tool almost march

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
