677f3f75b632bccde0c1ab5b6af35dddd9a2f6e8
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:37:41 +0000

    machine punishment bet rub ring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+text = "Hello, world!"
+print("Words:", len(text.split()))
