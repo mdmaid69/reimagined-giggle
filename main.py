1ab5f24d763a6a56baa00f9cb962270dcbfec517
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:45:23 +0000

    drink exhibition same neat offence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
