0fb1397457a04bf5c059fd6a8dc344e1e67ecb3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:44:39 +0000

    voice priority however contrast turnback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
