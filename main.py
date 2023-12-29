3095d5be6e71b52bb5b1f7b044633d1c81a8ba76
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:22:06 +0000

    hope beat sudden shrink tragedy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
