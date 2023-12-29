c55e64416f92651bc3fbe7ed7a38006a4b3470e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:25:43 +0000

    underneath day dress burn carry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
