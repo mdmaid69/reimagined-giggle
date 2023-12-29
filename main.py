5181cbb39acd3f60399b7467fcb6f55a5395606d
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:35:55 +0000

    tap patience direction call broken

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
