4c7e8fafa4151c575320d8f67e927991c7cfcdc8
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:34:42 +0000

    past university wealth group within

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
