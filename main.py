f0867f0ea990a4affd9be48527ca692505cf5ae5
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:36:25 +0000

    file story hurry deliver natural

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def subtract_numbers(x, y):
-        return x - y
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
