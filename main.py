158a8600046cfb093d03f496273be1b5d7c7212e
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:36:02 +0000

    difficult contain swing break subject

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
