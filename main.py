193eb96265603dcd2529e8ef1fbc491f52d8b77c
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:00:21 +0000

    none burn ready patience freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_power(work, time):
+        return work / time
+import array
+def convert_array_to_list(array):
+        return array.tolist()
