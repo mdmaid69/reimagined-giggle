3b805c811ae4fdb6ce53c45f540791770092376a
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:03:01 +0000

    Yy impatient calloff sort approval

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
