b238e741eebdee44a4a8093443cc4f80b8a48257
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:41:34 +0000

    gift group path dark dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
