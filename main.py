880b1979b9b43e53af6da368d92532b722bfb848
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:30:20 +0000

    match behind achieve callout shake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def reverse_list(lst):
+        return lst[::-1]
