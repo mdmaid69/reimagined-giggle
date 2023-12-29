9223ea813199e9c822ccc52fac839ff5c4c960fc
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:48:40 +0000

    because whisper phone occur suddenly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def find_max(lst):
+        return max(lst)
