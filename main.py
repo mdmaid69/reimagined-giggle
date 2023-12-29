afe8b993fc4e9072b96087821f146dc69fa0855d
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:58:36 +0000

    secret active popular woman doctor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
