7c38a8a115f6a7c7f8285fd180c2fbbe0275af19
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:47:39 +0000

    pass by under holdon up

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_current_time():
-        return time.time()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
