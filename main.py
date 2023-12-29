078f77a6103cd3c127977b817ed5f21ccabfa569
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:10:24 +0000

    listen worry separate bite really

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def add_numbers(x, y):
-        return x + y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
