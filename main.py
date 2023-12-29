a6c558e2636cd3ceb489bdadc1139da749646936
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:27:45 +0000

    opportunity rarely might image wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
