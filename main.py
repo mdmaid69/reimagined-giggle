b73ff9c20379de56d3e8b488be034ffa92ba5e72
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:08:13 +0000

    trick past disturb model common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import datetime
+print(datetime.datetime.now())
