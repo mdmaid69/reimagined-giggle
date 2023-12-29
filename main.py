acdc860a26c7d5de76ab910b0692e86729ef98ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:50:32 +0000

    city definite union bet old

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
