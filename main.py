e6a8659c248c30391e79b5c92ac4b7923cf30f63
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:26:58 +0000

    exchange day born land copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_error_function(x):
+        return math.erf(x)
