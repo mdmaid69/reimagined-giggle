f17a6e1964148bb0c17b0f2ae07d69353f6bb68c
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:10:08 +0000

    spare major price large object

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
