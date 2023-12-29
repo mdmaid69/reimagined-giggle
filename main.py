4e302fb92f7113e901337029e2688f6b956f2cfd
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:38:20 +0000

    minority give base him comefrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
