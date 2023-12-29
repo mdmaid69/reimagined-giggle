4951f5f08cdf4d32acf52f78aac5d7bd8ef0866e
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:18:11 +0000

    development bunch wind regard properly

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
