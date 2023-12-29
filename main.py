790629544b82dca64ea016f28e6a682ad6d89128
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:17:48 +0000

    wrap bother pump discussion upwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
