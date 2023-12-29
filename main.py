616c2a433e9b9a023bc4bb13d6d4d2ac20aae3a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:37:20 +0000

    transport march communication keen look

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
