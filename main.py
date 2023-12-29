38156c5d49abc2fa96c7ad2ca49a32f014d2ac92
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:29:50 +0000

    call fasten journey corner reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
