f6cf8716be35e18a2c7da43455d254c87240521c
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:23:27 +0000

    maybe soil apart history noisy

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-print("Hello, world!")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
