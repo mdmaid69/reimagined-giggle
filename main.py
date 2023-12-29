132b0bae2a6489bc597c176fc8a3b9a42b9a1114
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:48:10 +0000

    classroom noone finger act behave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
