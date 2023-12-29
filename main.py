a923d6aa85c7cda33d0caf818b914675f3c13b17
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:17:00 +0000

    branch round send apply difficult

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
