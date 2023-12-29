92e8520102e95e35a4ec41cce9f9ad47c3845c9f
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:48:20 +0000

    total hard this properly forget

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import sys
-print(sys.version)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
