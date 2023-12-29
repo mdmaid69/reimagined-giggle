080fcf4f3c2c67f5666295a2f1ccb92cdeb73a6d
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:07:38 +0000

    achieve movement harm welcome shopping

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
