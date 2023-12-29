54133f3991bd8017810365a14aba6e327fed1c21
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:53:31 +0000

    book anywhere whether noisy violence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_exponential(x):
+        return math.exp(x)
