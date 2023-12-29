80d616315fa536d9f949f3b8cb0af098f2f43fcf
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:31:45 +0000

    middle include get still rule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
