87c791ffe05f619495980868ef3e0f4b41538660
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:17:40 +0000

    affect pump grade on where

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import time
-def get_current_time():
-        return time.time()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
