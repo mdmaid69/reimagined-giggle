1ff4bb51a1879bd7229b26d7c2613dd4c2024f5a
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:11:31 +0000

    thought neither style cut contents

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
