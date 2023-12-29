41c3ceccbc5fd28cb3882c0556898ea26aed9b25
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:33:40 +0000

    life set difficult education strange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
