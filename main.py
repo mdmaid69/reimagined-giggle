9a8699f369a3deb5175cfb84db36ce8f4e99620e
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:21:18 +0000

    month reaction minor practise on

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_power(work, time):
+        return work / time
