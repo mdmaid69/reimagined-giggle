a9f74ec4c9cbe477e9b7a23956e012a6678a173e
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:56:11 +0000

    peaceful possession joke business star

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
