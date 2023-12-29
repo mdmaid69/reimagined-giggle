0e5288263ffccc3d27b6973d5d4224f0ac56dafa
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:40:34 +0000

    reply mixture broken waste late

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
