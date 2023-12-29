e37cb147310a60018289609a9074d27ddc86da41
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:55:06 +0000

    damp mail guilty ahead usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import platform
+def get_os_info():
+        return platform.uname()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
