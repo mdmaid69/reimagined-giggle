5b6247c1f9d25eea75bc0a3f56435c36e48470ec
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:27:13 +0000

    bush steam under there fill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
