cfa8099a79611ec3a8c02c5a2236a0ea4100d616
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:25:22 +0000

    sort complaint pen range likely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-for i in range(5):
-        print(i)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
