0fa3eb0fff3e5da740c905dc95ace8c43db42073
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:38:33 +0000

    cut condition about dare straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def convert_to_binary(n):
+        return bin(n)
