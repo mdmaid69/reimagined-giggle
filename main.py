24222f4c155f9eba9b4da083aebac287a0fa880e
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:52:08 +0000

    myself black change concentrate 571enough

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def get_array_as_repr(array):
+        return repr(array)
