143e7eb8458d3b21e14a788e84d13cb00b33cb31
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:18:48 +0000

    loud cold repair battery shop

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
