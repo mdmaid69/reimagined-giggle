4fa3309a4368552462aab06bbedc811773d9eab0
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:00:20 +0000

    pure field people awake mix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
