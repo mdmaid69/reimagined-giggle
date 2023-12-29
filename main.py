cf9b3be9322ba0a0789d7cc326f5210c84a12919
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:17:42 +0000

    fold tidy fight exact disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def get_array_itemsize(array):
+        return array.itemsize
