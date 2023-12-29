e258b514cc0e2ff5f7576f4f8fe663f21c913357
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:30:42 +0000

    create exciting ashamed Jj fast

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+for i in range(5):
+        print(i)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
