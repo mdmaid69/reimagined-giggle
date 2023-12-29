a2c374929ce713487ab30995b7bdeaedce75b49f
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:08:16 +0000

    mouth job nor protection turn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
