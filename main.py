42c25f678d3eb6dd310f488278516d9e26a94555
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:30:15 +0000

    keepaway increase expect tongue duty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-for i in range(5):
-        print(i)
+def cube_number(x):
+        return x**3
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
