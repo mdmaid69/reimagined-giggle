998c6f0bfd3bb5b56822bd37e0dffeeca3dea75e
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:10:39 +0000

    somehow kind unlucky precise correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_odd(n):
+        return n % 2 != 0
