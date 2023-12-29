22a6e880902882c28159df4292e092c9309c2263
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:52:47 +0000

    pass suppose material stick bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_cosine(x):
+        return math.cos(x)
