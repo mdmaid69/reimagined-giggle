60a0de303bfe4dde5d720e5546430dae626468ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:41:40 +0000

    approve health chance meanwhile shallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
