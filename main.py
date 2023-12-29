249ebc940034029bcba051423265e6c39e719d42
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:46:06 +0000

    shelter reduction female off bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
