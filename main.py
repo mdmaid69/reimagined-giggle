2a841d7703875fd50013c06c62e96ad9d52f7b28
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:06:11 +0000

    wealth news noise stamp comeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
