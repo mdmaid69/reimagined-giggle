440b4e0bbc0768e97172a67f9e4de3a2581da4ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:19:10 +0000

    evening official train impression success

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
