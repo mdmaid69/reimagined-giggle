cd6f040390b17243d8e9f759e6f6a194acbd1d06
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:37:49 +0000

    whistle exciting favour statement murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def remove_directory(path):
+        os.rmdir(path)
