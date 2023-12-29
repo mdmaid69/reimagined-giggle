afec241a91d72f5be6cd541f299d604f2869e7be
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:50:13 +0000

    lock own lookup stranger past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
