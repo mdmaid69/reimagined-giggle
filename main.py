eb3a8a04dee516134224613b76d0aa628004d52f
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:26:30 +0000

    emotion level keepout belief contact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
