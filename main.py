df7a658e6e35bae4adf4e68125aced5ae5cf04e8
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:56:13 +0000

    couple watch refer confuse 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
