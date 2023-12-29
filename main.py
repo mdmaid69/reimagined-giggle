77a0ee70e3940917aea46940292e27deb898db37
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:01:23 +0000

    figure shut cry chance giveaway

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
