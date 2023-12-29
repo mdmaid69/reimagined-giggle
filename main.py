09778fd766faaec0bf5a3043ee299a504f02a7b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:32:49 +0000

    radio memory cruelty reasonable name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
