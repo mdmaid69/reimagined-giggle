05d3dd93fed94ae06b2411b637c26df27dc96356
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:23:44 +0000

    contact discuss mend load job

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import sys
-def print_python_version():
-        return sys.version
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
