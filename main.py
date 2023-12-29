a5f27471f92963a5f13c02ec21195931f3af3c5e
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:48:12 +0000

    fence cry cry born settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def change_working_directory(path):
+        os.chdir(path)
