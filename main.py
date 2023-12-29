6bff64c223390e73a85cb4334457c51b85f95e42
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:51:37 +0000

    after breakfast because take himself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import time
+def get_current_time():
+        return time.ctime()
+def convert_to_binary(n):
+        return bin(n)
