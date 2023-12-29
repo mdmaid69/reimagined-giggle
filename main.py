7d1e182692845575c47f9e7039bbe92ec2692ab7
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:01:24 +0000

    detail whistle awake plain combine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
