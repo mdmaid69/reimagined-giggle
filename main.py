4ea7c5c311652a587f9fb1c6a8568098d06a6253
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:33:50 +0000

    spend future culture risk summer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
