d8699b1f5cc4b3c92d8f2bc6c8b212818a47acd2
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:15:14 +0000

    peace night clothes yesterday pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
