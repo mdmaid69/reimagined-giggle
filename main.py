23361eb3b93a99a638339134076d3edfd15a1961
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:22:39 +0000

    motion gather bunch risk certainly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-print([x**2 for x in range(10)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+def get_current_date():
+        return datetime.date.today()
