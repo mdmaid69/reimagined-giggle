5eb841cfc2a6356eeed4e28f443900dbc15d00b4
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:03:11 +0000

    remember although respect soft until

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def convert_to_binary(n):
-        return bin(n)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
