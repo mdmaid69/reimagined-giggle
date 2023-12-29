e941c29a5c38dbfe8d400e8c390381024a0abd9a
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:26:41 +0000

    dress tread underneath release target

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
