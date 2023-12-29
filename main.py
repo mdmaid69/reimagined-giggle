59189a2561f9fd01c988985dbc166e234e4601b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:20:43 +0000

    letter piece total broad minimum

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
