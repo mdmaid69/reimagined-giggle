c08ef288f95d3e49101093b10750b4d211a697f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:11:01 +0000

    instrument sweep gradually just sign

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import datetime
-print(datetime.datetime.now())
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
