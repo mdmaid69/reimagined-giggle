402fe8fb14ea4f57403e9e93278fe350ba7d368f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:37:52 +0000

    567enemy contact plan really mistake

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def subtract_numbers(x, y):
+        return x - y
