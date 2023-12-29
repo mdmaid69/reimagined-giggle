8007200946dd8d798886ec9e11727bc77a54c10b
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:55:44 +0000

    comment minimum connection tomorrow everyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_max(lst):
+        return max(lst)
