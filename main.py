a36061114fd8b94a0f20071bd98ae25738b0eda1
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:19:05 +0000

    loose proper juice distance instead

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
