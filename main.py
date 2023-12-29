ea899bd5b4a904e00e22f29d430fc7049383837b
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:17:44 +0000

    present it sport funny recover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
