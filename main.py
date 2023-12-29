463df92daee56e1e39d790511324468a7f557329
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:44:19 +0000

    save careless shop worth cruelty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-name = "Python"
-print("Hello,", name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
