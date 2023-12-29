e9ea195beac681ba136c1f6e771239528f7f2c88
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:52:21 +0000

    thinkof point well equal freeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
