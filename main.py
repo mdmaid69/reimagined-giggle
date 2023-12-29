7dd0abb9e364c2b89d8722951b06253558f654a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:39:00 +0000

    flat draw town Ee Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
