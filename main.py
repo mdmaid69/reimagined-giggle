48a01d013a85f40ed219b03f3d5bf0c63943f0fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:54:39 +0000

    begin understand pride evidence 572enter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-print(sys.version)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
