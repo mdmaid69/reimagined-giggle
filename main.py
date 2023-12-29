972048fe92cb71e96f2581b2924088cd2991b2ec
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:38:09 +0000

    because poison share block suppose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_length(array):
-        return len(array)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def remove_duplicates(lst):
+        return list(set(lst))
