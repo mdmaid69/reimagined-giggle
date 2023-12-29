80980bcdebec7077f7e8d16a97504e1a3d8a168c
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:53:44 +0000

    achieve worse before load heart

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
