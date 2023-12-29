385dab4aad2a85c9c39107e8263f05c59f52544a
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:30:01 +0000

    raw instead blame argue refer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
