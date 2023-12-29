0b334978c3a84d17f779ee98daa6365254f92e31
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:43:25 +0000

    scream rain warning bored mean

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+from collections import Counter
+print(Counter("hello world"))
