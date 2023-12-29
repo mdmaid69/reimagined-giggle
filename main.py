4bd975ae8e367237e825b1d3a5e1ba884783c286
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:43:37 +0000

    probably demonstrate hurt blame gift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
