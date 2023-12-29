5f97364e5d5a2c1dffacc54fb375466ae131c3fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:18:01 +0000

    behaviour force arrival home boil

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def convert_to_binary(n):
+        return bin(n)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
