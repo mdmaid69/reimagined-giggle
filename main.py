d042e8825bc069e07af05b6d349ac35f7060a1cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:38:02 +0000

    relationship majority issue price transport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import datetime
+print(datetime.datetime.now())
+import time
+def get_current_time():
+        return time.time()
