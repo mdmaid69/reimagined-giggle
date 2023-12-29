1b2dbd86391e29f7296d9b5b463c188be57167ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:20:29 +0000

    broad cheque date pattern here

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
