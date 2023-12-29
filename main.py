098f87a18f56e3b4e3ad97d3b6059efa6e7da18c
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:24:18 +0000

    treat extraordinary street sometimes mystery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_exponential(x):
+        return math.exp(x)
