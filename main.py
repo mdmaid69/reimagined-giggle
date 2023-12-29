7d37ca2aa17dbb26e1f29ef93bcc210eb4020710
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:14:29 +0000

    false shallow shout live dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
