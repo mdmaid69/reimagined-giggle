ab3327cc07182d008a3342ce1936746d8dd1afad
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:06:49 +0000

    battery you impression start belief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
