99a3b69e3f24bba8bfb39c3b3933d87007620365
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:42:35 +0000

    callup stop platform sentence cold

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
