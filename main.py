9d12c9570d37606fd6c5f324bbbc160520b2f222
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:24:49 +0000

    healthy block can because callback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def is_even(n):
-        return n % 2 == 0
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
