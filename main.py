7f6b7941e027e8dfab27518601770d789483fe37
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:01:03 +0000

    become spare combine occasional accommodation

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def convert_bytes_to_array(bytes, typecode):
+def convert_string_to_array(string, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        a.fromstring(string)
         return a
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
