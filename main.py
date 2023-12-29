eca14a35fec1c9ab3e518ef66ded2a8c88551818
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:52:30 +0000

    some news whatever mistake fork

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def set_array_item(array, i, item):
+        array[i] = item
