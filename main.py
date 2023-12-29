d8741bb4262177aec997436a30d21d8d0ced1a89
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:40:41 +0000

    declaration switch breakdown perfect cruelty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
