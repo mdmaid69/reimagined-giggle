89faaea3e706caa689b0c0a5a03967ec85bb993b
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:28:29 +0000

    dig excellent easy complicated main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
