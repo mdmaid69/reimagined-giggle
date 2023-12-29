cc20e997e6e30c1743983e4409208ef17231f83c
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:49:22 +0000

    afternoon dream experiment study light

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_list(array):
-        return list(array)
+def find_max(lst):
+        return max(lst)
+def is_odd(n):
+        return n % 2 != 0
