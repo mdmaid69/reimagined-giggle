04daccce535761abedd6fd37e1ef4f78ff66ba2a
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:27:50 +0000

    meeting chief tear bread grade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
