53c9787b55093b6e819528058e89587ebe6508c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:04:33 +0000

    before them clear under sweet

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
