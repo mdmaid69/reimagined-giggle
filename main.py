b23c9cdf18ecb9c26cfddbf7a170d0512cd5a265
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:58:37 +0000

    everyone first poison moment worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
