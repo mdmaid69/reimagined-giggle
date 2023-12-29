8c20040c23bc79b9f9933f597257842651abc697
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:19:38 +0000

    dry convenient pay activity interesting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
