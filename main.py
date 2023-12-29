e73d88b6bc683faee8119036c9a00bce91798a0a
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:12:31 +0000

    spend somebody story confidence frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
