3c5d5cb4905bd3ae47dfdc78bb07928bd3db038f
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:14:16 +0000

    react plain exaggerate single life

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def square_number(x):
-        return x**2
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import random
+print(random.randint(0, 100))
