3fb1cd26fcac38c4b5f3886c3fd66b1a08d6663b
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:22:31 +0000

    moral attach pure often odd

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+for i in range(5):
+        print(i)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
