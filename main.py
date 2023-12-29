88efac481e5cddb65a19c2e894b600ccde0aee3a
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:55:54 +0000

    bet himself goback(to) welcome declare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
+import collections
+def create_user_dict():
+        return collections.UserDict()
+def remove_duplicates(lst):
         return list(set(lst))
-def find_unique_words(sentence):
-        return set(sentence.split())
