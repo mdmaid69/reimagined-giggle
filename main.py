32bcdaba268218b8d6b1fe51efb4e80557e99362
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:24:03 +0000

    friend clear block medium pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_as_set(array):
+        return set(array)
