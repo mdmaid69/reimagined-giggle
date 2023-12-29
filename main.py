b5c4befde36e6ae9ffb6ef231199c78459cc49cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:40:32 +0000

    be confused drive charge butter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import os
+print(os.getcwd())
