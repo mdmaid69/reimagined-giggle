7647d0eba3862018690e4006608a0ba52e441128
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:55:17 +0000

    tool concerned but ill deserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
