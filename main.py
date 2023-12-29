9a86aab7820a763649e627ee70425c3f83c4448c
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:24:46 +0000

    run myself scheme vision after

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
