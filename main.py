4b18e6c6d476cb768d3ef64cc4aff6d61269a42b
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:26:28 +0000

    variety enthusiastic wave that Itself

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_float(array):
+        return float(array[0])
