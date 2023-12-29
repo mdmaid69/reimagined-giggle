d30f7ef1fdea3cf857179e59744580a0383aad8a
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:44:58 +0000

    quit wide most mistake unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
