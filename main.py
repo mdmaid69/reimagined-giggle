05548b03dddbec0089541694dcc1efd4fafdba54
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:40:17 +0000

    interesting reasonable fly shrink knock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+for i in range(5):
+        print(i)
