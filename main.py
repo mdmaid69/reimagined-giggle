98c15fb4b0528b92153b03b33f4c9ffddc1e265e
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:19:10 +0000

    fine flat noone road score

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
