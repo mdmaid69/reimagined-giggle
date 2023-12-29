80a5ec1b9c255e9da775fce066c2aaa5d8c78e90
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:06:51 +0000

    high mess thank crash foreign

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def convert_to_octal(n):
+        return oct(n)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
