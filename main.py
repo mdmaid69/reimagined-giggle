e4adcfc11b21214bd74c741d3bf48d86755d47b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:30:38 +0000

    prepare entrance family body stick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
