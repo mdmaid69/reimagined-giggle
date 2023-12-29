de7856fcd33b96f0f7260f6e92f2f0841d18dc65
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:00:32 +0000

    eye nonsense tragedy cold about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def find_min(lst):
-        return min(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
