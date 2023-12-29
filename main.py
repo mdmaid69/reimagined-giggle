5351053e68c1c37dc2af83a6ec52070c1fa07a69
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:24:59 +0000

    stick to average second fail

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import random
+def roll_die():
+        return random.randint(1, 6)
