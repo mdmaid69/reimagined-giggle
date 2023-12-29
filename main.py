7f36c5b754f07dc05ca91b537cab6be6e80be265
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:56:57 +0000

    freedom discovery shrink every surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
