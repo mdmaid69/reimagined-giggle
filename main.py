2758d3213023f4ac96d5f4faed7ca2382fca88a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:54:58 +0000

    condition harm official answer programme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
