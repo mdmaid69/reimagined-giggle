d700571eb7c978aadf6805186fa2600193f1558c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:36:36 +0000

    nice bad danger classroom honest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 def calculate_speed(distance, time):
         return distance / time
-import datetime
-print(datetime.datetime.now())
