504b8b30c2a0a064011f4b43e5e7b0665267e980
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:03:05 +0000

    at thinkthrough OKorokay scare phone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
