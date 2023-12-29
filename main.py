87d57b0bba9ca213157fce9d1b9e18f4759f8547
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:40:47 +0000

    walk problem drawing public close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
