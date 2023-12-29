a0f2a7147270621492d0861d9bbac257115ad887
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:42:11 +0000

    comeover giveaway lead figure flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def add_numbers(x, y):
-        return x + y
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def append_to_array(array, item):
+        array.append(item)
