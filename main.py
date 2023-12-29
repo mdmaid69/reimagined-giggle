2721b2b27d9c1412165d5d7c2fe7f55d7cdeb27f
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:41:00 +0000

    away murder more worry declaration

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
