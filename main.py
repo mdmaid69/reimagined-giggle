b6cd463c3b8b65e742b8055f11ab320721f27312
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:26:54 +0000

    scare help disappointed use schedule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
