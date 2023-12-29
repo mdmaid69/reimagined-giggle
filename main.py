da0f09a2f1eed701bef596cf8395da17846e2464
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:43:45 +0000

    minimum criticize big busy sight

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
-print(sys.version)
-import numpy as np
-print(np.array([1, 2, 3]))
+  def sort_list(lst):
+        return sorted(lst)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
