bf9ecf2fbaad0020e48b0553c2551cf215618b49
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:17:18 +0000

    occasion mail pay hear sweep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import tensorflow as tf
+print(tf.__version__)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
