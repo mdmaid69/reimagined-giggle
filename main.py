2b4e16af450a758dbbfe68f4ecca18b81a286d6d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:21:34 +0000

    angry hope leave much mistake

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
