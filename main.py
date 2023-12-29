192162b60d0f704569c24c39c0e1c4ca60933e2e
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:39:49 +0000

    nearly mind strike especially importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
