875e238e9634f743b3021c6e546cb68bd6cd45d1
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:34:31 +0000

    freeze someone treat neck regional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
