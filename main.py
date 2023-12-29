80ffd5868081d8a268ab425f8dfdf39ac5fd6454
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:02:45 +0000

    getout organize prefer occasion common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
