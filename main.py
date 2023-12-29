dfbd26a4ed0c5960079a1fdc941abf3b641f92f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:27:08 +0000

    complicated firm detail deliver cross

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-print([x**2 for x in range(10)])
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
