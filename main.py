2b48ce37cc6bb0cde0cb0f4c3833a514b89550a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:51:50 +0000

    time can step tip used

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
