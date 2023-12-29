ef5d478cb58dbd5c6e035df290ca0009e97b0888
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:01:32 +0000

    second possibly trade getaway figure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
