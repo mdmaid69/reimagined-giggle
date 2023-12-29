50c31397a7490e956fd848b81631774af76af0df
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:03:14 +0000

    sign separate before worry market

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def sort_list(lst):
+        return sorted(lst)
