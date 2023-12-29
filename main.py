facbb249cbdd1fe6a4037eae3236edaa9c3c03e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:14:08 +0000

    tear ground tune receive hollow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
