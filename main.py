d6621040bca5c2a1c85b47bd726ca5f061665807
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:52:02 +0000

    lock public breakinto mine practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import logging
-def log_message(message):
-        logging.info(message)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
