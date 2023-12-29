0bfa4b5a6746e055535d0d566eacec77ad557020
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:34:53 +0000

    moment so practice will address

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def convert_to_octal(n):
+        return oct(n)
