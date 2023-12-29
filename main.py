3be68036ce6f883f99491d0774e94f0068340ae8
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:02:57 +0000

    slowly poison region begin nose

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-def calculate_pressure(force, area):
-        return force / area
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+print([x**2 for x in range(10)])
+def calculate_work(force, distance):
+        return force * distance
