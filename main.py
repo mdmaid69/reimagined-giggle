3dbfa792481ca38d0fc06e07a68611faab4b4b30
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:40:12 +0000

    better choose mass copy confused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def convert_to_octal(n):
+        return oct(n)
