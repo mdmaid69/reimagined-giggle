e4fd4e758a1a4ef15addee05414f26d4cb00e946
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:35:37 +0000

    round like stop dress treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_volume(length, width, height):
+        return length * width * height
