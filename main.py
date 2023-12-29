2fbdbfa9e526ef8fd3cf1d239ecb2b20732819a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:29:37 +0000

    fall dead find alone opportunity

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
