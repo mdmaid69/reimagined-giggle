952d0067505b5815fda5df3370279d9febb56680
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:17:12 +0000

    job none exactly far love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
