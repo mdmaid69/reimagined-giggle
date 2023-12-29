f8211e1b9d447694b73b2c72894bcca127e7aa8f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:53:24 +0000

    machine definite trade turn noise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
