c09b6dab2a36f2775e3ad135c4fe3bdfe6baf430
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:16:38 +0000

    any apparently dry risk variety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
