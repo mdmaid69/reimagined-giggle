5c031742c88944b11e3bd9eac09d6c0fe3f7240a
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:24:29 +0000

    art chin suit gap comfort

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
