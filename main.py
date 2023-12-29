98b8b8b0fc600d3e0f7134707a0ac7e5746345aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:33:03 +0000

    someone easy pen take pick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
