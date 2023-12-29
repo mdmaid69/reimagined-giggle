ba71d94da1e7977ca6ed9d4e6b61e9a5aa8f2afd
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:52:38 +0000

    exchange couple I mystery information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
