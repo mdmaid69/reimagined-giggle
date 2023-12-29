80c0ddc814bb033ef4cef805e58c1e3678a502f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:14:36 +0000

    achieve Ll fashion borrow visit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
