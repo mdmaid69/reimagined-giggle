29ddd08e533106be63294b5380472a0590d1a0da
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:46:08 +0000

    Vv breakfast clean performance still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
