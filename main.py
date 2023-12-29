5f7227052ccaf78da9ca4a2e4e1bd6ba539cbd25
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:37:55 +0000

    attractive band scream accommodation inquire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_current_working_directory():
+        return os.getcwd()
