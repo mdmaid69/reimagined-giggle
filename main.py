e05dd38824a7ef41427511dcce7b1a752a3b282b
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:22:45 +0000

    history cut apply offer number

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
