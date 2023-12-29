5ebfcdb35cc5f88e061b984d319f20a8a7cae147
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:45:43 +0000

    succeed which neck willing weekend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
