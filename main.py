3f073aecb0a5cc2f634b02cd232ec5086684a172
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:00:52 +0000

    experiment whistle load deliberately mean

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_density(mass, volume):
+        return mass / volume
