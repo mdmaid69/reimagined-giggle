ca2589325d08ca048d451f559c19ab954c32c143
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:37:07 +0000

    excuse would powerful that honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
