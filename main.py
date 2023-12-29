e5c4d482217ea82d5b0324a7a9a5d0fb671b1a68
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:16:27 +0000

    move touch correct obvious hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def add_numbers(x, y):
-        return x + y
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
