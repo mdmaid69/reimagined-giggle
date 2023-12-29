0844a7d000b1fa348af2c43970f39a0cc6d5513d
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:26:24 +0000

    tired invitation belief invite willing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
