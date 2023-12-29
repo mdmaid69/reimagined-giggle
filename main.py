5b184dd27006c81ebdf02538b4a8b287049be2fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:48:50 +0000

    post hope activity dream distant

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import math
 def calculate_square_root(x):
         return math.sqrt(x)
