7dc61bca173294bc8607b614cc10bae00f5e53c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:42:15 +0000

    know independent walk tap so

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
