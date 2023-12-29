c2f6878b111e5846bfb96fcde90521cc873c69a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:34:40 +0000

    forever themselves interfere otherwise hungry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
