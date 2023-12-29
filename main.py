52975f8ce61acabe007615fb019c4b6fd806a10a
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:22:30 +0000

    temper bet stand 566end happy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_bytes(array):
+        return bytes(array)
