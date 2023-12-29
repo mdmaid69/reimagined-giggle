93665c3a648dd8fb8321cf36a31ae4ea9deea7dd
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:57:56 +0000

    male single related chat call

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
