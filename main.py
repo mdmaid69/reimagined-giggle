7482733d8b954ea478ed9e86462e4601070c068a
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:24:07 +0000

    job value case square nearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
