71ed76edb30662ba0c0bbdf7d810114d6723826c
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:39:16 +0000

    town waste ring that I

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
