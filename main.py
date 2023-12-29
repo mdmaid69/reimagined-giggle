f1a233cb717b649e99f899a46fdd05d40eba6ddc
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:14:07 +0000

    disturbing sky put raw its

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-print("Hello, world!")
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
