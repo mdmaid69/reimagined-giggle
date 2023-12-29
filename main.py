b76bebf37b6effc632b54f0a1f90097e73f29bc1
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:55:26 +0000

    any tour which popular permission

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_typecode(array):
+        return array.typecode
