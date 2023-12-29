9f20745a09543652cb3c84e23f210e30bc660aad
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:40:20 +0000

    sea accept valuable choose Uu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
