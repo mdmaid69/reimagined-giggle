368b7a2d740f091717647413fc8744d0c8c8fa4d
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:12:58 +0000

    suggestion public definite obvious dig

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
