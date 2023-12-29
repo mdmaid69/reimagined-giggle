10ce18281444933704066fcfa870ec944902168f
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:53:18 +0000

    power careless appearance main weekend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
