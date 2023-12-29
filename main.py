1eb3671f70f38d3882eb14e65174cc1b212e67c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:40:23 +0000

    famous mean copy of risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
