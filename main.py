9bf33dfbe5058abd240361f6c80fdb2fd1d2b833
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:54:37 +0000

    safety tongue plan who doctor

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
