a045a855af818f71c2f7a29c87bbda5d7f30f767
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:57:45 +0000

    teach represent card satisfactory repeat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
