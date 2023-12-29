a00eb69b9015103b2e65a7a106f0514d8c1a1af6
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:18:08 +0000

    pay rely communicate question plan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
