8735aa7bb0d235e6ea77cb32e215290f6cb46bc2
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:10:06 +0000

    traffic heart so stiff goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
