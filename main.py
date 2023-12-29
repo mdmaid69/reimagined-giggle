9eb255ee9d84ef2b95328ba30f3988c5a64eceba
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:42:13 +0000

    preparation at speech guess imagine

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
