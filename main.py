a4d873e12fb70d75cc9376d38d2d7f5ec17b5d9a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:53:58 +0000

    replace match show extra watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
