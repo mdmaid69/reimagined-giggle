db05fe22583a34c22c95a0be732cf6fd62f17d3c
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:34:40 +0000

    should square wish lesson true

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
