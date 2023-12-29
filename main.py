7619435a8dba23363dc411c342fab605f2a1aa24
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:36:44 +0000

    hers region mark swallow across

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_typecode(array):
+        return array.typecode
