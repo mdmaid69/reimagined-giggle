a2a39ed91b5890011cc0008221d75fad05b854d7
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:17:55 +0000

    check imagine geton pick float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
