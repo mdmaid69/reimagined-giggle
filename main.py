0865d92746b22063167f5c09142a0a3342e4e50f
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:57:37 +0000

    correct properly artificial Vv experiment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_area_rectangle(l, w):
+        return l * w
