ab3a62882a1168afd881ec3dba0282a830431eae
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:06:46 +0000

    rare confusing meet spray sister

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
