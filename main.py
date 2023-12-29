14e5d6a19736287c430458ee86d96e05a07432b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:34:05 +0000

    another thinkof hope either distance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import time
+print(time.time())
