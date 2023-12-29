f4d4932d4cf19cc3a7a26eba3f39108b1700230d
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:29:11 +0000

    urgent force doctor doubt sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
