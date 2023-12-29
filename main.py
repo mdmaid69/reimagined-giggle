06abfd7fe9437b3b48720b06830a5f07ac9d2b57
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:58:02 +0000

    active translate hurry subject sleep

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
