2c6933c8ba67093d1d4b299d02ab1f9dd1ce5562
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:56:51 +0000

    journey motion slightly eat themselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
