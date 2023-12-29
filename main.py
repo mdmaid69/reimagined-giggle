50e9d638d294107b174f04ce97ad417f9280e1e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:37:09 +0000

    top flavour path process call

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
