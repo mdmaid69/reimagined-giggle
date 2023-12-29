4d72ad7d5266b31b9f46203f7b4f74f1f53339c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:43:15 +0000

    period keep tear interfere artificial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
