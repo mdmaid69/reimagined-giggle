5defb490bfcd12be10b510ec752dd7f5fbf89e16
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:49:23 +0000

    seriously even aim pain appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
