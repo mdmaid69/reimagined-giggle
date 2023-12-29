d08b4d2173109fc1493225c4c1a8a7aeb76c8cb4
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:39:30 +0000

    slight system represent check chief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
