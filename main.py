eb4eaf863de6ebc503eed0f9c3ff50614e0bcfa2
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:54:00 +0000

    matter beat recognize excuse defend

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
