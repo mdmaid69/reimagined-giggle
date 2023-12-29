8d4a630e04e169937c50a7855b1a238bee95a160
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:09:59 +0000

    translate that pull pay difficult

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
