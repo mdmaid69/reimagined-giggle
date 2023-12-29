c7461c3774bdde93fbe11f013ed4392991f6a456
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:09:16 +0000

    successful reaction typical bottom target

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
