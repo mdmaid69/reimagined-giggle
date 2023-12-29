d93c7ff5833198b5b6aa5d3c8870f207b363f539
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:42:17 +0000

    cost about drawing entrance surprising

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
