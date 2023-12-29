4254662a17d4f52ee25b354bd7a3f7412d5db445
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:24:14 +0000

    march part side blame attract

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
