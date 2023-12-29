05880f086eb2440f61279eebad9924aabd2044df
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:00:31 +0000

    accommodation represent emphasize reduce moral

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
