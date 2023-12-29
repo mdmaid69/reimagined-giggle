5e1e59cde3c16e919ae7926152ccd982390a23e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:28:56 +0000

    tip slowly left least even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
