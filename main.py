e40f897e9131da2ca51e07f7903d37b057927815
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:36:04 +0000

    quantity production pocket copy beat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_as_list(array):
+        return list(array)
+def find_max(numbers):
+        return max(numbers)
