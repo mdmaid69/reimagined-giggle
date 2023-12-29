ed85ff893c990b382df88b07b6897fbff38b641d
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:26:01 +0000

    turndown further grade level bridge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_typecode(array):
+        return array.typecode
