1cfb4ab3e7b0dd07d4ed93b7e073b55b5d062beb
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:13:29 +0000

    independent classroom us present introduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_cosine(x):
+        return math.cos(x)
