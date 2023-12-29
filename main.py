08ef9940a5d7d37a2365084f3e1ebb0b86f445dd
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:39:59 +0000

    female hang brush scheme influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def reverse_string(s):
-        return s[::-1]
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
