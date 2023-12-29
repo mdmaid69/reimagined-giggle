e6a63205ce42acb8ab38b594547e0048d125a89e
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:58:54 +0000

    of organize become organization OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_volume(length, width, height):
+        return length * width * height
