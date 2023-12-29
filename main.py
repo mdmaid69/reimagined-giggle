1962d3eb4d270c467103a193158650171e2f43bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:16:08 +0000

    climb yours seriously soak make

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
