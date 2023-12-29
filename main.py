2f50beac852c1c2016a9adfa5c4d3383d944c735
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:26:13 +0000

    Yy recovery fresh land turn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
