01353939837626fc74544ea63027fc8467c0a79e
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:50:32 +0000

    ignore sea bring thinkthrough interest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
