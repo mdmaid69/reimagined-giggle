e9fa51403a617acbd05d9753fec052cd149eef15
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:21:04 +0000

    right many crash creature near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
