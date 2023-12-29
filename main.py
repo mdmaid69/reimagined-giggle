0fb3f6794ec615ad92e858b63573df2187d260ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:19:28 +0000

    rush chance bank now right

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import tensorflow as tf
-print(tf.__version__)
+def is_palindrome(s):
+        return s == s[::-1]
+def reverse_list(lst):
+        return lst[::-1]
