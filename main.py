8ce6915353a22bba4685a52fc9e63a996d3530d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:11:40 +0000

    act necessary understand turnoff rarely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def subtract_numbers(x, y):
+        return x - y
