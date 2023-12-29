e0baf76dfe439b0a37e36c223118b2e5337a7f6d
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:23:28 +0000

    little fairly before situation hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
