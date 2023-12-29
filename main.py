f929e5c1b939e04ceeaca6445cfdcf4ba5b7cf70
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:35:20 +0000

    base snow accidentally achieve container

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def add_numbers(a, b):
-        return a + b
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
