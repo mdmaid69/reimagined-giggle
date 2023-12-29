7e9357468d880f2bb290d28ea22c064c9bcab83c
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:25:47 +0000

    follow know market fairly complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
