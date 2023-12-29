4cb48cf7ce83dae8ec78c31c252cf3da7024dd8e
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:46:17 +0000

    woman cause copy unhappy study

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_priority_queue():
+        return collections.deque()
