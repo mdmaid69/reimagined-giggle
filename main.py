9cf18a3b53fd522d73b2b36f67af0ca040e672a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:52:07 +0000

    minimum here trick deserve government

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
