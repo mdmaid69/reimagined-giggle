1ea37f4ee327394bd740878f86ce146e139e0f31
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:09:02 +0000

    instrument far inside getback splash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
