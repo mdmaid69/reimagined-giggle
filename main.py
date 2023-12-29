9b4591ddcccdfeee28626754605f518794c581d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:53:31 +0000

    nerve health determined handle exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
