ec32b46d672e600a850529517e59cdbbb3cbc8dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:37:33 +0000

    meet matter wide inform left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
