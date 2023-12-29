7d8183b38fbdd30ddc89763c30a46e06449b98eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:25:06 +0000

    plan defeat release really teach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
