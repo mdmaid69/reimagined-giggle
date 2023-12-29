b87ecf01e21d3dc793e6bfcd6ce0118a607ef9da
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:17:38 +0000

    drop priority accidentally turnback uncertain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
