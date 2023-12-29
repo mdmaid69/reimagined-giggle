abc03c3ace8600c215562d9a55a80f37d6ec9ffa
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:13:45 +0000

    temperature floor anywayanyhow exit guide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def square_number(x):
+        return x**2
+import logging
+def log_message(message):
+        logging.info(message)
