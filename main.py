4545f5a535df83d6c1c1b7caf3701ba8a01cecdc
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:42:53 +0000

    runaway treatment live stop them

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_sine(x):
+        return math.sin(x)
