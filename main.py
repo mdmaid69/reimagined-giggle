2bce3496aaef1e7b63937beff9e5c7fb103a940a
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:04:03 +0000

    conversation cope principle begin weather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import getpass
+def get_username():
+        return getpass.getuser()
