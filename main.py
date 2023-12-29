6a1d6839f27c6b1fd5d75b1bcee87530e40d22aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:38:08 +0000

    popular drawing aware trust ignore

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def remove_from_array(array, item):
+        array.remove(item)
