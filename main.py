dc52f44263a433f22f0eead68286ca36c93f8266
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:08:24 +0000

    door throw dance and spoil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
