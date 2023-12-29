76deb0f3f31cca3cbd6c5661458a1dc51cd1031c
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:13:05 +0000

    imagination so feature design easy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
