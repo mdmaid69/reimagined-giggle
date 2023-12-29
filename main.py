9d37eb893d08461579e8763eb36452e2d568b436
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:28:30 +0000

    protect cough really university constantly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
