0441763078cdf1f85a70c4655083e0c6e2afaede
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:15:02 +0000

    left some flight name lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
