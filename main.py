90e4586edc5fb31fc0184c784e9c5d71e4954484
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:26:35 +0000

    practical giveout operate appeal sure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def convert_to_binary(n):
-        return bin(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_work(force, distance):
+        return force * distance
