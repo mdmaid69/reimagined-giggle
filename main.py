ca598aeee499feae9c1875b896bdde98050cf03e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:47:42 +0000

    anybody giveback music Itself fashion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_pressure(force, area):
-        return force / area
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
