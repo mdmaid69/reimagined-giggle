d9dfc0af0cbf8a1a11fadbc544f067bb43334195
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:08:51 +0000

    on past allow respect surprise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_area_circle(r):
+        return 3.14 * r**2
