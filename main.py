136188f9fe53b52f12d2b03dbb856b2845b46fce
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:25:59 +0000

    old settle safety after hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
