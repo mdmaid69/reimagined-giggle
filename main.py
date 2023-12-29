d5ac7d4fa69138ca5ec7d5a367df4deaaeb7b4a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:12:37 +0000

    perfect Uu admire combination visit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
