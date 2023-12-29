64c140bc08edc4cd32a4b719dd4552b38396411c
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:40:12 +0000

    moral eat design watch seat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
