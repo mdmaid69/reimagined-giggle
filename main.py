9e0d0c1f60752b79b00599b02411e7a3b6e89e3a
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:36:25 +0000

    occasional mystery transport statement air

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import random
-print(random.randint(0, 100))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
