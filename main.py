596570e449f1a3b45a7964b5ac0e5504d91ba11f
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:58:41 +0000

    why pocket target steep war

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
