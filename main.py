919255195d50c138504f4009d0bf3c8a9f17c871
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:36:15 +0000

    convinced quick area case someone

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
