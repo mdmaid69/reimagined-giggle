89215ae36a8a0db384e844cfc254782dc54f676f
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:24:07 +0000

    play tie perform mend only

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
