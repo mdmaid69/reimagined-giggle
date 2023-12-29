bda6ee06d223310886daf1e3287b001ea893d864
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:47:59 +0000

    distance attitude stick sting shade

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
