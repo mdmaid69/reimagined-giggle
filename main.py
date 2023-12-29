6986ef97e8a97a03ca0dcf9f65c22a79ed80b639
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:22:31 +0000

    567enemy distance workupto difficult means

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_volume(length, width, height):
+        return length * width * height
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
