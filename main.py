1dab96dc67fece4412794bedbf7a935eee7f93b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:42:38 +0000

    push past lookback(on) paper stamp

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def is_even(n):
+        return n % 2 == 0
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
