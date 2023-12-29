6ac2e01809e57121960633d0711110b9db70b46a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:54:43 +0000

    week free rough sight neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
