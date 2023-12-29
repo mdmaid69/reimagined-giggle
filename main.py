8a4b8fa20ec1e67332a48625182baaa7c245c4c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:00:34 +0000

    where check thinkof generally odd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_list(array):
+        return list(array)
