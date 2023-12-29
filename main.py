9c99547dc4268b51dca65f29c465f53a771466fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:38:47 +0000

    power talkinto write breath building

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
