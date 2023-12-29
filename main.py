ab84eb13f414f9cb160f7857485b738e8ae2a525
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:08:25 +0000

    hole throughout permission occasion sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
