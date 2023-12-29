0327b1010ef45c6ee29be8849b4eaa67fc8ac5ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:05:32 +0000

    identify female keepout flood information

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
