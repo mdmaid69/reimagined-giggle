eda68f0023a878d2db381f0f2e76963a0e3b03b4
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:55:53 +0000

    smell brief total freedom deliberate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
