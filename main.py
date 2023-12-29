2f329e6cb497504a5cc0942c9c5f592a9e68b2aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:52:51 +0000

    offer content nature black empty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
