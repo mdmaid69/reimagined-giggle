d5e74930b969347f0d4f62977bf471326f95a5d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:17:23 +0000

    correct term fashion both married

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_even(n):
+        return n % 2 == 0
