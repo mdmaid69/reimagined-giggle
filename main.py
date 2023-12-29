a7f63dcd770053739b401f3b28a905d774c67520
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:19:41 +0000

    ready under with lend off

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
