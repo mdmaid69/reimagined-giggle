79d656ef14236070e8b803bbff3a5bc0e6ce1891
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:08:45 +0000

    stamp blame large cheap advice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
