6715f7167060a4d4c761024d1d3b949bf9a64b97
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:56:27 +0000

    urgent advice side explain illness

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def cube_number(x):
+        return x**3
