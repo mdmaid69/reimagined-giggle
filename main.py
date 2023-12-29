d0ce66a53e90580290ee4f5cd3d88d9b5d5cc5db
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:55:14 +0000

    prize false speech attention squeeze

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
