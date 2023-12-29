aee904bc6d489e47a6930e0e50493dbc3d4654aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:40:38 +0000

    gradually shine beside far follow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_acceleration(speed, time):
+        return speed / time
