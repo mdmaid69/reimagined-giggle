e53eaccc6b11eaf0678c007b7f190222fc123a6e
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:31:12 +0000

    sort present underneath hurt bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
