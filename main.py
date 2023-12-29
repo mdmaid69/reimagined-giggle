3e2210103d405a913043da16f5b763202525dc10
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:15:43 +0000

    yesterday takeback schedule firm rain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-print(sys.version)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
