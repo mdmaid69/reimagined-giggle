c199d80929b4234e12a55f55c117970a6acd6560
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:51:07 +0000

    wild please either warning bringdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
