31a3eb043216d4cdb037da580648952532ff0da8
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:49:52 +0000

    Dd punish uncertain loss press

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def subtract_numbers(x, y):
-        return x - y
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import os
+print(os.getcwd())
