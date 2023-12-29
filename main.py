98ec2bb470eb3944a1fe39d755a53a1680a019df
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:58:51 +0000

    pretty between succeed rest certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
