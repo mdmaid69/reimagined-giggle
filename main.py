817823370068e92c252d61ba16214e79de6ae985
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:43:01 +0000

    today model mean valuable hair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
