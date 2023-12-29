005d021c9b52b456486aa04451b1291a2c25df36
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:57:01 +0000

    human match then do throughout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def cube_number(x):
-        return x**3
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_min(lst):
+        return min(lst)
