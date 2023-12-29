3b0d813599145d58ca2aab40d35fafa645f61690
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:18:34 +0000

    relax connect trouble exciting about

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def get_array_as_str(array):
-        return str(array)
+def divide_numbers(x, y):
+        return x / y
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
