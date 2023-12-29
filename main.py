fb12693fc2f4df0285963d28ba704e82d8822325
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:12:03 +0000

    throughout thorough put yourself please

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_work(force, distance):
+        return force * distance
+def square_number(x):
+        return x**2
