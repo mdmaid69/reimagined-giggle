190a8ba2a5b5b601650101a753c56b6374745121
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:34:01 +0000

    skirt keepaway wrap trap model

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
