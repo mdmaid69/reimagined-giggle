c8cc3a41d145b2695f23ac7450942a285aedf48e
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:52:26 +0000

    that summer before travel day

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
