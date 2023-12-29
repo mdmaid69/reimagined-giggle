f90ec52823c352374a4c0ac18155523cee72fc04
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:59:13 +0000

    window of spread begin describe

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def square_number(x):
-        return x**2
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
