3e724086f641f2c01ba3aaa25e35b577137bbbb3
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:09:47 +0000

    deaf view factory from damage

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def reverse_array(array):
+        array.reverse()
