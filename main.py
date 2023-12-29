61c08f5d28819d54e8e2a24cad2b7b620635419a
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:18:18 +0000

    post trick willing first fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
