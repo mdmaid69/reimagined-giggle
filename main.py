eaf8c6fdf4ba5dc19bf9edb7e648191da5d5d85d
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:20:52 +0000

    common grade correction busy many

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
