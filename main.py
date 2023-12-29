a67e3e64104038c73c6f63ef3afdc08ee45b51d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:24:39 +0000

    bringdown flat join image long

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
