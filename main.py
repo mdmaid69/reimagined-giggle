1a4c0efedd69712dd5756918a8c830594e70031d
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:12:39 +0000

    favour stupid delight benefit easy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
