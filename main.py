e247ce89adcbb62f956c551100f59f5063bdc28b
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:15:31 +0000

    lead bringup comfort cover fail

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-for i in range(10): print(i)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
