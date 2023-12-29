bb76e926b5de6c867a993fc9236e300f31c7cd7d
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:48:15 +0000

    571enough effect string behind no

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
