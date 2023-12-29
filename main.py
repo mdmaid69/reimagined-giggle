c0cbbee0adebcecab47e843e6c8bc89ddc71e30d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:55:10 +0000

    bar equal see trouble steam

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
