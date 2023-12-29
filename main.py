fba6ab1c0b38ed0399827246f45f72263702720a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:47:31 +0000

    somehow slide before blind platform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def append_to_array(array, item):
+        array.append(item)
