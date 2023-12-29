ee2567691e77408f6a317fc32aeb883591206ca4
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:28:03 +0000

    emergency several doubt spot calmly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
