260c8742694531f803260d9864aedf7754153f29
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:29:42 +0000

    bank we admit of scream

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
