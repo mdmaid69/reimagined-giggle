01b3077a98d558aba7ae6af66e9991890d44ce8c
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:10:10 +0000

    knock information skirt top spend

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
