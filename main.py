858e30f0d1992b32c99328ddeb55b6f1a62f9e63
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:07:09 +0000

    fence search proof everybody bored

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
