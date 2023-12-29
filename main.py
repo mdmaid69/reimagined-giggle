b8c26ba2f82202b5902813649f5789b539a29082
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:37:25 +0000

    stretch own land sharp cheat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
