c4df793b726fe510061e3e946458b37415020ac6
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:47:37 +0000

    label warning birthday content ill

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def divide_numbers(x, y):
+        return x / y
