d0cb308f48e98d153b6377517d66ed9ea2836cdb
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:22:49 +0000

    style both react wing curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_acceleration(speed, time):
+        return speed / time
