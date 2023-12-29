460a8b03e5c218602543d07b952c3d477a222177
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:20:15 +0000

    black risk quantity relief block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
