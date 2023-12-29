78124571d9761f84f24e719bfa8a302a252d73c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:53:17 +0000

    try onto environment stranger past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
