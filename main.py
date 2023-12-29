59b0eb527299b192543ecd9c262ea4070144542a
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:06:47 +0000

    challenge getaway crisis total hardly

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def convert_to_hex(n):
+        return hex(n)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
