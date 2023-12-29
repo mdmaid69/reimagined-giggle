b3e6ed5b757aa7427f5b52d3cb73434790f5332b
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:12:28 +0000

    shine interfere computer scared why

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
