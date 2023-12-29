c8cd3bcc8a133577ea3b5c0bc8d844a9fceba0ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:55:59 +0000

    aim hard blind surprise afternoon

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+for i in range(5):
+        print(i)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
