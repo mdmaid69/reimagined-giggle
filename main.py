245b0b358c1d201ea42b0317e6260a92a4244377
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:43:54 +0000

    slip minimum date size occur

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
