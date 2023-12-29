d04433fd48cdd6c57cbe3a99ac6e8a5774527720
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:39:27 +0000

    too freeze nigga paper friendly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import collections
-def create_priority_queue():
-        return collections.deque()
+def count_words(sentence):
+        return len(sentence.split())
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
