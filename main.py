6a75dd35221947569ff3b88d2525d38923039b99
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:07:04 +0000

    display runinto obtain furniture mark

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_max(numbers):
+        return max(numbers)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
