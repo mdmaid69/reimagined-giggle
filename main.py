c386ad95b42287c30813686b02639db3a9a03ea9
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:22:13 +0000

    draw though mark describe block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area_rectangle(l, w):
+        return l * w
