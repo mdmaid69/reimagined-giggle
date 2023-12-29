a5d22ac27ff92b43103a32adb254e8666a4664bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:31:10 +0000

    suffer thank discussion follow afraid

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
