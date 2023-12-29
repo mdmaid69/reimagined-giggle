7b990d80c6a6ec6b945e1e9280cd702bb8ce04c0
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:32:50 +0000

    throughout young Kk solve march

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_odd(n):
+        return n % 2 != 0
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
