cb8ccd41a66e65a7b442314e65f4436da4db929c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:10:28 +0000

    copy wind relative matter neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_str(array):
+        return str(array)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
