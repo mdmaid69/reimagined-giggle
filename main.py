96ec628c98a070a10bb8df856b3eb3dee34121ba
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:55:25 +0000

    tape mean in somewhere sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import platform
+def get_os_info():
+        return platform.uname()
