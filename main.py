32b78098b39dd09a0ec8089c9b83f5d0ac359574
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:54:43 +0000

    turndown drawing deliberate makeinto borrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
