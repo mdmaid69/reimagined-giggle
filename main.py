85abd1ec0d86d3b57002a061e7a93cb128866c4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:22:28 +0000

    body agree turndown run mystery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def delete_file(file_name):
+        os.remove(file_name)
+import datetime
+def get_today_date():
+        return datetime.date.today()
