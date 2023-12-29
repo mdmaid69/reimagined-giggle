1ae5bc18eaf82a848c68bb2ac36c84fe8eba0e2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:20:03 +0000

    improve bus improvement yourself shallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
