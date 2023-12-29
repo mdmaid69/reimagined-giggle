eb89613ecc17233eea49f48f37e9cc9e1eb4ff1f
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:36:40 +0000

    joke copy food light warn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
