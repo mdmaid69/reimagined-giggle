62101224b8529892245b22f2aca8794a22a619b4
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:25:22 +0000

    base complaint she 572enter succeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
