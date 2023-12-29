ee96c81f7b326cfab003e1b3a6c327df02c97961
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:55:12 +0000

    holdout walk congratulations ticket cool

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
