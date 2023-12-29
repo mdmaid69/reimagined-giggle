06731671ab10b80c546a5083fd167c22b6540f2c
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:40:54 +0000

    slice unfair them on pleased

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_queue():
+        return collections.deque()
