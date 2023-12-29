ba57ee463f347ff515a35ae94bf56c12d8883652
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:24:51 +0000

    waste load include share practise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
