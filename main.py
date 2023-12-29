d193031a9206bef5e0f1b196bbf65bd8d08dfbb4
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:40:40 +0000

    harm spread suffer force noisy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
