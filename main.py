37a87fac9bb54722b916e2d6abd22db484357a50
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:59:21 +0000

    holiday creature benefit thin relationship

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def square_number(x):
+        return x**2
