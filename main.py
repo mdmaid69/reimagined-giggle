638f3007a4408f0b924beb536b41ea4f467cb4c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:06:47 +0000

    thinkthrough even genuine line worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
