8b5d32bd42296efa7ef7dc90a7417fd4eb1b6412
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:53:57 +0000

    general fine final patient criticize

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
