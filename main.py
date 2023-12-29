15770a21a2b8785ee6ca8b248bc6cd2d87c3c725
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:00:37 +0000

    blow basically rely prove dance

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
