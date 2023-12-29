217d9235dc569229175fb8a57495920ffdde9f4e
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:11:46 +0000

    fork vanish ours nurse underneath

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_min(lst):
-        return min(lst)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
