e75775728776bd47271df7cee8569367c82a448c
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:04:28 +0000

    mood fortunate behaviour sound tall

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-print(sum(range(10)))
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
