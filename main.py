2bd58b0954c7d181d2c8f97cf89d5fd1697fb968
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:43:30 +0000

    year bringforward proud favour emotion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
