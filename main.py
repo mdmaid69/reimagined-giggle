c33b1375f1288c2e5f21536929b582bf4495f8b4
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:56:58 +0000

    shopping confusing exact work frightened

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
