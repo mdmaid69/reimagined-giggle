562c88fa41375052084dd1233acc724183f4529c
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:12:27 +0000

    holdout card convenient thankyou drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
