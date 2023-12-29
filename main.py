263ee713e3246f38fa1dd4ce4384b577117dff82
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:12:35 +0000

    soak drawing back deliberately lot

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
