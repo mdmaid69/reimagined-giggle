521d8f6b4ee1c18722d54a51b513ed4cfdb54e82
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:18:11 +0000

    include question stretch compare win

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def cube_number(x):
+        return x**3
