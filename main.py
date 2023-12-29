cc22db707719d4704ef9d789361ce78364006299
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:10:49 +0000

    plastic easy dish anybody course

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_speed(distance, time):
-        return distance / time
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
