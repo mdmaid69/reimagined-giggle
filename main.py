cd6dc8c767f7798603b8ba82db49ab6b42c21679
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:54:57 +0000

    sudden yet start decision total

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import platform
-def get_python_version():
-        return platform.python_version()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
