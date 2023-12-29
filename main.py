5fddbcd785268102ea0a9be9bdb084ae7dddec35
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:05:11 +0000

    once breathe contrast emphasis stiff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_power(work, time):
+        return work / time
