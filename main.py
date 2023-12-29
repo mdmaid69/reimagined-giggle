cb1c58e611b71fe6dfe52907b71b0cf16632f43e
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:57:55 +0000

    impossible try skin 568energy organization

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-for i in range(10): print(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
