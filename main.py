d222d37dd6fa4875d8c647c9b4dc6e0dd040697c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:50:28 +0000

    plenty takeback join mother anxious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
