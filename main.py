d524e39982e03fa9bc7d096116e3640ecaafc3d0
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:06:57 +0000

    happy base passage bored achievement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
