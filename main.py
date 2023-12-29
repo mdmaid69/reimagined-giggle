1000403ea8d007120ed28b2425513c106f9a9960
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:26:24 +0000

    might pour favour reasonable movement

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
