5c3f41e837d1cde8a386d103a054b9bbdb4aad14
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:07:19 +0000

    almost total supply floor introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
