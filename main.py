16ecbb8d78ebb8942c19597b0bfa1f2d8769ce04
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:19:11 +0000

    since suffer opportunity squash whistle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
