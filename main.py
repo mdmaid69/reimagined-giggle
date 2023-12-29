d8accc1b684d1830d51750ad10f7d3456708e59b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:40:27 +0000

    priority proud fun remain hate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
