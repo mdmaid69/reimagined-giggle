859e3e8d076e858f44c9b84959196f911656b30d
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:01:35 +0000

    risk responsibility friend trick copy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def multiply_numbers(x, y):
-        return x * y
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
