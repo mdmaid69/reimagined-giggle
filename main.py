4dbf8639fe050a0ba305aba4d473939d5c8ef5b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:22:02 +0000

    pleasure difficulty live air club

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
