afefbca0b74d9017125f3ff2bc2a4f182a823d36
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:00:06 +0000

    brave unite borrow rise help

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
