3b1795242d1b212dac2d8a8784993fb26a02a64f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:28:16 +0000

    harm calloff remember dry concerned

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
