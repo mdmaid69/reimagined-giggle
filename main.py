58f6a975be80858c2c81f089d40895077a489a29
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:27:14 +0000

    independent live custom suppose sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
