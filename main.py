dfea3c4ee3c86d5d792dc388750aa1d9de3ee702
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:18:40 +0000

    of store trade building unable

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
