3f9b5685ebb1010ff44edf4bb15a2b2607197444
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:03:29 +0000

    again many target sort concern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
