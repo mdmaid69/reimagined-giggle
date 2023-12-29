c919a0848b2abf106d5d1ec5aa2ac993dcab43a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:33:32 +0000

    illness mention her awake importance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
