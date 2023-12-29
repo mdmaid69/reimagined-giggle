57a3fbe8d9e254bdb54a007b756f883dbf2ccffc
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:49:03 +0000

    born offend right free disturbing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
