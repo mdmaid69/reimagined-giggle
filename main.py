5da4706d96d7c64e02c42a075c5bd82060fad649
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:11:52 +0000

    smile relief concern offer use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
