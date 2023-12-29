11e93a5f5064dbff3fdc93c4844ff6eb42681c6f
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:27:48 +0000

    brain hear may store journey

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import sys
+def exit_program():
+        sys.exit()
