40893bac3f0705db6f7c7f5c28fdde575f80d319
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:49:01 +0000

    out OKorokay admire dark receive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
