dd8a9a9af81dc44557a6eedc54bbc4c0b5b32a77
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:15:33 +0000

    yesterday moral strong tend plant

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
