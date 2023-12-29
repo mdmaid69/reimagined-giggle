fdd43664303f36cea1fe9334ef05533f695490fc
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:54:00 +0000

    gate protest shout once agreement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import sys
-print(sys.version)
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
