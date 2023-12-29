c4c84c901dd9796908082472fa9615d10ac7936d
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:47:17 +0000

    wing break plain anybody cost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import tensorflow as tf
-print(tf.__version__)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def find_max(lst):
+        return max(lst)
