6a6004084062172b8ae5cc70c19c7ddbec7de3d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:58:19 +0000

    generally right trap politics succeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
