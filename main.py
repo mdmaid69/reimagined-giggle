af8754deb8e72f6283b9fd25af8d999675610f6f
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:34:41 +0000

    recommend leave final line down

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_current_directory():
+        return os.getcwd()
