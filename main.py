fe4a86dc13328d71d800bd4d12c6bffcd38befb6
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:56:19 +0000

    apparently shopping demand favour plate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def reverse_list(lst):
+        return lst[::-1]
