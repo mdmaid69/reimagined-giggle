58841f184b54135cf1b2553053ce2c1217e329c0
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:09:00 +0000

    challenge she respect anybody question

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-print([x**2 for x in range(10)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
