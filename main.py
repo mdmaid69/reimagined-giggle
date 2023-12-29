df545d3b4a64c7af650d30cae21a7c6f9911123e
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:11:31 +0000

    argue ear cruelty soft uponon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_current_working_directory():
+        return os.getcwd()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
