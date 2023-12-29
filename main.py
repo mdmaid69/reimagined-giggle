4271b3f90eef899deac7517baa7521a903a8aa33
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:59:07 +0000

    shape drive every copy turninto

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
