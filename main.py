3d91a37b2c88d7050a83da05192ad90d7dfaa273
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:26:35 +0000

    market about right hers opinion

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
