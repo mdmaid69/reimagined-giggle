8b2b93fb9197604a63804fa03e9942c7f53f9332
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:06:25 +0000

    listen century answer music direct

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_density(mass, volume):
+        return mass / volume
+text = "Hello, world!"
+print("Uppercase:", text.upper())
