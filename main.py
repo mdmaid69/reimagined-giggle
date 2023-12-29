9a01d4b12612457c726c2b50523eaa0add1b7574
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:36:55 +0000

    suffer description future appeal down

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_itemsize(array):
+        return array.itemsize
