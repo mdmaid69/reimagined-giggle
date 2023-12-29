0745adfcaa29d520f1afb209ea1a962b4ae9d24e
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:48:27 +0000

    platform joke frame then Kk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
