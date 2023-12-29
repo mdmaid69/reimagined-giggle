6a6f8a362689c0f187af04d1d8bce15a82cfd43a
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:11:37 +0000

    cruel communication except gap grand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
