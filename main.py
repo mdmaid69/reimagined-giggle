7ada1b827a396efae5650d03f58353ab60444ddd
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:01:05 +0000

    skin interested together ignore quiet

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
