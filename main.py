9072946817c2498ae9b3bbc74977052495465d53
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:22:41 +0000

    absolutely ours cap switch share

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
