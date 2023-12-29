8aca5418024fb0024653d082f04dbfc1f3e8d895
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:34:21 +0000

    official open life stretch sweet

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
