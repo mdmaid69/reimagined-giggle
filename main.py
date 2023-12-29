e5da196cf16159db6b9e85b80e357d36da1606d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:43:19 +0000

    mess call year keepup deal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
