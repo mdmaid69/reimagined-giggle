f77386dd02b0a4bb6d78bc25cb892da0929be61c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:05:17 +0000

    getoff total still block nowhere

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
