15503b39d42e639ca2e4ba50cef32e037d57b62c
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:32:42 +0000

    break progress pollution attempt backwards

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_binary(n):
+        return bin(n)
