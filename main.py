c38ddad6d611bc62c16efb67f3a8b454e392a26a
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:49:58 +0000

    unless make interest sell before

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
