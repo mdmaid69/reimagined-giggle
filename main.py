51c59df07944f7dca7050223bbe728526efd2c2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:44:04 +0000

    callup win blame suggest false

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
