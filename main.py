6a983c6b7b9e70b75ea30d7105cd90a5946df794
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:09:45 +0000

    prize pattern answer owe rich

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-for i in range(5):
-        print(i)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
