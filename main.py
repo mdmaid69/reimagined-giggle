b170f1df362eb6410064615f8dca9f13c9665b17
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:44:16 +0000

    late front motion me money

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
