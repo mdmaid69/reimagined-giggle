d9bbc56d019853cf22459beff02904a70de310fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:38:30 +0000

    floor answer famous object sorry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
