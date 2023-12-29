ffc5e915880f6a881b8e5bfaf6aeac757f7db30f
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:28:29 +0000

    formal scheme to reason boat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import os
-print(os.getcwd())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_area_circle(r):
+        return 3.14 * r**2
