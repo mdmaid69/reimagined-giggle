12deca2301b3f068fd380b7d745aba13ec5c3f8e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:10:03 +0000

    run know silent value direct

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def convert_to_binary(n):
+        return bin(n)
