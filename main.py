4349f9465fb29dff8d01929dbaaa7eed9c41f721
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:01:04 +0000

    police floor deep inform fair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
