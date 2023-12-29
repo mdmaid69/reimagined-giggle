4e1cc302de13bad29c268426ce8de63557c81d61
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:15:59 +0000

    move shade attach under round

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import platform
+def get_python_version():
+        return platform.python_version()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
