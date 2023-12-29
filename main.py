9ff462b981abd51ea89311bdb3c8a83a50617c19
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:33:13 +0000

    sting guide right rely mark

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
