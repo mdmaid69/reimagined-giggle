8b31af3d7f8532e9fdff95fc4c19d0f623ed14a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:14:02 +0000

    have no to 572enter market

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
