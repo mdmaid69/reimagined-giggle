e7add1f05188096c9e2b249152b1c7d5fc5f596e
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:36:26 +0000

    use raw and bridge sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
