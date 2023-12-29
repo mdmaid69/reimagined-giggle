821297583f443b57533da486403ae3d906c3d356
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:24:26 +0000

    confused out rank whom draw

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_area(radius):
+        return 3.14 * radius * radius
