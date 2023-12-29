f1b6b433467cf84bf0c81452163ee0c5e8867873
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:15:38 +0000

    569enjoy life old factory post

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+text = "Hello, world!"
+print("Words:", len(text.split()))
