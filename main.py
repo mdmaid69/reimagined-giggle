127f09c2f642e101b8ae6f3da6a5387375f62e97
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:09:56 +0000

    neck estimate level most cruelty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def square_number(x):
+        return x**2
+import math
+def calculate_floor(x):
+        return math.floor(x)
