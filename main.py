7b67acdc0ec7870d58b769c293d1175ed1ca46f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:21:33 +0000

    past cover embarrassed trousers sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
