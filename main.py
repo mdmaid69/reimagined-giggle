16c7f44437ee4318e540e431c8f5c05a3f839943
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:14:58 +0000

    runover by 568energy sharp powerful

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
