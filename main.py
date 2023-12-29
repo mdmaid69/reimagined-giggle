667321bdea103a0f87794c8ab24f691773511c3e
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:43:39 +0000

    light discover maximum wind still

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_power(work, time):
+        return work / time
