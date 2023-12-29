c1160965549400ff4df9aeedff6113b503873e4e
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:34:53 +0000

    play flavour obtain wise sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
