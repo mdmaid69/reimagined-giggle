cd6926da4c25665c71c41b085c5db76534e05f54
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:30:38 +0000

    callback never brush mistake anyone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def set_array_item(array, i, item):
+        array[i] = item
