a68dc086f7034bb741caa175a75fa6ddcc0134a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:22:39 +0000

    formal great together us brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
