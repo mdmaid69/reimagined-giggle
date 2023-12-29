9147369ea269b406a429e3f82da8a4aaf0e81fb7
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:08:50 +0000

    weather beneath have community harm

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
