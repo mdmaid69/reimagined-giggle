f51ee48443497780d926444f5ad62ed8163646bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:57:03 +0000

    before ever realize keepfrom along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
