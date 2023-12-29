73192fdf2730cc424948779d6ae83631527efb61
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:11:58 +0000

    love know fast talk fun

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
