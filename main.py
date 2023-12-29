7e4f2c8ff6df03412cab13f21bd7aab773d74525
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:20:26 +0000

    stare another activity other sharp

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
