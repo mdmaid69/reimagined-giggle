969d47213069834b82a9dd11cc756c5810c5ad6a
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:52:12 +0000

    brain forever motion never getaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+for i in range(5):
+        print(i)
+import sys
+def print_python_version():
+        return sys.version
