a2c0628471a5123691bb0f747c20bd48d1498a20
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:14:59 +0000

    since machine substance interfere live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
