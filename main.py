81ab072df0b07ad8634512616f67936d13e031d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:31:38 +0000

    quit life provide lead criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
