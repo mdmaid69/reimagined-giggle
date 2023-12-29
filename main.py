d7c5a55cc85c1359a495d7e8bdfbdda0910f5087
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:26:46 +0000

    group runinto spring rubbish hot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
