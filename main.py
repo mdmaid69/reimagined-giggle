f83eebbafba75e28a546b107839974da4bdc8e32
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:56:31 +0000

    refuse draw coffee bottom Rr

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
