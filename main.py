91baf6fcb1068f027ebcece9b14b11d1108db098
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:00:12 +0000

    sudden next man tour slightly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_float(array):
+        return float(array[0])
