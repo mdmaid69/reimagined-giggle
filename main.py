ee2fb3020c976c80ca12d9f1017ab3db5d15412a
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:25:29 +0000

    performance attempt best compete workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
