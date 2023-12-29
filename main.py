1e8cb5ae427118078621c222cb6135655970542e
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:48:26 +0000

    answer slightly correct conclusion question

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
