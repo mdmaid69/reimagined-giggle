556a0d39f71096f730fee4d308f44a5b14e77b3f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:56:42 +0000

    behaviour juice soil season message

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
