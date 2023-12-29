82543665c7cde90f817f6c820c467589d9399c55
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:31:45 +0000

    content pocket tough sometimes period

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+text = "Hello, world!"
+print("Words:", len(text.split()))
