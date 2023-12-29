1f6c5c9c2ff989d5816476f723911b6a6646d2f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:44:46 +0000

    afterwards discuss independent well thin

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def find_unique_words(sentence):
+        return set(sentence.split())
