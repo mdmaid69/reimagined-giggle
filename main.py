e0a47f159bf1171f6511a9d6b55ea861c9d74b4b
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:24:33 +0000

    when charge such smile curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
