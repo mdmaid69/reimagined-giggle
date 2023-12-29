827bf5127de8e6c8ada351bb75e2459f3ac5082d
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:51:35 +0000

    priority effect same cinema remain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
