96da2c0364c8659664bc1525776b6353c6d400b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:08:01 +0000

    business active boil react in

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import platform
+def get_python_version():
+        return platform.python_version()
