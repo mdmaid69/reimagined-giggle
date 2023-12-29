2cf7fdf1d9ee30f6714bd3046a83115ea8eb4a82
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:45:25 +0000

    fail where factory deal back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
