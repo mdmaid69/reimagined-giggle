9bb2694d26510875eefb5e3b979c2aec1b928b6b
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:32:58 +0000

    gradually taste apparently beside swimming

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import math
+def calculate_error_function(x):
+        return math.erf(x)
