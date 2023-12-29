6bb472a923ce24fb42e2cea83028afed00ed9442
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:10:12 +0000

    thorough slide just independent Nn

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
