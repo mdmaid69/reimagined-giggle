06c8cc9b20cded5bc76752004e5538ff7067f520
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:16:22 +0000

    level agree care swing row

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
