9fdca1dfc1f91955048bc6e12b974c0257fed95b
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:02:48 +0000

    friendly except rest transparent television

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
