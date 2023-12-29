027ae9100be48ca48ba7b9991b970c5d3ee60414
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:39:32 +0000

    hour bar delicate appearance bill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
