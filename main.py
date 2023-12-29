15475c6888191e0eb5f560abb8d75c7448001024
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:35:14 +0000

    feature turnup compare he slightly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def add_numbers(x, y):
-        return x + y
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
