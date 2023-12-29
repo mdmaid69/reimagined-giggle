8ec2ed32212db3448623aa38e279d764deb40562
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:07:21 +0000

    line period everyone shock convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
