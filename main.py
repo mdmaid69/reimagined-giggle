d338bf74fe57ddb68c9c061bb22460832930529f
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:19:16 +0000

    often surprising only though approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
