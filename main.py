90695bc6b41e6b29ba7f3330708fd4dd7a1b9935
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:23:30 +0000

    bit border uncertain study release

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import platform
+def get_os_info():
+        return platform.uname()
