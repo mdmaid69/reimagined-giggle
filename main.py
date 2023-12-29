640b3009cfeb024a56f17b1ee28b6f8c2f8aa870
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:53:24 +0000

    exchange doubt marry operate cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_floor(x):
-        return math.floor(x)
+print(math.pi)
