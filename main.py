b2b861f71d71162a908654c4b3c0954c42a79442
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:37:07 +0000

    science no too allow breathe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+text = "Hello, world!"
+print("Words:", len(text.split()))
