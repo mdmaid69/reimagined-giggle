f223ea7ffde3d3a42e6a8f18aa06a7bc43b201f9
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:18:14 +0000

    Vv hear appear family unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+from collections import Counter
+print(Counter("hello world"))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
