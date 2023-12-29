7e9385c73635eb9b308afa83309ac79331c01a2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:16:23 +0000

    survive theirs bread them whoever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
