92875959e01854946cdaef63e49cabbf69e52141
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:47:37 +0000

    bringforward cheque hair cruelty makeinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
