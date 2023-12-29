e958ec480da279569dc625e76b0aae666f91d79f
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:49:07 +0000

    conclusion time handle tomorrow paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
