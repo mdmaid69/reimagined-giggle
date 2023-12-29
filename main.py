53597db12d7e94db65f61acc96ebd8346d254325
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:52:47 +0000

    concentration voice off Tt murder

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
