b87a25a6cb27115933d9e8bc977862e810f5c0ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:29:28 +0000

    door 565end fire them recognize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-name = "Python"
-print("Hello,", name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
