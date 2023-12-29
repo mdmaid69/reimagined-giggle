8353d4573b1b35f38c0bd9994c1fe221b08e68b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:27:59 +0000

    use trick sport prefer 571enough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
