238c116e1ac23fc80e52961bee9c92524ff2f4f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:00:57 +0000

    say material show bang deaf

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
