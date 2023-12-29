ca9236bc4ad0fa03838f002ca3b9967fbb7cbc05
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:26:40 +0000

    ability ask likely subject book

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
