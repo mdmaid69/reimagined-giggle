23b2f4c54b309f031e3ccc7082c87604ae68c5a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:49:54 +0000

    take whenever mark you to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
