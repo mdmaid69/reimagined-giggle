de81b469a78e491ab9647cff9b255969e7201a23
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:06:53 +0000

    comeover vanish sorry swimming ought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
