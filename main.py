c0914919254325db61cd266f1514de75139f7443
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:05:49 +0000

    watch talk blind lookup happen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
