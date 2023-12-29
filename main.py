cc2dc67eede278416b368ff20eaa99d18e0c256a
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:51:35 +0000

    wind tread bag roll difficulty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
