47d571f7ae88f60999f2082dfece26106248a98a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:02:35 +0000

    exit cold pour sad equipment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
