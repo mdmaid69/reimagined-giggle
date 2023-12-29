fc027b66ba0423289a21483a52d8c620231f8010
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:37:22 +0000

    dance type able wish accordingto

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
