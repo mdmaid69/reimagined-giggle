bdd45e7cce8b52d27d42133e27a683eaf7779b0a
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:26:25 +0000

    nonsense connected correct justice many

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
