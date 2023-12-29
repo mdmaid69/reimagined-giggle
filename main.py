8b10245807e10cac22fb33cd33121461c79f13c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:25:37 +0000

    shop accident who fast fat

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
