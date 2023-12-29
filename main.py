ee26cf9164263810aaf6059bedd54dce0f7dab9c
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:57:42 +0000

    shadow use 573entertainment dare disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
