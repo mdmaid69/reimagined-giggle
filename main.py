86c1dc772764974beff7b312390b07a034d3063a
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:32:17 +0000

    theirs opinion marry somewhere factory

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
