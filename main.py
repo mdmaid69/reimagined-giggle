965c8cebf79ad2bf9a97610c80fc8f5f8f906f5b
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:09:38 +0000

    sometimes name law fashion congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
