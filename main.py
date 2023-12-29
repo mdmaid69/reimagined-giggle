f1395e52d98b4add5094d2419a95925f28a400bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:45:15 +0000

    actually wait invite seriously direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def get_array_as_set(array):
+        return set(array)
+import datetime
+def get_today_date():
+        return datetime.date.today()
