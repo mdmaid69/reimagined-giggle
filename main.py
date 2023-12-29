bf31fbef4a6effc89017959b91f6d8cb1625316b
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:06:30 +0000

    slip already generally cry somebody

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
