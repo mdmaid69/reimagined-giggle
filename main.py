bcb6e338e633cf3081c55af3d8dea690fccae138
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:41:07 +0000

    turnback why person knock frequent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
