52d8c9674add4741c6b25f181718d11a6ebc806c
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:42:40 +0000

    available ordinary open tip extraordinary

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
