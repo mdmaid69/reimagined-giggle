8bad01b1a8e9fe828a5bc0afbb96010b79393233
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:29:12 +0000

    aim breakout wave pleased divide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
