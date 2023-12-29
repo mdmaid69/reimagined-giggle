a06bb0b41c1166d4545eca4d762028edbabdaba6
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:06:43 +0000

    cheque giveout importance light then

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
