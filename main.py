6bb26f12fc7d2f1a4ab2a7ca34d41f38703d12d6
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:01:36 +0000

    delicate reason summer limit responsible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
