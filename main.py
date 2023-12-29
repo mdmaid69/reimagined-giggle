ee735e6d6e50a4a9c06e2692ab18c2e5668d283a
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:13:29 +0000

    building nearly disappointed difficult charge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
