828b08c94f5b349bad4a19db469f3f97805a96d0
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:31:57 +0000

    drive number better allow expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
