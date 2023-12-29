b5342af8cfafbca8d6ada43553ca96e7e8432c1b
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:44:24 +0000

    best platform control mass punishment

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
