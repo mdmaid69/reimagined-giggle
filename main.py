df1205dcaca8e929f7ed69649ba206f636a63e4b
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:12:22 +0000

    bottom special by relationship rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
