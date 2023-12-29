9df3f71e4dd53061de3cbaab46e634adb3a09853
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:26:09 +0000

    tidy morning pipe reduce only

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+print(math.pi)
