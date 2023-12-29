6431859506cf994f501f8cf16e57f8ac2e6a8e7a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:44:04 +0000

    only type past tonight machine

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def get_array_as_float(array):
+        return float(array[0])
+name = "Python"
+print("Hello,", name)
