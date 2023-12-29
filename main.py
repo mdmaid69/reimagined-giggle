9208d84eb0e6749601225c641f287fcaaf32a3e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:12:44 +0000

    marry deserve thin rely dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_item(array, i):
-        return array[i]
+def pop_from_array(array, i=-1):
+        return array.pop(i)
