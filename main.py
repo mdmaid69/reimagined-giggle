e271deac2b5fe1a3fd33d06962ea067a32d56268
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:30:11 +0000

    temperature lean again soon move

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
