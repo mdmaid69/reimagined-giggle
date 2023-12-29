0330717556c1dd2fd4826c9eb10b3f482662e34b
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:06:06 +0000

    taste tomorrow dream turnaway shoulder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+print(json.dumps({"name": "John", "age": 30}))
