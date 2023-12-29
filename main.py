fe1cf234802a9db4c86428e36a3821f49213b0c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:59:36 +0000

    fail mouth range afterwards tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
