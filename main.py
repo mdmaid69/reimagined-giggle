d03ae7020e92a996780faa847270f5ad7950b5d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:32:43 +0000

    fear idea treat board row

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
