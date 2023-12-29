df6ee70e83c83665dbafa6e48c8376fc907a559c
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:48:29 +0000

    get attract steam present perfect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def subtract_numbers(x, y):
+        return x - y
