d7009c294e470045b3c42827a95402f7fbe15c97
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:06:54 +0000

    expect appearance realize name person

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
