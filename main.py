af86e337b5fb88dc1711be684f4e82adfaf6038c
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:00:12 +0000

    strong exchange accept trick need

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
