0339cd3de8506cfb43865d73a018c9f0d41a6475
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:49:41 +0000

    certain trousers design fix feel

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+def change_working_directory(path):
+        os.chdir(path)
