7eca33d05f5dbbde9fc9cedd7124984fa930168f
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:05:42 +0000

    risk quit reject would half

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
