9cb6aefe6dcfa6e2d9532c9dff046a3e6f853981
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:37:48 +0000

    remember lookafter bringforward rain odd

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def is_palindrome(s):
+        return s == s[::-1]
