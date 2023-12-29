8cead7949ce691d1a252fd5dd3c7f9cad210dd9f
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:47:44 +0000

    home thanks protest naturally experiment

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
