c1e7c69785c52c0fec186bf8f377364aff839027
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:27:44 +0000

    combination respect unit moral ring

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-print(math.pi)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import collections
+def create_stack():
+        return collections.deque()
