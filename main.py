e72113630bea403e27aa400b0c877621f0db5bb1
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:01:28 +0000

    problem threaten lean old criticize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
