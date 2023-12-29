af5c8e56b7ff0deffba7ff368d1b97c3ff3c7975
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:12:43 +0000

    unpleasant few moral all bet

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import getpass
+def get_username():
+        return getpass.getuser()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
