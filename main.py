91d3ba8abb604aab521ceb8ac669721e28094f1e
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:44:01 +0000

    tip beyond spin settle sentence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
