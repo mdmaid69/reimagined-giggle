5fc43faad2daa32a70821d613b56e4baf210c821
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:45:53 +0000

    contribute runinto then problem total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
