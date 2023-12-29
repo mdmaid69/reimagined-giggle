269bac37d3299ee4dc030210db4ffcfde307447f
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:17:22 +0000

    label rank science why now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
