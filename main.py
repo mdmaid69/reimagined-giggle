22c6464f9ef160c69587d851e3cb4b8b83b47b0a
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:21:09 +0000

    strike thirsty OKorokay call regular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
