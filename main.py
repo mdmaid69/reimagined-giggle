1cb5f547e8fb79010cd89715ae3fa61486e4323b
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:02:56 +0000

    considering copy sick room determined

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def calculate_mean(arr):
         return np.mean(arr)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
