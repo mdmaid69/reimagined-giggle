f868bfa53f1a148baccbdaf0055aa1e124d84cc5
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:01:37 +0000

    him order soil kind very

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def square_number(x):
+        return x**2
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
