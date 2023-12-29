64c5e55f041403d14a558008e6d44c9b47d1ea69
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:57:17 +0000

    warn check study challenge Zz

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_volume(length, width, height):
-        return length * width * height
+import sys
+def print_python_version():
+        return sys.version
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
