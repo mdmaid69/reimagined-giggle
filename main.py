614b36a36547c51ad465e678a46e18b95f5b6858
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:31:03 +0000

    runover father hot sting follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def split_path(path):
+        return os.path.split(path)
+  def reverse_list(lst):
+        return lst[::-1]
