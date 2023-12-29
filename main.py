ca06e577ca0962344d717db890e9be649956228a
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:09:12 +0000

    gradually difference Hh dark serious

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
