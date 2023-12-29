993bfec97fda18a85514edea868123d6afb62fcc
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:31:33 +0000

    describe free somewhere communication maximum

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
