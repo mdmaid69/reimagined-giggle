0137b992ba032811752b6418eab3503ceb579906
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:44:48 +0000

    forward broken talkinto both lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
