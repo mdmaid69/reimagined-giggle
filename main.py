5db0637abae345cd8a387911c26c19ecbe824775
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:04:22 +0000

    pleasant such real large both

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
