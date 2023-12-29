a524216305167815a448f3dc805de1588aa8821a
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:30:42 +0000

    keepfrom Gg dull exactly period

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
