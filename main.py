c51a58b98e88fb0b7b2d7c396d2dbb506b808763
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:02:37 +0000

    uncomfortable trip pair foot purpose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
