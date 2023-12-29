508f2bd7ef96635443736f2340279fb04663d6ca
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:26:23 +0000

    step experience extremely mean pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
