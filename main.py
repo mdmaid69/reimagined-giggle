fba73f84230763bb0fe41d7d3aa637f929a0ab8a
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:48:34 +0000

    matter bringup person question unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
