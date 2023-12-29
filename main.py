391b3aaee39d5ffa196b4b82dc219e4208e1dae1
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:34:10 +0000

    another wide Vv Jj than

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
