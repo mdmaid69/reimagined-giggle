7ead40dd0d103f9edeb24068a11950cc104081c1
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:53:20 +0000

    floor fit stick bad line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_dev(file_name):
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
-import array
-def get_array_itemsize(array):
-        return array.itemsize
