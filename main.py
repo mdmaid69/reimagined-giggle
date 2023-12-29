e651521154bb6bf3977253d8fef1d5f0dca8517f
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:13:22 +0000

    ahead steady scale pressure separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
