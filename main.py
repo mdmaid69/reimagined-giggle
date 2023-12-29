18d5884456af718f03e3d933a3f4810b9ec19a59
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:59:27 +0000

    chat mother sorry scrap doubt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-for i in range(10): print(i)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
