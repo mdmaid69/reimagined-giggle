06feb60f5636f26b4c6f368f04c39f1b99838741
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:07:00 +0000

    choose drink yet from address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def append_to_array(array, item):
+        array.append(item)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
