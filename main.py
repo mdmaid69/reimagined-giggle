ad6498d03e16cffc5685f448fb2b1d23c8cd67ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:34:15 +0000

    suggest afternoon create fortunately recover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def reverse_array(array):
-        array.reverse()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
