3497f792b966374fee409b415a45b8e382786bf1
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:35:46 +0000

    lift low opportunity cruel set

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
