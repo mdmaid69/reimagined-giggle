1a2f5c567c7bdf7f51df6387b5d8ac6077027581
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:13:30 +0000

    crash opposite comfortable play distance

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def delete_file(file_name):
+        os.remove(file_name)
