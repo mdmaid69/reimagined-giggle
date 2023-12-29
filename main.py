2a6b6fb63cabe20e12f5e1d3d0d380caccdc6a19
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:36:28 +0000

    start study score sensible drawing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
