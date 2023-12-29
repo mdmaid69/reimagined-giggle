9ea2eeb01e4b99ebded7dff3427426150ee42585
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:55:57 +0000

    will objection hit government split

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
   def get_file_group(file_name):
         return os.stat(file_name).st_gid
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
