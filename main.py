672c04c4a6fc9233274e85728d24358b188165d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:19:19 +0000

    takeback myself gas before complicated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_current_directory():
-        return os.getcwd()
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
