4f5e17a4445d9a9a37f041ecc6841f5e07089d4a
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:33:33 +0000

    swallow Dd development where build

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
