af2fb0aae1468212f4a35c7b7bf8601426e69006
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:37:20 +0000

    however difficulty age instruction avoid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_error_function(x):
+        return math.erf(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
