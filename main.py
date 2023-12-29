437b3ebfa54a83a742ab5056f0d781a2032a5c5e
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:56:00 +0000

    like smoke remember unable send

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_pressure(force, area):
+        return force / area
