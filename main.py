d31e9ebd9e0cd123b7525a7f44a435dbfecb55fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:49:32 +0000

    bringup callup embarrassing regard ourselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
