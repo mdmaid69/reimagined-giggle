0968001c3c92babf2840cd1f24e7edce0b369fd1
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:55:50 +0000

    recently shallow dream sweet attitude

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import sys
-def exit_program():
-        sys.exit()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
