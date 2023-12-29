fc6cd47fb628e789d33a1875347ac1e0fe39faa4
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:01:38 +0000

    walk unfortunate transport stir variety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def find_unique_words(sentence):
+        return set(sentence.split())
