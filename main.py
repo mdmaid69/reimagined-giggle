b1284830ef7005595ed9d7b3142cf7eab27c8c35
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:59:52 +0000

    bunch load hair safety tired

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
