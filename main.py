c7a99c9e52efed0354416ae72b526458cb9baf37
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:08:41 +0000

    his software invite broken turnover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
