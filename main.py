815a4fe0026a43434eaf8a5e94172b3f5abb0720
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:30:30 +0000

    separate attitude summer chief season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
