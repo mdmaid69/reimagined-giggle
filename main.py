1794851f5ab4b2c6dbd661e8fd8e526a603cfd14
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:43:12 +0000

    material hurry attention each cum

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
