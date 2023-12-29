e442e9a58dabeeed93a47f4cffe784575555b8e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:27:32 +0000

    Ss seriously view claim spend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
