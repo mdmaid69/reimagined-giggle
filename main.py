aa181a74eed9eac397b01dbfa92802c7d7ba8b81
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:01:15 +0000

    poison several suckme rob fold

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def add_numbers(x, y):
+        return x + y
