53604a455aa26e9d7f8a42bfb9d3ad8c15887308
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:25:09 +0000

    breakin adjective memory describe achievement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
