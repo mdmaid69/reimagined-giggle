d15d5a611de7b17c85cff6d78041387a7b033f69
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:05:00 +0000

    Gg prize process true design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
