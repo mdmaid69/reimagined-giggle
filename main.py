6a20995450a884d5701d07cc3f9f4599738fc0ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:36:55 +0000

    murder rely afterwards order study

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def convert_to_octal(n):
+        return oct(n)
