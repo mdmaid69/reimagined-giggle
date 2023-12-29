8ce702166e26a96127f20f3bc42b513791cc0e5b
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:43:51 +0000

    table greeting spot mainly action

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
