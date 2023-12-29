d14366bbceccc376c511eebb6d2507a89d6983c0
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:23:45 +0000

    sort distant breakdown fresh authority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import datetime
+print(datetime.datetime.now())
