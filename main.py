b54984581498e6a35a92169a9f2acfd56ef26e90
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:05:26 +0000

    one 567enemy government way include

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_work(force, distance):
+        return force * distance
 import json
-print(json.dumps({"name": "John", "age": 30}))
+def convert_to_json(data):
+        return json.dumps(data)
