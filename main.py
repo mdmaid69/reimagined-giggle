0498f94219b19638cb80c0e95c9179c97ca4eee1
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:57:38 +0000

    style regularly especially statement room

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
