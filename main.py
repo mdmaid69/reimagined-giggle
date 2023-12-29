606f32d296b4ffa015b453836d4054a4cb924249
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:42:15 +0000

    happen their lie shallow image

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import os
+def remove_directory(path):
+        os.rmdir(path)
