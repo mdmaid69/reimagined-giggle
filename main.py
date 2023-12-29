a99bf727c1ce612ba0c1b92ca4d41c95ffe4bd8c
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:07:39 +0000

    equipment let gift black wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
