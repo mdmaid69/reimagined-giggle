9dc0691b674d26773702d9f58197f253813916ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:39:05 +0000

    prize band law guess present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
