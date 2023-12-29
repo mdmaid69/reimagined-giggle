549b0ebed2bc8da1e1fc81adabcf096715bb506f
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:52:04 +0000

    variety it your food runinto

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-print([x**2 for x in range(10)])
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_power(work, time):
+        return work / time
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
