b3599af0ddd6a995c1a780e6fe46ae789cb31f3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:29:15 +0000

    minor favourite crash indeed pause

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+for i in range(10): print(i)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
