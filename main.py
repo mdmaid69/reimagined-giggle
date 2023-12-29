ba0bc7ec337a545fdb6b57ec180339b11aa84b1f
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:04:25 +0000

    moment perform feeling independent cruel

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def multiply_numbers(x, y):
-        return x * y
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+print(sum(range(10)))
