4d2de8f15fc34b952df7e9ba9d8dd681caacb968
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:37:43 +0000

    shrink himself unlucky style rank

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
