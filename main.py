cdddb5cbd43e6686ee69d63f2d1b40f4dc9be689
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:31:02 +0000

    rate workupto stay failure cool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def count_elements(lst):
-        return len(lst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
