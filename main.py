786a92050dc2ec217b96200e0e17bf8a0e43f0ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:04:32 +0000

    together kill fashion settle rubbish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
