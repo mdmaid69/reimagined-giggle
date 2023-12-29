4a4e5ed14236c205647ad4dabecfe0f17682b5ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:25:44 +0000

    pass wing pipe influence bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
