e37641233155bc28c7b0885843b3796faf0fffe0
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:01:23 +0000

    worst hurry explain continue word

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def calculate_density(mass, volume):
+        return mass / volume
