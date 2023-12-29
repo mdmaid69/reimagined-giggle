f3b01cfd9cce62c4a67d214d3356452662a28a96
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:30:36 +0000

    impression just design march interrupt

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sys
-def exit_program():
-        sys.exit()
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import collections
+def create_stack():
+        return collections.deque()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
