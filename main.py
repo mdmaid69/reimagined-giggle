90e7e9590d718f7ce35a4575db52727a6a8582a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:19:47 +0000

    hit wild previous mix month

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
