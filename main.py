9c10ba02e2a6d152f321bdaf7aa8d090515a56a8
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:32:53 +0000

    plan turnaway hope react charge

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
