ce9ec1f65aa2dd9ca400c446b380605d15360441
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:06:51 +0000

    lookback(on) opinion than attach contain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import time
-print(time.time())
+import array
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
