6503d0417254248c17b1c76dccbbeaa1acf53d6f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:25:30 +0000

    branch pure refer present find

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
