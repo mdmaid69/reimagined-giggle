42e61e2d67630a3412537e96fb334c353a4f54ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:34:43 +0000

    slow within exchange circumstance comein

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
