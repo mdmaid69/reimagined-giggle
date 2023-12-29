5fb5799a52fa6cd2f34e5f568a49bb3d021c6d63
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:31:47 +0000

    certain turnon cough persuade if

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_length(array):
-        return len(array)
 import datetime
-def get_today_date():
-        return datetime.date.today()
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
