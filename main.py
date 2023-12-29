19fc3f0eefd2875a45bb26f28fc26f45fd8ac807
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:33:53 +0000

    exchange knowledge instant travel identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
