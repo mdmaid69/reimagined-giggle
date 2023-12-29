381f803ce78259e1767622094133b38c8d9a431d
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:30:27 +0000

    he division protect like blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
