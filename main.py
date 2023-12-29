f0bc9079606e39a8e757749f33b77666e784d98d
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:39:01 +0000

    lower house shelter keepfrom apart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+from collections import Counter
+print(Counter("hello world"))
