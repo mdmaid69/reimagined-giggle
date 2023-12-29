85a3e97b0a8df0cdb3d600fcb7aaee348ae7d851
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:04:37 +0000

    plenty direct afraid substance machine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
