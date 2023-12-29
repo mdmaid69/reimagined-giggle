49c1b979f2313e6a1fdc78c74744dc9010706948
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:31:19 +0000

    order suddenly side their thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import sys
+def print_python_version():
+        return sys.version
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
