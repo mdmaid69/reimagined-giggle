b2596573953ba0b8346a393ab5b06816667c2504
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:55:14 +0000

    connected walk tear shoulder breath

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def find_min(lst):
+        return min(lst)
