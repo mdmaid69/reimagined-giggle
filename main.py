3d627ce0b8fb4719f75640dcef116514ae06062c
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:25:38 +0000

    murder skill Oo hit sit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
