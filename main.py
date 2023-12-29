dde1ff2731229176e19cdc4eb90eb8c1764cc500
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:20:37 +0000

    official would tongue murder band

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
