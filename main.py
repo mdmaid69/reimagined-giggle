bff443c3002cdf0af1bddd63f86844d740f8f1af
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:37:18 +0000

    list lower payment callback reaction

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
