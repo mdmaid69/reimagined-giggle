6b812931bc290567fb59c8aed713be1dce0a2606
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:10:47 +0000

    score pleasant trap squeeze main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
