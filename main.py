11ba5633365874c1d7d91bb30963eaeff7f8bc87
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:33:11 +0000

    breakfast treat easy and escape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
