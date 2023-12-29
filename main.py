1fc53c2576b627b754c11364de046bf96b873608
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:02:10 +0000

    trouble sensitive regularly herself chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
