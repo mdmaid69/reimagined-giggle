1274f0525495c931e71662a43b90c04a48e4f4fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:02:26 +0000

    stare maybe after violence sit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
