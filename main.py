14d78b539b5fee3f134e44000a7303eeaf645b9c
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:50:13 +0000

    ill innocent copy message move

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
