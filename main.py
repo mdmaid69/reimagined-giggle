3a6e864b975cc32a94614d826a12f0e51144734e
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:52:03 +0000

    bottom prove practise coffee use

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-name = "Python"
-print("Hello,", name)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
