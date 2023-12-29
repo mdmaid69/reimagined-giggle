30215364d912a4529f70e4c1de8f81467d585e56
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:31:24 +0000

    that cruelty within 573entertainment life

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
