b927bd14261b99050a3158b7459477da83468daa
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:36:15 +0000

    suggestion night thanks cover development

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_acceleration(speed, time):
+        return speed / time
