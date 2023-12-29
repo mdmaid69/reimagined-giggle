27cffdfd8e53d74b9ec2cbfb06d4c6e2318eee03
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:25:06 +0000

    interruption behind explain ring life

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
