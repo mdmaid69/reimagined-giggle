e8b94d4bf7ce96ebbb8217d7d0d82db69eb8cb56
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:47:11 +0000

    drink compete takeaway holdup will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+for i in range(10): print(i)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
