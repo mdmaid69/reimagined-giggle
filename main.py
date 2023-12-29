89c18ef871984b23297d38e4e75a2e4d6f23533e
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:40:08 +0000

    Pp soon dry loud fill

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
