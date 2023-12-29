aa19a4ae40cd4cc9314e5ddd967e0ff323d425c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:55:05 +0000

    direction book brother last acceptable

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def count_elements(lst):
-        return len(lst)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
