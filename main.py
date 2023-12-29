6a2c6ddc67d85ced069be7e7774f175c22e329d1
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:05:14 +0000

    cinema base smell roll occur

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_sine(x):
-        return math.sin(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
