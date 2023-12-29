fe639db08ad23ba475a2986a6ff4512d9db99f86
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:00:34 +0000

    across order turnoff weather mad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
