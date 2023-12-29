8a7367d94aacc16d5f7f31293ffb9fb7a1ad60f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:12:37 +0000

    day takeout anywhere communication provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_length(array):
+        return len(array)
