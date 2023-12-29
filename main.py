1dab6f15add6a1b50b4b8055757a88029bdb820e
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:40:31 +0000

    admire piece accident skin sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
 def calculate_mortgage(principal, rate, time):
         return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
