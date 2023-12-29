cfc32d929c6e3c4ae6f89739fb124f3412a48255
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:06:58 +0000

    curious makeinto fast tired cruel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
