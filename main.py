0a1fa7316b56c4746fe500df2aac59eaea143412
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:53:47 +0000

    wait after towards holiday violence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
