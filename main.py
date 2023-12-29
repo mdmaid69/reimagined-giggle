85faa046e127d40740cf08d11ba70f4ecb7e2417
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:25:28 +0000

    extremely habit artificial loud politics

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
