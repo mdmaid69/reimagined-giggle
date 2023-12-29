fb4383c9baea81a6b6d1ff48ba870f59cb7b4d86
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:06:53 +0000

    art shock content bored cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_density(mass, volume):
+        return mass / volume
