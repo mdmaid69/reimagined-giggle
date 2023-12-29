cb607ec7e0842e87b0521fa936dff7faa9a95bc5
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:42:52 +0000

    risk board bored side catch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_area(radius):
+        return 3.14 * radius * radius
