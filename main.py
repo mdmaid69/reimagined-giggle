a767bfb4b640275dfa36349606b9112ca1ba99d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:14:15 +0000

    command use crowded knowledge warm

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def reverse_list(lst):
+        return lst[::-1]
