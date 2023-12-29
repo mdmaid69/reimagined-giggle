d23ba67bd10f9536d6c62f04ded1c0b5e32fcbd4
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:10:31 +0000

    swim natural system treatment bill

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  def calculate_area_rectangle(l, w):
+        return l * w
