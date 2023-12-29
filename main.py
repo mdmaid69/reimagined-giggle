1ba056fb70f4382ebbb6e30a2fbd36bc126a5c63
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:50:26 +0000

    ride fairly hungry anything thinkof

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def is_odd(n):
+        return n % 2 != 0
