87caaa39d35d1b7009dd92794639fa023713df64
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:49:24 +0000

    blue awake cool would environment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
