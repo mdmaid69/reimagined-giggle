bda5d10f8a6748d0e27631b10a34f526a9465178
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:21:30 +0000

    crime steer goon birthday term

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def count_characters(sentence):
+        return len(sentence)
