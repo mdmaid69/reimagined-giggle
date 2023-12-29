bce3b99150eef70dc9d65b385fd6d84fa4aeced4
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:57:58 +0000

    night after century frame stamp

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
