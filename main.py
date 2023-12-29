c687577260631ade55ea5e20caa7fb53fbe27665
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:52:04 +0000

    regret success contact over lesson

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
