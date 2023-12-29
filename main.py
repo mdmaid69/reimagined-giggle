cba6eba07e43b5dede9eac97ae3947f0e4c016be
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:48:56 +0000

    throughout borrow arrangement tone suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
