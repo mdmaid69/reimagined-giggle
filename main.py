ceb3354d64bd4f98b7a4862d20c07108b95add69
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:11:32 +0000

    foreign loan one get shout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def sort_list(lst):
+        return sorted(lst)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
