0a96eb57175b45a8b85aa66a847966ec1c7851b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:01:20 +0000

    attract splash punish pause present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
