046823c204189b24cd11e8096ccc0374a4e6d69d
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:10:08 +0000

    pen subject confusing with ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print([x**2 for x in range(10)])
