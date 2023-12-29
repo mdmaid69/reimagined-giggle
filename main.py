5252f2c00cd80db606a9886ee0e62252604411df
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:01:47 +0000

    aim entry uncomfortable morning holiday

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
