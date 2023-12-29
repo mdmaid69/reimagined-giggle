2a2fc9fa7cdab70074cc3608d3ebe3bb60603d38
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:19:13 +0000

    deserve bang politics Tt pleasure

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
