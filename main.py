947ef0d03563dc32fbe03b7c84df9d38bef0e15b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:59:30 +0000

    middle thin recommend stare expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
