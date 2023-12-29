5a6108c31362cfb4d22518a87f69b6f55d2c594e
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:26:32 +0000

    what lend nothing organize giveout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
