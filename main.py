313150d59fa765e9cb43cd3ac5986ca73cf04a13
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:24:07 +0000

    legal announce quality middle strong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+  def calculate_area_circle(r):
+        return 3.14 * r**2
