a2d355157efe9a74b257977b5bb6fa4d9d925aaf
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:01:44 +0000

    exit university world dress consider

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
