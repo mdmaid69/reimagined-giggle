320e1dce7cbd198185bf2788406738cfd0653007
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:13:01 +0000

    right left warm fond benefit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
