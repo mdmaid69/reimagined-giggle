dd45210bc5b7ae7cb3fd6c7e128bda46a76fb6db
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:13:26 +0000

    571enough someone majority wise sure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
