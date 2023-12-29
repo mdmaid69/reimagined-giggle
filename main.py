43a26c467c0b5d9d49bc2f086ccca973bb164f38
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:44:32 +0000

    friend welcome cut original release

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import array
+def get_array_as_float(array):
+        return float(array[0])
