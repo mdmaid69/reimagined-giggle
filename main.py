903b3698ffc56dc562d5769c47cc8641fd7ee0d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:06:22 +0000

    ball exhibition inform brave weak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
