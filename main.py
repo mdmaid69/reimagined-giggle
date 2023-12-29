381329f562d145a11f825585c1ae45ec31bdbb1b
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:18:50 +0000

    distance whenever message holiday almost

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
