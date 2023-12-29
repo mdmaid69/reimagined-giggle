90b76dc85bb2016eda3d405ff6005d802a24e932
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:27:47 +0000

    union disapprove acceptable Kk cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
