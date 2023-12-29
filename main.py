830b6186a0a1f25ea46ecf0145abc5b10ceca895
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:55:31 +0000

    software machine tight improvement future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_binary(n):
+        return bin(n)
