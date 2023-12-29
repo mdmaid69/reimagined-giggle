d783d70d97a09572add7f4f402487ce0205e37c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:15:41 +0000

    difference reply challenge visit alone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_arc_sine(x):
+        return math.asin(x)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
