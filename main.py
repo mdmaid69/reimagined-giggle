e3a584c7629f823eb01da24fc8f824ae3455b230
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:36:47 +0000

    look mention feel slice Ww

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_pressure(force, area):
+        return force / area
