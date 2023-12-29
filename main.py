aec05def5416a22b3dc80de224b9ebd715caec4c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:13:48 +0000

    check afraid keep business trap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def subtract_numbers(x, y):
-        return x - y
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_acceleration(speed, time):
+        return speed / time
