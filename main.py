198424ce8fd70171d50c9e4e718a1bc1aa61b9f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:48:55 +0000

    load little use bridge sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
