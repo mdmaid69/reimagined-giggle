7f58050aff84d16dfdf55fbdb27f0d667ec14082
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:53:49 +0000

    table sale anybody connect escape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
