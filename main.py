c7e50c8d2575919a8c4ba95cc621e221f2965935
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:26:25 +0000

    anyone leave achievement rent start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
