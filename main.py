12fc30ccd3f9c0817b43040402c8efafcecf667c
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:24:37 +0000

    scrap cross look apart through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
