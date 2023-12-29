82208bbd8a8f3708d48b83a3ed4ba4d5a5f44350
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:24:57 +0000

    best chin on identify impossible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
