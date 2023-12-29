37a8f5a6818986a90f9987215b5cb692d7d22f32
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:04:39 +0000

    absolutely Oo punish hurt watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_floor(x):
+        return math.floor(x)
