eed47e5f3f4525d5f2f10e5eccca6acbbd9e7384
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:14:06 +0000

    drive beyond offer before music

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Uppercase:", text.upper())
