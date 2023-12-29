20804070614fff406a36bdba297b69ba1b36bd92
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:20:35 +0000

    wind travel do uncomfortable makeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
