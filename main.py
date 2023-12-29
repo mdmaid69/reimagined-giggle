98430c8b37e598efdad9664d0338818ec745f794
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:02:08 +0000

    understand as happen home fond

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
