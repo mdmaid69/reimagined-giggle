ad3253476426ac31be8298f73d6b53d61b3ef244
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:31:48 +0000

    bank comfort demand expression wheel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
