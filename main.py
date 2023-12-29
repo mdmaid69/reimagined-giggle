9bb572d92e8fca68c962e444600f24705e687555
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:48:33 +0000

    yesterday turnon Dd cruelty sell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def find_max(numbers):
-        return max(numbers)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+print(sys.version)
