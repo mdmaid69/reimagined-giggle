15ad1567dde6448dc8a8b09a8760d2e49a82879c
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:43:04 +0000

    true kind warm along patient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import collections
-def create_user_string():
-        return collections.UserString()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
