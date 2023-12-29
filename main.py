b95363343f6a7f7ec7feeaa01a3f7ea63af84ad0
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:28:23 +0000

    undo unfair injury strike 571enough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
