d5e9f4112c008c46b4ccc69c59127e3b0c2867f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:57:24 +0000

    quit board giveup case lie

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_power(work, time):
-        return work / time
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
