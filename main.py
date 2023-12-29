d1fb9e56d125ac7141f590f158b4a8d9dd444b2c
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:05:08 +0000

    dress picture anybody stare disgusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import datetime
+print(datetime.datetime.now())
