fd5984d0816074415f27d4817b2f22cbdc4d6f51
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:34:14 +0000

    treat shame sleep confused day

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
