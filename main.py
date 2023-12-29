70731ad7a013be509ccd5e7a33c6bab80a62f7a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:46:13 +0000

    spare afraid drawing speed achievement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def count_words(sentence):
-        return len(sentence.split())
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
