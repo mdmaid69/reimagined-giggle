6c4bd953c08cf839b3c4f65374d6255b9078e090
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:58:04 +0000

    desperate perform settle intelligent proper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def reverse_array(array):
-        array.reverse()
+def append_to_array(array, item):
+        array.append(item)
