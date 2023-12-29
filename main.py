4b64ae38ce99cf33fb54173f963117cff24b94b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:49:26 +0000

    turninto famous chief breakout perhaps

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
