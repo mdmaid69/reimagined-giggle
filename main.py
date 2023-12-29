72a0febd68042a0874cc5a999ba44616244d1588
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:40:37 +0000

    defeat bar rest length meal

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_counter():
+        return collections.Counter()
