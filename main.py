cbfa7d0f376763a65bdce528cb415ae653895b6e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:16:17 +0000

    strange Ee arrange clear familiar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
