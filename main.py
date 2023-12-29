3c3baf2028519d2640d735d07d4b5ba197b36710
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:02:46 +0000

    nervous hungry secret comfort picture

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_work(force, distance):
+        return force * distance
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
