d00254828a849722ca9144fad3669a3e88198562
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:04:33 +0000

    567enemy size art control crowd

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+text = "Hello, world!"
+print("Characters:", len(text))
