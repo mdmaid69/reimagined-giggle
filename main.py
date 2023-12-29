37845611eb974a0cecfac8bb68ad363889442ea3
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:43:46 +0000

    bother feature mean normal bear

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
