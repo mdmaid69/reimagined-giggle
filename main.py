9637441a4e0cd0c85803ea86f9967dac579e5948
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:14:33 +0000

    calm remark snow nigga then

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import re
-print(re.match("h.*o", "hello world"))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_array_as_repr(array):
+        return repr(array)
