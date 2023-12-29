f96b549c594f664434563a094e7e57b72cec0b98
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:08:40 +0000

    steal behind congratulations always risk

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print("Hello, world!")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
