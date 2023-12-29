551fb1c07cc0a206521dfc8955de661a690f5132
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:56:47 +0000

    breakup habit shock smile capital

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
 import array
-def get_array_item(array, i):
-        return array[i]
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def check_if_array_contains_item(array, item):
+        return item in array
