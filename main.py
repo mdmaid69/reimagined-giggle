6c33d9dda3361b5dc6e39c8e8ba49af1d11f06c8
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:42:54 +0000

    many lunch plant cheque school

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
