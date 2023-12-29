61a32e80d4ef20e40b0a65d5edd5015f43c75731
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:05:02 +0000

    speed always dark standard your

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import collections
+def create_user_string():
+        return collections.UserString()
