0e82d25fae45ac308ef07d5491e9a87f2691ab75
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:32:47 +0000

    breakfast fold cheap guess occasional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
