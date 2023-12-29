ec79c47d19cd18b35718f58306ae54bb0fe2749a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:50:03 +0000

    consider culture important beginning temper

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
