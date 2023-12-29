761b59a80ad40f92f6d3ca44027cbac32df0cbc2
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:32:29 +0000

    aim keepup cause free eat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
