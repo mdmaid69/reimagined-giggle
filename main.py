1e1e6d3ca87092ef746b5817d5320a8cb99ab8ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:35:30 +0000

    appeal cinema movement line talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_itemsize(array):
+        return array.itemsize
+import collections
+def create_queue():
+        return collections.deque()
