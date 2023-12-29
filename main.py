71e2709ffe9bdcf7782daef5174e9f63dc52a47b
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:02:51 +0000

    miss talkback however phone upwards

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import time
+print(time.time())
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
