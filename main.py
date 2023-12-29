43e6adbc980b424a7ef5f42582a83bb31bf35779
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:55:51 +0000

    most lookafter test final value

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
