c1b2199add18aefa416c6fc39f6cd266373cc9db
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:33:11 +0000

    turnback detail offer firm bin

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import random
-print(random.randint(0, 100))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
