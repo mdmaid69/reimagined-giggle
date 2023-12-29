72ee6154fdbfdd685d82fa8db32c4161d5564179
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:24:31 +0000

    again exact act confident shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
 import math
 def calculate_hyperbolic_arc_sine(x):
         return math.asinh(x)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
