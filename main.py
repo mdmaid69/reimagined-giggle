f793a862c8b792e53281b24b215774e67145f882
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:48:49 +0000

    female whatever section difficulty correct

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
