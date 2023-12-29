27a705a83184c53156252161bb5f488efd2301a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:08:37 +0000

    character getup grow hit smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def convert_to_octal(n):
-        return oct(n)
+def get_bytes_from_array(array):
+        return array.tobytes()
