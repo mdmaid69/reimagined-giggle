7a975301c3d73978ee039cf641ddf19621472625
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:24:09 +0000

    below system miss gift reject

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_work(force, distance):
-        return force * distance
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
