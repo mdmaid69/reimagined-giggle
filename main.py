6a9c395798501a99e89bc83ac7d3997fc9d40f53
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:28:19 +0000

    dance bend unkind rather leg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
