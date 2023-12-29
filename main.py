341a2bd3fca9bb84cdc011c5d5903b0a1d611d61
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:04:11 +0000

    ever smart between switch key

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
