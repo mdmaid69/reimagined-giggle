1b8a901480bb11ffb4962ff5dee8c3e0acae0cc9
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:32:23 +0000

    foreign smooth play fear proof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
