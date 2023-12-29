05a3daead53139d3b8f398e873cbea37ff3fe984
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:50:04 +0000

    meet nothing interview bin rare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
