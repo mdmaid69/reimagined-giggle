f64f30b469eef9794dadea7614ad1f6ce0688c6a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:23:05 +0000

    another impossible while tune press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
