f14dc23b30149bf54f10774dbf1070040b7686a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:09:19 +0000

    story simple about edge intend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
