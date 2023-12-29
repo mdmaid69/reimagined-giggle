2be5566c4fe63f8c8d6facfb60eff74263911a92
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:23:27 +0000

    shout maybe move anyone follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def add_numbers(a, b):
+        return a + b
