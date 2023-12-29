6c97550fd500e2da01a24d6755cc4f72b97b3b9e
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:51:49 +0000

    ticket ticket practise smooth giveup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_str(array):
+        return str(array)
+import collections
+def create_user_dict():
+        return collections.UserDict()
