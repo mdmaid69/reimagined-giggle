161586ef9a457b35be715a9ef45f2d8044ca0367
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:13:11 +0000

    exact powerful mail even rush

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def count_elements(lst):
+        return len(lst)
