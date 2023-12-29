5262cf0e1b2798e04bb538a1dca7148e9c47cc42
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:41:15 +0000

    ill close plan home trust

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def append_to_array(array, item):
+        array.append(item)
