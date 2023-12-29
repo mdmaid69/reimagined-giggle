3c4c6c2db94eda93a3091f235174ee3e3aa71d89
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:14:55 +0000

    classroom juice trial bush already

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
