9d900f1849a939ece7e9adec79bb6f1753479d50
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:18:59 +0000

    official tragedy secret agreement reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def split_path(path):
+        return os.path.split(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
