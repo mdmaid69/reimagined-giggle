bc5d25b94a7ab2a9b779327d1b589f2ce2000c56
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:36:08 +0000

    pot recommend without pattern Ii

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
