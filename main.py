0bc965e41e5b667ad7a5a3fce46307b2abfa4721
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:38:09 +0000

    straight coat high open open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
