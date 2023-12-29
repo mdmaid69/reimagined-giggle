da74331aef8416d6ff6b4760f5189254d84b8f74
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:43:01 +0000

    back plant hate upwards charge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
