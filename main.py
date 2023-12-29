b288abf852e27046f43bdaec848d1b4dfeaa40f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:25:52 +0000

    cold correct block home no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
