bac8336f89263e44e58dff3908854417f78d52ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:19:17 +0000

    stick round bread dangerous progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def add_numbers(x, y):
-        return x + y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
