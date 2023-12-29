61b3ea602a1ec5e0886a344b78c1790f3bd3edec
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:10:42 +0000

    attempt refuse curious skill beside

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def remove_duplicates(lst):
+        return list(set(lst))
