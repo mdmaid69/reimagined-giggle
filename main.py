f9dd596ab43a96a9ad03ac8cbd3db2f559a183f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:00:48 +0000

    side greeting spring take second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
