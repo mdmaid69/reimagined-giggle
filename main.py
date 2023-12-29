6066f9c1aded968e6f9e37f5080d9d0dd52f231a
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:06:04 +0000

    next attach joke one mood

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def divide_numbers(x, y):
+        return x / y
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
