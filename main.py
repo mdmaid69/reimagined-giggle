e38fc46d72d0c9105681a75314e8dd1e067cf235
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:31:56 +0000

    left tidy patience post slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
