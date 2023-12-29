eba3a85cbea8226995e427308dcb57ec8f9c9d7e
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:45:48 +0000

    behind ours oil nothing brief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import tensorflow as tf
+print(tf.__version__)
