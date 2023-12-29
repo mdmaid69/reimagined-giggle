225ed945f22f9ccc91d5b6ba4fa0554763ca1b42
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:20:07 +0000

    through promise generous include everybody

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
