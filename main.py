6e767d96501ee85e334de0529d5b1bfcba20471c
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:39:33 +0000

    recover appeal wrong thirsty gate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
