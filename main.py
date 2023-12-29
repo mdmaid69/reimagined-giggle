1fd71be9adb893b196429a44b358b60d159caa9e
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:17:40 +0000

    partner harm sure wild swim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_area(radius):
+        return 3.14 * radius * radius
