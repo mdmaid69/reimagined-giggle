dcf3222bbbb41c565369f09f80e7087fc4acb565
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:10:16 +0000

    bye year guide present attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def convert_to_octal(n):
+        return oct(n)
