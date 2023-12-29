d1389ad96a7a71dd672416b7ccfcfdd01ae37ea9
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:00:44 +0000

    bet except laugh cum name

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_min(lst):
+        return min(lst)
