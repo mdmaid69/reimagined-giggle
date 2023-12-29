0788e95ca2ed2682bd76dd346b5e6b4f992ad8ec
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:21:13 +0000

    partner wall offer ignore check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_typecode(array):
+        return array.typecode
