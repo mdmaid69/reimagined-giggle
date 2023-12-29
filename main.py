33d6556b7ea020dfb531cb247e1542e7ef6d268c
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:57:28 +0000

    carry effect practical profit model

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import logging
+def log_message(message):
+        logging.info(message)
