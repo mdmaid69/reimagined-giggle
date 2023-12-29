b8ecb165caa853fc8320b801e656ce3e77c0ca97
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:17:33 +0000

    guilty thin create visit process

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
         return "Hello, World!"
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
