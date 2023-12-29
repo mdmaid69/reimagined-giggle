f254fc10d522812e7372b5d7205804447cbcd38f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:10:21 +0000

    heavy too brave juice early

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def greet(name):
+        print(f"Hello, {name}!")
