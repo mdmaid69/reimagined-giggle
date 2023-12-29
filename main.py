75226655725b1f92793dbb8f885576a2d49d4818
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:44:22 +0000

    pain partner evidence mend OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def convert_to_hex(n):
+        return hex(n)
