775740f553d009d95301504972a0265f0cfe2996
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:02:57 +0000

    increase cover issue wipe deliberately

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+print([x**2 for x in range(10)])
