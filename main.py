e04d9d873ff53391dde3b6c5ec0a5c9d5d408448
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:30:29 +0000

    comfort sense soft pupil deliberately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_array_length(array):
-        return len(array)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def is_palindrome(s):
+        return s == s[::-1]
