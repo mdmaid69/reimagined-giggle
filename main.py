2b8d867f4b7583f225a1a856827fc24b7b3670de
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:50:43 +0000

    opponent nurse conclusion prevent far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
