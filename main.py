577ea359bb10411cfdf360ae4038d666afd3f1b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:03:01 +0000

    traffic kind neck terrible explain

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
