59859bd802356de1600080cef0c4d3de805549d1
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:43:23 +0000

    book right and cheat something

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import logging
-def log_message(message):
-        logging.info(message)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
