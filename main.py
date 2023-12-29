fca7498b800d094d7afc9ab55667e25c2e39c817
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:03:47 +0000

    buy argument themselves clue square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
