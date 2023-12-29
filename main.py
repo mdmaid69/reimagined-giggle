09b5b2bd9878d1edfe9b87c3b6a1b723acdf1b91
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:49:49 +0000

    delicate oil right sweet probably

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+name = "Python"
+print("Hello,", name)
