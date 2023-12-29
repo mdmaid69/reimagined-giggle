1aa21b4023480089fb21c728afc5be8d1577baad
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:30:53 +0000

    myself joke I pleasure his

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
