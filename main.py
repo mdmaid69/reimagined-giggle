a4c08afce9a6bf196c9c5bb85ed80bb9ca735238
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:39:48 +0000

    criticize birthday confident copy church

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-for i in range(5):
-        print(i)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
