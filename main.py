cfd1c4ecc5cca0cdaaea4864b450baed8fdf521f
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:32:14 +0000

    damage theirs sense patience country

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_power(work, time):
-        return work / time
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
