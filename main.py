419241524e7a6fe25dc4369c288ac3964584e59b
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:09:18 +0000

    shape run immediate set question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  def is_even(n):
-        return n % 2 == 0
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
