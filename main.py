bfd3ebc6c265eb0cb04574ff060e648d6fa1a882
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:16:24 +0000

    important at under along collect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
