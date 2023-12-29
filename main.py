97ecda40ab68b67cdf3e7d4310c8374404021684
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:34:43 +0000

    separate gate underneath birth post

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def subtract_numbers(x, y):
+        return x - y
