505aabce6c925b64281dbe1ab8a1ab99c6863641
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:10:34 +0000

    piece fail admire active project

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_acceleration(speed, time):
-        return speed / time
+def remove_duplicates(lst):
+        return list(set(lst))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
