0dda980e18c165e88b5e120e14bc0f1da5622b36
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:56:51 +0000

    tall official change promise balance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def square_number(x):
-        return x**2
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
