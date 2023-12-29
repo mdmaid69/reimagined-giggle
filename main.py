2062eaeaaf247ac2c91cc055772f4b65f97a64f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:50:38 +0000

    back accommodation cheque put spray

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
