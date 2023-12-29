544f6b44ed28cf717f5ea26c51406b1a183ae139
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:59:08 +0000

    usually need custom rich wide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
