b759bebf5140dade52f31eeb38231c5a2434e2a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:16:02 +0000

    strike age raw maintain height

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
