0ab5f149e1dff7bf66f0d572b24ab4860f0e4b25
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:16:15 +0000

    upwards ever class good Bb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def add_numbers(x, y):
+        return x + y
