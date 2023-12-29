c3d90b213518f1c7fc57e0ab61a5cebd9aeff834
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:23:11 +0000

    like attractive feel imagine also

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def reverse_list(lst):
-        return lst[::-1]
+import os
+def remove_directory(path):
+        os.rmdir(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
