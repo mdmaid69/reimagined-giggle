e1d981cffdf475cc07a5ec4813469834988b308a
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:37:54 +0000

    pause when story juice imagine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
