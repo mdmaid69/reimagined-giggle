b5a6f4305b91ab4b23ade1aeb89e81dded89b3b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:57:24 +0000

    go centre also climb takeback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-text = "Hello, world!"
-print("Characters:", len(text))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def find_max(lst):
+        return max(lst)
