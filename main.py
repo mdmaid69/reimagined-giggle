85e0737ab5f4ec1a57abeb558392c5403b320510
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:42:40 +0000

    getaway perfect regular ability still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def print_python_version():
+        return sys.version
