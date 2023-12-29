ca827bfa5a74e5e518c995172a3c56dbeef1bdd0
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:50:53 +0000

    sweet serious artificial face vision

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
