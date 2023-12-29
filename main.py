f473716325aa4dae084057580bd6441b2127a58b
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:18:33 +0000

    runaway judge ordinary go characteristic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
