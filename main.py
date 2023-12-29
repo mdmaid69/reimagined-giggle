17e9afa818edaaf21fddcf0c7ca958d5cb0ad031
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:06:43 +0000

    nowhere smooth fine clearly runover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
