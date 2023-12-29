a96414ae058759cea87e2b18db80caf74b4bc3ec
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:24:33 +0000

    getaway reasonable everything warn chat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def is_even(n):
+        return n % 2 == 0
+def is_palindrome(s):
+        return s == s[::-1]
