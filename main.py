f854e91a39fc200e4ca9b3b602bda54bb5e8fbd9
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:52:31 +0000

    subject discover speak attract fill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_float(array):
+        return float(array[0])
