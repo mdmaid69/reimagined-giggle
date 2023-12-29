ddbc25bfef03d7797fe5844cd20abd3f0634127a
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:23:10 +0000

    turnup shrink rich environment once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
