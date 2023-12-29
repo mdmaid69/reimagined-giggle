a5ab7b2a7fc71669234c09cb9eae2402878ac11e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:52:19 +0000

    wash definite both fairly that

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
