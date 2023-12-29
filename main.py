01b6dddee18149f6c8e7c6a1f7bf069a4ff56c43
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:21:59 +0000

    expect cut breath belief friend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
