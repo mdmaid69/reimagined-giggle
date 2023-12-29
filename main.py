8593b535529eede19c321e245a6e9b982ccdb2de
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:01:07 +0000

    grateful clearly continue copy cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
