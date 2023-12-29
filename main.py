906b576758d8c7b56b8ed30d3fb53600b7af3b4d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:39:47 +0000

    else strong unlucky stop scene

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_typecode(array):
+        return array.typecode
