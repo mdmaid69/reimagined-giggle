7e6723ef21e1635cbc9a55fa19a384bf680d4b86
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:43:13 +0000

    its use interesting water clothes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
