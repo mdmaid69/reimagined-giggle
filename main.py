ad4da640c28dd62d42e431793380b237b9daacca
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:42:00 +0000

    size anyone touch achievement blame

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
