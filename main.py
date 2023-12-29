cf7ef0ea18db8085d0d8c7428c9250df3491d2bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:05:15 +0000

    little order within comeback rather

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
