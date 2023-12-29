5f33bc9d4b73e290a2df87b3b96ffafc82471e2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:17:16 +0000

    burst lucky no artificial danger

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+from collections import Counter
+print(Counter("hello world"))
