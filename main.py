9aea3fd2a649a8b49d8a11a9eb2fe7518ef870fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:11:04 +0000

    protest teach distance minority either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
