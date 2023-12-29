872581cc29c097466faff275a7c4c61305c7fa52
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:36:35 +0000

    past far turnaway let reach

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def reverse_array(array):
+        array.reverse()
+for i in range(5):
+        print(i)
