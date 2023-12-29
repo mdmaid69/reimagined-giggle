83572a4d8eab751ca068fc8eaba821d0e7b04728
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:43:33 +0000

    contact detail sail party thin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def remove_duplicates(lst):
+        return list(set(lst))
+import time
+def get_current_time():
+        return time.ctime()
