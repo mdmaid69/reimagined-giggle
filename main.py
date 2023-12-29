5d1873cac4fd7bafb4ffc08d943fe31efe68183d
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:01:48 +0000

    drive conversation complicated street behave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import getpass
 def get_username():
         return getpass.getuser()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
