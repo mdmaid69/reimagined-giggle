c114feca36bd2d47cbfa7f94a0469fd04ca4e2ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:23:42 +0000

    pause break silent tape trial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import sys
+def print_python_version():
+        return sys.version
