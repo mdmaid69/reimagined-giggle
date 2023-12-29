94ecf6943ef09a674811c3f01ba1e67c43b19516
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:09:49 +0000

    upset throw so purpose scream

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
