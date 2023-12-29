be1be9777eac9269ecf16de70b4f247ea3ab2705
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:33:13 +0000

    appeal cause early demand objection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def split_path(path):
-        return os.path.split(path)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
