0d4138ccf4b2853796203397c22c8ed1bd853dde
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:10:16 +0000

    father raw front takeout top

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_perpetuity(payment, rate):
+        return payment / rate
