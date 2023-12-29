123b947e831b3f340b6754a386106570c2f433f9
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:39:51 +0000

    wide lucky later anybody usual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
