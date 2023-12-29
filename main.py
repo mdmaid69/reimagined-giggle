ca6ed173937c60c20ddabdc3ff656dc15835c7b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:45:44 +0000

    about once record interested advantage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
