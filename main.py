9e85427dddccfdda7b42fcdf4cb372c467dbbe68
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:00:40 +0000

    dream wing estimate holiday block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
