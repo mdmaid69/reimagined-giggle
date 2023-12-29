8786513e6f1be020500b20dfc9f9ca63fc9abd4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:53:10 +0000

    seem change terrible position pronounce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  def sort_list(lst):
+        return sorted(lst)
