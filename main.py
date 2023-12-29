63e277cc214ca773e832b2d497e764d8383e1740
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:42:07 +0000

    blame oil keepoff beginning mixture

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+print("Hello, world!")
