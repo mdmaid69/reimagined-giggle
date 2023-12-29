155da002c9d92c26d191d9b0348a7a75100413b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:16:06 +0000

    fat thought priority sensible certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_average(lst):
+        return sum(lst) / len(lst)
