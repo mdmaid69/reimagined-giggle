b5923fad84f047ebdfe9a481d7aa87496bdf239b
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:07:29 +0000

    regular damage reserve branch dream

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
