0127bc42384e4ff562979d441b1e98dfcb0569b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:11:59 +0000

    practical dream fresh spend admit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def convert_to_binary(n):
+        return bin(n)
