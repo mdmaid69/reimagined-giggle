b7c2bd635852c8b7365ce286004ed4c043d84e31
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:17:23 +0000

    easy attract because cheque considering

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
