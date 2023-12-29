9d1b8369513f17ad3a7e002d177ca86d4a757e39
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:33:44 +0000

    software children contribute hospital will

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
