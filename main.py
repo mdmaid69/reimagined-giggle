cd0339dc46437daaecb79df2632b76221a531d92
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:49:14 +0000

    Itself anywayanyhow read mistake increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import os
+def remove_directory(path):
+        os.rmdir(path)
