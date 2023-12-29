6a63968d0a2a660f080be6e10904ef8d916ea933
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:03:43 +0000

    sure position nervous prove believe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
