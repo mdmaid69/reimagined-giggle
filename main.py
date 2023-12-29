10d59ca7c19f7e474cbc079cbce98b45cbee1b1a
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:06:37 +0000

    behind Ss noisy tongue extremely

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_average(lst):
+        return sum(lst) / len(lst)
