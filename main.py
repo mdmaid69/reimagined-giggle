f4f48eb467a3b8c661ab5380009005988898fc3a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:12:47 +0000

    church rule seriously trial 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
