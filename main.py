56abbde5015ad21a142ce5eb61c3d7f61a79e84c
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:07:56 +0000

    clever turnon weekend perform colour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
