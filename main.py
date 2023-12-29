57c7d67d58c3a580d1e743e4fc962b7e06e17557
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:10:20 +0000

    stop advantage throw like wish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_work(force, distance):
-        return force * distance
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
