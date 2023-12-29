dbf6c1cf44fe62979ce0336343f288fd3ebe89b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:39:02 +0000

    chase everything slowly alone method

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
