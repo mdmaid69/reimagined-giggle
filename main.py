884965047f34e0fcc3df482cf922349d0513918c
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:02:56 +0000

    wear makeup original delight waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
