ff62d93d0c89e9b77ad194d8a5737e045b369856
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:39:30 +0000

    rate pencil maybe available somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
