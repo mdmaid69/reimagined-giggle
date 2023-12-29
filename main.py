d27bb6c02df7fc0e04b59e412cfee7167dc046fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:46:06 +0000

    ball hide least honest point

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
