0f1fc9efa3e912a7ed05e9b1dcdf4bd829d8104b
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:19:31 +0000

    show surprise base favourite lunch

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
