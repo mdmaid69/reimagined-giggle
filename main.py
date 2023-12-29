8c3114bd73715d925c18fda548727b80f475936f
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:30:30 +0000

    cross fun everything new arrange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_average(lst):
+        return sum(lst) / len(lst)
