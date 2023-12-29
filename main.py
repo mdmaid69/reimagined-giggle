b73f3122e56bd39150c9ee5e99d56ca49bbfd56f
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:19:49 +0000

    wake boil thorough prize entrance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_min(numbers):
-        return min(numbers)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
