b29f961e21063bcee255b8905f003f6da3597801
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:10:33 +0000

    noise benefit runinto legal interview

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
