f365bc542b651c7cc0766d57572404dbda066510
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:16:48 +0000

    influence sister prefer perhaps thinkover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def split_path(path):
+        return os.path.split(path)
