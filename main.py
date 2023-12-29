a1d1a4df1c7a9c82e3023edd924f25308f43af98
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:53:37 +0000

    shop callout low affect prevent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
