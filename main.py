32dc5536995698c3929e827dd72901c5a0e62b78
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:21:54 +0000

    human ought last increase obtain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
