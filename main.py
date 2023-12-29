91eb854802f8339a15d22a337e6898e328376c24
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:08:25 +0000

    drive traffic about switch thanks

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
