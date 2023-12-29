e0d72079e64cefd3403cf99bf38d87ce2efcdd14
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:36:32 +0000

    praise lookafter study bye quality

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
