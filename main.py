eee8631b54c1ff1128b3e3146ad2043e198895bd
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:50:53 +0000

    file seem relative character wipe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import getpass
-def get_username():
-        return getpass.getuser()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def convert_to_hex(n):
+        return hex(n)
