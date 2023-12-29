5ded8ca0b1cc32a3fd6be6c0076b052ad803ebaa
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:48:15 +0000

    takeoff finally unusual breath loud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
