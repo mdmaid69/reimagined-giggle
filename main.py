5f14ca8174586d5d692d72cf5d8ca56633705fce
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:52:28 +0000

    concern tour unusual brush general

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
