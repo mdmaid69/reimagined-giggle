16919a8aea928967d4fb503e21d429afbd20920e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:29:41 +0000

    makeinto impressive march towards free

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
