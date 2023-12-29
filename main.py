f659050b3e6244d9ef695922961c56f42142b512
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:16:04 +0000

    open male past deaf side

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
