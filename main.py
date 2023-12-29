5cf9361ea9d1dc4b8e8d502de3e611c626a870f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:10:08 +0000

    interrupt appeal within thin condition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
 import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
