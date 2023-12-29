dadeec7629671b2ccdd8050217796b6ea8b62690
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:06:58 +0000

    last star you market stranger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
