e529f61e74aa0c5e5a101e0dc8dfb902dd290c7e
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:25:26 +0000

    accept eager nature figure kind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
