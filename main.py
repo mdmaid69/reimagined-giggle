4bf18b2a2431361b6f270299bf088424cdede453
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:45:29 +0000

    comeoff operate trick afternoon although

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
