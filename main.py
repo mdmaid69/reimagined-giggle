16456cb9e14faafba0355c01c63c75b77549f32a
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:26:32 +0000

    valuable wake operate live spend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
