71f602fce79c1c659b135f8cc512590ae4e5633b
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:23:41 +0000

    lay clean stand matter clever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-print(random.randint(0, 100))
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def sort_list(lst):
+        return sorted(lst)
