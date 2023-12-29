4f51a2af50d7030fbde70fc332bffaaf8041f267
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:16:04 +0000

    basis plenty experience share object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
