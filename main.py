053b21c56f1c2359c607bfdecbe1551db8dacbca
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:51:00 +0000

    earn downstairs memory window close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
