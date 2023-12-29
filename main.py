e1bc267362f79147134bc8309d0c8b292b639495
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:58:26 +0000

    late absolutely test treat help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
