2bb7c63345593a623cfd796e51a53c7bda5e9807
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:10:43 +0000

    bar unlikely custom church runaway

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
