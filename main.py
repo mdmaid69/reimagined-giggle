b2140b012aed52595641900a3195757f12fc91c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:20:13 +0000

    tough reduce on file appeal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Characters:", len(text))
+def find_min(numbers):
+        return min(numbers)
+import tensorflow as tf
+print(tf.__version__)
