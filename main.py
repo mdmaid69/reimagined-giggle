cd9d0c72b1ced671aff84609b69e8ed916afffe2
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:37:46 +0000

    cruel lock offer waste tread

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ def read_csv_file(filename):
         with open(filename, "r") as f:
         reader = csv.reader(f)
         return list(reader)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
