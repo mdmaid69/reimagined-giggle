8c0b38632df3e53631a92a9d729a0afa48764448
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:43:28 +0000

    condition insist protest material fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
