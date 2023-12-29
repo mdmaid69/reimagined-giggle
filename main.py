fabfe9cee93cb184d7bc60665f3e4307cdc8e24e
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:26:20 +0000

    fortunate forward file treat 572enter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
