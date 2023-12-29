a6b99f2511aa19cb600abe98a28f2fe3a212325c
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:58:00 +0000

    sort hang smell measure wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
