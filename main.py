fdcf2baef209413bd178200ff3fbfff8c3c3b99e
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:57:35 +0000

    comfortable murder language finish store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_typecode(array):
+        return array.typecode
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
