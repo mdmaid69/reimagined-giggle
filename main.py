e73d898908d03cdbd667e7ca39ebc68f5b82b455
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:07:50 +0000

    far stamp likely own hurry

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
