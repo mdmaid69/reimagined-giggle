1a671c99a890af3f7a28d4aeb8514b94e9181868
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:50:09 +0000

    original building petrol order difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
