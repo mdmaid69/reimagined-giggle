3f35a58e2d3d5abdcec91ad6a0ed9692565804a8
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:48:58 +0000

    of sometimes interfere turnoff tooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
