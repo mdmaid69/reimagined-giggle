b640070307c6ffff5d98f1dac8cd0742c865fb42
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:55:34 +0000

    suit window best throughout whistle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
