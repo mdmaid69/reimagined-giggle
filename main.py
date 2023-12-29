69d0a43118b516c11a76e58d60ac85de39edcc20
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:44:16 +0000

    cold blow downstairs comeoff forget

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
