453865e4a105047b1edc220a119fcd149212429a
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:26:59 +0000

    rob instead ours here feeling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
