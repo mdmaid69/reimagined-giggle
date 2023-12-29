7690c1f235afaee75df033d83473b3cda80cbf00
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:38:26 +0000

    score serve foreign Vv run

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+print(os.getcwd())
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
