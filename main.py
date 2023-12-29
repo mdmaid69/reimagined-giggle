98c94caf0f9120ece35d6727ead3f43e67477680
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:30:52 +0000

    bet long grade increase string

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
