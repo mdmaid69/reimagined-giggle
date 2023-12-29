27b13da64ea19c6be34566ad6858e1309dcb2586
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:41:53 +0000

    trust care fact inform announce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
