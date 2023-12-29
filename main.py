5aae8a958314036ef3dc25aee1c8543c562e6e82
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:23:55 +0000

    draw Kk twist complete general

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def find_max(numbers):
+        return max(numbers)
