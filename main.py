1cf94d0d00a940ddfd9240b762a59f09d4acc07c
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:36:16 +0000

    because home towards bet fierce

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
