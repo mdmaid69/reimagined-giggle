b672a50caa7507072f08668f3e13827902b326eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:37:41 +0000

    advantage cross responsible lift such

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
