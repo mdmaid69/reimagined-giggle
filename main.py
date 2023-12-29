3fc5ae715f7e4a140b1ec8a52dc397477ef91cc9
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:10:24 +0000

    rub matter balance though switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def convert_to_hex(n):
+        return hex(n)
