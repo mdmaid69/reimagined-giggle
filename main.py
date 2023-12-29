0a0e92e29aa01cc581c5203e404120ed6e709b58
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:53:48 +0000

    any sink attach OKorokay squash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
