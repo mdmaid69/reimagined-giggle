3151b80cb75a339e04a95000ca0b984d6cf8261d
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:36:01 +0000

    turnover embarrassing fresh do dress

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
