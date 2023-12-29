316ddae3076f299fbeea54e4bcd4719605a8d99e
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:20:17 +0000

    left exhibition defeat picture content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
