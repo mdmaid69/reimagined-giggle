7796788834858851c832dfc7b7338106032535a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:26:35 +0000

    Ss slowly silent glass lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
