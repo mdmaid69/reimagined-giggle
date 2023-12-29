2a47d1c67ca59e6db803d32e3d0434f06421693b
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:52:48 +0000

    combination within this available clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-x = 10
-y = 20
-print("Sum:", x + y)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_volume(length, width, height):
+        return length * width * height
