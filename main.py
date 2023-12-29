34fc62c83181c199fa116f7ff0de94e8670249ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:09:49 +0000

    stop unite load dream conversation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
