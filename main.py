a1002ad26341918d56fb3dd991fe7473cce30113
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:56:37 +0000

    remove promise wrong section experience

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def convert_to_binary(n):
-        return bin(n)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
