2e59a02ebf9b2143211663ec338a79291d1530ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:59:06 +0000

    gentle meeting fly call prove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
