8714b4527fb1e5b2c4acbb8d464f6ed8bd030355
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:18:31 +0000

    terrible wild sister write ocean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
