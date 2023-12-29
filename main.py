37e36f42bc6b30c1cbbbfb73da67d2f3683cf41a
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:12:21 +0000

    effort inside gooff official emphasis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
