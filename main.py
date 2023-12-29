57e65f02fefa97628d6a1a15253aee153efc6fb9
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:28:35 +0000

    this sign correct need now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
