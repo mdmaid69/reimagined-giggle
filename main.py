f91b5805fe75bd111afab1b5b8cd3490b6f7a639
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:50:30 +0000

    second tall shine board medium

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def add_numbers(x, y):
+        return x + y
