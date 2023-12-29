3bf6984319802db6211a442dadb79f73b70081c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:39:49 +0000

    sensible funny foreign back control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
 import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def create_user_list():
+        return collections.UserList()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
