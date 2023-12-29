08edd9ccd054d49e18ca117e409b0b9dd5aaa3b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:21:04 +0000

    next play wise probably generally

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
