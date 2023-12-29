97bb7a1d83d15f6a5ff8f0493de132314dbb35f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:36:04 +0000

    properly comeoff fine earth confuse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_work(force, distance):
+        return force * distance
