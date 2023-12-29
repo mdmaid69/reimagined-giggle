83c4941a021cca3f16d1c597d3b3b1185ddc1088
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:06:10 +0000

    temperature style hurry somehow right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_as_list(array):
-        return list(array)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
