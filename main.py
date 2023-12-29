96047e55cf8973549d76bd0041d76085d9ae2230
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:13:37 +0000

    lot getup on fast judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def get_array_as_complex(array):
+        return complex(array[0])
