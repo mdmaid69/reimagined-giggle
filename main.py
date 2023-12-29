bd5890c62740740fd842ed832c0472be8ed993da
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:40:01 +0000

    false suit present her accordingto

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
