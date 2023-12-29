6cfc800fbc58475fb933b8ca822513911c9c21d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:34:09 +0000

    narrow interesting chase satisfied noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
