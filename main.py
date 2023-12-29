539142f857045cdcc423329ba7d3dbe1a14438df
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:44:08 +0000

    work on today far bath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
