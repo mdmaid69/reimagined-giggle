9f7743ef578795ea3e825b9ee6b7d78c7af8c94d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:19:10 +0000

    measure desperate once direct look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
