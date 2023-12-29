1bea85fa609b05e485cf04a890c22cc375c8e609
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:45:14 +0000

    sure persuade environment disappointed correct

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
