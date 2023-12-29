e2ddc8787d9f3f4b0adc8548e013fcb815792f21
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:04:43 +0000

    profit embarrassed swim when underneath

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def set_array_item(array, i, item):
+        array[i] = item
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
