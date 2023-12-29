0ef6fe9698665bfe041ff2ad991b4acf06c35bb4
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:07:28 +0000

    programme interesting could question nearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def count_elements(lst):
+        return len(lst)
