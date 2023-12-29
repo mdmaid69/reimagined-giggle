fce581a809481353d3b69f976f390dcaddbf8feb
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:18:49 +0000

    around freedom special theirs children

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
