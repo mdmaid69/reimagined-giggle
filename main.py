8d540de78b7b8f74258c1f32e652653f9c5689a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:11:37 +0000

    formal joke 573entertainment start lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import numpy as np
-print(np.array([1, 2, 3]))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
