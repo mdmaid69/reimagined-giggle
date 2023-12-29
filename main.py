37cc99992afc96cb86eb3ad5e75e9fe012914085
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:00:10 +0000

    mess scene lot dead attempt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
