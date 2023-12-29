af8b6562d6e02f3f4ace72be7f829ab6cf8f6c8b
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:25:08 +0000

    first steer string trousers mess

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_acceleration(speed, time):
+        return speed / time
