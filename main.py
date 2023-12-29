99b884df5f1362df9c0858c59e1a236a81133ce6
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:24:48 +0000

    prevent centre interfere deaf gradually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_as_str(array):
+        return str(array)
