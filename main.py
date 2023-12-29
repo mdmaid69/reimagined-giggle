7d64ec31f39202f6eddc562ae530a30a25180cb3
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:22:55 +0000

    suck regional comeup middle however

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
