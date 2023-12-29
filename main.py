76e24682d195219851ed6f078cdfd7f6507378d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:09:04 +0000

    rank attention dress wherever treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
