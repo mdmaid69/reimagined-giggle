c5899deb5289c1c08eb0b24fce99be24153b8832
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:17:43 +0000

    war complain react Pp accidentally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def reverse_list(lst):
+        return lst[::-1]
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
