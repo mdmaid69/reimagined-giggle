80e293f526ea51bc545cadc4c3d70e28875790df
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:37:26 +0000

    rather basically impossible callat or

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def is_even(n):
-        return n % 2 == 0
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
