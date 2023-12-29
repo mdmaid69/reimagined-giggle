14238f138f42cc6e7eb349612bf89258460e9fa8
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:26:22 +0000

    shut age soon heart naturally

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-import numpy as np
-print(np.array([1, 2, 3]))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def greet(name):
+        print(f"Hello, {name}!")
