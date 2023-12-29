f54f9609548b2c2ab7662b3d1165e5f38c7665fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:16:37 +0000

    run too wish swim past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_force(mass, acceleration):
+        return mass * acceleration
