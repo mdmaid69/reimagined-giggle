e5bdb541662dada835e9920ddbaba24aa1e6c3d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:44:31 +0000

    embarrassed equipment image herself giveaway

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def calculate_area_rectangle(l, w):
+        return l * w
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
