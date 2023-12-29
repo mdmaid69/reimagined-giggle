876db12aa43ddaafecfd08f683dd138cb4957b46
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:22:46 +0000

    breakfast appear lonely wild harm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
