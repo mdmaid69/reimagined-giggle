6c4c1ec7ea0feb459736a06d1c5f0e80019848a8
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:14:05 +0000

    tidy interest protect reply confusing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_pressure(force, area):
+        return force / area
