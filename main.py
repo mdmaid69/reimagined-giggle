6314a8e17e5faedd47d30e80dbeaef480aa971e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:02:00 +0000

    break busy time tidy building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
