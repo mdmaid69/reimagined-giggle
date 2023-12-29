21ca920af134cfa2f66b84f1cd97988336042206
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:36:36 +0000

    spill factory spring anyone conduct

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
