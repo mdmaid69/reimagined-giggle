3aac538f4caec4b2262c86522b4276c88e417c63
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:10:24 +0000

    react quality meal contact argument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+print(sys.version)
