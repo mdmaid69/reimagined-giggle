ac78a85ad22750d7f20c71843a28fdc3a99ed028
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:05:46 +0000

    sleep ear considering fall finger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
