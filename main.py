f0868beb013a80999bb857f8523e02b6494f98af
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:19:45 +0000

    when right sort comein stop

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import collections
+def create_user_string():
+        return collections.UserString()
+import random
+print(random.randint(0, 100))
