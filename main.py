e518875d7f017154139ee41f6567cc1b8518314d
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:14:43 +0000

    length make recover begin plate

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import math
+def calculate_error_function(x):
+        return math.erf(x)
