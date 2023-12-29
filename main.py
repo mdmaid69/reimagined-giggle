b297b7135905dcc21ee914bf295f7fc0760ee20a
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:21:50 +0000

    remark burst plan but school

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def split_path(path):
+        return os.path.split(path)
