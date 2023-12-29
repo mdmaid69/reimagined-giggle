be7d99c1721521571f89aa82c29a9332bd10c1b4
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:53:47 +0000

    distance takeoff impressed spite possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
