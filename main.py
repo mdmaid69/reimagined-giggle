5fb9de65a7e81f9888640d7e19bcfb68c68d8848
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:34:21 +0000

    satisfactory cause hers another 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
