9d7b195d171299d98374ce9cc3b25650eff3f464
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:35:47 +0000

    whistle unfair promise guess sure

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
