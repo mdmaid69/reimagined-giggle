235022b3c2d22bc258caf9c10aeec8bb836ec924
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:55:24 +0000

    mean takeback several dig responsible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
 print(np.array([1, 2, 3]))
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
