beb9e5059942a5c8bf07976f6302d8729ff2c552
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:02:53 +0000

    central weigh callback through 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
