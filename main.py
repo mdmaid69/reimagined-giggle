a74ca280f2bc073da5d6a40dad5b64f78645baca
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:36:59 +0000

    sorry manner tradition unpleasant crisis

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_density(mass, volume):
+        return mass / volume
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
