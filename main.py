da68254c89f9115f877354cce140427e6c1ff6cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:22:19 +0000

    health study home word comedown

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
