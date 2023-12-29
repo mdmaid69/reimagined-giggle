4f276eb25f3a0a0880ead816f2f090e3107bd440
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:30:13 +0000

    petrol avoid happy opportunity knowledge

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import datetime
+print(datetime.datetime.now())
+for i in range(5):
+        print(i)
