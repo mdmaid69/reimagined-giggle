a040a281eb97c6fc31d91ace0194e998c529075e
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:01:17 +0000

    measure wind fail result lend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import numpy as np
+print(np.array([1, 2, 3]))
