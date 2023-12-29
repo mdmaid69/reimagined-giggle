80a7ba9b610ae9c80da8212c55e3db88d896db10
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:30:32 +0000

    boat little exam Kk appear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-from collections import Counter
-print(Counter("hello world"))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
