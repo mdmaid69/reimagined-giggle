1a54f8215ba2ec6554cb41c0189476f2d3c31c18
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:43:28 +0000

    market behind contact careless wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
