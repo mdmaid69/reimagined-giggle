e500e7dc838b78396fdca5e6b992a4f8aa6cf9ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:16:43 +0000

    bright greeting alone book sky

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
