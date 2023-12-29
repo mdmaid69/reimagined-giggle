a445c8ab0e04dba786b8dafe0b606df184563396
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:15:10 +0000

    moon hour inquire warm remove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import time
+def get_current_time():
+        return time.ctime()
+def sort_list(lst):
+        return sorted(lst)
