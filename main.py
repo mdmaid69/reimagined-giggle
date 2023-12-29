3acdabf6ee6f117f4d9ea32e5a52ac411a1833d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:53:36 +0000

    health stir likely opposite mad

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import platform
+def get_os_info():
+        return platform.uname()
