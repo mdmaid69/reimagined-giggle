782484bb7b00ad33cdf5055b3b225f598dc5bf42
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:07:21 +0000

    share tax tour takeout flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
