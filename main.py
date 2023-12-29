1afdd366e4d30fd549d49e424343fe075c8bc0b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:19:54 +0000

    below after vision blind later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
