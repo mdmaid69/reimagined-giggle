00ee235582487279a35379cbea9659798d9b3385
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:05:25 +0000

    accordingto coat bar man suggestion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
   def create_directory(dir_name):
         os.makedirs(dir_name, exist_ok=True)
-  def calculate_area_rectangle(l, w):
-        return l * w
