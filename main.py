c13004f7b3de3f4ee2e2ff975abef89891e7e5fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:29:50 +0000

    system appear solve wrap pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_as_set(array):
+        return set(array)
+def find_max(lst):
+        return max(lst)
