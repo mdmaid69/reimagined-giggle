f721569e2570c617708087a459f05cd43e50998b
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:05:32 +0000

    blame summer throughout field quantity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
