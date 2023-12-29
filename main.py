a05c53da70e9b083f515e42718ed0295ec04f98c
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:08:43 +0000

    first before calloff near describe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def multiply_numbers(x, y):
+        return x * y
