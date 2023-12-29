af114ac6e9775b9d599e5fe3389bcd3ad8437462
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:12:38 +0000

    raise freedom anxious interrupt neat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
