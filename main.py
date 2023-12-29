42143559092bc4c761bf78b4161677ddc7da2bc3
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:27:26 +0000

    relationship embarrassed satisfied fierce contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_user_dict():
+        return collections.UserDict()
