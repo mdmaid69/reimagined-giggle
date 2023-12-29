5125429ebefc0be76739826df592b926a9ac7253
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:10:46 +0000

    point suggest minimum study ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_counter():
-        return collections.Counter()
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
