70c99470b589016f18dbd356c2c552cb51c063f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:39:07 +0000

    attitude scream ahead foreign callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def add_numbers(x, y):
+        return x + y
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
