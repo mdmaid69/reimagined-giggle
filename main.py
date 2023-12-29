89e95ca31c7326e0df1ce7495c24c00514b9a24f
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:40:42 +0000

    our touch murder cross patience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
