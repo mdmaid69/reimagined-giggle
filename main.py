8899a0a228ff60db32ca52678abde38c0e035464
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:20:50 +0000

    flat human mend relationship mean

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
