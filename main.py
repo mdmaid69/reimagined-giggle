ef8d06e7cc8b56e1f955a32cfeb19f8983f57199
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:51:30 +0000

    sensible cook week total owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
