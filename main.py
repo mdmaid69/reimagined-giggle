f87ca78c97a752e86db07165dd5c3871d009e1fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:05:53 +0000

    exam disagree turnoff surface body

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
