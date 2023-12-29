09ec13b16328384e6bbd9c4bd7e987ac7d3902f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:51:03 +0000

    wander curious let passage awake

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
