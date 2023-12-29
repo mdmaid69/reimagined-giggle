7733a1674d5601623c80605ca5d87c4c01c99dd5
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:29:40 +0000

    tonight book sign during bread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def is_palindrome(s):
+        return s == s[::-1]
+import sys
+def print_python_version():
+        return sys.version
