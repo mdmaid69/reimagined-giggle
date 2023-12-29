a5cdf5af4aeb0ccaa96bf9bc1baa10a603d91b10
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:10:52 +0000

    concentration Ii plastic drop excited

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def find_min(numbers):
+        return min(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
