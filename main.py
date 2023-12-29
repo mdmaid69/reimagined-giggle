d2206619772fa1ae38f7b713660d117c9afd20b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:41:31 +0000

    exaggerate smoke kind turnoff exit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
