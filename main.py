41f17b50b0fe129749fe815bc8b5ecfc82ede81c
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:02:59 +0000

    upstairs home minor cry happen

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print([x**2 for x in range(10)])
-def calculate_work(force, distance):
-        return force * distance
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
