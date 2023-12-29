1bd29652c978557c660549d6857700283f053bf1
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:23:25 +0000

    world pleasant control anywhere married

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
