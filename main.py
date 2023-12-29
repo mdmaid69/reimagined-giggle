3b37bfdd3b625c26f48699ebe831187f448fb6df
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:20:54 +0000

    right open pocket land minimum

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
