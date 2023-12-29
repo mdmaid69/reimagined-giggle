e95b83147c27f8f585209615f3f1798acf6999fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:01:59 +0000

    clear colour familiar shoot own

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
