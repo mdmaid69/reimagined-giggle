a3a6fae2a1cc2a13bbd57dde5c9f79f3c8ee2206
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:48:31 +0000

    fire passage form takeback main

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
