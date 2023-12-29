45e55972c0fd90ac68316b07ccddd2a036b9a495
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:56:30 +0000

    nearly meet declaration section waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def change_working_directory(path):
+        os.chdir(path)
