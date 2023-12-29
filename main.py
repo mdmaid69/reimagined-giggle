401581191293b165b578521179e3c46abef5beec
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:51:01 +0000

    stage contact powerful shop blow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
