ca4b48bee85dac35272e41595536e2b825f79852
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:04:33 +0000

    round strike tomorrow next equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
