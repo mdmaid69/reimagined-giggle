566a17c3aafa557d60218611392e3e5a051396aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:42:51 +0000

    responsible action actual fall after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_user_string():
+        return collections.UserString()
