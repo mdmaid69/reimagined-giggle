fb9fe92257fa1a28f4a3fce3ac8045f63e4e7fad
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:33:16 +0000

    turnaway pause cruelty truth lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
