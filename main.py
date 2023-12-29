cf5ae69c0225ebd6497b6cdbb796d3f171fde762
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:47:39 +0000

    everyone work decide getback turn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import tensorflow as tf
-print(tf.__version__)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def find_max(numbers):
+        return max(numbers)
