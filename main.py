ba9778a83d1a3a49f0f9e64ef5287d9527eea4f3
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:59:41 +0000

    shadow sure second lose basic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
