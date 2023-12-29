fa8048dd458939d1b97dca646b6898634448df30
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:10:31 +0000

    pay well strike world after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+x = 10
+y = 20
+print("Sum:", x + y)
