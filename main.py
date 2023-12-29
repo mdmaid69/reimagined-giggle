08352a918e03be77cde459a57090b0d52ada73b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:33:02 +0000

    accordingto sweep over nigga police

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
