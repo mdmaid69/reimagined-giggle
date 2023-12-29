100f0a6ec6156e7bdb379bbe36ab2345755ba2f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:08:56 +0000

    steam content perform bend agreement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_index(array, item):
+        return array.index(item)
