b7de211463bacb6814dfc82c50ee9201579a3146
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:32:35 +0000

    there further criticize care mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
