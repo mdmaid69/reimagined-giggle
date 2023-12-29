6fedcd08cd353f415f654f48d7aa718b59feccd9
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:33:30 +0000

    side soft closely race poison

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
 def calculate_tangent(x):
         return math.tan(x)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
