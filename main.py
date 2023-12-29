df52b074552df352d9f17877902c50b9d23bb511
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:03:48 +0000

    object whatever damage sound along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def convert_to_binary(n):
+        return bin(n)
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
