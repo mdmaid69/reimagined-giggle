b47fef6cecc437ba0d561b6c96435a72d7681b42
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:01:10 +0000

    bang water giveup office broad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
