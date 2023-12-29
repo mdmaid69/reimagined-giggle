2b2ba4f8b94d50b9722e5712470db11b43a7e732
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:03:18 +0000

    she along example muscle inside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def is_even(n):
+        return n % 2 == 0
+import json
+def convert_to_json(data):
+        return json.dumps(data)
