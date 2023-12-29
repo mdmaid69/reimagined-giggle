06a1b23ef742d4cb079180170ec13722c85d3ddc
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:59:32 +0000

    tidy suddenly cross active last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
