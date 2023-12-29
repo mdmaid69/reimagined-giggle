3763f769777032b842ab499f7f8c6741ee9f71a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:03:40 +0000

    impatient fly blame impressed table

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def find_max(lst):
+        return max(lst)
