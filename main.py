4315b4a551fb75fbd2a924c8a1e497c3e0355a09
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:00:12 +0000

    blow miss once brush age

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
