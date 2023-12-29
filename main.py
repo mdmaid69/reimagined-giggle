4a22f87c84f0879a1addeda6e3791a4a1d625ba6
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:34:58 +0000

    in drawing whoever suckme party

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def count_words(sentence):
+        return len(sentence.split())
