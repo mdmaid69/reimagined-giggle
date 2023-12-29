88c98be2c18f47e253aaac535c137a7cec1f7059
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:00:48 +0000

    means pleased front birthday want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
