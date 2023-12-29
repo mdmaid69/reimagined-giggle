929b9b985faca68b080df00c88952c9e81d3dff7
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:42:14 +0000

    group guilty adjective real moment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
