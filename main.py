d32910597e8a979a5d4a8028c3ce1bbe560ff6ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:30:16 +0000

    discussion rest cheap cover 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
