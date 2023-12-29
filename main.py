6152332ae7d9d145bc9c287820d0f8e60cfa3203
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:12:16 +0000

    reason desperate help recent turnout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
