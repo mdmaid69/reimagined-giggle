34b5f67f219b84c399240026c1939b141a3a5f93
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:29:07 +0000

    satisfactory in soak famous complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  def convert_to_binary(n):
+        return bin(n)
