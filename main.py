75bbc0158cb7871e06e45d6b9bd74a962a2661dd
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:17:46 +0000

    exist cost size feel urgent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
