9abc1fd18f6ab84f0e8eba5895d47aee4b4a1ea5
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:30:03 +0000

    deliver forget transparent hello shut

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
