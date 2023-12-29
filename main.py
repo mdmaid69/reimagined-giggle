fd19cffaa102a29c07fc5caf4a3451db2bb045cf
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:00:03 +0000

    manner improve minority edge mend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-for i in range(5):
-        print(i)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
