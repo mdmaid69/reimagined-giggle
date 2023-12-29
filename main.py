c4986f0b2e779e18d711e6ae43200e2f51ca18dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:16:21 +0000

    waste unit damage objection old

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
