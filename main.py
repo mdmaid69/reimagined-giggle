388a3f319e56c074889f09a29519084749b4f008
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:19:34 +0000

    anywhere be approve support 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_as_memoryview(array):
+        return memoryview(array)
