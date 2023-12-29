c5ee7fc5c6cdeead97efec6517db892a29c45b57
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:35:02 +0000

    case apply split apparently of

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
