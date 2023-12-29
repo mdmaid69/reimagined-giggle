394cba89b980b9cdca868ffb8d5fad6c16a81f59
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:31:46 +0000

    sign see range beside section

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
