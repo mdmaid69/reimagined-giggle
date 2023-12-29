23191a3cea6e426426cc3fbea43211b4d2c14afd
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:42:20 +0000

    roof hungry score door guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
