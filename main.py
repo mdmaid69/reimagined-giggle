d2df5bc0d80a47116aa9d4660099970566997388
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:45:19 +0000

    tall cinema seat flavour involve

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
