69a4e86e7c728fd6286f518930669b1f6c2d1ebf
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:50:54 +0000

    goup underneath organization 572enter effect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
