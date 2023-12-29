7daf75e09a78f0e0079d890e0e22859b004f7bcb
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:31:42 +0000

    fear deliberate load old trial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_unique_words(sentence):
+        return set(sentence.split())
