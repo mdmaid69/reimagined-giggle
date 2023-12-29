d0bc3a8ee30d3a79f0674c32a626e3d691fb348e
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:36:56 +0000

    past sudden order weather treatment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
