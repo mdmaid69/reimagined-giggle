6dde0ef7be95ce1849c7d61cdbd4ea12151ff344
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:19:59 +0000

    drive pronunciation water comeoff contact

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
