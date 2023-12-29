2be001f19ee4d308a5001e96e0c1051db9bc6bd0
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:49:36 +0000

    minor bin avoid turn load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
