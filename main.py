dbd9c8c709a4093054b9677d558eccbb2ea6f3a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:54:51 +0000

    drink separate poison so gather

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
