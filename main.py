2b6f33af9600163be8bfe196f3fdfc63a5d99aeb
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:13:31 +0000

    light completely true slightly book

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
