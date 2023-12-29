d1e6ec383b3f5efe8cefce2a388bd620f0ca02aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:51:30 +0000

    port rank boat production else

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import platform
+def get_os_info():
+        return platform.uname()
