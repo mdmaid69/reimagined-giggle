be03464b93b1c8b9f8e10962e59eabd7414ed19d
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:16:52 +0000

    pair pupil ordinary ask question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import math
+print(math.pi)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
