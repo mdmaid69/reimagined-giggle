785a121c1cef5d4992d432d840a0cf82f0b39e05
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:29:13 +0000

    failure pause healthy Bb fun

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
