734e9d8c57a16189767fb22a770e17af134ed532
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:27:04 +0000

    born yet embarrassed press behave

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_sine(x):
+        return math.sin(x)
