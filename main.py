0d29693ef9424bbdd2bfd70761047afdaca51755
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:34:37 +0000

    maintain year land weak idea

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sys
-print(sys.version)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
