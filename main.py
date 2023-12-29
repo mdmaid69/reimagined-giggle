abc54f92a55b9988a498ee12bec1db5789de7788
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:12:10 +0000

    play long stir exist almost

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
