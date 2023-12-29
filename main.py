5290b06d26e1f5eb58e629a399cf84879be04187
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:02:06 +0000

    pool left do comedown sport

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def convert_to_octal(n):
-        return oct(n)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
