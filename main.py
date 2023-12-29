55d3782809a3278afd629ef14918fc85ebcc42b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:27:20 +0000

    bang mess complete attention join

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_as_set(array):
+        return set(array)
