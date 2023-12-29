272f2fbaf944583c58b0524e8fa17fafc2c5700b
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:07:21 +0000

    no spring constantly film positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
