5bf4e470b82a1fc17b289cb4059768cc7a3e33b5
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:39:22 +0000

    note ability force godown survive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import collections
+def create_user_list():
+        return collections.UserList()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
