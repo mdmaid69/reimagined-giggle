0bddd01cd6c688062b90b53b8c4b32cd234014db
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:38:10 +0000

    completely tie business pupil pretty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
