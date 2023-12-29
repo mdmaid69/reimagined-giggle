263a1542804d277e19ade5356713c4d84afad823
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:38:35 +0000

    address tap good pencil repair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import datetime
+def get_today_date():
+        return datetime.date.today()
