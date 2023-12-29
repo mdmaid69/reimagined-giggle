50a1d2b1ec46bff3d68cdbcbedcbaf76367f017f
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:45:56 +0000

    politics respect satisfactory in mouth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
