b5f656327a7333581e774ad1505bc37f8e3e300b
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:09:57 +0000

    interfere address skin pump talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_acceleration(speed, time):
+        return speed / time
