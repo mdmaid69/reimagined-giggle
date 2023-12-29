1db9484b58c51669982b511a4a9aba982da91cf9
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:28:44 +0000

    active stamp wave willing demand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def count_elements(lst):
+        return len(lst)
