cb77f8ac8f0dff03b1cd97fe2805e84cec3cf664
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:45:02 +0000

    yours ocean present waste fork

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print("Cube numbers:", [x**3 for x in range(n)])
+def find_max(numbers):
+        return max(numbers)
