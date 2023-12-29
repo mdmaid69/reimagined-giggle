8051ba49641f6ffda59d73af9a115165249dcce6
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:55:23 +0000

    talkinto store control Rr evidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
