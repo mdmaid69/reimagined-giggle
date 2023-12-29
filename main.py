b3f7946f6c17ca5f8aaf1970b37d1dbc74096ef8
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:56:07 +0000

    estimate poison church evidence flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
