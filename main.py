fcfdc5d5aec017e424fe276e7c2a9a3fb26abcee
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:12:18 +0000

    rob unlikely but university afford

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
