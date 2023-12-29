3176a4a1c3538f086a53d46b5d95a724f7dece47
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:08:41 +0000

    production and neat lead legal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_array_as_set(array):
-        return set(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
