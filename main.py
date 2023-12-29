2baf4e8a5af90b128f360b6728d374b153512af2
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:13:30 +0000

    Zz further secret low visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import array
+def get_array_as_set(array):
+        return set(array)
