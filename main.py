f2da978ff38c446644dbabbb3cd36a855abcf9c1
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:34:30 +0000

    Oo delight result passenger flight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
 import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_file_size(filename):
+        return os.path.getsize(filename)
