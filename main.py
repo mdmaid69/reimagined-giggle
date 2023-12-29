d07f6405d6eeda5a8bafaf4425f9777ba220bd55
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:16:44 +0000

    dream say acceptable turnover area

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def reverse_list(lst):
+        return lst[::-1]
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
