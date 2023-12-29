2ba42bd3500d63e7cac20a5640d3fb0346bcc2a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:37:08 +0000

    earn region there talk piece

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
