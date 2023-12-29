309adffb9bbeef2381cf5f09d7b9501610294b00
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:57:43 +0000

    Pp next include bridge mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def reverse_string(s):
+        return s[::-1]
