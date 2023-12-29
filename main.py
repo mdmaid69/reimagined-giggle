5022c3838c45b87e63acb631294082627cf70b9d
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:53:37 +0000

    always treatment hospital schedule forever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
