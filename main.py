6506867816a4ffa87cc1fc5dd80ca8314d51cf31
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:59:36 +0000

    computer repair doubt accidental passenger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
