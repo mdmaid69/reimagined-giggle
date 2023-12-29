cd93d81f94a90fcd602a135b952ccdc79073807f
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:47:36 +0000

    square justice gather open abroad

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def get_array_as_str(array):
-        return str(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
