5bfbce7a52108cb618275448148d9a58f0d7381b
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:05:19 +0000

    run doubt when person dangerous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
