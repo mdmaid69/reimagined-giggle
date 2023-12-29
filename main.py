80682d1d9b4ee2c0185bf136309280792509a590
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:42:53 +0000

    worried public hospital since dress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
