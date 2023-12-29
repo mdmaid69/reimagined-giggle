547dc0cbc7e8c276a37bcc8394462446ba647410
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:30:30 +0000

    waste fortunate rob practical provide

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
