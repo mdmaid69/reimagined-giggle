8ef4b0c7cd03db00b8dda454abd95a06bf8cdccb
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:40:54 +0000

    mistake practice comeover grade law

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
