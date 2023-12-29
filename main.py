f111cd7c344a7f30774a436128e2d72450554c90
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:52:18 +0000

    gas taste family hole rare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_max(numbers):
+        return max(numbers)
