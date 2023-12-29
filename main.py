bd280e4f90b6a2eab0e8a24f33af70b8f591aba9
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:51:18 +0000

    soon grade sad meet human

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
