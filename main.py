1569b972b432d40d568122f74fd22bef394d982f
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:31:05 +0000

    contrast winter rarely freeze warn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def count_words(sentence):
+        return len(sentence.split())
