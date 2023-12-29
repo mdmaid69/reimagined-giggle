01e601b9939bbe431df72579330aea2d6d85b429
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:11:33 +0000

    look tight book pinch goaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
