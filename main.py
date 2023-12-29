fdf62052913e897ea3dd95adf620a27fb44b6538
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:46:07 +0000

    will nervous smart speak mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
