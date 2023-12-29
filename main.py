77b684d19d7c0663b837e4badac2588a119a85b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:06:07 +0000

    exhibition exercise guess under report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def count_elements(lst):
+        return len(lst)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
