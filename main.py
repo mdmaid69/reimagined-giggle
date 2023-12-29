5080d3a68858412fea049f495cea6c3278befd5b
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:29:56 +0000

    because fear shrink apply operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
