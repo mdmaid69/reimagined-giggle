55ae7669fa5e6335acafd6e7003305a0d8c5ceb1
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:46:28 +0000

    compare man light stand play

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
