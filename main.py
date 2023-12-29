603552e1ea7b45cf64bdf089f44fcbb81ecbfaa5
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:38:17 +0000

    combine pretty hot whichever stay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
