d646dbacb9bc8e3eaaedf3db9b63ad97b06f2e22
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:05:53 +0000

    slip steal judge treat worth

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
