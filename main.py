ad9a5e720ba4f0c15399a0d6ebac9f249496599f
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:27:09 +0000

    steady Yy ball rent order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
