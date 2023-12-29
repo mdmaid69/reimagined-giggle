20243425bde94aef7f60a1aa2cc2a1acd6e51e92
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:01:12 +0000

    stick height state fasten responsible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
