50eb2eaa13cdc08c3b8743891b3a6d3b3de0bca5
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:59:55 +0000

    trade reach perhaps blow star

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
