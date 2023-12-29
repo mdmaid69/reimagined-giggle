2715dfdf25d90f2d7ba7ea723faa5b4e937aa509
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:50:12 +0000

    confuse he hear pupil challenge

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import getpass
+def get_username():
+        return getpass.getuser()
