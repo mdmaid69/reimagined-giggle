f10a9615ed1ea7fc4959b907e50cbc3a00382df4
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:56:54 +0000

    doubt relationship comedown 572enter apparently

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
