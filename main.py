a718db923452014bf322a41bee9d862c6cf81f44
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:31:10 +0000

    letter dangerous imagination mark accept

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def find_max(numbers):
+        return max(numbers)
