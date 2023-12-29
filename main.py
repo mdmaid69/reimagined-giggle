8bdeac436e3c0ff94ee978ee58294e4fca529950
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:32:45 +0000

    stage year mixture start speak

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
