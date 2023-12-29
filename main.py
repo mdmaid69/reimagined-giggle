9db154123605dde6138ca6713630157395b69164
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:01:50 +0000

    stretch still spend first dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
