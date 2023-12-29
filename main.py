c92b808b7eaa5ca00b4ea135628a263e9b472b4c
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:11:30 +0000

    sweet raise about separate drink

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
