c8841e2aabb699eb66a79cfc3d54e79b1ba645b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:31:31 +0000

    crowded special float television accommodation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
