1fe795bc9c191449ca0941e03ec9acc60d3086b4
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:20:01 +0000

    brown mend young danger release

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def get_array_as_list(array):
+        return list(array)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
