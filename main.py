8be97393cd170560b1aa5cc4e7d1381e8a68eb07
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:16:12 +0000

    possibility punishment in form temperature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
