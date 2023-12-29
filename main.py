2e555acb040297152a6a8a6bdcb6ee8ed2ec7f23
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:47:55 +0000

    child strike strength noise damp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
