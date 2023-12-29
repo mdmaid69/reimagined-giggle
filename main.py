33cdde174ef25e97b68c6459549610db5374a87f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:50:08 +0000

    turnon shake transparent emphasis control

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def convert_to_binary(n):
+        return bin(n)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
