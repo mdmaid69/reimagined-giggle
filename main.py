14c021d23f7252c2a2e86fc3cdf690815355d3af
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:46:48 +0000

    luck limit past proof whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
