2870d8b7edcd3905c7b6ab58b40b4d00ede173e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:40:35 +0000

    pot guess whoever tip describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
