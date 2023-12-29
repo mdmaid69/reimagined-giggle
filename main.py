bfde3448c3cbdf240a69b346a59e1417a098b0d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:44:20 +0000

    plastic pair coat treatment roof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
