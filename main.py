5760b650923f234300489ee5b0a9151e2450f3af
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:22:27 +0000

    hope relationship cheap acceptable hold

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
