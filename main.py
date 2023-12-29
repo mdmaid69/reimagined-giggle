1e1abd45dc2bd726d164bfcf00f6860fab05b1f9
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:12:48 +0000

    win top unkind heat benefit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def find_min(lst):
-        return min(lst)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
