d231c3271d4be043267d9a5b079c60e449743440
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:14:34 +0000

    confidence dream bet below equipment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
