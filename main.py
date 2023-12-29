b61b484fd6041d239679d2e54c461dc2e617113b
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:58:33 +0000

    choose lend tongue dinner head

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-print(sum(range(10)))
+def is_even(n):
+        return n % 2 == 0
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
