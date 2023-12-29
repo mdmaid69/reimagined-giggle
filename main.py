0081b7ed94a16f3ffb7760261837ffe3f8830e4a
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:50:59 +0000

    must communication deep prefer plant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
