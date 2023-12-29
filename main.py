9730edcc021b1d535864cd07007ff6476a73b996
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:18:00 +0000

    odd content smell Ll wave

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
