99f95666d38dbe3f27585d2d3f01321bdb88088a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:19:38 +0000

    let entry open capital there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
