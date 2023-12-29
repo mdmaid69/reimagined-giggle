8db606c534b56287d487a29be594d255477f0f3b
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:06:31 +0000

    male key imagine except polite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
