cc96e89292acc841b9a41f43ac1897568fdd5189
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:47:16 +0000

    narrow callup again suppose bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def count_elements(lst):
+        return len(lst)
