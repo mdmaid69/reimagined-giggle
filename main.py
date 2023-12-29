74382e3cb494574aeb8a6a8069eb0fa8c07af897
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:59:39 +0000

    delicate gas runinto sleep range

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
