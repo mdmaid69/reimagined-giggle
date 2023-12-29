fd734590e278415cc5df1cde28329718b55db39d
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:43:15 +0000

    Rr still tip food although

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
