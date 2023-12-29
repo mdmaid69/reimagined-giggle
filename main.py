a1695c7290b8a35643e329936bcf02f2696a54f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:27:30 +0000

    special press interested choose communicate

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import csv
-def write_csv_file(filename, data):
+def save_csv(data, filename):
         with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
