9f31d7e4eda2722c39ffc18b21e3d7a5726054d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:14:39 +0000

    professional boat holiday terrible alone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
