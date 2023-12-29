9bead7cf0352e3a46ea3eb7e103d5d928c071f70
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:09:25 +0000

    worth say attract notice flat

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
