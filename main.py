3f908d045debcc8a44e9bd39a24a3b9962f60a50
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:51:26 +0000

    hide badly problem we lock

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
