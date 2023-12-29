5f782d7a1939d26e86071213b8f4a35c6fee6a44
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:35:46 +0000

    improve every Ee Qq juice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
