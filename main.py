675efa144d58c819e62830f1d574d9a5f4439444
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:45:30 +0000

    able show shape confusing whom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
