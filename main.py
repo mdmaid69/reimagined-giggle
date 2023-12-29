f932320939589340d189dae9f060032264b4f885
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:10:31 +0000

    section practice possibly little brief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
