d40d458f55b6aef5b1c0cd2049a407c9bd3f3719
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:08:51 +0000

    calloff workupto convenient exercise force

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
