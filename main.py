ef9f5b4cc195f940f873d622d4b8929c1b433037
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:17:52 +0000

    terrible pile scare convenient temper

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def reverse_list(lst):
-        return lst[::-1]
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import platform
+def get_os_info():
+        return platform.uname()
