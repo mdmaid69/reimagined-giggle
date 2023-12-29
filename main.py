c8f7b3467bdddb24c093e9e6d58de6bacaf1fd62
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:42:03 +0000

    bang suit skin needle fold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def square_number(x):
+        return x**2
