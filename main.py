daf698abe2a4adab7bea856e042bb6ef421d2d39
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:27:29 +0000

    high increase afternoon card front

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
