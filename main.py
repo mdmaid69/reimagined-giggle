74686a6a00093adba5109c53a47faa38ca15e3ee
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:43:36 +0000

    none turndown high almost but

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-for i in range(10): print(i)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
