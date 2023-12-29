3ee09d393261d7c17d76fcccd3a7af3f3655e224
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:06:48 +0000

    easy ourselves doctor complete famous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_density(mass, volume):
-        return mass / volume
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
