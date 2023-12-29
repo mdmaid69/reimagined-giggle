2ff28d63c698c972ef690f4d8ed1d293399a17a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:21:25 +0000

    affect edge recent uniform dick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def find_max(lst):
+        return max(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
