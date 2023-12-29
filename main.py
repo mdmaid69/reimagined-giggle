75fb4fbf85ad86ba47713c4923ba2da9d9cca282
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:35:28 +0000

    distant recently tradition niggaacademy loss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
