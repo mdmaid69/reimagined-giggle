3e3b5308086ae6c64663a1d346df91ea54a3b406
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:19:42 +0000

    issue offer receive contribute odd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_binary(n):
+        return bin(n)
