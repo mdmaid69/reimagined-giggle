2100531d82bfe5ee4fe03cd078a8ed8c796d01de
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:20:01 +0000

    attention flat disturbing cover contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
