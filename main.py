c273a7090272b0a42124058e9bb3ee424d97ee5e
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:17:18 +0000

    step touch already against favourite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import numpy as np
-print(np.array([1, 2, 3]))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
