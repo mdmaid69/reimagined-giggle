cda3c0e4bf264cdbaf3aae365eced6ef97c43dc7
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:30:50 +0000

    male communication nose translate blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_bool(array):
+        return bool(array)
