eada9892938e3b10f039079069a30af357bd94b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:16:33 +0000

    yourself performance reply surprising first

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
