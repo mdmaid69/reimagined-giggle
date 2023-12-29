ed396519c97b14eeaa776cb02f8b1ccf2a7fe024
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:36:47 +0000

    send everywhere to then point

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_sine(x):
+        return math.sin(x)
