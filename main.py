420deec3691993b5c66f033b73c9cccbb234b64a
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:34:20 +0000

    action wipe visit shoot need

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def sort_numbers(numbers):
+        return sorted(numbers)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
