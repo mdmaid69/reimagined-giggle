4d19d4579fcb649415fcf342d21b1e77bff48b92
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:28:15 +0000

    getoff lookafter turnover hide trap

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
