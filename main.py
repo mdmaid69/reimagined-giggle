00a093a64c9a13a7a00e8535f8313634210c70db
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:17:01 +0000

    famous matter keepaway probably refuse

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
