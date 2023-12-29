8fba01f04f83b4d778902f7a39d5aa593e17bd92
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:39:41 +0000

    whichever face name wrap shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_as_repr(array):
+        return repr(array)
