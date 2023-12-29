c6677504262035d98b72fca23fa56a54e99d19e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:33:38 +0000

    little apply by whoever table

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
