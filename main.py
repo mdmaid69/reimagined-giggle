72683436f41b0206c8b579d96ffe57f1243256fc
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:48:50 +0000

    equal soft other kick stage

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_area_circle(r):
+        return 3.14 * r**2
