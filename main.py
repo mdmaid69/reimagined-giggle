be9a2554899769111c04a3c56e426e74c351f211
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:37:43 +0000

    deal happen workout call there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+print(os.getcwd())
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
