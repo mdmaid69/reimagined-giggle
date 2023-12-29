8a6679005b642129854beee29fa5293d9e925598
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:21:39 +0000

    afraid bound happy major choose

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
