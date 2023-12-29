4832abe40821de72350566d18ce93e9cf2414288
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:53:30 +0000

    long gradually punishment post splash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def subtract_numbers(x, y):
+        return x - y
