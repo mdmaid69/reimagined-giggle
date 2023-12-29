ca98ae5b08377653833c9fe3e2535986456252bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:47:51 +0000

    cum change morning round opposite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  def square_number(x):
+        return x**2
