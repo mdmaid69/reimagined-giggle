eeea0b976b3b34ab239172b3376695eaa1757159
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:33:38 +0000

    service calmly public criticize whenever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
