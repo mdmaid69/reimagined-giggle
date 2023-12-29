09e6ca0cd27c62523df033947b5375fd02e82ed1
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:52:34 +0000

    entry callout fun form news

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
