1538d5876835724283364f0af803d82902feaba8
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:08:26 +0000

    himself worth thinkover we call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
