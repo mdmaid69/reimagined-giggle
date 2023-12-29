27ec0981c14bb6c1d07373becbc17fb174432751
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:10:57 +0000

    drive afraid instant abroad discover

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_speed(distance, time):
-        return distance / time
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
