97d08ab0c9c59abc0d88b322887d91560b482055
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:01:53 +0000

    tone keepout sight closely pretend

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
