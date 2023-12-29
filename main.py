0386bb40463f3ea8f2f81bae46b7513b4aac7f09
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:05:45 +0000

    generally unhappy correction obtain convenient

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+for i in range(10): print(i)
