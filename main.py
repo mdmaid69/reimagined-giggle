3753a87d86f26579df3353bf638afe299d653575
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:46:42 +0000

    noone force upstairs breakfast blue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import sys
+def print_python_version():
+        print(sys.version)
