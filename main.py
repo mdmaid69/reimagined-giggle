a8b9dbbb073b761e1a9f4cce0b34c6930b5c08b5
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:12:20 +0000

    spend group appear century why

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
