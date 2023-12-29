fceed32c05e4ed751d2c7bc7a7325a8ca259f134
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:10:58 +0000

    listenfor surprised order drive emotion

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
