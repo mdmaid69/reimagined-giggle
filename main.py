d661d59787843a308286e127a545829d14fa13fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:49:06 +0000

    year gradually face field smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def convert_to_binary(n):
+        return bin(n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
