59883769932f34050394b1fd8ccd5b82ef360e7b
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:21:01 +0000

    giveout burst test behind freeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
