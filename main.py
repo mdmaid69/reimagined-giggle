5499bf6e52f99715341338e2d1477d6d5f965e90
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:28:14 +0000

    slip emphasis drawing Dd blue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
