c7659f50ebad33810c0a3b79c6b7f343812890bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:48:03 +0000

    whistle account post example apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
