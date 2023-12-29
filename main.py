910fe356f0775adc349f168ee8cc91b6dcdbab30
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:15:56 +0000

    pour waste Zz everybody cruel

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
