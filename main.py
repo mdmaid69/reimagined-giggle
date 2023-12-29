f3154597cfb3d99036ee614e1abeb47265dddf2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:45:00 +0000

    empty morning cheek interview history

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
