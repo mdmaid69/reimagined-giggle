76991ab89372ff17ae279fa3515e1997e8829b99
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:46:59 +0000

    out interfere pile hand bunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import numpy as np
+print(np.array([1, 2, 3]))
