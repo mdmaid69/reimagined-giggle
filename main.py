42cdea22ba6020df496f9e3a836cdde3d780b59a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:14:06 +0000

    ill connect rich contents convince

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
