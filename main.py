553eab56d09f582be8894a70669fbbb647921b23
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:03:08 +0000

    tape picture mistake finish black

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def find_unique_words(sentence):
+        return set(sentence.split())
