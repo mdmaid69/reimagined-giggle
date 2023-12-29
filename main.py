a5ca74a0395459721a681526775800aaa22d625f
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:50:19 +0000

    hate certainly night else none

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
