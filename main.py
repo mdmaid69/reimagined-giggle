761fc27a6e31d8b179e3a963cdfe96bda58ad151
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:52:07 +0000

    plain either worth splash bang

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
