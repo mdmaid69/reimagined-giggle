178cfb47d449039d23b9f7b96b48ba2c3700123f
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:32:47 +0000

    fairly criticism mad trip wonder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
