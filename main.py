0262f5fcb3ab6b6bc40dea738b8a45b60a74db41
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:06:21 +0000

    wake smile capital event sink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
