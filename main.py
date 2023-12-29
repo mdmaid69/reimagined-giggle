f15430f07b8da0f350522507f43cc0d02d03c7ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:35:47 +0000

    dance shut shake breakinto concentrate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def square_number(x):
+        return x**2
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
