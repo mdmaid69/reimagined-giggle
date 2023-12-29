c5d9b766938ace68e005cf061a84d8d4a591aeac
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:11:54 +0000

    beneath confused count Oo disappear

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
