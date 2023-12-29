7ea4f254fa1132504e100957c717fc29cf49f4dd
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:20:09 +0000

    knock murder edge base pretty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
