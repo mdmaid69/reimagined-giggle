3606e020fcd23ca74171fa59768dd2b9ad0d2637
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:53:50 +0000

    clean onto comedown make permission

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
