ef7c84f948b250346b95804ae49162e87592d1c1
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:13:55 +0000

    nonsense though communicate down far

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
