d4972aea9fde5d155f6f87ab65ceba0b5ec2cffa
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:16:52 +0000

    concern constantly guess design unlikely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import platform
+def get_os_info():
+        return platform.uname()
