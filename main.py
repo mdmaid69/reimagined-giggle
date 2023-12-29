2f85900c90671756bb08d258e231f9cba1a2c9fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:53:34 +0000

    fear advantage motion future stage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import os
+def get_current_working_directory():
+        return os.getcwd()
