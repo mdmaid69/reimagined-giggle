7d8ef85c3427f879a3a09ad757dfee178d04f503
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:13:50 +0000

    check considering secret arrive unusual

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
