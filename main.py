bb5041a853540c5233107437f70e2b3f364f334e
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:34:23 +0000

    charge impressed but empty hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def is_even(n):
-        return n % 2 == 0
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
