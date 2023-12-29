4cc51420396d47a9449c8fda6db7b2f9107cec93
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:53:02 +0000

    team permission plate always turninto

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
