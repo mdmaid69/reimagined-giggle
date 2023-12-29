8ebf0e10cd42f59bf281476570550faa32487af9
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:37:55 +0000

    rich flat undo when never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import time
-def get_current_time():
-        return time.ctime()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
