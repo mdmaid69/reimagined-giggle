2644dc06c74070eceb0999befc174e099cf1ef37
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:22:47 +0000

    average history fierce right shock

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
