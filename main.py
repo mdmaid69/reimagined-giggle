0b388335a2b9233b1a1c43535e5d2e7be4877ff5
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:40:58 +0000

    necessary swim before she should

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def cube_number(x):
-        return x**3
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
