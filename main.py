3c6b5cd7452dff476b080034450cdb13f6eea82b
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:47:59 +0000

    measure like mine back fasten

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
