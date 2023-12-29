d2b17bc309d551abab86863227eab6117577249c
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:40:12 +0000

    tap upwards neither gather honest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+print([x**2 for x in range(10)])
