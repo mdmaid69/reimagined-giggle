6fc84ae57286b399c09d5516ea503f4e2dc528a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:03:03 +0000

    round possible sell remain lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
