6247f4d4553e2f42cc8643be751d282837589bbd
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:38:28 +0000

    exact cross refer clever short

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
