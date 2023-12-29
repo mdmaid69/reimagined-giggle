77a970346dd95abc1e102cd27c591d184b18bf42
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:50:38 +0000

    section attract plan previous neither

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
