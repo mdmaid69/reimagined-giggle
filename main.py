c1c6de9b933877a802a79ee153084e15428c95a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:19:53 +0000

    string possession lead car getout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def get_array_as_list(array):
-        return list(array)
+print(json.dumps({"name": "John", "age": 30}))
+import os
+def remove_directory(path):
+        os.rmdir(path)
