368852561bad214ea7f3e7817eecc0e1fc87916b
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:03:26 +0000

    worth peaceful proper cross bag

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_acceleration(speed, time):
+        return speed / time
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
