5bcc26952e61545bd8efa0f4e9fc3742cce6c443
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:21:25 +0000

    attract wake use ear violence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
