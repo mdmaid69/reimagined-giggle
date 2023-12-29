ad318b22f75e903102a231137e1fad87453ee144
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:07:43 +0000

    plant foot downstairs young relationship

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import tensorflow as tf
+print(tf.__version__)
+import getpass
+def get_username():
+        return getpass.getuser()
