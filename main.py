79356c4e6fa42637b868876e6a69f77137cec88e
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:31:02 +0000

    in nerve identify circumstance rob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
