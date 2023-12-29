75a94474a846ac27014ae4ae1e65bbc262bf2c7c
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:08:32 +0000

    board keen complete develop plant

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
