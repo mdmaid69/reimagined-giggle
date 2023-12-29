65d21ea7d879eaf9fb8b000bc323920c35319506
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:12:25 +0000

    conclusion declare basically estimate rely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def add_numbers(a, b):
-        return a + b
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_queue():
+        return collections.deque()
