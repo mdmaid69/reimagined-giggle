271331182ef691d2429fc128a4f98f927f2f703e
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:00:12 +0000

    this admire but very love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
