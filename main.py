e61a1fef31594755e82385436ed994eb565aaee7
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:17:53 +0000

    yet watch keepup importance agree

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_work(force, distance):
+        return force * distance
