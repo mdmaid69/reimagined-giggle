7e22ac4c9390e5a402428d76eeeb8a2ea723f57d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:13:23 +0000

    keepoff unpleasant beside actual season

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
