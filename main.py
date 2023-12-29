3651e253fff0d8a2a4ad18cd640d42cfd7433a9b
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:26:36 +0000

    rub sale steep under hope

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
