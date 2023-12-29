0510a0df602401cd43656967efd24a8ff8d8ee73
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:34:35 +0000

    expensive workout doubt wake culture

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
