f5a37ebc650067fd4535cfffb136bbe1ef227847
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:09:59 +0000

    slight ought long story plain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
