4c22182a78f6602252f581c03a8d089494170a50
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:15:17 +0000

    wind better rate doubt whoever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
