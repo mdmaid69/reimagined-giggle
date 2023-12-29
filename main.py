e0a419a94e7221745e5f06d3084ecfd910e753da
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:46:14 +0000

    provide exam that keepoff pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def convert_to_binary(n):
+        return bin(n)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
