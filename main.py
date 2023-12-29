d2b728c20c09ea55e7f5a3b77e9540a3d5162885
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:45:18 +0000

    out each little weigh concern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
