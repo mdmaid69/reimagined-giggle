2a7fbea11ae2ddbd730b087f6649cd638b802e77
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:29:57 +0000

    home trick creature 564encourage possession

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import tensorflow as tf
-print(tf.__version__)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
