0db79e680fcea82bd3ec26b9cecaa2b1048c6222
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:49:50 +0000

    dry beneath belong she have

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
