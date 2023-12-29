3ea5f9c583944c3169d8cc21bf0767e065f0bb71
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:36:56 +0000

    laugh formal left loss call

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
