1ac1a6f0c9eceb10bf6a190190cb10961304dee9
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:31:30 +0000

    whom separate turn degree see

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
