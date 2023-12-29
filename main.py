2089e288a197d3f02f0d1cbe04072d062904b40e
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:42:01 +0000

    must shout action produce quiet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-for i in range(5):
-        print(i)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
