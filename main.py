d2dd3779c7d2e38e91606db66db89580119611b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:24:48 +0000

    smile slight full on method

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
