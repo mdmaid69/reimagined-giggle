767e74a8d13555e125fd7f131a62c0abf5f0954d
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:23:38 +0000

    nurse raise appeal fit burst

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import sys
+def exit_program():
+        sys.exit()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
