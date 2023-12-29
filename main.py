32f9af4407f1b6b732697f92ecd094bd7894ce56
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:55:08 +0000

    flavour breakthrough experiment soak create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
