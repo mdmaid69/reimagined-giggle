4c22e5f698987fa0007c591735e2245d4ce3ad0e
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:51:05 +0000

    involve example during bit firm

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
