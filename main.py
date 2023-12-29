82c2bd4d3b99c038535a382895ef5f2f4af0faad
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:08:11 +0000

    Mm turnback sit trip style

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_power(work, time):
+        return work / time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
