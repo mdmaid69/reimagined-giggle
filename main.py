229fb61fa70a688b0fdf111ec82205e0c7419137
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:33:36 +0000

    imagine ability equal object need

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
