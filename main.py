2b74aadbb589b06b7d0f52c8f6d0cfafd0e7ef80
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:16:31 +0000

    fit nervous later busy 565end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_max(numbers):
-        return max(numbers)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
