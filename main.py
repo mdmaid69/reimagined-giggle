120f6e3a230a50190d8c3f6c0149912ee1f5b116
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:04:59 +0000

    close thinkover outside perfect muscle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
