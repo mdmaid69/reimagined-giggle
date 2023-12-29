2d2159b0665f55ca6b15690d91f68750b78e941f
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:00:06 +0000

    sign kill fold surprised no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_work(force, distance):
+        return force * distance
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
