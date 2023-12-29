68aedbeae352b81559c2a3951efec9b401dc9175
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:55:38 +0000

    let everywhere immediately abroad breathe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import array
-def get_array_item(array, i):
-        return array[i]
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
