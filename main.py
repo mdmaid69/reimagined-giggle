8125745b6ad03b179a97da4b924a114ae7eb2365
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:13:31 +0000

    embarrassed worth cross record think

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
