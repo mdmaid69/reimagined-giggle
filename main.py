d3f2f490a8a6b7320bac44d773ecad201f379926
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:26:36 +0000

    waste regular warn mine town

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def is_odd(n):
+        return n % 2 != 0
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
