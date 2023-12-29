fca5337e0ac95a619f0e8344e6a67024f4652dcf
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:50:59 +0000

    turnout region accordingto open many

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
