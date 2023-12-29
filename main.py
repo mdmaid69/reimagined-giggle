fb0c59035ad7edf08b52c803910d7e01caf23bd7
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:41:51 +0000

    keepout first talkback well season

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
