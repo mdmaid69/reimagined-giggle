c4413515858e2293e8f5a4a79274d9014c860a70
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:30:00 +0000

    pretty point pile takeover dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
