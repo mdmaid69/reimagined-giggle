61f60c1f9a6b5287da50ea69fd8558017934c8af
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:49:39 +0000

    copy attempt love point breakthrough

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
