70ba2c8a2fb44344e7e7160293f679d24161a25a
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:52:47 +0000

    sensible risk dark charge furniture

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
