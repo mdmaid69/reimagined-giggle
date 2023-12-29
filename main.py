d382b9c53be624816f55fd34f4625f054001be88
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:16:37 +0000

    judge important hot spill dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
