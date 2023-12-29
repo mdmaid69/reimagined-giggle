fef0db8090eb667ee10c32693c4b9bdb61441181
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:53:09 +0000

    Ee some good effort calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
