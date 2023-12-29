18280d85ef656445a1f2e82dd4aaacbb2fa25046
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:21:39 +0000

    among advise burn fit cold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_area_rectangle(l, w):
-        return l * w
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
