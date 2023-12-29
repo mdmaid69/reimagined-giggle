e8b18be85b7783948827ed094dc394aaa60104f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:14:35 +0000

    natural formal appearance accept during

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
