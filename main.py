87d2a09f932521012df81acd8e0c48a93915f6b5
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:35:48 +0000

    direct impatient feature kind who

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-print("Hello, world!")
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
