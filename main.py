81c2c25408f3851cb4c068ecf1628eb1cc365655
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:33:58 +0000

    square turnup judge fire recent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
