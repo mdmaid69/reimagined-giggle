e2b1ec12150784584505037868fa57907da76e8f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:59:18 +0000

    tomorrow regular tap turnover getup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_itemsize(array):
+        return array.itemsize
