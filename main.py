99897d78f2e14c16a1deeb46efef581995f74315
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:52:16 +0000

    method crisis cheap steady Ee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
