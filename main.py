1beaed8a19d962a38368a1a9be5e2180d8ecbdcb
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:20:35 +0000

    will either brother education passenger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
