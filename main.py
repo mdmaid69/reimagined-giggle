84c70bfea75c2ddec6f11f217aabcda20a435904
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:12:30 +0000

    Ww window underneath bringback slowly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_min(numbers):
+        return min(numbers)
