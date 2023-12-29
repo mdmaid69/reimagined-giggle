52680649cbeacd207afe0c1ea356bdd2989a9364
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:36:40 +0000

    climb peace task full team

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
