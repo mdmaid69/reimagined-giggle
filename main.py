5c3697ff3d428cefd0345bb4d4feb65c29e6815b
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:09:00 +0000

    determined rubbish bill unite accordingto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_odd(n):
+        return n % 2 != 0
