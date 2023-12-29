684ad0ada68bbe225a7d7f0611ca15b0765811e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:15:08 +0000

    achievement image realize equipment then

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
