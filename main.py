4c3bc4fe1d400e70382ad899f18923e75c6b5b16
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:37:43 +0000

    basis area she stay complain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
