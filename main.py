9ccd2f74ae79db746d363b76fdd5d06c1d5e8ca8
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:12:37 +0000

    temperature second zero behind straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
