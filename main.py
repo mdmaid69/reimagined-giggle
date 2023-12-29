ce836df6ee2578814fba53b4e7ed0fb40a3022b4
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:34:44 +0000

    finger pause object catch wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
