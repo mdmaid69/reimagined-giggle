4d88884915968b473e851a8d83fe3dac2961c991
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:41:32 +0000

    receive book live me pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
