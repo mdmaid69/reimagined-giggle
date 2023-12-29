3e8186a1af318f97722baa216391cb129e1a9b73
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:26:37 +0000

    quantity answer pause our separate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
