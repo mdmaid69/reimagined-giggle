504eb9040220d688dcccb43601cdd8e5f8cb61b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:32:58 +0000

    exit brother human most wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
