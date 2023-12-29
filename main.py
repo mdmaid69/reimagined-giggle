01bb1787ce79131ab8e1cf52af17ba3a8e6ba115
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:37:12 +0000

    rain lookback(on) stone doubt light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
