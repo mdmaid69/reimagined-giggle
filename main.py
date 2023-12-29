278901ca00a85b2c137a58089b93348908f6cd9d
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:37:59 +0000

    declare gas surprise amount general

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
