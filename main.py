896c83875f112da9536dfddbd488ea261ea78963
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:59:40 +0000

    brown create condition free own

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
