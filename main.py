aaaa838353094a58bd5a9bd7cff3b26a7ae47f6c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:29:35 +0000

    offence speak abroad mend begin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
