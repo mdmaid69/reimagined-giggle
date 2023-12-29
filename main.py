294d60dae6d7322820ac86b8d36f8172ddb589ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:32:25 +0000

    order team hold laugh who

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
