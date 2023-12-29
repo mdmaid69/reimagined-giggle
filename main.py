5458b404290acfb706b1ff6b39c119d7aa9bb388
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:41:30 +0000

    always Ww this comeoff again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+print(math.pi)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
