b520059e56b48167bac3ffb1e6b3c9073e1d36a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:25:20 +0000

    success skin love instant frightened

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
