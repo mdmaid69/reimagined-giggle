db662934a54206902444185d629747a454c58b9e
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:54:41 +0000

    wherever computer increase correct surprise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
