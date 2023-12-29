2e3cebeda537434ae7dfc414d961a428c50f1b81
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:23:55 +0000

    organization declaration guard base support

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
