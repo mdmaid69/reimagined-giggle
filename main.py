edb10d3b14a1ec06ef5ea2a7ff6ef7c9889a81e1
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:23:33 +0000

    there figure 179bite important thumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_as_int(array):
+        return int(array[0])
