1d10ceb17b4b99e342647500d5f790217fa21ea8
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:12:46 +0000

    reduction permission attract form protest

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def find_min(lst):
+        return min(lst)
