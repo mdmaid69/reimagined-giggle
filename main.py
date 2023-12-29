a92aa7ee4ed9fbaf293f579fdc329d04c026fb8b
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:55:39 +0000

    quiet band rather fierce light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
