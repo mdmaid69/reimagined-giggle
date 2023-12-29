8e7deae8b469d8590a1086540f341fcf56e17f09
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:28:12 +0000

    buy anywayanyhow form cover open

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def subtract_numbers(x, y):
-        return x - y
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_area_circle(r):
+        return 3.14 * r**2
