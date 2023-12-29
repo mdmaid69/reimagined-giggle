db8d0a4b069ed789a38c70b9c7fd8de9b04adb70
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:01:22 +0000

    unusual blame 569enjoy point maximum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_length(array):
+        return len(array)
