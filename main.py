72ee033310c44464af669d986475a7cd30ff61df
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:09:38 +0000

    expression talkinto moon point lunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
