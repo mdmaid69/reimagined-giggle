12d8312ccc8e5e8ce8e1fa9bda252f72121e3e1d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:41:42 +0000

    comeback name process progress border

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
