3db286f231b36359fc3e406de4fea338ef3ec344
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:16:46 +0000

    onto sleep address model hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def append_to_array(array, item):
-        array.append(item)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
