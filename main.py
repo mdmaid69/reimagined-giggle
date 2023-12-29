162b0b27faf2cd62f69fe99a6027e0ffc386f8ca
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:51:09 +0000

    year similar scheme tough science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
