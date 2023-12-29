450b67ac79a267a7da578ac53bff48d7d43845cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:56:14 +0000

    generous different mail reduce describe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import sys
+print(sys.version)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
