c38a7ed1458d84cfeda1b252c0ac7bfc40385e49
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:34:32 +0000

    upstairs lot recover politics satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
