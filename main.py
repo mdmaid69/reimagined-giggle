5d243d43ba6a671945d4eb88794d58deee8c2e67
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:51:57 +0000

    confidence actually speak general wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
