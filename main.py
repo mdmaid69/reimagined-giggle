91a30e5a37c7fee28156605a0c934eccf59bf773
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:41:24 +0000

    health life deep in uponon

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
