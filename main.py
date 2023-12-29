425ec100e68f70401bff117d59c2bc7ea3fd8fe8
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:29:46 +0000

    frighten callback who improvement blow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def is_even(n):
+        return n % 2 == 0
