cae753b47fb2ada06bc494fa6d6e1ace7081e3a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:44:11 +0000

    wing die general less often

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
