52d9ca43556dab5d96c777f89df66da3b8b2a586
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:07:57 +0000

    either instrument say comfort few

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
