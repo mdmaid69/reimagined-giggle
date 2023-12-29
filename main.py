82f6a1a42adb2695f813c9781562c2ebec110f33
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:57:15 +0000

    train once present pin fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_floor(x):
+        return math.floor(x)
