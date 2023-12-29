b515a71a6dd50ba2460f1ad95eccdd0e250ec78a
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:17:50 +0000

    rest unkind he ahead claim

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
