12c317f8e13daeec89a6634679add8d405b93fa3
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:08:36 +0000

    day round tax experiment join

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def convert_to_hex(n):
+        return hex(n)
+def remove_duplicates(lst):
+        return list(set(lst))
