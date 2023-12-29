78eb85e463ee9124c10c1e66147b4507e840928f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:26:07 +0000

    inquire occasional base respect recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def get_current_working_directory():
+        return os.getcwd()
