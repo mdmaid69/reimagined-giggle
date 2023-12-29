93efda22eec157101e671c9b193cc4e930048161
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:33:43 +0000

    degree organization definite send store

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-for i in range(10): print(i)
+  def count_elements(lst):
+        return len(lst)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
