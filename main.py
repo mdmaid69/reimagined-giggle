8b4630263419406c4a5531de28e1df3ac13ef1b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:39:02 +0000

    block treatment early naturally defend

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def square_number(x):
+        return x**2
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
