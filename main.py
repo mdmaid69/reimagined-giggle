03992c5f88baad26fab3de9147a7652a31bb2c96
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:57:50 +0000

    again season punishment bend delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
