529eefcf754ad1cf4af40474514e696e0623dda3
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:11:49 +0000

    class guide exploration opinion talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_exponential(x):
+        return math.exp(x)
