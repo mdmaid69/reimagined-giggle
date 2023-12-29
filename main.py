6ac12528b930b0ca7741c3b86eddd9a81a008706
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:22:29 +0000

    shallow geton exam modern interest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
