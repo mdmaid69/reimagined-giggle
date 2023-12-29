53af9844b5260dd2103997000db8c068211aa335
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:01:10 +0000

    common police thirsty mainly fail

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
