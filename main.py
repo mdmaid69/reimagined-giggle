8aef93a8b5478c0bd42dd676fb89c106f75257ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:53:47 +0000

    young steep cope sister slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
