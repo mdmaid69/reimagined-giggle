82d177ca1c745f38d98173a74511ee79ba89be5d
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:53:16 +0000

    dress affect false pleased children

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def sort_list(lst):
+        return sorted(lst)
