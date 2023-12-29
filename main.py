1bddf8292cef5c8e666e2b3f469365d0b2b0cc6e
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:35:51 +0000

    block poison long question both

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
