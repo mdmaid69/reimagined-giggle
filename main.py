821c39e13fd8a6215988e9008e3e09c9e8f0b20f
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:45:20 +0000

    computer pull hold plain cook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
 def find_frequency(sentence):
         from collections import Counter
         return Counter(sentence.split())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
