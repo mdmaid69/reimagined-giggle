78fffd452e81aed7a5a426cfd0bdbb6508f33fb6
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:44:37 +0000

    destroy tone building minimum necessary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_as_bool(array):
+        return bool(array)
