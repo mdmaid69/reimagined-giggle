aaac273e3325da9615f30d25a04dcbbce2101700
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:09:51 +0000

    traffic by build future book

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
