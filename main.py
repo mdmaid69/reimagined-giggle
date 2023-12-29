3c5e62691ffd00a34f668495da2b87daf2218bee
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:35:54 +0000

    majority hollow unite bad kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def multiply_numbers(x, y):
+        return x * y
