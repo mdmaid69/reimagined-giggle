e8d45969914a5c6b45481b01261ab0b37f0c7e27
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:46:50 +0000

    tidy drop holiday Ii immediately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
