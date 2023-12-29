816b9faa3dacd2ee33f24899a09d6399381509e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:12:04 +0000

    sort passage somebody payment affect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
