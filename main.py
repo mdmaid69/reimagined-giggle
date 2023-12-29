9a9c0562c003b50db6e98c00f1d8f0c2a67215f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:27:39 +0000

    it quality getup wash board

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
