ccfb650859afdda8e7115ce10373a9800214ccac
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:42:59 +0000

    noone protest outside this sale

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def clear_array(array):
+        array *= 0
