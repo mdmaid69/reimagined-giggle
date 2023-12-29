b217b52e33f54e72bc6f1d4f27b629ac6ce822e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:57:11 +0000

    port branch die translate yesterday

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+for i in range(10): print(i)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
