076f8f1938a034699826b784945437c981f7ec19
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:18:00 +0000

    kind hate rather bringback terrible

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def sort_list(lst):
+        return sorted(lst)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
