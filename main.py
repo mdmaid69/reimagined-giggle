951b6fe7ee21423294312bd842944a626b1a44e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:34:44 +0000

    discover hurt blame relax any

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_density(mass, volume):
+        return mass / volume
