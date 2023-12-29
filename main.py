821f6ee634de1984865153f072e90a07b2fc52b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:47:15 +0000

    friendly off fault trust whether

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
