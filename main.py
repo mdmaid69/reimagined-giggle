94e845396d52b759db0a6126b73e5743d0cb8e1f
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:13:58 +0000

    fear board disappear especially shallow

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def square_number(x):
-        return x**2
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
