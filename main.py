d208b880b9f17e7a068c9dcb10c2753c84fe516d
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:12:44 +0000

    personal phone callat along welcome

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
