f4e99bf002f5e593915e51eddd4f579d150c1084
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:27:28 +0000

    rub well body myself keen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
