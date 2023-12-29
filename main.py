f1f761c2c8a4f0d2c9a1e6fed2388b2a92670c4d
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:38:08 +0000

    inform wind open danger model

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def is_odd(n):
+        return n % 2 != 0
