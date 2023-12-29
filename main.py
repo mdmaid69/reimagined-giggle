0ec3092cd613e26cf019938aebc16d9076abe126
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:41:45 +0000

    down contribute convinced comeout Qq

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
