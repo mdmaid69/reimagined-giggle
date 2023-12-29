c2e339db68ed025ac03dc5e6b8effba44bdc88f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:58:15 +0000

    pause lookaround collection many cover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-print(math.pi)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+print([x**2 for x in range(10)])
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
