b03125e50fdda725d71be154f25db3674455420d
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:17:20 +0000

    knock lunch interested throw lower

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
