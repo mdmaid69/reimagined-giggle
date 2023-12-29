17250a4e2c8481769845fe646c79fc2a1253801c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:55:12 +0000

    arm else friend study climb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
