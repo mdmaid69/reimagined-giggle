722ffe00dcf74ae2d50934f19cb9846274be90db
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:06:06 +0000

    against balance shut travel grade

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-  def cube_number(x):
-        return x**3
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
