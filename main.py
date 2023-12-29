8b329d41b8f965252b3dde24929f3a140681b914
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:55:39 +0000

    thank walk occasion cheap object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import sys
-def exit_program():
-        sys.exit()
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
