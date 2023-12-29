f3b6f6ba7e9ba7d29ee8ea43baac434e4dbf50f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:02:13 +0000

    mind than object general future

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
