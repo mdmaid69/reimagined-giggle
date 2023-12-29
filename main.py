f4b6f1e843c183897882d216b222aa3154e22b82
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:43:14 +0000

    detail pin worried sensible flood

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
