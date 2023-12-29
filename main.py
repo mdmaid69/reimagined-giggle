d8032768101f1028fb3e64b38048826f7db04bdb
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:55:19 +0000

    address say community wander appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
