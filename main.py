b34452ac499347c570b40f4d9647d84867d80e38
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:27:47 +0000

    stay dry talk loose slice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
