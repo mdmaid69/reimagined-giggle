67cea3109a3c2a5996c7c57f1dc798ec9bbc60f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:41:32 +0000

    long purpose group trial breath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def convert_to_hex(n):
+        return hex(n)
