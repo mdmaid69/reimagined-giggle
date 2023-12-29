06b145720dd05626c6a6ffaa04d7244cb25a84a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:09:17 +0000

    fierce giveup contact aware question

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def add_numbers(x, y):
-        return x + y
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
