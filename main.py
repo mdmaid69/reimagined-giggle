d65e047cfdc204b9384c76e82f9ff4a7dcf97703
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:35:02 +0000

    praise official hit obtain she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
