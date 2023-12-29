c8e7d662ac26e78d08cd3d0f85e39660e5931617
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:26:08 +0000

    soak willing regret hospital for

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import random
-def roll_die():
-        return random.randint(1, 6)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
