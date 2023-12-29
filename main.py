2f459100ac813901b948b6967991f8bda972db13
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:36:21 +0000

    perhaps talkinto doubt natural subject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import sys
-print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
