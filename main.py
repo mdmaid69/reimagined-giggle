66977d33ce03012d0db36143b24a194cffe49e63
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:57:07 +0000

    platform fold each weigh night

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+print([x**2 for x in range(10)])
+def calculate_speed(distance, time):
+        return distance / time
