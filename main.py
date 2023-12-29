9e5c76af5c5c4dd8b7f0a41bec68265dc6f9462e
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:45:10 +0000

    drive Nn occur may breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
