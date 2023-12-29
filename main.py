86a3ab397e80de0525b6350539307f54c825c3c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:01:42 +0000

    whom passage guilty sometimes handle

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-print([x**2 for x in range(10)])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
