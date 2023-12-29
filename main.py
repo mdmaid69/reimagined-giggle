68026714eade7aa7565fcd94e146dcc32784900b
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:37:49 +0000

    for study goon unit trick

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
