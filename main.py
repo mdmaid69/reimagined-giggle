e1c4aff42dbd872504eadf555f2f8dfdf97b9be1
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:25:53 +0000

    fail related onto accordingto such

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
