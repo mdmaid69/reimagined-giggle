6c9a53b4a26c5b6ba60cf15ed53e28ed944b4eb9
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:35:03 +0000

    content shall remain sit key

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
