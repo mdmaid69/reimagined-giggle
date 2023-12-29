c9ef4747bd426ccbec13656e6dd658e79a5516e1
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:55:28 +0000

    matter treat equal bet petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
