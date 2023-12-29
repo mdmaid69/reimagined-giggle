d058037fd5bd47e53395db735e3b371b2bccbe0a
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:39:43 +0000

    offer pot trick getup stamp

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
