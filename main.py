ac106325c3aa24e71fb4dcc53629071a5c80cf09
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:37:08 +0000

    declaration when butter home recover

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
