1e5287a24f81b033486721ddc37c2d379ca2552c
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:42:45 +0000

    lesson later please whistle warn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import time
   def wait_for_seconds(seconds):
         time.sleep(seconds)
