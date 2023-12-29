2ec287cfc98fe88a75c579b53c8c87a4b25fb79f
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:49:20 +0000

    exciting start mean rent sky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_work(force, distance):
+        return force * distance
