846a2d9f38c14b0c640bbfd16c7246aad64d2881
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:09:02 +0000

    sink straight fear takeback best

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def cube_number(x):
-        return x**3
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
