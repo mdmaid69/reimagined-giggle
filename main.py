1b5cb6fdcfd88418ae24327d432d9e813673e907
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:59:05 +0000

    disagree yours trust disgusting duty

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
 import datetime
 def get_current_datetime():
         return datetime.datetime.now()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
