03ea801b42fe8d804a778ea54af8b6404c3035e3
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:15:59 +0000

    bag ear shop plenty teach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
