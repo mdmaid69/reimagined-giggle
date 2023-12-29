8af4db4aab439b2e4deadcd70552c43e055aa284
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:02:21 +0000

    organization insist set OKorokay low

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def convert_to_hex(n):
+        return hex(n)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
