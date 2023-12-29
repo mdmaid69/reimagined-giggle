3d8e624c82234c1619ba558261efeb91c393cb4b
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:25:40 +0000

    worried start dead method cut

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_power(work, time):
+        return work / time
