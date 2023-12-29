5fb17fcb05643558f02311f986a07d560d43353f
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:58:26 +0000

    subject decide from bother no

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
