26cd0fd0a6f4999a8e6f5807c3c4ce39dafb7ce3
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:42:09 +0000

    sky music plan among path

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
