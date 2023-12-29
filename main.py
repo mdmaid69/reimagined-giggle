5e4da281c874b0d55dc9eba5a684f63784af351a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:43:01 +0000

    factory mess difficulty lock ignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def convert_array_to_list(array):
+        return array.tolist()
