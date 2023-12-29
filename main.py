1bc84fc2f163a3f627a8cbfc85710fec9e90cbe9
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:11:21 +0000

    improvement getback mad cool rain

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import tensorflow as tf
-print(tf.__version__)
+text = "Hello, world!"
+print("Characters:", len(text))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
