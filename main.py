03540d39a5470f64e2f3895e4b58607169b83cf1
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:14:59 +0000

    undo rise star walk friendly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def square_number(x):
-        return x**2
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
