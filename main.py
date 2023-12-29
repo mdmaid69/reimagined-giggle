9abe86bf318177bfbaec46aa1852a49654da94f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:46:17 +0000

    several content legal carry thumb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+from collections import Counter
+print(Counter("hello world"))
