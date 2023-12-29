246d39c1f76d56a64f41971331eb5b591376eeaf
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:05:42 +0000

    substance pool Yy afternoon transparent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
