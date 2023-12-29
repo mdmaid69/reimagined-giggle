d550b55769a11b699c7c19f738ac15310243690d
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:22:42 +0000

    crazy any rare drawing count

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
