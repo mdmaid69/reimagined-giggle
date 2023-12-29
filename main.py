bc18cfaedc002229a60d50c980d8811dbb611cce
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:55:00 +0000

    soil face poison pattern tool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
