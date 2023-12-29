4717e594cb381ead48ae375cb0c391e07aa93e72
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:57:12 +0000

    empty last pattern dry else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  def reverse_list(lst):
+        return lst[::-1]
