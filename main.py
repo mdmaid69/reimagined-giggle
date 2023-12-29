fc5194fa83c02ea41ca13dd1ef8123a98cc81b3a
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:11:31 +0000

    build last tip downstairs lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
