ef1dc79adc580588e1ab9fac7714f18793b3a798
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:45:17 +0000

    handle subject mess above thanks

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import collections
-def create_priority_queue():
-        return collections.deque()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
