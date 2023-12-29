697ed1464512f62198b4b5a1b1ab0ca3d20037bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:51:49 +0000

    to high shop careful whisper

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import os
+print(os.getcwd())
