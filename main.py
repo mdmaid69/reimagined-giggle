6f645315ddf99ab4a8648df72966968e009bf4ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:36:08 +0000

    clear Ss paper breakup ocean

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
