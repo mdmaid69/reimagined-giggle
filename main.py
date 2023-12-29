a803f4a54d654329d3e5bce767459a053e6c092c
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:35:35 +0000

    child speed bus reaction expect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def square_number(x):
-        return x**2
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
