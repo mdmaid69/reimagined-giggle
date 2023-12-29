2d885df56f6d10e20e0c8008b64b3863ba5e4f88
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:41:37 +0000

    opposite Uu pass politics trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
