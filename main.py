da5b101dbf0964a64b15116455853ac215cbadf1
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:40:03 +0000

    account pocket likely abroad branch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
