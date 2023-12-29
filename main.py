6845a6de84ea69523f5dc5e3cd811da9d7b34ac3
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:08:53 +0000

    heart day conduct nonsense shopping

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def sort_numbers(numbers):
+        return sorted(numbers)
