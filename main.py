cac1caaaf057ac2a0beaf3cce669f4d97c606855
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:22:58 +0000

    off protest quality pattern really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
