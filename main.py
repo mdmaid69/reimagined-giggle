abfc4081a67f374e04023fb5e4d966495d366728
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:24:03 +0000

    deliver ought fond open by

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
