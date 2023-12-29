71ff8e68efc9f7e3f69786648f6263008042f682
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:43:59 +0000

    book run fix warning young

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_acceleration(speed, time):
+        return speed / time
