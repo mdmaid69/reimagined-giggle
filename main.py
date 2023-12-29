328235675b32c1f10b3108197190c1b36de68546
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:45:43 +0000

    late turnaway mail match actually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
