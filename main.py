5160890119a9eabcec8a2dc556fa002aa1caa36e
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:25:18 +0000

    likely ocean shout drink strength

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def clear_array(array):
-        array *= 0
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
