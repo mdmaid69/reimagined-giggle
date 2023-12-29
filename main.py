f8953a72918f544d8e9b89368046ee1669d045d6
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:43:09 +0000

    convenient appear into field brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_volume(length, width, height):
+        return length * width * height
