67c7160fc22acdc42ba6bec0c43d6e4607e94270
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:34:35 +0000

    sick ignore unit dish calloff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
