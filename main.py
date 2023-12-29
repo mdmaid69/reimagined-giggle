3a74a51dc96653908bf832ac1184bdf81f7df837
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:43:09 +0000

    grow safe address spare properly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_area_circle(r):
+        return 3.14 * r**2
