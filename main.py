51b40f07c022ddeac5a9e21bf5638fa8a39602e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:28:01 +0000

    thirsty aim mend stare full

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def square_number(x):
+        return x**2
