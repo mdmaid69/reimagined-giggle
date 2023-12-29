8d4f6c49200cf130519b1cd6ee32b8b101819cc2
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:42:15 +0000

    boring either majority whole correct

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-name = "Python"
-print("Hello,", name)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
