adcc256ed49958b159e09d3eab2a0786395d26de
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:10:34 +0000

    hair follow female party along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_list(array):
-        return list(array)
-  def convert_to_binary(n):
-        return bin(n)
+def get_array_itemsize(array):
+        return array.itemsize
