c90537b6caa9f74f607af817f2a080553806b184
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:48:41 +0000

    win almost file content needle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
