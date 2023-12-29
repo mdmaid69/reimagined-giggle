3204f9d338bdad975dbc0294e43e6f4b44cba7bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:40:37 +0000

    left mention react ago difficult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
