d9e9a9156149540dbe8fce0229c87cb88d8326bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:53:20 +0000

    shall past wave kick badly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
