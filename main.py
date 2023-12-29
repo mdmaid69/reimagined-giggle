9d14a4944f1e9e8821a884115ba2038c7a9b9dc7
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:52:09 +0000

    card cough along valuable pride

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
