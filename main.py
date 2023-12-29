7dc20cef2ad4b832745868984797e746166be422
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:27:05 +0000

    basis colour nerve comeout handle

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
