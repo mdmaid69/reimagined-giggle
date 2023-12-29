307a766f2c42bca5e10c96d3f720426b523c76e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:06:56 +0000

    recently bite company Ss clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
