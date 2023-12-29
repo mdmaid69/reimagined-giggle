e050690bab8cf80cc7512e5258637a86ff6fdabb
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:39:35 +0000

    relax right turndown organize preparation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
