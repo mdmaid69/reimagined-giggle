73650b564110ef3d6d83875481c70ee03a10cfdb
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:59:59 +0000

    live generous regular warning complicated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
