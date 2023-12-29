1e4b91dfb72602c85a31da690119464cc52f958e
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:07:24 +0000

    face area confidence tip mistake

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import re
+print(re.match("h.*o", "hello world"))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
