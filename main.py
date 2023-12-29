dd1e43e0b5dc78b18fb82fb5b142f98d6f969c62
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:51:49 +0000

    soil abroad classroom dead key

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
