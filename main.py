f55836b0fa09e2944e78ab91e95971e3844737ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:44:06 +0000

    better soon present plate crazy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
