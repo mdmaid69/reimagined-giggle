72498c77b0e437cdae828b737d983bae8667e2a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:00:09 +0000

    stone small frame hold Rr

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def remove_duplicates(lst):
+        return list(set(lst))
