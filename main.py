cf5a7fc06dd2577d4260c592a94110ad539544aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:42:23 +0000

    polite borrow getback front use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  def remove_duplicates(lst):
+        return list(set(lst))
