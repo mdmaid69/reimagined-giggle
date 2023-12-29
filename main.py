5ce3715b04daeff9864667fe5e348c6756f01c2e
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:20:54 +0000

    callout hardly prevent argue offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
