448a37af8d3a85280c9049d8442074fec6bde407
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:53:48 +0000

    geton guide spin out effort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def multiply_numbers(x, y):
-        return x * y
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
