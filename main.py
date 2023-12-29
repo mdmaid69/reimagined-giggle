d34e2ed4b18b8161813129087cb73f1cbf0360d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:46:20 +0000

    destroy noisy young path loose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
