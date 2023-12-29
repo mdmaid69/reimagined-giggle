940af8c0e1d697d4133b66de14e398ac5f0daa39
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:49:25 +0000

    run hurt family threaten divide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import collections
+def create_user_list():
+        return collections.UserList()
