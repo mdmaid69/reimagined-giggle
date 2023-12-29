9fbbe73c604cea2d78d7bd56aa8e7353f4a11f1b
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:24:27 +0000

    unfortunate peaceful watch nose somebody

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
