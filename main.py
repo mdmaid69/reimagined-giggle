c5080c09b9cf1f0d2970295ea950e2f46d04d6e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:27:28 +0000

    past tooth big care about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def convert_to_octal(n):
+        return oct(n)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
