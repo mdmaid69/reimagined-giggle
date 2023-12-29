f2beaac32ad45dac6fb0dd60aa3cb84a69eeca56
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:17:10 +0000

    represent fact show dream miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
