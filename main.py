9ca449c5c5c4bdeb693ad568373dca150b455982
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:31:47 +0000

    quit exchange likely callat funny

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
