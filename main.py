e786273a449b753d5af719440b637c335e82fc68
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:38:47 +0000

    approve nerve favourite comeround close

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import getpass
+def get_username():
+        return getpass.getuser()
