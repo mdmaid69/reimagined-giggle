9c4f13fb9ad7d384d846fbc8be566e3838a3224d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:05:00 +0000

    blame apologizeapologise situation ready pool

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+print(math.pi)
