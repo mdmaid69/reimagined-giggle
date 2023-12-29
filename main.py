13d4cc8b698eb0686848e00457e39ea0db8b16cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:27:41 +0000

    similar tap unlucky finger central

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
