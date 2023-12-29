c5bdc71cccc7398c3053b6386dc132d1d3f42f44
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:31:31 +0000

    comeout straight perhaps hit grateful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
