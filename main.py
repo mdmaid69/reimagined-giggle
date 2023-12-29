c4f6cdb70d153a985265c3e67e2ac715e8689c31
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:23:50 +0000

    fierce guilty true suit present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
