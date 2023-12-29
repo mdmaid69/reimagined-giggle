2b5067a521a23f642f7aadcecb33e161014fbc83
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:40:13 +0000

    air sell introduction keep close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
