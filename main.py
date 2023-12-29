099ebc946146432476327fe72ff90ec067418022
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:35:49 +0000

    characteristic friend pair pronounce switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
