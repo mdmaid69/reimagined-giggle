8bf213696d15cc7dbfd1ee964023b9999c5c16b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:51:33 +0000

    spin year suckme 572enter drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
