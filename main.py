97847b4dd12517b2f8924739ebe560db583dcbf7
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:01:33 +0000

    pressure minute teach Ee build

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
