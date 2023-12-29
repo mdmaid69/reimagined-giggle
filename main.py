e56cb1c1ef34e8bbc28cf9ffc49610af553f351b
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:35:38 +0000

    bar letter pattern play size

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  def remove_duplicates(lst):
-        return list(set(lst))
+def add_numbers(x, y):
+        return x + y
+  def reverse_list(lst):
+        return lst[::-1]
