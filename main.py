2c685b6aeab06ed24d1887432c96e6cc98a3a27c
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:47:43 +0000

    hope not bringup share ill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
