37b95b7ecc297a99b105bb45ad9d605a13c39b16
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:07:29 +0000

    ocean comment touch pure board

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
