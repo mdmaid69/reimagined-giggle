39c3b32f44b57b91f9db9a454a8ea21a4330110f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:23:06 +0000

    judge float fast deliberately anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_speed(distance, time):
+        return distance / time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
