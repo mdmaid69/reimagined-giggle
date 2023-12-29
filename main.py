ceccb9ac14d8f98557b2d36bdeb22889638f2089
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:01:09 +0000

    tend legal statement sentence Bb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
