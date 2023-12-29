3d585c7bfdd85498053be23eee9ebd241b4f7e5d
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:30:58 +0000

    doctor history tidy hard deaf

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
