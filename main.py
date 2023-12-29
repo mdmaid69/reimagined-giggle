3c4e6d1b9ec2784dbe9a4e20e176bf4feede4ddd
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:12:40 +0000

    shopping many prize trip large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
