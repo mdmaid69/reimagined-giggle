0c59617b91191d68844e28fc0c8195f49f944e4b
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:36:00 +0000

    trip edge opposite getback noise

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
