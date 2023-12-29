d11ea36b99451d3b97ae1d3703d0c8ceeea247b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:40:01 +0000

    comein uncertain properly underneath juice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
