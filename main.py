f8516a39a0b0e433b576790ff5bc89c221efeb15
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:00:08 +0000

    makeup born gate contribute wait

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def square_number(x):
-        return x**2
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
