f6b04ab8abf9d23b091b87b051652a2b121a55f3
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:20:57 +0000

    book opportunity beside reduction maximum

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
