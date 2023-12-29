ff751c0ea79d98ed29f6a71573bd0a0aea43284a
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:10:12 +0000

    movement cause like train excuse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import time
+print(time.time())
+def is_palindrome(s):
+        return s == s[::-1]
