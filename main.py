b427aa4375dc2c13af6b096bffb4617fc37995be
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:38:03 +0000

    pleasure work promise understand measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def get_array_as_set(array):
+        return set(array)
