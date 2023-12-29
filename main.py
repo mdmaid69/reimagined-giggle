8e090c8ffb953015ac697504205607409bd35133
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:11:59 +0000

    179bite neat type 179bite goaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
