97acb331321b895ce3899e4d74e87dee5e81a2e1
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:16:43 +0000

    active wave brain gooff spread

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
