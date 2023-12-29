3f50b1e8dfded2e36c7e662bf799577270e20443
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:39:47 +0000

    payment path must sell work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
