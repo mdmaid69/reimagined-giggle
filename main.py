b21ea511467bbc728f682b4b02c1591b575e4d0f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:05:47 +0000

    wild rescue black reach entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_area_circle(r):
+        return 3.14 * r**2
