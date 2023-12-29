73a8d1d16266e49d8328fba5d961e76660f3166e
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:21:13 +0000

    age sight maybe squeeze claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
