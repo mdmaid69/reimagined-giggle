7eba41422b25ecf6832823a5dd44d813f29558ba
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:22:54 +0000

    crazy every difficult wonder circle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
