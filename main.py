61aafe7afcaa31b550fcfe17bc4be1912f39b441
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:30:12 +0000

    clear church convinced lead appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
