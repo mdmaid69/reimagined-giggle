30851154dbe2a1373684f1d32639cb049ef181ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:30:24 +0000

    somehow war passage safety road

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import datetime
+def get_today_date():
+        return datetime.date.today()
