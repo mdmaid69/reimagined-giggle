e2b10244c8537783624ee4f7d47164493311abca
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:19:04 +0000

    permission health leg public debt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_speed(distance, time):
-        return distance / time
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
