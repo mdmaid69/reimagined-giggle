456dc9746c185175e4850ae397f26eb2771b698a
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:11:48 +0000

    takeback afford to settle watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
