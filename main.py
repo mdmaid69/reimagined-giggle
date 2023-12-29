98f8d1f819f5aee813ccbc4da2f595a7b4d2381f
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:25:54 +0000

    choice familiar aware float valuable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def get_time_since_epoch():
+        return time.time()
