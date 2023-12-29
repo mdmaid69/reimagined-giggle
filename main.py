8aa36eeff3253c39a24d52d94a10ae0f7bf327e1
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:01:18 +0000

    stupid Ee sore sound otherwise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import time
+print(time.time())
