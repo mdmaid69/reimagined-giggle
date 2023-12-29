9a85f20edeff5556822bb535fc3d85d7cf8aafeb
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:18:25 +0000

    information ball work depend top

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_speed(distance, time):
+        return distance / time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
