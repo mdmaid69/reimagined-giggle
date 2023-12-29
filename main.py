1e07a79b45968d48c4815068bde985d756c48133
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:02:29 +0000

    build suck exciting habit tend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import getpass
+def get_username():
+        return getpass.getuser()
+import os
+def change_working_directory(path):
+        os.chdir(path)
