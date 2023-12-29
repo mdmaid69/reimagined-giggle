d81dd084c8d9ac6a8b2ed3b8f7a087d15788b492
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:38:55 +0000

    for loan float square match

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def subtract_numbers(x, y):
+        return x - y
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print("Sum:", sum(numbers))
