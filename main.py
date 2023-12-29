85649b582dee117d099e62ad79a5dd2d1ab61902
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:12:44 +0000

    pause there petrol count lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
