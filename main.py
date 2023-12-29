26d346dcad5f59d40678ddbfd4e00d60146e860f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:29:46 +0000

    offence fairly stamp sudden better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
