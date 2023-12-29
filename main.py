a836f61c0791a2162c2a488cc003ab18ee7437b5
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:27:39 +0000

    promise instead rub admit run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
