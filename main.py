be9e14ba29dc88428e3728eb5a9bbe7f8c18be96
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:54:02 +0000

    stiff reduction wrap send swimming

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
