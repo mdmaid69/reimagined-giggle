b225e1cf938a4c9ae15ba8d97ffa2420855b3602
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:11:17 +0000

    clean petrol talk start clearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
