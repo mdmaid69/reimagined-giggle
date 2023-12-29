8f9c07b12cc9b94854da0652d6fd542245e14a50
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:47:20 +0000

    fortunate mixture flavour cheat brief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
