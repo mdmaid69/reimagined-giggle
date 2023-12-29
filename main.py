6eb574fe4f3ad7a8393fe7ae8101eeb8f73628d6
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:45:19 +0000

    suit tune alive comeup past

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
