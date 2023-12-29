5280950a77718560b1934fd947890cedeaac93dd
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:50:27 +0000

    seat strike listenfor lonely branch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def find_max(lst):
-        return max(lst)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
