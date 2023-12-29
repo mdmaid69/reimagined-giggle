84a6a51a3381e0f6df138dd19cdb4cef3e1efb32
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:06:24 +0000

    party range ability hungry hers

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import time
-def get_time_since_epoch():
-        return time.time()
+def convert_array_to_unicode(array):
+        return array.tounicode()
