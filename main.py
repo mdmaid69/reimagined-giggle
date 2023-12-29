e647aed24e8aa54a22fd3bc383251bfd3c723aca
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:47:51 +0000

    claim total it demand doctor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def subtract_numbers(x, y):
-        return x - y
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
