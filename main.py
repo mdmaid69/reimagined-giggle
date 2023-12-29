8b938e9b549bdec08f09d0e0888bd33d41a0ee67
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:09:25 +0000

    matter basically help only pinch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
