80ff34f9a886c52060e02e1c1b64836daa4857b5
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:52:16 +0000

    round getaway myself back sleep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_hex(n):
+        return hex(n)
