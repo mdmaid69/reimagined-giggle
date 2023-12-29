3e196e5d0de22999df90ce9aae630eccf2c7a1c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:35:39 +0000

    rush trust terrible far loose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_volume(length, width, height):
-        return length * width * height
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
