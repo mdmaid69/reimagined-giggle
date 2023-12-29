3afa4a70f2edfef605c5264f84e618ccd5fc16db
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:46:18 +0000

    respect taste discovery seem light

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_stack():
-        return collections.deque()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
