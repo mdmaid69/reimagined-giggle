60c3f815eb15e2c06ff170cf46e5dc40008f44e8
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:43:05 +0000

    copy slowly conclusion how admit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
