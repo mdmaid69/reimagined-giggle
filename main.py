e4d371820c2a6a87a114abee096c1763933a4c6c
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:50:16 +0000

    wise commit fine mistake centimetre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
+  def is_odd(n):
+        return n % 2 != 0
   import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
