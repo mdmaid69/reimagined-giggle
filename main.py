5f6b63e3283bd1ba5f6f7096626d5128197f8900
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:31:46 +0000

    tear window feature better belong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
