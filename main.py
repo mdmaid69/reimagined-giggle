fabb13134b1ba3fb2deca75eb2d0b41da25be989
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:47:02 +0000

    famous much impressive object operation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
