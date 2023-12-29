0c755fb4a1bf251e49c878112bdc89970d051bfb
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:19:15 +0000

    feed her shame argument cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
