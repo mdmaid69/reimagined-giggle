2e3e1a62db015a9b56a1722e80a353a249f8aefd
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:38:35 +0000

    manner so bottom ticket admire

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
