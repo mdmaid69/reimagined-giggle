8e67c9494c490f88f420221d157bebbf7bf2bd57
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:10:52 +0000

    to attempt arm pipe down

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
