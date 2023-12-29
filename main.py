1f4e5b99ad4bc02818ae9cf27e63b653ec7fc7ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:34:31 +0000

    swim use smile call Ll

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
