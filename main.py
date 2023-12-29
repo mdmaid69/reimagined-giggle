63a337f5a554a55d5569ba92acaa3477ed331844
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:30:43 +0000

    back scream breakfast shut from

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_length(array):
+        return len(array)
