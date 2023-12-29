c56f156bb8b4655adf8872f73a1291b5f2dadc8d
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:16:53 +0000

    favourite warning bet pretty admit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
