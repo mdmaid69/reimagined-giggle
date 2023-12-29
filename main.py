5b08306b015f76e0efa3ff12eccea588e4d63ac3
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:58:25 +0000

    at mean area worth film

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
