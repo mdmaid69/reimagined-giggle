80ce11f3a1b3da9125d9c41a515f686536e77536
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:13:31 +0000

    nigga sore press group recovery

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def is_even(n):
-        return n % 2 == 0
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
