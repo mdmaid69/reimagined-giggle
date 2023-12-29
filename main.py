8abf6de91b63328aef21967e457b63bbaa88c6ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:14:31 +0000

    difficulty now upstairs wish high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_array_to_string(array):
+        return array.tostring()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
