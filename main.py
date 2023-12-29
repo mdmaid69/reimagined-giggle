e3ab45517598f85a8bce7aca6a75367ab6574437
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:33:47 +0000

    speed cross wrong peaceful shake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
