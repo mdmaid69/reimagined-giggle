cda68698907043c9541c02e5a42a2c13b905d21c
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:55:09 +0000

    free flood shop container study

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
