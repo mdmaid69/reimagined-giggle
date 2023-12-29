c2c1b6efdd564bd3a96818c8ebcb0e4def0053d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:01:16 +0000

    swimming lean house trap scale

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-print(datetime.datetime.now())
+  def get_base_name(path):
+        return os.path.basename(path)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
