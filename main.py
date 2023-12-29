4af54380586cbac90f2efd2e07b4e5404900e681
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:49:26 +0000

    runover terrible copy view practise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_pressure(force, area):
+        return force / area
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
