e58c420ed15669dc3a548dc16130e9cce62d1fe1
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:39:35 +0000

    remark watch turn remove often

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_area_rectangle(l, w):
+        return l * w
