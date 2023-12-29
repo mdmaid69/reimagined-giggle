58e77c6b189bd1f008817d2820e92af22b94820d
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:07:05 +0000

    carry name he vanish far

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
