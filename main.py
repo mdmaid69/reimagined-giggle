8406f28d842524ce47b2da16e554a2a8ee66dd1e
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:34:44 +0000

    delight pass powerful admit anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
