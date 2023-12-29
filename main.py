6ab86f1de0d08e7da91ed64c248eb34f6ccb926c
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:22:09 +0000

    improvement state fit inquire week

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def remove_directory(path):
+        os.rmdir(path)
