3eeacfcee5e6b32444aed810accd1c4638731434
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:59:30 +0000

    little city spin demand along

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
