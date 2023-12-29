84d5d1f4e34e48d9ba927c080603a045bfffa2eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:18:40 +0000

    crowded consider with normal bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
