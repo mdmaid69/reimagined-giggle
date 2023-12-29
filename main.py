c0ace1d0485dcadc636ab2f46fc9666494873feb
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:52:59 +0000

    protection stretch hold guess increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
