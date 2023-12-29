e016e9200bb82e65f5f9677befff3931d1aebae0
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:02:49 +0000

    short face show turnon fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+def print_python_version():
+        print(sys.version)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
