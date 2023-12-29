d1c561f76ec7d305be4d70a15264db9879e66fc4
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:20:51 +0000

    turnout on heavy angry art

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
