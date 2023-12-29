b02914a40d912e54994dfccea22b31ab2940f3d1
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:53:02 +0000

    display guilty judge close favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
