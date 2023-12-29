fc9ccb665fc8ee7818cbab53ca39eb4526857a3f
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:12:43 +0000

    door our loud Rr date

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_string_from_array(array):
+        return array.tobytes()
