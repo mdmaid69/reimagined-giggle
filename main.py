2bb89899edc72d7998909a8d6234a4709392c245
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:23:15 +0000

    easy Bb direct house rise

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_time_since_epoch():
-        return time.time()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+for i in range(10): print(i)
