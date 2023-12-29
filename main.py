af2721bf6bd8955bc3fe43a165f5f88f9bbaaa2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:20:11 +0000

    embarrassing child land touch petrol

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
