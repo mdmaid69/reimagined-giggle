7e0d427e81b87fff236f567f5d7f268199bf6aeb
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:18:09 +0000

    slightly information corner rob check

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
