ed589dc0cc175c6aec8f8e533bae9e0fa746170b
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:26:16 +0000

    reaction dark temperature prevent declare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def reverse_list(lst):
+        return lst[::-1]
