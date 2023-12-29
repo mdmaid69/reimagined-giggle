914336e19c30b73cd59a97d67c6affbbbda1ecc2
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:55:47 +0000

    juice vote anything contact discover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
