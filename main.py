0d730f1ec0cb39972e6cd2d3ac8dcb506af98220
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:54:10 +0000

    thinkof point until on mouth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
