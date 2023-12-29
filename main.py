5fae5fbf134206df07f57c9307b6baf9cb3ae7be
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:35:22 +0000

    bridge great hang check fail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
