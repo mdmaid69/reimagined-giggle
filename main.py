a31b10bbdcb4c273c7c28843ad4933cbff33d9b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:40:01 +0000

    hang fly later certain almost

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
