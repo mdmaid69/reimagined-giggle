cc15081c15da539d4e563283993864c9e1d4385e
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:04:28 +0000

    rank fence swimming somewhere neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
