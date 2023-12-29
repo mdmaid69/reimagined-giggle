0836ee91532699b41cdcf4f1f5bf3f165a58dbbb
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:50:08 +0000

    maybe everywhere quiet drop use

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
