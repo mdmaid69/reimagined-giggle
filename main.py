ad4c8909c9c5c8b46047da2bc8508a26f9eb7b18
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:45:44 +0000

    black Yy guess back Yy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
