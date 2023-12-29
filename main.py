6ada44e8093d050de531a139a73568375d0e5a74
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:02:14 +0000

    region foreign promise recently Yy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import os
-print(os.getcwd())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
