dadf847ad372146a273cfc07f8813f5faa5a9c69
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:29:26 +0000

    dream address expression maybe while

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+name = "Python"
+print("Hello,", name)
