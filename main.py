4b7020e5583a38ae7c68e2dc1b75ce84498bd024
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:04:38 +0000

    angry unpleasant wide tap centimetre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
