0378efcec1cecffb6ffda9da0de4fb17884ccc7c
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:42:32 +0000

    bit impossible I warning snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import os
-def change_working_directory(path):
-        os.chdir(path)
