a1686647754170ff9ac46181a47e25cac38956f9
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:18:16 +0000

    share gentle exchange surface minor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def cube_number(x):
+  def cube_number(x):
         return x**3
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
