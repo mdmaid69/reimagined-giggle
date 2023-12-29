90221c83d2efed754634ec5ab41e6ca199cb9593
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:54:27 +0000

    happen Ff comment star creature

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_pressure(force, area):
+        return force / area
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
