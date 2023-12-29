5194ad3260bd79c5458fba8b8920b6a32971816f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:37:08 +0000

    late depend important play eager

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_factorial(n):
+        return math.factorial(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
