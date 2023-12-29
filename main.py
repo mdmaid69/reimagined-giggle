a64af951b715fba0491305600b734d6e4be3c82e
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:48:15 +0000

    order claim better turnaway accidentally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
