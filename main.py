18e318128ab592d0707de67eaa7337d34eab6488
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:25:17 +0000

    born mine shopping unlikely swing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def reverse_string(s):
+        return s[::-1]
