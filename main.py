c0a99f92c95cb9375342b9bec89a93815ae773ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:31:27 +0000

    main raw importance breathe takeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
