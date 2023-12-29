4f8808b088c8a214c5679dd414634f0b48b9149b
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:46:15 +0000

    yours record spot soon free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
