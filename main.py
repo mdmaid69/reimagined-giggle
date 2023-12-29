0665426fb84a73b62e1cca4e16cd4849cc5997a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:23:43 +0000

    manner stone summer government sorry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
