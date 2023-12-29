ee4ede7a78f607dd8bad1785e17e47f98f4042e8
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:46:36 +0000

    gas important authority slice pipe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def count_elements(lst):
+        return len(lst)
+import time
+print(time.time())
