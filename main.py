57f4cfa44060d171db61606c372e922c2e146813
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:59:35 +0000

    belief free task sleep crisis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
