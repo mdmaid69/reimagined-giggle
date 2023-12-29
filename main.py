4bc8e4b46cd3ab797a32d4fd3d0b7632f3cd4f39
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:55:32 +0000

    name inquire project clothes union

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
