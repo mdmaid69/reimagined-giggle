958230d7028ee7d84fd82f199666be81f7bea539
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:56:07 +0000

    unusual makeinto squeeze limit see

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
