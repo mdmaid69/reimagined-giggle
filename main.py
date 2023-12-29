14debbe44591631ab6dfea91d75ad9a30050a3d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:26:02 +0000

    clothes quiet smart variety prize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
