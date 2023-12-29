a5e814787feb2499e775ba77442f0de66ddb581f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:02:53 +0000

    slight majority knock birth sit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
