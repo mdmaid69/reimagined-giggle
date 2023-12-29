5ec66b0abdf2fcb961ad157e074ab930a751b19e
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:07:56 +0000

    ticket town show uncomfortable pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+print(sys.version)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
