0439a8dcd98ec80a0a5366512c48675784ff46a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:44:57 +0000

    thing open fold stick feeling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
