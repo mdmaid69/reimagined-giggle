0e9c8ac03854f5bd8247d523c34dc9976f34c950
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:51:09 +0000

    object use hello hit children

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
