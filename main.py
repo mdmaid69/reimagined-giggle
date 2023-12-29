d10b86f794449897f760531b8a269c2597e9009d
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:20:53 +0000

    crash new guilty someone just

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
