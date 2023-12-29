ed7a39e72ab7c8496b1af64ab673ed51d36792d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:46:02 +0000

    difficult organization table narrow left

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
