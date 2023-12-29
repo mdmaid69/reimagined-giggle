4ad294ffb9e5a5652a7b1d162746d19bdca6b4ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:42:25 +0000

    holiday hand relax odd create

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-print([x**2 for x in range(10)])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
