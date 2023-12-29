6367c4a8654ff22a736af11d5327d2e326f18e32
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:21:24 +0000

    century 570enjoyable tip major keepout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
