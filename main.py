5a79b5407401d5279a95f0783a6e4613c0ac3059
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:03:44 +0000

    cry swim noisy eager transport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
