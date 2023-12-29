f35d5676921271543f5e01d91d2ac8ea9831cb31
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:43:22 +0000

    high claim react danger but

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import array
 def get_array_from_file(filename, typecode):
         a = array.array(typecode)
         with open(filename, "rb") as f:
         a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
+import collections
+def create_priority_queue():
+        return collections.deque()
