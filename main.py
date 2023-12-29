584f558de1e13ab81fd3767f1919cb2ebe2db6c0
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:14:28 +0000

    quick bright cough but accommodation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Uppercase:", text.upper())
