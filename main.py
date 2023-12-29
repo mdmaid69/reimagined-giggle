0b65d257c0f342312d1edc07709ce05909b6dd9f
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:39:06 +0000

    look holiday run come flat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_pressure(force, area):
+        return force / area
