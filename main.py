c7d4a70f70eb43aedcd508dc078ecc328230b6fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:44:51 +0000

    me best impatient now nor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
