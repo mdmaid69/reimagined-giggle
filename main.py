33c2d20d8f9e0f74d00adafc57e089414453e342
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:37:24 +0000

    correct sign organization receive purpose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
