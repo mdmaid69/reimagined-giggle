f25b777faab23e940f6a1937b06eb4f4954d11f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:49:37 +0000

    human report order post whom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
