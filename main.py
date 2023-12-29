d193830c86cb9c8de3e11ebd2b94ecc2621ff98b
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:25:12 +0000

    past 567enemy keep country aware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import sys
-def print_python_version():
-        return sys.version
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
