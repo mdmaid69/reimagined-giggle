8bdc69a15de1524641a9ae151a634c2e51a8696d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:50:02 +0000

    goback(to) clear lock safety war

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
