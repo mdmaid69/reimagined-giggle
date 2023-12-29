1c494d973cfcec96cf79a9171a7ae1f53563189d
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:36:58 +0000

    the top spill trousers dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+print("Difference:", set(list1) - set(list2))
