3ea83b828f74b5ba60b7e44529a5d8b4af58378f
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:29:54 +0000

    punishment war high issue unite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import platform
-def get_python_version():
-        return platform.python_version()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
