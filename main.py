f8ea47d423302fe7e79c14735c7b326e631b7db7
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:25:40 +0000

    correct responsible line clue road

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import sys
+def exit_program():
+        sys.exit()
