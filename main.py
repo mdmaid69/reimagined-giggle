7de2b5cc1be3c723cfd2aeee0cfa53a8b19e6903
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:39:58 +0000

    allow business wave drawing smart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
