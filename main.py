12bd5b81647d60aa31e37bd6a6ca458fd743fb60
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:17:08 +0000

    them answer protest scared meaning

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
