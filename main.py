236a44d332f4eba017e929ecd2fda552891d3867
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:41:46 +0000

    scream burst port stay old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
