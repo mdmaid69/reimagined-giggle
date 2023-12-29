c12312c06658a6e96423dda4e770d6dfa9cf80e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:05:21 +0000

    same concentrate appeal delicate centimetre

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
