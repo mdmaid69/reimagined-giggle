27a53ab9c0eb62f36c6ac5ac9f191591911ac744
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:47:18 +0000

    breakfast sight leg switch beautiful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
