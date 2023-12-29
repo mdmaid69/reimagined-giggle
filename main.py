c9bcb2c9768ffcbe5c81e77cfd09b5bdc4bdf460
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:16:56 +0000

    club bank agree object reasonable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
