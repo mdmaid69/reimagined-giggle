7e90c19ff307216e7d4293c820507063acb10007
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:29:02 +0000

    up break level best fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def add_numbers(a, b):
-        return a + b
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
