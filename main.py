d0b572beefe5a7b4c5b4bedafb3b663dc917b241
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:43:07 +0000

    force happy comein comein challenge

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
