d0a4f249bdf52b0d50d0169850dc23eb00aae123
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:40:21 +0000

    tomorrow bath appear wind situation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import numpy as np
+print(np.array([1, 2, 3]))
