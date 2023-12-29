a897bddb611f1966da8c18317f318cd2f72821b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:34:16 +0000

    emergency dig touch tense intend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
