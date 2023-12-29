a3718456e9f91b37cdfaaff7171e08aaf2892e64
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:07:02 +0000

    ocean fairly mistake fall laugh

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
