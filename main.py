a93a8fcf5684d971c36fc5856b7beda7bd190d09
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:05:51 +0000

    odd neither wheel ring call

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
