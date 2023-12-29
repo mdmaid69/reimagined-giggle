0b37d7869ebd0e4809744b9bb9c03cc1572cc073
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:08:15 +0000

    aware neither convenient beautiful creature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def count_elements(lst):
-        return len(lst)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
