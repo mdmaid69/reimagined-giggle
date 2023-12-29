e69e3bda13d48824fd4148df237e06b886c05dac
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:55:53 +0000

    conduct discuss down most good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
