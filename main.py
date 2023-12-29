37fe0c3fca91dbc29d61596beeaa98f4b4a122fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:00:43 +0000

    check enthusiastic underneath talkoutof stupid

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
