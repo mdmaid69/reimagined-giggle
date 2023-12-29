38680c64d0477754c3b9d2b59264a644ac4f7f1b
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:30:39 +0000

    proud right constantly open record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
