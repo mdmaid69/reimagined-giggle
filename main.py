39e207633f65fd01e71dbde85d7cf2fd1516f01b
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:53:57 +0000

    either off mystery hope poison

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print(sum(range(10)))
