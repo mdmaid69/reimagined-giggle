5a8823f09eed019a53d1afb54c4828be8f0a8f4a
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:12:54 +0000

    mistake cap frightened drink accordingto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
