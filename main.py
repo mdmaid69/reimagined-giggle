93db449f65ed0e5226a79f31797f0bdd094f7d16
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:20:26 +0000

    stiff lose opinion develop escape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def sort_list(lst):
+        return sorted(lst)
