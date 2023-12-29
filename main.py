7a428f31dec5432e0c8abee56789b6e12dd2634f
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:37:11 +0000

    find for answer hot wake

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
