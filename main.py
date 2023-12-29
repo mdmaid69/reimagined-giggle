49f5ac466f9fd8888c86975a43e524144717cf9b
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:30:15 +0000

    of treatment hold board fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def find_min(numbers):
-        return min(numbers)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
