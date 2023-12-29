e985cd747ca801eac0fa0dc776faa43bd46217c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:53:09 +0000

    mark dish tap contrast advise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
