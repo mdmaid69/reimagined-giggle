83a2e2bfd4a53a8e2bfe7331367d1d9b86cb1670
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:33:24 +0000

    round persuade shallow leave spoil

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
