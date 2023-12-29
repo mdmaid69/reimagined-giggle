e7b364e9c4c5fee565797184a230ea551054a6c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:01:46 +0000

    trip tie taste pronounce Ww

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
