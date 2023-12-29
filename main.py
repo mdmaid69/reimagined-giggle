26276418c1b4fb89c2f36031b3d3291175599471
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:09:32 +0000

    little clear full rise save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
