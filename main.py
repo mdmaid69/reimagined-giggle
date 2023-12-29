07b2376d3ffaf859df20fea35a6f14fa3d772753
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:16:51 +0000

    depend let trip married time

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
