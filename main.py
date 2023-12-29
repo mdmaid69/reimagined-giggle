c79026c72f0449f7906cfedc1b103fbe92e64989
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:38:13 +0000

    bus home several complain corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
