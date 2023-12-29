71b5136b4865efad2da885f5df83832a4b2718cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:54:25 +0000

    when professional damage miss trick

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def reverse_list(lst):
-        return lst[::-1]
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
