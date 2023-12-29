763ae822f28f1202dd333268485c3bdbd0382a3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:29:36 +0000

    remember Ii worry discuss band

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import getpass
+def get_username():
+        return getpass.getuser()
