696230faa48a4301f627b9ed9fb84800d4bb3413
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:08:58 +0000

    fork conduct somebody getout breakup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
