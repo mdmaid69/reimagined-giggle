58a7891cd15986ea64a37218473c62489cfc82a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:29:05 +0000

    tune region below pin recommend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+for i in range(5):
+        print(i)
