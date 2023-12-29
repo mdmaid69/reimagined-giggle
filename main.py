e9c64ce9f428df0f3584635e528b1410fe31276c
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:17:06 +0000

    butter length theirs please mark

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
