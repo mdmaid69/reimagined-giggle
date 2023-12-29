0e4c37d015c247a42494b968fe8f705102fa1248
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:07:53 +0000

    hang winter issue injury hers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
