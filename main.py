3abb4d92025bb293a97714d3a4ef021feff7e440
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:11:02 +0000

    century state key fun radio

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
