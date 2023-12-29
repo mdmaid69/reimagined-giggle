d06834d17438d94049c051ea8e898ff401d9e571
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:51:52 +0000

    sweet emergency jump recently ball

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def greet(name):
-        print(f"Hello, {name}!")
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
