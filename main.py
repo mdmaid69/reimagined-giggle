d6525048b5943033a3ee7acd59e41e51290bfd41
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:00:14 +0000

    raw case damp accommodation unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Words:", len(text.split()))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
