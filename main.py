575fd9565448af2c7045104a8bb68defe946f869
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:23:25 +0000

    sink claim big runaway earth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
