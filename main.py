97a26832b7d7f7e5596f44b0f4df91d22193336c
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:07:07 +0000

    pride challenge closed deliberately lower

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
