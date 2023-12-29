a144cff6ccd03245b6f3ac49cbb9b4430543f351
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:44:29 +0000

    statement close subject backwards evidence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
