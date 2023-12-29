7d7496f152a9ebb8928d42ae6729ed9640508e4b
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:52:55 +0000

    row cross train pronunciation impression

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
