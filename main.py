632aafa9856936f79f2e4ecc84e89f253195dedb
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:21:04 +0000

    opponent joke pair attract survive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
