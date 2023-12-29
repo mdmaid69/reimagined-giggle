d1bffbc19dce1b1a19d99b6f4cbdece3db399c74
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:09:53 +0000

    prefer rush sound objection little

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
