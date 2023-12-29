87c6b7b7c9d6e8fb0babc4dce74e8c8726c62315
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:38:30 +0000

    stiff successful glad phone ahead

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def cube_number(x):
-        return x**3
-def convert_to_hex(n):
-        return hex(n)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import datetime
+def get_today_date():
+        return datetime.date.today()
