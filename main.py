6904e80dedad65a0ac973b2c53847d0157dd3623
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:37:30 +0000

    truth trouble pocket closely too

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
