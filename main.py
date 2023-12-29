3f1efb8bd5dab77c5ccd1f2c8892b37174dcd212
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:37:54 +0000

    secret suck strike yesterday fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
