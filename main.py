3f8c4cb616d062f46b0bca5d39939b8b71d646e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:58:05 +0000

    dry child means tragedy noise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def reverse_list(lst):
-        return lst[::-1]
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
