be077bef83d348cf54dc630c88f3d37b2bdd36f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:30:33 +0000

    disgusting trip spin talkback takeback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
