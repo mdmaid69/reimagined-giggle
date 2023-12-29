6c2f6971132318acd3d0d6a38ca173543b4baa31
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:36:27 +0000

    bored miss low hear worried

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def split_path(path):
+        return os.path.split(path)
