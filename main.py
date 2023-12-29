0f1109296354df13693f33dcef9167b1c19d6115
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:05:08 +0000

    share Gg preparation service finish

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
