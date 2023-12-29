29c878effe84cc0b34e56ba06df602888ee390ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:39:17 +0000

    equal promise wherever keepup bringforward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def convert_to_hex(n):
+        return hex(n)
