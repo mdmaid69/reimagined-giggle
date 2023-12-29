9044ab4414f948f76ae48b67170b8a224f9cbc94
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:07:52 +0000

    keepup 571enough store deep nice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_string_from_array(array):
+        return array.tobytes()
