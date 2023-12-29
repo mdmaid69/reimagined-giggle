6c3c942e84b4af6e1c75e31e2329924a215f7a09
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:16:42 +0000

    recommend respect repeat tune mail

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
