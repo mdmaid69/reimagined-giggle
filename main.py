02009d3f9a794baad84a5b647a48918b87ccbea6
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:53:09 +0000

    actually frequent false cry trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def convert_to_octal(n):
+        return oct(n)
