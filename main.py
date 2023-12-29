7808f7767e35d5c41637d1b5376bb7ba87f61622
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:48:50 +0000

    mystery dish mean notice next

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def is_odd(n):
+        return n % 2 != 0
