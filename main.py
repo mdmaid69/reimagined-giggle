1f5b66e0719c3081a08ac84249a2cceb2a01eb7b
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:53:43 +0000

    goback(to) land birth because memory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
