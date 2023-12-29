7dc98c57fb4480c9685699d8e22d291182fedd1e
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:08:51 +0000

    mystery world your compare pressure

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
