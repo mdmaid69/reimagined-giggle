459ef39d61e96f6425f101b9bb0c203251cf683b
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:30:44 +0000

    mean confusing convenient broad community

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
