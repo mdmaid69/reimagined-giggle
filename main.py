0b15977b65dd55fe2e5fbba766d209fae25027d0
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:58:04 +0000

    feature pretend shout aim respect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
