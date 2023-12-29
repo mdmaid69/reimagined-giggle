18d28221b1016b8ed697a9589dac3342209663b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:03:39 +0000

    idea maintain face calmly available

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
