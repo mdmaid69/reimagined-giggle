e0534b3db171062933b597673ab2984f2681557a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:30:59 +0000

    determined giveup exit or careful

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import datetime
-print(datetime.datetime.now())
+  def subtract_numbers(x, y):
+        return x - y
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
