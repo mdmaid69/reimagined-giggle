0ed460460241a804cc64224d4600ee0502f55b61
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:19:04 +0000

    exact suffer support twist pleasure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_density(mass, volume):
+        return mass / volume
