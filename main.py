1071253a7b05e826ecc05fbbd98cd859194d1c3f
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:15:34 +0000

    market accordingto rid blame power

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_list(array):
+        return list(array)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
