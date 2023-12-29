3310fdc6ddfccc0e7aeeb6cf87f7ef90b6729f80
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:38:00 +0000

    debt sentence chat waste argument

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
