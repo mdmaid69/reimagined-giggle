7b73471a626ad7f21278f3dc864dff2ce016503b
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:58:40 +0000

    violence ability new hate float

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
