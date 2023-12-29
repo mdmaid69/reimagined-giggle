f0b086715fa9cd3e7eafe0d67abf7e0f66e591cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:56:36 +0000

    on turnaway certainly human knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
