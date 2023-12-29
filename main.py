20ae16e922b6d5085869b2ff1f6e4e297c6faa7a
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:34:28 +0000

    takeback forever feel birth in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
