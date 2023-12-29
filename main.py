c3d085c8d27ffc1ca4ba87200e4c1c1d113753ab
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:32:50 +0000

    bite work long month as

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def cube_number(x):
-        return x**3
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
