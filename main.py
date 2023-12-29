635d071705d437d0688fabb35ca86249a8450cfc
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:39:48 +0000

    strange waste plan hear sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
