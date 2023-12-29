3cea5f8011e0aba3e7759305ae3179b4b6ad5e11
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:17:33 +0000

    keen giveup length early autumn

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def convert_to_binary(n):
+        return bin(n)
+  def sort_list(lst):
+        return sorted(lst)
