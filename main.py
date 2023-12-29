17f854b3b88916bbf542f8b19ef1292583aae61b
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:40:55 +0000

    grateful majority sticky by card

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
