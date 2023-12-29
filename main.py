08c5fe8855e0e1b0c5bdfeb71b7f20475c403a53
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:14:03 +0000

    priority point heat blind mystery

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
-print(os.getcwd())
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+print("Hello, world!")
