5a51e3828220205817cb2930d2a4a451ee8d1d9e
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:48:36 +0000

    lower so goout chance hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
