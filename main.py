7fbd60012f9db80db30ad37ea8eb1122d3ab518e
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:56:44 +0000

    if guilty instant demonstrate deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
