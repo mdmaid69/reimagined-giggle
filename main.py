79158d4e32a55b5b9d0937081e41f473f539d53b
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:55:10 +0000

    worth underneath before scale breakfast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_average(lst):
+        return sum(lst) / len(lst)
