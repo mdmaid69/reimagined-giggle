3df75d02e1845cf8528d2a0206c29534ed14a10d
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:51:01 +0000

    television weather together discover alone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
