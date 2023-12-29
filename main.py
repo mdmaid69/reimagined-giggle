22928c87d1c22eaa06265f155ed00e1a90801da9
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:30:21 +0000

    increase confused purpose listen money

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
