0d9bb159cd357cde03df013390703a168aa36be8
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:12:06 +0000

    difference opinion section confuse spring

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
