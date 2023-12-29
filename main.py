946a0ea9b517f4db13078a24d4614f5dfe4cb352
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:00:10 +0000

    bet condition write Zz tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
