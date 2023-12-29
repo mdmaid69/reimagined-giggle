3a1ef9b0fa1a6eb72a443262002220cd7d223654
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:54:53 +0000

    taste convince turnon past better

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+for i in range(10): print(i)
