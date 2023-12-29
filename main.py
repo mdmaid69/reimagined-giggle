2f6c9d4121cb24904fe9d65f85a8fd13da769158
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:02:42 +0000

    goodbye unit fair scale belief

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
