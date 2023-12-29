2e14d04768f470815915739c37df59f8736e9c60
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:18:48 +0000

    new detail drop downstairs tip

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def count_elements(lst):
-        return len(lst)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
