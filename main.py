e72487ea7ee7907e8b692d3083d65d1ad25ba545
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:16:54 +0000

    pollution blue brown violence later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
