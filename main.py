65d3ebe9d45c15b0574c59aef099c5d058fddfaa
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:47:27 +0000

    clean minority tread stay cheek

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
