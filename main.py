d81a2a43c604ea50874af9bbb71ea9d538e62879
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:34:45 +0000

    always live centimetre every hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
