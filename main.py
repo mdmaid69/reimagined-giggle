7399111a675bc889351d2c92051c78853f101030
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:51:29 +0000

    voice become soon ride really

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def divide_numbers(x, y):
+        return x / y
+x = 10
+y = 20
+print("Sum:", x + y)
