776db2d642503f9a2a121ab698f9050bae965f7c
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:10:07 +0000

    callup bang must cheque practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
