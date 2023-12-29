5326c91f199cee36fb7d7406fa9a56c69feb11c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:11:28 +0000

    exam door thought rent hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
