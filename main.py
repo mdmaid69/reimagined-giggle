e93e7796c2b49ffbd294163c900fa8b0f7d4429f
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:26:53 +0000

    finger wall scream could watch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
