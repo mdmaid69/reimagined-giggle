237a7d89dd8d14119250c4a7076b58c3b0d2e4f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:29:32 +0000

    clothes promise fun leave air

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
