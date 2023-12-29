5e6afa263f1669c12f67c0762356d8767ae59102
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:21:22 +0000

    stop file follow tall impression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def is_even(n):
-        return n % 2 == 0
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_priority_queue():
+        return collections.deque()
