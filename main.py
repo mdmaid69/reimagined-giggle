f0c3ddfb32ef7b28e8b3419543d9769e318d87d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:50:42 +0000

    swim realize seem admit be

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
