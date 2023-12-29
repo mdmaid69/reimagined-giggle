06a08c62b5b58aac99e63e4b129981cab55c4483
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:14:08 +0000

    build bunch owe draw mysterious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def count_elements(lst):
+        return len(lst)
