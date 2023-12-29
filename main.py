ca00e5b18dae3ac4d3938cb11c3b3e47e355f2fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:43:01 +0000

    trip section name whether sticky

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_pressure(force, area):
+        return force / area
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
