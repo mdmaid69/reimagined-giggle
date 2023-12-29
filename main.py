a88b1ff0c2fcd7b4ab4284743ac64c8f8bd383fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:22:33 +0000

    unfortunate convenient walk somebody model

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(5):
-        print(i)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
