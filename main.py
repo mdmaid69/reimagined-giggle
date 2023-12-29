ccf5e211e85e2870b315b0ecce2c40c60c93b9a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:48:25 +0000

    play floor string computer past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import sys
+  def get_python_version():
+        return sys.version
