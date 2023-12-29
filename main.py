2f73ec5aa742db2522a83da5b05e44caa3583065
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:25:04 +0000

    smart goon punishment fill add

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_list(array):
+        return list(array)
