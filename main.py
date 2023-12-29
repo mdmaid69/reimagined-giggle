93f24979ee7060061c3cd9971f4781439ca80334
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:39:58 +0000

    store there lookaround priority bottom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def square_number(x):
-        return x**2
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+for i in range(10): print(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
