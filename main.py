67e92249a01a750a51e11865e50d0092a953e54f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:22:49 +0000

    workout trust back satisfactory card

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
