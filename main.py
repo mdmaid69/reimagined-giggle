a3e0e6ce4593a0b376ff9795373b25ff1e3eb144
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:51:09 +0000

    active risk emergency breakfast eye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import numpy as np
+print(np.array([1, 2, 3]))
