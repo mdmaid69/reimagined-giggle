33aa9e93b12082178064b64e630e96b714890506
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:28:56 +0000

    rather under Vv hollow translate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_base_name(path):
+        return os.path.basename(path)
