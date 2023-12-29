e12618c3ecb34751ef5cf6fb400d2e1d82def192
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:58:47 +0000

    hit used stay branch turnon

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
