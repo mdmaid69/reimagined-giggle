43857a776fc11e32b09929b46ed3655c1a7821fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:15:25 +0000

    past thumb wide its develop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_set(array):
+        return set(array)
