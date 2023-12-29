30eb2b4eba98e86fca7065a5c4da477abf4f0817
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:45:09 +0000

    practise organize loan well start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
