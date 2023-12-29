b7527f120659742e9d3a46e5af11fd4d4ce82ed2
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:39:04 +0000

    as sale blue near whoever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import time
+def get_current_time():
+        return time.time()
