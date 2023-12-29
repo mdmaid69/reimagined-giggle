b862e3328ec8b14bebc867cb335c19a2d2affcae
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:48:08 +0000

    drink punishment discover swimming represent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def check_if_array_contains_item(array, item):
+        return item in array
