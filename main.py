cab1495161eeaf95a1857e9899ecbd25c2f17a1f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:17:28 +0000

    tread rob bringback sore backwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import re
+print(re.match("h.*o", "hello world"))
