1fde01da5ec9a5ff7052bcb2ef8e05f71b9c385e
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:19:02 +0000

    destroy confusing like alive convincing

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_speed(distance, time):
+        return distance / time
