4a2eaab23dd89757f0078fa0d47ec1dfc4504591
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:45:57 +0000

    education several convince herself guide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import time
+def get_time_since_epoch():
+        return time.time()
