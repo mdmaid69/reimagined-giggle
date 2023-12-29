15565f3f9fa73820db3a2617183619d03862f281
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:19:28 +0000

    further previous high capital judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
