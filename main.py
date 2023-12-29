d1d6535b81f24c6960136a84edbcb2f08fc3b852
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:05:04 +0000

    turnaway store afterwards shout record

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def find_unique_words(sentence):
-        return set(sentence.split())
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
