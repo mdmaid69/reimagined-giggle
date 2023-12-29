3ad7ecfbb493459711af882512385b8274cd2ada
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:57:57 +0000

    lonely performance sail actual recently

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
