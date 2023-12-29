9aba5753fa119c6538dddf75d5671fe93a6320b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:17:00 +0000

    content holdout squash because quick

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import time
-def get_current_time():
-        return time.time()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
