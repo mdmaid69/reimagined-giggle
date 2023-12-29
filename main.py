e4c146123b041136536a1932572c10feb426cf8c
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:22:21 +0000

    Zz duty cruelty left accommodation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_current_directory():
+        return os.getcwd()
+import time
+def get_current_time():
+        return time.ctime()
