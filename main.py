3342cc6749c163fdb367f8aa8f1714750b6120c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:21:34 +0000

    that murder activity free point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
