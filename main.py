500941befdac20585dbbb46a12d63c5755d75912
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:19:51 +0000

    sister honest damage water grade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import tensorflow as tf
-print(tf.__version__)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_array_as_list(array):
+        return list(array)
