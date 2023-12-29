a6dd91130abf697087ed6f6e0560cac9aecbaa2a
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:03:28 +0000

    cross line unfair joke spin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
