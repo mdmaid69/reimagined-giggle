04a37b82a967923c083b275e75f41be99f2483e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:09:38 +0000

    it no repair one firm

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
