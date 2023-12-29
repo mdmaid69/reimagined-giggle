3c6e56c9cf3144bed1e5b29ea35816d6bcc968a8
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:22:00 +0000

    load talkoutof polite persuade since

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
