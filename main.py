d1e19e01c5dea5c37ddd4ecc1f4bb9d27078df40
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:56:10 +0000

    before best dream such bunch

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
 import os
-print(os.getcwd())
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
