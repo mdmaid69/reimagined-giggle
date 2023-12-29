f1f7c12ca134203410ea967278dddf383d175dda
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:11:21 +0000

    on foreign regret off meeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-def roll_die():
-        return random.randint(1, 6)
-import sys
-print(sys.version)
+print(random.randint(0, 100))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
