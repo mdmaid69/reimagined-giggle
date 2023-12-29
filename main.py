708f398162c4bed161645a36d3d21b9100feca7a
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:06:26 +0000

    mistake young learn bringup address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
