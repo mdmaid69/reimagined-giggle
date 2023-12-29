714b398a2a1c4a772f524e9c0638c28283332946
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:12:09 +0000

    shop Hh direct material comment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+print([x**2 for x in range(10)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
