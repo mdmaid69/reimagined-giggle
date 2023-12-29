f7f9022f692e8324ad37db0e7ca2ef6ae29d51ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:16:50 +0000

    comeround record Dd objection whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
