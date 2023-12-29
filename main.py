29a83ab393d6ee8f212f2e8c7e91822d9491597c
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:13:03 +0000

    press exploration kick party square

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import os
+print(os.getcwd())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
