92790034edbe94aca8baf19f6319a3a72c492425
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:28:51 +0000

    use teach practise out performance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
