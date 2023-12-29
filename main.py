c92223745b6e9f090a343d52765b69a525f35ce0
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:56:55 +0000

    sense visit stare accident around

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+print(re.match("h.*o", "hello world"))
