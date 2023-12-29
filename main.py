1241474521232841495d2d2aae40c158d3515f9b
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:54:26 +0000

    one drink single soak means

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-print("Hello, world!")
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import random
+print(random.randint(0, 100))
