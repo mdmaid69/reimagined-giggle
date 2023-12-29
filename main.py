808d5b3b072dc2c2b0c52c785c3b4500567b19f9
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:17:35 +0000

    close fetch add interview Gg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
 import math
-print(math.pi)
+def calculate_exponential(x):
+        return math.exp(x)
