394e515accaeaab5699075afc74feacfc8a4f980
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:38:31 +0000

    record emphasize bend either intelligent

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def calculate_area_rectangle(l, w):
+        return l * w
