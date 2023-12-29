0bfc42aa0427f1ea963797359bec9bf2449e7add
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:45:27 +0000

    become shallow conversation geton top

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+for i in range(10): print(i)
