89b73dcb3d631abc1e1cb7fcaef61ce05517f050
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:51:57 +0000

    press loss tear guess glad

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
