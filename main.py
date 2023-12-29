6ebb40fe4067fbbf672c6d3a44606b2c0ac875fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:44:59 +0000

    instruction valuable mysterious children bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
