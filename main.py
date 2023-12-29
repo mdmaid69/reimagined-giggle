543333b811c5b7bd50bdd8f4004e52b6de933716
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:01:08 +0000

    fly join achieve determined inform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
