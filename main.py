86a0a7972f7d7fba7a67c8e86a61a73daaa0ba50
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:53:32 +0000

    tend move naturally frighten complain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_typecode(array):
+        return array.typecode
