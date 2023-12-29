ddab5a5d75fd0460f076f4dee4df9a7cd2486b25
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:41:42 +0000

    coffee string climb part body

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
