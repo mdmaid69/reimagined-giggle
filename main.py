f017a8db4d9fdaf3c95013e2ef37553d6c3b580a
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:57:16 +0000

    rule warning influence provide bag

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
