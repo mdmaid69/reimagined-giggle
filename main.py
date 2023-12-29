7fbb043290ef7b64ee4997f18903dbc33e29d627
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:20:30 +0000

    may calloff equipment kick ago

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
