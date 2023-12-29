3edf45a67a1213e2f92619447c6b9a87ffd42634
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:21:46 +0000

    allow space heavy urgent crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_file(file_name):
+        os.remove(file_name)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
