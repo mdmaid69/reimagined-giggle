4c1e45d3bdcee00e06991fdd5e39b739edf5cd10
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:40:14 +0000

    rent break roll form apply

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def square_number(x):
+        return x**2
