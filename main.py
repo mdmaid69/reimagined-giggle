ec39053b098e7bd6facc3f99495c847ff75f9a12
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:31:31 +0000

    probably look delicate exciting basic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
