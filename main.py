ea3c778d5c86832b17fead660b7c0794cab9e49e
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:55:28 +0000

    cut wide seriously congratulations runaway

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
