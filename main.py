7b02183fe1ba67779b7b82ee3142fd56efd26bf2
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:04:44 +0000

    comeout freeze train related emphasize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
