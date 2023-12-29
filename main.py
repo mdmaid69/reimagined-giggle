93f2d4fba1e0b13e5e8503cc49b70c2a9834b741
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:08:13 +0000

    watch adjective frighten sometimes far

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+text = "Hello, world!"
+print("Characters:", len(text))
