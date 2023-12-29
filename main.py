aad8c5056fab471ecf9646457b7f26ce72f63c33
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:05:10 +0000

    stop deliberate basically maximum hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_acceleration(speed, time):
+        return speed / time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
