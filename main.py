56408c2c337417704e7b2911211c7e6ade0b3d53
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:44:07 +0000

    that foreign body though punish

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
