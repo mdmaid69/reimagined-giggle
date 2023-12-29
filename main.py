eaa25adbef9d05a05315eb6614f604860a5a4b95
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:18:29 +0000

    effort behind risk shadow murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
