f1855907918cc617ab56f1005d3b1e5f11e0417b
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:54:35 +0000

    note precise plant future comein

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
