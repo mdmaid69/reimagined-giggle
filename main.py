d5b9a5729e9dd9b9173e607b564d055504b81eff
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:08:31 +0000

    book threaten final whisper common

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
