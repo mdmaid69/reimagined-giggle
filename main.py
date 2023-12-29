e01d6102ad245a21f6f1e645fd4cf1916c310b30
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:35:20 +0000

    lot relax beside proof offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
