9ca29d545658da8582be746c86f9cf3e60fc9796
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:31:02 +0000

    bright smell match process report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
