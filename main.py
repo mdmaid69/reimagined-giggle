01eba541e9b2c4b8fc25263d6d581b74deb15b11
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:22:00 +0000

    violent business finger related pleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
