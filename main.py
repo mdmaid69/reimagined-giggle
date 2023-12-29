afe951d26e4de838c4344c4ad931b9af2e978d0f
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:52:23 +0000

    load delicate habit exact cause

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
