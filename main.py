30b59a0d8d64ac29d68bf82f71b36a9b6b164b66
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:23:46 +0000

    sticky callout round than change

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
