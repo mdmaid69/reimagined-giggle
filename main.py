700c4a5d075d80679e8524f41cd6708b61d98a3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:00:17 +0000

    calloff visit because could floor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
