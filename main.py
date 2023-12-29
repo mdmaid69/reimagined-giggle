1b95a45deaee9cf7647c4e2d6491bb1d77f8c5ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:09:14 +0000

    stand colour he available line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
