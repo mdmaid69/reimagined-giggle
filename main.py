45faf743340def73fc0a0a5812a44aed13d2076b
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:39:41 +0000

    entrance also century wait uniform

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def print_python_version():
-        print(sys.version)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
