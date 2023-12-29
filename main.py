8781412fc6dd53d83fe2b923381422362582283b
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:34:53 +0000

    lower disturbing scream public attach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
