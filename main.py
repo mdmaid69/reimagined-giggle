4eb9d6f63447b9c8652ba63c8cc2a0f87b315ee7
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:13:57 +0000

    seriously drink plan 568energy keepup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_power(work, time):
-        return work / time
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
