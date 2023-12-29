c9cdc12c5b5032f349cd02b4546118c202d7ee34
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:02:17 +0000

    memory children decide occasion unable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
