5e1a077668de36e6099280a9ee7ab0dafd6a2c18
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:55:05 +0000

    popular plan apart smile great

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  def subtract_numbers(x, y):
-        return x - y
+for i in range(5):
+        print(i)
+import array
+def get_array_as_bool(array):
+        return bool(array)
