4b33608a7051d34f690335f4e413981dfed55351
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:58:21 +0000

    yes sentence quick satisfied strike

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def add_numbers(a, b):
+        return a + b
