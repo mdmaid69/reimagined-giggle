0f9e3ec7ac50c146b865f9e8f08124e91b5b73a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:48:14 +0000

    type lock takeout appearance trip

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def count_elements(lst):
+        return len(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
