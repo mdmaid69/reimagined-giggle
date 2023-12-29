7e4bff1a50533889a5735f4c6be3464f599a07bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:38:53 +0000

    dare entry fault sing window

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
