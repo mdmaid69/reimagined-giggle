c218480ca3c8da40f251d441f821e61535d8afa7
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:45:01 +0000

    pronounce ill circumstance shape sign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def convert_to_octal(n):
+        return oct(n)
