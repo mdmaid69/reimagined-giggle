0cae58f1de2221c68053e7d410ef467244549662
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:45:28 +0000

    again dry payment swallow word

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
