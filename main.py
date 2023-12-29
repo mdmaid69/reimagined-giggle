c4faa232bf99fe6196c9d55c9241adcfcd164395
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:11:25 +0000

    advise popular whoever accidentally immediate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
