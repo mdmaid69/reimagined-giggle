bac5ba5437021dd8148358222099dfd3dde99a3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:19:33 +0000

    think appeal set radio 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
