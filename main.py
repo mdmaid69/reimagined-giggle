11df19a19a407c781579fbc4b3124aff380e5fcd
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:51:55 +0000

    shall whichever drink ilovedicks thirsty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
