c7d1878e759d1d4291f8deab2079edb1812638e1
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:53:49 +0000

    walk insist save through that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
