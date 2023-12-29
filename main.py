20c1124b912f17ab7f46492a2f279842f882871b
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:22:28 +0000

    pretty seriously collection path opposite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_current_directory():
+        return os.getcwd()
