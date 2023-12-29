50b7868af560c9ee11226167dc9b637b339b6f1e
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:25:55 +0000

    visit whoever board slice error

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
