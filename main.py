eaf69301655557890bcf83006e0fb211130d6623
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:18:41 +0000

    club system do should forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import sys
-def exit_program():
-        sys.exit()
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
