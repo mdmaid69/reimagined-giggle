6323926cb456c60c8f4660848a785d23cd17beb8
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:08:45 +0000

    capital young there goback(to) mood

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
