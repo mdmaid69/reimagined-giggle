b2bd5ae9343f4b6a5bd4693e21866747a3355fd3
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:38:10 +0000

    actual develop abroad clean ahead

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def count_elements(lst):
+        return len(lst)
