59dfe49d5d9bc1f2441136b5262682a947fc5632
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:16:21 +0000

    hello detail wander check club

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def convert_to_hex(n):
-        return hex(n)
+  def get_base_name(path):
+        return os.path.basename(path)
