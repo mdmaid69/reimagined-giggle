47b83d3f0eacc4ae7278d6af06d6afd64a32ac3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:31:10 +0000

    interfere scream put bin unlikely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  def add_numbers(x, y):
+        return x + y
