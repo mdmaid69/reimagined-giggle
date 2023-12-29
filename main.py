743148c3fb80720b2c9f7a1f48d2c1fef1257e14
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:40:25 +0000

    minor getoff port whistle stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
