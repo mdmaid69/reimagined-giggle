58cde175c277d52a368a88e6934443c9d798d096
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:24:52 +0000

    key brother combination reply trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_area_rectangle(l, w):
+        return l * w
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
