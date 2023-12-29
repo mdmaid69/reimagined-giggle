5ea045cd193666c79e26e23135d8f552bfade803
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:25:45 +0000

    command music thinkof search plan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
