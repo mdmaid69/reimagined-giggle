dcd0cac43865bbf50c71b0377a81316e6db217ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:31:17 +0000

    wherever across bound argument circle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
