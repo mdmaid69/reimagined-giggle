8a7736223b851d09c15f66ad0da37d15089260e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:37:24 +0000

    quarter general comefrom smoke city

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
