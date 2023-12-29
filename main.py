8c04f57d7f94add7d3113aceebd67a3cc5dadf4e
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:04:33 +0000

    rent flight born allow possible

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def square_number(x):
-        return x**2
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
