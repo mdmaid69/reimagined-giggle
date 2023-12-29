bb6c540cb5e3ffa07e65021401d0bc375b6efe56
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:47:50 +0000

    board closed square undo talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def is_palindrome(s):
+        return s == s[::-1]
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
