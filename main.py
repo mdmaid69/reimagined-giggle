d18c715a6ce94d0844be6f7334eb27a4e6af4df2
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:28:23 +0000

    natural school transport match move

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
