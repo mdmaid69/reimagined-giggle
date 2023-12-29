99c7ecd356d3a9347eda9fcf8a58698cbaab91e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:41:48 +0000

    platform skill original zero necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
 import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_list(array):
+        return list(array)
