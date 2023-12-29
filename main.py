a28374a714a8a43d6015c75b74eae8f20512c2f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:54:44 +0000

    produce surprising mail fly sleep

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
