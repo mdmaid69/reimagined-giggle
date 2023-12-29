d277dab0ed2458449af5fb6b5c582a3400141330
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:35:00 +0000

    responsible pattern respect part introduce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_acceleration(speed, time):
+        return speed / time
