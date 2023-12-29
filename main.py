d1a90b67e1680355cae8f377d8b446e810227dba
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:37:34 +0000

    flavour war well limit public

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
