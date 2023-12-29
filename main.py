070b97ba063b81e3df7863327ea7daf33ef24f40
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:47:22 +0000

    scheme today custom skin independent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
