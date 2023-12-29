e0663eb6820cc5a441fb6cf873f1fc4a210e4dad
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:29:15 +0000

    deep within formal calloff post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_set(array):
+        return set(array)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
