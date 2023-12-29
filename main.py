5d5500cd4c7eb28e4d264af9132b9df34b4e2f34
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:02:39 +0000

    tonight herself whistle aware score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def clear_array(array):
+        array *= 0
