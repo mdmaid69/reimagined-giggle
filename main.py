6525710344cd70e50f35c2bf46aa1908e92310d1
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:47:12 +0000

    come through single live mine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
