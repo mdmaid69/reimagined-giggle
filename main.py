fc6c1a737807fed11af8665578c5fd0718ac7b12
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:53:33 +0000

    takeaway concerned smell bank usually

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
