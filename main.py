a181d9101b4616c0cc3124c52b494afd2fcb4b90
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:25:13 +0000

    care learn makeup disturbing divide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
