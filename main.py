2cf85a4ef032a0d2ebc98047118ae75e06b6f9f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:36:05 +0000

    trade wander worried chief trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
