4c792c9ba8ac500a23520485791ed1e7d5036b32
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:12:13 +0000

    slow common health bath division

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
