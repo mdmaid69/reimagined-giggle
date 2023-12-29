257fd40e1108c6a0aafc24bf715bdce3b035c639
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:54:34 +0000

    breakdown chance shall split show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
