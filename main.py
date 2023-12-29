99b4fbaa52c626b0388b8d7d6466d82527c4be0e
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:16:28 +0000

    disgusting but turnback to convince

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
