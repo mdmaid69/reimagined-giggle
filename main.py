1a6bdb30377f46dc24d2e48ebb67776f4f858e94
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:05:43 +0000

    relative in false smile passage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
