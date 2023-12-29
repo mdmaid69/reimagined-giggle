075c34b30a359d863d08351874b248988a3c649c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:50:53 +0000

    detail fortunately way history standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
