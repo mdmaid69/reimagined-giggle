a0fe7a72283d5026182fa308af42836b98781447
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:38:59 +0000

    way Mm chance smell goout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
