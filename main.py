ca7990c017c306c738e98dda2908a6f5a3e8857d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:37:39 +0000

    bury care fear music strange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_average(lst):
+        return sum(lst) / len(lst)
