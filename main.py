22dc6ee33f20ce1e7849a77a433be2f1859232a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:00:47 +0000

    car experience circle forever tap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
