2830108bef270bbac6d41a87180424d4f5a08e3c
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:41:49 +0000

    drink tap thirsty worse contrast

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
