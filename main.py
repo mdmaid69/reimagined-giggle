5934c6b0884539e3bf99d0b8d27b152c6735324b
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:38:27 +0000

    goback(to) congratulations miss protest phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
