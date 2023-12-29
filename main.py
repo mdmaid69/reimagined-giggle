c5085e97d9db508c63feaf66e5d81429ec8bc494
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:06:31 +0000

    quickly door method also near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_area_rectangle(l, w):
+        return l * w
