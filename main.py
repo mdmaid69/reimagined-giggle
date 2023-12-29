d60da19bf1c0180fc2008eee258ec6955186f04a
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:34:12 +0000

    shopping late clearly mistake send

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def reverse_string(s):
-        return s[::-1]
+def find_max(lst):
+        return max(lst)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
