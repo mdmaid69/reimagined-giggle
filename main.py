ebb280afa7c9233dedc6e965f5a7a59ca32f72cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:32:48 +0000

    spite hair suggest public rate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_as_list(array):
+        return list(array)
