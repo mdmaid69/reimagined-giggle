d431ad74aa6d1b054305061d37365ddcb7549c28
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:29:03 +0000

    work runout believe nose mail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_volume(length, width, height):
+        return length * width * height
