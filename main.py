f834bc729d420aa6cf1686255f14789701b34302
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:52:36 +0000

    issue sing moral usual swallow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_index(array, item):
+        return array.index(item)
