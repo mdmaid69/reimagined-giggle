deb333ba6fe0a902039db970672b9c0b968e7822
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:48:18 +0000

    total bus goon detail lead

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
