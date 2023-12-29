de2dc05cf252811206657d505e9efd840cab458c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:33:26 +0000

    from back model Hh Oo

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def add_numbers(a, b):
+        return a + b
