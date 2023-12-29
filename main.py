86ac62f1c64dccb087c57ba877902bd3a62993fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:06:41 +0000

    getoff noone extraordinary note top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
