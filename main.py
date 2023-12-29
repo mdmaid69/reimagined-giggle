47437fb3e56289308c5b510bd2e8be8f0d8e8192
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:25:19 +0000

    pair crowded bend workupto takeout

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def count_characters(sentence):
+        return len(sentence)
