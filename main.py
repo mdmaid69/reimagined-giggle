159a623bbae5f66e66b85116083be783bba518f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:56:53 +0000

    stamp either gather intend bath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def convert_to_hex(n):
+        return hex(n)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
