8e966c77ac2b26bce102a16449eec1b30974236f
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:35:50 +0000

    black press picture relief announce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import math
 def calculate_gamma_function(x):
         return math.gamma(x)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
