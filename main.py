f28bde06f6ba0f197ede9749f31a1707d32db9e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:46:11 +0000

    persuade agree hard goodbye noone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def square_number(x):
-        return x**2
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
