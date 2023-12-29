78268cabf4ae4aca2eec96acb1f4caf1803f659d
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:14:51 +0000

    567enemy obvious guess often leave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+print(re.match("h.*o", "hello world"))
+import json
+print(json.dumps({"name": "John", "age": 30}))
