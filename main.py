2534a9bc276afa4366d5bbf346a8c6d07e205bb3
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:25:34 +0000

    welcome feed there treat ball

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
 def calculate_work(force, distance):
         return force * distance
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
