5bd7385d59145c4e4e5a290bfa1bde7b81634216
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:09:33 +0000

    public trust action reply height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
