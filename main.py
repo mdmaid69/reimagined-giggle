e7f3c61217602fc352569cf0da743e1592e87544
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:02:04 +0000

    lead private surprised angry visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def get_array_as_int(array):
+        return int(array[0])
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
