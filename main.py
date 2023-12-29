bc73814b78ccaba0b93c778a592dceec655c5619
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:00:55 +0000

    stand pinch naturally cruelty object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
