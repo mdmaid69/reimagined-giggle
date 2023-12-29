84fc08fed21f1f0fdf97867c6ec41027d5919844
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:15:50 +0000

    wake breakthrough shape equal sport

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
