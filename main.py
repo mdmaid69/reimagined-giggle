ee051ca3f239c070f9a4effd8d8067816622bbba
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:02:23 +0000

    large trick Hh bad so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-print(sys.version)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
