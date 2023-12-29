78398605c3659a97a26e9a5d7f20e33bed8b6054
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:09:04 +0000

    lookaround call means throw air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import sys
+print(sys.version)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
