46a7e4cc0adea96608115aaccc846c8d358df4ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:14:21 +0000

    unpleasant shout delicate day excellent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
