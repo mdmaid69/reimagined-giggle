8b77c50904beb11d912f074fe3327dbaccbe322b
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:43:44 +0000

    suddenly without gentle nice wild

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
 def is_prime(n):
+        if n < 2: return False
         for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
+        if n % i == 0:
                 return False
         return True
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
