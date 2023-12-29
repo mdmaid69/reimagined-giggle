ed0ad704922a185821c7e3f692a91ec0af1ec3c1
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:00:16 +0000

    last range back finally bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
