a4375a9e67f24bfceae6edb3ffc9ad47de0e8a2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:08:18 +0000

    such behind decision mad round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def append_to_array(array, item):
-        array.append(item)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
