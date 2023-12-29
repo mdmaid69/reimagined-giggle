be722dbdd12b582425057462904a65ea1ae3adc9
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:31:57 +0000

    secret moral fault please your

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
