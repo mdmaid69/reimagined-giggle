65e0f24b565bd6d4d3481077d6dc4caf82eb2875
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:53:41 +0000

    crash protest transparent aim serve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
