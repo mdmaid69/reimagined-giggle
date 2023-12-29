033bb2855b073d4e2c3d1645e023529fd44ed215
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:49:06 +0000

    develop family cut dangerous friendly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def remove_duplicates(lst):
+        return list(set(lst))
