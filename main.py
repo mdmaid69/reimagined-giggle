24fdb13c89aa4a778286e893e9488de243d341ab
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:15:46 +0000

    Tt radio quit weekend keepup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def sort_list(lst):
-        return sorted(lst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import datetime
+def get_today_date():
+        return datetime.date.today()
