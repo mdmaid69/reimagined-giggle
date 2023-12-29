ee52d21d399493384cf079999d12de18a857ef56
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:15:32 +0000

    race priority fear inform exhibition

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def count_characters(sentence):
+        return len(sentence)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
