f6534469b9b8f371dce95dc8ea1b406d6f74b36c
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:45:54 +0000

    born bet ourselves sharp stupid

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
