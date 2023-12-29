8d6b9951261a0a5e010f0700a42aadf194f2410b
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:57:18 +0000

    office forget correct fasten put

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
