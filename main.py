e53d01fbf75afcc9e9e0577970965e1564a26099
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:00:01 +0000

    holdout manner mistake close interested

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_work(force, distance):
-        return force * distance
+for i in range(5):
+        print(i)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
