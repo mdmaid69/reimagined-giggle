541f6bf800436d6f4c4f6a0007d4bc68a77bf5f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:03:55 +0000

    appear tough bottom bang clever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
