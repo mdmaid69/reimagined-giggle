519738d821e5f33574ab5f18f8757c09ead987b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:58:15 +0000

    tear still secret practice cry

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import re
+print(re.match("h.*o", "hello world"))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
