e308a1ef058e40b41b0e30b863664377b8b7d478
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:34:08 +0000

    debt fight university instant finish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
