5d6e7a2183e25067f5a4864eca104363ff81b600
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:36:04 +0000

    solve brief must boil complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
