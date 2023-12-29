f6d46fa4c1c70c42124e275393b4b5dfa9cdce89
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:48:14 +0000

    worth stretch mean lie advise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
