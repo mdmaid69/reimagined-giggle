d80087964f4c5dc69cfc8ebcb351c9cd5263057f
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:52:50 +0000

    debt delight kick smile quickly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
