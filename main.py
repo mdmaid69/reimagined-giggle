67583a49ff1304b0355731e5fa535dd1b09b7ec0
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:07:07 +0000

    bet mystery offer we plant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
