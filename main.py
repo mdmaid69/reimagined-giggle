6baa31d44a2170a3eec1322ff3d5418933c27173
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:18:22 +0000

    light square tidy music pain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_pressure(force, area):
-        return force / area
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
