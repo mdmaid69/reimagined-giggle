161c9378da2f93c31ccc84a4ec42b799f72afacd
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:44:21 +0000

    contact burn birth let back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def add_numbers(x, y):
-        return x + y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
