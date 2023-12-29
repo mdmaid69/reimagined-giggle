09429bb4fc4ab65db7965bbe44843dbc6b31cceb
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:03:43 +0000

    fasten father confused support bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def find_max(lst):
-        return max(lst)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
