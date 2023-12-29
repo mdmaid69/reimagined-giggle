32e1be68a14b907b85f701faa12efb5f5979cdb6
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:23:20 +0000

    winter trap greeting wish conversation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
