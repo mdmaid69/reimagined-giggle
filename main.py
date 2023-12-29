77d75d90b72ac5b3d21211a5a3dfc34063c8731a
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:12:44 +0000

    closely sink tradition science maintain

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
