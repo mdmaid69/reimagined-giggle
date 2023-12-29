92d03ab626f96a83d8567e0b50bbedfc4c477e42
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:13:42 +0000

    cruelty acceptable memory but test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
