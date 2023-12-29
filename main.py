718db9f590dd4d7668b671d97f5734fbdf438281
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:50:28 +0000

    pronunciation degree question mind what

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
