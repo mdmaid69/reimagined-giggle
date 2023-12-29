cfc5bac8b8729c7404bc5b5fa4fc3cb4a99eefb0
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:44:14 +0000

    bang still blind cook concerning

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+i = 0
+while i < 5:
+        print(i)
+        i += 1
