b5c3f4e00f8f551e6a80a6259b011813c8ba8240
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:09:47 +0000

    spill emphasize close deliberately achieve

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
