574032eb9b54ac05a2ff8f3e4baebddc24cae2b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:34:26 +0000

    straight swim comeround apologizeapologise priority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def add_numbers(a, b):
-        return a + b
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
