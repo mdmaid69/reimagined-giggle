4a88f2affb09cd6feb378d20ae14ab2a3a36db23
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:18:30 +0000

    yourself confused steep after happen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
