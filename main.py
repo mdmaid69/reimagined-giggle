49c140d7e24d84a5a3a9cdc11322909ca3357f4a
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:33:23 +0000

    announce extraordinary as know put

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
