31f0b1d61d0538f48c2d370f30a788f720a00d08
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:55:24 +0000

    brother shout responsible immediate wild

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import sys
+def exit_program():
+        sys.exit()
