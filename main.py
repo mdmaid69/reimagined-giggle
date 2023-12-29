23a5929119adf5bec947bd5b135f25103562ed8f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:02:59 +0000

    hurry certainly ball light strength

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
