35fa8d81c10e71e13169090454df52da09e9dffa
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:30:27 +0000

    ready bag awake loan workon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import math
-print(math.pi)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
