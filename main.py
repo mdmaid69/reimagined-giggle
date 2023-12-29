5ccd537404e1b2992c7cc7ef79dda8e51f3cd8c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:24:43 +0000

    hungry bottom fire spoil dear

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
