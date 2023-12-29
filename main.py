4876fcd5206c1030035d880839cb89459d2ecd77
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:26:47 +0000

    lock now later improve stick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_power(work, time):
-        return work / time
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def get_array_typecode(array):
+        return array.typecode
