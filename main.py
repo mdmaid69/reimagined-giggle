16481fc9ba9dc6358b1c220bd0089b8106ada441
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:40:31 +0000

    silent programme less interview send

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
