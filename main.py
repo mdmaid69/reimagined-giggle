d7b55f30269b81c5d5d9af94b9d7ef34b8ffb245
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:44:26 +0000

    copy demand smoke once sign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
