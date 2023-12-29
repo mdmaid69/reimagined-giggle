fee280458948ac62a79cc632a37dd64499e01589
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:50:01 +0000

    confusing much perhaps disapprove summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import sys
+def exit_program():
+        sys.exit()
