89905c50950a2c5855e514ed617b364736e98da4
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:29:48 +0000

    lock on stretch visit earth

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
