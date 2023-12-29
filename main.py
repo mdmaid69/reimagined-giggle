aca54c2d986ddfa8d1bcbe8cb166022284664a41
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:37:23 +0000

    eye worry equipment rough proud

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
