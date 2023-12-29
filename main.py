1580ff8de7acb9d0bdbe6535b4d38ad93d7a6480
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:08:53 +0000

    cost combine themselves suggest spite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
