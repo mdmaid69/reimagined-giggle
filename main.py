d5a625ac098c107e8590574842e716a353dc68bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:17:07 +0000

    back point entry drink sink

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
