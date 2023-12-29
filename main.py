78a5767a7e924992ea498393ac857e48e7c2b04a
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:24:03 +0000

    miss near confused tell object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_int(array):
+        return int(array[0])
