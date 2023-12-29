7b8d16e7c17cff91804f7a20b7388945acaf17f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:09:12 +0000

    physical programme modern meaning disappointed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_force(mass, acceleration):
+        return mass * acceleration
