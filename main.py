4c5888d16951d81a7add3f3c77b7b2fb31501851
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:34:02 +0000

    back absolutely breakdown embarrassed arrival

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
