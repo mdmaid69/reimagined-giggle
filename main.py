eaec4e67257c14f2aede49fd45a67aeae54a663a
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:33:59 +0000

    there idea artificial everybody round

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
