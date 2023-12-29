20944acb716f29d1fc65f1f35d11bc1a723bf2e8
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:05:03 +0000

    left bang transport claim squash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
