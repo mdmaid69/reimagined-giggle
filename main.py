f30e973fc0bf44e2bc744207b374b2e22414ad78
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:56:51 +0000

    disappear steep scheme holiday any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
