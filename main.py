71d5047f7dbad4c233e16115e0e580a2c02b9ef5
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:32:35 +0000

    lonely well frame fly last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
