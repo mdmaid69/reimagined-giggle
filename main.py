926d1cb3dab3d48a326b7467b215f747391c217a
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:37:33 +0000

    lookup Ii accommodation foreign name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
