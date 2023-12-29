7801f8ea6b725238b28bf44746422dba7ace102a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:09:53 +0000

    maintain permission keepfrom draw swallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def count_words(sentence):
-        return len(sentence.split())
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
