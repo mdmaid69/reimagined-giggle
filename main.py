1d850f1c18a1a76bf1e0f567e48bca063ddbd108
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:21:42 +0000

    represent behave comeup plant exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
