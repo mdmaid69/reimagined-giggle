c50787a6effc7cb5e8d4b7495d84cd0bc52eca5d
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:08:24 +0000

    wrong wear taste quality clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
