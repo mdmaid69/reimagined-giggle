5c114cb36a698ab0658e4f4f342804be970f1430
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:40:44 +0000

    punishment world talk impressed return

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
