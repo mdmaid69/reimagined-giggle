5ea994b0cde99072392a4769d935f0d54c08e831
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:43:13 +0000

    praise check mess stop ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import sys
+def print_python_version():
+        return sys.version
