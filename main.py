e2f9a1dc13cec721e49217f140ddec0d1af33719
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:47:01 +0000

    unlikely account government unkind minute

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import numpy as np
-print(np.array([1, 2, 3]))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
