34fc46a51e3fbe95591c09aa45e72c9427ea239f
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:24:52 +0000

    union ring deaf wherever cap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
