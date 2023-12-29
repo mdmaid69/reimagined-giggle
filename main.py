3a74986dfb526831134d614b2959bb995966e259
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:50:40 +0000

    anything education recover computer far

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
