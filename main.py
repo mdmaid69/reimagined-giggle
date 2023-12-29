d62aad9e8086f66fe6c9841cdcc4c87509392a95
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:28:45 +0000

    last half defeat production medium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
