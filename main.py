a2b49eab1c93e8b4b90b4d69ff06bd7e624ac5e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:44:49 +0000

    official organization between breakup cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
