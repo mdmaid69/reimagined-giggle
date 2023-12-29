947abf0c1eebda50d08dcf1b48525034f1afe608
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:32:09 +0000

    unkind birth dead write vision

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
