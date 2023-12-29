7738ece2615743abffd4654c6e1838fc6b2e3e20
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:23:37 +0000

    finally science sail control exercise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import datetime
+print(datetime.datetime.now())
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
