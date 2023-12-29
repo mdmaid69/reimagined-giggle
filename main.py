ff51aed01335202cadfeb080f347b5c7643c619f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:14:32 +0000

    combine muscle later practical silent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def square_number(x):
-        return x**2
+def calculate_volume(length, width, height):
+        return length * width * height
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
