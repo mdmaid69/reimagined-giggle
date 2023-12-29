711d613b52303135148014adcffa864e72ca71d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:52:42 +0000

    tight obtain level attract shine

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def clear_array(array):
+        array *= 0
