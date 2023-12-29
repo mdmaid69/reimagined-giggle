41af11ab0779c834370694eeab2921218f97c616
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:54:13 +0000

    better summer brain visit tear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
 import sys
-def print_python_version():
-        return sys.version
+print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
