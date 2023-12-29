b71f9985268304ce150012990157b318152e4fdc
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:57:54 +0000

    step division scrap exciting treatment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
