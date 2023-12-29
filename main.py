21dec0677df404dbe651f759aef067b54bb2685b
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:23:14 +0000

    edge just but tap transport

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def get_array_typecode(array):
-        return array.typecode
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def convert_to_binary(n):
+        return bin(n)
