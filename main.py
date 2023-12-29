8d225ad84b96400e2cb6f7a3b026a17106edc8c0
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:27:47 +0000

    brother gather ask split various

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_item(array, i):
+        return array[i]
+  def convert_to_hex(n):
+        return hex(n)
