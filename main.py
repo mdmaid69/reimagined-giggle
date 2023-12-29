b8d9b102265a176a6b6db332fa52dc259ff593a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:21:10 +0000

    now foreign please comfort sensible

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def is_odd(n):
-        return n % 2 != 0
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import datetime
+print(datetime.datetime.now())
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
