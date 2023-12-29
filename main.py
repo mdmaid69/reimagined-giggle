167618de8effc1ab2733a8272dfb1c4af4a18f76
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:04:11 +0000

    lookafter skin remain just post

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
