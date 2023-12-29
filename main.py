fc3fa81d5a876df33e898d9246d43b12e147d793
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:27:10 +0000

    any sore previous sister drink

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print([x**2 for x in range(10)])
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
