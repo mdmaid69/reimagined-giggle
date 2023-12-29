f5ac4ca740575e25110ba550b404ed888bf872fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:21:38 +0000

    excuse fit only plenty mad

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import time
-print(time.time())
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
