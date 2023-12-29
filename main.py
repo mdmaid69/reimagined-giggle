0261a847566fa91e8a2584dc00777952cf8a8580
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:22:58 +0000

    ticket unkind lift foot progress

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def is_odd(n):
+        return n % 2 != 0
+def is_even(n):
+        return n % 2 == 0
