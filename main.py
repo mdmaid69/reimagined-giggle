09a88ceeab629d98e854d2de7232fd78cdc6c543
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:04:41 +0000

    today ashamed scream spoil carry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
