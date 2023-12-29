07613a9249c607c5b30d7069c0af7347dc28227a
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:01:20 +0000

    correct dry practice habit claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def cube_number(x):
+        return x**3
