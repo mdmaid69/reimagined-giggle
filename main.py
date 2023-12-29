79510831993e510bdf12e79ac9fd779a24e92b06
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:02:46 +0000

    responsible development receive bunch lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_as_set(array):
+        return set(array)
