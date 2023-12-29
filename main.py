e202b57aee97628cb8ddc4550d314eac1a797c68
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:15:12 +0000

    brain key callat throughout excuse

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_min(numbers):
+        return min(numbers)
+import os
+print(os.getcwd())
