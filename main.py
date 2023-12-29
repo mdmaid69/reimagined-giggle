1bc6771a54a2e417988b694e78b73de7a7f2fbc7
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:15:32 +0000

    result fit carry something hungry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
