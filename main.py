27d1d386a4804f06667fd9d7e79cb70280eeeced
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:32:11 +0000

    thorough ground his like mention

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
