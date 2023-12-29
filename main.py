2af04d681bc17bc3c0e9e0892fa004c42d8d8e8e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:40:43 +0000

    sensitive underneath lunch Gg sometimes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
