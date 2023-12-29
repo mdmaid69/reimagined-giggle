1c4eb9685e76b56253a288c33d74df87f8295f8f
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:37:22 +0000

    bunch comeout belong tour conversation

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
