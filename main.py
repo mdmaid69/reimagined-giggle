c4b0d2ac7cf854dcbf89bf5fcdabc3b4ea59c681
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:31:17 +0000

    content abroad about forgive tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def is_odd(n):
+        return n % 2 != 0
