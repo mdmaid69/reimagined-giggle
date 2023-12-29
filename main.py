fa63f07b8def2312ed170714d4a9d4ba33af18d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:05:54 +0000

    always unable more steer callout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import sys
-def exit_program():
-        sys.exit()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
