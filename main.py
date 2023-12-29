ac435481409954a788cd4059627dd722df63975e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:28:11 +0000

    spend share chief callat kick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
