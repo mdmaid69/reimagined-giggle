4d14a9862af13524fb7752bef45461ce6a54e5d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:03:42 +0000

    Ll plate giveback pipe slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
