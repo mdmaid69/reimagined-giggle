619a1d5c3c428bebc9c20f458a77a54aa86413ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:04:43 +0000

    tight suppose disappointed fear mysterious

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
