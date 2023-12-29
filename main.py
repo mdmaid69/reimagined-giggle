e6fe08e52741aa7c82c21e34adaa9a794b35fc5f
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:37:35 +0000

    evidence style fill before spread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
