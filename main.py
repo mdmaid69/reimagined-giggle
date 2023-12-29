b01f60edfefbfc567459d1e0e17b613576020e4c
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:26:28 +0000

    play kind place choose uncertain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
+def calculate_perpetuity(payment, rate):
+        return payment / rate
