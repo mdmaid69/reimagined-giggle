50e462813cc719f63a62583f1b5d301c46a71ee8
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:37:31 +0000

    opinion left correct design cheat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+print(re.match("h.*o", "hello world"))
+import collections
+def create_priority_queue():
+        return collections.deque()
