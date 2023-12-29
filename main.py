32abf33764b9acd4ad9af3e745d7b268687c4df4
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:25:22 +0000

    club serve character rough basic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
