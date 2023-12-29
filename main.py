f934984b119bbd3a59f607df746ce73b10decdd3
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:36:22 +0000

    direct holdon catch love other

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def calculate_work(force, distance):
-        return force * distance
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
