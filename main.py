6f2f2bc68cb6962085970456de56b6fdc630ba81
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:54:26 +0000

    bury everywhere couple lookup spite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_counter():
+        return collections.Counter()
