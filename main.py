e923943b4bcf7c4fd27d0b94b811540bccae3831
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:14:26 +0000

    happy shut ignore coffee gentle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
