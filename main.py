ae3b54568597e118f940872b05e2d0fe0c68cbb0
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:24:16 +0000

    sing reasonable thick talkback condition

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  def count_elements(lst):
-        return len(lst)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
