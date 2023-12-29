5e39a98446912c211ed8ae61272d1b5c984b69a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:42:07 +0000

    wild after speed lucky before

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
