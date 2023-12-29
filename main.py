a7c96805e5b82048d60b167813295872907d62c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:56:27 +0000

    use row new opponent alive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def calculate_area_circle(r):
+        return 3.14 * r**2
