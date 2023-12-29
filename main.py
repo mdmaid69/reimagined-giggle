feddf42197134c7a3e51727aaf8075b1a6230c1c
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:00:30 +0000

    respect small least hard edge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+print([x**2 for x in range(10)])
