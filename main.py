ff6ba554cca2f49dba38b46a7b8d0ed199768982
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:11:07 +0000

    lookback(on) beneath port constantly lift

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def get_array_item(array, i):
+        return array[i]
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
