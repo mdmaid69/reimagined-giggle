8e7ed1c727c006b46461fc7ef4189ad450c53318
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:38:51 +0000

    train complete fond pretty attach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_max(numbers):
-        return max(numbers)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
