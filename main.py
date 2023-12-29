ffb8e0b2f68393be0e5120233a178f41c5e47315
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:41:36 +0000

    history spend Kk cruelty ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
