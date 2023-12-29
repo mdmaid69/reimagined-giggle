f2a650b74bae75aab8f96b0ff6afc4c319437c66
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:30:56 +0000

    class collect clue display behind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
