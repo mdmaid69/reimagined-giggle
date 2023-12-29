53b22c17a411674e0ac44c22c43d70dbd4c8488a
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:33:29 +0000

    desperate promise clear board oil

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def create_queue():
+        return collections.deque()
