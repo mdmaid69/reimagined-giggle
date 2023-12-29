eb58b46a67a0c450575a3be59bcf4b3e4382b117
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:31:12 +0000

    lucky bottom sudden understand feed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
