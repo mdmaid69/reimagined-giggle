0991bd56524eacc42c9fd1c79bb45e948bb8f38b
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:02:16 +0000

    keepout legal copy where promise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
