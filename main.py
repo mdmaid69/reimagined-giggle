417cc5dafa9edfa54aea9ff2c0f6645b28664f45
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:14:56 +0000

    comeup offer admit famous bear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
