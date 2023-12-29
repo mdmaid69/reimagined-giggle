d1a90478cd3773937a290e93af7f7b4031e806fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:40:38 +0000

    not passenger separate judge kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
