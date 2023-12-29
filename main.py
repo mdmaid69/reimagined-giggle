d7f534f6388b2efb687ecb673403206a87b1471a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:36:14 +0000

    powerful between suggest giveaway sure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
