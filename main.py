dd0d7d44ed7186d2d5bebe3684c8b67ba50e0c0b
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:42:11 +0000

    sort bit new gas interfere

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
