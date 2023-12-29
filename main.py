0590daac245f7d9071caa61a73c1f6e366b31e51
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:40:18 +0000

    goon fond tongue pile wear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_area(radius):
+        return 3.14 * radius * radius
