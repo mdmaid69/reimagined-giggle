5a99a155ad001c6aa60c7d2695e692570b3c5bcd
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:01:19 +0000

    match term rob tune sad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
