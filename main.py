a9af8986bd88c7775441fb980c05ed0293c126ee
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:19:09 +0000

    tooth comein compare tap sore

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import os
+def remove_directory(path):
+        os.rmdir(path)
