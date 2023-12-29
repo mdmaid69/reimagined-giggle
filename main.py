af9009e34ff68a8875beae10d85eb02d46ffc011
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:59:29 +0000

    smile there poor properly fence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def square_number(x):
+        return x**2
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
