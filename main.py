f7ee657a310ecedf313fddebb7514f62b9987716
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:21:23 +0000

    565end prefer positive bin regard

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
