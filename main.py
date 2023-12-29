1bfd2937f92f967bfe68f1c581b5d5f52cd9d53b
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:47:05 +0000

    about proud too balance forever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
