d17224b17d85a32a2ccc0612cbe5a06e567e1b26
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:31:50 +0000

    ever habit Tt detail stamp

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
