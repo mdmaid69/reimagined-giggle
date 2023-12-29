ff7361b89723ee7ce82fe1daabcf42ffe6225501
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:45:56 +0000

    stop runover much trip common

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
