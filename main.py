2651f7378df70f892142f858defdb011a4245937
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:14:15 +0000

    formal before limit bill detail

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
