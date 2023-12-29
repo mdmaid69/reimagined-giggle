201a9a43843fdc25cc4ae82ff7ddebb2911031a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:31:57 +0000

    describe choose flow far draw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
