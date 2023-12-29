9af85c1e52c8283ea6c417ae809beb7e8f0242ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:39:46 +0000

    until round walk their walk

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
