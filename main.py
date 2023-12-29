fccfc63aa62740a2cc3a3932b66251c9eac094dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:39:05 +0000

    often glass maybe convenient no

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
