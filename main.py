013dc35cab7c056698a02c80db586ef08dd17465
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:41:33 +0000

    arrange activity except sleep concerned

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
