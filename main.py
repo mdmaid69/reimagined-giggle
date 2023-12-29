e6db5002b7f37611e901ee2027d13320d6ba9ad0
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:44:44 +0000

    insist people send put complicated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
