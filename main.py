ec1880ebe0f892904ed6d1110e35a4b41ec9bd50
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:43:24 +0000

    excited soak can accidental lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-name = "Python"
-print("Hello,", name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
