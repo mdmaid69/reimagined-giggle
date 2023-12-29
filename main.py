f48c9226781f79dba0de58d46f3ddf11d7936f29
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:32:37 +0000

    mention main embarrassed beat report

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def add_numbers(a, b):
-        return a + b
+def get_array_index(array, item):
+        return array.index(item)
