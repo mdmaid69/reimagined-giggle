3ad364781d04a85b77556382ca4fa8465f3c3f70
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:21:41 +0000

    free head down uncomfortable centimetre

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
   import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
-def calculate_work(force, distance):
-        return force * distance
