ed2ab1f756d1d695ed7e966793eeb30d7b5dce08
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:38:02 +0000

    once suppose street unable send

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_power(work, time):
-        return work / time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
