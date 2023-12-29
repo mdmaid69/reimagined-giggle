d85a3338bd84963be36e7f5ee2c2759a380ac2a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:11:03 +0000

    off card collection most advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import platform
-def get_python_version():
-        return platform.python_version()
+print(list(itertools.permutations([1, 2, 3])))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
