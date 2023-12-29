7fd7c4037ec210524f62b87e7493650a8b1d1bab
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:41:03 +0000

    frame consist plan measure mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
