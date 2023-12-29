3f0e5038610f5fe43d3ee824155ac79220a1f6bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:41:42 +0000

    further inside owe display appeal

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
