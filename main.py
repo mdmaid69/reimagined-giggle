f112962b2f86c662b1cc0c1d8929de84516c6aea
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:47:13 +0000

    light impatient front character before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
