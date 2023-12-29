22ea68232482e4a31ed311893acc00c231f52c4a
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:26:49 +0000

    style later objection question thumb

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
