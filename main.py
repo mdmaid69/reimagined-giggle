04edf5bf71fa27b16cdb5568ac87823372bf1f1f
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:16:45 +0000

    maybe road dry 179bite admit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
