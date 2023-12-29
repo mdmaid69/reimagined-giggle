b6d905ea64953bb61e0fb01c7fac886f2617ba2a
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:55:39 +0000

    regular cross contact bunch practical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
