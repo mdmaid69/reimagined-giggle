3707d30619bdac90cff75e27e390b1f0e6ec6bde
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:39:31 +0000

    choice organization comein within listen

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
