fd7daae010be3a30cababdbf5be499a636339bd7
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:01:24 +0000

    empty can steer limit dinner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_bool(array):
+        return bool(array)
+def is_odd(n):
+        return n % 2 != 0
