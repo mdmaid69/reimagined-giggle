babb0063ac4f79f8fb5f611540e081a63cd96715
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:13:31 +0000

    think pay actual each act

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.time()
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
