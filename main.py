b54e9fdd0c7fc536cbbaede2693626e02cd4eae9
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:43:53 +0000

    kind takeaway mouth block journey

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import platform
+def get_python_version():
+        return platform.python_version()
