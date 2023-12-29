8da52625736fc8467cbd162d82cda0761b09c4d0
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:39:18 +0000

    aim her fashion turnaway eye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
