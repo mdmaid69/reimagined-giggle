6c6f2cba163d441dc276c9c5035e05f52dc5c8cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:21:10 +0000

    Cc fork ignore powerful anything

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
