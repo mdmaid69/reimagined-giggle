66e4be4397ecf2c4ebc821edfed83017f748cb58
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:43:35 +0000

    mess gather before natural apply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
