d9bafce43e6a5be8acc407265070e4d1760fb913
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:00:41 +0000

    indeed Kk sick record abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
 import array
-def get_array_as_set(array):
-        return set(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
