ba04ba1b0616c75765977440a11911730d777e5a
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:26:30 +0000

    friendly window mother rubbish worry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
