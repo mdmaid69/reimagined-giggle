924415f35ef5e5e85f146c1ecd582360051a209c
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:34:57 +0000

    gentle test reason care backwards

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import os
-print(os.getcwd())
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
