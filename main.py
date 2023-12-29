6fff8746052cb8cea22a1a010f4e4a20be510e45
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:19:15 +0000

    rest modern passenger time excuse

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
