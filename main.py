2e9bb9a4310152b5dff1bc49848364e80c1d5bc6
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:03:50 +0000

    clear past mistake discovery profit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def convert_to_octal(n):
+        return oct(n)
