9a3b59a09d4d37e2185d0979cbc262008ff3b06d
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:16:22 +0000

    normal comeout pile except sky

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  def square_number(x):
+        return x**2
