51cf4b983f8fb18d50981e31aa0f8c6524ee5383
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:49:34 +0000

    dance her basic myself science

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_power(work, time):
+        return work / time
