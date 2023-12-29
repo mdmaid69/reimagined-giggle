97754682144a9b2d458e6a037c93ca67b185f271
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:39:54 +0000

    close recovery minute post edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_user_dict():
+        return collections.UserDict()
