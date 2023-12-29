e43dbb733c566a1e13a668bbc0e827e54efc34b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:18:12 +0000

    same maximum entry task black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
