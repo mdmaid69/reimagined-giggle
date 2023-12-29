6550f41b648841c9c477b28542930d43cebe1d8d
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:09:45 +0000

    gooff lay tradition crash valuable

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
