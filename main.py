7f27e8e03a208351ced9e9ee1dc259ae6109171f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:00:07 +0000

    separate attitude level young custom

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
