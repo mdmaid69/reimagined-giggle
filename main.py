027ab633543379692dc445d4fa5063b5e4ace9e3
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:40:51 +0000

    Vv cruel thankyou deserve completely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
