551ff15c2cd055591f5dda820dff6bbf354d5645
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:45:04 +0000

    expensive university pollution next comeround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
