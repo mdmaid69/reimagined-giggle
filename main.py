5ee4a7be4b5d1a42caa08ce0389cd47a07f0e6f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:10:09 +0000

    sweep pity legal Rr sudden

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-print(sum(range(10)))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
