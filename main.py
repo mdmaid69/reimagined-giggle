3951ba3ecf0574ecfd411103d4a9636062601618
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:02:59 +0000

    pride exercise sport unusual professional

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
