adbb658c619c15bf337dfd72ad7df01f1d2085bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:08:09 +0000

    means tune flat learn knowledge

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
