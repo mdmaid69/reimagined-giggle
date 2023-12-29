ea64ad6923f3fd5964df85fb094f23b91c87d288
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:36:59 +0000

    cinema fire afford strength sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
