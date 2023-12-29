dd380776a109d17a826511f5ca4314f2c5eff2ca
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:36:19 +0000

    mind pleasant big careful concerning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
