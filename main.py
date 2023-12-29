e687421dc8218d502c39a9f73cca5da28a92fcd6
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:53:35 +0000

    sharp bringforward absolutely book himself

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def square_number(x):
-        return x**2
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
