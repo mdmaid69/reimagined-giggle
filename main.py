bd77977fff3a755edb773e7fdc56e9a70db91458
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:33:17 +0000

    duty trust by getaway attention

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def convert_to_binary(n):
+        return bin(n)
