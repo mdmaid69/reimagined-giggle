cb3185074cb9e6a04b6fe49791630d0b70425ba9
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:58:59 +0000

    broad block kick sweep thirsty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
