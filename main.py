452be29a38889bec50a3d7810fa38b0e9f7d8633
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:54:09 +0000

    runout match ball murder record

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
