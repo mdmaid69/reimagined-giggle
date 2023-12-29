4ba8fb33faa7a583b8497baa32305b9c0e1de96f
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:34:48 +0000

    apologizeapologise soft direct breakinto play

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
