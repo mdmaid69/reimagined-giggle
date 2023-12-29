d93080ca8ab5e061fe17ea61717a0c965304e3b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:07:05 +0000

    sail so shout avoid rate

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
