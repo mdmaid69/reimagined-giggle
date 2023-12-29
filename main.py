700283fd6dfaae683dd8b377013fbef98425b88b
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:45:34 +0000

    ride bad night out as

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
