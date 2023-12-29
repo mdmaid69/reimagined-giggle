2b5d4d3ee21469dc92dc0bed8990f9f8f242b7ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:48:19 +0000

    swimming arrive office herself safe

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  def convert_to_binary(n):
-        return bin(n)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  def cube_number(x):
+        return x**3
