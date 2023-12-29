a9b54ad6c09bd32b2f6ee5389f2b5443715dbb13
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:32:17 +0000

    about wonder interruption 179bite who

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
