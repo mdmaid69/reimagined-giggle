aefa90dbda4018213551b3d58722cc3f2c85ebd6
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:55:14 +0000

    take there silent laugh warning

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
