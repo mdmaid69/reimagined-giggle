ae75c9c17e6f46fe62c31b66b0af6c75a89af3a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:32:14 +0000

    government Bb maximum produce joke

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
