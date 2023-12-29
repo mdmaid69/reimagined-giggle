3e8aec22a8746d19e909afa1c2594ba149129731
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:17:19 +0000

    instrument threaten early confuse science

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
