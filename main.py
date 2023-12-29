6f64df1f8389d2b295c4572682dd093574b72be8
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:51:51 +0000

    couple flow slice exercise brother

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(10): print(i)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
