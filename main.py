1fc595b3606ea8d09da9234812546e15c19c8040
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:45:40 +0000

    story interest love hear their

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def reverse_list(lst):
+        return lst[::-1]
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
