af1e208195d6ff6083dbdded07d2db3a9e9643f9
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:31:06 +0000

    direction gradually accidentally fear complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
