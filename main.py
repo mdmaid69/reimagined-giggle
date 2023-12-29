752265da752b0cac526b31b609ba3c270565d67f
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:47:56 +0000

    brush square criticize mark joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def find_max(numbers):
-        return max(numbers)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
