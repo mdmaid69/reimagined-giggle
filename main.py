5a8d3d33b5c5a6f68698f2577c47212853476eab
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:09:04 +0000

    half stiff morning smile no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
