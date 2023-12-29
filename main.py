7e994e086d0bd0438f7dee92cbdc1bd1d32f4cbf
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:54:49 +0000

    bringup within some there artificial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
