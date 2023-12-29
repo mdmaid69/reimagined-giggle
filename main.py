c7e92c33c365087b5c80b1d0e480355bfd9a2894
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:52:20 +0000

    herself disgusting lunch flavour pinch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  def sort_list(lst):
-        return sorted(lst)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
