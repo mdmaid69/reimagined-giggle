9c9cdede83696848c55e546d342d79a63b197929
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:48:49 +0000

    pool creature sit treat stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
