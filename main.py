2d9c5f9e0449ca0862ec5630fd44d7e7bb1c8fa0
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:11:25 +0000

    569enjoy wash demonstrate thirsty positive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
