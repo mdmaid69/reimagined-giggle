22ebf91df1227f5a1a4403f3e8eda2fc3ef9dc72
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:19:11 +0000

    every bar miss today car

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
