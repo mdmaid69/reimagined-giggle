999eddf0e43825123a721ef58748d45e845b75c8
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:31:22 +0000

    on hit evening regularly worry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
