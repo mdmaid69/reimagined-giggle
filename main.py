9936f8f0f80fac609069c628fba4f4b5106bcede
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:53:29 +0000

    importance fond second able glass

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
