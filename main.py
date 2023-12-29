8c3abe55bd3c18a34bcf8348ff2018236028a765
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:02:19 +0000

    offence feel poison operate interesting

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def set_array_item(array, i, item):
+        array[i] = item
