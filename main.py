221785789fc0cb5281bc9004cd9d6b209f37fb47
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:37:40 +0000

    operation exchange especially minor now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
