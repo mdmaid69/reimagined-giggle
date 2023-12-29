836247fe0d1f6632111788e3518031a70f307351
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:34:51 +0000

    personal weak branch second news

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import random
   def generate_random_number(start, end):
         return random.randint(start, end)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
