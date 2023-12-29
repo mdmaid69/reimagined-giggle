2c0c8564011cd3fd4591a225cfd2006013b85631
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:55:21 +0000

    Rr thought declaration Ww height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
