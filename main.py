bf8e8cad7eb7180dcca17c66e961eb400415bd00
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:34:14 +0000

    describe criticism contrast silent send

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
