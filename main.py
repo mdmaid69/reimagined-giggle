1581d113d71a0738ffbeb6544961bec43f8f5e51
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:46:34 +0000

    ourselves skirt turnoff phone underneath

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
