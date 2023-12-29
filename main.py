11a279c064b786db3ad479abfbd65ca8aea4035c
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:07:49 +0000

    connection go experiment government Yy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
