004ff9b4da3805b4b7d3deac64906065f6995322
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:51:16 +0000

    chat practical slowly tradition side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
