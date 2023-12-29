89ef4b0d2e93addc5f9103a44812d7bf081649ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:06:53 +0000

    soon brown free settle land

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
