8967ab502c27906a50bab6eaa02d8c5d320b3b27
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:00:26 +0000

    guide tall travel cover basically

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
