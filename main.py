af8a3a9c9d88d19b14b9fe2d08c4cfa194d6a91f
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:44:30 +0000

    weather turnon quarter dry meaning

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
