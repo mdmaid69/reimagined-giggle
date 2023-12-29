c220e5e67adcae792c8854359ce390df95873355
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:40:46 +0000

    lead damp bright character even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_queue():
+        return collections.deque()
