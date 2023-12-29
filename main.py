50dea878ad28842029268b1cfeecddb7f6a99e2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:34:37 +0000

    mysterious advantage comeover mess main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def count_words(sentence):
+        return len(sentence.split())
