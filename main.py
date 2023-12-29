0ff7a6f5d51760786209f71e53b6720fdc752a81
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:26:38 +0000

    conduct confused fill range lay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def remove_duplicates(lst):
+        return list(set(lst))
