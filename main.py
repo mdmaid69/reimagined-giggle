7f2b99d24358e681eabca05f9ec100f46640486d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:19:35 +0000

    regularly similar row Gg skin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
