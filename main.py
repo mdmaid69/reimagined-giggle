11317d45c51ca5eef9a29823723874a8ad022a49
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:21:52 +0000

    moon flight somebody guide bank

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
