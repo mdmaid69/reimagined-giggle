f3c44f74011824a158640e396a8cf3d6e20ab9d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:15:34 +0000

    thinkof shake closely boil fit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
