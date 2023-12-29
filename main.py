15fbff8c57ad0fc0712e4aaf728c46b63e7c623e
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:39:16 +0000

    stop see supply concerned quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
