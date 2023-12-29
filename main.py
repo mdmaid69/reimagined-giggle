9d7f29be06de1d698e9a9a078c57efd0db9ce9ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:40:57 +0000

    thanks sweep fence recover translate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
