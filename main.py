65a6c1e7ae795aeb10835d8f0cdcd3aebc2ddd69
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:40:19 +0000

    example scrap sky basically sound

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-for i in range(5):
-        print(i)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
