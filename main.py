8605a739b82f2210383e2c794652b3b2dc6b88c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:55:41 +0000

    loose attractive production handle squash

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
