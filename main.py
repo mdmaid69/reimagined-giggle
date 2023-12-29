d7034b6160f0577a2a9313e7d224997978afd4d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:39:08 +0000

    close flat possibility secret funny

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_pressure(force, area):
-        return force / area
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
