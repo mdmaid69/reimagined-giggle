c1fef0f8ac98eaf68d06ab65e55db4cd1adbd3b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:33:58 +0000

    hollow female clue song agree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
