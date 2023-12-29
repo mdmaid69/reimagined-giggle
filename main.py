5ae9766a198f7d582eabac3b7e809413b55d66fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:57:41 +0000

    name stretch creature suddenly work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
