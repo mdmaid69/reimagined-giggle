0475755dee755d2e2fcdb22fd261c1e8717430c0
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:11:35 +0000

    negative ourselves pressure cut wise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
