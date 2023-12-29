dc2f5a414c828a9e578ecf1b3e03616a1984cdd3
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:25:44 +0000

    matter shout influence attract rob

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import random
+print(random.randint(0, 100))
