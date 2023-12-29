5b2d39e41c522f93d10fd7f275646321401c3582
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:47:36 +0000

    birthday pattern surprise finally usual

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-for i in range(5):
-        print(i)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_power(work, time):
+        return work / time
