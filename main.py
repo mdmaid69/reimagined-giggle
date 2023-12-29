b5c8e6aa85239a632c3991619765806cfb35c483
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:33:12 +0000

    bang announce female demand stone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def find_max(numbers):
-        return max(numbers)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
