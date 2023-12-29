b87aadb0c4fe1b81db038313dd5c92c933383dc7
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:50:42 +0000

    really train boil achievement level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
