4545be4eeb33c2ff056467e1c674e6251e435840
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:56:19 +0000

    cook relative passage first charge

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
