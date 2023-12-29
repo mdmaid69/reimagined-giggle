a1317443e85fd984c039f19ff78dcce1caeab136
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:06:27 +0000

    occasion great deliberate order moral

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  def convert_to_hex(n):
-        return hex(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
