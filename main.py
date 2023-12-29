a315a32ea317d0cacffd2d3c02b82093801f113a
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:31:39 +0000

    far fortunate plain great stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+for i in range(5):
+        print(i)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
