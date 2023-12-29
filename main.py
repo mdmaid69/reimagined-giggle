4e30be316cd80a174f51b3988a292d46df92141a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:19:38 +0000

    bank Ii sick confused personal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
