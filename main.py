92492805a7674e74ac9c87b1f21b142e068563b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:36:57 +0000

    ring dead inform draw anxious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import tensorflow as tf
+print(tf.__version__)
+  def sort_list(lst):
+        return sorted(lst)
