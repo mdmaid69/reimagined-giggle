f8a19e26daa3b69202b5f0377e689aef5327ecc5
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:06:24 +0000

    earn arrangement regret cut imagination

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_current_directory():
+        return os.getcwd()
+def calculate_speed(distance, time):
+        return distance / time
