547ccf88f70b4b1ab56ed1f6e20b9159790fce51
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:58:19 +0000

    turninto closed little obtain plan

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
