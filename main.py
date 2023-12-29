e220da9cfa6c099e3c9d8693e8ad7e0fe67f8c40
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:37:56 +0000

    use lead else bang identify

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
