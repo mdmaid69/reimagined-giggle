c7dbba46aef783400b0863e8af9910acbf96cf7f
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:09:54 +0000

    bottom light simple hard protect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
