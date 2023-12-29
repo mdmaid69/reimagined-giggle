ea9a856a29e22abb23a81335c44ef828163bf6cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:08:58 +0000

    arrival week wind culture now

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-print(sum(range(10)))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
