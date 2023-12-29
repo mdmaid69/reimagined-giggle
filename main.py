492cb2b40a195b998c8ea89973ce6876186c3b59
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:23:02 +0000

    emphasize impressive example neither wrap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
