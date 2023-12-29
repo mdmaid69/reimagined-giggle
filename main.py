1d554d0ea114ae1f44d06a51a65fd602e2eba8b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:55:27 +0000

    divide politics rank holiday worse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
