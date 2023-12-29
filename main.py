d2485112d4c0dcd283a75170ead6ada7569dbae5
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:12:10 +0000

    limit hold present estimate vanish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
