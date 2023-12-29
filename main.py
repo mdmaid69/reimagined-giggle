5ef2c045d9856409086e75dff283ce09811ce6bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:04:22 +0000

    brush Vv thinkof pupil shame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+print(re.match("h.*o", "hello world"))
