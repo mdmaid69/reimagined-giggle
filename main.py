ca2538d20fd3fdb52bb7ce339d9906b93d20ffd9
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:50:50 +0000

    tongue payment bush draw talkback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-print("Hello, world!")
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
