010d9017f02f0870281576ea7580c46d5fc244dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:59:16 +0000

    by address far snow label

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
