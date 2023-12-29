744b7848fcf317fdb0df06f74310a13092419f22
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:13:48 +0000

    watch square hit degree comfort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
