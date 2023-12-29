731a15227b97e5afbf539666c442f2c0f305c207
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:27:24 +0000

    shape zero declaration rank gradually

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import re
+print(re.match("h.*o", "hello world"))
+def count_characters(sentence):
+        return len(sentence)
