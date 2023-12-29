88c147d2e2981b97b89cb68911baead3834178a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:34:31 +0000

    greeting concern join complain thinkover

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def square_number(x):
-        return x**2
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_array_itemsize(array):
+        return array.itemsize
