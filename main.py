f6f2c829ad4c659a7042bd8fc722dd6158960a06
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:32:27 +0000

    throughout considering pool join search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-print(math.pi)
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
