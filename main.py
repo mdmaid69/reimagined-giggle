2cbcc77f88106f7e551c175c2850a8b7013678b4
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:24:05 +0000

    so person spring exhibition possible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
