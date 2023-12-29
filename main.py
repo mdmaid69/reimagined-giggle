a4e9338880e21a343506082803dadf223653acf0
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:24:39 +0000

    until flood welcome vote greeting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
