9bd886b3aff7d1aae962238b89c5a39e88fe8b9c
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:14:44 +0000

    stir limit tradition spring soil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_pressure(force, area):
+        return force / area
