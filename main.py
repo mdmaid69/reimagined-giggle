cb4357cab8081b69fef82564c479687aaf215f8a
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:40:01 +0000

    drink hold date live relax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
