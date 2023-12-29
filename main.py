7de85ac90ee647cb0d1740950fa1c4a12305fd92
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:40:55 +0000

    fear argument circle situation downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
