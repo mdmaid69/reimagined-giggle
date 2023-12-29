280bb0d6da3ca15c75ccca6a2e769696ff8bf7fc
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:22:02 +0000

    jump art very used moon

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
