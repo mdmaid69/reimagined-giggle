ab6d35dd3fc163e37b336a200f695c306cd6f828
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:13:47 +0000

    again complicated unable tip step

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import collections
+def create_user_string():
+        return collections.UserString()
