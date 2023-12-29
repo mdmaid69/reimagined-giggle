c562c8d076c7639023e84af6d7e3ec6585bdba48
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:58:58 +0000

    crash circle regret loss thanks

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
