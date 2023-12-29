8b9ab25d0bc2c6f7a8f81c6ecc280fc9b6099230
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:04:20 +0000

    buy stone top us to

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
