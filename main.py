1ac31256f255c8732620555feff550704ddc3b74
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:29:00 +0000

    voice deep politics character prove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
