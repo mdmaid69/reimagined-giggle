7d701d97763141207d0fa686db934cf327d32885
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:50:26 +0000

    usual bang learn delicate yet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def is_odd(n):
+        return n % 2 != 0
