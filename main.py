03de4bd9d97b5c6af54f2c00166de67667a697b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:58:47 +0000

    name 570enjoyable row honest offence

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
