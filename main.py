f045c193c0c302af985fd0b9d829d6084ee89f67
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:33:45 +0000

    excuse pinch 568energy problem treat

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
