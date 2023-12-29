3db11bef614b8ad9f84f834cab38e1e5ce5d1c45
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:03:33 +0000

    tidy Pp scale prepare translate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
