bafefbc1a1c5eb8e571ff4ad72eb40bdaf555dc2
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:38:31 +0000

    arrival doubt breakin colour glass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def create_numpy_array(lst):
+        return np.array(lst)
+def find_unique_words(sentence):
+        return set(sentence.split())
