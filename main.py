5ecc55c4d164dc2873b8cb55b1c3c297827f2af2
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:57:46 +0000

    tone release ill get present

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print(sum(range(10)))
