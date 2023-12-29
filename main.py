f731dda2bce11b778fe58ffac4ad2fbad3c703ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:58:01 +0000

    degree organization demand hour effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_buffer_info(array):
+        return array.buffer_info()
