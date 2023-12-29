9e118fa9320a459092a8a792e366b542e9ba5ad6
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:03:03 +0000

    polite cover else bush later

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
