a86e66d3e5ee77989d2824b57464b875b84a9291
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:47:21 +0000

    complicated similar free term border

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def reverse_list(lst):
-        return lst[::-1]
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
