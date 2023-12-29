4bae49ae4c1b3ad3486d177371cc4c4d1339c655
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:15:03 +0000

    later mysterious cost intelligent scrap

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
