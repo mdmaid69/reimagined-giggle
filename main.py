4726c4ee27e30511db9066265deced8ce8276fcc
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:54:05 +0000

    couple excuse fire forward ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
