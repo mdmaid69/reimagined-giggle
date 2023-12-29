72db4bd372dcb436c6b922a61b8e2fb15bc3f02b
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:47:59 +0000

    sit short shout term bread

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_list(array):
-        return list(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def find_unique_words(sentence):
+        return set(sentence.split())
