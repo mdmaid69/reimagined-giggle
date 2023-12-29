5c1142b6184ed1528307c45822aefa0421d4f4dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:56:05 +0000

    little brother generally completely the

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
