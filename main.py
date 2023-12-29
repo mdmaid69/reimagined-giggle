c9d9e911f80b75c4fbf47b3c70bbcf042e816ba8
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:18:34 +0000

    sweep obvious worry warm arrangement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
