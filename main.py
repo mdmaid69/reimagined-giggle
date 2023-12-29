64f588de264c965c5f6471d55fc3ff8681b302b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:39:49 +0000

    visit walk some separate address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
   def get_file_birthtime(file_name):
         return os.stat(file_name).st_birthtime
