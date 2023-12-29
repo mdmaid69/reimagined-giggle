9d1ecad89bb6eab0ccd8f6c683804f6667c74187
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:34:13 +0000

    substance container dig splash word

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
