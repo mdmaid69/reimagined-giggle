fb6145263df7478dc08dfe5b95b4b7b7277172a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:28:44 +0000

    present steam normal sound back

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import sqlite3
   def execute_sql_query(connection, query):
         cursor = connection.cursor()
         cursor.execute(query)
         return cursor.fetchall()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
