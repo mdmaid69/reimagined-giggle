5a1d331902ce4070eaa97ca001842a622670c05a
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:13:09 +0000

    freedom otherwise declare own talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
