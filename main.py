ab7899772a0d9b89265ffe92f8a048a359bf2a3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:34:06 +0000

    do hold warning die risk

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def is_odd(n):
+        return n % 2 != 0
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
