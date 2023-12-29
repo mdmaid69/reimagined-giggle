69fa43841e7f63c9235f54acf6f6fbe294bf54d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:46:47 +0000

    feeling Ee worth careful minute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_str(array):
+        return str(array)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
