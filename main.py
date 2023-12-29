bbf7408a4f0ed3597b8ee9218c4a5952b22cbf75
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:22:23 +0000

    birthday dangerous her exaggerate married

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
