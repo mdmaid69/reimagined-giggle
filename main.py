62ccfb681b3eeec212fd03880af766377a5d3c20
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:56:51 +0000

    step laugh live control dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def convert_to_binary(n):
-        return bin(n)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
