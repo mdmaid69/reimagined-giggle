57b77491b2498b949cd9b64f2df9eca4c85398f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:12:30 +0000

    solve goback(to) total race bad

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
