a0c9fffb56dba2a26dbc703f3a9294a4dea43de8
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:16:58 +0000

    touch guilty message average runaway

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def calculate_area_rectangle(l, w):
+        return l * w
