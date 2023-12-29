dca4ef85255da7152b835e520fb466ee9544188a
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:29:48 +0000

    murder wander nothing hard smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
