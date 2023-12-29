0eae210587ea4d9d9ee56a936de0cb46874cc83c
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:07:52 +0000

    hide bin expression rich community

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def convert_to_octal(n):
+        return oct(n)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
