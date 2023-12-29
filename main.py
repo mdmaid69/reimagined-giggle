f3ee1bd5849f20c1f1becdd3563d984329bdc496
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:25:31 +0000

    whole underneath weak which soft

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def add_numbers(x, y):
-        return x + y
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
