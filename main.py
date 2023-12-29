65ec3b13c02c4abc1ece0c6b397b7f191aaab481
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:42:48 +0000

    this tear feel mood work

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
