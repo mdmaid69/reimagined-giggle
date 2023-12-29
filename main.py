73447c456e50312b4754a51e29e6a81832ad25e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:23:33 +0000

    day country onto closely explanation

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
