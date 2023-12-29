57beef5ec4b7c1bebe9393d18ce52ecedd676dd8
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:32:15 +0000

    by whenever cheap slowly protection

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
