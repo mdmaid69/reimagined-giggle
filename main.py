35c0d6fdfc40d4c86db5680865bcc3a4cf981912
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:09:04 +0000

    runinto tax convincing careless say

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import numpy as np
   def calculate_standard_deviation(arr):
         return np.std(arr)
