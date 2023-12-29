8e8eb5521c0ced282b758422be07f82dd917afd3
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:10:48 +0000

    equal equal distance mind together

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
