8c9c7abbca8aa9bfd89e177e417ee67fbc69f990
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:57:26 +0000

    charge complain side summer actual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
