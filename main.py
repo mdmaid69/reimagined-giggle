a37f2eace9e3808d91a3de11a76e08488835d8fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:34:16 +0000

    sorry nor join rank Rr

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def multiply_numbers(x, y):
+        return x * y
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
