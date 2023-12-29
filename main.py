304eeafe1b5dccc048f8b6ebebde3387915bd1ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:39:01 +0000

    finger work react idea thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
