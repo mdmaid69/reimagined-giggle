bda025f352c500581c8aec234a3aee5541649e6e
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:45:06 +0000

    physical stranger sensible break dear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def reverse_list(lst):
+        return lst[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
