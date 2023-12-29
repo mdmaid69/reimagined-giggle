5d91bdf830434fe542be55275341216b26355d51
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:49:43 +0000

    single off behaviour influence takeaway

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
