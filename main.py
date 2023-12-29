9ed63bb6edc8eb5a2971dbb208a852a535c29a8a
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:30:37 +0000

    introduce mail slightly letter trust

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
