4aed727a399c9afc02ae83cb516a36712c039cc5
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:08:35 +0000

    represent enthusiastic environment dare answer

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import random
+print(random.randint(0, 100))
