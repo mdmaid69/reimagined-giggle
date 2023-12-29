e570d5ac59a6a04451df96b24d7348ff2272df99
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:29:07 +0000

    birthday perhaps university subject form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
