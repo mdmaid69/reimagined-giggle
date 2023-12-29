459998b6fb12fb0156033f5afe74180e9d600a55
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:09:13 +0000

    spring count question Oo tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
