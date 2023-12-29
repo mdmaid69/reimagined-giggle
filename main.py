e82188f9220fc3a44ef7fe734d390cca53c6c05a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:47:48 +0000

    plastic defend trap cope content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_ceiling(x):
+        return math.ceil(x)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
