eac9887efb7daa8ffb47464eac9f9c5f958df9f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:48:38 +0000

    frequent sleep already flavour priority

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def is_odd(n):
+        return n % 2 != 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
