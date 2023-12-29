059ec36a467adb274e54ba8b77ff547d32ded2a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:15:53 +0000

    force thinkthrough cruel warn open

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def reverse_list(lst):
+        return lst[::-1]
