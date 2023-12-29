a7929761e2ab6e16f8cac6e0a890c3572843802e
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:07:57 +0000

    how need suppose partner letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_set(array):
+        return set(array)
