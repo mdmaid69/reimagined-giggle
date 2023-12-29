b52d27489ef8628ca7c00d023f7af4f4dd853009
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:57:57 +0000

    future whenever sticky definite land

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
