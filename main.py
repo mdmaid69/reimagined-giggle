05cbce86815932f54a9e1de375c8e22046a3f6cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:17:12 +0000

    concerning thumb copy not shock

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
