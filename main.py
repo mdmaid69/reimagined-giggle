c60ed278cf0fda30265fbc1326c4edfdb948681c
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:10:56 +0000

    record blame breakfast their likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_as_str(array):
+        return str(array)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
