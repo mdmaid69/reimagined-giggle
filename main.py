035c5c4cbd0531984781bd87b238fbba8d7a3ed4
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:11:19 +0000

    contain draw burn can lunch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def find_min(lst):
-        return min(lst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
