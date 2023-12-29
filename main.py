32f8827ca15548da65bb5d4a00add016b02f40d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:18:45 +0000

    achieve choose dull bar turnout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def multiply_numbers(x, y):
+        return x * y
