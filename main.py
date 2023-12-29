cdfd887e3c4c12a2f7738c0a57df8d36732ad73b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:01:15 +0000

    holdon freedom priority goodbye train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
