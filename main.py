e17e08023e6dd8a32f791679a1ae1417b3675c31
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:52:55 +0000

    to fire raw swimming main

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
