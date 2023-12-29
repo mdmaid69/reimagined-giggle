257f606b26f4e108de6d7ad4651f596ed5a199bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:12:04 +0000

    beat destroy forward range stamp

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
