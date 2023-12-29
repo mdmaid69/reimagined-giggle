80dae095cdd305c18ceb9da83063949be2538959
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:34:11 +0000

    afraid wind complete entry live

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def subtract_numbers(x, y):
+        return x - y
+def find_unique_words(sentence):
+        return set(sentence.split())
