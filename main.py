e4ca614141c808052047a800928aa47d855944af
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:25:43 +0000

    fast odd runout achieve base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
