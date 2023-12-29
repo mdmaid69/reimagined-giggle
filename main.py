441c9c8a56869ede255085063794bb48d814e91c
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:09:15 +0000

    combine remove country confusing fence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_cosine(x):
+        return math.cos(x)
