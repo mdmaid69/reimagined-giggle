87ca4c59c35983ccafded651552c1d22f53a846c
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:02:01 +0000

    hers consult best noone entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_as_set(array):
-        return set(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
