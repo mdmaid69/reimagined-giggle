0e58a2e523b45555495a0525380cd840e049861e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:39:23 +0000

    step workupto 573entertainment various hardly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
