ddb9d8704e6cd41958409dc4448660af70f4f2b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:34:04 +0000

    means exaggerate bad old comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
