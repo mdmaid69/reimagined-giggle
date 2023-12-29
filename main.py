51b472f0783566d406c146273e6ae4f30fabdebd
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:39:20 +0000

    file remark culture shadow majority

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def square_number(x):
+        return x**2
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
