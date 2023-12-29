ef56f861554ed55ac74cf08b605efd47b4184432
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:43:39 +0000

    important rough level skirt equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def find_max(lst):
+        return max(lst)
