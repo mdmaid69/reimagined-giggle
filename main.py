9206643b635d5a06c7ed7894eb40b60dbf85e161
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:34:37 +0000

    own he odd whoever square

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
