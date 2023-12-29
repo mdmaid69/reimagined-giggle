3f510cf4dc085a255a83a6988be102423419e872
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:27:50 +0000

    family sorry sea roll lunch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
