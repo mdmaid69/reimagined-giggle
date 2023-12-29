3cd120b834710cc3258263da7d873ee7a84ff417
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:50:25 +0000

    official runinto goback(to) Bb point

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
