74f9b0df8a0690a36e3c31bd326032a5d99812f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:22:54 +0000

    body takeout surprise various female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
