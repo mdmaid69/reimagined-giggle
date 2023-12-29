536b5a1d69c07768148ca48a19cf32319cd52bbd
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:01:58 +0000

    fire between himself away ill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
