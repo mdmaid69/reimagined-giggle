32cc424fe2544e6b32b2151753cc5ce7b3ca938f
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:29:14 +0000

    noone collection connected better zero

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
