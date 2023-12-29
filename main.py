ef030fd5a76834d2f7aec3ea565640b5669ebe70
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:21:18 +0000

    today shrink without closed immediately

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import datetime
-print(datetime.datetime.now())
+def get_list_from_array(array):
+        return array.tolist()
