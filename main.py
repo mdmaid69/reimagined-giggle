c7795eb262eb9ee7c926c2314f942aa0b13b9033
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:51:55 +0000

    effort constantly leave moment 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def is_odd(n):
-        return n % 2 != 0
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
