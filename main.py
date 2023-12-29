a6958310245e9e1464ec5be15fff889cb7ec8495
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:46:35 +0000

    term at escape fast black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def add_numbers(a, b):
+        return a + b
+import math
+def calculate_floor(x):
+        return math.floor(x)
