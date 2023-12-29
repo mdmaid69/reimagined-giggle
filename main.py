4612e06165ea2e9ca2b4d6d82fa2d0b1dd7fb961
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:54:05 +0000

    trip them man exhibition bringup

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_array_as_float(array):
+        return float(array[0])
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
