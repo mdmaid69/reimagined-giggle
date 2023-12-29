95d1e41cb6f63f96ac501cb66ea8a11030664ed4
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:14:29 +0000

    myself geton region thorough deep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import re
-print(re.match("h.*o", "hello world"))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
