212d6c67419df02af69b964dec8868ed5dda63f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:24:01 +0000

    beyond meaning moment floor complicated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
