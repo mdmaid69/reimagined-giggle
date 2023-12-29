4706e2c7f0b091ed7740d8676ff1acc77f83a7be
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:37:03 +0000

    turnover tie in exit him

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
