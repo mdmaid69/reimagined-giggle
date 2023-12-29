89d16ec3c796baa008fb7c0d57b8e60897e7003d
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:36:21 +0000

    computer run takeout simple explanation

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
