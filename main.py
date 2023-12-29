7844ce702b50fb9dcace7d9fd4a1a69fa3a11bb0
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:31:33 +0000

    control leave throughout lunch because

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
