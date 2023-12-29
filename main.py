3af45127077a2dd9ce0f6b03ee0cd1c539164932
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:05:58 +0000

    beautiful charge church university family

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_hex(n):
+        return hex(n)
