38e79791b465d31c3f270d9378d58cabb2b682b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:57:29 +0000

    it ball birth part sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
