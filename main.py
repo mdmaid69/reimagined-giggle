530e652fd7a8ba94553837aa00e7287bf7e5b395
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:20:09 +0000

    workout each rate stupid ground

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
