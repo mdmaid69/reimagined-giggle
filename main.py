e0fbab9cd0c9d9e31db5b0ae148e2187e9eaf3a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:32:40 +0000

    continue holiday weigh from entry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def reverse_list(lst):
-        return lst[::-1]
+for i in range(5):
+        print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
