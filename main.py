84703c0ca893a1ae5a56959efc3062289d35f35b
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:06:10 +0000

    sick business run thinkover fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def get_array_as_set(array):
-        return set(array)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
