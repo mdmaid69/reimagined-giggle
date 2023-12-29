a7fcaadb933a47d4d604c4d9a32795f904803255
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:05:23 +0000

    quality married hurt worry 566end

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_area_rectangle(l, w):
-        return l * w
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def square_number(x):
+        return x**2
