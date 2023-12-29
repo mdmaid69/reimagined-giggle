d0ebc7ab81c1afd9bf91c834e2d8ea6d8c562b2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:37:52 +0000

    treat comefrom plastic protect food

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
