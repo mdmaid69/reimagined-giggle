7c46b7f9f86da24d917b10b911e91e8933f62c71
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:30:22 +0000

    polite shallow side who reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_directory():
+        return os.getcwd()
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
