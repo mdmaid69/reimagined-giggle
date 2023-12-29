3e437c34219be6ea6d577163dd7e7b8f5c6c3993
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:53:56 +0000

    right language any arrive bit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
