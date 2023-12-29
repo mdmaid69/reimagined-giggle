3cf4b9964f6ff81d21df09c5271117ad256172f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:09:52 +0000

    typical control do must honest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
