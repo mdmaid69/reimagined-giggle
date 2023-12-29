b9ddd3385af73fee413bf92382737cb1a5605dba
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:21:49 +0000

    true wide bang visit extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_max(lst):
+        return max(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
