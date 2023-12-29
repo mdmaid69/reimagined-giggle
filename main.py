828d0ec7c8123f7679d96a6816032870f34b7763
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:48:44 +0000

    advice freedom book stare square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
