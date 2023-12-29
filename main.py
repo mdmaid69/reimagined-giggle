fb6f16cae476626eefb06977d1fb212edc3b2c92
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:50:53 +0000

    miss handle cross then mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
