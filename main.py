c08d02ff292c2dce0d2f4efa94b853bfcf1db4c5
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:48:25 +0000

    innocent conduct rather 571enough close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
