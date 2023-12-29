faa9e1b6bdd30d11ebefe48a5df27bf162907a92
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:59:27 +0000

    there shopping plain suffer sell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import collections
-def create_user_dict():
-        return collections.UserDict()
-import datetime
-print(datetime.datetime.now())
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
