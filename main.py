3d4ba24e44ef9a206569016c1378deaedb86aec8
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:56:26 +0000

    accommodation shout hurt service everyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
