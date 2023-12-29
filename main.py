2ee00b967635525e922069f3746f83c30b88c99d
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:04:43 +0000

    confused convincing port among phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+for i in range(5):
+        print(i)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
