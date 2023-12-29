0c8bbc8e291688ace1416f3e46694ebfbbb5f292
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:49:04 +0000

    cut along but shall full

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_string_from_array(array):
+        return array.tobytes()
