c0afd7f0b95622f6555c191506bed1495f8ca019
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:27:04 +0000

    difference steady important oil scrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import sys
-print(sys.version)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
