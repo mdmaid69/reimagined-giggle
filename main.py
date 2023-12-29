f374aafbbe6c91bc6cca1ce5f48dd7c94b25d5f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:33:22 +0000

    neat please maintain decide next

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
 def calculate_work(force, distance):
         return force * distance
+import collections
+def create_priority_queue():
+        return collections.deque()
