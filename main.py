5294c0e320ef59278bf9752ff23fa42b7d5a2de0
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:10:25 +0000

    rain live friend grow spill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
