ec73b09c6a77a70464b450ef9bd789d344ce249f
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:33:05 +0000

    turndown gift reach way yours

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
