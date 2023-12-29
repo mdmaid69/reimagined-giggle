2d7cf7a6c33962f43b92cd22a515cda5c598779e
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:52:37 +0000

    goout gather bit increase 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
