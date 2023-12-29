4976c480cd15173c7d041101660084144e3906fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:17:48 +0000

    comment run fat talkoutof schedule

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
