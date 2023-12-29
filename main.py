4a561a84a42edf53ddd1b84998e034f144c049a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:07:07 +0000

    step cut mind form surface

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_pressure(force, area):
-        return force / area
+def calculate_circle_area(radius):
+        return math.pi * radius**2
