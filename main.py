8ad0dbaaef642120044a9be360e52741f798089d
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:27:42 +0000

    music stand main fall importance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
