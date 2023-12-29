640246cdabd918afa02e82ac593cef1728341f30
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:22:03 +0000

    well difficult experiment apologizeapologise tread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
