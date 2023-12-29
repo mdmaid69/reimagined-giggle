5308c146545d8fa3a4fcbd36c7b40d7d49a909ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:41:21 +0000

    emergency refer split use increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
