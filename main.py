0a8c312a537c0b2c6cfc1c5fec371f94f19ed1fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:59:39 +0000

    principle system fix turninto today

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
