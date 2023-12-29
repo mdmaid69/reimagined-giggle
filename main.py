19921ce7472178fa266d5afbd752d85006f5d13a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:39:25 +0000

    lookback(on) pause willing serve try

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
