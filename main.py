9b2d49869e72e59cb26762afd784a4824899a67a
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:16:56 +0000

    trick up discover challenge pay

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
