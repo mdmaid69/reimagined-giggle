8d49346dcb927d52a2c7f03f1c6c33de58190510
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:39:03 +0000

    colour interrupt direction uncertain effort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
