bda841a59e95466138fe27514820bec13c8293e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:20:27 +0000

    drawing dress fairly centimetre song

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
