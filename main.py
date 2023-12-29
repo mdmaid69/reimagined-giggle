eee9c32b9a1762c6d250c33352570814ade287a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:03:07 +0000

    from open strange comein production

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
