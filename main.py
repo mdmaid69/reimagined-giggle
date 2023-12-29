07c92c34c147a92970de124af992b5f784f1c39c
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:00:28 +0000

    deep see be theirs medium

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def clear_array(array):
-        array *= 0
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
