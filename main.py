0ce0b1cdd122915f2405abd3341bcb5b28fd52bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:11:48 +0000

    pretty common tip platform scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
