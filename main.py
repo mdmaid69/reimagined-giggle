18b422a964713693bb5f17dfd89f8fa434678d30
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:03:41 +0000

    almost whether weigh criticize interrupt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
