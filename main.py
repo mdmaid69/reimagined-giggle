1cd85c37204ba511ecc20203b2baa8e2e679d082
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:40:30 +0000

    pocket shut her recently large

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_power(work, time):
+        return work / time
