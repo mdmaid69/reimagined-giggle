980903bf2290df38eb694d706dc3880e320ffd3a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:22:35 +0000

    mainly affect little we impressed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_directory():
+import os
+def get_current_working_directory():
         return os.getcwd()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+for i in range(5):
+        print(i)
