9e54c32514c7ba49d7ce9bc13fc145a7d2bd2807
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:17:05 +0000

    callback straight pencil side straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
