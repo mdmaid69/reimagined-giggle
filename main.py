b7afcfccae90193f1502392cb7f2d18ff2e39422
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:22:21 +0000

    foreign pain switch deliberate down

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
