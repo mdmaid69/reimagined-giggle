aa7bf5d9b9fb20ababc95536b426d20c5e0084ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:31:19 +0000

    advise split meaning approve as

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def sort_numbers(numbers):
+        return sorted(numbers)
