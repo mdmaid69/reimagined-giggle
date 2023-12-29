5dc9969248bc7381c52644545a877b01cbbb7583
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:08:53 +0000

    character benefit too eager regular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
