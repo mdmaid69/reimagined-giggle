1c54a080f897faed682426975be0975e02560065
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:27:14 +0000

    speak behave catch mind trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_even(n):
+        return n % 2 == 0
