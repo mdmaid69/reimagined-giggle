3783987928f9f19111e4a509091e1dfac91c70f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:51:18 +0000

    term ticket private imagination harm

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def add_numbers(a, b):
-        return a + b
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
