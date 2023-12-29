8d72e7ecc39404a352d7753e7ac06bef9504d994
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:29:07 +0000

    complicated ought persuade noisy pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-for i in range(5):
-        print(i)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
