fb230ee311cdf1d5eff6755a7ef820c1c9a08a8f
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:11:44 +0000

    concerning 564encourage heart wander takeout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
