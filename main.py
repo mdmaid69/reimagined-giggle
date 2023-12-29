55c6b9793760326c9b8ba8ca623ec98b67ddd2a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:34:10 +0000

    start sight job open press

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def is_prime(n):
+def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
+                if n % i == 0:
+                return False
         return True
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
