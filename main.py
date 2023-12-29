ced585d066234120b069b0e0ee80be8133439ea7
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:09:37 +0000

    rubbish since treat body awake

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
