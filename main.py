1305b3f6227726f49887a393c751bc9adc0f4aa0
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:58:00 +0000

    ever nor goout remove failure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_current_directory():
+        return os.getcwd()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
