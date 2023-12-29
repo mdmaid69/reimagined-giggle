4681b3da3f73955c0025526c982b343fca18214d
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:50:24 +0000

    thanks possession weak married authority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import collections
-def create_stack():
-        return collections.deque()
-def square_number(x):
-        return x**2
+def create_counter():
+        return collections.Counter()
