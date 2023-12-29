3dc1b9846fc6754751dfb82e74bd6e2e22460ed7
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:45:35 +0000

    explanation gap battery operation makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
