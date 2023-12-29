dd93e9c0e309615e47f5c1a446bbd34bf18249b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:44:10 +0000

    listenfor split law bottom party

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def convert_to_hex(n):
+        return hex(n)
