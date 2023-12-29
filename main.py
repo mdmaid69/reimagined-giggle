99f79538ad2ca9ee4a450622acdea68f5bf42561
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:13:44 +0000

    confused think approve journey round

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def remove_duplicates(lst):
+        return list(set(lst))
