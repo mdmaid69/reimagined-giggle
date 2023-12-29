b066a1660fd129149ecea5b71ddcc58a149f0382
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:44:29 +0000

    even no seem shut above

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
