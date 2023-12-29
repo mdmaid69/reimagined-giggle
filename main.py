610058a605fb651378ec6987b29561587ca5ac36
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:04:08 +0000

    happen peaceful beat practise same

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-for i in range(5):
-        print(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
