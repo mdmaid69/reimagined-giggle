1ab6b4fd1c77425cd3fee1a4c2522958918d7ef5
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:41:03 +0000

    mention wipe circle match fat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
