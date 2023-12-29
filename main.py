15bb423ad0e4f42e2a00a8de9413e72ff34ce99d
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:42:51 +0000

    spring bar century back back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
