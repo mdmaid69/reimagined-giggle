0d81086a389daf0b109f0c09a6bbb9769a185f4a
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:21:34 +0000

    classroom smooth nose entry sing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import getpass
-def get_username():
-        return getpass.getuser()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
