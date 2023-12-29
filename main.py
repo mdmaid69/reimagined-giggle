22ddd5947b7e13f70d1530947b48f8376339dd7f
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:34:45 +0000

    turnback experiment difficult team cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
