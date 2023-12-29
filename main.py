f6e8a1ddd8e126645dcd194e9c047ea41c21b091
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:40:09 +0000

    similar miss comein far condition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def convert_to_octal(n):
+        return oct(n)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
