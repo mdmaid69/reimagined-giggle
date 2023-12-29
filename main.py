75d29d97815ab626ac4f90c73ac5d58d834de5e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:36:03 +0000

    pump least quick mistake tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
