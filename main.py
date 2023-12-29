6206dee483203e07f3cd003e130bff75d5ed5cd2
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:38:09 +0000

    keepoff off most feature dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
