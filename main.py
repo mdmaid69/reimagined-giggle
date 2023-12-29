046fa680d3cf26114eac869d5e99e62448c56996
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:54:32 +0000

    form wild interested search study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_as_set(array):
+        return set(array)
