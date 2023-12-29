ae3d155bddc23731fe790b2c923101852122fc1b
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:42:56 +0000

    offer cough represent badly appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
