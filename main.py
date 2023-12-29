faed3cc23be33c6f7ebcea257b3df0edd66db755
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:42:09 +0000

    second walk crisis among sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
