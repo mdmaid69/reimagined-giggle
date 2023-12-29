15699e05a7b1f6c7ebbfe75e823a7970fc05626e
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:20:03 +0000

    material track thinkthrough question wet

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-  def sort_list(lst):
-        return sorted(lst)
-print([x**2 for x in range(10)])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
