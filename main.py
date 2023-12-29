ad0dae51935271868b9ed41292e80f957379194a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:42:38 +0000

    surprised door arrive shoot interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
 import datetime
-print(datetime.datetime.now())
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def get_current_datetime():
+        return datetime.datetime.now()
