a9ba768cf52abd095c585e023652e7f2d802105e
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:38:42 +0000

    turninto joke criticize left interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_stack():
+        return collections.deque()
