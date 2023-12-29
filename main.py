0e327589de259ec7141de9bb512b3ed4c92eaa29
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:42:58 +0000

    unlikely us negative lookback(on) face

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def create_stack():
+        return collections.deque()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
