1a59001392d9353dc9e06cc100aa849499beacb3
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:46:10 +0000

    programme though pair child pity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
