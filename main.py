fd2c80aa9839f97cb8e41f2bea0c9b09e667ca96
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:04:55 +0000

    with rely ocean attitude sure

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
