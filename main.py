49698d80178f45a13151434127bfa7f99c3d2048
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:53:55 +0000

    pure height thing minority yourself

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
