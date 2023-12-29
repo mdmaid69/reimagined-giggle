992222873683fd66a3c4610dc4c43b03fe980e5a
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:03:48 +0000

    trust regret unable paper question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_speed(distance, time):
-        return distance / time
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
