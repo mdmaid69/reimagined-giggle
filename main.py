cec19bf693358ec8f287f9cf68d95fa416378e05
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:32:33 +0000

    consult nigga personal short much

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def count_elements(lst):
+        return len(lst)
