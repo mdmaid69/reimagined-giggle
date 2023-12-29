e08eb43d876f8b8d69f83b0b17034faaa3c86a5e
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:33:26 +0000

    welcome fear holdout interested label

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
