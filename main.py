3da9d70488133bd0e53475ee538b597944f53f93
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:33:40 +0000

    ignore offend gas total achievement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
