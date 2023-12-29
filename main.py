e7ec902c0d89c91e5d5719091127ad2525db58c1
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:23:34 +0000

    lock however special healthy meeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
