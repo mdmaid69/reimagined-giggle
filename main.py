b2ece510ba2e4da8a70560ae5be17e547614b095
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:49:38 +0000

    proof ahead whichever exist somehow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_max(numbers):
-        return max(numbers)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
