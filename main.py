cc12e6bfd388e2b49ed6203942721ad961743eda
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:11:21 +0000

    dear increase instead round Cc

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
