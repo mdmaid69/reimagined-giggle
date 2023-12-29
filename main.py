3db17d4a1d5595717eb25e819927dcbe704e1c7f
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:02:24 +0000

    they waste prepare undo week

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import sys
-def exit_program():
-        sys.exit()
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
