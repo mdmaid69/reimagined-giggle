f168142b6924208f28c75714e30f6fc1649c6b29
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:08:15 +0000

    react let me such immediate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import datetime
-print(datetime.datetime.now())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
