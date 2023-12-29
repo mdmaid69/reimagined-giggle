c3a53d338eed2ea57406ecd976fdc3e1dbee3263
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:15:30 +0000

    his disturb afternoon place upstairs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
