ce7781e7f3348ed5aca545467c7bdf6f363bbfff
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:29:15 +0000

    healthy decision holdon first worried

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
