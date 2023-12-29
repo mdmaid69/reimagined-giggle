ddaa0e7b7f32c2462bc46a2299728f444e708b76
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:24:54 +0000

    everyone common who congratulate flat

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
