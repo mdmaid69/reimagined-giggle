7b266c763f9502ffb869aa95188b134601d2823c
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:25:11 +0000

    Vv constantly pull narrow careful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import os
-print(os.getcwd())
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
