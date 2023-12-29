fb5305e2b942a837fc347806e58428c675f3e613
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:09:44 +0000

    attention means container sail wise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
