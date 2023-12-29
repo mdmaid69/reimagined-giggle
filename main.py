0b0931c3727d73e216a9a4bb799b8d4d0693cffc
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:08:26 +0000

    properly mess no close cruel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
