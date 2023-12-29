f2308469cf1fb84ab27ff5ab5e5776b208398b23
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:39:57 +0000

    scream apart influence bury score

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def divide_numbers(x, y):
-        return x / y
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
