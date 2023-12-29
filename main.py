7b28dad5da0457c8b70d1488e9d61a22bb0c8119
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:22:09 +0000

    small bright wise patient damage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def multiply_numbers(x, y):
+        return x * y
