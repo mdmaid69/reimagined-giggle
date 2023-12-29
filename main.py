4682bc39b5cfc265ca18df0b36a9856156547be3
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:57:14 +0000

    loose demand cut everywhere runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
