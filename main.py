648d38922da892463a4c83ddcecab1dc5b34f336
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:30:24 +0000

    soil radio press hit top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
