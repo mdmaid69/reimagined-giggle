03baa3abafe55fdc85e15072e4132fa82e5fd770
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:03:19 +0000

    seriously declare rarely allow patient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area_circle(r):
+        return 3.14 * r**2
