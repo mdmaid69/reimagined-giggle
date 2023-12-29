9d5534f316579e1bbd2f3e412489e17acaabfa27
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:43:49 +0000

    type noise gift party finally

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
