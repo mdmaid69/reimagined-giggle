f49b2458699a7373662c809ee7bd154ef2bf4808
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:15:36 +0000

    target turnback exaggerate accordingto shine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_acceleration(speed, time):
+        return speed / time
