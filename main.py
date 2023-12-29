08bb9bede9d86920f4b645fa79b73d6d7bcbc0f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:56:19 +0000

    straight possession way apparently plastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def reverse_array(array):
+        array.reverse()
