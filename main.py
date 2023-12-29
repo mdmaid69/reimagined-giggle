1843e010e54650d3abc55079bde591500d6808fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:57:27 +0000

    brown at touch expensive spend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_density(mass, volume):
+        return mass / volume
