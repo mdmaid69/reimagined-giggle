6470c5f4647c52f864211ecfa65730705b11d320
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:21:55 +0000

    birth turn hope abroad powerful

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
