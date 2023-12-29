b91c6523f8f0345156fa317d5d659df009aaea87
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:29:34 +0000

    his makeup eye history active

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
