ebee71f509dbae1bac1ce948b9ee0c73d609dafa
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:56:57 +0000

    right forward proper takeover tune

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
