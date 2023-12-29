fc26c7f5c615c38f3d8aafe029ba92fc4e3e90df
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:30:58 +0000

    effect discussion contrast address lean

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
