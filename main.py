988ed86d230ff536bd7fd87c82892b81fa2058dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:11:37 +0000

    accidental succeed mail Ee situation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
