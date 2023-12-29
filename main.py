4c4a9a0c0b847d2b4222c63b61b90b25a916ae18
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:09:04 +0000

    oil greeting tip everything accidentally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
