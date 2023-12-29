180101eaee3f5af3b025647a9ae67f58eb9e74ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:05:49 +0000

    Rr violent chase hand swim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
