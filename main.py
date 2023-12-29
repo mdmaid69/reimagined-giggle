46d8dce642dfd4cd5891a0c48b4288318def8a5f
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:12:10 +0000

    section better moment thorough repair

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
