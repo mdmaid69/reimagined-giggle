0af8fe892edf0dc4957123c357b5b210d3b4d554
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:37:57 +0000

    record city extraordinary rest young

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
