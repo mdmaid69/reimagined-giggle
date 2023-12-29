57091bb531b09d8b76f4cde632724faf1d25db85
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:40:53 +0000

    admit connection century bag town

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
