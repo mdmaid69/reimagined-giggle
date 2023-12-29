3c2b3fd2814e32598dbd2f49083fdf45d830697d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:24:37 +0000

    scrap regard brother kill get

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
