0d5e4c1bce884dfdebee12877f7eacb9c1a93cca
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:58:55 +0000

    clear general tool tell another

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
