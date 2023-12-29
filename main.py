ea74a9cd8a4cd6c945e824c57271e9e16f9c364b
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:52:53 +0000

    temper giveback away important standard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def sort_list(lst):
+        return sorted(lst)
