fd02b339e8c63654eea57a32912c308b31483302
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:22:35 +0000

    past well brother trousers own

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
