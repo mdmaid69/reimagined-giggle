00233646f58cb7a7ccfa4e2fd971399d78d1ccc8
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:42:36 +0000

    violence here hurry vote meet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
