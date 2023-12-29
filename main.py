cf7b88c73d47274d4188e9a468a0cc428aed1fc2
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:13:20 +0000

    hospital present tap yourself mark

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def multiply_numbers(x, y):
+        return x * y
