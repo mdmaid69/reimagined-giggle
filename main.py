006150810465db5eff7b4079840ba07c02df3e15
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:03:26 +0000

    often peace drop watch appeal

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_sine(x):
+        return math.sin(x)
