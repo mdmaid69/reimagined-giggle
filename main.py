bc3742249c28cfca0330eb9445a02ebb35eb620e
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:20:48 +0000

    window warning broad wipe bother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
