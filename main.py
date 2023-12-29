6e49900fa9f3eb8d2f65966ee18ffeaee9c9cb85
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:37:32 +0000

    bridge rain Cc past tomorrow

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
