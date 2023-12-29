441a649fb64c57679f24ce0747410a9fe9cb42e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:33:28 +0000

    not back hole little measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
