fff2d9e896c05421595ea41f5369bc05f2087dcc
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:29:14 +0000

    constantly study once even stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
