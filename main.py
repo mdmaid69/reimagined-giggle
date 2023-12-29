2976a5b2e51f948b78154b956999264cd3860a04
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:49:49 +0000

    decision proud yesterday while piece

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def create_user_list():
+        return collections.UserList()
+import collections
+def create_user_list():
+        return collections.UserList()
