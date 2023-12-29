71590348aae10573491d0b6f997dd229565afdaf
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:29:37 +0000

    trick part sad to sting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import array
+def get_array_index(array, item):
+        return array.index(item)
