4693b405e91cd660869d2aa00949141426130bb3
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:24:13 +0000

    179bite city tend word difference

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
