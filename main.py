ff0a84a95e68e81645626bced8d2ffb931bd81db
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:10:25 +0000

    effect gather curious divide genuine

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_power(work, time):
+        return work / time
