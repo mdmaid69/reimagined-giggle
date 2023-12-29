ee743730a425eb0e2e6dee607705bd5e65463722
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:29:41 +0000

    attach computer cinema weak considering

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
