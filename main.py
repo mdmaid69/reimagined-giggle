dd81914067e3f497921c9068eb33864cf5e90893
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:29:13 +0000

    Mm lookup special matter prefer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def find_unique_words(sentence):
+        return set(sentence.split())
