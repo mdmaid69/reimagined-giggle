d62038a1eee7bf9ea54ca3c5a444b927b176ba32
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:56:14 +0000

    soil press look term balance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
