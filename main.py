db2cc7c6bbb2500c23010655601a387400237626
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:30:22 +0000

    difficult identify giveback on niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
