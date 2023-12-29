819c0a8d5180b31be38b0d877ac3affdd374a7d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:30:28 +0000

    she idea rule stamp hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
