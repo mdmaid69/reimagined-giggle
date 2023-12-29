657352e9eeeabb246443f2d2bb13ec5216ed945b
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:05:17 +0000

    term education brother borrow level

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+x = 10
+y = 20
+print("Sum:", x + y)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
