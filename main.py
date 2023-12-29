7017666e04d00cef07e5ad298ee153de59dfda05
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:36:55 +0000

    land quarter afterwards drink destroy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
