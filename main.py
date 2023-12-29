0f0b2b890a6e5f7c001d3a41be583cf1095a49a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:25:17 +0000

    safe cut gate little information

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
