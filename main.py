c8ada2586493e8b1650e13cd18f32f534bf97b5c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:01:07 +0000

    war ear smile basically when

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
