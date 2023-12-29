f815329aa20ddf6444f83271d3a73abfeab69481
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:54:20 +0000

    stay noisy slight time reduce

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import os
-print(os.getcwd())
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_array_to_list(array):
+        return array.tolist()
