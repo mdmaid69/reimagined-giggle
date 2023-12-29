644f15da29b918fcc35b48bb368495fe21948b2d
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:58:06 +0000

    treat lucky street dull gift

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
