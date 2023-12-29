524c4309dff3d80715694b805d0b00f09a01e087
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:34:51 +0000

    basis bend variety rare coffee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
