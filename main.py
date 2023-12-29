2f5d84f65e1c851b2762e4f1e4cab6153645f9de
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:44:58 +0000

    strike include everyone crisis operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def get_array_as_str(array):
+        return str(array)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
