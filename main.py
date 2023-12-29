2ad9466a4a08f278a022cbaf05c03792006f9318
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:57:51 +0000

    pot less her write wander

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
