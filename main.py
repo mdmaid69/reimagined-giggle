9aeab8920aaf31e89d03b2dd974e119f4fe82dc8
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:06:17 +0000

    authority transparent course cost push

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-print(sum(range(10)))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
