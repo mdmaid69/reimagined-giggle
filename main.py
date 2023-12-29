0a8187b9cbada28a057055283d39cd8a3deab808
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:48:16 +0000

    test when company might compare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  def convert_to_octal(n):
-        return oct(n)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  def remove_duplicates(lst):
+        return list(set(lst))
