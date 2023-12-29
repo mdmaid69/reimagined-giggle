106b968ee4c4f6fe1fa23b97f29e6e64fe0a0ad1
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:53:27 +0000

    practice respect dangerous slow unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_area_rectangle(l, w):
+        return l * w
