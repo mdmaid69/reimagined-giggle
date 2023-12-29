55ac7c4f19caea1bc633691c0f1a99c516f5b357
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:08:12 +0000

    opposite pleasant special so goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_bool(array):
+        return bool(array)
