7b8e5a5494c141e487931a199f27461bcc84075d
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:24:23 +0000

    advantage you reaction have run

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
