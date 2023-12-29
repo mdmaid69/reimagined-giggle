be4d95d6e06b6fc48e4718b9ea2e43741a058aa9
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:12:39 +0000

    now street Oo road tread

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def reverse_list(lst):
-        return lst[::-1]
+for i in range(10): print(i)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
