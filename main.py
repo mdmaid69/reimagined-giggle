340ae95360246f06e7046fd25410d2df153fa10e
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:04:37 +0000

    interrupt theirs fond tough there

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
