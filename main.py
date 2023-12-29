8ddd654721c995f247090c3650f3ea9657c3d7e1
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:40:46 +0000

    give bye nearly cry everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_typecode(array):
+        return array.typecode
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
