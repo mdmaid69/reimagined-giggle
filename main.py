213d82c99403b2a2cb2cad585809c2070d4b1320
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:04:36 +0000

    supply turnoff idea keepaway father

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
