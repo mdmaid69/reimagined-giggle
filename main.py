84056f2e9a1b9ea64e289d79543b26988fb5ac5c
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:52:07 +0000

    slice dress swallow thank find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
