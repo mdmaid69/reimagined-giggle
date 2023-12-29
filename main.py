3e57c4a1489c24db38f2b2b6f913e76600cdef6c
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:45:37 +0000

    reserve question sweet hurry arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
