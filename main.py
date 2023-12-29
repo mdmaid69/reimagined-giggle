f91764a4c77a5bbe0d2ba33d710ba2bbd07cd5f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:30:56 +0000

    proper hand dream hole convinced

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+for i in range(10): print(i)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
