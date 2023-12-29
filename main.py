8885849f038dcf3782caddcf6a9167da6127a7a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:00:08 +0000

    chat rush appeal wash direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
