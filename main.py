b5180abce936ee1e7b8837f86f2889a28c04522c
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:22:19 +0000

    smile Bb block busy onto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
