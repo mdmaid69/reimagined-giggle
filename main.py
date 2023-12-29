b30f02ad07cf801d87c4711a3e55f15eeef7752e
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:28:41 +0000

    embarrassing near last appear behave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
