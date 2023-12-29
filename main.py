dab008b8b4b3079026eff454cfe4096db1bf0dee
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:44:13 +0000

    priority strong while point yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_list_from_array(array):
+        return array.tolist()
