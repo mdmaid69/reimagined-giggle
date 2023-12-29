5a94e41de413d6f51457679399d86e21a7d47684
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:12:18 +0000

    tight bath turn turnaway mouth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def convert_to_hex(n):
-        return hex(n)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
