181dc34575132315539279929e3ff16144750be8
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:22:37 +0000

    on scare already boat neck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
