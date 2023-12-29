1b5799b2c8b6af0ffbba3397ee9d4e4a3220dc58
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:11:45 +0000

    prepare herself fly try minute

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Words:", len(text.split()))
