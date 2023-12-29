f2abc36d930e2fb244f03ebef675baa11d0f0a3e
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:25:53 +0000

    favour interview shoulder offend regret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def get_string_from_array(array):
+        return array.tobytes()
