6e069ecdaa607d0acc34758b72106b87c02972c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:21:37 +0000

    learn ought 565end remember usually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
