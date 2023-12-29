07d0484ad0106d77e6f4b78571d2821fd7d642aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:52:41 +0000

    call gate pick chat love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
