2ae966025f90dc8ac1a859d18558b71c82f3485b
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:41:30 +0000

    earth catch invitation door nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
