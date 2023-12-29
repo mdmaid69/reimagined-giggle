278e7eaaf79c5f63bcf988021ac0ee9b40dc75c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:04:09 +0000

    divide advise necessary test Ii

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import random
+def roll_die():
+        return random.randint(1, 6)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
