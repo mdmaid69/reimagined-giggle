07a4a3963ef7833581552b388d418fad92b9a1e8
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:57:38 +0000

    home nothing how runout risk

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
