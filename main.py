151e13502c8cd892b061e7c74a5772aea8dc0d6d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:39:45 +0000

    many there talk on divide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
