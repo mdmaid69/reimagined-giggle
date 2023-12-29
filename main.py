afee787efb81e849f1b8fe5eda7f37fa0bc12a3b
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:26:58 +0000

    somewhere prepare thorough somewhere steep

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
