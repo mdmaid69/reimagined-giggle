e461f18dc55a9369ed344c8151bafa1a6ad4b7f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:24:13 +0000

    against soak knowledge towards all

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+print(math.pi)
 import json
 def save_json(data, filename):
         with open(filename, "w") as f:
         json.dump(data, f)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
