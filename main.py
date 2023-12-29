ec472ee302d4f2bf81c7c354a5da535923dcf2aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:37:53 +0000

    second region machine influence 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
