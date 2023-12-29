de157616f4ad4d2aa419e3767b09874c72b42d89
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:58:56 +0000

    dry conversation note necessary distant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+for i in range(5):
+        print(i)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
