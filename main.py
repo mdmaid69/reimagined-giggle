0206009578c3decc91dc3b5b21e556f7ed9c6ed3
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:11:58 +0000

    criticize own gate complete print

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
