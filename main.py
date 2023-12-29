52f291627f8bffb66fb4daba8987474668ffb9de
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:12:45 +0000

    circle production point wipe present

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import collections
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
