766749a4fc0cc0faa5c75f06d18fd3b192576a0e
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:03:27 +0000

    more arrival fashion everything perform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def find_min(lst):
+        return min(lst)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
