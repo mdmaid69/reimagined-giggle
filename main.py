46cb086de5542bcac5f61068ca35a91b69938ce1
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:55:16 +0000

    talkback rain perfect begin how

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_density(mass, volume):
-        return mass / volume
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
