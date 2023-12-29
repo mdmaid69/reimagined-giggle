393b641e3c5200be06b8dcdefa2c7f4543ae7cb5
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:08:56 +0000

    statement start 568energy shoot shadow

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
