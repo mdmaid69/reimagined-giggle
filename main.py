e9290297df0825549ab3ac461bc28031aacaf12d
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:34:11 +0000

    further step female start Kk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
