ee8ebef37ad33b3d845ee61dba0de2226870b422
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:42:10 +0000

    tomorrow tomorrow build phone rule

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-print(sys.version)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
