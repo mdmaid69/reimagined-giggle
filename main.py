e6eeb7c8810d11df745cecfdd4dc4e466cc5230b
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:41:10 +0000

    wind satisfied game opinion amount

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
