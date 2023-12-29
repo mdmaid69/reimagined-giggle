ff9d811f92b206bf575bd714a9fec167f69acd4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:48:55 +0000

    usual suggestion giveout terrible any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
