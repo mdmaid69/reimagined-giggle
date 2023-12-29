8a977a52aa61830a46ba27da96d078caec6c0471
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:36:12 +0000

    excuse advise meanwhile obtain concentration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def reverse_array(array):
+        array.reverse()
