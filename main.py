6152ad285bcd47a9a72768281b1b93238c3a5e4b
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:58:29 +0000

    agree world fork threaten pretend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import tensorflow as tf
+print(tf.__version__)
