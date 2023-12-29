2cb9e13643fd02527b1ed7b7a00d413a6d50391f
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:50:09 +0000

    underneath message that recent compare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
