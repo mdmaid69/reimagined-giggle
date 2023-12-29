3d905bae1a5191158b1f6896af0571daf0e1d843
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:14:56 +0000

    exam schedule calloff supply thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
