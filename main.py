f7d505dbfab2a159cffe71ba01ab926c56a4f732
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:41:34 +0000

    takeout rush advice lead just

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
