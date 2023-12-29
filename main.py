12ef1a6992775536d2e84da99e125d14e3355da7
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:30:11 +0000

    used accidental see collect criticize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
