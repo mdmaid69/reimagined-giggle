2b5fc79138ce155aebd0907c817e298962f88384
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:27:49 +0000

    physical declaration show experiment rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
