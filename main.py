75ed365e5459a215848555272a75c99d31f00160
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:39:25 +0000

    point word over contrast computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
