ca54a5dabbe9499a5c6f0ee4e552ec13dfac3752
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:18:11 +0000

    improve love or plenty become

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_density(mass, volume):
+        return mass / volume
