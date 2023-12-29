886a255a907bed22b4485e8e40b8d4f06ff67f70
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:38:16 +0000

    shout least shall safe true

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def add_numbers(x, y):
-        return x + y
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
