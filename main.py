9fceb8713386583ee88a824f7ed0ef8903dae422
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:08:12 +0000

    meaning spite shelter just machine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def square_number(x):
+        return x**2
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
