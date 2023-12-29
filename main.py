4ea4363ee61af20a14bae75af3d4a1619ecde10e
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:58:13 +0000

    keep trade muscle ill me

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_current_directory():
+        return os.getcwd()
+def multiply_numbers(x, y):
+        return x * y
