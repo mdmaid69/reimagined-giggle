48ec364d2cf2b7ed2b51b40ca406fa394250a79c
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:43:05 +0000

    rent pronunciation seat memory another

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def square_number(x):
+        return x**2
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
