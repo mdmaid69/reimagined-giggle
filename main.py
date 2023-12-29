b6f2da2051794a2e6625927d55a108bb84ca4a46
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:33:51 +0000

    smile practical chin plan comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
