2e944944d2a51939f178a8f39d8f70c34f273e73
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:14:04 +0000

    wish break nature opponent lunch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import random
+def roll_die():
+        return random.randint(1, 6)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
