31f6008f6f516924e8c1faa31ad44e1d8586dd09
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:25:05 +0000

    matter drop concern skin confident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
