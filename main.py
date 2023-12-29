6bde0ebf455c69e8b11dba0f44d3172452e2fd9c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:17:10 +0000

    meal steal judge quick advantage

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+name = "Python"
+print("Hello,", name)
