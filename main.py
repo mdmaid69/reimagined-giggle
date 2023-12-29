17efeb829ebe5372c97c1dfc30a20c08c47d18ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:14:59 +0000

    brush mean dark thanks block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
