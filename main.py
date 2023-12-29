03713c57b80cf71354cd7e9e03c7843e4ff33fff
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:01:22 +0000

    at guess whistle display national

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
