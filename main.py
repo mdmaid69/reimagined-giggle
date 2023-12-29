a367ef5d7b29f10c872de102f66b1cd32cefe407
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:22:38 +0000

    separate convenient natural step centre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-x = 10
-y = 20
-print("Sum:", x + y)
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def convert_array_to_list(array):
+        return array.tolist()
