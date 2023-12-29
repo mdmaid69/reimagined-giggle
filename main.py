a0f83535ae4c9641d75a0d6e1495be1084af9160
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:15:05 +0000

    contact beginning far yes objection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
