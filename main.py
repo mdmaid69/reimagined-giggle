e64c9d42d3a194c46e14b0a429d5341486c760b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:36:59 +0000

    software word charge off avoid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import collections
+def create_user_string():
+        return collections.UserString()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
