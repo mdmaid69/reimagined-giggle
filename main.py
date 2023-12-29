086c07367a1c5a38f13fb1298b91dd84ba17c3f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:06:58 +0000

    violence himself telephone uniform pretend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
