cdf6fc93e710513365d582b6f2945390efaa3c8a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:18:56 +0000

    sight figure runinto fat breakfast

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
