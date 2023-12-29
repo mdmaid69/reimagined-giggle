e0b66f7571f05103e9202b52f2b7be4bf467951f
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:29:33 +0000

    software circumstance blame hurry breath

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print("Hello, world!")
-  def fibonacci(n):
+def fibonacci(n):
         return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
