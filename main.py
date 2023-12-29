c05ebee6220baf30968289d0643903b50eb23ae5
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:27:53 +0000

    ring ability no cope offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_power(work, time):
-        return work / time
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
