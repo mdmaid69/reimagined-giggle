7b8899b20ae2e00b0eb68a6e78ef0b8f82ee5303
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:04:47 +0000

    birth ours instruction sight scene

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
