219b4dcac5ce34b90c34bfe157dfe9ad6249340a
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:38:17 +0000

    that hang city goon because

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
