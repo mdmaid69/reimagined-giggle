b6bcc041fba6384d78d21697bab5edb4170ad341
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:04:01 +0000

    breakin bury handle run produce

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
