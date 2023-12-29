321e52e72c5cdbb187a2bad2b912b5a1655a0e66
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:19:18 +0000

    talk decision sense bury Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
