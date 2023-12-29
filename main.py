cecc185838f45917de0c50528a352b7c17cb91e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:48:02 +0000

    leg action shall embarrassed join

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import re
+print(re.match("h.*o", "hello world"))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
