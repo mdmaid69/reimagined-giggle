d32a4e5306f6528036b18893a516629fe00fc1dd
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:41:21 +0000

    game especially benefit clear object

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
