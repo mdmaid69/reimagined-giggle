883723596bade9b96bf6f4d9262b14badab43e38
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:41:26 +0000

    improve takeaway print it total

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_density(mass, volume):
+        return mass / volume
