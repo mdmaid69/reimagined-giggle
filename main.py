80b729c1bb9a08d55ab937af28f284055131c9cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:58:43 +0000

    of room load forgive at

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
