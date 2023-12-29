b473773a853fb16f02888cb6f6163c5acb0c1989
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:14:37 +0000

    big bringdown arrange wall convincing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def create_stack():
+        return collections.deque()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
