9be6bbaaeca40dc7f21105f607d7c78a5b88172d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:29:07 +0000

    forward contribute expression come sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+name = "Python"
+print("Hello,", name)
