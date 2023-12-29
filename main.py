7092b55c0d3e046d42a192490f71796b5d7d7e24
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:46:05 +0000

    sensitive us out tonight Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
