c0db99b3728a0d4eae4a2bf8981069f96809dc98
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:33:14 +0000

    obvious spin protection different wall

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+for i in range(5):
+        print(i)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
