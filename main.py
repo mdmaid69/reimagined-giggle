0c16e59e9857d9ed1c5203bfd569a38b6b8d3683
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:22:59 +0000

    load education empty solve ear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
