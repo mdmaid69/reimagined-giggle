51436b1071d6b83c3d1b5464592c002f2b237c93
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:07:49 +0000

    tear beautiful typical you drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_volume(length, width, height):
-        return length * width * height
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
