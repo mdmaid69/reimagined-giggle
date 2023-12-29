facf31ce5d87c7a87ae484962af95a2c16cecb92
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:39:06 +0000

    low teach nearly tape see

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
