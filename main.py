0e8e08a5223c2a6ead735ac381c9051a476f5228
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:00:18 +0000

    challenge heat confidence snow fail

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def find_max(numbers):
+        return max(numbers)
