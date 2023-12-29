82f50446320dce469540978eb27b0d33f7e6bdf2
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:16:37 +0000

    home fair arrival medium study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def clear_array(array):
+        array *= 0
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
