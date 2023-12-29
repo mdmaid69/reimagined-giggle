1fb18ccd7a5b0facf7a4ae6c3f3569945115757c
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:49:50 +0000

    border argue turn by Itself

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
 import math
-print(math.pi)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
