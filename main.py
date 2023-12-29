c57df1afaff73af82bc16abd6fa83141448db82e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:02:25 +0000

    total description peaceful right characteristic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import time
+print(time.time())
