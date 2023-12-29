7325d267ab0926c7130c309ca797476597d0387b
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:27:16 +0000

    ground contribute skin body during

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
