7a0e0ce90e856576ecd4500def19136281577829
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:34:28 +0000

    but fix drive shame trust

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import random
+print(random.randint(0, 100))
