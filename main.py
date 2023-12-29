7ffaf18995908acffa58f8c4ebd647c5e3f1eaaf
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:31:51 +0000

    dress always cheek land visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
