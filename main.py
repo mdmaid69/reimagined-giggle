16684ef4d72c5de2bb1f8fe295b55a726e0c7de5
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:11:27 +0000

    listenfor but under combination tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
