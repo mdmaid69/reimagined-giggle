ac700ef55ba00f0ad8cd990b88ab8c51e933f589
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:40:21 +0000

    patience slight calmly importance stop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
