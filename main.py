95a1628614778ddd68b6a7e4c2da4957909c9f23
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:56:18 +0000

    callat pass seriously warning tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_array_as_str(array):
+        return str(array)
