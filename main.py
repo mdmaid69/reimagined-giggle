90b825510ab20f894e8e940dcff115a928ebf6a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:27:45 +0000

    constantly pass dangerous most lesson

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
