2c35d30f9ad55e436b6327f49a792e0671731441
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:46:31 +0000

    demand street hospital 565end imagination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
