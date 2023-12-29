4bb90ec541c8ce439bd6476e3dd943be8fa19c9a
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:28:03 +0000

    pleasure figure support different shrink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def square_number(x):
-        return x**2
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area_circle(r):
+        return 3.14 * r**2
