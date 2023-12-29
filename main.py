1bee01f1d17f4f46c4c8b679e7937af087c934fc
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:50:18 +0000

    interested pronounce record thanks stretch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
