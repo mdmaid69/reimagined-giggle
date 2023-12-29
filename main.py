32b1bb71dfaa61f309bdbb647527c828b464228e
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:41:52 +0000

    drink study weigh maintain get

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
