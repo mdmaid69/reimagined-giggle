80eafb26ee6e2bd688affdb4a602b79057105c38
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:39:41 +0000

    air traffic the let idea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
