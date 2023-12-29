ddb9b51133e47de27620f02a13c7c2b86acad6c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:39:03 +0000

    hold right turnback apologizeapologise personal

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
