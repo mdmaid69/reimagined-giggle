22fb3bf9c39b8d4a32487ff9215dd6e78b542b0b
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:46:24 +0000

    rain club suggestion uncomfortable skin

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
