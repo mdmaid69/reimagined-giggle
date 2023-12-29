9bb8ba172d31dec5dd76e7c7e7f3dc0ca531cf42
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:25:12 +0000

    love hollow throughout principle hello

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
