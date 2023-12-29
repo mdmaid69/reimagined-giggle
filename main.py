9e8b537a4ce4d4304cf986c111d0b3daef69c4d0
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:47:54 +0000

    evening lean considering about opinion

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
