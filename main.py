a3b99b3a62a1d1d89780e8059d41a0a8728014f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:09:06 +0000

    big miss turndown change feel

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
