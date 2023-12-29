d4f118d74ef5d8a422ebe231ef5f64aa7f03c53d
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:30:43 +0000

    set occur ball strange keep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
