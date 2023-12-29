55beff8a798d2c9305700df94cc37c741073da63
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:08:27 +0000

    tragedy design relax hard press

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def exit_program():
-        sys.exit()
+import time
+def get_current_time():
+        return time.time()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
