622c79ef7737f2c7afd42bb06210e3619a70985c
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:24:35 +0000

    sorry demand friendly development difference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
