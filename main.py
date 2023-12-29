0caf42fc3371e7371c57105ba4f929219bd8a1eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:16:14 +0000

    gradually Tt home welcome complaint

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-def calculate_speed(distance, time):
-        return distance / time
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
