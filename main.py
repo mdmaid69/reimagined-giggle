4a3561c18d5de973e3c7dd29877a6690381a8671
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:51:40 +0000

    translate tidy OKorokay sort come

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+  def count_elements(lst):
+        return len(lst)
 def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
