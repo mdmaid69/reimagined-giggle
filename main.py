10e5a2f15d7d1f94634a635c7b637d7e5f3ac598
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:57:33 +0000

    home nature trade movement pin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_max(lst):
+        return max(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
