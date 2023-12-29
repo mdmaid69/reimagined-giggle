aa922320c2a4cc8ad37436d23bac35495589c849
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:32:26 +0000

    suggestion no physical your experiment

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
