a5e934929bad98253f64740f1238c9f25b1ed764
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:47:22 +0000

    sweet return in tread estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def find_max(numbers):
+        return max(numbers)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
