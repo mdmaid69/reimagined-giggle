bb9e97cf537dd22f0aac28ca46c56eefacb99ce0
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:36:44 +0000

    nose sing own study by

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
