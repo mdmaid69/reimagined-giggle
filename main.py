e720078b566408981f431d7a738e0493cbd4b461
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:02:06 +0000

    score whisper wander consider instruction

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def calculate_area_circle(r):
+        return 3.14 * r**2
