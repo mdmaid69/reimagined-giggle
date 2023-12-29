b0c21182e052ea826b024fdcf3ff9f88c7cf2dfa
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:06:43 +0000

    artificial bang there all measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_list(array):
+        return list(array)
