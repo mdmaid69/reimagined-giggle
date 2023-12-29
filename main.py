fd777cc0b7a1977f2c0432cf11096b4896a1aefa
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:17:16 +0000

    giveaway object but official common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import os
+def change_working_directory(path):
+        os.chdir(path)
