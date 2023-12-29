175978bc42f87195c3072285829d654cf236ee1c
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:02:16 +0000

    accommodation takeaway meanwhile little frequent

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_area_rectangle(l, w):
+        return l * w
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
