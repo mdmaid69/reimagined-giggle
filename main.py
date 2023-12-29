a804a680299db170453ec43aae5d81d8cf4c4cda
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:57:12 +0000

    forget state so excuse country

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-for i in range(10): print(i)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
