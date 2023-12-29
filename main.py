334615f1004d834e7085d35d63d16b6623313b5e
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:01:00 +0000

    influence consist view limit criticize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import collections
 def group_by(iterable, key_func):
         return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import collections
+def create_user_list():
+        return collections.UserList()
