fa436ef3303aadedb1d5a9d1837ac5fc07682d18
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:05:39 +0000

    calloff clothes load food rank

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
