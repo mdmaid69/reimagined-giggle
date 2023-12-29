ab4a8f8a8d5743accb93ebaf7b9c8f42c43dcbb8
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:14:44 +0000

    bringdown better plenty ride ocean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
