53be3da772229f7bcd8a092bfea5a23afa86bea1
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:17:39 +0000

    runinto watch again collection either

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  def count_elements(lst):
-        return len(lst)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import time
+print(time.time())
