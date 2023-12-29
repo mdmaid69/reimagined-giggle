5159698a460cfa931b0d5462a7ea627cb99cfcfb
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:03:22 +0000

    small ask ring unless amount

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_area_circle(r):
+        return 3.14 * r**2
