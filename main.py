80d6213365602a2aed278cb9b549835831faf5a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:06:50 +0000

    mention interesting estimate emergency care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
