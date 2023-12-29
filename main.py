5bda81b59899bbecff4224cb6b74a79d6957fe2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:28:06 +0000

    bank blind hope plan past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+text = "Hello, world!"
+print("Characters:", len(text))
