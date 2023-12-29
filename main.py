e331386e8aee2943c14997f5816578fa2dfd2255
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:15:31 +0000

    satisfactory uponon proud weather collection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
