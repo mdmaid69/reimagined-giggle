4339997a5d0b221ab6969e685fba40181a586402
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:45:09 +0000

    drink land scared complicated doubt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
 import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+print(datetime.datetime.now())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
