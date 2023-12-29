cf6f60c9e52ba38e1f9dab01467247b6ba32eea5
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:42:02 +0000

    exchange trust cheque thirsty mail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
