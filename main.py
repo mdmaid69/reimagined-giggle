25cfd92b15c97a614083d6826afb50d883644b46
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:04:05 +0000

    could beat black window tone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def create_queue():
+        return collections.deque()
