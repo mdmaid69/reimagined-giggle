5290fbe59bd8be5c82166a2304ae642ff2032122
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:04:22 +0000

    turnon thinkthrough space principle ordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
