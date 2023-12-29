693aa59583ec2a02f642c8f18305089fc98802b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:20:22 +0000

    activity twist pressure offend offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
