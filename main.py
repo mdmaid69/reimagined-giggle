f10785b87f3495fe63a4c2770fb21e4b52004df1
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:39:37 +0000

    mean disapprove trousers turnout realize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
