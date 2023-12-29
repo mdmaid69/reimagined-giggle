7b8f44f0b8b81a69ddd22449f86119c26799b668
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:19:51 +0000

    off speed similar mixture mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
