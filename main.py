39996d9bb1b671e521c2a65cab64dce22100c148
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:48:42 +0000

    listen fairly balance thin surprised

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
