e79d48a790c8c087856c1348eb2ae6b954b4304e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:10:54 +0000

    offer slice no who design

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
