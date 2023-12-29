8b70fd858f95b6da5bb0164ebcb199a8ee029ac2
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:16:32 +0000

    feature sharp write move nose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
