14e000ebac453b3123b56e0731afde8093dcc800
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:05:34 +0000

    fashion lot 572enter high press

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
