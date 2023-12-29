1157608f66ca7b2c7744ebb37a533b3f95889b66
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:01:55 +0000

    cover sorry necessary adjective ourselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_power(work, time):
-        return work / time
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
