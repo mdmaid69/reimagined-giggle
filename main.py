65c4fc9f0d43625c3ccfd07059c3ec395529d4c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:58:51 +0000

    choose save taste previous imagine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
