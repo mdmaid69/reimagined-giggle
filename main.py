395ff01b8a0d1a86f5a2111bd419b7e323d45bb1
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:44:04 +0000

    dull build safe tap impression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def is_odd(n):
+        return n % 2 != 0
+import array
+def reverse_array(array):
+        array.reverse()
