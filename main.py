2cacebf6da53ec02b151303df6abf5c3b4539462
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:40:52 +0000

    proof dress Ss mine spot

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
