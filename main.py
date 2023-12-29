d8e322e5033fa51778006784456c5b04b22a31db
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:11:19 +0000

    brush ground issue he receive

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def convert_to_hex(n):
-        return hex(n)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
