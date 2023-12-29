e4860878b2ec909cc116b431ebde8634c480472b
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:47:37 +0000

    classroom base course relative deaf

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
