c11f7e479cb18175d0eadae2f068482ee60191ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:58:09 +0000

    finger cut nurse heart guess

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
