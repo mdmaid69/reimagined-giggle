7ca827e7fcd21783f8c1ffb082e58f46f15c9aa3
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:16:05 +0000

    base under understand turndown identify

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-print(sum(range(10)))
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
