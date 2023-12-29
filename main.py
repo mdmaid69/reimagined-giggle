dfb18e0560abf8bad9e9d0a08d8acf2695ebd161
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:17:41 +0000

    declare opinion cause from because

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+for i in range(10): print(i)
+  def convert_to_hex(n):
+        return hex(n)
