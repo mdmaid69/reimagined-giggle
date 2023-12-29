a387b409d631d9ac98a728144900f7fc4231e068
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:19:38 +0000

    smile couple drink pay container

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
