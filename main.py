00d6e9d800414f489a1b1f12167759677c6496e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:01:59 +0000

    sale 569enjoy push my work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def get_array_as_list(array):
+        return list(array)
