8b7fee98683cec6e070a7fe8c215bef61776026d
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:12:15 +0000

    lean light ocean basis after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import getpass
+def get_username():
+        return getpass.getuser()
