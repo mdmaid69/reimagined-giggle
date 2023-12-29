10393bd56f78c616faf3a79088ce9b481e7a534a
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:09:57 +0000

    along fork print plenty fairly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
