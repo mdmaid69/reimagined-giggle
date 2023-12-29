aeb75c5f82f6ce310ebc161ec33ec1c782eede10
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:39:59 +0000

    minor borrow still spite key

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-print("Hello, world!")
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
