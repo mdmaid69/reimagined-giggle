35fab5703fed880bba7fe750b011b6b5ee9b43b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:32:56 +0000

    beginning wild weekend listenfor judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
