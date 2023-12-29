b0770c0c178c9abc1608aa45c0e133fc6c27a157
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:55:12 +0000

    hollow label space trousers search

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def square_number(x):
-        return x**2
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
