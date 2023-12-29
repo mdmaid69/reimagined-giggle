5a2239da4150b42d71d03ec9d4ecbe308404c887
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:40:52 +0000

    history first recover surprised walk

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
