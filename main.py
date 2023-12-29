e0de653f9fcd22ea6d165aa3925ace6abf59e912
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:45:05 +0000

    surprised bread lookup lower increase

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
