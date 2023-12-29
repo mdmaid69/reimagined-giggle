8c030d367cc4164d0aa485681632fdaa1a171208
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:19:47 +0000

    breath city identify pencil set

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def convert_to_hex(n):
-        return hex(n)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
