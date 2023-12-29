5107b90017ce4ab5ae3e253e4c30f8ccd66a5509
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:21:44 +0000

    culture situation crash warning everyone

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_power(work, time):
+        return work / time
