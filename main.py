26c331b90f4db529ce79aab53def374e424731d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:17:39 +0000

    eager trade odd strike either

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
