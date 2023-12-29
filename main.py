7a9157edc5ccdc8ba09c5411384ef878a8ceae45
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:42:11 +0000

    spite bury turnoff meal smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
