d863a3f914fd1e54abf4482187b98102fa4618f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:37:46 +0000

    tape smile hard destroy next

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def convert_to_binary(n):
+        return bin(n)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
