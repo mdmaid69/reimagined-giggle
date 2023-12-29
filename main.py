1e28c1aff9097ba0e3a0a64a39551b9d57c810ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:26:41 +0000

    slight while quiet fire contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
