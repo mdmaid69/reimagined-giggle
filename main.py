5c61703d8f5432c7c6f81ddead21916a58460bb1
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:38:48 +0000

    leg window lookback(on) geton repair

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import re
+print(re.match("h.*o", "hello world"))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
