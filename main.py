0508b982ad9f86a352f9435d9ae1133dd552aee3
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:16:24 +0000

    bottom office level team whom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  def square_number(x):
-        return x**2
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
