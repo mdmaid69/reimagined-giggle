1e3b36ca55d9107ed6b13a871d1dbd4d1d18e4fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:11:44 +0000

    content previous black blame something

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
