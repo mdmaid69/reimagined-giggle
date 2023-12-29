9b5da633f5fc75b00fb3b11ad532e0440d1bfb66
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:33:57 +0000

    approve flood here wander score

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
