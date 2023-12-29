ec7ce3d10cef72d213de8e4a97e83fd53cbf61bd
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:11:49 +0000

    bridge noise buy brown else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_floor(x):
+        return math.floor(x)
