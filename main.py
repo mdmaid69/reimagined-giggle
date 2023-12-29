94b126a9d56c75e01ae7bbdb03a0f27699b5037d
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:06:33 +0000

    shall prove upstairs when workout

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def divide_numbers(x, y):
-        return x / y
+print([x**2 for x in range(10)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
