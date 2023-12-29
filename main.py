a68adfa3f8a972e637c94bba61cc2897f0edea95
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:22:43 +0000

    curious cap majority weigh I

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
