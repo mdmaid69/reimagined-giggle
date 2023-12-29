cf8fe842ae723d8ca310bdb8d2f8cb3451c4cc0e
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:35:13 +0000

    legal likely imagination note keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def is_odd(n):
+        return n % 2 != 0
