872053bc233b9b36b2f78fb1fbc96109b6bc6a50
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:31:48 +0000

    use inform front takeover our

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_index(array, item):
+        return array.index(item)
