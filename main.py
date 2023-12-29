ebbed88571cc7ef5537b57844226aa944cfe4f28
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:38:22 +0000

    rub trade extra previous laugh

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
