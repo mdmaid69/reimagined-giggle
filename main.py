e19284adeca28a31f42a2f231285020b9566eb4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:53:55 +0000

    feed expect press someone flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def cube_number(x):
-        return x**3
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_user_dict():
+        return collections.UserDict()
