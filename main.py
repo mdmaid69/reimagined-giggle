3c97c98ee773ee71b5ef28ccd0482c36e56e96f3
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:09:41 +0000

    report play ago care sale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
