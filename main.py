5ab95fa9510663de5a6dc1947cb18bed730a5b61
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:58:19 +0000

    so weekend schedule wander Rr

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
