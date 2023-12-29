0356f8a0e9ce206d7cfd8b538c9f49574f601bc5
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:53:54 +0000

    weekend avoid part increase bother

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
