adbeead054b9957847d36998e175603b5f0fdf26
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:26:17 +0000

    again wave future furniture collection

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_work(force, distance):
-        return force * distance
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
