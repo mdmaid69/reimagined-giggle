fc7c8e041862736301ac3d748c7450aee8103f03
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:27:07 +0000

    emphasis activity natural ordinary fill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
