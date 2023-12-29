4c9904f4a93697b93b8d42ea9aea29de47cef7d6
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:59:04 +0000

    exchange contrast embarrassed such divide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
