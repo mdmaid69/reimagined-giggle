393d1d8050b1ab00205813253e190e9b668a8b24
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:51:22 +0000

    scrap there why dress attitude

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
