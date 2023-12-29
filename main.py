ac4a8f30c432db1f2d0d409b6fcade5c00987f54
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:42:44 +0000

    advantage greeting rely comment borrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_pressure(force, area):
+        return force / area
