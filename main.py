f55a4f368f8f86962c954beebbc3e7ccc9042423
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:53:03 +0000

    standard workupto letter style switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import time
+def get_current_time():
+        return time.time()
+def convert_to_binary(n):
+        return bin(n)
