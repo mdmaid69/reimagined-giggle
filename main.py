7e8937687052b728f119d6bae9ed4cd6c06c849a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:58:40 +0000

    break tone takeoff work cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
