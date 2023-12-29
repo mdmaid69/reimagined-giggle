defd3ab9dffbdb2e6d00e831cb38315b9818fd9a
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:07:55 +0000

    school well set tear later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def find_max(numbers):
+        return max(numbers)
