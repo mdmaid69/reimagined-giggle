9a909571afb94238c1acdff4238cfcf10fb2282c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:48:54 +0000

    badly brown stir new method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
