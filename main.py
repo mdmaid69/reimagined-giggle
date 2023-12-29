30a170ac385efe7e68c5ec8553008c596ef72a44
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:33:36 +0000

    modern concern burn protection Bb

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
