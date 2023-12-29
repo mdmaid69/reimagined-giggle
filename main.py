02c26f9bf48cf18f5918d9c236a099b792b667c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:57:03 +0000

    Qq commit fix activity stand

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
