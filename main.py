8d6155fc0c640d5fd51889b6d3ddeef2006a1e40
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:18:11 +0000

    ignore takeaway again takeover fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
