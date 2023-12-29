de8c5eb094b006a2cd45e4b5119900940d4c22a8
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:54:04 +0000

    unless outside worse standard estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area(radius):
+        return 3.14 * radius * radius
