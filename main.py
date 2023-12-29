86fc3b735e30f23c47e9f2dcf17f09bb115c4859
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:00:57 +0000

    lie control unlikely weigh often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
