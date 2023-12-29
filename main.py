63e98da8db998e2bea7da98ab96c4754bfa141a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:10:41 +0000

    see everywhere luck bite hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
