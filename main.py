e55546e05f8600b1145a904aea9f9ed46a6f17ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:53:43 +0000

    gift strict live proof clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
