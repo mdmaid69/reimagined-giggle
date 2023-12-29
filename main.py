b68f9e12fb5ec7e993be83cac353eb3b36c978f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:37:16 +0000

    influence abroad surface refuse scream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_pressure(force, area):
-        return force / area
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import sys
+def print_python_version():
+        return sys.version
