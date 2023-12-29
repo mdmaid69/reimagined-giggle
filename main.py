2f57fcacd8935f93d8f67ba31c6458a5bd30325f
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:03:29 +0000

    minimum judge thumb certainly unlucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
