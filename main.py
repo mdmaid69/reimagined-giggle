deff549930f2d081ab7d6a9a4a0d4f875c6e2535
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:58:25 +0000

    sometimes my pocket could grow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def add_numbers(a, b):
+        return a + b
