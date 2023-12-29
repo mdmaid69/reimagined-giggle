089bb70260c5e4c05cb5fea26cfe34cbc49b1315
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:16:57 +0000

    freedom special inform persuade comeout

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
