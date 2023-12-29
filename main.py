6f897ff144ba4eff17cda9896e9d285d0e1188b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:25:10 +0000

    mad lower history measure time

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_power(work, time):
-        return work / time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
