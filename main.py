1280c68468437a9f92c9a75b6cd4687ed39016fc
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:15:08 +0000

    gap open respect arrangement rough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
