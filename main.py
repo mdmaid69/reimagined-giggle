98ffffef180f91b1fef5fa76be466067e1cd65e3
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:25:10 +0000

    doubt estimate division over add

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def sort_list(lst):
+        return sorted(lst)
+import sys
+def print_python_version():
+        return sys.version
