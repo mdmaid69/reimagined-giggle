c23a6a4f54c60c3f5b6e1ea938e1f923d2eb36ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:24:47 +0000

    anywhere spring smart wing post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
