e41f63d623f6d4c1a859186799f05224e1f24513
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:19:05 +0000

    direction week side square like

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
