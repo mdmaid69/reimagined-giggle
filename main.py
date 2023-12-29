58b460cc7ece88b14af80743a79520f1b1056a3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:05:17 +0000

    wind Itself pass snow tape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
