ba9066b7030f3dda2a020338e7bba227d94eeaf5
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:54:37 +0000

    exchange voice single breath club

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
