f41f387508652baae15698282e29bb6505f52c0c
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:26:00 +0000

    getaway command themselves friendly unlikely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import platform
-def get_python_version():
-        return platform.python_version()
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
