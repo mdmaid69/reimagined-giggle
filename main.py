8aebedc946cf7a28e5113258bd503ac6346c6fed
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:42:06 +0000

    you desperate buy takeaway free

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def add_numbers(x, y):
-        return x + y
+print(sum(range(10)))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
