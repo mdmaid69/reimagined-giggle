e21585235393417f9cb43aaa049fc2435ffa4f5d
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:17:18 +0000

    outside down guess curious dull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
