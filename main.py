86fb5a639dc3a541bf853065a5a5e9c0706a542e
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:37:44 +0000

    grade must delicate bet load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def find_unique_words(sentence):
+        return set(sentence.split())
