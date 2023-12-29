60e57c645e218659ee816e386f44f2acd0cdd398
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:14:32 +0000

    long work grand wide add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
