7bc186e37bc49b62bbedadb90967b74291a1c689
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:53:04 +0000

    smile concerned reasonable repeat prepare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+print(os.getcwd())
