9c2740d651830fea437dc2a5376ed000185c765e
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:56:37 +0000

    defeat Nn remark instruction than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
