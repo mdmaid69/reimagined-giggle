89b893040f22b2445815bbbc2d9fbe6c2c53bb63
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:26:41 +0000

    use crazy under takeback workout

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import time
+def get_current_time():
+        return time.ctime()
