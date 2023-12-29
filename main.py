3bef4313ee74c3c261bd6703d405b47f50e63caa
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:21:19 +0000

    list one regular buy near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
