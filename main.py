b34d5f8aea503902ff3eb74faf0025b568b3e9b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:59:13 +0000

    goback(to) empty either damage dress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
