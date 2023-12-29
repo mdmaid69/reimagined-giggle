dfac231aff0fac3198746369bfaa059315fae718
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:31:39 +0000

    turnon no performance will unusual

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
