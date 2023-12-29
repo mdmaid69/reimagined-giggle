e77b2fa7f77794eefce208360060eab9e580406e
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:47:39 +0000

    future same ashamed breakdown hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
