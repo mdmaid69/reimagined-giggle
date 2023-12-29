3f875b657e30f3a9141ee75653677aa0517ddcfb
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:01:01 +0000

    because mess forward ball exploration

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
