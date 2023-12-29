924e4c1d2948a75633a7bf20e8c34e59dfa3fac7
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:36:33 +0000

    confuse giveback copy goback(to) demonstrate

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
