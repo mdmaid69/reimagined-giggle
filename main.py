0d1c3b60b49b6f702e23affd4ca12fdb52c4c70a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:30:44 +0000

    discover noisy suppose beside love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
