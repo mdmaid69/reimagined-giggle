c9a526480a04cd7515b21e78863b2c33c799c903
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:54:30 +0000

    mainly foot trousers exact believe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def sort_list(lst):
+        return sorted(lst)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
