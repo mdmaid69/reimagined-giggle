ddcef6a9250df67ef0b6ab24c504a75cbdc8b2db
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:15:51 +0000

    look confident busy constantly walk

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import re
+print(re.match("h.*o", "hello world"))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
