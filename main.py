16b919588381ce98df8934e6aa78a060bad41bf5
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:05:27 +0000

    moon service gooff comfort need

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_min(numbers):
+        return min(numbers)
