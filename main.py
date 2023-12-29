31e83760328b31f53c8ec00c075d71f7288459f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:21:55 +0000

    operation godown development impatient interest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
