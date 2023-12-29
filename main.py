152f1702a7fb6f6b51cfb14120981b343480abde
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:05:21 +0000

    block difficulty cover declaration image

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def convert_to_binary(n):
+        return bin(n)
