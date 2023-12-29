2a09771e8ad92b120868a5bc36ec6c9a7d84bcc7
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:28:09 +0000

    leave dry smoke willing neat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
