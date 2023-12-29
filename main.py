add528ce3af3e965dddbbe7d7be8e78f681191af
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:30:21 +0000

    delicate concerned may scrap scene

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
