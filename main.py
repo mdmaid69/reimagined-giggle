dac001534653a2b828f11c2b28d8c75a05dc0138
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:02:02 +0000

    turnover bottom laugh clear regular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
