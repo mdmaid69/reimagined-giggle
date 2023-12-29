22c75ac2f4f5271c9d36fa74dd730ebf48b5685f
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:32:15 +0000

    further no lookup scare nice

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
