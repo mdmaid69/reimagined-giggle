28fc3e0a94b347504825f843ff9ec89ad0a6794d
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:42:31 +0000

    regularly week edge deep holiday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_str(array):
-        return str(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
