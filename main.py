da8b23e9fcec1409201eaf8bd198744586e93631
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:27:10 +0000

    admit both near dream rough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-print(math.pi)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
