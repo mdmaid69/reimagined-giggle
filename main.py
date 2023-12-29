5a198756579eef6b34814d77d1b51ac13c2b647a
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:25:51 +0000

    shelter example promise thin sell

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def load_json(filename):
+import csv
+def load_csv(filename):
         with open(filename, "r") as f:
-        return json.load(f)
+        reader = csv.reader(f)
+        return list(reader)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
