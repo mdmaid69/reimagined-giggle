648b6d169c9fcd6429040a74c8c1d28762ba77c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:14:29 +0000

    belong after beneath compete charge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
