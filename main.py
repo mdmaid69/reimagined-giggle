5e6e7a59380428d61f91a59f2ab3bad7386f1528
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:22:18 +0000

    result meanwhile vote average no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import time
+def get_time_since_epoch():
+        return time.time()
