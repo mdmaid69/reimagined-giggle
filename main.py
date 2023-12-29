0bb425b7ed91764860e286f6b16455af4bb5f087
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:55:27 +0000

    flow sea sound key aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def remove_from_array(array, item):
+        array.remove(item)
