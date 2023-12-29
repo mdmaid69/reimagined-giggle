7f034448cffe4798909f6e7b03d0e1a967a9e48d
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:27:42 +0000

    plain off film herself often

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_area_circle(r):
+        return 3.14 * r**2
