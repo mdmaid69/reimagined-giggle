9639db5762fbdc3142f2a8b8a96d2ddc7580523a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:20:01 +0000

    convenient surprised grand familiar perhaps

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
