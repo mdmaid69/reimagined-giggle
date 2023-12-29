6d87b9bef37c1602a0a313249f0826f5f13e5e44
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:58:26 +0000

    soil switch total Ss street

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
