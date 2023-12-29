e2427c775cca8a3c788ab34738b0312968ebf5c0
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:27:32 +0000

    comeback bag remove illness reserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import tensorflow as tf
-print(tf.__version__)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
