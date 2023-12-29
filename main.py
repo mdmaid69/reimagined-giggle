bc2e3a0d7569a030990be9e17017abb40a6cc947
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:23:46 +0000

    platform contribute runover terrible everywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
