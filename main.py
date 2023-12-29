f40f99d49281176f3b65285eeebb78ff4e66ade9
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:07:06 +0000

    settle generally everybody cruelty comeround

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
