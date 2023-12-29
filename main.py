ca973a01baf9865a5616900a02328fac8025a920
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:03:41 +0000

    tooth with comfortable today affect

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
