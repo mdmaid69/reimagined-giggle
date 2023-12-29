bbfb5c0c9763237ec9ab30d012bfef0eaa235bc2
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:03:36 +0000

    goup kick shoot concentration forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_length(array):
+        return len(array)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
