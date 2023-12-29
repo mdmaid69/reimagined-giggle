a38197f652e2bf725172a4c883d440f5bec23e3f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:05:06 +0000

    roll belief attempt turnaway blow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
