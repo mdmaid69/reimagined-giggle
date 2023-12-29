41c69a61c8d05167a13f884bb345723165d4d2c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:37:10 +0000

    pretty ourselves somebody at dark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import numpy as np
+print(np.array([1, 2, 3]))
