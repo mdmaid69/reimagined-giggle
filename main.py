00b734d405067627cde1cbd0d985af8199ce1d31
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:20:07 +0000

    blame out present congratulate ability

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import sys
+  def get_python_version():
+        return sys.version
