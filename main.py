76952e6f604996f0fdff5a8f6a483bd0fae791df
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:51:08 +0000

    spot definite cover trick organize

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_max(numbers):
-        return max(numbers)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
