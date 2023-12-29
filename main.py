d89972c795539b2970edec442e7adaea84f1ea5e
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:58:51 +0000

    lookafter calm art goback(to) sleep

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import platform
+def get_python_version():
+        return platform.python_version()
