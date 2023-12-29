2ba6e6694567d10b5876522483667624a53e3611
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:52:24 +0000

    lay fair else obtain protection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_length(array):
-        return len(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import sys
+print(sys.version)
