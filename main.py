cf8ed71d64d40e0fdc6aaf8fce887dd7eef5d44e
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:22:32 +0000

    neat passenger breakup answer comedown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
