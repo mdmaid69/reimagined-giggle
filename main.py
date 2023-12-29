e72a9fa97c91ec1bab7ac33790f276cbc6a1d3c8
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:00:38 +0000

    drink large call suggest niggaacademy

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import os
-print(os.getcwd())
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
