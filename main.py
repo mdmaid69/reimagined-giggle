3748ddcd5eac0d51859580aaeb74e20c57a853d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:39:09 +0000

    dear tall crisis course greeting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
+  def reverse_list(lst):
+        return lst[::-1]
