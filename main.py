e03fec14dd6aab9812dadfda775b7d885d93c79c
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:08:48 +0000

    suit extra praise plan invitation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
