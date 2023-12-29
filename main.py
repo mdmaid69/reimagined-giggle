81417ce0b173e1c6d6557216ff991c9574afb620
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:37:04 +0000

    nervous callup class embarrassed acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
