548b32b50f66ef238119a787d59df8bb9087cda7
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:39:39 +0000

    born fast whichever child wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
