50a0ec6028855bff7f72bc1ed0d1813327a6ca8f
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:38:32 +0000

    group lift bar our friend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
