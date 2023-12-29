012aa3fd87b1ca7de87e2678f1a98e239bc6e753
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:36:25 +0000

    left shake flight runinto partner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
