bd580d89520655c821b02c90c70bfed0e4fb93fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:54:51 +0000

    secret guide shine block bringdown

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
