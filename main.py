1e793491ebe2102a0cc25bb9061713b9a3142395
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:35:14 +0000

    ticket holdon scheme stick holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
