d6e05369553e32cee5657f839cff58c02612880a
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:29:51 +0000

    job store holiday hot design

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-from collections import Counter
-print(Counter("hello world"))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
