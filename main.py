6dc4a6ae87c913bcc74df6e453f3e8e78a5e5a5f
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:27:31 +0000

    afternoon maximum rarely ring thought

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import collections
+def create_user_list():
+        return collections.UserList()
