3850ef3df6c109a8ccf2dbb46d9389636986272d
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:38:12 +0000

    rank joke goback(to) perfect wild

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
