e1ea09070ebb1228f90ddad59e37ce47c37d5c1d
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:41:28 +0000

    target shape floor cross remove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
