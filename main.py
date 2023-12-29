dd63f8ceeaf582893a7070b1535575738b61ee45
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:54:08 +0000

    rain runover dull surprised neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
