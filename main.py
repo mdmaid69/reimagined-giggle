8f6af96c77408a899db8ca24e0938427b4c267ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:25:21 +0000

    story average walk little scheme

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
