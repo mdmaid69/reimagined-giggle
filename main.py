72a3c959e7f070fe5e188500cfc0e8bc01014718
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:29:16 +0000

    card row comedown open certainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
