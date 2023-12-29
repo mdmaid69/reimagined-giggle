38d1416f61780f0c04620be16f4f664352a44ebe
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:18:29 +0000

    arrival search store drink avoid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def count_words(sentence):
+        return len(sentence.split())
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
