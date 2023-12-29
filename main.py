f7f875955f4a6d82e93dbb606be149d99d5d87c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:36:55 +0000

    Tt on second programme shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
