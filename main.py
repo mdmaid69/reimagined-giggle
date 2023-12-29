d009079c79dcda6bb0a55e49970fe3de1ea1dd45
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:05:41 +0000

    single Rr peace receive charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
