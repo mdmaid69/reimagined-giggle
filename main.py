eee73cac1a787712c4e0a0de5dd50d8ed1328b94
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:21:32 +0000

    window pour exchange middle weather

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
