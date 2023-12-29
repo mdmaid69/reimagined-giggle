432fea3eb9a8775c4c485298242d727a1ce62228
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:17:06 +0000

    able thing force sail talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
