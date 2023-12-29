139afaf07462194574217ef23a95ee978b70f64c
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:04:33 +0000

    birthday bottom kind shoot total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_floor(x):
+        return math.floor(x)
