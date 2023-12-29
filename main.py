8a3593200257798cf837adbd6c43eaf9d57558b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:17:57 +0000

    design cum describe creature smile

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+print([x**2 for x in range(10)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
