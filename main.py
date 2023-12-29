32f032b3ad37a185f37c7e548c2f8689c5d60daa
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:42:38 +0000

    square every fit lock comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_list_from_array(array):
+        return array.tolist()
