0c1926d4cc590c03e17436910bcdae7ca9176f20
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:23:32 +0000

    nice treatment past about can

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def multiply_numbers(x, y):
-        return x * y
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def append_to_array(array, item):
+        array.append(item)
