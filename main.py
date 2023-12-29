b101aaa2c36e5b039ef24efb1d2ac4ea7f88fe19
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:40:03 +0000

    active food opposite minimum please

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_binary(n):
+        return bin(n)
