4f5c793e0254474899a8be403c3d143e1a6b75cf
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:55:03 +0000

    command morning damage treat risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
