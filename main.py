ae99bf219a223e55a404b8c9d09f8c3b47c77211
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:26:11 +0000

    high gooff callback train Vv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
