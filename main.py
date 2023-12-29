3d64b2384efd0553b8ef099d756d7092df6bbdd7
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:46:54 +0000

    tidy long excellent familiar leave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
