f156b968d66bc4e35185d8471eebf87641db4435
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:56:53 +0000

    bunch seriously rate question worse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 def calculate_product_of_sequence(start, stop, step):
         return math.prod(range(start, stop, step))
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
