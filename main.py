c40cb4fc9c1e7a001f086942d24b1a47c3c925a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:05:34 +0000

    commit question interested somehow home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_acceleration(speed, time):
+        return speed / time
