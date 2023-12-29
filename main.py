c3add13e2cc779d0c29543f8c1c8252ae47af194
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:24:29 +0000

    situation steady great moral wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
