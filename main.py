902e586ee90b139bd8889e0ebe6428dc8f501b69
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:28:35 +0000

    search Nn fence spill chief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
