da1b58abf2f79aa0a69597705a9c6be894fa2bb8
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:34:00 +0000

    ticket interview apparently blow tonight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def cube_number(x):
+        return x**3
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
