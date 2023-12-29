9be21900952476c7a70e441a530cb4cdcd90ef67
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:08:10 +0000

    evening runout inquire test interrupt

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
