b74811e4660b0023b3cb85ffada50f8f3b522e30
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:22:50 +0000

    creature march gate crash declaration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
