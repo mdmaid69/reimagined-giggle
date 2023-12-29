aba9df197d440e465160d66cce69044be5a89ffd
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:13:08 +0000

    underneath spend rough afterwards moon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_itemsize(array):
+        return array.itemsize
