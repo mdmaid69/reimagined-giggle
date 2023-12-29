3c39ded51fea74df5a715198daf43f170a4bbb54
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:47:47 +0000

    become wave schedule actual modern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
