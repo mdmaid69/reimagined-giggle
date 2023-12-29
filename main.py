18e08beebc09d3b2cdf92b1b8bc55e7420e41049
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:28:02 +0000

    plastic hurry pain train serious

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_hex(n):
-        return hex(n)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
