2915f56f332d8caa91cc9492bde0e460b27b044f
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:25:28 +0000

    wear without pain trip pleasure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
