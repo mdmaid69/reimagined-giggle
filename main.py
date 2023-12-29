31186e1588cb66e6dffcc7a092f2ba170564caef
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:50:36 +0000

    shop provide government frighten duty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
