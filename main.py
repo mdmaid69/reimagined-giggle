d267204c9d55fa431c5851992007fe468451c0e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:00:46 +0000

    battery room have tense lock

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
