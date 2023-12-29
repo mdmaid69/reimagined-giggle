e2af2e69423eceb697c27a57b0fbdffe115d3c83
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:32:41 +0000

    touch score punishment trust hollow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
