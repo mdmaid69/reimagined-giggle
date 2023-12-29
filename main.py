531facf18e74db5d3b1e8add866dfe61ce119b23
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:43:28 +0000

    introduce all radio up getout

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
