0670b52db3d3989166a02df0a7ad4d1142c3dbba
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:45:55 +0000

    book answer become free habit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
