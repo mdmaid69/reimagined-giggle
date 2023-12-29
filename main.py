b588209295d00201735dbe2d3870e6773fe3254e
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:47:34 +0000

    open factory performance along blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import collections
+def create_user_string():
+        return collections.UserString()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
