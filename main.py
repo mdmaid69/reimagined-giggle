4a2a24e960ab107ed9845d03319f6ecdd510cff5
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:57:18 +0000

    age various physical evening most

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_pressure(force, area):
-        return force / area
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_string_from_array(array):
+        return array.tobytes()
