f03a551ac7df283d473174e733830b631eb69af6
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:20:36 +0000

    bang plenty at relation sure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-print("Hello, world!")
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_pressure(force, area):
+        return force / area
