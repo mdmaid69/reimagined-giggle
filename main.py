ab92924258c31f00b56231d6b7c4757e4db21298
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:57:15 +0000

    exhibition trial fence road better

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
