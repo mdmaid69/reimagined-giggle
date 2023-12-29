aa284cd1a9de5e0dbaa19f36e4c10a158798caff
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:55:49 +0000

    touch embarrassed week funny even

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
