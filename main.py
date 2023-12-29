31cae461270868d6d6a9d5ecd6506a37b86c7fe5
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:37:30 +0000

    live actual experiment respect talkoutof

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
