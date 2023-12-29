068d901c62c832aee6a765ed02d3f3e71055e342
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:01:05 +0000

    hungry catch broken difference how

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
