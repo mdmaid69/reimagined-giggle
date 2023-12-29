5795463845dc35de6d758a77fe54a6762e22337b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:00:33 +0000

    imagine also pity themselves and

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_string_from_array(array):
+        return array.tobytes()
