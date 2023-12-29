a114c6bf39ba7c13addc85816f32f387b70f388a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:34:25 +0000

    curious would treat post by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
