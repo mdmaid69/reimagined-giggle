e7a1259d82d73f155920dfb3c9cd6187ddc9dff2
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:14:11 +0000

    smoke base getup invite season

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import os
+def change_working_directory(path):
+        os.chdir(path)
