d91ed7a76ac7b01802568c745ee26eb0febd9fb0
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:07:51 +0000

    passage free flight behind confused

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
