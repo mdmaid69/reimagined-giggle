923d902f0da09f7b766fc913e5bd2792aecdb2d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:13:09 +0000

    when authority tend feature comfortable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def clear_array(array):
-        array *= 0
-def reverse_list(lst):
-        return lst[::-1]
+def reverse_string(s):
+        return s[::-1]
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
