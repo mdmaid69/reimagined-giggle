ca03b7ffd464c88c074f6911ec209c4c1c98cd57
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:37:39 +0000

    572enter break excited friend attention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
