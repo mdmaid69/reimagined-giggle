a7b83d60a5af34a9a360d77041e0b6f883ea1b13
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:08:50 +0000

    than avoid complain accidentally healthy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
