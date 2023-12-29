bfe3e5d299d7e447fae1d0005f27618e04f5a543
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:15:30 +0000

    honest expression herself rush instruction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
