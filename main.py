2b18ff830ad33787d4671c7960bd083dc83da2aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:13:05 +0000

    keepout big body recommend ashamed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
