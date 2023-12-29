ac89b47bd98d6942b55057c1ebefe677d1507d8a
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:44:06 +0000

    possibly within concerned involve cut

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
