d32127569150bc3a0c2325898752605952047900
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:22:33 +0000

    recommend aware guilty shake mention

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
