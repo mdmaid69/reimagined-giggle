09a299bf2715831face6d131349225a407492bda
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:40:49 +0000

    godown common track yourself stone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
