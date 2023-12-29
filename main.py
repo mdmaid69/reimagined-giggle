9f9da420bbf9a29111714c1985a29243c731dfc0
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:13:14 +0000

    payment love final race wish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
