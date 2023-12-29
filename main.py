b46c97d5316c8bfd9404e9810ae01855ef22698e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:56:11 +0000

    successful pity cruelty sail damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_average(lst):
+        return sum(lst) / len(lst)
