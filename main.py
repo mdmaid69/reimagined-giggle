a7f4144f1664ec37135608bd9bc9638eb82443c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:57:18 +0000

    lock hurt back contents react

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def count_elements(lst):
-        return len(lst)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
