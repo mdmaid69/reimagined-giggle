62ac07b72e1d6222f09d0030f298ae1fe81e1c96
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:54:44 +0000

    crowd whoever quickly use open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
