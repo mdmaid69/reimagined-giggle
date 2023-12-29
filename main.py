999f99071778f957a3bfd127ac5c3817dfcb4cda
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:06:24 +0000

    circumstance hers rather this bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_area_circle(r):
+        return 3.14 * r**2
