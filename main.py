edcc6d8ace89c654d657e002cb3f30be30b7ca30
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:57:53 +0000

    arrangement outside heart degree murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
