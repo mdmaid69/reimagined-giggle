6f5d304f2e6651a04f10f348021a99fef567d784
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:31:33 +0000

    cheap kind where affect contents

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
