326407dec8bc5391a14e9bb5aee00442c6fe2163
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:20:37 +0000

    record thought ever plenty print

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
