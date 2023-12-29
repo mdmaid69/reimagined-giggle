5808e9c5e6296efcc0be92b86d7629256562365b
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:07:00 +0000

    feature press organization risk classroom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
