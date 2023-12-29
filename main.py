6a26298a8ad395507e8c0891062fac8f313d8f92
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:25:34 +0000

    cool naturally rich fetch comein

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_ceiling(x):
+        return math.ceil(x)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
