4bab5309580fb4d64615ea0b1cd1a7cb6a89b039
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:57:06 +0000

    towards sink foot only leave

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def append_to_array(array, item):
+        array.append(item)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
