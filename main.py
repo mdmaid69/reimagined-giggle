7f57c53786fec44284190b2a0b765827a78ce73b
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:19:00 +0000

    tell eat habit thumb positive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
