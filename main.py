048324e11c9dfc04fc58079e93c7afc4c6feac10
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:01:47 +0000

    nonsense roof heavy operate shade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
