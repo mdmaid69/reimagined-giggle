4a58c912265af2d4d112d564b9abc8a373073fd1
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:59:10 +0000

    compete cross concerned post rid

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
+with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
