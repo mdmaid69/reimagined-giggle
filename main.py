4d9bfa0ddae66b809367677f7d4544dbd03db688
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:51:14 +0000

    congratulations 572enter chance plate light

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_average(lst):
+        return sum(lst) / len(lst)
