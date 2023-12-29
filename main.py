4d86045fe731cac8b10cf8a12d26a186935f7d05
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:35:22 +0000

    giveout always laugh science between

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import numpy as np
+print(np.array([1, 2, 3]))
