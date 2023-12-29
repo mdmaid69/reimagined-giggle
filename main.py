12aa9b93478caf40216d079189bbd797a1b4670a
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:40:12 +0000

    make production secret future accidental

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
