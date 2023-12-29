8dd9b625c6c1188f13562011e65ee7f2b609662f
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:08:54 +0000

    content any father strike suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_counter():
+        return collections.Counter()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
