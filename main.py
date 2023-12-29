6258873c5487db5a0ad3cbe9543a828c1277835b
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:00:05 +0000

    much come zero quiet choice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
