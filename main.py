969f54e932e1d3a7469070f52b217121e574542d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:56:12 +0000

    shade tradition expect active cut

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import numpy as np
-print(np.array([1, 2, 3]))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
