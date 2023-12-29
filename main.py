bcbf709868172ba1a88ec70635c0f3bd54bb4184
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:32:43 +0000

    handle Itself clean allow computer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
