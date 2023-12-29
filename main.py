503e6c3f33768e026e4c8f1d65080264f916a2e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:50:18 +0000

    part mark this burst aim

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
