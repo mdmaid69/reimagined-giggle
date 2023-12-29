49cb68199d71b1a2569a635a4fb072f1da2fe8b5
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:45:39 +0000

    word meaning ashamed 572enter familiar

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_user_list():
+        return collections.UserList()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
