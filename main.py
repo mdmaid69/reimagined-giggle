4c2f4797b57b3903804090d87aa4074706d73262
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:31:26 +0000

    private slice movement natural wrong

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
