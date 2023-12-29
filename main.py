985dd78c7af0c28fd7b883cf7d035b467771a5f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:26:31 +0000

    declare realize everyone finger manage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
