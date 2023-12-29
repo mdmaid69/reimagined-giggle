3ea011af0e3f1b63e88f9ba65d74ba39b97a26df
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:01:13 +0000

    upset drive along clean dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
