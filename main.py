bc1e963f1f69a8b48160e003b5c13edfd502523a
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:50:48 +0000

    sign tight scale Yy pleased

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-print(random.randint(0, 100))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
