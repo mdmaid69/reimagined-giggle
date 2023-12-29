12f6d11e5ed089bd5d30daa3b5bf5df4b565a750
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:05:10 +0000

    speed edge tune arrive entrance

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
