d72fdb6c29d1dde296dfb0282316450c3a47374c
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:45:50 +0000

    news crowded understand unhappy talkinto

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
