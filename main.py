ea28c36f537797441256022456fee1779bbd2258
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:05:39 +0000

    hole memory exit shut fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
