c8b9f0d4c7e3ab57b6d731503f25844f4a68f3d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:28:12 +0000

    immediately leg future next goout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
