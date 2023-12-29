8833b742f6269c3752a3ce3d1da3713b43b0c588
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:57:35 +0000

    pull underneath interruption minimum getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
