43e41a8ef7a7e25dbcc72a9580b3abea5c6e756b
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:04:26 +0000

    dry no opposite cheat better

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+print(json.dumps({"name": "John", "age": 30}))
