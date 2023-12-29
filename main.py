40bf128cb4f2c35b79cbb74f3404db9a308bef2a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:07:23 +0000

    better able offer chase lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
