2944937a053d43ba0b8c47940a932858e112f39f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:35:32 +0000

    scream available getback advice like

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
