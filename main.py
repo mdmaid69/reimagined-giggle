c0555b8f363cf0166c00889ff464acc3efdb70a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:59:26 +0000

    Oo finger comeoff welcome acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_length(array):
+        return len(array)
