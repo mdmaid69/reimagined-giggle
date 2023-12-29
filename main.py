531273520750770c3652872d2ba27c07b9eeea74
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:39:07 +0000

    admit touch survive skin know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
