b818c5dd3e33e7ad19fa26e91f22389168960b4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:33:46 +0000

    exchange crazy little prevent temper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_density(mass, volume):
+        return mass / volume
