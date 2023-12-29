9da14e01ac61b898df7de505813d20b5669a0fd6
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:47:20 +0000

    for improvement rush bright declare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+print(re.match("h.*o", "hello world"))
