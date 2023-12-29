441023e91cef97ac0ca4b3555b9392b93fbf6aeb
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:05:53 +0000

    special smile autumn rent smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def remove_directory(path):
+        os.rmdir(path)
