62e3288d5b69d3a0299ff9372da8765d9f3da22a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:08:00 +0000

    trade run range along pity

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
