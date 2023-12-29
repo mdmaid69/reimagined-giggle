ca28dadf77ef36902e27d907b2367b2089c90cc6
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:06:17 +0000

    persuade wild crime level meal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
