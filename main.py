d5733c7554b6eb7a02451c28cb2c48da1b37febf
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:35:42 +0000

    yourself the meeting love goup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def remove_duplicates(lst):
+        return list(set(lst))
