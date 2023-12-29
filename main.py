af0b0463df8bde147542c3ee9025808098cd098c
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:13:03 +0000

    life slip step pretend sorry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
