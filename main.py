100d3eb6503b3deb3a2c373da2af901214b39eb6
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:13:31 +0000

    move party ashamed light play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
