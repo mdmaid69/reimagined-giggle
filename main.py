2b7e8c8fe5120cdfbf856c12882b111c7e358ad7
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:26:08 +0000

    decision power bin behaviour community

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
