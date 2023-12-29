4630217a4a1f8f595a1861af406b1f77b31a8fe7
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:12:12 +0000

    poor miss late steal goaway

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_list(array):
+        return list(array)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
