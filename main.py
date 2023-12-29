2fd9c56b0dc030ea0cbfc85295404f62ea7324ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:15:20 +0000

    prove improvement direct before content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def count_elements(lst):
+        return len(lst)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
