523380683c3d9c282248e6126d01871b79f94e7a
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:36:24 +0000

    odd offer limit flat sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def multiply_numbers(x, y):
+        return x * y
