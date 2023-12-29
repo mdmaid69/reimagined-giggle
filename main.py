e55189c3b135e969ce5862f3546913b71a95531b
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:51:26 +0000

    well address answer get Pp

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  def is_even(n):
+        return n % 2 == 0
