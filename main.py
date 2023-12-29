37a712b4d0708fac34246240e5b6ab42a5b2e840
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:48:00 +0000

    date keepup support strict first

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def convert_to_binary(n):
+        return bin(n)
+def calculate_acceleration(speed, time):
+        return speed / time
