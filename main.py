66a5d1d4984f74a63215a8abe30a6223c0090a66
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:29:20 +0000

    battery second same private cool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def get_array_as_float(array):
+        return float(array[0])
