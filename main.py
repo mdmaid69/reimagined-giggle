479ee8e159e801678764401cb15108fd11d7bd16
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:57:18 +0000

    summer hour phone object common

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_force(mass, acceleration):
+        return mass * acceleration
