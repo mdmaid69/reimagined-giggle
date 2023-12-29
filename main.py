406508e4e1c2338fe86b66c53f3a52745a039a98
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:41:34 +0000

    serious at somewhere part mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
