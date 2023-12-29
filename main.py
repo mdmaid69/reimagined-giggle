8a66b91639010f3533dcbf236b44fe04d0e8f696
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:02:37 +0000

    answer woman feel prove welcome

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
