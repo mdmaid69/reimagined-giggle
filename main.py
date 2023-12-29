0c532d779e3a432514b1c3a9b7a1b339ec2e87bd
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:22:12 +0000

    wash separate eat false settle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
