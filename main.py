5bc8a61b834c1e805100ff2e1eec1fb39838e787
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:12:11 +0000

    advice Hh separate nerve opponent

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
