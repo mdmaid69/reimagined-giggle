d88ad277259fb22f83bb165c53886b1ea938104e
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:58:28 +0000

    pinch culture hour border load

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_density(mass, volume):
+        return mass / volume
