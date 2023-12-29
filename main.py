c0cffd1bed2d0fd972a695cbe2b9690cfcaf5170
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:09:36 +0000

    definite broken spring difficult school

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_queue():
+        return collections.deque()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
