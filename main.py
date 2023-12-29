bb8dafd69d34f5553424a8679b20705c7d3ef201
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:47:36 +0000

    into relief lower exist both

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import collections
+def create_user_list():
+        return collections.UserList()
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_exponential(x):
+        return math.exp(x)
