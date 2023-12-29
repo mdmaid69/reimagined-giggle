a053ae0bcd1a05cba220827edcc53ae857b361b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:04:16 +0000

    temperature supply mail ground wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
