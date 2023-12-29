c58a814d0ea316539d3921dd1534a24a243cf871
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:04:59 +0000

    tie rest wave plastic negative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
