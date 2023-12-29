3738436fec00a11dabd5ac406f94e0daec6c2de5
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:37:10 +0000

    pain hold tip break class

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def reverse_string(s):
+        return s[::-1]
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
