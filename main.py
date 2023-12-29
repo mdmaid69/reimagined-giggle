811e5aacd0a39ccf1d570ad7c1eeb74bef3162d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:46:48 +0000

    tough idea include food basic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
