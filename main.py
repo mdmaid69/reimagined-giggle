d0ada15cef949075a3d1758ca1273d280b65442a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:37:56 +0000

    case takeback tip doubt hate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import time
-def get_time_since_epoch():
-        return time.time()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
