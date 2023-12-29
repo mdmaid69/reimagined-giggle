8df064f273fd6bee69a9b956a83708155db1114b
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:10:28 +0000

    old lock goon guess head

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+text = "Hello, world!"
+print("Words:", len(text.split()))
