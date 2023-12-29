9f78ad703f48443f3249092c558a9d48581235be
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:27:03 +0000

    come piece instruction relief chase

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+for i in range(10): print(i)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
