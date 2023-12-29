b234258eaad5bf71c57d69c743185d3972631db1
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:03:59 +0000

    refer report maybe birthday popular

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_density(mass, volume):
+        return mass / volume
+def convert_to_hex(n):
+        return hex(n)
