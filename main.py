8a3a316c3786dadd890d481f4134e22a716c0f50
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:45:57 +0000

    letter heavy feeling view mine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def convert_to_hex(n):
+        return hex(n)
