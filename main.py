f71fe5a6e198504efbe683ab88c6972a451a415a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:25:38 +0000

    organize minimum as national aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
