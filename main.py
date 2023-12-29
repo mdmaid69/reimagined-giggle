2d9904e23d10487e7cecde32a0551be599181d60
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:39:26 +0000

    me steam colour team announce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import datetime
-print(datetime.datetime.now())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
