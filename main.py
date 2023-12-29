1566108fe106e0238db0f7e7af85d177a5f41dac
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:01:52 +0000

    bit interested dance contain worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
