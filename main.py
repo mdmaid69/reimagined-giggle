ad18d504812ee2c1a9013e1a385db91e8181eb5a
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:42:40 +0000

    pronunciation reasonable thinkover marry unkind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
