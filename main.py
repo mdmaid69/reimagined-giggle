1f2296cf7b25e539fd69f428f6daf475cfbf8bdf
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:40:57 +0000

    bye appear yesterday poor obvious

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
