f1819e8ec6e6517032067338b5a6bafb8ec7cf2a
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:30:01 +0000

    offer want eager private look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import sys
+def exit_program():
+        sys.exit()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
