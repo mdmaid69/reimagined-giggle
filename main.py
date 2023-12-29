e0b41a8c7fcf730efc1f451c80b40f18b51f1307
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:22:08 +0000

    since likely protest frightened nothing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
