993bd0e2245be499a45db8bdc9af209cf44b1238
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:46:24 +0000

    owe normal burst tidy modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
