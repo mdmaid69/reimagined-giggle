fa14021a50a941af9644ecd6768edad97b605bb8
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:37:41 +0000

    gift principle objection pleasant control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
