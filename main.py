212bdca4e42e15742639dfad15036afbb384a4bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:27:36 +0000

    knock lie bringback true charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import platform
+def get_os_info():
+        return platform.uname()
