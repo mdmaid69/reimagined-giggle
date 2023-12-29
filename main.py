3430deef44709c9353a741878bf7146baa1a4bec
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:20:39 +0000

    breathe various straight work odd

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_itemsize(array):
+        return array.itemsize
