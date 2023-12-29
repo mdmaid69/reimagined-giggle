effe0aa5ae246ed3cde86033e00a8d7548f47e4d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:08:22 +0000

    will good find break period

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
