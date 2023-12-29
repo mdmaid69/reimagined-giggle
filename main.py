97f23283d75785e706d15dd95cff2b8cbf105f91
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:17:34 +0000

    dream fit notice window expect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+name = "Python"
+print("Hello,", name)
+def calculate_work(force, distance):
+        return force * distance
