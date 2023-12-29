d83e6c198b76aca389723d4741fc75b63ddbcec4
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:05:37 +0000

    pipe own furniture pen much

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
