671de277f3523fb5d58d36fe5679ce81a2927102
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:38:27 +0000

    ask ordinary there fond original

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import os
+def get_current_working_directory():
+        return os.getcwd()
