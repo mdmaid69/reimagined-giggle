97d20f8e7fd82ef8c0cc58d95c0e26cd1d47b074
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:36:08 +0000

    thick I like advantage pleasure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
