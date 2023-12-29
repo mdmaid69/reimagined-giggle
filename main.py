51b88484f68a4a0c4bee8767212035737b962019
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:20:11 +0000

    declare turnoff stamp control love

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area_circle(r):
-        return 3.14 * r**2
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def convert_array_to_list(array):
+        return array.tolist()
