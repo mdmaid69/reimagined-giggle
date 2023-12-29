f1f77d189067037e688cf586193d6070835baf68
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:31:54 +0000

    alone equal late stick food

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
