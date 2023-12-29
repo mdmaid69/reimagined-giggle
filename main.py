bc990554ff58d5a03a97613309b0efc3f310709a
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:11:38 +0000

    emphasis have accidentally right talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
