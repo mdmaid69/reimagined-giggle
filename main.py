0b9368e26889030ee0210ad74677dad435f0e881
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:51:11 +0000

    ready table takeaway heavy blue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
