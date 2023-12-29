cb77bde8579543afce9085acf565f7cba97b5941
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:18:25 +0000

    upset peace attempt near real

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def square_number(x):
+        return x**2
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
