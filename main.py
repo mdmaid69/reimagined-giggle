6a2bfb57df3ba7cc7375aabc7292309286375732
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:34:00 +0000

    wild allow read trap perfect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
