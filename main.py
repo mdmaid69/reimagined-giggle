f253f367e8743e8318310558f44a233787da5b70
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:26:41 +0000

    serve public announce show pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_volume(length, width, height):
+        return length * width * height
