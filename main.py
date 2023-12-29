9317601568773ae2a9fa4d196302054128baa786
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:21:28 +0000

    able constantly lonely workon rather

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
