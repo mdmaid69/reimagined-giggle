3e2c01e7803a80b40e8f73d895a12b4b8d29292d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:39:36 +0000

    powerful object similar interview alone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
