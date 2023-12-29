e30d9bbba2caef19cea088dadeb7966b78bd537f
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:39:41 +0000

    soil stare consider congratulations leg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def reverse_array(array):
-        array.reverse()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
