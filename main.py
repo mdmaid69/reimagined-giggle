f36989bae2f7887e2831d05c44198528a3e11b57
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:42:22 +0000

    nearly principle wheel general bank

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def multiply_numbers(x, y):
+        return x * y
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
