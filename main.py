8130ad7faa10b4282e4f8eddb3a0372296967589
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:15:02 +0000

    wear march absolutely change steady

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import sys
+print(sys.version)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
