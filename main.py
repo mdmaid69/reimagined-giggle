161ab8d1bc68cdb5d25c04667e8b48ca2c6a0ee4
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:51:29 +0000

    ability soak exploration strike offence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
