21d8fb19191309b609bdf1ab772c9b49e38d0fcd
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:02:50 +0000

    laugh appeal breath choice breakinto

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
