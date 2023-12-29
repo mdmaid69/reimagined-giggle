150cd149940fb345cd0c02ddb7685a9d44211cbb
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:23:42 +0000

    bottom realize complaint behaviour balance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def divide_numbers(x, y):
+        return x / y
