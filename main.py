dba9ef29a8a8137bd0e855d88a44d8c9a35c9a2d
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:07:18 +0000

    corner forgive pocket mix exploration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
