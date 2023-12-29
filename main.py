bc42498c5186a597579849759097efec6122c0be
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:56:31 +0000

    show slow account goodbye smart

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
