2e3ed604aa0acd31180ccb3acb427a8e8b2ce068
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:37:26 +0000

    no behaviour message whistle bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
