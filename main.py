9dc893441966a85aad65610c2f0f1e3a0f7c5536
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:20:59 +0000

    basis tooth point guess connected

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
