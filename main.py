c3df83032fbc7e49aae4217a54c84fc6649a446d
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:16:04 +0000

    scheme accidental warm feeling sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
