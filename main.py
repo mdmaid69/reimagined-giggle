f3a66bca453035c7d77c06df3ae61ba8a5fa59ba
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:56:09 +0000

    somewhere strength battery practise react

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_min(lst):
-        return min(lst)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
