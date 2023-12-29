9aaf39e53e90b57d9ffa6bcf303df9ce523a4947
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:07:50 +0000

    bad recovery artificial speak terrible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+print(sys.version)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
