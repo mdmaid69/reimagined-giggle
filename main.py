1bd5804ca6832fb12b15f5f57c6a23e3bb94590f
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:17:19 +0000

    trick danger loud along bringback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
