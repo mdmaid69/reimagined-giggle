1877f564d5f11e7ab0faa658337d2723ef187525
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:40:17 +0000

    stone have now normal warning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+for i in range(10): print(i)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
