ef7d70076ddd0d9eb2433c4350647ac273f3126c
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:55:59 +0000

    and then unite system object

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
