ba1c0fc0520c244da255ec89e44c87be22d5e374
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:41:16 +0000

    whoever swing over pile workon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
