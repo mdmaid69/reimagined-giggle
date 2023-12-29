12c591712ef3f784761f3104ad01843fc7136df2
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:39:59 +0000

    increase slip information centre spot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
