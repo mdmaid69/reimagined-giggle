3a9434a5b73312be6f7dfafbbdefd697d9c04ef8
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:05:23 +0000

    project support die sink tall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
