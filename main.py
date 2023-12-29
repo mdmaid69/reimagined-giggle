75241b04cb3f2551366c984d488ceb6696ee46bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:53:05 +0000

    either calmly breakin previous journey

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def count_words(sentence):
+        return len(sentence.split())
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
