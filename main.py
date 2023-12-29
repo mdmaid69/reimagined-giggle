f6166d49ba9b43e181ba09cc1426d20890d0413b
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:40:15 +0000

    beyond behind operation fun dish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
