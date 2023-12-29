da203da89bcf5eaf43c988993330c690f6f46d18
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:33:45 +0000

    soil word certainly release friend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
