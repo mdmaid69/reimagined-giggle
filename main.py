e363b035fac378014a551f68d82ec215d2cd1962
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:19:02 +0000

    us need marry attention tune

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
