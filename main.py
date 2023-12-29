95f8a4c2b30ee26ce7b177a06ddf7eb6ecc3c281
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:05:16 +0000

    twist surprising physical bush place

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def is_even(n):
+        return n % 2 == 0
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_memoryview(array):
+        return memoryview(array)
