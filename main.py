e5f4a4475640ce9bbe88cf717db3a9dad95efa6d
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:49:43 +0000

    upstairs sentence remove gooff normal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def multiply_numbers(x, y):
+        return x * y
