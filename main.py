1cfadfb0f3f3b9b6c5385f7cf476a1fc68ac0e57
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:52:23 +0000

    Jj perhaps ever possible turndown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
