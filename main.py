8fac706185994ffe0a42e8123e177125f287d0bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:10:52 +0000

    somewhere Vv interest sight delicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-print(math.pi)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
