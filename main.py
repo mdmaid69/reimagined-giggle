afd27184db13bc6d83caff38b8239426e8f3a3cf
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:55:28 +0000

    commit marry as clear survive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
