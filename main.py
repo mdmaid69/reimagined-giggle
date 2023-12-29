d5dca89ef49a74c2799380f095155c862bba0a7d
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:28:50 +0000

    Oo put burn well branch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
