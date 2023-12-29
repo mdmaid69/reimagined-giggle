11bc2049e02fc69ba6a487d32900d55660cedd00
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:33:57 +0000

    mouth unless broken fetch relative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def count_elements(lst):
+        return len(lst)
