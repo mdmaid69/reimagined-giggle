c187dbc6bbab748a72ee9ea72053196f13556691
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:13:31 +0000

    fork recognize cross short eager

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
