17f19443ac70305ba2212b890689c21e9a020a94
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:55:27 +0000

    turnback wave base bar when

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
