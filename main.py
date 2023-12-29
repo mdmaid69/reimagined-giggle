d4dbe6ed85d8371940d072f11e7e3aba9ae4658b
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:01:40 +0000

    knock ball please usually into

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
