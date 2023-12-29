9c12ec08539a64f6124f8a0d8f4dac4c797810c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:03:11 +0000

    earn will involve goout fresh

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_area_circle(r):
+        return 3.14 * r**2
