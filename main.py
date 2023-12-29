bec5f7a8f88dec9a2b4df837901a048f2eb341cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:01:54 +0000

    of active goodbye talkoutof but

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
