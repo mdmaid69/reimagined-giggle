4021cf20e6624f26a89683a1ad2101e579006e1d
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:54:29 +0000

    traffic union complete nowhere mend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
