d9d47187674cd6ad8043bd2a11b9b34f39adde2d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:57:25 +0000

    climb opposite repair regional street

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import collections
+def create_priority_queue():
+        return collections.deque()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
