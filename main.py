d0a26b9ae914a7685beff34ed9331563ba467a7d
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:11:00 +0000

    custom onto injury shop goup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_min(numbers):
-        return min(numbers)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
