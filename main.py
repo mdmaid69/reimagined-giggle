7acc605add5cd2046c90f616ee1e400090a6d445
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:01:19 +0000

    should lie nowhere exact grade

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
