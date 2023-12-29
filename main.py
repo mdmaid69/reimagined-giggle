c480e3ff090f188a3b9f54fd3a8a5b38b4ccee42
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:01:50 +0000

    worse hold sign record combination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
