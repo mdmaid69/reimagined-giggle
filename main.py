be91ae20ba67c04c4dfa0819373c4b3da89af577
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:31:56 +0000

    safety error once calloff artificial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-x = 10
-y = 20
-print("Sum:", x + y)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_power(work, time):
+        return work / time
