cd60c4ccf8d748961df07cf31d8223ad0d0cf0e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:55:02 +0000

    thumb cross sort children thinkof

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
