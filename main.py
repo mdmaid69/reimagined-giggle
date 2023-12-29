f5e4a5bdb67c8ce6b6b9bb308990f1150d5f5136
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:21:17 +0000

    listen getoff feature row ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+from collections import Counter
+print(Counter("hello world"))
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
