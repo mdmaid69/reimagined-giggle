f0ce7dacb1596b1733b25606e7999fa3dbd88333
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:47:02 +0000

    wheel on exchange cheque stiff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
