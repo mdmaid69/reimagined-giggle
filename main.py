041b61d1036ce4fc691178729bb1208d645be4ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:07:02 +0000

    wander anywayanyhow party breathe success

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
