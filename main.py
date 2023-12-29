b4adaf0492edd21b3d868d285d0c2399a8eaeae4
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:26:39 +0000

    obvious noisy to soon problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
