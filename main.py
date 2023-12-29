cd2f5cbac4c12c3c8f49373b6f89ae22c66a0740
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:25:55 +0000

    goodbye surprised protest convinced genuine

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import os
+def remove_directory(path):
+        os.rmdir(path)
