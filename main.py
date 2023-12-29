f916801bbb541751df71ab4694f4ad3f18e79b2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:08:14 +0000

    keepoff runaway workon operate once

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_speed(distance, time):
+        return distance / time
