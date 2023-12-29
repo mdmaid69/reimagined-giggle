0976b8fb8ba465b05ee432357e50b41cd4277a32
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:19:34 +0000

    someone criticize legal deep sometimes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
