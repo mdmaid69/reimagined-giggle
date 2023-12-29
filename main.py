3833fb8dd324226d1c7fb981784e1ee308aeef82
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:12:12 +0000

    finger allow bring afterwards our

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def is_even(n):
+        return n % 2 == 0
