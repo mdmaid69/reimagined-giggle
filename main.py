59d01f07a0f3b27551820c2a82a97dfe0d606987
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:29:11 +0000

    grow calm embarrassed whenever Ll

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
