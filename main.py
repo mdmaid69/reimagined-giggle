b278c472e2e22ce500ed0222f0cb2bdd6cad0fdf
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:02:56 +0000

    cool callback leg comment active

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
