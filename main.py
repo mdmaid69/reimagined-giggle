13ec026a477fb87d18986026befbacc1f679177c
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:27:11 +0000

    battery head private consist thin

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def reverse_array(array):
-        array.reverse()
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
