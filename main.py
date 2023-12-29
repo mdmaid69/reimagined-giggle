b106f4fa8f51745c9afc9ce16edb7fd76aae85f3
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:56:26 +0000

    often sky slightly into paper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
