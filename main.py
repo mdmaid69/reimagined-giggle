430cd8a5374a376c766135823cc5db524dee57a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:36:56 +0000

    state stare earn confuse pick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
