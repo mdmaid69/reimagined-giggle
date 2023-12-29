5b33c645bc9556159fc8c0703fc4c039b2e55785
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:20:05 +0000

    bye brush care wash bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
