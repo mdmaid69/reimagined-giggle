2ce5024072e9ab12792538736e9a5aca640d9683
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:36:52 +0000

    design keepout match experience light

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
