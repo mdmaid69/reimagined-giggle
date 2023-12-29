382765337c1840392be672302808a9cb7e45bd1d
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:35:05 +0000

    level 567enemy share mean behaviour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
