33d3906e9918f85696599cff824ae7d738aa6071
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:41:42 +0000

    line behave attempt month pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+print("Hello, world!")
