78142364836366e6367f3eb79a56fd95ebd99ff8
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:52:48 +0000

    equipment computer evidence confused bringback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
