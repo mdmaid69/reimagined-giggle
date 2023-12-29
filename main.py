c8a3ff20b4f932853636cdc29797574f3e6dfb8c
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:47:19 +0000

    service accommodation field will wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
