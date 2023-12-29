bf8e42e224a1b4e0d876537bd56e34e1d43a1e62
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:04:06 +0000

    guess today his rain meeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
