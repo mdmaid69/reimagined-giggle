bc2679a6997956bc3f84a44a6752ff66ae344f82
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:32:17 +0000

    Zz fall design frighten quick

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def remove_directory(path):
+        os.rmdir(path)
