370ef250dfe671f1d74a5a2a1ce520c93746e1e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:53:35 +0000

    medium thing destroy justice argue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def find_min(lst):
         return min(lst)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
