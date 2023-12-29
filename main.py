e800f565a2926d9f1709ff76fb2f2f609c67351f
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:16:43 +0000

    over relative moon pinch save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
