93a121c35a078659b37f993b17bbf32ce2d5a6a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:18:14 +0000

    subject tomorrow think tonight drop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def cube_number(x):
+        return x**3
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
