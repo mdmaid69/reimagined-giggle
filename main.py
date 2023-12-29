6da03a41a15079767f52203deb520193b9961862
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:47:42 +0000

    contain please wipe 566end noisy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
