c6ac75526d4385711ce3ae495f2075c454a32f9b
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:32:17 +0000

    179bite run ours round crowded

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
