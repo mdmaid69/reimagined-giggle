afc305fd5b87bd01e42ac168024313926129e342
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:49:02 +0000

    tell music admit shut fairly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
