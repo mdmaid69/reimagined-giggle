6f75a41c07c252deb0832fb17662ee55738a8899
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:54:14 +0000

    confident hers easy other plain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
