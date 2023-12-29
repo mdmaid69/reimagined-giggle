21bb4c86bf04cffa79a4a4ed7a88ddbc8d9b00f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:48:35 +0000

    any label now hard much

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
