22573a2785c03137cb44a3e1fa35dec4251427e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:29:18 +0000

    communicate runaway field region able

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def square_number(x):
-        return x**2
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
