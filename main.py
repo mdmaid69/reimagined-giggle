cd541eb2c4c97a5513d3aecb83226fa7b3e20abf
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:16:55 +0000

    everybody knock tip honest discover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import tensorflow as tf
-print(tf.__version__)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_as_set(array):
+        return set(array)
