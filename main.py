3f06ee373c9db13615fe4caac57ee4d49087dd80
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:27:22 +0000

    start law insist attitude flood

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
