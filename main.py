b583a5ea18717f3bd32c8162e4669932c8f6dd2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:40:34 +0000

    pencil flow content nigga Kk

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
