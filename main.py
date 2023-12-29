8815a321aeb717f39d1f3506c7a2b52cf4a5f0a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:31:09 +0000

    in image handle national still

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
