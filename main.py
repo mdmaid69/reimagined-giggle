a477166faf48ed5860fcc1501277994977be20fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:58:34 +0000

    flat could closed interest impatient

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
