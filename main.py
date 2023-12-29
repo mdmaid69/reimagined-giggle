27ed2ed4eb730e41ed0265e06f5df2fc577e9fce
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:07:27 +0000

    black correction complicated forgive correction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_speed(distance, time):
-        return distance / time
+import sys
+print(sys.version)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
