add0127266ca18e40b71ae7c1592df7b9709af65
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:34:04 +0000

    possibility wish hit likely nothing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
