1ff9cf72fffd7f6886d2ebf54bc749f2d7099ec2
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:22:27 +0000

    people region danger slip plan

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
