90490dc5c3166e7e2b87d6fa709b237899119a76
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:26:28 +0000

    whom paper tape frighten mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
