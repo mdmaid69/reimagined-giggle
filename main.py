f611b3ffe4e3894305de82b877b1f54f781d88b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:58:36 +0000

    sensitive obvious recover Ww attract

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
