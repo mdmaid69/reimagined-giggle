e5a4b9db2696eb378447aff4214b24b2434f5c43
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:30:45 +0000

    prevent along bad lost any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
