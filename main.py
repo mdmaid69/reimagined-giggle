db6840918094dfdfcadd1ae52782c66b8bfb6dfe
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:23:30 +0000

    loss sure nurse wish later

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def cube_number(x):
+        return x**3
+  def square_number(x):
+        return x**2
