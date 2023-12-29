93af84014b33e00a3a994b9efd055aa68e180d80
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:03:45 +0000

    ring achieve journey almost long

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import platform
-def get_python_version():
-        return platform.python_version()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
