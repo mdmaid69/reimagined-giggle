213821ff1147ae7361711e2d498394205789ec89
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:13:21 +0000

    mine me hurt Cc above

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
