f0c9b92db25a5374c9d0e53a39ebbaa2200d53c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:48:07 +0000

    stamp fetch free fresh raw

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-for i in range(10): print(i)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
