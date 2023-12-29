85871826eb9db9fb56a0b78a5760e6bbcd3cb2b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:52:29 +0000

    arrive cap complete operation reaction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_acceleration(speed, time):
+        return speed / time
