c59e711a84b7f548468492073a1da3dc7f46c7df
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:02:23 +0000

    himself rise interrupt strike however

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
