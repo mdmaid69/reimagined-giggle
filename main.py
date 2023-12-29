7e15443a5cfa9db1b2e7a21b97fa1c334f12f6c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:09:46 +0000

    whisper autumn happy half indeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
