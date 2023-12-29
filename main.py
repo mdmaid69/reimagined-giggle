761a1ecd8db22aa971006102acab2886f78228a8
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:33:00 +0000

    drink tidy gate story pick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
