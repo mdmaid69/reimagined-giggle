f50b4003887ef2e07dd7ef1153352f6a2de8aa59
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:53:55 +0000

    hold little rarely early window

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_area_circle(r):
+        return 3.14 * r**2
