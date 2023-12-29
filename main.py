810e19c9a039a0818033f251e1c7d73643a26c53
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:27:55 +0000

    choose boring key slip thumb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_unique_words(sentence):
+        return set(sentence.split())
