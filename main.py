a00fd38dab0b098906ecc08853afcc57d6237ff2
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:47:42 +0000

    wander glad hot ashamed depend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
