e05480aa88b9ae0812a39c2061ee989fcb592b0f
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:43:47 +0000

    agree unfair innocent worse grow

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_density(mass, volume):
+        return mass / volume
