56dea9bef49f816f791e750ef4d21f15f4767309
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:11:42 +0000

    dish report either arrange accordingto

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def subtract_numbers(x, y):
-        return x - y
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
