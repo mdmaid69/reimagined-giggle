91c331f995b747bf9b1cca15b27277b7c1261736
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:48:13 +0000

    though unkind nervous advanced winter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
