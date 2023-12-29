2ee380a02716f6971cd488040f37634bb4de9c9a
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:19:37 +0000

    cope hang aware welcome rubbish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
