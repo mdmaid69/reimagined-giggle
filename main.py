366198c07ec78e327aac71c041c6cdb8d9fa0dfb
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:16:39 +0000

    waste alive letter equipment recent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_tangent(x):
+        return math.tan(x)
