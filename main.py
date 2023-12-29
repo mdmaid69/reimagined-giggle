39570fef3f729bfd0f1cc938837a34672ece06f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:03:06 +0000

    view moon pressure whichever Ll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
