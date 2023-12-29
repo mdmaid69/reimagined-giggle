350b22f74c608795db0430883cd5279fe556b66c
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:16:20 +0000

    drop under knowledge pour throw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
