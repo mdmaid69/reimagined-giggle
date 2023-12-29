b9eb2adba78e8c24ac2be365594efd391c4bdad2
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:39:23 +0000

    shade opponent discussion this contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
