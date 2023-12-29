acecb7d96c39900df70894ef8bc5f233e02273a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:13:41 +0000

    convenient describe law hollow several

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import random
-print(random.randint(0, 100))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
