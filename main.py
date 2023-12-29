41d185286dc23b893b7c06ffd188b1d2a7b03b4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:06:39 +0000

    order majority season listen message

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import collections
+def create_user_string():
+        return collections.UserString()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
