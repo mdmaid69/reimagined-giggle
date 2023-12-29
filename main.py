8f46f39aa70a698717058ae865fc6761da5ffd8f
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:55:24 +0000

    female expression gentle edge university

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_repr(array):
+        return repr(array)
