7302f834695ff1c27dc9d75ae2144b1ac8436043
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:09:36 +0000

    previous total dear slightly difficult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
