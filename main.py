13a34fd30b28908ecf17022a331228e2969227bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:31:00 +0000

    round return list authority single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
