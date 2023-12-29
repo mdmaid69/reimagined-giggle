a55b5eaa61b60ca3a8aa7046f9afe7b47c9f62ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:14:42 +0000

    lose strict permission neither choose

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
