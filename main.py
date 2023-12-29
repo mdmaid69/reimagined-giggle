df0539d7d7373dd95851922c18acddfb1dca9f6e
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:57:25 +0000

    clean until flow arm wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
