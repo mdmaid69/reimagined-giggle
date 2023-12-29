e51b8071498bd154d2b77200b91cf68be8a5df7f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:25:46 +0000

    land forward realize shade Nn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def reverse_list(lst):
+        return lst[::-1]
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
