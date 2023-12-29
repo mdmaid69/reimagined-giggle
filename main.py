a7b74022078d21847ee8e09bdbd9cb866551cb49
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:41:54 +0000

    wet shut opinion up case

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
