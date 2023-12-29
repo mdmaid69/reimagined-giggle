9e8d0d030f9c5bedcee6fc14692838dca73f1ca9
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:50:15 +0000

    remember medium travel spare about

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  def is_even(n):
-        return n % 2 == 0
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
