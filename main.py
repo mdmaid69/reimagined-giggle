b9a9b999fb222db17e7f218134dfbd1e4f36f269
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:21:28 +0000

    forgive right chase when scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
