f361b2a099a83aaa437769255c7e70019c1409ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:28:56 +0000

    dinner or bet next uniform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
