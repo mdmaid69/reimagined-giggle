7063afd65d5d04235e20311077315481331f8735
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:12:05 +0000

    health border Tt very comment

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
