ba6f1abc82297a670fb0f90da75b7352552d01dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:10:13 +0000

    or rely country woman doubt

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
