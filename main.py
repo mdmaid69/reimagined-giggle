c48767b4f20b683040727d90fd662d276ab4ca40
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:17:58 +0000

    criticize too thankyou point weather

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  def add_numbers(x, y):
-        return x + y
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
