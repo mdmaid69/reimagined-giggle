d2cc88faa45e91842c52eedfcda2838d01ab1639
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:32:01 +0000

    tense excuse worry person that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
