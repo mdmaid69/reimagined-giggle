4c3abf581105d92c1b958bce67e41b40c8f6caef
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:10:09 +0000

    order action uniform ilovedicks imagination

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
