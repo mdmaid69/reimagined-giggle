8bfb2ca463fcae5af100466840f709cb82d5e7a8
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:45:57 +0000

    extremely news government mark pain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
