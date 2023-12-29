39c47d874d00862bc531d42a064d99c6c15635da
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:33:09 +0000

    bet sea tool depend decide

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import random
+print(random.randint(0, 100))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
