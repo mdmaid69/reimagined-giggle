9e39be1a5540549565c02ad0b0ec1a1c709b6c37
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:13:03 +0000

    instant count minimum hungry exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def split_path(path):
+        return os.path.split(path)
