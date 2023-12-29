8f1bedb9703eee704ce7a9eb6973103341fd5c63
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:50:58 +0000

    badly difficult guilty attitude curious

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
