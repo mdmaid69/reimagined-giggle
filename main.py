d2b91e79fb7360c57eba07faf4a2a454d88135ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:12:17 +0000

    complete off exercise motion club

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
