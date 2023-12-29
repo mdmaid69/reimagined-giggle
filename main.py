e078d74de092c41db49a80f32aba0c2dccf07f25
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:53:15 +0000

    why afternoon walk gas certain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
