322b2cb4fc78d83dd7c6e2b6aff0dbe3e49db055
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:24:36 +0000

    flow talkback age replace extra

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def square_number(x):
+        return x**2
