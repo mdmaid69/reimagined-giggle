f38d8ee63cf83a6f17a2e021ad18f4cfde4a92d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:02:29 +0000

    cross hope unfortunate charge rent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_bool(array):
+        return bool(array)
