3a4374b5190cffa70c14c56d9d1dd9333ba90ae1
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:15:39 +0000

    until frightened any fast feature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
