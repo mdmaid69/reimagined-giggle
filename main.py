b81ca1e6b4cce4349b3e654696700f5bbbb81fe6
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:39:31 +0000

    smoke every spread light shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
