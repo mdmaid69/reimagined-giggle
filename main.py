2e4518c247db49bf0760f7f3935736d495d6065b
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:51:06 +0000

    politics abroad home just decide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print(sum(range(10)))
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
