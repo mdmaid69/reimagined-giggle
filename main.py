5206788102a3e823ccff2e1ee7172959956b30a8
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:49:25 +0000

    guilty familiar phone guilty crazy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
