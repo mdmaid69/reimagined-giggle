d09be2237ec3030d3762ab70206e70a91712e2dd
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:45:28 +0000

    relief wide war amount crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+print(datetime.datetime.now())
+import array
+def get_array_as_int(array):
+        return int(array[0])
