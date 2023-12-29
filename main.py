42243765ca17449b90065e6d8ee68fa9c963893d
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:05:10 +0000

    original even artificial further live

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
