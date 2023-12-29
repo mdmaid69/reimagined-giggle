c499ee63078f5bd5128ca302d67249bb2c5a047e
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:55:30 +0000

    permission sport character left fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def get_array_index(array, item):
+        return array.index(item)
