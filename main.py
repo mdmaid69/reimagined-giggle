ef2020793ab34d96d06c30e1ab4aedbf451894cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:27:52 +0000

    direct easy range crowded react

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
