885d7f59cceab8fb05aa97edef8d0c5e1f78a74e
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:30:11 +0000

    deal window concerned excuse callout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
