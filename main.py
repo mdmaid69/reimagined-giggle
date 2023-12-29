30d0788647aaecee5bce58ce1721f90bf4ccc6ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:51:01 +0000

    shadow he after partner shallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
