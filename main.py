05c278babbe2ebfcdb8390442e8ceea12a812fbe
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:09:39 +0000

    declare high disgusting sea experiment

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
