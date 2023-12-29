b2a9101d803eec247061e7ec769a06847d4b7656
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:13:32 +0000

    occur may getback record excited

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
