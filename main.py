eaa10828e45e31eda2b49846e3abae8f9e742391
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:43:51 +0000

    children guilty ever land cope

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Uppercase:", text.upper())
