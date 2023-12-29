0862b5d3ef836e06a49f3baf7ce0fbef058200c8
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:39:28 +0000

    later control lookup month narrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
