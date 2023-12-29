e73b38534bdbce02fa37d049d82cea755a34b049
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:56:58 +0000

    language Zz cover dry afternoon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
