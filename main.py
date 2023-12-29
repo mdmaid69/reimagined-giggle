f5712dd3a6e34e95ae1fa3268bdb2a1340a1fd8b
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:56:11 +0000

    organize control office ought appear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
