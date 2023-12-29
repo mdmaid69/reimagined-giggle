67cf04cbd1f8dd706d7d4f2729c7c36a2c267b85
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:35:06 +0000

    strict stare rule sort each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
