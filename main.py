daf32bd56fd55b803b9ae5b261f07100d766c831
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:54:14 +0000

    office 564encourage ability thought whistle

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
