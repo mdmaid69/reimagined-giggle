2e82b2a5f11d6981ad668c96f3a67dcde8023b1a
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:29:14 +0000

    comein influence activity hit pair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
