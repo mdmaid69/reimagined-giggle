f998ac1682e6eb6ac3465d76dc88bcab70d7577e
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:39:22 +0000

    instrument custom against extra off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
