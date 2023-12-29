0f4db0c1ab36aa7ad38e1e6a5a41447d8eb41786
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:57:49 +0000

    chance actual goon meaning total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
