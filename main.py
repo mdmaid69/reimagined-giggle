bd4ce73960f3b23be3e73a395e442f6bf9534926
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:44:44 +0000

    giveaway comment moon scare brave

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def find_min(lst):
+        return min(lst)
