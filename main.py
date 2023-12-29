740a3d3a7b679172ea759f1f24fa0fc41f24e619
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:29:23 +0000

    report makeinto some neat would

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
