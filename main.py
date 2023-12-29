2b43ea96258251594fe6a1e99844630b40ce737c
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:22:35 +0000

    defeat bound run direction takeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
