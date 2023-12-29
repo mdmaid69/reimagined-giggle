0c9e3b2560e9b08db8bbc109b3a4f9d8656c74ba
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:52:05 +0000

    task bright joke copy book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
