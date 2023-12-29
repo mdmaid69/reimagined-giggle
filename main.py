f81516637fbd75575d9ff40dccc14585764c17b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:24:35 +0000

    post cold properly person themselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
