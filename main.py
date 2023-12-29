7d3501982805b90d9d342fcaa76298c1c49a8dee
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:46:54 +0000

    aware month still since keepup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print("Hello, world!")
