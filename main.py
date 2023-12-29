ed976846ddbe14ecf5eaa9d4cc43bcfe1144d40e
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:07:42 +0000

    part affect receive taste callback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
