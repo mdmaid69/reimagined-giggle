609e4389ec27ee35080c6e4881c84d1526e78da1
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:19:46 +0000

    accordingto wire shine quality keepout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
