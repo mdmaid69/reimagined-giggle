4b95b1148595f63bd071a0a806ac0e5c7fc0badb
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:50:04 +0000

    person date future worry priority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
