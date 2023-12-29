dc61d0bf2ad13e5cc657ef9f012afb1399bf070b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:08:51 +0000

    contrast comment realize shop confidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
