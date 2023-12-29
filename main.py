d1c23056ebf62b52634c454b90082e5dd9fa5795
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:43:55 +0000

    through Ss difficult cinema tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
