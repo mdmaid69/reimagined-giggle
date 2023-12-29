846568938aa9156b4e68e191b99196e782fc23a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:27:52 +0000

    clever draw comment set unusual

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
