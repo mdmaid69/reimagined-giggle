cc7b8a66b76b0b6423d54912bfb102134d8c21d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:12:34 +0000

    comment yourself stop meet after

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
