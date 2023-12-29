9232fbed855934cfe45cabc065eadfc41f2c51e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:03:53 +0000

    share test impressed concern object

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
