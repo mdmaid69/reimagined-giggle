7b3eec2cf0c4b405c0f6df49a8e8ae73b99c5775
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:54:38 +0000

    drop clear recognize high legal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
