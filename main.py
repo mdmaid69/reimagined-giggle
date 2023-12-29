16f74cd57bd07ea45195d8ec0113f104e651ffbb
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:28:33 +0000

    company freeze spare road tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def divide_numbers(x, y):
+        return x / y
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
