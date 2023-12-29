1a48907e6d7dd7781e8f15ba39264a042c557e45
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:15:38 +0000

    crazy reply hair chase another

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
