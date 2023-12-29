4774d3421c575490a187438044f034f03c9fc1b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:17:33 +0000

    trial my payment appearance slowly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
