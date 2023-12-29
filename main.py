f8a2d811422f6777530fd44376dee51ad7a28ba0
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:15:37 +0000

    attract hang bring turninto pure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
