3cd11a19e2e75ff6866346832dcac95a75f001e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:19:08 +0000

    correction rough scare stop sister

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_work(force, distance):
+        return force * distance
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
