856cbf6abb00ea80427a9aa18494fc6956a17ee5
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:49:55 +0000

    majority aware represent connect free

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
