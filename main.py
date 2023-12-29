2bf2a8e028891a71e7c5200d703ffe97da901e7f
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:58:38 +0000

    food brain travel either rid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
