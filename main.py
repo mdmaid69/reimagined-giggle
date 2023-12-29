5e875c18b8facf8dfde61f113bf9dc39beac7db2
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:02:50 +0000

    check hurry equal illness land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
