af72f7376f87c7e2f2e1c4287fa76a36fa0c8e75
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:36:11 +0000

    compare tear centre arrival shoulder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
