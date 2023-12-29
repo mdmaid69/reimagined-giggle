2f211e82139646c632fa7eb8aed4a5124dd75462
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:59:19 +0000

    frame rest attach reach practise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def create_user_string():
+        return collections.UserString()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
