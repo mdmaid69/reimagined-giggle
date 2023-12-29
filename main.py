bcbab591fde93b9f304b0725306b05bdec3e94d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:51:56 +0000

    feature tone bush sport early

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
