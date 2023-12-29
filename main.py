00c00196f462242cc7b97c3a1cacc6252fe1c056
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:20:26 +0000

    yes live wish swing punishment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
