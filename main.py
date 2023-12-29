0b09675740d502b798dc9e57ded65db388877d05
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:36:43 +0000

    computer offer pinch fine disturb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_work(force, distance):
-        return force * distance
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
