85ff08e6db3177262492013b67459f55c5c70b36
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:01:40 +0000

    journey apply dry runover effect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
