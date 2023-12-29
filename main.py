b54aa7334f8563f66e4f1a595805fcceef21bfbf
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:06:34 +0000

    holiday sell allow natural 179bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
