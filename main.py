7ad3ed0d7af26cc4d8a507c5fbcb55453e9483b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:21:42 +0000

    afford well rescue trick break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import itertools
 def get_permutations(iterable):
         return list(itertools.permutations(iterable))
