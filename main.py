e8ee777b1892d4f4532886576837ee813a194bd9
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:44:41 +0000

    capital translate among thinkover powerful

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+print(math.pi)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
