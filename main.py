ed7115f87d28185673c11321c731acbcfb85d586
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:00:58 +0000

    together drink unite lower invite

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+name = "Python"
+print("Hello,", name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
