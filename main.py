5b26fcbd06ce3cce76e67b83ce1248e2bf024cc7
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:45:23 +0000

    sure anyone none name when

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
