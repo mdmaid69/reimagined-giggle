611bf7067b0c9e0b73a2baff15e21424cb8bd345
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:58:51 +0000

    dinner wild into medium over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
