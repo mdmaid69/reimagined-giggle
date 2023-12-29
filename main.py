e3980e35030166c12d6f2cd80f08d69470c8888d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:26:03 +0000

    broken bringback takeover waste clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
