f53e857e94116219029d499146f35741cdd8bbfb
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:20:55 +0000

    shall bill yes innocent curious

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
