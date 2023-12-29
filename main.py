3c2a0900f9dd4cbc124e5925500f1ee6202a7ed1
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:29:44 +0000

    speak born Itself maximum on

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_force(mass, acceleration):
+        return mass * acceleration
