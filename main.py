7e4191a278ca06264f8aaa7624a6d0839853e46e
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:55:42 +0000

    both state perfect game mad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import platform
-def get_os_info():
-        return platform.uname()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
