c9ef3e4e05214220778d54af2f6a4b0caf48393d
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:54:04 +0000

    crash back smooth until stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
