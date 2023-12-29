df9e0a81ab25a68f520ec9a8d5c5bd13576d6037
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:54:24 +0000

    after live stone nose best

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
