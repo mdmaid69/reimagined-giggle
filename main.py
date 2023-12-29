931146d3ae7f15502ad807435215718d819dcaad
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:20:38 +0000

    partner difficulty talk profit since

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
