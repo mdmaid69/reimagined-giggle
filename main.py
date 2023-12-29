61e9086735478e49d67d8c6a291e7fd8674415bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:37:21 +0000

    compare risk godown harm funny

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
