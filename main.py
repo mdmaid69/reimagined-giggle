7b32e511cf8266434cdcbecad72d0c94327c250f
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:32:31 +0000

    sticky anywayanyhow favourite target spite

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_index(array, item):
+        return array.index(item)
