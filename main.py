a7021549c0af53c63abadfcca2fbdcf41bf3c0e8
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:49:51 +0000

    female kill confident workout cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-name = "Python"
-print("Hello,", name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
