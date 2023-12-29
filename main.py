83ec987617ba1cecce429aa75d0b16dbbbd180f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:28:25 +0000

    total we purpose so check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
