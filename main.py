be4e8956737a40bb96eb4146b83cd976c7cbb761
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:50:45 +0000

    fall night rise us detail

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_acceleration(speed, time):
+        return speed / time
