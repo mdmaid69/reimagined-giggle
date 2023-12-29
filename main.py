a105f29e421dbdb446177bbaa71c5bd4da053a68
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:45:45 +0000

    difference share mention run mistake

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
