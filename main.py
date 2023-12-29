3d1ac785c00e4d743dc9f5f61b648a049580339b
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:30:34 +0000

    bit free subject Pp drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
