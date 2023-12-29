3a975adcf3143e391ca33b90983350402acd7dd5
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:44:24 +0000

    see disagree suggestion suddenly mine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
