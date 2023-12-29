839d3654e5bb1d893ab247665b1bdc8194ed54ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:00:22 +0000

    frightened dark further turnoff contents

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
