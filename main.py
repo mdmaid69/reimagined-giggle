21bbab72383e2277c4c38b845ce892c2cda87890
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:03:52 +0000

    in careful dull issue fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
