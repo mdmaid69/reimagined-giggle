4516093618f22105ca4438acb9d9e6ee8f7d6b6b
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:47:48 +0000

    accident father shut grow tune

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import json
+print(json.dumps({"name": "John", "age": 30}))
