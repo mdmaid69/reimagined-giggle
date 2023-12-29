b9875e80c5c3a3fb6f5da9d31d30bab8bdad7742
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:24:34 +0000

    whole term send fortunate shelter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
