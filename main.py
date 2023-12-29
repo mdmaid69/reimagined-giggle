4ee53e7dfd75ae4809bcfca61a97fdbee3a36ae1
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:33:43 +0000

    give middle run badly myself

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
