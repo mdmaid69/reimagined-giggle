f44c883d15a65232a307c8d04c3c13b2000ae148
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:53:47 +0000

    goout prevent play sort push

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
