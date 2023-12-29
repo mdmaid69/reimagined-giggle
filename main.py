5bed73c89348aefc835f027cac120e703f586225
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:42:55 +0000

    beautiful holdup good time crowded

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
