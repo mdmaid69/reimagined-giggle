287c7404b99e3410486187ce04cecd50f8720133
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:38:24 +0000

    easy professional flavour join dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_item(array, i):
+        return array[i]
