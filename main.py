17a8b526fc98e5ec0c29af5a4de0346f78939c22
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:26:22 +0000

    mark somebody scare produce thank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
