9e1b1f820ed28e63bf3134233b91259eaae59b69
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:55:27 +0000

    ever soft door maintain plastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
