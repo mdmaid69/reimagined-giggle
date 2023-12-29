af88a75a886d1a883bc2507733a740c22df74043
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:00:22 +0000

    healthy worried nerve touch delight

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import sys
+print(sys.version)
