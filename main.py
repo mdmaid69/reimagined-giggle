d915f6895c6c2a379263ac69ea58eeef74bb94ca
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:06:58 +0000

    effect train ilovedicks back fair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
