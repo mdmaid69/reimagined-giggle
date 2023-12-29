78cc71a72a29fdcafb323b33f3489463334ce1e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:18:02 +0000

    dress block sight runinto talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
