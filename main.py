27e4647c44d21637821b3d1b1c7e6be9bd609c0c
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:55:08 +0000

    board suck rent repair common

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
