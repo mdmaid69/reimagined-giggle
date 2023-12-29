c55aa05e0169384af2c95a599be01cefb833bc81
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:48:44 +0000

    complete able pull turn congratulations

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
