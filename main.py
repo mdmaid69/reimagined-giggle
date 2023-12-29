d013e8b37960edff18721fa3c32d6560db355ff1
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:20:34 +0000

    attach test environment failure scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
