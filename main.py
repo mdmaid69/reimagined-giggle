8d6c9d3218ab42d349848476d43360e72f0fb450
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:36:48 +0000

    able pass turndown fact will

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
