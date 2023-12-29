c3a09811fdcdb3bf6f01371e9804602078af24d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:41:29 +0000

    Ff difficult interruption lock join

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
