4f44cc26102f97dcf720291e254e99fedd2aaff1
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:16:08 +0000

    free most pause write themselves

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+print([x**2 for x in range(10)])
+def cube_number(x):
+        return x**3
