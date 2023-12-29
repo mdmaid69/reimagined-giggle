baee23a7f8f7b31ae7e08c2a286c0bc0b54044ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:30:16 +0000

    basically choice condition forgive once

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def subtract_numbers(x, y):
+        return x - y
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
