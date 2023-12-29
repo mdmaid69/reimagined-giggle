91d826193ca746e2c155d4a0a4349456fbbc5541
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:17:02 +0000

    listen creature please join callup

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_area_circle(r):
+        return 3.14 * r**2
