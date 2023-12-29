bc64aeee23c84542fde609aa02de355d697916de
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:53:29 +0000

    attractive thank hurt contrast card

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def subtract_numbers(x, y):
+        return x - y
