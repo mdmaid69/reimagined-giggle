3bd4e4430fdde985b4607c228225e657b77d5d66
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:14:52 +0000

    loan poison down programme keen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
