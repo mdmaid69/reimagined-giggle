8c44b032a869e4ab1f05d3717ce09c4caaf2bc9b
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:10:17 +0000

    month neat father drop habit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_bool(array):
+        return bool(array)
