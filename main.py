19933c357ddc7d4b0eb4d042f55c7a59b9f23ebe
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:23:39 +0000

    practice birth lead practise clearly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+print(time.time())
