472ae8c7ea25603656ee9e47e06bb566ad6e2d66
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:31:05 +0000

    smart company affect boat recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_slice(array, i, j):
+        return array[i:j]
