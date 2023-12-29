07b029a2770af5c959efc8af8d392dd563c301ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:55:36 +0000

    guilty pure pretend safe wake

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+print("Hello, world!")
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
