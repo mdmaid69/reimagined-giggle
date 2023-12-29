a1707f68736f81903c4dba98076caaa4787a0414
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:39:35 +0000

    pleasure speed roof well crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_str(array):
+        return str(array)
+import numpy as np
+print(np.array([1, 2, 3]))
