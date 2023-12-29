ef0a7a378de50833cfdd25331ab17a56c43ef500
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:00:04 +0000

    last emotion platform direct count

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
