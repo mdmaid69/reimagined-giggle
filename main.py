8437d69d6a6ed3124925d738338dafd7fd291248
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:17:55 +0000

    bunch communication satisfied practice tradition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
