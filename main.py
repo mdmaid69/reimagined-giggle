966ee821981b7333bf43a80298992b93d97992a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:45:08 +0000

    they tone face turnoff ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
