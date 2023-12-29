ed60cd89023dc17f2cdcebc961943e134b16ddb7
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:48:10 +0000

    because home discover Ee exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
