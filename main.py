cd0089a2e1090b41517d44fdc008e95771036575
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:17:31 +0000

    vanish question stop illness enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
