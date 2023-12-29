4996831ee3fdb2ba7e4e4a13a401b39656fccfae
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:40:58 +0000

    centimetre certain performance sink comeover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def greet(name):
+        print(f"Hello, {name}!")
+  def add_numbers(x, y):
+        return x + y
