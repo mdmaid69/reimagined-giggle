7d439eff6647c928f9ca85162898dd661feb51cf
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:00:43 +0000

    influence protection match present write

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def divide_numbers(x, y):
-        return x / y
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
