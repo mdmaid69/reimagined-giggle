15ca2d24713d256cb768f7a7632910a96161a487
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:26:35 +0000

    damp professional complain power height

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_area_rectangle(l, w):
+        return l * w
