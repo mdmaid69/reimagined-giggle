6251af00ca462bc4691207d6a2924fdf87cffd17
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:18:18 +0000

    genuine until great pipe brief

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_index(array, item):
+        return array.index(item)
