c429571984312c19cb762bec02001d1836c0a857
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:24:39 +0000

    event bill nowhere begin choose

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
