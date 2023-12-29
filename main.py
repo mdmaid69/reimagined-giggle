b32e7ddb7ef4ddb1574828c4f4ee4a32c5678722
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:33:25 +0000

    invitation nothing crime lucky note

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_typecode(array):
+        return array.typecode
