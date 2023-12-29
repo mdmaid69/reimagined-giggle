a0aa817d78be8a75aac8bdc1a29ce72ebc41a98e
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:20:40 +0000

    dull grade pin recent news

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
