c4da75c80e18fb93480912b25ce33438b145faf1
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:41:27 +0000

    neat estimate result medium control

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
