e311d67fbd5e3ae2d4e2ab47ac875289276762e3
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:30:59 +0000

    skirt brief doubt if please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
