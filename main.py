cff4943e8bd0587d2be4bb6fe4876645a41e33fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:06:31 +0000

    personal wish cum soil worse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def find_max(numbers):
+        return max(numbers)
