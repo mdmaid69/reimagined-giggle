d164a486cac7c3fef2de7641be4ca633f503981c
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:47:33 +0000

    female ahead blame spread better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
