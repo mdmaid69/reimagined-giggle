ec29765395b24dd7354316008dad23306e9402ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:42:55 +0000

    want male sea do position

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import sys
+def exit_program():
+        sys.exit()
