5ca5282fbc76f1a549d3afaa1c2efa0c98602772
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:39:34 +0000

    turnover trade entry meal please

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
