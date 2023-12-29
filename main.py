1ea3161c417d3caa034a4738f0fbe9682e4fc27c
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:59:28 +0000

    underneath impatient compare probably realize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
