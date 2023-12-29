c02f3affa57b51dba5d6f7f8e200c32232d9af74
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:45:25 +0000

    share voice pocket married unless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
