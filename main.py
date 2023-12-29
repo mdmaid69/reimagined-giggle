2709561aed8ab0fe7f689723e6a3159403991586
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:33:40 +0000

    substance partner pity admit more

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def count_elements(lst):
-        return len(lst)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_int(array):
+        return int(array[0])
