e2b336c296b23f551097033f5aba9b3aa4ff5d09
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:05:28 +0000

    stamp shut completely disturbing broken

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_current_date():
+        return datetime.date.today()
