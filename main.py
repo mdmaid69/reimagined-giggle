40ad7ae29f3d98cab6b4194733fea45916a763f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:01:40 +0000

    expect down display damage suckme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
