48f795c6823040c33018d1bc5020bfc222b17390
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:34:45 +0000

    relative tongue relief lean accidentally

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def count_words(sentence):
-        return len(sentence.split())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
