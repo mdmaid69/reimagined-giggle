9440b3af5f259f69b2601241a8c4dea55ea6b9b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:17:02 +0000

    key office sea load forward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_perpetuity(payment, rate):
+        return payment / rate
