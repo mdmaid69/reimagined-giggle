f9c73916c842d45d4c7439b7b1dabb1b8fb04cc8
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:52:48 +0000

    calmly talk realize because lay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def convert_to_hex(n):
+        return hex(n)
