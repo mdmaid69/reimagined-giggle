a39b209ab856ad790fa6ec2581e96508fdd618ec
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:27:03 +0000

    opposite jump name capital behave

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
