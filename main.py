6d9b4196054ebe8354996dcabadd42a3a78faab1
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:12:01 +0000

    instead trip promise consult cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
