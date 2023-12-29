a3c72cbaf41570c89d4f9efff866a5bf576e9d26
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:16:00 +0000

    test man stamp upstairs know

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
