93b84eb949be8fde6db3047169792dcc19309d07
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:38:42 +0000

    arrange judge present party confidence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import time
-print(time.time())
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  def reverse_list(lst):
+        return lst[::-1]
