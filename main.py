adb973edaccbd097ffd3a6a29224666505645967
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:15:46 +0000

    burst on gate disappointed round

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_list(lst):
-        return lst[::-1]
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
