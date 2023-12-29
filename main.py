ca6acfbc1d8ca4bcc9e3ffb19fc5dcac2d75a8a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:35:17 +0000

    our attempt next speak die

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def cube_number(x):
+        return x**3
