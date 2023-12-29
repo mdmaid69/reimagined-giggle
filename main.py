40c03f82fd3053e7bb078366eb00a60c1b218266
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:39:01 +0000

    stage pretend recognize lucky company

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
