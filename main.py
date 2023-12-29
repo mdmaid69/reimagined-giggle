8557883da9ed102e32ef9c6bc2cccb619b27cf3e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:50:01 +0000

    order fairly rather pretty escape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
