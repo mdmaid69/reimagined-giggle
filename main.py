bae8de2cbe39f61a9439be0221aaf63e9bf70d55
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:34:18 +0000

    measure difficult press tomorrow 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
