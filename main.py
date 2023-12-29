fb3c8f9119be911c72a3638eeb49ee20b50b959d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:35:18 +0000

    story want touch which maintain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
