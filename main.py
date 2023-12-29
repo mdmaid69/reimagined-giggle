75f8432b152a74366133e47709387bfdf45d7479
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:36:35 +0000

    accordingto pronunciation brush idea considering

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def count_elements(lst):
+        return len(lst)
