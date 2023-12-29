fdc971a4c11abfdde8bb1fb7b32d75f5726a7e2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:09:17 +0000

    wave somehow hit advise centimetre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
