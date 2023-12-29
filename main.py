51b56b34ab0632c32c8492e47165f0071e9f2b81
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:58:08 +0000

    spare law worry brain story

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
