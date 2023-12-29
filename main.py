5a87d6713689aefa244b1dac6e2dd9b6da75a669
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:46:23 +0000

    of even understand group stand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
