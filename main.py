15b226838656e03bbfef8106572455dba90822a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:49:26 +0000

    trust practise distance Ff calmly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def reverse_string(s):
-        return s[::-1]
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import datetime
+print(datetime.datetime.now())
