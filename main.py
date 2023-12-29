8bd1050edc3fc497e57bacdc309d8989c49e9749
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:24:03 +0000

    pin later little callback intelligent

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-for i in range(10): print(i)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
