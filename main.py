ff7f737041169935c50f88d68b0606932ff90607
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:03:58 +0000

    urgent less modern start getup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
