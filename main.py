9cdd304a7d85bc9eb17df852629979e809038c7e
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:54:55 +0000

    murder purpose pleasure poison estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
