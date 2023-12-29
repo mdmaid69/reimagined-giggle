2fade559514f0e642fe1e020edd4f275e71b2e23
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:28:01 +0000

    collect boat give guard cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_tangent(x):
+        return math.tan(x)
