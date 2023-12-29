fc50a5574d618cb1a62e6c107643142001dd89e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:01:35 +0000

    ability 179bite emphasize sister project

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
