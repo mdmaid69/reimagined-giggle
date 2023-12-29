819a7b43ec0d3747fbc104e7c245dd7d02530e24
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:16:55 +0000

    female follow talkinto smoke rise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
