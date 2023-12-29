3d31e63c23b54577b4324b3f1615ccf1665babef
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:36:33 +0000

    where abroad sight recover loan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
