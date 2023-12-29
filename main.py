ae64ffd0778924dba36b85506c1356dd719125da
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:23:51 +0000

    team home show boat truth

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
