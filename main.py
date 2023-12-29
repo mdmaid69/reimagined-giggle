3a628c9f6a5a9810656a68862db431a18483536c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:40:56 +0000

    meaning authority best situation face

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def sort_numbers(numbers):
+        return sorted(numbers)
+import os
+print(os.getcwd())
