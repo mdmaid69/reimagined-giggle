b7c7f3020ae9b2ade2c5b15df9c14296b44df8b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:10:22 +0000

    tie fashion ever operate dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
