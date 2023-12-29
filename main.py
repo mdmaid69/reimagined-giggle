e23e761225bb54f6b98d9d33fa9f883e1be13dee
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:06:20 +0000

    letter cinema major land confuse

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
