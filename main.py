04ea74fb5374653c34b369823d52783115dc2577
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:21:56 +0000

    cope beside flow proud target

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import os
+print(os.getcwd())
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
