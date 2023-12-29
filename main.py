21e21ad3fe00b6748c0b4932cb0dfc15bca4e0ec
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:40:45 +0000

    to fear rush tidy though

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
