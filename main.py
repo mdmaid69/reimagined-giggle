7faef34a356bd48a9a95bc77a663ee56090beb3a
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:55:57 +0000

    piece stamp natural upstairs slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
