742354951da1906c699be9603fbb2f195c08b59c
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:27:45 +0000

    round through Tt activity close

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import os
+print(os.getcwd())
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
