3cd915e57ec9a864d620f4e8755480c97371c5d6
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:42:01 +0000

    head slight rise gather one

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def print_python_version():
+        return sys.version
