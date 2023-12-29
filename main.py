12cc3303b5fcd331006599d62f07d500fb138b41
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:21:06 +0000

    sit strong partner demand belong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
