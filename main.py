0c38f9f991ffaac9422f62687470ab2af6c4cc63
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:47:34 +0000

    heart split people far behaviour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_power(work, time):
-        return work / time
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import array
+def get_array_as_str(array):
+        return str(array)
