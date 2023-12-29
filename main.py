0cd5d48b2d92dffabb3ec2e577a82b495ec0cd9d
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:10:31 +0000

    wrong decide fork lift smell

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
