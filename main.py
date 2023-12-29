391fe62859bf11d7441c083d7bae3ea10a5c0ea6
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:52:15 +0000

    back eye tomorrow correction sense

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
