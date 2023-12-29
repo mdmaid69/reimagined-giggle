235e38eac841778443021b1452c6f3abc53d76b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:33:59 +0000

    club demonstrate branch workout whatever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
