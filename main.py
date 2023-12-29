2810823d09e817d4aab5757133621a4907095c73
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:55:13 +0000

    goodbye light walk treat children

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import platform
+def get_os_info():
+        return platform.uname()
