606812d468dea89faacaa04510342f3c62b6f32f
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:48:05 +0000

    otherwise impatient pure brief for

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-print(random.randint(0, 100))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
