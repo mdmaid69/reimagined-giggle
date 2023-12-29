0b5fa43ac909032ff4bd19c0b3c2c9b014bdf8f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:14:16 +0000

    relax success decision majority university

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
