7888b2e33f35481a97f15f9591b280a4d4df8f54
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:29:10 +0000

    equal now thankyou government quickly

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import sys
-print(sys.version)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_work(force, distance):
+        return force * distance
