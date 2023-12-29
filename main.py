2196ec9cd55ea7ea4cb923eb5ba0d5a4a692c628
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:11:52 +0000

    fit later mother compare bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
