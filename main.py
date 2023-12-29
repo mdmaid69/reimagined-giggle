2450f7c4c496a53aaa026353ff66553776073a10
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:29:10 +0000

    defend agreement because anyone bath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
