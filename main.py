fb1f5a002f7b2c241552acafb16fdd07315ae3f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:57:43 +0000

    dream mouth warn search would

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
