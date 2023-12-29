1521ec49dc14ae3187fa0fbf5512bf7af8f5868e
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:56:22 +0000

    sea quiet workout club male

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
