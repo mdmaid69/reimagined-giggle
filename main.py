d3c52a053673e2c29519d6c7b7d80e5c4f7c6c07
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:30:11 +0000

    area keepup pinch hard dress

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def clear_array(array):
+        array *= 0
