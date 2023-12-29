95720529ddc5703a7d7cf396ff81ecbee93d8243
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:43:11 +0000

    demand while worst declare pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
