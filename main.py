ebe3a887dff77902f0d597bfa6f2d0ae041dc3d0
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:06:45 +0000

    friendly whole can aware moral

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
