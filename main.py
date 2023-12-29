7b6425d6eaa99966f9884a252a01e346c3a13628
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:39:58 +0000

    how emergency unable unhappy middle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
