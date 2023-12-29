2124b4731ca7070e224df01ae3c579a8f188b8e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:10:41 +0000

    worse turnover shock at want

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import sys
+  def get_python_version():
+        return sys.version
