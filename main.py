95aadff1b4fcbc1abd32b97e624e9cad21afaddc
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:51:53 +0000

    getoff manner report bit roof

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
