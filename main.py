9bb401ca1dd5d416e56c15a16eb36ae999255b32
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:41:56 +0000

    safety increase deliver mine feature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
