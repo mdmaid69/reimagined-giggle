91bc7d6f50c70fc490942d9af3e4abc5b7be35d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:49:43 +0000

    error mother Oo react beyond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
