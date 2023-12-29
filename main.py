60f8aab8fc25c57af02c6503680934ada8bb0a72
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:15:32 +0000

    embarrassing machine hear general Cc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import sys
+  def get_python_version():
+        return sys.version
