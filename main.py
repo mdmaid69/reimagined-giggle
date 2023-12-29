5f84b6d5238f46751a8d7aa182f539ee48ce7bd4
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:40:58 +0000

    steep come bin when pay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
