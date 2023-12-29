b286c51cb14ba7f481a3925d9dbb01245f962d38
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:54:41 +0000

    gap vote please keepoff deep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  def sort_list(lst):
+        return sorted(lst)
