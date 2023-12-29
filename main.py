aaa5fa965be2256ed778fcc03660ba3c5bc635f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:32:41 +0000

    interest underneath physical actual there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
