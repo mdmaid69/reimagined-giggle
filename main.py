d477b11b6ac0e76e84133b0aeae3f5a845ef031a
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:15:26 +0000

    formal mention everybody stay rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
