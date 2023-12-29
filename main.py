729a1f754acb3002af9c1b1fd86dca1c0583516f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:30:57 +0000

    sign light hurt raw vision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def find_max(lst):
+        return max(lst)
