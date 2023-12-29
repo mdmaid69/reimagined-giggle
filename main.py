03aa1d949850775ea0694867a6c0e89086906fd8
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:47:57 +0000

    just passage declare recent pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_list(array):
+        return list(array)
