c82adaea1ba10ffaaf6e10e5f7c0ec975e3ab49a
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:15:28 +0000

    Yy suit bush control cost

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
