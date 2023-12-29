d277dc7758638003f0d65a7664354d0b7cca87f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:18:51 +0000

    declare wander answer question pain

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
