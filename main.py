4ad9e66253568317bbea967b143ad152e90c4e0a
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:29:23 +0000

    separate tip nature theirs so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import tensorflow as tf
+print(tf.__version__)
