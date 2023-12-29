d66c13c42c9bb42d687de0f421bf4a6bc11269cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:53:58 +0000

    Hh worst switch accident project

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def convert_to_octal(n):
-        return oct(n)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
