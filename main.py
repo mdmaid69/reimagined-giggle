65a1a74cf516b15811eda37692786b4049b8869d
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:31:08 +0000

    personal gas who step mend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def remove_duplicates(lst):
+        return list(set(lst))
