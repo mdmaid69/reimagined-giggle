4145e28f159bc8108a33844ed69fa7aa9caeb584
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:06:24 +0000

    tidy emotion bank off breakin

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def convert_array_to_list(array):
+        return array.tolist()
