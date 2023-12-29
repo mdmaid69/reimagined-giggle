0556607b35e1816849f638ec2c4ef4b9d04ec8f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:08:12 +0000

    complete love temperature welcome Yy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
