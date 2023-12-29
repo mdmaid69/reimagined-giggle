42ea1ddb41cbdfbb70db99d1676230a1a9e9ae26
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:20:47 +0000

    chase cruel tread funny check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
