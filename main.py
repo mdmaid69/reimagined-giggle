6dacdec6a6f54e3e7ba67435db6ace4a280be491
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:01:02 +0000

    even or pattern gradually card

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
 import os
-def remove_directory(path):
-        os.rmdir(path)
+def list_files_in_directory(path):
+        return os.listdir(path)
