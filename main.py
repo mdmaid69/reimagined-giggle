4d23da972b025346a6ce6bf829522df01f948206
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:41:34 +0000

    style anywayanyhow tomorrow tragedy message

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
