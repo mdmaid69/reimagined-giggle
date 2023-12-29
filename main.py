71c70eaecc6a5346d071ed7fff323c02615d32a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:10:18 +0000

    home turnover all reduce extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
