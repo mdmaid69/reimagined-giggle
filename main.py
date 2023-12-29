e7100cc36bd43fe0feda6ed0d34db696543c16d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:34:27 +0000

    combination winter test approval organization

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
