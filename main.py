fc8d0026304c6914a15e8a36daa01e76efb01ab4
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:55:12 +0000

    study go emphasize keepfrom replace

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
