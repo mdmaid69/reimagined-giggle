fdfd36607258b258a9744c455d7ae5b8f8b9c7ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:04:43 +0000

    allow recommend loud sure him

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
