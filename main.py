13e3c43f6315cbff7f54ce55764d00bd583ba04e
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:19:20 +0000

    first interfere organize way deep

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import tensorflow as tf
+print(tf.__version__)
