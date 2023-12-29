afb16dc3572cbe691a6e85e189d15ae963698fd5
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:41:32 +0000

    except occasion use runout coffee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
