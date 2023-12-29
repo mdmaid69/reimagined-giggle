95f63a7e1b8aef2e00840638a75d0a8c1edc79da
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:10:26 +0000

    strength poison education yes use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
