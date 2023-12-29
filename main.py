b3ef56a55410748254a4a9fa4c9ec8edf4e74ddb
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:52:21 +0000

    increase for other store takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def clear_array(array):
+        array *= 0
