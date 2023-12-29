81c6858e4e364a77fc8662ba5883b1cce1365981
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:39:26 +0000

    love plan watch away declaration

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_typecode(array):
+        return array.typecode
+def divide_numbers(x, y):
+        return x / y
