f542d319a60ae001df3145b28df4c7ed804db6ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:19:48 +0000

    consist neither wall account violence

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import platform
+def get_python_version():
+        return platform.python_version()
