fa6be495e3ea7d515580920ae276db91828fbc22
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:24:47 +0000

    this urgent brief bet manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
