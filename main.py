ba2d65e50dd3a9e725bf8f0737b1e845402c1801
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:35:20 +0000

    ilovedicks pay wealth someone ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def square_number(x):
+        return x**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
