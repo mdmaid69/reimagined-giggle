80e631102040f695dec0cf81f715fbf1c7d75c0a
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:01:22 +0000

    react world house stone small

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_acceleration(speed, time):
-        return speed / time
+import tensorflow as tf
+print(tf.__version__)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
