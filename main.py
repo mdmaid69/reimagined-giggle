1611b810c3fce03a814729b4e0475d5e211932f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:48:19 +0000

    grateful abroad instrument across ground

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
