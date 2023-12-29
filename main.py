22895b654cd569240a7f1b3115892083791d1ade
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:45:17 +0000

    perform trap ever plenty takeout

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def convert_array_to_list(array):
+        return array.tolist()
