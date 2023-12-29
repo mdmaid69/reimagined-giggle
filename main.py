e15b5431186aea6f3abafa8d2192c7f0e06ca5aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:12:05 +0000

    by human effect about otherwise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_item(array, i):
+        return array[i]
