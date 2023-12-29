b12df7126c4b2b5bd66e883d201809910a4c6cfc
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:24:05 +0000

    ought already classroom keepoff care

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import platform
-def get_os_info():
-        return platform.uname()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def divide_numbers(x, y):
+        return x / y
