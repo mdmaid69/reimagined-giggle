96559f5f11ef5b33e9ced52323632b237cc87b32
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:55:46 +0000

    properly fortunate swim seriously holdout

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_area_circle(r):
+        return 3.14 * r**2
