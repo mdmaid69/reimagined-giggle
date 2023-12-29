8df2f2861a93a402fd3ecf3b40a6b702259bbff7
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:38:02 +0000

    respect perform form accidental sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
