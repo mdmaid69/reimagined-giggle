ae9083103003f09b5e7c95874dc817f322aa650d
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:07:15 +0000

    shopping Pp beyond protect Gg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
