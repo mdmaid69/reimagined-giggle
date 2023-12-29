3dee44b4fa3d99a232a48f2af1e97b8e7a9546f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:59:59 +0000

    include welcome price sister invitation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_tangent(x):
+        return math.tan(x)
