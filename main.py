43ae0e42ff97c75882de65ed25001dc96e943d2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:26:22 +0000

    vote previous watch produce land

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
