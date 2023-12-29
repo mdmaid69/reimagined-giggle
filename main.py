9fffc387bc16c7d259250da623befb7682e22235
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:10:04 +0000

    foot increase describe callup arrange

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
