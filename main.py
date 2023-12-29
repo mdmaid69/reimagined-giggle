5e093e929c0551fd564cf76bf56e429b79f6008b
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:52:33 +0000

    prepare keepup explanation negative safety

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+print(sum(range(10)))
