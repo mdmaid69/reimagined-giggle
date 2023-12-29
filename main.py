7f308036f8271d6970a6c57da1a91d3e6c41d725
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:11:00 +0000

    nowhere his steal among involve

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
 import math
 def calculate_polar_to_cartesian_coordinates(r, theta):
         return math.polar((r, theta))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
