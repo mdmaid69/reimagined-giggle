a11482dde861678a9f1e09f481e35008621ea87f
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:40:31 +0000

    eat occasional bottom load delicate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import getpass
+def get_username():
+        return getpass.getuser()
