cc4f3cfeb02d2db0fc898afa19f372fdaaf7a128
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:53:39 +0000

    worse mend embarrassed delicate excuse

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
