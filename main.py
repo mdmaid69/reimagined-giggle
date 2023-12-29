a9e14a3ad5c93c797dc95c7b81b3d1ff6182f26d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:35:29 +0000

    goon regular 179bite suddenly ready

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
