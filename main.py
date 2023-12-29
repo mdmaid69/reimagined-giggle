46c23f97187e207e8159fbf819389395779addba
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:54:22 +0000

    admire alone less evidence pay

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import os
+def get_current_working_directory():
+        return os.getcwd()
