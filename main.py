695747d585a1166297e66a33eafe865ccff3b9b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:58:19 +0000

    release inquire breakfast unit beautiful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
