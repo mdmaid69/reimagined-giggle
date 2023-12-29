9a9c1330bbfb35a35777ec380287e8e7096aeb78
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:49:32 +0000

    centre split track wonder spot

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def subtract_numbers(x, y):
+        return x - y
