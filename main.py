e3b7f61d034d76838c13d48bab2ed08a2314d97e
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:48:52 +0000

    majority instrument comeup fine seat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
