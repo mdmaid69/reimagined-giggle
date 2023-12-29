99a0f132a111e1a9c2f521e500c8d603abe75e31
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:45:33 +0000

    tradition quality test this feel

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+for i in range(5):
+        print(i)
