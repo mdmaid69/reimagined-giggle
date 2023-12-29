9c2471ffd756eba4ffdb6438f5f9b7a01d5cdf0c
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:42:12 +0000

    pain false careful immediate calloff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
