25bc56e84f810eb08bc6eb50040f96c5180f2dab
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:47:22 +0000

    winter and jump question shallow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
