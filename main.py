888448e2b12d380fed0ba6201845d726f6f119ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:51:55 +0000

    own bunch present neat ashamed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import time
+def get_current_time():
+        return time.time()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
