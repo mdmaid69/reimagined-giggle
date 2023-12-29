4141fee1f7d086598abde88e4724a41e10424093
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:10:10 +0000

    luck everywhere comedown straight shall

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
