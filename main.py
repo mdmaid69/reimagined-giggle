0fca23a92f9e88909778aa9cd86fdc709da23cfd
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:28:16 +0000

    bringback square than late smooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
