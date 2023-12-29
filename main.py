ffe19fa4c3cd653f56867d51afa794b152098b2e
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:03:41 +0000

    secret before rain compare fine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+print("Hello, world!")
