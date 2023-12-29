77000c8660eaacd47a1965b4663e0d45f93a0ac9
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:46:02 +0000

    sound realize consist fence sign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
