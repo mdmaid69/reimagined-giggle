6dcbedebe8e93e9b5210d4345fd1a935922a8f9a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:50:26 +0000

    considering manner throughout something sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
