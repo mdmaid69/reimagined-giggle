f080b16ea2593c160325ef07917c2d8adcc129cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:30:07 +0000

    shelter crowded delight break switch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
