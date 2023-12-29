68ebed08c5c9bba50ff06ed59cc172ece1f04e45
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:02:15 +0000

    weekend there takeback back smoke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
