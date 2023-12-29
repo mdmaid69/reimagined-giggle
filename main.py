4e5b734db1f14be3413759abffdd575eb0c1c053
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:26:57 +0000

    report cross little drive closely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
