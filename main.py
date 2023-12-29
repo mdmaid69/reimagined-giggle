877718bac69865ec9291973bbeafa91e1b42c9d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:04:13 +0000

    congratulations sorry that condition certain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def convert_to_octal(n):
-        return oct(n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+for i in range(10): print(i)
