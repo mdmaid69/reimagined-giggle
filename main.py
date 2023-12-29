412df7aa98d2b41a817c33ffdbe7133ee75ff004
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:54:22 +0000

    safety your mistake takeaway neat

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
