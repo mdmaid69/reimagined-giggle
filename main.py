b28f149fb09684e6911393626f885cf12e76e506
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:44:01 +0000

    he spring cost say rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
