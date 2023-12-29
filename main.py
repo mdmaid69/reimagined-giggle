f63c6cafe8db19418ad9463927f1a053954d7221
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:06:51 +0000

    niggaacademy giveup actually period one

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
