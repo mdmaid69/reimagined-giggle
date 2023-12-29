6ebea7c04bb51cfbbe624289410b2a3fe87ebdaf
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:03:47 +0000

    experiment us brown straight possibly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
