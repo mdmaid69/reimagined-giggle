4cb0c975248e39e814fe384de63e1e87afe41afb
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:59:55 +0000

    surprise appeal private influence own

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def append_to_array(array, item):
+        array.append(item)
