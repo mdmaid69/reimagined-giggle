5cb2ff75341352af102d7e7fb6694c4c5a36aae0
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:22:14 +0000

    approval mark sure home 566end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
