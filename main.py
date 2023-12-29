684094a99613646a03705298347b738923d0ef4e
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:25:03 +0000

    cheap 572enter shoot church raw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def find_union(list1, list2):
+        return set(list1) | set(list2)
