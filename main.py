4cef12c57efb26f5c7a9188ca56a46b5f3a15d31
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:59:12 +0000

    come 565end juice control action

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
