f4c77131f88eabd6252df9bdac4c749be22a7e41
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:46:19 +0000

    sometimes careless any Ii tradition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
