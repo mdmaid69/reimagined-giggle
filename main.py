9bddc9042f8397e56cc5093a67f9ded9f19e5690
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:08:36 +0000

    definite square task born possibly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def append_to_array(array, item):
-        array.append(item)
+import datetime
+print(datetime.datetime.now())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
