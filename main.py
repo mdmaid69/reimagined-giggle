24af88329e5ce6fb724e6191c9217b300abdcc1c
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:21:09 +0000

    mess goaway arrange race ear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
 text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+print("Reversed:", text[::-1])
