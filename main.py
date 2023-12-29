b41e87af517bf01693c45fa251772ca34dec1be9
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:02:59 +0000

    illness refer congratulations send appearance

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
