091d23a1460ffb8e1965d1f85c446b71b8084a98
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:03:45 +0000

    protect whistle able badly present

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def sort_list(lst):
+        return sorted(lst)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
