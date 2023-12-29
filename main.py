fbdff7c0608733536f203aea233b2c8afef2db33
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:15:22 +0000

    owe awake goup correct guess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def count_words(sentence):
+        return len(sentence.split())
