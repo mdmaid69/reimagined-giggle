1eca5ddffb12a27ed4fd6260e76e0b20cd5eb91f
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:40:06 +0000

    listen length even winter tune

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
