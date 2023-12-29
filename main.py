89357ba97399f82169463d81e5a1351698550c87
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:49:10 +0000

    nonsense peace cover describe feature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import sys
+def exit_program():
+        sys.exit()
