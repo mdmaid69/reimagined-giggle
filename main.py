b796fe0cbe67f62d9d5da4d07fd09d4944b82632
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:48:35 +0000

    available address off contrast about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
