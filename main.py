481f61563ff2003404d1da3d741f21d30fb66ecf
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:32:55 +0000

    mark precise often obtain protect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
