3e8106e676f95878268e485c91b142410de8b105
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:01:26 +0000

    meaning check no join something

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
