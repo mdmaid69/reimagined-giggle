74469d556b6a4cfd0140574c992bd18a38a2960b
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:10:08 +0000

    train minor cause shallow warm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
