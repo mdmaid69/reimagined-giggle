325d75df01e0b66ad5b9c09b9d969668782defcf
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:06:27 +0000

    attention club decision anywhere fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
