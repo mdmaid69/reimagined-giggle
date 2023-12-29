2865eb0d758254e83bf9910e086b044133ed5cd0
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:25:53 +0000

    remove head friend influence owe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
