92d3aea85bdb341ec6be325cf634d806c76e84b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:14:41 +0000

    agreement dry form peace regular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
