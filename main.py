658667eb8b267f837e55f70870d46fcd1b08390f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:34:55 +0000

    sensible press market voice bunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
