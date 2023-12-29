86085a9828c808b122728c77e2e8c4f2d7a5e0dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:49:27 +0000

    private march tomorrow city major

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
