9c818ce3b23ab9284bbd25f7edfc6e1b54d7b20d
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:47:43 +0000

    whenever promise forward breakup Tt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import time
+def get_current_time():
+        return time.ctime()
