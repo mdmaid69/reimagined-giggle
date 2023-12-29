d8fcac56d1564d02c8041e764c9301023797a47b
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:17:31 +0000

    carry rent possession Gg takeover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import platform
+def get_os_info():
+        return platform.uname()
