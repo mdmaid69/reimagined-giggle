288aa82efb889adcff028542d6853e2f892e4044
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:12:50 +0000

    chin greeting wash snow reaction

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def print_python_version():
-        return sys.version
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
