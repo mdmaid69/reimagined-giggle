4148a49ee8b23df9478a35cb768159a2d8d40c5e
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:38:05 +0000

    suck bend tomorrow each dish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
