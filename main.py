a5da07d014d0f882e55c5bece5d4051c716a5afc
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:19:33 +0000

    hour almost face goaway welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
