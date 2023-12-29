c885bca3aeea49db370e1d921ed8f3c8352f9c15
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:29:24 +0000

    awake godown pupil love picture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
