0c05e2414ff8f67fd52643ad14dbf2800767c143
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:50:53 +0000

    just past land stupid talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
