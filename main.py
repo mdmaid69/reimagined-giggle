678bfb129dfeb6c4c1117aa2cdf8a25c7a69cd73
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:19:22 +0000

    flow 568energy communicate now number

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
