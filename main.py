6c39b2c4e03dea2c4483795958c77062055edda4
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:23:25 +0000

    main everything 572enter seem interruption

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
