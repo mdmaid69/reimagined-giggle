c4da5d10cf11282a46ae2afc0a302fe5cb6f5436
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:14:35 +0000

    suddenly improve foot also during

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
