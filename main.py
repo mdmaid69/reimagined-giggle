d7249ef34b5a5c6b7b0513e04d0a413a6c876dbb
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:03:33 +0000

    question connected mistake Nn land

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
