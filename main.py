0f15b2bd16d37e46a571989f7a36c1faa665bd1e
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:49:27 +0000

    pretend listen previous glass though

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
