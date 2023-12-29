4771ecebcbfd6663fab5771b09685e97bb487fe0
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:53:28 +0000

    die allow expect become where

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
