8daa1e7d69fbacc871bc86d30b0241f8aa614011
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:11:11 +0000

    black communication preparation any worse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import tensorflow as tf
+print(tf.__version__)
