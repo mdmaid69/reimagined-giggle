4c93135c87660385aed49a584bebaad0f9ac4a63
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:26:03 +0000

    target where old maintain wall

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
