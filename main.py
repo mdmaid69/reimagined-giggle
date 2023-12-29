4da2ca406cf129985e7fab33c9c129865d7523ba
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:38:37 +0000

    seem how step suck exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
