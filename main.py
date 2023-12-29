c7fb40cab1f6346c929cb73a6796d88d7f0384ca
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:51:09 +0000

    past answer figure reply happy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import os
+def remove_directory(path):
+        os.rmdir(path)
