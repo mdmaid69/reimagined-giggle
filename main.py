309cd3986ed248f2a6a08415fff2b5658d59b237
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:11:32 +0000

    lookaround holdon pronunciation grow start

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
