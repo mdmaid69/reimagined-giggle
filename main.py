90fb339906ea775354373697c34037b23df9d5b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:11:09 +0000

    embarrassing runover definite by needle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
