f58d29fc3b09a2d9273c2cefc490656ee17df9e1
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:15:56 +0000

    stamp touch organize about finger

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_acceleration(speed, time):
+        return speed / time
