82e66a5ceff8bbce7378d91568751095ff57d938
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:15:30 +0000

    expect fact uncomfortable arrival moment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
