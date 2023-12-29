d125fc5479ec03bdfee7571aa877ba21852018ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:36:12 +0000

    swing praise keepout exploration opposite

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
