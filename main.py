99be664b05707dfbf68b6b0cd15e658ba6ec7c5f
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:25:52 +0000

    must idea joke gradually just

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-def is_palindrome(s):
-        return s == s[::-1]
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
