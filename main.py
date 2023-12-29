e6739e4256c13a5c73b3dea92439edc3be22f3c8
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:06:18 +0000

    single bridge bath equal Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
