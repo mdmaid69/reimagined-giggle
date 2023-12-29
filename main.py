7a1074e6214b1baf261ff29ed12bf08380c67c76
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:14:20 +0000

    basically culture Nn dark undo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
