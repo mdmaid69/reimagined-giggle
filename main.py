7e5a70108a3ee1fef145d09a91df96c706c73d31
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:49:51 +0000

    class male cinema doubt row

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-print(math.pi)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import tensorflow as tf
+print(tf.__version__)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
