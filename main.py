487758a5290b34efb08fc8a919f9aa50b3092650
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:16:10 +0000

    about wear 566end downstairs perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
