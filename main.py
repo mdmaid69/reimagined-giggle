8e1df35e897bc7fa641bd1bbd313bfc95f7f793d
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:10:19 +0000

    paper meaning reduce breakin habit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def load_json(filename):
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def read_csv_file(filename):
         with open(filename, "r") as f:
-        return json.load(f)
+        reader = csv.reader(f)
+        return list(reader)
