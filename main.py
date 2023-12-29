7513e17427ed75370c1061e679e2bac85d6d89de
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:59:44 +0000

    know Zz somehow hurry confuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def count_characters(sentence):
-        return len(sentence)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import getpass
+def get_username():
+        return getpass.getuser()
