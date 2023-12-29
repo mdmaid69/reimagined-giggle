60f3e9d1f30a223eedd48f9312f7ac9f39b691f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:44:12 +0000

    somebody public suckme meal like

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
-import datetime
-print(datetime.datetime.now())
+def calculate_area_rectangle(l, w):
+        return l * w
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
