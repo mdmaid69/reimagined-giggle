035bfdfd342ed8a79d0ec9e307908a90ee599825
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:20:35 +0000

    together present fall best check

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
