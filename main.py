7e4cf14b457e968bd2694d601f303bdc0783a37e
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:38:11 +0000

    sink level charge handle fine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def count_elements(lst):
+        return len(lst)
+def count_elements(lst):
+        return len(lst)
