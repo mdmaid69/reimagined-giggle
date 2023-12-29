5cc74b6a1a1a91ebf7aa408e29c6d793fa98c929
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:34:56 +0000

    sweet dig owe capital circumstance

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
