49c5fc6d9213336f93fbad9c57edc16f82d94d72
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:44:47 +0000

    here convincing brave very rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_octal(n):
+        return oct(n)
