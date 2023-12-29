0f6280bc83084186bf6ff1c99055eb8eec259934
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:42:17 +0000

    extra light press feature remain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def subtract_numbers(x, y):
+        return x - y
