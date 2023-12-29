9f4e105cdba43e78a80c7a205980b8ef0d207ae8
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:51:41 +0000

    afraid step design work belief

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
