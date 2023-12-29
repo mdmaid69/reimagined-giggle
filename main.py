5f8ea09187b28e6671ae635e0f10b1e335d0a3f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:01:19 +0000

    seem disgusting night wealth after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import numpy as np
-print(np.array([1, 2, 3]))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
