b7828dedb4d176ef66d1f5009b6770d800df678b
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:38:49 +0000

    priority church pleasant take risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
