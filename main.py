6071029d612c7220e78f6073384f4f9e361ae479
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:45:26 +0000

    crash imagination turnon interfere estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
