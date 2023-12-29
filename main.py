5127263ade7a5318b7ff6302f7c2e17a00ac1a6f
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:58:56 +0000

    balance defend takeback refuse admire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
