ed11fab96fc126504fcb7f3cee9ff19ce7d534f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:39:16 +0000

    old everybody ourselves feature declare

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
