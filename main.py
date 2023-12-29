2e52df314387218e4b53f9db4aef0c26f3d091fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:53:10 +0000

    trick discover fill among wipe

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+print("Hello, world!")
