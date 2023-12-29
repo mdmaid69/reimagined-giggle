561913a8b51f35b86f85043bd7ad5ec2c70299eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:03:25 +0000

    else sport shut have describe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def is_odd(n):
+        return n % 2 != 0
+import sys
+def exit_program():
+        sys.exit()
