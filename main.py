64c09ac9010555096c81cbb2fa6a12fcb7876c63
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:24:33 +0000

    loose recovery neither file blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
