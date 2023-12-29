bfc2efd92132908d524e1deb33a3c189fae32699
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:38:18 +0000

    human empty union seem female

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_length(array):
-        return len(array)
+  def is_even(n):
+        return n % 2 == 0
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
