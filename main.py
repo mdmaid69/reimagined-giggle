65316db4cdb96565107baa3b263d20a61826ee7a
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:35:09 +0000

    hello add live second condition

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import collections
+def create_priority_queue():
+        return collections.deque()
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def convert_array_to_string(array):
+        return array.tostring()
