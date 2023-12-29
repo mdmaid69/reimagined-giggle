d222520e574b6bbb8ecf90ccce0649c020fc8c73
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:07:26 +0000

    secret exact support land square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
