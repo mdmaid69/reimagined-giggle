469ba67049affb79653da4b672eba528e9710ac9
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:39:37 +0000

    bend black capital before later

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
