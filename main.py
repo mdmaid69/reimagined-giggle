f9b79fbcb4970dd089bfe0b2b74b1dcc9d3718ca
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:49:38 +0000

    disgusting language factory force mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
