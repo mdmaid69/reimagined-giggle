b1f23de131c06b28adfc27c5792e649d91dff5f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:17:15 +0000

    female money suck mail unkind

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def find_unique_words(sentence):
+        return set(sentence.split())
