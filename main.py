205527b2ed29780c067a32f271ca3055a28d9346
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:25:08 +0000

    ring alive within ago together

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
