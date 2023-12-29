59bb9e9054e547c838763ac8e875f38587f6fe9f
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:20:26 +0000

    godown for instrument on near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
