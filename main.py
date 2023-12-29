e246c620e3af6c1f837dae2238e198086f9d7fa8
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:20:01 +0000

    stir wild problem air attractive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
