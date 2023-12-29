65bc72170e829c0a0141772bdd53cfa71d93b30d
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:15:26 +0000

    shame Tt base oil door

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
