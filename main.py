3082e5d1db4e472da6ced7d083d7fdc9a95471e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:19:27 +0000

    write purpose scream burst better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
