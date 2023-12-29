4be87ed25f44b6f596fc2cd587b7a327654860f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:49:38 +0000

    equal awake breakdown refuse around

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
