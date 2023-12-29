69de5c7c55af156f811fc73b842cd7e2096b94bd
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:12:03 +0000

    typical swallow crazy used bank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
