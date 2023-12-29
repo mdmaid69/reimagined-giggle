7380c16147fbae5e29eaf30884490b56154dd41a
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:17:37 +0000

    considering goback(to) lean surprised communication

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
