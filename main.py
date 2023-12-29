a9e5bbc90cb333910b5f058f32e8e74ddc588d48
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:46:27 +0000

    present file noisy her affect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def find_unique_words(sentence):
+        return set(sentence.split())
