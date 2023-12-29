dc2f736b28d66ecdc06f3cae8011bb2fbbf0497a
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:16:51 +0000

    noise move plain suck Ii

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def divide_numbers(x, y):
-        return x / y
-def find_max(lst):
-        return max(lst)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
