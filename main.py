8d461dab3a1b254c4cec0b283543b55e4c31c262
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:58:33 +0000

    pause national winter waste room

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
