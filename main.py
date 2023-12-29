e6f2608153789438372e609de79bad12bae8e4bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:37:10 +0000

    kind Ss break damp either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
