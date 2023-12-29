2f289ec124cae66ed9dc7443d9feae3d01f4362b
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:56:09 +0000

    tear voice once figure walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import numpy as np
+print(np.array([1, 2, 3]))
