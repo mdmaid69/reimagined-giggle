5f088565a23c580ac39106df70d214c3d733edd6
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:40:24 +0000

    maximum damp comeover represent command

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_pressure(force, area):
-        return force / area
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
