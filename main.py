a6582765cf4d5ca55331e927b620e29f70d2a94b
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:02:21 +0000

    offer degree round seem intelligent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_max(numbers):
+        return max(numbers)
+import sys
+def exit_program():
+        sys.exit()
