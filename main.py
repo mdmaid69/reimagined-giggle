47a84d3cc8d7b2e5eba0c5c80fe1caeda5f3f794
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:01:33 +0000

    circumstance children touch male combination

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def multiply_numbers(x, y):
+        return x * y
