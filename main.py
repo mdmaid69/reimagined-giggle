722651936ec1d574babaa5828d829f46311af323
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:37:41 +0000

    provide reserve concentration receive butter

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
