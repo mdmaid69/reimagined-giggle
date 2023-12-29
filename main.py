33ee91650e99a174341be1f3d5af034d8f61dba3
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:02:55 +0000

    agree pencil breakinto official curious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_perpetuity(payment, rate):
+        return payment / rate
