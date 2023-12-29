315d6fdf1a4b0fdf1c19c34b47aa3262ef66c173
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:26:30 +0000

    slide dick arm concerning workupto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
