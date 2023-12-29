9ef0e1a30d4f4409ec7e687c5b5f2cff3f92f344
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:19:25 +0000

    drive whichever majority wealth possession

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def reverse_array(array):
+        array.reverse()
