8930a2465535417633bc89e1a0fa08e3603d4570
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:31:54 +0000

    alone respect thin intend crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_average(lst):
+        return sum(lst) / len(lst)
