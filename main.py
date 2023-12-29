92778d8b75662ad50807c5e7db53d417ea8b7e83
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:04:39 +0000

    bye before bye ago head

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
