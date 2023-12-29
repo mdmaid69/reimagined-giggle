e2e382f89490012059771a28b2d30e4503d48a17
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:32:04 +0000

    instant responsible near cross their

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
