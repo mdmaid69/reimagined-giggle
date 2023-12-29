5ae1d5958f96b1a2418137063a782996bbec0f08
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:57:01 +0000

    leave preparation sleep suppose keepfrom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
