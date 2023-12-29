2114e873dcdaf2d4d5a49da09d543cca759f23d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:40:25 +0000

    charge my rush practice towards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
