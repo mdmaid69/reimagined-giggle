0f9cbbea901fda6dde17e606e601597e321bc62a
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:35:23 +0000

    public free bury chase none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
