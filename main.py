f47a0881bf9746a46f446d8b25a2e9f2008a3830
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:13:31 +0000

    dear smart black idea legal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
