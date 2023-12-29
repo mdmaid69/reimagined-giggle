b906fd9ae4d583f59c7d475fc71569dcf4984cd1
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:25:08 +0000

    neither tip since duty occasion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
