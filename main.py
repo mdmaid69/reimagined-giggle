d7b49380b73bc558cf9916f633ce61f490b1649d
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:37:52 +0000

    scheme tune kill sweet state

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
