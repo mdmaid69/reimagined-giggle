01281f48ccc45d4940626d81d6202dc7631d49c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:41:03 +0000

    this loss shoot discovery culture

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def clear_array(array):
-        array *= 0
+import re
+print(re.match("h.*o", "hello world"))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
