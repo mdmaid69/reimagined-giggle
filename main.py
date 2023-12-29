ca659fe3a02c1f237f0056a118032f77ddecb172
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:29:08 +0000

    anything surprising quarter trade lookup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
