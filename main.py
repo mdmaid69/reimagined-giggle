ca9901431144d306a47630dd92f7675df00817f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:28:04 +0000

    telephone quickly unable spray block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def add_numbers(x, y):
-        return x + y
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
