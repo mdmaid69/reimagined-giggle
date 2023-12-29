38f1a693900af1057b33640d769763291bfd78e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:04:51 +0000

    exchange discover now stiff Ss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def split_path(path):
+        return os.path.split(path)
