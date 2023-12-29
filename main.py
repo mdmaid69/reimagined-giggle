42db90491d824063545afe6611baea3d7f4e37b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:32:58 +0000

    open pump food move young

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
 import os
 def get_file_modification_time(filename):
         return os.path.getmtime(filename)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
