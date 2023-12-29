1e1edbf884a01f248465197839846151b49d13b4
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:35:36 +0000

    surprised tap OKorokay ball hide

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def remove_duplicates(lst):
+        return list(set(lst))
