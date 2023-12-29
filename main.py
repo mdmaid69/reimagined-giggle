96ba5aea53463d967953abf5846129a33cb80e15
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:39:51 +0000

    mix cough workupto perfect steal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
