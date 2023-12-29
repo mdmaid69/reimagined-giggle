40eaa2acdaeeb113637f4b5895de3468abf8d77f
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:34:22 +0000

    land constantly next type everywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
