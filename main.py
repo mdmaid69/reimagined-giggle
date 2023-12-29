3131f746a4ffb1646edd94c281d4727678cb20be
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:37:58 +0000

    tell tool agreement situation strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
