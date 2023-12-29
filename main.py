d177dbf303d51d46bafd416f320fb2887e60ec60
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:11:17 +0000

    why glass part order worried

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
