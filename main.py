f90e8f20655f62825d6e36f2457ba2785642d97e
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:50:36 +0000

    sound offer skirt fashion law

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import time
-print(time.time())
+import array
+def get_array_as_set(array):
+        return set(array)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
