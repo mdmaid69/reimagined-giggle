f103ffe3d86870bd933fa28c32172e6ead8ccedf
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:31:48 +0000

    soil dry complaint thankyou kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
