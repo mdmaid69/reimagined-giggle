c7ab2c6e7890fea5aa539113f7a3e2e948f73c64
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:00:12 +0000

    process church better fairly different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
