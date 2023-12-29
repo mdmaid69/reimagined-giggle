1e51649e9d8ec3966fdca470561a94e840417919
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:51:20 +0000

    favourite storm our comeup curious

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_power(work, time):
-        return work / time
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
