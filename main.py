34ee70ea16a5cb2679978fb558287b5b2119679e
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:53:27 +0000

    567enemy squash buy influence throw

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
