2674e634ffeb6666fea4e62ac9bf7a6731cdb9d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:07:02 +0000

    again coat uncomfortable thought speech

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
