49c129028053424319711e628c0332d22a6a64ba
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:06:25 +0000

    tip introduce joke embarrassing subject

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_octal(n):
+        return oct(n)
