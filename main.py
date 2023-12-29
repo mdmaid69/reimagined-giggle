b55dd9808424e46c2b39ab9a95de06a3dd4c6ddf
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:18:57 +0000

    normal breakin stiff guess voice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import collections
+def create_user_list():
+        return collections.UserList()
