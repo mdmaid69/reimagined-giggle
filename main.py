820024d5c9e45da534ca1eef371150c1043a5393
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:59:23 +0000

    view excuse suggest already rain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def sort_list(lst):
-        return sorted(lst)
+  def convert_to_binary(n):
+        return bin(n)
+def count_elements(lst):
+        return len(lst)
