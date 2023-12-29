11fada0fcb869c099c6280b899ff870946855d25
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:51:59 +0000

    impression Pp news away leg

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
