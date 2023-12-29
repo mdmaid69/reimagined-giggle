ff0d2e0b5c3ec646e66c41e15c3366dc6b35ec9c
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:57:34 +0000

    slide under female fault exciting

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
