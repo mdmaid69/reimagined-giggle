7b459b8192f458a8122525c90386a6c6a243475f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:43:07 +0000

    actually aim bread zero effect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def square_number(x):
-        return x**2
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
