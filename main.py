ea69c12356c72a552a70a488bd8ccb9d22a5ebad
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:41:08 +0000

    opposite poor boil children total

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
