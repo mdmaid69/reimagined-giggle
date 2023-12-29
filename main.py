cb8f1eb7f6dfd283f9683216493ec314df20a7fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:13:57 +0000

    damage Kk this hear nice

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
