bd494b847fad8ce71cf8f41325131b6cc1486103
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:50:25 +0000

    back breakout respect dare contents

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
