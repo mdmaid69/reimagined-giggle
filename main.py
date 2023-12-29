075eb1acb9fe6376e1d1f10eec6d67d3fef6f5f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:34:07 +0000

    rain turn estimate over rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import time
-def get_current_time():
-        return time.ctime()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
