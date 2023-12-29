3f88224c53d9c0f993fa154c1803a1ca7e41a05b
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:17:07 +0000

    muscle bury hide before represent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+print(os.getcwd())
