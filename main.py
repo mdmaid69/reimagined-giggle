20667b26b4ce31d26a7921643a57d5f995522086
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:02:10 +0000

    touch heavy once bother confuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
