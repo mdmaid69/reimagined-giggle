5035970491a4171e1b4715dba58fdc2909c55a90
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:36:49 +0000

    produce describe hello remove quality

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_power(work, time):
+        return work / time
+name = "Python"
+print("Hello,", name)
