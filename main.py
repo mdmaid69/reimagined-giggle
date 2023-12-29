b00a2f1317be4ab9bc0706b50da899e486d13d2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:10:14 +0000

    meaning throw operate light stupid

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_power(work, time):
-        return work / time
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
