3feb26bde6c4233c8842b8ce51b74ba58e327705
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:45:10 +0000

    offer ever mine report copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
