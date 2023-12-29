d4db606207b23fab156cd6b364a1741fb668bd40
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:02:21 +0000

    obtain see medium display remain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
