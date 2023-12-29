59d69c7e390a9669ea6cbb540335e563f6fd609c
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:40:31 +0000

    seriously object age wheel feel

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def is_odd(n):
+        return n % 2 != 0
