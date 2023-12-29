a45a7222c816da2b88986122e5df72224d7f658d
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:02:00 +0000

    approval little string exist major

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_error_function(x):
+        return math.erf(x)
