96818b7bcda4f5172e0a6879b79f8923fe2826b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:07:02 +0000

    motion cry notice suit means

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
