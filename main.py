8923c65885c62d337c8c7f3344f70ae06d4b17c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:24:57 +0000

    cheat doubt 179bite base arrangement

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
