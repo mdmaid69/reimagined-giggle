1b05c08edfc93c47416bab1a996a7aad581e28c8
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:45:27 +0000

    book comeout shake copy underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+print(os.getcwd())
