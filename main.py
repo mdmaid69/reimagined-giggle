ca62dd7045dd6f13a934deb81521c67e54ce1abe
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:53:44 +0000

    class birth push otherwise birth

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
