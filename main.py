f7ccebaf1363e7d47ac7709cf6805e6814db79f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:48:51 +0000

    smile turnon production consist bother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_directory_name(path):
+        return os.path.dirname(path)
