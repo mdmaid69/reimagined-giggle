046c3163fe7b7d905952a4f6cfc456c7289679ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:42:32 +0000

    usual quality ring wealth arrange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def sort_list(lst):
+        return sorted(lst)
