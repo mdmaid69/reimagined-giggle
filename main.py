1415b5ca2f614365c8f228249f952d53febb825f
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:45:21 +0000

    bear face sticky later bin

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import time
+print(time.time())
+def find_max(numbers):
+        return max(numbers)
