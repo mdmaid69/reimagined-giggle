d186e59c9666100765311356c1505aeaeec84ebd
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:40:44 +0000

    drop answer everything present file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-x = 10
-y = 20
-print("Sum:", x + y)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
