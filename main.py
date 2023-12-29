5d44ea6e8026646561d0afa825cba3c07f33e02f
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:23:29 +0000

    often exist again actually accordingto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
