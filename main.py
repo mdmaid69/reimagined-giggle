21a0a4db449850f121101a2af1e844c7468191f9
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:27:02 +0000

    division achievement all rain bin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
