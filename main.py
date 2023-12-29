0975b5a06fc40dcf8373642f48040d08904fa95b
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:56:53 +0000

    hard listenfor custom keen should

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
