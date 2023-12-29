2da3d644ca7083577350229bf0d417c08a7da5fc
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:21:47 +0000

    skill legal loan harm because

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
