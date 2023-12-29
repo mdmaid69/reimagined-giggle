6df47a28e3e86b6510d99b3a9e30a39a0d35bd25
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:24:06 +0000

    rest various proof please train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
