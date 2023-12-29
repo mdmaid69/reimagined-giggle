baa2a4f8ea67cd7920e03b214539db93256d940f
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:53:53 +0000

    already sign combination private accept

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
