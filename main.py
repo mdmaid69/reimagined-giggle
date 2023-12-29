a99db6560c711fccb59299ca246a645b6f3af930
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:26:05 +0000

    shrink job bear hurry entry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
