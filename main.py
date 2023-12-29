47007b82fadbbb3ea1ace9117f5dd1e5edd2ef1b
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:34:30 +0000

    appearance may raw pay market

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def greet(name):
-        print(f"Hello, {name}!")
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_density(mass, volume):
+        return mass / volume
