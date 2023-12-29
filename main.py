61bd151ce79587ec56ffc10f89f180da2a2eb740
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:55:17 +0000

    thank abroad record meeting ocean

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+print([x**2 for x in range(10)])
