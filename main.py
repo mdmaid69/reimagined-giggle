8786af11dc4579157c8ee7b817f78cb4b51a0366
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:23:37 +0000

    since sort just drawing concern

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
