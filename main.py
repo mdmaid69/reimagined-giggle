b69b1f1b0c5eaeb60fb0652191308fb7c5125e10
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:52:04 +0000

    breakout wild thinkthrough development introduction

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
