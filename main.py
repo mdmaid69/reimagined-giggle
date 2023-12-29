fef3242d26fc5d9e55e672f624005570ffdeed62
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:51:37 +0000

    make disagree touch area successful

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
