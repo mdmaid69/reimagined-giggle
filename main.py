26297109c4dc2868542c9c7d4eef9b60110134d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:30:03 +0000

    acceptable pool dry finger previous

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
