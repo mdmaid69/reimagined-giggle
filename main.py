a0d605f1b98163d1627709e62dacc5cac4afe14d
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:36:44 +0000

    form spring whistle sudden might

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
