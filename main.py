871d30d4c6f5a4fae7b87ab2c8a7c8ca07833e37
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:30:45 +0000

    first over spoil side near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
