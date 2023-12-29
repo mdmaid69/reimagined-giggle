033390f67cf4c04ef48c54a39013ccc0a7ef4e60
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:52:55 +0000

    actual constantly process care wonder

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  def sort_list(lst):
-        return sorted(lst)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
