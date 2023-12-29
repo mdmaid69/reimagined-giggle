6a8145baaaf2dd5415fc440f5693878f5b7a6b20
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:53:16 +0000

    follow dick upstairs poor Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
