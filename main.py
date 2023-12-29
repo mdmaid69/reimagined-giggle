5b76873f185d7c897ad9648ecbeb38221f09899a
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:15:21 +0000

    likely high relationship error business

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
