00108e45d5b2ca3b473544fd74b1ab89cfa01f85
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:04:07 +0000

    weekend watch bin herself traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
