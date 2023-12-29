328ba5cf1f3f84796acd4f3386e526e122dc5710
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:00:54 +0000

    later makeup scare matter list

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
