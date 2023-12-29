89597d990da36aab8a1797f8a1c5c9bf0733ac18
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:08:49 +0000

    pool fine back impossible press

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
