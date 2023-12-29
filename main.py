f761c7bb047449678ef34b4a02eb2d2f0bad6289
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:50:46 +0000

    maintain ride off both top

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def get_array_as_set(array):
+        return set(array)
