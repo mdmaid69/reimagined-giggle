be3314d0d51d79e981b51c55622af6f2c7e9f9d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:49:47 +0000

    responsibility instead environment careful discover

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+for i in range(5):
+        print(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
