127d2dbae29dd93a22f3696774fe30017467cd3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:11:09 +0000

    now complain cry job stretch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
