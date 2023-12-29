7b58c73e0370c664bd01f02a51187ae369a19c21
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:35:32 +0000

    leg pay both event wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
