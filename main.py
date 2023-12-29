90e85dd3edc1e8240d7c02c2e461be52b15f6f47
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:36:31 +0000

    spin window geton bright thick

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
