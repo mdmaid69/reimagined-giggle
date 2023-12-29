a596825a87c4814019bc195ccbd0534ba5a77a3f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:50:27 +0000

    here communicate interview grand violence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
