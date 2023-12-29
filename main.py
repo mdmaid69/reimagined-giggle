32a5e1d6ce3144678bd98832f30cd2a1d334a5c8
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:55:19 +0000

    entry convince wave pain process

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
