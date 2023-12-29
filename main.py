fac5f86e559f3fcb92dbf09e671b0cf425eff15b
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:40:12 +0000

    fork raw beneath start slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_speed(distance, time):
+        return distance / time
