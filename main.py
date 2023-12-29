a839ddd654407bda4448afe2ef5e75189a629a78
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:29:03 +0000

    respect breakdown connection sensible impressive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
