bfb755b5aba1f7a783f7f3392066cfec35b8bc4c
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:32:33 +0000

    possibly train unkind no separate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
