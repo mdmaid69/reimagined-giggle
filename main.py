4d0f913bfff09e3cf9bdf512de69fda19087994d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:22:35 +0000

    murder evening style crisis tune

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import time
+print(time.time())
+def calculate_density(mass, volume):
+        return mass / volume
