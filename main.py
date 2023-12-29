9ef79bd8e1a462838bebfe70aba3f2887638ded9
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:03:36 +0000

    blow shop explanation hate my

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
