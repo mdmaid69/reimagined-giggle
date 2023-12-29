553fe230d2638c278b670854d2d4019f1bc846b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:12:31 +0000

    meaning sign should after top

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
