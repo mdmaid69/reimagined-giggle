4fd5301ca3bc7c9126f7688648bd08b7e26abe91
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:49:56 +0000

    OKorokay serious touch birthday dream

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
