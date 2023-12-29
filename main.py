4141e1e28609cb65f2ff5033d8b84d64e3c4e18e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:55:16 +0000

    include whether pocket feeling ability

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
