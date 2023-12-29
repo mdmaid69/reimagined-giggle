9fd112fdac4c4db550c617bd74ad5f721fb86f95
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:45:03 +0000

    pool occasional ask confused school

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
