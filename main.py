1051aa9325cee11f722afb028bc2449b7e946542
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:37:47 +0000

    eager prevent equal rush beautiful

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
