50e7070042f998167fc44b059dce3ecb7192516d
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:24:38 +0000

    sale 568energy estimate tradition regret

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import os
+def get_current_working_directory():
+        return os.getcwd()
