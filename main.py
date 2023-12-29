1bb9d8993f1c7eecb8376a5aaa990a4b441b8c4b
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:41:16 +0000

    difference immediate wind wild neat

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
