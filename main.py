6ee2abe3daa55e815077598df78dd5fee1f2de44
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:36:54 +0000

    platform hardly second nerve just

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
