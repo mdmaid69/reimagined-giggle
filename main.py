b330992a02c6938ea11d5e6224453073fbd81482
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:00:51 +0000

    indeed squeeze single vanish importance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
