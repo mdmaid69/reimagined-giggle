3c492e1ccb282ba26eb711a51bb1bc41ce97c967
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:14:56 +0000

    protest deserve settle sign damage

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
