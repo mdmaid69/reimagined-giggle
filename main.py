0ef7704baaa327eff10b3fb7a100673124c675b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:48:09 +0000

    risk add general row breakin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
