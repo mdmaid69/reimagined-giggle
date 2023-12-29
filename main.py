c15b6bc95c9368f7bee9070ad8bb0d7972d74534
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:20:47 +0000

    patient married steal any confusing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_string_from_array(array):
+        return array.tobytes()
