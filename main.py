6b16501054edbc89770616eb1b075e0020ba26ca
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:21:07 +0000

    that accident mean spread squeeze

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
