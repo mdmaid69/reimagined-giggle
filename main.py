32bcb47717539b247533339378ac88bada527bf6
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:44:25 +0000

    way design shake roll question

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
