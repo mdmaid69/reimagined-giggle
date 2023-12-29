d4d277855272575932d04f11612063a1239dc6f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:41:49 +0000

    swimming worry sleep worth rush

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
