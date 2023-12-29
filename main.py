77040e20be7af0af2fcef114b67680d3c3460457
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:37:22 +0000

    wait sleep shut Ww excellent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
