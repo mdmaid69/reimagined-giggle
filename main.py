c0255fc3247689a1b0868c440f87e55dbd1136e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:42:09 +0000

    takeoff recover eye yesterday surprising

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def subtract_numbers(x, y):
+        return x - y
