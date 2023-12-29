ee16dbd1c9a0f703d19fee89cbe16f3d833ef78c
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:59:46 +0000

    this nearly survive contents pin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Characters:", len(text))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
