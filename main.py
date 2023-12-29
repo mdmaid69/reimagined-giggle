bf6e5f261f312640095b5954e8d40765588c9b19
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:51:47 +0000

    positive find let keen please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
