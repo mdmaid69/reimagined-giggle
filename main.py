4ffa2a8f40fc12fccf1851565e1873503ca5c36d
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:46:50 +0000

    Nn sure authority conversation check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
