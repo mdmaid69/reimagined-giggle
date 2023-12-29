a91c80eb1c891bb5df34e4c06705a7b2c56aa6ca
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:54:04 +0000

    ourselves connection certain moment please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_to_octal(n):
+        return oct(n)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
