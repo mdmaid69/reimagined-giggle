74c38af9df8a1de2ed14906b849500efac957e82
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:52:58 +0000

    impressed order further sort judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
