9c2f9d238f5addb0b6a1da9b0564fcdd653b920a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:27:57 +0000

    taste trousers her union mystery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def remove_duplicates(lst):
+        return list(set(lst))
