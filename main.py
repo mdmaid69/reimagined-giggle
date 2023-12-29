0e99a38d5fd0a9606104b06ac1d578428cf6f26d
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:15:07 +0000

    journey tune aim advanced person

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
