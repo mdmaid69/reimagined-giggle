548f09b111ef16804548c870f80ab0718d7fd528
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:43:08 +0000

    sensitive behaviour take escape luck

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
