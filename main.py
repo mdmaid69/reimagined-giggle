6fd055c0488f814d2ab5264a2dc9db1ffa26c677
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:10:52 +0000

    arm giveup wet chance last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
