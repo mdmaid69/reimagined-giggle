de154c7a43a362d88766dbf80e8f6cf8afe3fe5c
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:23:56 +0000

    avoid skirt blow skin familiar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
