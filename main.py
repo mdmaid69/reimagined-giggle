12692f468f21eed998ba36aea8dc514577f60ac3
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:38:03 +0000

    concerned fit 565end play sick

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
