caf6ae957cc648cd6091d5f4692cbc37a1318c9b
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:17:57 +0000

    almost translate better acceptable expression

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def sort_numbers(numbers):
+        return sorted(numbers)
+def reverse_string(s):
+        return s[::-1]
