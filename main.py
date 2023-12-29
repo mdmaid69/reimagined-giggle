cfe113a20bc7d5994e3ee72f7d5d45ac3c101699
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:15:00 +0000

    shall connect conversation refuse style

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
