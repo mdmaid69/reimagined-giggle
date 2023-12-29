474431c9204d12e8bd10451f5f37f3b87b3356c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:49:15 +0000

    bottom right long contact please

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
