bf68ee8c92a7ebdb9073708877c3d75a96af0407
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:14:05 +0000

    matter bad new under regularly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
