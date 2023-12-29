ce2f679129f2f330ef828c33335a93ca28d07cbb
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:50:20 +0000

    visit familiar cost meanwhile many

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_as_set(array):
-        return set(array)
+def find_max(numbers):
+        return max(numbers)
+import getpass
+def get_username():
+        return getpass.getuser()
