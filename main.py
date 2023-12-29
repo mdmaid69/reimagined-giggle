b27935082ed8401c0c8dd126dd2bb06598cea71b
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:13:07 +0000

    small sweep giveout finally concentrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
