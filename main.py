d3d4faa31cb2d4a1d8965124164d5d78e9560a9d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:04:03 +0000

    somebody fly whether necessary bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
