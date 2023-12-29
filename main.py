aa52b4740c4e574a5cfb451038d4f2b12615b781
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:38:10 +0000

    work add waste behind general

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
