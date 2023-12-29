8091e4abb62d761d92f033cc863e8c62b0986f88
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:36:54 +0000

    average example more century pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+import getpass
+def get_username():
+        return getpass.getuser()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
