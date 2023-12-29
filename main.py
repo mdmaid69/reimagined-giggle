1afc5b52f321bd2155bc1c17f03c63f169bc2409
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:20:26 +0000

    extraordinary turnout listenfor terrible cruelty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_bool(array):
+        return bool(array)
