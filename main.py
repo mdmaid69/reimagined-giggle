dd51424e1ce8a5c87284e02e4960518550a843f3
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:17:50 +0000

    greeting stir weak answer another

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
