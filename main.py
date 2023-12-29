a35b6b650bc7e73872ad13d4e85b2ba1c58ebd2e
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:50:38 +0000

    actual able present bringback second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
