5c114ae9338fa01c882d80f5a9dfa884a7812828
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:52:32 +0000

    vision replace answer majority yes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def find_min(lst):
+        return min(lst)
