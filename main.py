82278f7e6fc3a5ea1334e85a63d4e86fbc6fa831
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:12:27 +0000

    protect comfort moment serve model

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def subtract_numbers(x, y):
+        return x - y
