373440bf7e5d9782d75528ee5044e980a746d5a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:57:36 +0000

    common total round through punishment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
