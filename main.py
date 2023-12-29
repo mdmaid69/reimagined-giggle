54707a3ee4b75323a34c36226a1950f1683c32e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:22:56 +0000

    disappointed building wild goback(to) temperature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
