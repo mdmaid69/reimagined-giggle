7c8972c7f5af9e3b6a377d5f81300a37401a0af5
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:28:21 +0000

    slight careful connection order willing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
