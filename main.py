ffc0996f08ef728d911b0fd7455237519e921133
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:36:44 +0000

    broad plate trick sharp else

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def find_min(numbers):
+        return min(numbers)
+def calculate_speed(distance, time):
+        return distance / time
