893fc110c56aba04e0335ca000deeb8ae8d0c884
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:56:51 +0000

    division shrink rate nonsense help

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def count_elements(lst):
-        return len(lst)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
