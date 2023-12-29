604e57702b54b73800a9a2febd0d471b0927de3b
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:23:00 +0000

    legal callup seriously die detail

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
