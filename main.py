377d514c3574c805146bf5a4599b03a520196220
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:47:43 +0000

    would with talkinto gap giveout

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
