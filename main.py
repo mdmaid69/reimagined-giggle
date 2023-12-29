b39b3ab1cbf3aefebf25b4c0185084b8e10dfd55
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:24:53 +0000

    appearance rush sensible 568energy chief

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def set_array_item(array, i, item):
+        array[i] = item
