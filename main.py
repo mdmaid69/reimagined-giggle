a75d6541a75c21e07a112e954a5a435fa80b4a9c
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:27:18 +0000

    Tt fasten risk fine tune

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
