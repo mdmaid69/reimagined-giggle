93f27613e50dc2b7a6c52f54a4698b07e1fe020f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:12:25 +0000

    term lay attempt reaction well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
