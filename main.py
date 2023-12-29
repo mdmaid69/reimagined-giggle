6b9bfd014e7dd91fffa45ce615cdf9ca7737379b
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:01:21 +0000

    convinced bar meal usual mad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_tangent(x):
+        return math.tan(x)
