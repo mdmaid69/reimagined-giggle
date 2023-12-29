e8747ce5a5178fcdaddf2eb04414e759661ccdcc
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:48:48 +0000

    dare slightly successful switch century

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
