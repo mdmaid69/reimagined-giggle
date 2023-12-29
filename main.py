ffd44bb6d8b04d10540518be67d72b6c95d8a279
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:55:44 +0000

    choose personal head acceptable vanish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
