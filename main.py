a38d9af1174fa0896e733781d6645b5d87e60269
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:33:23 +0000

    display weekend concerned flow effect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
