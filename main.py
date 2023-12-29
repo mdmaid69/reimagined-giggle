ded15793cd5c626db7948453e6289061f6266106
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:17:14 +0000

    name repeat need please then

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
