05d83dd1892d8b0fdf8c1dedcda67b13c0b4b13b
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:08:46 +0000

    science machine used live better

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
