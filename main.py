cf500f0a40350d0c2d786b65d354b975cfe459b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:11:51 +0000

    small towards impatient high ill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
