a39910ed129e7064754db2e9b614f40df45cd998
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:23:40 +0000

    shoot everything tip success attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
