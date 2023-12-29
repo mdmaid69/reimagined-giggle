2e4d1b715c45a8291ee8615b9565b6a3b3a068d1
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:15:46 +0000

    serious promise speech border disapprove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
