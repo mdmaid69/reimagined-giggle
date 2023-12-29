9afe4ca0a0be3cfcea1f7bb14c919d8d413067e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:51:19 +0000

    fun callout workout century simple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def change_working_directory(path):
+        os.chdir(path)
