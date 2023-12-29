94ae33919dfdbf317c0ccef428d72f1b6b66e7de
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:08:00 +0000

    on pour exciting unusual little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
