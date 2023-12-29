3951643aee12a5429c997a202befc65eb9a46c95
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:15:10 +0000

    search change contact female most

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
