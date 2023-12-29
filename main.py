d46cbea996d832045e19aeecf3da45b2c1908063
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:48:24 +0000

    hard breakdown public dick steady

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+for i in range(5):
+        print(i)
