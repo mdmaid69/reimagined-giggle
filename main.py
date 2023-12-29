a9fc90ed2d1f2d50c6755dc3dfbab2f35b6d35df
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:19:56 +0000

    ocean offence anything pen dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
