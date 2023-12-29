3286885dfa938c1ef5fc72a862f687a86e27be30
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:19:31 +0000

    miss university stage deep control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
