a8c94f4d4a0628259feeda919a8d36fa6aeeffca
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:47:33 +0000

    direction across quiet wear accommodation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
 import os
-def change_working_directory(path):
-        os.chdir(path)
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import time
+def get_current_time():
+        return time.ctime()
