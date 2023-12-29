07ab13381468a8a9daa823115c43ba38f91d8150
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:26:32 +0000

    mark limit heavy realize determined

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
