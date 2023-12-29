0e140bb91b291c257cb3b55cebc29a7d8df493c5
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:11:38 +0000

    try invite small appeal talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
