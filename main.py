d0903f443568e69da933cec9fa70f83afdaa3fa3
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:32:00 +0000

    connected wish worse load on

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
