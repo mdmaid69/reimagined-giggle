67c7d0edc08b05a5d21320315a775630094307ca
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:00:00 +0000

    if circumstance until list breakinto

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import collections
+def create_user_dict():
+        return collections.UserDict()
