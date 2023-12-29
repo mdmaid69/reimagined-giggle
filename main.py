c06aae239d5c8db63d59374ba3e596c98950ef02
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:36:19 +0000

    necessary destroy later sick programme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
