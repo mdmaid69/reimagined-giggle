e63ecf6b20d55e2452485cf3c4a8956238326fc8
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:41:57 +0000

    sweep decision part answer central

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
