029a267582412262ce1252c48aeac53342aacdb9
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:34:12 +0000

    cool ahead claim bored ride

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
