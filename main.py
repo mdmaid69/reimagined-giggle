f986f0f06e6363221a15570b17f2d55d119d5330
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:53:23 +0000

    high afternoon face nigga factory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
