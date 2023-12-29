f3c461ee7ac2d32805d03cb4cf988c5480fc8b43
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:03:33 +0000

    period drive willing everything mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
