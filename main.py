a0959146a40d69ff9c48989c6118fce425e1cc91
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:23:05 +0000

    variety paper wind mark dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
