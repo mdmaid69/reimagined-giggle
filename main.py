d296fdbd8b1f1c0ff625a56efc5d5168329c3d0e
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:55:23 +0000

    basic promise necessary free total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
