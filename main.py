6b276a6aba14ad406f121c03d8c36c412ea18e18
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:44:49 +0000

    large fairly proof wonder grand

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
