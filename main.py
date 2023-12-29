2632855ffa04f82ce1928c06739d8cd4f4115599
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:53:14 +0000

    prefer wave attract sudden level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
