04fd80d7c49f8579c6e25d3484047db0ca86265a
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:06:05 +0000

    deep beautiful university row stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
