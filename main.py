170fc0df47ea3dd7a52851810c3c11e1cc7ed0f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:30:32 +0000

    still mean neat shine warm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
