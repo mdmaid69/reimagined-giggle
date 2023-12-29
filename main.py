a4e52706043dfd1fcc8647a7384410361137a75c
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:30:08 +0000

    neither upwards realize trade flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+def get_today_date():
+        return datetime.date.today()
