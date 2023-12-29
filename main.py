e7dae423a6eb0dd6dba6d0bf9bb23023afa8d92a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:50:55 +0000

    fence negative birth point once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
