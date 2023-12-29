b17aa112be8b6ea735feccab733287a4c96a8cf0
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:11:34 +0000

    murder sister give Zz scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
