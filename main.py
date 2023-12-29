d180c61b8cd35e622a52ea9a17c0cf5a93107e51
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:43:26 +0000

    himself pick pity to whom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
