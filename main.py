2b4989f549b15b9604d34dbbbec04efcf9b053d1
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:11:50 +0000

    correct necessary instead dead female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
