fa4c28b3e200c9dbbd832441a75af9c611364656
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:24:13 +0000

    sensible land commit pleasure exercise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
