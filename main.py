49d788857b3319b6baca2ed115335b4d127228ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:46:16 +0000

    computer again equal for smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import platform
-def get_python_version():
-        return platform.python_version()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
