ab7a52a517d1c281d0a7badb562b0ab019c307c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:54:24 +0000

    street decide gas replace arrangement

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import random
+print(random.randint(0, 100))
