7751c48e9a8d75da83c59e315986a5962b1ff35e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:43:14 +0000

    music always rent pot sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
