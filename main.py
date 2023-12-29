87f0bb240615cff119dff60b5b44d4a713cf7644
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:55:38 +0000

    interview urgent bite though tradition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_hex(n):
+        return hex(n)
