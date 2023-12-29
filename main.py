00d096173ad90b8bdcef4bb917dc933ff56faaae
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:37:15 +0000

    OKorokay live joke lose company

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
