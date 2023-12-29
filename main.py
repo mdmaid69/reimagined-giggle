f8112e0f7b45c24f12d2d20a057c4398d94e061b
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:05:19 +0000

    possession goodbye shame popular extra

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
