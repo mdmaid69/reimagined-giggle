1cd1dac77a8f501acffcbcf9ae87e0aefbbe2b27
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:36:28 +0000

    whistle approval knowledge control worry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_current_directory():
+        return os.getcwd()
