f121f2c2d934c469352e529b8a9a5135a47a7509
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:30:19 +0000

    ocean floor life case tongue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
