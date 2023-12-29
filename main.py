7e10208eec73ce1e8dbdb516fd4c346716f06238
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:05:57 +0000

    attempt even middle summer lead

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
