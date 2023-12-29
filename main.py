649a6f65e8d92ea90314013ecb6d8d178b195a26
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:08:32 +0000

    shop breakinto matter turnup up

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
