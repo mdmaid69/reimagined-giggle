312cd949225e82fc411b030d141b78af74ace797
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:04:18 +0000

    fit meet confuse pretty interesting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
