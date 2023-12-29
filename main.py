87c61ff25553c2a885e9b10605eaecaf4f2598c0
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:19:17 +0000

    act mixture thin responsibility artificial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
