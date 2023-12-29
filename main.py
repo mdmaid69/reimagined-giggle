4674fc9134ab41c9870efa9a796466794f0bd140
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:55:30 +0000

    comefrom promise burn now tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
