5006e94337c6c7d1d3ccc848d6c2a92c1522fb07
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:13:21 +0000

    follow ill doctor 179bite section

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def exit_program():
+        sys.exit()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
