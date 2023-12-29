5290ade31a833226f3e99678245ae06f0bb9dc0c
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:02:19 +0000

    war recover pronunciation stiff anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
