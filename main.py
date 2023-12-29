58b1f35ee034c1ee02e82a444ee49b3754b81dda
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:03:17 +0000

    national damage break birthday car

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
