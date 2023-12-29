44613e2c64e69acc5c209f68de0dc6d084328c1d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:47:56 +0000

    unable run past some in

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
