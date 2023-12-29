1390c9e7688a13009791a12dabc808c1ffd10143
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:56:28 +0000

    miss rent respect yourself climb

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
