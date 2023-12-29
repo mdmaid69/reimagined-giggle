760c547d401d54d3b6d89384f0400014d4887b60
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:56:56 +0000

    far little come control cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
