81061f57f160f202be5224c1576f722f3c2e463f
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:59:00 +0000

    ago job wet actual matter

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
