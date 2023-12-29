6bef45f82a68c645f585787cb69aacdb84a8a0ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:05:40 +0000

    bring Bb book forgive agreement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def count_elements(lst):
-        return len(lst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
