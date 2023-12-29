0ff6d4c6a050c86bdd0c78bb19cb2b601dda0947
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:08:55 +0000

    top use on ready inquire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def find_min(numbers):
+        return min(numbers)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
