c8fcd75a4381309a032c1677f7578094bab031e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:32:55 +0000

    probably rain regularly Dd hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
