64028e15cd7fb435a927e355a8a356aaa6bbf728
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:09:10 +0000

    beside state on surprised concerned

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import tensorflow as tf
-print(tf.__version__)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def count_characters(sentence):
+        return len(sentence)
