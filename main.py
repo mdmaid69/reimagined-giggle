dc0af9b5715ffed8fc03c237ddb4ce2cf001b545
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:46:47 +0000

    have wander decide design great

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
