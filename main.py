ed55cb922562149020357a34d99369f20774ae91
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:27:19 +0000

    against terrible copy serve tax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def count_words(sentence):
+        return len(sentence.split())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
