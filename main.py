133fc636fb76c663d3e450cce56ac99404cc12aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:19:56 +0000

    other mind sort position whatever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
