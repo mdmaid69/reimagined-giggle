c69e71d19a47bb23535307e844b4b685829d099b
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:23:02 +0000

    high juice moon office tense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
