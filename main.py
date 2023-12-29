39c6e12b059bd60db74d75b244b31c589d90d3fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:49:15 +0000

    bath between equal common bringforward

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def add_numbers(x, y):
-        return x + y
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
