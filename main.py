af927947cf666df0050065354f7cc308264825f3
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:28:23 +0000

    near activity decide bath under

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def reverse_string(s):
+        return s[::-1]
+def reverse_list(lst):
+        return lst[::-1]
