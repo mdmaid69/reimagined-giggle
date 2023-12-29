0e8f96a354b949fb2031230ac8b9ffb3168e5369
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:52:58 +0000

    approval store fit knock evidence

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
