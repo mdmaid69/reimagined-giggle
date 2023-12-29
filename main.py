d2ad234299e069e8b9b9882e7508dcce5f994aa7
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:04:19 +0000

    lonely fold compete time bus

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def find_min(lst):
+        return min(lst)
