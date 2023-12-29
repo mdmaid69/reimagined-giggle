ae8f299eba6c11e88ce00eeb05fe192b8c1b70e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:48:25 +0000

    law sweet commit tour explanation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
