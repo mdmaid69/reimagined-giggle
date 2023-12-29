c0b5dec7f84e347a1da6206ff73c6faf280cb179
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:34:20 +0000

    pupil foot pile something left

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
