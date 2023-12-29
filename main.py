443e45e0301a57dc699b51ccd6d3c4809be6983f
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:01:32 +0000

    old block takeback frighten loud

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-def sort_list(lst):
-        return sorted(lst)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
