5fcb044d6f6692b32ad153f4b86c65c4ee2899fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:15:57 +0000

    receive win market collection flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
