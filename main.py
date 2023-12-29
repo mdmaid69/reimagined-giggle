e36b7f39d51838b17a9085e1cf1c0ef829a3a69b
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:18:08 +0000

    whom share tune book house

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def convert_to_binary(n):
+        return bin(n)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
