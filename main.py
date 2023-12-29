65b3e7d49614b2f5223665d813d5728acea6320c
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:52:49 +0000

    disturb bear production form everybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
