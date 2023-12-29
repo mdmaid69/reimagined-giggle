bf02dd9c620c4d1ab530272d1dea29a44a9e447b
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:51:45 +0000

    firm price type guess impossible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import sys
-  def get_python_version():
-        return sys.version
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def reverse_string(s):
+        return s[::-1]
