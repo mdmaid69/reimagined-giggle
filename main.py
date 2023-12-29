a96514b41e5c5834e51c7c37b68d6512c582185b
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:39:46 +0000

    send success find indeed lay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+for i in range(5):
+        print(i)
