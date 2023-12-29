3679676320061f858779443a97161eee36e1d08e
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:39:09 +0000

    under 566end check tidy support

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
