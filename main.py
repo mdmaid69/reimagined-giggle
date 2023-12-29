da6a362a7941c243122ed5676feaeb91719e3ce6
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:32:15 +0000

    capital sensible day morning unite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
