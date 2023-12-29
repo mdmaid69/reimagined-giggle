7254f5b1a76abf45fd45e2ce5df8a084354fa216
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:04:09 +0000

    complete pour responsibility pay gather

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def convert_to_hex(n):
+        return hex(n)
