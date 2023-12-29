c3736a384873cf39761218e47efc2897f9682413
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:57:42 +0000

    suffer improve easy home must

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
