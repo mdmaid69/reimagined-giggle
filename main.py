d5b5bfa56de070efc2568ab82bce900d9702321b
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:26:11 +0000

    myself compare prize onto curious

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-print(math.pi)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
