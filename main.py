38ad802e4f4bb8e5e8b82fec3259119ceb13607b
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:45:21 +0000

    breakthrough feature wave ball beneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def convert_to_binary(n):
+        return bin(n)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
