476442748ab05f717af9092b9ec17f1e8c1ce2e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:06:38 +0000

    equipment born brown write pocket

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
