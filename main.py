5d63700693e64f04ee344b4a5b146b2f659aa2ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:39:43 +0000

    turnover task best Bb short

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
