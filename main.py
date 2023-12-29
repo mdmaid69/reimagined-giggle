abd1e83b2beb464ecee54631e99326a8ae9ba902
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:53:03 +0000

    breakfast trap getaway shake address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
