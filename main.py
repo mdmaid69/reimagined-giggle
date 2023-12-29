edfd78df2215c1bc102b636beb19e0e45a5a69f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:00:17 +0000

    hers somebody wake mine sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
