55542ea09fa4d52c7d5498618ec504e67b4f8844
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:47:35 +0000

    relative bus dry pause bringup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_list_from_array(array):
+        return array.tolist()
