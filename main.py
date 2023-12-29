8b1b485cfab6b676a51ae0afa6da5dfe18054638
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:55:38 +0000

    anything other exact whom final

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def cube_number(x):
-        return x**3
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
