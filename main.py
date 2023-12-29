85fe3e7b970f9f03925a5bffb84192f8e8645a02
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:31:24 +0000

    force disappointed stage takeover simple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
