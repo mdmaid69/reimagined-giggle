1d24eaeb519cf5033d58fabe9964c609270d641e
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:14:21 +0000

    think every though bother responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
