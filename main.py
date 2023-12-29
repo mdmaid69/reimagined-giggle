0e1e14631a720cd06f30c92a79996c71d927258a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:23:09 +0000

    trap pool write fight crime

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
