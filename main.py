5937232913fb31777e353a604a65ce43f6b6516f
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:20:36 +0000

    fear clear best admire set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def get_array_as_str(array):
+        return str(array)
