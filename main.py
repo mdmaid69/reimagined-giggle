43a6952245abadb2f86a6225f37160f703e099e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:09:36 +0000

    cold believe silent float heavy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_array_to_list(array):
+        return array.tolist()
