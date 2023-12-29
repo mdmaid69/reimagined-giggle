dbcbc3b609d3c55183f00bd9e7f5df07cd03d6a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:16:23 +0000

    into name repair truth luck

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import array
-def get_array_from_string(string, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
