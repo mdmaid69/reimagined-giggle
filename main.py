f24203d78100b05110dddf7ff13fa999e85f8d27
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:35:53 +0000

    your crime strike decide herself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
