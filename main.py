c2e2eb57d5557900e0fb9dcf890ccc2b1a68f66c
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:44:28 +0000

    possibility belief friendly accidentally die

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def add_numbers(x, y):
-        return x + y
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
