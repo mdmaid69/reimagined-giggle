fddbb92f99e2a9e4977bea83f8e835c6c5c2237e
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:26:29 +0000

    affect state education wish hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
