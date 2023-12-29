25551aacab8ff47821816ae6ef3f6cd6b506f85c
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:24:19 +0000

    everybody typical rescue point very

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_typecode(array):
+        return array.typecode
