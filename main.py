a469f759c305b118659f0a078798705700bbda30
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:24:41 +0000

    passage must ago trouble guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
