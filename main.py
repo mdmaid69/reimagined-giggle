622706515cc5eb76ab0767790e3ae825b6c2b45b
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:55:40 +0000

    injury contrast pull real fun

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_current_working_directory():
+        return os.getcwd()
