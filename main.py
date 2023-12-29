de71a7726c008f535b69ba822f1b0573adc24606
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:53:08 +0000

    worried sport runout earn season

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def square_number(x):
-        return x**2
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+print(datetime.datetime.now())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
