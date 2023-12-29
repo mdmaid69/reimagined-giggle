c352b1f96a3c2352d1dbb99b0d51f5597cd90f56
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:47:19 +0000

    our building sudden love talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-print(math.pi)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def reverse_list(lst):
+        return lst[::-1]
