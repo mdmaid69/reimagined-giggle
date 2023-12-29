ad408b8b736c53a926f17dffe8e9aa08c3fc6fc4
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:03:37 +0000

    brush book block expect break

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
