e451767ebba4eaeb0f8f7a4086467991ea4a4055
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:54:26 +0000

    escape apologizeapologise ability birth cut

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_odd(n):
+        return n % 2 != 0
