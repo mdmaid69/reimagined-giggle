03c104b4e1a9192c0d2eeca370c8ec962cce611a
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:17:06 +0000

    turnaway group thing attract change

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
