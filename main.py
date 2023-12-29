69d79f62aba655efdca5268266d8e25c62125e37
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:55:05 +0000

    mess nothing snow building anxious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
