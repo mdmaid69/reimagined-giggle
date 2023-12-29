4884291588d77d9acdbf32b163d4cd10336a6fc3
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:16:55 +0000

    568energy workon Itself declaration bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def convert_to_binary(n):
+        return bin(n)
