c17ebd667f88a5f2fe004f9b8e0491bd8c1f5494
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:49:44 +0000

    interview past brief ignore stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
