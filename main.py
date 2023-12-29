daea7ba751d29536ef4083236bdc6b13b5d25c01
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:26:15 +0000

    over now partner organize avoid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
