4ef3178f4cfac3726c56eb059a49c87841c73a2e
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:33:01 +0000

    equipment hardly label bend contact

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
