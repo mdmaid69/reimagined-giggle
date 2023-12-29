e31b72a5c1295522e4cd9b2b9a9d08d42f410760
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:19:39 +0000

    culture turnaway file actually stick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
