600d08b8e7b5cf6607705ab9a5d7ba912d620b69
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:58:00 +0000

    open visit rain raise favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
