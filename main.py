72f62dafc0a4f297f61feb0d541f450f49b0ddcb
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:45:16 +0000

    573entertainment trick deaf space content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import platform
+def get_python_version():
+        return platform.python_version()
