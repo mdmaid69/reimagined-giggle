fa1ce98cf028472e2ea39e0b57b8e628b340a57c
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:40:23 +0000

    immediate trial computer back match

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
