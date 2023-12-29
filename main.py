201f0e6d30176ed0b359e4b8b6d8b1896d38fac7
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:38:33 +0000

    increase help rely original black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
