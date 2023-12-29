da0c3921b52ac965dd295966843b07b4f40d2ebe
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:10:37 +0000

    proof promise sure board group

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
