eb440f6d9ebe97924f7c79cf00c50e9a623c43bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:24:14 +0000

    pocket last vanish drawing getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def sort_list(lst):
+        return sorted(lst)
