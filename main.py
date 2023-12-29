73fb5032c926bb1e5ca5bc07d8dd08bdec47fc4e
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:55:35 +0000

    recommend building bury pleasant famous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
