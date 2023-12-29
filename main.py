2c5fa6ad7e74bfb51ad99c2558c8a9b965c924e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:26:05 +0000

    close sail getoff quick shame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
