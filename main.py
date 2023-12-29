d75e2618816bc223157db9a08394c80133f2f1a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:14:41 +0000

    bottom whistle start promise prove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def cube_number(x):
-        return x**3
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
