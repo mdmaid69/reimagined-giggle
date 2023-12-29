90f56f4db19f06b3a5407628d2dd026a5b9ce448
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:21:01 +0000

    turnon callup advise earn breath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
