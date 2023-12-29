6a5cb785d13e1b648ff02e616bd7203f69725363
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:58:56 +0000

    pinch except squash comment communication

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_directory_name(path):
+        return os.path.dirname(path)
