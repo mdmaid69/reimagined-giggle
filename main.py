6429fe38a2a0dcb8b25b4aa7b20f3b6f0d151ce7
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:48:46 +0000

    play OKorokay then basic shoulder

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
