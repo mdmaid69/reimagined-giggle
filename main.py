f2330fde82b7e584bfd8946dd7ced9a7838e7649
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:34:39 +0000

    behind which hungry crime hope

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_directory_name(path):
+        return os.path.dirname(path)
