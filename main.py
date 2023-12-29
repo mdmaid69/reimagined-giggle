964480ae4b83fe1199d1c934f5d2f020e942bf17
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:45:46 +0000

    breakdown pleased respect bag somehow

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
