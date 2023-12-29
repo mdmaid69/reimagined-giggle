9b4aacc791db61402082ef4bec68792bbd25297b
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:52:58 +0000

    charge demonstrate suggest fit combine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def greet(name):
+        print(f"Hello, {name}!")
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
