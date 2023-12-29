bed39d4f217ad5ef83b6c630f9abc0e68b22f2e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:32:47 +0000

    consider care official 573entertainment Yy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_itemsize(array):
+        return array.itemsize
