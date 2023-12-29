06df1830ecbc0053c6b8a1a2cf30c7732a2a64f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:50:00 +0000

    attempt affect contact science street

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def count_elements(lst):
+        return len(lst)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
