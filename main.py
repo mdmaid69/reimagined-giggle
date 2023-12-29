465ace4ac6512b684a06ad19dd6c4da72b91c1b5
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:04:21 +0000

    shrink fill any seat punishment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_current_directory():
+        return os.getcwd()
