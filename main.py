9c1b72f6c420663ecbf60863a323bcbc7c2493c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:32:36 +0000

    sudden print illness balance best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
