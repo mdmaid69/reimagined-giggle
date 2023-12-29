fc2730d82bb45df413c07702fcef5b4b08f2e38a
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:52:55 +0000

    she reserve reduction the cheek

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_int(array):
+        return int(array[0])
