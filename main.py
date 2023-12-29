c276a144b0f36109cc91d2ac36ad1523512a6595
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:47:14 +0000

    nothing turninto vote own store

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def count_words(sentence):
+        return len(sentence.split())
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
