0a183dca81f91a0276f2277e30eea88495519fea
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:33:13 +0000

    direct machine file control arrival

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
