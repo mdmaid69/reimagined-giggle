4673197b8d81ab392c25b8274184f82acca19325
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:16:50 +0000

    careful exact dance relax mystery

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
