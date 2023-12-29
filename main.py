3617249c9a9b0a96b343207feebfb3b22336dbd0
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:26:57 +0000

    entry lock scream job action

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
