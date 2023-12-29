ddfd9a4aa7c082a63b2450b30fd89b5ff8102196
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:16:31 +0000

    shout target height strange deep

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
