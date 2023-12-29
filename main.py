7e38d8741fe83515cb2c8012304d073751e01e1b
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:25:02 +0000

    side claim disappear course far

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
