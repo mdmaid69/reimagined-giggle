4daa0bb8d3319ac415a313ae09329207beb51535
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:01:39 +0000

    needle spend tough seriously back

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def square_number(x):
-        return x**2
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_density(mass, volume):
+        return mass / volume
+def is_odd(n):
+        return n % 2 != 0
