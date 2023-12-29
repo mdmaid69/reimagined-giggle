f187ebd69deab359133f74af2ba7637a538ef2ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:28:14 +0000

    smoke illness expression whenever male

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
