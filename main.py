153726065be237562188cce6b18dcba5be6243d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:01:49 +0000

    we breakout morning today abroad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
