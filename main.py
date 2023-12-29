a86803ca73a9c42846cd457c86b44cde78cc8259
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:02:01 +0000

    contents nowhere sure row determined

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
