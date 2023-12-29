1a41cd1139275180d91bba90054d0ab72717660c
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:36:38 +0000

    embarrassing display best forgive impressed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
