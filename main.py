501effc7a476c197ba47c26131a589b0617c5843
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:23:35 +0000

    occasion fit profit show pair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
