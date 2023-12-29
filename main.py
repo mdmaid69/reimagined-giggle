6a9dd589d4b6a355fbca61da3eafb64942ca552c
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:27:22 +0000

    evidence practice needle can rent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
