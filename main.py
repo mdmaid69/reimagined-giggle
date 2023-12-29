7f49d9431a3b0ad76eb934cbbb58f2378e9fb5bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:20:36 +0000

    relationship quit introduce lonely can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def find_max(numbers):
+        return max(numbers)
