bb2e71b47362a7184dc3a750f5cb5d1e053fbae9
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:01:34 +0000

    tap reserve control swallow position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  def is_even(n):
+        return n % 2 == 0
