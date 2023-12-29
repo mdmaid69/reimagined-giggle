221e82b685fa5f492c96a16c41d2b169c300a57d
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:56:29 +0000

    makeinto accidental appear dream pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
