5bd2970b3ba0401ea36233e92b6bc7b5ba0a87dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:10:45 +0000

    lookback(on) test comefrom mysterious Pp

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
