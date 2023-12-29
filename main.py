5bedc98ba5fd208f1b93d2b2bb9b657c1b96cc34
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:56:14 +0000

    blame book neat fortunately defend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_min(numbers):
-        return min(numbers)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
