6a52adccfce13972af687a2079a02dc2a6fdf5b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:20:47 +0000

    unable sign search boil mistake

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
