1c81a43eebbd9be1a6da7cc123d8108cb5e80f2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:22:02 +0000

    lead on cough congratulate straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
