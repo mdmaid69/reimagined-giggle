6faceac51cf867ba8b10b6925bd16d5f5958ea09
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:49:19 +0000

    justice healthy otherwise plan progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
