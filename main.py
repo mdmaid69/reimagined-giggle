efded9b4ecb91ba6ed9a509fd26139dc457980c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:30:41 +0000

    someone matter separate excellent walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
