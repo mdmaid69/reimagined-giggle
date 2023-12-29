0788b294212b620fa9eeaa3c633eaf12c5cb4c55
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:50:47 +0000

    outside aware speed operate boring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import platform
-def get_os_info():
-        return platform.uname()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def reverse_array(array):
+        array.reverse()
