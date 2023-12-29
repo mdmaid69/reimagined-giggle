0f89fa25969bae773ef525dfcc9d4c509ea3c46b
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:50:42 +0000

    shock comeround delight pick here

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
