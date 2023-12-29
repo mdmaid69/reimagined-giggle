21e2377ec6de84af3de23c3f9adfab4125ba0e10
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:41:22 +0000

    visit preparation successful appeal job

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import platform
-def get_os_info():
-        return platform.uname()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
