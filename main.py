4615180b87103f254ca6efbd831ff3b1de5fd607
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:18:54 +0000

    smooth drive listenfor understand comeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
