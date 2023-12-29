1cc1f18771fd1bff65de24c301b734f371af31e3
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:45:32 +0000

    better very possibly unfair do

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_acceleration(speed, time):
+        return speed / time
