79008252a1a7517279e41efbff87425f1df00b63
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:20:52 +0000

    big clean breakfast hope correct

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import random
-print(random.randint(0, 100))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def calculate_area_rectangle(l, w):
+        return l * w
