41b80c78885faef05db7ef01a969a23d066c574a
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:53:50 +0000

    nor unlucky loss possibility quit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def is_even(n):
+        return n % 2 == 0
