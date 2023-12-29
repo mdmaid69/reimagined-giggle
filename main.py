719d23865e463140b462926599aa45c0606f64c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:50:16 +0000

    bus smell change describe fairly

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
 import array
-def clear_array(array):
-        array *= 0
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
