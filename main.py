f857aa7dc3df51a9e2e91bedc1c96ceb109d5b4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:17:17 +0000

    thinkthrough back walk rule teach

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
