c73dca46fe7bba433b48169a0e7b0d811d049a19
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:34:50 +0000

    communication rush disappointed anywayanyhow book

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import platform
+def get_python_version():
+        return platform.python_version()
