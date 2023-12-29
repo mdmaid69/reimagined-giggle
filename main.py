3fddc3784c9e07995951268bc1ebe702c0dc9272
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:47:31 +0000

    agree fault port play legal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
   import sys
   def get_python_version():
         return sys.version
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
