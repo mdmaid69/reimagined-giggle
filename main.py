78d42a6e780855be5f62b842dbb80243d2199b16
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:51:07 +0000

    embarrassing sting comeround someone deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
