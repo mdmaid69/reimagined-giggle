6cd071aa1c37154b1c6642c9d23328d3d7468d56
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:58:55 +0000

    total whoever turnup issue total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import getpass
+def get_username():
+        return getpass.getuser()
