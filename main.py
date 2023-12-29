96d9b5090dbe3570b21fe5091c1b65cc701bbbba
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:05:59 +0000

    by treatment bottom tip share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def get_current_time():
-        return time.time()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
