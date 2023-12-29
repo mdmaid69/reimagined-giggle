7ac50193cc46cfb3a0a44d2cac292f2ee2f2bd71
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:09:57 +0000

    top powerful telephone mixture constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
