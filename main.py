77c8f3b473689ef9910e44e729606382e3641527
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:32:48 +0000

    contact run getoff callback left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
