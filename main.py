9eb11b55a01b2388c6f7f272f6761f8b567526fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:56:48 +0000

    keepfrom cruel dream objection fierce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
