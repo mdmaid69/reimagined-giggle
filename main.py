986caab86f7ba45f54f99d9f9fd4426eed081b84
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:37:43 +0000

    last hold mess difference less

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
