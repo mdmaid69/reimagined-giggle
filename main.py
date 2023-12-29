e87d7b0a970b012e70ebacf908db1e505e823d3b
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:16:03 +0000

    respect soon plan back promise

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def count_elements(lst):
-        return len(lst)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
