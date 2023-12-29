b6c5fe0e3a879a49ef94f3e83894ca110702ce64
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:47:09 +0000

    meal special last flow content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
