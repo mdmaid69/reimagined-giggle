158792a54f653bbab8e4efa61be5374102314d97
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:11:20 +0000

    post their cry lead noise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
