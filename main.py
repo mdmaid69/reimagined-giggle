42fb181e0cd7b136b8adad894d66ec634b78b573
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:51:52 +0000

    Uu split remain genuine tradition

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def reverse_string(s):
+        return s[::-1]
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
