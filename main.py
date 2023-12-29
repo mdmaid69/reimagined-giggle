0befa52f99b99a7ce3941ee24678d6cf79725c4b
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:17:41 +0000

    deal either lookup authority without

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
