d64548f8984245a2fdf20eb7631e415d9d5f8265
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:28:51 +0000

    getaway great bye beat expensive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
