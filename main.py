290489b9de5beddef9d7f709266d3a880c83c57c
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:38:04 +0000

    apologizeapologise frighten rent game join

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
