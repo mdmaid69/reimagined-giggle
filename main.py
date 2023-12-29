5969a620fa7fe829b8217da4185707cb59415e2c
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:39:58 +0000

    show sudden act must hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def find_max(lst):
+        return max(lst)
