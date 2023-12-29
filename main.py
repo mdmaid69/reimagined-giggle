c872b654cdc85533dc4251d6e5e570c59c66badf
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:54:44 +0000

    offer section doubt word same

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def convert_to_hex(n):
+        return hex(n)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
