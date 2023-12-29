28571bba4293a6c616f084a9821ddfdb427a352f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:31:16 +0000

    secret Nn apply pull tune

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
