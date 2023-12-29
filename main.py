0bd0c9da552f57e520f11d94c4e79d574999e630
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:12:24 +0000

    equipment impressive convince many sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
