04bbf9f5ddcf7165b62e1c8d60521fcdaa95aaa1
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:36:55 +0000

    used free piece order pin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
