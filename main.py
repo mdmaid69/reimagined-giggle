4591cf191c5ee30c80b1e65dfd993de68279342f
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:36:10 +0000

    promise fact collection dream fresh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def is_odd(n):
+        return n % 2 != 0
