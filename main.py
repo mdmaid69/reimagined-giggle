8062f74ace34d5b21b09d9b39eda6549e4470f4b
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:05:06 +0000

    rest equal who fresh solve

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_work(force, distance):
+        return force * distance
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
