8651f2c392dc8d4c442f24d24d1f2278f2feb8fc
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:35:57 +0000

    block runout block minimum early

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def multiply_numbers(x, y):
-        return x * y
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import platform
+def get_python_version():
+        return platform.python_version()
