6c596ce941093ea999a40e319aa55156818c4aef
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:27:12 +0000

    accidentally heart climb sad whichever

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
