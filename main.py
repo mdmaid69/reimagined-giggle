5b39c087abb1d4e1df37fe3eeda8b0b9d4383d85
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:19:50 +0000

    task legal lot Zz congratulations

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
