c7f145de85ac7d88eb6294f1738711d11fdd0824
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:08:07 +0000

    shopping nor female start Bb

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import time
+print(time.time())
