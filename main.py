2a230ece510a561dc871f3c4d169bdee3a20e4d0
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:51:15 +0000

    ear patience sensible guess really

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def add_numbers(a, b):
+        return a + b
