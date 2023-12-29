e9fd92a840b1064dd5f4a116d074b8a4bd9dd575
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:05:47 +0000

    my concentrate table picture Ee

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
