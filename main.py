5c1edbc92becf23060ef213810cb4240388967ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:42:40 +0000

    talk Ww rate arm trap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def add_numbers(a, b):
+        return a + b
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
