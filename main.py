ce3ae6695d9dd5f5c978973064719336df36095c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:45:02 +0000

    perfect colour climb more polite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
