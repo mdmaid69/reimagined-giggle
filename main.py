48125d1bde8fec2eb17affc1d4e15ca3c98f864a
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:43:59 +0000

    collection 572enter coat advise tooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
