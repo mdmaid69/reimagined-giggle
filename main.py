f53b6dbe89c1523e640631bfd6d1f821d00689a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:30:19 +0000

    punish disgusting answer increase concentrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
