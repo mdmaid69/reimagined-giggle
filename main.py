5ed0c4c58eeb1397eb8815fcd23974a6fc51430f
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:01:58 +0000

    university guess away somewhere runaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
