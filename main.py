faeb33700070d9c2eba7ace6a354aa6c05e059f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:45:44 +0000

    report one type artificial sport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
