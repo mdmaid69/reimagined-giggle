1b5303b071398dacedbd4af34443f2e3493fdd26
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:22:07 +0000

    suppose wind minor nothing blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
