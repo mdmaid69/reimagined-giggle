68ac33199474318d3dc693ebb1d7d47b08d269a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:39:24 +0000

    development include value progress hand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
+import platform
+def get_os_info():
+        return platform.uname()
